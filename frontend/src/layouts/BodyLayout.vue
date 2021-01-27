ffba726d636856fab012fa4343af79c4a1eca4f0
Tianzhou Chen <tz@bytebase.com>
2021-01-28 01:47:14 +0800

    Improve comment for BodyLayout

--- frontend/src/layouts/BodyLayout.vue
+++ frontend/src/layouts/BodyLayout.vue
@@ -1,32 +1,11 @@
 <template>
-  <!-- This example requires Tailwind CSS v2.0+ -->
-  <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
   <div class="h-full flex overflow-hidden">
+    <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
     <div v-if="state.showMobileOverlay" class="lg:hidden">
       <div class="fixed inset-0 flex z-40">
-        <!--
-        Off-canvas menu overlay, show/hide based on off-canvas menu state.
-
-        Entering: "transition-opacity ease-linear duration-300"
-          From: "opacity-0"
-          To: "opacity-100"
-        Leaving: "transition-opacity ease-linear duration-300"
-          From: "opacity-100"
-          To: "opacity-0"
-      -->
         <div class="fixed inset-0">
           <div class="absolute inset-0 bg-gray-600 opacity-75"></div>
         </div>
-        <!--
-        Off-canvas menu, show/hide based on off-canvas menu state.
-
-        Entering: "transition ease-in-out duration-300 transform"
-          From: "-translate-x-full"
-          To: "translate-x-0"
-        Leaving: "transition ease-in-out duration-300 transform"
-          From: "translate-x-0"
-          To: "-translate-x-full"
-      -->
         <div
           tabindex="0"
           class="relative flex-1 flex flex-col max-w-xs w-full bg-white focus:outline-none"
@@ -56,6 +35,7 @@
               </svg>
             </button>
           </div>
+          <!-- Mobile Sidebar -->
           <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
             <router-view name="leftSidebar" />
           </div>
@@ -118,7 +98,8 @@
       </div>
     </aside>
     <div class="flex flex-col min-w-0 flex-1">
-      <div class="lg:hidden">
+      <!-- Static sidebar for mobile -->
+      <aside class="lg:hidden">
         <div
           class="flex items-center justify-start bg-gray-50 border-b border-gray-200 px-4 py-1.5"
         >
@@ -151,7 +132,7 @@
             <Breadcrumb />
           </div>
         </div>
-      </div>
+      </aside>
       <main class="w-full mx-auto lg:flex overflow-y-auto">
         <!-- main wrapper -->
         <div class="lg:min-w-0 lg:flex-1">
