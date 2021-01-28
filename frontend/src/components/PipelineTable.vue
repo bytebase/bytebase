814e76ceec024da8591c8a575202ff50bf4b4f9a
Tianzhou Chen <tz@bytebase.com>
2021-01-29 03:42:06 +0800

    Use check instead thumb for done state

--- frontend/src/components/PipelineTable.vue
+++ frontend/src/components/PipelineTable.vue
@@ -42,14 +42,17 @@
           </template>
           <template v-else-if="pipeline.attributes.status == `DONE`">
             <svg
-              class="w-3 h-3"
-              fill="currentColor"
-              viewBox="0 0 20 20"
+              class="w-4 h-4"
               xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
             >
               <path
-                d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z"
-              ></path>
+                fill-rule="evenodd"
+                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              />
             </svg>
           </template>
           <template v-else-if="pipeline.attributes.status == `FAILED`">
