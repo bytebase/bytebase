659648e96ff52a4bf62f560f814e606f3540b93e
Tianzhou Chen <tz@bytebase.com>
2021-01-29 17:48:13 +0800

    Use animation pulse for the running state
    PENDING and RUNNING share the same icon to make it more consistent

--- frontend/src/bbkit/BBStepBar.vue
+++ frontend/src/bbkit/BBStepBar.vue
@@ -27,27 +27,13 @@
             ></span>
           </template>
           <template v-else-if="step.status == `RUNNING`">
-            <svg
-              class="h-6 w-6"
-              style="animation: spin 2.5s linear infinite"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                class="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              ></circle>
-              <path
-                class="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
+            <span
+              class="h-2.5 w-2.5 bg-blue-600 hover:bg-blue-700 rounded-full"
+              style="
+                animation: pulse 2.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+              "
+              aria-hidden="true"
+            ></span>
           </template>
           <template v-else-if="step.status == `DONE`">
             <svg
@@ -133,7 +119,7 @@ export default {
         case "PENDING":
           return "bg-white border-2 border-blue-600 text-blue-600 hover:text-blue-700 hover:border-blue-700";
         case "RUNNING":
-          return "bg-white text-blue-600 hover:text-blue-700 hover:border-blue-700";
+          return "bg-white border-2 border-blue-600 text-blue-600 hover:text-blue-700 hover:border-blue-700";
         case "DONE":
           return "bg-green-600 hover:bg-green-700 text-white";
         case "FAILED":
--- frontend/src/components/PipelineTable.vue
+++ frontend/src/components/PipelineTable.vue
@@ -18,27 +18,13 @@
             ></span>
           </template>
           <template v-else-if="pipeline.attributes.status == `RUNNING`">
-            <svg
-              class="h-6 w-6"
-              style="animation: spin 2.5s linear infinite"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                class="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              ></circle>
-              <path
-                class="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
+            <span
+              class="h-2 w-2 bg-blue-600 hover:bg-blue-700 rounded-full"
+              style="
+                animation: pulse 2.5s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+              "
+              aria-hidden="true"
+            ></span>
           </template>
           <template v-else-if="pipeline.attributes.status == `DONE`">
             <svg
@@ -137,7 +123,8 @@ const statusMap = {
   },
   RUNNING: {
     name: "Running",
-    class: "bg-white text-blue-600 hover:text-blue-700 hover:border-blue-700",
+    class:
+      "bg-white border-2 border-blue-600 text-blue-600 hover:text-blue-700 hover:border-blue-700",
   },
   DONE: {
     name: "Done",
