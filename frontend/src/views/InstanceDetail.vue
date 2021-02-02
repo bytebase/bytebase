943e92254c65090fdfbd441677c26d6a874f53b4
Tianzhou Chen <tz@bytebase.com>
2021-02-02 19:34:17 +0800

    Change external link icon

--- frontend/src/views/InstanceDetail.vue
+++ frontend/src/views/InstanceDetail.vue
@@ -114,7 +114,7 @@ input[type="number"] {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
               ></path>
             </svg>
           </button>
