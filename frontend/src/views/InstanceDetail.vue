6559d6559e78a7d5dc18c83b4f76753f939b27ab
Tianzhou Chen <tz@bytebase.com>
2021-02-03 15:55:46 +0800

    Show/Hide password button

--- frontend/src/views/InstanceDetail.vue
+++ frontend/src/views/InstanceDetail.vue
@@ -169,15 +169,58 @@ input[type="number"] {
           <label for="password" class="block text-sm font-medium text-gray-700">
             Password
           </label>
-          <div class="mt-1">
+          <div class="mt-1 inline-flex">
             <input
               id="password"
               name="password"
-              type="password"
+              :type="state.showPassword ? 'text' : 'password'"
               class="shadow-sm focus:ring-accent block w-full sm:text-sm border-control-border rounded-md"
               :value="state.adminDataSource.attributes.password"
               @input="updateDataSource('password', $event.target.value)"
             />
+            <div class="ml-2 flex items-center">
+              <button
+                class="btn-icon"
+                @click.prevent="state.showPassword = !state.showPassword"
+              >
+                <svg
+                  v-if="state.showPassword"
+                  class="w-7 h-7"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                  ></path>
+                </svg>
+                <svg
+                  v-else
+                  class="w-7 h-7"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  ></path>
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
 
@@ -279,6 +322,7 @@ interface LocalState {
   originalAdminDataSource?: DataSource;
   adminDataSource?: DataSource | NewDataSource;
   showDeleteModal: boolean;
+  showPassword: boolean;
 }
 
 export default {
@@ -300,6 +344,7 @@ export default {
     const state = reactive<LocalState>({
       new: props.instanceId.toLowerCase() == "new",
       showDeleteModal: false,
+      showPassword: false,
     });
 
     onMounted(() => {
