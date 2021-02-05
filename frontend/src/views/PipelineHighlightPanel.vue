f066bee63887dfc7dcde36687d16ed231e4689b5
Tianzhou Chen <tz@bytebase.com>
2021-02-05 18:18:03 +0800

    Make padding in content so that the divider is still full width

--- frontend/src/views/PipelineHighlightPanel.vue
+++ frontend/src/views/PipelineHighlightPanel.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
+  <div class="">
     <div
-      class="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200"
+      class="px-4 py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200"
     >
       <div class="flex-1 min-w-0">
         <!-- Profile -->
