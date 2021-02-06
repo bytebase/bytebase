b0ce6e5adbb5264b05bd641eeff904641b14299c
Tianzhou Chen <tz@bytebase.com>
2021-02-06 23:54:30 +0800

    Swap sidebar section, put editable section first

--- frontend/src/views/PipelineSidebar.vue
+++ frontend/src/views/PipelineSidebar.vue
@@ -1,7 +1,41 @@
 <template>
   <aside>
     <h2 class="sr-only">Details</h2>
-    <div class="space-y-5">
+    <div class="space-y-4">
+      <div>
+        <h2 class="text-sm font-medium text-gray-500">Assignee</h2>
+        <ul class="mt-3 space-y-3">
+          <li class="flex justify-start items-center space-x-2">
+            <div class="flex-shrink-0">
+              <BBAvatar
+                :size="'small'"
+                :username="pipeline.attributes.assignee.name"
+              />
+            </div>
+            <div class="text-sm font-medium text-gray-900">
+              {{ pipeline.attributes.assignee.name }}
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 class="text-sm font-medium text-gray-500">Creator</h2>
+        <ul class="mt-3 space-y-3">
+          <li class="flex justify-start items-center space-x-2">
+            <div class="flex-shrink-0">
+              <BBAvatar
+                :size="'small'"
+                :username="pipeline.attributes.creator.name"
+              />
+            </div>
+            <div class="text-sm font-medium text-gray-900">
+              {{ pipeline.attributes.creator.name }}
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="mt-6 border-t border-gray-200 py-6 space-y-4">
       <div class="flex items-center space-x-2">
         <!-- Heroicon name: solid/lock-open -->
         <svg
@@ -50,66 +84,9 @@
           />
         </svg>
         <span class="text-gray-900 text-sm font-medium"
-          >Created on <time datetime="2020-12-02">Dec 2, 2020</time></span
+          >Created on
+          {{ moment(pipeline.attributes.createdTs).format("LL") }}</span
         >
-      </div>
-    </div>
-    <div class="mt-6 border-t border-b border-gray-200 py-6 space-y-4">
-      <div>
-        <h2 class="text-sm font-medium text-gray-500">Assignee</h2>
-        <ul class="mt-3 space-y-3">
-          <li class="flex justify-start items-center space-x-2">
-            <div class="flex-shrink-0">
-              <BBAvatar
-                :size="'small'"
-                :username="pipeline.attributes.assignee.name"
-              />
-            </div>
-            <div class="text-sm font-medium text-gray-900">
-              {{ pipeline.attributes.assignee.name }}
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2 class="text-sm font-medium text-gray-500">Creator</h2>
-        <ul class="mt-3 space-y-3">
-          <li class="flex justify-start items-center space-x-2">
-            <div class="flex-shrink-0">
-              <BBAvatar
-                :size="'small'"
-                :username="pipeline.attributes.creator.name"
-              />
-            </div>
-            <div class="text-sm font-medium text-gray-900">
-              {{ pipeline.attributes.creator.name }}
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2 class="text-sm font-medium text-gray-500">Creation Time</h2>
-        <ul class="mt-3 space-y-3">
-          <li class="flex justify-start items-center space-x-2">
-            <!-- Heroicon name: solid/calendar -->
-            <svg
-              class="h-5 w-5 text-gray-400"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clip-rule="evenodd"
-              />
-            </svg>
-            <span class="text-gray-900 text-sm font-medium">
-              {{ moment(pipeline.attributes.createdTs).format("LLL") }}</span
-            >
-          </li>
-        </ul>
       </div>
     </div>
   </aside>
