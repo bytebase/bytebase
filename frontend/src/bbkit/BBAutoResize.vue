44218200f4c96f5c6b3ab5edff92e210f13d02a0
Tianzhou Chen <tz@bytebase.com>
2021-02-27 10:59:18 +0800

    Remove the initial resize on mount. This code doesn’t work
    We need to reference the textarea instead of the container otherwise the scrollheight is incorrect

--- frontend/src/bbkit/BBAutoResize.vue
+++ frontend/src/bbkit/BBAutoResize.vue
@@ -1,36 +1,21 @@
 <template>
-  <div ref="control">
+  <div>
     <slot name="main" :resize="resize" />
   </div>
   <slot name="accessory" :resize="resize" />
 </template>
 
 <script lang="ts">
-import { onMounted, nextTick, ref } from "vue";
-
 export default {
   name: "ResizeAuto",
   setup(props, ctx) {
-    const control = ref();
-
-    onMounted(() => {
-      nextTick(() => {
-        control.value.setAttribute(
-          "style",
-          "height",
-          // Extra 2px is to prevent jiggling upon entering the text
-          `${control.value.scrollHeight + 2}px`
-        );
-      });
-    });
-
     const resize = (el: HTMLTextAreaElement) => {
       el.style.height = "auto";
+      // Extra 2px is to prevent jiggling upon entering the text
       el.style.height = `${el.scrollHeight + 2}px`;
     };
 
     return {
-      control,
       resize,
     };
   },
