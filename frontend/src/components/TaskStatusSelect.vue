a5e39e654cf875aaa1a2ba1cc75605022a5a8b86
Tianzhou Chen <tz@bytebase.com>
2021-03-04 16:42:34 +0800

    Implement status select using BBSelect

--- frontend/src/components/TaskStatusSelect.vue
+++ frontend/src/components/TaskStatusSelect.vue
@@ -1,35 +1,37 @@
 <template>
-  <select
-    id="status"
-    name="status"
-    class="pl-3 pr-10 py-2 btn-select w-full"
-    @change="
-      (e) => {
-        $emit('change-status', e.target.value);
-      }
-    "
+  <BBSelect
+    :selectedItem="selectedStatus"
+    :itemList="['OPEN', 'DONE', 'CANCELED']"
+    :placeholder="'Unknown Status'"
+    @select-item="(status) => $emit('change-status', status)"
   >
-    <option value="OPEN" :selected="selectedStatus == 'OPEN'">Open</option>
-    <option value="DONE" :selected="selectedStatus == 'DONE'">Done</option>
-    <option value="CANCELED" :selected="selectedStatus == 'CANCELED'">
-      Canceled
-    </option>
-  </select>
+    <template v-slot:menuItem="{ item }">
+      <span class="flex items-center space-x-2">
+        <TaskStatusIcon :taskStatus="item" :size="'small'" />
+        <span>
+          {{ item }}
+        </span>
+      </span>
+    </template>
+  </BBSelect>
 </template>
 
 <script lang="ts">
 import { PropType } from "vue";
+import TaskStatusIcon from "../components/TaskStatusIcon.vue";
 import { TaskStatus } from "../types";
 
 export default {
   name: "TaskStatusSelect",
   emits: ["change-status"],
-  components: {},
+  components: { TaskStatusIcon },
   props: {
     selectedStatus: {
       type: String as PropType<TaskStatus>,
     },
   },
-  setup() {},
+  setup() {
+    return {};
+  },
 };
 </script>
