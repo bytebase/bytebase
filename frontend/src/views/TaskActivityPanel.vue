d410f1d2e0174c3443837c699b5a1961c2f8b1c3
Tianzhou Chen <tz@bytebase.com>
2021-03-06 02:41:47 +0800

    Use the same edit icon for update activity icon

--- frontend/src/views/TaskActivityPanel.vue
+++ frontend/src/views/TaskActivityPanel.vue
@@ -47,18 +47,13 @@
                       class="w-7 h-7 bg-control-bg rounded-full ring-8 ring-white flex items-center justify-center"
                     >
                       <svg
-                        class="w-5 h-5 text-control"
+                        class="w-4 h-4 text-control"
                         fill="currentColor"
                         viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
-                        ></path>
-                        <path
-                          fill-rule="evenodd"
-                          d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                          clip-rule="evenodd"
+                          d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
                         ></path>
                       </svg>
                     </div>
