31f1303b6be0641a31332d01aea479150b3f31d2
Tianzhou Chen <tz@bytebase.com>
2021-03-06 19:53:30 +0800

    Improve the btn-icon style in normal/hover state

--- frontend/src/views/TaskDescription.vue
+++ frontend/src/views/TaskDescription.vue
@@ -9,7 +9,7 @@
     >
       <!-- Heroicon name: solid/pencil -->
       <svg
-        class="h-6 w-6 text-control"
+        class="h-6 w-6"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 20 20"
         fill="currentColor"
