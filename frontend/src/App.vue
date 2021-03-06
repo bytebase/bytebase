eb8f23b476be5a72ea97d88853927c9c33e87934
Tianzhou Chen <tz@bytebase.com>
2021-03-06 14:18:06 +0800

    Capture and print error if context loading fails.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
+  <h1 v-if="error">Failed to load {{ error.stack }}</h1>
   <!-- Suspense is experimental, be aware of the potential change -->
-  <Suspense>
+  <Suspense v-else>
     <template #default>
       <div>
         <ProvideContext>
@@ -15,11 +16,21 @@
 </template>
 
 <script lang="ts">
+import { ref, onErrorCaptured } from "vue";
 import ProvideContext from "./components/ProvideContext.vue";
 
 export default {
   name: "App",
   components: { ProvideContext },
-  setup(props, ctx) {},
+  setup(props, ctx) {
+    const error = ref();
+
+    onErrorCaptured((e) => {
+      error.value = e;
+      return true;
+    });
+
+    return { error };
+  },
 };
 </script>
