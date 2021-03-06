e7a7182dbf2dbe31e2955a29da27edf293fa3082
Tianzhou Chen <tz@bytebase.com>
2021-03-06 16:07:02 +0800

    add whitespace-pre-wrap to BBNotification description text

--- frontend/src/bbkit/BBNotification.vue
+++ frontend/src/bbkit/BBNotification.vue
@@ -95,7 +95,7 @@
             </div>
             <div class="ml-3 w-0 flex-1 pt-0.5">
               <p class="text-sm font-medium text-gray-900">{{ title }}</p>
-              <p class="mt-1 text-sm text-gray-500">
+              <p class="mt-1 text-sm text-gray-500 whitespace-pre-wrap">
                 {{ description }}
               </p>
             </div>
