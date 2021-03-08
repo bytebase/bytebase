7ed94dc163c4cf5c5b173617a29e51fbb3713490
Tianzhou Chen <tz@bytebase.com>
2021-03-08 22:27:38 +0800

    Limit setting layout width

--- frontend/src/layouts/SettingLayout.vue
+++ frontend/src/layouts/SettingLayout.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view />
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-5xl mx-auto">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
