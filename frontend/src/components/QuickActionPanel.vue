2d210b59ed19b183d7ca9de9fc63ddd7ecf05bd8
Tianzhou Chen <tz@bytebase.com>
2021-03-11 15:09:12 +0800

    Fix QuickActionPanel button style

--- frontend/src/components/QuickActionPanel.vue
+++ frontend/src/components/QuickActionPanel.vue
@@ -4,7 +4,7 @@
       v-if="quickActionList.includes('instance.create')"
       class="flex flex-col items-center w-28"
     >
-      <router-link to="/instance/new" class="">
+      <router-link to="/instance/new" class="btn-icon">
         <span class="btn-icon-primary inline-flex p-3">
           <svg
             class="w-6 h-6"
@@ -112,23 +112,24 @@
       v-if="quickActionList.includes('datasource.create')"
       class="flex flex-col items-center w-28"
     >
-      <router-link to="/task/new?template=bytebase.datasource.create" class="">
-        <span class="btn-icon-primary inline-flex p-3">
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
-            ></path>
-          </svg>
-        </span>
+      <router-link
+        to="/task/new?template=bytebase.datasource.create"
+        class="p-3 btn-icon-primary"
+      >
+        <svg
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+          ></path>
+        </svg>
       </router-link>
       <h3 class="mt-1 text-base font-normal text-main tracking-tight">
         Request DB
@@ -141,24 +142,22 @@
     >
       <router-link
         to="/task/new?template=bytebase.datasource.schema.update"
-        class=""
+        class="btn-icon-primary p-3"
       >
-        <span class="btn-icon-primary inline-flex p-3">
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M3 10h18M3 14h18m-9-4v8m-7 0h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
-            ></path>
-          </svg>
-        </span>
+        <svg
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 10h18M3 14h18m-9-4v8m-7 0h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+          ></path>
+        </svg>
       </router-link>
       <h3 class="mt-1 text-center text-base font-normal text-main">
         Edit Schema
@@ -169,23 +168,21 @@
       v-if="quickActionList.includes('ticket.create')"
       class="flex flex-col items-center w-28"
     >
-      <router-link to="/task/new" class="">
-        <span class="btn-icon-primary inline-flex p-3">
-          <svg
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
-            ></path>
-          </svg>
-        </span>
+      <router-link to="/task/new" class="btn-icon-primary p-3">
+        <svg
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
+          ></path>
+        </svg>
       </router-link>
       <h3 class="mt-1 text-center text-base font-normal text-main">Ticket</h3>
     </div>
