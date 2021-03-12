9a4547084a025d1813b026435537436b3da3ec59
天舟 <tianzhouchen@gmail.com>
2021-03-12 13:37:16 +0800

    support link to github discussions page

--- frontend/src/components/ProfileDropdown.vue
+++ frontend/src/components/ProfileDropdown.vue
@@ -68,7 +68,8 @@
           >Changelog</a
         >
         <a
-          href="#"
+          href="https://github.com/bytebase/bytebase/discussions"
+          target="_blank"
           class="cursor-pointer block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
           role="menuitem"
           >Support</a
