931e86cbd6bc766e978816f10f52064ed30d4080
天舟 <tianzhouchen@gmail.com>
2021-03-15 17:42:19 +0800

    Display the data source type info below the data source name

--- frontend/src/views/DataSourceDetail.vue
+++ frontend/src/views/DataSourceDetail.vue
@@ -36,11 +36,43 @@
             <div>
               <div class="flex items-center">
                 <h1 class="text-xl font-bold leading-7 text-main truncate">
-                  {{ dataSource.name }} -
-                  <span v-data-source-type>{{ dataSource.type }}</span>
+                  {{ dataSource.name }}
                 </h1>
               </div>
               <dl class="flex flex-col mt-2 sm:flex-row sm:flex-wrap">
+                <dt class="sr-only">RoleType</dt>
+                <dd
+                  class="flex items-center text-sm text-control-light font-medium sm:mr-6"
+                >
+                  <!-- Heroicon name: solid/pencil -->
+                  <svg
+                    v-if="dataSource.type == 'RW'"
+                    class="mr-1.5 w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                    ></path>
+                  </svg>
+                  <!-- Heroicon name: solid/pencil -->
+                  <svg
+                    v-if="dataSource.type == 'RO'"
+                    class="mr-1.5 w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                    <path
+                      fill-rule="evenodd"
+                      d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span v-data-source-type>{{ dataSource.type }}</span>
+                </dd>
                 <dt class="sr-only">Instance</dt>
                 <dd
                   class="flex items-center text-sm text-control-light font-medium sm:mr-6"
