f237906c5f4e2637404a6144d3d4b2687c3a22ea
天舟 <tianzhouchen@gmail.com>
2021-03-16 21:44:31 +0800

    Create data source form style tweak

--- frontend/src/components/DataSourceNewForm.vue
+++ frontend/src/components/DataSourceNewForm.vue
@@ -1,142 +1,130 @@
 <template>
   <form
-    class="px-4 space-y-4"
+    class="space-y-6 divide-y divide-block-border"
     @submit.prevent="$emit('create', state.dataSource)"
   >
-    <div class="pt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-3">
-      <div class="sm:col-span-2">
-        <label for="name" class="text-lg leading-6 font-medium text-control">
-          Name <span class="text-red-600">*</span>
-        </label>
-        <input
-          required
-          id="name"
-          name="name"
-          type="text"
-          class="textfield mt-2 w-full"
-          :value="state.dataSource.name"
-          @input="updateDataSource('name', $event.target.value)"
-        />
-      </div>
-    </div>
-
-    <div class="pt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-3">
-      <div class="sm:col-span-2">
-        <label
-          for="database"
-          class="text-lg leading-6 font-medium text-control"
-        >
-          Database <span class="text-red-600">*</span>
-        </label>
-        <DatabaseSelect
-          class="mt-2"
-          :instanceId="instanceId"
-          @select-database-id="
-            (databaseId) => {
-              updateDataSource('databaseId', databaseId);
-            }
-          "
-        />
-      </div>
-
-      <div class="sm:col-span-1">
-        <BBSwitch
-          class="mt-10"
-          :label="'Read-only'"
-          :value="state.dataSource.type == 'RO'"
-          @toggle="
-            (on) => {
-              updateDataSource('type', on ? 'RO' : 'RW');
-            }
-          "
-        />
-      </div>
-    </div>
-
-    <div class="pt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-3">
-      <div class="sm:col-span-2">
-        <label
-          for="username"
-          class="text-lg leading-6 font-medium text-control"
-        >
-          Username
-        </label>
-        <input
-          id="username"
-          name="username"
-          type="text"
-          class="textfield mt-2 w-full"
-          :value="state.dataSource.username"
-          @input="updateDataSource('username', $event.target.value)"
-        />
-      </div>
-    </div>
-
-    <div class="pt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-3">
-      <div class="sm:col-span-2">
-        <div class="flex flex-row space-x-1">
-          <label
-            for="username"
-            class="text-lg leading-6 font-medium text-control"
-          >
-            Password
+    <div class="space-y-4">
+      <div class="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-3">
+        <div class="sm:col-span-2">
+          <label for="name" class="textlabel">
+            Name <span class="text-red-600">*</span>
           </label>
-          <button
-            class="btn-icon"
-            @click.prevent="state.showPassword = !state.showPassword"
-          >
-            <svg
-              v-if="state.showPassword"
-              class="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-              ></path>
-            </svg>
-            <svg
-              v-else
-              class="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              ></path>
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-              ></path>
-            </svg>
-          </button>
+          <input
+            required
+            id="name"
+            name="name"
+            type="text"
+            class="textfield mt-1 w-full"
+            :value="state.dataSource.name"
+            @input="updateDataSource('name', $event.target.value)"
+          />
         </div>
-        <input
-          id="password"
-          name="password"
-          autocomplete="off"
-          :type="state.showPassword ? 'text' : 'password'"
-          class="textfield mt-2 w-full"
-          :value="state.dataSource.password"
-          @input="updateDataSource('password', $event.target.value)"
-        />
+      </div>
+
+      <div class="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-3">
+        <div class="sm:col-span-2">
+          <label for="database" class="textlabel">
+            Database <span class="text-red-600">*</span>
+          </label>
+          <DatabaseSelect
+            class="mt-1"
+            :instanceId="instanceId"
+            @select-database-id="
+              (databaseId) => {
+                updateDataSource('databaseId', databaseId);
+              }
+            "
+          />
+        </div>
+
+        <div class="sm:col-span-1">
+          <BBSwitch
+            class="mt-9"
+            :label="'Read-only'"
+            :value="state.dataSource.type == 'RO'"
+            @toggle="
+              (on) => {
+                updateDataSource('type', on ? 'RO' : 'RW');
+              }
+            "
+          />
+        </div>
+      </div>
+
+      <div class="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-3">
+        <div class="sm:col-span-2">
+          <label for="username" class="textlabel"> Username </label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            class="textfield mt-1 w-full"
+            :value="state.dataSource.username"
+            @input="updateDataSource('username', $event.target.value)"
+          />
+        </div>
+      </div>
+
+      <div class="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-3">
+        <div class="sm:col-span-2">
+          <div class="flex flex-row space-x-1">
+            <label for="username" class="textlabel"> Password </label>
+            <button
+              class="btn-icon"
+              @click.prevent="state.showPassword = !state.showPassword"
+            >
+              <svg
+                v-if="state.showPassword"
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                ></path>
+              </svg>
+              <svg
+                v-else
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                ></path>
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <input
+            id="password"
+            name="password"
+            autocomplete="off"
+            :type="state.showPassword ? 'text' : 'password'"
+            class="textfield mt-1 w-full"
+            :value="state.dataSource.password"
+            @input="updateDataSource('password', $event.target.value)"
+          />
+        </div>
       </div>
     </div>
-
     <!-- Create button group -->
-    <div class="flex justify-end pt-5">
+    <div class="pt-4 flex justify-end">
       <button
         type="button"
         class="btn-normal py-2 px-4"
