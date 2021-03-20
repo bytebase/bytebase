8d2c74e396b71ccd9fd32b91a1ffd99153a6fc86
天舟 <tianzhouchen@gmail.com>
2021-03-20 23:17:08 +0800

    Add  select-none to BBSwitch button

--- frontend/src/bbkit/BBSwitch.vue
+++ frontend/src/bbkit/BBSwitch.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center">
     <button
       type="button"
-      class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent"
+      class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent select-none"
       :class="on ? 'bg-accent' : 'bg-gray-200'"
       aria-pressed="false"
       @click.prevent="
