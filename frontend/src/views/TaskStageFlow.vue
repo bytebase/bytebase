b32c0864ce22060114fd830b3726e62f56c65b70
天舟 <tianzhouchen@gmail.com>
2021-03-22 14:08:55 +0800

    Change icon for SKIPPED to be consistent with other cancel icon

--- frontend/src/views/TaskStageFlow.vue
+++ frontend/src/views/TaskStageFlow.vue
@@ -61,19 +61,16 @@
               </template>
               <template v-else-if="stage.status == 'SKIPPED'">
                 <svg
-                  class="w-4 h-4"
+                  class="w-5 h-5"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
                 >
+                  >
                   <path
                     fill-rule="evenodd"
-                    d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
-                    clip-rule="evenodd"
-                  ></path>
-                  <path
-                    fill-rule="evenodd"
-                    d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+                    d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                     clip-rule="evenodd"
                   ></path>
                 </svg>
