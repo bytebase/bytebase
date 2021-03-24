8d66c741aa89ede49f5aafddeafed4cb9a0ce2b8
天舟 <tianzhouchen@gmail.com>
2021-03-24 14:19:30 +0800

    Improve SchemaUpdateTemplate
    - Add the missing environment field
    
    - Set the default environment field value to the last environemnt (usually the prod)
    
    - Fix the database empty value check
    
    - Wording improvement

--- frontend/src/plugins/task/DatabaseSchemaUpdateTemplate.ts
+++ frontend/src/plugins/task/DatabaseSchemaUpdateTemplate.ts
@@ -1,10 +1,19 @@
+import isEmpty from "lodash-es/isEmpty";
+
 import { TaskTemplate, TemplateContext, TaskBuiltinFieldId } from "../types";
 
-import { TaskNew, DatabaseId, UNKNOWN_ID } from "../../types";
+import { TaskNew, DatabaseId, EnvironmentId, UNKNOWN_ID } from "../../types";
 
 const template: TaskTemplate = {
   type: "bytebase.database.schema.update",
   buildTask: (ctx: TemplateContext): TaskNew => {
+    const payload: any = {};
+    if (ctx.environmentList.length > 0) {
+      // Set the last element as the default value.
+      // Normally the last environment is the prod env and is most commonly used.
+      payload[TaskBuiltinFieldId.ENVIRONMENT] =
+        ctx.environmentList[ctx.environmentList.length - 1].id;
+    }
     return {
       name: "Update Schema",
       type: "bytebase.database.schema.update",
@@ -24,19 +33,30 @@ const template: TaskTemplate = {
         };
       }),
       creatorId: ctx.currentUser.id,
-      payload: {},
+      payload,
     };
   },
   fieldList: [
     {
       category: "INPUT",
+      id: TaskBuiltinFieldId.ENVIRONMENT,
+      slug: "env",
+      name: "Environment",
+      type: "Environment",
+      required: true,
+      isEmpty: (value: EnvironmentId): boolean => {
+        return isEmpty(value);
+      },
+    },
+    {
+      category: "INPUT",
       id: TaskBuiltinFieldId.DATABASE,
       slug: "db",
-      name: "DB Name",
+      name: "Database",
       type: "Database",
       required: true,
       isEmpty: (value: DatabaseId): boolean => {
-        return value != undefined && value != UNKNOWN_ID;
+        return value == undefined || value == UNKNOWN_ID;
       },
     },
   ],
