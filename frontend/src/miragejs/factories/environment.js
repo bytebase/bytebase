b32723ab93fbbf05f4331a565ef01b5b891422a8
天舟 <tianzhouchen@gmail.com>
2021-03-29 22:38:26 +0800

    Remove host/port from environment seed model

--- frontend/src/miragejs/factories/environment.js
+++ frontend/src/miragejs/factories/environment.js
@@ -19,27 +19,5 @@ export default {
     order(i) {
       return i;
     },
-    host(i) {
-      if (i == 0) {
-        return "localhost";
-      } else if (i == 1) {
-        return "127.0.0.1";
-      } else if (i == 2) {
-        return "13.24.32.122";
-      } else {
-        return "mydb.com";
-      }
-    },
-    port(i) {
-      if (i == 0) {
-        return "3306";
-      } else if (i == 1) {
-        return "";
-      } else if (i == 2) {
-        return "15202";
-      } else {
-        return "5432";
-      }
-    },
   }),
 };
