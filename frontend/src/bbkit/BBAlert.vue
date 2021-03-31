5eccce09d584e1d99afe951c69f5a58c921ed6b0
天舟 <tianzhouchen@gmail.com>
2021-03-31 18:50:20 +0800

    Use base size text for BBAlert description

--- frontend/src/bbkit/BBAlert.vue
+++ frontend/src/bbkit/BBAlert.vue
@@ -137,7 +137,7 @@
             class="mt-3 text-center sm:mt-0 sm:ml-14 sm:text-left"
           >
             <div class="mt-2">
-              <p class="text-sm text-gray-500 whitespace-pre-wrap">
+              <p class="text-gray-500 whitespace-pre-wrap">
                 {{ description }}
               </p>
             </div>
