721c95aaf01862ca3beedec71263062840791346
天舟 <tianzhouchen@gmail.com>
2021-04-05 18:15:57 +0800

    Use grid layout for TaskSidebar so label and control can verticaly aligned

--- frontend/src/views/TaskSidebar.vue
+++ frontend/src/views/TaskSidebar.vue
@@ -1,10 +1,12 @@
 <template>
   <aside>
     <h2 class="sr-only">Details</h2>
-    <div class="space-y-6">
-      <div v-if="!$props.new" class="flex flex-row space-x-2 pb-4 border-b">
-        <h2 class="flex items-center textlabel w-36">Status</h2>
-        <div class="w-full">
+    <div class="grid gap-y-6 gap-x-6 grid-cols-3">
+      <template v-if="!$props.new">
+        <h2 class="textlabel flex items-center col-span-1 col-start-1">
+          Status
+        </h2>
+        <div class="col-span-2">
           <span class="flex items-center space-x-2">
             <TaskStatusIcon :taskStatus="task.status" :size="'normal'" />
             <span class="text-main capitalize">
@@ -12,33 +14,33 @@
             </span>
           </span>
         </div>
+      </template>
+
+      <h2 class="textlabel flex items-center col-span-1 col-start-1">
+        Assignee<span v-if="$props.new" class="text-red-600">*</span>
+      </h2>
+      <div class="col-span-2">
+        <PrincipalSelect
+          :disabled="!allowEditAssignee"
+          :selectedId="$props.new ? task.assigneeId : task.assignee?.id"
+          :allowAllRoles="false"
+          @select-principal-id="
+            (principalId) => {
+              $emit('update-assignee-id', principalId);
+            }
+          "
+        />
       </div>
-      <div class="flex flex-row space-x-2">
-        <h2 class="flex items-center textlabel w-36">
-          Assignee<span v-if="$props.new" class="text-red-600">*</span>
-        </h2>
-        <div class="w-full">
-          <PrincipalSelect
-            :disabled="!allowEditAssignee"
-            :selectedId="$props.new ? task.assigneeId : task.assignee?.id"
-            :allowAllRoles="false"
-            @select-principal-id="
-              (principalId) => {
-                $emit('update-assignee-id', principalId);
-              }
-            "
-          />
-        </div>
-      </div>
+
       <template v-for="(field, index) in fieldList" :key="index">
-        <div class="flex flex-row space-x-2">
+        <h2 class="textlabel flex items-center col-span-1 col-start-1">
+          {{ field.name }}
+          <span v-if="field.required" class="text-red-600">*</span>
+        </h2>
+        <div class="col-span-2">
           <template v-if="field.type == 'String'">
-            <h2 class="flex items-center textlabel w-36">
-              {{ field.name }}
-              <span v-if="field.required" class="text-red-600">*</span>
-            </h2>
             <BBTextField
-              class="w-full mt-4 text-sm"
+              class="mt-4 text-sm"
               :disabled="!allowEdit"
               :required="true"
               :value="fieldValue(field)"
@@ -47,113 +49,91 @@
             />
           </template>
           <template v-else-if="field.type == 'Environment'">
-            <h2 class="flex items-center textlabel w-36">
-              {{ field.name }}
-              <span v-if="field.required" class="text-red-600">*</span>
-            </h2>
-            <div class="w-full">
-              <EnvironmentSelect
-                :disabled="!allowEdit"
-                :name="field.id"
-                :selectedId="fieldValue(field)"
-                :selectDefault="false"
-                @select-environment-id="
-                  (environmentId) => {
-                    trySaveCustomField(field, environmentId);
-                  }
-                "
-              />
-            </div>
+            <EnvironmentSelect
+              :disabled="!allowEdit"
+              :name="field.id"
+              :selectedId="fieldValue(field)"
+              :selectDefault="false"
+              @select-environment-id="
+                (environmentId) => {
+                  trySaveCustomField(field, environmentId);
+                }
+              "
+            />
           </template>
           <template v-else-if="field.type == 'Database'">
-            <h2 class="flex items-center textlabel w-36">
-              {{ field.name }}
-              <span v-if="field.required" class="text-red-600">*</span>
-            </h2>
-            <div class="w-full">
-              <DatabaseSelect
-                class="w-full"
-                :disabled="!allowEdit"
-                :selectedId="fieldValue(field)"
-                :mode="'ENVIRONMENT'"
-                :environmentId="environmentId"
-                @select-database-id="
-                  (databaseId) => {
-                    trySaveCustomField(field, databaseId);
-                  }
-                "
-              />
-            </div>
+            <DatabaseSelect
+              class="w-full"
+              :disabled="!allowEdit"
+              :selectedId="fieldValue(field)"
+              :mode="'ENVIRONMENT'"
+              :environmentId="environmentId"
+              @select-database-id="
+                (databaseId) => {
+                  trySaveCustomField(field, databaseId);
+                }
+              "
+            />
           </template>
           <template v-else-if="field.type == 'NewDatabase'">
-            <h2 class="flex textlabel mt-2 w-36">
-              {{ field.name }}
-              <span v-if="field.required" class="text-red-600">*</span>
-            </h2>
-            <div class="w-full">
-              <BBTextField
-                type="text"
-                class="w-full text-sm"
-                :disabled="!allowEdit"
-                :required="true"
-                :value="fieldValue(field)"
-                :placeholder="field.placeholder"
-                @end-editing="(text) => trySaveCustomField(field, text)"
-              />
-            </div>
+            <BBTextField
+              type="text"
+              class="w-full text-sm"
+              :disabled="!allowEdit"
+              :required="true"
+              :value="fieldValue(field)"
+              :placeholder="field.placeholder"
+              @end-editing="(text) => trySaveCustomField(field, text)"
+            />
           </template>
           <template v-else-if="field.type == 'Boolean'">
-            <h2 class="flex items-center textlabel w-36">
-              {{ field.name }}
-              <span v-if="field.required" class="text-red-600">*</span>
-            </h2>
-            <div class="flex justify-start">
-              <BBSwitch
-                :disabled="!allowEdit"
-                :value="fieldValue(field)"
-                @toggle="
-                  (on) => {
-                    trySaveCustomField(field, on);
-                  }
-                "
-              />
-            </div>
+            <BBSwitch
+              :disabled="!allowEdit"
+              :value="fieldValue(field)"
+              @toggle="
+                (on) => {
+                  trySaveCustomField(field, on);
+                }
+              "
+            />
           </template>
         </div>
       </template>
     </div>
     <div
       v-if="!$props.new"
-      class="mt-6 border-t border-block-border pt-6 space-y-6"
+      class="mt-6 border-t border-block-border pt-6 grid gap-y-6 gap-x-6 grid-cols-3"
     >
-      <div class="flex flex-row space-x-2">
-        <h2 class="flex items-center textlabel w-36">Creator</h2>
-        <ul class="w-full">
-          <li class="flex justify-start items-center space-x-2">
-            <div class="flex-shrink-0">
-              <BBAvatar :size="'small'" :username="task.creator.name" />
-            </div>
-            <router-link
-              :to="`/u/${task.creator.id}`"
-              class="text-sm font-medium text-main hover:underline"
-            >
-              {{ task.creator.name }}
-            </router-link>
-          </li>
-        </ul>
-      </div>
-      <div class="flex flex-row space-x-2">
-        <h2 class="textlabel w-36">Updated</h2>
-        <span class="textfield w-full">
-          {{ moment(task.lastUpdatedTs).format("LLL") }}</span
-        >
-      </div>
-      <div class="flex flex-row space-x-2">
-        <h2 class="textlabel w-36">Created</h2>
-        <span class="textfield w-full">
-          {{ moment(task.createdTs).format("LLL") }}</span
-        >
-      </div>
+      <h2 class="textlabel flex items-center col-span-1 col-start-1">
+        Creator
+      </h2>
+      <ul class="col-span-2">
+        <li class="flex justify-start items-center space-x-2">
+          <div class="flex-shrink-0">
+            <BBAvatar :size="'small'" :username="task.creator.name" />
+          </div>
+          <router-link
+            :to="`/u/${task.creator.id}`"
+            class="text-sm font-medium text-main hover:underline"
+          >
+            {{ task.creator.name }}
+          </router-link>
+        </li>
+      </ul>
+
+      <h2 class="textlabel flex items-center col-span-1 col-start-1">
+        Updated
+      </h2>
+      <span class="textfield col-span-2">
+        {{ moment(task.lastUpdatedTs).format("LLL") }}</span
+      >
+
+      <h2 class="textlabel flex items-center col-span-1 col-start-1">
+        Created
+      </h2>
+      <span class="textfield col-span-2">
+        {{ moment(task.createdTs).format("LLL") }}</span
+      >
     </div>
   </aside>
 </template>
