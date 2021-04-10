3b47381fb06f422cdb77d1094acc6d5bc2d26c4b
天舟 <tianzhouchen@gmail.com>
2021-04-10 18:49:38 +0800

    Create EnvironmentTable component

---
+++ frontend/src/components/EnvironmentTable.vue
@@ -0,0 +1,70 @@
+<template>
+  <BBTable
+    :columnList="COLUMN_LIST"
+    :dataSource="environmentList"
+    :showHeader="true"
+    :leftBordered="false"
+    :rightBordered="false"
+    @click-row="clickEnvironment"
+  >
+    <template v-slot:body="{ rowData: environment }">
+      <BBTableCell :leftPadding="4" class="w-4 table-cell text-gray-500">
+        <span class="">#{{ environment.id }}</span>
+      </BBTableCell>
+      <BBTableCell class="w-48 table-cell">
+        {{ environment.name }}
+      </BBTableCell>
+      <BBTableCell class="w-24 table-cell">
+        {{ humanizeTs(environment.createdTs) }}
+      </BBTableCell>
+      <BBTableCell class="w-24 table-cell">
+        {{ humanizeTs(environment.lastUpdatedTs) }}
+      </BBTableCell>
+    </template>
+  </BBTable>
+</template>
+
+<script lang="ts">
+import { PropType } from "vue";
+import { useRouter } from "vue-router";
+import { Environment } from "../types";
+
+const COLUMN_LIST = [
+  {
+    title: "ID",
+  },
+  {
+    title: "Name",
+  },
+  {
+    title: "Created",
+  },
+  {
+    title: "Last updated",
+  },
+];
+
+export default {
+  name: "EnvironmentTable",
+  components: {},
+  props: {
+    environmentList: {
+      required: true,
+      type: Object as PropType<Environment[]>,
+    },
+  },
+  setup(props, ctx) {
+    const router = useRouter();
+
+    const clickEnvironment = function (section: number, row: number) {
+      const environment = props.environmentList[row];
+      router.push(`/environment#${environment.id}`);
+    };
+
+    return {
+      COLUMN_LIST,
+      clickEnvironment,
+    };
+  },
+};
+</script>
