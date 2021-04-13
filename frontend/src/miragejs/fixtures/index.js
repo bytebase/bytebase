2e995080e479966222f156b6c64f5d0c816ffc29
天舟 <tianzhouchen@gmail.com>
2021-04-13 23:59:48 +0800

    Fix the project to really load the default dba project as the fixture

--- frontend/src/miragejs/fixtures/index.js
+++ frontend/src/miragejs/fixtures/index.js
@@ -5,11 +5,11 @@
 import workspaces from "./workspaceList.json";
 import roleMappings from "./roleMappingList.json";
 import users from "./userList.json";
-import projectList from "./projectList.json";
+import projects from "./projectList.json";
 
 export default {
   workspaces,
   roleMappings,
   users,
-  projectList,
+  projects,
 };
