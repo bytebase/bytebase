8e15eee5c36623af5096d8351629091d0a0ef934
天舟 <tianzhouchen@gmail.com>
2021-04-14 15:36:33 +0800

    Remove the /user/:userId/database route
    This has already been moved to /databaser?user=xxx

--- frontend/src/miragejs/routes/user.js
+++ frontend/src/miragejs/routes/user.js
@@ -30,36 +30,4 @@ export default function configureUser(route) {
 
     return user.update(attrs);
   });
-
-  route.get("/user/:userId/database", function (schema, request) {
-    const user = schema.users.find(request.params.userId);
-
-    if (!user) {
-      return new Response(
-        404,
-        {},
-        {
-          errors: "User id " + request.params.userId + " not found",
-        }
-      );
-    }
-
-    return schema.databases.where((database) => {
-      const dataSourceList = schema.dataSources.where({
-        workspaceId: WORKSPACE_ID,
-        databaseId: database.id,
-      });
-
-      for (const dataSource of dataSourceList.models) {
-        if (
-          dataSource.memberList.find((item) => {
-            return item.principalId == request.params.userId;
-          })
-        ) {
-          return true;
-        }
-      }
-      return false;
-    });
-  });
 }
