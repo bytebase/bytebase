38bc0ce62b2198fa2ef6989adfa83b4810f6079a
天舟 <tianzhouchen@gmail.com>
2021-04-15 17:31:30 +0800

    Revert the previous slug order swapping. And put the id last
    Eat my own hat here…
    
    The triggering reason is we just added project key and it serves better if it puts first.  PRJ-project-foo-5 looks better than 5-PRJ-project-foo.
    
    Put the id last does obscure the hint that user can just use id alone as the link like task/123. However, this convention requires some more education and could be built through other methods. (e.g. provide the generating shortlink feature which produces task/123)

--- frontend/src/utils/slug.ts
+++ frontend/src/utils/slug.ts
@@ -8,46 +8,46 @@ import {
 } from "../types";
 import slug from "slug";
 
+export function idFromSlug(slug: string) {
+  const parts = slug.split("-");
+  return parts[parts.length - 1];
+}
+
 export function taskSlug(taskName: string, taskId: TaskId) {
-  return [taskId, slug(taskName)].join("-");
+  return [slug(taskName), taskId].join("-");
 }
 
 // On the other hand, it's not possible to de-slug due to slug's one-way algorithm
 export function environmentSlug(environment: Environment): string {
-  return [environment.id, slug(environment.name)].join("-");
+  return [slug(environment.name), environment.id].join("-");
 }
 
 export function projectSlug(project: Project) {
-  return [project.id, slug(project.name)].join("-");
+  return [project.key, slug(project.name), project.id].join("-");
 }
 
 export function instanceSlug(instance: Instance): string {
   return [
-    instance.id,
     slug(instance.environment.name),
     slug(instance.name),
+    instance.id,
   ].join("-");
 }
 
 export function databaseSlug(database: Database): string {
-  return [database.id, slug(database.name)].join("-");
+  return [slug(database.name), database.id].join("-");
+}
+
+export function dataSourceSlug(dataSource: DataSource): string {
+  return [slug(dataSource.name), dataSource.id].join("-");
 }
 
 export function fullDatabasePath(database: Database): string {
   return `/db/${databaseSlug(database)}`;
 }
 
-export function dataSourceSlug(dataSource: DataSource): string {
-  return [dataSource.id, slug(dataSource.name)].join("-");
-}
-
 export function fullDataSourcePath(dataSource: DataSource): string {
   return `/db/${databaseSlug(dataSource.database)}/datasource/${dataSourceSlug(
     dataSource
   )}`;
-}
-
-export function idFromSlug(slug: string) {
-  const parts = slug.split("-");
-  return parts[0];
 }
