0cdc32cac51515e91959f823a8ccc0b8727dff9a
天舟 <tianzhouchen@gmail.com>
2021-04-28 13:02:33 +0800

    add init backend go code

---
+++ backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	r := gin.Default()
+	// Dont worry about this line just yet, it will make sense in the Dockerise bit!
+	r.Use(static.Serve("/", static.LocalFile("./web", true)))
+	api := r.Group("/api")
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong from backend",
+		})
+	})
+
+	r.Run()
+}
