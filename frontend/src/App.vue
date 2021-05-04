d0555ca46c7e8b43c5452887d3eec4d5892ef4a6
天舟 <tianzhouchen@gmail.com>
2021-05-04 22:32:35 +0800

    Improve controls when loading layout
    This layout would show upon first rendering

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,10 +7,10 @@
       </ProvideContext>
     </template>
     <template #fallback>
-      <div class="flex flex-col p-4 space-y-4">
-        <span>Loading...</span>
+      <div class="flex flex-row justify-between p-4 space-x-2">
+        <span class="items-center flex">Loading...</span>
         <button
-          class="items-center flex justify-center btn-primary"
+          class="items-center flex justify-center btn-normal"
           @click.prevent="ping"
         >
           Ping
