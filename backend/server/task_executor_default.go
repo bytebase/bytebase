fdbc9950121f9cf7c713f2f9f03c78557cc52814
天舟 <tianzhouchen@gmail.com>
2021-05-27 17:29:18 +0800

    Rename task_executor_approve to task_executor_default

--- backend/server/task_executor_default.go
+++ backend/server/task_executor_default.go
@@ -18,5 +18,7 @@ type DefaultTaskExecutor struct {
 }
 
 func (exec *DefaultTaskExecutor) Run(ctx context.Context, server *Server, taskRun api.TaskRun) (terminated bool, err error) {
+	exec.l.Info("Default task executor", zap.String("task_run", taskRun.Name))
+
 	return true, nil
 }
