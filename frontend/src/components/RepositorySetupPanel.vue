ad6614a6d7597cb35b7b50ace144574ceb94f840
天舟 <tianzhouchen@gmail.com>
2021-06-07 16:34:07 +0800

    Add DONE icon for the completed step

--- frontend/src/components/RepositorySetupPanel.vue
+++ frontend/src/components/RepositorySetupPanel.vue
@@ -18,16 +18,48 @@
             }
           "
         >
-          <span
-            class="text-xs font-semibold tracking-wide uppercase"
-            :class="
-              state.currentStep >= index
-                ? 'text-accent group-hover:text-accent-hover'
-                : 'text-control-light'
-            "
-            >Step {{ index + 1 }}</span
-          >
-          <span class="text-sm font-medium">{{ step }}</span>
+          <div class="flex items-center justify-between">
+            <div class="flex flex-col">
+              <span
+                class="text-xs font-semibold tracking-wide uppercase"
+                :class="
+                  state.currentStep >= index
+                    ? 'text-accent group-hover:text-accent-hover'
+                    : 'text-control-light'
+                "
+                >Step {{ index + 1 }}</span
+              >
+              <span class="text-sm font-medium">{{ step }}</span>
+            </div>
+            <div
+              v-if="state.currentStep > index"
+              class="
+                flex
+                items-center
+                justify-center
+                w-6
+                h-6
+                bg-accent
+                text-white
+                rounded-full
+                select-none
+              "
+            >
+              <svg
+                class="w-4 h-4"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
       </li>
     </ol>
