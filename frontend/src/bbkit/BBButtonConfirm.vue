0be94110e4c194195194c6c0a38aca1b55883f58
天舟 <tianzhouchen@gmail.com>
2021-06-13 23:52:03 +0800

    Change BBButtonConfirm restore icon style

--- frontend/src/bbkit/BBButtonConfirm.vue
+++ frontend/src/bbkit/BBButtonConfirm.vue
@@ -37,7 +37,7 @@
         stroke-linecap="round"
         stroke-linejoin="round"
         stroke-width="2"
-        d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+        d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6"
       ></path>
       <path
         v-if="style == 'EDIT'"
