643ad155c4d287a90ba8d03dee8baa4ff402ba5a
天舟 <tianzhouchen@gmail.com>
2021-06-14 23:54:25 +0800

    Add tooltip css class and use it to indicate whether the project is version control enabled in the database list

--- frontend/src/components/DatabaseTable.vue
+++ frontend/src/components/DatabaseTable.vue
@@ -9,7 +9,36 @@
   >
     <template v-slot:body="{ rowData: database }">
       <BBTableCell v-if="showProjectColumn" :leftPadding="4" class="w-16">
-        {{ projectName(database.project) }}
+        <div class="flex flex-row space-x-2 items-center">
+          <div class="tooltip-wrapper">
+            <svg
+              v-if="database.project.workflowType == 'UI'"
+              class="w-4 h-4"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            ></svg>
+            <template v-else-if="database.project.workflowType == 'VCS'">
+              <span class="tooltip">Version control enabled</span>
+              <svg
+                class="w-4 h-4 text-control hover:text-control-hover"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+                ></path>
+              </svg>
+            </template>
+          </div>
+          <div>{{ projectName(database.project) }}</div>
+        </div>
       </BBTableCell>
       <BBTableCell
         :leftPadding="showProjectColumn ? undefined : 4"
