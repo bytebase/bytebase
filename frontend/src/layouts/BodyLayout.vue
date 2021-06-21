0580a309fa837dce8b6136a2287b3b0cd736a596
天舟 <tianzhouchen@gmail.com>
2021-06-21 14:47:09 +0800

    Increase archive top padding to leave more space from the content list

--- frontend/src/layouts/BodyLayout.vue
+++ frontend/src/layouts/BodyLayout.vue
@@ -58,7 +58,7 @@
           </div>
           <router-link
             to="/archive"
-            class="outline-item group flex items-center px-4 py-2"
+            class="outline-item group flex items-center px-4 pt-4 pb-2"
           >
             <svg
               class="w-5 h-5 mr-2"
@@ -117,7 +117,7 @@
         </div>
         <router-link
           to="/archive"
-          class="outline-item group flex items-center px-4 py-2"
+          class="outline-item group flex items-center px-4 pt-4 pb-2"
         >
           <svg
             class="w-5 h-5 mr-2"
