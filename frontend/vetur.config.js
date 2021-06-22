c7484effce9436a98b0ee33b77912c1043cd390d
天舟 <tianzhouchen@gmail.com>
2021-06-22 13:10:20 +0800

    Move vetur under frontend/

--- frontend/vetur.config.js
+++ frontend/vetur.config.js
@@ -4,7 +4,7 @@ module.exports = {
       // **required**
       // Where is your project?
       // It is relative to `vetur.config.js`.
-      root: "./frontend",
+      root: "./",
       // **optional** default: `'package.json'`
       // Where is `package.json` in the project?
       // We use it to determine the version of vue.
