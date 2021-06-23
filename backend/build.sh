ca43766a6b838f2577864e8c8ac0fb2705c79dad
天舟 <tianzhouchen@gmail.com>
2021-06-24 02:46:08 +0800

    Improve bulid script

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 version=0.1.0
-flags="-X 'github.com/bytebase/bytebase/bin/server/cmd.version=${version}' -X 'github.com/bytebase/bytebase/bin/server/cmd.goversion=$(go version)' -X 'github.com/bytebase/bytebase/bin/server/cmd.gitcommit=$(git rev-parse HEAD)' -X 'github.com/bytebase/bytebase/bin/server/cmd.buildtime=$(date) ($(date -u))' -X 'github.com/bytebase/bytebase/bin/server/cmd.builduser=$(id -u -n)'"
+
+flags="-X 'github.com/bytebase/bytebase/bin/server/cmd.version=${version}'
+-X 'github.com/bytebase/bytebase/bin/server/cmd.goversion=$(go version)'
+-X 'github.com/bytebase/bytebase/bin/server/cmd.gitcommit=$(git rev-parse HEAD)'
+-X 'github.com/bytebase/bytebase/bin/server/cmd.buildtime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")'
+-X 'github.com/bytebase/bytebase/bin/server/cmd.builduser=$(id -u -n)'"
+
+echo "Start building..."
+
 go build -ldflags "$flags" -o ./bytebase-build/bytebase ./bin/server/main.go
+
+echo "Completed building."
