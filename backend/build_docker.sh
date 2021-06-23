614248f7d398c591b422a6637cf195e4809b8727
天舟 <tianzhouchen@gmail.com>
2021-06-24 02:46:22 +0800

    Add initial dockerfile and build_docker.sh

---
+++ backend/build_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+version=0.1.0
+
+docker build \
+    --build-arg VERSION=${version} \
+    --build-arg GO_VERSION="$(go version)" \
+    --build-arg GIT_COMMIT="$(git rev-parse HEAD)"\
+    --build-arg BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"  \
+    --build-arg BUILD_USER="$(id -u -n)" \
+    -t bytebase/bytebase .
