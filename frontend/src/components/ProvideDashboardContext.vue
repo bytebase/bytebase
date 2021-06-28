09c85b8972e78cfb0218ba5724f96776ea9a3752
天舟 <tianzhouchen@gmail.com>
2021-06-28 22:38:33 +0800

    Group remote fetch together in ProvideDashboardContext

--- frontend/src/components/ProvideDashboardContext.vue
+++ frontend/src/components/ProvideDashboardContext.vue
@@ -17,23 +17,18 @@ export default {
       store.getters["auth/currentUser"]()
     );
 
-    await store.dispatch("plan/fetchCurrentPlan");
-
-    await store.dispatch("actuator/info");
-
-    // Fetch member and principal list so PrincipalSelect can have the data.
-    await store.dispatch("member/fetchMemberList");
-    await store.dispatch("principal/fetchPrincipalList");
-
-    await store.dispatch("environment/fetchEnvironmentList");
-
     await Promise.all([
+      store.dispatch("plan/fetchCurrentPlan"),
+      store.dispatch("actuator/info"),
+      // Fetch member and principal list so PrincipalSelect can have the data.
+      store.dispatch("member/fetchMemberList"),
+      store.dispatch("principal/fetchPrincipalList"),
+      store.dispatch("environment/fetchEnvironmentList"),
       // The default project hosts databases not explicitly assigned to other users project.
       store.dispatch("project/fetchProjectById", DEFAULT_PROJECT_ID),
+      store.dispatch("message/fetchMessageListByUser", currentUser.value.id),
       store.dispatch("uistate/restoreState"),
     ]);
-
-    store.dispatch("message/fetchMessageListByUser", currentUser.value.id);
   },
 };
 </script>
