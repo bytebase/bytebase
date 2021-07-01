b7ee51cd4fb043ed899827a7344a8ea77fa836c5
天舟 <tianzhouchen@gmail.com>
2021-07-01 15:15:37 +0800

    Use word instead of icon to denote switch plan/role
    Icon meaning is not that obvious

--- frontend/src/views/DashboardHeader.vue
+++ frontend/src/views/DashboardHeader.vue
@@ -38,23 +38,19 @@
       <div class="flex items-center space-x-3">
         <div
           v-if="showSwitchPlan"
-          class="hidden md:flex sm:flex-row items-center space-x-2 text-sm"
+          class="
+            hidden
+            md:flex
+            sm:flex-row
+            items-center
+            space-x-2
+            text-sm
+            font-medium
+          "
         >
-          <span class="hidden lg:block"
-            ><svg
-              class="w-5 h-5 -mr-1"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              ></path></svg
-          ></span>
+          <span class="hidden lg:block font-normal text-accent"
+            >Switch plan</span
+          >
           <div
             v-if="currentPlan != 0"
             class="bar-link"
@@ -82,23 +78,18 @@
         </div>
         <div
           v-if="!isRelease"
-          class="hidden md:flex sm:flex-row items-center space-x-2 text-sm"
+          class="
+            hidden
+            md:flex
+            sm:flex-row
+            items-center
+            space-x-2
+            text-sm
+            font-medium
+          "
         >
-          <span class="hidden lg:block"
-            ><svg
-              class="w-5 h-5 -mr-1"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-              ></path>
-            </svg>
+          <span class="hidden lg:block font-normal text-accent"
+            >Switch role
           </span>
           <div
             v-if="currentUser.role != 'OWNER'"
