98b38ac195733828d44f5cd74adeb903ab46e70c
天舟 <tianzhouchen@gmail.com>
2021-07-17 01:07:07 +0800

    Add reload button in select respository

--- frontend/src/components/RepositorySelectionPanel.vue
+++ frontend/src/components/RepositorySelectionPanel.vue
@@ -1,7 +1,23 @@
 <template>
   <BBAttention :description="attentionText" />
   <div class="mt-4 space-y-2">
-    <div class="flex justify-end items-center">
+    <div class="flex justify-between items-center">
+      <button class="btn-icon" @click.prevent="prepareRepositoryList">
+        <svg
+          class="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+          ></path>
+        </svg>
+      </button>
       <BBTableSearch
         :placeholder="'Search repository'"
         @change-text="(text) => changeSearchText(text)"
@@ -120,6 +136,7 @@ export default {
       repositoryList,
       attentionText,
       changeSearchText,
+      prepareRepositoryList,
     };
   },
 };
