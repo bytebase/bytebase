788abfb1775fcc9383a1481cc0dd42b10d7aec96
spinningbot <spinningbot@hotmail.com>
2021-07-18 23:28:12 -0700

    BB CLI: move database connection library to its own connect package.

--- bin/bb/dump/dump.go
+++ bin/bb/dump/dump.go
@@ -2,13 +2,11 @@
 package dump
 
 import (
-	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
 
+	"github.com/bytebase/bytebase/bin/bb/connect"
 	"github.com/bytebase/bytebase/bin/bb/dump/mysqldump"
 	"github.com/bytebase/bytebase/bin/bb/dump/pgdump"
 )
@@ -16,7 +14,7 @@ import (
 // Dump exports the schema of a database instance.
 // All non-system databases will be exported to the input directory in the format of database_name.sql for each database.
 // When directory isn't specified, the schema will be exported to stdout.
-func Dump(databaseType, username, password, hostname, port, database, directory string, tlsCfg TlsConfig, schemaOnly bool) error {
+func Dump(databaseType, username, password, hostname, port, database, directory string, tlsCfg connect.TlsConfig, schemaOnly bool) error {
 	if directory != "" {
 		dirInfo, err := os.Stat(directory)
 		if os.IsNotExist(err) {
@@ -39,12 +37,13 @@ func Dump(databaseType, username, password, hostname, port, database, directory 
 		if err != nil {
 			return fmt.Errorf("TlsConfig.GetSslConfig() got error: %v", err)
 		}
-		dp, err := mysqldump.New(username, password, hostname, port, tlsConfig)
+		conn, err := connect.NewMysql(username, password, hostname, port, tlsConfig)
 		if err != nil {
-			return fmt.Errorf("mysqldump.New(%q, %q, %q, %q) got error: %v", username, password, hostname, port, err)
+			return fmt.Errorf("connect.NewMysql(%q, %q, %q, %q) got error: %v", username, password, hostname, port, err)
 		}
-		defer dp.Close()
+		defer conn.Close()
 
+		dp := mysqldump.New(conn)
 		databases, err := dp.GetDumpableDatabases(database)
 		if err != nil {
 			return err
@@ -62,11 +61,13 @@ func Dump(databaseType, username, password, hostname, port, database, directory 
 		}
 		return nil
 	case "pg":
-		dp, err := pgdump.New(username, password, hostname, port, database, tlsCfg.SslCA, tlsCfg.SslCert, tlsCfg.SslKey)
+		conn, err := connect.NewPostgres(username, password, hostname, port, database, tlsCfg.SslCA, tlsCfg.SslCert, tlsCfg.SslKey)
 		if err != nil {
-			return fmt.Errorf("pgdump.New(%q, %q, %q, %q) got error: %v", username, password, hostname, port, err)
+			return fmt.Errorf("connect.NewPostgres(%q, %q, %q, %q) got error: %v", username, password, hostname, port, err)
 		}
-		defer dp.Close()
+		defer conn.Close()
+
+		dp := pgdump.New(conn)
 		databases, err := dp.GetDumpableDatabases(database)
 		if err != nil {
 			return err
@@ -100,60 +101,4 @@ func getOutFile(dbName, directory string) (*os.File, error) {
 		}
 		return f, nil
 	}
-}
-
-// TlsConfig is the configuration for SSL connection.
-type TlsConfig struct {
-	SslCA   string
-	SslCert string
-	SslKey  string
-}
-
-// GetSslConfig gets the SSL config for connection.
-func (tc TlsConfig) GetSslConfig() (*tls.Config, error) {
-	if tc.SslCA == "" {
-		return nil, nil
-	}
-	rootCertPool := x509.NewCertPool()
-	pem, err := ioutil.ReadFile(tc.SslCA)
-	if err != nil {
-		return nil, err
-	}
-	if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
-		return nil, fmt.Errorf("rootCertPool.AppendCertsFromPEM() failed to append server CA pem.")
-	}
-
-	cfg := &tls.Config{
-		RootCAs: rootCertPool,
-	}
-	if (tc.SslCert == "" && tc.SslKey != "") || (tc.SslCert != "" && tc.SslKey == "") {
-		return nil, fmt.Errorf("ssl-cert and ssl-key must be both set or unset.")
-	}
-	if tc.SslCert != "" && tc.SslKey != "" {
-		clientCert := make([]tls.Certificate, 0, 1)
-		certs, err := tls.LoadX509KeyPair(tc.SslCert, tc.SslKey)
-		if err != nil {
-			return nil, err
-		}
-		clientCert = append(clientCert, certs)
-
-		cfg.Certificates = clientCert
-	}
-
-	cfg.InsecureSkipVerify = true
-	cfg.VerifyPeerCertificate = func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
-		if len(rawCerts) == 0 {
-			return fmt.Errorf("empty certificate to verify.")
-		}
-		cert, err := x509.ParseCertificate(rawCerts[0])
-		if err != nil {
-			return err
-		}
-		opts := x509.VerifyOptions{Roots: rootCertPool}
-		if _, err = cert.Verify(opts); err != nil {
-			return fmt.Errorf("SSL cert failed to verify: %v", err)
-		}
-		return nil
-	}
-	return cfg, nil
 }
