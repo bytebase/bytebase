a5368ee1cc46692a360e7158b4c6edbdb8a626ff
天舟 <tianzhouchen@gmail.com>
2021-07-29 13:19:37 +0800

    Add level column to inbox table. Level can be INFO, WARNING, ERROR
    Also shows an icon if level is WARNING or ERROR from the Inbox UI

--- store/seed/test/10150__inbox.sql
+++ store/seed/test/10150__inbox.sql
@@ -4,176 +4,196 @@ INSERT INTO
         id,
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
     (
         15001,
         101,
         14001,
-        'READ'
+        'READ',
+        'INFO'
     );
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (101, 14003, 'UNREAD');
+    (101, 14003, 'UNREAD', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (101, 14004, 'UNREAD');
+    (101, 14004, 'UNREAD', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (101, 14005, 'UNREAD');
+    (101, 14005, 'UNREAD', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (101, 14010, 'UNREAD');
+    (101, 14010, 'UNREAD', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (101, 14012, 'UNREAD');
+    (101, 14012, 'UNREAD', 'ERROR');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (101, 14013, 'UNREAD');
+    (101, 14013, 'UNREAD', 'INFO');
 
 -- Inbox for receiver 102
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (102, 14001, 'READ');
+    (102, 14001, 'READ', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (102, 14002, 'READ');
+    (102, 14002, 'READ', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (102, 14004, 'READ');
+    (102, 14004, 'READ', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (102, 14005, 'UNREAD');
+    (102, 14005, 'UNREAD', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (102, 14006, 'UNREAD');
+    (102, 14006, 'UNREAD', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (102, 14013, 'UNREAD');
+    (102, 14013, 'UNREAD', 'INFO');
 
 -- Inbox for receiver 103
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (103, 14001, 'READ');
+    (103, 14001, 'READ', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (103, 14002, 'READ');
+    (103, 14002, 'READ', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (103, 14003, 'READ');
+    (103, 14003, 'READ', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (103, 14005, 'READ');
+    (103, 14005, 'READ', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (103, 14006, 'UNREAD');
+    (103, 14006, 'UNREAD', 'INFO');
 
 INSERT INTO
     inbox (
         receiver_id,
         activity_id,
-        `status`
+        `status`,
+        `level`
     )
 VALUES
-    (103, 14013, 'UNREAD');
+    (103, 14013, 'UNREAD', 'INFO');
