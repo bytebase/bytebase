6aef685cb3ab2dba4f8061997935b4d526cdfa43
天舟 <tianzhouchen@gmail.com>
2021-08-09 15:07:47 +0800

    ProjectHook related CRUD UI
    Former-commit-id: 64186862134a137a6b3a21f9b4f1250265d738f4

---
+++ frontend/src/components/ProjectWebhookForm.vue
@@ -0,0 +1,362 @@
+<template>
+  <div class="space-y-4">
+    <div v-if="create">
+      <label class="textlabel"> Destination </label>
+      <div class="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-6">
+        <template
+          v-for="(item, index) in PROJECT_HOOK_TYPE_ITEM_LIST"
+          :key="index"
+        >
+          <div
+            class="
+              flex
+              justify-center
+              px-2
+              py-4
+              border border-control-border
+              hover:bg-control-bg-hover
+              cursor-pointer
+            "
+            @click.capture="state.webhook.type = item.type"
+          >
+            <div class="flex flex-col items-center">
+              <img
+                class="h-10 w-10 rounded-full"
+                :src="`/src/assets/${item.logo}`"
+                alt=""
+              />
+              <p class="mt-1 text-center textlabel">
+                {{ item.name }}
+              </p>
+              <div class="mt-3 radio text-sm">
+                <input
+                  type="radio"
+                  class="btn"
+                  :checked="state.webhook.type == item.type"
+                />
+              </div>
+            </div>
+          </div>
+        </template>
+      </div>
+    </div>
+    <div>
+      <label for="name" class="textlabel">
+        Name <span class="text-red-600">*</span>
+      </label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        class="textfield mt-1 w-full"
+        :placeholder="namePlaceholder"
+        :disabled="!allowEdit"
+        v-model="state.webhook.name"
+      />
+    </div>
+    <div>
+      <label for="url" class="textlabel">
+        Webhook URL <span class="text-red-600">*</span>
+      </label>
+      <input
+        id="url"
+        name="url"
+        type="text"
+        class="textfield mt-1 w-full"
+        :placeholder="urlPlaceholder"
+        :disabled="!allowEdit"
+        v-model="state.webhook.url"
+      />
+    </div>
+    <div>
+      <div class="text-md leading-6 font-medium text-main">Events</div>
+      <div
+        v-for="(item, index) in PROJECT_HOOK_EVENT_ITEM_LIST"
+        :key="index"
+        class="mt-4 space-y-4"
+      >
+        <BBCheckbox
+          :title="item.title"
+          :label="item.label"
+          :value="eventOn(item.event)"
+          @toggle="
+            (on) => {
+              toggleEvent(item.event, on);
+            }
+          "
+        />
+      </div>
+    </div>
+    <div
+      class="flex pt-5"
+      :class="!create && allowEdit ? 'justify-between' : 'justify-end'"
+    >
+      <BBButtonConfirm
+        v-if="!create && allowEdit"
+        :style="'DELETE'"
+        :buttonText="'Delete this webhook'"
+        :okText="'Delete'"
+        :confirmTitle="`Delete webhook '${webhook.name}' and all its execution history?`"
+        :requireConfirm="true"
+        @confirm="deleteWebhook"
+      />
+      <div>
+        <button
+          type="button"
+          class="btn-normal py-2 px-4"
+          @click.prevent="cancel"
+        >
+          {{ allowEdit ? "Cancel" : "Back" }}
+        </button>
+        <template v-if="allowEdit">
+          <button
+            v-if="create"
+            type="submit"
+            class="btn-primary ml-3 inline-flex justify-center py-2 px-4"
+            :disabled="!allowCreate"
+            @click.prevent="createWebhook"
+          >
+            Create
+          </button>
+          <button
+            v-else
+            type="submit"
+            class="btn-primary ml-3 inline-flex justify-center py-2 px-4"
+            :disabled="!valueChanged"
+            @click.prevent="updateWebhook"
+          >
+            Update
+          </button>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive } from "@vue/reactivity";
+import { computed, PropType, watch } from "@vue/runtime-core";
+import {
+  ActionType,
+  Project,
+  ProjectHook,
+  ProjectHookCreate,
+  ProjectHookPatch,
+  PROJECT_HOOK_TYPE_ITEM_LIST,
+  PROJECT_HOOK_EVENT_ITEM_LIST,
+} from "../types";
+import { cloneDeep, isEmpty, isEqual } from "lodash";
+import { useRouter } from "vue-router";
+import { projectHookSlug, projectSlug } from "../utils";
+import { useStore } from "vuex";
+
+interface LocalState {
+  webhook: ProjectHook | ProjectHookCreate;
+}
+
+export default {
+  name: "ProjectWebhookForm",
+  emits: ["change-repository"],
+  props: {
+    allowEdit: {
+      default: true,
+      type: Boolean,
+    },
+    create: {
+      type: Boolean,
+      default: false,
+    },
+    project: {
+      required: true,
+      type: Object as PropType<Project>,
+    },
+    webhook: {
+      required: true,
+      type: Object as PropType<ProjectHook | ProjectHookCreate>,
+    },
+  },
+  components: {},
+  setup(props, { emit }) {
+    const store = useStore();
+    const router = useRouter();
+
+    const state = reactive<LocalState>({
+      webhook: cloneDeep(props.webhook),
+    });
+
+    watch(
+      () => props.webhook,
+      (cur: ProjectHook | ProjectHookCreate) => {
+        state.webhook = cloneDeep(cur);
+      }
+    );
+
+    const namePlaceholder = computed(() => {
+      if (state.webhook.type == "bb.plugin.webhook.slack") {
+        return "Slack Webhook";
+      } else if (state.webhook.type == "bb.plugin.webhook.discord") {
+        return "Discord Webhook";
+      } else if (state.webhook.type == "bb.plugin.webhook.teams") {
+        return "Teams Webhook";
+      } else if (state.webhook.type == "bb.plugin.webhook.dingtalk") {
+        return "DingTalk Webhook";
+      } else if (state.webhook.type == "bb.plugin.webhook.feishu") {
+        return "Feishu Webhook";
+      } else if (state.webhook.type == "bb.plugin.webhook.wecom") {
+        return "WeCom Webhook";
+      }
+
+      return "My Webhook";
+    });
+
+    const urlPlaceholder = computed(() => {
+      if (state.webhook.type == "bb.plugin.webhook.slack") {
+        return "https://hooks.slack.com/services/...";
+      } else if (state.webhook.type == "bb.plugin.webhook.discord") {
+        return "https://discord.com/api/webhooks/...";
+      } else if (state.webhook.type == "bb.plugin.webhook.teams") {
+        return "https://acme123.webhook.office.com/webhookb2/...";
+      } else if (state.webhook.type == "bb.plugin.webhook.dingtalk") {
+        return "https://oapi.dingtalk.com/robot/...";
+      } else if (state.webhook.type == "bb.plugin.webhook.feishu") {
+        return "https://open.feishu.cn/open-apis/bot/v2/hook/...";
+      } else if (state.webhook.type == "bb.plugin.webhook.wecom") {
+        return "https://qyapi.weixin.qq.com/cgi-bin/webhook/...";
+      }
+
+      return "Webhook URL";
+    });
+
+    const valueChanged = computed(() => {
+      return !isEqual(props.webhook, state.webhook);
+    });
+
+    const allowCreate = computed(() => {
+      return (
+        !isEmpty(state.webhook.type) &&
+        !isEmpty(state.webhook.name) &&
+        !isEmpty(state.webhook.url)
+      );
+    });
+
+    const eventOn = (type: ActionType) => {
+      for (const activityType of props.webhook.activityList) {
+        if (activityType == type) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    const toggleEvent = (type: ActionType, on: boolean) => {
+      if (on) {
+        for (const activityType of state.webhook.activityList) {
+          if (activityType == type) {
+            return;
+          }
+        }
+        state.webhook.activityList.push(type);
+      } else {
+        const list: ActionType[] = [];
+        for (const activityType of state.webhook.activityList) {
+          if (activityType != type) {
+            list.push(activityType);
+          }
+        }
+        state.webhook.activityList = list;
+      }
+      state.webhook.activityList.sort();
+    };
+
+    const cancel = () => {
+      router.push({
+        name: "workspace.project.detail",
+        params: {
+          projectSlug: projectSlug(props.project),
+        },
+        hash: "#hook",
+      });
+    };
+
+    const createWebhook = () => {
+      store
+        .dispatch("projectHook/createProjectHook", {
+          projectId: props.project.id,
+          projectHookCreate: state.webhook,
+        })
+        .then((webhook: ProjectHook) => {
+          store.dispatch("notification/pushNotification", {
+            module: "bytebase",
+            style: "SUCCESS",
+            title: `Successfully created webhook "${webhook.name}".`,
+          });
+          router.push({
+            name: "workspace.project.hook.detail",
+            params: {
+              projectHookSlug: projectHookSlug(webhook),
+            },
+          });
+        });
+    };
+
+    const updateWebhook = () => {
+      const projectHookPatch: ProjectHookPatch = {};
+      if (props.webhook.name != state.webhook.name) {
+        projectHookPatch.name = state.webhook.name;
+      }
+      if (props.webhook.url != state.webhook.url) {
+        projectHookPatch.url = state.webhook.url;
+      }
+      if (props.webhook.activityList != state.webhook.activityList) {
+        projectHookPatch.activityList = state.webhook.activityList.join(",");
+      }
+      store
+        .dispatch("projectHook/updateProjectHookById", {
+          projectId: props.project.id,
+          projectHookId: (state.webhook as ProjectHook).id,
+          projectHookPatch,
+        })
+        .then((webhook: ProjectHook) => {
+          store.dispatch("notification/pushNotification", {
+            module: "bytebase",
+            style: "SUCCESS",
+            title: `Successfully updated webhook "${webhook.name}".`,
+          });
+        });
+    };
+
+    const deleteWebhook = () => {
+      const name = state.webhook.name;
+      store
+        .dispatch("projectHook/deleteProjectHookById", {
+          projectId: props.project.id,
+          projectHookId: (state.webhook as ProjectHook).id,
+        })
+        .then(() => {
+          store.dispatch("notification/pushNotification", {
+            module: "bytebase",
+            style: "SUCCESS",
+            title: `Successfully deleted webhook "${name}".`,
+          });
+          cancel();
+        });
+    };
+
+    return {
+      PROJECT_HOOK_TYPE_ITEM_LIST,
+      PROJECT_HOOK_EVENT_ITEM_LIST,
+      state,
+      namePlaceholder,
+      urlPlaceholder,
+      valueChanged,
+      allowCreate,
+      eventOn,
+      toggleEvent,
+      cancel,
+      createWebhook,
+      updateWebhook,
+      deleteWebhook,
+    };
+  },
+};
+</script>
