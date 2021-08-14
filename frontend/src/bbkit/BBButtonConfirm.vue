bef9c91d73db85782976816d30ad62938ec48477
天舟 <tianzhouchen@gmail.com>
2021-08-14 21:10:53 +0800

    Add CLONE style to BBButonConfirm

--- frontend/src/bbkit/BBButtonConfirm.vue
+++ frontend/src/bbkit/BBButtonConfirm.vue
@@ -54,6 +54,13 @@
         stroke-width="2"
         d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
       ></path>
+      <path
+        v-if="style == 'CLONE'"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+      ></path>
     </svg>
     <span v-if="buttonText" class="ml-1">{{ buttonText }}</span>
   </button>
