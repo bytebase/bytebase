b0ba59d914b916d5844102040b79a2000f59cb2d
天舟 <tianzhouchen@gmail.com>
2021-08-14 15:31:58 +0800

    Generate a default manual backup name as <<project-name>>-<<environment-name>>-<<time>>
    e.g. blog-prod-20210814T153033

--- frontend/src/components/DatabaseBackupCreateForm.vue
+++ frontend/src/components/DatabaseBackupCreateForm.vue
@@ -76,6 +76,8 @@
 import { computed, PropType, reactive, ref } from "vue";
 import { Database } from "../types";
 import { isEmpty } from "lodash";
+import moment from "moment";
+import slug from "slug";
 
 interface LocalState {
   backupName: string;
@@ -96,9 +98,10 @@ export default {
     const commentTextArea = ref("");
 
     const state = reactive<LocalState>({
-      backupName: `${
-        props.database.name
-      }-${new Date().toLocaleTimeString()}-manual`,
+      // The default format is consistent with the default automatic backup name format used in the server.
+      backupName: `${slug(props.database.project.name)}-${slug(
+        props.database.instance.environment.name
+      )}-${moment.utc().local().format("YYYYMMDDTHHmmss")}`,
       comment: "",
     });
 
