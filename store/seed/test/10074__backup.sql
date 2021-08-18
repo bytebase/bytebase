37bbff2e107c189b0534706e995a300d0138e774
天舟 <tianzhouchen@gmail.com>
2021-08-18 18:24:59 +0800

    Add backup test seed data

--- store/seed/test/10074__backup.sql
+++ store/seed/test/10074__backup.sql
@@ -1,5 +1,37 @@
+-- Backup for database 7015 (shop database in prod environment)
+INSERT INTO
+    backup (
+        id,
+        creator_id,
+        created_ts,
+        updater_id,
+        updated_ts,
+        database_id,
+        name,
+        `status`,
+        `type`,
+        storage_backend,
+        migration_history_version,
+        path,
+        `comment`
+    )
+VALUES
+    (
+        7401,
+        101,
+        1629136800,
+        101,
+        1629136800,
+        7015,
+        'shop-prod-20210817T094000',
+        'DONE',
+        'MANUAL',
+        'LOCAL',
+        'v1_20210817094000',
+        'data/backup/db/7015/shop-prod-20210817T094000.sql',
+        ''
+    );
 
--- Backup for database 7008 blog
 INSERT INTO
     backup (
         creator_id,
@@ -18,15 +50,202 @@ INSERT INTO
 VALUES
     (
         101,
-        1624558090,
+        1629250800,
         101,
-        1624558090,
-        7008,
-        'blog-backup-1',
+        1629250800,
+        7015,
+        'shop-prod-20210818T140000',
         'DONE',
         'MANUAL',
         'LOCAL',
-        '0',
-        'data/backup/db/7008/blog-backup-1.sql',
-        'The first backup'
+        'v2_20210818140000',
+        'data/backup/db/7015/shop-prod-20210818T140000.sql',
+        ''
+    );
+
+INSERT INTO
+    backup (
+        creator_id,
+        created_ts,
+        updater_id,
+        updated_ts,
+        database_id,
+        name,
+        `status`,
+        `type`,
+        storage_backend,
+        migration_history_version,
+        path,
+        `comment`
+    )
+VALUES
+    (
+        101,
+        1627754400,
+        101,
+        1627754400,
+        7015,
+        'shop-prod-20210801T020000-autobackup',
+        'FAILED',
+        'AUTOMATIC',
+        'LOCAL',
+        'v3_20210801020000',
+        'data/backup/db/7015/shop-prod-20210801T020000-autobackup.sql',
+        'Something unfortunate happened. Here is a long long long long long long long long long long long long long long long long long long long long long long long long error message.'
+    );
+
+INSERT INTO
+    backup (
+        creator_id,
+        created_ts,
+        updater_id,
+        updated_ts,
+        database_id,
+        name,
+        `status`,
+        `type`,
+        storage_backend,
+        migration_history_version,
+        path,
+        `comment`
+    )
+VALUES
+    (
+        101,
+        1628359200,
+        101,
+        1628359200,
+        7015,
+        'shop-prod-20210808T020000-autobackup',
+        'DONE',
+        'AUTOMATIC',
+        'LOCAL',
+        'v3_20210808020000',
+        'data/backup/db/7015/shop-prod-20210808T020000-autobackup.sql',
+        ''
+    );
+
+-- Backup for database 7016 (blog database in prod environment)
+INSERT INTO
+    backup (
+        creator_id,
+        created_ts,
+        updater_id,
+        updated_ts,
+        database_id,
+        name,
+        `status`,
+        `type`,
+        storage_backend,
+        migration_history_version,
+        path,
+        `comment`
+    )
+VALUES
+    (
+        101,
+        1629250800,
+        101,
+        1629250800,
+        7016,
+        'blog-prod-20210818T094000',
+        'DONE',
+        'MANUAL',
+        'LOCAL',
+        'v1_20210818094000',
+        'data/backup/db/7016/blog-prod-20210818T094000.sql',
+        ''
+    );
+
+INSERT INTO
+    backup (
+        creator_id,
+        created_ts,
+        updater_id,
+        updated_ts,
+        database_id,
+        name,
+        `status`,
+        `type`,
+        storage_backend,
+        migration_history_version,
+        path,
+        `comment`
+    )
+VALUES
+    (
+        101,
+        1629337200,
+        101,
+        1629337200,
+        7016,
+        'blog-prod-20210819T140000',
+        'DONE',
+        'MANUAL',
+        'LOCAL',
+        'v2_20210819140000',
+        'data/backup/db/7016/blog-prod-20210819T140000.sql',
+        ''
+    );
+
+INSERT INTO
+    backup (
+        creator_id,
+        created_ts,
+        updater_id,
+        updated_ts,
+        database_id,
+        name,
+        `status`,
+        `type`,
+        storage_backend,
+        migration_history_version,
+        path,
+        `comment`
+    )
+VALUES
+    (
+        101,
+        1627754400,
+        101,
+        1627754400,
+        7016,
+        'blog-prod-20210801T020000-autobackup',
+        'FAILED',
+        'AUTOMATIC',
+        'LOCAL',
+        'v3_20210801020000',
+        'data/backup/db/7016/blog-prod-20210801T020000-autobackup.sql',
+        'Something unfortunate happened. Here is a long long long long long long long long long long long long long long long long long long long long long long long long error message.'
+    );
+
+INSERT INTO
+    backup (
+        creator_id,
+        created_ts,
+        updater_id,
+        updated_ts,
+        database_id,
+        name,
+        `status`,
+        `type`,
+        storage_backend,
+        migration_history_version,
+        path,
+        `comment`
+    )
+VALUES
+    (
+        101,
+        1628359200,
+        101,
+        1628359200,
+        7016,
+        'blog-prod-20210808T020000-autobackup',
+        'DONE',
+        'AUTOMATIC',
+        'LOCAL',
+        'v3_20210808020000',
+        'data/backup/db/7016/blog-prod-20210808T020000-autobackup.sql',
+        ''
     );
