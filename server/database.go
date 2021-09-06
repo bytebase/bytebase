318f102a64dee95058fcb60e7c7c236b099c0d36
天舟 <tianzhouchen@gmail.com>
2021-09-06 22:55:14 +0800

    If caller is requesting for database list under a project, then we should return all databases
    Former-commit-id: 102a872de0eaf96fc2115e4fc9505496e148e684

--- server/database.go
+++ server/database.go
@@ -71,13 +71,14 @@ func (s *Server) registerDatabaseRoutes(g *echo.Group) {
 
 		filteredList := []*api.Database{}
 		role := c.Get(GetRoleContextKey()).(api.Role)
-		// If caller is NOT requesting for a paritcular instance or the caller is a Developer,
-		// then we will only return databases belonging to the project where the caller is a member of.
+		// If caller is NOT requesting for a particular project and is NOT requesting for a paritcular
+		// instance or the caller is a Developer, then we will only return databases belonging to the
+		// project where the caller is a member of.
 		// Looking from the UI perspective:
 		// - The database list left sidebar will only return databases related to the caller regardless of the caller's role.
 		// - The database list on the instance page will return all databases if the caller is Owner or DBA, but will only return
 		//   related databases if the caller is Developer.
-		if databaseFind.InstanceId == nil || role == api.Developer {
+		if projectIdStr == "" && (databaseFind.InstanceId == nil || role == api.Developer) {
 			principalId := c.Get(GetPrincipalIdContextKey()).(int)
 			for _, database := range list {
 				for _, projectMember := range database.Project.ProjectMemberList {
