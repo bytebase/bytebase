f2d81f99e7ff1041d4234503e910d37b345a0787
天舟 <tianzhouchen@gmail.com>
2021-09-07 21:02:37 +0800

    Move postgres dump/restore to plugin/bb/pg

--- bin/bb/cmd/restore.go
+++ bin/bb/cmd/restore.go
@@ -7,8 +7,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/bytebase/bytebase/bin/bb/connect"
-	"github.com/bytebase/bytebase/bin/bb/restore/pgrestore"
 	"github.com/bytebase/bytebase/plugin/db"
 	"github.com/spf13/cobra"
 )
@@ -20,7 +18,7 @@ func init() {
 	restoreCmd.Flags().StringVar(&hostname, "hostname", "", "Hostname of database.")
 	restoreCmd.Flags().StringVar(&port, "port", "", "Port of database. (default mysql:3306 pg:5432).")
 	restoreCmd.Flags().StringVar(&database, "database", "", "Database to connect and export.")
-	restoreCmd.Flags().StringVar(&fileOrDirectory, "file", "", "Result file or directory to store the dump, see dump --file for more details.")
+	restoreCmd.Flags().StringVar(&file, "file", "", "File to store the dump.")
 	restoreCmd.MarkFlagRequired("database")
 	restoreCmd.MarkFlagRequired("file")
 
@@ -42,7 +40,7 @@ var (
 				SslCert: sslCert,
 				SslKey:  sslKey,
 			}
-			return restoreDatabase(databaseType, username, password, hostname, port, database, fileOrDirectory, tlsCfg)
+			return restoreDatabase(databaseType, username, password, hostname, port, database, file, tlsCfg)
 		},
 	}
 )
@@ -56,49 +54,38 @@ func restoreDatabase(databaseType, username, password, hostname, port, database,
 	defer f.Close()
 	sc := bufio.NewScanner(f)
 
+	var dbType db.Type
 	switch databaseType {
 	case "mysql":
+		dbType = db.MySQL
 		if username == "" {
 			username = "root"
 		}
-
-		db, err := db.Open(
-			db.MySQL,
-			db.DriverConfig{Logger: logger},
-			db.ConnectionConfig{
-				Host:      hostname,
-				Port:      port,
-				Username:  username,
-				Password:  password,
-				Database:  database,
-				TlsConfig: tlsCfg,
-			},
-			db.ConnectionContext{},
-		)
-		if err != nil {
-			return err
-		}
-		defer db.Close(context.Background())
-
-		if err := db.Restore(context.Background(), sc); err != nil {
-			return fmt.Errorf("failed to restore from database dump %s got error: %w", file, err)
-		}
-		return nil
 	case "pg":
-		conn, err := connect.NewPostgres(username, password, hostname, port, database, tlsCfg.SslCA, tlsCfg.SslCert, tlsCfg.SslKey)
-		if err != nil {
-			return fmt.Errorf("connect.NewPostgres(%q, %q, %q, %q) got error: %v", username, password, hostname, port, err)
-		}
-		defer conn.Close()
-
-		if err := conn.SwitchDatabase(database); err != nil {
-			return fmt.Errorf("conn.SwitchDatabase(%q) got error: %v", database, err)
-		}
-		if err := pgrestore.Restore(conn, sc); err != nil {
-			return fmt.Errorf("mysqlrestore.Restore() got error: %v", err)
-		}
-		return nil
+		dbType = db.Postgres
 	default:
 		return fmt.Errorf("database type %q not supported; supported types: mysql, pg", databaseType)
 	}
+	db, err := db.Open(
+		dbType,
+		db.DriverConfig{Logger: logger},
+		db.ConnectionConfig{
+			Host:      hostname,
+			Port:      port,
+			Username:  username,
+			Password:  password,
+			Database:  database,
+			TlsConfig: tlsCfg,
+		},
+		db.ConnectionContext{},
+	)
+	if err != nil {
+		return err
+	}
+	defer db.Close(context.Background())
+
+	if err := db.Restore(context.Background(), sc); err != nil {
+		return fmt.Errorf("failed to restore from database dump %s got error: %w", file, err)
+	}
+	return nil
 }
