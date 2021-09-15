47474d46d3e5f4fbdbd524d3cc65fb2b05ee6da0
天舟 <tianzhouchen@gmail.com>
2021-09-15 16:46:06 +0800

    Make IssueStagePanel support multi-task stage
    Former-commit-id: c22e1e42190ca2f99e85ca51fe061551c93ab0a8

--- frontend/src/components/IssueStagePanel.vue
+++ frontend/src/components/IssueStagePanel.vue
@@ -1,14 +1,35 @@
 <template>
-  <template v-for="(task, index) in stage.taskList" :key="index">
-    <div class="mb-2">{{ task.name }}</div>
-    <TaskRunTable :taskRunList="task.taskRunList" />
-  </template>
+  <div class="space-y-4">
+    <div v-for="(task, index) in stage.taskList" :key="index">
+      <div class="flex flex-row items-center mb-2 space-x-1">
+        <svg
+          v-if="stage.taskList.length > 1 && activeTask.id == task.id"
+          class="w-5 h-5 text-info"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <div v-if="stage.taskList.length > 1" class="textlabel">
+          <span v-if="stage.taskList.length > 1"> Step {{ index + 1 }} - </span>
+          {{ task.name }}
+        </div>
+      </div>
+      <TaskRunTable :taskRunList="task.taskRunList" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { reactive, watch, PropType } from "vue";
+import { reactive, watch, PropType, computed } from "vue";
 import TaskRunTable from "../components/TaskRunTable.vue";
-import { Stage } from "../types";
+import { Stage, Task } from "../types";
+import { activeTaskInStage } from "../utils";
 
 interface LocalState {}
 
@@ -29,7 +50,11 @@ export default {
       (curStage, _) => {}
     );
 
-    return { state };
+    const activeTask = computed((): Task => {
+      return activeTaskInStage(props.stage);
+    });
+
+    return { state, activeTask };
   },
 };
 </script>
