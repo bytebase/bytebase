e7c9912201c08762de646188e75cad158ccbc441
spinningbot <spinningbot@hotmail.com>
2021-09-18 17:08:51 -0700

    Implement policy service for upserting and finding policies in an environment.
    Former-commit-id: 5068d9ec491b867523f3551ee98fdd4c22dabd1f

---
+++ api/policy.go
@@ -0,0 +1,61 @@
+package api
+
+import (
+	"context"
+)
+
+type Policy struct {
+	ID int `jsonapi:"primary,environment"`
+
+	// Standard fields
+	RowStatus RowStatus `jsonapi:"attr,rowStatus"`
+	CreatorId int
+	Creator   *Principal `jsonapi:"attr,creator"`
+	CreatedTs int64      `jsonapi:"attr,createdTs"`
+	UpdaterId int
+	Updater   *Principal `jsonapi:"attr,updater"`
+	UpdatedTs int64      `jsonapi:"attr,updatedTs"`
+
+	// Related fields
+	EnvironmentId int `jsonapi:"attr,environmentId"`
+	// Do not return this to the client since the client always has the database context and fetching the
+	// database object and all its own related objects is a bit expensive.
+	Environment *Environment
+
+	// Domain specific fields
+	Name    string `jsonapi:"attr,name"`
+	Payload string `jsonapi:"attr,payload"`
+}
+
+// PolicyFind is the message to get a policy.
+type PolicyFind struct {
+	ID *int
+
+	// Related fields
+	EnvironmentId *int
+
+	// Domain specific fields
+	Name *string `jsonapi:"attr,name"`
+}
+
+// PolicyUpsert is the message to upsert a policy.
+// NOTE: We use PATCH for Upsert, this is inspired by https://google.aip.dev/134#patch-and-put
+type PolicyUpsert struct {
+	// Standard fields
+	// Value is assigned from the jwt subject field passed by the client.
+	// CreatorId is the ID of the creator.
+	UpdaterId int
+
+	// Related fields
+	EnvironmentId int `jsonapi:"attr,environmentId"`
+
+	// Domain specific fields
+	Name    string `jsonapi:"attr,name"`
+	Payload string `jsonapi:"attr,payload"`
+}
+
+// PolicyService is the backend for policies.
+type PolicyService interface {
+	FindPolicy(ctx context.Context, find *PolicyFind) (*Policy, error)
+	UpsertPolicy(ctx context.Context, upsert *PolicyUpsert) (*Policy, error)
+}
--- store/seed/test/10050__environment.sql
+++ store/seed/test/10050__environment.sql
@@ -101,7 +101,26 @@ INSERT INTO
         creator_id,
         updater_id,
         environment_id,
-        type,
+        name,
+        payload
+    )
+VALUES
+    (
+        1001,
+        101,
+        101,
+        5004,
+        'approval_policy',
+        'MANUAL_APPROVAL_NEVER'
+    );
+
+INSERT INTO
+    policy (
+        id,
+        creator_id,
+        updater_id,
+        environment_id,
+        name,
         payload
     )
 VALUES
@@ -112,4 +131,6 @@ VALUES
         5004,
         'approval_policy',
         'MANUAL_APPROVAL_ALWAYS'
-    );
+    )
+    ON CONFLICT(environment_id, name) DO UPDATE SET
+				payload = excluded.payload;
