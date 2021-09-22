82d53d201084666ea45827f91468811b91553c09
天舟 <tianzhouchen@gmail.com>
2021-09-23 00:30:01 +0800

    Assign default creator/updater upon returning policy
    Former-commit-id: 6cd86a3dd2fa4cd393c56166394003e2eb7a0a7c

--- store/policy.go
+++ store/policy.go
@@ -49,6 +49,8 @@ func (s *PolicyService) FindPolicy(ctx context.Context, find *api.PolicyFind) (*
 		return nil, err
 	} else if len(list) == 0 {
 		ret = &api.Policy{
+			CreatorId:     api.SYSTEM_BOT_ID,
+			UpdaterId:     api.SYSTEM_BOT_ID,
 			EnvironmentId: *find.EnvironmentId,
 			Type:          *find.Type,
 		}
