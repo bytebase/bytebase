f232e7db1007db98c6ce40ae962555576af1cfe9
天舟 <tianzhouchen@gmail.com>
2021-09-23 17:58:45 +0800

    Switch frontend to use the policy api for CRUD environment approval policy

--- frontend/src/types/environment.ts
+++ frontend/src/types/environment.ts
@@ -2,9 +2,6 @@ import { RowStatus } from "./common";
 import { EnvironmentId } from "./id";
 import { Principal } from "./principal";
 
-// Approval policy
-export type ApprovalPolicy = "MANUAL_APPROVAL_NEVER" | "MANUAL_APPROVAL_ALWAYS";
-
 export type Environment = {
   id: EnvironmentId;
 
@@ -18,13 +15,11 @@ export type Environment = {
   // Domain specific fields
   name: string;
   order: number;
-  approvalPolicy: ApprovalPolicy;
 };
 
 export type EnvironmentCreate = {
   // Domain specific fields
   name: string;
-  approvalPolicy: ApprovalPolicy;
 };
 
 export type EnvironmentPatch = {
@@ -34,5 +29,4 @@ export type EnvironmentPatch = {
   // Domain specific fields
   name?: string;
   order?: number;
-  approvalPolicy?: ApprovalPolicy;
 };
