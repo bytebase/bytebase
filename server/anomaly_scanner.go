33f939fa5ddf6d7d596dfa36023e5568be1f56cb
天舟 <tianzhouchen@gmail.com>
2021-10-05 00:36:20 +0800

    Set interval to 30 mins for anomaly scanner
    Former-commit-id: 23684be2fe9aebaa1be1a94d43596b8298641eba

--- server/anomaly_scanner.go
+++ server/anomaly_scanner.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	ANOMALY_SCAN_INTERVAL = time.Duration(1) * time.Second
+	ANOMALY_SCAN_INTERVAL = time.Duration(30) * time.Minute
 )
 
 func NewAnomalyScanner(logger *zap.Logger, server *Server) *AnomalyScanner {
