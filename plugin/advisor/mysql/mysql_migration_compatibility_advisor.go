c7888bbfb90c39c88213a598e95e01bd7d8a20e8
天舟 <tianzhouchen@gmail.com>
2021-10-05 18:27:05 +0800

    Add more incompatible check

--- plugin/advisor/mysql/mysql_migration_compatibility_advisor.go
+++ plugin/advisor/mysql/mysql_migration_compatibility_advisor.go
@@ -59,11 +59,82 @@ type compatibilityChecker struct {
 }
 
 func (v *compatibilityChecker) Enter(in ast.Node) (ast.Node, bool) {
-	if node, ok := in.(*ast.DropTableStmt); ok {
+	compatible := true
+	// DROP DATABASE
+	if _, ok := in.(*ast.DropDatabaseStmt); ok {
+		compatible = false
+		goto END
+	}
+	// RENAME TABLE
+	if _, ok := in.(*ast.RenameTableStmt); ok {
+		compatible = false
+		goto END
+	}
+	// DROP TABLE/VIEW
+	if _, ok := in.(*ast.DropTableStmt); ok {
+		compatible = false
+		goto END
+	}
+	// ALTER TABLE
+	if node, ok := in.(*ast.AlterTableStmt); ok {
+		for _, spec := range node.Specs {
+			fmt.Printf("spec %d: %+v\n\n", spec.Tp, spec)
+			// RENAME COLUMN
+			// DROP COLUMN
+			if spec.Tp == ast.AlterTableRenameColumn ||
+				spec.Tp == ast.AlterTableDropColumn {
+				compatible = false
+				goto END
+			}
+
+			if spec.Tp == ast.AlterTableAddConstraint {
+				// ADD PRIMARY KEY
+				// ADD UNIQUE/UNIQUE KEY/UNIQUE INDEX
+				// ADD FOREIGN KEY
+				if spec.Constraint.Tp == ast.ConstraintPrimaryKey ||
+					spec.Constraint.Tp == ast.ConstraintUniq ||
+					spec.Constraint.Tp == ast.ConstraintUniqKey ||
+					spec.Constraint.Tp == ast.ConstraintUniqIndex ||
+					spec.Constraint.Tp == ast.ConstraintForeignKey {
+					compatible = false
+					goto END
+				}
+				// Check is only supported after 8.0.16 https://dev.mysql.com/doc/refman/8.0/en/create-table-check-constraints.html
+				// ADD CHECK ENFORCED
+				if spec.Constraint.Tp == ast.ConstraintCheck && spec.Constraint.Enforced {
+					compatible = false
+					goto END
+				}
+			}
+
+			// Check is only supported after 8.0.16 https://dev.mysql.com/doc/refman/8.0/en/create-table-check-constraints.html
+			// ALTER CHECK ENFORCED
+			if spec.Tp == ast.AlterTableAlterCheck {
+				if spec.Constraint.Enforced {
+					compatible = false
+					goto END
+				}
+			}
+		}
+	}
+	// ALTER VIEW TBD: https://github.com/pingcap/parser/pull/1252
+	// if node, ok := in.(*ast.AlterViewStmt); ok {
+	// }
+
+	// CREATE UNIQUE INDEX
+	if node, ok := in.(*ast.CreateIndexStmt); ok {
+		if node.KeyType == ast.IndexKeyTypeUnique {
+			compatible = false
+			goto END
+		}
+	}
+
+END:
+	if !compatible {
 		v.advisorList = append(v.advisorList, advisor.Advice{
 			Status:  advisor.Warn,
 			Title:   "Incompatible migration",
-			Content: fmt.Sprintf("%s is backward incompatible", node.Text()),
+			Content: fmt.Sprintf("%s is backward incompatible", in.Text()),
 		})
 	}
 	return in, false
