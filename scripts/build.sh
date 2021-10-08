69a29251d3448857431f2715d9e3fa379ad6c094
天舟 <tianzhouchen@gmail.com>
2021-10-08 13:59:03 +0800

    Really fix the bash build script this time

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # cd to the root directory and run
-# scripts/build.sh [outdir]
+# ./scripts/build.sh [outdir]
 
 # exit when any command fails
 set -e
@@ -10,7 +10,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-if [ `dirname "${BASH_SOURCE[0]}"` != "./scripts" || `dirname "${BASH_SOURCE[0]}"` != "scripts"]
+if [ `dirname "${BASH_SOURCE[0]}"` != "./scripts" ] && [ `dirname "${BASH_SOURCE[0]}"` != "scripts" ]
 then
   echo "${RED}Precheck failed.${NC} Build script must run from root directory: scripts/build.sh"; exit 1;
 fi
@@ -38,7 +38,7 @@ fi
 
 # Step 1 - Build the frontend release version into the backend/server/dist folder
 # Step 2 - Build the monolithic app by building backend release version together with the backend/server/dist (leveraing embed introduced in Golang 1.16).
-VERSION=`cat scripts/VERSION`
+VERSION=`cat ./scripts/VERSION`
 echo "Start building Bytebase monolithic ${VERSION}..."
 
 echo ""
--- scripts/build_bb.sh
+++ scripts/build_bb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # cd to the root directory and run
-# scripts/build_bb.sh [outdir]
+# ./scripts/build_bb.sh [outdir]
 
 # exit when any command fails
 set -e
@@ -10,7 +10,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-if [ `dirname "${BASH_SOURCE[0]}"` != "./scripts" || `dirname "${BASH_SOURCE[0]}"` != "scripts" ]
+if [ `dirname "${BASH_SOURCE[0]}"` != "./scripts" ] && [ `dirname "${BASH_SOURCE[0]}"` != "scripts" ]
 then
   echo "${RED}Precheck failed.${NC} Build script must run from root directory: scripts/build_bb.sh"; exit 1;
 fi
@@ -25,7 +25,7 @@ fi
 
 OUTPUT_BINARY=$OUTPUT_DIR/bb
 
-VERSION=`cat scripts/VERSION`
+VERSION=`cat ./scripts/VERSION`
 echo "Start building bb ${VERSION}..."
 
 flags="-X 'github.com/bytebase/bytebase/bin/bb/cmd.version=${VERSION}'
