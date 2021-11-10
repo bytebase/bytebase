bytebase/bytebase

commit 190d3db9103cf3010bfb73511d9165d2e45249f6

Set session cookie as secure
Bytebase is a mono app combining frontend/backend together so it's OK to use secure session cookie.

Found by https://huntr.dev/

server/jwt.go
@@ -125,7 +125,7 @@ func setTokenCookie(c echo.Context, name, token string, expiration time.Time) {
 	cookie.Path = "/"
 	// Http-only helps mitigate the risk of client side script accessing the protected cookie.
 	cookie.HttpOnly = true
-
+	cookie.Secure = true
 	c.SetCookie(cookie)
 }
 
@@ -135,7 +135,7 @@ func removeTokenCookie(c echo.Context, name string) {
 	cookie.Value = ""
 	cookie.Expires = time.Unix(0, 0)
 	cookie.Path = "/"
-
+	cookie.Secure = true
 	c.SetCookie(cookie)
 }
 
@@ -146,6 +146,7 @@ func setUserCookie(c echo.Context, user *api.Principal, expiration time.Time) {
 	cookie.Value = strconv.Itoa(user.ID)
 	cookie.Expires = expiration
 	cookie.Path = "/"
+	cookie.Secure = true
 	c.SetCookie(cookie)
 }
 

commit 86202c1968b6bf98f6794956b9effc3bf3517795

Do not set cookie as secure since we still allow user to run bytebase on non-https host
See https://github.com/bytebase/bytebase/issues/31

The regression is introduced in 190d3db9103cf3010bfb73511d9165d2e45249f6 . The initial assumption was incorrect since user can specify a different host name from localhost in the browser.

server/jwt.go
@@ -125,7 +125,8 @@ func setTokenCookie(c echo.Context, name, token string, expiration time.Time) {
 	cookie.Path = "/"
 	// Http-only helps mitigate the risk of client side script accessing the protected cookie.
 	cookie.HttpOnly = true
-	cookie.Secure = true
+	// For now, we allow bytebase to run on non-https host, see https://github.com/bytebase/bytebase/issues/31
+	// cookie.Secure = true
 	cookie.SameSite = http.SameSiteStrictMode
 	c.SetCookie(cookie)
 }
@@ -136,7 +137,6 @@ func removeTokenCookie(c echo.Context, name string) {
 	cookie.Value = ""
 	cookie.Expires = time.Unix(0, 0)
 	cookie.Path = "/"
-	cookie.Secure = true
 	c.SetCookie(cookie)
 }
 
@@ -147,7 +147,8 @@ func setUserCookie(c echo.Context, user *api.Principal, expiration time.Time) {
 	cookie.Value = strconv.Itoa(user.ID)
 	cookie.Expires = expiration
 	cookie.Path = "/"
-	cookie.Secure = true
+	// For now, we allow bytebase to run on non-https host, see https://github.com/bytebase/bytebase/issues/31
+	// cookie.Secure = true
 	cookie.SameSite = http.SameSiteStrictMode
 	c.SetCookie(cookie)
 }