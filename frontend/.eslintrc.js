d94424afc07d596b40a6af730631b7feb3bab97e
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2021-11-23 14:21:05 +0800

    Adjust lint rules.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -13,15 +13,12 @@ module.exports = {
   rules: {
     "no-empty-pattern": "warn",
     "no-useless-escape": "warn",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-empty-interface": "warn",
-    "@typescript-eslint/no-empty-function": "warn",
     "vue/no-mutating-props": "warn",
     "vue/multi-word-component-names": "warn",
     "vue/no-unused-components": "warn",
     "vue/no-useless-template-attributes": "warn",
+    "vue/attribute-hyphenation": ["warn", "never"],
   },
   ignorePatterns: ["node_modules", "build", "dist", "public"],
   overrides: [
