4bececc8bf75f0eca7a9e40b047ae2a62a0d7622
zhouzilong2020 <zl@bytebase.com>
2021-11-25 09:23:08 +0800

    style: add comment for HMR

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
 // const path = require("path");
 import vue from "@vitejs/plugin-vue";
 
-const IS_RUNNING_GITPOD = process.env["GITPOD_WORKSPACE_ID"] !== null;
-
 module.exports = {
   plugins: [vue()],
   optimizeDeps: {
@@ -24,6 +22,9 @@ module.exports = {
   },
 };
 
+// NOTE: the following lines is to solve https://github.com/gitpod-io/gitpod/issues/6719
+// tl;dr : the HMR(hot module replacement) will behave differently when VPN is on, and by manually set its prot to 443 should prevent this issue. 
+const IS_RUNNING_GITPOD = process.env["GITPOD_WORKSPACE_ID"] !== null;
 if (IS_RUNNING_GITPOD === true) {
   module.exports.server.hmr = { port: 443 };
 }
