bytebase/bytebase

commit 2ac8cf6bb3a65e3dfa7b9cf8e60d81cbf9d9a159

fix: solve gitpod refreshing issue (#57)

frontend/vite.config.js
@@ -21,3 +21,10 @@ module.exports = {
     // },
   },
 };
+
+// NOTE: the following lines is to solve https://github.com/gitpod-io/gitpod/issues/6719
+// tl;dr : the HMR(hot module replacement) will behave differently when VPN is on, and by manually set its prot to 443 should prevent this issue. 
+const IS_RUNNING_GITPOD = process.env["GITPOD_WORKSPACE_ID"] !== null;
+if (IS_RUNNING_GITPOD === true) {
+  module.exports.server.hmr = { port: 443 };
+}

commit 8ccb85d89513428b82c7d8a5c7c0b5ebcc82e7be

fix: fixed refreshing loop when starting the frontend dev
It seems caused by #57 PR
https://github.com/bytebase/bytebase/pull/57

frontend/vite.config.js
@@ -1,30 +1,0 @@
-// const path = require("path");
-import vue from "@vitejs/plugin-vue";
-
-module.exports = {
-  plugins: [vue()],
-  optimizeDeps: {
-    allowNodeBuiltins: ["postcss", "bytebase"],
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-  resolve: {
-    // alias: {
-    //   "/@/": path.resolve(__dirname, "./src"),
-    // },
-  },
-};
-
-// NOTE: the following lines is to solve https://github.com/gitpod-io/gitpod/issues/6719
-// tl;dr : the HMR(hot module replacement) will behave differently when VPN is on, and by manually set its prot to 443 should prevent this issue. 
-const IS_RUNNING_GITPOD = process.env["GITPOD_WORKSPACE_ID"] !== null;
-if (IS_RUNNING_GITPOD === true) {
-  module.exports.server.hmr = { port: 443 };
-}

frontend/vite.config.ts
@@ -1,0 +1,37 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const SERVER_PORT = process.env.PORT || 3000;
+const HTTPS_PORT = 443;
+
+export default defineConfig(() => {
+  // NOTE: the following lines is to solve https://github.com/gitpod-io/gitpod/issues/6719
+  // tl;dr : the HMR(hot module replacement) will behave differently when VPN is on, and by manually set its prot to 443 should prevent this issue.
+  const IS_RUNNING_GITPOD =
+    process.env["GITPOD_WORKSPACE_ID"] !== null &&
+    process.env["GITPOD_WORKSPACE_ID"] !== undefined;
+  return {
+    plugins: [vue()],
+    optimizeDeps: {
+      allowNodeBuiltins: ["postcss", "bytebase"],
+    },
+    server: {
+      port: SERVER_PORT,
+      proxy: {
+        "/api": {
+          target: "http://localhost:8080/api",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+      hmr: {
+        port: IS_RUNNING_GITPOD ? HTTPS_PORT : SERVER_PORT,
+      },
+    },
+    resolve: {
+      // alias: {
+      //   "/@/": path.resolve(__dirname, "./src"),
+      // },
+    },
+  };
+});