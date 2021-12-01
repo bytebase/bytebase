3625a2f244fb980713cb6eb2509e2b2bd43a2b55
spinningbot <87540428+spinningbot@users.noreply.github.com>
2021-12-01 16:36:44 +0800

    Tenancy: add label API and store service. (#84)
    * Tenancy: add label API and store service.

---
+++ api/label.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"context"
+)
+
+// LabelKey is the available key for labels.
+type LabelKey struct {
+	ID int `jsonapi:"primary,labelKey"`
+
+	// Standard fields
+	CreatorID int
+	Creator   *Principal `jsonapi:"attr,creator"`
+	CreatedTs int64      `jsonapi:"attr,createdTs"`
+	UpdaterID int
+	Updater   *Principal `jsonapi:"attr,updater"`
+	UpdatedTs int64      `jsonapi:"attr,updatedTs"`
+
+	// Domain specific fields
+	Key string `jsonapi:"attr,key"`
+}
+
+// LabelKeyFind is the find request for label keys.
+type LabelKeyFind struct {
+}
+
+// LabelService is the service for labels.
+type LabelService interface {
+	// FindLabelKeys finds all available keys for labels.
+	FindLabelKeys(ctx context.Context, find *LabelKeyFind) ([]LabelKey, error)
+}
