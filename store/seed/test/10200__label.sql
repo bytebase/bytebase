ece5f95ce5b0493f1a4ae8bd5c8fbaebf37ff127
spinningbot <87540428+spinningbot@users.noreply.github.com>
2021-12-01 14:25:57 +0800

    Tenancy: add label_key table to bytebase database. (#82)
    * (FR)tenancy: add label_key table to bytebase database.
    
    Former-commit-id: b59a8044257f1302f2e21f0962bfd5345334c051 [formerly 775a64b4422a14ca4004c4ac315f53e50e6402b9]
    Former-commit-id: 3e00395e182407ba4623e3b5509adef0486ddef7

---
+++ store/seed/test/10200__label.sql
@@ -0,0 +1,31 @@
+-- Label key for `bb.location`.
+INSERT INTO
+    label_key (
+        id,
+        creator_id,
+        updater_id,
+        key
+    )
+VALUES
+    (
+        20001,
+        1,
+        1,
+        'bb.location'
+    );
+
+-- Label key for `bb.tenant`.
+INSERT INTO
+    label_key (
+        id,
+        creator_id,
+        updater_id,
+        key
+    )
+VALUES
+    (
+        20002,
+        1,
+        1,
+        'bb.tenant'
+    );
