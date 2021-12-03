4f1d5daad669d77744dc80aa23466f6c52cba17b
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2021-12-03 17:09:05 +0800

    feat(frontend): add k-bar experience to bytebase console (#100)
    * feat(frontend): add k-bar experience to bytebase console
    
    cmd+k (ctrl+k) to open k-bar.
    Now we've introduced main modules (Home, Anomaly Center, Projects, Databases, Instances, Environments, Settings, Inbox) navigation with global shortcuts (e.g. g h -> go home).
    Bookmarks, project detail, database detail page is also reachable.
    Action's on QuickActionPanel is dynamically prepended to the kbar's action list.
    
    * fix(frontend): unifying kbar action id namespace.
    
    Using dot "." separated namespace.
    
    * fix(frontend): add comments to explain
    
    * fix(frontend): make active actions's background the same as other controls' hover/active state
    
    use `bg-control-bg-hover`
    
    * fix(frontend): use `defineAction` instead of `createAction`
    
    this function is only used to type inferring. so "define" is better than "create".
    
    * fix(frontend): do not use npmmirror in yarn.lock
    
    * fix(frontend): update kbar action namespace prefix "bb."
    
    Use "bb." as a "root" namespace. Now we use these "primary" types below:
    - bb.navigation.*
    - bb.project.*
    - bb.database.*
    - bb.bookmark.*
    - bb.quickaction.*
    
    Former-commit-id: a0fa3be467b5fd234a7ee475c9f877637f6f5aba

---
+++ frontend/src/components/KBar/RenderResults.vue
@@ -0,0 +1,80 @@
+<template>
+  <KBarResults
+    :items="matches.results"
+    :item-height="itemHeight"
+    class="max-h-72"
+  >
+    <template #item="{ item, index, active }">
+      <div v-if="typeof item === 'string'" class="section" :data-index="index">
+        {{ item }}
+      </div>
+      <div v-else class="item" :class="{ active }" :data-index="index">
+        <div class="content">
+          <div class="main">
+            <span>{{ item.name }}</span>
+
+            <span v-for="(tag, i) in item.data?.tags" :key="i" class="tag">
+              {{ tag }}
+            </span>
+          </div>
+          <span v-if="item.subtitle" class="subtitle">
+            {{ item.subtitle }}
+          </span>
+        </div>
+
+        <div v-if="item.shortcut?.length > 0" aria-hidden class="shortcut">
+          <kbd v-for="(sc, j) in item.shortcut" :key="j">{{ sc }}</kbd>
+        </div>
+      </div>
+    </template>
+  </KBarResults>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useKBarMatches, KBarResults } from "@bytebase/vue-kbar";
+
+export default defineComponent({
+  name: "RenderResults",
+  components: { KBarResults },
+  setup() {
+    const matches = useKBarMatches();
+    const itemHeight = (params: { item: any; index: number }) => {
+      if (typeof params.item === "string") return 32;
+      return 48;
+    };
+
+    return { matches, itemHeight };
+  },
+});
+</script>
+
+<style scoped>
+.section {
+  @apply h-8 px-4 text-xs uppercase text-gray-500 flex items-center bg-gray-50;
+}
+.item {
+  @apply h-12 box-border px-3 py-4 text-lg bg-transparent border-l-4 border-transparent cursor-pointer flex justify-between items-center gap-2;
+}
+.item.active {
+  @apply bg-control-bg-hover border-current;
+}
+.content {
+  @apply flex flex-col;
+}
+.main {
+  @apply flex items-center text-base gap-1;
+}
+.tag {
+  @apply inline-block text-xs px-1 py-0.5 bg-black bg-opacity-10 rounded-sm;
+}
+.subtitle {
+  @apply text-xs text-gray-500;
+}
+.shortcut {
+  @apply grid grid-flow-col gap-1 justify-self-end text-gray-500;
+}
+.shortcut kbd {
+  @apply w-6 h-6 flex items-center justify-center bg-black bg-opacity-10 rounded text-sm;
+}
+</style>
--- frontend/src/components/ProjectListSidePanel.vue
+++ frontend/src/components/ProjectListSidePanel.vue
@@ -14,12 +14,15 @@ import { useStore } from "vuex";
 import { Project, UNKNOWN_ID } from "../types";
 import { projectName, projectSlug } from "../utils";
 import { BBOutlineItem } from "../bbkit/types";
+import { Action, defineAction, useRegisterActions } from "@bytebase/vue-kbar";
+import { useRouter } from "vue-router";
 
 export default {
   name: "ProjectListSidePanel",
   props: {},
   setup() {
     const store = useStore();
+    const router = useRouter();
 
     const currentUser = computed(() => store.getters["auth/currentUser"]());
 
@@ -51,6 +54,23 @@ export default {
           return a.name.localeCompare(b.name);
         });
     });
+
+    const kbarActions = computed((): Action[] => {
+      const actions = outlineItemList.value.map((proj: any) =>
+        defineAction({
+          // here `id` looks like "bb.project.1234"
+          id: `bb.project.${proj.id}`,
+          section: "Projects",
+          name: proj.name,
+          keywords: "project",
+          perform: () => {
+            router.push({ path: proj.link });
+          },
+        })
+      );
+      return actions;
+    });
+    useRegisterActions(kbarActions);
 
     return {
       outlineItemList,
