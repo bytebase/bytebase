eeb0a1357aec39987d71c0be8c60d5f7bdc65a99
ecmadao <wlec@outlook.com>
2021-12-03 21:35:32 +0800

    feat: use pre-commit to lint commit message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    // need to use relative path case our node project isn't in the same folder level as git
-    // similar problem: https://github.com/conventional-changelog/commitlint/issues/613
-    extends: ['./frontend/node_modules/@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional']
 };
