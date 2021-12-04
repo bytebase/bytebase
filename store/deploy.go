2bfc253d7ab51a75eaf6e52dc6578e178b289165
spinningbot <87540428+spinningbot@users.noreply.github.com>
2021-12-04 16:17:11 +0800

    Tenancy: add deployment configuration API and service. (#111)
    * Tenancy: add deployment configuration API and service.
    
    * Tenancy: use enum for operator.
    
    * Tenancy: move operator type comment to enum comment.

---
+++ store/deploy.go
@@ -0,0 +1,137 @@
+package store
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bytebase/bytebase/api"
+	"github.com/bytebase/bytebase/common"
+	"go.uber.org/zap"
+)
+
+var (
+	_ api.DeploymentConfigService = (*DeploymentConfigService)(nil)
+)
+
+// DeploymentConfigService represents a service for managing deployment configurations.
+type DeploymentConfigService struct {
+	l  *zap.Logger
+	db *DB
+}
+
+// NewDeploymentConfigService returns a new instance of DeploymentConfigService.
+func NewDeploymentConfigService(logger *zap.Logger, db *DB) *DeploymentConfigService {
+	return &DeploymentConfigService{l: logger, db: db}
+}
+
+// FindDeploymentConfig finds the deployment configuration in a project.
+func (s *DeploymentConfigService) FindDeploymentConfig(ctx context.Context, find *api.DeploymentConfigFind) (*api.DeploymentConfig, error) {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, FormatError(err)
+	}
+	defer tx.Rollback()
+
+	rows, err := tx.QueryContext(ctx, `
+		SELECT
+			id,
+			creator_id,
+			created_ts,
+			updater_id,
+			updated_ts,
+			project_id,
+		    name,
+			config
+		FROM deployment_config`,
+	)
+	if err != nil {
+		return nil, FormatError(err)
+	}
+	defer rows.Close()
+
+	// Iterate over result set and deserialize rows into list.
+	var ret []*api.DeploymentConfig
+	for rows.Next() {
+		var cfg api.DeploymentConfig
+		if err := rows.Scan(
+			&cfg.ID,
+			&cfg.CreatorID,
+			&cfg.CreatedTs,
+			&cfg.UpdaterID,
+			&cfg.UpdatedTs,
+			&cfg.ProjectID,
+			&cfg.Name,
+			&cfg.Payload,
+		); err != nil {
+			return nil, FormatError(err)
+		}
+
+		ret = append(ret, &cfg)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, FormatError(err)
+	}
+
+	switch len(ret) {
+	case 0:
+		return nil, nil
+	case 1:
+		return ret[0], nil
+	default:
+		return nil, &common.Error{Code: common.Conflict, Err: fmt.Errorf("found %d deployment configurations with filter %+v, expect 1", len(ret), find)}
+	}
+}
+
+// UpsertDeploymentConfig upserts a deployment configuration to a project.
+func (s *DeploymentConfigService) UpsertDeploymentConfig(ctx context.Context, upsert *api.DeploymentConfigUpsert) (*api.DeploymentConfig, error) {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, FormatError(err)
+	}
+	defer tx.Rollback()
+
+	row, err := tx.QueryContext(ctx, `
+	INSERT INTO deployment_config (
+		creator_id,
+		updater_id,
+		project_id,
+		name,
+		config
+	)
+	VALUES (?, ?, ?, ?, ?)
+	ON CONFLICT(project_id) DO UPDATE SET
+		creator_id = excluded.creator_id,
+		updater_id = excluded.updater_id
+		name = excluded.name,
+		config = excluded.config
+	RETURNING id, creator_id, created_ts, updater_id, updated_ts, project_id, name, config
+	`,
+		upsert.UpdaterID,
+		upsert.UpdaterID,
+		upsert.ProjectID,
+		upsert.Name,
+		upsert.Payload,
+	)
+
+	if err != nil {
+		return nil, FormatError(err)
+	}
+	defer row.Close()
+
+	row.Next()
+	var cfg api.DeploymentConfig
+	if err := row.Scan(
+		&cfg.ID,
+		&cfg.CreatorID,
+		&cfg.CreatedTs,
+		&cfg.UpdaterID,
+		&cfg.UpdatedTs,
+		&cfg.ProjectID,
+		&cfg.Name,
+		&cfg.Payload,
+	); err != nil {
+		return nil, FormatError(err)
+	}
+
+	return &cfg, nil
+}
