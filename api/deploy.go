6cf1d23ae709f6892d1a3fc921a3fc5e02810e37
spinningbot <87540428+spinningbot@users.noreply.github.com>
2021-12-04 16:17:11 +0800

    Tenancy: add deployment configuration API and service. (#111)
    * Tenancy: add deployment configuration API and service.
    
    * Tenancy: use enum for operator.
    
    * Tenancy: move operator type comment to enum comment.
    
    Former-commit-id: 2bfc253d7ab51a75eaf6e52dc6578e178b289165

---
+++ api/deploy.go
@@ -0,0 +1,104 @@
+package api
+
+import (
+	"context"
+)
+
+// DeploymentConfig is the API message for deployment configurations.
+type DeploymentConfig struct {
+	ID int `jsonapi:"primary,deploymentConfig"`
+
+	// Standard fields
+	CreatorID int
+	Creator   *Principal `jsonapi:"attr,creator"`
+	CreatedTs int64      `jsonapi:"attr,createdTs"`
+	UpdaterID int
+	Updater   *Principal `jsonapi:"attr,updater"`
+	UpdatedTs int64      `jsonapi:"attr,updatedTs"`
+
+	// Related fields
+	ProjectID int
+	Project   *Project `jsonapi:"relation,project"`
+
+	// Domain specific fields
+	Name string `jsonapi:"attr,name"`
+	// Payload encapsulates DeploymentSchedule in json string format. We use json instead jsonapi because this configuration isn't queryable as HTTP format.
+	Payload string `jsonapi:"attr,payload"`
+}
+
+// DeploymentSchedule is the API message for deployment schedule.
+type DeploymentSchedule struct {
+	Deployments []*Deployment `json:"deployments"`
+}
+
+// Deployment is the API message for deployment.
+type Deployment struct {
+	Spec *DeploymentSpec `json:"spec"`
+}
+
+// DeploymentSpec is the API message for deployment specification.
+type DeploymentSpec struct {
+	Selector *LabelSelector `json:"selector"`
+}
+
+// LabelSelector is the API message for label selector.
+type LabelSelector struct {
+	// MatchExpressions is a list of label selector requirements. The requirements are ANDed.
+	MatchExpressions *LabelSelectorRequirement `json:"matchExpressions"`
+}
+
+// OperatorType is the type of label selector requirement operator.
+// Valid operators are In, Exists.
+// Note: NotIn and DoesNotExist are not supported initially.
+type OperatorType string
+
+const (
+	// InOperatorType is the operator type for In.
+	InOperatorType OperatorType = "In"
+	// ExistsOperatorType is the operator type for Exists.
+	ExistsOperatorType OperatorType = "Exists"
+)
+
+// LabelSelectorRequirement is the API message for label selector.
+type LabelSelectorRequirement struct {
+	// Key is the label key that the selector applies to.
+	Key string `json:"key"`
+
+	// Operator represents a key's relationship to a set of values.
+	Operator OperatorType `json:"operator"`
+
+	// Values is an array of string values. If the operator is In or NotIn, the values array must be non-empty. If the operator is Exists or DoesNotExist, the values array must be empty. This array is replaced during a strategic merge patch.
+	Values []string `json:"values"`
+}
+
+// DeploymentConfigFind is the find request for deployment configs.
+type DeploymentConfigFind struct {
+	ID *int
+
+	// Related fields
+	ProjectID *int
+}
+
+// DeploymentConfigUpsert is the message to upsert a deployment configuration.
+// NOTE: We use PATCH for Upsert, this is inspired by https://google.aip.dev/134#patch-and-put
+type DeploymentConfigUpsert struct {
+	// Standard fields
+	// Value is assigned from the jwt subject field passed by the client.
+	// CreatorID is the ID of the creator.
+	UpdaterID int
+
+	// Related fields
+	ProjectID *int `jsonapi:"attr,projectId"`
+
+	// Domain specific fields
+	Name    string `jsonapi:"attr,name"`
+	Payload string `jsonapi:"attr,payload"`
+}
+
+// DeploymentConfigService is the service for deployment configurations.
+type DeploymentConfigService interface {
+	// FindDeploymentConfig finds the deployment configuration in a project.
+	FindDeploymentConfig(ctx context.Context, find *DeploymentConfigFind) (*DeploymentConfig, error)
+	// UpsertDeploymentConfig upserts a deployment configuration to a project.
+	UpsertDeploymentConfig(ctx context.Context, upsert *DeploymentConfigUpsert) (*DeploymentConfig, error)
+}
