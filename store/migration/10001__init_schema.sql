390f04e48e5eb875673448dbd623bfd3b23622f8
zhouzilong2020 <zl@bytebase.com>
2021-12-08 22:04:23 +0800

    refactor(schema): fix wrong default type in TABLE task

--- store/migration/10001__init_schema.sql
+++ store/migration/10001__init_schema.sql
@@ -842,8 +842,7 @@ CREATE TABLE task (
     `type` TEXT NOT NULL CHECK (`type` LIKE 'bb.task.%'),
     payload TEXT NOT NULL DEFAULT '',
     -- not_before_ts specifies the earliest allowed time for a task to be executed -- 
-    not_before_ts BIGINT NOT NULL DEFAULT '',
-
+    `not_before_ts` BIGINT NOT NULL DEFAULT 0
 );
 
 CREATE INDEX idx_task_pipeline_id_stage_id ON task(pipeline_id, stage_id);
