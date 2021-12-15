cb08b5d47773f6763f0dcfd44777d36aad53f031
Luke Zhou <60960532+zhouzilong2020@users.noreply.github.com>
2021-12-15 12:01:19 +0800

    feat(frontend): i18n support for auth page (#158)
    * feat: i18n for auth page
    
    * refactor: extracted common words
    
    * style(annotation): changed old-user to existing-user
    
    * style(annotation): changed a space between english and chinese character
    
    * style(annotation): adopted a respectful expression in chinese
    
    Former-commit-id: 225342dc03a0d741b27a607a1ad44d408c33024d [formerly 63768e41d94a4e3fe838089ef55cdb2ccb65d034]
    Former-commit-id: a4c6703213f360aa340151dd64a7f9d61a89ed1e

--- frontend/src/views/auth/PasswordReset.vue
+++ frontend/src/views/auth/PasswordReset.vue
@@ -7,7 +7,7 @@
         alt="Bytebase"
       />
       <h2 class="mt-6 text-3xl leading-9 font-extrabold text-main">
-        Reset your password
+        {{ $t("auth.password-reset.title") }}
       </h2>
     </div>
 
@@ -19,8 +19,7 @@
               for="email"
               class="block text-base font-medium leading-5 text-control"
             >
-              Enter your email address and we will send you a password reset
-              link
+              {{ $t("auth.password-reset.content") }}
             </label>
             <div class="mt-4 rounded-md shadow-sm">
               <input
@@ -29,20 +28,7 @@
                 type="email"
                 required
                 placeholder="jim@example.com"
-                class="
-                  appearance-none
-                  block
-                  w-full
-                  px-3
-                  py-2
-                  border border-control-border
-                  rounded-md
-                  placeholder-control-placeholder
-                  focus:outline-none
-                  focus:shadow-outline-blue
-                  focus:border-control-border
-                  sm:text-sm sm:leading-5
-                "
+                class="appearance-none block w-full px-3 py-2 border border-control-border rounded-md placeholder-control-placeholder focus:outline-none focus:shadow-outline-blue focus:border-control-border sm:text-sm sm:leading-5"
               />
             </div>
           </div>
@@ -54,7 +40,7 @@
                 :disabled="!allowReset"
                 class="btn-primary w-full flex justify-center py-2 px-4"
               >
-                Send reset link
+                {{ $t("auth.password-reset.send-reset-link") }}
               </button>
             </span>
           </div>
@@ -68,7 +54,7 @@
       </div>
       <div class="relative flex justify-center text-sm">
         <router-link to="/auth/signin" class="accent-link bg-white px-2">
-          Return to Sign in
+          {{ $t("auth.password-reset.return-to-sign-in") }}
         </router-link>
       </div>
     </div>
