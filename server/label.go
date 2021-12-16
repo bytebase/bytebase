7aad19c9bc2327b1eb2373803b684ad6c865ceff
spinningbot <87540428+spinningbot@users.noreply.github.com>
2021-12-16 17:16:21 +0800

    feat(tenancy): implement patch label key API (#184)
    * feat(tenancy): implement patch label key API
    
    * feat(tenancy): address comment
    
    Former-commit-id: df1a0749eebf0d04deb9cf811528eb00316b0f20

--- server/label.go
+++ server/label.go
@@ -2,9 +2,12 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/bytebase/bytebase/api"
+	"github.com/bytebase/bytebase/common"
 	"github.com/google/jsonapi"
 	"github.com/labstack/echo/v4"
 )
@@ -14,7 +17,10 @@ var ()
 func (s *Server) registerLabelRoutes(g *echo.Group) {
 	g.GET("/label", func(c echo.Context) error {
 		ctx := context.Background()
-		find := &api.LabelKeyFind{}
+		rowStatus := api.Normal
+		find := &api.LabelKeyFind{
+			RowStatus: &rowStatus,
+		}
 		list, err := s.LabelService.FindLabelKeyList(ctx, find)
 		if err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to fetch label keys").SetInternal(err)
@@ -23,6 +29,36 @@ func (s *Server) registerLabelRoutes(g *echo.Group) {
 		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
 		if err := jsonapi.MarshalPayload(c.Response().Writer, list); err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to marshal label keys response").SetInternal(err)
+		}
+		return nil
+	})
+
+	g.PATCH("/label/:id", func(c echo.Context) error {
+		ctx := context.Background()
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("id is not a number: %s", c.Param("id"))).SetInternal(err)
+		}
+
+		patch := &api.LabelKeyPatch{
+			ID:        id,
+			UpdaterID: c.Get(getPrincipalIDContextKey()).(int),
+		}
+		if err := jsonapi.UnmarshalPayload(c.Request().Body, patch); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, "Malformatted patch label key request").SetInternal(err)
+		}
+
+		labelKey, err := s.LabelService.PatchLabelKey(ctx, patch)
+		if err != nil {
+			if common.ErrorCode(err) == common.NotFound {
+				return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Label ID not found: %d", id))
+			}
+			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to patch label ID: %v", id)).SetInternal(err)
+		}
+
+		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
+		if err := jsonapi.MarshalPayload(c.Response().Writer, labelKey); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to marshal label key response: %v", id)).SetInternal(err)
 		}
 		return nil
 	})
