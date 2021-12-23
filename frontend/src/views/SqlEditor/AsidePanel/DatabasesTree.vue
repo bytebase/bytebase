7d2b726ec9efca2f951d6aff261510b239444de3
Yunwei Xiao <xiaoluoboding@gmail.com>
2021-12-23 23:26:21 +0800

    feat(sql-editor): add the connection tree view

--- frontend/src/views/SqlEditor/AsidePanel/DatabasesTree.vue
+++ frontend/src/views/SqlEditor/AsidePanel/DatabasesTree.vue
@@ -1,1 +1,16 @@
-<template>Databases Tree</template>
+<template>
+  <h1>Connection Tree</h1>
+  <NTree 
+    block-line
+    :data="connectionTree"
+  />
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const connectionTree = computed(() => store.state.sqlEditor.connectionTree);
+</script>
--- frontend/src/views/SqlEditor/EditorPanel/EditorActions.vue
+++ frontend/src/views/SqlEditor/EditorPanel/EditorActions.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="sqleditor-editor-actions">
-    <n-button type="primary">
-      <mdi:play class="h-5 w-5" /> Run (⌘+⏎)
-    </n-button>
+    <div class="actions-left">
+        <n-button type="primary">
+        <mdi:play class="h-5 w-5" /> Run (⌘+⏎)
+      </n-button>
+    </div>
+    <div class="actions-right space-x-2">
+      <n-button secondary strong type="primary">
+        <carbon:save class="h-5 w-5" /> &nbsp; Save (⌘+S)
+      </n-button>
+      <n-button>
+        <carbon:share class="h-5 w-5" /> &nbsp; Share (⌘+S)
+      </n-button>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .sqleditor-editor-actions {
-  @apply w-full p-2;
+  @apply w-full flex justify-between items-center p-2;
 }
 </style>
