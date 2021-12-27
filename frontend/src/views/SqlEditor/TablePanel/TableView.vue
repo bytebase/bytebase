b43ededf92464a34bab9540a8d33aeab9d2b2faa
Yunwei Xiao <xiaoluoboding@gmail.com>
2021-12-27 17:08:08 +0800

    feat(sql-editor): make sure the table view is always full of the pane

--- frontend/src/views/SqlEditor/TablePanel/TableView.vue
+++ frontend/src/views/SqlEditor/TablePanel/TableView.vue
@@ -1,21 +1,36 @@
 <template>
-  <NDataTable :columns="columns" :data="data" :bordered="false">
-    <template #empty>
-      <div class="p-20 h-full">Click Run to execute the query.</div>
-    </template>
-  </NDataTable>
+  <div class="table-view h-full" ref="tableViewRef">
+    <NDataTable
+      size="mini"
+      :columns="columns"
+      :data="data"
+      :bordered="false"
+      flex-height
+      :style="{ height: `${tableMaxHeight}px` }"
+    >
+      <template #empty>
+        <div class="p-20 h-full flex justify-center items-center">
+          Click Run to execute the query.
+        </div>
+      </template>
+    </NDataTable>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useStore } from "vuex";
 import { useVuex } from "@vueblocks/vue-use-vuex";
+import { useResizeObserver } from "@vueuse/core";
 
 const store = useStore();
 
 const { useState } = useVuex("sqlEditor", store);
 
 const { queryResult } = useState(["queryResult"]) as any;
+
+const tableViewRef = ref<HTMLDivElement>();
+const tableMaxHeight = ref(0);
 
 const columns = computed(() => {
   return queryResult.value.length > 0
@@ -28,4 +43,11 @@ const columns = computed(() => {
     : [];
 });
 const data = computed(() => queryResult.value);
+
+// make sure the table view is always full of the pane
+useResizeObserver(tableViewRef, (entries) => {
+  const entry = entries[0];
+  const { height } = entry.contentRect;
+  tableMaxHeight.value = height;
+});
 </script>
