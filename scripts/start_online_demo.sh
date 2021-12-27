687591fce5d8a4a78e92f8c87a7e92ebb45cf346
Zilong Zhou <529620861@qq.com>
2021-12-27 17:34:03 +0800

    feat: added scripted

--- scripts/start_online_demo.sh
+++ scripts/start_online_demo.sh
@@ -2,14 +2,14 @@
 
 function seedData(){
     echo 'Seeding data for online demo'
-    bytebase --host http://localhost --port 80 --demo >& /dev/null &
+    bytebase --host http://localhost --port 80 --demo --data /var/opt/bytebase >& /dev/null &
     sleep 5
     ps | grep 'bytebase --demo' | grep -v  grep | xargs kill -9
 }
 
 function startReadonly(){
     echo 'Initiating readonly mode for online demo'
-    bytebase --host http://localhost --port 80  --readonly
+    bytebase --host http://localhost --port 80  --readonly --data /var/opt/bytebase
 }
 
 seedData; startReadonly
