9e49fe7876a36468e7a127ae7115852599652fab
spinningbot <87540428+spinningbot@users.noreply.github.com>
2021-12-27 11:23:06 +0800

    feat(tenancy): allow setting database labels in create database (#257)
    * feat(tenancy): allow setting database labels in create database
    
    * feat(tenancy): address comment
    
    * feat(tenancy): address comment

--- server/database.go
+++ server/database.go
@@ -37,9 +37,16 @@ func (s *Server) registerDatabaseRoutes(g *echo.Group) {
 			}
 			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to create database").SetInternal(err)
 		}
-
 		if err := s.composeDatabaseRelationship(ctx, database); err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to fetch created database relationship").SetInternal(err)
+		}
+
+		// Set database labels, except bb.environment is immutable and must match instance environment.
+		// This needs to be after we compose database relationship.
+		if databaseCreate.Labels != nil {
+			if err := s.setDatabaseLabels(ctx, *databaseCreate.Labels, database, databaseCreate.CreatorID); err != nil {
+				return err
+			}
 		}
 
 		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
@@ -145,41 +152,22 @@ func (s *Server) registerDatabaseRoutes(g *echo.Group) {
 			return echo.NewHTTPError(http.StatusBadRequest, "Malformatted patch database request").SetInternal(err)
 		}
 
+		database, err := s.composeDatabaseByFind(ctx, &api.DatabaseFind{
+			ID: &id,
+		})
+		if err != nil {
+			if common.ErrorCode(err) == common.NotFound {
+				return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Database ID not found: %d", id))
+			}
+			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to patch database ID: %v", id)).SetInternal(err)
+		}
+
 		// Patch database labels
-		// We will completely replace the old labels with the new ones
+		// We will completely replace the old labels with the new ones, except bb.environment is immutable and
+		// must match instance environment.
 		if databasePatch.Labels != nil {
-			database, err := s.composeDatabaseByFind(ctx, &api.DatabaseFind{
-				ID: &id,
-			})
-			if err != nil {
-				if common.ErrorCode(err) == common.NotFound {
-					return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Database ID not found: %d", id))
-				}
-				return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to patch database ID: %v", id)).SetInternal(err)
-			}
-
-			// databasePatch.Labels is a JSON-encoded string
-			var labels []*api.DatabaseLabel
-			json.Unmarshal([]byte(*databasePatch.Labels), &labels)
-
-			// For scalability, each database can have up to four labels for now.
-			if len(labels) > api.DatabaseLabelSizeMax {
-				err := fmt.Errorf("database labels are up to a maximum of %d", api.DatabaseLabelSizeMax)
-				return echo.NewHTTPError(http.StatusBadRequest, err.Error()).SetInternal(err)
-			}
-
-			rowStatus := api.Normal
-			labelKeyList, err := s.LabelService.FindLabelKeyList(ctx, &api.LabelKeyFind{RowStatus: &rowStatus})
-			if err != nil {
-				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to find label key list").SetInternal(err)
-			}
-
-			if err = validateDatabaseLabelList(labels, labelKeyList, database.Instance.Environment.Name); err != nil {
-				return echo.NewHTTPError(http.StatusInternalServerError, "Failed to validate database labels").SetInternal(err)
-			}
-
-			if _, err = s.LabelService.SetDatabaseLabelList(ctx, labels, databasePatch.ID, databasePatch.UpdaterID); err != nil {
-				return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to set database labels, database ID: %v", id)).SetInternal(err)
+			if err := s.setDatabaseLabels(ctx, *databasePatch.Labels, database, databasePatch.UpdaterID); err != nil {
+				return err
 			}
 		}
 
@@ -198,7 +186,7 @@ func (s *Server) registerDatabaseRoutes(g *echo.Group) {
 			}
 		}
 
-		database, err := s.DatabaseService.PatchDatabase(ctx, databasePatch)
+		database, err = s.DatabaseService.PatchDatabase(ctx, databasePatch)
 		if err != nil {
 			if common.ErrorCode(err) == common.NotFound {
 				return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Database ID not found: %d", id))
@@ -637,6 +625,32 @@ func (s *Server) registerDatabaseRoutes(g *echo.Group) {
 		}
 		return nil
 	})
+}
+
+func (s *Server) setDatabaseLabels(ctx context.Context, labelsJSON string, database *api.Database, updaterID int) error {
+	var labels []*api.DatabaseLabel
+	json.Unmarshal([]byte(labelsJSON), &labels)
+
+	// For scalability, each database can have up to four labels for now.
+	if len(labels) > api.DatabaseLabelSizeMax {
+		err := fmt.Errorf("database labels are up to a maximum of %d", api.DatabaseLabelSizeMax)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error()).SetInternal(err)
+	}
+
+	rowStatus := api.Normal
+	labelKeyList, err := s.LabelService.FindLabelKeyList(ctx, &api.LabelKeyFind{RowStatus: &rowStatus})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to find label key list").SetInternal(err)
+	}
+
+	if err = validateDatabaseLabelList(labels, labelKeyList, database.Instance.Environment.Name); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to validate database labels").SetInternal(err)
+	}
+
+	if _, err = s.LabelService.SetDatabaseLabelList(ctx, labels, database.ID, updaterID); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to set database labels, database ID: %v", database.ID)).SetInternal(err)
+	}
+	return nil
 }
 
 func (s *Server) composeDatabaseByFind(ctx context.Context, find *api.DatabaseFind) (*api.Database, error) {
