c35db15aeb5f34afd6c70a658663ebec0ab15c24
Zilong Zhou <529620861@qq.com>
2021-12-27 17:03:31 +0800

    feat: added online demo starting script

---
+++ scripts/start_online_demo.sh
@@ -0,0 +1,13 @@
+function seedData(){
+    echo 'Seeding data for online demo'
+    ./.air/bytebase --host http://localhost --port 80 --demo >& /dev/null &
+    sleep 5
+    ps | grep 'bytebase --demo' | grep -v  grep | xargs kill -9
+}
+
+function startReadonly(){
+    echo 'Initiating readonly mode for online demo'
+    ./.air/bytebase --host http://localhost --port 80  --readonly
+}
+
+seedData; startReadonly
