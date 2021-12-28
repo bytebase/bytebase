fd1e412dd32c3f1380ff5c466a1491cd7c88ab3f
zhouzilong2020 <zl@bytebase.com>
2021-12-28 11:40:59 +0800

    fix: test

--- scripts/start_online_demo.sh
+++ scripts/start_online_demo.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-function seedData(){
-    echo 'Seeding data for online demo'
-    bytebase --host http://localhost --port 80 --data /var/opt/bytebase &
-    sleep 10
-    echo 'Killing seeding program'
-    ps | grep 'bytebase' | grep -v  grep | xargs kill -9
-}
+# function seedData(){
+#     echo 'Seeding data for online demo'
+#     bytebase --host http://localhost --port 80 --data /var/opt/bytebase &
+#     sleep 10
+#     echo 'Killing seeding program'
+#     ps | grep 'bytebase' | grep -v  grep | xargs kill -9
+# }
 
 function startReadonly(){
     echo 'Initiating readonly mode for online demo'
-    bytebase --host http://localhost --port 80 --readonly --data /var/opt/bytebase
+    bytebase --host http://localhost --port 80 --readonly --demo --data /var/opt/bytebase
 }
 
-seedData; startReadonly
+startReadonly
