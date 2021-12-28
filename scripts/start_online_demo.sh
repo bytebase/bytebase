cc3d24296a48bb1ba7a765ba1540ed80a3121b64
zhouzilong2020 <zl@bytebase.com>
2021-12-28 11:21:29 +0800

    fix: test

--- scripts/start_online_demo.sh
+++ scripts/start_online_demo.sh
@@ -3,8 +3,9 @@
 function seedData(){
     echo 'Seeding data for online demo'
     bytebase --host http://localhost --port 80 --demo --data /var/opt/bytebase >& /dev/null &
-    sleep 5
-    ps | grep 'bytebase --demo' | grep -v  grep | xargs kill -9
+    sleep 10
+    echo 'Killing seeding program'
+    ps | grep 'bytebase' | grep -v  grep | xargs kill -9
 }
 
 function startReadonly(){
