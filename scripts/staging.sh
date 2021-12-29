da2c7351a0aa2f68eced8c58f51913e49f94c58a
Zilong Zhou <529620861@qq.com>
2021-12-29 13:12:42 +0800

    feat: added script for staging env

---
+++ scripts/staging.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# For now, we use this script to start our staging preview on render
+# by changing the ENTRYPOINT and CMD at the dockerfile to this.
+
+# example usages:
+# ./staging.sh
+# ./staging.sh https://example.com
+# ./staging.sh https://example.com:8080
+
+# If no parameter is passed, use https://staging.bytebase.com as host and 80 as port by default
+ONLINE_DEMO_HOST='https://staging.bytebase.com'
+ONLINE_DEMO_PORT='80'
+if [ $1 ]; then
+    PROTOCAL=`echo $1 | awk -F ':' '{ print $1 }'`
+    URI=`echo $1 | awk -F '[/:]' '{ print $4; }'`
+    PORT=`echo $1 | awk -F '[/:]' '{ print $5; }'`
+
+    ONLINE_DEMO_HOST=$PROTOCAL://$URI
+
+    if [ $PORT ]; then
+        ONLINE_DEMO_PORT=$PORT
+    fi
+fi
+
+echo "Starting Bytebase at ${ONLINE_DEMO_HOST}:${ONLINE_DEMO_PORT}..."
+
+bytebase --host ${ONLINE_DEMO_HOST} --port ${ONLINE_DEMO_PORT} --data /var/opt/bytebase
