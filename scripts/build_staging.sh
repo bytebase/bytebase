b69c37c7321e64a1068c4b7c4997ee162b7adacd
Zilong Zhou <529620861@qq.com>
2021-12-29 17:02:19 +0800

    feat: added commit info

--- scripts/build_staging.sh
+++ scripts/build_staging.sh
@@ -8,10 +8,10 @@ BUILD_USER="unknown"
 echo "Start building Bytebase staging docker image version: ${VERSION}, commit: ${GIT_COMMIT}..."
 
 # we append 5 digits commit hash to the version for stating env (e.g. v0.10.0-abcde)
-# STAGING_VERSION=${VERSION}-${GIT_COMMIT:0:5}
+STAGING_VERSION=${VERSION}-${GIT_COMMIT:0:5}
 
-# CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build \
-#     --tags ${MODE} \
-#     -ldflags="-w -s -X 'github.com/bytebase/bytebase/bin/server/cmd.version=${STAGING_VERSION}' -X 'github.com/bytebase/bytebase/bin/server/cmd.goversion=${GO_VERSION}' -X 'github.com/bytebase/bytebase/bin/server/cmd.gitcommit=${GIT_COMMIT}' -X 'github.com/bytebase/bytebase/bin/server/cmd.buildtime=${BUILD_TIME}' -X 'github.com/bytebase/bytebase/bin/server/cmd.builduser=${BUILD_USER}'" \
-#     -o bytebase \
-#     ./bin/server/main.go
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build \
+    --tags ${MODE} \
+    -ldflags="-w -s -X 'github.com/bytebase/bytebase/bin/server/cmd.version=${STAGING_VERSION}' -X 'github.com/bytebase/bytebase/bin/server/cmd.goversion=${GO_VERSION}' -X 'github.com/bytebase/bytebase/bin/server/cmd.gitcommit=${GIT_COMMIT}' -X 'github.com/bytebase/bytebase/bin/server/cmd.buildtime=${BUILD_TIME}' -X 'github.com/bytebase/bytebase/bin/server/cmd.builduser=${BUILD_USER}'" \
+    -o bytebase \
+    ./bin/server/main.go
