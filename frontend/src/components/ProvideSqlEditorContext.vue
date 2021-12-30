4abac3458ce5a3d85bf0d5bccb883b3028088a99
Yunwei Xiao <xiaoluoboding@gmail.com>
2021-12-30 11:17:03 +0800

    feat(sql-editor): support search databases and add loading spin ui (#293)
    * feat(sql-editor): initial sql-editor route
    
    * feat(sql-editor): split the feature modules use the splitpanes components
    
    * refactor(sql-editor): rename sql-editor dir to SqlEditor
    
    * refactor(sql-editor): redefine sql editor component structure
    
    * Merge branch 'main' into feat/sql-editor
    
    # Conflicts:
    #	frontend/src/router/index.ts
    
    * Merge branch 'main' into feat/sql-editor
    
    * feat(sql-editor): add the monaco-editor as query editor
    
    * feat(sql-editor): auto import all modules
    
    * feat(sql-editor): add the connection tree view
    
    * feat(sql-editor): add monaco-editor auto-completion
    
    * feat(sql-editor): execute sql by typed sql queries
    
    * feat(sql-editor): execute queries with selected queries
    
    * feat(sql-editor): make sure the table view is always full of the pane
    
    * feat(sql-editor): make sql-editor store typed works
    
    * feat(sql-editor): add connectionSlug logic
    
    * refactor(sql-editor): add tabs ui for database & queries
    
    * feat(sql-editor): add notification for execute queries
    
    * feat(sql-editor): initial the sql-editor store
    
    * refactor(sql-editor): delete the comment
    
    * refactor(sql-editor): remove the unuse config
    
    * refactor(sql-editor): rename some variable names
    
    * refactor(sql-editor): reduce some code
    
    * feat(sql-editor): add the notification if server is in readonly mode
    
    * refactor(sql-editor): fix the sentence
    
    * refactor(sql-editor): rename the sql-editor child route
    
    * refactor(sql-editor): remove the banner
    
    * feat(frontend): fixed the font size bug
    
    * feat(sql-editor): add the spin ui when connection is loading
    
    * feat(sql-editor): add the search ui for connection tree
    
    * fix(sql-editor): fixed the connections selected error when first time enter the sql-editor
    
    Former-commit-id: d681dc88fac85d6e7273843ae45f2998223b641b

--- frontend/src/components/ProvideSqlEditorContext.vue
+++ frontend/src/components/ProvideSqlEditorContext.vue
@@ -5,26 +5,36 @@
 <script lang="ts" setup>
 import { watchEffect } from "vue";
 import { useStore } from "vuex";
-import { Instance, Database, Table, ConnectionAtom } from "../types";
-// import { idFromSlug } from "../utils";
+import {
+  Instance,
+  Database,
+  Table,
+  ConnectionAtom,
+  ConnectionAtomType,
+} from "../types";
 
 const store = useStore();
 
 const prepareSqlEdtiorContext = async function () {
+  store.dispatch("sqlEditor/setConnectionContext", {
+    isLoadingTree: true,
+  });
   let connectionTree = [];
 
-  const mapConnectionAtom = (item: Instance | Database | Table) => {
-    const connectionAtom: ConnectionAtom = {
-      id: item.id,
-      key: item.id,
-      label: item.name,
+  const mapConnectionAtom =
+    (type: ConnectionAtomType) => (item: Instance | Database | Table) => {
+      const connectionAtom: ConnectionAtom = {
+        id: item.id,
+        key: item.id,
+        label: item.name,
+        type,
+      };
+
+      return connectionAtom;
     };
 
-    return connectionAtom;
-  };
-
   const instanceList = await store.dispatch("instance/fetchInstanceList");
-  connectionTree = instanceList.map(mapConnectionAtom);
+  connectionTree = instanceList.map(mapConnectionAtom("instance"));
 
   for (const instance of instanceList) {
     const databaseList = await store.dispatch(
@@ -35,7 +45,7 @@ const prepareSqlEdtiorContext = async function () {
     const instanceItem = connectionTree.find(
       (item: ConnectionAtom) => item.id === instance.id
     );
-    instanceItem.children = databaseList.map(mapConnectionAtom);
+    instanceItem.children = databaseList.map(mapConnectionAtom("database"));
 
     for (const db of databaseList) {
       const tableList = await store.dispatch(
@@ -47,11 +57,14 @@ const prepareSqlEdtiorContext = async function () {
         (item: ConnectionAtom) => item.id === db.id
       );
 
-      databaseItem.children = tableList.map(mapConnectionAtom);
+      databaseItem.children = tableList.map(mapConnectionAtom("table"));
     }
   }
 
   store.dispatch("sqlEditor/setConnectionTree", connectionTree);
+  store.dispatch("sqlEditor/setConnectionContext", {
+    isLoadingTree: false,
+  });
 };
 
 watchEffect(prepareSqlEdtiorContext);
