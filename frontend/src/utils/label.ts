d68d9f8de34d1c7498ba8599e3074f3e53bacc1d
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2021-12-30 11:14:10 +0800

    fix: address comments

--- frontend/src/utils/label.ts
+++ frontend/src/utils/label.ts
@@ -1,5 +1,7 @@
 import { Label } from "../types";
 
+// reserved labels (e.g. bb.environment) have zero ID and their values are immutable.
+// see api/label.go for more details
 export const RESERVED_LABEL_ID = 0;
 
 export const isReservedLabel = (label: Label): boolean => {
