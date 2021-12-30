b6b1b13355f0c75f7f46ab804958f5bef721ea14
Yunwei Xiao <xiaoluoboding@gmail.com>
2021-12-30 16:35:06 +0800

    feat(sql-editor): show table schema when selected table in connection tree (#298)
    * feat(sql-editor): initial sql-editor route
    
    * feat(sql-editor): split the feature modules use the splitpanes components
    
    * refactor(sql-editor): rename sql-editor dir to SqlEditor
    
    * refactor(sql-editor): redefine sql editor component structure
    
    * Merge branch 'main' into feat/sql-editor
    
    # Conflicts:
    #	frontend/src/router/index.ts
    
    * Merge branch 'main' into feat/sql-editor
    
    * feat(sql-editor): add the monaco-editor as query editor
    
    * feat(sql-editor): auto import all modules
    
    * feat(sql-editor): add the connection tree view
    
    * feat(sql-editor): add monaco-editor auto-completion
    
    * feat(sql-editor): execute sql by typed sql queries
    
    * feat(sql-editor): execute queries with selected queries
    
    * feat(sql-editor): make sure the table view is always full of the pane
    
    * feat(sql-editor): make sql-editor store typed works
    
    * feat(sql-editor): add connectionSlug logic
    
    * refactor(sql-editor): add tabs ui for database & queries
    
    * feat(sql-editor): add notification for execute queries
    
    * feat(sql-editor): initial the sql-editor store
    
    * refactor(sql-editor): delete the comment
    
    * refactor(sql-editor): remove the unuse config
    
    * refactor(sql-editor): rename some variable names
    
    * refactor(sql-editor): reduce some code
    
    * feat(sql-editor): add the notification if server is in readonly mode
    
    * refactor(sql-editor): fix the sentence
    
    * refactor(sql-editor): rename the sql-editor child route
    
    * refactor(sql-editor): remove the banner
    
    * feat(frontend): fixed the font size bug
    
    * feat(sql-editor): add the spin ui when connection is loading
    
    * feat(sql-editor): add the search ui for connection tree
    
    * fix(sql-editor): fixed the connections selected error when first time enter the sql-editor
    
    * refactor(sql-editor): rename dir
    
    * feat(sql-editor): show table schema when selected tabe in connection tree
    
    * style(sql-editor): change the splitter style
    
    * refactor(sql-editor): i18n for EditorHeader
    
    * refactor(sql-editor): declare a const for pane size
    
    Former-commit-id: 20c26082cb03a7036057dec3b1d4b0e7abf2cac9

--- frontend/src/views/SqlEditor/AsidePanel/AsidePanel.vue
+++ frontend/src/views/SqlEditor/AsidePanel/AsidePanel.vue
@@ -2,21 +2,59 @@
   <div class="aside-panel h-full">
     <n-tabs type="segment" default-value="databases" class="h-full">
       <n-tab-pane name="databases" tab="Databases">
-        <Splitpanes horizontal class="default-theme">
-          <Pane size="100"><DatabasesTree /></Pane>
-          <Pane size="0" max-size="40"><TableSchema /></Pane>
+        <Splitpanes
+          horizontal
+          class="default-theme"
+          :dbl-click-splitter="false"
+          @resized="handleResized"
+        >
+          <Pane :size="databasePaneSzie"><DatabaseTree /></Pane>
+          <Pane
+            :size="FULL_HEIGHT - databasePaneSzie"
+            :max-size="TABLE_SCHEMA_PANE_SIZE"
+          >
+            <TableSchema @close-pane="handleCloseTableSchemaPane" />
+          </Pane>
         </Splitpanes>
       </n-tab-pane>
-      <n-tab-pane name="queries" tab="Queries">
-        Queries Page
-      </n-tab-pane>
+      <n-tab-pane name="queries" tab="Queries"> Queries Page </n-tab-pane>
     </n-tabs>
   </div>
 </template>
 
 <script lang="ts" setup>
-import DatabasesTree from "./DatabasesTree.vue";
+import { ref, watch } from "vue";
+import { useNamespacedState } from "vuex-composition-helpers";
+
+import type { SqlEditorState } from "../../../types";
+import DatabaseTree from "./DatabaseTree.vue";
 import TableSchema from "./TableSchema.vue";
+
+const FULL_HEIGHT = 100;
+const DATABASE_PANE_SIZE = 60;
+const TABLE_SCHEMA_PANE_SIZE = FULL_HEIGHT - DATABASE_PANE_SIZE;
+
+const { connectionContext } = useNamespacedState<SqlEditorState>("sqlEditor", [
+  "connectionContext",
+]);
+
+const databasePaneSzie = ref(FULL_HEIGHT);
+
+const handleResized = (data: any) => {
+  const [top, bottom] = data;
+  databasePaneSzie.value = top.size;
+};
+
+const handleCloseTableSchemaPane = () => {
+  databasePaneSzie.value = FULL_HEIGHT;
+};
+
+watch(
+  () => connectionContext.value.selectedTableName,
+  () => {
+    databasePaneSzie.value = DATABASE_PANE_SIZE;
+  }
+);
 </script>
 
 <style scoped>
