ceb599db528100c92a17f49a560d0232edc4d9bb
spinningbot <87540428+spinningbot@users.noreply.github.com>
2021-12-31 10:04:22 +0800

    feat(tenancy): check schema version and schema matching when a database is transferred to a tenant mode project (#300)
    Former-commit-id: c93d31a944ee7c6b1040eb3962263ff866784fe0 [formerly 99dfc7292fc361b52ee8f7eb45f40b2120690937]
    Former-commit-id: c27eca81f25182b0244d9f99b17820fb79fab53b

--- server/database.go
+++ server/database.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -160,6 +161,45 @@ func (s *Server) registerDatabaseRoutes(g *echo.Group) {
 				return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Database ID not found: %d", id))
 			}
 			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to patch database ID: %v", id)).SetInternal(err)
+		}
+
+		if databasePatch.ProjectID != nil {
+			toProject, err := s.composeProjectByID(ctx, *databasePatch.ProjectID)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Project ID not found: %d", *databasePatch.ProjectID))
+			}
+			if toProject.TenantMode == api.TenantModeTenant {
+				// For database being transferred to a tenant mode project, its schema version and schema has to match a peer tenant database.
+				// When a peer tenant database doesn't exist, we will return an error if there are databases in the project with the same name.
+				peerSchemaVersion, peerSchema, err := s.getSchemaFromPeerTenantDatabase(ctx, database.Instance, *databasePatch.ProjectID, database.Name)
+				if err != nil {
+					return err
+				}
+
+				// Tenant database exists when peerSchemaVersion or peerSchema are not empty.
+				if peerSchemaVersion != "" || peerSchema != "" {
+					driver, err := getDatabaseDriver(ctx, database.Instance, database.Name, s.l)
+					if err != nil {
+						return err
+					}
+					defer driver.Close(ctx)
+					schemaVersion, err := getLatestSchemaVersion(ctx, driver, database.Name)
+					if err != nil {
+						return fmt.Errorf("failed to get migration history for database %q: %w", database.Name, err)
+					}
+					if peerSchemaVersion != schemaVersion {
+						return fmt.Errorf("the schema version %q does not match the peer database schema version %q in the target tenant mode project %q", schemaVersion, peerSchemaVersion, toProject.Name)
+					}
+
+					var schemaBuf bytes.Buffer
+					if err := driver.Dump(ctx, database.Name, &schemaBuf, true /* schemaOnly */); err != nil {
+						return fmt.Errorf("failed to get database schema for database %q: %w", database.Name, err)
+					}
+					if peerSchema != schemaBuf.String() {
+						return fmt.Errorf("the schema for database %q does not match the peer database schema in the target tenant mode project %q", database.Name, toProject.Name)
+					}
+				}
+			}
 		}
 
 		// Patch database labels
