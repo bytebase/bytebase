c114472f4f08399f70a7342e5b89e6a3dc33def5
spinningbot <87540428+spinningbot@users.noreply.github.com>
2022-01-03 13:06:27 +0800

    feat(tenancy): require bb.environment on deployment (#320)

--- store/seed/test/10210__deployment_config.sql
+++ store/seed/test/10210__deployment_config.sql
@@ -1,4 +1,4 @@
--- Deployment config with regional deployments for project 3005.
+-- Deployment config with regional deployments for tenant mode project.
 INSERT INTO
     deployment_config (
         id,
@@ -14,6 +14,25 @@ VALUES
         101,
         101,
         3005,
-        'regional',
-        '{"deployments":[{"spec":{"selector":{"matchExpressions":[{"key":"location","operator":"IN","values":["us-central1","europe-west1"]}]}}},{"spec":{"selector":{"matchExpressions":[{"key":"location","operator":"EXISTS"}]}}}]}'
+        'regional-1',
+        '{"deployments":[{"spec":{"selector":{"matchExpressions":[{"key":"bb.environment","operator":"In","values":["Dev"]},{"key":"bb.location","operator":"In","values":["earth"]}]}}},{"spec":{"selector":{"matchExpressions":[{"key":"bb.environment","operator":"In","values":["Integration"]},{"key":"bb.tenant","operator":"Exists"}]}}}]}'
+    );
+
+INSERT INTO
+    deployment_config (
+        id,
+        creator_id,
+        updater_id,
+        project_id,
+        name,
+        config
+    )
+VALUES
+    (
+        21002,
+        101,
+        101,
+        3006,
+        'regional-2',
+        '{"deployments":[{"spec":{"selector":{"matchExpressions":[{"key":"bb.environment","operator":"In","values":["Dev"]},{"key":"bb.location","operator":"In","values":["earth"]}]}}},{"spec":{"selector":{"matchExpressions":[{"key":"bb.environment","operator":"In","values":["Integration"]},{"key":"bb.tenant","operator":"Exists"}]}}}]}'
     );
