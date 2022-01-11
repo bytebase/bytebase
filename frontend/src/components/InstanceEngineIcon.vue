c0f3cacd41e63a632adb257af6555453f88a32b9
Luke Zhou <60960532+zhouzilong2020@users.noreply.github.com>
2022-01-11 14:24:51 +0800

    style: added dynamic src (#356)
    Former-commit-id: e25f6cd53ec4ab25f8fb819b1a7db13b73d8810f

--- frontend/src/components/InstanceEngineIcon.vue
+++ frontend/src/components/InstanceEngineIcon.vue
@@ -1,36 +1,12 @@
 <template>
   <div class="tooltip-wrapper">
     <span class="tooltip">{{ instance.engineVersion }}</span>
-    <img
-      v-if="instance.engine == 'CLICKHOUSE'"
-      class="h-4 w-auto"
-      src="../assets/db-clickhouse.png"
-    />
-    <img
-      v-else-if="instance.engine == 'MYSQL'"
-      class="h-4 w-auto"
-      src="../assets/db-mysql.png"
-    />
-    <img
-      v-else-if="instance.engine == 'POSTGRES'"
-      class="h-4 w-auto"
-      src="../assets/db-postgres.png"
-    />
-    <img
-      v-else-if="instance.engine == 'SNOWFLAKE'"
-      class="h-4 w-auto"
-      src="../assets/db-snowflake.png"
-    />
-    <img
-      v-else-if="instance.engine == 'TIDB'"
-      class="h-4 w-auto"
-      src="../assets/db-tidb.png"
-    />
+    <img class="h-4 w-auto" :src="SelectedEngineIconPath" />
   </div>
 </template>
 
 <script lang="ts">
-import { PropType } from "vue";
+import { computed, PropType } from "vue";
 import { Instance } from "../types";
 
 export default {
@@ -40,6 +16,19 @@ export default {
       required: true,
       type: Object as PropType<Instance>,
     },
+  },
+  setup(props) {
+    const EngineIconPath = {
+      MYSQL: new URL("../assets/db-mysql.png", import.meta.url).href,
+      POSTGRES: new URL("../assets/db-postgres.png", import.meta.url).href,
+      TIDB: new URL("../assets/db-tidb.png", import.meta.url).href,
+      SNOWFLAKE: new URL("../assets/db-snowflake.png", import.meta.url).href,
+      CLICKHOUSE: new URL("../assets/db-clickhouse.png", import.meta.url).href,
+    };
+    const SelectedEngineIconPath = computed(() => {
+      return EngineIconPath[props.instance.engine];
+    });
+    return { SelectedEngineIconPath };
   },
 };
 </script>
