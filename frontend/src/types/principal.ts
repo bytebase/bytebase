5891f839dadc7fabefc19a821ca92f0427904d06
Luke Zhou <60960532+zhouzilong2020@users.noreply.github.com>
2022-01-12 19:29:37 +0800

    fix: fixed error when admin create new members (#368)
    Former-commit-id: d28ef9a2e2ae9a846b8e6884bc05a66341ef2aea [formerly 5f7d1d0f600414960652a7d2263a83c56d3f353a]
    Former-commit-id: 24e8a6e97095a104a392637982e04047f3218a2a

--- frontend/src/types/principal.ts
+++ frontend/src/types/principal.ts
@@ -1,6 +1,7 @@
 // This is a facet of the underlying identity entity.
 // For now, there is only user type. In the future,
 
+import { AuthProviderType } from ".";
 import { PrincipalId } from "./id";
 import { RoleType } from "./member";
 
@@ -19,6 +20,7 @@ export type Principal = {
   updatedTs: number;
 
   // Domain specific fields
+  authProvider: AuthProviderType;
   type: PrincipalType;
   name: string;
   email: string;
@@ -27,6 +29,7 @@ export type Principal = {
 
 export type PrincipalCreate = {
   // Domain specific fields
+  authProvider: AuthProviderType;
   name: string;
   email: string;
 };
