d51ee016c710ee934f93b920d3ff3b268e92a41e
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-01-13 18:35:02 +0800

    feat(sql-editor): alter the DML schema with statements from SQL editor

--- frontend/src/views/SqlEditor/EditorPanel/EditorPanel.vue
+++ frontend/src/views/SqlEditor/EditorPanel/EditorPanel.vue
@@ -3,10 +3,41 @@
     <EditorAction />
 
     <QueryEditor />
+
+    <BBModal
+      v-if="isShowExecutingHint"
+      :title="$t('common.tips')"
+      @close="handleClose"
+    >
+      <ExecuteHint @close="handleClose" />
+    </BBModal>
   </div>
 </template>
 
 <script lang="ts" setup>
+import {
+  useNamespacedState,
+  useNamespacedActions,
+} from "vuex-composition-helpers";
+
+import { SqlEditorState, SqlEditorActions } from "../../../types";
 import EditorAction from "./EditorAction.vue";
 import QueryEditor from "./QueryEditor.vue";
+import ExecuteHint from "./ExecuteHint.vue";
+
+const { isShowExecutingHint } = useNamespacedState<SqlEditorState>(
+  "sqlEditor",
+  ["isShowExecutingHint"]
+);
+
+const { setSqlEditorState } = useNamespacedActions<SqlEditorActions>(
+  "sqlEditor",
+  ["setSqlEditorState"]
+);
+
+const handleClose = () => {
+  setSqlEditorState({
+    isShowExecutingHint: false,
+  });
+};
 </script>
