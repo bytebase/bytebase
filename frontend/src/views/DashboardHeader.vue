1b54a86fdde78c6d1b50c3fb0cbf0d4bd0322ad1
tianzhou <t@bytebase.com>
2022-01-13 17:43:09 +0800

    feat: add the issues tab as the 1st tab in the dashboard header for DBA and owner (#376)
    Former-commit-id: f43950212408b03b972ae1508ab5b352a958d888 [formerly 277dee852f4d441f276880ee5ff7d2f49b3ec827]
    Former-commit-id: 281a3596eaea47d5726f089ff5a6f9fee89141e4

--- frontend/src/views/DashboardHeader.vue
+++ frontend/src/views/DashboardHeader.vue
@@ -3,46 +3,43 @@
     <div class="flex items-center">
       <div class="flex-shrink-0 w-44">
         <router-link to="/" class="select-none" active-class exact-active-class>
-          <img
-            class="h-12 w-auto"
-            src="../assets/logo-full.svg"
-            alt="Bytebase"
-          />
+          <img class="h-12 w-auto" src="../assets/logo-full.svg" alt="Bytebase" />
         </router-link>
       </div>
       <div class="hidden sm:block">
-        <div class="ml-6 flex items-baseline space-x-1">
-          <router-link to="/project" class="bar-link px-2 py-2 rounded-md">{{
-            $t("common.projects")
-          }}</router-link>
-
-          <router-link to="/db" class="bar-link px-2 py-2 rounded-md">
-            {{ $t("common.databases") }}
-          </router-link>
-
+        <div class="ml-6 flex items-baseline space-x-1 whitespace-nowrap">
           <router-link
             v-if="showDBAItem"
+            to="/issue"
+            class="bar-link px-2 py-2 rounded-md"
+          >{{ $t("common.issues") }}</router-link>
+
+          <router-link to="/project" class="bar-link px-2 py-2 rounded-md">
+            {{
+              $t("common.projects")
+            }}
+          </router-link>
+
+          <router-link to="/db" class="bar-link px-2 py-2 rounded-md">{{ $t("common.databases") }}</router-link>
+
+          <router-link
             to="/instance"
             class="bar-link px-2 py-2 rounded-md"
-            >{{ $t("common.instances") }}</router-link
-          >
+          >{{ $t("common.instances") }}</router-link>
 
           <router-link
             to="/environment"
             class="bar-link px-2 py-2 rounded-md"
-            >{{ $t("common.environments") }}</router-link
-          >
+          >{{ $t("common.environments") }}</router-link>
           <router-link
             to="/setting/member"
             class="bar-link px-2 py-2 rounded-md"
-            >{{ $t("common.settings") }}</router-link
-          >
+          >{{ $t("common.settings") }}</router-link>
           <router-link
             v-if="isDevFeatures"
             to="/sql-editor"
             class="bar-link px-2 py-2 rounded-md"
-            >{{ $t("sql-editor.self") }}</router-link
-          >
+          >{{ $t("sql-editor.self") }}</router-link>
         </div>
       </div>
     </div>
@@ -52,23 +49,21 @@
           v-if="isDevFeatures"
           class="hidden md:flex sm:flex-row items-center space-x-2 text-sm font-medium"
         >
-          <span class="hidden lg:block font-normal text-accent">{{
-            $t("setting.plan.self")
-          }}</span>
+          <span class="hidden lg:block font-normal text-accent">
+            {{
+              $t("setting.plan.self")
+            }}
+          </span>
           <div
             class="bar-link"
             :class="currentPlan == 0 ? 'underline' : ''"
             @click.prevent="switchToFree"
-          >
-            {{ $t("setting.plan.free") }}
-          </div>
+          >{{ $t("setting.plan.free") }}</div>
           <div
             class="bar-link"
             :class="currentPlan == 1 ? 'underline' : ''"
             @click.prevent="switchToTeam"
-          >
-            {{ $t("setting.plan.team") }}
-          </div>
+          >{{ $t("setting.plan.team") }}</div>
           <!-- <div
             class="bar-link"
             :class="currentPlan == 2 ? 'underline' : ''"
@@ -81,30 +76,26 @@
           v-if="!isRelease"
           class="hidden md:flex sm:flex-row items-center space-x-2 text-sm font-medium"
         >
-          <span class="hidden lg:block font-normal text-accent">{{
-            $t("settings.profile.role")
-          }}</span>
+          <span class="hidden lg:block font-normal text-accent">
+            {{
+              $t("settings.profile.role")
+            }}
+          </span>
           <div
             class="bar-link"
             :class="currentUser.role == 'OWNER' ? 'underline' : ''"
             @click.prevent="switchToOwner"
-          >
-            {{ $t("common.role.owner") }}
-          </div>
+          >{{ $t("common.role.owner") }}</div>
           <div
             class="bar-link"
             :class="currentUser.role == 'DBA' ? 'underline' : ''"
             @click.prevent="switchToDBA"
-          >
-            {{ $t("common.role.dba") }}
-          </div>
+          >{{ $t("common.role.dba") }}</div>
           <div
             class="bar-link"
             :class="currentUser.role == 'DEVELOPER' ? 'underline' : ''"
             @click.prevent="switchToDeveloper"
-          >
-            {{ $t("common.role.developer") }}
-          </div>
+          >{{ $t("common.role.developer") }}</div>
         </div>
         <router-link to="/inbox" exact-active-class>
           <span
@@ -144,32 +135,38 @@
       Open: "block", closed: "hidden"
   -->
   <div v-if="state.showMobileMenu" class="block md:hidden">
-    <router-link to="/project" class="bar-link rounded-md block px-3 py-2">{{
-      $t("common.projects")
-    }}</router-link>
+    <router-link v-if="showDBAItem" to="/issue" class="bar-link rounded-md block px-3 py-2">
+      {{
+        $t("common.issues")
+      }}
+    </router-link>
 
-    <router-link to="/db" class="bar-link rounded-md block px-3 py-2">{{
-      $t("common.databases")
-    }}</router-link>
+    <router-link to="/project" class="bar-link rounded-md block px-3 py-2">
+      {{
+        $t("common.projects")
+      }}
+    </router-link>
+
+    <router-link to="/db" class="bar-link rounded-md block px-3 py-2">
+      {{
+        $t("common.databases")
+      }}
+    </router-link>
 
     <router-link
-      v-if="showDBAItem"
       to="/instance"
       class="bar-link rounded-md block px-3 py-2"
-      >{{ $t("common.instances") }}</router-link
-    >
+    >{{ $t("common.instances") }}</router-link>
 
     <router-link
       to="/environment"
       class="bar-link rounded-md block px-3 py-2"
-      >{{ $t("common.environments") }}</router-link
-    >
+    >{{ $t("common.environments") }}</router-link>
 
     <router-link
       to="/setting/member"
       class="bar-link rounded-md block px-3 py-2"
-      >{{ $t("common.settings") }}</router-link
-    >
+    >{{ $t("common.settings") }}</router-link>
   </div>
 </template>
 
@@ -208,7 +205,6 @@ export default {
 
     const showDBAItem = computed((): boolean => {
       return (
-        !store.getters["plan/feature"]("bb.dba-workflow") ||
         isDBAOrOwner(currentUser.value.role)
       );
     });
