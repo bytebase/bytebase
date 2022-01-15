a101a4260c32a75418ab344c1e14f3b9833b17fc
Danny <spinningbot@hotmail.com>
2022-01-15 09:18:16 +0800

    feat(tenancy): add seed data for tenant mode project using database name pattern

--- store/seed/test/10180__repository.sql
+++ store/seed/test/10180__repository.sql
@@ -96,3 +96,53 @@ VALUES
         0,
         "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     );
+
+-- This is for demo purpose and the webhook has already been deleted
+INSERT INTO
+    repository (
+        id,
+        creator_id,
+        updater_id,
+        vcs_id,
+        project_id,
+        name,
+        full_path,
+        web_url,
+        branch_filter,
+        base_directory,
+        file_path_template,
+        schema_path_template,
+        external_id,
+        external_webhook_id,
+        webhook_url_host,
+        webhook_endpoint_id,
+        webhook_secret_token,
+        access_token,
+        expires_ts,
+        refresh_token
+    )
+VALUES
+    (
+        18003,
+        101,
+        101,
+        17001,
+        3005,
+        "Tenant",
+        "bytebase-demo/tenant",
+        "http://gitlab.bytebase.com/bytebase-demo/tenant",
+        "master",
+        "bytebase",
+        "{{ENV_NAME}}/{{DB_NAME}}__{{VERSION}}__{{TYPE}}__{{DESCRIPTION}}.sql",
+        "{{ENV_NAME}}/.{{DB_NAME}}__LATEST.sql",
+        -- Refers to the bytebase-demo/tenant
+        "15",
+        -- Refers to the webhook in bytebase-demo/tenant
+        "62",
+        "https://demo.bytebase.com",
+        "e48bf625-7f58-4d6b-a5be-b97af313d7ea",
+        "xxxxxxxxxxxxxxxx",
+        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+        0,
+        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    );
