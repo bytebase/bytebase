e7d76e7e01a807ce58a03da3d4f93e246715487e
Zilong Zhou <529620861@qq.com>
2022-01-17 19:40:24 +0800

    feat: added payload field

--- store/migration/10001__init_schema.sql
+++ store/migration/10001__init_schema.sql
@@ -278,7 +278,9 @@ CREATE TABLE project_member (
     updated_ts BIGINT NOT NULL DEFAULT (strftime('%s', 'now')),
     project_id INTEGER NOT NULL REFERENCES project (id),
     -- allowed role_provider are 'BYTEBASE', 'GITLAB_SELF_HOST'.
-    role_provider TEXT NOT NULL,
+    role_provider TEXT NOT NULL DEFAULT "BYTEBASE",
+    -- role_provider_payload is determined by the type of role_provider
+    role_provider_payload TEXT NOT NULL DEFAULT,
     -- allowed roles are 'OWNER', 'DEVELOPER'.
     role TEXT NOT NULL,
     principal_id INTEGER NOT NULL REFERENCES principal (id),
