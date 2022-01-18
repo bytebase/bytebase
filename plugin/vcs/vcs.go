120200c1f35c1227d7fd0418d85f8b1fa3d22fac
Zilong Zhou <529620861@qq.com>
2022-01-18 10:35:37 +0800

    feat: added membershipstatus to projectMember

--- plugin/vcs/vcs.go
+++ plugin/vcs/vcs.go
@@ -83,10 +83,11 @@ type UserInfo struct {
 
 // ProjectMember is the API message for project member info.
 type ProjectMember struct {
-	Email       string    `json:"email"`
-	Name        string    `json:"name"`
-	State       UserState `json:"state"`
-	AccessLevel int32     `json:"accessLevel"`
+	Email           string    `json:"email"`
+	Name            string    `json:"name"`
+	State           UserState `json:"state"`
+	MembershipState UserState `json:"membershipState"`
+	AccessLevel     int32     `json:"accessLevel"`
 }
 
 // Provider is the interface for VCS provider.
