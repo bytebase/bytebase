efc50a053257055dacdd003af153ba479677b82c
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-01-18 11:56:49 +0800

    feat(tenancy): label group-by selector UI updated (#398)
    Former-commit-id: fc65d78deca9ad6fa4f5aa5ecf796d5103016088

--- frontend/src/components/TenantDatabaseTable/index.ts
+++ frontend/src/components/TenantDatabaseTable/index.ts
@@ -1,7 +1,13 @@
 import TenantDatabaseTable from "./TenantDatabaseTable.vue";
 import DatabaseMatrix from "./DatabaseMatrix.vue";
 import DeployDatabaseTable from "./DeployDatabaseTable.vue";
+import YAxisRadioGroup from "./YAxisRadioGroup.vue";
 
 export default TenantDatabaseTable;
 
-export { TenantDatabaseTable, DatabaseMatrix, DeployDatabaseTable };
+export {
+  TenantDatabaseTable,
+  DatabaseMatrix,
+  DeployDatabaseTable,
+  YAxisRadioGroup,
+};
