c121a0e20d6f5accd3398046e2412d74f268dc92
tianzhou <t@bytebase.com>
2022-01-20 17:04:44 +0800

    fix: use the correct syntax to hide the subscription menu item (#444)

--- frontend/src/views/SettingSidebar.vue
+++ frontend/src/views/SettingSidebar.vue
@@ -64,7 +64,7 @@
             class="outline-item group w-full flex items-center pl-11 pr-2 py-2"
           >{{ $t("settings.sidebar.version-control") }}</router-link>
           <router-link
-            v-if="isDev"
+            v-if="isDev()"
             to="/setting/subscription"
             class="outline-item group w-full flex items-center pl-11 pr-2 py-2"
           >{{ $t("settings.sidebar.subscription") }}</router-link>
