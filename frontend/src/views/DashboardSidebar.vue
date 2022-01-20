83c8839f58f773d580a2e4c3cca0c31523a26068
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-01-20 17:42:10 +0800

    refactor(sql-editor): move sql editor menu above the anomaly center

--- frontend/src/views/DashboardSidebar.vue
+++ frontend/src/views/DashboardSidebar.vue
@@ -6,18 +6,18 @@
       {{ $t("common.home") }}
     </router-link>
     <router-link
-      to="/anomaly-center"
-      class="outline-item group flex items-center px-2 py-2"
-    >
-      <heroicons-outline:shield-exclamation class="w-5 h-5 mr-2" />
-      {{ $t("anomaly-center") }}
-    </router-link>
-    <router-link
       to="/sql-editor"
       class="outline-item group flex items-center px-2 py-2"
     >
       <heroicons-outline:terminal class="w-5 h-5 mr-2" />
       {{ $t("sql-editor.self") }}
+    </router-link>
+    <router-link
+      to="/anomaly-center"
+      class="outline-item group flex items-center px-2 py-2"
+    >
+      <heroicons-outline:shield-exclamation class="w-5 h-5 mr-2" />
+      {{ $t("anomaly-center") }}
     </router-link>
     <div>
       <BookmarkListSidePanel />
