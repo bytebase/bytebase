f022f1c840515a2ab9502a235663257db032e01a
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-01-25 18:47:58 +0800

    feat(test): implement a test checking service start properly (#471)

--- bin/server/main.go
+++ bin/server/main.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"math/rand"
-	"time"
-
 	"github.com/bytebase/bytebase/bin/server/cmd"
-
-	// Import sqlite3 driver.
-	_ "github.com/mattn/go-sqlite3"
-
-	// Register clickhouse driver.
-	_ "github.com/bytebase/bytebase/plugin/db/clickhouse"
-	// Register mysql driver.
-	_ "github.com/bytebase/bytebase/plugin/db/mysql"
-	// Register postgres driver.
-	_ "github.com/bytebase/bytebase/plugin/db/pg"
-	_ "github.com/lib/pq"
-
-	// Register snowflake driver.
-	_ "github.com/bytebase/bytebase/plugin/db/snowflake"
-	// Register sqlite driver.
-	_ "github.com/bytebase/bytebase/plugin/db/sqlite"
-
-	// Register pingcap parser driver.
-	_ "github.com/pingcap/tidb/types/parser_driver"
-	// Register fake advisor.
-	_ "github.com/bytebase/bytebase/plugin/advisor/fake"
-	// Register mysql advisor.
-	_ "github.com/bytebase/bytebase/plugin/advisor/mysql"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
 	cmd.Execute()
 }
