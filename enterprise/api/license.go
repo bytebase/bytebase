35d15b485a601669a0e1cc0cc058dea639c2a602
ecmadao <wlec@outlook.com>
2022-01-26 00:25:11 +0800

    feat: init enterprise (#465)
    * feat: init enterprise
    
    * feat: resolve comments
    
    * feat: resolve comment
    
    * Update enterprise/service/license.go
    
    Co-authored-by: tianzhou <t@bytebase.com>
    
    * feat: resolve comment
    
    * feat: add comments
    
    * feat: update comment
    
    * feat: add more comments
    
    * feat: use embed to load pem file
    
    * feat: resolve comments
    
    * feat: use seconds in expiredts
    
    Co-authored-by: tianzhou <t@bytebase.com>
    Former-commit-id: 44c3b697fe40e591378141fee0962203a5425d4f

---
+++ enterprise/api/license.go
@@ -0,0 +1,17 @@
+package api
+
+// License is the API message for enterprise license.
+type License struct {
+	Audience      string `jsonapi:"attr,audience"`
+	InstanceCount int    `jsonapi:"attr,instanceCount"`
+	ExpiresTs     int64  `jsonapi:"attr,expiresTs"`
+	Plan          string `jsonapi:"attr,plan"`
+}
+
+// LicenseService is the service for enterprise license.
+type LicenseService interface {
+	// StoreLicense will store license into file.
+	StoreLicense(tokenString string) error
+	// ParseLicense will valid and parse license from file.
+	ParseLicense() (*License, error)
+}
