0a083fcf210a27cd0ba64286dc4dc060501103a6
tianzhou <t@bytebase.com>
2022-01-26 21:18:12 +0800

    feat: update feature matrix to include new paid features and comment the usage (#485)
    Former-commit-id: 276587e3c0c6ce83bb017cc950508e71c36463c4

--- frontend/src/components/ProjectSelect.vue
+++ frontend/src/components/ProjectSelect.vue
@@ -8,44 +8,34 @@
       }
     "
   >
-    <option disabled :selected="UNKNOWN_ID == state.selectedId">
-      Select project
-    </option>
+    <option disabled :selected="UNKNOWN_ID == state.selectedId">Select project</option>
     <!-- If includeDefaultProject is false but the selected project is the default
          project, we will show it. If includeDefaultProject is true, then it's
-         already in the list, so no need to show it twice -->
+    already in the list, so no need to show it twice-->
     <option
       v-if="!includeDefaultProject && state.selectedId == DEFAULT_PROJECT_ID"
       :selected="true"
-    >
-      Default
-    </option>
+    >Default</option>
     <!-- It may happen the selected id might not be in the project list.
          e.g. the selected project is deleted after the selection and we
          are unable to cleanup properly. In such case, the seleted project id
-         is orphaned and we just display the id.  -->
+    is orphaned and we just display the id.-->
     <option
       v-else-if="selectedIdNotInList"
       :value="state.selectedId"
       :selected="true"
-    >
-      {{ state.selectedId }}
-    </option>
+    >{{ state.selectedId }}</option>
     <template v-for="(project, index) in projectList" :key="index">
       <option
         v-if="project.rowStatus == 'NORMAL'"
         :value="project.id"
         :selected="project.id == state.selectedId"
-      >
-        {{ projectName(project) }}
-      </option>
+      >{{ projectName(project) }}</option>
       <option
         v-else-if="project.id == state.selectedId"
         :value="project.id"
         :selected="true"
-      >
-        {{ projectName(project) }}
-      </option>
+      >{{ projectName(project) }}</option>
     </template>
   </select>
 </template>
@@ -124,7 +114,7 @@ export default defineComponent({
     watchEffect(prepareProjectList);
 
     const hasAdminFeature = computed(() =>
-      store.getters["plan/feature"]("bb.admin")
+      store.getters["plan/feature"]("bb.feature.rbac")
     );
 
     const projectList = computed((): Project[] => {
