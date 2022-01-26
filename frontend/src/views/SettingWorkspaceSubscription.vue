1081498e13929fcd2e9b1189a24145028e15333e
ecmadao <wlec@outlook.com>
2022-01-26 11:11:56 +0800

    feat: update plan table

--- frontend/src/views/SettingWorkspaceSubscription.vue
+++ frontend/src/views/SettingWorkspaceSubscription.vue
@@ -2,12 +2,7 @@
   <div class="bg-white">
     <div class="mx-auto">
       <div class="sm:flex sm:flex-col sm:align-center">
-        <h1 class="text-5xl font-extrabold text-gray-900 sm:text-center">
-          Pricing Plans
-        </h1>
-        <p class="mt-5 text-xl text-gray-500 sm:text-center">
-          During beta period, we offer a 25% discount for the Standard plan.
-        </p>
+        <!-- TODO: implement license upload -->
         <PricingTable />
       </div>
     </div>
