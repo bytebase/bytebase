7ac3c18216348d5bda98a8f3a399826c8615f8d4
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-01-27 14:46:51 +0800

    feat(sql-editor): implement sheet API

---
+++ api/sheet.go
@@ -0,0 +1,108 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+)
+
+// Sheet is the API message for a sheet.
+type Sheet struct {
+	ID int `jsonapi:"primary,sheet"`
+
+	// Standard fields
+	CreatorID int
+	Creator   *Principal `jsonapi:"relation,creator"`
+	CreatedTs int64      `jsonapi:"attr,createdTs"`
+	UpdaterID int
+	Updater   *Principal `jsonapi:"relation,updater"`
+	UpdatedTs int64      `jsonapi:"attr,updatedTs"`
+
+	// Related fields
+	InstanceID int `jsonapi:"attr,instanceId"`
+	// Instance anomaly doesn't have databaseID
+	DatabaseID *int `jsonapi:"attr,databaseId"`
+
+	// Domain specific fields
+	Name      string `jsonapi:"attr,name"`
+	Statement string `jsonapi:"attr,statement"`
+	Visibility string `jsonapi:"attr,visibility"`
+}
+
+// SheetCreate is the API message for creating a sheet.
+type SheetCreate struct {
+	// Standard fields
+	// Value is assigned from the jwt subject field passed by the client.
+	CreatorID int
+
+	// Related fields
+	InstanceID int `jsonapi:"attr,instanceId"`
+	DatabaseID *int `jsonapi:"attr,databaseId"`
+
+	// Domain specific fields
+	Name      string `jsonapi:"attr,name"`
+	Statement string `jsonapi:"attr,statement"`
+	Visibility string `jsonapi:"attr,visibility"`
+}
+
+// SheetPatch is the API message for patching a sheet.
+type SheetPatch struct {
+	ID int
+
+	// Standard fields
+	// Value is assigned from the jwt subject field passed by the client.
+	UpdaterID int
+
+	// Related fields
+	InstanceID int `jsonapi:"attr,instanceId"`
+	DatabaseID *int `jsonapi:"attr,databaseId"`
+
+	// Domain specific fields
+	Name      *string `jsonapi:"attr,name"`
+	Statement *string `jsonapi:"attr,statement"`
+	Visibility *string `jsonapi:"attr,visibility"`
+}
+
+// SheetFind is the API message for finding sheets.
+type SheetFind struct {
+	// Standard fields
+	ID *int
+	RowStatus *RowStatus
+	// Value is assigned from the jwt subject field passed by the client.
+	CreatorID *int
+
+	// Related fields
+	InstanceID *int
+	DatabaseID *int
+
+	// Domain fields
+	Visibility *string
+
+	// Only applicable if InstanceID is specified, if true, then we only return instanceId (database_id is NULL)
+	InstanceOnly bool
+}
+
+func (find *SheetFind) String() string {
+	str, err := json.Marshal(*find)
+	if err != nil {
+		return err.Error()
+	}
+	return string(str)
+}
+
+// SheetDelete is the API message for deleting a sheet.
+type SheetDelete struct {
+	ID int
+
+	// Standard fields
+	// Value is assigned from the jwt subject field passed by the client.
+	DeleterID int
+}
+
+// SheetService is the service for sheet.
+type SheetService interface {
+	CreateSheet(ctx context.Context, create *SheetCreate) (*Sheet, error)
+	PatchSheet(ctx context.Context, patch *SheetPatch) (*Sheet, error)
+	FindSheetList(ctx context.Context, find *SheetFind) ([]*Sheet, error)
+	FindSheet(ctx context.Context, find *SheetFind) (*Sheet, error)
+	DeleteSheet(ctx context.Context, delete *SheetDelete) error
+}
