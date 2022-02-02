e7a5c678697fe39bb8456954e0a096670b5e2afa
ecmadao <wlec@outlook.com>
2022-02-02 18:44:44 +0800

    feat: update subscription page ux

--- frontend/src/views/SettingWorkspaceSubscription.vue
+++ frontend/src/views/SettingWorkspaceSubscription.vue
@@ -2,10 +2,32 @@
   <div class="mx-auto">
     <div class="textinfolabel">
       {{ $t("subscription.description") }}
-      <span class="text-accent">{{
-        $t("subscription.description-highlight")
-      }}</span>
+      <a class="text-accent" href="https://hub.bytebase.com" target="__blank">
+        {{ $t("subscription.description-highlight") }}
+      </a>
     </div>
+    <dl class="text-left grid grid-cols-2 gap-x-6 my-5 sm:grid-cols-4">
+      <div class="my-3">
+        <dt class="text-gray-400">
+          {{ $t("subscription.current") }}
+        </dt>
+        <dd class="text-indigo-600 mt-1 text-4xl">
+          {{ currentPlan }}
+        </dd>
+      </div>
+      <div class="my-3">
+        <dt class="text-gray-400">
+          {{ $t("subscription.instance-count") }}
+        </dt>
+        <dd class="mt-1 text-4xl">{{ instanceCount }}</dd>
+      </div>
+      <div class="my-3 col-span-2">
+        <dt class="text-gray-400">
+          {{ $t("subscription.expires-at") }}
+        </dt>
+        <dd class="mt-1 text-4xl">{{ expiresAt }}</dd>
+      </div>
+    </dl>
     <div class="w-full mt-5 flex flex-col">
       <textarea
         id="license"
@@ -32,15 +54,18 @@
       <div class="textinfolabel">
         {{ $t("subscription.plan-compare") }}
       </div>
-      <PricingTable />
+      <PricingTable :subscription="subscription" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, ref, reactive } from "vue";
+import { computed, reactive } from "vue";
 import { useStore } from "vuex";
+import { useI18n } from "vue-i18n";
+import dayjs from "dayjs";
 import PricingTable from "../components/PricingTable.vue";
+import { PlanType, Subscription } from "../types";
 
 interface LocalState {
   loading: boolean;
@@ -54,6 +79,7 @@ export default {
   },
   setup() {
     const store = useStore();
+    const { t } = useI18n();
 
     const state = reactive<LocalState>({
       loading: false,
@@ -75,9 +101,41 @@ export default {
       }
     };
 
+    const subscription = computed((): Subscription | undefined => {
+      return store.getters["subscription/subscription"]();
+    });
+
+    const instanceCount = computed((): number => {
+      return subscription.value?.instanceCount ?? 5;
+    });
+
+    const expiresAt = computed((): string => {
+      const expiresTs = subscription.value?.expiresTs ?? 0;
+      if (expiresTs <= 0) {
+        return "/";
+      }
+      return dayjs(expiresTs * 1000).format("YYYY-MM-DD");
+    });
+
+    const currentPlan = computed((): string => {
+      const plan = store.getters["subscription/currentPlan"]();
+      switch (plan) {
+        case PlanType.TEAM:
+          return t("setting.plan.team");
+        case PlanType.ENTERPRISE:
+          return t("setting.plan.enterprise");
+        default:
+          return t("setting.plan.free");
+      }
+    });
+
     return {
       state,
       disabled,
+      expiresAt,
+      currentPlan,
+      instanceCount,
+      subscription,
       uploadLicense,
     };
   },
