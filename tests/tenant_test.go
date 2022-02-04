068a54b9639daaaf4c7ac078cacc966f9def185a
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-02-04 20:52:43 +0800

    feat(test): add tenant test for cases with database name template (#545)

--- tests/tenant_test.go
+++ tests/tenant_test.go
@@ -14,13 +14,13 @@ import (
 )
 
 var (
-	stagingInstancesNumber    = 2
-	prodInstancesNumber       = 6
-	stagingInstanceNamePrefix = "testInstanceStaging"
-	prodInstanceNamePrefix    = "testInstanceProd"
+	stagingTenantNumber = 2
+	prodTenantNumber    = 6
+	stagingInstanceName = "testInstanceStaging"
+	prodInstanceName    = "testInstanceProd"
 )
 
-func TestTenantSchemaUpdate(t *testing.T) {
+func TestTenant(t *testing.T) {
 	ctx := context.Background()
 	ctl := &controller{}
 	dataDir := t.TempDir()
@@ -50,15 +50,15 @@ func TestTenantSchemaUpdate(t *testing.T) {
 
 	var stagingInstanceDirs []string
 	var prodInstanceDirs []string
-	for i := 0; i < stagingInstancesNumber; i++ {
-		instanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, fmt.Sprintf("%s-%d", stagingInstanceNamePrefix, i))
+	for i := 0; i < stagingTenantNumber; i++ {
+		instanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, fmt.Sprintf("%s-%d", stagingInstanceName, i))
 		if err != nil {
 			t.Fatal(err)
 		}
 		stagingInstanceDirs = append(stagingInstanceDirs, instanceDir)
 	}
-	for i := 0; i < prodInstancesNumber; i++ {
-		instanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, fmt.Sprintf("%s-%d", prodInstanceNamePrefix, i))
+	for i := 0; i < prodTenantNumber; i++ {
+		instanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, fmt.Sprintf("%s-%d", prodInstanceName, i))
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -83,7 +83,7 @@ func TestTenantSchemaUpdate(t *testing.T) {
 	for i, stagingInstanceDir := range stagingInstanceDirs {
 		instance, err := ctl.addInstance(api.InstanceCreate{
 			EnvironmentID: stagingEnvironment.ID,
-			Name:          fmt.Sprintf("%s-%d", stagingInstanceNamePrefix, i),
+			Name:          fmt.Sprintf("%s-%d", stagingInstanceName, i),
 			Engine:        db.SQLite,
 			Host:          stagingInstanceDir,
 		})
@@ -95,7 +95,7 @@ func TestTenantSchemaUpdate(t *testing.T) {
 	for i, prodInstanceDir := range prodInstanceDirs {
 		instance, err := ctl.addInstance(api.InstanceCreate{
 			EnvironmentID: prodEnvironment.ID,
-			Name:          fmt.Sprintf("%s-%d", prodInstanceNamePrefix, i),
+			Name:          fmt.Sprintf("%s-%d", prodInstanceName, i),
 			Engine:        db.SQLite,
 			Host:          prodInstanceDir,
 		})
@@ -108,7 +108,7 @@ func TestTenantSchemaUpdate(t *testing.T) {
 	// Set up label values for tenants.
 	// Prod and staging are using the same tenant values. Use prodInstancesNumber because it's larger than stagingInstancesNumber.
 	var tenants []string
-	for i := 0; i < prodInstancesNumber; i++ {
+	for i := 0; i < prodTenantNumber; i++ {
 		tenants = append(tenants, fmt.Sprintf("tenant%d", i))
 	}
 	if err := ctl.addLabelValues(api.TenantLabelKey, tenants); err != nil {
@@ -164,11 +164,11 @@ func TestTenantSchemaUpdate(t *testing.T) {
 			}
 		}
 	}
-	if len(stagingDatabases) != stagingInstancesNumber {
-		t.Fatalf("invalid number of staging databases %v in project %v, expecting %d database", project.ID, len(databases), stagingInstancesNumber)
+	if len(stagingDatabases) != stagingTenantNumber {
+		t.Fatalf("invalid number of staging databases %v in project %v, expecting %d database", project.ID, len(databases), stagingTenantNumber)
 	}
-	if len(prodDatabases) != prodInstancesNumber {
-		t.Fatalf("invalid number of prod databases %v in project %v, expecting %d database", project.ID, len(databases), prodInstancesNumber)
+	if len(prodDatabases) != prodTenantNumber {
+		t.Fatalf("invalid number of prod databases %v in project %v, expecting %d database", project.ID, len(databases), prodTenantNumber)
 	}
 
 	// Create an issue that updates database schema.
@@ -225,7 +225,7 @@ func TestTenantSchemaUpdate(t *testing.T) {
 	}
 }
 
-func TestTenantVCSSchemaUpdate(t *testing.T) {
+func TestTenantVCS(t *testing.T) {
 	ctx := context.Background()
 	ctl := &controller{}
 	dataDir := t.TempDir()
@@ -297,15 +297,15 @@ func TestTenantVCSSchemaUpdate(t *testing.T) {
 
 	var stagingInstanceDirs []string
 	var prodInstanceDirs []string
-	for i := 0; i < stagingInstancesNumber; i++ {
-		instanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, fmt.Sprintf("%s-%d", stagingInstanceNamePrefix, i))
+	for i := 0; i < stagingTenantNumber; i++ {
+		instanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, fmt.Sprintf("%s-%d", stagingInstanceName, i))
 		if err != nil {
 			t.Fatal(err)
 		}
 		stagingInstanceDirs = append(stagingInstanceDirs, instanceDir)
 	}
-	for i := 0; i < prodInstancesNumber; i++ {
-		instanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, fmt.Sprintf("%s-%d", prodInstanceNamePrefix, i))
+	for i := 0; i < prodTenantNumber; i++ {
+		instanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, fmt.Sprintf("%s-%d", prodInstanceName, i))
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -330,7 +330,7 @@ func TestTenantVCSSchemaUpdate(t *testing.T) {
 	for i, stagingInstanceDir := range stagingInstanceDirs {
 		instance, err := ctl.addInstance(api.InstanceCreate{
 			EnvironmentID: stagingEnvironment.ID,
-			Name:          fmt.Sprintf("%s-%d", stagingInstanceNamePrefix, i),
+			Name:          fmt.Sprintf("%s-%d", stagingInstanceName, i),
 			Engine:        db.SQLite,
 			Host:          stagingInstanceDir,
 		})
@@ -342,7 +342,7 @@ func TestTenantVCSSchemaUpdate(t *testing.T) {
 	for i, prodInstanceDir := range prodInstanceDirs {
 		instance, err := ctl.addInstance(api.InstanceCreate{
 			EnvironmentID: prodEnvironment.ID,
-			Name:          fmt.Sprintf("%s-%d", prodInstanceNamePrefix, i),
+			Name:          fmt.Sprintf("%s-%d", prodInstanceName, i),
 			Engine:        db.SQLite,
 			Host:          prodInstanceDir,
 		})
@@ -355,7 +355,7 @@ func TestTenantVCSSchemaUpdate(t *testing.T) {
 	// Set up label values for tenants.
 	// Prod and staging are using the same tenant values. Use prodInstancesNumber because it's larger than stagingInstancesNumber.
 	var tenants []string
-	for i := 0; i < prodInstancesNumber; i++ {
+	for i := 0; i < prodTenantNumber; i++ {
 		tenants = append(tenants, fmt.Sprintf("tenant%d", i))
 	}
 	if err := ctl.addLabelValues(api.TenantLabelKey, tenants); err != nil {
@@ -411,11 +411,11 @@ func TestTenantVCSSchemaUpdate(t *testing.T) {
 			}
 		}
 	}
-	if len(stagingDatabases) != stagingInstancesNumber {
-		t.Fatalf("invalid number of staging databases %v in project %v, expecting %d database", project.ID, len(databases), stagingInstancesNumber)
+	if len(stagingDatabases) != stagingTenantNumber {
+		t.Fatalf("invalid number of staging databases %v in project %v, expecting %d database", project.ID, len(databases), stagingTenantNumber)
 	}
-	if len(prodDatabases) != prodInstancesNumber {
-		t.Fatalf("invalid number of prod databases %v in project %v, expecting %d database", project.ID, len(databases), prodInstancesNumber)
+	if len(prodDatabases) != prodTenantNumber {
+		t.Fatalf("invalid number of prod databases %v in project %v, expecting %d database", project.ID, len(databases), prodTenantNumber)
 	}
 
 	// Simulate Git commits.
@@ -471,6 +471,202 @@ func TestTenantVCSSchemaUpdate(t *testing.T) {
 		}
 	}
 	for _, prodInstance := range prodInstances {
+		result, err := ctl.query(prodInstance, databaseName)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if schemaSQLResult != result {
+			t.Fatalf("SQL result want %q, got %q, diff %q", schemaSQLResult, result, pretty.Diff(schemaSQLResult, result))
+		}
+	}
+}
+
+func TestTenantDatabaseNameTemplate(t *testing.T) {
+	ctx := context.Background()
+	ctl := &controller{}
+	dataDir := t.TempDir()
+	if err := ctl.StartMain(ctx, dataDir); err != nil {
+		t.Fatal(err)
+	}
+	defer ctl.Close()
+	if err := ctl.Login(); err != nil {
+		t.Fatal(err)
+	}
+	if err := ctl.setLicense(); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a project.
+	project, err := ctl.createProject(api.ProjectCreate{
+		Name:           "Test Project",
+		Key:            "TestSchemaUpdate",
+		TenantMode:     api.TenantModeTenant,
+		DBNameTemplate: "{{DB_NAME}}_{{TENANT}}",
+	})
+	if err != nil {
+		t.Fatalf("failed to create project, error: %v", err)
+	}
+
+	// Provision instances.
+	instanceRootDir := t.TempDir()
+	stagingInstanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, stagingInstanceName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	prodInstanceDir, err := ctl.provisionSQLiteInstance(instanceRootDir, prodInstanceName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	environments, err := ctl.getEnvironments()
+	if err != nil {
+		t.Fatal(err)
+	}
+	stagingEnvironment, err := findEnvironment(environments, "Staging")
+	if err != nil {
+		t.Fatal(err)
+	}
+	prodEnvironment, err := findEnvironment(environments, "Prod")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Add the provisioned instances.
+	stagingInstance, err := ctl.addInstance(api.InstanceCreate{
+		EnvironmentID: stagingEnvironment.ID,
+		Name:          stagingInstanceName,
+		Engine:        db.SQLite,
+		Host:          stagingInstanceDir,
+	})
+	if err != nil {
+		t.Fatalf("failed to add instance, error: %v", err)
+	}
+	prodInstance, err := ctl.addInstance(api.InstanceCreate{
+		EnvironmentID: prodEnvironment.ID,
+		Name:          prodInstanceName,
+		Engine:        db.SQLite,
+		Host:          prodInstanceDir,
+	})
+	if err != nil {
+		t.Fatalf("failed to add instance, error: %v", err)
+	}
+
+	// Set up label values for tenants.
+	// Prod and staging are using the same tenant values. Use prodTenantNumber because it's larger than stagingInstancesNumber.
+	var tenants []string
+	for i := 0; i < prodTenantNumber; i++ {
+		tenants = append(tenants, fmt.Sprintf("tenant%d", i))
+	}
+	if err := ctl.addLabelValues(api.TenantLabelKey, tenants); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create deployment configuration.
+	if _, err := ctl.upsertDeploymentConfig(
+		api.DeploymentConfigUpsert{
+			ProjectID: project.ID,
+		},
+		deploymentSchdule,
+	); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create issues that create databases.
+	baseDatabaseName := "testTenant"
+	for i := 0; i < stagingTenantNumber; i++ {
+		databaseName := fmt.Sprintf("%s_tenant%d", baseDatabaseName, i)
+		if err := ctl.createDatabase(project, stagingInstance, databaseName, map[string]string{api.TenantLabelKey: fmt.Sprintf("tenant%d", i)}); err != nil {
+			t.Fatal(err)
+		}
+	}
+	for i := 0; i < prodTenantNumber; i++ {
+		databaseName := fmt.Sprintf("%s_tenant%d", baseDatabaseName, i)
+		if err := ctl.createDatabase(project, prodInstance, databaseName, map[string]string{api.TenantLabelKey: fmt.Sprintf("tenant%d", i)}); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Getting databases for each environment.
+	databases, err := ctl.getDatabases(api.DatabaseFind{
+		ProjectID: &project.ID,
+	})
+	if err != nil {
+		t.Fatalf("failed to get databases, error: %v", err)
+	}
+
+	var stagingDatabases []*api.Database
+	var prodDatabases []*api.Database
+	for i := 0; i < stagingTenantNumber; i++ {
+		databaseName := fmt.Sprintf("%s_tenant%d", baseDatabaseName, i)
+		for _, database := range databases {
+			if database.Instance.ID == stagingInstance.ID && database.Name == databaseName {
+				stagingDatabases = append(stagingDatabases, database)
+				break
+			}
+		}
+	}
+	for i := 0; i < prodTenantNumber; i++ {
+		databaseName := fmt.Sprintf("%s_tenant%d", baseDatabaseName, i)
+		for _, database := range databases {
+			if database.Instance.ID == prodInstance.ID && database.Name == databaseName {
+				prodDatabases = append(prodDatabases, database)
+				break
+			}
+		}
+	}
+	if len(stagingDatabases) != stagingTenantNumber {
+		t.Fatalf("invalid number of staging databases %v in project %v, expecting %d database", project.ID, len(databases), stagingTenantNumber)
+	}
+	if len(prodDatabases) != prodTenantNumber {
+		t.Fatalf("invalid number of prod databases %v in project %v, expecting %d database", project.ID, len(databases), prodTenantNumber)
+	}
+
+	// Create an issue that updates database schema.
+	createContext, err := json.Marshal(&api.UpdateSchemaContext{
+		MigrationType: db.Migrate,
+		UpdateSchemaDetailList: []*api.UpdateSchemaDetail{
+			{
+				DatabaseName: baseDatabaseName,
+				Statement:    migrationStatement,
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("failed to construct schema update issue CreateContext payload, error: %v", err)
+	}
+	issue, err := ctl.createIssue(api.IssueCreate{
+		ProjectID:   project.ID,
+		Name:        "update schema for tenants",
+		Type:        api.IssueDatabaseSchemaUpdate,
+		Description: "This updates the schema of tenant databases.",
+		// Assign to self.
+		AssigneeID:    project.Creator.ID,
+		CreateContext: string(createContext),
+	})
+	if err != nil {
+		t.Fatalf("failed to create schema update issue, error: %v", err)
+	}
+	status, err := ctl.waitIssuePipeline(issue.ID)
+	if err != nil {
+		t.Fatalf("failed to wait for issue %v pipeline %v, error: %v", issue.ID, issue.Pipeline.ID, err)
+	}
+	if status != api.TaskDone {
+		t.Fatalf("issue %v pipeline %v is expected to finish with status done got %v", issue.ID, issue.Pipeline.ID, status)
+	}
+
+	// Query schema.
+	for i := 0; i < stagingTenantNumber; i++ {
+		databaseName := fmt.Sprintf("%s_tenant%d", baseDatabaseName, i)
+		result, err := ctl.query(stagingInstance, databaseName)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if schemaSQLResult != result {
+			t.Fatalf("SQL result want %q, got %q, diff %q", schemaSQLResult, result, pretty.Diff(schemaSQLResult, result))
+		}
+	}
+	for i := 0; i < prodTenantNumber; i++ {
+		databaseName := fmt.Sprintf("%s_tenant%d", baseDatabaseName, i)
 		result, err := ctl.query(prodInstance, databaseName)
 		if err != nil {
 			t.Fatal(err)
