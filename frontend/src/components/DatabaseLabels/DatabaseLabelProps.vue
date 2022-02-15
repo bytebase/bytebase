b20cb5086593b36ae2c709af15c3fb99f33bb1d3
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-02-15 11:54:11 +0800

    feat: render db labels as if they are normal props (#586)
    Former-commit-id: a4dcba3013195d4b2fb5daeff0757d33bf9fa9f5

---
+++ frontend/src/components/DatabaseLabels/DatabaseLabelProps.vue
@@ -0,0 +1,113 @@
+<template>
+  <template v-for="label in availableLabelList" :key="label.key">
+    <dd class="flex items-center text-sm md:mr-4">
+      <slot name="label" :label="label"></slot>
+      <DatabaseLabelPropItem
+        :label="label"
+        :value="getLabelValue(label.key)"
+        :label-list="availableLabelList"
+        :required-label-list="requiredLabelList"
+        :database="database"
+        :allow-edit="allowEdit"
+        @update:value="(value) => onUpdateValue(label.key, value)"
+      />
+    </dd>
+  </template>
+</template>
+
+<script lang="ts" setup>
+import { cloneDeep } from "lodash-es";
+import {
+  computed,
+  defineProps,
+  defineEmits,
+  withDefaults,
+  watch,
+  watchEffect,
+  reactive,
+} from "vue";
+import { useStore } from "vuex";
+import {
+  Database,
+  DatabaseLabel,
+  Label,
+  LabelKeyType,
+  LabelValueType,
+} from "../../types";
+import { isReservedLabel, parseLabelListInTemplate } from "../../utils";
+import DatabaseLabelPropItem from "./DatabaseLabelPropItem.vue";
+
+const props = withDefaults(
+  defineProps<{
+    labelList: DatabaseLabel[];
+    database: Database;
+    allowEdit?: boolean;
+  }>(),
+  {
+    allowEdit: false,
+  }
+);
+
+const emit = defineEmits<{
+  (event: "update:labelList", labels: DatabaseLabel[]): void;
+}>();
+
+const state = reactive({
+  labelList: cloneDeep(props.labelList),
+});
+
+const store = useStore();
+
+const prepareLabelList = () => {
+  store.dispatch("label/fetchLabelList");
+};
+watchEffect(prepareLabelList);
+
+watch(
+  () => props.labelList,
+  (list) => (state.labelList = cloneDeep(list))
+);
+
+const availableLabelList = computed(() => {
+  const allList = store.getters["label/labelList"]() as Label[];
+  return allList.filter((label) => !isReservedLabel(label));
+});
+
+const requiredLabelList = computed(() => {
+  const { project } = props.database;
+  if (project.tenantMode !== "TENANT") return [];
+  if (!project.dbNameTemplate) return [];
+
+  return parseLabelListInTemplate(
+    project.dbNameTemplate,
+    availableLabelList.value
+  );
+});
+
+const getLabelValue = (key: LabelKeyType): LabelValueType | undefined => {
+  return state.labelList.find((label) => label.key === key)?.value || "";
+};
+
+const setLabelValue = (key: LabelKeyType, value: LabelValueType) => {
+  const index = state.labelList.findIndex((label) => label.key === key);
+
+  if (index < 0) {
+    if (value) {
+      // push new value
+      state.labelList.push({ key, value });
+    }
+  } else {
+    if (value) {
+      state.labelList[index].value = value;
+    } else {
+      // remove empty value from the list
+      state.labelList.splice(index, 1);
+    }
+  }
+};
+
+const onUpdateValue = (key: LabelKeyType, value: LabelValueType) => {
+  setLabelValue(key, value);
+  emit("update:labelList", state.labelList);
+};
+</script>
