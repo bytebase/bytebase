758fdb231dea68ef86df54d47d608a0092597bc8
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-02-15 18:43:13 +0800

    fix(sqlite): skip dumping SQLite internal sequence table if dump only (#588)

--- plugin/db/sqlite/sqlite.go
+++ plugin/db/sqlite/sqlite.go
@@ -638,6 +638,10 @@ func (driver *Driver) dumpOneDatabase(ctx context.Context, database string, out 
 	}
 
 	for _, s := range sqliteSchemas {
+		// We should skip sqlite sequence table if we're dumping schema only.
+		if schemaOnly && s.name == "sqlite_sequence" {
+			continue
+		}
 		if _, err := io.WriteString(out, fmt.Sprintf("%s;\n", s.statement)); err != nil {
 			return err
 		}
