78ce79dd6026ac28e6d021a25ecccc3a9a9f633e
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-02-15 11:54:11 +0800

    feat: render db labels as if they are normal props (#586)
    Former-commit-id: b20cb5086593b36ae2c709af15c3fb99f33bb1d3 [formerly a4dcba3013195d4b2fb5daeff0757d33bf9fa9f5]
    Former-commit-id: 1d11dc4c85cbd347238216e3bc5c421773672acb

--- frontend/src/components/DatabaseLabels/index.ts
+++ frontend/src/components/DatabaseLabels/index.ts
@@ -1,6 +1,7 @@
 import DatabaseLabels from "./DatabaseLabels.vue";
 import DatabaseLabelsEditor from "./DatabaseLabelsEditor.vue";
+import DatabaseLabelProps from "./DatabaseLabelProps.vue";
 
 export default DatabaseLabels;
 
-export { DatabaseLabels, DatabaseLabelsEditor };
+export { DatabaseLabels, DatabaseLabelsEditor, DatabaseLabelProps };
