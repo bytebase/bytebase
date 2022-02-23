f4118821d083e5079ef7dd6ce3b70af028ea5feb
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-02-23 11:31:59 +0800

    refactor: move issue pipeline creation from client-side to server-side (#644)
    Former-commit-id: 6a1b90cdf7df5007b97d1319ccf1f7d81a3152d8 [formerly 754f3a8c0085aa5dd321c4853706fb2d9541893b] [formerly 35caf407f2f7c196bddd09f710dba08d795c61b6]
    Former-commit-id: b7dcef1f53f60edce4605c2638f821a37b405d60
    Former-commit-id: d2a2a0bf564b4ed81349c27ed89ff7c12516da77

--- frontend/src/types/instance.ts
+++ frontend/src/types/instance.ts
@@ -105,7 +105,7 @@ export type InstanceMigration = {
 
 export type MigrationEngine = "UI" | "VCS";
 
-export type MigrationType = "BASELINE" | "MIGRATE" | "BRANCH";
+export type MigrationType = "BASELINE" | "MIGRATE" | "BRANCH" | "DATA";
 
 export type MigrationStatus = "PENDING" | "DONE" | "FAILED";
 
