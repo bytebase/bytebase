fc69793f2174590160dc43d58b8f385449502827
Zilong Zhou <529620861@qq.com>
2022-03-02 10:58:20 +0800

    style: added unique index

--- store/migration/10001__init_schema.sql
+++ store/migration/10001__init_schema.sql
@@ -237,7 +237,7 @@ CREATE TABLE project_member (
     UNIQUE(project_id, principal_id, role_provider)
 );
 
-CREATE UNIQUE INDEX idx_project_member_unique_project_id_principal_id ON project_member(project_id, principal_id);
+CREATE UNIQUE INDEX idx_project_member_unique_project_id_principal_id_role_provider ON project_member(project_id, principal_id, role_provider));
 
 ALTER SEQUENCE project_member_id_seq RESTART WITH 100;
 
