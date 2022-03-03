0c24a0559577012b6d2bf4f8dc409403c0b90d49
ecmadao <wlec@outlook.com>
2022-03-03 18:24:09 +0800

    fix: subscription layout (#737)
    Former-commit-id: ade0e557c7744909303dc3bb9ac71def6f1a3568 [formerly 0e2b191d72ea02cdd9c4b17cfd43a590f463f0e9] [formerly 4f3b29b2719e001a8547c312f3cd2961ca8b05c8]
    Former-commit-id: 904784ac991846426a8ce75178dfd94dddd7f204
    Former-commit-id: 36af4d0c636b914d54bee5b48b252b187d6f9581

--- frontend/src/components/PricingTable.vue
+++ frontend/src/components/PricingTable.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hidden md:block">
+  <div class="hidden xl:block">
     <table id="plans" class="w-full h-px table-fixed">
       <caption class="sr-only">
         Pricing plan comparison
@@ -18,13 +18,13 @@
             <div class="flex-1">
               <img :src="plan.image" class="hidden lg:block p-5" />
 
-              <div class="flex items-center h-10">
+              <div class="flex flex-row items-center h-10">
                 <h3 class="text-xl font-semibold text-gray-900">
                   {{ $t(`subscription.plan.${plan.title}.title`) }}
                 </h3>
                 <span
                   v-if="plan.label"
-                  class="ml-2 inline-flex items-center px-3 py-0.5 rounded-full text-base font-sm bg-indigo-100 text-indigo-800"
+                  class="ml-2 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-sm bg-indigo-100 text-indigo-800"
                 >
                   {{ plan.label }}
                 </span>
@@ -54,7 +54,7 @@
                   plan.highlight
                     ? 'border-indigo-500  text-white  bg-indigo-500 hover:bg-indigo-600 hover:border-indigo-600'
                     : 'border-accent text-accent hover:bg-accent',
-                  'mt-8 block w-full border rounded-md py-2 lg:py-4 text-sm lg:text-xl font-semibold text-center hover:text-white whitespace-nowrap overflow-hidden',
+                  'mt-8 block w-full border rounded-md py-2 lg:py-4 text-sm md:text-base font-semibold text-center hover:text-white whitespace-nowrap overflow-hidden',
                 ]"
                 @click="onButtonClick(plan)"
               >
@@ -160,6 +160,141 @@
       </tfoot>
     </table>
   </div>
+  <div class="xl:hidden">
+    <div v-for="plan in plans" :key="plan.type" class="mb-16">
+      <div class="flex flex-col items-center">
+        <img :src="plan.image" class="block w-2/4" />
+
+        <div class="flex flex-row items-center">
+          <h3 class="text-2xl font-semibold text-gray-900">
+            {{ $t(`subscription.plan.${plan.title}.title`) }}
+          </h3>
+          <span
+            v-if="plan.label"
+            class="ml-2 inline-flex items-center px-3 py-0.5 rounded-full text-sm font-sm bg-indigo-100 text-indigo-800"
+          >
+            {{ plan.label }}
+          </span>
+        </div>
+
+        <p class="text-gray-500">
+          {{ $t(`subscription.plan.${plan.title}.desc`) }}
+        </p>
+
+        <p class="mt-4 flex items-baseline text-gray-900 text-xl">
+          <span class="text-4xl"> ${{ plan.pricePerInstancePerMonth }} </span>
+          &nbsp;
+          {{ $t("subscription.per-instance") }}
+          {{ $t("subscription.per-month") }}
+        </p>
+
+        <div class="text-gray-400">
+          {{ $t(`subscription.${plan.title}-price-intro`) }}
+        </div>
+
+        <button
+          type="button"
+          :class="[
+            plan.highlight
+              ? 'border-indigo-500  text-white  bg-indigo-500 hover:bg-indigo-600 hover:border-indigo-600'
+              : 'border-accent text-accent hover:bg-accent',
+            'mt-8 block w-full border rounded-md py-4 text-base font-semibold text-center hover:text-white whitespace-nowrap overflow-hidden',
+          ]"
+          @click="onButtonClick(plan)"
+        >
+          {{ plan.buttonText }}
+        </button>
+
+        <div v-if="plan.isAvailable" class="px-4 py-8 text-right text-gray-500">
+          <i18n-t keypath="subscription.announcement">
+            <template #cancel>
+              <a
+                class="underline"
+                href="https://bytebase.com/refund"
+                target="_blank"
+                >{{ $t("subscription.cancel") }}</a
+              >
+            </template>
+          </i18n-t>
+        </div>
+      </div>
+      <table class="w-full h-px table-fixed mt-10">
+        <caption class="sr-only">
+          Feature comparison
+        </caption>
+        <tbody class="border-t border-gray-200 divide-y divide-gray-200">
+          <template v-for="section in sections" :key="section.id">
+            <tr>
+              <th
+                class="bg-gray-50 py-3 pl-6 text-sm font-medium text-gray-900 text-left"
+                scope="colgroup"
+              >
+                {{ $t(`subscription.feature-sections.${section.id}.title`) }}
+              </th>
+            </tr>
+            <tr
+              v-for="feature in section.features"
+              :key="feature"
+              class="hover:bg-gray-50"
+            >
+              <th
+                class="py-5 px-6 text-sm font-normal text-gray-500 text-left"
+                scope="row"
+              >
+                {{
+                  $t(
+                    `subscription.feature-sections.${section.id}.features.${feature}`
+                  )
+                }}
+              </th>
+              <td
+                class="py-5 px-6 font-semibold tooltip-wrapper w-3/4"
+                :class="plan.highlight ? 'text-indigo-600' : 'text-gray-600'"
+              >
+                <div class="flex justify-center">
+                  <template v-if="getFeature(plan, feature)">
+                    <span
+                      v-if="getFeature(plan, feature)?.content"
+                      class="block text-sm"
+                      >{{ $t(getFeature(plan, feature)?.content) }}</span
+                    >
+                    <heroicons-solid:check v-else class="w-5 h-5" />
+                  </template>
+                  <template v-else>
+                    <heroicons-solid:minus class="w-5 h-5" />
+                  </template>
+                  <template v-if="getFeature(plan, feature)?.tooltip">
+                    <heroicons-solid:question-mark-circle
+                      class="w-5 h-5 ml-1"
+                    />
+                    <span
+                      v-if="getFeature(plan, feature)?.tooltip"
+                      class="tooltip whitespace-nowrap"
+                    >
+                      {{ $t(getFeature(plan, feature)?.tooltip) }}
+                    </span>
+                  </template>
+                </div>
+              </td>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+      <button
+        v-if="!plan.isFreePlan"
+        type="button"
+        :class="[
+          plan.highlight
+            ? 'border-indigo-500  text-white  bg-indigo-500 hover:bg-indigo-600 hover:border-indigo-600'
+            : 'border-accent text-accent hover:bg-accent',
+          'mt-8 block w-full border rounded-md py-4 text-base font-semibold text-center hover:text-white whitespace-nowrap overflow-hidden',
+        ]"
+        @click="onButtonClick(plan)"
+      >
+        {{ plan.buttonText }}
+      </button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -187,6 +322,7 @@ interface LocalPlan extends Plan {
   buttonText: string;
   highlight: boolean;
   isFreePlan: boolean;
+  isAvailable: boolean;
 }
 
 const minimumInstanceCount = 5;
@@ -239,6 +375,7 @@ export default {
             : t("subscription.price", { price: getPlanPrice(plan) }),
         buttonText: getButtonText(plan),
         highlight: plan.type === PlanType.TEAM,
+        isAvailable: plan.type === PlanType.TEAM,
         isFreePlan: plan.type === PlanType.FREE,
         label: t(`subscription.plan.${plan.title}.label`),
       }));
@@ -253,7 +390,7 @@ export default {
       if (plan.type === PlanType.ENTERPRISE)
         return t("subscription.contact-us");
       if (plan.type === props.subscription?.plan)
-        return t("subscription.current-plan");
+        return t("subscription.upgrade");
       if (plan.trialDays && plan.trialPrice) {
         return t("subscription.start-trial", {
           price: plan.trialPrice,
