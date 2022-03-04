c96e0e4cd99e828ec94b97f48a64ca297536b37c
qsliu <57934883+qsliu2017@users.noreply.github.com>
2022-03-04 15:26:44 +0800

    refactor(resources): refactor pg and add mysql (#686)
    Former-commit-id: dc634c06d2d9651e995453ae662e814db8e8ef80

--- resources/postgres/resources_dev.go
+++ resources/postgres/resources_dev.go
@@ -1,9 +1,9 @@
 //go:build !release
 // +build !release
 
-package resources
+package postgres
 
 import "embed"
 
 //go:embed postgres-darwin-x86_64.txz postgres-linux-x86_64-alpine_linux.txz postgres-linux-x86_64.txz
-var postgresResources embed.FS
+var resources embed.FS
