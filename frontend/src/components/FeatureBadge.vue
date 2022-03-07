517c8aac26eb5c2077c1818899da99df9bb63c02
ecmadao <wlec@outlook.com>
2022-03-07 16:36:36 +0800

    feat: add feature badge (#785)
    * feat: add feature badge
    
    * fix: move the badge after the text

---
+++ frontend/src/components/FeatureBadge.vue
@@ -0,0 +1,31 @@
+<template>
+  <router-link v-if="!hasFeature" to="/setting/subscription" exact-active-class>
+    <heroicons-solid:sparkles class="w-5 h-5 text-accent" />
+  </router-link>
+</template>
+
+<script lang="ts">
+import { PropType, computed } from "vue";
+import { useStore } from "vuex";
+import { FeatureType } from "../types";
+
+export default {
+  props: {
+    feature: {
+      required: true,
+      type: String as PropType<FeatureType>,
+    },
+  },
+  setup(props) {
+    const store = useStore();
+
+    const hasFeature = computed(() =>
+      store.getters["subscription/feature"](props.feature)
+    );
+
+    return {
+      hasFeature,
+    };
+  },
+};
+</script>
