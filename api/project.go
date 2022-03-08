61ab6ebab7ee1e3a236f15f1ac13115665f9310a
Yilong Li <liyilongko@gmail.com>
2022-03-08 17:05:53 +0800

    refactor: split data model object for api.VCS (#792)
    Former-commit-id: ee3d65e9adf2be5c0b40bece4a7e274fc3ba78e9 [formerly f622edda5cafb3e8b1fae39b6e1c4beacc6f8ecc] [formerly da7489e4eb21ab6fff811ab814f53ea5a1a9c87d]
    Former-commit-id: 8c95295115daee4a14979ca9c763527c98cbdc86
    Former-commit-id: 06a4978a2d8aa95d25ad97576357fed2e22d0705

--- api/project.go
+++ api/project.go
@@ -65,6 +65,7 @@ const (
 )
 
 // ProjectRaw is the store model for a project.
+// Fields have exactly the same meanings as Project.
 type ProjectRaw struct {
 	ID int
 
@@ -76,36 +77,35 @@ type ProjectRaw struct {
 	UpdatedTs int64
 
 	// Domain specific fields
-	Name         string
-	Key          string
-	WorkflowType ProjectWorkflowType
-	Visibility   ProjectVisibility
-	TenantMode   ProjectTenantMode
-	// DBNameTemplate is only used when a project is in tenant mode.
-	// Empty value means {{DB_NAME}}.
+	Name           string
+	Key            string
+	WorkflowType   ProjectWorkflowType
+	Visibility     ProjectVisibility
+	TenantMode     ProjectTenantMode
 	DBNameTemplate string
 	RoleProvider   ProjectRoleProvider
 }
 
-// CopyToProject copies fields of ProjectRaw to an instance of Project.
-// This is intented to be used when we need to compose a Project relationship.
-func (raw *ProjectRaw) CopyToProject(p *Project) {
-	p.ID = raw.ID
+// ToProject creates an instance of Project based on the ProjectRaw.
+// This is intended to be used when we need to compose a Project relationship.
+func (raw *ProjectRaw) ToProject() *Project {
+	return &Project{
+		ID: raw.ID,
 
-	p.RowStatus = raw.RowStatus
-	p.CreatorID = raw.CreatorID
-	p.CreatedTs = raw.CreatedTs
-	p.UpdaterID = raw.UpdaterID
-	p.UpdatedTs = raw.UpdatedTs
+		RowStatus: raw.RowStatus,
+		CreatorID: raw.CreatorID,
+		CreatedTs: raw.CreatedTs,
+		UpdaterID: raw.UpdaterID,
+		UpdatedTs: raw.UpdatedTs,
 
-	p.Name = raw.Name
-	p.Key = raw.Key
-	p.WorkflowType = raw.WorkflowType
-	p.Visibility = raw.Visibility
-	p.TenantMode = raw.TenantMode
-
-	p.DBNameTemplate = raw.DBNameTemplate
-	p.RoleProvider = raw.RoleProvider
+		Name:           raw.Name,
+		Key:            raw.Key,
+		WorkflowType:   raw.WorkflowType,
+		Visibility:     raw.Visibility,
+		TenantMode:     raw.TenantMode,
+		DBNameTemplate: raw.DBNameTemplate,
+		RoleProvider:   raw.RoleProvider,
+	}
 }
 
 // Project is the API message for a project.
