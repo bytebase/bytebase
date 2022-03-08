bfaa6340b363b0d9312e8ef2cc694b329adc81e2
tianzhou <t@bytebase.com>
2022-03-08 14:59:16 +0800

    fix: couple UX fix (#794)
    Former-commit-id: 3cfe4a89bdb102c710cff43471ec9ba46ecc8ef7 [formerly 915df13c92e2d653e14761cf24deab3c0aa7244f] [formerly 0dbc0a255d794aacc2b6e61dd94f5319d75f7ef6]
    Former-commit-id: eadfb05ba328f2c671193d43f849f38f967a1763
    Former-commit-id: 1d31f4f31a560447d485b43f6bfdea8f6e8f0c20

--- frontend/src/components/FeatureModal.vue
+++ frontend/src/components/FeatureModal.vue
@@ -4,16 +4,14 @@
     @close="$emit('cancel')"
   >
     <div>
-      <div class="flex items-start">
-        <div
-          class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-yellow-100 sm:mx-0 sm:h-10 sm:w-10"
-        >
+      <div class="flex items-start space-x-2">
+        <div class="flex items-center">
           <!-- Heroicons name: outline/exclamation -->
-          <heroicons-outline:exclamation class="h-6 w-6 text-yellow-600" />
+          <heroicons-solid:sparkles class="h-6 w-6 text-accent" />
         </div>
         <h3
           id="modal-headline"
-          class="ml-4 flex self-center text-lg leading-6 font-medium text-gray-900"
+          class="flex self-center text-lg leading-6 font-medium text-gray-900"
         >
           {{ featureTitle }}
         </h3>
@@ -25,23 +23,16 @@
       </div>
       <div class="mt-3">
         <p class="whitespace-pre-wrap">{{ $t("subscription.trial") }}*</p>
-        <p class="text-gray-500 whitespace-pre-wrap text-sm">
-          {{ $t("subscription.trial-comment") }}
-        </p>
       </div>
-      <div class="mt-7 flex justify-end">
+      <div class="mt-7 flex justify-end space-x-2">
         <button
           type="button"
-          class="btn-normal mt-3 px-4 py-2 sm:mt-0 sm:w-auto"
+          class="btn-normal"
           @click.prevent="$emit('cancel')"
         >
           {{ $t("common.dismiss") }}
         </button>
-        <button
-          type="button"
-          class="sm:ml-3 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-error text-base font-medium text-white hover:bg-error-hover focus:outline-none focus-visible:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:w-auto sm:text-sm btn-primary"
-          @click.prevent="ok"
-        >
+        <button type="button" class="btn-primary" @click.prevent="ok">
           {{ $t("common.learn-more") }}
         </button>
       </div>
