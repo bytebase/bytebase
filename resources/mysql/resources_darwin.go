9827f1bc7c4bffcb25917ad8c84bae14df6be6cd
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-03-09 16:46:13 +0800

    chore: use file suffix to only download the mysql for the platform (#815)
    * chore: clean up rollback SQL related code
    
    * fix: fix isTenantDeployMode() otherwise db name template tenant deployment will fail because UI sends full database name instead of base database name to backend
    
    * chore: use file suffix to only download the mysql for the platform
    
    * chore: update

---
+++ resources/mysql/resources_darwin.go
@@ -0,0 +1,11 @@
+package mysql
+
+import "embed"
+
+// f1943053b12428e4c0e4ed309a636fd0 is the md5 hash.
+//go:generate ./fetch_mysql.sh mysql-8.0.28-macos11-arm64.tar.gz f1943053b12428e4c0e4ed309a636fd0
+
+//go:embed mysql-8.0.28-macos11-arm64.tar.gz
+// To use this package in testing, download the MySQL binary first:
+// go generate ./...
+var resources embed.FS
