fd470cb140dd5befac9f623b2876279d48e23980
Yilong Li <liyilongko@gmail.com>
2022-03-09 17:38:49 +0800

    refactor: split data model object for api.Repository (#813)
    * refactor: add api.ProjectPlain to represent store data
    
    api.ProjectPlain contains no pointer, which means it maps records in db.
    If server code wants the composed object tree with related pointers
    filled with concret data, we need to call composeXxxRelationship to
    query db and fill in the pointers.
    
    * fix: make slice panic
    
    * fix: address reviewer's comment
    
    * Apply suggestions from code review
    
    Co-authored-by: tianzhou <t@bytebase.com>
    
    * fix: apply suggestions from code review
    
    * refactor: return error instead of panic
    
    * refactor: rename ProjectPlain to ProjectRaw
    
    * refactor: manually copy fields, reduce magic
    
    * refactor: add VCSRaw to represent store model for VCS
    
    * refactor: add RepositoryRaw to represent store model for Repository
    
    * refactor: address reviewer's comments
    
    * refactor: address reviewer's comments
    
    * refactor: add corresponding comments for fields
    
    * fix: add missing fields
    
    * refactor: eliminate predefined err variable
    
    * fix: fix logic of runMigration
    
    Co-authored-by: tianzhou <t@bytebase.com>
    Former-commit-id: 37376fab1d8a36d711af7a4cc5f39d6787e52572
    Former-commit-id: de56ded1cb64b3073e6d29a94912592242366cee

--- server/repository.go
+++ server/repository.go
@@ -7,32 +7,36 @@ import (
 	"github.com/bytebase/bytebase/api"
 )
 
-func (s *Server) composeRepositoryRelationship(ctx context.Context, repository *api.Repository) error {
-	var err error
+func (s *Server) composeRepositoryRelationship(ctx context.Context, raw *api.RepositoryRaw) (*api.Repository, error) {
+	repository := raw.ToRepository()
 
-	repository.Creator, err = s.composePrincipalByID(ctx, repository.CreatorID)
+	creator, err := s.composePrincipalByID(ctx, repository.CreatorID)
 	if err != nil {
-		return err
+		return nil, err
 	}
+	repository.Creator = creator
 
-	repository.Updater, err = s.composePrincipalByID(ctx, repository.UpdaterID)
+	updater, err := s.composePrincipalByID(ctx, repository.UpdaterID)
 	if err != nil {
-		return err
+		return nil, err
 	}
+	repository.Updater = updater
 
-	repository.VCS, err = s.composeVCSByID(ctx, repository.VCSID)
+	vcs, err := s.composeVCSByID(ctx, repository.VCSID)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	// We should always expect VCS to exist when ID isn't the default zero.
-	if repository.VCSID > 0 && repository.VCS == nil {
-		return fmt.Errorf("VCS not found for ID: %v", repository.VCSID)
+	if repository.VCSID > 0 && vcs == nil {
+		return nil, fmt.Errorf("VCS not found for ID: %v", repository.VCSID)
 	}
+	repository.VCS = vcs
 
-	repository.Project, err = s.composeProjectByID(ctx, repository.ProjectID)
+	project, err := s.composeProjectByID(ctx, repository.ProjectID)
 	if err != nil {
-		return err
+		return nil, err
 	}
+	repository.Project = project
 
-	return nil
+	return repository, nil
 }
