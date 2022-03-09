41fdfa506bffb8c7795360ca3a83426cd06ef4df
d-bytebase <d@bytebase.com>
2022-03-09 16:30:45 +0800

    chore: update

--- resources/mysql/resources_darwin.go
+++ resources/mysql/resources_darwin.go
@@ -2,6 +2,7 @@ package mysql
 
 import "embed"
 
+// f1943053b12428e4c0e4ed309a636fd0 is the md5 hash.
 //go:generate ./fetch_mysql.sh mysql-8.0.28-macos11-arm64.tar.gz f1943053b12428e4c0e4ed309a636fd0
 
 //go:embed mysql-8.0.28-macos11-arm64.tar.gz
--- resources/mysql/resources_linux.go
+++ resources/mysql/resources_linux.go
@@ -2,6 +2,7 @@ package mysql
 
 import "embed"
 
+// 55a7759e25cc527416150c8181ce3f6d is the md5 hash.
 //go:generate ./fetch_mysql.sh mysql-8.0.28-linux-glibc2.17-x86_64-minimal.tar.xz 55a7759e25cc527416150c8181ce3f6d
 
 //go:embed mysql-8.0.28-linux-glibc2.17-x86_64-minimal.tar.xz
