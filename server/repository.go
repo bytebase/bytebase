e1b33aeb8268ceac5382c0be6ba679ea2b8519fb
dragonly <liyilongko@gmail.com>
2022-03-09 14:04:38 +0800

    refactor: eliminate predefined err variable

--- server/repository.go
+++ server/repository.go
@@ -8,33 +8,35 @@ import (
 )
 
 func (s *Server) composeRepositoryRelationship(ctx context.Context, raw *api.RepositoryRaw) (*api.Repository, error) {
-	var err error
-
 	repository := raw.ToRepository()
 
-	repository.Creator, err = s.composePrincipalByID(ctx, repository.CreatorID)
+	creator, err := s.composePrincipalByID(ctx, repository.CreatorID)
 	if err != nil {
 		return nil, err
 	}
+	repository.Creator = creator
 
-	repository.Updater, err = s.composePrincipalByID(ctx, repository.UpdaterID)
+	updater, err := s.composePrincipalByID(ctx, repository.UpdaterID)
 	if err != nil {
 		return nil, err
 	}
+	repository.Updater = updater
 
-	repository.VCS, err = s.composeVCSByID(ctx, repository.VCSID)
+	vcs, err := s.composeVCSByID(ctx, repository.VCSID)
 	if err != nil {
 		return nil, err
 	}
 	// We should always expect VCS to exist when ID isn't the default zero.
-	if repository.VCSID > 0 && repository.VCS == nil {
+	if repository.VCSID > 0 && vcs == nil {
 		return nil, fmt.Errorf("VCS not found for ID: %v", repository.VCSID)
 	}
+	repository.VCS = vcs
 
-	repository.Project, err = s.composeProjectByID(ctx, repository.ProjectID)
+	project, err := s.composeProjectByID(ctx, repository.ProjectID)
 	if err != nil {
 		return nil, err
 	}
+	repository.Project = project
 
 	return repository, nil
 }
