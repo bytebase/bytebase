c14116425bd87be42369b48b6a5ddce2c6928ef6
qsliu <57934883+qsliu2017@users.noreply.github.com>
2022-03-10 17:35:38 +0800

    feat(cli-test): add test framework (#661)
    Former-commit-id: 17ad5090a0d58aaf554ff98e3b18e4f1a93c4e45 [formerly b54bfa3638c9422a9d088813ca6f2d4d58d92473] [formerly 70a64505ef9f0e320a8ccc0ad3df051bc936c78f]
    Former-commit-id: 07971dfda01fc30511c46aae36a6b3b988249b37
    Former-commit-id: d4349fae65da01757de9f5bd9591a72612b24798

---
+++ bin/bb/cmd/testdata/mysql_test_schema/2_blog.sql
@@ -0,0 +1,104 @@
+-- Covered objects:
+-- - Table
+-- - Index
+-- - View
+-- - Procedure
+-- - Function
+-- - Event
+-- - Trigger
+
+CREATE DATABASE bytebase_test_blog;
+
+-- Table and Index
+CREATE TABLE bytebase_test_blog.author (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) COMMENT 'name of the author',
+	email VARCHAR(255),
+    coin INTEGER DEFAULT 0 COMMENT 'coin can be earned by posting posts, comments'
+);
+
+CREATE TABLE bytebase_test_blog.post (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	created_ts BIGINT NOT NULL,
+	updated_ts BIGINT NOT NULL,
+	author_id INTEGER NOT NULL,
+	name VARCHAR(255) COMMENT 'name of the post',
+	content TEXT,
+	like_count INTEGER DEFAULT 0,
+	FOREIGN KEY (author_id) REFERENCES author (id) ON DELETE CASCADE,
+	INDEX (name),
+	INDEX (created_ts)
+);
+
+CREATE TABLE bytebase_test_blog.comment (
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	created_ts BIGINT NOT NULL,
+	updated_ts BIGINT NOT NULL,
+	post_id INTEGER,
+	author_id INTEGER,
+	content TEXT,
+	FOREIGN KEY (post_id) REFERENCES post (id) ON DELETE CASCADE,
+	FOREIGN KEY (author_id) REFERENCES author (id) ON DELETE CASCADE,
+	INDEX (created_ts)
+);
+
+-- View
+CREATE VIEW bytebase_test_blog.top_like_post AS
+SELECT
+	*
+FROM
+	bytebase_test_blog.post
+ORDER BY
+	like_count DESC
+LIMIT 10;
+
+-- Procedure
+DELIMITER //
+CREATE PROCEDURE bytebase_test_blog.author_post_count (IN author_id INT, OUT post_count INT)
+BEGIN
+	SELECT
+		COUNT(*) INTO post_count
+		FROM
+			author, post
+		WHERE
+			author.id = post.author_id AND author.id = author_id;
+END //
+DELIMITER ;
+
+-- Function
+DELIMITER //
+CREATE FUNCTION bytebase_test_blog.author_comment_count (author_id INT)
+	RETURNS INT DETERMINISTIC
+BEGIN
+DECLARE
+	comment_count INT DEFAULT 0;
+	SELECT
+		COUNT(*) INTO comment_count
+	FROM
+		author,
+		COMMENT
+	WHERE
+		author.id = comment.author_id
+		AND author.id = author_id;
+	RETURN comment_count;
+END //
+DELIMITER ;
+
+-- Event
+DELIMITER //
+CREATE EVENT bytebase_test_blog.increase_author_coin_daily ON SCHEDULE EVERY 1 DAY DO UPDATE author
+SET coin = coin + 1;
+
+-- Trigger
+CREATE TRIGGER bytebase_test_blog.update_post_updated_ts
+	AFTER UPDATE ON post
+	FOR EACH ROW
+BEGIN
+	UPDATE
+		post
+	SET
+		updated_ts = NOW()
+	WHERE
+		id = OLD.id;
+END //
+DELIMITER ;
