e2818b230874a2d923fff93670d6e4936eeeb408
qsliu <qsliu2017@outlook.com>
2022-03-10 10:52:09 +0800

    add dump test

---
+++ bin/bb/cmd/dump_test.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	// Embeded expected output.
+	_ "embed"
+
+	"github.com/bytebase/bytebase/resources/mysql"
+)
+
+var (
+	//go:embed testdata/expected/test_dump_todo
+	expectedTodo string
+)
+
+func TestDump(t *testing.T) {
+	basedir, datadir := t.TempDir(), t.TempDir()
+	mysql, err := mysql.Install(basedir, datadir, "root")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := mysql.Start(13306, os.Stdout, os.Stderr, 60); err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := mysql.Stop(os.Stdout, os.Stderr); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	if err := mysql.Import("testdata/mysql_test_schema", os.Stdout, os.Stderr); err != nil {
+		t.Fatal(err)
+	}
+
+	tt := []testTable{
+		{
+			args: []string{
+				"dump",
+				"--type", "mysql",
+				"--username", "root",
+				"--hostname", "localhost",
+				"--port", fmt.Sprint(mysql.Port()),
+				"--database", "bytebase_test_todo",
+			},
+			expected: expectedTodo,
+		},
+	}
+	tableTest(t, tt)
+}
