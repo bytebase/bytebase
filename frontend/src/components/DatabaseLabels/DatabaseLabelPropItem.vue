7759bc59cf5debd714566736a4a459e9057059e1
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-03-11 11:42:51 +0800

    feat: confirm before changing label value

--- frontend/src/components/DatabaseLabels/DatabaseLabelPropItem.vue
+++ frontend/src/components/DatabaseLabels/DatabaseLabelPropItem.vue
@@ -6,10 +6,10 @@
     <template v-else>
       <select
         class="absolute inset-0 opacity-0 m-0 p-0"
-        :value="value"
+        :value="state.value"
         @change="onChange"
       >
-        <option :value="''" :selected="!!value">
+        <option :value="''" :selected="!!state.value">
           {{ $t("label.empty-label-value") }}
         </option>
         <option
@@ -20,15 +20,19 @@
           {{ labelValue }}
         </option>
       </select>
-      {{ value || $t("label.empty-label-value") }}
+      {{ state.value || $t("label.empty-label-value") }}
       <heroicons-outline:chevron-down class="w-4 h-4 ml-0.5" />
     </template>
   </span>
 </template>
 
 <script lang="ts" setup>
-import { computed, defineProps, defineEmits } from "vue";
+import { capitalize } from "lodash-es";
+import { useDialog } from "naive-ui";
+import { computed, defineProps, defineEmits, reactive, watch } from "vue";
+import { useI18n } from "vue-i18n";
 import { Database, Label, LabelValueType } from "../../types";
+import { hidePrefix } from "../../utils";
 
 const props = defineProps<{
   label: Label;
@@ -43,6 +47,19 @@ const emit = defineEmits<{
   (e: "update:value", value: LabelValueType): void;
 }>();
 
+const { t } = useI18n();
+
+const state = reactive({
+  value: props.value,
+});
+
+const dialog = useDialog();
+
+watch(
+  () => props.value,
+  (value) => (state.value = value)
+);
+
 const editable = computed((): boolean => {
   if (!props.allowEdit) return false;
 
@@ -56,7 +73,30 @@ const editable = computed((): boolean => {
 
 const onChange = (e: Event) => {
   const select = e.target as HTMLSelectElement;
-  const { value } = select;
-  emit("update:value", value);
+  dialog.create({
+    positiveText: t("common.confirm"),
+    negativeText: t("common.cancel"),
+    title: t("label.confirm-change", {
+      label: capitalize(hidePrefix(props.label.key)),
+    }),
+    closable: false,
+    maskClosable: false,
+    closeOnEsc: false,
+    onClose: () => {
+      console.log("asdf");
+    },
+    onMaskClick: () => {
+      console.log("222");
+    },
+    onNegativeClick: () => {
+      state.value = props.value;
+      select.value = state.value || "";
+      console.log(state.value);
+    },
+    onPositiveClick: () => {
+      const { value } = select;
+      emit("update:value", value);
+    },
+  });
 };
 </script>
