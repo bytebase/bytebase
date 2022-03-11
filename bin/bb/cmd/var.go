b6d63652a46259ba8c455dd554ff598c0ae00060
qsliu <57934883+qsliu2017@users.noreply.github.com>
2022-03-11 18:22:15 +0800

    refactor(cli): get rid of global var (#850)
    Former-commit-id: d10b70a0746a132b1fa4890e82f454ac75da0fb7

--- bin/bb/cmd/var.go
+++ bin/bb/cmd/var.go
@@ -4,21 +4,5 @@ package cmd
 import "go.uber.org/zap"
 
 var (
-	databaseType string
-	username     string
-	password     string
-	hostname     string
-	port         string
-	database     string
-	file         string
-
-	// SSL flags.
-	sslCA   string // server-ca.pem
-	sslCert string // client-cert.pem
-	sslKey  string // client-key.pem
-
-	// Dump options.
-	schemaOnly bool
-
 	logger *zap.Logger
 )
