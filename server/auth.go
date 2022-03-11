b29ef4eb1a466fed170afab083a80835d01c288a
Zilong Zhou <60960532+zhouzilong2020@users.noreply.github.com>
2022-03-11 10:23:45 +0800

    fix: fixed a security issue caused by fake VCS (#834)
    * fix: fixed a security issue caused by fake VCS
    
    * chore: rename
    
    Former-commit-id: b368e37ccdb224b7bab4fc8f6f033f1e0a1b6390

--- server/auth.go
+++ server/auth.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/bytebase/bytebase/api"
 	"github.com/bytebase/bytebase/common"
+	"github.com/bytebase/bytebase/plugin/vcs"
 	vcsPlugin "github.com/bytebase/bytebase/plugin/vcs"
 	"github.com/google/jsonapi"
 	"github.com/labstack/echo/v4"
@@ -28,6 +29,7 @@ func (s *Server) registerAuthRoutes(g *echo.Group) {
 		var authProviderList []*api.AuthProvider
 		for _, vcs := range list {
 			newProvider := &api.AuthProvider{
+				ID:            vcs.ID,
 				Type:          vcs.Type,
 				Name:          vcs.Name,
 				InstanceURL:   vcs.InstanceURL,
@@ -81,15 +83,24 @@ func (s *Server) registerAuthRoutes(g *echo.Group) {
 				if err := jsonapi.UnmarshalPayload(c.Request().Body, gitlabLogin); err != nil {
 					return echo.NewHTTPError(http.StatusBadRequest, "Malformatted gitlab login request").SetInternal(err)
 				}
-				gitlabUserInfo, err := vcsPlugin.Get("GITLAB_SELF_HOST", vcsPlugin.ProviderConfig{Logger: s.l}).TryLogin(ctx,
+				findVCS := &api.VCSFind{ID: &gitlabLogin.ID}
+				vcsFound, err := s.VCSService.FindVCS(ctx, findVCS)
+				if err != nil {
+					return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to fetch vcs, name: %v, ID: %v", gitlabLogin.Name, gitlabLogin.Name)).SetInternal(err)
+				}
+				if vcsFound == nil {
+					return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("vcs do not exist, name: %v, ID: %v", gitlabLogin.Name, gitlabLogin.Name)).SetInternal(err)
+				}
+
+				gitlabUserInfo, err := vcsPlugin.Get(vcs.GitLabSelfHost, vcsPlugin.ProviderConfig{Logger: s.l}).TryLogin(ctx,
 					common.OauthContext{
-						ClientID:     gitlabLogin.ApplicationID,
-						ClientSecret: gitlabLogin.Secret,
+						ClientID:     vcsFound.ApplicationID,
+						ClientSecret: vcsFound.Secret,
 						AccessToken:  gitlabLogin.AccessToken,
 						RefreshToken: "",
 						Refresher:    nil,
 					},
-					gitlabLogin.InstanceURL,
+					vcsFound.InstanceURL,
 				)
 				if err != nil {
 					return echo.NewHTTPError(http.StatusInternalServerError, "Fail to fetch user info from gitlab").SetInternal(err)
