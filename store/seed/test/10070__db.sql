e5baadbea8c837be30e4771a9cc0f45415175698
email <imrealleonardo@gmail.com>
2022-03-15 18:51:20 +0800

    fix: check database exists including wildcards

--- store/seed/test/10070__db.sql
+++ store/seed/test/10070__db.sql
@@ -496,7 +496,7 @@ VALUES
         ''
     );
 
--- Database for dev environment PostgreSQL instance 6006
+-- Wildcard database for dev environment PostgreSQL instance 6006
 INSERT INTO
     db (
         id,
@@ -514,6 +514,36 @@ INSERT INTO
 VALUES
     (
         7018,
+        101,
+        101,
+        6006,
+        1,
+        '*',
+        'utf8mb4',
+        'utf8mb4_general_ci',
+        'OK',
+        0,
+        ''
+    );
+
+-- Database for dev environment PostgreSQL instance 6006
+INSERT INTO
+    db (
+        id,
+        creator_id,
+        updater_id,
+        instance_id,
+        project_id,
+        name,
+        character_set,
+        "collation",
+        sync_status,
+        last_successful_sync_ts,
+        schema_version
+    )
+VALUES
+    (
+        7019,
         101,
         101,
         6006,
@@ -543,7 +573,7 @@ INSERT INTO
     )
 VALUES
     (
-        7019,
+        7020,
         101,
         101,
         6007,
@@ -573,7 +603,7 @@ INSERT INTO
     )
 VALUES
     (
-        7020,
+        7021,
         101,
         101,
         6008,
@@ -603,7 +633,7 @@ INSERT INTO
     )
 VALUES
     (
-        7021,
+        7022,
         101,
         101,
         6009,
@@ -633,38 +663,10 @@ INSERT INTO
     )
 VALUES
     (
-        7022,
-        101,
-        101,
-        6001,
-        3005,
-        'tenant_db1',
-        'utf8mb4',
-        'utf8mb4_general_ci',
-        'OK',
-        1624558090,
-        'v1'
-    );
-INSERT INTO
-    db (
-        id,
-        creator_id,
-        updater_id,
-        instance_id,
-        project_id,
-        name,
-        character_set,
-        "collation",
-        sync_status,
-        last_successful_sync_ts,
-        schema_version
-    )
-VALUES
-    (
         7023,
         101,
         101,
-        6002,
+        6001,
         3005,
         'tenant_db1',
         'utf8mb4',
@@ -692,14 +694,14 @@ VALUES
         7024,
         101,
         101,
-        6001,
-        3006,
-        'tenant_db2',
+        6002,
+        3005,
+        'tenant_db1',
         'utf8mb4',
         'utf8mb4_general_ci',
         'OK',
         1624558090,
-        '2021v1'
+        'v1'
     );
 INSERT INTO
     db (
@@ -720,7 +722,7 @@ VALUES
         7025,
         101,
         101,
-        6002,
+        6001,
         3006,
         'tenant_db2',
         'utf8mb4',
@@ -748,6 +750,34 @@ VALUES
         7026,
         101,
         101,
+        6002,
+        3006,
+        'tenant_db2',
+        'utf8mb4',
+        'utf8mb4_general_ci',
+        'OK',
+        1624558090,
+        '2021v1'
+    );
+INSERT INTO
+    db (
+        id,
+        creator_id,
+        updater_id,
+        instance_id,
+        project_id,
+        name,
+        character_set,
+        "collation",
+        sync_status,
+        last_successful_sync_ts,
+        schema_version
+    )
+VALUES
+    (
+        7027,
+        101,
+        101,
         6001,
         3007,
         'game_dev',
@@ -773,7 +803,7 @@ INSERT INTO
     )
 VALUES
     (
-        7027,
+        7028,
         101,
         101,
         6003,
@@ -801,7 +831,7 @@ INSERT INTO
     )
 VALUES
     (
-        7028,
+        7029,
         101,
         101,
         6004,
@@ -829,7 +859,7 @@ INSERT INTO
     )
 VALUES
     (
-        7029,
+        7030,
         101,
         101,
         6004,
@@ -859,7 +889,7 @@ INSERT INTO
     )
 VALUES
     (
-        7030,
+        7031,
         101,
         101,
         6010,
@@ -872,4 +902,4 @@ VALUES
         ''
     );
 
-ALTER SEQUENCE db_id_seq RESTART WITH 7031;
+ALTER SEQUENCE db_id_seq RESTART WITH 7032;
