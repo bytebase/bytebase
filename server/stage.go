8b101017892ce5fdca5b98ab75cfad2f8953fde9
Yilong Li <liyilongko@gmail.com>
2022-03-16 23:25:18 +0800

    refactor: split data model object for api.Task (#889)
    * WIP: --no-verify
    
    * WIP: --no-verify
    
    * refactor: add TaskRaw to represent store model for Task
    
    * refactor: add a special code path for validate only checks of Issue (#891)
    
    * refactor: add a special code path for validate only checks of Issue
    
    * refactor: remove empty path in validate only check
    
    * refactor: remove empty path in validate only check
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    
    * fix: compilation
    
    * refactor: address reviewer's comments
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    Former-commit-id: e670ab6e5ac0630163e669cae33c220d0d3e424c [formerly 0124dbf70162e60922961b3cba20998a88e685f1] [formerly 2d3cf252d37fe05c729eae2961445334ef7d5c9b]
    Former-commit-id: 87f33f971019fbc3f9c55fa8658b99e1d2dc54b0
    Former-commit-id: 8942aacd1df0c57ebd7423f404a1b18b1193f708

--- server/stage.go
+++ server/stage.go
@@ -41,17 +41,9 @@ func (s *Server) composeStageRelationship(ctx context.Context, stage *api.Stage)
 		return err
 	}
 
-	if stage.TaskList == nil {
-		stage.TaskList, err = s.composeTaskListByPipelineAndStageID(ctx, stage.PipelineID, stage.ID)
-		if err != nil {
-			return err
-		}
-	} else {
-		for _, task := range stage.TaskList {
-			if err := s.composeTaskRelationship(ctx, task); err != nil {
-				return err
-			}
-		}
+	stage.TaskList, err = s.composeTaskListByPipelineAndStageID(ctx, stage.PipelineID, stage.ID)
+	if err != nil {
+		return err
 	}
 
 	return nil
