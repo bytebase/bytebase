f276913089a06a90fb3c4597e5d7aeea3b3d0d4a
ecmadao <wlec@outlook.com>
2022-03-24 14:21:34 +0800

    feat: enable the branding feature

--- frontend/src/views/SettingWorkspaceSubscription.vue
+++ frontend/src/views/SettingWorkspaceSubscription.vue
@@ -4,7 +4,7 @@
       {{ $t("subscription.description") }}
       <a
         class="text-accent"
-        href="https://hub.bytebase.com/pricing"
+        href="https://hub.bytebase.com/pricing?plan=team&source=console"
         target="__blank"
       >
         {{ $t("subscription.description-highlight") }}
