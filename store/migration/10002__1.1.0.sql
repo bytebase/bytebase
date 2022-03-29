fb6bc688de99894f86c4773c43302f12b837ce39
STEVEN <imrealleonardo@gmail.com>
2022-03-29 16:06:37 +0800

    feat: update `sheet` schema (#883)
    * feat: update `sheet` schema
    
    * chore: revert commit
    
    * chore: revert commit
    
    * feat: alter `sheet` schema
    
    * chore: add default value
    
    * chore: code clean
    
    * chore: rename migration file
    
    Former-commit-id: be6184f0ddda06114801276350e8834e12f71e55 [formerly ada9e61351be3a9835faf6faa08c6f54608ba494] [formerly 38cd6ddd278678040a6a262b0bc11b3098f4005e]
    Former-commit-id: 8bbbfdc7fb7d4d66b8297950be725b9d4b89f7aa
    Former-commit-id: d909e9d6f12950728a9ad61ae382bfcbccf7158f

--- store/migration/10002__1.1.0.sql
+++ store/migration/10002__1.1.0.sql
@@ -6,3 +6,9 @@ ALTER TABLE project_member ADD CONSTRAINT project_member_role_provider_check CHE
 
 ALTER TABLE vcs DROP CONSTRAINT vcs_type_check;
 ALTER TABLE vcs ADD CONSTRAINT vcs_type_check CHECK (type IN ('GITLAB_SELF_HOST', 'GITHUB_COM'));
+
+ALTER TABLE sheet ADD source TEXT NOT NULL CHECK (source IN ('BYTEBASE', 'GITLAB_SELF_HOST', 'GITHUB_COM')) DEFAULT 'BYTEBASE';
+ALTER TABLE sheet ADD type TEXT NOT NULL CHECK (type IN ('SQL')) DEFAULT 'SQL';
+ALTER TABLE sheet ADD payload JSONB NOT NULL DEFAULT '{}';
+
+ALTER TABLE repository ADD sheet_path_template TEXT NOT NULL DEFAULT '';
