626e904b69429b476714a00cb341eefeadb45b6c
email <imrealleonardo@gmail.com>
2022-03-29 23:14:27 +0800

    chore: fields order

--- store/migration/10002__1.1.0.sql
+++ store/migration/10002__1.1.0.sql
@@ -10,7 +10,7 @@ ALTER TABLE vcs ADD CONSTRAINT vcs_type_check CHECK (type IN ('GITLAB_SELF_HOST'
 ALTER TABLE sheet ADD source TEXT NOT NULL CHECK (source IN ('BYTEBASE', 'GITLAB_SELF_HOST', 'GITHUB_COM')) DEFAULT 'BYTEBASE';
 ALTER TABLE sheet ADD type TEXT NOT NULL CHECK (type IN ('SQL')) DEFAULT 'SQL';
 ALTER TABLE sheet ADD payload JSONB NOT NULL DEFAULT '{}';
-CREATE UNIQUE INDEX idx_sheet_project_id_name_source ON sheet(project_id, name, source);
+CREATE UNIQUE INDEX idx_sheet_project_id_source_name ON sheet(project_id, source, name);
 
 ALTER TABLE repository ADD sheet_path_template TEXT NOT NULL DEFAULT '';
 
