4d7fb6ce532170e196515d6fd54a33a0c6c765d0
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-03-31 11:08:57 +0800

    feat(semantic): migrate migration history version to semantic version storage format (#972)
    Former-commit-id: 5c409d3e48218384f3576a802388f5bec4afdd04 [formerly 4a560fcc1ed7bd90a14b3a35359049923b0f4c48] [formerly 7850413c4504af04e11b78aff3bce992baa46511]
    Former-commit-id: 5682e4ab9fba09a829b152da1acc9978a46339e0
    Former-commit-id: ff54c815efdaa33f3c1fd24639a12b5dd12f4379

--- plugin/db/clickhouse/clickhouse.go
+++ plugin/db/clickhouse/clickhouse.go
@@ -618,7 +618,63 @@ func (driver *Driver) FindMigrationHistoryList(ctx context.Context, find *db.Mig
 	if v := find.Limit; v != nil {
 		query += fmt.Sprintf(" LIMIT %d", *v)
 	}
-	return util.FindMigrationHistoryList(ctx, query, params, driver, find, baseQuery)
+	history, err := util.FindMigrationHistoryList(ctx, query, params, driver, find, baseQuery)
+	// TODO(d): remove this block once all existing customers all migrated to semantic versioning.
+	if err != nil {
+		if !strings.Contains(err.Error(), "invalid stored version") {
+			return nil, err
+		}
+		if err := driver.updateMigrationHistoryStorageVersion(ctx); err != nil {
+			return nil, err
+		}
+		return util.FindMigrationHistoryList(ctx, query, params, driver, find, baseQuery)
+	}
+	return history, err
+}
+
+func (driver *Driver) updateMigrationHistoryStorageVersion(ctx context.Context) error {
+	sqldb, err := driver.GetDbConnection(ctx, "bytebase")
+	if err != nil {
+		return err
+	}
+	query := `SELECT id, version FROM bytebase.migration_history`
+	rows, err := sqldb.Query(query)
+	if err != nil {
+		return err
+	}
+	type ver struct {
+		id      int
+		version string
+	}
+	var vers []ver
+	for rows.Next() {
+		var v ver
+		if err := rows.Scan(&v.id, &v.version); err != nil {
+			return err
+		}
+		vers = append(vers, v)
+	}
+	if err := rows.Close(); err != nil {
+		return err
+	}
+
+	updateQuery := `
+		ALTER TABLE
+			bytebase.migration_history
+		UPDATE
+			version = $1
+		WHERE id = $2 AND version = $3
+	`
+	for _, v := range vers {
+		if strings.HasPrefix(v.version, util.NonSemanticPrefix) {
+			continue
+		}
+		newVersion := fmt.Sprintf("%s%s", util.NonSemanticPrefix, v.version)
+		if _, err := sqldb.Exec(updateQuery, newVersion, v.id, v.version); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // Dump and restore
