847676d04fdf8363ece543bee3cc1459b7a817f7
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-03-31 10:36:11 +0800

    chore: fix linter warning (#973)
    * chore: fix warning
    
    * chore: Update api/plan.go
    
    Co-authored-by: ecmadao <wlec@outlook.com>
    
    * chore: update
    
    Co-authored-by: ecmadao <wlec@outlook.com>
    Former-commit-id: b4d61368d63ca70e40f6fe5ca376c49ffe15ca31 [formerly c2f19f4ad2775bfd56abfa45710c1ce9c16bf576] [formerly 82b85f8ec4096400db7c14d054adfde2314235dc]
    Former-commit-id: f122670d4bc9af7e3dadac13bcbc4d5202630836
    Former-commit-id: 8678126c80acdc55f86bfe0ddadba4611821087f

--- api/plan.go
+++ api/plan.go
@@ -96,6 +96,8 @@ const (
 	Feature3rdPartyAuth FeatureType = "bb.feature.3rd-party-auth"
 
 	// Branding
+
+	// FeatureBranding enables customized branding.
 	//
 	// Currently, we only support customizing the logo.
 	FeatureBranding FeatureType = "bb.feature.branding"
