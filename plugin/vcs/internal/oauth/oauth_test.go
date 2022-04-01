b2076802d3b79f97e1c6d32ae2d38ee1500ede22
Joe Chen <jc@unknwon.io>
2022-04-01 09:36:10 +0800

    test: oauth

--- plugin/vcs/internal/oauth/oauth_test.go
+++ plugin/vcs/internal/oauth/oauth_test.go
@@ -9,10 +9,85 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/bytebase/bytebase/common"
 )
 
-func TestMethods(t *testing.T) {
-	// todo: POST, GET, PUT, DELETE
+func TestPost(t *testing.T) {
+	ctx := context.Background()
+	client := &http.Client{
+		Transport: &common.MockRoundTripper{
+			MockRoundTrip: func(r *http.Request) (*http.Response, error) {
+				assert.Equal(t, http.MethodPost, r.Method)
+				assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+				assert.Equal(t, "Bearer token", r.Header.Get("Authorization"))
+
+				body, err := io.ReadAll(r.Body)
+				require.NoError(t, err)
+				assert.Equal(t, "POST body", string(body))
+				return &http.Response{}, nil
+			},
+		},
+	}
+	token := "token"
+	_, _, err := Post(ctx, client, "", &token, strings.NewReader("POST body"), nil)
+	require.NoError(t, err)
+}
+
+func TestGet(t *testing.T) {
+	ctx := context.Background()
+	client := &http.Client{
+		Transport: &common.MockRoundTripper{
+			MockRoundTrip: func(r *http.Request) (*http.Response, error) {
+				assert.Equal(t, http.MethodGet, r.Method)
+				assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+				assert.Equal(t, "Bearer token", r.Header.Get("Authorization"))
+				return &http.Response{}, nil
+			},
+		},
+	}
+	token := "token"
+	_, _, err := Get(ctx, client, "", &token, nil)
+	require.NoError(t, err)
+}
+
+func TestPut(t *testing.T) {
+	ctx := context.Background()
+	client := &http.Client{
+		Transport: &common.MockRoundTripper{
+			MockRoundTrip: func(r *http.Request) (*http.Response, error) {
+				assert.Equal(t, http.MethodPut, r.Method)
+				assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+				assert.Equal(t, "Bearer token", r.Header.Get("Authorization"))
+
+				body, err := io.ReadAll(r.Body)
+				require.NoError(t, err)
+				assert.Equal(t, "PUT body", string(body))
+				return &http.Response{}, nil
+			},
+		},
+	}
+	token := "token"
+	_, _, err := Put(ctx, client, "", &token, strings.NewReader("PUT body"), nil)
+	require.NoError(t, err)
+}
+
+func TestDelete(t *testing.T) {
+	ctx := context.Background()
+	client := &http.Client{
+		Transport: &common.MockRoundTripper{
+			MockRoundTrip: func(r *http.Request) (*http.Response, error) {
+				assert.Equal(t, http.MethodDelete, r.Method)
+				assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+				assert.Equal(t, "Bearer token", r.Header.Get("Authorization"))
+				return &http.Response{}, nil
+			},
+		},
+	}
+	token := "token"
+	_, _, err := Delete(ctx, client, "", &token, nil)
+	require.NoError(t, err)
 }
 
 func TestRetry_Exceeded(t *testing.T) {
