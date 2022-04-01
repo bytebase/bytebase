03cdc4f62884cca705b35f94b9d9845327507c45
email <imrealleonardo@gmail.com>
2022-04-01 12:22:10 +0800

    chore: fix test

--- tests/fake/gitlab.go
+++ tests/fake/gitlab.go
@@ -176,9 +176,9 @@ func (gl *GitLab) readProjectFileMetadata(c echo.Context) error {
 	content := pd.files[filePath]
 
 	buf, err := json.Marshal(&gitlab.FileMeta{
-		FileName: fileName,
-		FilePath: filePath,
-		Content:  content,
+		FileName:      fileName,
+		FilePath:      filePath,
+		ContentBase64: content,
 	})
 	if err != nil {
 		return c.String(http.StatusInternalServerError, fmt.Sprintf("failed to marshal FileMeta, error %v", err))
