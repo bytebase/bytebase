dee5719ba1b34b93b47ab6e4e031b9dc2e1efacf
tianzhou <t@bytebase.com>
2022-04-04 13:10:55 +0800

    feat: allow OWNER and DBA to toggle debug mode (#1009)
    * docs: add air version requirement
    
    * chore: use lower case, kebab-case for api and slug
    
    * misc accidental checkin
    
    * doc: change case
    
    * chore: repurpose render-preview to use it for demo as well
    
    * chore: add demo dockerfile
    
    * bug: fix release build
    
    * feat: enable debug mode in release
    
    Former-commit-id: 04b57685efb505ee811fa261b7b1bd55db0493d2

---
+++ frontend/src/views/BannerDebug.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-warning">
+    <div class="text-center py-3 px-3 font-medium text-white truncate">
+      {{ $t("banner.debug") }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BannerDebug",
+});
+</script>
