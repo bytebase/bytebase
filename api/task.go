7009b23c598b114570a45e5f85095c3c3b3a6393
p0ny <p0uy@outlook.com>
2022-04-07 10:53:55 +0800

    fix: address comment

--- api/task.go
+++ api/task.go
@@ -118,9 +118,9 @@ type TaskDatabaseSchemaUpdateGhostCutoverPayload struct {
 
 // TaskDatabaseSchemaUpdateGhostDropOriginalTablePayload is the task type for dropping the original table
 type TaskDatabaseSchemaUpdateGhostDropOriginalTablePayload struct {
-	// DatabaseName is like `_tablename_del`.
 	DatabaseName string `json:"databaseName,omitempty"`
-	TableName    string `json:"tableName,omitempty"`
+	// TableName is like `_tablename_del`.
+	TableName string `json:"tableName,omitempty"`
 }
 
 // TaskDatabaseDataUpdatePayload is the task payload for database data update (DML).
