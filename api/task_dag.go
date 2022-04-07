3f4a4683a7c9fe66494c9ba777cc4d4be6c9c9b5
p0ny <p0uy@outlook.com>
2022-04-07 15:20:35 +0800

    feat(gh-ost): task dag api definition

---
+++ api/task_dag.go
@@ -0,0 +1,30 @@
+package api
+
+// TaskDAG describes task dependency relationship.
+// FromTaskID is blocked by ToTaskID
+// It's rather DAGEdge than DAG
+type TaskDAG struct {
+	ID int
+
+	// Standard fields
+	CreatedTs int64
+	UpdatedTs int64
+
+	// Domain Specific fields
+	FromTaskID int
+	ToTaskID   int
+	Payload    string
+}
+
+// TaskDAGCreate is the API message to create TaskDAG.
+type TaskDAGCreate struct {
+	FromTaskID int
+	ToTaskID   int
+	Payload    string
+}
+
+// TaskDAGFind is the API message to find TaskDAG.
+type TaskDAGFind struct {
+	FromTaskID *int
+	ToTaskID   *int
+}
