1008dd8305da0929cca59d660b07f174ccab238a
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-04-12 17:27:12 +0800

    refactor: re-implement inbox store module with pinia (#1085)
    * refactor: re-implement inbox store module with pinia
    
    * refactor: migrate inbox store module from vuex to pinia
    
    * chore: remove inbox vuex module
    
    Former-commit-id: 875702da87acc4578f687be5f9c7718ebe1f0b31 [formerly 7dce000edef0dce3dbf609d728ad2b3b75e20e4f] [formerly 4fd444a491abbef4a5da2ee6af39bb162577f4b7]
    Former-commit-id: 7b68c89f0391e27d17f851935809b1c616f9b6cb
    Former-commit-id: d153ecd5e0fe04f7c9f6ba3a3c52dae5486bed35

---
+++ frontend/src/store/pinia-modules/inbox.ts
@@ -0,0 +1,129 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+import {
+  Activity,
+  Inbox,
+  InboxId,
+  InboxPatch,
+  InboxState,
+  InboxSummary,
+  PrincipalId,
+  ResourceIdentifier,
+  ResourceObject,
+  unknown,
+} from "../../types";
+import { useActivityStore } from "./activity";
+
+function convert(inbox: ResourceObject, includedList: ResourceObject[]): Inbox {
+  const activityId = (inbox.relationships!.activity.data as ResourceIdentifier)
+    .id;
+  let activity: Activity = unknown("ACTIVITY") as Activity;
+  activity.id = parseInt(activityId);
+
+  const activityStore = useActivityStore();
+  for (const item of includedList || []) {
+    if (item.type == "activity" && item.id == activityId) {
+      activity = activityStore.convert(item, includedList);
+    }
+  }
+
+  return {
+    ...(inbox.attributes as Omit<Inbox, "id">),
+    id: parseInt(inbox.id),
+    activity,
+  };
+}
+
+export const useInboxStore = defineStore("inbox", {
+  state: (): InboxState => ({
+    inboxListByUser: new Map(),
+    inboxSummaryByUser: new Map(),
+  }),
+  actions: {
+    getInboxListByUser(userId: PrincipalId): Inbox[] {
+      return this.inboxListByUser.get(userId) || [];
+    },
+    getInboxSummaryByUser(userId: PrincipalId): InboxSummary {
+      return (
+        this.inboxSummaryByUser.get(userId) || {
+          hasUnread: false,
+          hasUnreadError: false,
+        }
+      );
+    },
+    setInboxListByUser({
+      userId,
+      inboxList,
+    }: {
+      userId: PrincipalId;
+      inboxList: Inbox[];
+    }) {
+      this.inboxListByUser.set(userId, inboxList);
+    },
+    setInboxSummaryByUser({
+      userId,
+      inboxSummary,
+    }: {
+      userId: PrincipalId;
+      inboxSummary: InboxSummary;
+    }) {
+      this.inboxSummaryByUser.set(userId, inboxSummary);
+    },
+    updateInboxById({ inboxId, inbox }: { inboxId: InboxId; inbox: Inbox }) {
+      for (const [_, inboxList] of this.inboxListByUser) {
+        const i = inboxList.findIndex((item: Inbox) => item.id == inboxId);
+        if (i >= 0) {
+          inboxList[i] = inbox;
+        }
+      }
+    },
+    async fetchInboxListByUser({
+      userId,
+      readCreatedAfterTs,
+    }: {
+      userId: PrincipalId;
+      readCreatedAfterTs?: number;
+    }) {
+      let url = `/api/inbox/user/${userId}`;
+      if (readCreatedAfterTs) {
+        url += `?created=${readCreatedAfterTs}`;
+      }
+      const data = (await axios.get(url)).data;
+      const inboxList = data.data.map((inbox: ResourceObject) => {
+        return convert(inbox, data.included);
+      });
+
+      this.setInboxListByUser({ userId, inboxList });
+      return inboxList;
+    },
+    async fetchInboxSummaryByUser(userId: PrincipalId) {
+      const inboxSummary = (
+        await axios.get(`/api/inbox/user/${userId}/summary`)
+      ).data;
+
+      this.setInboxSummaryByUser({ userId, inboxSummary });
+      return inboxSummary;
+    },
+    async patchInbox({
+      inboxId,
+      inboxPatch,
+    }: {
+      inboxId: InboxId;
+      inboxPatch: InboxPatch;
+    }) {
+      const data = (
+        await axios.patch(`/api/inbox/${inboxId}`, {
+          data: {
+            type: "inboxPatch",
+            attributes: inboxPatch,
+          },
+        })
+      ).data;
+      const updatedInbox = convert(data.data, data.included);
+
+      this.updateInboxById({ inboxId, inbox: updatedInbox });
+
+      return updatedInbox;
+    },
+  },
+});
