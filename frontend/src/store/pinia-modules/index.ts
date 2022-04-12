b61ba815918717856f7736528d55e9935f671f51
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-04-12 15:31:15 +0800

    refactor: implement database store module with pinia

--- frontend/src/store/pinia-modules/index.ts
+++ frontend/src/store/pinia-modules/index.ts
@@ -4,6 +4,7 @@ export * from "./auth";
 export * from "./backup";
 export * from "./bookmark";
 export * from "./command";
+export * from "./database";
 export * from "./dataSource";
 export * from "./debug";
 export * from "./deployment";
