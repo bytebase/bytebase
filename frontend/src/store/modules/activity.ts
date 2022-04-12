06dae949edfeca5a06136af48604edec36202f58
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-04-12 17:48:39 +0800

    fix: use type prefix filter to find activities (#1086)
    Former-commit-id: fd9f2aa8e8b1e5dee9c25a56a35a31d8dec62af6

--- frontend/src/store/modules/activity.ts
+++ frontend/src/store/modules/activity.ts
@@ -77,7 +77,8 @@ const actions = {
     { commit, rootGetters }: any,
     issueId: IssueId
   ) {
-    const data = (await axios.get(`/api/activity?container=${issueId}`)).data;
+    const queryList = ["typePrefix=bb.issue.", `container=${issueId}`];
+    const data = (await axios.get(`/api/activity?${queryList.join("&")}`)).data;
     const activityList = data.data.map((activity: ResourceObject) => {
       return convert(activity, data.included, rootGetters);
     });
@@ -97,7 +98,7 @@ const actions = {
       limit?: number;
     }
   ) {
-    const queryList = [`container=${projectId}`];
+    const queryList = ["typePrefix=bb.project.", `container=${projectId}`];
     if (limit) {
       queryList.push(`limit=${limit}`);
     }
@@ -119,7 +120,7 @@ const actions = {
   ) {
     const { currentUser } = useAuthStore();
     const queryList = [
-      "type=bb.sql-editor.query",
+      "typePrefix=bb.sql-editor.query",
       `user=${currentUser.id}`,
       `limit=${limit}`,
       // only fetch the successful query history
