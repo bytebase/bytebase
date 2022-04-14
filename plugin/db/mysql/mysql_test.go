03bfd48b73625e9f0b094a8db164f4fc9acf49b4
dragonly <liyilongko@gmail.com>
2022-04-14 11:40:31 +0800

    refactor: add constraint name generation logic

--- plugin/db/mysql/mysql_test.go
+++ plugin/db/mysql/mysql_test.go
@@ -7,43 +7,83 @@ import (
 )
 
 func TestRestoreFuncWithGhostTable(t *testing.T) {
-	a := require.New(t)
-	var modifiedSQL string
-	fnCopyModifiedSQL := func(query string) error {
-		modifiedSQL = query
-		return nil
-	}
-	fnExecuteStmt := genRestoreFuncWithGhostTable(fnCopyModifiedSQL)
-
 	testCases := [][]string{
-		{
+		{"multiple constraints",
+
 			"CREATE TABLE `table` (\n" +
 				"  `id` int DEFAULT NULL,\n" +
-				"  CONSTRAINT `table_chk_1` CHECK ((`id` >= 0))",
+				"  `fk_id` int DEFAULT NULL,\n" +
+				"  UNIQUE KEY `fk_id` (`fk_id`),\n" +
+				"  CONSTRAINT `chk_table_1` CHECK ((`id` >= 0))\n" +
+				"  CONSTRAINT `fk_table_1` FOREIGN KEY (fk_id) REFERENCES another_table (id)",
 
 			"CREATE TABLE `table_ghost` (\n" +
 				"  `id` int DEFAULT NULL,\n" +
-				"  CONSTRAINT CHECK ((`id` >= 0))",
+				"  `fk_id` int DEFAULT NULL,\n" +
+				"  UNIQUE KEY `fk_id` (`fk_id`),\n" +
+				"  CONSTRAINT `chk_table_1_20060102150405` CHECK ((`id` >= 0))\n" +
+				"  CONSTRAINT `fk_table_1_20060102150405` FOREIGN KEY (fk_id) REFERENCES another_table (id)",
 		},
-		{
-			// We currently DO NOT support dbPrefix in CREATE TABLE statement, so this case should fail
+
+		{"constraint name has timestamp postfix with 14 digits (unix timestamp to the second)",
+
+			"CREATE TABLE `table` (\n" +
+				"  `id` int DEFAULT NULL,\n" +
+				"  CONSTRAINT `chk_table_1_12345678901234` CHECK ((`id` >= 0))",
+
+			"CREATE TABLE `table_ghost` (\n" +
+				"  `id` int DEFAULT NULL,\n" +
+				"  CONSTRAINT `chk_table_1_20060102150405` CHECK ((`id` >= 0))",
+		},
+
+		{"constraint name has wrong timestamp postfix digits",
+
+			"CREATE TABLE `table` (\n" +
+				"  `id` int DEFAULT NULL,\n" +
+				"  CONSTRAINT `chk_table_1_1234567890` CHECK ((`id` >= 0))",
+
+			"CREATE TABLE `table_ghost` (\n" +
+				"  `id` int DEFAULT NULL,\n" +
+				"  CONSTRAINT `chk_table_1_1234567890_20060102150405` CHECK ((`id` >= 0))",
+		},
+
+		{"constraint name is longer than 64 after appending timestamp postfix",
+
+			"CREATE TABLE `table` (\n" +
+				"  `id` int DEFAULT NULL,\n" +
+				"  CONSTRAINT `chk_table_1_12345678901234567890123456789012345678901234567890` CHECK ((`id` >= 0))",
+
+			"CREATE TABLE `table_ghost` (\n" +
+				"  `id` int DEFAULT NULL,\n" +
+				"  CONSTRAINT `chk_table_1_1234567890123456789012345678901234567_20060102150405` CHECK ((`id` >= 0))",
+		},
+
+		{"should fail with database prefix",
+
 			"CREATE TABLE `database`.`table`",
 			"",
 		},
-		{
+
+		{"insert without database prefix",
+
 			"INSERT INTO `table` VALUES (1, 2, 3);",
 			"INSERT INTO `table_ghost` VALUES (1, 2, 3);",
 		},
-		{
+
+		{"insert with database prefix",
+
 			"INSERT INTO `database`.`table` VALUES (1, 2, 3);",
 			"INSERT INTO `database`.`table_ghost` VALUES (1, 2, 3);",
 		},
 	}
 
 	for _, testCase := range testCases {
-		originalSQL := testCase[0]
-		expectedSQL := testCase[1]
-		a.NoError(fnExecuteStmt(originalSQL))
-		a.Equal(expectedSQL, modifiedSQL)
+		caseName := testCase[0]
+		originalSQL := testCase[1]
+		expectedSQL := testCase[2]
+		t.Run(caseName, func(t *testing.T) {
+			rewrittenSQL := rewriteToGhostTableForRestore(originalSQL, "20060102150405")
+			require.Equal(t, expectedSQL, rewrittenSQL)
+		})
 	}
 }
