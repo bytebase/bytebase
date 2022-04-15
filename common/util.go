0bbe5b0f8c21449da9084c414a423c3188e31c88
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-04-15 16:37:10 +0800

    fix: jwt key leakage risk (#1118)
    * fix: block startedTs for users
    
    * fix: fix jwt key leakage risk
    
    * fix: add comment of why not using startedTs
    
    * fix: fix startedTs json field
    
    * fix: add comment of why using crypto/rand instead of math/rand
    
    * fix: fix comment of not using startedTs
    
    Co-authored-by: tianzhou <t@bytebase.com>
    
    Co-authored-by: tianzhou <t@bytebase.com>
    Former-commit-id: ef3f0f117e042253c462c293a77ca03c4220b76a

--- common/util.go
+++ common/util.go
@@ -1,7 +1,8 @@
 package common
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"math/big"
 	"path"
 	"sort"
 	"strings"
@@ -25,7 +26,15 @@ func RandomString(n int) string {
 	var sb strings.Builder
 	sb.Grow(n)
 	for i := 0; i < n; i++ {
-		sb.WriteRune(letters[rand.Intn(len(letters))])
+		// The reason for using crypto/rand instead of math/rand is that
+		// the former relies on hardware to generate random numbers and
+		// thus has a stronger source of random numbers.
+		randNum, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			// Use 0 as default index when encounter error
+			sb.WriteRune(letters[0])
+		}
+		sb.WriteRune(letters[randNum.Uint64()])
 	}
 	return sb.String()
 }
