972890f21c89df259db13dd62ed41888c175aa72
ecmadao <wlec@outlook.com>
2022-04-18 14:13:22 +0800

    feat: add InputWithTemplate component (#1082)
    * feat: add InputWithTemplate component
    
    * chore: update component
    
    * fix: remove duplicate class
    
    * chore: extract variables
    
    * chore: remove the event listener on component unmounted
    
    * chore: update the comments
    
    * chore: update safelist in tailwind config
    
    * fix: the UI for BBBadge
    
    Former-commit-id: ac51bf10e53b84178b19c7538e823ed6ab35761a

---
+++ frontend/src/components/InputWithTemplate/AutoWidthInput.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div ref="divRef" class="fixed invisible text-base leading-6">
+      {{ state.data }}
+    </div>
+    <input
+      ref="inputRef"
+      v-model="state.data"
+      :style="`width: ${state.width}px;`"
+      class="px-0 m-0 py-1 cleared-input outline-none"
+      type="text"
+      @keyup="(e) => $emit('keyup', e)"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { watch, reactive, ref, watchEffect, nextTick } from "vue";
+
+const props = defineProps({
+  value: {
+    default: "",
+    type: String,
+  },
+  maxWidth: {
+    default: 0,
+    type: Number,
+  },
+});
+
+const emit = defineEmits(["change", "keyup"]);
+
+const state = reactive<{ data: string; width: number }>({
+  data: props.value,
+  width: 0,
+});
+
+const inputRef = ref<HTMLInputElement>();
+const divRef = ref<HTMLDivElement>();
+
+watch(
+  () => state.data,
+  (val) => {
+    nextTick(updateWidth);
+    emit("change", val);
+  }
+);
+
+watch(
+  () => props.value,
+  (val) => {
+    state.data = val;
+    nextTick(updateWidth);
+  }
+);
+
+const updateWidth = () => {
+  const width = divRef.value?.offsetWidth ?? state.width;
+  state.width = Math.min(props.maxWidth ?? width, width);
+};
+
+watchEffect(updateWidth);
+</script>
+
+<style scoped>
+.cleared-input,
+.cleared-input:focus {
+  @apply shadow-none ring-0 border-0 border-none;
+}
+</style>
