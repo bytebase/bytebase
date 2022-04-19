f7f4c93144a85d19630b42ed22faddf1bc1200d6
STEVEN <imrealleonardo@gmail.com>
2022-04-19 15:51:08 +0800

    feat: new table `sheet_stargazer` (#1127)
    * feat: new table `sheet_stargazer`
    
    * feat: update table definition
    
    * feat: update table fields
    
    * fix: typo
    
    * chore: update seed data
    
    * chore: update seed data
    
    * chore: update seed data
    
    * fix: reset data
    
    * Update store/migration/dev/20220419000000__sheet_organizer.sql
    
    Co-authored-by: tianzhou <t@bytebase.com>
    
    * Update store/migration/dev/LATEST.sql
    
    * fix: remove prod reset
    
    Co-authored-by: tianzhou <t@bytebase.com>
    Former-commit-id: b0e3db2849d0bbb9f596d55fb6a805dd1234c58a

---
+++ store/migration/dev/20220419000000__sheet_organizer.sql
@@ -0,0 +1,12 @@
+-- sheet_organizer table stores the sheet status for a principal.
+CREATE TABLE sheet_organizer (
+    id SERIAL PRIMARY KEY,
+    sheet_id INTEGER NOT NULL REFERENCES sheet (id),
+    principal_id INTEGER NOT NULL REFERENCES principal (id),
+    starred BOOLEAN NOT NULL DEFAULT false,
+    pinned BOOLEAN NOT NULL DEFAULT false
+);
+
+CREATE UNIQUE INDEX idx_sheet_organizer_unique_sheet_id_principal_id ON sheet_organizer(sheet_id, principal_id);
+
+CREATE INDEX idx_sheet_organizer_principal_id ON sheet_organizer(principal_id);
