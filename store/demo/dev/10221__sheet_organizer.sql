737cdd2822303fc36de47e8c9c68bebf3fba1c42
email <imrealleonardo@gmail.com>
2022-04-19 12:03:15 +0800

    chore: update seed data

--- store/demo/dev/10221__sheet_organizer.sql
+++ store/demo/dev/10221__sheet_organizer.sql
@@ -1,12 +1,52 @@
 INSERT INTO
     sheet_organizer (
+        id,
         sheet_id,
         principal_id,
-        starred
+        starred,
+        pinned
     )
 VALUES
     (
+        23001,
+        22001,
+        101,
+        false,
+        false
+    );
+
+INSERT INTO
+    sheet_organizer (
+        id,
+        sheet_id,
+        principal_id,
+        starred,
+        pinned
+    )
+VALUES
+    (
+        23002,
         22002,
         101,
+        true,
+        false
+    );
+
+INSERT INTO
+    sheet_organizer (
+        id,
+        sheet_id,
+        principal_id,
+        starred,
+        pinned
+    )
+VALUES
+    (
+        23003,
+        22003,
+        101,
+        true,
         true
     );
+
+ALTER SEQUENCE sheet_id_seq RESTART WITH 23004;
