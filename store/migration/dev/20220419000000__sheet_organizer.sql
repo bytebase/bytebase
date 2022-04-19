7d4ef8fd5c18475173c7f12e04aec5eb959cced3
email <imrealleonardo@gmail.com>
2022-04-19 11:36:38 +0800

    fix: typo

--- store/migration/dev/20220419000000__sheet_organizer.sql
+++ store/migration/dev/20220419000000__sheet_organizer.sql
@@ -4,7 +4,7 @@ CREATE TABLE sheet_organizer (
     sheet_id INTEGER NOT NULL REFERENCES sheet (id),
     principal_id INTEGER NOT NULL REFERENCES principal (id),
     starred BOOLEAN NOT NULL DEFAULT false,
-    pinned BOOLEAN NOT NULL DEFAULT false,
+    pinned BOOLEAN NOT NULL DEFAULT false
 );
 
 CREATE UNIQUE INDEX idx_sheet_organizer_unique_sheet_id_principal_id ON sheet_organizer(sheet_id, principal_id);
