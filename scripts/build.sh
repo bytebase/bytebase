a1f4fb40241997ee82677576d16e6e2d51ed673a
Yaohui Wang <wangyaohuicn@gmail.com>
2022-04-21 11:49:46 +0000

    fix(scripts): posix compliant fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,31 +1,16 @@
 #!/bin/sh
-
-# cd to the root directory and run
-# ./scripts/build.sh [outdir]
+# ===========================================================================
+# File: build.sh
+# Description: usage: ./build.sh [outdir]
+# ===========================================================================
 
 # exit when any command fails
 set -e
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+cd "$(dirname "$0")/../"
+. ./scripts/init.sh
 
-# Version function used for version string comparison
-function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
-
-if [ `dirname "${BASH_SOURCE[0]}"` != "./scripts" ] && [ `dirname "${BASH_SOURCE[0]}"` != "scripts" ]
-then
-  echo "${RED}Precheck failed.${NC} Build script must run from root directory: scripts/build.sh"; exit 1;
-fi
-
-if [ -z "$1" ];
-then
-  mkdir -p bytebase-build
-  OUTPUT_DIR=$( cd bytebase-build &> /dev/null && pwd )
-else
-  OUTPUT_DIR="$1"
-fi
-
+OUTPUT_DIR=$(mkdir_output "$1")
 OUTPUT_BINARY=$OUTPUT_DIR/bytebase
 
 GO_VERSION=`go version | { read _ _ v _; echo ${v#go}; }`
@@ -34,20 +19,19 @@ then
    echo "${RED}Precheck failed.${NC} Require go version >= 1.16. Current version ${GO_VERSION}."; exit 1;
 fi
 
-if ! command -v npm &> /dev/null
+if ! command -v npm > /dev/null
 then
    echo "${RED}Precheck failed.${NC} npm is not installed."; exit 1;
 fi
 
 # Step 1 - Build the frontend release version into the backend/server/dist folder
 # Step 2 - Build the monolithic app by building backend release version together with the backend/server/dist (leveraing embed introduced in Golang 1.16).
-VERSION=`cat ./scripts/VERSION`
 echo "Start building Bytebase monolithic ${VERSION}..."
 
 echo ""
 echo "Step 1 - building bytebase frontend..."
 
-if command -v pnpm &> /dev/null
+if command -v pnpm > /dev/null
 then
     pnpm --dir ./frontend i && pnpm --dir ./frontend release
 else
