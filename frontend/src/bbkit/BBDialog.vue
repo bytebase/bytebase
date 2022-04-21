3553d6d7e0c1aa539dfdf6ba65a300ecb6c2bc93
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-04-21 16:21:26 +0800

    feat: implement BBDialog

---
+++ frontend/src/bbkit/BBDialog.vue
@@ -0,0 +1,78 @@
+<template>
+  <BBModal
+    v-if="state.visible"
+    :title="title"
+    :subtitle="subtitle"
+    :show-close="false"
+    :esc-closable="false"
+  >
+    <slot name="default"></slot>
+
+    <div class="pt-4 border-t border-block-border flex justify-end space-x-3">
+      <button
+        type="button"
+        class="btn-normal py-2 px-4"
+        @click.prevent="goNegative"
+      >
+        {{ negativeText || $t("common.cancel") }}
+      </button>
+      <button
+        type="button"
+        class="btn-primary py-2 px-4"
+        @click.prevent="goPositive"
+      >
+        {{ positiveText || $t("common.confirm") }}
+      </button>
+    </div>
+  </BBModal>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from "vue";
+import { Defer, defer } from "@/utils";
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    default: "",
+    type: String,
+  },
+  negativeText: {
+    type: String,
+    default: undefined,
+  },
+  positiveText: {
+    type: String,
+    default: undefined,
+  },
+});
+
+const state = reactive({
+  visible: false,
+  defer: undefined as Defer<boolean> | undefined,
+});
+
+const open = () => {
+  if (state.defer) {
+    state.defer.reject(new Error("duplicated call"));
+  }
+
+  state.defer = defer<boolean>();
+  state.visible = true;
+
+  return state.defer.promise;
+};
+
+const goPositive = () => {
+  state.defer?.resolve(true);
+};
+
+const goNegative = () => {
+  state.defer?.resolve(false);
+};
+
+defineExpose({ open });
+</script>
