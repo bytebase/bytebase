a0c485bdc0ee90c31f6b51689614b5093b946a66
STEVEN <imrealleonardo@gmail.com>
2022-04-21 18:23:41 +0800

    feat: `sheet_organizer` api (#1130)
    * feat: `sheet_organizer` api
    
    * feat: add `upsert`
    
    * chore: update casbin csv
    
    * feat: update get shared and starred sheet api
    
    * chore: update casbin csv
    
    * chore: remove unuse find
    
    * chore: add compose function
    
    * chore: update method
    
    * chore: update test
    
    * Update api/sheet_organizer.go
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    
    * Update server/sheet.go
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    
    * Update server/sheet.go
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    
    * Update api/sheet.go
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    
    * Update api/sheet.go
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    
    * chore: update sheet composer
    
    * chore: fix casbin csv
    
    * chore: fix casbin csv
    
    * chore: move to store
    
    * Update store/sheet_organizer.go
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    
    * chore: update store function name
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    Former-commit-id: 62233676e7cfe38ece1ebdc7eeaa112ec62ea598 [formerly 0f4126d3a9b193741fd0e75e618ea64ff81aa3fa] [formerly 3d30315aba5668f8a9d088e0126333827d2eef78]
    Former-commit-id: 4566bd0602ad1c7db9260b407dd01e01b2779732
    Former-commit-id: acfbcc3fd944e1ba63c7bf9bb5f1a0c5b4d8195f

---
+++ store/sheet_organizer.go
@@ -0,0 +1,160 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"github.com/bytebase/bytebase/api"
+	"github.com/bytebase/bytebase/common"
+)
+
+// sheetOrganizerRaw is the store model for SheetOrganizer.
+type sheetOrganizerRaw struct {
+	ID int
+
+	// Related fields
+	SheetID     int
+	PrincipalID int
+	Starred     bool
+	Pinned      bool
+}
+
+// toSheetOrganizer creates an instance of SheetOrganizer based on the sheetOrganizerRaw.
+func (raw *sheetOrganizerRaw) toSheetOrganizer() *api.SheetOrganizer {
+	return &api.SheetOrganizer{
+		ID:          raw.ID,
+		SheetID:     raw.SheetID,
+		PrincipalID: raw.PrincipalID,
+		Starred:     raw.Starred,
+		Pinned:      raw.Pinned,
+	}
+}
+
+// UpsertSheetOrganizer upserts a new SheetOrganizer.
+func (s *Store) UpsertSheetOrganizer(ctx context.Context, upsert *api.SheetOrganizerUpsert) (*api.SheetOrganizer, error) {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, FormatError(err)
+	}
+	defer tx.PTx.Rollback()
+
+	sheetOrganizerRaw, err := upsertSheetOrganizerImpl(ctx, tx.PTx, upsert)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := tx.PTx.Commit(); err != nil {
+		return nil, FormatError(err)
+	}
+
+	sheetOrganizer := sheetOrganizerRaw.toSheetOrganizer()
+
+	return sheetOrganizer, nil
+}
+
+// FindSheetOrganizer retrieves a SheetOrganizer.
+func (s *Store) FindSheetOrganizer(ctx context.Context, find *api.SheetOrganizerFind) (*api.SheetOrganizer, error) {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, FormatError(err)
+	}
+	defer tx.PTx.Rollback()
+
+	sheetOrganizerRawlist, err := findSheetOrganizerListImpl(ctx, tx.PTx, find)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(sheetOrganizerRawlist) == 0 {
+		return nil, nil
+	} else if len(sheetOrganizerRawlist) > 1 {
+		return nil, &common.Error{Code: common.Conflict, Err: fmt.Errorf("found %d sheet organizer with filter %+v, expect 1. ", len(sheetOrganizerRawlist), find)}
+	}
+
+	sheetOrganizer := sheetOrganizerRawlist[0].toSheetOrganizer()
+
+	return sheetOrganizer, nil
+}
+
+func upsertSheetOrganizerImpl(ctx context.Context, tx *sql.Tx, upsert *api.SheetOrganizerUpsert) (*sheetOrganizerRaw, error) {
+	row, err := tx.QueryContext(ctx, `
+	  INSERT INTO sheet_organizer (
+			sheet_id,
+			principal_id,
+			starred,
+			pinned
+		)
+		VALUES ($1, $2, $3, $4)
+		ON CONFLICT(sheet_id, principal_id) DO UPDATE SET
+			starred = EXCLUDED.starred,
+			pinned = EXCLUDED.pinned
+		RETURNING id, sheet_id, principal_id, starred, pinned
+	`,
+		upsert.SheetID,
+		upsert.PrincipalID,
+		upsert.Starred,
+		upsert.Pinned,
+	)
+	if err != nil {
+		return nil, FormatError(err)
+	}
+	defer row.Close()
+
+	row.Next()
+	var sheetOrganizerRaw sheetOrganizerRaw
+	if err := row.Scan(
+		&sheetOrganizerRaw.ID,
+		&sheetOrganizerRaw.SheetID,
+		&sheetOrganizerRaw.PrincipalID,
+		&sheetOrganizerRaw.Starred,
+		&sheetOrganizerRaw.Pinned,
+	); err != nil {
+		return nil, FormatError(err)
+	}
+
+	return &sheetOrganizerRaw, nil
+}
+
+func findSheetOrganizerListImpl(ctx context.Context, tx *sql.Tx, find *api.SheetOrganizerFind) ([]*sheetOrganizerRaw, error) {
+	where, args := []string{"1 = 1"}, []interface{}{}
+	where, args = append(where, fmt.Sprintf("sheet_id = $%d", len(args)+1)), append(args, find.SheetID)
+	where, args = append(where, fmt.Sprintf("principal_id = $%d", len(args)+1)), append(args, find.PrincipalID)
+
+	rows, err := tx.QueryContext(ctx, `
+	SELECT
+		id,
+		sheet_id,
+		principal_id,
+		starred,
+		pinned
+	FROM sheet_organizer
+	WHERE `+strings.Join(where, " AND "),
+		args...,
+	)
+	if err != nil {
+		return nil, FormatError(err)
+	}
+	defer rows.Close()
+
+	var sheetOrganizerRawList []*sheetOrganizerRaw
+	for rows.Next() {
+		var sheetOrganizerRaw sheetOrganizerRaw
+		if err := rows.Scan(
+			&sheetOrganizerRaw.ID,
+			&sheetOrganizerRaw.SheetID,
+			&sheetOrganizerRaw.PrincipalID,
+			&sheetOrganizerRaw.Starred,
+			&sheetOrganizerRaw.Pinned,
+		); err != nil {
+			return nil, FormatError(err)
+		}
+		sheetOrganizerRawList = append(sheetOrganizerRawList, &sheetOrganizerRaw)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, FormatError(err)
+	}
+
+	return sheetOrganizerRawList, nil
+}
