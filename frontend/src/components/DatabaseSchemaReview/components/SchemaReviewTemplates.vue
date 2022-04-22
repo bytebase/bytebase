e7590038fe474cfd55bc7d2e443e7781b7f3d5af
ecmadao <wlec@outlook.com>
2022-04-22 16:58:41 +0800

    feat: implement the schema review in frontend (#1124)
    * feat: add InputWithTemplate component
    
    * chore: update component
    
    * fix: remove duplicate class
    
    * chore: extract variables
    
    * chore: remove the event listener on component unmounted
    
    * chore: update the comments
    
    * feat: guideline creation
    
    * feat: initial the guideline dashboard and detail page
    
    * chore: update guideline detail page
    
    * feat: allow edit guideline
    
    * chore: add notification & bugfix
    
    * chore: limit the environment
    
    * chore: update interface
    
    * chore: revert file
    
    * chore: update router guard
    
    * chore: code optimize
    
    * chore: support filter by db
    
    * chore: rename to schema review
    
    * chore: filter by rule level
    
    * chore: disable the rule select and remove
    
    * chore: add empty view
    
    * chore: add count for each filter item
    
    * chore: update delete button
    
    * chore: ux optimize
    
    * chore: ux improvement
    
    * chore: bugfix for animation
    
    * chore: update the environment
    
    * chore: show the selected env in preview
    
    * chore: update template selection
    
    * fix: typo
    
    * chore: ux optimize
    
    * chore: optimize
    
    * chore: fix for frontend dev
    
    * feat: add permission limit
    
    * chore: hide the entry
    
    * chore: update level badge
    
    * chore: clean code
    
    * chore: code optimize
    
    * chore: update ux
    
    * chore: update interface name
    
    * chore: update enum name
    
    * chore: update the variable name
    
    * chore: update schema rule engine
    
    * chore: update the template interface
    
    * chore: reset changes in instance table
    
    * chore: resolve comment
    
    * chore: add rule payload type
    
    * chore: resolve comment
    
    * chore: update type
    
    * chore: update payload interface
    
    * chore: update types
    
    * chore: resolve comments
    
    * chore: optimize the type
    
    * chore: update the type name
    
    * chore: update interface name
    
    * chore: rename rule id to rule type
    
    * chore: update interface for rule
    
    * chore: can only add one environment
    
    * chore: update localization
    
    * chore: ux optimize
    
    * chore: add comments
    
    * fix: environment id
    
    * chore: resolve comment
    
    * chore: address comment
    
    * chore: support archive and restore in the frontend
    
    * chore: update localization
    
    * chore: use array payload type
    
    * chore: update interface field name
    
    * chore: code optimize
    
    Former-commit-id: 40ac343930fc571d7f6f12886c6ec90992a3277c

---
+++ frontend/src/components/DatabaseSchemaReview/components/SchemaReviewTemplates.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <p class="textlabel" v-if="title">
+      {{ title }}
+      <span v-if="required" style="color: red">*</span>
+    </p>
+    <div
+      class="flex flex-col sm:flex-row justify-start items-center gap-x-10 gap-y-10 mt-4"
+    >
+      <div
+        v-for="(template, index) in templateList"
+        :key="template.name"
+        class="relative border border-gray-300 hover:bg-gray-100 rounded-lg p-6 transition-all flex flex-col justify-center items-center w-full sm:max-w-xs"
+        :class="
+          index == selectedTemplateIndex
+            ? 'bg-gray-100'
+            : 'bg-transparent cursor-pointer'
+        "
+        @click="$emit('select', index)"
+      >
+        <img class="h-24" :src="template.imagePath" alt="" />
+        <span class="text-lg lg:text-xl mt-4">{{ template.name }}</span>
+        <heroicons-solid:check-circle
+          v-if="index == selectedTemplateIndex"
+          class="w-7 h-7 text-gray-500 absolute top-3 left-3"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from "vue";
+import { SchemaReviewPolicyTemplate } from "../../../types/schemaSystem";
+
+const props = defineProps({
+  templateList: {
+    required: true,
+    type: Object as PropType<SchemaReviewPolicyTemplate[]>,
+  },
+  selectedTemplateIndex: {
+    required: false,
+    default: -1,
+    type: Number,
+  },
+  title: {
+    required: false,
+    default: "",
+    type: String,
+  },
+  required: {
+    required: true,
+    type: Boolean,
+  },
+});
+
+const emit = defineEmits(["select"]);
+</script>
---
+++ frontend/src/components/DatabaseSchemaReview/components/SchemaRuleConfig.vue
@@ -0,0 +1,178 @@
+<template>
+  <div>
+    <div
+      class="flex justify-center items-center py-4 px-2 group cursor-pointer hover:bg-gray-100"
+      :class="active ? 'bg-gray-100' : ''"
+      @click="$emit('activate', selectedRule.type)"
+    >
+      <heroicons-solid:chevron-right
+        class="w-5 h-5 transform transition-all"
+        :class="active ? 'rotate-90' : ''"
+      />
+
+      <div class="flex-1 flex flex-col ml-3">
+        <div class="flex mb-2 items-center space-x-2">
+          <h1 class="text-base font-semibold text-gray-900">
+            {{ selectedRule.type }}
+          </h1>
+          <BBBadge
+            :text="$t(`engine.${selectedRule.engine.toLowerCase()}`)"
+            :can-remove="false"
+          />
+          <SchemaRuleLevelBadge :level="selectedRule.level" />
+        </div>
+        <div class="text-sm text-gray-400">
+          {{ selectedRule.description }}
+        </div>
+      </div>
+    </div>
+    <div v-if="active" class="px-10 py-5 text-sm">
+      <div class="mb-7">
+        <p class="mb-3">{{ $t("schema-review-policy.error-level.name") }}</p>
+        <div class="flex gap-x-3">
+          <div
+            v-for="(level, index) in LEVEL_LIST"
+            :key="index"
+            class="flex items-center"
+          >
+            <input
+              :id="`level-${level}`"
+              :value="level"
+              type="radio"
+              :checked="level === selectedRule.level"
+              @input="emit('level-change', level)"
+              class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+            />
+            <label
+              :for="`level-${level}`"
+              class="ml-2 items-center text-sm text-gray-600"
+            >
+              {{
+                $t(`schema-review-policy.error-level.${level.toLowerCase()}`)
+              }}
+            </label>
+          </div>
+        </div>
+      </div>
+      <div
+        v-for="(config, index) in selectedRule.componentList"
+        :key="index"
+        class="mb-7"
+      >
+        <p class="mb-3">
+          {{ config.title }}
+        </p>
+        <input
+          v-if="config.payload.type == 'STRING'"
+          v-model="state.payload[index]"
+          type="text"
+          class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full border-gray-300 rounded-md"
+          :placeholder="config.payload.default"
+        />
+        <div
+          v-else-if="
+            config.payload.type == 'STRING_ARRAY' &&
+            Array.isArray(state.payload[index])
+          "
+        >
+          <div class="flex flex-wrap gap-4 mb-4">
+            <BBBadge
+              v-for="(val, i) in state.payload[index]"
+              :key="`${index}-${i}`"
+              :text="val"
+              @remove="() => removeFromList(index, val)"
+            />
+          </div>
+          <input
+            type="text"
+            pattern="[a-z]+"
+            class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full border-gray-300 rounded-md"
+            placeholder="Input the value then press enter to add"
+            @keyup.enter="(e) => pushToList(index, e)"
+          />
+        </div>
+        <InputWithTemplate
+          v-else-if="config.payload.type == 'TEMPLATE'"
+          :template-list="config.payload.templateList"
+          :value="getStringPayload(index)"
+          @change="(val) => (state.payload[index] = val)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, PropType, watch } from "vue";
+import {
+  LEVEL_LIST,
+  RuleTemplate,
+  RuleTemplatePayload,
+} from "../../../types/schemaSystem";
+
+type PayloadValueList = (string | string[])[];
+interface LocalState {
+  payload: PayloadValueList;
+}
+
+const initStatePayload = (
+  componentList: RuleTemplatePayload[] | undefined
+): PayloadValueList => {
+  return (componentList ?? []).reduce((res, component) => {
+    res.push(component.payload.value ?? component.payload.default);
+    return res;
+  }, [] as PayloadValueList);
+};
+
+const props = defineProps({
+  selectedRule: {
+    required: true,
+    type: Object as PropType<RuleTemplate>,
+  },
+  active: {
+    require: true,
+    type: Boolean,
+  },
+});
+
+const emit = defineEmits(["activate", "payload-change", "level-change"]);
+
+const state = reactive<LocalState>({
+  payload: initStatePayload(props.selectedRule.componentList),
+});
+
+watch(
+  () => state.payload,
+  (val) => emit("payload-change", val),
+  { deep: true }
+);
+
+const removeFromList = (i: number, val: any) => {
+  if (!Array.isArray(state.payload[i])) {
+    return;
+  }
+
+  const values = state.payload[i] as string[];
+  const index = values.indexOf(val);
+  if (index < 0) {
+    return;
+  }
+
+  state.payload[i] = [...values.slice(0, index), ...values.slice(index + 1)];
+};
+
+const pushToList = (i: number, e: any) => {
+  if (!Array.isArray(state.payload[i])) {
+    return;
+  }
+
+  const val = e.target.value.trim();
+  (state.payload[i] as string[]).push(val);
+
+  e.target.value = "";
+};
+
+const getStringPayload = (i: number): string => {
+  return state.payload[i] as string;
+};
+</script>
