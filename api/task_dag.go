9691ab7dd1c2d605d1de863bbdf3783d9bf9c687
p0ny <xz@bytebase.com>
2022-04-24 17:10:43 +0800

    feat(gh-ost): implement creation for issue type gh-ost migration non-tenant mode (#1134)
    * feat(gh-ost): implement creation for issue type gh-ost migration non-tenant mode
    
    * docs: add some comment
    
    * fix: remove unused fields
    
    * fix: fix nil pointer
    
    * fix: address comment
    
    * fix: use taskIndexDAG
    
    Former-commit-id: 78befc42665e290cd583c0c93cd6c13d2617379a

--- api/task_dag.go
+++ api/task_dag.go
@@ -18,6 +18,7 @@ type TaskDAG struct {
 
 // TaskDAGCreate is the API message to create TaskDAG.
 type TaskDAGCreate struct {
+	// Domain Specific fields
 	FromTaskID int
 	ToTaskID   int
 	Payload    string
@@ -26,4 +27,11 @@ type TaskDAGCreate struct {
 // TaskDAGFind is the API message to find TaskDAG.
 type TaskDAGFind struct {
 	ToTaskID int
+}
+
+// TaskIndexDAG describes task dependency relationship using array index to represent task.
+// It is needed because we don't know task id before insertion.
+type TaskIndexDAG struct {
+	FromIndex int
+	ToIndex   int
 }
