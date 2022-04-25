d08f37f035c14917f318462a35edc10d4e340dd6
qsliu <qsliu2017@outlook.com>
2022-04-25 17:37:19 +0800

    add more help info

--- scripts/install_bb.sh
+++ scripts/install_bb.sh
@@ -1,44 +1,66 @@
 #! /bin/sh
 set -u
 
-PREV_WD="$(pwd)"
-mkdir -p .bytebase && cd .bytebase
 abort() {
-    cd $PREV_WD && rm -rf .bytebase
     printf "%s\n" "$@" >&2
     exit 1
 }
 
-BB_VERSION="1.0.3"
-
-OS="$(uname -s)"
-if [[ "${OS}" != "Darwin" && "${OS}" != "Linux" ]]; then
-    abort "OS ${OS} is not support, bb is only supported on Linux and MacOS"
-fi
-
-ARCH=$(uname -m)
-if [[ "${ARCH}" == "amd64" || "${ARCH}" == "x86_64" ]]; then
-    ARCH="x86_64"
-elif [[ "${ARCH}" != "arm64" ]]; then
-    abort "Arch ${ARCH} is not support, bb is only supported on x86_64, amd64 and arm64"
-fi
-
-download_tar() {
-    curl -LO "https://github.com/bytebase/bytebase/releases/download/${BB_VERSION}/bytebase_${BB_VERSION}_${OS}_${ARCH}.tar.gz"
+uname_os() {
+    OS="$(uname -s)"
+    if [[ "${OS}" != "Darwin" && "${OS}" != "Linux" ]]; then
+        abort "OS ${OS} is not support, bb is only supported on Linux and MacOS"
+    fi
+    echo $OS
 }
 
-extract_tar() {
-    tar -xf "bytebase_${BB_VERSION}_${OS}_${ARCH}.tar.gz" "bb"
+uname_arch() {
+    ARCH=$(uname -m)
+    if [[ "${ARCH}" == "amd64" || "${ARCH}" == "x86_64" ]]; then
+        ARCH="x86_64"
+    elif [[ "${ARCH}" != "arm64" ]]; then
+        abort "Arch ${ARCH} is not support, bb is only supported on x86_64, amd64 and arm64"
+    fi
+    echo $ARCH
 }
 
-install_bb_binary() {
-    sudo mv "bb" "/usr/local/bin/bb"
-    echo "bb ${BB_VERSION} has been installed in /usr/local/bin"
+http_download() {
+    local_file=$1
+    source_url=$2
+
+    echo "Start downloading $source_url..."
+
+    code=$(curl -w '%{http_code}' -sL -o "$local_file" "$source_url")
+    if [ "$code" != "200" ]; then
+        abort "Fail to download $source_url, status code: $code"
+    fi
+
+    echo "Completed downloading $source_url"
+    return 0
 }
 
-download_tar
-extract_tar
-install_bb_binary
-cd $PREV_WD && rm -rf .bytebase
-bb version &>/dev/null
-exit $@
+execute() {
+    BB_VERSION="1.0.3"
+    OS="$(uname_os)"
+    ARCH="$(uname_arch)"
+
+    tmpdir=$(mktemp -d)
+    echo "Downloading tarball into ${tmpdir}"
+
+    http_download "${tmpdir}/bytebase_${BB_VERSION}_${OS}_${ARCH}.tar.gz" \
+        "https://github.com/bytebase/bytebase/releases/download/${BB_VERSION}/bytebase_${BB_VERSION}_${OS}_${ARCH}.tar.gz"
+
+    echo "Start extracting tarball..."
+    (cd "${tmpdir}" && tar -xf "bytebase_${BB_VERSION}_${OS}_${ARCH}.tar.gz" "bb")
+    echo "Completed extracting tarball"
+
+    echo "Start installing bb..."
+    install "${tmpdir}/bb" "/usr/local/bin"
+    echo "Installed bb to /usr/local/bin"
+
+    echo "Start cleaning up..."
+    rm -rf "${tmpdir}"
+    echo "Completed cleaning up"
+}
+
+execute
