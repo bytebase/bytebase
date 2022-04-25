5d825f3026149e3b654d5230e1f1ec817fee9411
ecmadao <wlec@outlook.com>
2022-04-25 11:15:21 +0800

    feat: implement the CRUD in the backend for the schema review policy (#1144)
    Former-commit-id: 840bb968c9603d36520cc527e88d9459ce2c9604

--- server/policy.go
+++ server/policy.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/bytebase/bytebase/api"
+	"github.com/bytebase/bytebase/common"
 	"github.com/google/jsonapi"
 	"github.com/labstack/echo/v4"
 )
@@ -13,18 +14,31 @@ import (
 // hasAccessToUpdatePolicy checks if user can access to policy control feature.
 // return nil if user has access.
 func (s *Server) hasAccessToUpsertPolicy(policyUpsert *api.PolicyUpsert) error {
+	// nil payload means user doesn't update the payload field
+	if policyUpsert.Payload == nil {
+		return nil
+	}
+
 	defaultPolicy, err := api.GetDefaultPolicy(policyUpsert.Type)
 	if err != nil {
 		return err
 	}
+
+	if defaultPolicy == *policyUpsert.Payload {
+		return nil
+	}
 	switch policyUpsert.Type {
 	case api.PolicyTypePipelineApproval:
-		if policyUpsert.Payload != defaultPolicy && !s.feature(api.FeatureApprovalPolicy) {
+		if !s.feature(api.FeatureApprovalPolicy) {
 			return fmt.Errorf(api.FeatureApprovalPolicy.AccessErrorMessage())
 		}
 	case api.PolicyTypeBackupPlan:
-		if policyUpsert.Payload != defaultPolicy && !s.feature(api.FeatureBackupPolicy) {
+		if !s.feature(api.FeatureBackupPolicy) {
 			return fmt.Errorf(api.FeatureBackupPolicy.AccessErrorMessage())
+		}
+	case api.PolicyTypeSchemaReview:
+		if !s.feature(api.FeatureSchemaReviewPolicy) {
+			return fmt.Errorf(api.FeatureSchemaReviewPolicy.AccessErrorMessage())
 		}
 	}
 	return nil
@@ -57,6 +71,9 @@ func (s *Server) registerPolicyRoutes(g *echo.Group) {
 
 		policy, err := s.store.UpsertPolicy(ctx, policyUpsert)
 		if err != nil {
+			if common.ErrorCode(err) == common.Invalid {
+				return echo.NewHTTPError(http.StatusBadRequest, err.Error()).SetInternal(err)
+			}
 			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to set policy for type %q", pType)).SetInternal(err)
 		}
 
@@ -64,6 +81,31 @@ func (s *Server) registerPolicyRoutes(g *echo.Group) {
 		if err := jsonapi.MarshalPayload(c.Response().Writer, policy); err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to marshal create set policy response").SetInternal(err)
 		}
+		return nil
+	})
+
+	g.DELETE("/policy/environment/:environmentID", func(c echo.Context) error {
+		environmentID, err := strconv.Atoi(c.Param("environmentID"))
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("ID is not a number: %s", c.Param("environmentID"))).SetInternal(err)
+		}
+
+		policyDelete := &api.PolicyDelete{
+			EnvironmentID: environmentID,
+			DeleterID:     c.Get(getPrincipalIDContextKey()).(int),
+			Type:          api.PolicyType(c.QueryParam("type")),
+		}
+
+		ctx := c.Request().Context()
+		if err := s.store.DeletePolicy(ctx, policyDelete); err != nil {
+			if common.ErrorCode(err) == common.Invalid {
+				return echo.NewHTTPError(http.StatusBadRequest, err.Error()).SetInternal(err)
+			}
+			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to delete policy by environment ID %d", environmentID)).SetInternal(err)
+		}
+
+		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
+		c.Response().WriteHeader(http.StatusOK)
 		return nil
 	})
 
@@ -89,6 +131,29 @@ func (s *Server) registerPolicyRoutes(g *echo.Group) {
 		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
 		if err := jsonapi.MarshalPayload(c.Response().Writer, policy); err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to marshal get policy response: %v", pType)).SetInternal(err)
+		}
+		return nil
+	})
+
+	g.GET("/policy", func(c echo.Context) error {
+		pType := api.PolicyType(c.QueryParam("type"))
+		if err := api.ValidatePolicy(pType, ""); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Invalid policy type: %q", pType)).SetInternal(err)
+		}
+
+		policyFind := &api.PolicyFind{
+			Type: &pType,
+		}
+
+		ctx := c.Request().Context()
+		policyList, err := s.store.ListPolicy(ctx, policyFind)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to list policy for type %q", pType)).SetInternal(err)
+		}
+
+		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
+		if err := jsonapi.MarshalPayload(c.Response().Writer, policyList); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to marshal list policy response: %v", pType)).SetInternal(err)
 		}
 		return nil
 	})
