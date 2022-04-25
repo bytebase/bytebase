1ee710e9c0ead6f8ee9f4a4ecc025eccff5d2021
ecmadao <wlec@outlook.com>
2022-04-25 15:23:00 +0800

    chore: review policy update (#1156)
    * feat: add InputWithTemplate component
    
    * chore: update component
    
    * fix: remove duplicate class
    
    * chore: extract variables
    
    * chore: remove the event listener on component unmounted
    
    * chore: update the comments
    
    * feat: guideline creation
    
    * feat: initial the guideline dashboard and detail page
    
    * chore: update guideline detail page
    
    * feat: allow edit guideline
    
    * chore: add notification & bugfix
    
    * chore: limit the environment
    
    * chore: update interface
    
    * chore: revert file
    
    * chore: update router guard
    
    * chore: code optimize
    
    * chore: support filter by db
    
    * chore: rename to schema review
    
    * chore: filter by rule level
    
    * chore: disable the rule select and remove
    
    * chore: add empty view
    
    * chore: add count for each filter item
    
    * chore: update delete button
    
    * chore: ux optimize
    
    * chore: ux improvement
    
    * chore: bugfix for animation
    
    * chore: update the environment
    
    * chore: show the selected env in preview
    
    * chore: update template selection
    
    * fix: typo
    
    * chore: ux optimize
    
    * chore: optimize
    
    * chore: fix for frontend dev
    
    * feat: add permission limit
    
    * chore: hide the entry
    
    * chore: update level badge
    
    * chore: clean code
    
    * chore: code optimize
    
    * chore: update ux
    
    * chore: update interface name
    
    * chore: update enum name
    
    * chore: update the variable name
    
    * chore: update schema rule engine
    
    * chore: update the template interface
    
    * chore: reset changes in instance table
    
    * chore: resolve comment
    
    * chore: add rule payload type
    
    * chore: resolve comment
    
    * chore: update type
    
    * chore: update payload interface
    
    * chore: update types
    
    * chore: resolve comments
    
    * chore: optimize the type
    
    * chore: update the type name
    
    * chore: update interface name
    
    * chore: rename rule id to rule type
    
    * chore: update interface for rule
    
    * chore: can only add one environment
    
    * chore: update localization
    
    * chore: ux optimize
    
    * chore: add comments
    
    * fix: environment id
    
    * chore: resolve comment
    
    * chore: address comment
    
    * chore: support archive and restore in the frontend
    
    * chore: update localization
    
    * chore: use array payload type
    
    * chore: update interface field name
    
    * chore: code optimize
    
    * feat: implement the CRUD for schema review policy
    
    * chore: resolve comments
    
    * chore: resolve comments
    
    * chore: update endpoints
    
    * chore: update casbin
    
    * chore: resolve comment
    
    * chore: resolve comment
    
    * chore: resolve comments
    
    * chore: update hasAccessToUpsertPolicy
    
    * chore: localization for rules
    
    * chore: add foreign naming rule
    
    * chore: update the rule type in the backend
    
    * chore: rename RuleTemplatePayload to RuleConfigComponent
    
    Former-commit-id: 267e3978b54f13cf00652db195c5221170f26ce1

--- frontend/src/types/schemaSystem.ts
+++ frontend/src/types/schemaSystem.ts
@@ -1,3 +1,4 @@
+import { useI18n } from "vue-i18n";
 import { PolicyId } from "./id";
 import { Principal } from "./principal";
 import { RowStatus } from "./common";
@@ -7,7 +8,12 @@ import { Environment } from "./environment";
 export type SchemaRuleEngineType = "MYSQL" | "COMMON";
 
 // The category type for rule template
-export type CategoryType = "ENGINE" | "NAMING" | "QUERY" | "TABLE" | "COLUMN";
+export type CategoryType =
+  | "ENGINE"
+  | "NAMING"
+  | "STATEMENT"
+  | "TABLE"
+  | "COLUMN";
 
 // The rule level
 export enum RuleLevel {
@@ -47,9 +53,9 @@ interface TemplatePayload {
   value?: string;
 }
 
-// RuleTemplatePayload is the rule configuration options and default value.
+// RuleConfigComponent is the rule configuration options and default value.
 // Used by the frontend.
-export interface RuleTemplatePayload {
+export interface RuleConfigComponent {
   title: string;
   description: string;
   payload: StringPayload | TemplatePayload | StringArrayPayload;
@@ -63,12 +69,13 @@ export type RuleType =
   | "naming.column"
   | "naming.index.pk"
   | "naming.index.uk"
+  | "naming.index.fk"
   | "naming.index.idx"
   | "column.required"
   | "column.no-null"
-  | "query.select.no-select-all"
-  | "query.where.require"
-  | "query.where.no-leading-wildcard-like";
+  | "statement.select.no-select-all"
+  | "statement.where.require"
+  | "statement.where.no-leading-wildcard-like";
 
 // The naming format rule payload.
 // Used by the backend.
@@ -91,7 +98,6 @@ export interface SchemaPolicyRule {
 }
 
 // The API for schema review policy in backend.
-// TODO: just use the existed Policy entity
 export interface DatabaseSchemaReviewPolicy {
   id: PolicyId;
 
@@ -113,28 +119,26 @@ export interface RuleTemplate {
   type: RuleType;
   category: CategoryType;
   engine: SchemaRuleEngineType;
-  description: string;
-  // TODO: rename componentList to RuleConfigComponent
-  componentList: RuleTemplatePayload[];
+  componentList: RuleConfigComponent[];
   level: RuleLevel;
 }
 
 // SchemaReviewPolicyTemplate is the rule template set
 export interface SchemaReviewPolicyTemplate {
-  name: string;
+  title: string;
   imagePath: string;
   ruleList: RuleTemplate[];
 }
 
 // RULE_TEMPLATE_PAYLOAD_MAP is the relationship mapping for the rule type and payload.
 // Used by frontend to get different rule payload configurations.
-export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
+export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleConfigComponent[]> =
   new Map([
     [
       "naming.table",
       [
         {
-          title: "Table name format",
+          title: "table-name-format",
           description: "",
           payload: {
             type: "STRING",
@@ -147,7 +151,7 @@ export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
       "naming.column",
       [
         {
-          title: "Column name format",
+          title: "column-name-format",
           description: "",
           payload: {
             type: "STRING",
@@ -160,7 +164,7 @@ export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
       "naming.index.pk",
       [
         {
-          title: "Primary key name format",
+          title: "pk-name-format",
           description: "",
           payload: {
             type: "TEMPLATE",
@@ -168,11 +172,13 @@ export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
             templateList: [
               {
                 id: "table",
-                description: "The table name",
+                description:
+                  "schema-review-policy.payload-config.template.table-name",
               },
               {
                 id: "column_list",
-                description: "Index column names, joined by _",
+                description:
+                  "schema-review-policy.payload-config.template.column-list",
               },
             ],
           },
@@ -183,7 +189,7 @@ export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
       "naming.index.uk",
       [
         {
-          title: "Unique key name format",
+          title: "uk-name-format",
           description: "",
           payload: {
             type: "TEMPLATE",
@@ -191,11 +197,49 @@ export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
             templateList: [
               {
                 id: "table",
-                description: "The table name",
+                description:
+                  "schema-review-policy.payload-config.template.table-name",
               },
               {
                 id: "column_list",
-                description: "Index column names, joined by _",
+                description:
+                  "schema-review-policy.payload-config.template.column-list",
+              },
+            ],
+          },
+        },
+      ],
+    ],
+    [
+      "naming.index.fk",
+      [
+        {
+          title: "fk-name-format",
+          description: "",
+          payload: {
+            type: "TEMPLATE",
+            default:
+              "^fk_{{referencing_table}}_{{referencing_column}}_{{referenced_table}}_{{referenced_column}}$",
+            templateList: [
+              {
+                id: "referencing_table",
+                description:
+                  "schema-review-policy.payload-config.template.referencing-table",
+              },
+              {
+                id: "referencing_column",
+                description:
+                  "schema-review-policy.payload-config.template.referencing-column",
+              },
+              {
+                id: "referenced_table",
+                description:
+                  "schema-review-policy.payload-config.template.referenced-table",
+              },
+              {
+                id: "referenced_column",
+                description:
+                  "schema-review-policy.payload-config.template.referenced-column",
               },
             ],
           },
@@ -206,7 +250,7 @@ export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
       "naming.index.idx",
       [
         {
-          title: "Index name format",
+          title: "idx-name-format",
           description: "",
           payload: {
             type: "TEMPLATE",
@@ -214,11 +258,13 @@ export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
             templateList: [
               {
                 id: "table",
-                description: "The table name",
+                description:
+                  "schema-review-policy.payload-config.template.table-name",
               },
               {
                 id: "column_list",
-                description: "Index column names, joined by _",
+                description:
+                  "schema-review-policy.payload-config.template.column-list",
               },
             ],
           },
@@ -229,7 +275,7 @@ export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
       "column.required",
       [
         {
-          title: "Required column names",
+          title: "required-column",
           description: "",
           payload: {
             type: "STRING_ARRAY",
@@ -247,13 +293,11 @@ export const RULE_TEMPLATE_PAYLOAD_MAP: Map<RuleType, RuleTemplatePayload[]> =
   ]);
 
 // ruleTemplateList stores the default value for each rule template
-// TODO: i18n
 export const ruleTemplateList: RuleTemplate[] = [
   {
     type: "engine.mysql.use-innodb",
     category: "ENGINE",
     engine: "MYSQL",
-    description: "Require InnoDB as the storage engine.",
     level: RuleLevel.ERROR,
     componentList: [],
   },
@@ -261,7 +305,6 @@ export const ruleTemplateList: RuleTemplate[] = [
     type: "table.require-pk",
     category: "TABLE",
     engine: "COMMON",
-    description: "Require the table to have a primary key.",
     level: RuleLevel.ERROR,
     componentList: [],
   },
@@ -269,7 +312,6 @@ export const ruleTemplateList: RuleTemplate[] = [
     type: "naming.table",
     category: "NAMING",
     engine: "COMMON",
-    description: "Enforce the table name format. Default snake_lower_case.",
     componentList: RULE_TEMPLATE_PAYLOAD_MAP.get("naming.table") ?? [],
     level: RuleLevel.ERROR,
   },
@@ -277,7 +319,6 @@ export const ruleTemplateList: RuleTemplate[] = [
     type: "naming.column",
     category: "NAMING",
     engine: "COMMON",
-    description: "Enforce the column name format. Default snake_lower_case.",
     componentList: RULE_TEMPLATE_PAYLOAD_MAP.get("naming.column") ?? [],
     level: RuleLevel.ERROR,
   },
@@ -285,7 +326,6 @@ export const ruleTemplateList: RuleTemplate[] = [
     type: "naming.index.pk",
     category: "NAMING",
     engine: "COMMON",
-    description: "Enforce the primary key name format.",
     componentList: RULE_TEMPLATE_PAYLOAD_MAP.get("naming.index.pk") ?? [],
     level: RuleLevel.ERROR,
   },
@@ -293,15 +333,20 @@ export const ruleTemplateList: RuleTemplate[] = [
     type: "naming.index.uk",
     category: "NAMING",
     engine: "COMMON",
-    description: "Enforce the unique key name format.",
     componentList: RULE_TEMPLATE_PAYLOAD_MAP.get("naming.index.uk") ?? [],
+    level: RuleLevel.ERROR,
+  },
+  {
+    type: "naming.index.fk",
+    category: "NAMING",
+    engine: "COMMON",
+    componentList: RULE_TEMPLATE_PAYLOAD_MAP.get("naming.index.fk") ?? [],
     level: RuleLevel.ERROR,
   },
   {
     type: "naming.index.idx",
     category: "NAMING",
     engine: "COMMON",
-    description: "Enforce the index name format.",
     componentList: RULE_TEMPLATE_PAYLOAD_MAP.get("naming.index.idx") ?? [],
     level: RuleLevel.ERROR,
   },
@@ -309,7 +354,6 @@ export const ruleTemplateList: RuleTemplate[] = [
     type: "column.required",
     category: "COLUMN",
     engine: "COMMON",
-    description: "Enforce the required columns in each table.",
     componentList: RULE_TEMPLATE_PAYLOAD_MAP.get("column.required") ?? [],
     level: RuleLevel.ERROR,
   },
@@ -317,32 +361,27 @@ export const ruleTemplateList: RuleTemplate[] = [
     type: "column.no-null",
     category: "COLUMN",
     engine: "COMMON",
-    description: "Columns cannot have NULL value.",
     level: RuleLevel.ERROR,
     componentList: [],
   },
   {
-    type: "query.select.no-select-all",
-    category: "QUERY",
+    type: "statement.select.no-select-all",
+    category: "STATEMENT",
     engine: "COMMON",
-    description: "Disallow 'SELECT *'.",
     level: RuleLevel.ERROR,
     componentList: [],
   },
   {
-    type: "query.where.require",
-    category: "QUERY",
+    type: "statement.where.require",
+    category: "STATEMENT",
     engine: "COMMON",
-    description: "Require 'WHERE' clause.",
     level: RuleLevel.ERROR,
     componentList: [],
   },
   {
-    type: "query.where.no-leading-wildcard-like",
-    category: "QUERY",
+    type: "statement.where.no-leading-wildcard-like",
+    category: "STATEMENT",
     engine: "COMMON",
-    description:
-      "Disallow leading '%' in LIKE, e.g. LIKE foo = '%x' is not allowed.",
     level: RuleLevel.ERROR,
     componentList: [],
   },
@@ -360,7 +399,7 @@ export const convertToCategoryList = (
   const categoryOrder: Map<CategoryType, number> = new Map([
     ["ENGINE", 5],
     ["NAMING", 4],
-    ["QUERY", 3],
+    ["STATEMENT", 3],
     ["TABLE", 2],
     ["COLUMN", 1],
   ]);
@@ -422,6 +461,7 @@ export const convertPolicyRuleToRuleTemplate = (
     case "naming.index.idx":
     case "naming.index.pk":
     case "naming.index.uk":
+    case "naming.index.fk":
       const templateComponent = ruleTemplate.componentList[0];
       const indexRulePayload = {
         ...templateComponent.payload,
@@ -482,6 +522,7 @@ export const convertRuleTemplateToPolicyRule = (
     case "naming.index.idx":
     case "naming.index.pk":
     case "naming.index.uk":
+    case "naming.index.fk":
       const templatePayload = rule.componentList[0].payload as TemplatePayload;
       return {
         ...base,
@@ -501,4 +542,19 @@ export const convertRuleTemplateToPolicyRule = (
   }
 
   throw new Error(`Invalid rule ${rule.type}`);
+};
+
+export const getRuleLocalization = (
+  type: RuleType
+): { title: string; description: string } => {
+  const { t } = useI18n();
+  const key = type.split(".").join("-");
+
+  const title = t(`schema-review-policy.rule.${key}.title`);
+  const description = t(`schema-review-policy.rule.${key}.description`);
+
+  return {
+    title,
+    description,
+  };
 };
