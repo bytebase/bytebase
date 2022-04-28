0bf9494aa0527dee35390994c3c210d9a6c9ff46
Yilong Li <liyilongko@gmail.com>
2022-04-28 17:24:27 +0800

    refactor: decouple api/service/store packages for Stage (#1185)
    * refactor: decouple api/service/store packages for Stage
    
    * chore: remove unused function
    
    Former-commit-id: c30ef3fd7664df9e796f18f089848f59c6f874d9 [formerly ba08a6816e42dd3e50e4160691dca67df742818c] [formerly 359a6a793e39773ae4253bbe13aba3f1dcc866d9]
    Former-commit-id: 2887a7b8a6793c653639b5f81e903b4d96d44bb1
    Former-commit-id: 0cffcae6c22a8e1fc7325a93570a366710036218

--- server/stage.go
+++ server/stage.go
@@ -6,61 +6,6 @@ import (
 	"github.com/bytebase/bytebase/api"
 )
 
-func (s *Server) composeStageListByPipelineID(ctx context.Context, pipelineID int) ([]*api.Stage, error) {
-	stageFind := &api.StageFind{
-		PipelineID: &pipelineID,
-	}
-	stageRawList, err := s.StageService.FindStageList(ctx, stageFind)
-	if err != nil {
-		return nil, err
-	}
-
-	var stageList []*api.Stage
-	for _, stageRaw := range stageRawList {
-		stage, err := s.composeStageRelationship(ctx, stageRaw)
-		if err != nil {
-			return nil, err
-		}
-		stageList = append(stageList, stage)
-	}
-
-	return stageList, nil
-}
-
-func (s *Server) composeStageRelationship(ctx context.Context, raw *api.StageRaw) (*api.Stage, error) {
-	stage := raw.ToStage()
-
-	creator, err := s.store.GetPrincipalByID(ctx, stage.CreatorID)
-	if err != nil {
-		return nil, err
-	}
-	stage.Creator = creator
-
-	updater, err := s.store.GetPrincipalByID(ctx, stage.UpdaterID)
-	if err != nil {
-		return nil, err
-	}
-	stage.Updater = updater
-
-	env, err := s.store.GetEnvironmentByID(ctx, stage.EnvironmentID)
-	if err != nil {
-		return nil, err
-	}
-	stage.Environment = env
-
-	taskFind := &api.TaskFind{
-		PipelineID: &stage.PipelineID,
-		StageID:    &stage.ID,
-	}
-	taskList, err := s.store.FindTask(ctx, taskFind, true)
-	if err != nil {
-		return nil, err
-	}
-	stage.TaskList = taskList
-
-	return stage, nil
-}
-
 // TODO(dragonly): remove this hack
 func (s *Server) composeStageRelationshipValidateOnly(ctx context.Context, stage *api.Stage) error {
 	var err error
