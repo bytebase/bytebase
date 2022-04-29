dc108cc9c4dd49317b9f3070b058b5cfdc770c97
rebelice <yangrebelice@gmail.com>
2022-04-29 11:07:15 +0800

    feat: generate TaskCheck by the corresponding SchemaReviewPolicy (#1169)
    * feat: generate TaskChecks by the corresponding policy
    
    * fix: fix format
    
    * fix: remove dev log
    
    * chore: use debug log for not support rule
    
    * fix: typo
    
    Co-authored-by: tianzhou <t@bytebase.com>
    
    * fix: typo
    
    Co-authored-by: tianzhou <t@bytebase.com>
    
    * refactor: create only one TaskCheckRun for one schema review policy
    
    * chore: refine comments and add success content for advisor
    
    * fix: address comments
    
    * fix: fix test
    
    * chore: fix typo
    
    * fix: address comments
    
    * fix: address comments
    
    * fix: address comments
    
    * refactor: use policy ID instead of environment ID
    
    Co-authored-by: tianzhou <t@bytebase.com>
    Former-commit-id: 1a2e7507cf8285c3fd54aa6451d3777bd62518b0 [formerly 6edb9b2f601d668348355620095420262e68fd9f] [formerly d7cac9bb1efd2aab8c032db2dedbd7f3d1e24a55]
    Former-commit-id: b1a3f6976f80787c1e156ae17711b33775ad50aa
    Former-commit-id: bca8a9eca96a28d475ea84ecd5a6276e86b3d30b

---
+++ server/task_check_executor_statement_advisor_composite.go
@@ -0,0 +1,124 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/bytebase/bytebase/api"
+	"github.com/bytebase/bytebase/common"
+	"github.com/bytebase/bytebase/plugin/advisor"
+	"github.com/bytebase/bytebase/plugin/db"
+	"go.uber.org/zap"
+)
+
+// Schema review policy consists of a list of schema review rules.
+// There is such a logical mapping in bytebase backend:
+//   1. One schema review policy maps a TaskCheckRun.
+//   2. Each schema reivew rule type maps an advisor.Type.
+//   3. Each [db.Type][AdvisorType] maps an advisor.
+//
+// How to add a schema review rule:
+//   1. Implement an advisor.(plugin/xxx)
+//   2. Register this advisor in map[db.Type][AdvisorType].(plugin/advisor.go)
+//   3. Map SchemaReviewRuleType to advisor.Type in getAdvisorTypeByRule(current file).
+
+// NewTaskCheckStatementAdvisorCompositeExecutor creates a task check statement advisor composite executor.
+func NewTaskCheckStatementAdvisorCompositeExecutor(logger *zap.Logger) TaskCheckExecutor {
+	return &TaskCheckStatementAdvisorCompositeExecutor{
+		l: logger,
+	}
+}
+
+// TaskCheckStatementAdvisorCompositeExecutor is the task check statement advisor composite executor with has sub-advisor.
+type TaskCheckStatementAdvisorCompositeExecutor struct {
+	l *zap.Logger
+}
+
+// Run will run the task check statement advisor composite executor once, and run its sub-advisor one-by-one.
+func (exec *TaskCheckStatementAdvisorCompositeExecutor) Run(ctx context.Context, server *Server, taskCheckRun *api.TaskCheckRun) (result []api.TaskCheckResult, err error) {
+	if taskCheckRun.Type != api.TaskCheckDatabaseStatementAdvise {
+		return nil, common.Errorf(common.Invalid, fmt.Errorf("invalid check statement advisor composite type: %v", taskCheckRun.Type))
+	}
+	if !server.feature(api.FeatureSchemaReviewPolicy) {
+		return nil, common.Errorf(common.NotAuthorized, fmt.Errorf(api.FeatureSchemaReviewPolicy.AccessErrorMessage()))
+	}
+
+	payload := &api.TaskCheckDatabaseStatementAdvisePayload{}
+	if err := json.Unmarshal([]byte(taskCheckRun.Payload), payload); err != nil {
+		return nil, common.Errorf(common.Invalid, fmt.Errorf("invalid check statement advise payload: %w", err))
+	}
+
+	policy, err := server.store.GetSchemaReviewPolicyByID(ctx, payload.PolicyID)
+	if err != nil {
+		return nil, common.Errorf(common.Internal, fmt.Errorf("failed to get schema review policy: %w", err))
+	}
+
+	result = []api.TaskCheckResult{}
+	for _, rule := range policy.RuleList {
+		if rule.Level == api.SchemaRuleLevelDisabled {
+			continue
+		}
+		advisorType, err := getAdvisorTypeByRule(rule.Type, payload.DbType)
+		if err != nil {
+			exec.l.Debug("not supported rule", zap.Error(err))
+			continue
+		}
+		adviceList, err := advisor.Check(
+			payload.DbType,
+			advisorType,
+			advisor.Context{
+				Logger:    exec.l,
+				Charset:   payload.Charset,
+				Collation: payload.Collation,
+				Rule:      rule,
+			},
+			payload.Statement,
+		)
+		if err != nil {
+			return nil, common.Errorf(common.Internal, fmt.Errorf("failed to check statement: %w", err))
+		}
+
+		for _, advice := range adviceList {
+			status := api.TaskCheckStatusSuccess
+			switch advice.Status {
+			case advisor.Success:
+				continue
+			case advisor.Warn:
+				status = api.TaskCheckStatusWarn
+			case advisor.Error:
+				status = api.TaskCheckStatusError
+			}
+
+			result = append(result, api.TaskCheckResult{
+				Status:  status,
+				Code:    advice.Code,
+				Title:   advice.Title,
+				Content: advice.Content,
+			})
+
+		}
+	}
+	if len(result) == 0 {
+		result = append(result, api.TaskCheckResult{
+			Status:  api.TaskCheckStatusSuccess,
+			Code:    common.Ok,
+			Title:   "OK",
+			Content: "",
+		})
+	}
+	return result, nil
+
+}
+
+func getAdvisorTypeByRule(ruleType api.SchemaReviewRuleType, engine db.Type) (advisor.Type, error) {
+	switch ruleType {
+	case api.SchemaRuleStatementRequireWhere:
+		switch engine {
+		case db.MySQL, db.TiDB:
+			return advisor.MySQLWhereRequirement, nil
+		}
+		return advisor.Fake, fmt.Errorf("unknown schema review rule type %v for %v", ruleType, engine)
+	}
+	return advisor.Fake, fmt.Errorf("unknown schema review rule type %v for %v", ruleType, engine)
+}
