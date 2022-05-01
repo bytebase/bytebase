9c739f1efe6935f6c0df2ea9fb06b8afec03f348
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-05-01 13:45:45 +0800

    chore: move profile to server package (#1201)
    Former-commit-id: 85a043d0a69e92d184ba728efe6878cc5c2f247e

--- bin/server/cmd/profile_release.go
+++ bin/server/cmd/profile_release.go
@@ -8,20 +8,21 @@ import (
 	"time"
 
 	"github.com/bytebase/bytebase/common"
+	"github.com/bytebase/bytebase/server"
 )
 
-func activeProfile(dataDir string, port, datastorePort int, isDemo bool) Profile {
+func activeProfile(dataDir string, port, datastorePort int, isDemo bool) server.Profile {
 	demoDataDir := ""
 	if isDemo {
 		demoDataDir = fmt.Sprintf("demo/%s", common.ReleaseModeProd)
 	}
-	return Profile{
-		mode:                 common.ReleaseModeProd,
-		port:                 port,
-		datastorePort:        datastorePort,
-		pgUser:               "bb",
-		dataDir:              dataDir,
-		demoDataDir:          demoDataDir,
-		backupRunnerInterval: 10 * time.Minute,
+	return server.Profile{
+		Mode:                 common.ReleaseModeProd,
+		Port:                 port,
+		DatastorePort:        datastorePort,
+		PgUser:               "bb",
+		DataDir:              dataDir,
+		DemoDataDir:          demoDataDir,
+		BackupRunnerInterval: 10 * time.Minute,
 	}
 }
