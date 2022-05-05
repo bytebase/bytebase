2467111bc6ab0a11cd0812ca9cb9a667a51a13e5
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-05 18:14:59 +0800

    refactor: boot logic (#1203)
    * refactor: add comment of check flags stage
    
    * refactor: wrap command line config
    
    * refactor: move component to metadb folder
    
    * refactor: add more fields in Profile, add a profile filed in server
    
    * refactor: remove main
    
    * refactor: gracefully shutdown when encounter error in NewServer() by using defer
    
    * refactor: remove unuse comment
    
    * refactor: remove extra arguments in XXXProfile() to keep consistency(e.g.: using flags)
    
    * chore: rename metadb/component.go -> component/metadb.go
    
    * refactor: fix comment
    
    * fix: using flags.host in TestProfile
    
    * fix: pass ctx to server.ShutDown()
    
    * refactor: pass value of profile instead of ref
    
    * refactor: remove greeting banner in profile
    
    * refactor: move metadb.go to store package
    
    * refactor: using prof instead of s.prof to keep consistency
    
    Former-commit-id: caa1f1fbcf140533542df72183a6dcff88f4da2b
    Former-commit-id: 8a287abdb5f1ff223e3f81693f00c23cc2509472

--- bin/server/cmd/root.go
+++ bin/server/cmd/root.go
@@ -9,13 +9,9 @@ import (
 	"path/filepath"
 	"strings"
 	"syscall"
-	"time"
 
-	"github.com/bytebase/bytebase/api"
 	"github.com/bytebase/bytebase/common"
-	enterprise "github.com/bytebase/bytebase/enterprise/service"
 	"github.com/bytebase/bytebase/server"
-	"github.com/bytebase/bytebase/store"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -41,8 +37,6 @@ import (
 
 // -----------------------------------Global constant BEGIN----------------------------------------
 const (
-	// secretLength is the length for the secret used to sign the JWT auth token
-	secretLength = 32
 
 	// greetingBanner is the greeting banner.
 	// http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Bytebase
@@ -75,32 +69,33 @@ ________________________________________________________________________________
 
 // -----------------------------------Command Line Config BEGIN------------------------------------
 var (
-	// Used for flags.
-	host         string
-	port         int
-	frontendHost string
-	frontendPort int
-	dataDir      string
-	// When we are running in readonly mode:
-	// - The data file will be opened in readonly mode, no applicable migration or seeding will be applied.
-	// - Requests other than GET will be rejected
-	// - Any operations involving mutation will not start (e.g. Background schema syncer, task scheduler)
-	readonly bool
-	demo     bool
-	debug    bool
-	// pgURL must follow PostgreSQL connection URIs pattern.
-	// https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-	pgURL string
-
+	flags struct {
+		// Used for bytebase command line config
+		host         string
+		port         int
+		frontendHost string
+		frontendPort int
+		dataDir      string
+		// When we are running in readonly mode:
+		// - The data file will be opened in readonly mode, no applicable migration or seeding will be applied.
+		// - Requests other than GET will be rejected
+		// - Any operations involving mutation will not start (e.g. Background schema syncer, task scheduler)
+		readonly bool
+		demo     bool
+		debug    bool
+		// pgURL must follow PostgreSQL connection URIs pattern.
+		// https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+		pgURL string
+	}
 	rootCmd = &cobra.Command{
 		Use:   "bytebase",
 		Short: "Bytebase is a database schema change and version control tool",
 		Run: func(_ *cobra.Command, _ []string) {
-			if frontendHost == "" {
-				frontendHost = host
+			if flags.frontendHost == "" {
+				flags.frontendHost = flags.host
 			}
-			if frontendPort == 0 {
-				frontendPort = port
+			if flags.frontendPort == 0 {
+				flags.frontendPort = flags.port
 			}
 
 			start()
@@ -116,67 +111,39 @@ func Execute() error {
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringVar(&host, "host", "http://localhost", "host where Bytebase backend is accessed from, must start with http:// or https://. This is used by Bytebase to create the webhook callback endpoint for VCS integration")
-	rootCmd.PersistentFlags().IntVar(&port, "port", 80, "port where Bytebase backend is accessed from. This is also used by Bytebase to create the webhook callback endpoint for VCS integration")
-	rootCmd.PersistentFlags().StringVar(&frontendHost, "frontend-host", "", "host where Bytebase frontend is accessed from, must start with http:// or https://. This is used by Bytebase to compose the frontend link when posting the webhook event. Default is the same as --host")
-	rootCmd.PersistentFlags().IntVar(&frontendPort, "frontend-port", 0, "port where Bytebase frontend is accessed from. This is used by Bytebase to compose the frontend link when posting the webhook event. Default is the same as --port")
-	rootCmd.PersistentFlags().StringVar(&dataDir, "data", ".", "directory where Bytebase stores data. If relative path is supplied, then the path is relative to the directory where bytebase is under")
-	rootCmd.PersistentFlags().BoolVar(&readonly, "readonly", false, "whether to run in read-only mode")
-	rootCmd.PersistentFlags().BoolVar(&demo, "demo", false, "whether to run using demo data")
-	rootCmd.PersistentFlags().BoolVar(&debug, "debug", false, "whether to enable debug level logging")
+	rootCmd.PersistentFlags().StringVar(&flags.host, "host", "http://localhost", "host where Bytebase backend is accessed from, must start with http:// or https://. This is used by Bytebase to create the webhook callback endpoint for VCS integration")
+	rootCmd.PersistentFlags().IntVar(&flags.port, "port", 80, "port where Bytebase backend is accessed from. This is also used by Bytebase to create the webhook callback endpoint for VCS integration")
+	rootCmd.PersistentFlags().StringVar(&flags.frontendHost, "frontend-host", "", "host where Bytebase frontend is accessed from, must start with http:// or https://. This is used by Bytebase to compose the frontend link when posting the webhook event. Default is the same as --host")
+	rootCmd.PersistentFlags().IntVar(&flags.frontendPort, "frontend-port", 0, "port where Bytebase frontend is accessed from. This is used by Bytebase to compose the frontend link when posting the webhook event. Default is the same as --port")
+	rootCmd.PersistentFlags().StringVar(&flags.dataDir, "data", ".", "directory where Bytebase stores data. If relative path is supplied, then the path is relative to the directory where bytebase is under")
+	rootCmd.PersistentFlags().BoolVar(&flags.readonly, "readonly", false, "whether to run in read-only mode")
+	rootCmd.PersistentFlags().BoolVar(&flags.demo, "demo", false, "whether to run using demo data")
+	rootCmd.PersistentFlags().BoolVar(&flags.debug, "debug", false, "whether to enable debug level logging")
 	// TODO(tianzhou): this needs more bake time. There are couple blocking issues:
 	// 1. Currently, we will create a separate bytebase database to store the migration_history table, we need to put it inside the specified database here.
 	// 2. We need to move the logic of creating bytebase metadata db logic outside. Because with --pg option, the db has already been created.
-	rootCmd.PersistentFlags().StringVar(&pgURL, "pg", "", "optional external PostgreSQL instance connection url(must provide dbname); for example postgresql://user:secret@masterhost:5432/dbname?sslrootcert=cert")
+	rootCmd.PersistentFlags().StringVar(&flags.pgURL, "pg", "", "optional external PostgreSQL instance connection url(must provide dbname); for example postgresql://user:secret@masterhost:5432/dbname?sslrootcert=cert")
 }
 
 // -----------------------------------Command Line Config END--------------------------------------
 
 // -----------------------------------Main Entry Point---------------------------------------------
 
-// retrieved via the SettingService upon startup
-type config struct {
-	// secret used to sign the JWT auth token
-	secret string
-}
-
-// Main is the main server for Bytebase.
-type Main struct {
-	profile *server.Profile
-
-	l   *zap.Logger
-	lvl *zap.AtomicLevel
-
-	server *server.Server
-	// serverCancel cancels any runner on the server.
-	// Then the runnerWG waits for all runners to finish before we shutdown the server.
-	// Otherwise, we will get database is closed error from runner when we shutdown the server.
-	serverCancel context.CancelFunc
-
-	metadataDB *metadataDB
-	// db is a connection to the database storing Bytebase data.
-	db *store.DB
-}
-
-func useEmbedDB() bool {
-	return len(pgURL) == 0
-}
-
 func checkDataDir() error {
 	// Convert to absolute path if relative path is supplied.
-	if !filepath.IsAbs(dataDir) {
-		absDir, err := filepath.Abs(filepath.Dir(os.Args[0]) + "/" + dataDir)
+	if !filepath.IsAbs(flags.dataDir) {
+		absDir, err := filepath.Abs(filepath.Dir(os.Args[0]) + "/" + flags.dataDir)
 		if err != nil {
 			return err
 		}
-		dataDir = absDir
+		flags.dataDir = absDir
 	}
 
 	// Trim trailing / in case user supplies
-	dataDir = strings.TrimRight(dataDir, "/")
+	flags.dataDir = strings.TrimRight(flags.dataDir, "/")
 
-	if _, err := os.Stat(dataDir); err != nil {
-		error := fmt.Errorf("unable to access --data %s, %w", dataDir, err)
+	if _, err := os.Stat(flags.dataDir); err != nil {
+		error := fmt.Errorf("unable to access --data %s, %w", flags.dataDir, err)
 		return error
 	}
 
@@ -186,7 +153,7 @@ func checkDataDir() error {
 // GetLogger will return a logger.
 func GetLogger() (*zap.Logger, *zap.AtomicLevel, error) {
 	atom := zap.NewAtomicLevelAt(zap.InfoLevel)
-	if debug {
+	if flags.debug {
 		atom.SetLevel(zap.DebugLevel)
 	}
 	logger := zap.New(zapcore.NewCore(
@@ -204,8 +171,9 @@ func start() {
 	}
 	defer logger.Sync()
 
-	if !common.HasPrefixes(host, "http://", "https://") {
-		logger.Error(fmt.Sprintf("--host %s must start with http:// or https://", host))
+	// check flags
+	if !common.HasPrefixes(flags.host, "http://", "https://") {
+		logger.Error(fmt.Sprintf("--host %s must start with http:// or https://", flags.host))
 		return
 	}
 	if err := checkDataDir(); err != nil {
@@ -213,16 +181,9 @@ func start() {
 		return
 	}
 
-	// We use port+1 for datastore port.
-	datastorePort := port + 1
-	activeProfile := activeProfile(dataDir, port, datastorePort, demo)
-	m, err := NewMain(activeProfile, logger)
-	if err != nil {
-		logger.Error(err.Error())
-		return
-	}
-	m.lvl = level
+	activeProfile := activeProfile(flags.dataDir)
 
+	var s *server.Server
 	// Setup signal handlers.
 	ctx, cancel := context.WithCancel(context.Background())
 	c := make(chan os.Signal, 1)
@@ -231,163 +192,29 @@ func start() {
 	// which is taken as the graceful shutdown signal for many systems, eg., Kubernetes, Gunicorn.
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		<-c
-		m.l.Info("SIGINT received.")
-		if err := m.Close(); err != nil {
-			fmt.Fprintln(os.Stderr, err)
+		sig := <-c
+		logger.Info(fmt.Sprintf("%s received.", sig.String()))
+		if s != nil {
+			_ = s.Shutdown(ctx)
 		}
 		cancel()
 	}()
 
+	s, err = server.NewServer(ctx, activeProfile, logger, level)
+	if err != nil {
+		fmt.Printf("cannot new server, error: %v\n", err)
+		return
+	}
+	fmt.Printf(greetingBanner, fmt.Sprintf("Version %s has started at %s:%d", activeProfile.Version, activeProfile.BackendHost, activeProfile.BackendPort))
 	// Execute program.
-	if err := m.Run(ctx); err != nil {
+	if err := s.Run(ctx); err != nil {
 		if err != http.ErrServerClosed {
-			m.l.Error(err.Error())
-			m.Close()
+			logger.Error(err.Error())
+			_ = s.Shutdown(ctx)
 			cancel()
 		}
 	}
 
 	// Wait for CTRL-C.
 	<-ctx.Done()
-}
-
-// NewMain creates a main server based on profile.
-func NewMain(activeProfile server.Profile, logger *zap.Logger) (*Main, error) {
-	fmt.Println("-----Config BEGIN-----")
-	fmt.Printf("mode=%s\n", activeProfile.Mode)
-	fmt.Printf("server=%s:%d\n", host, activeProfile.Port)
-	fmt.Printf("datastore=%s:%d\n", host, activeProfile.DatastorePort)
-	fmt.Printf("frontend=%s:%d\n", frontendHost, frontendPort)
-	fmt.Printf("demoDataDir=%s\n", activeProfile.DemoDataDir)
-	fmt.Printf("readonly=%t\n", readonly)
-	fmt.Printf("demo=%t\n", demo)
-	fmt.Printf("debug=%t\n", debug)
-	fmt.Println("-----Config END-------")
-
-	metadataDB, err := createMetadataDB(&activeProfile, logger)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Main{
-		profile:    &activeProfile,
-		l:          logger,
-		metadataDB: metadataDB,
-	}, nil
-}
-
-func initSetting(ctx context.Context, store *store.Store) (*config, error) {
-	result := &config{}
-	{
-		configCreate := &api.SettingCreate{
-			CreatorID:   api.SystemBotID,
-			Name:        api.SettingAuthSecret,
-			Value:       common.RandomString(secretLength),
-			Description: "Random string used to sign the JWT auth token.",
-		}
-		config, err := store.CreateSettingIfNotExist(ctx, configCreate)
-		if err != nil {
-			return nil, err
-		}
-		result.secret = config.Value
-	}
-
-	return result, nil
-}
-
-func initBranding(ctx context.Context, store *store.Store) error {
-	configCreate := &api.SettingCreate{
-		CreatorID:   api.SystemBotID,
-		Name:        api.SettingBrandingLogo,
-		Value:       "",
-		Description: "The branding logo image in base64 string format.",
-	}
-	_, err := store.CreateSettingIfNotExist(ctx, configCreate)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Run will run the main server.
-func (m *Main) Run(ctx context.Context) error {
-	ctx, cancel := context.WithCancel(ctx)
-	m.serverCancel = cancel
-
-	db, err := m.metadataDB.connect()
-	if err != nil {
-		return fmt.Errorf("cannot new db: %w", err)
-	}
-	if err := db.Open(ctx); err != nil {
-		return fmt.Errorf("cannot open db: %w", err)
-	}
-	m.db = db
-
-	cacheService := server.NewCacheService(m.l)
-	storeInstance := store.New(m.l, db, cacheService)
-
-	config, err := initSetting(ctx, storeInstance)
-	if err != nil {
-		return fmt.Errorf("failed to init config: %w", err)
-	}
-
-	err = initBranding(ctx, storeInstance)
-	if err != nil {
-		return fmt.Errorf("failed to init branding: %w", err)
-	}
-
-	s := server.NewServer(m.l, storeInstance, m.lvl, version, host, m.profile.Port, frontendHost, frontendPort, m.profile.DatastorePort, m.profile.Mode, m.profile.DataDir, m.profile.BackupRunnerInterval, config.secret, readonly, demo, debug)
-
-	s.ActivityManager = server.NewActivityManager(s, storeInstance)
-
-	licenseService, err := enterprise.NewLicenseService(m.l, m.profile.DataDir, m.profile.Mode)
-	if err != nil {
-		return err
-	}
-	s.LicenseService = licenseService
-	s.InitSubscription()
-
-	m.server = s
-
-	fmt.Printf(greetingBanner, fmt.Sprintf("Version %s has started at %s:%d", version, host, m.profile.Port))
-
-	if err := s.Run(ctx); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Close gracefully stops the program.
-func (m *Main) Close() error {
-	m.l.Info("Trying to stop Bytebase...")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	if m.server != nil {
-		m.l.Info("Trying to gracefully shutdown server...")
-		m.serverCancel()
-		m.server.Shutdown(ctx)
-	}
-
-	if m.db != nil {
-		m.l.Info("Trying to close database connections...")
-		if err := m.db.Close(); err != nil {
-			return err
-		}
-	}
-
-	if err := m.metadataDB.close(); err != nil {
-		return err
-	}
-
-	m.l.Info("Bytebase stopped properly.")
-	return nil
-}
-
-// GetServer returns the server in main.
-func (m *Main) GetServer() *server.Server {
-	return m.server
 }
