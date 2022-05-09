8b16bc8095a95f5b8a12ef1113ff7ce67a4db25f
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-09 23:07:08 +0800

    test: add TestBootWithExternalPg for external postgres (#1238)
    * fix: fix redirectUri of oauth
    
    * test: add TestBootWithExternalPg
    
    * refactor: remove some changes that no merge
    
    * refactor: add comment of StartServerWithExternalPg()
    
    * refactor: add log to debug github action
    
    * refactor: waitForHealthz() will only GET /healthz only once
    
    * fix: specify Unix Domain Socket in pgURL query
    
    * fix: q.Get("host") instead of q.Get(u.Host)
    
    * refactor: remove some logs
    
    * refactor: use common.GetPostgresSocketDir() to replace /tmp
    
    * chore: waitForHealthz() need will use for-select to wait server boot completely
    
    Former-commit-id: 52914d4950d450299040bd5ef919e7ce47acc909 [formerly 6e264e3fcf838f66b57510a4849dfa7ef8aa17d7] [formerly c5e510152164c421f38673900edcf4db989ba9fd]
    Former-commit-id: 6c9efd175ae38b3d1abf1631e2e521196569433b
    Former-commit-id: c416a22e19670c0555c38e9c1707128e964090fb

---
+++ tests/external_pg_test.go
@@ -0,0 +1,68 @@
+package tests
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/bytebase/bytebase/common"
+	"github.com/bytebase/bytebase/resources/postgres"
+	"github.com/stretchr/testify/require"
+)
+
+type fakeExternalPg struct {
+	pgIns  *postgres.Instance
+	pgURL  string
+	pgUser string
+}
+
+// newFakeExternalPg will install postgres in tmpDir and listen on Unix domain socket with port
+func newFakeExternalPg(tmpDir string, port int) (*fakeExternalPg, error) {
+	resourceDir := path.Join(tmpDir, "resources")
+	dataDir := path.Join(tmpDir, "pgdata")
+	pgUser := "bbexternal"
+	pgIns, err := postgres.Install(resourceDir, dataDir, pgUser)
+	if err != nil {
+		return nil, fmt.Errorf("cannot install postgres, error: %w", err)
+	}
+
+	err = pgIns.Start(port, os.Stderr, os.Stderr)
+	if err != nil {
+		return nil, fmt.Errorf("cannot start postgres server, error: %w", err)
+	}
+
+	return &fakeExternalPg{
+		pgIns:  pgIns,
+		pgURL:  fmt.Sprintf("postgresql://%s@:%d/%s?host=%s", pgUser, port, "postgres", common.GetPostgresSocketDir()),
+		pgUser: pgUser,
+	}, nil
+}
+
+func (f *fakeExternalPg) Destroy() error {
+	return f.pgIns.Stop(os.Stderr, os.Stderr)
+}
+func TestBootWithExternalPg(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	pgTmpDir := t.TempDir()
+
+	port := getTestPort(t.Name())
+	serverPort := port + 1
+
+	externalPg, err := newFakeExternalPg(pgTmpDir, port)
+	require.NoError(t, err)
+	defer func() {
+		if err = externalPg.Destroy(); err != nil {
+			fmt.Printf("cannot destroy pginstance, error: %s", err.Error())
+			t.FailNow()
+		}
+	}()
+
+	ctl := &controller{}
+	err = ctl.StartServerWithExternalPg(ctx, serverPort, externalPg.pgUser, externalPg.pgURL)
+	require.NoError(t, err)
+	defer ctl.Close(ctx)
+}
