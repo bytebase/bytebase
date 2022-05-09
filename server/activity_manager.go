3be57e133cedb2fb4b5dbca3ca4c52c096cc203d
Yilong Li <liyilongko@gmail.com>
2022-05-09 14:45:11 +0800

    chore: remove unused code

--- server/activity_manager.go
+++ server/activity_manager.go
@@ -76,7 +76,6 @@ func (m *ActivityManager) CreateActivity(ctx context.Context, create *api.Activi
 		if project == nil {
 			return nil, fmt.Errorf("failed to find project ID %v for posting webhook event after changing the issue status %q", meta.issue.ProjectID, meta.issue.Name)
 		}
-		// TODO(dragonly): revisit the necessity of this function to depend on ActivityMeta.
 		meta.issue.Project = project
 	}
 
