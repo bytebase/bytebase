b6082f60fe9d05415c3b5669d2c2f7d715d643f0
Yilong Li <liyilongko@gmail.com>
2022-05-10 20:34:00 +0800

    test: use require.Assertions to simplify test assertions

--- tests/start_test.go
+++ tests/start_test.go
@@ -4,35 +4,35 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestServiceRestart(t *testing.T) {
 	t.Parallel()
+	a := require.New(t)
 	ctx := context.Background()
 	ctl := &controller{}
 	dataDir := t.TempDir()
 	err := ctl.StartServer(ctx, dataDir, getTestPort(t.Name()))
-	require.NoError(t, err)
+	a.NoError(err)
 
 	err = ctl.Login()
-	require.NoError(t, err)
+	a.NoError(err)
 
 	projects, err := ctl.getProjects()
-	require.NoError(t, err)
+	a.NoError(err)
 
 	// Test seed should have more than one project.
-	assert.Greater(t, len(projects), 1)
+	a.Greater(len(projects), 1)
 
 	// Restart the server.
 	err = ctl.Close(ctx)
-	require.NoError(t, err)
+	a.NoError(err)
 
 	err = ctl.StartServer(ctx, dataDir, getTestPort(t.Name()))
-	require.NoError(t, err)
+	a.NoError(err)
 	defer ctl.Close(ctx)
 
 	err = ctl.Login()
-	require.NoError(t, err)
+	a.NoError(err)
 }
