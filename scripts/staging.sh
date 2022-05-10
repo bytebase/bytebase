e67289e6d806f148481b88854c0a7f0354dbec6c
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-10 16:34:59 +0800

    fix: using 443 port instead of 80 in https schema

--- scripts/staging.sh
+++ scripts/staging.sh
@@ -8,9 +8,9 @@
 # ./staging.sh https://example.com
 # ./staging.sh https://example.com:8080
 
-# If no parameter is passed, use https://staging.bytebase.com as host and 80 as port by default
+# If no parameter is passed, use https://staging.bytebase.com as host and 443 as port by default
 ONLINE_DEMO_HOST='https://staging.bytebase.com'
-ONLINE_DEMO_PORT='80'
+ONLINE_DEMO_PORT='443'
 if [ $1 ]; then
     PROTOCAL=`echo $1 | awk -F ':' '{ print $1 }'`
     URI=`echo $1 | awk -F '[/:]' '{ print $4; }'`
