9760c22df056b9a483c56990a568c97220fc050b
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-05-11 15:11:50 +0800

    chore: remove unused file

--- frontend/src/plugins/issue/logic/types.ts
+++
@@ -1,8 +0,0 @@
-import { Ref } from "vue";
-import { useRoute } from "vue-router";
-import { IssueTemplate } from "@/plugins/types";
-
-export type BuildNewIssueContext = {
-  template: Ref<IssueTemplate>;
-  route: ReturnType<typeof useRoute>;
-};
