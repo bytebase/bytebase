d4c45096208c0cb5f2ba9c69ef86f0f125bee2bf
ecmadao <wlec@outlook.com>
2022-05-11 15:39:04 +0800

    feat(schema system): support MySQL index/pk/uk/fk naming convention rule (#1230)
    * chore: parse payload for index naming rule
    
    * chore: update comment
    
    * chore: resolve comment
    
    * chore: remove unused struct
    
    * chore: update the comment
    
    * chore: rename functions
    
    * chore: add index naming convention advisor
    
    * chore: initial the test
    
    * chore: update default index naming convention
    
    * chore: rename files and variables
    
    * chore: rename function
    
    * chore: add rename index check
    
    * chore: rename variables
    
    * chore: add tests
    
    * chore: add tests
    
    * chore: update error content
    
    * chore: update the format in the frontend
    
    * chore: advisor for foreign key
    
    * chore: use individual advisors for index naming convenstion
    
    * chore: fix code
    
    * chore: support advisor for rename index
    
    * chore: resolve comments
    
    * chore: add comments
    
    * chore: resolve comment
    
    * chore: resolve comments
    
    Former-commit-id: c79aeba8b11de3770112293ed1f430ac78bed2c1 [formerly 903031402a5bb0ee65c707bab2e43a0edd270c33] [formerly 6b7d8de6c95c005aa4edee88fbbe7bd8b9c2e0c1]
    Former-commit-id: 3f51c21bec2dbd18a9352f3d99b5d77ebd9159b0
    Former-commit-id: 9adf9050cb1bceea71fee94c2f0c04f19c122863

---
+++ plugin/advisor/mysql/advisor_naming_primary_key_convention.go
@@ -0,0 +1,148 @@
+package mysql
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/bytebase/bytebase/api"
+	"github.com/bytebase/bytebase/common"
+	"github.com/bytebase/bytebase/plugin/advisor"
+	"github.com/bytebase/bytebase/plugin/db"
+	"github.com/pingcap/tidb/parser/ast"
+)
+
+var (
+	_ advisor.Advisor = (*NamingPKConventionAdvisor)(nil)
+)
+
+func init() {
+	advisor.Register(db.MySQL, advisor.MySQLNamingPKConvention, &NamingPKConventionAdvisor{})
+	advisor.Register(db.TiDB, advisor.MySQLNamingPKConvention, &NamingPKConventionAdvisor{})
+}
+
+// NamingPKConventionAdvisor is the advisor checking for primary key naming convention.
+type NamingPKConventionAdvisor struct {
+}
+
+// Check checks for primary key naming convention.
+func (check *NamingPKConventionAdvisor) Check(ctx advisor.Context, statement string) ([]advisor.Advice, error) {
+	root, errAdvice := parseStatement(statement, ctx.Charset, ctx.Collation)
+	if errAdvice != nil {
+		return errAdvice, nil
+	}
+
+	level, err := advisor.NewStatusBySchemaReviewRuleLevel(ctx.Rule.Level)
+	if err != nil {
+		return nil, err
+	}
+
+	format, templateList, err := api.UnmarshalNamingRulePayloadAsTemplate(ctx.Rule.Type, ctx.Rule.Payload)
+	if err != nil {
+		return nil, err
+	}
+	checker := &namingPKConventionChecker{
+		level:        level,
+		format:       format,
+		templateList: templateList,
+	}
+	for _, stmtNode := range root {
+		(stmtNode).Accept(checker)
+	}
+
+	if len(checker.adviceList) == 0 {
+		checker.adviceList = append(checker.adviceList, advisor.Advice{
+			Status:  advisor.Success,
+			Code:    common.Ok,
+			Title:   "OK",
+			Content: "",
+		})
+	}
+	return checker.adviceList, nil
+}
+
+type namingPKConventionChecker struct {
+	adviceList   []advisor.Advice
+	level        advisor.Status
+	format       string
+	templateList []string
+}
+
+// Enter implements the ast.Visitor interface
+func (checker *namingPKConventionChecker) Enter(in ast.Node) (ast.Node, bool) {
+	indexDataList := checker.getMetaDataList(in)
+
+	for _, indexData := range indexDataList {
+		regex, err := getTemplateRegexp(checker.format, checker.templateList, indexData.metaData)
+		if err != nil {
+			checker.adviceList = append(checker.adviceList, advisor.Advice{
+				Status:  checker.level,
+				Code:    common.Internal,
+				Title:   "Internal error for primary key naming convention rule",
+				Content: fmt.Sprintf("%q meet internal error %q", in.Text(), err.Error()),
+			})
+			continue
+		}
+		if !regex.MatchString(indexData.indexName) {
+			checker.adviceList = append(checker.adviceList, advisor.Advice{
+				Status:  checker.level,
+				Code:    common.NamingPKConventionMismatch,
+				Title:   "Mismatch primary key naming convention",
+				Content: fmt.Sprintf("Primary key in table `%s` mismatches the naming convention, expect %q but found `%s`", indexData.tableName, regex, indexData.indexName),
+			})
+		}
+	}
+
+	return in, false
+}
+
+// Leave implements the ast.Visitor interface
+func (checker *namingPKConventionChecker) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
+}
+
+// getMetaDataList returns the list of primary key with meta data.
+func (checker *namingPKConventionChecker) getMetaDataList(in ast.Node) []*indexMetaData {
+	var res []*indexMetaData
+
+	switch node := in.(type) {
+	case *ast.CreateTableStmt:
+		for _, constraint := range node.Constraints {
+			if constraint.Tp == ast.ConstraintPrimaryKey {
+				var columnList []string
+				for _, key := range constraint.Keys {
+					columnList = append(columnList, key.Column.Name.String())
+				}
+				metaData := map[string]string{
+					api.ColumnListTemplateToken: strings.Join(columnList, "_"),
+					api.TableNameTemplateToken:  node.Table.Name.String(),
+				}
+				res = append(res, &indexMetaData{
+					indexName: constraint.Name,
+					tableName: node.Table.Name.String(),
+					metaData:  metaData,
+				})
+			}
+		}
+	case *ast.AlterTableStmt:
+		for _, spec := range node.Specs {
+			if spec.Tp == ast.AlterTableAddConstraint && spec.Constraint.Tp == ast.ConstraintPrimaryKey {
+				var columnList []string
+				for _, key := range spec.Constraint.Keys {
+					columnList = append(columnList, key.Column.Name.String())
+				}
+
+				metaData := map[string]string{
+					api.ColumnListTemplateToken: strings.Join(columnList, "_"),
+					api.TableNameTemplateToken:  node.Table.Name.String(),
+				}
+				res = append(res, &indexMetaData{
+					indexName: spec.Constraint.Name,
+					tableName: node.Table.Name.String(),
+					metaData:  metaData,
+				})
+			}
+		}
+	}
+
+	return res
+}
