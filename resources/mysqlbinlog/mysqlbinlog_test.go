3db7fade9fbe99ad85d1163350ef22044ec1a845
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-11 18:47:08 +0800

    refactor: mysqlbinlog test

--- resources/mysqlbinlog/mysqlbinlog_test.go
+++ resources/mysqlbinlog/mysqlbinlog_test.go
@@ -1,10 +1,6 @@
-//go:build linux && amd64
-// +build linux,amd64
-
 package mysqlbinlog
 
 import (
-	"embed"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -13,14 +9,11 @@ import (
 	"github.com/bytebase/bytebase/resources/utils"
 )
 
-//go:embed mysqlbinlog-8.0.28-linux-glibc-2.17-x86_64.tar.gz
-var resources embed.FS
-
 // TestBootMySQLBinlog tests whether mysqlbinlog can be started on the target platform
 // to check whether the lib extraction is correct.
 func TestBootMySQLBinlog(t *testing.T) {
-	tarName := "mysqlbinlog-8.0.28-linux-glibc-2.17-x86_64.tar.gz"
-	version := "mysqlbinlog-8.0.28-linux-glibc-2.17-x86_64"
+	tarName := "mysqlbinlog-8.0.28-macos11-arm64.tar.gz"
+	version := "mysqlbinlog-8.0.28-macos11-arm64"
 	tarF, err := resources.Open(tarName)
 	if err != nil {
 		t.Errorf("failed to open mysqlbinlog dist %q, error: %v", tarName, err)
