b48e1d62e7f618873c5211974e1367047654d73a
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-11 18:55:39 +0800

    fix: using switch-case to judge tarname

--- resources/mysqlbinlog/mysqlbinlog_test.go
+++ resources/mysqlbinlog/mysqlbinlog_test.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/bytebase/bytebase/resources/utils"
@@ -12,8 +13,18 @@ import (
 // TestBootMySQLBinlog tests whether mysqlbinlog can be started on the target platform
 // to check whether the lib extraction is correct.
 func TestBootMySQLBinlog(t *testing.T) {
-	tarName := "mysqlbinlog-8.0.28-macos11-arm64.tar.gz"
-	version := "mysqlbinlog-8.0.28-macos11-arm64"
+	var tarName string
+	var version string
+
+	switch {
+	case runtime.GOOS == "darwin" && runtime.GOARCH == "arm64":
+		tarName = "mysqlbinlog-8.0.28-macos11-arm64.tar.gz"
+		version = "mysqlbinlog-8.0.28-macos11-arm64"
+	case runtime.GOOS == "linux" && runtime.GOARCH == "amd64":
+		tarName = "mysqlbinlog-8.0.28-linux-glibc-2.17-x86_64.tar.gz"
+		version = "mysqlbinlog-8.0.28-linux-glibc-2.17-x86_64"
+	}
+
 	tarF, err := resources.Open(tarName)
 	if err != nil {
 		t.Errorf("failed to open mysqlbinlog dist %q, error: %v", tarName, err)
