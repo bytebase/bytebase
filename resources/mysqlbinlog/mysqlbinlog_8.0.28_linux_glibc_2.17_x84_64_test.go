afd26d9fd6b8dd2ae5a7fafdedce2e13e33cd431
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-11 17:22:31 +0800

    fix: golint

--- resources/mysqlbinlog/mysqlbinlog_8.0.28_linux_glibc_2.17_x84_64_test.go
+++ resources/mysqlbinlog/mysqlbinlog_8.0.28_linux_glibc_2.17_x84_64_test.go
@@ -23,7 +23,7 @@ func TestBootMySQLBinlog(t *testing.T) {
 	version := "mysqlbinlog-8.0.28-linux-glibc-2.17-x86_64"
 	tarF, err := resources.Open(tarName)
 	if err != nil {
-		t.Errorf("failed to open mysqlbinlog dist %q, error: %w", tarName, err)
+		t.Errorf("failed to open mysqlbinlog dist %q, error: %v", tarName, err)
 	}
 	defer tarF.Close()
 
@@ -38,6 +38,6 @@ func TestBootMySQLBinlog(t *testing.T) {
 	cmd.Stderr = os.Stderr
 
 	if err := cmd.Run(); err != nil {
-		t.Errorf("failed to boot mysqlbinlog, cmd: %s, err: %w", cmd.String(), err)
+		t.Errorf("failed to boot mysqlbinlog, cmd: %s, err: %v", cmd.String(), err)
 	}
 }
