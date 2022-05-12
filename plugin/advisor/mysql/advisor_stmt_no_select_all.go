6d8237296d5e398668e0c52d4eca0367acc5869c
rebelice <yangrebelice@gmail.com>
2022-05-12 12:31:27 +0800

    feat(schema system): support MySQL no SELECT all rule (#1278)
    Former-commit-id: 7a424b9b2e98da5594667d1230608ba38e7bdc79
    Former-commit-id: 747ea9f5f45cc20efa1025619d8bacfd0dd5448b

---
+++ plugin/advisor/mysql/advisor_stmt_no_select_all.go
@@ -0,0 +1,79 @@
+package mysql
+
+import (
+	"fmt"
+
+	"github.com/bytebase/bytebase/common"
+	"github.com/bytebase/bytebase/plugin/advisor"
+	"github.com/bytebase/bytebase/plugin/db"
+	"github.com/pingcap/tidb/parser/ast"
+)
+
+var (
+	_ advisor.Advisor = (*NoSelectAllAdvisor)(nil)
+)
+
+func init() {
+	advisor.Register(db.MySQL, advisor.MySQLNoSelectAll, &NoSelectAllAdvisor{})
+	advisor.Register(db.TiDB, advisor.MySQLNoSelectAll, &NoSelectAllAdvisor{})
+}
+
+// NoSelectAllAdvisor is the advisor checking for no "select *".
+type NoSelectAllAdvisor struct {
+}
+
+// Check checks for no "select *".
+func (adv *NoSelectAllAdvisor) Check(ctx advisor.Context, statement string) ([]advisor.Advice, error) {
+	root, errAdvice := parseStatement(statement, ctx.Charset, ctx.Collation)
+	if errAdvice != nil {
+		return errAdvice, nil
+	}
+
+	level, err := advisor.NewStatusBySchemaReviewRuleLevel(ctx.Rule.Level)
+	if err != nil {
+		return nil, err
+	}
+	checker := &noSelectAllChecker{level: level}
+	for _, stmtNode := range root {
+		(stmtNode).Accept(checker)
+	}
+
+	if len(checker.adviceList) == 0 {
+		checker.adviceList = append(checker.adviceList, advisor.Advice{
+			Status:  advisor.Success,
+			Code:    common.Ok,
+			Title:   "OK",
+			Content: "",
+		})
+	}
+	return checker.adviceList, nil
+}
+
+type noSelectAllChecker struct {
+	adviceList []advisor.Advice
+	level      advisor.Status
+}
+
+// Enter implements the ast.Visitor interface
+func (v *noSelectAllChecker) Enter(in ast.Node) (ast.Node, bool) {
+	if node, ok := in.(*ast.SelectStmt); ok {
+		for _, field := range node.Fields.Fields {
+			if field.WildCard != nil {
+				v.adviceList = append(v.adviceList, advisor.Advice{
+					Status:  v.level,
+					Code:    common.StatementSelectAll,
+					Title:   "No SELECT all",
+					Content: fmt.Sprintf("%q uses SELECT all", in.Text()),
+				})
+				break
+			}
+		}
+
+	}
+	return in, false
+}
+
+// Leave implements the ast.Visitor interface
+func (v *noSelectAllChecker) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
+}
