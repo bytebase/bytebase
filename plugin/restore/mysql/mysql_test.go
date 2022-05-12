18b56341de3b49278e53a699b13ca31ad90ead6e
Yilong Li <liyilongko@gmail.com>
2022-05-12 13:44:20 +0800

    test: improve the ut case for testing the buggy application CUJ in PITR (#1219)
    * refactor: extract common routines in TestPITR
    
    * feat: add interface method RestoreIncremental() for restoring incremental backup
    
    * feat: add IncrementalRecoveryConfig
    
    * refactor: move things into Driver
    
    * feat: add getSafeTableName as gh-ost
    
    * chore: address reviewer's comment
    
    * test: add unit test
    
    * chore: address reviewer's comment
    
    * chore: use multiStatement to simplify test code
    
    * refactor: move mysql PITR implementation into restore package
    
    * fix: test compiles
    
    * chore: combine multiple SQL statements
    
    * chore: add TODO
    
    * chore: add ut and address reviewer's comment
    
    * chore: extract inline function
    
    * chore: unexport driver.db
    
    * chore: add newlines
    
    Former-commit-id: 88dc92495bee11fe89284bd5b4a58014ee7d17ef
    Former-commit-id: 7708cd1390ef7d1f1b0d4eddcb3041292c52b710

---
+++ plugin/restore/mysql/mysql_test.go
@@ -0,0 +1,61 @@
+package mysql
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetSafeName(t *testing.T) {
+	a := require.New(t)
+	tests := []struct {
+		baseName string
+		suffix   string
+		expected string
+	}{
+		{
+			baseName: "normal_database_name",
+			suffix:   "pitr_1652237293",
+			expected: "normal_database_name_pitr_1652237293",
+		},
+		{
+			baseName: "normal_database_name",
+			suffix:   "old",
+			expected: "normal_database_name_old",
+		},
+		{
+			baseName: "long_database_name1234567890123456789012345678901",
+			suffix:   "pitr_1652237293",
+			expected: "long_database_name123456789012345678901234567890_pitr_1652237293",
+		},
+	}
+
+	for _, test := range tests {
+		safeName := getSafeName(test.baseName, test.suffix)
+		a.Equal(test.expected, safeName)
+	}
+}
+
+func TestGetPITRDatabaseName(t *testing.T) {
+	a := require.New(t)
+	tests := []struct {
+		database  string
+		timestamp int
+		expected  string
+	}{
+		{
+			database:  "normal_database_name",
+			timestamp: 1652237293,
+			expected:  "normal_database_name_pitr_1652237293",
+		},
+		{
+			database:  "long_database_name1234567890123456789012345678901",
+			timestamp: 1652237293,
+			expected:  "long_database_name123456789012345678901234567890_pitr_1652237293",
+		},
+	}
+	for _, test := range tests {
+		name := getPITRDatabaseName(test.database, int64(test.timestamp))
+		a.Equal(test.expected, name)
+	}
+}
