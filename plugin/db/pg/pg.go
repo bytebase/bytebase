3547aa58d4e11b17d4a44db9d68797931c621cb8
Cluas <Cluas@live.cn>
2022-05-12 23:23:59 +0800

    feat: compatible with pg 10 syntax

--- plugin/db/pg/pg.go
+++ plugin/db/pg/pg.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/blang/semver/v4"
 	"github.com/bytebase/bytebase/common"
 
 	// embed will embeds the migration schema.
@@ -871,6 +872,11 @@ func (driver *Driver) dumpOneDatabase(ctx context.Context, database string, out 
 		return err
 	}
 
+	version, err := driver.GetVersion(ctx)
+	if err != nil {
+		return err
+	}
+
 	txn, err := driver.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -989,7 +995,7 @@ func (driver *Driver) dumpOneDatabase(ctx context.Context, database string, out 
 		return fmt.Errorf("failed to get event triggers from database %q: %s", database, err)
 	}
 	for _, evt := range events {
-		if _, err := io.WriteString(out, evt.Statement()); err != nil {
+		if _, err := io.WriteString(out, evt.Statement(version)); err != nil {
 			return err
 		}
 	}
@@ -1274,7 +1280,7 @@ func (t triggerSchema) Statement() string {
 }
 
 // Statement returns the create statement of an event trigger.
-func (t eventTriggerSchema) Statement() string {
+func (t eventTriggerSchema) Statement(serverVersion string) string {
 	s := fmt.Sprintf(""+
 		"--\n"+
 		"-- Event trigger structure for %s\n"+
@@ -1284,7 +1290,14 @@ func (t eventTriggerSchema) Statement() string {
 	if t.tags != "" {
 		s += fmt.Sprintf("\n         WHEN TAG IN (%s)", t.tags)
 	}
-	s += fmt.Sprintf("\n   EXECUTE FUNCTION %s();\n", t.funcName)
+
+	functionSyntax := "FUNCTION"
+	version, _ := semver.ParseTolerant(serverVersion)
+	if version.Major == 10 {
+		functionSyntax = "PROCEDURE"
+	}
+
+	s += fmt.Sprintf("\n   EXECUTE %s %s();\n", functionSyntax, t.funcName)
 
 	if t.enabled != "O" {
 		s += fmt.Sprintf("ALTER EVENT TRIGGER %s ", t.name)
@@ -1651,10 +1664,7 @@ func getIndexColumnExpressions(stmt string) ([]string, error) {
 
 	var cols []string
 	re := regexp.MustCompile(`\(\(.*\)\)`)
-	for {
-		if len(columnStmt) <= 0 {
-			break
-		}
+	for len(columnStmt) > 0 {
 		// Get a token
 		token := ""
 		// Expression has format of "((exp))".
@@ -1870,7 +1880,7 @@ func getEventTriggers(txn *sql.Tx) ([]*eventTriggerSchema, error) {
 
 // quoteIdentifier will quote identifiers including keywords, capital characters, or special characters.
 func quoteIdentifier(s string) string {
-	quote := false
+	var quote bool
 	if reserved[strings.ToUpper(s)] {
 		quote = true
 	}
