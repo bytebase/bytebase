7ca5eeda61366826c251ddf85d50debaade94468
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-05-12 13:22:56 +0800

    refactor(frontend): IssueDetail part.3 (#1273)
    * refactor: extract issue template create logic
    
    * chore: move types.ts to common.ts
    
    * chore: remove unused file
    
    * chore: move stuff from constants.ts to common.ts
    
    * refactor: extract different modes' issue logic into independent context providers
    
    * refactor: rename IssueContext to IssueLogic
    
    * chore: use import type
    
    * chore: rename
    
    * refactor: reuse patchTask and patchIssue via IssueLogic
    
    * chore: address comments
    
    * fix: issueSlug not updated when doCreate
    
    * chore: re-order code
    
    * chore: address comments
    
    * chore: rename
    
    * chore: nits
    
    * chore: disable statement editing for bb.issue.database.create
    
    * chore: tuning style
    
    Former-commit-id: b0e1c9803fb8e6210cc70339a3ba4928f2b45cea
    Former-commit-id: 96a77b3cce3831d0263361ce78a87ca999884d51

---
+++ frontend/src/components/Issue/logic/GhostModeProvider.ts
@@ -0,0 +1,70 @@
+import { computed, defineComponent } from "vue";
+import { provideIssueLogic, useIssueLogic } from "./index";
+import { maybeFormatStatementOnSave, useCommonLogic } from "./common";
+import {
+  IssueCreate,
+  Task,
+  TaskCreate,
+  TaskDatabaseSchemaUpdateGhostSyncPayload,
+  UpdateSchemaGhostContext,
+} from "@/types";
+import { useDatabaseStore } from "@/store";
+
+export default defineComponent({
+  name: "GhostModeProvider",
+  setup() {
+    const { create, issue, selectedTask } = useIssueLogic();
+    const databaseStore = useDatabaseStore();
+
+    // In gh-ost mode, each stage can own its SQL statement
+    // But only for task.type === "bb.task.database.schema.update.ghost.sync"
+    const selectedStatement = computed(() => {
+      const task = selectedTask.value;
+      if (task.type === "bb.task.database.schema.update.ghost.sync") {
+        if (create.value) {
+          return (task as TaskCreate).statement;
+        } else {
+          const payload = (task as Task)
+            .payload as TaskDatabaseSchemaUpdateGhostSyncPayload;
+          return payload.statement;
+        }
+      } else {
+        return "";
+      }
+    });
+
+    const doCreate = () => {
+      const issueCreate = issue.value as IssueCreate;
+
+      // for gh-ost mode, copy user edited tasks back to issue.createContext
+      // only the first subtask (bb.task.database.schema.update.ghost.sync) has statement
+      const stageList = issueCreate.pipeline!.stageList;
+      const createContext =
+        issueCreate.createContext as UpdateSchemaGhostContext;
+      const detailList = createContext.updateSchemaDetailList;
+      stageList.forEach((stage, i) => {
+        const detail = detailList[i];
+        const syncTask = stage.taskList.find(
+          (task) => task.type === "bb.task.database.schema.update.ghost.sync"
+        )!;
+        const db = databaseStore.getDatabaseById(syncTask.databaseId!);
+
+        detail.databaseId = syncTask.databaseId!;
+        detail.databaseName = syncTask.databaseName!;
+        detail.statement = maybeFormatStatementOnSave(syncTask.statement, db);
+        detail.earliestAllowedTs = syncTask.earliestAllowedTs;
+      });
+    };
+
+    const logic = {
+      ...useCommonLogic(),
+      selectedStatement,
+      doCreate,
+    };
+    provideIssueLogic(logic);
+    return logic;
+  },
+  render() {
+    return this.$slots.default?.();
+  },
+});
