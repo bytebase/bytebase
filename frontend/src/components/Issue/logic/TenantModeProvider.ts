06fcf1afd6caddbf429ab764f549cc05dc6baaf5
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-05-12 13:22:56 +0800

    refactor(frontend): IssueDetail part.3 (#1273)
    * refactor: extract issue template create logic
    
    * chore: move types.ts to common.ts
    
    * chore: remove unused file
    
    * chore: move stuff from constants.ts to common.ts
    
    * refactor: extract different modes' issue logic into independent context providers
    
    * refactor: rename IssueContext to IssueLogic
    
    * chore: use import type
    
    * chore: rename
    
    * refactor: reuse patchTask and patchIssue via IssueLogic
    
    * chore: address comments
    
    * fix: issueSlug not updated when doCreate
    
    * chore: re-order code
    
    * chore: address comments
    
    * chore: rename
    
    * chore: nits
    
    * chore: disable statement editing for bb.issue.database.create
    
    * chore: tuning style
    
    Former-commit-id: 7ca5eeda61366826c251ddf85d50debaade94468 [formerly 96a77b3cce3831d0263361ce78a87ca999884d51] [formerly b0e1c9803fb8e6210cc70339a3ba4928f2b45cea]
    Former-commit-id: fa721dbddb3a215a75f0ea7495857468437ec0eb
    Former-commit-id: 82af17af4c870e4381d0e037382c8a26b75c25f8

---
+++ frontend/src/components/Issue/logic/TenantModeProvider.ts
@@ -0,0 +1,100 @@
+import { computed, defineComponent } from "vue";
+import { cloneDeep } from "lodash-es";
+import { useDatabaseStore } from "@/store";
+import {
+  Issue,
+  IssueCreate,
+  TaskCreate,
+  TaskDatabaseSchemaUpdatePayload,
+  UpdateSchemaContext,
+} from "@/types";
+import {
+  errorAssertion,
+  flattenTaskList,
+  maybeFormatStatementOnSave,
+  useCommonLogic,
+} from "./common";
+import { provideIssueLogic, useIssueLogic } from "./index";
+
+export default defineComponent({
+  name: "TenantModeProvider",
+  setup() {
+    const { create, issue, createIssue } = useIssueLogic();
+    const databaseStore = useDatabaseStore();
+
+    const allowEditStatement = computed(() => {
+      if (create.value) {
+        return true;
+      }
+      // Once a tenant-mode issue created, its statement can never be changed.
+      return false;
+    });
+
+    // In tenant mode, the entire issue shares only one SQL statement
+    const selectedStatement = computed(() => {
+      if (create.value) {
+        const issueCreate = issue.value as IssueCreate;
+        const context = issueCreate.createContext as UpdateSchemaContext;
+        return context.updateSchemaDetailList[0].statement;
+      } else {
+        const issueEntity = issue.value as Issue;
+        const task = issueEntity.pipeline.stageList[0].taskList[0];
+        const payload = task.payload as TaskDatabaseSchemaUpdatePayload;
+        return payload.statement;
+      }
+    });
+
+    const updateStatement = (newStatement: string) => {
+      if (create.value) {
+        // For tenant deploy mode, we apply the statement to all stages and all tasks
+        const allTaskList = flattenTaskList<TaskCreate>(issue.value);
+        allTaskList.forEach((task) => {
+          task.statement = newStatement;
+        });
+
+        const issueCreate = issue.value as IssueCreate;
+        const context = issueCreate.createContext as UpdateSchemaContext;
+        // We also apply it back to the CreateContext
+        context.updateSchemaDetailList.forEach(
+          (detail) => (detail.statement = newStatement)
+        );
+      } else {
+        // But not editable in non-create mode
+        errorAssertion();
+      }
+    };
+
+    // We are never allowed to "apply statement to other stages" in tenant mode.
+    const allowApplyStatementToOtherStages = computed(() => false);
+
+    const doCreate = () => {
+      const issueCreate = cloneDeep(issue.value as IssueCreate);
+
+      // for multi-tenancy issue pipeline (M * N)
+      // createContext is up-to-date already
+      // so we just format the statement if needed
+      const context = issueCreate.createContext as UpdateSchemaContext;
+      context.updateSchemaDetailList.forEach((detail) => {
+        const db = databaseStore.getDatabaseById(detail.databaseId!);
+        detail.statement = maybeFormatStatementOnSave(detail.statement, db);
+      });
+
+      createIssue(issueCreate);
+    };
+
+    const logic = {
+      ...useCommonLogic(),
+      allowEditStatement,
+      selectedStatement,
+      updateStatement,
+      allowApplyStatementToOtherStages,
+      applyStatementToOtherStages: errorAssertion,
+      doCreate,
+    };
+    provideIssueLogic(logic);
+    return logic;
+  },
+  render() {
+    return this.$slots.default?.();
+  },
+});
