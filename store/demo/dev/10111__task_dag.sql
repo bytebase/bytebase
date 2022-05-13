6dd6e7238fe31bab6720bdc65ac19f85b36946b5
Yilong Li <liyilongko@gmail.com>
2022-05-13 18:30:13 +0800

    feat(PITR): add dev demo seed data (#1301)
    * feat(PITR): add dev demo seed data
    
    * chore: break up insert statements
    
    Former-commit-id: 1f504575f77aaa59902f5813e69f75c3e848e7af

--- store/demo/dev/10111__task_dag.sql
+++ store/demo/dev/10111__task_dag.sql
@@ -1,5 +1,4 @@
 -- task_dag for task dependency
-
 INSERT INTO
     task_dag (
         id,
@@ -26,4 +25,31 @@ VALUES
         11019
     );
 
-ALTER SEQUENCE task_dag_id_seq RESTART WITH 11103;
+-- task_dag for PITR tasks
+INSERT INTO
+    task_dag (
+        id,
+        from_task_id,
+        to_task_id
+    )
+VALUES
+    (
+        11103,
+        11020,
+        11021
+    );
+
+INSERT INTO
+    task_dag (
+        id,
+        from_task_id,
+        to_task_id
+    )
+VALUES
+    (
+        11104,
+        11021,
+        11022
+    );
+
+ALTER SEQUENCE task_dag_id_seq RESTART WITH 11105;
