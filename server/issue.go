e352876eaa98becd336cfe06c180d7c0d73d6bbd
Cluas <Cluas@live.cn>
2022-05-13 12:44:58 +0800

    fix: create issue error

--- server/issue.go
+++ server/issue.go
@@ -645,9 +645,6 @@ func (s *Server) getPipelineCreate(ctx context.Context, issueCreate *api.IssueCr
 			}
 
 			baseDBName := d.DatabaseName
-			if err != nil {
-				return nil, fmt.Errorf("api.GetBaseDatabaseName(%q, %q) failed, error: %v", d.DatabaseName, project.DBNameTemplate, err)
-			}
 			deployments, matrix, err := s.getTenantDatabaseMatrix(ctx, issueCreate.ProjectID, project.DBNameTemplate, dbList, baseDBName)
 			if err != nil {
 				return nil, err
