0cf95a9151724ed0cd8c2402b41fbf419aa6838e
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-05-17 12:03:20 +0800

    feat: disable the button when the db doesn't meet pitr's requirement

--- frontend/src/plugins/pitr/index.ts
+++ frontend/src/plugins/pitr/index.ts
@@ -1,8 +1,14 @@
 import { computed, Ref } from "vue";
+import semverCompare from "semver/functions/compare";
 import { Database, IssueCreate, PITRContext, SYSTEM_BOT_ID } from "@/types";
 import { useBackupListByDatabaseId, useIssueStore } from "@/store";
+import { useI18n } from "vue-i18n";
+
+export const MIN_PITR_SUPPORT_MYSQL_VERSION = "5.7.0";
 
 export const usePITRLogic = (database: Ref<Database>) => {
+  const { t } = useI18n();
+
   const backupList = useBackupListByDatabaseId(
     computed(() => database.value.id)
   );
@@ -10,7 +16,28 @@ export const usePITRLogic = (database: Ref<Database>) => {
     backupList.value.filter((backup) => backup.status === "DONE")
   );
 
-  const pitrAvailable = computed(() => doneBackupList.value.length > 0);
+  const pitrAvailable = computed((): { result: boolean; message: string } => {
+    const { engine, engineVersion } = database.value.instance;
+    if (
+      engine === "MYSQL" &&
+      semverCompare(engineVersion, MIN_PITR_SUPPORT_MYSQL_VERSION) >= 0
+    ) {
+      if (doneBackupList.value.length > 0) {
+        return { result: true, message: "ok" };
+      }
+      return {
+        result: false,
+        message: t("database.pitr.no-available-backup"),
+      };
+    }
+    return {
+      result: false,
+      message: t("database.pitr.minimum-supported-engine-and-version", {
+        engine: "MySQL",
+        min_version: MIN_PITR_SUPPORT_MYSQL_VERSION,
+      }),
+    };
+  });
 
   const createPITRIssue = async (
     pointTimeTs: number,
