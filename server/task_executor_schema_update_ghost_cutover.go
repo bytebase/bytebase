72b84750c8bca909f87249658dd60ef0233a1e44
p0ny <xz@bytebase.com>
2022-05-19 14:53:35 +0800

    feat(gh-ost): gh-ost task executor implementation (phase 2) (#1315)
    * feat(gh-ost): gh-ost task executor implementation (phase 2)
    
    * fix: address comment
    
    * fix: gh-ost replica-server-id should be unique
    
    * fix: use sync.WaitGroup instead of channel
    
    * fix: cutover task executor waits for gh-ost
    
    * refactor: get socket & postpone flag filename
    
    * fix: grant user
    
    * fix: address comment
    
    * fix: naming convention
    
    * docs: comment
    
    * Update server/task_executor_schema_update_ghost_cutover.go
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    Former-commit-id: a22e7e44ded5d4677bda254bbeeb55c0d8b2dc88

--- server/task_executor_schema_update_ghost_cutover.go
+++ server/task_executor_schema_update_ghost_cutover.go
@@ -2,6 +2,11 @@ package server
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/bytebase/bytebase/api"
 	"go.uber.org/zap"
@@ -16,10 +21,46 @@ func NewSchemaUpdateGhostCutoverTaskExecutor(logger *zap.Logger) TaskExecutor {
 
 // SchemaUpdateGhostCutoverTaskExecutor is the schema update (gh-ost) cutover task executor.
 type SchemaUpdateGhostCutoverTaskExecutor struct {
-	l *zap.Logger //nolint
+	l *zap.Logger
 }
 
 // RunOnce will run SchemaUpdateGhostCutover task once.
 func (exec *SchemaUpdateGhostCutoverTaskExecutor) RunOnce(ctx context.Context, server *Server, task *api.Task) (terminated bool, result *api.TaskRunResultPayload, err error) {
-	return true, &api.TaskRunResultPayload{Detail: "not implemented yet"}, nil
+
+	taskDAG, err := server.store.GetTaskDAGByToTaskID(ctx, task.ID)
+	if err != nil {
+		return true, nil, fmt.Errorf("failed to get a single taskDAG for schema update gh-ost cutover task, id: %v, error: %w", task.ID, err)
+	}
+	syncTaskID := taskDAG.FromTaskID
+	syncTask, err := server.store.GetTaskByID(ctx, syncTaskID)
+	if err != nil {
+		return true, nil, fmt.Errorf("failed to get schema update gh-ost sync task for cutover task, error: %w", err)
+	}
+	payload := &api.TaskDatabaseSchemaUpdateGhostSyncPayload{}
+	if err := json.Unmarshal([]byte(syncTask.Payload), payload); err != nil {
+		return true, nil, fmt.Errorf("invalid database schema update gh-ost sync payload: %w", err)
+	}
+
+	statement := strings.TrimSpace(payload.Statement)
+	tableName, err := getTableNameFromStatement(statement)
+	if err != nil {
+		return true, nil, fmt.Errorf("failed to parse table name from statement, error: %w", err)
+	}
+
+	socketFilename := getSocketFilename(syncTaskID, task.Database.ID, task.Database.Name, tableName)
+	postponeFilename := getPostponeFlagFilename(syncTaskID, task.Database.ID, task.Database.Name, tableName)
+
+	if err := os.Remove(postponeFilename); err != nil {
+		return true, nil, fmt.Errorf("failed to remove postpone flag file, error: %w", err)
+	}
+
+	ticker := time.NewTicker(time.Second * 1)
+
+	for range ticker.C {
+		if _, err := os.Stat(socketFilename); err != nil {
+			break
+		}
+	}
+
+	return true, &api.TaskRunResultPayload{Detail: "cutover done"}, nil
 }
