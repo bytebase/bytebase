6211916ff140d963e2dd5dac20a8bf9a0dce2c40
ecmadao <wlec@outlook.com>
2022-05-19 17:01:04 +0800

    feat: initial the segment (#1293)

---
+++ api/metric.go
@@ -0,0 +1,16 @@
+package api
+
+import "github.com/bytebase/bytebase/plugin/db"
+
+// InstanceCountMetric is the API message for instance count metric
+type InstanceCountMetric struct {
+	Engine        db.Type
+	EnvironmentID int
+	Count         int
+}
+
+// IssueCountMetric is the API message for issue count metric
+type IssueCountMetric struct {
+	Type  IssueType
+	Count int
+}
