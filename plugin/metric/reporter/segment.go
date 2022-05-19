02661fb0c794ac9198e4d8e02a96cf7e01fe4b2e
ecmadao <wlec@outlook.com>
2022-05-19 17:01:04 +0800

    feat: initial the segment (#1293)
    Former-commit-id: f2080cb4278d64efb9d31353957705f194dc80a4 [formerly 9975055964b5ecde002f062e7459c0d963976764] [formerly 6fc92db1b09fa50aeee9206d4d0f84f36cc62303]
    Former-commit-id: 78b3ce32272f970e69f5c6e58fc462dd235389bc
    Former-commit-id: 922d719d38a89c2b3c3aa7ee1154cb01011e7325

---
+++ plugin/metric/reporter/segment.go
@@ -0,0 +1,67 @@
+package reporter
+
+import (
+	"time"
+
+	"github.com/bytebase/bytebase/api"
+	"github.com/bytebase/bytebase/plugin/metric/collector"
+
+	"github.com/segmentio/analytics-go"
+	"go.uber.org/zap"
+)
+
+// Segment is the metrics collector https://segment.com/.
+type segment struct {
+	l          *zap.Logger
+	identifier string
+	client     analytics.Client
+}
+
+const (
+	// identifyTraitForPlan is the trait key for subscription plan.
+	identifyTraitForPlan = "plan"
+	// metricValueField is the property key for value
+	metricValueField = "value"
+)
+
+// NewSegmentReporter creates a new instance of segment
+func NewSegmentReporter(l *zap.Logger, key string, identifier string) MetricReporter {
+	client := analytics.New(key)
+
+	return &segment{
+		l:          l,
+		identifier: identifier,
+		client:     client,
+	}
+}
+
+// Close will close the segment client.
+func (s *segment) Close() {
+	s.client.Close()
+}
+
+// Report will exec all the segment reporter.
+func (s *segment) Report(metric *collector.Metric) error {
+	properties := analytics.NewProperties().
+		Set(metricValueField, metric.Value)
+
+	for key, value := range metric.Labels {
+		properties.Set(key, value)
+	}
+
+	return s.client.Enqueue(analytics.Track{
+		Event:      string(metric.Name),
+		UserId:     s.identifier,
+		Properties: properties,
+		Timestamp:  time.Now().UTC(),
+	})
+}
+
+// Identify will identify the workspace with license.
+func (s *segment) Identify(workspace *api.Workspace) error {
+	return s.client.Enqueue(analytics.Identify{
+		UserId:    s.identifier,
+		Traits:    analytics.NewTraits().Set(identifyTraitForPlan, workspace.Plan),
+		Timestamp: time.Now().UTC(),
+	})
+}
