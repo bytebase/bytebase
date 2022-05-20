02b8a8a5c5e36be5f0022f6b9a9d683ecea24d49
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-20 12:03:23 +0800

    feat: add Install function to install mysqlbinlog

---
+++ resources/mysqlbinlog/mysqlbinlog.go
@@ -0,0 +1,55 @@
+package mysqlbinlog
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+
+	"github.com/bytebase/bytebase/resources/utils"
+)
+
+// Install will extract the mysqlbinlog tar in resourceDir, returns the mysqlbinlog binary directory if no error occur.
+func Install(resourceDir string) (string, error) {
+	var tarName string
+	var version string
+	switch {
+	case runtime.GOOS == "darwin" && runtime.GOARCH == "arm64":
+		tarName = "mysqlbinlog-8.0.28-macos11-arm64.tar.gz"
+		version = "mysqlbinlog-8.0.28-macos11-arm64"
+	case runtime.GOOS == "linux" && runtime.GOARCH == "amd64":
+		tarName = "mysqlbinlog-8.0.28-linux-glibc-2.17-x86_64.tar.gz"
+		version = "mysqlbinlog-8.0.28-linux-glibc-2.17-x86_64"
+	default:
+		return "", fmt.Errorf("Unsupported combination of OS[%s] and ARCH[%s]", runtime.GOOS, runtime.GOARCH)
+	}
+
+	mysqlbinlogDir := path.Join(resourceDir, version)
+	if _, err := os.Stat(mysqlbinlogDir); err != nil {
+		if !os.IsNotExist(err) {
+			return "", fmt.Errorf("failed to check binary directory path %q, error: %w", mysqlbinlogDir, err)
+		}
+		// Install if not exist yet
+		tmpDir := path.Join(resourceDir, fmt.Sprintf("tmp-%s", version))
+		if err := os.RemoveAll(tmpDir); err != nil {
+			return "", fmt.Errorf("failed to remove mysqlbinlog binary temp directory %q, error: %w", tmpDir, err)
+		}
+
+		f, err := resources.Open(tarName)
+		if err != nil {
+			return "", fmt.Errorf("failed to find %q in embedded resources, error: %v", tarName, err)
+		}
+		defer f.Close()
+
+		if err := utils.ExtractTarGz(f, tmpDir); err != nil {
+			return "", fmt.Errorf("failed to extract tar.gz file, error: %w", err)
+		}
+
+		if err := os.Rename(tmpDir, mysqlbinlogDir); err != nil {
+			return "", fmt.Errorf("failed to rename mysqlbinlog binary directory from %q to %q, error: %w", tmpDir, mysqlbinlogDir, err)
+		}
+	}
+
+	return filepath.Join(mysqlbinlogDir, version, "bin"), nil
+}
