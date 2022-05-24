1a73da63a48dc75f51096cb51998902f49fcc9d8
ecmadao <wlec@outlook.com>
2022-05-24 09:06:08 +0800

    fix: metric identify (#1358)
    * fix: update the identify
    
    * chore: update segment key for dev env

--- bin/server/cmd/profile_dev.go
+++ bin/server/cmd/profile_dev.go
@@ -30,7 +30,7 @@ func activeProfile(dataDir string) server.Profile {
 		BackupRunnerInterval: 10 * time.Second,
 		Version:              version,
 		PgURL:                flags.pgURL,
-		MetricConnectionKey:  "Q4krM9MitLvUSlty7TAt9kAcThp2GfMH",
+		MetricConnectionKey:  "3zcZLeX3ahvlueEJqNyJysGfVAErsjjT",
 	}
 }
 
