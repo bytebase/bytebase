a03e931110172857ef36a245126e721763b0928a
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-24 16:16:45 +0800

    feat: embed mysqlbinlog8.0.28-macos11-x86_64.tar.gz (#1367)
    * feat: embed mysqlbinlog8.0.28-macos11-x86_64.tar.gz
    
    * fix: name
    
    Former-commit-id: 94a03b2dda457dcd847305a20892ba5504991e34

--- resources/mysqlbinlog/resources_darwin_arm64.go
+++ resources/mysqlbinlog/resources_darwin_arm64.go
@@ -1,10 +1,9 @@
-//go:build darwin
-// +build darwin
+//go:build darwin && arm64
+// +build darwin,arm64
 
 package mysqlbinlog
 
 import "embed"
 
-//TODO(zp): need test for darwin amd64
 //go:embed mysqlbinlog-8.0.28-macos11-arm64.tar.gz
 var resources embed.FS
