76321dee1ec5ac68a6a145e64e8016b8c7e47d38
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-05-24 11:22:03 +0800

    chore: address comments

--- frontend/src/store/modules/instance.ts
+++ frontend/src/store/modules/instance.ts
@@ -490,6 +490,10 @@ export const useInstanceStore = defineStore("instance", {
 
 export const useInstanceList = (rowStatusList?: RowStatus[]) => {
   const store = useInstanceStore();
+  // SQL Editor will visit instanceList very early.
+  // Using `watchEffect` here might get a data race here, which leads a vue's
+  // internal error.
+  // So we fetch data when "before mount" - trying to be early but not too early.
   onBeforeMount(() => store.fetchInstanceList(rowStatusList));
   return computed(() => store.getInstanceList(rowStatusList));
 };
