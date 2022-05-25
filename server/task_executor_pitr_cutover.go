9c311c8fe16fef56afb01e6efeeb978705e1a47f
Yilong Li <liyilongko@gmail.com>
2022-05-25 13:42:49 +0800

    feat(PITR): implement PITR cutover task executor (#1328)
    * feat(PITR): implement PITR cutover task executor
    
    * refactor: getIssueByPipelineID
    
    * chore: remove function
    
    * chore: remove nil err
    
    * Update server/task_executor_pitr_cutover.go
    
    Co-authored-by: tianzhou <t@bytebase.com>
    
    * chore: address reviewer's comment
    
    * chore: remove todo
    
    * chore: do not leak pitr database naming mechanism
    
    * chore: fix function description comments
    
    * refactor: add detailed comments about swapping, and delete pitr database
    
    * chore: refine comments
    
    * feat: check the pitr database is empty before dropping
    
    * refactor: swap
    
    * chore: add TODO
    
    * Update plugin/restore/mysql/mysql.go
    
    Co-authored-by: tianzhou <t@bytebase.com>
    
    * chore: remove todo
    
    * improve error message
    
    * fix compile
    
    * fix
    
    * fix test
    
    * Apply suggestions from code review
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    
    * remove the pitr delete task
    
    * refactor: remove named return values
    
    Co-authored-by: tianzhou <t@bytebase.com>
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    Former-commit-id: 924fa651243f6cb3d43c2f382056ba8632cccde7

---
+++ server/task_executor_pitr_cutover.go
@@ -0,0 +1,77 @@
+package server
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bytebase/bytebase/api"
+	pluginmysql "github.com/bytebase/bytebase/plugin/db/mysql"
+	restoremysql "github.com/bytebase/bytebase/plugin/restore/mysql"
+	"github.com/bytebase/bytebase/resources/mysqlbinlog"
+	"go.uber.org/zap"
+)
+
+// NewPITRCutoverTaskExecutor creates a PITR cutover task executor.
+func NewPITRCutoverTaskExecutor(logger *zap.Logger, instance *mysqlbinlog.Instance) TaskExecutor {
+	return &PITRCutoverTaskExecutor{
+		l:           logger,
+		mysqlbinlog: instance,
+	}
+}
+
+// PITRCutoverTaskExecutor is the PITR cutover task executor.
+type PITRCutoverTaskExecutor struct {
+	l           *zap.Logger
+	mysqlbinlog *mysqlbinlog.Instance
+}
+
+// RunOnce will run the PITR cutover task executor once.
+func (exec *PITRCutoverTaskExecutor) RunOnce(ctx context.Context, server *Server, task *api.Task) (terminated bool, result *api.TaskRunResultPayload, err error) {
+	exec.l.Info("Run PITR cutover task", zap.String("task", task.Name))
+
+	// Currently api.TaskDatabasePITRCutoverPayload is empty, so we do not need to unmarshal from task.Payload.
+
+	return exec.pitrCutover(ctx, task, server)
+}
+
+func (exec *PITRCutoverTaskExecutor) pitrCutover(ctx context.Context, task *api.Task, server *Server) (terminated bool, result *api.TaskRunResultPayload, err error) {
+	driver, err := getAdminDatabaseDriver(ctx, task.Instance, "", exec.l)
+	if err != nil {
+		return true, nil, err
+	}
+	defer driver.Close(ctx)
+
+	issue, err := getIssueByPipelineID(ctx, server.store, exec.l, task.PipelineID)
+	if err != nil {
+		return true, nil, err
+	}
+
+	mysqlDriver, ok := driver.(*pluginmysql.Driver)
+	if !ok {
+		exec.l.Error("failed to cast driver to mysql.Driver", zap.Stack("stack"))
+		return true, nil, fmt.Errorf("[internal] cast driver to mysql.Driver failed")
+	}
+	mysqlRestore := restoremysql.New(mysqlDriver, exec.mysqlbinlog)
+
+	exec.l.Info("Start swapping the original and PITR database",
+		zap.String("instance", task.Instance.Name),
+		zap.String("original_database", task.Database.Name),
+	)
+	pitrDatabaseName, pitrOldDatabaseName, err := mysqlRestore.SwapPITRDatabase(ctx, task.Database.Name, issue.CreatedTs)
+	if err != nil {
+		exec.l.Error("failed to swap the original and PITR database",
+			zap.Int("issueID", issue.ID),
+			zap.String("database", task.Database.Name),
+			zap.Stack("stack"),
+			zap.Error(err))
+		return true, nil, fmt.Errorf("failed to swap the original and PITR database, error[%w]", err)
+	}
+
+	exec.l.Info("Finish swapping the original and PITR database",
+		zap.String("original_database", task.Database.Name),
+		zap.String("pitr_database", pitrDatabaseName),
+		zap.String("old_database", pitrOldDatabaseName))
+	return true, &api.TaskRunResultPayload{
+		Detail: fmt.Sprintf("Swapped PITR database for target database %q", task.Database.Name),
+	}, nil
+}
