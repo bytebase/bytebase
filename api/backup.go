e71592f0ed871cfc90da5b68984bd66cdf1f58d5
Yilong Li <liyilongko@gmail.com>
2022-05-26 13:46:22 +0800

    Update api/backup.go
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>

--- api/backup.go
+++ api/backup.go
@@ -114,7 +114,7 @@ type Backup struct {
 	MigrationHistoryVersion string `jsonapi:"attr,migrationHistoryVersion"`
 	Path                    string `jsonapi:"attr,path"`
 	Comment                 string `jsonapi:"attr,comment"`
-	// Payload contains data like PITR info, which will not be created at first.
+	// Payload contains data such as backup position info which will not be created at first.
 	// When backup runner executes the real backup job, it will fill this field.
 	Payload BackupPayload `jsonapi:"attr,payload"`
 }
