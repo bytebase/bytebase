e9b52a23486c013e2b1aca6e469c4f0a9737926a
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-26 15:24:42 +0800

    chore: update comment

--- plugin/restore/mysql/mysql.go
+++ plugin/restore/mysql/mysql.go
@@ -43,7 +43,7 @@ func New(driver *mysql.Driver, instance *mysqlbinlog.Instance) *Restore {
 	}
 }
 
-// ReplayBinlog restores the database using incremental backup specified by infos, reading the binlog in dataDir.
+// ReplayBinlog replays the binlog from `startInfo.Position` to `stopTs` that reading from `binlogDir`.
 func (r *Restore) ReplayBinlog(ctx context.Context, originDatabase, pitrDatabase, binlogDir string, startInfo mysql.BinlogInfo, stopTs int64) error {
 	db, err := r.driver.GetDbConnection(ctx, "")
 	if err != nil {
