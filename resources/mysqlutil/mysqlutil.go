c9dcc850103d70a607d3dd974a093f0ca47987ed
h3n4l <87714218+h3n4l@users.noreply.github.com>
2022-05-27 16:49:19 +0800

    feat: embed mysqlbinlog8.0.28-macos11-x86_64.tar.gz

---
+++ resources/mysqlutil/mysqlutil.go
@@ -0,0 +1,107 @@
+package mysqlutil
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/bytebase/bytebase/resources/utils"
+)
+
+type binaryName string
+
+const (
+	// MySQL is the name of mysql binary
+	MySQL binaryName = "mysql"
+	// MySQLBinlog is the name of mysqlbinlog binary
+	MySQLBinlog binaryName = "mysqlbinary"
+)
+
+// Instance involve the path of all binaries binary.
+type Instance struct {
+	mysqlPath       string
+	mysqlbinlogPath string
+}
+
+// GetPath returns the mysqlbinlog binary path.
+func (ins *Instance) GetPath(binName binaryName) string {
+	switch binName {
+	case MySQL:
+		return ins.mysqlPath
+	case MySQLBinlog:
+		return ins.mysqlbinlogPath
+	}
+	return "UNKNOWN"
+}
+
+// Version returns the raw output of ``binName` -V`.
+func (ins *Instance) Version(binName binaryName) (string, error) {
+	var cmd *exec.Cmd
+	var version bytes.Buffer
+	switch binName {
+	case MySQL:
+		cmd = exec.Command(ins.GetPath(MySQL), "-V")
+	case MySQLBinlog:
+		cmd = exec.Command(ins.GetPath(MySQLBinlog), "-V")
+	default:
+		return "", fmt.Errorf("Unknown binary name: %s", binName)
+	}
+
+	cmd.Stdout = &version
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+	return version.String(), nil
+}
+
+// Install will extract the mysqlbinlog tar in resourceDir.
+func Install(resourceDir string) (*Instance, error) {
+	var tarName string
+	switch {
+	case runtime.GOOS == "darwin" && runtime.GOARCH == "arm64":
+		tarName = "mysqlutil-8.0.28-macos11-arm64.tar.gz"
+	case runtime.GOOS == "darwin" && runtime.GOARCH == "amd64":
+		tarName = "mysqlbinlog-8.0.28-macos11-x86_64.tar.gz"
+	case runtime.GOOS == "linux" && runtime.GOARCH == "amd64":
+		tarName = "mysqlbinlog-8.0.28-linux-glibc-2.17-x86_64.tar.gz"
+	default:
+		return nil, fmt.Errorf("Unsupported combination of OS[%s] and ARCH[%s]", runtime.GOOS, runtime.GOARCH)
+	}
+
+	version := strings.TrimRight(tarName, "tar.gz")
+	mysqlbinlogDir := path.Join(resourceDir, version)
+	if _, err := os.Stat(mysqlbinlogDir); err != nil {
+		if !os.IsNotExist(err) {
+			return nil, fmt.Errorf("failed to check binary directory path %q, error: %w", mysqlbinlogDir, err)
+		}
+		// Install if not exist yet
+		tmpDir := path.Join(resourceDir, fmt.Sprintf("tmp-%s", version))
+		if err := os.RemoveAll(tmpDir); err != nil {
+			return nil, fmt.Errorf("failed to remove mysqlbinlog binary temp directory %q, error: %w", tmpDir, err)
+		}
+
+		f, err := resources.Open(tarName)
+		if err != nil {
+			return nil, fmt.Errorf("failed to find %q in embedded resources, error: %v", tarName, err)
+		}
+		defer f.Close()
+
+		if err := utils.ExtractTarGz(f, tmpDir); err != nil {
+			return nil, fmt.Errorf("failed to extract tar.gz file, error: %w", err)
+		}
+
+		if err := os.Rename(tmpDir, mysqlbinlogDir); err != nil {
+			return nil, fmt.Errorf("failed to rename mysqlbinlog binary directory from %q to %q, error: %w", tmpDir, mysqlbinlogDir, err)
+		}
+	}
+	return &Instance{
+		mysqlPath:       filepath.Join(mysqlbinlogDir, "bin", "mysql"),
+		mysqlbinlogPath: filepath.Join(mysqlbinlogDir, "bin", "mysqlbinlog"),
+	}, nil
+}
---
+++ resources/mysqlutil/resources_darwin_amd64.go
@@ -0,0 +1,9 @@
+//go:build darwin && amd64
+// +build darwin,amd64
+
+package mysqlutil
+
+import "embed"
+
+//go:embed mysqlbinlog-8.0.28-macos11-x86_64.tar.gz
+var resources embed.FS
