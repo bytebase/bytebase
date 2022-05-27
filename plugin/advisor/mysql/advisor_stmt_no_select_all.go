7cd2fa22a2eecd276b0867fb1e89e134b396a664
rebelice <yangrebelice@gmail.com>
2022-05-27 12:49:30 +0800

    chore(schema system): format SQL text in error content (#1403)
    * chore(schema system): format SQL text in error content
    
    * chore: address comment
    
    * refactor: use %s instead of %q
    
    Former-commit-id: 263aafd3ae35ec05eed1e40911864025f15ca4c4 [formerly a5de86bb13fe5d737839227f3abac4c2bb723c88] [formerly 81f34dc173cecce540ab5d9356b49c65b01b647d]
    Former-commit-id: d41b80bcd427cf5cf5cf3ee62690b9cda661b9d9
    Former-commit-id: 248203a02d4babef72fcafbf906c4e908cb75071

--- plugin/advisor/mysql/advisor_stmt_no_select_all.go
+++ plugin/advisor/mysql/advisor_stmt_no_select_all.go
@@ -65,7 +65,7 @@ func (v *noSelectAllChecker) Enter(in ast.Node) (ast.Node, bool) {
 					Status:  v.level,
 					Code:    common.StatementSelectAll,
 					Title:   "Not SELECT all",
-					Content: fmt.Sprintf("%q uses SELECT all", v.text),
+					Content: fmt.Sprintf("\"%s\" uses SELECT all", v.text),
 				})
 				break
 			}
