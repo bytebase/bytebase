1119a3b6bdbeccf935573b6929e659a0a317198b
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-05-27 14:00:10 +0800

    feat: check issue/task status transition again before patching (#1404)
    Former-commit-id: 871b1a0d2a33aa401858284f8d1799338bf9ea43
    Former-commit-id: 79baa3ce7b1eba78686c74cb9f1e1040826fd21b

---
+++ frontend/src/components/Issue/logic/transition.ts
@@ -0,0 +1,113 @@
+import { useCurrentUser } from "@/store";
+import {
+  Issue,
+  SYSTEM_BOT_ID,
+  ONBOARDING_ISSUE_ID,
+  IssueStatusTransitionType,
+  ASSIGNEE_APPLICABLE_ACTION_LIST,
+  CREATOR_APPLICABLE_ACTION_LIST,
+  ISSUE_STATUS_TRANSITION_LIST,
+  IssueStatusTransition,
+} from "@/types";
+import {
+  allTaskList,
+  applicableTaskTransition,
+  isDBAOrOwner,
+  TaskStatusTransition,
+} from "@/utils";
+import { useIssueLogic } from ".";
+
+export const getApplicableIssueStatusTransitionList = (
+  issue: Issue
+): IssueStatusTransition[] => {
+  const currentUser = useCurrentUser();
+  const { activeTaskOfPipeline } = useIssueLogic();
+  const currentTask = activeTaskOfPipeline(issue.pipeline);
+
+  const issueEntity = issue as Issue;
+  if (issueEntity.id == ONBOARDING_ISSUE_ID) {
+    return [];
+  }
+  const list: IssueStatusTransitionType[] = [];
+  // Allow assignee, or assignee is the system bot and current user is DBA or owner
+  if (
+    currentUser.value.id === issueEntity.assignee?.id ||
+    (issueEntity.assignee?.id == SYSTEM_BOT_ID &&
+      isDBAOrOwner(currentUser.value.role))
+  ) {
+    list.push(...ASSIGNEE_APPLICABLE_ACTION_LIST.get(issueEntity.status)!);
+  }
+  if (currentUser.value.id === issueEntity.creator.id) {
+    CREATOR_APPLICABLE_ACTION_LIST.get(issueEntity.status)!.forEach((item) => {
+      if (list.indexOf(item) == -1) {
+        list.push(item);
+      }
+    });
+  }
+
+  return list
+    .filter((item) => {
+      const pipeline = issueEntity.pipeline;
+      // Disallow any issue status transition if the active task is in RUNNING state.
+      if (currentTask.status == "RUNNING") {
+        return false;
+      }
+
+      const taskList = allTaskList(pipeline);
+      // Don't display the Resolve action if the last task is NOT in DONE status.
+      if (
+        item == "RESOLVE" &&
+        taskList.length > 0 &&
+        (currentTask.id != taskList[taskList.length - 1].id ||
+          currentTask.status != "DONE")
+      ) {
+        return false;
+      }
+
+      return true;
+    })
+    .map(
+      (type: IssueStatusTransitionType) =>
+        ISSUE_STATUS_TRANSITION_LIST.get(type)!
+    )
+    .reverse();
+};
+
+export const getApplicableTaskStatusTransitionList = (
+  issue: Issue
+): TaskStatusTransition[] => {
+  const currentUser = useCurrentUser();
+  if (issue.id == ONBOARDING_ISSUE_ID) {
+    return [];
+  }
+  switch (issue.status) {
+    case "DONE":
+    case "CANCELED":
+      return [];
+    case "OPEN": {
+      let list: TaskStatusTransition[] = [];
+
+      // Allow assignee, or assignee is the system bot and current user is DBA or owner
+      if (
+        currentUser.value.id === issue.assignee?.id ||
+        (issue.assignee?.id == SYSTEM_BOT_ID &&
+          isDBAOrOwner(currentUser.value.role))
+      ) {
+        list = applicableTaskTransition(issue.pipeline);
+      }
+
+      return list;
+    }
+  }
+  console.assert(false, "Should never reach this line");
+};
+
+export function isApplicableTransition<
+  T extends IssueStatusTransition | TaskStatusTransition
+>(target: T, list: T[]): boolean {
+  return (
+    list.findIndex((applicable) => {
+      return applicable.to === target.to && applicable.type === target.type;
+    }) >= 0
+  );
+}
