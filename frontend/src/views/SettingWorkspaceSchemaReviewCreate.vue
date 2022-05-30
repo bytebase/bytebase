2db17efae6e7d53c8e4a8e34e0d24df42cd0bd09
ecmadao <wlec@outlook.com>
2022-05-30 19:57:37 +0800

    chore: feature guard update for schema system (#1434)
    * chore: remove the feature guard
    
    * chore: remove feature guard in the env form
    
    * fix: i18n
    
    * chore: add feature attention
    
    * chore: add feature modal for policy creation
    
    Former-commit-id: ff9c36bdebe980f374fd83e21679942146d435c6
    Former-commit-id: b023237b11090969dbe1b664bff691a2cc7831dd

--- frontend/src/views/SettingWorkspaceSchemaReviewCreate.vue
+++ frontend/src/views/SettingWorkspaceSchemaReviewCreate.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="my-4 space-y-4 divide-y divide-block-border">
+    <FeatureAttention
+      v-if="!hasSchemaReviewPolicyFeature"
+      custom-class="mb-5"
+      feature="bb.feature.schema-review-policy"
+      :description="
+        $t('subscription.features.bb-feature-schema-review-policy.desc')
+      "
+    />
     <SchemaReviewCreation
       :selected-rule-list="[]"
       :selected-environment="environment"
@@ -9,12 +17,16 @@
 
 <script lang="ts" setup>
 import { computed } from "vue";
-import { useEnvironmentStore } from "@/store";
+import { useEnvironmentStore, featureToRef } from "@/store";
 import { EMPTY_ID } from "@/types";
 
 const url = new URL(window.location.href);
 const params = new URLSearchParams(url.search);
 const environmentId = params.get("environmentId") ?? "";
+
+const hasSchemaReviewPolicyFeature = featureToRef(
+  "bb.feature.schema-review-policy"
+);
 
 const environment = computed(() => {
   if (!environmentId || Number.isNaN(environmentId)) {
