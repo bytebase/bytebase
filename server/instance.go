647539e176d14d931db59e5019764c2f2933a22e
qsliu <qsliu2017@outlook.com>
2022-06-06 15:36:31 +0800

    address comments

--- server/instance.go
+++ server/instance.go
@@ -157,7 +157,7 @@ func (s *Server) registerInstanceRoutes(g *echo.Group) {
 					return echo.NewHTTPError(http.StatusInternalServerError,
 						fmt.Errorf("Failed to find databases in the instance %d", id)).SetInternal(err)
 				}
-				databaseNameList := make([]string, 0)
+				var databaseNameList []string
 				for _, database := range databases {
 					if database.ProjectID != api.DefaultProjectID {
 						databaseNameList = append(databaseNameList, database.Name)
@@ -165,7 +165,8 @@ func (s *Server) registerInstanceRoutes(g *echo.Group) {
 				}
 				if len(databaseNameList) > 0 {
 					return echo.NewHTTPError(http.StatusBadRequest,
-						"You should transfer these databases to the default project before archiving the instance: "+strings.Join(databaseNameList, ", "))
+						"You should transfer these databases to the default project before archiving the instance: "+
+							strings.Join(databaseNameList, ", ")+".")
 				}
 			}
 			instancePatched, err = s.store.PatchInstance(ctx, instancePatch)
