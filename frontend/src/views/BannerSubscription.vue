91b74ad185e1ba5264b52ddb7e4bf9fe170c6c64
ecmadao <wlec@outlook.com>
2022-06-15 12:18:26 +0800

    chore: update subscription and banner

--- frontend/src/views/BannerSubscription.vue
+++ frontend/src/views/BannerSubscription.vue
@@ -1,40 +1,44 @@
 <template>
-  <div v-if="show" class="bg-accent">
+  <div class="bg-warning">
     <div class="mx-auto py-3 px-3">
       <div class="flex items-center justify-between flex-wrap">
         <div class="w-0 flex-1 flex items-center">
           <p class="ml-3 font-medium text-white truncate">
-            <span>
+            <span v-if="isExpired">
+              {{
+                $t("banner.license-expires", {
+                  plan: currentPlan,
+                  expireAt: expireAt,
+                })
+              }}
+            </span>
+            <span v-else-if="isTrialing">
               {{
                 $t("banner.trial-expires", {
                   plan: currentPlan,
-                  days: nextPaymentDays,
+                  days: daysBeforeExpire,
+                  expireAt: expireAt,
                 })
               }}
             </span>
           </p>
         </div>
         <div
-          class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto"
+          class="order-3 mt-2 mr-3 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto"
         >
           <a
-            :href="extendTrialingEmail"
             target="_self"
+            href="https://hub.bytebase.com/subscription?source=console.banner"
             class="flex items-center justify-center p-2 border border-transparent rounded-md shadow-sm text-base font-medium text-accent bg-white hover:bg-indigo-50"
           >
-            {{ $t("banner.extend-trial") }}
+            {{
+              $t(
+                isTrialing
+                  ? "subscription.description-highlight"
+                  : "banner.update-license"
+              )
+            }}
           </a>
-        </div>
-        <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
-          <button
-            type="button"
-            class="-mr-1 flex p-2 rounded-md hover:bg-accent-hover focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
-            @click.prevent="show = false"
-          >
-            <span class="sr-only">{{ $t("common.dismiss") }}</span>
-            <!-- Heroicon name: outline/x -->
-            <heroicons-outline:x class="h-6 w-6 text-white" />
-          </button>
         </div>
       </div>
     </div>
@@ -42,18 +46,17 @@
 </template>
 
 <script lang="ts">
-import { computed, ref } from "vue";
+import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 import { PlanType } from "../types";
 import { useSubscriptionStore } from "@/store";
 import { storeToRefs } from "pinia";
 
 export default {
-  name: "BannerTrial",
+  name: "BannerSubscription",
   setup() {
     const subscriptionStore = useSubscriptionStore();
     const { t } = useI18n();
-    const show = ref(true);
 
     const emailBody = [
       "Hi bytebase support,\n",
@@ -75,12 +78,21 @@ export default {
       }
     });
 
-    const { nextPaymentDays } = storeToRefs(subscriptionStore);
+    const {
+      expireAt,
+      isExpired,
+      isTrialing,
+      isNearExpireTime,
+      daysBeforeExpire,
+    } = storeToRefs(subscriptionStore);
 
     return {
-      show,
       currentPlan,
-      nextPaymentDays,
+      expireAt,
+      isTrialing,
+      isExpired: true,
+      isNearExpireTime,
+      daysBeforeExpire,
       extendTrialingEmail: `mailto:support@bytebase.com?subject=Request to extend trial&body=${encodeURIComponent(
         emailBody
       )}`,
