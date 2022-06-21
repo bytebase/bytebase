2f8d313fe786ac897db28e3f107a34469361a4a9
Yilong Li <liyilongko@gmail.com>
2022-06-21 12:48:24 +0800

    refactor(PITR): download binlog files (#1547)
    * refactor: part 1 of "feat(PITR) download binlog files to targetTs"
    
    * refactor: revert some changes
    
    * refactor: revert some changes
    
    * chore: revert os.Remove() to make more explicit
    
    * refactor: avoid negative if
    
    * test: add case 999999
    
    * refactor: extract common helpers
    
    * fix: sort files on server
    
    * refactor: get latest binlog file on server
    
    * refactor: sort local binlog files
    
    * feat(PITR): download binlog files to targetTs
    
    * refactor: decouple seq parsing and sorting
    
    * chore: remove unused test case
    
    * chore: update test
    
    * chore: remove unused function
    
    * test: add empty case
    
    * fix: test
    
    * refactor: address reviewer's comments
    
    * fix: sort
    
    * refactor: address reviewer's comments
    
    * refactor: extract large function
    
    * refactor: extract large function
    
    * refactor: extract large function
    
    * refactor: download all binlog files
    
    * chore: reword
    
    * chore: remove unused function
    
    * refactor: simplify downloading
    
    * chore: address reviewer's comment
    
    Former-commit-id: 878a67c23848cd1ed3889d94c48be3b6391b87e2
    Former-commit-id: 248d30f597812868636c63b2cb31adbd6102da47

--- tests/mysql_test.go
+++ tests/mysql_test.go
@@ -3,13 +3,21 @@ package tests
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
 	"strconv"
 	"testing"
+	"time"
 
+	"github.com/bytebase/bytebase/common/log"
 	pluginmysql "github.com/bytebase/bytebase/plugin/db/mysql"
 	restoremysql "github.com/bytebase/bytebase/plugin/restore/mysql"
 	resourcemysql "github.com/bytebase/bytebase/resources/mysql"
+	"github.com/bytebase/bytebase/resources/mysqlutil"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zapcore"
 )
 
 func TestCheckEngineInnoDB(t *testing.T) {
@@ -119,4 +127,119 @@ func TestCheckServerVersionAndBinlogForPITR(t *testing.T) {
 	// binlog format is default to ROW in MySQL 8.0.
 	err = mysqlRestore.CheckBinlogRowFormat(ctx)
 	a.NoError(err)
+}
+
+func TestFetchBinlogFiles(t *testing.T) {
+	t.Parallel()
+	a := require.New(t)
+	ctx := context.Background()
+	log.SetLevel(zapcore.DebugLevel)
+
+	port := getTestPort(t.Name())
+	_, stopFn := resourcemysql.SetupTestInstance(t, port)
+	defer stopFn()
+
+	db, err := connectTestMySQL(port, "")
+	a.NoError(err)
+	defer db.Close()
+
+	driver, err := getTestMySQLDriver(ctx, strconv.Itoa(port), "")
+	a.NoError(err)
+	defer driver.Close(ctx)
+
+	connCfg := getMySQLConnectionConfig(strconv.Itoa(port), "")
+	mysqlDriver, ok := driver.(*pluginmysql.Driver)
+	a.Equal(true, ok)
+	utilDir := t.TempDir()
+	utilInstance, err := mysqlutil.Install(utilDir)
+	a.NoError(err)
+	binlogDir := t.TempDir()
+	mysqlRestore := restoremysql.New(mysqlDriver, utilInstance, connCfg, binlogDir)
+
+	// init schema
+	_, err = db.ExecContext(ctx, `
+		CREATE DATABASE test;
+		USE test;
+		CREATE TABLE tbl (id int);
+		`)
+	a.NoError(err)
+
+	// Rotate to create multiple binlog files for test.
+	var startTsList []int64
+	numRotate := 10
+	// There'll be `numBinlogFiles` binlog files generated, and the last one contains no actual data.
+	numBinlogFiles := numRotate + 1
+	for i := 0; i < numRotate; i++ {
+		// Record the start event ts of the current binlog file.
+		startTsList = append(startTsList, time.Now().Unix())
+		// Insert some data to grow the current binlog file.
+		_, err = db.ExecContext(ctx, fmt.Sprintf(`
+			USE test;
+			INSERT INTO tbl VALUES (%d);
+			`, i))
+		a.NoError(err)
+		// Rotate the binlog file.
+		_, err = db.ExecContext(ctx, "FLUSH BINARY LOGS;")
+		a.NoError(err)
+	}
+	t.Logf("startTsList: %v\n", startTsList)
+
+	binlogFilesOnServerSorted, err := mysqlRestore.GetSortedBinlogFilesMetaOnServer(ctx)
+	a.NoError(err)
+
+	t.Log("Download binlog files in empty dir")
+	binlogFilesBefore, err := ioutil.ReadDir(binlogDir)
+	a.NoError(err)
+	for _, file := range binlogFilesBefore {
+		path := filepath.Join(binlogDir, file.Name())
+		err = os.Remove(path)
+		a.NoError(err)
+	}
+	err = mysqlRestore.FetchAllBinlogFiles(ctx)
+	a.NoError(err)
+	binlogFilesDownloaded, err := restoremysql.GetSortedLocalBinlogFiles(binlogDir)
+	a.NoError(err)
+	a.Equal(numBinlogFiles, len(binlogFilesDownloaded))
+	for j := range binlogFilesDownloaded {
+		a.Equal(binlogFilesOnServerSorted[j].Name, binlogFilesDownloaded[j].Name)
+		a.Equal(binlogFilesOnServerSorted[j].Size, binlogFilesDownloaded[j].Size)
+	}
+
+	t.Log("Truncate or delete downloaded files and re-download")
+	rand.Seed(time.Now().Unix())
+	// Fetch and randomly truncate/delete some binlog files.t.Log("Clean up binlog dir")
+	binlogFiles, err := ioutil.ReadDir(binlogDir)
+	a.NoError(err)
+	for _, file := range binlogFiles {
+		path := filepath.Join(binlogDir, file.Name())
+		err = os.Remove(path)
+		a.NoError(err)
+	}
+	t.Log("Fetch binlog files")
+	err = mysqlRestore.FetchAllBinlogFiles(ctx)
+	a.NoError(err)
+	binlogFilesDownloaded, err = restoremysql.GetSortedLocalBinlogFiles(binlogDir)
+	a.NoError(err)
+	t.Logf("Downloaded %d files to empty dir", len(binlogFilesDownloaded))
+	truncateIndex := rand.Intn(numBinlogFiles)
+	path := filepath.Join(binlogDir, binlogFilesDownloaded[truncateIndex].Name)
+	t.Logf("Truncating file %s", binlogFilesDownloaded[truncateIndex].Name)
+	err = os.Truncate(path, 1)
+	a.NoError(err)
+	deleteIndex := rand.Intn(numBinlogFiles)
+	path = filepath.Join(binlogDir, binlogFilesDownloaded[deleteIndex].Name)
+	t.Logf("Deleting file %s", binlogFilesDownloaded[deleteIndex].Name)
+	err = os.Remove(path)
+	a.NoError(err)
+	// Re-download and check.
+	t.Log("Re-downloading binlog files")
+	err = mysqlRestore.FetchAllBinlogFiles(ctx)
+	a.NoError(err)
+	binlogFilesDownloadedAgain, err := restoremysql.GetSortedLocalBinlogFiles(binlogDir)
+	a.NoError(err)
+	a.Equal(numBinlogFiles, len(binlogFilesDownloadedAgain))
+	for i := range binlogFilesDownloadedAgain {
+		a.Equal(binlogFilesOnServerSorted[i].Name, binlogFilesDownloadedAgain[i].Name)
+		a.Equal(binlogFilesOnServerSorted[i].Size, binlogFilesDownloadedAgain[i].Size)
+	}
 }
