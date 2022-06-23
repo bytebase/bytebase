0d1a29300dab6b10647667412619eb0c0747fbb8
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-06-23 16:43:53 +0800

    chore: move dump/restore driver functions to dump.go (#1618)
    * chore: move sync schema utils to separate sync.go to reduce the main driver file size
    
    * chore: move migration related functions to migrate.go in the driver
    
    * chore: move dump/restore driver functions to dump.go
    
    Former-commit-id: bf563cb60ab3e833726ce4799814299bfc6abb45 [formerly 34d4b251cc0881b8e60fa7c3f65c95345c512094] [formerly e09dd5eabe4c673883d4c6b7ee44c2fd092a1535]
    Former-commit-id: aa01368037da4d43c8983b690988f0870fb38b8d
    Former-commit-id: 31d85f592c44b9f23264f8eb3cf9897e6e38dd2a

--- plugin/db/pg/pg.go
+++ plugin/db/pg/pg.go
@@ -5,14 +5,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"io"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"regexp"
 	"strings"
-
-	"github.com/bytebase/bytebase/common/log"
 
 	// Import pg driver.
 	// init() in pgx/v4/stdlib will register it's pgx driver
@@ -183,6 +177,25 @@ func (driver *Driver) GetDbConnection(ctx context.Context, database string) (*sq
 	return driver.db, nil
 }
 
+// getDatabases gets all databases of an instance.
+func (driver *Driver) getDatabases() ([]*pgDatabaseSchema, error) {
+	var dbs []*pgDatabaseSchema
+	rows, err := driver.db.Query("SELECT datname, pg_encoding_to_char(encoding), datcollate FROM pg_database;")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var d pgDatabaseSchema
+		if err := rows.Scan(&d.name, &d.encoding, &d.collate); err != nil {
+			return nil, err
+		}
+		dbs = append(dbs, &d)
+	}
+	return dbs, nil
+}
+
 // GetVersion gets the version of Postgres server.
 func (driver *Driver) GetVersion(ctx context.Context) (string, error) {
 	query := "SHOW server_version"
@@ -326,154 +339,6 @@ func (driver *Driver) Query(ctx context.Context, statement string, limit int) ([
 	return util.Query(ctx, driver.db, statement, limit)
 }
 
-// Dump and restore
-
-// Dump dumps the database.
-func (driver *Driver) Dump(ctx context.Context, database string, out io.Writer, schemaOnly bool) (string, error) {
-	// pg_dump -d dbName --schema-only+
-
-	// Find all dumpable databases
-	databases, err := driver.getDatabases()
-	if err != nil {
-		return "", fmt.Errorf("failed to get databases: %s", err)
-	}
-
-	var dumpableDbNames []string
-	if database != "" {
-		exist := false
-		for _, n := range databases {
-			if n.name == database {
-				exist = true
-				break
-			}
-		}
-		if !exist {
-			return "", fmt.Errorf("database %s not found", database)
-		}
-		dumpableDbNames = []string{database}
-	} else {
-		for _, n := range databases {
-			if systemDatabases[n.name] {
-				continue
-			}
-			dumpableDbNames = append(dumpableDbNames, n.name)
-		}
-	}
-
-	for _, dbName := range dumpableDbNames {
-		includeUseDatabase := len(dumpableDbNames) > 1
-		if err := driver.dumpOneDatabaseWithPgDump(ctx, dbName, out, schemaOnly, includeUseDatabase); err != nil {
-			return "", err
-		}
-	}
-
-	return "", nil
-}
-
-// Restore restores a database.
-func (driver *Driver) Restore(ctx context.Context, sc *bufio.Scanner) (err error) {
-	txn, err := driver.db.BeginTx(ctx, nil)
-	if err != nil {
-		return err
-	}
-	defer txn.Rollback()
-
-	f := func(stmt string) error {
-		if _, err := txn.Exec(stmt); err != nil {
-			return err
-		}
-		return nil
-	}
-
-	if err := util.ApplyMultiStatements(sc, f); err != nil {
-		return err
-	}
-
-	if err := txn.Commit(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// RestoreTx restores the database in the given transaction.
-func (driver *Driver) RestoreTx(ctx context.Context, tx *sql.Tx, sc *bufio.Scanner) error {
-	return fmt.Errorf("Unimplemented")
-}
-
-func (driver *Driver) dumpOneDatabaseWithPgDump(ctx context.Context, database string, out io.Writer, schemaOnly bool, includeUseDatabase bool) error {
-	var args []string
-	args = append(args, fmt.Sprintf("--username=%s", driver.config.Username))
-	if driver.config.Password == "" {
-		args = append(args, "--no-password")
-	}
-	args = append(args, fmt.Sprintf("--host=%s", driver.config.Host))
-	args = append(args, fmt.Sprintf("--port=%s", driver.config.Port))
-	if schemaOnly {
-		args = append(args, "--schema-only")
-	}
-	args = append(args, "--inserts")
-	args = append(args, "--use-set-session-authorization")
-	args = append(args, database)
-	pgDumpPath := filepath.Join(driver.pgInstanceDir, "bin", "pg_dump")
-	cmd := exec.Command(pgDumpPath, args...)
-	if driver.config.Password != "" {
-		// Unlike MySQL, PostgreSQL does not support specifying commands in commands, we can do this by means of environment variables.
-		cmd.Env = append(cmd.Env, fmt.Sprintf("PGPASSWORD=%s", driver.config.Password))
-	}
-	cmd.Stderr = os.Stderr
-	r, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-	s := bufio.NewScanner(r)
-	previousLineComment := false
-	previousLineEmpty := false
-	for s.Scan() {
-		line := s.Text()
-		// Skip "SET SESSION AUTHORIZATION" till we can support it.
-		if strings.HasPrefix(line, "SET SESSION AUTHORIZATION ") {
-			continue
-		}
-		// Skip comment lines.
-		if strings.HasPrefix(line, "--") {
-			previousLineComment = true
-			continue
-		}
-		if previousLineComment && line == "" {
-			previousLineComment = false
-			continue
-		}
-		previousLineComment = false
-		// Skip extra empty lines.
-		if line == "" {
-			if previousLineEmpty {
-				continue
-			}
-			previousLineEmpty = true
-		} else {
-			previousLineEmpty = false
-		}
-
-		if _, err := io.WriteString(out, line); err != nil {
-			return err
-		}
-		if _, err := io.WriteString(out, "\n"); err != nil {
-			return err
-		}
-	}
-	if s.Err() != nil {
-		log.Warn(s.Err().Error())
-	}
-	if err := cmd.Wait(); err != nil {
-		return err
-	}
-	return nil
-}
-
 func (driver *Driver) switchDatabase(dbName string) error {
 	if driver.db != nil {
 		if err := driver.db.Close(); err != nil {
@@ -489,43 +354,4 @@ func (driver *Driver) switchDatabase(dbName string) error {
 	driver.db = db
 	driver.databaseName = dbName
 	return nil
-}
-
-// getDatabases gets all databases of an instance.
-func (driver *Driver) getDatabases() ([]*pgDatabaseSchema, error) {
-	var dbs []*pgDatabaseSchema
-	rows, err := driver.db.Query("SELECT datname, pg_encoding_to_char(encoding), datcollate FROM pg_database;")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var d pgDatabaseSchema
-		if err := rows.Scan(&d.name, &d.encoding, &d.collate); err != nil {
-			return nil, err
-		}
-		dbs = append(dbs, &d)
-	}
-	return dbs, nil
-}
-
-func (driver *Driver) strictUseDb() bool {
-	return len(driver.strictDatabase) != 0
-}
-
-// quoteIdentifier will quote identifiers including keywords, capital characters, or special characters.
-func quoteIdentifier(s string) string {
-	quote := false
-	if reserved[strings.ToUpper(s)] {
-		quote = true
-	}
-	if !ident.MatchString(s) {
-		quote = true
-	}
-	if quote {
-		return fmt.Sprintf("\"%s\"", strings.ReplaceAll(s, "\"", "\"\""))
-	}
-	return s
-
 }
