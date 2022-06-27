33a9905486f7729ce59cfe39308b04f11bcf13cd
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-06-27 10:34:20 +0800

    fix: support new gh-ost table name format

--- frontend/src/components/TableTable.vue
+++ frontend/src/components/TableTable.vue
@@ -146,11 +146,11 @@ export default defineComponent({
 function isGhostTable(table: Table): boolean {
   const { name } = table;
   // for future name support with timestamp, e.g. _table_1234567890_del
-  if (name.match(/^_(.+?)_(\d+)_(ghc|gho|del)$/)) {
+  if (name.match(/^(_|~)(.+?)_(\d+)_(ghc|gho|del)$/)) {
     return true;
   }
   // for legacy name support without timestamp, e.g. _table_del
-  if (name.match(/^_(.+?)_(ghc|gho|del)$/)) {
+  if (name.match(/^(_|~)(.+?)_(ghc|gho|del)$/)) {
     return true;
   }
 
