5dc5485b414754290ca45658ba6b9337f6b5b23c
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-06-28 15:23:34 +0800

    feat(frontend): add feature gate to gh-ost (#1665)
    Former-commit-id: 0ba4b1ba7f33993108e720075fff60fc0581c23a [formerly 221b37c544f8e56cbb63da7c6c5bc3263c461bb3] [formerly 5e522bb7769f997f5bb128c9d418a0cd70c8d08e]
    Former-commit-id: 9bda2ac575585902e6f76d681ee1261671a6915b
    Former-commit-id: 63ceaa256fcbb2a951a82be90bfb1e61ba26a5e0

--- frontend/src/components/AlterSchemaPrepForm/GhostDialog.vue
+++ frontend/src/components/AlterSchemaPrepForm/GhostDialog.vue
@@ -3,6 +3,7 @@
     ref="dialog"
     :title="$t('issue.migration-mode.title')"
     :positive-text="$t('common.next')"
+    :on-before-positive-click="checkFeature"
     data-label="bb-migration-mode-dialog"
   >
     <div class="w-[28rem] space-y-4 pl-8 pr-2 pb-4">
@@ -32,8 +33,12 @@
           value="online"
         />
         <div @click="state.mode = 'online'">
-          <div class="textlabel space-x-2">
+          <div class="textlabel flex items-center space-x-2">
             <span>{{ $t("issue.migration-mode.online.title") }}</span>
+            <FeatureBadge
+              feature="bb.feature.online-migration"
+              class="text-accent"
+            />
             <BBBetaBadge />
           </div>
           <div class="textinfolabel mt-1">
@@ -49,24 +54,48 @@
       </div>
     </div>
   </BBDialog>
+
+  <FeatureModal
+    v-if="state.showFeatureModal"
+    feature="bb.feature.online-migration"
+    @cancel="state.showFeatureModal = false"
+  />
 </template>
 
 <script lang="ts" setup>
 import { reactive, ref } from "vue";
 import { BBDialog } from "@/bbkit";
 import LearnMoreLink from "../LearnMoreLink.vue";
+import { featureToRef } from "@/store";
 
 type Mode = "normal" | "online";
 
 type LocalState = {
   mode: Mode;
+  showFeatureModal: boolean;
 };
 
 const dialog = ref<InstanceType<typeof BBDialog> | null>(null);
 
 const state = reactive<LocalState>({
   mode: "normal",
+  showFeatureModal: false,
 });
+
+const hasGhostFeature = featureToRef("bb.feature.online-migration");
+
+const checkFeature = (): boolean => {
+  if (state.mode === "normal") {
+    // Don't block anything when selecting normal migration
+    return true;
+  }
+
+  if (!hasGhostFeature.value) {
+    state.showFeatureModal = true;
+    return false;
+  }
+  return true;
+};
 
 const open = (): Promise<{ result: boolean; mode: Mode }> => {
   state.mode = "normal"; // reset state
