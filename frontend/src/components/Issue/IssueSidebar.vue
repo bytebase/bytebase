dc2d6329128e64c6e133d094b84177c9addc85f2
boojack <imrealleonardo@gmail.com>
2022-06-28 14:07:05 +0800

    chore(demo guide): support keyboard change event (#1662)
    Former-commit-id: 1acf624a593051ccb811bdb71577f3d2f8f5f619

--- frontend/src/components/Issue/IssueSidebar.vue
+++ frontend/src/components/Issue/IssueSidebar.vue
@@ -24,13 +24,14 @@
         }}<span v-if="create" class="text-red-600">*</span>
       </h2>
       <!-- Only DBA can be assigned to the issue -->
-      <div class="col-span-2">
+      <div class="col-span-2" data-label="bb-assignee-select-container">
         <!-- eslint-disable vue/attribute-hyphenation -->
         <MemberSelect
           class="w-full"
           :disabled="!allowEditAssignee"
           :selectedId="create ? (issue as IssueCreate).assigneeId : (issue as Issue).assignee?.id"
           :allowed-role-list="['OWNER', 'DBA']"
+          data-label="bb-assignee-select"
           @select-principal-id="
             (principalId: number) => {
               updateAssigneeId(principalId)
