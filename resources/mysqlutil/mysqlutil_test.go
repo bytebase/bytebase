a53b7b727e9d1363f22dd401eed66d7b6add7485
h3n4l <oysterdays@gmail.com>
2022-06-30 17:55:22 +0800

    feat: reinstall mysqlbinlog if libncurses5 does not exist

--- resources/mysqlutil/mysqlutil_test.go
+++ resources/mysqlutil/mysqlutil_test.go
@@ -1,6 +1,9 @@
 package mysqlutil
 
 import (
+	"os"
+	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -23,6 +26,41 @@ func TestRunBinary(t *testing.T) {
 
 	t.Run("run mysqlbinlog", func(t *testing.T) {
 		_, err := ins.Version(MySQLBinlog)
+		a.NoError(err)
+	})
+}
+
+// TODO(zp): remove this test when remove the related block in mysqlutil.go.
+// TestReinstall tests is it possible to reinstall mysqlutil.
+func TestReinstallOnLinuxAmd64(t *testing.T) {
+	t.Parallel()
+
+	if !(runtime.GOOS == "linux" && runtime.GOARCH == "amd64") {
+		return
+	}
+
+	a := require.New(t)
+	tmpDir := t.TempDir()
+	ins, err := Install(tmpDir)
+	a.NoError(err)
+
+	t.Run("run mysql client", func(t *testing.T) {
+		_, err := ins.Version(MySQL)
+		a.NoError(err)
+	})
+
+	os.Remove(filepath.Join(ins.libraryPath, "libncurses.so.5"))
+
+	t.Run("run mysql client after drop libncurses.so.5", func(t *testing.T) {
+		_, err := ins.Version(MySQL)
+		a.Error(err)
+	})
+
+	ins, err = Install(tmpDir)
+	a.NoError(err)
+
+	t.Run("run mysql client after resinstall", func(t *testing.T) {
+		_, err := ins.Version(MySQL)
 		a.NoError(err)
 	})
 }
