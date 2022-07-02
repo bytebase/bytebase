42813fe89bf1154866d3a16c668140b36c6d4ba3
ecmadao <wlec@outlook.com>
2022-07-02 12:15:01 +0800

    chore: use nanoseconds for latency

--- server/openapi_metric.go
+++ server/openapi_metric.go
@@ -24,7 +24,7 @@ func openAPIMetricMiddleware(s *Server, next echo.HandlerFunc) echo.HandlerFunc 
 					Name:  metricAPI.OpenAPIMetricName,
 					Value: 1,
 					Labels: map[string]string{
-						"latency_ms":     strconv.FormatInt(duration.Milliseconds(), 10),
+						"latency_ns":     strconv.FormatInt(duration.Nanoseconds(), 10),
 						"request_method": requestMethod,
 						"request_path":   requestPath,
 						"response_code":  strconv.Itoa(responseCode),
