69703e8b8d87f1d6e4d41c06857a4d31bb8d0ff8
rebelice <yangrebelice@gmail.com>
2022-07-04 15:51:09 +0800

    feat: support PostgreSQL ALTER TABLE RENAME/ADD/DROP CONSTRAINT and ADD COLUMN CONSTRAINT

--- plugin/parser/engine/pg/convert.go
+++ plugin/parser/engine/pg/convert.go
@@ -25,19 +25,40 @@ func convert(node *pgquery.Node) (ast.Node, error) {
 						return nil, parser.NewConvertErrorf("expected ColumnDef but found %t", alterCmd.Def.Node)
 					}
 
+					column, err := convertColumnDef(def)
+					if err != nil {
+						return nil, err
+					}
+
 					addColumn := &ast.AddColumnListStmt{
-						Table: alterTable.Table,
-						ColumnList: []*ast.ColumnDef{
-							{
-								ColumnName: def.ColumnDef.Colname,
-							},
-						},
+						Table:      alterTable.Table,
+						ColumnList: []*ast.ColumnDef{column},
 					}
 
 					alterTable.AlterItemList = append(alterTable.AlterItemList, addColumn)
-				case pgquery.AlterTableType_AT_AddIndex:
-					// TODO.
-					// Trick linter to use switch...case...
+				case pgquery.AlterTableType_AT_AddConstraint:
+					def, ok := alterCmd.Def.Node.(*pgquery.Node_Constraint)
+					if !ok {
+						return nil, parser.NewConvertErrorf("expected Constraint but found %t", alterCmd.Def.Node)
+					}
+					constraint, err := convertConstraint(def)
+					if err != nil {
+						return nil, err
+					}
+
+					addConstraint := &ast.AddConstraintStmt{
+						Table:      alterTable.Table,
+						Constraint: constraint,
+					}
+
+					alterTable.AlterItemList = append(alterTable.AlterItemList, addConstraint)
+				case pgquery.AlterTableType_AT_DropConstraint:
+					dropConstraint := &ast.DropConstraintStmt{
+						Table:          alterTable.Table,
+						ConstraintName: alterCmd.Name,
+					}
+
+					alterTable.AlterItemList = append(alterTable.AlterItemList, dropConstraint)
 				}
 			}
 		}
@@ -51,23 +72,10 @@ func convert(node *pgquery.Node) (ast.Node, error) {
 		for _, elt := range in.CreateStmt.TableElts {
 			switch item := elt.Node.(type) {
 			case *pgquery.Node_ColumnDef:
-				column := &ast.ColumnDef{
-					ColumnName: item.ColumnDef.Colname,
+				column, err := convertColumnDef(item)
+				if err != nil {
+					return nil, err
 				}
-
-				for _, cons := range item.ColumnDef.Constraints {
-					constraint, ok := cons.Node.(*pgquery.Node_Constraint)
-					if !ok {
-						return nil, parser.NewConvertErrorf("expected Constraint but found %t", cons.Node)
-					}
-					columnCons, err := convertConstraint(constraint)
-					if err != nil {
-						return nil, err
-					}
-					columnCons.KeyList = append(columnCons.KeyList, item.ColumnDef.Colname)
-					column.ConstraintList = append(column.ConstraintList, columnCons)
-				}
-
 				table.ColumnList = append(table.ColumnList, column)
 			case *pgquery.Node_Constraint:
 				cons, err := convertConstraint(item)
@@ -91,6 +99,12 @@ func convert(node *pgquery.Node) (ast.Node, error) {
 				Table:   convertRangeVarToTableName(in.RenameStmt.Relation),
 				NewName: in.RenameStmt.Newname,
 			}, nil
+		case pgquery.ObjectType_OBJECT_TABCONSTRAINT:
+			return &ast.RenameConstraintStmt{
+				Table:          convertRangeVarToTableName(in.RenameStmt.Relation),
+				ConstraintName: in.RenameStmt.Subname,
+				NewName:        in.RenameStmt.Newname,
+			}, nil
 		}
 	}
 
@@ -108,18 +122,19 @@ func convertRangeVarToTableName(in *pgquery.RangeVar) *ast.TableDef {
 func convertConstraint(in *pgquery.Node_Constraint) (*ast.ConstraintDef, error) {
 	cons := &ast.ConstraintDef{
 		Name: in.Constraint.Conname,
-		Type: convertConstraintType(in.Constraint.Contype),
+		Type: convertConstraintType(in.Constraint.Contype, in.Constraint.Indexname != ""),
 	}
 
-	for _, key := range in.Constraint.Keys {
-		name, ok := key.Node.(*pgquery.Node_String_)
-		if !ok {
-			return nil, parser.NewConvertErrorf("expected String but found %t", key.Node)
+	switch cons.Type {
+	case ast.ConstraintTypePrimary, ast.ConstraintTypeUnique:
+		for _, key := range in.Constraint.Keys {
+			name, ok := key.Node.(*pgquery.Node_String_)
+			if !ok {
+				return nil, parser.NewConvertErrorf("expected String but found %t", key.Node)
+			}
+			cons.KeyList = append(cons.KeyList, name.String_.Str)
 		}
-		cons.KeyList = append(cons.KeyList, name.String_.Str)
-	}
-
-	if cons.Type == ast.ConstraintTypeForeign {
+	case ast.ConstraintTypeForeign:
 		cons.Foreign = &ast.ForeignDef{
 			Table: convertRangeVarToTableName(in.Constraint.Pktable),
 		}
@@ -139,19 +154,51 @@ func convertConstraint(in *pgquery.Node_Constraint) (*ast.ConstraintDef, error) 
 			}
 			cons.KeyList = append(cons.KeyList, name.String_.Str)
 		}
+	case ast.ConstraintTypePrimaryUsingIndex, ast.ConstraintTypeUniqueUsingIndex:
+		cons.IndexName = in.Constraint.Indexname
 	}
 
 	return cons, nil
 }
 
-func convertConstraintType(in pgquery.ConstrType) ast.ConstraintType {
+func convertConstraintType(in pgquery.ConstrType, usingIndex bool) ast.ConstraintType {
 	switch in {
 	case pgquery.ConstrType_CONSTR_PRIMARY:
+		if usingIndex {
+			return ast.ConstraintTypePrimaryUsingIndex
+		}
 		return ast.ConstraintTypePrimary
 	case pgquery.ConstrType_CONSTR_UNIQUE:
+		if usingIndex {
+			return ast.ConstraintTypeUniqueUsingIndex
+		}
 		return ast.ConstraintTypeUnique
 	case pgquery.ConstrType_CONSTR_FOREIGN:
+		if usingIndex {
+			return ast.ConstraintTypeUndefined
+		}
 		return ast.ConstraintTypeForeign
 	}
 	return ast.ConstraintTypeUndefined
+}
+
+func convertColumnDef(in *pgquery.Node_ColumnDef) (*ast.ColumnDef, error) {
+	column := &ast.ColumnDef{
+		ColumnName: in.ColumnDef.Colname,
+	}
+
+	for _, cons := range in.ColumnDef.Constraints {
+		constraint, ok := cons.Node.(*pgquery.Node_Constraint)
+		if !ok {
+			return nil, parser.NewConvertErrorf("expected Constraint but found %t", cons.Node)
+		}
+		columnCons, err := convertConstraint(constraint)
+		if err != nil {
+			return nil, err
+		}
+		columnCons.KeyList = append(columnCons.KeyList, in.ColumnDef.Colname)
+		column.ConstraintList = append(column.ConstraintList, columnCons)
+	}
+
+	return column, nil
 }
