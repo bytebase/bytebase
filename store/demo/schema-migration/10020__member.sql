69cd5a48f41d1ffc3ed08db4f21980c8b8a60b71
boojack <imrealleonardo@gmail.com>
2022-07-05 20:32:51 +0800

    feat: add `demo-name` to args (#1745)
    * feat: add `demo-name` to flags
    
    * chore: add test data
    
    * chore: fix typo
    
    * chore: update demo name in dev mode
    
    Former-commit-id: 0605945992ec32679fea7656a51280b335dee4a6
    Former-commit-id: e88c8a0e0ce00daf54240b55ae0cb1b7395ed12d

---
+++ store/demo/schema-migration/10020__member.sql
@@ -0,0 +1,37 @@
+INSERT INTO
+    member (
+        id,
+        creator_id,
+        updater_id,
+        status,
+        role,
+        principal_id
+    )
+VALUES
+    (2001, 101, 101, 'ACTIVE', 'OWNER', 101);
+
+INSERT INTO
+    member (
+        id,
+        creator_id,
+        updater_id,
+        status,
+        role,
+        principal_id
+    )
+VALUES
+    (2002, 101, 101, 'ACTIVE', 'DBA', 102);
+
+INSERT INTO
+    member (
+        id,
+        creator_id,
+        updater_id,
+        status,
+        role,
+        principal_id
+    )
+VALUES
+    (2003, 101, 101, 'ACTIVE', 'DEVELOPER', 103);
+
+ALTER SEQUENCE member_id_seq RESTART WITH 2004;
