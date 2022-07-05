4238e4c8fbc028193762a0fab7f3d200ecd0e38c
Yilong Li <liyilongko@gmail.com>
2022-07-05 16:22:31 +0800

    refactor: schedule backup task after PITR

--- server/task_executor_pitr_cutover.go
+++ server/task_executor_pitr_cutover.go
@@ -2,11 +2,8 @@ package server
 
 import (
 	"context"
-	"database/sql"
 	"encoding/json"
 	"fmt"
-	"os"
-	"path/filepath"
 	"strconv"
 
 	"github.com/bytebase/bytebase/api"
@@ -15,7 +12,6 @@ import (
 	"github.com/bytebase/bytebase/plugin/db"
 	"github.com/bytebase/bytebase/plugin/db/mysql"
 	"github.com/bytebase/bytebase/resources/mysqlutil"
-	"github.com/bytebase/bytebase/store"
 	"go.uber.org/zap"
 )
 
@@ -98,18 +94,8 @@ func (exec *PITRCutoverTaskExecutor) pitrCutover(ctx context.Context, task *api.
 	if err != nil {
 		return true, nil, err
 	}
-	defer func() {
-		if err != nil {
-			// The conn is used in an asynchronous goroutine in backupDatabaseAfterPITR(), so we should not close it in a normal deferred function here.
-			// But if error happens elsewhere, we should close the connection to return it to the database connection pool.
-			// In this case, the backup process failure is expected.
-			conn.Close()
-		}
-	}()
-	log.Debug("Acquiring table locks in database", zap.String("database", task.Database.Name))
-	if err := mysql.FlushTablesWithReadLock(ctx, conn, task.Database.Name); err != nil {
-		return true, nil, err
-	}
+	defer conn.Close()
+
 	log.Debug("Swapping the original and PITR database", zap.String("originalDatabase", task.Database.Name))
 	pitrDatabaseName, pitrOldDatabaseName, err := mysql.SwapPITRDatabase(ctx, conn, task.Database.Name, issue.CreatedTs)
 	if err != nil {
@@ -118,28 +104,9 @@ func (exec *PITRCutoverTaskExecutor) pitrCutover(ctx context.Context, task *api.
 	}
 	log.Debug("Finished swapping the original and PITR database", zap.String("originalDatabase", task.Database.Name), zap.String("pitrDatabase", pitrDatabaseName), zap.String("oldDatabase", pitrOldDatabaseName))
 
-	binlogInfo, err := mysql.GetBinlogInfo(ctx, conn)
-	if err != nil {
-		return true, nil, err
-	}
-	payload := api.BackupPayload{BinlogInfo: binlogInfo}
-	payloadBytes, err := json.Marshal(payload)
-	if err != nil {
-		return true, nil, err
-	}
-	backupPayload := string(payloadBytes)
-
-	log.Debug("Starting new transaction and unlock tables")
-	options := sql.TxOptions{ReadOnly: true}
-	// Beginning a transaction in the same session will implicitly release existing table locks.
-	// ref: https://dev.mysql.com/doc/refman/8.0/en/lock-tables.html, section "Interaction of Table Locking and Transactions".
-	txn, err := conn.BeginTx(ctx, &options)
-	if err != nil {
-		return true, nil, err
-	}
-
-	if err := exec.backupDatabaseAfterPITR(ctx, conn, txn, backupPayload, driver, server.store, task.Database, server.profile.DataDir, issue.CreatedTs); err != nil {
-		return true, nil, fmt.Errorf("failed to backup database %q after PITR, error: %w", task.Database.Name, err)
+	backupName := fmt.Sprintf("%s-%s-pitr_%d", api.ProjectShortSlug(task.Database.Project), api.EnvSlug(task.Database.Instance.Environment), issue.CreatedTs)
+	if _, err := server.scheduleBackupTask(ctx, task.Database, backupName, api.BackupTypePITR, api.BackupStorageBackendLocal, api.SystemBotID); err != nil {
+		return true, nil, fmt.Errorf("failed to schedule backup task for database %q after PITR, error: %w", task.Database.Name, err)
 	}
 
 	log.Debug("Appending new migration history record")
@@ -173,81 +140,4 @@ func (exec *PITRCutoverTaskExecutor) pitrCutover(ctx context.Context, task *api.
 	return true, &api.TaskRunResultPayload{
 		Detail: fmt.Sprintf("Swapped PITR database for target database %q", task.Database.Name),
 	}, nil
-}
-
-func (exec *PITRCutoverTaskExecutor) backupDatabaseAfterPITR(ctx context.Context, conn *sql.Conn, txn *sql.Tx, backupPayload string, driver db.Driver, store *store.Store, database *api.Database, dataDir string, suffixTs int64) error {
-	backupName := fmt.Sprintf("%s-%s-pitr_%d", api.ProjectShortSlug(database.Project), api.EnvSlug(database.Instance.Environment), suffixTs)
-	if err := createBackupDirectory(dataDir, database.ID); err != nil {
-		return fmt.Errorf("failed to create backup directory, error: %w", err)
-	}
-	path := getBackupRelativeFilePath(database.ID, backupName)
-	migrationHistoryVersion, err := getLatestSchemaVersion(ctx, driver, database.Name)
-	if err != nil {
-		return fmt.Errorf("failed to get migration history for database %q, error: %w", database.Name, err)
-	}
-	backupCreate := &api.BackupCreate{
-		CreatorID:               api.SystemBotID,
-		DatabaseID:              database.ID,
-		Name:                    backupName,
-		Type:                    api.BackupTypePITR,
-		MigrationHistoryVersion: migrationHistoryVersion,
-		StorageBackend:          api.BackupStorageBackendLocal,
-		Path:                    path,
-	}
-	backup, err := store.CreateBackup(ctx, backupCreate)
-	if err != nil {
-		if common.ErrorCode(err) == common.Conflict {
-			return fmt.Errorf("backup name %q already exists", backupName)
-		}
-		return fmt.Errorf("failed to create backup %q, error: %w", backupName, err)
-	}
-
-	backupFile, err := os.Create(filepath.Join(dataDir, path))
-	if err != nil {
-		return fmt.Errorf("failed to create backup path %q", path)
-	}
-	binlogDir := getBinlogAbsDir(dataDir, database.Instance.ID)
-	if err := createBinlogDir(dataDir, database.Instance.ID); err != nil {
-		return err
-	}
-	mysqlDriver, ok := driver.(*mysql.Driver)
-	if !ok {
-		log.Error("failed to cast driver to mysql.Driver")
-		return fmt.Errorf("[internal] cast driver to mysql.Driver failed")
-	}
-	mysqlDriver.SetUpForPITR(exec.mysqlutil, binlogDir)
-
-	// Asynchronously dump the database and update the backup state.
-	go func() {
-		log.Debug("Taking a backup with type AUTOMATIC_PITR_CUTOVER asynchronously")
-		defer conn.Close()
-		defer txn.Rollback()
-		defer backupFile.Close()
-		backupPatch := api.BackupPatch{
-			ID:        backup.ID,
-			Status:    string(api.BackupStatusDone),
-			UpdaterID: api.SystemBotID,
-			Comment:   "",
-			Payload:   backupPayload,
-		}
-		backupErr := func() error {
-			if err := mysqlDriver.DumpTx(ctx, txn, database.Name, backupFile); err != nil {
-				return err
-			}
-			if err := txn.Commit(); err != nil {
-				return err
-			}
-			return nil
-		}()
-		if backupErr != nil {
-			backupPatch.Status = string(api.BackupStatusFailed)
-			backupPatch.Comment = fmt.Sprintf("failed to dump backup for database %q, error: %v", database.Name, backupErr)
-		}
-		if _, err := store.PatchBackup(ctx, &backupPatch); err != nil {
-			log.Error("Failed to patch backup metadata for backup after PITR", zap.Error(err))
-		}
-		log.Debug("Asynchronous backup process after PITR is finished")
-	}()
-
-	return nil
 }
