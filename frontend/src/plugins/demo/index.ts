01774e3a78f7d8fbf010dc01229563f683b15743
boojack <imrealleonardo@gmail.com>
2022-07-05 18:25:17 +0800

    chore: add hint for highlight target element (#1740)
    Former-commit-id: 357d73b18735e0366b5f52194ab692f34a23eee1
    Former-commit-id: 09710576852648e5d9e42152f6477fa64bde731a

--- frontend/src/plugins/demo/index.ts
+++ frontend/src/plugins/demo/index.ts
@@ -1,16 +1,23 @@
-import initGuideListeners from "./listener";
+import { initGuideListeners, initHintListeners } from "./listener";
 import * as storage from "./storage";
 
 // initial guide listeners when window loaded
 window.addEventListener(
   "load",
   () => {
+    const { guide: storageGuide, hint: storageHint } = storage.get([
+      "guide",
+      "hint",
+    ]);
     const params = new URLSearchParams(window.location.search);
     const paramGuide = params.get("guide");
-    const { guide: storageGuide } = storage.get(["guide"]);
+    const paramHint = params.get("hint");
 
     if (paramGuide || storageGuide) {
       initGuideListeners();
+    }
+    if (paramHint || storageHint) {
+      initHintListeners();
     }
   },
   {
