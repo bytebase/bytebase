b108b78377aac765658c5c6337c4574f0c2db6ef
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-07-05 17:05:58 +0800

    feat(frontend): fetch pipeline activities and merge (#1752)
    * feat(frontend): fetch pipeline activities and merge
    
    * chore: remove logs
    
    * chore: request concurrently
    
    Former-commit-id: 3a1fd718572ff123d8e429cfb6f3de5b761d1d31

--- frontend/src/store/modules/activity.ts
+++ frontend/src/store/modules/activity.ts
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import axios from "axios";
+import { stringify } from "qs";
 import {
   Activity,
   ActivityCreate,
@@ -83,22 +84,42 @@ export const useActivityStore = defineStore("activity", {
       this.setActivityListForUser({ userId, activityList });
       return activityList;
     },
-    async fetchActivityListForIssue(issueId: IssueId) {
-      const queryList = [
-        "typePrefix=bb.issue.",
-        `container=${issueId}`,
-        `order=ASC`,
-      ];
-      const data = (await axios.get(`/api/activity?${queryList.join("&")}`))
-        .data;
-      const activityList: Activity[] = data.data.map(
+    async fetchActivityList(params: {
+      typePrefix: string;
+      container: number | string;
+      order: "ASC" | "DESC";
+      limit?: number;
+    }) {
+      const url = `/api/activity?${stringify(params)}`;
+      const response = (await axios.get(url)).data;
+      const activityList: Activity[] = response.data.map(
         (activity: ResourceObject) => {
-          return convert(activity, data.included);
+          return convert(activity, response.included);
         }
       );
-
-      this.setActivityListForIssue({ issueId, activityList });
       return activityList;
+    },
+    async fetchActivityListForIssue(issueId: IssueId) {
+      const requestListForIssue = this.fetchActivityList({
+        typePrefix: "bb.issue.",
+        container: issueId,
+        order: "ASC",
+      });
+      const requestListForPipeline = this.fetchActivityList({
+        typePrefix: "bb.pipeline.",
+        container: issueId,
+        order: "ASC",
+      });
+      const [listForIssue, listForPipeline] = await Promise.all([
+        requestListForIssue,
+        requestListForPipeline,
+      ]);
+
+      const mergedList = [...listForIssue, ...listForPipeline];
+      mergedList.sort((a, b) => a.createdTs - b.createdTs);
+
+      this.setActivityListForIssue({ issueId, activityList: mergedList });
+      return mergedList;
     },
     // We do not store the returned list because the caller will specify different limits
     async fetchActivityListForProject({
