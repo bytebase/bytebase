9f9df8d6b2d341173ecdaef8e3736651192dbd93
h3n4l <oysterdays@gmail.com>
2022-07-06 15:07:35 +0800

    fix: PostgreSQL double quotes in name (#1765)
    * fix: rmeove double quotes
    
    * fix: remove backslash
    
    Former-commit-id: f3193b31928bea40188da680c2566ac69f45e9e4

--- plugin/db/pg/sync.go
+++ plugin/db/pg/sync.go
@@ -707,8 +707,7 @@ func quoteIdentifier(s string) string {
 		quote = true
 	}
 	if quote {
-		return fmt.Sprintf("\"%s\"", strings.ReplaceAll(s, "\"", "\"\""))
+		return fmt.Sprintf("\"%s\"", s)
 	}
 	return s
-
 }
