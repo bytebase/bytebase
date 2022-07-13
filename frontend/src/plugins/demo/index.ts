5cfb9e47147a48901f3e3e236ef7c48707ba05a3
boojack <imrealleonardo@gmail.com>
2022-07-13 13:50:01 +0800

    chore: set should show demo from server (#1846)
    Former-commit-id: 8e1b8f33c8b46feb5048ccd449df27fbbdb8ef2d
    Former-commit-id: e19570cebd60d27aa35b37cbfc53870a257d14db

--- frontend/src/plugins/demo/index.ts
+++ frontend/src/plugins/demo/index.ts
@@ -1,6 +1,9 @@
+import axios from "axios";
 import { isNull } from "lodash-es";
 import { initGuideListeners, initHintListeners } from "./listener";
 import * as storage from "./storage";
+
+const invalidDemoNameList = ["dev", "prod"];
 
 // TODO(steven): after we using help to replace the guide, we should remove this.
 // In live demo mode, we should not show the guide dialog.
@@ -21,30 +24,55 @@ const hideConsoleGuides = () => {
 // initial guide listeners when window loaded
 window.addEventListener(
   "load",
-  () => {
-    const { guide: storageGuide, hint: storageHint } = storage.get([
-      "guide",
-      "hint",
-    ]);
-    const params = new URLSearchParams(window.location.search);
+  async () => {
+    const serverInfo = (
+      await axios.get<{
+        demo: boolean;
+        demoName: string;
+      }>(`/api/actuator/info`)
+    ).data;
 
-    // If there is a cleardemo flag in the url, then clear the demo storage and return
+    // In online demo, we only show the hints.
+    if (
+      serverInfo.demo &&
+      serverInfo.demoName &&
+      !invalidDemoNameList.includes(serverInfo.demoName)
+    ) {
+      const demoName = serverInfo.demoName;
+      if (demoName) {
+        storage.set({
+          hint: {
+            name: demoName,
+          },
+        });
+        hideConsoleGuides();
+        initHintListeners();
+      }
+    } else {
+      // This is using for dev mainly.
+      const { guide: storageGuide, hint: storageHint } = storage.get([
+        "guide",
+        "hint",
+      ]);
+      const params = new URLSearchParams(window.location.search);
+      const paramGuide = params.get("guide");
+      const paramHint = params.get("hint");
+
+      if (paramGuide || storageGuide) {
+        hideConsoleGuides();
+        initGuideListeners();
+      }
+      if (paramHint || storageHint) {
+        hideConsoleGuides();
+        initHintListeners();
+      }
+    }
+
+    // If there is a `cleardemo` flag in the url, then clear the demo storage.
+    const params = new URLSearchParams(window.location.search);
     const clearDemo = params.get("cleardemo");
     if (!isNull(clearDemo)) {
       storage.remove(["guide", "hint"]);
-      return;
-    }
-
-    const paramGuide = params.get("guide");
-    const paramHint = params.get("hint");
-
-    if (paramGuide || storageGuide) {
-      initGuideListeners();
-      hideConsoleGuides();
-    }
-    if (paramHint || storageHint) {
-      initHintListeners();
-      hideConsoleGuides();
     }
   },
   {
