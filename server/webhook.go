d0cea8b7b12f967629756fdfcbe9b9f8adde2b70
h3n4l <oysterdays@gmail.com>
2022-07-19 13:17:11 +0800

    feat(VCS): matching author on email for VCS triggered issues (#1923)
    * feat: push events will create the issue with principle id
    
    * Update server/webhook.go
    
    Co-authored-by: tianzhou <t@bytebase.com>
    
    Co-authored-by: tianzhou <t@bytebase.com>
    Former-commit-id: acac4d30615ba8eba11fcb9a2489424702d29b8f
    Former-commit-id: 1cd1aa4d301746f549d9175cf07c0200c69f0d19

--- server/webhook.go
+++ server/webhook.go
@@ -104,13 +104,14 @@ func (s *Server) registerWebhookRoutes(g *echo.Group) {
 				RepositoryFullPath: pushEvent.Project.FullPath,
 				AuthorName:         pushEvent.AuthorName,
 				FileCommit: vcs.FileCommit{
-					ID:         commit.ID,
-					Title:      commit.Title,
-					Message:    commit.Message,
-					CreatedTs:  item.createdTime.Unix(),
-					URL:        commit.URL,
-					AuthorName: commit.Author.Name,
-					Added:      addedEscaped,
+					ID:          commit.ID,
+					Title:       commit.Title,
+					Message:     commit.Message,
+					CreatedTs:   item.createdTime.Unix(),
+					URL:         commit.URL,
+					AuthorName:  commit.Author.Name,
+					AuthorEmail: commit.Author.Email,
+					Added:       addedEscaped,
 				},
 			}
 
@@ -178,6 +179,19 @@ func (s *Server) registerWebhookRoutes(g *echo.Group) {
 			}
 
 			// Create schema update issue.
+			creatorID := api.SystemBotID
+			if commit.Author.Email != "" {
+				committerPrinciple, err := s.store.GetPrincipalByEmail(ctx, commit.Author.Email)
+				if err != nil {
+					log.Error("failed to find the principal with committer email", zap.String("email", commit.Author.Email), zap.Error(err))
+				}
+				if committerPrinciple == nil {
+					log.Debug("cannot find the principal with committer email, use system bot instead", zap.String("email", commit.Author.Email))
+				} else {
+					creatorID = committerPrinciple.ID
+				}
+			}
+
 			var createContext string
 			if repo.Project.TenantMode == api.TenantModeTenant {
 				if !s.feature(api.FeatureMultiTenancy) {
@@ -204,7 +218,7 @@ func (s *Server) registerWebhookRoutes(g *echo.Group) {
 				AssigneeID:    api.SystemBotID,
 				CreateContext: createContext,
 			}
-			issue, err := s.createIssue(ctx, issueCreate, api.SystemBotID)
+			issue, err := s.createIssue(ctx, issueCreate, creatorID)
 			if err != nil {
 				errMsg := "Failed to create schema update issue"
 				if issueType == api.IssueDatabaseDataUpdate {
@@ -226,7 +240,7 @@ func (s *Server) registerWebhookRoutes(g *echo.Group) {
 			}
 
 			activityCreate := &api.ActivityCreate{
-				CreatorID:   api.SystemBotID,
+				CreatorID:   creatorID,
 				ContainerID: repo.ProjectID,
 				Type:        api.ActivityProjectRepositoryPush,
 				Level:       api.ActivityInfo,
