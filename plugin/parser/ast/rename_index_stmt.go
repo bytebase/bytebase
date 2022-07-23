7efb69ea8d5cde4bb2b7d29e9399c9cabc31bd61
rebelice <yangrebelice@gmail.com>
2022-07-23 23:28:57 +0800

    chore: fix comments

--- plugin/parser/ast/rename_index_stmt.go
+++ plugin/parser/ast/rename_index_stmt.go
@@ -5,7 +5,7 @@ type RenameIndexStmt struct {
 	node
 
 	// For PostgreSQL, we only use TableDef.Schema.
-	// If this rename index statement doesn't contain schema name, Table will be nil.
+	// If this rename index statement doesn't contain schema name, Table will be not nil and the Schema name is empty string.
 	Table     *TableDef
 	IndexName string
 	NewName   string
