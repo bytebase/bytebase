4f216c37b4c06c739e922038bbb120cc52ad463a
ecmadao <wlec@outlook.com>
2022-07-29 17:58:31 +0800

    fix: script

--- scripts/sql_service.sh
+++ scripts/sql_service.sh
@@ -9,20 +9,20 @@
 # ./sql_service.sh https://example.com:8080
 
 # If no parameter is passed, use https://sql.bytebase.com as host and 443 as port by default
-ONLINE_DEMO_HOST='https://sql.bytebase.com'
-ONLINE_DEMO_PORT='443'
+ONLINE_HOST='https://sql.bytebase.com'
+ONLINE_PORT='443'
 if [ $1 ]; then
     PROTOCAL=$(echo $1 | awk -F ':' '{ print $1 }')
     URI=$(echo $1 | awk -F '[/:]' '{ print $4; }')
     PORT=$(echo $1 | awk -F '[/:]' '{ print $5; }')
 
-    ONLINE_DEMO_HOST=$PROTOCAL://$URI
+    ONLINE_HOST=$PROTOCAL://$URI
 
     if [ $PORT ]; then
-        ONLINE_DEMO_PORT=$PORT
+        ONLINE_PORT=$PORT
     fi
 fi
 
-echo "Starting Bytebase SQL Service in debug mode at ${ONLINE_DEMO_HOST}:${ONLINE_DEMO_PORT}..."
+echo "Starting Bytebase SQL Service in debug mode at ${ONLINE_HOST}:${ONLINE_PORT}..."
 
-sql-service --host ${ONLINE_DEMO_HOST} --port ${ONLINE_DEMO_PORT} --debug
+sql-service --host ${ONLINE_HOST} --port ${ONLINE_PORT} --debug --data /var/opt/sql-service
