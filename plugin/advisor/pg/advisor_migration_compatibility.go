ee38fac567bc86f67f6f095aa99697776af5bbb5
rebelice <yangrebelice@gmail.com>
2022-07-31 00:09:31 +0800

    feat: support PG migration compatibility rule (#2090)
    * feat: support PG migration compatibility rule
    
    * chore: add ALTER COLUMN TYPE check
    
    Former-commit-id: f0065ec900eb2047ccaf597f8a1f5d0922e5e615

---
+++ plugin/advisor/pg/advisor_migration_compatibility.go
@@ -0,0 +1,117 @@
+package pg
+
+import (
+	"fmt"
+
+	"github.com/bytebase/bytebase/plugin/advisor"
+	"github.com/bytebase/bytebase/plugin/parser/ast"
+)
+
+var (
+	_ advisor.Advisor = (*CompatibilityAdvisor)(nil)
+	_ ast.Visitor     = (*compatibilityChecker)(nil)
+)
+
+func init() {
+	advisor.Register(advisor.Postgres, advisor.PostgreSQLMigrationCompatibility, &CompatibilityAdvisor{})
+}
+
+// CompatibilityAdvisor is the advisor checking for schema backward compatibility.
+type CompatibilityAdvisor struct {
+}
+
+// Check checks schema backward compatibility.
+func (*CompatibilityAdvisor) Check(ctx advisor.Context, statement string) ([]advisor.Advice, error) {
+	stmts, errAdvice := parseStatement(statement)
+	if errAdvice != nil {
+		return errAdvice, nil
+	}
+
+	level, err := advisor.NewStatusBySQLReviewRuleLevel(ctx.Rule.Level)
+	if err != nil {
+		return nil, err
+	}
+
+	checker := &compatibilityChecker{
+		level: level,
+		title: string(ctx.Rule.Type),
+	}
+	for _, stmt := range stmts {
+		checker.text = stmt.Text()
+		ast.Walk(checker, stmt)
+	}
+
+	if len(checker.adviceList) == 0 {
+		checker.adviceList = append(checker.adviceList, advisor.Advice{
+			Status:  advisor.Success,
+			Code:    advisor.Ok,
+			Title:   "OK",
+			Content: "",
+		})
+	}
+	return checker.adviceList, nil
+}
+
+type compatibilityChecker struct {
+	adviceList []advisor.Advice
+	level      advisor.Status
+	title      string
+	text       string
+}
+
+func (checker *compatibilityChecker) Visit(node ast.Node) ast.Visitor {
+	code := advisor.Ok
+	switch n := node.(type) {
+	// DROP DATABASE
+	case *ast.DropDatabaseStmt:
+		code = advisor.CompatibilityDropDatabase
+	// RENAME TABLE/VIEW
+	case *ast.RenameTableStmt:
+		code = advisor.CompatibilityRenameTable
+	// DROP TABLE/VIEW
+	case *ast.DropTableStmt:
+		code = advisor.CompatibilityDropTable
+	// ALTER TABLE RENAME COLUMN
+	case *ast.RenameColumnStmt:
+		code = advisor.CompatibilityRenameColumn
+	// ALTER TABLE DROP COLUMN
+	case *ast.DropColumnStmt:
+		code = advisor.CompatibilityDropColumn
+	case *ast.AddConstraintStmt:
+		switch n.Constraint.Type {
+		// ADD PRIMARY KEY/ ADD PRIMARY KEY USING INDEX
+		case ast.ConstraintTypePrimary, ast.ConstraintTypePrimaryUsingIndex:
+			code = advisor.CompatibilityAddPrimaryKey
+		// ADD UNIQUE CONSTRAINT
+		case ast.ConstraintTypeUnique:
+			// ConstraintTypeUniqueUsingIndex doesn't add a new constraint or unique index.
+			code = advisor.CompatibilityAddUniqueKey
+		// ADD FOREIGIN KEY
+		case ast.ConstraintTypeForeign:
+			code = advisor.CompatibilityAddForeignKey
+		// ADD CHECK
+		case ast.ConstraintTypeCheck:
+			if !n.Constraint.SkipValidation {
+				code = advisor.CompatibilityAddCheck
+			}
+		}
+	// ALTER TABLE ALTER COLUMN TYPE
+	case *ast.AlterColumnTypeStmt:
+		code = advisor.CompatibilityAlterColumn
+	// CREATE UNIQUE INDEX
+	case *ast.CreateIndexStmt:
+		if n.Index.Unique {
+			code = advisor.CompatibilityAddUniqueKey
+		}
+	}
+
+	if code != advisor.Ok {
+		checker.adviceList = append(checker.adviceList, advisor.Advice{
+			Status:  checker.level,
+			Code:    code,
+			Title:   checker.title,
+			Content: fmt.Sprintf("\"%s\" may cause incompatibility with the existing data and code", checker.text),
+		})
+	}
+	return checker
+}
