4da0bcfa88b37e863bc26bcf2f229037219b44f5
rebelice <yangrebelice@gmail.com>
2022-08-01 14:52:01 +0800

    chore: remove migration file in dev

--- store/migration/dev/20220715000000__primary_key.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE idx ADD COLUMN "primary" BOOLEAN NOT NULL DEFAULT false;
-ALTER TABLE idx ADD CONSTRAINT check_primary_must_unique CHECK(NOT "primary" OR ("primary" AND "unique"));
-ALTER TABLE idx ALTER COLUMN "primary" DROP DEFAULT;
