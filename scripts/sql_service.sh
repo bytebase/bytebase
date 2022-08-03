4293e75bbd53271698afa9e884cc208bb290459f
ecmadao <wlec@outlook.com>
2022-08-03 22:59:25 +0800

    chore: update comment

--- scripts/sql_service.sh
+++ scripts/sql_service.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Start the SQL service with workspace id
+# For now, we use this script to start our SQL service on render
+# by changing the ENTRYPOINT and CMD at the dockerfile to this.
+
+# example usages:
+# ./sql_service.sh
+# ./sql_service.sh --host=https://example.com
+# ./sql_service.sh --host=https://example.com --port=8080 --workspace-id=bytebase-sql-service
 
 ONLINE_HOST='http://localhost'
 ONLINE_PORT='80'
@@ -29,4 +35,5 @@ done
 
 echo "Starting Bytebase SQL Service at ${ONLINE_HOST}:${ONLINE_PORT} with workspace ${WORKSPACE_ID}..."
 
+# Start the SQL service with workspace id
 sql-service --host ${ONLINE_HOST} --port ${ONLINE_PORT} --workspace-id ${WORKSPACE_ID}
