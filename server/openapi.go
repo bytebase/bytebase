a706a5d461505d2f63b9be7b4393253999a47d57
ecmadao <wlec@outlook.com>
2022-08-04 16:35:05 +0800

    chore: update endpoint for SQL check (#2161)
    Former-commit-id: 43be675f5b499858b040462c874aca60ec897b68

--- server/openapi.go
+++ server/openapi.go
@@ -57,7 +57,7 @@ func (s *Server) registerOpenAPIRoutes(g *echo.Group) {
 // @Success  200  {array}   advisor.Advice
 // @Failure  400  {object}  echo.HTTPError
 // @Failure  500  {object}  echo.HTTPError
-// @Router  /sql/advise  [get].
+// @Router  /sql/advise  [post].
 func (s *Server) sqlCheckController(c echo.Context) error {
 	request := &sqlCheckRequestBody{}
 	body, err := io.ReadAll(c.Request().Body)
