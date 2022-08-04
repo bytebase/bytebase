d8c7c66910514f20e32be5bf0390433c92aaf6ff
h3n4l <oysterdays@gmail.com>
2022-08-04 14:07:55 +0800

    feat: use mysqldump

--- plugin/db/mysql/dump.go
+++ plugin/db/mysql/dump.go
@@ -2,12 +2,13 @@ package mysql
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
-	"regexp"
+	"os/exec"
 	"strings"
 	"time"
 
@@ -17,20 +18,11 @@ import (
 	"github.com/bytebase/bytebase/common"
 	"github.com/bytebase/bytebase/common/log"
 	"github.com/bytebase/bytebase/plugin/db/util"
+	"github.com/bytebase/bytebase/resources/mysqlutil"
 )
 
 // Dump and restore.
 const (
-	databaseHeaderFmt = "" +
-		"--\n" +
-		"-- MySQL database structure for `%s`\n" +
-		"--\n"
-	useDatabaseFmt = "USE `%s`;\n\n"
-	settingsStmt   = "" +
-		"SET character_set_client  = %s;\n" +
-		"SET character_set_results = %s;\n" +
-		"SET collation_connection  = %s;\n" +
-		"SET sql_mode              = '%s';\n"
 	tableStmtFmt = "" +
 		"--\n" +
 		"-- Table structure for `%s`\n" +
@@ -41,35 +33,6 @@ const (
 		"-- View structure for `%s`\n" +
 		"--\n" +
 		"%s;\n"
-	routineStmtFmt = "" +
-		"--\n" +
-		"-- %s structure for `%s`\n" +
-		"--\n" +
-		settingsStmt +
-		"DELIMITER ;;\n" +
-		"%s ;;\n" +
-		"DELIMITER ;\n"
-	eventStmtFmt = "" +
-		"--\n" +
-		"-- Event structure for `%s`\n" +
-		"--\n" +
-		settingsStmt +
-		"SET time_zone = '%s';\n" +
-		"DELIMITER ;;\n" +
-		"%s ;;\n" +
-		"DELIMITER ;\n"
-	triggerStmtFmt = "" +
-		"--\n" +
-		"-- Trigger structure for `%s`\n" +
-		"--\n" +
-		settingsStmt +
-		"DELIMITER ;;\n" +
-		"%s ;;\n" +
-		"DELIMITER ;\n"
-)
-
-var (
-	excludeAutoIncrement = regexp.MustCompile(`AUTO_INCREMENT=\d+ `)
 )
 
 // Dump dumps the database.
@@ -83,16 +46,39 @@ func (driver *Driver) Dump(ctx context.Context, database string, out io.Writer, 
 	}
 	defer conn.Close()
 
+	mysqldumpArgs := []string{
+		// Dump stored routines (procedures and functions) from dumped databases
+		"--routines",
+		// Dump events from dumped databases
+		"--events",
+		// Dump triggers for each dumped table
+		"--triggers",
+		// Produce more compact output
+		"--compact",
+		"--databases", database,
+		"--host", driver.connCfg.Host,
+		"--user", driver.connCfg.Username,
+	}
+	if driver.connCfg.Port != "" {
+		mysqldumpArgs = append(mysqldumpArgs, "--port", driver.connCfg.Port)
+	}
+	if driver.connCfg.Password != "" {
+		mysqldumpArgs = append(mysqldumpArgs, fmt.Sprintf("--password=%s", driver.connCfg.Password))
+	}
+
 	var payloadBytes []byte
 	// Before we dump the real data, we should record the binlog position for PITR.
 	// Please refer to https://github.com/bytebase/bytebase/blob/main/docs/design/pitr-mysql.md#full-backup for details.
 	if !schemaOnly {
 		log.Debug("flush tables in database with read locks",
 			zap.String("database", database))
-		if err := FlushTablesWithReadLock(ctx, conn, database); err != nil {
+		if err := flushTablesWithReadLock(ctx, conn, database); err != nil {
 			log.Error("flush tables failed", zap.Error(err))
 			return "", err
 		}
+		defer func() {
+			_ = unlockTables(ctx, conn)
+		}()
 
 		binlog, err := GetBinlogInfo(ctx, conn)
 		if err != nil {
@@ -107,6 +93,8 @@ func (driver *Driver) Dump(ctx context.Context, database string, out io.Writer, 
 		if err != nil {
 			return "", err
 		}
+	} else {
+		mysqldumpArgs = append(mysqldumpArgs, "--no-data")
 	}
 
 	options := sql.TxOptions{}
@@ -117,26 +105,27 @@ func (driver *Driver) Dump(ctx context.Context, database string, out io.Writer, 
 	// If `schemaOnly` is false, now we are still holding the tables' exclusive locks.
 	// Beginning a transaction in the same session will implicitly release existing table locks.
 	// ref: https://dev.mysql.com/doc/refman/8.0/en/lock-tables.html, section "Interaction of Table Locking and Transactions".
-	txn, err := conn.BeginTx(ctx, &options)
-	if err != nil {
-		return "", err
-	}
-	defer txn.Rollback()
-
-	log.Debug("begin to dump database", zap.String("database", database), zap.Bool("schemaOnly", schemaOnly))
-	if err := dumpTxn(ctx, txn, database, out, schemaOnly); err != nil {
-		return "", err
-	}
-
-	if err := txn.Commit(); err != nil {
-		return "", err
+	mysqldumpCmd := exec.Command(mysqlutil.GetPath(mysqlutil.MySQLDump, driver.resourceDir), mysqldumpArgs...)
+	mysqldumpCmd.Stdout = out
+	var stderr bytes.Buffer
+	mysqldumpCmd.Stderr = &stderr
+	if err := mysqldumpCmd.Run(); err != nil {
+		return "", fmt.Errorf("mysqldump command failed, error: %q", stderr.String())
 	}
 
 	return string(payloadBytes), nil
 }
 
-// FlushTablesWithReadLock runs FLUSH TABLES table1, table2, ... WITH READ LOCK for all the tables in the database.
-func FlushTablesWithReadLock(ctx context.Context, conn *sql.Conn, database string) error {
+// unlockTables runs `UNLOCK TABLES`.
+func unlockTables(ctx context.Context, conn *sql.Conn) error {
+	if _, err := conn.ExecContext(ctx, "UNLOCK TABLES;"); err != nil {
+		return err
+	}
+	return nil
+}
+
+// flushTablesWithReadLock runs FLUSH TABLES table1, table2, ... WITH READ LOCK for all the tables in the database.
+func flushTablesWithReadLock(ctx context.Context, conn *sql.Conn, database string) error {
 	// The lock acquiring could take a long time if there are concurrent exclusive locks on the tables.
 	// We ensures that the execution is canceled after 30 seconds, otherwise we may get dead lock and stuck forever.
 	ctxWithTimeout, cancel := context.WithTimeout(ctx, 30*time.Second)
@@ -173,121 +162,6 @@ func FlushTablesWithReadLock(ctx context.Context, conn *sql.Conn, database strin
 	return nil
 }
 
-func dumpTxn(ctx context.Context, txn *sql.Tx, database string, out io.Writer, schemaOnly bool) error {
-	// Find all dumpable databases
-	dbNames, err := getDatabases(ctx, txn)
-	if err != nil {
-		return fmt.Errorf("failed to get databases: %s", err)
-	}
-
-	var dumpableDbNames []string
-	if database != "" {
-		exist := false
-		for _, n := range dbNames {
-			if n == database {
-				exist = true
-				break
-			}
-		}
-		if !exist {
-			return common.Errorf(common.NotFound, "database %s not found", database)
-		}
-		dumpableDbNames = []string{database}
-	} else {
-		for _, dbName := range dbNames {
-			if systemDatabases[dbName] {
-				continue
-			}
-			dumpableDbNames = append(dumpableDbNames, dbName)
-		}
-	}
-
-	for _, dbName := range dumpableDbNames {
-		// Include "USE DATABASE xxx" if dumping multiple databases.
-		if len(dumpableDbNames) > 1 {
-			// Database header.
-			header := fmt.Sprintf(databaseHeaderFmt, dbName)
-			if _, err := io.WriteString(out, header); err != nil {
-				return err
-			}
-			dbStmt, err := getDatabaseStmt(txn, dbName)
-			if err != nil {
-				return fmt.Errorf("failed to get database %q: %s", dbName, err)
-			}
-			if _, err := io.WriteString(out, dbStmt); err != nil {
-				return err
-			}
-			// Use database statement.
-			useStmt := fmt.Sprintf(useDatabaseFmt, dbName)
-			if _, err := io.WriteString(out, useStmt); err != nil {
-				return err
-			}
-		}
-
-		// Table and view statement.
-		tables, err := getTablesTx(txn, dbName)
-		if err != nil {
-			return fmt.Errorf("failed to get tables of database %q, error: %w", dbName, err)
-		}
-		for _, tbl := range tables {
-			if schemaOnly && tbl.TableType == baseTableType {
-				tbl.Statement = excludeSchemaAutoIncrementValue(tbl.Statement)
-			}
-			if _, err := io.WriteString(out, fmt.Sprintf("%s\n", tbl.Statement)); err != nil {
-				return err
-			}
-			if !schemaOnly && tbl.TableType == baseTableType {
-				// Include db prefix if dumping multiple databases.
-				includeDbPrefix := len(dumpableDbNames) > 1
-				if err := exportTableData(txn, dbName, tbl.Name, includeDbPrefix, out); err != nil {
-					return err
-				}
-			}
-		}
-
-		// Procedure and function (routine) statements.
-		routines, err := getRoutines(txn, dbName)
-		if err != nil {
-			return fmt.Errorf("failed to get routines of database %q: %s", dbName, err)
-		}
-		for _, rt := range routines {
-			if _, err := io.WriteString(out, fmt.Sprintf("%s\n", rt.statement)); err != nil {
-				return err
-			}
-		}
-
-		// Event statements.
-		events, err := getEvents(txn, dbName)
-		if err != nil {
-			return fmt.Errorf("failed to get events of database %q: %s", dbName, err)
-		}
-		for _, et := range events {
-			if _, err := io.WriteString(out, fmt.Sprintf("%s\n", et.statement)); err != nil {
-				return err
-			}
-		}
-
-		// Trigger statements.
-		triggers, err := getTriggers(txn, dbName)
-		if err != nil {
-			return fmt.Errorf("failed to get triggers of database %q: %s", dbName, err)
-		}
-		for _, tr := range triggers {
-			if _, err := io.WriteString(out, fmt.Sprintf("%s\n", tr.statement)); err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
-// excludeSchemaAutoIncrementValue excludes the starting value of AUTO_INCREMENT if it's a schema only dump.
-// https://github.com/bytebase/bytebase/issues/123
-func excludeSchemaAutoIncrementValue(s string) string {
-	return excludeAutoIncrement.ReplaceAllString(s, ``)
-}
-
 // GetBinlogInfo queries current binlog info from MySQL server.
 func GetBinlogInfo(ctx context.Context, conn *sql.Conn) (api.BinlogInfo, error) {
 	query := "SHOW MASTER STATUS"
@@ -308,43 +182,11 @@ func GetBinlogInfo(ctx context.Context, conn *sql.Conn) (api.BinlogInfo, error) 
 	return binlogInfo, nil
 }
 
-// getDatabaseStmt gets the create statement of a database.
-func getDatabaseStmt(txn *sql.Tx, dbName string) (string, error) {
-	query := fmt.Sprintf("SHOW CREATE DATABASE IF NOT EXISTS `%s`;", dbName)
-	var stmt, unused string
-	if err := txn.QueryRow(query).Scan(&unused, &stmt); err != nil {
-		if err == sql.ErrNoRows {
-			return "", common.FormatDBErrorEmptyRowWithQuery(query)
-		}
-		return "", err
-	}
-	return fmt.Sprintf("%s;\n", stmt), nil
-}
-
 // TableSchema describes the schema of a table or view.
 type TableSchema struct {
 	Name      string
 	TableType string
 	Statement string
-}
-
-// routineSchema describes the schema of a function or procedure (routine).
-type routineSchema struct {
-	name        string
-	routineType string
-	statement   string
-}
-
-// eventSchema describes the schema of an event.
-type eventSchema struct {
-	name      string
-	statement string
-}
-
-// triggerSchema describes the schema of a trigger.
-type triggerSchema struct {
-	name      string
-	statement string
 }
 
 // getTablesTx gets all tables of a database using the provided transaction.
@@ -418,247 +260,6 @@ func getTableStmt(txn *sql.Tx, dbName, tblName, tblType string) (string, error) 
 	default:
 		return "", fmt.Errorf("unrecognized table type %q for database %q table %q", tblType, dbName, tblName)
 	}
-}
-
-// exportTableData gets the data of a table.
-func exportTableData(txn *sql.Tx, dbName, tblName string, includeDbPrefix bool, out io.Writer) error {
-	query := fmt.Sprintf("SELECT * FROM `%s`.`%s`;", dbName, tblName)
-	rows, err := txn.Query(query)
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
-
-	cols, err := rows.ColumnTypes()
-	if err != nil {
-		return err
-	}
-	if len(cols) == 0 {
-		return nil
-	}
-	values := make([]*sql.NullString, len(cols))
-	refs := make([]interface{}, len(cols))
-	for i := 0; i < len(cols); i++ {
-		refs[i] = &values[i]
-	}
-	for rows.Next() {
-		if err := rows.Scan(refs...); err != nil {
-			return err
-		}
-		tokens := make([]string, len(cols))
-		for i, v := range values {
-			switch {
-			case v == nil || !v.Valid:
-				tokens[i] = "NULL"
-			case isNumeric(cols[i].ScanType().Name()):
-				tokens[i] = v.String
-			default:
-				tokens[i] = fmt.Sprintf("'%s'", v.String)
-			}
-		}
-		dbPrefix := ""
-		if includeDbPrefix {
-			dbPrefix = fmt.Sprintf("`%s`.", dbName)
-		}
-		stmt := fmt.Sprintf("INSERT INTO %s`%s` VALUES (%s);\n", dbPrefix, tblName, strings.Join(tokens, ", "))
-		if _, err := io.WriteString(out, stmt); err != nil {
-			return err
-		}
-	}
-	if err := rows.Err(); err != nil {
-		return err
-	}
-	if _, err := io.WriteString(out, "\n"); err != nil {
-		return err
-	}
-	return nil
-}
-
-// isNumeric determines whether the value needs quotes.
-// Even if the function returns incorrect result, the data dump will still work.
-func isNumeric(t string) bool {
-	return strings.Contains(t, "int") || strings.Contains(t, "bool") || strings.Contains(t, "float") || strings.Contains(t, "byte")
-}
-
-// getRoutines gets all routines of a database.
-func getRoutines(txn *sql.Tx, dbName string) ([]*routineSchema, error) {
-	var routines []*routineSchema
-	for _, routineType := range []string{"FUNCTION", "PROCEDURE"} {
-		query := fmt.Sprintf("SHOW %s STATUS WHERE Db = ?;", routineType)
-		rows, err := txn.Query(query, dbName)
-		if err != nil {
-			return nil, err
-		}
-		defer rows.Close()
-
-		cols, err := rows.Columns()
-		if err != nil {
-			return nil, err
-		}
-		var values []interface{}
-		for i := 0; i < len(cols); i++ {
-			values = append(values, new(interface{}))
-		}
-		for rows.Next() {
-			var r routineSchema
-			if err := rows.Scan(values...); err != nil {
-				return nil, err
-			}
-			r.name = fmt.Sprintf("%s", *values[1].(*interface{}))
-			r.routineType = fmt.Sprintf("%s", *values[2].(*interface{}))
-
-			routines = append(routines, &r)
-		}
-		if err := rows.Err(); err != nil {
-			return nil, err
-		}
-	}
-
-	for _, r := range routines {
-		stmt, err := getRoutineStmt(txn, dbName, r.name, r.routineType)
-		if err != nil {
-			return nil, fmt.Errorf("getRoutineStmt(%q, %q, %q) got error: %s", dbName, r.name, r.routineType, err)
-		}
-		r.statement = stmt
-	}
-	return routines, nil
-}
-
-// getRoutineStmt gets the create statement of a routine.
-func getRoutineStmt(txn *sql.Tx, dbName, routineName, routineType string) (string, error) {
-	query := fmt.Sprintf("SHOW CREATE %s `%s`.`%s`;", routineType, dbName, routineName)
-	var sqlmode, stmt, charset, collation, unused string
-	if err := txn.QueryRow(query).Scan(
-		&unused,
-		&sqlmode,
-		&stmt,
-		&charset,
-		&collation,
-		&unused,
-	); err != nil {
-		if err == sql.ErrNoRows {
-			return "", common.FormatDBErrorEmptyRowWithQuery(query)
-		}
-		return "", err
-	}
-	return fmt.Sprintf(routineStmtFmt, getReadableRoutineType(routineType), routineName, charset, charset, collation, sqlmode, stmt), nil
-}
-
-// getReadableRoutineType gets the printable routine type.
-func getReadableRoutineType(s string) string {
-	switch s {
-	case "FUNCTION":
-		return "Function"
-	case "PROCEDURE":
-		return "Procedure"
-	default:
-		return s
-	}
-}
-
-// getEvents gets all events of a database.
-func getEvents(txn *sql.Tx, dbName string) ([]*eventSchema, error) {
-	var events []*eventSchema
-	query := fmt.Sprintf("SHOW EVENTS FROM `%s`;", dbName)
-	rows, err := txn.Query(query)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	cols, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-	var values []interface{}
-	for i := 0; i < len(cols); i++ {
-		values = append(values, new(interface{}))
-	}
-	for rows.Next() {
-		var r eventSchema
-		if err := rows.Scan(values...); err != nil {
-			return nil, err
-		}
-		r.name = fmt.Sprintf("%s", *values[1].(*interface{}))
-		events = append(events, &r)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, util.FormatErrorWithQuery(err, query)
-	}
-
-	for _, r := range events {
-		stmt, err := getEventStmt(txn, dbName, r.name)
-		if err != nil {
-			return nil, fmt.Errorf("getEventStmt(%q, %q) got error: %s", dbName, r.name, err)
-		}
-		r.statement = stmt
-	}
-	return events, nil
-}
-
-// getEventStmt gets the create statement of an event.
-func getEventStmt(txn *sql.Tx, dbName, eventName string) (string, error) {
-	query := fmt.Sprintf("SHOW CREATE EVENT `%s`.`%s`;", dbName, eventName)
-	var sqlmode, timezone, stmt, charset, collation, unused string
-	if err := txn.QueryRow(query).Scan(&unused, &sqlmode, &timezone, &stmt, &charset, &collation, &unused); err != nil {
-		if err == sql.ErrNoRows {
-			return "", common.FormatDBErrorEmptyRowWithQuery(query)
-		}
-		return "", err
-	}
-	return fmt.Sprintf(eventStmtFmt, eventName, charset, charset, collation, sqlmode, timezone, stmt), nil
-}
-
-// getTriggers gets all triggers of a database.
-func getTriggers(txn *sql.Tx, dbName string) ([]*triggerSchema, error) {
-	var triggers []*triggerSchema
-	query := fmt.Sprintf("SHOW TRIGGERS FROM `%s`;", dbName)
-	rows, err := txn.Query(query)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	cols, err := rows.Columns()
-	if err != nil {
-		return nil, err
-	}
-	var values []interface{}
-	for i := 0; i < len(cols); i++ {
-		values = append(values, new(interface{}))
-	}
-	for rows.Next() {
-		var tr triggerSchema
-		if err := rows.Scan(values...); err != nil {
-			return nil, err
-		}
-		tr.name = fmt.Sprintf("%s", *values[0].(*interface{}))
-		triggers = append(triggers, &tr)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, util.FormatErrorWithQuery(err, query)
-	}
-	for _, tr := range triggers {
-		stmt, err := getTriggerStmt(txn, dbName, tr.name)
-		if err != nil {
-			return nil, fmt.Errorf("getTriggerStmt(%q, %q) got error: %s", dbName, tr.name, err)
-		}
-		tr.statement = stmt
-	}
-	return triggers, nil
-}
-
-// getTriggerStmt gets the create statement of a trigger.
-func getTriggerStmt(txn *sql.Tx, dbName, triggerName string) (string, error) {
-	query := fmt.Sprintf("SHOW CREATE TRIGGER `%s`.`%s`;", dbName, triggerName)
-	var sqlmode, stmt, charset, collation, unused string
-	if err := txn.QueryRow(query).Scan(&unused, &sqlmode, &stmt, &charset, &collation, &unused, &unused); err != nil {
-		if err == sql.ErrNoRows {
-			return "", common.FormatDBErrorEmptyRowWithQuery(query)
-		}
-		return "", err
-	}
-	return fmt.Sprintf(triggerStmtFmt, triggerName, charset, charset, collation, sqlmode, stmt), nil
 }
 
 // Restore restores a database.
