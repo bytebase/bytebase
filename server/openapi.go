a5ef03ecc5ba4b20e65493d22c9c5451efeb66d7
ecmadao <wlec@outlook.com>
2022-08-05 13:38:00 +0800

    fix: return empty database (#2170)
    * fix: return empty database
    
    * fix: return empty if the id is nil
    
    Former-commit-id: d33c2e2e4750dcee2f97f7bca954cdaa11671fec

--- server/openapi.go
+++ server/openapi.go
@@ -26,7 +26,7 @@ type catalogService struct{}
 // GetDatabase is the API message in catalog.
 // We will not connect to the user's database in the early version of sql check api.
 func (*catalogService) GetDatabase(_ context.Context) (*catalog.Database, error) {
-	return nil, nil
+	return &catalog.Database{}, nil
 }
 
 type sqlCheckRequestBody struct {
