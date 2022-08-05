a071a88196b6186c2dbafa297d698fd462bc9757
ZhengX <56376387+Megrax@users.noreply.github.com>
2022-08-05 16:05:28 +0800

    feat: open links in help in new tabs (#2174)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -31,7 +31,7 @@ import { isDev } from "./utils";
 import { BBNotificationItem } from "./bbkit/types";
 import KBarWrapper from "./components/KBar/KBarWrapper.vue";
 import BBModalStack from "./bbkit/BBModalStack.vue";
-import HelpDrawer from "@/components/HelpDrawer.vue";
+import HelpDrawer from "@/components/HelpDrawer";
 
 import { NConfigProvider, NDialogProvider } from "naive-ui";
 import { themeOverrides, dateLang, generalLang } from "../naive-ui.config";
