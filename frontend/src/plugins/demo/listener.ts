5bf1db5d88f4aad53e7bc86d0ec8cfcb227af8af
boojack <imrealleonardo@gmail.com>
2022-08-08 19:43:20 +0800

    chore: add some vue components for demo (#2079)
    Former-commit-id: a9622b86f1a5840bcd4ccfc8c9a82941278221c6
    Former-commit-id: 30e9cdadb8b128dbf69f3004beed55e4f1f55894

--- frontend/src/plugins/demo/listener.ts
+++ frontend/src/plugins/demo/listener.ts
@@ -1,46 +1,14 @@
 import { isEqual } from "lodash-es";
-import { fetchGuideDataWithName, fetchHintDataWithName } from "./data";
+import {
+  fetchDemoDataWithName,
+  fetchGuideDataWithName,
+  fetchHintDataWithName,
+} from "./data";
 import { removeGuideDialog, showGuideDialog } from "./guide";
 import { removeHint, showHints } from "./hint";
 import * as storage from "./storage";
 
-const refreshLocalStorageGuideData = async () => {
-  const params = new URLSearchParams(window.location.search);
-  const guideName = params.get("guide");
-  const { guide } = storage.get(["guide"]);
-
-  if (guideName && guideName !== guide?.name) {
-    const guideData = await fetchGuideDataWithName(guideName);
-
-    if (guideData) {
-      storage.set({
-        guide: {
-          name: guideName,
-          stepIndex: 0,
-        },
-      });
-    }
-  }
-};
-
-const refreshLocalStorageHintData = async () => {
-  const params = new URLSearchParams(window.location.search);
-  const hintName = params.get("hint");
-
-  if (hintName) {
-    const hintData = await fetchHintDataWithName(hintName);
-
-    if (hintData) {
-      storage.set({
-        hint: {
-          name: hintName,
-        },
-      });
-    }
-  }
-};
-
-const tryToShowGuideDialog = async () => {
+const tryToShowGuides = async () => {
   const { guide } = storage.get(["guide"]);
 
   if (guide) {
@@ -52,10 +20,10 @@ const tryToShowGuideDialog = async () => {
 };
 
 const tryToShowHints = async () => {
-  const { hint } = storage.get(["hint"]);
+  const { demo } = storage.get(["demo"]);
 
-  if (hint) {
-    const hintData = await fetchHintDataWithName(hint.name);
+  if (demo) {
+    const hintData = await fetchHintDataWithName(demo.name);
     showHints(hintData);
   } else {
     removeHint();
@@ -67,12 +35,11 @@ const initLocationListenerForGuide = () => {
   let prevLocationString = "";
 
   const observer = new MutationObserver(async () => {
-    await refreshLocalStorageGuideData();
     const locationString = window.location.toString();
 
     if (!isEqual(prevLocationString, locationString)) {
       prevLocationString = locationString;
-      tryToShowGuideDialog();
+      tryToShowGuides();
     }
   });
 
@@ -84,19 +51,16 @@ const initLocationListenerForGuide = () => {
 
 // initMutationListenerForHint using mutation observer to detect DOM changes.
 const initMutationListenerForHint = () => {
-  const observer = new MutationObserver(async (mutations: MutationRecord[]) => {
-    setTimeout(() => {
+  const observer = new MutationObserver((mutations: MutationRecord[]) => {
+    setTimeout(async () => {
       for (const mutation of mutations) {
         const changedNodes = [...mutation.addedNodes, ...mutation.removedNodes];
         for (const changedNode of changedNodes) {
           if (
             changedNode instanceof HTMLElement &&
-            !changedNode.matches(".bb-hint-wrapper") &&
-            !changedNode.matches(".bb-hint-cover-wrapper") &&
-            !changedNode.matches(".bb-hint-dialog") &&
-            !changedNode.matches(".bb-hint-tooltip-wrapper")
+            !changedNode.matches(".bb-demo-element")
           ) {
-            tryToShowHints();
+            await tryToShowHints();
             return;
           }
         }
@@ -110,17 +74,75 @@ const initMutationListenerForHint = () => {
   });
 };
 
-export const initGuideListeners = () => {
+const initGuideListeners = () => {
   initLocationListenerForGuide();
   window.addEventListener("storage", async () => {
-    tryToShowGuideDialog();
+    tryToShowGuides();
   });
 };
 
-export const initHintListeners = async () => {
-  await refreshLocalStorageHintData();
+const initHintListeners = () => {
   initMutationListenerForHint();
   window.addEventListener("storage", async () => {
     tryToShowHints();
   });
+};
+
+const initDemoListeners = async () => {
+  const { demo } = storage.get(["demo"]);
+  if (demo && demo.name) {
+    const demoData = await fetchDemoDataWithName(demo.name);
+    if (demoData) {
+      try {
+        const guideData = await fetchGuideDataWithName(demo.name);
+        if (guideData) {
+          await tryToShowGuides();
+          initGuideListeners();
+        }
+      } catch (error) {
+        // do nth
+      }
+
+      try {
+        const hintData = await fetchHintDataWithName(demo.name);
+        if (hintData) {
+          await tryToShowHints();
+          initHintListeners();
+        }
+      } catch (error) {
+        // do nth
+      }
+    }
+  }
+};
+
+export const initLocationListenerForDemo = async () => {
+  const { demo } = storage.get(["demo"]);
+
+  if (demo && demo.name) {
+    const demoData = await fetchDemoDataWithName(demo.name);
+    if (demoData) {
+      let prevLocationString = "";
+
+      const observer = new MutationObserver(async () => {
+        const locationString = window.location.toString();
+
+        if (!isEqual(prevLocationString, locationString)) {
+          prevLocationString = locationString;
+          // Do not show demo element in /auth pages.
+          if (window.location.pathname.startsWith("/auth")) {
+            return;
+          }
+
+          initDemoListeners();
+          observer.disconnect();
+        }
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+      });
+    }
+  }
 };
