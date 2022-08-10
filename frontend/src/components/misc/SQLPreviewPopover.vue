50a72e2e60aa0d9292ecd822a776f58f5d9c04dc
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-08-10 14:04:56 +0800

    feat(frontend): BYT-1053 make SQL change activity more readable (#2205)
    * fix: remove redundant tooltip-wrapper class
    
    * feat: extract SQLPreviewPopover component
    
    * feat: extract IssueActivityActionSentence component
    
    * chore: simplify code

---
+++ frontend/src/components/misc/SQLPreviewPopover.vue
@@ -0,0 +1,50 @@
+<template>
+  <NPopover
+    :disabled="statement.length <= maxLength"
+    :placement="placement"
+    style="max-height: 300px"
+    width="trigger"
+    scrollable
+  >
+    <highlight-code-block :code="statement" class="whitespace-pre-wrap" />
+
+    <template #trigger>
+      <span :class="statementClass">
+        {{
+          statement.length <= maxLength
+            ? statement
+            : statement.substring(0, maxLength) + "..."
+        }}
+      </span>
+    </template>
+  </NPopover>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { NPopover, PopoverPlacement } from "naive-ui";
+import { VueClass } from "@/utils";
+
+export default defineComponent({
+  name: "SQLPreviewPopover",
+  components: { NPopover },
+  props: {
+    statement: {
+      type: String,
+      required: true,
+    },
+    maxLength: {
+      type: Number,
+      default: 100,
+    },
+    placement: {
+      type: String as PropType<PopoverPlacement>,
+      default: "top",
+    },
+    statementClass: {
+      type: [String, Object, Array] as PropType<VueClass>,
+      default: undefined,
+    },
+  },
+});
+</script>
