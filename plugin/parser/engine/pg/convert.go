fefded9771700d7c24237817b06417d582358d61
rebelice <yangrebelice@gmail.com>
2022-08-11 12:04:39 +0800

    chore: not parse value list as SELECT statement (#2212)
    * chore: skip INSERT statment in WHERE requirement rule
    
    * chore: not parse value list as SELECT statement
    
    * Update plugin/parser/engine/pg/convert.go
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>

--- plugin/parser/engine/pg/convert.go
+++ plugin/parser/engine/pg/convert.go
@@ -336,8 +336,11 @@ func convert(node *pgquery.Node, text string) (res ast.Node, err error) {
 
 		if in.InsertStmt.SelectStmt != nil {
 			if selectNode, ok := in.InsertStmt.SelectStmt.Node.(*pgquery.Node_SelectStmt); ok {
-				if insertStmt.Select, err = convertSelectStmt(selectNode.SelectStmt); err != nil {
-					return nil, err
+				// PG parser will parse the value list as a SELECT statement.
+				if selectNode.SelectStmt.ValuesLists == nil {
+					if insertStmt.Select, err = convertSelectStmt(selectNode.SelectStmt); err != nil {
+						return nil, err
+					}
 				}
 			} else {
 				return nil, parser.NewConvertErrorf("expected SelectStmt but found %t", in.InsertStmt.SelectStmt.Node)
