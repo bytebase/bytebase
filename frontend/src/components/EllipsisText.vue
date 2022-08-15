997d7218516977aed1e7253138784911156f1585
ZhengX <56376387+Megrax@users.noreply.github.com>
2022-08-15 16:46:30 +0800

    fix(frontend): long name text overflow (#2262)

---
+++ frontend/src/components/EllipsisText.vue
@@ -0,0 +1,17 @@
+<template>
+  <span class="tooltip-wrapper w-full">
+    <span v-if="hasTextOverflow" class="tooltip whitespace-nowrap">
+      <slot></slot>
+    </span>
+    <div ref="textRef" class="truncate w-full"><slot></slot></div>
+  </span>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from "vue";
+
+const textRef = ref<HTMLElement | null>(null);
+const hasTextOverflow = computed(() => {
+  return textRef.value && textRef.value.offsetWidth < textRef.value.scrollWidth;
+});
+</script>
