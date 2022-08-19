f43e770a1671545841ba8d8b17e9244fe4bd10f9
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-08-19 14:12:02 +0800

    feat(frontend): disable enter key press (#2319)
    Former-commit-id: aea23a0ea1381aa45fa4e19c784c00aba4ab9c8f
    Former-commit-id: 54955502f0d5608b345a10e4b94935b056dfcce0

--- frontend/src/components/InstanceForm.vue
+++ frontend/src/components/InstanceForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="space-y-6 divide-y divide-block-border">
+  <div class="space-y-6 divide-y divide-block-border">
     <div class="divide-y divide-block-border px-1">
       <!-- Instance Name -->
       <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-4">
@@ -317,7 +317,7 @@
         </div>
       </div>
     </div>
-  </form>
+  </div>
 </template>
 
 <script lang="ts" setup>
