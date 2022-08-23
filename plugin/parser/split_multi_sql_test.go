48008ad6e54f7a258459ed5691757ceb08dcbc10
rebelice <yangrebelice@gmail.com>
2022-08-23 11:35:05 +0800

    chore: implement SplitMultiSQL for MySQL dialect (#2338)
    * chore: implement SplitMultiSQL for MySQL dialect
    
    * chore: refine the order
    
    * fix: address comment
    
    Former-commit-id: ecdc46e1ef0b0f03d4ceff488749912a4397ebb2
    Former-commit-id: b5e25e468cb93a26b585742b3e7d7f5096328023

--- plugin/parser/split_multi_sql_test.go
+++ plugin/parser/split_multi_sql_test.go
@@ -181,3 +181,200 @@ func TestPGSplitMultiSQL(t *testing.T) {
 		require.Equal(t, test.want, resData{res, errStr}, test.statement)
 	}
 }
+
+func TestMySQLSplitMultiSQL(t *testing.T) {
+	tests := []testData{
+		{
+			statement: "    CREATE TABLE t(a int); CREATE TABLE t1(a int)",
+			want: resData{
+				res: []SingleSQL{
+					{
+						Text: "CREATE TABLE t(a int);",
+						Line: 1,
+					},
+					{
+						Text: "CREATE TABLE t1(a int)",
+						Line: 1,
+					},
+				},
+			},
+		},
+		{
+			statement: "CREATE TABLE `tech_Book`(id int, name varchar(255));\n" +
+				"INSERT INTO `tech_Book` VALUES (0, 'abce_ksdf'), (1, 'lks''kjsafa\\'jdfl;\"ka');",
+			want: resData{
+				res: []SingleSQL{
+					{
+						Text: "CREATE TABLE `tech_Book`(id int, name varchar(255));",
+						Line: 1,
+					},
+					{
+						Text: "INSERT INTO `tech_Book` VALUES (0, 'abce_ksdf'), (1, 'lks''kjsafa\\'jdfl;\"ka');",
+						Line: 2,
+					},
+				},
+			},
+		},
+		{
+			statement: `
+						/* this is the comment. */
+						CREATE /* inline comment */TABLE tech_Book(id int, name varchar(255));
+						-- this is the comment.
+						INSERT INTO tech_Book VALUES (0, 'abce_ksdf'), (1, 'lks''kjsafa\'jdfl;"ka');
+						# this is the comment.
+						INSERT INTO tech_Book VALUES (0, 'abce_ksdf'), (1, 'lks''kjsafa\'jdfl;"ka');`,
+			want: resData{
+				res: []SingleSQL{
+					{
+						Text: `/* this is the comment. */
+						CREATE /* inline comment */TABLE tech_Book(id int, name varchar(255));`,
+						Line: 2,
+					},
+					{
+						Text: `-- this is the comment.
+						INSERT INTO tech_Book VALUES (0, 'abce_ksdf'), (1, 'lks''kjsafa\'jdfl;"ka');`,
+						Line: 4,
+					},
+					{
+						Text: `# this is the comment.
+						INSERT INTO tech_Book VALUES (0, 'abce_ksdf'), (1, 'lks''kjsafa\'jdfl;"ka');`,
+						Line: 6,
+					},
+				},
+			},
+		},
+		{
+			statement: `# test for defining stored programs
+						delimiter //
+						CREATE PROCEDURE dorepeat(p1 INT)
+						BEGIN
+							SET @x = 0;
+							REPEAT SET @x = @x + 1; UNTIL @x > p1 END REPEAT;
+						END
+						//
+						delimiter ;
+						CALL dorepeat(1000);
+						SELECT @x;
+						`,
+			want: resData{
+				res: []SingleSQL{
+					{
+						Text: `# test for defining stored programs
+						delimiter //`,
+						Line: 1,
+					},
+					{
+						Text: `CREATE PROCEDURE dorepeat(p1 INT)
+						BEGIN
+							SET @x = 0;
+							REPEAT SET @x = @x + 1; UNTIL @x > p1 END REPEAT;
+						END
+						//`,
+						Line: 3,
+					},
+					{
+						Text: `delimiter ;`,
+						Line: 9,
+					},
+					{
+						Text: `CALL dorepeat(1000);`,
+						Line: 10,
+					},
+					{
+						Text: `SELECT @x;`,
+						Line: 11,
+					},
+				},
+			},
+		},
+		{
+			statement: `# test for defining stored programs
+						delimiter //
+						CREATE PROCEDURE dorepeat(p1 INT)
+						/* This is a comment */
+						BEGIN
+							SET @x = 0;
+							REPEAT SET @x = @x + 1; UNTIL @x > p1 END REPEAT;
+						END
+						//
+						delimiter ;
+						CALL dorepeat(1000);
+						SELECT @x;
+						`,
+			want: resData{
+				res: []SingleSQL{
+					{
+						Text: `# test for defining stored programs
+						delimiter //`,
+						Line: 1,
+					},
+					{
+						Text: `CREATE PROCEDURE dorepeat(p1 INT)
+						/* This is a comment */
+						BEGIN
+							SET @x = 0;
+							REPEAT SET @x = @x + 1; UNTIL @x > p1 END REPEAT;
+						END
+						//`,
+						Line: 3,
+					},
+					{
+						Text: `delimiter ;`,
+						Line: 10,
+					},
+					{
+						Text: `CALL dorepeat(1000);`,
+						Line: 11,
+					},
+					{
+						Text: `SELECT @x;`,
+						Line: 12,
+					},
+				},
+			},
+		},
+		{
+			// test for Windows
+			statement: `CREATE TABLE t` + "\r\n" + `(a int);` + "\r\n" + `CREATE TABLE t1(b int);`,
+			want: resData{
+				res: []SingleSQL{
+					{
+						Text: "CREATE TABLE t\r\n(a int);",
+						Line: 1,
+					},
+					{
+						Text: "CREATE TABLE t1(b int);",
+						Line: 3,
+					},
+				},
+			},
+		},
+		{
+			statement: `INSERT INTO t VALUES ('klajfas)`,
+			want: resData{
+				err: "invalid string: not found delimiter: ', but found EOF",
+			},
+		},
+		{
+			statement: "INSERT INTO `t VALUES ('klajfas)",
+			want: resData{
+				err: "invalid indentifier: not found delimiter: `, but found EOF",
+			},
+		},
+		{
+			statement: "/*INSERT INTO `t VALUES ('klajfas)",
+			want: resData{
+				err: "invalid comment: not found */, but found EOF",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		res, err := SplitMultiSQL(MySQL, test.statement)
+		errStr := ""
+		if err != nil {
+			errStr = err.Error()
+		}
+		require.Equal(t, test.want, resData{res, errStr}, test.statement)
+	}
+}
