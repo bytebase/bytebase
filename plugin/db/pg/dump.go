6fc7e2ebc68a26e7d07bfaee66f009d6d8d539db
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-08-26 13:02:19 +0800

    fix: convert EXECUTE FUNCTION to PROCEDURE for CREATE EVENT TRIGGER statements (#2409)
    * fix: convert EXECUTE FUNCTION to PROCEDURE for CREATE EVENT TRIGGER statements
    
    * chore: update

--- plugin/db/pg/dump.go
+++ plugin/db/pg/dump.go
@@ -145,6 +145,16 @@ func (driver *Driver) Restore(ctx context.Context, sc io.Reader) error {
 	}
 
 	f := func(stmt string) error {
+		// CREATE EVENT TRIGGER statement only supports EXECUTE PROCEDURE in version 10 and before, while newer version supports both EXECUTE { FUNCTION | PROCEDURE }.
+		// Since we use pg_dump version 14, the dump uses new style even for old version of PostgreSQL.
+		// We should convert EXECUTE FUNCTION to EXECUTE PROCEDURE to make the restore to work on old versions.
+		// https://www.postgresql.org/docs/14/sql-createeventtrigger.html
+		if strings.Contains(strings.ToUpper(stmt), "CREATE EVENT TRIGGER") {
+			stmt = strings.ReplaceAll(stmt, "EXECUTE FUNCTION", "EXECUTE PROCEDURE")
+		}
+		if isSuperuserStatement(stmt) {
+			stmt = fmt.Sprintf("SET LOCAL ROLE NONE;%sSET LOCAL ROLE %s;", stmt, owner)
+		}
 		if _, err := txn.Exec(stmt); err != nil {
 			return err
 		}
