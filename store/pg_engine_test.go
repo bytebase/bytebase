b1ab882ad664a08c61cdc637c5c8216811b1aaaf
p0ny <xz@bytebase.com>
2022-08-30 10:55:06 +0800

    chore: move dml sql to prod (#2439)
    Former-commit-id: e97c59997f7445841bd267f0733964b6bb86c0de

--- store/pg_engine_test.go
+++ store/pg_engine_test.go
@@ -219,5 +219,5 @@ func TestMigrationCompatibility(t *testing.T) {
 func TestGetCutoffVersion(t *testing.T) {
 	releaseVersion, err := getProdCutoffVersion()
 	require.NoError(t, err)
-	require.Equal(t, semver.MustParse("1.3.3"), releaseVersion)
+	require.Equal(t, semver.MustParse("1.3.4"), releaseVersion)
 }
