859e46fb48efb3f50b62f69af947450d8554989b
rebelice <yangrebelice@gmail.com>
2022-08-30 13:38:08 +0800

    feat(parser): convert PG numeric data type (#2440)
    * feat: convert PG numeric data type
    
    * chore: convert serial to integer
    
    * fix: typo
    
    * chore: rename filter to strip
    
    Former-commit-id: 32176d7db6cfd64d0b77b42973dbab818d3c8175

---
+++ plugin/parser/ast/unconverted_data_type.go
@@ -0,0 +1,8 @@
+package ast
+
+// UnconvertedDataType is the struct for unconverted data type.
+// TODO(rebelice): remove it.
+// We define this because we can not convert all data types now.
+type UnconvertedDataType struct {
+	dataType
+}
