1351ddb76265c678bd9bae5f8dc6300f94c7f955
p0ny <xz@bytebase.com>
2022-08-30 15:56:56 +0800

    chore: cache static assets (#2445)

--- server/server_frontend_embed.go
+++ server/server_frontend_embed.go
@@ -15,8 +15,8 @@ import (
 //go:embed dist
 var embeddedFiles embed.FS
 
-func getFileSystem() http.FileSystem {
-	fs, err := fs.Sub(embeddedFiles, "dist")
+func getFileSystem(path string) http.FileSystem {
+	fs, err := fs.Sub(embeddedFiles, path)
 	if err != nil {
 		panic(err)
 	}
@@ -29,6 +29,18 @@ func embedFrontend(e *echo.Echo) {
 	// refer: https://github.com/labstack/echo/blob/master/middleware/static.go
 	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
 		HTML5:      true,
-		Filesystem: getFileSystem(),
+		Filesystem: getFileSystem("dist"),
+	}))
+
+	g := e.Group("assets")
+	g.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Response().Header().Set(echo.HeaderCacheControl, "max-age=31536000, immutable")
+			return next(c)
+		}
+	})
+	g.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		HTML5:      true,
+		Filesystem: getFileSystem("dist/assets"),
 	}))
 }
