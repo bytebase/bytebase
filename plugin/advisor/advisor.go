a117f7c179a2c73fd85b51aa377e8d3e4c899922
rebelice <yangrebelice@gmail.com>
2022-08-31 11:31:34 +0800

    feat(sql-review): implement MySQL column-exists rule (#2451)

--- plugin/advisor/advisor.go
+++ plugin/advisor/advisor.go
@@ -91,6 +91,9 @@ const (
 	// MySQLColumnSetDefaultForNotNull is an advisor type for MySQL set default value for not null column.
 	MySQLColumnSetDefaultForNotNull Type = "bb.plugin.advisor.mysql.column.set-default-for-not-null"
 
+	// MySQLColumnExists is an advisor type for MySQL column existence.
+	MySQLColumnExists Type = "bb.plugin.advisor.mysql.column.exists"
+
 	// MySQLNoSelectAll is an advisor type for MySQL no select all.
 	MySQLNoSelectAll Type = "bb.plugin.advisor.mysql.select.no-select-all"
 
