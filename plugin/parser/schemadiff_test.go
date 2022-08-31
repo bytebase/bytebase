1d6f3213a48bc1df87fdb1adb69a9d0f04f2fc25
Joe Chen <jc@unknwon.io>
2022-08-31 19:38:15 +0800

    feat(schemadiff): support compute schema diff and generate migration scripts

---
+++ plugin/parser/schemadiff_test.go
@@ -0,0 +1,45 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/bytebase/bytebase/plugin/parser"
+	// Register PostgreSQL parser engine.
+	_ "github.com/bytebase/bytebase/plugin/parser/engine/pg"
+)
+
+func TestComputeDiff(t *testing.T) {
+	oldSchema, err := parser.Parse(parser.Postgres, parser.ParseContext{}, `
+CREATE TABLE projects ();
+`)
+	require.NoError(t, err)
+	newSchema, err := parser.Parse(parser.Postgres, parser.ParseContext{}, `
+CREATE TABLE users (
+	id serial PRIMARY KEY,
+	username TEXT NOT NULL
+);
+CREATE TABLE projects ();
+CREATE TABLE repositories (
+	id serial PRIMARY KEY
+);
+`)
+	require.NoError(t, err)
+
+	diff, err := parser.ComputeDiff(oldSchema, newSchema)
+	require.NoError(t, err)
+	got := diff.String()
+
+	// The DDLs for the diff should be in the exact same order as the DDLs in the new schema.
+	want := `CREATE TABLE users (
+	id serial PRIMARY KEY,
+	username TEXT NOT NULL
+);
+CREATE TABLE repositories (
+	id serial PRIMARY KEY
+);
+`
+	assert.Equal(t, want, got)
+}
