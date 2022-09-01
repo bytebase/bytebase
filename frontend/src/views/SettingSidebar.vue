17cb77c8f8555c4316778466f26e92da4c6ed318
Megrax <geronimooo@foxmail.com>
2022-09-01 11:01:36 +0800

    chore: make entry visible to OWNER and DBA

--- frontend/src/views/SettingSidebar.vue
+++ frontend/src/views/SettingSidebar.vue
@@ -84,6 +84,7 @@
             >{{ $t("settings.sidebar.subscription") }}</router-link
           >
           <router-link
+            v-if="showOwnerOrDBAItem"
             to="/setting/debug-log"
             class="outline-item group w-full flex items-center pl-11 pr-2 py-2"
             >{{ $t("settings.sidebar.debug-logs") }}</router-link
