0ef7fa560a6f06165293b07168081c97d1a9a529
ZhengX <56376387+Megrax@users.noreply.github.com>
2022-09-01 16:01:04 +0800

    feat(frontend): ui for debug logs (#2473)
    * feat(front-end): UI for debug logs
    
    * style: fix detail key wrap
    
    * chore: make entry visible to OWNER and DBA
    
    * feat: add date in exported filename
    
    * chore: enhance i18n
    
    * style: use mono font to display debug log details
    
    * Update frontend/src/locales/en-US.json
    
    Co-authored-by: boojack <imrealleonardo@gmail.com>
    
    * chore: extract the definition of DebugLogState
    
    * chore: add route guard
    
    * feat: role limit on server side
    
    * chore: use debug-log for consistency
    
    * chore: fix eslint error
    
    Co-authored-by: boojack <imrealleonardo@gmail.com>
    Former-commit-id: 312e06d6c7b712d1b59b643a23bdbddd59c33aba
    Former-commit-id: d72820a690e45d1be8ccd6230835140f4e3199cb

--- frontend/src/store/modules/debug.ts
+++ frontend/src/store/modules/debug.ts
@@ -1,10 +1,17 @@
-import { defineStore } from "pinia";
+import { watchEffect } from "vue";
+import { defineStore, storeToRefs } from "pinia";
 import axios from "axios";
-import type { DebugState, ResourceObject } from "@/types";
-import type { Debug, DebugPatch } from "@/types/debug";
+import type { DebugState, DebugLogState, ResourceObject } from "@/types";
+import type { Debug, DebugPatch, DebugLog } from "@/types/debug";
 
-function convert(debug: ResourceObject): Debug {
+function convertDebugState(debug: ResourceObject): Debug {
   return { ...(debug.attributes as Omit<Debug, "">) };
+}
+
+function convertDebugLogList(debugLogList: ResourceObject[]): DebugLog[] {
+  return debugLogList.map(
+    (debugLog) => debugLog.attributes.errorRecord as DebugLog
+  );
 }
 
 export const useDebugStore = defineStore("debug", {
@@ -17,12 +24,12 @@ export const useDebugStore = defineStore("debug", {
     },
     async fetchDebug() {
       const res = (await axios.get("/api/debug")).data;
-      const debug = convert(res.data);
+      const debug = convertDebugState(res.data);
       this.setDebug(debug);
       return debug;
     },
     async patchDebug(debugPatch: DebugPatch) {
-      const debugState = convert(
+      const debugState = convertDebugState(
         (
           await axios.patch("/api/debug", {
             data: {
@@ -36,3 +43,27 @@ export const useDebugStore = defineStore("debug", {
     },
   },
 });
+
+export const useDebugLogStore = defineStore("debugLog", {
+  state: (): DebugLogState => ({
+    debugLogList: [],
+  }),
+  actions: {
+    setDebugLogList(debugLogList: DebugLog[]) {
+      this.debugLogList = debugLogList;
+    },
+    async fetchDebugLogList() {
+      const res = (await axios.get("/api/debug/log")).data.data;
+      const list = convertDebugLogList(res);
+      this.setDebugLogList(list);
+      return list;
+    },
+  },
+});
+
+export const useDebugLogList = () => {
+  const store = useDebugLogStore();
+  watchEffect(() => store.fetchDebugLogList());
+
+  return storeToRefs(store).debugLogList;
+};
