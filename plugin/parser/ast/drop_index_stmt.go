1d5fd04ee40a5a5453eaa7c866caadf5eaaae2cc
rebelice <yangrebelice@gmail.com>
2022-09-01 18:30:17 +0800

    fix: only block DDL/SELECT/EXPLAIN statement in CHANGE DATA (#2484)

--- plugin/parser/ast/drop_index_stmt.go
+++ plugin/parser/ast/drop_index_stmt.go
@@ -2,7 +2,7 @@ package ast
 
 // DropIndexStmt is the struct for drop index statement.
 type DropIndexStmt struct {
-	node
+	ddl
 
 	// Here use IndexDef because the drop index statement needs the schema name for PostgreSQL.
 	// If the drop index statement doesn't contain schema name, the Table of this index is nil.
