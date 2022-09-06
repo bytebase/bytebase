a618323a73efca23a305245e692c462556f1f870
Yilong Li <liyilongko@gmail.com>
2022-09-06 17:29:53 +0800

    feat: remove project key in patch request

--- api/project.go
+++ api/project.go
@@ -131,7 +131,6 @@ type ProjectPatch struct {
 
 	// Domain specific fields
 	Name                *string `jsonapi:"attr,name"`
-	Key                 *string `jsonapi:"attr,key"`
 	WorkflowType        *string `jsonapi:"attr,workflowType"` // NOTE: We can't use *ProjectWorkflowType because "google/jsonapi" doesn't support.
 	RoleProvider        *string `jsonapi:"attr,roleProvider"`
 	SchemaMigrationType *string `jsonapi:"attr,schemaMigrationType"` // NOTE: We can't use *ProjectSchemaMigrationType because "google/jsonapi" doesn't support.
