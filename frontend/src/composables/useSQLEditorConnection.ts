5b7dd57207721e8764f23f5c3524b6dfa3ba7033
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-09-07 17:02:55 +0800

    chore: remove unused comments

--- frontend/src/composables/useSQLEditorConnection.ts
+++ frontend/src/composables/useSQLEditorConnection.ts
@@ -42,7 +42,6 @@ const useSQLEditorConnection = () => {
         });
       }
     } else {
-      // sqlEditorStore.(getDefaultConnectionContext());
       tabStore.updateCurrentTab({
         connection: getDefaultConnection(),
       });
