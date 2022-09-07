46666bca4934ac63ee4c348eb3513f02e2162e09
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-09-07 10:22:30 +0800

    feat: add anomaly list API (#2557)
    Former-commit-id: ed5aafbfc27ad68be4ef67d65f7058ad0414dd39
    Former-commit-id: b3d249653c97a1a1c2dbd764490f89310c9ba82b

---
+++ server/anomaly.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/google/jsonapi"
+	"github.com/labstack/echo/v4"
+
+	"github.com/bytebase/bytebase/api"
+)
+
+func (s *Server) registerAnomalyRoutes(g *echo.Group) {
+	g.GET("/anomaly", func(c echo.Context) error {
+		ctx := c.Request().Context()
+		rowStatus := api.Normal
+		anomalyFind := &api.AnomalyFind{
+			RowStatus: &rowStatus,
+		}
+		if instanceIDStr := c.QueryParam("instance"); instanceIDStr != "" {
+			instanceID, err := strconv.Atoi(instanceIDStr)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Query parameter instance is not a number: %s", instanceIDStr)).SetInternal(err)
+			}
+			anomalyFind.InstanceOnly = true
+			anomalyFind.InstanceID = &instanceID
+		}
+		if databaseIDStr := c.QueryParam("database"); databaseIDStr != "" {
+			databaseID, err := strconv.Atoi(databaseIDStr)
+			if err != nil {
+				return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Query parameter database is not a number: %s", databaseIDStr)).SetInternal(err)
+			}
+			anomalyFind.InstanceID = &databaseID
+		}
+		anomalyList, err := s.store.FindAnomaly(ctx, anomalyFind)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to fetch anomaly list").SetInternal(err)
+		}
+
+		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
+		if err := jsonapi.MarshalPayload(c.Response().Writer, anomalyList); err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to marshal anomaly list response").SetInternal(err)
+		}
+		return nil
+	})
+}
