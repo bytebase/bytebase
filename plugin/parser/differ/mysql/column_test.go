7a30541ac59551543580a8c4d2d05b20ace24fb6
h3n4l <oysterdays@gmail.com>
2022-09-09 15:42:27 +0800

    feat: generate alter table stmt add column spec if origin table missing some columns

---
+++ plugin/parser/differ/mysql/column_test.go
@@ -0,0 +1,42 @@
+package mysql
+
+import (
+	"testing"
+
+	"github.com/pingcap/tidb/parser"
+	"github.com/stretchr/testify/require"
+)
+
+func TestColumn(t *testing.T) {
+	tests := []struct {
+		old  string
+		new  string
+		want string
+	}{
+		{
+			old:  `CREATE TABLE book(id INT, PRIMARY KEY(id))`,
+			new:  `CREATE TABLE book(id INT, price INT, PRIMARY KEY(id));`,
+			want: "ALTER TABLE `book` ADD COLUMN (`price` INT);\n",
+		},
+		{
+			old:  `CREATE TABLE book(id INT, PRIMARY KEY(id))`,
+			new:  `CREATE TABLE book(id INT, price INT, code VARCHAR(50), PRIMARY KEY(id));`,
+			want: "ALTER TABLE `book` ADD COLUMN (`price` INT);\nALTER TABLE `book` ADD COLUMN (`code` VARCHAR(50));\n",
+		},
+		{
+			old:  `CREATE TABLE book(id INT, price INT, code VARCHAR(50), PRIMARY KEY(id));`,
+			new:  `CREATE TABLE book(id INT, price INT, code VARCHAR(50), PRIMARY KEY(id));`,
+			want: "",
+		},
+	}
+	a := require.New(t)
+	for _, test := range tests {
+		oldNodes, _, err := parser.New().Parse(test.old, "", "")
+		a.NoError(err)
+		newNodes, _, err := parser.New().Parse(test.new, "", "")
+		a.NoError(err)
+		out, err := SchemaDiff(oldNodes, newNodes)
+		a.NoError(err)
+		a.Equal(test.want, out)
+	}
+}
--- plugin/parser/differ/mysql/differ.go
+++ plugin/parser/differ/mysql/differ.go
@@ -6,23 +6,84 @@ import (
 
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/format"
+	"github.com/pingcap/tidb/parser/model"
 )
+
+// columnMap maps column name to columnDef.
+type columnMap map[string]*ast.ColumnDef
+
+// tableColumnMap maps table name to columnMap.
+type tableColumnMap map[string]columnMap
+
+// set sets the columnDef of the `tableName`.`columnName`.
+func (m tableColumnMap) set(tableName, columnName string, columnDef *ast.ColumnDef) {
+	if _, ok := m[tableName]; !ok {
+		m[tableName] = make(columnMap)
+	}
+	m[tableName][columnName] = columnDef
+}
+
+// get gets the columnDef of the `tableName`.`columnName`.
+func (m tableColumnMap) get(tableName, columnName string) (*ast.ColumnDef, bool) {
+	if _, ok := m[tableName]; !ok {
+		return nil, false
+	}
+	columnDef, ok := m[tableName][columnName]
+	return columnDef, ok
+}
 
 // SchemaDiff returns the schema diff.
 func SchemaDiff(old, new []ast.StmtNode) (string, error) {
-	oldTableMap := make(map[string]ast.Node)
-	tableDiff := make([]ast.Node, 0)
+	oldTableMap := make(map[string]*ast.CreateTableStmt)
+	oldColumnMap := make(tableColumnMap)
+	diff := make([]ast.Node, 0)
 	for _, node := range old {
-		if stmt, ok := node.(*ast.CreateTableStmt); ok {
+		switch stmt := node.(type) {
+		case *ast.CreateTableStmt:
 			tableName := stmt.Table.Name.String()
-			oldTableMap[tableName] = node
+			oldTableMap[tableName] = stmt
+			for _, column := range stmt.Cols {
+				columnName := column.Name.Name.String()
+				oldColumnMap.set(tableName, columnName, column)
+			}
+		default:
 		}
 	}
+
+	newColumnMap := make(tableColumnMap)
+	newTableMap := make(map[string]*ast.CreateTableStmt)
 	for _, node := range new {
-		if stmt, ok := node.(*ast.CreateTableStmt); ok {
-			if _, ok := oldTableMap[stmt.Table.Name.String()]; !ok {
-				stmt.IfNotExists = true
-				tableDiff = append(tableDiff, node)
+		switch stmt := node.(type) {
+		case *ast.CreateTableStmt:
+			tableName := stmt.Table.Name.String()
+			newTableMap[tableName] = stmt
+			for _, column := range stmt.Cols {
+				columnName := column.Name.Name.String()
+				newColumnMap.set(tableName, columnName, column)
+			}
+		default:
+		}
+	}
+
+	for tableName, stmt := range newTableMap {
+		if _, ok := oldTableMap[tableName]; !ok {
+			stmt.IfNotExists = true
+			diff = append(diff, stmt)
+			continue
+		}
+		for columnName, columnDef := range newColumnMap[tableName] {
+			if _, ok := oldColumnMap.get(tableName, columnName); !ok {
+				diff = append(diff, &ast.AlterTableStmt{
+					Table: &ast.TableName{
+						Name: model.NewCIStr(tableName),
+					},
+					Specs: []*ast.AlterTableSpec{
+						{
+							Tp:         ast.AlterTableAddColumns,
+							NewColumns: []*ast.ColumnDef{columnDef},
+						},
+					},
+				})
 			}
 		}
 	}
@@ -33,11 +94,11 @@ func SchemaDiff(old, new []ast.StmtNode) (string, error) {
 			if err := node.Restore(format.NewRestoreCtx(format.DefaultRestoreFlags, &buf)); err != nil {
 				return "", err
 			}
-			if _, err := buf.Write([]byte("\n")); err != nil {
+			if _, err := buf.Write([]byte(";\n")); err != nil {
 				return "", err
 			}
 		}
 		return buf.String(), nil
 	}
-	return deparse(tableDiff)
+	return deparse(diff)
 }
