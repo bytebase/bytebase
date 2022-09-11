e34ae3d7b7604914ceb8dfdbd2aa322557a8337c
d-bytebase <d@bytebase.com>
2022-09-11 11:06:51 +0800

    chore: update

--- plugin/db/mysql/dump_test.go
+++ plugin/db/mysql/dump_test.go
@@ -9,6 +9,6 @@ import (
 func TestGetTemporalView(t *testing.T) {
 	a := require.New(t)
 	got := getTemporalView("db1", []string{"col1", "col2"})
-	want := "--\n-- Temporal view structure for `db1`\n--\nCREATE VIEW `db1` AS SELECT\n  1 AS `col1`,\n  1 AS `col2`;\n\n"
+	want := "--\n-- Temporary view structure for `db1`\n--\nCREATE VIEW `db1` AS SELECT\n  1 AS `col1`,\n  1 AS `col2`;\n\n"
 	a.Equal(want, got)
 }
