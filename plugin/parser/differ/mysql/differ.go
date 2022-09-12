6f7f1cc204c7b289724b054ceba85081a0ecf812
h3n4l <oysterdays@gmail.com>
2022-09-12 19:38:49 +0800

    feat: generate alter table stmt add column spec if origin table missing some columns (#2615)
    * feat: generate alter table stmt add column spec if origin table missing some columns
    
    * Update plugin/parser/differ/mysql/differ.go
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>
    
    * chore: merge all alter table add column specs in one alter table statement
    
    * refactor: use struct map to make it more simple
    
    * chore: update
    
    * chore: iterate slice instead of map to keep order
    
    Co-authored-by: Yilong Li <liyilongko@gmail.com>

--- plugin/parser/differ/mysql/differ.go
+++ plugin/parser/differ/mysql/differ.go
@@ -6,24 +6,52 @@ import (
 
 	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/format"
+	"github.com/pingcap/tidb/parser/model"
 )
 
 // SchemaDiff returns the schema diff.
 func SchemaDiff(old, new []ast.StmtNode) (string, error) {
-	oldTableMap := make(map[string]ast.Node)
-	tableDiff := make([]ast.Node, 0)
-	for _, node := range old {
-		if stmt, ok := node.(*ast.CreateTableStmt); ok {
-			tableName := stmt.Table.Name.String()
-			oldTableMap[tableName] = node
-		}
-	}
+	var diff []ast.Node
+	oldTableMap := buildTableMap(old)
+
 	for _, node := range new {
-		if stmt, ok := node.(*ast.CreateTableStmt); ok {
-			if _, ok := oldTableMap[stmt.Table.Name.String()]; !ok {
+		switch newStmt := node.(type) {
+		case *ast.CreateTableStmt:
+			tableName := newStmt.Table.Name.String()
+			oldStmt, ok := oldTableMap[tableName]
+			if !ok {
+				stmt := *newStmt
 				stmt.IfNotExists = true
-				tableDiff = append(tableDiff, node)
+				diff = append(diff, &stmt)
+				continue
 			}
+
+			var alterTableAddColumnSpecs []*ast.AlterTableSpec
+			var oldColumnMap = make(map[string]*ast.ColumnDef)
+			for _, oldColumnDef := range oldStmt.Cols {
+				oldColumnName := oldColumnDef.Name.Name.String()
+				oldColumnMap[oldColumnName] = oldColumnDef
+			}
+
+			for _, columnDef := range newStmt.Cols {
+				newColumnName := columnDef.Name.Name.String()
+				if _, ok := oldColumnMap[newColumnName]; !ok {
+					alterTableAddColumnSpecs = append(alterTableAddColumnSpecs, &ast.AlterTableSpec{
+						Tp:         ast.AlterTableAddColumns,
+						NewColumns: []*ast.ColumnDef{columnDef},
+					})
+				}
+			}
+
+			if len(alterTableAddColumnSpecs) > 0 {
+				diff = append(diff, &ast.AlterTableStmt{
+					Table: &ast.TableName{
+						Name: model.NewCIStr(tableName),
+					},
+					Specs: alterTableAddColumnSpecs,
+				})
+			}
+		default:
 		}
 	}
 
@@ -33,11 +61,25 @@ func SchemaDiff(old, new []ast.StmtNode) (string, error) {
 			if err := node.Restore(format.NewRestoreCtx(format.DefaultRestoreFlags, &buf)); err != nil {
 				return "", err
 			}
-			if _, err := buf.Write([]byte("\n")); err != nil {
+			if _, err := buf.Write([]byte(";\n")); err != nil {
 				return "", err
 			}
 		}
 		return buf.String(), nil
 	}
-	return deparse(tableDiff)
+	return deparse(diff)
+}
+
+// buildTableMap returns a map of table name to create table statements.
+func buildTableMap(nodes []ast.StmtNode) map[string]*ast.CreateTableStmt {
+	oldTableMap := make(map[string]*ast.CreateTableStmt)
+	for _, node := range nodes {
+		switch stmt := node.(type) {
+		case *ast.CreateTableStmt:
+			tableName := stmt.Table.Name.String()
+			oldTableMap[tableName] = stmt
+		default:
+		}
+	}
+	return oldTableMap
 }
