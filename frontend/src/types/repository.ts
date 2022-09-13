e8d8f30dee759f70a2204185bf5f994c0b6c5d03
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-09-13 16:55:34 +0800

    feat(frontend): open the VCS filePathTemplate as long as possible (#2637)
    * feat: implement the replacement logic
    
    * feat: pass replacement params
    
    * chore: address comments
    
    * Update frontend/src/types/repository.ts
    
    Co-authored-by: boojack <imrealleonardo@gmail.com>
    
    Co-authored-by: boojack <imrealleonardo@gmail.com>
    Former-commit-id: 78ac6092d268543183aa6f38d601f05f5768dde3
    Former-commit-id: 4f01154688d76c38a5f72bd044d4a547583b92e2

--- frontend/src/types/repository.ts
+++ frontend/src/types/repository.ts
@@ -79,20 +79,83 @@ export type ExternalRepositoryInfo = {
   webUrl: string;
 };
 
-export function baseDirectoryWebUrl(repository: Repository): string {
+type WebUrlReplaceParams = {
+  DB_NAME?: string;
+  VERSION?: string;
+  TYPE?: "migrate" | "data";
+  ENV_NAME?: string;
+};
+
+export function baseDirectoryWebUrl(
+  repository: Repository,
+  params: WebUrlReplaceParams = {}
+): string {
+  let url = "";
   if (repository.vcs.type == "GITLAB_SELF_HOST") {
-    let url = `${repository.webUrl}/-/tree/${repository.branchFilter}`;
+    url = `${repository.webUrl}/-/tree/${repository.branchFilter}`;
     if (!isEmpty(repository.baseDirectory)) {
       url += `/${repository.baseDirectory}`;
     }
-    return url;
   } else if (repository.vcs.type == "GITHUB_COM") {
-    let url = `${repository.webUrl}/tree/${repository.branchFilter}`;
+    url = `${repository.webUrl}/tree/${repository.branchFilter}`;
     if (!isEmpty(repository.baseDirectory)) {
       url += `/${repository.baseDirectory}`;
     }
+  }
+  if (url) {
+    // Replace the patterns in the filePathTemplate if possible.
+    const segments = repository.filePathTemplate.split("/");
+    segments.pop(); // exclude the last one, it's the filename.
+    // Try to replace the segments from left to right.
+    // Once we meet a "dynamic" segment which has a pattern that cannot be replaced
+    // we won't push it, either the segments behind it.
+    // E.g., the filePathTemplate is
+    // configure/{{ENV_NAME}}/20220707-wechat/{{TYPE}}/**/**/**/{{DB_NAME}}__{{VERSION}}__{{DESCRIPTION}}.sql
+    /**
+      The segments are
+        - configure
+        - {{ENV_NAME}}
+        - 20220707-wechat
+        - {{TYPE}}
+        - **
+        - **
+        - **
+      When
+        - ENV_NAME=dev
+        - TYPE=migrate
+      we are confident enough that the path will be started with
+      "/configure/dev/20220707-wechat/migrate"
+      That's our best effort.
+     */
+    for (let i = 0; i < segments.length; i++) {
+      const segment = segments[i];
+      const replaced = replaceParams(segment, params);
+      if (replaced.match(/[*{}]/)) {
+        // Still remained some patterns cannot be replaced in the value.
+        break;
+      }
+      url += `/${replaced}`;
+    }
+
     return url;
   }
 
+  // Fallback for other types of VCS.
+  // Shouldn't reach this line.
   return repository.webUrl;
 }
+
+const replaceParams = (
+  template: string,
+  params: WebUrlReplaceParams = {}
+): string => {
+  let replaced = template;
+  Object.keys(params).forEach((key) => {
+    const pattern = `{{${key}}}`;
+    const value = params[key as keyof WebUrlReplaceParams];
+    if (value) {
+      replaced = replaced.replaceAll(pattern, value);
+    }
+  });
+  return replaced;
+};
