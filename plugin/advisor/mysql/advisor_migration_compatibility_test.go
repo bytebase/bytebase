c3f388e1b792022a815aecf9fc597081755dd6c4
rebelice <yangrebelice@gmail.com>
2022-09-20 17:50:29 +0800

    chore(sql-review): re-implement column no null and fix all MySQL advisor unit tests (#2735)
    * chore(sql-review): re-implement column no null and fix all MySQL advisor unit tests
    
    * test: fix tests
    
    Former-commit-id: 9cb30beec90b42bc156d0826e0e88e7de9678b83
    Former-commit-id: ec558096da671efc49061c312b5ed3fd2315d1e1

--- plugin/advisor/mysql/advisor_migration_compatibility_test.go
+++ plugin/advisor/mysql/advisor_migration_compatibility_test.go
@@ -11,37 +11,37 @@ import (
 func TestBasic(t *testing.T) {
 	tests := []advisor.TestCase{
 		{
-			Statement: "DROP DATABASE d1",
+			Statement: "DROP DATABASE test",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityDropDatabase,
 					Title:   "schema.backward-compatibility",
-					Content: "\"DROP DATABASE d1\" may cause incompatibility with the existing data and code",
+					Content: "\"DROP DATABASE test\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "DROP TABLE t1",
+			Statement: "DROP TABLE tech_book",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityDropTable,
 					Title:   "schema.backward-compatibility",
-					Content: "\"DROP TABLE t1\" may cause incompatibility with the existing data and code",
+					Content: "\"DROP TABLE tech_book\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "RENAME TABLE t1 to t2",
+			Statement: "RENAME TABLE tech_book to t2",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityRenameTable,
 					Title:   "schema.backward-compatibility",
-					Content: "\"RENAME TABLE t1 to t2\" may cause incompatibility with the existing data and code",
+					Content: "\"RENAME TABLE tech_book to t2\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
@@ -59,19 +59,19 @@ func TestBasic(t *testing.T) {
 			},
 		},
 		{
-			Statement: "CREATE UNIQUE INDEX idx1 ON t1 (f1)",
+			Statement: "CREATE UNIQUE INDEX idx1 ON tech_book (id)",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAddUniqueKey,
 					Title:   "schema.backward-compatibility",
-					Content: "\"CREATE UNIQUE INDEX idx1 ON t1 (f1)\" may cause incompatibility with the existing data and code",
+					Content: "\"CREATE UNIQUE INDEX idx1 ON tech_book (id)\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "DROP TABLE t1;DROP TABLE t2;",
+			Statement: "CREATE TABLE t1(a int);DROP TABLE t1;DROP TABLE tech_book;",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
@@ -84,7 +84,7 @@ func TestBasic(t *testing.T) {
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityDropTable,
 					Title:   "schema.backward-compatibility",
-					Content: "\"DROP TABLE t2;\" may cause incompatibility with the existing data and code",
+					Content: "\"DROP TABLE tech_book;\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
@@ -129,7 +129,7 @@ func TestBasic(t *testing.T) {
 func TestAlterTable(t *testing.T) {
 	tests := []advisor.TestCase{
 		{
-			Statement: "ALTER TABLE t1 ADD f1 TEXT",
+			Statement: "ALTER TABLE tech_book ADD f1 TEXT",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Success,
@@ -140,103 +140,103 @@ func TestAlterTable(t *testing.T) {
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 RENAME COLUMN f1 to f2",
+			Statement: "ALTER TABLE tech_book RENAME COLUMN id to f2",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityRenameColumn,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 RENAME COLUMN f1 to f2\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book RENAME COLUMN id to f2\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 DROP COLUMN f1",
+			Statement: "ALTER TABLE tech_book DROP COLUMN id",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityDropColumn,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 DROP COLUMN f1\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book DROP COLUMN id\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ADD PRIMARY KEY (f1)",
+			Statement: "ALTER TABLE tech_book DROP PRIMARY KEY;ALTER TABLE tech_book ADD PRIMARY KEY (id)",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAddPrimaryKey,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 ADD PRIMARY KEY (f1)\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book ADD PRIMARY KEY (id)\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ADD UNIQUE (f1)",
+			Statement: "ALTER TABLE tech_book ADD UNIQUE (id)",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAddUniqueKey,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 ADD UNIQUE (f1)\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book ADD UNIQUE (id)\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ADD UNIQUE KEY (f1)",
+			Statement: "ALTER TABLE tech_book ADD UNIQUE KEY (id)",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAddUniqueKey,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 ADD UNIQUE KEY (f1)\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book ADD UNIQUE KEY (id)\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ADD UNIQUE INDEX (f1)",
+			Statement: "ALTER TABLE tech_book ADD UNIQUE INDEX (id)",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAddUniqueKey,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 ADD UNIQUE INDEX (f1)\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book ADD UNIQUE INDEX (id)\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ADD FOREIGN KEY (f1) REFERENCES t2(f2)",
+			Statement: "ALTER TABLE tech_book ADD FOREIGN KEY (id) REFERENCES t2(f2)",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAddForeignKey,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 ADD FOREIGN KEY (f1) REFERENCES t2(f2)\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book ADD FOREIGN KEY (id) REFERENCES t2(f2)\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ADD CHECK (f1 > 0)",
+			Statement: "ALTER TABLE tech_book ADD CHECK (id > 0)",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAddCheck,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 ADD CHECK (f1 > 0)\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book ADD CHECK (id > 0)\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ADD CHECK (f1 > 0) NOT ENFORCED",
+			Statement: "ALTER TABLE tech_book ADD CHECK (id > 0) NOT ENFORCED",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Success,
@@ -246,19 +246,19 @@ func TestAlterTable(t *testing.T) {
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ALTER CHECK chk1 ENFORCED",
+			Statement: "ALTER TABLE tech_book ALTER CHECK chk1 ENFORCED",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAlterCheck,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 ALTER CHECK chk1 ENFORCED\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book ALTER CHECK chk1 ENFORCED\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ALTER CHECK chk1 NOT ENFORCED",
+			Statement: "ALTER TABLE tech_book ALTER CHECK chk1 NOT ENFORCED",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Success,
@@ -268,19 +268,19 @@ func TestAlterTable(t *testing.T) {
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ADD CONSTRAINT CHECK (f1 > 0)",
+			Statement: "ALTER TABLE tech_book ADD CONSTRAINT CHECK (id > 0)",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAddCheck,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 ADD CONSTRAINT CHECK (f1 > 0)\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book ADD CONSTRAINT CHECK (id > 0)\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 ADD CONSTRAINT CHECK (f1 > 0) NOT ENFORCED",
+			Statement: "ALTER TABLE tech_book ADD CONSTRAINT CHECK (id > 0) NOT ENFORCED",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Success,
@@ -291,13 +291,13 @@ func TestAlterTable(t *testing.T) {
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 RENAME TO t2",
+			Statement: "ALTER TABLE tech_book RENAME TO t2",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityRenameTable,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 RENAME TO t2\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book RENAME TO t2\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
@@ -314,61 +314,61 @@ func TestAlterTable(t *testing.T) {
 func TestAlterTableChangeColumnType(t *testing.T) {
 	tests := []advisor.TestCase{
 		{
-			Statement: "ALTER TABLE t1 CHANGE f1 f2 TEXT",
+			Statement: "ALTER TABLE tech_book CHANGE name f2 TEXT",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAlterColumn,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 CHANGE f1 f2 TEXT\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book CHANGE name f2 TEXT\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 MODIFY f1 TEXT",
+			Statement: "ALTER TABLE tech_book MODIFY name TEXT",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAlterColumn,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 MODIFY f1 TEXT\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book MODIFY name TEXT\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 MODIFY f1 TEXT NULL",
+			Statement: "ALTER TABLE tech_book MODIFY name TEXT NULL",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAlterColumn,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 MODIFY f1 TEXT NULL\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book MODIFY name TEXT NULL\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 MODIFY f1 TEXT NOT NULL",
+			Statement: "ALTER TABLE tech_book MODIFY name TEXT NOT NULL",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAlterColumn,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 MODIFY f1 TEXT NOT NULL\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book MODIFY name TEXT NOT NULL\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
 		},
 		{
-			Statement: "ALTER TABLE t1 MODIFY f1 TEXT COMMENT 'bla'",
+			Statement: "ALTER TABLE tech_book MODIFY name TEXT COMMENT 'bla'",
 			Want: []advisor.Advice{
 				{
 					Status:  advisor.Warn,
 					Code:    advisor.CompatibilityAlterColumn,
 					Title:   "schema.backward-compatibility",
-					Content: "\"ALTER TABLE t1 MODIFY f1 TEXT COMMENT 'bla'\" may cause incompatibility with the existing data and code",
+					Content: "\"ALTER TABLE tech_book MODIFY name TEXT COMMENT 'bla'\" may cause incompatibility with the existing data and code",
 					Line:    1,
 				},
 			},
