bd82405ef21b85ba5f2003892aaf304da748fe1e
steven <stevenlgtm@gmail.com>
2022-09-23 11:01:27 +0800

    fix: fetch data directly for onboarding guide

--- frontend/src/components/OnboardingGuide.vue
+++ frontend/src/components/OnboardingGuide.vue
@@ -5,15 +5,10 @@
 </template>
 
 <script lang="ts" setup>
+import axios from "axios";
 import { onMounted, ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import {
-  useCurrentUser,
-  useProjectStore,
-  useInstanceStore,
-  useDatabaseStore,
-  useOnboardingGuideStore,
-} from "@/store";
+import { useCurrentUser, useOnboardingGuideStore } from "@/store";
 import { UNKNOWN_ID } from "@/types";
 import { isDev, isOwner } from "@/utils";
 import CreateDatabaseGuide from "./OnboardingGuides/CreateDatabaseGuide.vue";
@@ -45,9 +40,10 @@ const checkShouldShowCreateDatabaseGuide = async () => {
 
   // Show create database guide when user is owner and no data at all.
   if (isOwner(currentUser.value.role)) {
-    const instanceList = await useInstanceStore().fetchInstanceList();
-    const projectList = await useProjectStore().fetchAllProjectList();
-    const databaseList = await useDatabaseStore().fetchDatabaseList();
+    // Fetch data directly instead of useStore to prevent data from being cached in store.
+    const { data: instanceList } = (await axios.get(`/api/instance`)).data;
+    const { data: projectList } = (await axios.get(`/api/project`)).data;
+    const { data: databaseList } = (await axios.get(`/api/database`)).data;
 
     if (
       instanceList.length === 0 &&
