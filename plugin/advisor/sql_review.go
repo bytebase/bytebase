5210c046b594fba4583687b3798bd78d9bf967bd
rebelice <yangrebelice@gmail.com>
2022-09-28 00:12:56 +0800

    feat(sql-review): implement MySQL insert-row-limit rule (#2829)
    * feat(sql-review): implement MySQL insert-row-limit rule
    
    * Update plugin/advisor/mysql/advisor_insert_row_limit.go
    
    * Update plugin/advisor/mysql/advisor_insert_row_limit_test.go
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>

--- plugin/advisor/sql_review.go
+++ plugin/advisor/sql_review.go
@@ -108,6 +108,9 @@ const (
 	// SchemaRuleCharsetAllowlist enforce the charset allowlist.
 	SchemaRuleCharsetAllowlist SQLReviewRuleType = "charset.allowlist"
 
+	// SchemaRuleInsertRowLimit enforce the insert row limit.
+	SchemaRuleInsertRowLimit SQLReviewRuleType = "insert.row-limit"
+
 	// TableNameTemplateToken is the token for table name.
 	TableNameTemplateToken = "{{table}}"
 	// ColumnListTemplateToken is the token for column name list.
@@ -200,7 +203,7 @@ func (rule *SQLReviewRule) Validate() error {
 		if _, err := UnmarshalCommentConventionRulePayload(rule.Payload); err != nil {
 			return err
 		}
-	case SchemaRuleIndexKeyNumberLimit:
+	case SchemaRuleIndexKeyNumberLimit, SchemaRuleInsertRowLimit:
 		if _, err := UnmarshalNumberLimitRulePayload(rule.Payload); err != nil {
 			return err
 		}
@@ -763,6 +766,11 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine db.Type) (Type, err
 		switch engine {
 		case db.MySQL, db.TiDB:
 			return MySQLIndexTypeNoBlob, nil
+		}
+	case SchemaRuleInsertRowLimit:
+		switch engine {
+		case db.MySQL, db.TiDB:
+			return MySQLInsertRowLimit, nil
 		}
 	}
 	return Fake, errors.Errorf("unknown SQL review rule type %v for %v", ruleType, engine)
