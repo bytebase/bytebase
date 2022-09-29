a1026d61f9605dbd7ce15dde8e3aed2cc55f7973
d-bytebase <d@bytebase.com>
2022-09-29 10:16:58 +0800

    Revert "chore: update"
    This reverts commit b41b2bc979f8f3c96e7295f7f007f2e6c9959afa.

--- api/data_source.go
+++ api/data_source.go
@@ -62,11 +62,10 @@ type DataSource struct {
 	Type     DataSourceType `jsonapi:"attr,type"`
 	Username string         `jsonapi:"attr,username"`
 	// Do not return the password to client
-	Password string
-	SslCa    string
-	SslCert  string
-	SslKey   string
-	// HostOverride and PortOverride are only used for read-only data sources for user's read-replica instances.
+	Password     string
+	SslCa        string
+	SslCert      string
+	SslKey       string
 	HostOverride string
 	PortOverride string
 }
