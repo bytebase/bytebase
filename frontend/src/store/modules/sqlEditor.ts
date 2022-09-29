0f99f9547ca3f59afe91fceaf06c8d75856793f5
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-09-29 09:48:49 +0800

    feat: type definitions for sql editor query activity (#2838)

--- frontend/src/store/modules/sqlEditor.ts
+++ frontend/src/store/modules/sqlEditor.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import dayjs from "dayjs";
-import {
+import type {
   SQLEditorState,
   ConnectionAtom,
   QueryInfo,
@@ -8,11 +8,11 @@ import {
   DatabaseId,
   ProjectId,
   QueryHistory,
-  UNKNOWN_ID,
   InstanceId,
   Connection,
-  unknown,
+  ActivitySQLEditorQueryPayload,
 } from "@/types";
+import { UNKNOWN_ID, unknown } from "@/types";
 import { useActivityStore } from "./activity";
 import { useDatabaseStore } from "./database";
 import { useInstanceStore } from "./instance";
@@ -142,24 +142,23 @@ export const useSQLEditorStore = defineStore("sqlEditor", {
         await useActivityStore().fetchActivityListForQueryHistory({
           limit: 20,
         });
-      const queryHistoryList: QueryHistory[] = activityList.map(
-        (history: any) => {
-          return {
-            id: history.id,
-            creator: history.creator,
-            createdTs: history.createdTs,
-            updatedTs: history.updatedTs,
-            statement: history.payload.statement,
-            durationNs: history.payload.durationNs,
-            instanceName: history.payload.instanceName,
-            databaseName: history.payload.databaseName,
-            error: history.payload.error,
-            createdAt: dayjs(history.createdTs * 1000).format(
-              "YYYY-MM-DD HH:mm:ss"
-            ),
-          };
-        }
-      );
+      const queryHistoryList: QueryHistory[] = activityList.map((history) => {
+        const payload = history.payload as ActivitySQLEditorQueryPayload;
+        return {
+          id: history.id,
+          creator: history.creator,
+          createdTs: history.createdTs,
+          updatedTs: history.updatedTs,
+          statement: payload.statement,
+          durationNs: payload.durationNs,
+          instanceName: payload.instanceName,
+          databaseName: payload.databaseName,
+          error: payload.error,
+          createdAt: dayjs(history.createdTs * 1000).format(
+            "YYYY-MM-DD HH:mm:ss"
+          ),
+        };
+      });
 
       this.setQueryHistoryList(
         queryHistoryList.sort((a, b) => b.createdTs - a.createdTs)
