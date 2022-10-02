781915ecdaeccc240e87ec609c4b9c3066d6cd5a
ecmadao <wlec@outlook.com>
2022-10-02 16:59:45 +0800

    chore: setup sql review ci for gitlab

--- plugin/vcs/gitlab/gitlab.go
+++ plugin/vcs/gitlab/gitlab.go
@@ -13,19 +13,12 @@ import (
 	"strings"
 	"time"
 
-	_ "embed"
-
 	"github.com/pkg/errors"
 
 	"github.com/bytebase/bytebase/common"
 	"github.com/bytebase/bytebase/plugin/vcs"
 	"github.com/bytebase/bytebase/plugin/vcs/internal/oauth"
 )
-
-// CIFileContent is the GitLab CI for SQL review in VCS workflow.
-//
-//go:embed .gitlab-ci.yml
-var CIFileContent string
 
 const (
 	// SecretTokenLength is the length of secret token.
@@ -36,14 +29,20 @@ const (
 	// apiPageSize is the default page size when making API requests.
 	apiPageSize = 100
 
-	// SQLReviewCIFilePath is the remote file path for GitLab SQL review CI.
-	// We can include the remote file path in the GitLab CI.
-	// So we can store the script in our own GitHub repo.
-	// TODO: ed - change this to official repo.
-	// TODO: ed - and we'd better using version to control it, just like the GitHub action.
-	SQLReviewCIFilePath = "https://raw.githubusercontent.com/ed-bytebase/sql-review-gitlab-ci/main/sql-review.yml"
+	// SQLReviewCIVersion is the version for GitLab SQL review CI.
+	SQLReviewCIVersion = "0.0.1"
 	// CIFilePath is the local path for GitLab ci file.
 	CIFilePath = ".gitlab-ci.yml"
+)
+
+// SQLReviewCIFilePath is the remote file path for GitLab SQL review CI.
+// We can include the remote file path in the GitLab CI.
+// So we can store the script in our own GitHub repo.
+// TODO: ed - change this to official repo.
+// TODO: ed - and we'd better using version to control it, just like the GitHub action.
+var SQLReviewCIFilePath = fmt.Sprintf(
+	"https://raw.githubusercontent.com/ed-bytebase/sql-review-gitlab-ci/%s/sql-review.yml",
+	SQLReviewCIVersion,
 )
 
 var _ vcs.Provider = (*Provider)(nil)
