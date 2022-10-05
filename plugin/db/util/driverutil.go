0de12fbc8bec1ef6315e9d54171927687a239b0a
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-10-05 18:20:00 +0800

    chore: don't use readonly for clickhouse (#2897)

--- plugin/db/util/driverutil.go
+++ plugin/db/util/driverutil.go
@@ -313,7 +313,8 @@ func Query(ctx context.Context, dbType db.Type, sqldb *sql.DB, statement string,
 	readOnly := true
 	// TiDB doesn't support READ ONLY transactions. We have to skip the flag for it.
 	// https://github.com/pingcap/tidb/issues/34626
-	if dbType == db.TiDB {
+	// Clickhouse doesn't support READ ONLY transactions (Error: sql: driver does not support read-only transactions).
+	if dbType == db.TiDB || dbType == db.ClickHouse {
 		readOnly = false
 	}
 	tx, err := sqldb.BeginTx(ctx, &sql.TxOptions{ReadOnly: readOnly})
