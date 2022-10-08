6cf935d146a6a6cca44f56863b79cdd56e56de23
ecmadao <wlec@outlook.com>
2022-10-08 14:01:07 +0800

    chore: update plugin/vcs/vcs.go
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>

--- plugin/vcs/vcs.go
+++ plugin/vcs/vcs.go
@@ -22,7 +22,8 @@ const (
 	GitLabSelfHost Type = "GITLAB_SELF_HOST"
 	// GitHubCom is the VCS type for GitHub.com.
 	GitHubCom Type = "GITHUB_COM"
-	// SQLReviewAPISecretName is the api secret name used in GitHub action or GitLab CI workflow.
+
+		// SQLReviewAPISecretName is the api secret name used in GitHub action or GitLab CI workflow.
 	SQLReviewAPISecretName = "SQL_REVIEW_API_SECRET"
 )
 
