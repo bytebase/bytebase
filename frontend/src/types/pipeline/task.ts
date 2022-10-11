20c13cbf59a7eb06b0e6c0356393dd0a457fb9f8
p0ny <xz@bytebase.com>
2022-10-11 16:15:10 +0800

    feat: remove timing task check def & data (#2967)
    * feat: remove timing task check def & data
    
    * chore: remove on the frontend
    
    * fix: version
    
    * chore: remove on the fe
    
    Former-commit-id: f0055099a5b90c8728e91cb6d9689b54c57be033
    Former-commit-id: 22cf3c8cd3bb83e0d8cd0f994b991650b945de6a

--- frontend/src/types/pipeline/task.ts
+++ frontend/src/types/pipeline/task.ts
@@ -217,7 +217,6 @@ export type TaskCheckType =
   | "bb.task-check.database.statement.type"
   | "bb.task-check.database.connect"
   | "bb.task-check.instance.migration-schema"
-  | "bb.task-check.general.earliest-allowed-time"
   | "bb.task-check.database.ghost.sync"
   | "bb.task-check.issue.lgtm"
   | "bb.task-check.pitr.mysql";
