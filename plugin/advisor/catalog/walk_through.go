3caf76862e63034f596079daea55f84c03409ca2
rebelice <yangrebelice@gmail.com>
2022-10-11 11:07:04 +0800

    feat(catalog): disallow CREATE TABLE AS in walk through (#2952)
    * feat(catalog): disallow the CREATE TABLE AS statement in walk through
    
    * chore: remove disallow create table as rule
    
    Former-commit-id: ebb0c20e770d46d1ffff5ece36b663e65bb3bcb7
    Former-commit-id: 7ec72cc960db06b853c091f9fddd7b42b61a3e44

--- plugin/advisor/catalog/walk_through.go
+++ plugin/advisor/catalog/walk_through.go
@@ -48,6 +48,8 @@ const (
 	ErrorTypeTableExists = 301
 	// ErrorTypeTableNotExists is the error that table not exists.
 	ErrorTypeTableNotExists = 302
+	// ErrorTypeUseCreateTableAs is the error that using CREATE TABLE AS statements.
+	ErrorTypeUseCreateTableAs = 303
 
 	// 401 ~ 499 column error type.
 
@@ -968,6 +970,13 @@ func (d *DatabaseState) createTable(node *tidbast.CreateTableStmt) *WalkThroughE
 		return &WalkThroughError{
 			Type:    ErrorTypeTableExists,
 			Content: fmt.Sprintf("Table `%s` already exists", node.Table.Name.O),
+		}
+	}
+
+	if node.Select != nil {
+		return &WalkThroughError{
+			Type:    ErrorTypeUseCreateTableAs,
+			Content: fmt.Sprintf("Disallow the CREATE TABLE AS statement but \"%s\" uses", node.Text()),
 		}
 	}
 
