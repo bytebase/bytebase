71fbc828b421d5d92c7dca924c93d15731094747
p0ny <xz@bytebase.com>
2022-10-12 18:09:45 +0800

    feat: add cancel task result msg (#2989)
    * feat: add cancel task result msg
    
    * fix: result is a struct
    
    * chore: update

--- server/task.go
+++ server/task.go
@@ -684,9 +684,17 @@ func (s *Server) patchTaskStatus(ctx context.Context, task *api.Task, taskStatus
 		cancel, ok := s.TaskScheduler.runningExecutorsCancel[task.ID]
 		s.TaskScheduler.runningExecutorsMutex.Unlock()
 		if !ok {
-			return nil, errors.New("Failed to cancel task")
+			return nil, errors.New("failed to cancel task")
 		}
 		cancel()
+		result, err := json.Marshal(api.TaskRunResultPayload{
+			Detail: "Task cancellation requested.",
+		})
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to marshal TaskRunResultPayload")
+		}
+		resultStr := string(result)
+		taskStatusPatch.Result = &resultStr
 	}
 
 	taskPatched, err := s.store.PatchTaskStatus(ctx, taskStatusPatch)
