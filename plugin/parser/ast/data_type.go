614eee9dbc3191794346a8c0b370b352362342d9
rebelice <yangrebelice@gmail.com>
2022-10-12 18:46:57 +0800

    feat(parser): add EqualTypeName function to compare DataType with type name string (#2990)
    * feat(parser): add EqualTypeName function to compare DataType with type name string
    
    * chore: rename to EquivalentType
    
    Former-commit-id: b0e4f5f74a8adde0a2c92282360c5360ae344880
    Former-commit-id: 379e61948ff417b0fe1f5d5e51fdf8779c9ab078

--- plugin/parser/ast/data_type.go
+++ plugin/parser/ast/data_type.go
@@ -9,6 +9,7 @@ var (
 type DataType interface {
 	Node
 
+	EquivalentType(string) bool
 	dataTypeInterface()
 }
 
@@ -17,6 +18,10 @@ type dataType struct {
 }
 
 func (*dataType) dataTypeInterface() {}
+
+func (*dataType) EquivalentType(_ string) bool {
+	return false
+}
 
 // NumericType is the interface for numeric type.
 type NumericType interface {
