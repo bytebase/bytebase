f996cf7ec97ef39f39f754d05c73ae07c7c379e2
h3n4l <oysterdays@gmail.com>
2022-10-12 12:26:33 +0800

    chore: improve GitLab raw api comment (#2975)
    * chore: improve GitLab raw api comment
    
    * Update plugin/vcs/gitlab/gitlab.go
    
    Co-authored-by: Joe Chen <jc@unknwon.io>
    
    Co-authored-by: Joe Chen <jc@unknwon.io>
    Former-commit-id: 8d761e15fd014f00cdf44f3089f57c3f78a8c783

--- plugin/vcs/gitlab/gitlab.go
+++ plugin/vcs/gitlab/gitlab.go
@@ -1202,8 +1202,9 @@ func (p *Provider) DeleteWebhook(ctx context.Context, oauthCtx common.OauthConte
 // TODO: The same GitLab API endpoint supports using the HEAD request to only
 // get the file metadata.
 func (p *Provider) readFile(ctx context.Context, oauthCtx common.OauthContext, instanceURL, repositoryID, filePath, ref string) (*File, error) {
-	// Since GitLab of stale version doesn't support get large file content well, we move to use the raw api.
-	// See https://gitlab.com/gitlab-org/gitlab-pages/-/issues/315 for more details.
+	// GitLab is often deployed behind a reverse proxy, which may have compression enabled that is transparent to the GitLab instance.
+	// In such cases, the HTTP header "Content-Encoding" will, for example, be changed to "gzip" and makes the value of "Content-Length" untrustworthy.
+	// We can avoid dealing with this type of problem by using the raw API instead of the typical JSON API.
 	url := fmt.Sprintf("%s/projects/%s/repository/files/%s/raw?ref=%s", p.APIURL(instanceURL), repositoryID, url.QueryEscape(filePath), url.QueryEscape(ref))
 	code, header, body, err := oauth.Get(
 		ctx,
