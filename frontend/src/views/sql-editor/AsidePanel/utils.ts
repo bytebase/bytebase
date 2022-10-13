178800f6d4b8463ddf6c4d159772a1af9085253f
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-10-13 14:09:56 +0800

    fix: move the prefix render logic from item to the tree

--- frontend/src/views/sql-editor/AsidePanel/utils.ts
+++ frontend/src/views/sql-editor/AsidePanel/utils.ts
@@ -1,21 +1,12 @@
-import { h } from "vue";
 import { ConnectionAtom } from "@/types";
 import type { useInstanceStore } from "@/store";
-import InstanceEngineIconVue from "@/components/InstanceEngineIcon.vue";
-import HeroiconsOutlineDatabase from "~icons/heroicons-outline/database";
-import HeroiconsOutlineTable from "~icons/heroicons-outline/table";
 
 export const generateInstanceNode = (
   item: ConnectionAtom,
   instanceStore: ReturnType<typeof useInstanceStore>
 ) => {
-  const instance = instanceStore.getInstanceById(item.id);
   return {
     ...item,
-    prefix: () =>
-      h(InstanceEngineIconVue, {
-        instance,
-      }),
     children: item.children?.map(generateDatabaseItem),
   };
 };
@@ -23,10 +14,6 @@ export const generateInstanceNode = (
 export const generateDatabaseItem = (item: ConnectionAtom) => {
   return {
     ...item,
-    prefix: () =>
-      h(HeroiconsOutlineDatabase, {
-        class: "h-4 w-4",
-      }),
     children: item.children?.map(generateTableItem),
   };
 };
@@ -35,9 +22,5 @@ export const generateTableItem = (item: ConnectionAtom) => {
   return {
     ...item,
     isLeaf: true,
-    prefix: () =>
-      h(HeroiconsOutlineTable, {
-        class: "h-4 w-4",
-      }),
   };
 };
