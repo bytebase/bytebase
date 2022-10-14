0c84ac15ddfae6a3c685a669f199f95d0bd580cc
Yilong Li <liyilongko@gmail.com>
2022-10-14 17:16:50 +0800

    chore: calculate creatorID once

--- server/webhook.go
+++ server/webhook.go
@@ -649,6 +649,7 @@ func (s *Server) processFilesInProject(ctx context.Context, pushEvent vcs.PushEv
 	var createdIssueList []string
 	var fileNameList []string
 
+	creatorID := s.getIssueCreatorID(ctx, pushEvent.CommitList[0].AuthorEmail)
 	for _, fileInfo := range fileInfoList {
 		if fileInfo.fType == schemaFileType {
 			if repo.Project.SchemaChangeType == api.ProjectSchemaChangeTypeSDL {
@@ -659,7 +660,6 @@ func (s *Server) processFilesInProject(ctx context.Context, pushEvent vcs.PushEv
 					databaseName := fileInfo.migrationInfo.Database
 					issueName := fmt.Sprintf(issueNameTemplate, databaseName, "Alter schema")
 					issueDescription := fmt.Sprintf("Apply schema diff by file %s", strings.TrimPrefix(fileInfo.item.FileName, repo.BaseDirectory+"/"))
-					creatorID := s.getIssueCreatorID(ctx, pushEvent.CommitList[0].AuthorEmail)
 					if err := s.createIssueFromMigrationDetailList(ctx, issueName, issueDescription, pushEvent, creatorID, repo.ProjectID, migrationDetailListForFile); err != nil {
 						return "", false, activityCreateList, echo.NewHTTPError(http.StatusInternalServerError, "Failed to create issue").SetInternal(err)
 					}
@@ -700,7 +700,6 @@ func (s *Server) processFilesInProject(ctx context.Context, pushEvent vcs.PushEv
 	databaseName := fileInfoList[0].migrationInfo.Database
 	issueName := fmt.Sprintf(issueNameTemplate, databaseName, migrateType)
 	issueDescription := fmt.Sprintf("By VCS files %s", strings.Join(fileNameList, ", "))
-	creatorID := s.getIssueCreatorID(ctx, pushEvent.CommitList[0].AuthorEmail)
 	if err := s.createIssueFromMigrationDetailList(ctx, issueName, issueDescription, pushEvent, creatorID, repo.ProjectID, migrationDetailList); err != nil {
 		return "", len(createdIssueList) != 0, activityCreateList, echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to create issue %s", issueName)).SetInternal(err)
 	}
