81328f4f9dfacf600aaa75ae6797568bcee806fb
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-10-14 17:16:19 +0800

    feat(sql-editor): sub-query auto completion (#3031)
    * chore: misc definitions for subquery
    
    * feat: implement subquery mapping
    
    * chore: implement createSubQueryCandidates
    
    * feat: create subquery completion items as if they are virtual tables

---
+++ frontend/src/plugins/sql-lsp/server/complete/sub-query.ts
@@ -0,0 +1,38 @@
+import type { SubqueryNode } from "@joe-re/sql-parser";
+import type { SQLDialect, Table } from "@sql-lsp/types";
+import { getColumnsFromSelectStatement } from "./utils";
+
+export class SubQueryMapping {
+  tableList: Table[]; // not used yet
+  subQueries: SubqueryNode[];
+  dialect: SQLDialect; // not used yet
+  virtualTableList: Table[];
+
+  constructor(
+    tableList: Table[],
+    subQueries: SubqueryNode[],
+    dialect: SQLDialect
+  ) {
+    this.tableList = tableList;
+    this.subQueries = subQueries;
+    this.dialect = dialect;
+
+    this.virtualTableList = [];
+    subQueries.forEach((clause) => {
+      const { as } = clause;
+      if (!as) {
+        // If we met a sub query clause without alias,
+        // we cannot map it to a virtual table.
+        // So just skip it here.
+        return;
+      }
+      const columns = getColumnsFromSelectStatement(clause.subquery);
+      const virtualTable: Table = {
+        database: undefined,
+        name: as,
+        columns,
+      };
+      this.virtualTableList.push(virtualTable);
+    });
+  }
+}
