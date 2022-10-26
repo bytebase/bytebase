09e3972f511bc0e6b6206f015102ad9ea04e4791
Tianzhou (天舟) <t@bytebase.com>
2022-10-26 18:51:57 +0800

    chore: add back partial header (#3141)
    Former-commit-id: 37f5787b58c52f4aa1d3b26b68df37b285493a09

--- frontend/src/views/DashboardHeader.vue
+++ frontend/src/views/DashboardHeader.vue
@@ -62,6 +62,48 @@
     </div>
     <div>
       <div class="flex items-center space-x-3">
+        <div
+          v-if="isDevFeatures"
+          class="hidden md:flex sm:flex-row items-center space-x-2 text-sm font-medium"
+        >
+          <div
+            class="cursor-pointer hover:bg-link-hover focus:outline-none"
+            :class="currentPlan == 0 ? 'underline text-accent' : 'text-main'"
+            @click.prevent="switchToFree"
+          >
+            {{ $t("subscription.plan.free.title") }}
+          </div>
+          <div
+            class="cursor-pointer hover:bg-link-hover focus:outline-none"
+            :class="currentPlan == 1 ? 'underline text-accent' : 'text-main'"
+            @click.prevent="switchToTeam"
+          >
+            {{ $t("subscription.plan.team.title") }}
+          </div>
+          <div
+            class="cursor-pointer hover:bg-link-hover focus:outline-none"
+            :class="currentPlan == 2 ? 'underline text-accent' : 'text-main'"
+            @click.prevent="switchToEnterprise"
+          >
+            {{ $t("subscription.plan.enterprise.title") }}
+          </div>
+          <div class="cursor-pointer" @click="toggleLocales">
+            <heroicons-outline:translate class="w-6 h-6" />
+          </div>
+          <div class="cursor-pointer" @click="toggleDebug">
+            <svg
+              class="w-6 h-6"
+              :class="isDebug ? `text-accent` : `text-gray-400`"
+              fill="currentColor"
+              viewBox="0 0 32 32"
+            >
+              <path
+                d="M29,15h-5.1c-0.1-1.2-0.5-2.4-1-3.5c1.9-1.5,3.1-3.7,3.1-6.1V5c0-0.6-0.4-1-1-1s-1,0.4-1,1v0.4c0,1.8-0.8,3.4-2.2,4.5  c-0.5-0.7-1.2-1.2-1.9-1.7c0-0.1,0-0.1,0-0.2c0-2.2-1.8-4-4-4s-4,1.8-4,4c0,0.1,0,0.1,0,0.2c-0.7,0.5-1.3,1-1.9,1.7  C8.8,8.8,8,7.2,8,5.4V5c0-0.6-0.4-1-1-1S6,4.4,6,5v0.4c0,2.4,1.1,4.7,3.1,6.1c-0.5,1-0.9,2.2-1,3.5H3c-0.6,0-1,0.4-1,1s0.4,1,1,1  h5.1c0.1,1.2,0.5,2.4,1,3.5C7.1,21.9,6,24.2,6,26.6V27c0,0.6,0.4,1,1,1s1-0.4,1-1v-0.4c0-1.8,0.8-3.4,2.2-4.5  c1.5,1.8,3.5,2.9,5.8,2.9s4.4-1.1,5.8-2.9c1.4,1.1,2.2,2.7,2.2,4.5V27c0,0.6,0.4,1,1,1s1-0.4,1-1v-0.4c0-2.4-1.1-4.7-3.1-6.1  c0.5-1,0.9-2.2,1-3.5H29c0.6,0,1-0.4,1-1S29.6,15,29,15z"
+                stroke-width="1"
+              />
+            </svg>
+          </div>
+        </div>
         <router-link to="/inbox" exact-active-class>
           <span
             v-if="inboxSummary.hasUnread"
@@ -154,6 +196,7 @@ import { isDBAOrOwner, isDev } from "../utils";
 import { useLanguage } from "../composables/useLanguage";
 import {
   useCurrentUser,
+  useDebugStore,
   useSettingStore,
   useSubscriptionStore,
   useInboxStore,
@@ -170,12 +213,13 @@ export default defineComponent({
   components: { ProfileDropdown },
   setup() {
     const { t, availableLocales } = useI18n();
+    const debugStore = useDebugStore();
     const inboxStore = useInboxStore();
     const settingStore = useSettingStore();
     const subscriptionStore = useSubscriptionStore();
     const router = useRouter();
     const route = useRoute();
-    const { setLocale } = useLanguage();
+    const { setLocale, toggleLocales } = useLanguage();
 
     const state = reactive<LocalState>({
       showMobileMenu: false,
@@ -289,6 +333,30 @@ export default defineComponent({
     ]);
     useRegisterActions(kbarActions);
 
+    const switchToFree = () => {
+      subscriptionStore.patchSubscription("");
+    };
+
+    const switchToTeam = () => {
+      subscriptionStore.patchSubscription(
+        import.meta.env.BB_DEV_TEAM_LICENSE as string
+      );
+    };
+
+    const switchToEnterprise = () => {
+      subscriptionStore.patchSubscription(
+        import.meta.env.BB_DEV_ENTERPRISE_LICENSE as string
+      );
+    };
+
+    const { isDebug } = storeToRefs(debugStore);
+
+    const toggleDebug = () => {
+      debugStore.patchDebug({
+        isDebug: !isDebug.value,
+      });
+    };
+
     const I18N_CHANGE_ACTION_ID_NAMESPACE = "bb.preferences.locale";
     const i18nChangeAction = computed(() =>
       defineAction({
@@ -324,6 +392,12 @@ export default defineComponent({
       showDBAItem,
       isDevFeatures,
       inboxSummary,
+      switchToFree,
+      switchToTeam,
+      switchToEnterprise,
+      isDebug,
+      toggleDebug,
+      toggleLocales,
     };
   },
 });
