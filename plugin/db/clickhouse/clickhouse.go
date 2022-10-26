74866524fc18792bab02040ef3260ca5fd1b0f77
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-10-26 13:23:22 +0800

    feat: add query admin API support (#3135)
    * feat: add query admin API support
    
    * chore: update

--- plugin/db/clickhouse/clickhouse.go
+++ plugin/db/clickhouse/clickhouse.go
@@ -141,6 +141,6 @@ func (driver *Driver) Execute(ctx context.Context, statement string) error {
 }
 
 // Query queries a SQL statement.
-func (driver *Driver) Query(ctx context.Context, statement string, limit int) ([]interface{}, error) {
-	return util.Query(ctx, driver.dbType, driver.db, statement, limit)
+func (driver *Driver) Query(ctx context.Context, statement string, limit int, readOnly bool) ([]interface{}, error) {
+	return util.Query(ctx, driver.dbType, driver.db, statement, limit, readOnly)
 }
--- plugin/db/pg/pg.go
+++ plugin/db/pg/pg.go
@@ -365,8 +365,8 @@ func (driver *Driver) GetCurrentDatabaseOwner() (string, error) {
 }
 
 // Query queries a SQL statement.
-func (driver *Driver) Query(ctx context.Context, statement string, limit int) ([]interface{}, error) {
-	return util.Query(ctx, db.Postgres, driver.db, statement, limit)
+func (driver *Driver) Query(ctx context.Context, statement string, limit int, readOnly bool) ([]interface{}, error) {
+	return util.Query(ctx, db.Postgres, driver.db, statement, limit, readOnly)
 }
 
 func (driver *Driver) switchDatabase(dbName string) error {
