3fd29e034d64bc31180807214fe569613387813b
p0ny <p0uy@outlook.com>
2022-10-28 16:58:45 +0800

    fix: trigger & sequence

--- store/migration/dev/20221027000000##approval_instance.sql
+++ store/migration/dev/20221027000000##approval_instance.sql
@@ -11,3 +11,11 @@ CREATE TABLE approval_instance (
 );
 
 CREATE INDEX idx_approval_instance_issue_id ON approval_instance(issue_id); 
+
+ALTER SEQUENCE approval_instance_seq RESTART WITH 101;
+
+CREATE TRIGGER update_sheet_updated_ts
+BEFORE
+UPDATE
+    ON sheet FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
