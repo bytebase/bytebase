10269d4e1d5f08baeaf98b8f28ff7948c37876be
h3n4l <oysterdays@gmail.com>
2022-10-29 11:03:46 +0800

    chore: don't schedule backup task if backup already exist

--- server/database.go
+++ server/database.go
@@ -438,6 +438,17 @@ func (s *Server) registerDatabaseRoutes(g *echo.Group) {
 			return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Database not found with ID %d", id))
 		}
 
+		storeBackup, err := s.store.FindBackup(ctx, &api.BackupFind{
+			DatabaseID: &id,
+			Name:       &backupCreate.Name,
+		})
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to fetch backup Name %q", backupCreate.Name)).SetInternal(err)
+		}
+		if len(storeBackup) > 0 {
+			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("Backup Name %q already exists", backupCreate.Name))
+		}
+
 		backup, err := s.scheduleBackupTask(ctx, database, backupCreate.Name, backupCreate.Type, c.Get(getPrincipalIDContextKey()).(int))
 		if err != nil {
 			if common.ErrorCode(err) == common.DbConnectionFailure {
