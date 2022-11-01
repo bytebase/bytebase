cc7b8d78182508ca5e3e867eaa3833ed9542bffa
Yilong Li <liyilongko@gmail.com>
2022-11-01 15:02:48 +0800

    fix: use id to sort migration history (#3212)

--- plugin/db/snowflake/migrate.go
+++ plugin/db/snowflake/migrate.go
@@ -274,7 +274,7 @@ func (driver *Driver) FindMigrationHistoryList(ctx context.Context, find *db.Mig
 	}
 	var query = baseQuery +
 		db.FormatParamNameInQuestionMark(paramNames) +
-		`ORDER BY created_ts DESC`
+		`ORDER BY id DESC`
 	if v := find.Limit; v != nil {
 		query += fmt.Sprintf(" LIMIT %d", *v)
 	}
