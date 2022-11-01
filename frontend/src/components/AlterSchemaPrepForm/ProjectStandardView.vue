2f77513724cbc592a10e4505b85ffc824035b4c7
steven <stevenlgtm@gmail.com>
2022-11-01 15:15:41 +0800

    feat: multi database selector in tenant mode

--- frontend/src/components/AlterSchemaPrepForm/ProjectStandardView.vue
+++ frontend/src/components/AlterSchemaPrepForm/ProjectStandardView.vue
@@ -117,7 +117,7 @@ import {
   Project,
 } from "../../types";
 
-export type AlterType = "SINGLE_DB" | "MULTI_DB";
+export type AlterType = "SINGLE_DB" | "MULTI_DB" | "DB_GROUP";
 
 export type State = {
   alterType: AlterType;
