027c5047a170bd6ecdc41e42e12a41b5b75ae99f
ecmadao <wlec@outlook.com>
2022-11-17 14:26:26 +0800

    chore: implement CRUD for instance OpenAPI

--- api/v1/instance.go
+++ api/v1/instance.go
@@ -20,3 +20,30 @@ type Instance struct {
 	Port          string  `json:"port"`
 	Username      string  `json:"username"`
 }
+
+// InstanceCreate is the API message for creating an instance.
+type InstanceCreate struct {
+	// Related fields
+	Environment string `json:"environment"`
+
+	// Domain specific fields
+	Name         string  `json:"name"`
+	Engine       db.Type `json:"engine"`
+	ExternalLink string  `json:"externalLink"`
+	Host         string  `json:"host"`
+	Port         string  `json:"port"`
+	Username     string  `json:"username"`
+	Password     string  `json:"password"`
+	SslCa        string  `json:"sslCa"`
+	SslCert      string  `json:"sslCert"`
+	SslKey       string  `json:"sslKey"`
+}
+
+// InstancePatch is the API message for patching an instance.
+type InstancePatch struct {
+	// Domain specific fields
+	Name         *string `json:"name"`
+	ExternalLink *string `json:"externalLink"`
+	Host         *string `json:"host"`
+	Port         *string `json:"port"`
+}
--- server/openapi_instance.go
+++ server/openapi_instance.go
@@ -1,7 +1,11 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 
@@ -10,7 +14,10 @@ import (
 )
 
 func (s *Server) registerOpenAPIRoutesForInstance(g *echo.Group) {
+	g.POST("/instance", s.createInstanceByOpenAPI)
 	g.GET("/instance", s.listInstance)
+	g.GET("/instance/:instanceID", s.getInstanceByID)
+	g.PATCH("/instance/:instanceID", s.updateInstanceByOpenAPI)
 }
 
 func (s *Server) listInstance(c echo.Context) error {
@@ -31,6 +38,101 @@ func (s *Server) listInstance(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, response)
+}
+
+func (s *Server) createInstanceByOpenAPI(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	body, err := io.ReadAll(c.Request().Body)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Failed to read request body").SetInternal(err)
+	}
+
+	instanceCreate := &openAPIV1.InstanceCreate{}
+	if err := json.Unmarshal(body, instanceCreate); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Malformed create instance request").SetInternal(err)
+	}
+
+	environmentName := instanceCreate.Environment
+	envList, err := s.store.FindEnvironment(ctx, &api.EnvironmentFind{
+		Name: &environmentName,
+	})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to find environment by name: %s", instanceCreate.Environment)).SetInternal(err)
+	}
+	if len(envList) != 1 {
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Should only find one environment with name: %s", instanceCreate.Environment))
+	}
+
+	instance, err := s.createInstance(ctx, &api.InstanceCreate{
+		CreatorID:     c.Get(getPrincipalIDContextKey()).(int),
+		EnvironmentID: envList[0].ID,
+		Name:          instanceCreate.Name,
+		Engine:        instanceCreate.Engine,
+		ExternalLink:  instanceCreate.ExternalLink,
+		Host:          instanceCreate.Host,
+		Port:          instanceCreate.Port,
+		Username:      instanceCreate.Username,
+		Password:      instanceCreate.Password,
+		SslCa:         instanceCreate.SslCa,
+		SslCert:       instanceCreate.SslCert,
+		SslKey:        instanceCreate.SslKey,
+	})
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, convertToOpenAPIInstance(instance))
+}
+
+func (s *Server) updateInstanceByOpenAPI(c echo.Context) error {
+	ctx := c.Request().Context()
+	id, err := strconv.Atoi(c.Param("instanceID"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("ID is not a number: %s", c.Param("instanceID"))).SetInternal(err)
+	}
+
+	body, err := io.ReadAll(c.Request().Body)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Failed to read request body").SetInternal(err)
+	}
+
+	instancePatch := &openAPIV1.InstancePatch{}
+	if err := json.Unmarshal(body, instancePatch); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Malformed patch instance request").SetInternal(err)
+	}
+
+	instance, err := s.updateInstance(ctx, &api.InstancePatch{
+		ID:           id,
+		UpdaterID:    c.Get(getPrincipalIDContextKey()).(int),
+		Name:         instancePatch.Name,
+		ExternalLink: instancePatch.ExternalLink,
+		Host:         instancePatch.Host,
+		Port:         instancePatch.Port,
+	})
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, convertToOpenAPIInstance(instance))
+}
+
+func (s *Server) getInstanceByID(c echo.Context) error {
+	ctx := c.Request().Context()
+	id, err := strconv.Atoi(c.Param("instanceID"))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("ID is not a number: %s", c.Param("instanceID"))).SetInternal(err)
+	}
+
+	instance, err := s.store.GetInstanceByID(ctx, id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to fetch instance ID: %v", id)).SetInternal(err)
+	}
+	if instance == nil {
+		return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Instance ID not found: %d", id))
+	}
+
+	return c.JSON(http.StatusOK, convertToOpenAPIInstance(instance))
 }
 
 func convertToOpenAPIInstance(instance *api.Instance) *openAPIV1.Instance {
