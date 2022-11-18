80e41e6838705e3b708b083526e9220e23f53966
ecmadao <wlec@outlook.com>
2022-11-18 13:37:23 +0800

    chore: implement CRUD for instance OpenAPI (#3428)
    * chore: collect principal name
    
    * chore: add openapi for login and instance
    
    * chore: update api for openapi
    
    * chore: address comment
    
    * chore: implement CRUD for instance OpenAPI
    
    * chore: support delete instance api
    
    * chore: update rbac
    
    Former-commit-id: dd982251abf868ca37bc5a50b71e6e13d2d5ef15
    Former-commit-id: 441945fe36c8e525eae08b716b1e9de7889a0131

--- api/v1/instance.go
+++ api/v1/instance.go
@@ -6,7 +6,7 @@ import (
 
 // Instance is the API message for an instance.
 type Instance struct {
-	ID int `jsonapi:"primary,instance" json:"id"`
+	ID int `json:"id"`
 
 	// Related fields
 	Environment string `json:"environment"`
@@ -19,4 +19,31 @@ type Instance struct {
 	Host          string  `json:"host"`
 	Port          string  `json:"port"`
 	Username      string  `json:"username"`
+}
+
+// InstanceCreate is the API message for creating an instance.
+type InstanceCreate struct {
+	// Related fields
+	Environment string `json:"environment"`
+
+	// Domain specific fields
+	Name         string  `json:"name"`
+	Engine       db.Type `json:"engine"`
+	ExternalLink string  `json:"externalLink"`
+	Host         string  `json:"host"`
+	Port         string  `json:"port"`
+	Username     string  `json:"username"`
+	Password     string  `json:"password"`
+	SslCa        string  `json:"sslCa"`
+	SslCert      string  `json:"sslCert"`
+	SslKey       string  `json:"sslKey"`
+}
+
+// InstancePatch is the API message for patching an instance.
+type InstancePatch struct {
+	// Domain specific fields
+	Name         *string `json:"name"`
+	ExternalLink *string `json:"externalLink"`
+	Host         *string `json:"host"`
+	Port         *string `json:"port"`
 }
