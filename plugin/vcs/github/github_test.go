bf067ba9344bf78d61ba954da65f6db107982760
Yilong Li <liyilongko@gmail.com>
2022-11-18 15:28:10 +0800

    test(vcs): GetDiffFileList (#3442)
    Former-commit-id: ebe8f1b2238bbf4d99a908f72c8781faaeb63897
    Former-commit-id: 77b9b0ffaffd5e71c49f6d95f6ec686a49f8463a

--- plugin/vcs/github/github_test.go
+++ plugin/vcs/github/github_test.go
@@ -1308,6 +1308,46 @@ func TestProvider_ListPullRequestFile(t *testing.T) {
 	assert.Equal(t, want, got)
 }
 
+func TestProvider_GetDiffFileList(t *testing.T) {
+	p := newMockProvider(func(r *http.Request) (*http.Response, error) {
+		assert.Equal(t, "/api/v3/repos/1/compare/before_sha...after_sha", r.URL.Path)
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			// Example response taken from https://docs.github.com/en/rest/commits/commits#compare-two-commits
+			Body: io.NopCloser(strings.NewReader(`
+{
+	"files": [
+    {
+      "sha": "bbcd538c8e72b8c175046e27cc8f907076331401",
+      "filename": "file1.txt",
+      "status": "added",
+      "additions": 103,
+      "deletions": 21,
+      "changes": 124,
+      "blob_url": "https://github.com/octocat/Hello-World/blob/6dcb09b5b57875f334f61aebed695e2e4193db5e/file1.txt",
+      "raw_url": "https://github.com/octocat/Hello-World/raw/6dcb09b5b57875f334f61aebed695e2e4193db5e/file1.txt",
+      "contents_url": "https://api.github.com/repos/octocat/Hello-World/contents/file1.txt?ref=6dcb09b5b57875f334f61aebed695e2e4193db5e",
+      "patch": "@@ -132,7 +132,7 @@ module Test @@ -1000,7 +1000,7 @@ module Test"
+    }
+  ]
+}
+`)),
+		}, nil
+	},
+	)
+	ctx := context.Background()
+	got, err := p.GetDiffFileList(ctx, common.OauthContext{}, "", "1", "before_sha", "after_sha")
+	require.NoError(t, err)
+
+	want := []vcs.FileDiff{
+		{
+			Path: "file1.txt",
+			Type: vcs.FileDiffTypeAdded,
+		},
+	}
+	assert.Equal(t, want, got)
+}
+
 func newMockProvider(mockRoundTrip func(r *http.Request) (*http.Response, error)) vcs.Provider {
 	return newProvider(
 		vcs.ProviderConfig{
