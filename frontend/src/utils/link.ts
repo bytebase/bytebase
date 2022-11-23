14449c88eb224efd288479f7f7f247c81612673e
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-11-23 17:41:20 +0800

    feat: implement issue rollback button

--- frontend/src/utils/link.ts
+++ frontend/src/utils/link.ts
@@ -5,3 +5,9 @@ const resourceToPrefix = new Map([["DATABASE", "/db"]]);
 export function linkfy(type: ResourceType, id: string): string {
   return [resourceToPrefix.get(type), id].join("/");
 }
+
+export function absolutifyLink(rel: string): string {
+  const anchor = document.createElement("a");
+  anchor.setAttribute("href", rel);
+  return anchor.href;
+}
