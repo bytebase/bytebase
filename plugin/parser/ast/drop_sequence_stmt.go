594d0369e052a4efa26f315cf1da8ff2c9daa13d
h3n4l <oysterdays@gmail.com>
2022-11-23 18:04:26 +0800

    feat: deparse postgresql drop sequence statement

---
+++ plugin/parser/ast/drop_sequence_stmt.go
@@ -0,0 +1,10 @@
+package ast
+
+// DropSequenceStmt is the struct for drop sequence statement.
+type DropSequenceStmt struct {
+	ddl
+
+	IfExists         bool
+	SequenceNameList []SequenceNameDef
+	Behavior         DropBehavior
+}
