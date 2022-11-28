078a04c545449eeb4318a571216d50221f523d73
Tianzhou (天舟) <t@bytebase.com>
2022-11-28 17:39:18 +0800

    chore: add feature gate to store module to better track ongoing features (#3575)
    Former-commit-id: 87c5ceb742bbfe16ad7c85addb72349f10f94e5a

---
+++ store/feature_flag_prod.go
@@ -0,0 +1,11 @@
+//go:build release
+// +build release
+
+package store
+
+// FeatureFlag in release build always returns false.
+// Because we would remove the FeatureFlag check entirely after the schema change for the feature
+// is rolled out to prod.
+func FeatureFlag(_ FeatureFlagType) bool {
+	return false
+}
