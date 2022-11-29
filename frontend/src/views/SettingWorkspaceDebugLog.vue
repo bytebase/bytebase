65282632cb4fcc87c31ff008ab94ed4160896e3a
ZhengX <56376387+Megrax@users.noreply.github.com>
2022-11-29 16:44:19 +0800

    chore: sort debug logs in creation DESC order (#3605)

--- frontend/src/views/SettingWorkspaceDebugLog.vue
+++ frontend/src/views/SettingWorkspaceDebugLog.vue
@@ -16,7 +16,7 @@
         </button>
       </div>
       <DebugLogTable
-        :debug-log-list="debugLogList"
+        :debug-log-list="debugLogList.sort((a, b) => b.RecordTs - a.RecordTs)"
         @view-detail="
           (log: any) => {
             state.modalContent = log
