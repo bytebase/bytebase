b8ae3331cbde58f46fe08929acc689f6cb1343dc
d-bytebase <d@bytebase.com>
2022-12-02 12:08:53 +0800

    refactor: move some server code to enterprise service

--- server/subscription.go
+++ server/subscription.go
@@ -9,12 +9,9 @@ import (
 
 	"github.com/google/jsonapi"
 	"github.com/labstack/echo/v4"
-	"github.com/pkg/errors"
-	"go.uber.org/zap"
 
 	"github.com/bytebase/bytebase/api"
 	"github.com/bytebase/bytebase/common"
-	"github.com/bytebase/bytebase/common/log"
 	enterpriseAPI "github.com/bytebase/bytebase/enterprise/api"
 	metricAPI "github.com/bytebase/bytebase/metric"
 	"github.com/bytebase/bytebase/plugin/metric"
@@ -31,19 +28,19 @@ func (s *Server) registerSubscriptionRoutes(g *echo.Group) {
 
 	g.PATCH("/subscription", func(c echo.Context) error {
 		patch := &enterpriseAPI.SubscriptionPatch{}
+		ctx := c.Request().Context()
 		if err := jsonapi.UnmarshalPayload(c.Request().Body, patch); err != nil {
 			return echo.NewHTTPError(http.StatusBadRequest, "Malformed create subscription request").SetInternal(err)
 		}
 		patch.UpdaterID = c.Get(getPrincipalIDContextKey()).(int)
 
-		if err := s.LicenseService.StoreLicense(patch); err != nil {
+		if err := s.LicenseService.StoreLicense(ctx, patch); err != nil {
 			if common.ErrorCode(err) == common.Invalid {
 				return echo.NewHTTPError(http.StatusBadRequest, err.Error()).SetInternal(err)
 			}
 			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to store license").SetInternal(err)
 		}
 
-		ctx := c.Request().Context()
 		s.subscription = s.loadSubscription(ctx)
 
 		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
@@ -161,7 +158,7 @@ func (s *Server) loadSubscription(ctx context.Context) enterpriseAPI.Subscriptio
 		InstanceCount: 5,
 	}
 
-	license, _ := s.loadLicense(ctx)
+	license, _ := s.LicenseService.LoadLicense(ctx)
 	if license != nil {
 		subscription = enterpriseAPI.Subscription{
 			Plan:          license.Plan,
@@ -175,47 +172,6 @@ func (s *Server) loadSubscription(ctx context.Context) enterpriseAPI.Subscriptio
 	}
 
 	return subscription
-}
-
-// loadLicense will get and parse valid license from file.
-func (s *Server) loadLicense(ctx context.Context) (*enterpriseAPI.License, error) {
-	license, err := s.LicenseService.LoadLicense()
-	if license != nil {
-		log.Info(
-			"Load valid license",
-			zap.String("plan", license.Plan.String()),
-			zap.Time("expiresAt", time.Unix(license.ExpiresTs, 0)),
-			zap.Int("instanceCount", license.InstanceCount),
-		)
-
-		return license, nil
-	}
-
-	if common.ErrorCode(err) == common.NotFound {
-		log.Debug("Failed to find license", zap.String("error", err.Error()))
-	} else {
-		log.Warn("Failed to load valid license", zap.String("error", err.Error()))
-	}
-
-	// find free trial license in console
-	settingName := api.SettingEnterpriseTrial
-	settings, err := s.store.FindSetting(ctx, &api.SettingFind{
-		Name: &settingName,
-	})
-	if err != nil {
-		log.Warn("Failed to load trial license from settings", zap.String("error", err.Error()))
-		return nil, err
-	}
-	if len(settings) == 0 {
-		return nil, common.Wrapf(err, common.NotFound, "cannot find license")
-	}
-
-	var data enterpriseAPI.License
-	if err := json.Unmarshal([]byte(settings[0].Value), &data); err != nil {
-		return nil, errors.Wrapf(err, "failed to unmarshal value %q", settings[0].Value)
-	}
-
-	return &data, nil
 }
 
 func (s *Server) feature(feature api.FeatureType) bool {
