db15707b4f74a4638254d2e0551bbf1fd0590403
boojack <stevenlgtm@gmail.com>
2022-12-02 13:49:38 +0800

    feat: update i18n for UI editor (#3672)

--- frontend/src/components/UIEditor/EmptyTips.vue
+++ frontend/src/components/UIEditor/EmptyTips.vue
@@ -3,6 +3,6 @@
     class="w-full h-full flex flex-row justify-center items-center text-gray-600 font-medium"
   >
     <heroicons-solid:pencil-square class="w-4 h-auto mr-2" />
-    Click a database/table to start.
+    {{ $t("ui-editor.use-tips") }}
   </div>
 </template>
