28ceece99207c8e14d5df14a61dd1385102e749b
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-12-04 14:50:21 +0800

    feat: humanize with detail tooltip (#3690)

---
+++ frontend/src/components/misc/HumanizeTs.vue
@@ -0,0 +1,32 @@
+<template>
+  <NTooltip trigger="hover">
+    <template #trigger>
+      <span v-bind="$attrs">{{ humanized }}</span>
+    </template>
+
+    <span class="whitespace-nowrap">{{ detail }}</span>
+  </NTooltip>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import dayjs from "dayjs";
+import { NTooltip } from "naive-ui";
+
+import { humanizeTs } from "@/utils";
+
+const props = defineProps({
+  ts: {
+    type: Number,
+    required: true,
+  },
+  format: {
+    type: String,
+    default: "YYYY-MM-DD HH:mm:ss UTCZZ",
+  },
+});
+
+const humanized = computed(() => humanizeTs(props.ts));
+
+const detail = computed(() => dayjs(props.ts * 1000).format(props.format));
+</script>
