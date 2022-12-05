21f1e732717f2cb93fb4676feeda81a1b22ce5ed
adela <a@bytebase.com>
2022-12-05 16:46:08 +0800

    update devrel-demo

--- scripts/devrel-demo.sh
+++ scripts/devrel-demo.sh
@@ -7,8 +7,9 @@
 # ./devrel.sh
 # ./devrel.sh https://example.com
 # ./devrel.sh https://example.com:8080
+# ./staging.sh https://devrel-bytebase-demo.onrender.com' postgres://user:secret@postgres.example.com/bytebase
 
-# If no parameter is passed, use https://devrel.bytebase.com as host and 80 as port by default
+# If no parameter is passed, use https://devrel-bytebase-demo.onrender.com as host and 80 as port by default
 ONLINE_DEMO_HOST='https://devrel-bytebase-demo.onrender.com'
 ONLINE_DEMO_PORT='443'
 if [ $1 ]; then
@@ -16,13 +17,18 @@ if [ $1 ]; then
     URI=`echo $1 | awk -F '[/:]' '{ print $4; }'`
     PORT=`echo $1 | awk -F '[/:]' '{ print $5; }'`
 
-    ONLINE_DEMO_HOST=$PROTOCAL://$URI
+    ONLINE_DEMO_EXTERNAL_URL=$PROTOCAL://$URI
 
     if [ $PORT ]; then
         ONLINE_DEMO_PORT=$PORT
     fi
 fi
 
-echo "Starting Bytebase in demo mode at ${ONLINE_DEMO_HOST}:${ONLINE_DEMO_PORT}..."
+PG=''
+if [ $2 ]; then
+    PG="--pg $2"
+fi
 
-bytebase --host ${ONLINE_DEMO_HOST} --port ${ONLINE_DEMO_PORT} --data /var/opt/bytebase
+echo "Starting Bytebase in demo mode on port ${ONLINE_DEMO_PORT}, visiting from ${ONLINE_DEMO_EXTERNAL_URL} ..."
+
+bytebase --port ${ONLINE_DEMO_PORT} --external-url ${ONLINE_DEMO_EXTERNAL_URL} ${PG} --data /var/opt/bytebase
