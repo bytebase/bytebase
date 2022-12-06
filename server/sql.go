0a1128010da51adf1214c39ce5eabc8e6b9b42e2
rebelice <yangrebelice@gmail.com>
2022-12-06 11:05:57 +0800

    feat: masking sensitive column supports complex queries (#3702)
    * feat: masking sensitive column supports more queries
    
    * fix: get the correct column list
    
    * fix: create a new extractor for subquery
    
    * test: add unit tests for non-associated sub-query
    
    * fix: fix build
    
    * fix: skip the query which tries to access system database
    
    Former-commit-id: 083aa31b0f68b0da100093c22b7313b716f9d6f9

--- server/sql.go
+++ server/sql.go
@@ -308,14 +308,14 @@ func (s *Server) registerSQLRoutes(g *echo.Group) {
 			}
 		}
 
-		var sensitiveDataMap db.SensitiveDataMap
+		var sensitiveSchemaInfo *db.SensitiveSchemaInfo
 		if instance.Engine == db.MySQL || instance.Engine == db.TiDB {
 			databaseList, err := parser.ExtractDatabaseList(parser.MySQL, exec.Statement)
 			if err != nil {
 				return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to get database list: %s", exec.Statement)).SetInternal(err)
 			}
 
-			sensitiveDataMap, err = s.getSensitiveData(ctx, instance.Engine, instance.ID, databaseList, exec.DatabaseName)
+			sensitiveSchemaInfo, err = s.getSensitiveSchemaInfo(ctx, instance.Engine, instance.ID, databaseList, exec.DatabaseName)
 			if err != nil {
 				return err
 			}
@@ -331,10 +331,12 @@ func (s *Server) registerSQLRoutes(g *echo.Group) {
 			defer driver.Close(ctx)
 
 			rowSet, err := driver.Query(ctx, exec.Statement, &db.QueryContext{
-				Limit:            exec.Limit,
-				ReadOnly:         true,
-				CurrentDatabase:  exec.DatabaseName,
-				SensitiveDataMap: sensitiveDataMap,
+				Limit:           exec.Limit,
+				ReadOnly:        true,
+				CurrentDatabase: exec.DatabaseName,
+				// TODO(rebelice): we cannot deal with multi-SensitiveDataMaskType now. Fix it.
+				SensitiveDataMaskType: db.SensitiveDataMaskTypeDefault,
+				SensitiveSchemaInfo:   sensitiveSchemaInfo,
 			})
 			if err != nil {
 				return nil, err
@@ -480,10 +482,10 @@ func (s *Server) registerSQLRoutes(g *echo.Group) {
 			defer driver.Close(ctx)
 
 			rowSet, err := driver.Query(ctx, exec.Statement, &db.QueryContext{
-				Limit:            exec.Limit,
-				ReadOnly:         false,
-				CurrentDatabase:  exec.DatabaseName,
-				SensitiveDataMap: nil,
+				Limit:               exec.Limit,
+				ReadOnly:            false,
+				CurrentDatabase:     exec.DatabaseName,
+				SensitiveSchemaInfo: nil,
 			})
 			if err != nil {
 				return nil, err
@@ -715,8 +717,12 @@ func (s *Server) getDatabase(ctx context.Context, instanceID int, databaseName s
 	return dbList[0], nil
 }
 
-func (s *Server) getSensitiveData(ctx context.Context, engineType db.Type, instanceID int, databaseList []string, currentDatabase string) (db.SensitiveDataMap, error) {
-	res := make(db.SensitiveDataMap)
+func (s *Server) getSensitiveSchemaInfo(ctx context.Context, engineType db.Type, instanceID int, databaseList []string, currentDatabase string) (*db.SensitiveSchemaInfo, error) {
+	type sensitiveDataMap map[api.SensitiveData]api.SensitiveDataMaskType
+	isEmpty := true
+	result := &db.SensitiveSchemaInfo{
+		DatabaseList: []db.DatabaseSchema{},
+	}
 	for _, name := range databaseList {
 		databaseName := name
 		if name == "" {
@@ -735,20 +741,67 @@ func (s *Server) getSensitiveData(ctx context.Context, engineType db.Type, insta
 			return nil, err
 		}
 
+		columnMap := make(sensitiveDataMap)
+
 		policy, err := s.store.GetSensitiveDataPolicy(ctx, database.ID)
 		if err != nil {
 			return nil, echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to find sensitive data policy for database `%s` in instance ID: %d", databaseName, instanceID))
 		}
 		for _, data := range policy.SensitiveDataList {
-			res[db.SensitiveData{
-				Database: databaseName,
-				Table:    data.Table,
-				Column:   data.Column,
-			}] = db.SensitiveDataMaskType(data.Type)
+			columnMap[api.SensitiveData{
+				Table:  data.Table,
+				Column: data.Column,
+			}] = data.Type
 		}
+
+		tableList, err := s.store.FindTable(ctx, &api.TableFind{
+			DatabaseID: &database.ID,
+		})
+		if err != nil {
+			return nil, echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to find table list for database %q", databaseName))
+		}
+
+		databaseSchema := db.DatabaseSchema{
+			Name:      databaseName,
+			TableList: []db.TableSchema{},
+		}
+		for _, table := range tableList {
+			tableSchema := db.TableSchema{
+				Name:       table.Name,
+				ColumnList: []db.ColumnInfo{},
+			}
+			columnList, err := s.store.FindColumn(ctx, &api.ColumnFind{
+				DatabaseID: &database.ID,
+				TableID:    &table.ID,
+			})
+			if err != nil {
+				return nil, echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to find column list for table %q.%q", databaseName, table.Name))
+			}
+
+			for _, column := range columnList {
+				_, sensitive := columnMap[api.SensitiveData{
+					Table:  table.Name,
+					Column: column.Name,
+				}]
+				tableSchema.ColumnList = append(tableSchema.ColumnList, db.ColumnInfo{
+					Name:      column.Name,
+					Sensitive: sensitive,
+				})
+			}
+			databaseSchema.TableList = append(databaseSchema.TableList, tableSchema)
+		}
+		if len(databaseSchema.TableList) > 0 {
+			isEmpty = false
+		}
+		result.DatabaseList = append(result.DatabaseList, databaseSchema)
 	}
 
-	return res, nil
+	if isEmpty {
+		// If there is no tables, this query may access system databases, such as INFORMATION_SCHEMA.
+		// Skip to extract sensitive column for this query.
+		result = nil
+	}
+	return result, nil
 }
 
 func isExcludeDatabase(dbType db.Type, database string) bool {
