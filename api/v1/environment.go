6a2e1a359f03076e30d811ceb4d98de50e02e69e
ecmadao <wlec@outlook.com>
2022-12-08 21:01:22 +0800

    chore: support policy in environment for OpenAPI

--- api/v1/environment.go
+++ api/v1/environment.go
@@ -1,24 +1,29 @@
 package v1
 
+import "github.com/bytebase/bytebase/api"
+
 // Environment is the API message for an environment.
 type Environment struct {
 	ID int `json:"id"`
+
+	// Related fields
+	EnvironmentTierPolicy  *api.EnvironmentTierPolicy  `json:"environmentTierPolicy"`
+	PipelineApprovalPolicy *api.PipelineApprovalPolicy `json:"pipelineApprovalPolicy"`
+	BackupPlanPolicy       *api.BackupPlanPolicy       `json:"backupPlanPolicy"`
 
 	// Domain specific fields
 	Name  string `json:"name"`
 	Order int    `json:"order"`
 }
 
-// EnvironmentPatch is the API message for patching an environment.
-type EnvironmentPatch struct {
+// EnvironmentUpsert is the API message for upsert an environment.
+type EnvironmentUpsert struct {
+	// Related fields
+	EnvironmentTierPolicy  *api.EnvironmentTierPolicy  `json:"environmentTierPolicy"`
+	PipelineApprovalPolicy *api.PipelineApprovalPolicy `json:"pipelineApprovalPolicy"`
+	BackupPlanPolicy       *api.BackupPlanPolicy       `json:"backupPlanPolicy"`
+
 	// Domain specific fields
 	Name  *string `json:"name"`
 	Order *int    `json:"order"`
-}
-
-// EnvironmentCreate is the API message for creating an environment.
-type EnvironmentCreate struct {
-	// Domain specific fields
-	Name  string `json:"name"`
-	Order *int   `json:"order"`
 }
