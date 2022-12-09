0767dd8dc22a1132050eb2e67c8bccbbbbfb19d1
Steven <stevenlgtm@gmail.com>
2022-12-10 01:48:08 +0800

    chore: update column type&default validate

--- plugin/parser/edit/mysql/validate.go
+++ plugin/parser/edit/mysql/validate.go
@@ -1,6 +1,7 @@
 package mysql
 
 import (
+	"github.com/pingcap/tidb/parser/mysql"
 	"github.com/pkg/errors"
 
 	"github.com/bytebase/bytebase/api"
@@ -8,22 +9,37 @@ import (
 
 // ValidateDatabaseEdit validates the api message DatabaseEdit, including related column type.
 func (*SchemaEditor) ValidateDatabaseEdit(databaseEdit *api.DatabaseEdit) error {
+	var addColumnContextList []*api.AddColumnContext
+	var changeColumnContextList []*api.ChangeColumnContext
+
 	for _, createTableContext := range databaseEdit.CreateTableList {
-		for _, addColumnContext := range createTableContext.AddColumnList {
-			if _, err := transformColumnType(addColumnContext.Type); err != nil {
-				return errors.Errorf("invalid column type: %s", addColumnContext.Type)
+		addColumnContextList = append(addColumnContextList, createTableContext.AddColumnList...)
+	}
+	for _, alterTableContext := range databaseEdit.AlterTableList {
+		addColumnContextList = append(addColumnContextList, alterTableContext.AddColumnList...)
+		changeColumnContextList = append(changeColumnContextList, alterTableContext.ChangeColumnList...)
+	}
+
+	for _, addColumnContext := range addColumnContextList {
+		columnType, err := transformColumnType(addColumnContext.Type)
+		if err != nil {
+			return errors.Errorf("invalid column type: %s", addColumnContext.Type)
+		}
+		if addColumnContext.Default != nil {
+			if columnType.GetType() == mysql.TypeBlob || columnType.GetType() == mysql.TypeGeometry || columnType.GetType() == mysql.TypeJSON {
+				return errors.Errorf("column type `%s` cannot have a default value", addColumnContext.Type)
 			}
 		}
 	}
-	for _, alterTableContext := range databaseEdit.AlterTableList {
-		for _, addColumnContext := range alterTableContext.AddColumnList {
-			if _, err := transformColumnType(addColumnContext.Type); err != nil {
-				return errors.Errorf("invalid column type: %s", addColumnContext.Type)
-			}
+
+	for _, changeColumnContext := range changeColumnContextList {
+		columnType, err := transformColumnType(changeColumnContext.Type)
+		if err != nil {
+			return errors.Errorf("invalid column type: %s", changeColumnContext.Type)
 		}
-		for _, changeColumnContext := range alterTableContext.ChangeColumnList {
-			if _, err := transformColumnType(changeColumnContext.Type); err != nil {
-				return errors.Errorf("invalid column type: %s", changeColumnContext.Type)
+		if changeColumnContext.Default != nil {
+			if columnType.GetType() == mysql.TypeBlob || columnType.GetType() == mysql.TypeGeometry || columnType.GetType() == mysql.TypeJSON {
+				return errors.Errorf("column type `%s` cannot have a default value", changeColumnContext.Type)
 			}
 		}
 	}
