cef969110a15f86c8295d7f4b5ab23f495116e91
ecmadao <wlec@outlook.com>
2022-12-09 11:32:38 +0800

    chore: support policy in environment for OpenAPI (#3758)
    * chore: collect principal name
    
    * chore: support policy in environment for OpenAPI
    
    * chore: update comment
    
    * chore: support sql review policy
    
    * fix: openapi

--- api/v1/environment.go
+++ api/v1/environment.go
@@ -1,24 +1,34 @@
 package v1
 
+import (
+	"github.com/bytebase/bytebase/api"
+	"github.com/bytebase/bytebase/plugin/advisor"
+)
+
 // Environment is the API message for an environment.
 type Environment struct {
 	ID int `json:"id"`
+
+	// Related fields
+	EnvironmentTierPolicy  *api.EnvironmentTierPolicy  `json:"environmentTierPolicy,omitempty"`
+	PipelineApprovalPolicy *api.PipelineApprovalPolicy `json:"pipelineApprovalPolicy,omitempty"`
+	BackupPlanPolicy       *api.BackupPlanPolicy       `json:"backupPlanPolicy,omitempty"`
+	SQLReviewPolicy        *advisor.SQLReviewPolicy    `json:"sqlReviewPolicy,omitempty"`
 
 	// Domain specific fields
 	Name  string `json:"name"`
 	Order int    `json:"order"`
 }
 
-// EnvironmentPatch is the API message for patching an environment.
-type EnvironmentPatch struct {
+// EnvironmentUpsert is the API message for upsert an environment.
+type EnvironmentUpsert struct {
+	// Related fields
+	EnvironmentTierPolicy  *api.EnvironmentTierPolicy  `json:"environmentTierPolicy"`
+	PipelineApprovalPolicy *api.PipelineApprovalPolicy `json:"pipelineApprovalPolicy"`
+	BackupPlanPolicy       *api.BackupPlanPolicy       `json:"backupPlanPolicy"`
+	SQLReviewPolicy        *advisor.SQLReviewPolicy    `json:"sqlReviewPolicy"`
+
 	// Domain specific fields
 	Name  *string `json:"name"`
 	Order *int    `json:"order"`
-}
-
-// EnvironmentCreate is the API message for creating an environment.
-type EnvironmentCreate struct {
-	// Domain specific fields
-	Name  string `json:"name"`
-	Order *int   `json:"order"`
 }
