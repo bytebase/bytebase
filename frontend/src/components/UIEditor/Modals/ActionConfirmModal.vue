90177ed6782bee4e464f7165c3cc4cbf6d0141ff
boojack <stevenlgtm@gmail.com>
2022-12-10 01:24:40 +0800

    feat: add action confirm dialog for closing UI editor (#3776)
    * feat: add action confirm dialog for closing UI editor
    
    * chore: update
    
    Former-commit-id: 7769a6a5923afbc829e52c82e87ece3b2d87ed74
    Former-commit-id: b8d7a2a12846a9a001948a3fc5af51878a25f739

---
+++ frontend/src/components/UIEditor/Modals/ActionConfirmModal.vue
@@ -0,0 +1,47 @@
+<template>
+  <BBModal
+    :title="props.title"
+    class="shadow-inner outline outline-gray-200"
+    @close="dismissModal"
+  >
+    <div class="w-80 mb-2">
+      <p>{{ props.description }}</p>
+    </div>
+    <div class="w-full flex items-center justify-end mt-2 space-x-3 pr-1 pb-1">
+      <button type="button" class="btn-normal" @click="dismissModal">
+        {{ $t("common.cancel") }}
+      </button>
+      <button class="btn-primary" @click="handleConfirmButtonClick">
+        {{ $t("common.confirm") }}
+      </button>
+    </div>
+  </BBModal>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from "vue";
+
+const props = defineProps({
+  title: {
+    type: String as PropType<string>,
+    default: "",
+  },
+  description: {
+    type: String as PropType<string>,
+    default: "",
+  },
+});
+
+const emit = defineEmits<{
+  (event: "close"): void;
+  (event: "confirm"): void;
+}>();
+
+const handleConfirmButtonClick = async () => {
+  emit("confirm");
+};
+
+const dismissModal = () => {
+  emit("close");
+};
+</script>
