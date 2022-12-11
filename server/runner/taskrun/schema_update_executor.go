ea7eb0c4e642607fbfb6397948413e9d555fd444
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-12-12 00:06:59 +0800

    refactor: move sync schema to task executor (#3787)
    Former-commit-id: 9f717662d230d21a7bcca31f8c8eb773d8dbc19a
    Former-commit-id: b0bd706932daa3027187d98848dd1b9b5ea591b5

--- server/runner/taskrun/schema_update_executor.go
+++ server/runner/taskrun/schema_update_executor.go
@@ -5,23 +5,27 @@ import (
 	"encoding/json"
 
 	"github.com/pkg/errors"
+	"go.uber.org/zap"
 
 	"github.com/bytebase/bytebase/api"
+	"github.com/bytebase/bytebase/common/log"
 	"github.com/bytebase/bytebase/plugin/db"
 	"github.com/bytebase/bytebase/server/component/activity"
 	"github.com/bytebase/bytebase/server/component/config"
 	"github.com/bytebase/bytebase/server/component/dbfactory"
 	"github.com/bytebase/bytebase/server/component/state"
+	"github.com/bytebase/bytebase/server/runner/schemasync"
 	"github.com/bytebase/bytebase/store"
 )
 
 // NewSchemaUpdateExecutor creates a schema update (DDL) task executor.
-func NewSchemaUpdateExecutor(store *store.Store, dbFactory *dbfactory.DBFactory, activityManager *activity.Manager, stateCfg *state.State, profile config.Profile) Executor {
+func NewSchemaUpdateExecutor(store *store.Store, dbFactory *dbfactory.DBFactory, activityManager *activity.Manager, stateCfg *state.State, schemaSyncer *schemasync.Syncer, profile config.Profile) Executor {
 	return &SchemaUpdateExecutor{
 		store:           store,
 		dbFactory:       dbFactory,
 		activityManager: activityManager,
 		stateCfg:        stateCfg,
+		schemaSyncer:    schemaSyncer,
 		profile:         profile,
 	}
 }
@@ -32,15 +36,26 @@ type SchemaUpdateExecutor struct {
 	dbFactory       *dbfactory.DBFactory
 	activityManager *activity.Manager
 	stateCfg        *state.State
+	schemaSyncer    *schemasync.Syncer
 	profile         config.Profile
 }
 
 // RunOnce will run the schema update (DDL) task executor once.
-func (exec *SchemaUpdateExecutor) RunOnce(ctx context.Context, task *api.Task) (terminated bool, result *api.TaskRunResultPayload, err error) {
+func (exec *SchemaUpdateExecutor) RunOnce(ctx context.Context, task *api.Task) (bool, *api.TaskRunResultPayload, error) {
 	payload := &api.TaskDatabaseSchemaUpdatePayload{}
 	if err := json.Unmarshal([]byte(task.Payload), payload); err != nil {
 		return true, nil, errors.Wrap(err, "invalid database schema update payload")
 	}
 
-	return runMigration(ctx, exec.store, exec.dbFactory, exec.activityManager, exec.stateCfg, exec.profile, task, db.Migrate, payload.Statement, payload.SchemaVersion, payload.VCSPushEvent)
+	terminated, result, err := runMigration(ctx, exec.store, exec.dbFactory, exec.activityManager, exec.stateCfg, exec.profile, task, db.Migrate, payload.Statement, payload.SchemaVersion, payload.VCSPushEvent)
+
+	if err := exec.schemaSyncer.SyncDatabaseSchema(ctx, task.Instance, task.Database.Name); err != nil {
+		log.Error("failed to sync database schema",
+			zap.String("instanceName", task.Instance.Name),
+			zap.String("databaseName", task.Database.Name),
+			zap.Error(err),
+		)
+	}
+
+	return terminated, result, err
 }
