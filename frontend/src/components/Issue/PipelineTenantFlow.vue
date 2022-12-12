650eebdb54502333d87af83f9cad83432fe02eaa
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2022-12-12 11:52:09 +0800

    feat: task list overflow (#3795)
    Former-commit-id: 25971297ab1aff5a326eed7d6eafc0273a58fb9b
    Former-commit-id: eef56ae34c19b17dfd35e96d17be4a13974df2ea

--- frontend/src/components/Issue/PipelineTenantFlow.vue
+++ frontend/src/components/Issue/PipelineTenantFlow.vue
@@ -6,36 +6,47 @@
       </template>
     </PipelineStageList>
 
-    <div class="task-list gap-2 p-2 md:grid md:grid-cols-2 lg:grid-cols-4">
+    <div class="relative">
       <div
-        v-for="(task, j) in taskList"
-        :key="j"
-        class="task px-2 py-1 cursor-pointer border rounded"
-        :class="taskClass(task)"
-        @click="onClickTask(task, j)"
+        ref="taskBar"
+        class="task-list gap-2 p-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 4xl:grid-cols-6 max-h-48 overflow-y-auto"
+        :class="{
+          'more-bottom': !taskBarScrollState.arrivedState.bottom,
+          'more-top': !taskBarScrollState.arrivedState.top,
+        }"
       >
-        <div class="flex items-center pb-1">
-          <TaskStatusIcon
-            :create="create"
-            :active="isActiveTask(task)"
-            :status="task.status"
-            class="transform scale-75"
-          />
-          <heroicons-solid:arrow-narrow-right
-            v-if="isActiveTask(task)"
-            class="name w-5 h-5"
-          />
-          <div class="name">{{ j + 1 }} - {{ databaseForTask(task).name }}</div>
-        </div>
-        <div class="flex items-center justify-between px-1 py-1">
-          <div class="flex flex-1 items-center whitespace-pre-wrap">
-            <InstanceEngineIcon :instance="databaseForTask(task).instance" />
-            <span
-              class="flex-1 ml-2 overflow-x-hidden whitespace-nowrap overflow-ellipsis"
-              >{{ instanceName(databaseForTask(task).instance) }}</span
-            >
+        <div
+          v-for="(task, j) in taskList"
+          :key="j"
+          class="task px-2 py-1 cursor-pointer border rounded"
+          :class="taskClass(task)"
+          @click="onClickTask(task, j)"
+        >
+          <div class="flex items-center pb-1">
+            <TaskStatusIcon
+              :create="create"
+              :active="isActiveTask(task)"
+              :status="task.status"
+              class="transform scale-75"
+            />
+            <heroicons-solid:arrow-narrow-right
+              v-if="isActiveTask(task)"
+              class="name w-5 h-5"
+            />
+            <div class="name">
+              {{ j + 1 }} - {{ databaseForTask(task).name }}
+            </div>
           </div>
-          <TaskMarkAsDoneButton :task="(task as Task)" />
+          <div class="flex items-center justify-between px-1 py-1">
+            <div class="flex flex-1 items-center whitespace-pre-wrap">
+              <InstanceEngineIcon :instance="databaseForTask(task).instance" />
+              <span
+                class="flex-1 ml-2 overflow-x-hidden whitespace-nowrap overflow-ellipsis"
+                >{{ instanceName(databaseForTask(task).instance) }}</span
+              >
+            </div>
+            <TaskMarkAsDoneButton :task="(task as Task)" />
+          </div>
         </div>
       </div>
     </div>
@@ -43,7 +54,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, watchEffect } from "vue";
+import { computed, ref, watchEffect } from "vue";
 import type {
   Pipeline,
   Stage,
@@ -57,6 +68,7 @@ import TaskStatusIcon from "./TaskStatusIcon.vue";
 import TaskMarkAsDoneButton from "./TaskMarkAsDoneButton.vue";
 import { useDatabaseStore } from "@/store";
 import { useIssueLogic } from "./logic";
+import { useScroll } from "@vueuse/core";
 
 const {
   create,
@@ -70,6 +82,9 @@ const {
 const pipeline = computed(() => issue.value.pipeline!);
 
 const databaseStore = useDatabaseStore();
+const taskBar = ref<HTMLDivElement>();
+
+const taskBarScrollState = useScroll(taskBar);
 
 watchEffect(() => {
   if (create.value) {
@@ -165,5 +180,22 @@ const onClickTask = (task: Task | TaskCreate, index: number) => {
 }
 .task.status_failed .name {
   @apply text-red-500;
+}
+
+.task-list::before {
+  @apply absolute top-0 h-4 w-full -ml-2 z-50 pointer-events-none transition-shadow;
+  content: "";
+  box-shadow: none;
+}
+.task-list::after {
+  @apply absolute bottom-0 h-4 w-full -ml-2 z-50 pointer-events-none transition-shadow;
+  content: "";
+  box-shadow: none;
+}
+.task-list.more-top::before {
+  box-shadow: inset 0 0.5rem 0.25rem -0.25rem rgb(0 0 0 / 10%);
+}
+.task-list.more-bottom::after {
+  box-shadow: inset 0 -0.5rem 0.25rem -0.25rem rgb(0 0 0 / 10%);
 }
 </style>
