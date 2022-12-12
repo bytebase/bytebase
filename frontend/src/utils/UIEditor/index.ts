e7e70d7afbfa1868eef0de0c5b9218c378f49962
boojack <stevenlgtm@gmail.com>
2022-12-12 09:44:40 +0800

    chore: remove release guard for UI editor (#3788)
    Former-commit-id: c62be031b1f3e4f07192ed4bdefa4f7f24f0e4d2

--- frontend/src/utils/UIEditor/index.ts
+++ frontend/src/utils/UIEditor/index.ts
@@ -1,14 +1,10 @@
 import { Database, EngineType } from "@/types";
-import { isDev } from "..";
 
-// Only allow using UI Editor with MySQL in dev mode.
+// Only allow using UI Editor with MySQL.
 export const allowUsingUIEditor = (databaseList: Database[]): boolean => {
-  return (
-    isDev() &&
-    databaseList.every((db) => {
-      return db.instance.engine === "MYSQL";
-    })
-  );
+  return databaseList.every((db) => {
+    return db.instance.engine === "MYSQL";
+  });
 };
 
 export const getDataTypeSuggestionList = (engineType: EngineType = "MYSQL") => {
