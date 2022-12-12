641c1e2ebb5d0774afb67ae435f4399f7574a891
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2022-12-12 14:42:29 +0800

    chore: optimize passCheck() by reducing the number of queries (#3801)
    * chore: optimize passCheck() by reducing the number of queries
    
    * chore: update
    
    * Update server/runner/taskrun/scheduler.go
    
    Co-authored-by: p0ny <xz@bytebase.com>
    
    * chore: update
    
    Co-authored-by: p0ny <xz@bytebase.com>
    Former-commit-id: 820edb0443684918c9c4b3846d14ea5b3a3f6377
    Former-commit-id: 445e8c4b38de9be4ec172e29808fec318e9b632c

--- api/task_check_run.go
+++ api/task_check_run.go
@@ -181,8 +181,6 @@ type TaskCheckRunFind struct {
 
 	// Domain specific fields
 	StatusList *[]TaskCheckRunStatus
-	// If true, only returns the latest
-	Latest bool
 }
 
 func (find *TaskCheckRunFind) String() string {
