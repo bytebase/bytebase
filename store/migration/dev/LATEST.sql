a814ba14c5b4cbf0dcb7b880ae514762e5dd1c98
h3n4l <oysterdays@gmail.com>
2022-12-13 10:26:51 +0800

    feat: add options in data source schema

--- store/migration/dev/LATEST.sql
+++ store/migration/dev/LATEST.sql
@@ -544,7 +544,8 @@ CREATE TABLE data_source (
     ssl_ca TEXT NOT NULL DEFAULT '',
     -- host_override and port_override are used for read-replicas that have different connection addresses.
     host_override TEXT NOT NULL DEFAULT '',
-    port_override TEXT NOT NULL DEFAULT ''
+    port_override TEXT NOT NULL DEFAULT '',
+    options JSONB NOT NULL DEFAULT '{}'
 );
 
 CREATE INDEX idx_data_source_instance_id ON data_source(instance_id);
