58cbaed8224f537d96e4a898ab6c18b079461a68
boojack <stevenlgtm@gmail.com>
2022-12-13 15:58:43 +0800

    chore: update type definition for Schema Editor (#3842)
    Former-commit-id: da675a406d810ed9d8b484fc2318a6cae9482831
    Former-commit-id: 6b87db970e8fd7f60a7a3c4b956916eb2aa154ba

--- frontend/src/types/schemaEditor/index.ts
+++ frontend/src/types/schemaEditor/index.ts
@@ -1,40 +1,5 @@
-import { Database, DatabaseId, TableEngineType, TableType } from ".";
-
-type TableOrColumnStatus = "normal" | "created" | "dropped";
-
-export interface Column {
-  // Related fields
-  databaseId: DatabaseId;
-
-  // Domain specific fields
-  oldName: string;
-  newName: string;
-  type: string;
-  nullable: boolean;
-  comment: string;
-  default: string | null;
-
-  status: TableOrColumnStatus;
-}
-
-export interface Table {
-  // Related fields
-  databaseId: DatabaseId;
-
-  // Domain specific fields
-  oldName: string;
-  newName: string;
-  type: TableType;
-  engine: TableEngineType;
-  collation: string;
-  rowCount: number;
-  dataSize: number;
-  comment: string;
-  columnList: Column[];
-  originColumnList: Column[];
-
-  status: TableOrColumnStatus;
-}
+import { Database, DatabaseId } from "..";
+import { Table } from "./atomType";
 
 export enum SchemaEditorTabType {
   TabForDatabase = "database",
