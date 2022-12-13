d307fec9f2f1350bc1ebadfa7e4bfdcf45c9c0cf
h3n4l <oysterdays@gmail.com>
2022-12-13 17:40:55 +0800

    chore: merge main

--- store/migration/dev/20221212000000##add_datasource_options.sql
+++ store/migration/dev/20221212000000##add_datasource_options.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 ALTER TABLE data_source ADD COLUMN options JSONB NOT NULL DEFAULT '{}';
-=======
-ALTER TABLE data_source ADD COLUMN options JSONB NOT NULL DEFAULT '{}';
->>>>>>> a93608e0de941df48140d2f74958c431774dbc9e
