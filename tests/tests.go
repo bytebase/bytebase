106ba7ef3b8abf5c4ff3559d820b11907c79aa2c
ecmadao <wlec@outlook.com>
2022-12-13 16:18:58 +0800

    chore: add test for subscription (#3843)
    * chore: refresh the cache for subscription
    
    * chore: add test for subscription
    
    * chore: add comment
    
    Former-commit-id: 3f275fc8519adf5c993acde3aeb84bf925f39e0e
    Former-commit-id: 5c0ddc449402d1fd3470c22483b2f7ed3658b42b

--- tests/tests.go
+++ tests/tests.go
@@ -853,6 +853,29 @@ func (ctl *controller) removeLicense() error {
 	return nil
 }
 
+func (ctl *controller) getSubscription() (*enterpriseAPI.Subscription, error) {
+	body, err := ctl.get("/subscription", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	subscription := new(enterpriseAPI.Subscription)
+	if err = jsonapi.UnmarshalPayload(body, subscription); err != nil {
+		return nil, errors.Wrap(err, "fail to unmarshal get subscription response")
+	}
+	return subscription, nil
+}
+
+func (ctl *controller) trialPlan(trial *api.TrialPlanCreate) error {
+	buf := new(bytes.Buffer)
+	if err := jsonapi.MarshalPayload(buf, trial); err != nil {
+		return errors.Wrap(err, "failed to marshal subscription patch")
+	}
+
+	_, err := ctl.post("/subscription/trial", buf)
+	return err
+}
+
 func (ctl *controller) switchPlan(patch *enterpriseAPI.SubscriptionPatch) error {
 	buf := new(bytes.Buffer)
 	if err := jsonapi.MarshalPayload(buf, patch); err != nil {
