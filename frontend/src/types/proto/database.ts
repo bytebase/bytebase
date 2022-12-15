e5bd0377393a47473945ec0edbfe7ea142881d51
steven <stevenlgtm@gmail.com>
2022-12-15 14:09:54 +0800

    chore: update

--- frontend/src/types/proto/database.ts
+++ frontend/src/types/proto/database.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import * as Long from "long";
-import * as _m0 from "protobufjs/minimal";
+import Long from "long";
+import _m0 from "protobufjs/minimal";
 
 export const protobufPackage = "bytebase.store";
 
@@ -1051,8 +1051,6 @@ function longToNumber(long: Long): number {
   return long.toNumber();
 }
 
-// If you get a compile-error about 'Constructor<Long> and ... have no overlap',
-// add '--ts_proto_opt=esModuleInterop=true' as a flag when calling 'protoc'.
 if (_m0.util.Long !== Long) {
   _m0.util.Long = Long as any;
   _m0.configure();
--- proto/generate_ts.sh
+++ proto/generate_ts.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")/../"
 
-protoc --plugin=./frontend/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./frontend/src/types/proto -I=proto/store database.proto
+protoc --plugin=./frontend/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./frontend/src/types/proto --ts_proto_opt=esModuleInterop=true -I=proto/store database.proto
