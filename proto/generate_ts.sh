bd485fe8048307069b7547b7cb550213cfff7f55
boojack <stevenlgtm@gmail.com>
2022-12-15 18:03:40 +0800

    feat: add `useDBSchemaStore` module (#3896)
    * feat: add `useDBSchemaStore` module
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Co-authored-by: d-bytebase <d@bytebase.com>
    Former-commit-id: 7e9c1968fef967e46a2dc4dcec03ef374d7126b8
    Former-commit-id: 8c2944984bc23ece3d9de60f456a4a509502e688

--- proto/generate_ts.sh
+++ proto/generate_ts.sh
@@ -2,4 +2,9 @@
 
 cd "$(dirname "$0")/../"
 
-protoc --plugin=./frontend/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./frontend/src/types/proto --ts_proto_opt=esModuleInterop=true -I=proto/store database.proto
+protoc \
+  --plugin=./frontend/node_modules/.bin/protoc-gen-ts_proto \
+  --ts_proto_out=./frontend/src/types/proto \
+  --ts_proto_opt=esModuleInterop=true,env=browser,useOptionals=messages,initializeFieldsAsUndefined=true \
+  -I=proto/store \
+  database.proto
