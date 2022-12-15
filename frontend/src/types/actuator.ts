3c579c992cf9eac1cb36a59780f494ee49b9602f
ecmadao <wlec@outlook.com>
2022-12-15 16:36:24 +0800

    feat: support new version reminder (#3890)
    * feat: support new version reminder
    
    * fix: typo
    
    * fix: lint
    
    * chore: address comment
    
    * chore: address comment

--- frontend/src/types/actuator.ts
+++ frontend/src/types/actuator.ts
@@ -8,3 +8,20 @@ export type ServerInfo = {
   needAdminSetup: boolean;
   startedTs: number;
 };
+
+export type Release = {
+  draft: boolean;
+  prerelease: boolean;
+  name: string;
+  tag_name: string;
+  html_url: string;
+  body: string;
+  published_at: string;
+};
+
+export type ReleaseInfo = {
+  latest?: Release;
+  ignoreRemindModalTillNextRelease: boolean;
+  // The next check timestamp in milliseconds.
+  nextCheckTs: number;
+};
