51233f2a91c56d4e56438bc48b2c68cb10048860
ZhengX <56376387+Megrax@users.noreply.github.com>
2022-12-15 17:39:20 +0800

    feat: support finding activities with multiple levels (#3895)
    Former-commit-id: 82388d0eab40688734b81c7ae5d580ced1ea03cf

--- server/activity.go
+++ server/activity.go
@@ -68,9 +68,12 @@ func (s *Server) registerActivityRoutes(g *echo.Group) {
 		if typePrefixList := c.QueryParams()["typePrefix"]; typePrefixList != nil {
 			activityFind.TypePrefixList = typePrefixList
 		}
-		if levelStr := c.QueryParams().Get("level"); levelStr != "" {
-			activityLevel := api.ActivityLevel(levelStr)
-			activityFind.Level = &activityLevel
+		if levelList := c.QueryParams()["level"]; levelList != nil {
+			list := make([]api.ActivityLevel, len(levelList))
+			for i, level := range levelList {
+				list[i] = api.ActivityLevel(level)
+			}
+			activityFind.LevelList = list
 		}
 		if containerIDStr := c.QueryParams().Get("container"); containerIDStr != "" {
 			containerID, err := strconv.Atoi(containerIDStr)
