c286f1963780bec4fa3bdd30cd34007e70a6b657
d-bytebase <d@bytebase.com>
2022-12-15 12:10:03 +0800

    chore: optimize integration tests

---
+++ store/demo/prod/10061##instance_user.sql
@@ -0,0 +1,115 @@
+INSERT INTO
+    instance_user (
+        id,
+        creator_id,
+        updater_id,
+        instance_id,
+        name,
+        "grant"
+    )
+VALUES
+    (
+        6101,
+        1,
+        1,
+        6001,
+        '`root`@`%`',
+        'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER, CREATE TABLESPACE, CREATE ROLE, DROP ROLE ON *.* TO `root`@`localhost` WITH GRANT OPTION'
+    );
+
+INSERT INTO
+    instance_user (
+        id,
+        creator_id,
+        updater_id,
+        instance_id,
+        name,
+        "grant"
+    )
+VALUES
+    (
+        6102,
+        1,
+        1,
+        6002,
+        '`root`@`%`',
+        'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER, CREATE TABLESPACE, CREATE ROLE, DROP ROLE ON *.* TO `root`@`localhost` WITH GRANT OPTION'
+    );
+
+INSERT INTO
+    instance_user (
+        id,
+        creator_id,
+        updater_id,
+        instance_id,
+        name,
+        "grant"
+    )
+VALUES
+    (
+        6103,
+        1,
+        1,
+        6003,
+        '`root`@`%`',
+        'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER, CREATE TABLESPACE, CREATE ROLE, DROP ROLE ON *.* TO `root`@`localhost` WITH GRANT OPTION'
+    );
+
+INSERT INTO
+    instance_user (
+        id,
+        creator_id,
+        updater_id,
+        instance_id,
+        name,
+        "grant"
+    )
+VALUES
+    (
+        6104,
+        1,
+        1,
+        6004,
+        '`root`@`%`',
+        'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER, CREATE TABLESPACE, CREATE ROLE, DROP ROLE ON *.* TO `root`@`localhost` WITH GRANT OPTION'
+    );
+
+INSERT INTO
+    instance_user (
+        id,
+        creator_id,
+        updater_id,
+        instance_id,
+        name,
+        "grant"
+    )
+VALUES
+    (
+        6105,
+        1,
+        1,
+        6005,
+        '`root`@`%`',
+        'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER, CREATE TABLESPACE, CREATE ROLE, DROP ROLE ON *.* TO `root`@`localhost` WITH GRANT OPTION'
+    );
+
+INSERT INTO
+    instance_user (
+        id,
+        creator_id,
+        updater_id,
+        instance_id,
+        name,
+        "grant"
+    )
+VALUES
+    (
+        6106,
+        1,
+        1,
+        6006,
+        '`root`@`%`',
+        'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, SHUTDOWN, PROCESS, FILE, REFERENCES, INDEX, ALTER, SHOW DATABASES, SUPER, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE, REPLICATION SLAVE, REPLICATION CLIENT, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, CREATE USER, EVENT, TRIGGER, CREATE TABLESPACE, CREATE ROLE, DROP ROLE ON *.* TO `root`@`localhost` WITH GRANT OPTION'
+    );
+
+ALTER SEQUENCE instance_user_id_seq RESTART WITH 6107;
