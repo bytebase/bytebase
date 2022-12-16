9de2a26ec2c6fb7a250187a72d993a964af447c6
ZhengX <56376387+Megrax@users.noreply.github.com>
2022-12-16 11:36:24 +0800

    style: not wrap in audit type badge (#3904)
    Former-commit-id: 293a539f5b2411956d01610d1d513e078c5d0c92
    Former-commit-id: efa5e140164a287b6e7ddd5fb234510afd251b71

--- frontend/src/components/AuditLog/AuditLogTable.vue
+++ frontend/src/components/AuditLog/AuditLogTable.vue
@@ -17,7 +17,7 @@
       </BBTableCell>
       <BBTableCell class="table-cell w-28">
         <span
-          class="inline-flex items-center px-2 py-1 rounded-lg text-xs font-semibold bg-green-100 text-green-800"
+          class="inline-flex items-center px-2 py-1 rounded-lg text-xs font-semibold whitespace-nowrap bg-green-100 text-green-800"
         >
           {{
             t(AuditActivityTypeI18nNameMap[auditLog.type as AuditActivityType])
