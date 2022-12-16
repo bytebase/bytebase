5ca00979760c34c8f261ab628dc3b392185439ec
boojack <stevenlgtm@gmail.com>
2022-12-16 10:05:46 +0800

    feat: display database's table/column/index list with db schema (#3902)
    * feat: get database table/column/index from db schema
    
    * chore: update
    
    Former-commit-id: 965829e73d6825991ce8df0ebc60a4282cbfb208

--- frontend/src/store/modules/dbSchema.ts
+++ frontend/src/store/modules/dbSchema.ts
@@ -1,26 +1,51 @@
 import axios from "axios";
 import { defineStore } from "pinia";
 import { DatabaseId, DBSchemaState } from "@/types";
-import { DatabaseMetadata } from "@/types/proto/database";
+import { DatabaseMetadata, TableMetadata } from "@/types/proto/database";
 
 export const useDBSchemaStore = defineStore("dbSchema", {
   state: (): DBSchemaState => ({
     databaseMetadataById: new Map(),
   }),
   actions: {
-    async getOrFetchDatabaseMetadata(
+    async getOrFetchDatabaseMetadataById(
       databaseId: DatabaseId
     ): Promise<DatabaseMetadata> {
       if (this.databaseMetadataById.has(databaseId)) {
         return this.databaseMetadataById.get(databaseId) as DatabaseMetadata;
       }
 
-      const res = await axios.get<DatabaseMetadata>(
+      const res = await axios.get(
         `/api/database/${databaseId}/schema?metadata=true`
       );
       const databaseMetadata = DatabaseMetadata.fromJSON(res.data);
       this.databaseMetadataById.set(databaseId, databaseMetadata);
       return databaseMetadata;
+    },
+    getTableListByDatabaseId(databaseId: DatabaseId): TableMetadata[] {
+      const databaseMetadata = this.databaseMetadataById.get(databaseId);
+      if (!databaseMetadata) {
+        return [];
+      }
+
+      const tableList: TableMetadata[] = [];
+      // TODO(steven): get table list with schema name for PG.
+      for (const schema of databaseMetadata.schemas) {
+        tableList.push(...schema.tables);
+      }
+      return tableList;
+    },
+    getTableByDatabaseIdAndTableName(
+      databaseId: DatabaseId,
+      tableName: string
+    ): TableMetadata | undefined {
+      const databaseMetadata = this.databaseMetadataById.get(databaseId);
+      if (!databaseMetadata) {
+        return undefined;
+      }
+
+      const tableList = this.getTableListByDatabaseId(databaseId);
+      return tableList.find((table) => table.name === tableName);
     },
   },
 });
