709ab6569c78d183ace94f1b7e805fdd383a9f72
d-bytebase <d@bytebase.com>
2022-12-18 21:10:26 +0800

    chore: update

--- server/runner/taskrun/schema_update_executor.go
+++ server/runner/taskrun/schema_update_executor.go
@@ -49,9 +49,12 @@ func (exec *SchemaUpdateExecutor) RunOnce(ctx context.Context, task *api.Task) (
 
 	statement := payload.Statement
 	if payload.SheetID > 0 {
-		sheet, err := exec.store.GetSheet(ctx, &api.SheetFind{}, api.SystemBotID)
+		sheet, err := exec.store.GetSheet(ctx, &api.SheetFind{ID: &payload.SheetID}, api.SystemBotID)
 		if err != nil {
 			return true, nil, err
+		}
+		if sheet == nil {
+			return true, nil, errors.Errorf("sheet ID %v not found", payload.SheetID)
 		}
 		statement = sheet.Statement
 	}
