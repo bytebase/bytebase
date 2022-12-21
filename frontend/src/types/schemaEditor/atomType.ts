3f5ab2e70844b53c781fd0ec6521f62693570822
boojack <stevenlgtm@gmail.com>
2022-12-21 09:10:15 +0800

    refactor: update atom types definition for schema editor (#3942)
    * refactor: atom types definition for schema editor
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: ed1acc185fffdd5135d1859a8d616e3750cc0727
    Former-commit-id: 6b34eb10249adea1761b326c29d25bd8c5e1af47

--- frontend/src/types/schemaEditor/atomType.ts
+++ frontend/src/types/schemaEditor/atomType.ts
@@ -1,3 +1,9 @@
+import {
+  ColumnMetadata,
+  SchemaMetadata,
+  TableMetadata,
+} from "../proto/database";
+
 type TableOrColumnStatus = "normal" | "created" | "dropped";
 
 export interface Column {
@@ -7,7 +13,6 @@ export interface Column {
   nullable: boolean;
   comment: string;
   default?: string;
-
   status: TableOrColumnStatus;
 }
 
@@ -20,7 +25,100 @@ export interface Table {
   dataSize: number;
   comment: string;
   columnList: Column[];
-  originColumnList: Column[];
-
   status: TableOrColumnStatus;
 }
+
+export interface PrimaryKey {
+  schema: string;
+  table: string;
+  columnList: string[];
+}
+
+export interface ForeignKey {
+  schema: string;
+  table: string;
+  columnList: string[];
+  referencedSchema: string;
+  referencedTable: string;
+  referencedColumns: string[];
+}
+
+export interface Schema {
+  // It should be an empty string for MySQL/TiDB.
+  name: string;
+  tableList: Table[];
+  primaryKeyList: PrimaryKey[];
+  foreignKeyList: ForeignKey[];
+}
+
+export const convertColumnMetadataToColumn = (
+  columnMetadata: ColumnMetadata
+): Column => {
+  return {
+    oldName: columnMetadata.name,
+    newName: columnMetadata.name,
+    type: columnMetadata.type,
+    nullable: columnMetadata.nullable,
+    comment: columnMetadata.comment,
+    default: columnMetadata.default,
+    status: "normal",
+  };
+};
+
+export const convertTableMetadataToTable = (
+  tableMetadata: TableMetadata
+): Table => {
+  return {
+    oldName: tableMetadata.name,
+    newName: tableMetadata.name,
+    engine: tableMetadata.engine,
+    collation: tableMetadata.collation,
+    rowCount: tableMetadata.rowCount,
+    dataSize: tableMetadata.dataSize,
+    comment: tableMetadata.comment,
+    columnList: tableMetadata.columns.map((column) =>
+      convertColumnMetadataToColumn(column)
+    ),
+    status: "normal",
+  };
+};
+
+export const convertSchemaMetadataToSchema = (
+  schemaMetadata: SchemaMetadata
+): Schema => {
+  const tableList: Table[] = [];
+  const primaryKeyList: PrimaryKey[] = [];
+  const foreignKeyList: ForeignKey[] = [];
+
+  for (const tableMetadata of schemaMetadata.tables) {
+    tableList.push(convertTableMetadataToTable(tableMetadata));
+
+    for (const indexMetadata of tableMetadata.indexes) {
+      if (indexMetadata.primary === true) {
+        primaryKeyList.push({
+          schema: schemaMetadata.name,
+          table: tableMetadata.name,
+          columnList: indexMetadata.expressions,
+        });
+      }
+    }
+
+    for (const foreignKeyMetadata of tableMetadata.foreignKeys) {
+      foreignKeyList.push({
+        schema: schemaMetadata.name,
+        table: tableMetadata.name,
+        columnList: foreignKeyMetadata.columns,
+        referencedSchema: foreignKeyMetadata.referencedSchema,
+        referencedTable: foreignKeyMetadata.referencedTable,
+        referencedColumns: foreignKeyMetadata.referencedColumns,
+      });
+    }
+  }
+
+  return {
+    name: schemaMetadata.name,
+    tableList: tableList,
+    primaryKeyList: primaryKeyList,
+    foreignKeyList: foreignKeyList,
+  };
+};
