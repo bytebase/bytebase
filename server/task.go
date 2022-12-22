ffca4fca18cf8d66ce92b1dcd7ffe48f71235a65
p0ny <p0uy@outlook.com>
2022-12-22 10:22:33 +0000

    chore: update

--- server/task.go
+++ server/task.go
@@ -149,9 +149,6 @@ func (s *Server) registerTaskRoutes(g *echo.Group) {
 		}
 
 		if taskStatusPatch.Status == api.TaskDone {
-			if task.Status == api.TaskRunning {
-				return echo.NewHTTPError(http.StatusBadRequest, "Cannot mark the task as done because it's running")
-			}
 			// the user marks the task as DONE, set Skipped to true and SkippedReason to Comment.
 			skipped := true
 			taskStatusPatch.Skipped = &skipped
