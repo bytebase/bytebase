73a6e7651ffcbd36feba345c6a105a44d063eb5f
h3n4l <oysterdays@gmail.com>
2022-12-23 15:18:35 +0800

    Update plugin/db/util/driverutil.go
    Co-authored-by: Tianzhou (天舟) <t@bytebase.com>

--- plugin/db/util/driverutil.go
+++ plugin/db/util/driverutil.go
@@ -153,7 +153,7 @@ type MigrationExecutor interface {
 // Returns the created migration history id and the updated schema on success.
 func ExecuteMigration(ctx context.Context, executor MigrationExecutor, m *db.MigrationInfo, statement string, databaseName string) (migrationHistoryID int64, updatedSchema string, resErr error) {
 	var prevSchemaBuf bytes.Buffer
-	// Don't record schema if the database hasn't exist yet or the schemaless instance.
+	// Don't record schema if the database hasn't existed yet or is schemaless (e.g. Mongo).
 	if !m.CreateDatabase && executor.GetType() != db.MongoDB {
 		// For baseline migration, we also record the live schema to detect the schema drift.
 		// See https://bytebase.com/blog/what-is-database-schema-drift
