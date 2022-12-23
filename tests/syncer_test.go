28c9854aa426e20631586f265bfee5750850549e
rebelice <yangrebelice@gmail.com>
2022-12-23 12:05:17 +0800

    feat: support to get foreign key metadata (#3985)
    Former-commit-id: d93c6b50587cbbcbc091f89b49e32b8e053dca74
    Former-commit-id: 3d438756d93d24187ad36907596085a119ce3cb1

--- tests/syncer_test.go
+++ tests/syncer_test.go
@@ -15,8 +15,244 @@ import (
 	"github.com/bytebase/bytebase/plugin/db"
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 	"github.com/bytebase/bytebase/resources/mysql"
+	"github.com/bytebase/bytebase/resources/postgres"
 	"github.com/bytebase/bytebase/tests/fake"
 )
+
+func TestSyncerForPostgreSQL(t *testing.T) {
+	const (
+		databaseName = "test_sync_postgresql_schema_db"
+		createSchema = `
+		CREATE SCHEMA schema1;
+		CREATE TABLE schema1.trd (
+			"A" int DEFAULT NULL,
+			"B" int DEFAULT NULL,
+			c int DEFAULT NULL,
+			UNIQUE ("A","B",c)
+		  );
+		  CREATE TABLE "TFK" (
+			a int DEFAULT NULL,
+			b int DEFAULT NULL,
+			c int DEFAULT NULL,
+			CONSTRAINT tfk_ibfk_1 FOREIGN KEY (a, b, c) REFERENCES schema1.trd ("A", "B", c)
+		  );
+		`
+		expectedSchema = `
+		{
+			"name":"test_sync_postgresql_schema_db",
+			"schemas":[
+			   {
+				  "name":"public",
+				  "tables":[
+					 {
+						"name":"TFK",
+						"columns":[
+						   {
+							  "name":"a",
+							  "position":1,
+							  "default":"",
+							  "nullable":true,
+							  "type":"integer"
+						   },
+						   {
+							  "name":"b",
+							  "position":2,
+							  "default":"",
+							  "nullable":true,
+							  "type":"integer"
+						   },
+						   {
+							  "name":"c",
+							  "position":3,
+							  "default":"",
+							  "nullable":true,
+							  "type":"integer"
+						   }
+						],
+						"foreignKeys":[
+						   {
+							  "name":"tfk_ibfk_1",
+							  "columns":[
+								 "a",
+								 "b",
+								 "c"
+							  ],
+							  "referencedSchema":"schema1",
+							  "referencedTable":"trd",
+							  "referencedColumns":[
+								 "A",
+								 "B",
+								 "c"
+							  ],
+							  "onDelete":"NO ACTION",
+							  "onUpdate":"NO ACTION",
+							  "matchType":"SIMPLE"
+						   }
+						]
+					 }
+				  ]
+			   },
+			   {
+				  "name":"schema1",
+				  "tables":[
+					 {
+						"name":"trd",
+						"columns":[
+						   {
+							  "name":"A",
+							  "position":1,
+							  "default":"",
+							  "nullable":true,
+							  "type":"integer"
+						   },
+						   {
+							  "name":"B",
+							  "position":2,
+							  "default":"",
+							  "nullable":true,
+							  "type":"integer"
+						   },
+						   {
+							  "name":"c",
+							  "position":3,
+							  "default":"",
+							  "nullable":true,
+							  "type":"integer"
+						   }
+						],
+						"indexes":[
+						   {
+							  "name":"trd_A_B_c_key",
+							  "expressions":[
+								 "\"A\"",
+								 "\"B\"",
+								 "c"
+							  ],
+							  "type":"btree",
+							  "unique":true
+						   }
+						],
+						"indexSize":"8192"
+					 }
+				  ]
+			   }
+			],
+			"characterSet":"UTF8",
+			"collation":"en_US.UTF-8"
+		 }
+		`
+	)
+
+	t.Parallel()
+	a := require.New(t)
+	ctx := context.Background()
+	ctl := &controller{}
+	dataDir := t.TempDir()
+	err := ctl.StartServerWithExternalPg(ctx, &config{
+		dataDir:            dataDir,
+		vcsProviderCreator: fake.NewGitLab,
+	})
+	a.NoError(err)
+	defer ctl.Close(ctx)
+
+	// Create a PostgreSQL instance.
+	pgPort := getTestPort()
+	stopInstance := postgres.SetupTestInstance(t, pgPort, resourceDirOverride)
+	defer stopInstance()
+
+	pgDB, err := sql.Open("pgx", fmt.Sprintf("host=/tmp port=%d user=root database=postgres", pgPort))
+	a.NoError(err)
+	defer pgDB.Close()
+
+	err = pgDB.Ping()
+	a.NoError(err)
+
+	_, err = pgDB.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %v", databaseName))
+	a.NoError(err)
+
+	_, err = pgDB.Exec("CREATE USER bytebase WITH ENCRYPTED PASSWORD 'bytebase'")
+	a.NoError(err)
+
+	_, err = pgDB.Exec("ALTER USER bytebase WITH SUPERUSER")
+	a.NoError(err)
+
+	// Create a project.
+	project, err := ctl.createProject(api.ProjectCreate{
+		Name: "Test Syncer For PostgreSQL",
+		Key:  "TestSyncerForPostgreSQL",
+	})
+	a.NoError(err)
+
+	environments, err := ctl.getEnvironments()
+	a.NoError(err)
+	prodEnvironment, err := findEnvironment(environments, "Prod")
+	a.NoError(err)
+
+	instance, err := ctl.addInstance(api.InstanceCreate{
+		EnvironmentID: prodEnvironment.ID,
+		Name:          "pgInstance",
+		Engine:        db.Postgres,
+		Host:          "/tmp",
+		Port:          strconv.Itoa(pgPort),
+		Username:      "bytebase",
+		Password:      "bytebase",
+	})
+	a.NoError(err)
+
+	databases, err := ctl.getDatabases(api.DatabaseFind{
+		ProjectID: &project.ID,
+	})
+	a.NoError(err)
+	a.Nil(databases)
+
+	err = ctl.createDatabase(project, instance, databaseName, "bytebase", nil)
+	a.NoError(err)
+
+	databases, err = ctl.getDatabases(api.DatabaseFind{
+		ProjectID: &project.ID,
+	})
+	a.NoError(err)
+	a.Equal(1, len(databases))
+
+	database := databases[0]
+	a.Equal(instance.ID, database.Instance.ID)
+	// Create an issue that updates database schema.
+	createContext, err := json.Marshal(&api.MigrationContext{
+		DetailList: []*api.MigrationDetail{
+			{
+				MigrationType: db.Migrate,
+				DatabaseID:    database.ID,
+				Statement:     createSchema,
+			},
+		},
+	})
+	a.NoError(err)
+	issue, err := ctl.createIssue(api.IssueCreate{
+		ProjectID:   project.ID,
+		Name:        fmt.Sprintf("Create sequence for database %q", databaseName),
+		Type:        api.IssueDatabaseSchemaUpdate,
+		Description: fmt.Sprintf("Create sequence of database %q.", databaseName),
+		// Assign to self.
+		AssigneeID:    project.Creator.ID,
+		CreateContext: string(createContext),
+	})
+	a.NoError(err)
+	status, err := ctl.waitIssuePipeline(issue.ID)
+	a.NoError(err)
+	a.Equal(api.TaskDone, status)
+
+	metadata, err := ctl.getLatestSchemaMetadata(database.ID)
+	a.NoError(err)
+
+	var latestSchemaMetadata storepb.DatabaseMetadata
+	err = protojson.Unmarshal([]byte(metadata), &latestSchemaMetadata)
+	a.NoError(err)
+
+	var expectedSchemaMetadata storepb.DatabaseMetadata
+	err = protojson.Unmarshal([]byte(expectedSchema), &expectedSchemaMetadata)
+	a.NoError(err)
+	a.Equal(&expectedSchemaMetadata, &latestSchemaMetadata)
+}
 
 func TestSyncerForMySQL(t *testing.T) {
 	const (
