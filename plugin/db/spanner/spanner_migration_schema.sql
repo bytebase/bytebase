4a8b7f639835c538f09ad2de2992c0730d54c643
p0ny <xz@bytebase.com>
2022-12-30 12:45:20 +0800

    feat(backend): spanner driver (#4098)
    * feat(backend): spanner driver
    
    * Delete spanner_migration_schema_commeted.sql
    
    * Update plugin/db/spanner/spanner_migration_schema.sql
    
    * fix: register spanner driver
    
    * chore: update
    
    * fix: add spanner in instance_engine_check constraint
    
    * fix: lint

---
+++ plugin/db/spanner/spanner_migration_schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE migration_history (
+    id STRING(MAX) NOT NULL,
+    created_by STRING(MAX) NOT NULL,
+    created_ts INT64 NOT NULL,
+    updated_by STRING(MAX) NOT NULL,
+    updated_ts INT64 NOT NULL,
+    release_version STRING(MAX) NOT NULL,
+    namespace STRING(MAX) NOT NULL,
+    sequence INT64 NOT NULL,
+    CONSTRAINT sequence_is_non_negative CHECK (sequence >= 0),
+    source STRING(MAX) NOT NULL,
+    type STRING(MAX) NOT NULL,
+    status STRING(MAX) NOT NULL,
+    version STRING(MAX) NOT NULL,
+    description STRING(MAX) NOT NULL,
+    statement STRING(MAX) NOT NULL,
+    schema STRING(MAX) NOT NULL,
+    schema_prev STRING(MAX) NOT NULL,
+    execution_duration_ns INT64 NOT NULL,
+    issue_id STRING(MAX) NOT NULL,
+    payload STRING(MAX) NOT NULL
+) PRIMARY KEY(id);
+
+CREATE UNIQUE INDEX bytebase_idx_unique_migration_history_namespace_sequence ON migration_history (namespace, sequence);
+
+CREATE UNIQUE INDEX bytebase_idx_unique_migration_history_namespace_version ON migration_history (namespace, version);
+
+CREATE INDEX bytebase_idx_migration_history_namespace_source_type ON migration_history(namespace, source, type);
+
+CREATE INDEX bytebase_idx_migration_history_namespace_created ON migration_history(namespace, created_ts);
