73ecd6fdd6dd22f766f155437126931ba4491d03
h3n4l <oysterdays@gmail.com>
2022-12-30 17:41:53 +0800

    chore: using admin as default authentication database (#4117)
    Former-commit-id: 5f5308138b96a46c81378708b8e04d765be929ad

--- plugin/db/mongodb/mongodb.go
+++ plugin/db/mongodb/mongodb.go
@@ -184,12 +184,18 @@ func getMongoDBConnectionURI(connConfig db.ConnectionConfig) string {
 	if connConfig.Database != "" {
 		connectionURI = fmt.Sprintf("%s/%s", connectionURI, connConfig.Database)
 	}
-	if connConfig.AuthenticationDatabase != "" {
-		if connConfig.Database == "" {
-			connectionURI = fmt.Sprintf("%s/", connectionURI)
-		}
-		connectionURI = fmt.Sprintf("%s?authSource=%s", connectionURI, connConfig.AuthenticationDatabase)
+	// We use admin as the default authentication database.
+	// https://www.mongodb.com/docs/manual/reference/connection-string/#mongodb-urioption-urioption.authSource
+	authenticationDatabase := connConfig.AuthenticationDatabase
+	if authenticationDatabase == "" {
+		authenticationDatabase = "admin"
 	}
+
+	if connConfig.Database == "" {
+		connectionURI = fmt.Sprintf("%s/", connectionURI)
+	}
+	connectionURI = fmt.Sprintf("%s?authSource=%s", connectionURI, authenticationDatabase)
+
 	return connectionURI
 }
 
