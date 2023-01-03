8a4b80ab8ebd89661288bec2797eefe972f3d7d2
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-01-03 21:06:31 +0800

    feat(schema-diagram): preview schema changes in schema diagram from schema editor (#4139)
    * feat: types
    
    * feat: decorator styles for created/dropped tables/columns
    
    * feat: use metadata for diagram
    
    * chore: style
    
    * chore: stronger types
    
    * chore: stronger types
    
    * chore: naming
    
    * chore: comments
    
    * chore: simplify codes
    
    * chore: comments
    
    * chore: formatting
    
    * chore: comments
    
    Former-commit-id: 70fb02bf52b0a9b5b5c7490113d24df7bea6aaf9

--- frontend/src/components/SchemaDiagram/types/context.ts
+++ frontend/src/components/SchemaDiagram/types/context.ts
@@ -1,8 +1,9 @@
 import { Ref } from "vue";
 import type Emittery from "emittery";
-import { TableMetadata } from "@/types/proto/store/database";
 
+import { ColumnMetadata, TableMetadata } from "@/types/proto/store/database";
 import { Position, Rect } from "./geometry";
+import { EditStatus } from "./edit";
 
 // This is an abstract Schema Diagram context including states, methods and
 // events. This should be implemented in the root component of SchemaDiagram
@@ -22,6 +23,8 @@ export type SchemaDiagramContext = {
   render: () => void;
   // auto-layout all components
   layout: () => void;
+  tableStatus: (table: TableMetadata) => EditStatus;
+  columnStatus: (column: ColumnMetadata) => EditStatus;
 
   // Events
   events: Emittery<{
