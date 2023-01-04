1cdd77e0a08493264703f73f6020ba76916bf4ca
steven <stevenlgtm@gmail.com>
2023-01-04 16:25:49 +0800

    chore: update

--- plugin/parser/edit/pg/deparse.go
+++ plugin/parser/edit/pg/deparse.go
@@ -84,8 +84,8 @@ func transformCreateSchemaContext(ctx *DeparseContext, createSchemaContext *api.
 
 func transformRenameSchemaContext(ctx *DeparseContext, renameSchemaContext *api.RenameSchemaContext) error {
 	// TODO(steven): support rename schema with our pg parser.
-	commemtStmt := fmt.Sprintf(`ALTER SCHEMA "%s" RENAME TO "%s";`, renameSchemaContext.OldName, renameSchemaContext.NewName)
-	ctx.StmtList = append(ctx.StmtList, commemtStmt)
+	alterSchemaStmt := fmt.Sprintf(`ALTER SCHEMA "%s" RENAME TO "%s";`, renameSchemaContext.OldName, renameSchemaContext.NewName)
+	ctx.StmtList = append(ctx.StmtList, alterSchemaStmt)
 	return nil
 }
 
