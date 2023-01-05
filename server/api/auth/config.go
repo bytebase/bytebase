cd7235897d7057cbe73e0be25ed7716e12330dda
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-01-05 14:22:56 +0800

    chore: allow access unauthenticated methods even with expired token (#4174)

--- server/api/auth/config.go
+++ server/api/auth/config.go
@@ -8,5 +8,6 @@ var authenticationAllowlistMethods = map[string]bool{
 
 // IsAuthenticationAllowed returns whether the method is exempted from authentication.
 func IsAuthenticationAllowed(fullMethodName string) bool {
+	// TODO(d): skips actuator, GET /subscription request, OpenAPI SQL endpoint.
 	return authenticationAllowlistMethods[fullMethodName]
 }
