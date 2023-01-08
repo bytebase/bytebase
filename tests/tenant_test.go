2493499e51f33b93f1eb77548ae7f7e820b1a509
d-bytebase <d@bytebase.com>
2023-01-08 08:44:33 +0800

    chore: update

--- tests/tenant_test.go
+++ tests/tenant_test.go
@@ -1160,7 +1160,7 @@ func TestTenantVCSDatabaseNameTemplate_Empty(t *testing.T) {
 										{
 											Key:      api.EnvironmentLabelKey,
 											Operator: api.InOperatorType,
-											Values:   []string{"Test"},
+											Values:   []string{"test"},
 										},
 									},
 								},
