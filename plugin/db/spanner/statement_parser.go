325a0af11355f5a94b9b3301db70074f3258cfef
p0ny <p0uy@outlook.com>
2023-01-09 13:38:07 +0800

    feat(spanner): execute migration

---
+++ plugin/db/spanner/statement_parser.go
@@ -0,0 +1,138 @@
+package spanner
+
+import (
+	"strings"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var ddlStatements = map[string]bool{"CREATE": true, "DROP": true, "ALTER": true}
+
+// RemoveCommentsAndTrim removes any comments in the query string and trims any
+// spaces at the beginning and end of the query. This makes checking what type
+// of query a string is a lot easier, as only the first word(s) need to be
+// checked after this has been removed.
+func removeCommentsAndTrim(sql string) (string, error) {
+	const singleQuote = '\''
+	const doubleQuote = '"'
+	const backtick = '`'
+	const hyphen = '-'
+	const dash = '#'
+	const slash = '/'
+	const asterisk = '*'
+	isInQuoted := false
+	isInSingleLineComment := false
+	isInMultiLineComment := false
+	var startQuote rune
+	lastCharWasEscapeChar := false
+	isTripleQuoted := false
+	res := strings.Builder{}
+	res.Grow(len(sql))
+	index := 0
+	runes := []rune(sql)
+	for index < len(runes) {
+		c := runes[index]
+		if isInQuoted {
+			if (c == '\n' || c == '\r') && !isTripleQuoted {
+				return "", spanner.ToSpannerError(status.Errorf(codes.InvalidArgument, "statement contains an unclosed literal: %s", sql))
+			} else if c == startQuote {
+				if lastCharWasEscapeChar {
+					lastCharWasEscapeChar = false
+				} else if isTripleQuoted {
+					if len(runes) > index+2 && runes[index+1] == startQuote && runes[index+2] == startQuote {
+						isInQuoted = false
+						startQuote = 0
+						isTripleQuoted = false
+						res.WriteRune(c)
+						res.WriteRune(c)
+						index += 2
+					}
+				} else {
+					isInQuoted = false
+					startQuote = 0
+				}
+			} else if c == '\\' {
+				lastCharWasEscapeChar = true
+			} else {
+				lastCharWasEscapeChar = false
+			}
+			res.WriteRune(c)
+		} else {
+			// We are not in a quoted string.
+			if isInSingleLineComment {
+				if c == '\n' {
+					isInSingleLineComment = false
+					// Include the line feed in the result.
+					res.WriteRune(c)
+				}
+			} else if isInMultiLineComment {
+				if len(runes) > index+1 && c == asterisk && runes[index+1] == slash {
+					isInMultiLineComment = false
+					index++
+				}
+			} else {
+				if c == dash || (len(runes) > index+1 && c == hyphen && runes[index+1] == hyphen) {
+					// This is a single line comment.
+					isInSingleLineComment = true
+				} else if len(runes) > index+1 && c == slash && runes[index+1] == asterisk {
+					isInMultiLineComment = true
+					index++
+				} else {
+					if c == singleQuote || c == doubleQuote || c == backtick {
+						isInQuoted = true
+						startQuote = c
+						// Check whether it is a triple-quote.
+						if len(runes) > index+2 && runes[index+1] == startQuote && runes[index+2] == startQuote {
+							isTripleQuoted = true
+							res.WriteRune(c)
+							res.WriteRune(c)
+							index += 2
+						}
+					}
+					res.WriteRune(c)
+				}
+			}
+		}
+		index++
+	}
+	if isInQuoted {
+		return "", spanner.ToSpannerError(status.Errorf(codes.InvalidArgument, "statement contains an unclosed literal: %s", sql))
+	}
+	trimmed := strings.TrimSpace(res.String())
+	if len(trimmed) > 0 && trimmed[len(trimmed)-1] == ';' {
+		return trimmed[:len(trimmed)-1], nil
+	}
+	return trimmed, nil
+}
+
+func splitStatement(statement string) []string {
+	var res []string
+	for _, s := range strings.Split(statement, ";") {
+		trimmed := strings.TrimSpace(s)
+		if trimmed == "" {
+			continue
+		}
+		res = append(res, trimmed)
+	}
+	return res
+}
+
+func sanitizeSQL(sql string) ([]string, error) {
+	query, err := removeCommentsAndTrim(sql)
+	if err != nil {
+		return nil, err
+	}
+	return splitStatement(query), nil
+}
+
+// isDDL returns true if the given sql string is a DDL statement.
+func isDDL(query string) bool {
+	for ddl := range ddlStatements {
+		if strings.EqualFold(query[:len(ddl)], ddl) {
+			return true
+		}
+	}
+	return false
+}
---
+++ plugin/db/spanner/statement_parser_test.go
@@ -0,0 +1,384 @@
+package spanner
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRemoveCommentsAndTrim(t *testing.T) {
+	tests := []struct {
+		input   string
+		want    string
+		wantErr bool
+	}{
+		{
+			input: ``,
+			want:  ``,
+		},
+		{
+			input: `SELECT 1;`,
+			want:  `SELECT 1`,
+		},
+		{
+			input: `-- This is a single line comment
+SELECT 1;`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `# This is a single line comment
+SELECT 1;`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `/* This is a multi line comment on one line */
+SELECT 1;`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `/* This
+is
+a
+multiline
+comment
+*/
+SELECT 1;`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `/* This
+* is
+* a
+* multiline
+* comment
+*/
+SELECT 1;`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `/** This is a javadoc style comment on one line */
+SELECT 1;`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `/** This
+is
+a
+javadoc
+style
+comment
+on
+multiple
+lines
+*/
+SELECT 1;`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `/** This
+* is
+* a
+* javadoc
+* style
+* comment
+* on
+* multiple
+* lines
+*/
+SELECT 1;`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `-- First comment
+SELECT--second comment
+1`,
+			want: `SELECT
+1`,
+		},
+		{
+			input: `# First comment
+SELECT#second comment
+1`,
+			want: `SELECT
+1`,
+		},
+		{
+			input: `-- First comment
+SELECT--second comment
+1--third comment`,
+			want: `SELECT
+1`,
+		},
+		{
+			input: `# First comment
+SELECT#second comment
+1#third comment`,
+			want: `SELECT
+1`,
+		},
+		{
+			input: `/* First comment */
+SELECT/* second comment */
+1`,
+			want: `SELECT
+1`,
+		},
+		{
+			input: `/* First comment */
+SELECT/* second comment */
+1/* third comment */`,
+			want: `SELECT
+1`,
+		},
+		{
+			input: `SELECT "TEST -- This is not a comment"`,
+			want:  `SELECT "TEST -- This is not a comment"`,
+		},
+		{
+			input: `-- This is a comment
+SELECT "TEST -- This is not a comment"`,
+			want: `SELECT "TEST -- This is not a comment"`,
+		},
+		{
+			input: `-- This is a comment
+SELECT "TEST -- This is not a comment" -- This is a comment`,
+			want: `SELECT "TEST -- This is not a comment"`,
+		},
+		{
+			input: `SELECT "TEST # This is not a comment"`,
+			want:  `SELECT "TEST # This is not a comment"`,
+		},
+		{
+			input: `# This is a comment
+SELECT "TEST # This is not a comment"`,
+			want: `SELECT "TEST # This is not a comment"`,
+		},
+		{
+			input: `# This is a comment
+SELECT "TEST # This is not a comment" # This is a comment`,
+			want: `SELECT "TEST # This is not a comment"`,
+		},
+		{
+			input: `SELECT "TEST /* This is not a comment */"`,
+			want:  `SELECT "TEST /* This is not a comment */"`,
+		},
+		{
+			input: `/* This is a comment */
+SELECT "TEST /* This is not a comment */"`,
+			want: `SELECT "TEST /* This is not a comment */"`,
+		},
+		{
+			input: `/* This is a comment */
+SELECT "TEST /* This is not a comment */" /* This is a comment */`,
+			want: `SELECT "TEST /* This is not a comment */"`,
+		},
+		{
+			input: `SELECT 'TEST -- This is not a comment'`,
+			want:  `SELECT 'TEST -- This is not a comment'`,
+		},
+		{
+			input: `-- This is a comment
+SELECT 'TEST -- This is not a comment'`,
+			want: `SELECT 'TEST -- This is not a comment'`,
+		},
+		{
+			input: `-- This is a comment
+SELECT 'TEST -- This is not a comment' -- This is a comment`,
+			want: `SELECT 'TEST -- This is not a comment'`,
+		},
+		{
+			input: `SELECT 'TEST # This is not a comment'`,
+			want:  `SELECT 'TEST # This is not a comment'`,
+		},
+		{
+			input: `# This is a comment
+SELECT 'TEST # This is not a comment'`,
+			want: `SELECT 'TEST # This is not a comment'`,
+		},
+		{
+			input: `# This is a comment
+SELECT 'TEST # This is not a comment' # This is a comment`,
+			want: `SELECT 'TEST # This is not a comment'`,
+		},
+		{
+			input: `SELECT 'TEST /* This is not a comment */'`,
+			want:  `SELECT 'TEST /* This is not a comment */'`,
+		},
+		{
+			input: `/* This is a comment */
+SELECT 'TEST /* This is not a comment */'`,
+			want: `SELECT 'TEST /* This is not a comment */'`,
+		},
+		{
+			input: `/* This is a comment */
+SELECT 'TEST /* This is not a comment */' /* This is a comment */`,
+			want: `SELECT 'TEST /* This is not a comment */'`,
+		},
+		{
+			input: `SELECT '''TEST
+-- This is not a comment
+'''`,
+			want: `SELECT '''TEST
+-- This is not a comment
+'''`,
+		},
+		{
+			input: ` -- This is a comment
+SELECT '''TEST
+-- This is not a comment
+''' -- This is a comment`,
+			want: `SELECT '''TEST
+-- This is not a comment
+'''`,
+		},
+		{
+			input: `SELECT '''TEST
+# This is not a comment
+'''`,
+			want: `SELECT '''TEST
+# This is not a comment
+'''`,
+		},
+		{
+			input: ` # This is a comment
+SELECT '''TEST
+# This is not a comment
+''' # This is a comment`,
+			want: `SELECT '''TEST
+# This is not a comment
+'''`,
+		},
+		{
+			input: `SELECT '''TEST
+/* This is not a comment */
+'''`,
+			want: `SELECT '''TEST
+/* This is not a comment */
+'''`,
+		},
+		{
+			input: ` /* This is a comment */
+SELECT '''TEST
+/* This is not a comment */
+''' /* This is a comment */`,
+			want: `SELECT '''TEST
+/* This is not a comment */
+'''`,
+		},
+		{
+			input: `SELECT """TEST
+-- This is not a comment
+"""`,
+			want: `SELECT """TEST
+-- This is not a comment
+"""`,
+		},
+		{
+			input: ` -- This is a comment
+SELECT """TEST
+-- This is not a comment
+""" -- This is a comment`,
+			want: `SELECT """TEST
+-- This is not a comment
+"""`,
+		},
+		{
+			input: `SELECT """TEST
+# This is not a comment
+"""`,
+			want: `SELECT """TEST
+# This is not a comment
+"""`,
+		},
+		{
+			input: ` # This is a comment
+SELECT """TEST
+# This is not a comment
+""" # This is a comment`,
+			want: `SELECT """TEST
+# This is not a comment
+"""`,
+		},
+		{
+			input: `SELECT """TEST
+/* This is not a comment */
+"""`,
+			want: `SELECT """TEST
+/* This is not a comment */
+"""`,
+		},
+		{
+			input: ` /* This is a comment */
+SELECT """TEST
+/* This is not a comment */
+""" /* This is a comment */`,
+			want: `SELECT """TEST
+/* This is not a comment */
+"""`,
+		},
+		{
+			input: `/* This is a comment /* this is still a comment */
+SELECT 1`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `/** This is a javadoc style comment /* this is still a comment */
+SELECT 1`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `/** This is a javadoc style comment /** this is still a comment */
+SELECT 1`,
+			want: `SELECT 1`,
+		},
+		{
+			input: `/** This is a javadoc style comment /** this is still a comment **/
+SELECT 1`,
+			want: `SELECT 1`,
+		},
+	}
+	a := require.New(t)
+	for _, tc := range tests {
+		got, err := removeCommentsAndTrim(tc.input)
+		if tc.wantErr {
+			a.Error(err)
+		} else {
+			a.NoError(err)
+		}
+		a.Equal(tc.want, got)
+	}
+}
+
+func TestSanitizeQuery(t *testing.T) {
+	tests := []struct {
+		input   string
+		want    []string
+		wantErr bool
+	}{
+		{
+			input: `
+			--- 123456
+			SELECT 1;
+			--- 6312
+			SELECT 2;
+			---123
+			---123
+			/* 213123 */
+			SELECT 3;
+			`,
+			want: []string{
+				"SELECT 1", "SELECT 2", "SELECT 3",
+			},
+		},
+	}
+	a := require.New(t)
+	for _, tc := range tests {
+		got, err := sanitizeSQL(tc.input)
+		if tc.wantErr {
+			a.Error(err)
+		} else {
+			a.NoError(err)
+		}
+		a.Equal(tc.want, got)
+	}
+}
