602927322a3d391edfe605ec208d9706cafba588
ecmadao <wlec@outlook.com>
2023-01-09 13:21:11 +0800

    chore: enterprise plan trial (#4219)

--- frontend/src/components/FeatureAttention.vue
+++ frontend/src/components/FeatureAttention.vue
@@ -11,7 +11,7 @@
 
 <script lang="ts" setup>
 import { PropType, computed } from "vue";
-import { FeatureType, planTypeToString } from "@/types";
+import { FeatureType, PlanType, planTypeToString } from "@/types";
 import { useRouter } from "vue-router";
 import { useI18n } from "vue-i18n";
 import { useSubscriptionStore, pushNotification } from "@/store";
@@ -68,22 +68,26 @@ const descriptionText = computed(() => {
 const onClick = () => {
   if (subscriptionStore.canTrial) {
     const isUpgrade = subscriptionStore.canUpgradeTrial;
-    subscriptionStore.trialSubscription(requiredPlan).then(() => {
-      pushNotification({
-        module: "bytebase",
-        style: "SUCCESS",
-        title: t("common.success"),
-        description: isUpgrade
-          ? t("subscription.successfully-upgrade-trial", {
-              plan: t(
-                `subscription.plan.${planTypeToString(requiredPlan)}.title`
-              ),
-            })
-          : t("subscription.successfully-start-trial", {
-              days: subscriptionStore.trialingDays,
-            }),
+    subscriptionStore
+      .trialSubscription(PlanType.ENTERPRISE)
+      .then((subscription) => {
+        pushNotification({
+          module: "bytebase",
+          style: "SUCCESS",
+          title: t("common.success"),
+          description: isUpgrade
+            ? t("subscription.successfully-upgrade-trial", {
+                plan: t(
+                  `subscription.plan.${planTypeToString(
+                    subscription.plan
+                  )}.title`
+                ),
+              })
+            : t("subscription.successfully-start-trial", {
+                days: subscriptionStore.trialingDays,
+              }),
+        });
       });
-    });
   } else {
     router.push({ name: "setting.workspace.subscription" });
   }
