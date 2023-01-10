c06025057b038107958910b2b78cebc1483805c5
p0ny <xz@bytebase.com>
2023-01-10 10:13:11 +0800

    feat(spanner): implement query (#4225)
    Former-commit-id: 962170c8cf32c2f6501c17c90449cd0bfd7fa867

--- plugin/db/spanner/spanner_test.go
+++ plugin/db/spanner/spanner_test.go
@@ -3,6 +3,7 @@ package spanner
 import (
 	"testing"
 
+	"cloud.google.com/go/spanner/apiv1/spannerpb"
 	"github.com/stretchr/testify/require"
 )
 
@@ -33,5 +34,46 @@ func TestGetDatabaseFromDSN(t *testing.T) {
 		} else {
 			a.Error(err)
 		}
+	}
+}
+
+func TestGetColumnTypeName(t *testing.T) {
+	tests := []struct {
+		spannerType spannerpb.Type
+		want        string
+	}{
+		{
+			spannerType: spannerpb.Type{
+				Code: spannerpb.TypeCode_JSON,
+			},
+			want: "JSON",
+		},
+		{
+			spannerType: spannerpb.Type{
+				Code: spannerpb.TypeCode_DATE,
+			},
+			want: "DATE",
+		},
+		{
+			spannerType: spannerpb.Type{
+				Code: spannerpb.TypeCode_TIMESTAMP,
+			},
+			want: "TIMESTAMP",
+		},
+		{
+			spannerType: spannerpb.Type{
+				Code: spannerpb.TypeCode_ARRAY,
+				ArrayElementType: &spannerpb.Type{
+					Code: spannerpb.TypeCode_BYTES,
+				},
+			},
+			want: "[]BYTES",
+		},
+	}
+	a := require.New(t)
+	for i := range tests {
+		got, err := getColumnTypeName(&tests[i].spannerType)
+		a.NoError(err)
+		a.Equal(tests[i].want, got)
 	}
 }
