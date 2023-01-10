90760bee886742f605e5c9b0e833df9f26060bd4
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-01-10 14:48:33 +0800

    feat(sql-editor): some minor improvements and fixes (#4228)
    * feat: add parentheses around env names
    
    * feat: tuning style
    
    * feat: show instances view only for high-privileged users (BYT-2281)
    
    * chore: tuning styles BYT-2289
    
    * chore: change icon (BYT-2287)
    
    * feat: close table schema panel when needed (BYT-2291)
    
    * chore: make the tree not selectable (BYT-2280)
    
    * fix: wrap issue (BYT-2279)
    
    * chore: remove gradient shadows (BYT-2282)
    
    * fix: more precise css selector

---
+++ frontend/src/views/sql-editor/AsidePanel/TreeNode/InstancePrefix.vue
@@ -0,0 +1,30 @@
+<template>
+  <InstanceEngineIcon :instance="instance" />
+  <span class="text-sm -mr-0.5" :class="[!disabled && 'text-gray-500']">
+    (
+  </span>
+  <ProtectedEnvironmentIcon
+    :environment="instance.environment"
+    class="w-4 h-4 text-inherit"
+  />
+  <span class="text-sm" :class="[!disabled && 'text-gray-500']">
+    {{ instance.environment.name }}
+  </span>
+  <span class="text-sm -ml-0.5" :class="[!disabled && 'text-gray-500']">
+    )
+  </span>
+</template>
+
+<script setup lang="ts">
+import type { Instance } from "@/types";
+
+withDefaults(
+  defineProps<{
+    instance: Instance;
+    disabled?: boolean;
+  }>(),
+  {
+    disabled: false,
+  }
+);
+</script>
