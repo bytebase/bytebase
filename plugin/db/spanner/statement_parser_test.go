2c40fec4ee27951c8dc7ffebba90c525670aa855
p0ny <xz@bytebase.com>
2023-01-10 12:19:45 +0800

    fix: filter out empty sql (#4233)
    Former-commit-id: 65b538ddb6c25b32afc0a93cb0119e43bace0acf
    Former-commit-id: 0476d920381f8439078c184a2bcb67db125e8650

--- plugin/db/spanner/statement_parser_test.go
+++ plugin/db/spanner/statement_parser_test.go
@@ -477,6 +477,14 @@ CREATE INDEX bytebase_idx_migration_history_namespace_created ON migration_histo
 			`,
 			want: []string{"SELECT 1", "SELECT '2;3;4;'", "SELECT 2"},
 		},
+		{
+			input: `
+				SELECT 1;;;;
+				SELECT '2;3;4;';;
+				SELECT 2;;
+			`,
+			want: []string{"SELECT 1", "SELECT '2;3;4;'", "SELECT 2"},
+		},
 	}
 	a := require.New(t)
 	for _, tc := range tests {
