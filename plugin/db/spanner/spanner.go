d9424c3e83715c2d135a268434b010f070b2b500
p0ny <xz@bytebase.com>
2023-01-10 10:13:11 +0800

    feat(spanner): implement query (#4225)
    Former-commit-id: f0f9a37bb4792cec37e59ce22078e633080085d6
    Former-commit-id: 0431b324df035998bbb0b0c55c9affea9355ef0c

--- plugin/db/spanner/spanner.go
+++ plugin/db/spanner/spanner.go
@@ -7,12 +7,14 @@ import (
 	_ "embed"
 	"fmt"
 	"regexp"
+	"strings"
 
 	spanner "cloud.google.com/go/spanner"
 	spannerdb "cloud.google.com/go/spanner/admin/database/apiv1"
 	adminpb "cloud.google.com/go/spanner/admin/database/apiv1/databasepb"
 	"go.uber.org/zap"
 
+	sppb "cloud.google.com/go/spanner/apiv1/spannerpb"
 	"github.com/pkg/errors"
 
 	"github.com/bytebase/bytebase/common/log"
@@ -206,8 +208,286 @@ func (d *Driver) Execute(ctx context.Context, statement string, createDatabase b
 }
 
 // Query queries a SQL statement.
-func (*Driver) Query(context.Context, string, *db.QueryContext) ([]interface{}, error) {
-	panic("not implemented")
+func (d *Driver) Query(ctx context.Context, statement string, queryContext *db.QueryContext) ([]interface{}, error) {
+	stmts, err := sanitizeSQL(statement)
+	if err != nil {
+		return nil, err
+	}
+	if len(stmts) != 1 {
+		return nil, errors.Errorf("expect to get 1 statement, get %d", len(stmts))
+	}
+
+	statement = stmts[0]
+	if !queryContext.ReadOnly && !isSelect(statement) {
+		return d.queryAdmin(ctx, statement)
+	}
+
+	statement = getStatementWithResultLimit(statement, queryContext.Limit)
+	iter := d.client.Single().Query(ctx, spanner.NewStatement(statement))
+	defer iter.Stop()
+
+	row, err := iter.Next()
+	if err == iterator.Done {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	data := []interface{}{}
+	columnNames := getColumnNames(iter)
+	columnTypeNames, err := getColumnTypeNames(iter)
+	if err != nil {
+		return nil, err
+	}
+
+	for {
+		rowData, err := readRow(row)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, rowData)
+
+		row, err = iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+	return []interface{}{columnNames, columnTypeNames, data}, nil
+}
+
+func (d *Driver) queryAdmin(ctx context.Context, statement string) ([]interface{}, error) {
+	if isDDL(statement) {
+		op, err := d.dbClient.UpdateDatabaseDdl(ctx, &adminpb.UpdateDatabaseDdlRequest{
+			Database:   getDSN(d.config.Host, d.dbName),
+			Statements: []string{statement},
+		})
+		if err != nil {
+			return nil, err
+		}
+		return nil, op.Wait(ctx)
+	}
+
+	var rowCount int64
+	if _, err := d.client.ReadWriteTransaction(ctx, func(ctx context.Context, rwt *spanner.ReadWriteTransaction) error {
+		count, err := rwt.Update(ctx, spanner.NewStatement(statement))
+		if err != nil {
+			return err
+		}
+		rowCount = count
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	field := []string{"Affected Rows"}
+	types := []string{"INT64"}
+	rows := [][]interface{}{{rowCount}}
+	return []interface{}{field, types, rows}, nil
+}
+
+func getColumnNames(iter *spanner.RowIterator) []string {
+	var names []string
+	for _, field := range iter.Metadata.RowType.Fields {
+		names = append(names, field.Name)
+	}
+	return names
+}
+
+func getColumnTypeNames(iter *spanner.RowIterator) ([]string, error) {
+	var names []string
+	for _, field := range iter.Metadata.RowType.Fields {
+		typeName, err := getColumnTypeName(field.Type)
+		if err != nil {
+			return nil, err
+		}
+		names = append(names, typeName)
+	}
+	return names, nil
+}
+
+func getColumnTypeName(columnType *sppb.Type) (string, error) {
+	if columnType.Code == sppb.TypeCode_STRUCT {
+		return "", errors.New("spanner STRUCT type is not supported")
+	}
+	if columnType.Code == sppb.TypeCode_ARRAY {
+		if columnType.ArrayElementType.Code == sppb.TypeCode_STRUCT {
+			return "", errors.New("spanner STRUCT type is not supported")
+		}
+		return fmt.Sprintf("[]%s", columnType.ArrayElementType.Code.String()), nil
+	}
+	return columnType.Code.String(), nil
+}
+
+func readRow(row *spanner.Row) ([]interface{}, error) {
+	dest := make([]interface{}, row.Size())
+	for i := 0; i < row.Size(); i++ {
+		var col spanner.GenericColumnValue
+		if err := row.Column(i, &col); err != nil {
+			return nil, err
+		}
+		switch col.Type.Code {
+		case sppb.TypeCode_INT64:
+			var v spanner.NullInt64
+			if err := col.Decode(&v); err != nil {
+				return nil, err
+			}
+			if v.Valid {
+				dest[i] = v.Int64
+			} else {
+				dest[i] = nil
+			}
+		case sppb.TypeCode_FLOAT64:
+			var v spanner.NullFloat64
+			if err := col.Decode(&v); err != nil {
+				return nil, err
+			}
+			if v.Valid {
+				dest[i] = v.Float64
+			} else {
+				dest[i] = nil
+			}
+		case sppb.TypeCode_NUMERIC:
+			var v spanner.NullNumeric
+			if err := col.Decode(&v); err != nil {
+				return nil, err
+			}
+			if v.Valid {
+				dest[i] = v.Numeric
+			} else {
+				dest[i] = nil
+			}
+		case sppb.TypeCode_STRING:
+			var v spanner.NullString
+			if err := col.Decode(&v); err != nil {
+				return nil, err
+			}
+			if v.Valid {
+				dest[i] = v.StringVal
+			} else {
+				dest[i] = nil
+			}
+		case sppb.TypeCode_JSON:
+			var v spanner.NullJSON
+			if err := col.Decode(&v); err != nil {
+				return nil, err
+			}
+			// We always assign `v` to dest[i] here because there is no native type
+			// for JSON in the Go sql package. That means that instead of returning
+			// nil we should return a NullJSON with valid=false.
+			dest[i] = v
+		case sppb.TypeCode_BYTES:
+			// The column value is a base64 encoded string.
+			var v []byte
+			if err := col.Decode(&v); err != nil {
+				return nil, err
+			}
+			dest[i] = v
+		case sppb.TypeCode_BOOL:
+			var v spanner.NullBool
+			if err := col.Decode(&v); err != nil {
+				return nil, err
+			}
+			if v.Valid {
+				dest[i] = v.Bool
+			} else {
+				dest[i] = nil
+			}
+		case sppb.TypeCode_DATE:
+			var v spanner.NullDate
+			if err := col.Decode(&v); err != nil {
+				return nil, err
+			}
+			if v.Valid {
+				dest[i] = v.Date
+			} else {
+				dest[i] = nil
+			}
+		case sppb.TypeCode_TIMESTAMP:
+			var v spanner.NullTime
+			if err := col.Decode(&v); err != nil {
+				return nil, err
+			}
+			if v.Valid {
+				dest[i] = v.Time
+			} else {
+				dest[i] = nil
+			}
+		case sppb.TypeCode_ARRAY:
+			switch col.Type.ArrayElementType.Code {
+			case sppb.TypeCode_INT64:
+				var v []spanner.NullInt64
+				if err := col.Decode(&v); err != nil {
+					return nil, err
+				}
+				dest[i] = v
+			case sppb.TypeCode_FLOAT64:
+				var v []spanner.NullFloat64
+				if err := col.Decode(&v); err != nil {
+					return nil, err
+				}
+				dest[i] = v
+			case sppb.TypeCode_NUMERIC:
+				var v []spanner.NullNumeric
+				if err := col.Decode(&v); err != nil {
+					return nil, err
+				}
+				dest[i] = v
+			case sppb.TypeCode_STRING:
+				var v []spanner.NullString
+				if err := col.Decode(&v); err != nil {
+					return nil, err
+				}
+				dest[i] = v
+			case sppb.TypeCode_JSON:
+				var v []spanner.NullJSON
+				if err := col.Decode(&v); err != nil {
+					return nil, err
+				}
+				dest[i] = v
+			case sppb.TypeCode_BYTES:
+				var v [][]byte
+				if err := col.Decode(&v); err != nil {
+					return nil, err
+				}
+				dest[i] = v
+			case sppb.TypeCode_BOOL:
+				var v []spanner.NullBool
+				if err := col.Decode(&v); err != nil {
+					return nil, err
+				}
+				dest[i] = v
+			case sppb.TypeCode_DATE:
+				var v []spanner.NullDate
+				if err := col.Decode(&v); err != nil {
+					return nil, err
+				}
+				dest[i] = v
+			case sppb.TypeCode_TIMESTAMP:
+				var v []spanner.NullTime
+				if err := col.Decode(&v); err != nil {
+					return nil, err
+				}
+				dest[i] = v
+			}
+		}
+	}
+	return dest, nil
+}
+
+func getStatementWithResultLimit(stmt string, limit int) string {
+	stmt = strings.TrimRight(stmt, " \n\t;")
+	if !strings.HasPrefix(stmt, "EXPLAIN") {
+		limitPart := ""
+		if limit > 0 {
+			limitPart = fmt.Sprintf(" LIMIT %d", limit)
+		}
+		return fmt.Sprintf("WITH result AS (%s) SELECT * FROM result%s;", stmt, limitPart)
+	}
+	return stmt
 }
 
 func getDSN(host, database string) string {
