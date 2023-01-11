bb5c383aa3e0b76af75ff1525891191fa84c9dc2
h3n4l <oysterdays@gmail.com>
2023-01-11 10:32:36 +0800

    fix: don't filter the mongodb database out if trigger by change data

--- frontend/src/components/AlterSchemaPrepForm/AlterSchemaPrepForm.vue
+++ frontend/src/components/AlterSchemaPrepForm/AlterSchemaPrepForm.vue
@@ -278,8 +278,10 @@ const databaseList = computed(() => {
   }
 
   list = list.filter((db) => db.syncStatus === "OK");
-  // We disallow users to alter schema for MongoDB databases.
-  list = list.filter((db) => db.instance.engine !== "MONGODB");
+  if (props.type === "bb.issue.database.schema.update") {
+    // We disallow users to alter schema for MongoDB databases.
+    list = list.filter((db) => db.instance.engine !== "MONGODB");
+  }
 
   const keyword = state.searchText.trim();
   list = list.filter((db) =>
