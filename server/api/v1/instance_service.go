dc8dc28e2fe45dff5ff1751689ca2e28fb351e11
h3n4l <oysterdays@gmail.com>
2023-01-11 13:52:54 +0800

    chore: remove msg

--- server/api/v1/instance_service.go
+++ server/api/v1/instance_service.go
@@ -257,7 +257,7 @@ func (s *InstanceService) AddDataSource(ctx context.Context, request *v1pb.AddDa
 		return nil, status.Errorf(codes.InvalidArgument, "only support add read-only data source")
 	}
 
-	dataSourceMsg, err := convertToDataSourceMessage(request.DataSources)
+	dataSource, err := convertToDataSourceMessage(request.DataSources)
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, "failed to convert data source")
 	}
@@ -282,7 +282,7 @@ func (s *InstanceService) AddDataSource(ctx context.Context, request *v1pb.AddDa
 
 	principalID := ctx.Value(common.PrincipalIDContextKey).(int)
 
-	instance, err = s.store.AddDataSourceToInstanceV2(ctx, instance.UID, principalID, dataSourceMsg)
+	instance, err = s.store.AddDataSourceToInstanceV2(ctx, instance.UID, principalID, dataSource)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
@@ -300,7 +300,7 @@ func (s *InstanceService) RemoveDataSource(ctx context.Context, request *v1pb.Re
 		return nil, status.Errorf(codes.InvalidArgument, "only support remove read-only data source")
 	}
 
-	dataSourceMsg, err := convertToDataSourceMessage(request.DataSources)
+	dataSource, err := convertToDataSourceMessage(request.DataSources)
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, "failed to convert data source")
 	}
@@ -324,7 +324,7 @@ func (s *InstanceService) RemoveDataSource(ctx context.Context, request *v1pb.Re
 		return nil, status.Errorf(codes.InvalidArgument, "instance %q has been deleted", request.Instance)
 	}
 
-	instance, err = s.store.RemoveDataSourceV2(ctx, instance.UID, dataSourceMsg.Type)
+	instance, err = s.store.RemoveDataSourceV2(ctx, instance.UID, dataSource.Type)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
