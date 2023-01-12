5c09da538306ff0381bb203d61ed862c555368c4
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-01-12 11:51:38 +0800

    chore: change the uuid generation to be compatible with older version of postgres (#4274)

--- store/migration/prod/1.10/0002##resource_id.sql
+++ store/migration/prod/1.10/0002##resource_id.sql
@@ -4,8 +4,8 @@ ALTER TABLE instance ADD COLUMN resource_id TEXT;
 
 UPDATE environment SET resource_id=LOWER(name);
 UPDATE project SET resource_id='default' WHERE id = 1;
-UPDATE project SET resource_id=CONCAT('project-', LEFT(gen_random_uuid()::TEXT, 8)) WHERE id != 1;
-UPDATE instance SET resource_id=CONCAT('instance-', LEFT(gen_random_uuid()::TEXT, 8));
+UPDATE project SET resource_id=CONCAT('project-', LEFT(uuid_in(md5(random()::text || random()::text)::cstring)::TEXT, 8)) WHERE id != 1;
+UPDATE instance SET resource_id=CONCAT('instance-', LEFT(uuid_in(md5(random()::text || random()::text)::cstring)::TEXT, 8));
 
 ALTER TABLE environment ALTER COLUMN resource_id SET NOT NULL;
 ALTER TABLE project ALTER COLUMN resource_id SET NOT NULL;
