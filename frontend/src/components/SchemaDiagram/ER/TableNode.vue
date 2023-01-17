aa5c9122e74a766695beb7ba7bcbde725a787f3d
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-01-17 13:09:01 +0800

    chore: node ids

--- frontend/src/components/SchemaDiagram/ER/TableNode.vue
+++ frontend/src/components/SchemaDiagram/ER/TableNode.vue
@@ -2,7 +2,7 @@
   <div
     class="absolute overflow-hidden rounded-md shadow-lg border-b border-gray-200 bg-white w-[16rem] divide-y z-[10]"
     bb-node-type="table"
-    :bb-node-id="idOfTable(table)"
+    :bb-node-id="dummy ? `dummy-${idOfTable(table)}` : idOfTable(table)"
     :bb-status="tableStatus(table)"
     :style="{
       left: `${rect.x}px`,
@@ -39,7 +39,7 @@
       <tr
         v-for="(column, i) in table.columns"
         :key="i"
-        :bb-column-name="column.name"
+        :bb-column-name="dummy ? `dummy-${column.name}` : column.name"
         :bb-status="columnStatus(column)"
         :class="columnClasses(column)"
       >
@@ -101,6 +101,7 @@ const props = withDefaults(
   {}
 );
 const {
+  dummy,
   editable,
   idOfTable,
   rectOfTable,
