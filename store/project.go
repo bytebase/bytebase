b6703947e4bfbd21bdf5d0a171e36c2f63cdaba5
h3n4l <oysterdays@gmail.com>
2023-01-18 20:02:02 +0800

    feat(api): implement set project IAM policy (#4336)
    * feat: implement set project IAM policy
    
    * chore: fix linter
    
    * chore: migrate create project to set project IAM policy
    
    * feat: forbid duplicate user
    
    * feat: forbid duplicate user
    
    * chore: make linter happier
    
    * chore: make linter happier
    
    * chore: make linter happier
    
    * refactor: simplify logic
    
    * chore: update
    
    * chore: update
    
    * chore: fix linter
    
    Former-commit-id: 4700eca233bc18ac5e834e9b9976847ee60e2513

--- store/project.go
+++ store/project.go
@@ -247,6 +247,11 @@ func (s *Store) CreateProjectV2(ctx context.Context, create *ProjectMessage, cre
 		create.LGTMCheckSetting = api.GetDefaultLGTMCheckSetting()
 	}
 
+	user, err := s.GetUserByID(ctx, creatorID)
+	if err != nil {
+		return nil, err
+	}
+
 	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
 		return nil, FormatError(err)
@@ -304,14 +309,17 @@ func (s *Store) CreateProjectV2(ctx context.Context, create *ProjectMessage, cre
 		return nil, FormatError(err)
 	}
 
-	// TODO(h3n4l): migrate to project set IAM policy.
-	projectMember := &api.ProjectMemberCreate{
-		CreatorID:   creatorID,
-		ProjectID:   project.UID,
-		Role:        common.ProjectOwner,
-		PrincipalID: creatorID,
+	policy := &IAMPolicyMessage{
+		Bindings: []*PolicyBinding{
+			{
+				Role: api.Owner,
+				Members: []*UserMessage{
+					user,
+				},
+			},
+		},
 	}
-	if _, err = createProjectMemberImpl(ctx, tx, projectMember); err != nil {
+	if err := s.setProjectIAMPolicyImpl(ctx, tx, policy, project.RoleProvider, creatorID, project.UID); err != nil {
 		return nil, err
 	}
 
