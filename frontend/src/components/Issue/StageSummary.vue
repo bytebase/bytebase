0ca23eb382b4236774d09b6e59769637f5dec7a1
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-01-28 11:39:39 +0800

    feat: simplify

--- frontend/src/components/Issue/StageSummary.vue
+++ frontend/src/components/Issue/StageSummary.vue
@@ -1,50 +1,15 @@
 <template>
-  <NPopover>
-    <template #trigger>
-      <div v-bind="$attrs" class="!no-underline">
-        <span class="!no-underline">(</span>
-        <span class="!no-underline">{{ summary.done }}</span>
-        <span class="!no-underline">/</span>
-        <span class="!no-underline">{{ summary.failed }}</span>
-        <span class="!no-underline mx-1">&amp;</span>
-        <span class="!no-underline">{{ summary.canceled }}</span>
-        <span class="!no-underline">/</span>
-        <span class="!no-underline">{{ summary.running }}</span>
-        <span class="!no-underline">/</span>
-        <span class="!no-underline">{{ summary.total }}</span>
-        <span class="!no-underline">)</span>
-      </div>
-    </template>
-
-    <div class="w-28 flex flex-col gap-y-0.5">
-      <div class="flex justify-between">
-        <label class="textlabel">{{ $t("task.status.success") }}</label>
-        <span>{{ summary.done }}</span>
-      </div>
-      <div class="flex justify-between">
-        <label class="textlabel">{{ $t("task.status.failed") }}</label>
-        <span>{{ summary.failed }}</span>
-      </div>
-      <hr class="my-0.5" />
-      <div class="flex justify-between">
-        <label class="textlabel">{{ $t("task.status.running") }}</label>
-        <span>{{ summary.running }}</span>
-      </div>
-      <div class="flex justify-between">
-        <label class="textlabel">{{ $t("task.status.canceled") }}</label>
-        <span>{{ summary.canceled }}</span>
-      </div>
-      <div class="flex justify-between">
-        <label class="textlabel">{{ $t("common.total") }}</label>
-        <span>{{ summary.total }}</span>
-      </div>
-    </div>
-  </NPopover>
+  <div class="!no-underline">
+    <span class="!no-underline">(</span>
+    <span class="!no-underline">{{ summary.done + summary.canceled }}</span>
+    <span class="!no-underline">/</span>
+    <span class="!no-underline">{{ summary.total }}</span>
+    <span class="!no-underline">)</span>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { computed } from "vue";
-import { NPopover } from "naive-ui";
 
 import type { Stage } from "@/types";
 
