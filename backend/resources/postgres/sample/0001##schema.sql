1fcf42426bffa7c00e2b627561ab4cff9617dceb
Tianzhou Chen <t@bytebase.com>
2023-01-29 23:47:30 +0800

    chore: prepare real sample employee data

---
+++ backend/resources/postgres/sample/0001##schema.sql
@@ -0,0 +1,86 @@
+DROP TABLE IF EXISTS dept_emp,
+                     dept_manager,
+                     title,
+                     salary, 
+                     employee, 
+                     department CASCADE;
+
+CREATE TABLE employee (
+	emp_no      SERIAL NOT NULL,
+	birth_date  DATE NOT NULL,
+	first_name  TEXT NOT NULL,
+	last_name   TEXT NOT NULL,
+	gender      TEXT NOT NULL CHECK (gender IN('M', 'F')) NOT NULL,
+	hire_date   DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE department (
+	dept_no     TEXT NOT NULL,
+	dept_name   TEXT NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE      (dept_name)
+);
+
+CREATE TABLE dept_manager (
+	emp_no      INT NOT NULL,
+	dept_no     TEXT NOT NULL,
+	from_date   DATE NOT NULL,
+	to_date     DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employee (emp_no) ON DELETE CASCADE,
+	FOREIGN KEY (dept_no) REFERENCES department (dept_no) ON DELETE CASCADE,
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE dept_emp (
+	emp_no      INT NOT NULL,
+	dept_no     TEXT NOT NULL,
+	from_date   DATE NOT NULL,
+	to_date     DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employee (emp_no) ON DELETE CASCADE,
+	FOREIGN KEY (dept_no) REFERENCES department (dept_no) ON DELETE CASCADE,
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE title (
+	emp_no      INT NOT NULL,
+	title       TEXT NOT NULL,
+	from_date   DATE NOT NULL,
+	to_date     DATE,
+	FOREIGN KEY (emp_no) REFERENCES employee (emp_no) ON DELETE CASCADE,
+	PRIMARY KEY (emp_no, title, from_date)
+); 
+
+CREATE TABLE salary (
+	emp_no      INT NOT NULL,
+	amount      INT NOT NULL,
+	from_date   DATE NOT NULL,
+	to_date     DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employee (emp_no) ON DELETE CASCADE,
+	PRIMARY KEY (emp_no, from_date)
+);
+
+CREATE OR REPLACE VIEW dept_emp_latest_date AS
+SELECT
+	emp_no,
+	MAX(
+		from_date) AS from_date,
+	MAX(
+		to_date) AS to_date
+FROM
+	dept_emp
+GROUP BY
+	emp_no;
+
+-- shows only the current department for each employee
+CREATE OR REPLACE VIEW current_dept_emp AS
+SELECT
+	l.emp_no,
+	dept_no,
+	l.from_date,
+	l.to_date
+FROM
+	dept_emp d
+	INNER JOIN dept_emp_latest_date l ON d.emp_no = l.emp_no
+		AND d.from_date = l.from_date
+		AND l.to_date = d.to_date;
