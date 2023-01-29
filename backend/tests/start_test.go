498945addf469ca196e2bf998991d7dd9bbbca9c
Tianzhou (天舟) <t@bytebase.com>
2023-01-29 16:01:37 +0800

    feat: add demo data (#4439)

--- backend/tests/start_test.go
+++ backend/tests/start_test.go
@@ -27,9 +27,10 @@ func startStopServer(ctx context.Context, a *require.Assertions, ctl *controller
 	projects, err := ctl.getProjects()
 	a.NoError(err)
 
-	// Default project.
-	a.Equal(1, len(projects))
+	// Default + Sample project.
+	a.Equal(2, len(projects))
 	a.Equal("Default", projects[0].Name)
+	a.Equal("Sample Project", projects[1].Name)
 
 	err = ctl.Close(ctx)
 	a.NoError(err)
