7b34410d4019e71a7fde1a60c9c2e62e864db651
boojack <stevenlgtm@gmail.com>
2023-01-30 21:50:13 +0800

    chore: remove test connection modal (#4479)

--- frontend/src/components/IdentityProviderCreateForm.vue
+++ frontend/src/components/IdentityProviderCreateForm.vue
@@ -351,7 +351,7 @@
         <button
           :disabled="!allowTestConnection"
           class="btn-normal"
-          @click="state.showTestModal = true"
+          @click="testConnection"
         >
           {{ $t("identity-provider.test-connection") }}
         </button>
@@ -397,18 +397,11 @@
       </div>
     </div>
   </div>
-
-  <template v-if="state.showTestModal">
-    <OAuth2IdentityProviderTestModal
-      v-if="state.type === IdentityProviderType.OAUTH2"
-      :identity-provider="updatedIdentityProvider"
-      @cancel="state.showTestModal = false"
-    />
-  </template>
 </template>
 
 <script lang="ts" setup>
 import { cloneDeep, isEqual } from "lodash-es";
+import { ClientError } from "nice-grpc-common";
 import {
   computed,
   reactive,
@@ -416,6 +409,7 @@ import {
   defineProps,
   ref,
   onMounted,
+  onUnmounted,
 } from "vue";
 import {
   FieldMapping,
@@ -426,19 +420,20 @@ import {
   OIDCIdentityProviderConfig,
 } from "@/types/proto/v1/idp_service";
 import { useIdentityProviderStore } from "@/store/modules/idp";
-import { useActuatorStore } from "@/store";
+import { pushNotification, useActuatorStore } from "@/store";
 import {
   IdentityProviderTemplate,
   identityProviderTemplateList,
   identityProviderTypeToString,
   isDev,
+  openWindowForSSO,
 } from "@/utils";
-import OAuth2IdentityProviderTestModal from "./OAuth2IdentityProviderTestModal.vue";
 import ShowMoreIcon from "./ShowMoreIcon.vue";
+import { OAuthWindowEventPayload } from "@/types";
+import { identityProviderClient } from "@/grpcweb";
 
 interface LocalState {
   type: IdentityProviderType;
-  showTestModal: boolean;
 }
 
 const props = defineProps<{
@@ -454,7 +449,6 @@ const emit = defineEmits<{
 const identityProviderStore = useIdentityProviderStore();
 const state = reactive<LocalState>({
   type: IdentityProviderType.OAUTH2,
-  showTestModal: false,
 });
 const identityProvider = ref<IdentityProvider>(
   IdentityProvider.fromPartial({})
@@ -557,20 +551,21 @@ const allowTestConnection = computed(() => {
   return false;
 });
 
-const updatedIdentityProvider = computed(() => {
+const editedIdentityProvider = computed(() => {
   const tempIdentityProvider: IdentityProvider = {
     ...identityProvider.value,
+    type: state.type,
     config: IdentityProviderConfig.fromPartial({}),
   };
-  if (state.type === IdentityProviderType.OAUTH2) {
+  if (tempIdentityProvider.type === IdentityProviderType.OAUTH2) {
     tempIdentityProvider.config!.oauth2Config = {
       ...configForOAuth2.value,
       scopes: scopesStringOfConfig.value.split(" "),
     };
-  } else if (state.type === IdentityProviderType.OIDC) {
+  } else if (tempIdentityProvider.type === IdentityProviderType.OIDC) {
     tempIdentityProvider.config!.oidcConfig = configForOIDC.value;
   } else {
-    // should not reach here.
+    throw new Error(`identity provider type ${state.type} is invalid`);
   }
   return tempIdentityProvider;
 });
@@ -579,7 +574,7 @@ const allowUpdate = computed(() => {
   if (!isFormCompleted.value) {
     return false;
   }
-  if (isEqual(updatedIdentityProvider.value, originIdentityProvider.value)) {
+  if (isEqual(editedIdentityProvider.value, originIdentityProvider.value)) {
     return false;
   }
   return true;
@@ -607,6 +602,62 @@ onMounted(async () => {
   }
 });
 
+onMounted(() => {
+  if (state.type === IdentityProviderType.OAUTH2) {
+    window.addEventListener(
+      `bb.oauth.signin.${editedIdentityProvider.value.name}`,
+      loginWithIdentityProviderEventListener,
+      false
+    );
+  }
+});
+
+onUnmounted(() => {
+  if (state.type === IdentityProviderType.OAUTH2) {
+    window.removeEventListener(
+      `bb.oauth.signin.${editedIdentityProvider.value.name}`,
+      loginWithIdentityProviderEventListener,
+      false
+    );
+  }
+});
+
+const loginWithIdentityProviderEventListener = async (event: Event) => {
+  const payload = (event as CustomEvent).detail as OAuthWindowEventPayload;
+  if (payload.error) {
+    return;
+  }
+
+  const code = payload.code;
+  try {
+    await identityProviderClient().testIdentityProvider({
+      identityProvider: editedIdentityProvider.value,
+      oauth2Context: {
+        code: code,
+      },
+    });
+  } catch (error) {
+    pushNotification({
+      module: "bytebase",
+      style: "CRITICAL",
+      title: `Request error occurred`,
+      description: (error as ClientError).details,
+    });
+    return;
+  }
+  pushNotification({
+    module: "bytebase",
+    style: "SUCCESS",
+    title: "Test connection succeed",
+  });
+};
+
+const testConnection = () => {
+  if (state.type === IdentityProviderType.OAUTH2) {
+    openWindowForSSO(editedIdentityProvider.value);
+  }
+};
+
 const handleTemplateSelect = (template: IdentityProviderTemplate) => {
   if (template.type !== state.type) {
     return;
@@ -633,25 +684,29 @@ const handleCancelButtonClick = () => {
   emit("cancel");
 };
 
+const updateEditState = (updatedIdentityProvider: IdentityProvider) => {
+  const tempIdentityProvider = updatedIdentityProvider;
+  identityProvider.value = tempIdentityProvider;
+  state.type = tempIdentityProvider.type;
+  if (tempIdentityProvider.type === IdentityProviderType.OAUTH2) {
+    configForOAuth2.value =
+      tempIdentityProvider.config?.oauth2Config ||
+      OAuth2IdentityProviderConfig.fromPartial({
+        fieldMapping: FieldMapping.fromPartial({}),
+      });
+    scopesStringOfConfig.value = configForOAuth2.value.scopes.join(" ");
+  } else if (tempIdentityProvider.type === IdentityProviderType.OIDC) {
+    configForOIDC.value =
+      tempIdentityProvider.config?.oidcConfig ||
+      OIDCIdentityProviderConfig.fromPartial({
+        fieldMapping: FieldMapping.fromPartial({}),
+      });
+  }
+};
+
 const handleDiscardChangesButtonClick = async () => {
   if (originIdentityProvider.value) {
-    const tempIdentityProvider = cloneDeep(originIdentityProvider.value);
-    identityProvider.value = tempIdentityProvider;
-    state.type = tempIdentityProvider.type;
-    if (tempIdentityProvider.type === IdentityProviderType.OAUTH2) {
-      configForOAuth2.value =
-        tempIdentityProvider.config?.oauth2Config ||
-        OAuth2IdentityProviderConfig.fromPartial({
-          fieldMapping: FieldMapping.fromPartial({}),
-        });
-      scopesStringOfConfig.value = configForOAuth2.value.scopes.join(" ");
-    } else if (tempIdentityProvider.type === IdentityProviderType.OIDC) {
-      configForOIDC.value =
-        tempIdentityProvider.config?.oidcConfig ||
-        OIDCIdentityProviderConfig.fromPartial({
-          fieldMapping: FieldMapping.fromPartial({}),
-        });
-    }
+    updateEditState(originIdentityProvider.value);
   }
 };
 
@@ -667,7 +722,7 @@ const handleCreateButtonClick = async () => {
   } else if (state.type === IdentityProviderType.OIDC) {
     identityProviderCreate.config!.oidcConfig = configForOIDC.value;
   } else {
-    // should not reach here.
+    throw new Error(`identity provider type ${state.type} is invalid`);
   }
 
   const createdIdentityProvider =
@@ -676,8 +731,15 @@ const handleCreateButtonClick = async () => {
 };
 
 const handleUpdateButtonClick = async () => {
-  await identityProviderStore.patchIdentityProvider(
-    updatedIdentityProvider.value
-  );
+  const updatedIdentityProvider =
+    await identityProviderStore.patchIdentityProvider(
+      editedIdentityProvider.value
+    );
+  pushNotification({
+    module: "bytebase",
+    style: "SUCCESS",
+    title: "Update SSO succeed",
+  });
+  updateEditState(updatedIdentityProvider);
 };
 </script>
