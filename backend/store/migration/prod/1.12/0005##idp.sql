649990ecd872a0968f8ed6fe81350dbd7c809d0d
steven <stevenlgtm@gmail.com>
2023-01-30 14:55:13 +0800

    chore: format

--- backend/store/migration/prod/1.12/0005##idp.sql
+++ backend/store/migration/prod/1.12/0005##idp.sql
@@ -3,17 +3,9 @@ CREATE TABLE idp (
   id SERIAL PRIMARY KEY,
   row_status row_status NOT NULL DEFAULT 'NORMAL',
   creator_id INTEGER NOT NULL REFERENCES principal (id),
-  created_ts BIGINT NOT NULL DEFAULT extract(
-    epoch
-    from
-      now()
-  ),
+  created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
   updater_id INTEGER NOT NULL REFERENCES principal (id),
-  updated_ts BIGINT NOT NULL DEFAULT extract(
-    epoch
-    from
-      now()
-  ),
+  updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
   resource_id TEXT NOT NULL,
   name TEXT NOT NULL,
   domain TEXT NOT NULL,
@@ -26,18 +18,14 @@ CREATE UNIQUE INDEX idx_idp_unique_resource_id ON idp(resource_id);
 
 ALTER SEQUENCE idp_id_seq RESTART WITH 101;
 
-CREATE TRIGGER update_idp_updated_ts BEFORE
+CREATE TRIGGER update_idp_updated_ts
+BEFORE
 UPDATE
-  ON idp FOR EACH ROW EXECUTE FUNCTION trigger_update_updated_ts();
+  ON idp FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
 
 DROP INDEX IF EXISTS idx_principal_unique_email;
 
-ALTER TABLE
-  principal
-ADD
-  COLUMN idp_id INTEGER REFERENCES idp (id);
+ALTER TABLE principal ADD COLUMN idp_id INTEGER REFERENCES idp (id);
 
-ALTER TABLE
-  principal
-ADD
-  COLUMN idp_user_info JSONB NOT NULL DEFAULT '{}';
+ALTER TABLE principal ADD COLUMN idp_user_info JSONB NOT NULL DEFAULT '{}';
