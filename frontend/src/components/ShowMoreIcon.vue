536439d58c5dc80e422293b9702f7748227d959b
steven <stevenlgtm@gmail.com>
2023-01-30 13:28:12 +0800

    chore: update hint text for creating idp form

---
+++ frontend/src/components/ShowMoreIcon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tooltip-wrapper">
+    <span
+      v-if="content !== ''"
+      class="tooltip whitespace-nowrap"
+      :class="`tooltip-${position}`"
+    >
+      {{ content }}
+    </span>
+    <heroicons-outline:question-mark-circle class="w-5 h-5" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    content: string;
+    position: string;
+  }>(),
+  {
+    content: "",
+    position: "top",
+  }
+);
+</script>
