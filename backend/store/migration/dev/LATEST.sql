96f6781a9e1c6ed9148e7d14ae9602b2ef6481b2
boojack <stevenlgtm@gmail.com>
2023-01-30 16:58:17 +0800

    chore: move idp schema change to prod (#4460)
    * chore: move idp schema change to prod
    
    * chore: rename
    
    * chore: update
    
    * chore: format
    
    * chore: format
    
    * chore: update
    
    * chore: update test version cutoff
    
    Former-commit-id: eed21b2441cd4c36295fb13cca0aeaf17ad40598
    Former-commit-id: 9bc6e1101fc362f9199eabecc4afeac632b13081

--- backend/store/migration/dev/LATEST.sql
+++ backend/store/migration/dev/LATEST.sql
@@ -10,6 +10,30 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- idp stores generic identity provider.
+CREATE TABLE idp (
+  id SERIAL PRIMARY KEY,
+  row_status row_status NOT NULL DEFAULT 'NORMAL',
+  created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+  updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+  resource_id TEXT NOT NULL,
+  name TEXT NOT NULL,
+  domain TEXT NOT NULL,
+  type TEXT NOT NULL CONSTRAINT idp_type_check CHECK (type IN ('OAUTH2', 'OIDC')),
+  -- config stores the corresponding configuration of the IdP, which may vary depending on the type of the IdP.
+  config JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE UNIQUE INDEX idx_idp_unique_resource_id ON idp(resource_id);
+
+ALTER SEQUENCE idp_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_idp_updated_ts
+BEFORE
+UPDATE
+    ON idp FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
+
 -- principal
 CREATE TABLE principal (
     id SERIAL PRIMARY KEY,
@@ -1181,30 +1205,4 @@ CREATE TRIGGER update_external_approval_updated_ts
 BEFORE
 UPDATE
     ON external_approval FOR EACH ROW
-EXECUTE FUNCTION trigger_update_updated_ts();
-
--- idp stores generic identity provider.
-CREATE TABLE idp (
-  id SERIAL PRIMARY KEY,
-  row_status row_status NOT NULL DEFAULT 'NORMAL',
-  creator_id INTEGER NOT NULL REFERENCES principal (id),
-  created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-  updater_id INTEGER NOT NULL REFERENCES principal (id),
-  updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-  resource_id TEXT NOT NULL,
-  name TEXT NOT NULL,
-  domain TEXT NOT NULL,
-  type TEXT NOT NULL CONSTRAINT idp_type_check CHECK (type IN ('OAUTH2', 'OIDC')),
-  -- config stores the corresponding configuration of the IdP, which may vary depending on the type of the IdP.
-  config JSONB NOT NULL DEFAULT '{}'
-);
-
-CREATE UNIQUE INDEX idx_idp_unique_resource_id ON idp(resource_id);
-
-ALTER SEQUENCE idp_id_seq RESTART WITH 101;
-
-CREATE TRIGGER update_idp_updated_ts
-BEFORE
-UPDATE
-    ON idp FOR EACH ROW
 EXECUTE FUNCTION trigger_update_updated_ts();
