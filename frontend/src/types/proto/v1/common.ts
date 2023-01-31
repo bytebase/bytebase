7aa70a1b4a86b922577ac2548c589619b4173fc8
rebelice <yangrebelice@gmail.com>
2023-01-31 17:50:21 +0800

    chore(advisor): split SQL review rules by engine type (#4488) (#4501)
    * chore(advisor): split SQL review rules by engine type
    
    * chore: split and merge sql review rules in server
    
    * chore: update
    
    ---------
    
    Co-authored-by: d-bytebase <d@bytebase.com>
    Former-commit-id: d03337cead2cb72e68c8bcdbd85ee38133b8c029
    Former-commit-id: fbf52a2af1358234120fd4ad05f8e56b6d10dc0d

--- frontend/src/types/proto/v1/common.ts
+++ frontend/src/types/proto/v1/common.ts
@@ -40,3 +40,72 @@ export function stateToJSON(object: State): string {
       return "UNRECOGNIZED";
   }
 }
+
+export enum Engine {
+  ENGINE_UNSPECIFIED = 0,
+  CLICKHOUSE = 1,
+  MYSQL = 2,
+  POSTGRES = 3,
+  SNOWFLAKE = 4,
+  SQLITE = 5,
+  TIDB = 6,
+  MONGODB = 7,
+  UNRECOGNIZED = -1,
+}
+
+export function engineFromJSON(object: any): Engine {
+  switch (object) {
+    case 0:
+    case "ENGINE_UNSPECIFIED":
+      return Engine.ENGINE_UNSPECIFIED;
+    case 1:
+    case "CLICKHOUSE":
+      return Engine.CLICKHOUSE;
+    case 2:
+    case "MYSQL":
+      return Engine.MYSQL;
+    case 3:
+    case "POSTGRES":
+      return Engine.POSTGRES;
+    case 4:
+    case "SNOWFLAKE":
+      return Engine.SNOWFLAKE;
+    case 5:
+    case "SQLITE":
+      return Engine.SQLITE;
+    case 6:
+    case "TIDB":
+      return Engine.TIDB;
+    case 7:
+    case "MONGODB":
+      return Engine.MONGODB;
+    case -1:
+    case "UNRECOGNIZED":
+    default:
+      return Engine.UNRECOGNIZED;
+  }
+}
+
+export function engineToJSON(object: Engine): string {
+  switch (object) {
+    case Engine.ENGINE_UNSPECIFIED:
+      return "ENGINE_UNSPECIFIED";
+    case Engine.CLICKHOUSE:
+      return "CLICKHOUSE";
+    case Engine.MYSQL:
+      return "MYSQL";
+    case Engine.POSTGRES:
+      return "POSTGRES";
+    case Engine.SNOWFLAKE:
+      return "SNOWFLAKE";
+    case Engine.SQLITE:
+      return "SQLITE";
+    case Engine.TIDB:
+      return "TIDB";
+    case Engine.MONGODB:
+      return "MONGODB";
+    case Engine.UNRECOGNIZED:
+    default:
+      return "UNRECOGNIZED";
+  }
+}
