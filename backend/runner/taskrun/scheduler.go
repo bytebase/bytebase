759098468fb0978927cd0cee06180b8e5e15bd36
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-01-31 15:54:39 +0800

    chore: reduce composed pipelines (#4496)
    * chore: update
    
    * chore: update
    
    Former-commit-id: 1a5f7eacd8d1f598639a7bac74354c4c60106597

--- backend/runner/taskrun/scheduler.go
+++ backend/runner/taskrun/scheduler.go
@@ -303,11 +303,12 @@ func (s *Scheduler) Run(ctx context.Context, wg *sync.WaitGroup) {
 							// The task has finished, and we may move to a new stage.
 							// if the current assignee doesn't fit in the new assignee group, we will reassign a new one based on the new assignee group.
 							if issue != nil {
-								composedPipeline, err := s.store.GetPipelineByID(ctx, issue.PipelineUID)
+								stages, err := s.store.ListStageV2(ctx, issue.PipelineUID)
 								if err != nil {
 									return
 								}
-								if activeStage := utils.GetActiveStage(composedPipeline); activeStage != nil && activeStage.ID != task.StageID {
+								activeStage := utils.GetActiveStage(stages)
+								if activeStage != nil && activeStage.ID != task.StageID {
 									environmentID := activeStage.EnvironmentID
 									ok, err := s.CanPrincipalBeAssignee(ctx, issue.Assignee.ID, environmentID, issue.Project.UID, issue.Type)
 									if err != nil {
@@ -449,7 +450,7 @@ func (s *Scheduler) PatchTaskStatement(ctx context.Context, task *api.Task, task
 
 	// Update statement activity.
 	if taskPatch.Statement != nil {
-		oldStatement, err := utils.GetTaskStatement(task)
+		oldStatement, err := utils.GetTaskStatement(task.Payload)
 		if err != nil {
 			return nil, err
 		}
