9572d60c1990f2e7c3a4ed0b6052946c3a752728
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-01-31 18:02:32 +0800

    chore: disable watermark when not supported

--- frontend/src/components/misc/Watermark.vue
+++ frontend/src/components/misc/Watermark.vue
@@ -19,14 +19,16 @@
 import { computed } from "vue";
 import { NWatermark } from "naive-ui";
 
-import { useCurrentUser, useSettingByName } from "@/store";
+import { featureToRef, useCurrentUser, useSettingByName } from "@/store";
 import { UNKNOWN_ID } from "@/types";
 
 const currentUser = useCurrentUser();
 const setting = useSettingByName("bb.workspace.watermark");
+const hasWatermarkFeature = featureToRef("bb.feature.watermark");
 
 const watermark = computed(() => {
   if (currentUser.value.id === UNKNOWN_ID) return "";
+  if (!hasWatermarkFeature.value) return "";
   if (setting.value?.value !== "1") return "";
   return `${currentUser.value.name} (${currentUser.value.id})`;
 });
