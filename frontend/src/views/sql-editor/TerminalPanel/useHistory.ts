3631332284d398120409cb142d3c12b7a90c17ff
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-02-01 15:01:41 +0800

    feat(sql-editor): split history items and query items (#4544)
    * feat: split history items and query items
    
    * chore: count limitation
    
    * fix: use ref

---
+++ frontend/src/views/sql-editor/TerminalPanel/useHistory.ts
@@ -0,0 +1,83 @@
+import { computed, watch } from "vue";
+
+import { TabMode, WebTerminalQueryItem } from "@/types";
+import { useTabStore, useWebTerminalStore } from "@/store";
+import { minmax } from "@/utils";
+
+const MAX_HISTORY_ITEM_COUNT = 1000;
+
+type HistoryState = {
+  index: number;
+  list: WebTerminalQueryItem[];
+};
+
+export const useHistory = () => {
+  const tabStore = useTabStore();
+  const webTerminalStore = useWebTerminalStore();
+  const historyByTabId = new Map<string, HistoryState>();
+
+  const currentQuery = computed(() => {
+    const queryList = webTerminalStore.getQueryListByTab(tabStore.currentTab);
+    return queryList[queryList.length - 1];
+  });
+
+  const currentStack = () => {
+    const tab = tabStore.currentTab;
+    if (tab.mode === TabMode.ReadOnly) return undefined;
+
+    const existed = historyByTabId.get(tab.id);
+    if (existed) {
+      return existed;
+    }
+    const initial: HistoryState = {
+      index: -1,
+      list: [],
+    };
+    historyByTabId.set(tab.id, initial);
+    return initial;
+  };
+
+  const push = (query: WebTerminalQueryItem) => {
+    const stack = currentStack();
+    if (!stack) return;
+    const { list } = stack;
+    list.push(query);
+    if (list.length > MAX_HISTORY_ITEM_COUNT) {
+      list.shift();
+    }
+    stack.index = list.length - 1;
+  };
+
+  const move = (direction: "up" | "down") => {
+    const stack = currentStack();
+    if (!stack) return;
+    const { index, list } = stack;
+    const delta = direction === "up" ? -1 : 1;
+    const nextIndex = minmax(index + delta, 0, list.length - 1);
+    if (nextIndex === index) {
+      return;
+    }
+    if (nextIndex === list.length - 1) {
+      currentQuery.value.sql = "";
+    } else {
+      const historyQuery = list[nextIndex];
+      if (historyQuery) {
+        currentQuery.value.sql = historyQuery.sql;
+      }
+    }
+
+    stack.index = nextIndex;
+  };
+
+  watch(
+    () => currentQuery.value,
+    (query) => {
+      push(query);
+    },
+    {
+      immediate: true,
+    }
+  );
+
+  return { push, move };
+};
