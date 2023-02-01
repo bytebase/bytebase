2d8b1e0947cb4b60495031f85e0016bdbb15bdd5
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-02-01 20:22:49 +0800

    chore: remove created time from vcs list (#4558) (#4559)
    * chore: remove created time from vcs list
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: ad794fb2bfbb8824056c7e0a546caa4c2912a29b

--- frontend/src/components/VCSCard.vue
+++ frontend/src/components/VCSCard.vue
@@ -40,14 +40,6 @@
             {{ vcs.applicationId }}
           </dd>
         </div>
-        <div class="grid grid-cols-4 gap-4 px-4 py-2 items-center">
-          <dt class="text-sm font-medium text-control-light">
-            {{ $t("common.created-at") }}
-          </dt>
-          <dd class="mt-1 flex text-sm text-main col-span-2">
-            {{ humanizeTs(vcs.createdTs) }}
-          </dd>
-        </div>
       </dl>
     </div>
   </div>
