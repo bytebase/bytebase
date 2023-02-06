4965328cfc2aacec42b4ae763153cbd1d086f00c
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-02-06 17:30:37 +0800

    feat: move paging control to the top toolbar (#4611)
    Former-commit-id: 06c7ba6e2bbb85d1f3c6f3f9e1d30a4130950735

--- frontend/src/views/sql-editor/EditorCommon/DataTable.vue
+++ frontend/src/views/sql-editor/EditorCommon/DataTable.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="bb-data-table relative w-full h-full overflow-hidden flex flex-col"
+    class="bb-data-table relative w-full flex-1 overflow-hidden flex flex-col"
   >
-    <div class="w-full flex-1 overflow-hidden">
+    <div class="w-full flex-1 flex flex-col overflow-hidden">
       <div
         class="header-track absolute z-0 left-0 top-0 right-0 h-[34px] border border-block-border bg-gray-50 dark:bg-gray-700"
       />
@@ -60,31 +60,12 @@
         </div>
       </div>
     </div>
-
-    <div v-if="showPagination" class="flex justify-end pt-2">
-      <NPagination
-        :item-count="table.getCoreRowModel().rows.length"
-        :page="table.getState().pagination.pageIndex + 1"
-        :page-size="table.getState().pagination.pageSize"
-        :show-quick-jumper="true"
-        :show-size-picker="true"
-        :page-sizes="[20, 50, 100]"
-        @update-page="handleChangePage"
-        @update-page-size="(ps) => table.setPageSize(ps)"
-      />
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { computed, nextTick, PropType, ref, watch } from "vue";
-import {
-  getCoreRowModel,
-  useVueTable,
-  getPaginationRowModel,
-  ColumnDef,
-} from "@tanstack/vue-table";
-import { NPagination } from "naive-ui";
+import { ColumnDef, Table } from "@tanstack/vue-table";
 import useTableColumnWidthLogic from "./useTableResize";
 
 export type DataTableColumn = {
@@ -92,17 +73,18 @@ export type DataTableColumn = {
   title: string;
 };
 
-const PAGE_SIZES = [20, 50, 100];
-const DEFAULT_PAGE_SIZE = 50;
-
 const props = defineProps({
   data: {
     type: Array as PropType<string[][]>,
     default: () => [],
   },
   columns: {
-    type: Array as PropType<DataTableColumn[]>,
+    type: Array as PropType<ColumnDef<string[]>[]>,
     default: () => [],
+  },
+  table: {
+    type: Object as PropType<Table<string[]>>,
+    required: true,
   },
 });
 
@@ -117,42 +99,22 @@ const tableResize = useTableColumnWidthLogic({
 });
 
 const data = computed(() => props.data);
-const columns = computed(() =>
-  props.columns.map<ColumnDef<string[]>>((col, index) => ({
-    id: `${col.key}@${index}`,
-    accessorFn: (item) => item[index],
-    header: col.title,
-  }))
-);
 
-const table = useVueTable<string[]>({
-  get data() {
-    return data.value;
-  },
-  get columns() {
-    return columns.value;
-  },
-  getCoreRowModel: getCoreRowModel(),
-  getPaginationRowModel: getPaginationRowModel(),
-});
-
-table.setPageSize(DEFAULT_PAGE_SIZE);
-const showPagination = computed(() => props.data.length > PAGE_SIZES[0]);
-
-const handleChangePage = (page: number) => {
-  table.setPageIndex(page - 1);
-  scrollerRef.value?.scrollTo(0, 0);
+const scrollTo = (x: number, y: number) => {
+  scrollerRef.value?.scroll(x, y);
 };
 
 watch(
-  () => props.columns.map((col) => col.title).join("|"),
+  () => props.columns.map((col) => col.header).join("|"),
   () => {
     nextTick(() => {
       // Re-calculate the column widths once the column definition changed.
-      scrollerRef.value?.scrollTo(0, 0);
+      scrollTo(0, 0);
       tableResize.reset();
     });
   },
   { immediate: true }
 );
+
+defineExpose({ scrollTo });
 </script>
