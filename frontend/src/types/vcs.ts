529c0deadd68262a280e05f4bdc3f62a74e6cb82
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-02-07 22:56:44 +0800

    fix(gitops): wrong jsonapi annotations for new format of commit structs (#4634)
    * refactor: use commits instead of fileCommit
    
    * feat: jsonapi definition for Commit struct
    
    * feat: use new struct
    
    * chore: strict condition
    
    * chore: gracefully downgrade

--- frontend/src/types/vcs.ts
+++ frontend/src/types/vcs.ts
@@ -53,6 +53,18 @@ export type VCSFileCommit = {
   added: string;
 };
 
+export type VCSCommit = {
+  id: string;
+  title: string;
+  message: string;
+  createdTs: number;
+  url: string;
+  authorName: string;
+  authorEmail: string;
+  addedList: string[];
+  modifiedList: string[];
+};
+
 export type VCSPushEvent = {
   vcsType: VCSType;
   ref: string;
@@ -61,6 +73,7 @@ export type VCSPushEvent = {
   repositoryFullPath: string;
   authorName: string;
   fileCommit: VCSFileCommit;
+  commits: VCSCommit[];
 };
 
 export function isValidVCSApplicationIdOrSecret(
