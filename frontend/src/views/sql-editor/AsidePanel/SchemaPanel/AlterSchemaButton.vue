884e07acb00770b23ca0df62b033dfe415ebad81
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-02-08 11:24:22 +0800

    chore: remove unused codes

--- frontend/src/views/sql-editor/AsidePanel/SchemaPanel/AlterSchemaButton.vue
+++ frontend/src/views/sql-editor/AsidePanel/SchemaPanel/AlterSchemaButton.vue
@@ -33,46 +33,4 @@ defineProps<{
 const emit = defineEmits<{
   (event: "click"): void;
 }>();
-
-// const exampleSQL = computed(() => {
-//   const { schema, table } = props;
-//   if (schema && table) {
-//     if (schema.name) {
-//       return `ALTER TABLE ${schema.name}.${table.name}`;
-//     }
-//     return `ALTER TABLE ${table.name}`;
-//   }
-//   return `ALTER TABLE`;
-// });
-
-// const gotoAlterSchema = () => {
-//   const { database } = props;
-
-//   const { project } = database;
-//   if (project.workflowType === "VCS") {
-//     useRepositoryStore()
-//       .fetchRepositoryByProjectId(database.project.id)
-//       .then((repository: Repository) => {
-//         window.open(
-//           baseDirectoryWebUrl(repository, {
-//             DB_NAME: database.name,
-//             ENV_NAME: database.instance.environment.name,
-//             TYPE: "ddl",
-//           }),
-//           "_blank"
-//         );
-//       });
-//     return;
-//   }
-
-//   const query = {
-//     template: "bb.issue.database.schema.update",
-//     name: `[${database.name}] Alter schema`,
-//     project: database.project.id,
-//     databaseList: database.id,
-//     sql: exampleSQL.value,
-//   };
-//   const url = `/issue/new?${stringify(query)}`;
-//   window.open(url, "_blank");
-// };
 </script>
