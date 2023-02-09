16a160d44ec0453a803f656080bb7a5f7593f502
p0ny <p0uy@outlook.com>
2023-02-09 11:03:39 +0800

    fix: reset rollbackStatement and rollbackError before retrying

--- backend/runner/taskrun/scheduler.go
+++ backend/runner/taskrun/scheduler.go
@@ -352,6 +352,14 @@ func (s *Scheduler) PatchTask(ctx context.Context, task *store.TaskMessage, task
 		}
 	}
 
+	// Reset rollbackStatement and rollbackError because we are trying to build
+	// the rollbackStatement again and there could be previous runs.
+	if taskPatch.RollbackEnabled != nil && *taskPatch.RollbackEnabled {
+		empty := ""
+		taskPatch.RollbackStatement = &empty
+		taskPatch.RollbackError = &empty
+	}
+
 	taskPatched, err := s.store.UpdateTaskV2(ctx, taskPatch)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to update task \"%v\"", task.Name)).SetInternal(err)
@@ -374,7 +382,7 @@ func (s *Scheduler) PatchTask(ctx context.Context, task *store.TaskMessage, task
 					cancel()
 				}
 			}
-			// We don't erase the keys for RollbacksCancel and RollbackGenerateMap here because they will eventually be erased by the rollback runner.
+			// We don't erase the keys for RollbackCancel and RollbackGenerate here because they will eventually be erased by the rollback runner.
 		}
 	}
 
