83342fa6c66650ab641536756b553ecc1097444c
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-02-10 15:35:04 +0800

    feat: implement issue rollback componets

---
+++ frontend/src/components/Issue/rollback/RollbackSwitch.vue
@@ -0,0 +1,14 @@
+<template>
+  <BBCheckbox
+    :disabled="!allowRollback"
+    :value="rollbackEnabled"
+    @toggle="toggleRollback"
+  />
+</template>
+
+<script lang="ts" setup>
+import { BBCheckbox } from "@/bbkit";
+import { useRollbackLogic } from "./common";
+
+const { allowRollback, rollbackEnabled, toggleRollback } = useRollbackLogic();
+</script>
