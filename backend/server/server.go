3d3f86f8722e81f0bb49d1364d0b4175efe89069
Tianzhou Chen <t@bytebase.com>
2023-02-12 16:39:26 +0800

    feat: configure a Sample SQL Review Policy

--- backend/server/server.go
+++ backend/server/server.go
@@ -51,6 +51,8 @@ import (
 	api "github.com/bytebase/bytebase/backend/legacyapi"
 	"github.com/bytebase/bytebase/backend/metric"
 	metricCollector "github.com/bytebase/bytebase/backend/metric/collector"
+	"github.com/bytebase/bytebase/backend/plugin/advisor"
+	advisorDb "github.com/bytebase/bytebase/backend/plugin/advisor/db"
 	"github.com/bytebase/bytebase/backend/plugin/app/feishu"
 	"github.com/bytebase/bytebase/backend/plugin/db"
 	bbs3 "github.com/bytebase/bytebase/backend/plugin/storage/s3"
@@ -752,6 +754,48 @@ func (s *Server) GetWorkspaceID() string {
 	return s.workspaceID
 }
 
+// getSampleSQLReviewPolicy returns a sample SQL review policy for preparing onboardign data.
+func getSampleSQLReviewPolicy() *advisor.SQLReviewPolicy {
+	policy := &advisor.SQLReviewPolicy{
+		Name: "SQL Review Sample Policy",
+	}
+
+	ruleList := []*advisor.SQLReviewRule{}
+
+	// Add DropEmptyDatabase rule for MySQL and TiDB.
+	for _, e := range []advisorDb.Type{advisorDb.MySQL, advisorDb.TiDB} {
+		ruleList = append(ruleList, &advisor.SQLReviewRule{
+			Type:    advisor.SchemaRuleDropEmptyDatabase,
+			Level:   advisor.SchemaRuleLevelError,
+			Engine:  e,
+			Payload: "{}",
+		})
+	}
+
+	// Add ColumnNotNull rule for MySQL, TiDB and Postgres.
+	for _, e := range []advisorDb.Type{advisorDb.MySQL, advisorDb.TiDB, advisorDb.Postgres} {
+		ruleList = append(ruleList, &advisor.SQLReviewRule{
+			Type:    advisor.SchemaRuleColumnNotNull,
+			Level:   advisor.SchemaRuleLevelWarning,
+			Engine:  e,
+			Payload: "{}",
+		})
+	}
+
+	// Add TableDropNamingConvention rule for MySQL, TiDB and Postgres.
+	for _, e := range []advisorDb.Type{advisorDb.MySQL, advisorDb.TiDB, advisorDb.Postgres} {
+		ruleList = append(ruleList, &advisor.SQLReviewRule{
+			Type:    advisor.SchemaRuleTableDropNamingConvention,
+			Level:   advisor.SchemaRuleLevelError,
+			Engine:  e,
+			Payload: "{\"format\":\"_del$\"}",
+		})
+	}
+
+	policy.RuleList = ruleList
+	return policy
+}
+
 // generateOnboardingData generates onboarding data after the first signup.
 func (s *Server) generateOnboardingData(ctx context.Context, userID int) error {
 	project, err := s.store.CreateProjectV2(ctx, &store.ProjectMessage{
@@ -828,6 +872,26 @@ func (s *Server) generateOnboardingData(ctx context.Context, userID int) error {
 		return errors.Wrapf(err, "failed to sync sample database schema")
 	}
 
+	// Add a sample SQL Review policy to the prod environment. This pairs with the following schema
+	// change issue to demonstrate the SQL Review feature.
+	policyPayload, err := json.Marshal(*getSampleSQLReviewPolicy())
+	if err != nil {
+		return errors.Wrapf(err, "failed to marshal onboarding SQL Review policy")
+	}
+
+	_, err = s.store.CreatePolicyV2(ctx, &store.PolicyMessage{
+		ResourceUID:       api.DefaultProdEnvironmentUID,
+		ResourceType:      api.PolicyResourceTypeEnvironment,
+		Payload:           string(policyPayload),
+		Type:              api.PolicyTypeSQLReview,
+		InheritFromParent: true,
+		// Enforce cannot be false while creating a policy.
+		Enforce: true,
+	}, userID)
+	if err != nil {
+		return errors.Wrapf(err, "failed to create onboarding SQL Review policy")
+	}
+
 	// Create a schema update issue.
 	createContext, err := json.Marshal(
 		&api.MigrationContext{
@@ -835,7 +899,9 @@ func (s *Server) generateOnboardingData(ctx context.Context, userID int) error {
 				{
 					MigrationType: db.Migrate,
 					DatabaseID:    database.UID,
-					Statement:     "ALTER TABLE employee ADD COLUMN IF NOT EXISTS email TEXT NOT NULL DEFAULT '';",
+					// This will violate the NOT NULL SQL Review policy configured above and emit a
+					// warning. Thus to demonstrate the SQL Review capability.
+					Statement: "ALTER TABLE employee ADD COLUMN IF NOT EXISTS email TEXT DEFAULT '';",
 				},
 			},
 		})
