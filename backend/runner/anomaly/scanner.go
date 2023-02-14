c3fab47b30b119a1a0ebb373166178c72b18cd2f
p0ny <xz@bytebase.com>
2023-02-14 11:21:57 +0800

    fix: skip checking backup anomalies for unsupported databases (#4737)
    Former-commit-id: 5df399d0ed06780db38de24dcb517dc760ee3a82

--- backend/runner/anomaly/scanner.go
+++ backend/runner/anomaly/scanner.go
@@ -364,6 +364,11 @@ func (s *Scanner) checkDatabaseAnomaly(ctx context.Context, instance *store.Inst
 }
 
 func (s *Scanner) checkBackupAnomaly(ctx context.Context, environment *store.EnvironmentMessage, instance *store.InstanceMessage, database *store.DatabaseMessage, policyMap map[int]*api.BackupPlanPolicy) {
+	if instance.Engine == db.MongoDB || instance.Engine == db.Spanner {
+		// skip checking backup anomalies for MongoDB and Spanner because they don't support Backup.
+		return
+	}
+
 	schedule := api.BackupPlanPolicyScheduleUnset
 	backupSetting, err := s.store.GetBackupSettingV2(ctx, database.UID)
 	if err != nil {
