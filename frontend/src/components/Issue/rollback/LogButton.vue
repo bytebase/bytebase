c652c9f8d59e942395c9623d39e2e51f1fa92d0b
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-02-15 11:26:20 +0800

    feat(frontend): SQL rollback button UI update (#4739) (#4763)
    * feat: button UI update
    
    * chore: update style
    
    Former-commit-id: 70bf4ee5efbe4345ae8d5d80e2bed70eda7559c2
    Former-commit-id: e842925c9ebcf251060bc6182dd334fb27b0d185

---
+++ frontend/src/components/Issue/rollback/LogButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button
+    class="btn-normal !px-3 !py-1"
+    :disabled="disabled"
+    @click="toggleRollback(true)"
+  >
+    {{ $t("task.rollback.log") }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useRollbackLogic } from "./common";
+
+const { allowRollback, toggleRollback } = useRollbackLogic();
+
+const disabled = computed(() => !allowRollback.value);
+</script>
