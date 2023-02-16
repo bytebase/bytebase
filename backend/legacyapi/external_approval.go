b64cbbcee77d4ce7cd539a3284e2898a9167d3e3
h3n4l <oysterdays@gmail.com>
2023-02-16 17:42:05 +0800

    chore: migrate external approval to v1 store

--- backend/legacyapi/external_approval.go
+++ backend/legacyapi/external_approval.go
@@ -6,28 +6,6 @@ type ExternalApprovalType string
 // ExternalApprovalTypeFeishu is the ExternalApproval from feishu.
 const ExternalApprovalTypeFeishu = "bb.plugin.app.feishu"
 
-// ExternalApproval is the API message of ExternalApproval.
-// It only lives in the backend.
-type ExternalApproval struct {
-	ID int
-
-	// Standard fields
-	RowStatus RowStatus
-	CreatedTs int64
-	UpdatedTs int64
-
-	// Related fields
-	IssueID     int
-	RequesterID int
-	Requester   *Principal
-	ApproverID  int
-	Approver    *Principal
-
-	// Domain specific fields
-	Type    ExternalApprovalType
-	Payload string
-}
-
 // ExternalApprovalPayloadFeishu is the payload for feishu type ExternalApproval.
 type ExternalApprovalPayloadFeishu struct {
 	StageID    int
@@ -38,31 +16,6 @@ type ExternalApprovalPayloadFeishu struct {
 	RequesterID  string
 	// Rejected tells if the approval has been rejected on Feishu.
 	Rejected bool
-}
-
-// ExternalApprovalCreate is the API message for creating an ExternalApproval.
-type ExternalApprovalCreate struct {
-	// Related fields
-	IssueID     int
-	RequesterID int
-	ApproverID  int
-
-	// Domain specific fields
-	Type    ExternalApprovalType
-	Payload string
-}
-
-// ExternalApprovalFind is the API message for finding ExternalApprovals.
-type ExternalApprovalFind struct {
-	IssueID *int
-}
-
-// ExternalApprovalPatch is the API message for patching an ExternalApproval.
-type ExternalApprovalPatch struct {
-	ID        int
-	RowStatus RowStatus
-
-	Payload *string
 }
 
 // ExternalApprovalEventActionType is the type of the action which the user took.
