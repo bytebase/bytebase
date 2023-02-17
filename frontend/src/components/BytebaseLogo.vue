5bdf393c3f46ab8908291d6f646f9822150650a7
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-02-17 10:08:50 +0800

    chore: move logo position (#4808)
    Former-commit-id: dcfc6541c3ca959fdb0c3bcc232beb77eca5cc14

---
+++ frontend/src/components/BytebaseLogo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex-shrink-0 w-44">
+    <router-link
+      to="/"
+      class="select-none"
+      active-class=""
+      exact-active-class=""
+    >
+      <img class="h-12 w-auto" src="../assets/logo-full.svg" alt="Bytebase" />
+    </router-link>
+  </div>
+</template>
