e816df5a23e4ea93a1eb82b7fbd68a343cc86051
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-02-21 01:00:43 +0800

    refactor: idp provider (#4834)
    * refactor: idp provider
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 50144e2f9b7c663bd451fa21e240d56120fe4ff8

--- frontend/src/types/proto/v1/auth_service.ts
+++ frontend/src/types/proto/v1/auth_service.ts
@@ -211,7 +211,6 @@ export interface User {
   email: string;
   title: string;
   password: string;
-  identityProvider: string;
   userType: UserType;
   /** The user role will not be respected in the create user request, because the role is controlled by workspace owner. */
   userRole: UserRole;
@@ -920,7 +919,7 @@ export const LogoutRequest = {
 };
 
 function createBaseUser(): User {
-  return { name: "", state: 0, email: "", title: "", password: "", identityProvider: "", userType: 0, userRole: 0 };
+  return { name: "", state: 0, email: "", title: "", password: "", userType: 0, userRole: 0 };
 }
 
 export const User = {
@@ -940,14 +939,11 @@ export const User = {
     if (message.password !== "") {
       writer.uint32(42).string(message.password);
     }
-    if (message.identityProvider !== "") {
-      writer.uint32(50).string(message.identityProvider);
-    }
     if (message.userType !== 0) {
-      writer.uint32(56).int32(message.userType);
+      writer.uint32(48).int32(message.userType);
     }
     if (message.userRole !== 0) {
-      writer.uint32(64).int32(message.userRole);
+      writer.uint32(56).int32(message.userRole);
     }
     return writer;
   },
@@ -975,12 +971,9 @@ export const User = {
           message.password = reader.string();
           break;
         case 6:
-          message.identityProvider = reader.string();
-          break;
-        case 7:
           message.userType = reader.int32() as any;
           break;
-        case 8:
+        case 7:
           message.userRole = reader.int32() as any;
           break;
         default:
@@ -998,7 +991,6 @@ export const User = {
       email: isSet(object.email) ? String(object.email) : "",
       title: isSet(object.title) ? String(object.title) : "",
       password: isSet(object.password) ? String(object.password) : "",
-      identityProvider: isSet(object.identityProvider) ? String(object.identityProvider) : "",
       userType: isSet(object.userType) ? userTypeFromJSON(object.userType) : 0,
       userRole: isSet(object.userRole) ? userRoleFromJSON(object.userRole) : 0,
     };
@@ -1011,7 +1003,6 @@ export const User = {
     message.email !== undefined && (obj.email = message.email);
     message.title !== undefined && (obj.title = message.title);
     message.password !== undefined && (obj.password = message.password);
-    message.identityProvider !== undefined && (obj.identityProvider = message.identityProvider);
     message.userType !== undefined && (obj.userType = userTypeToJSON(message.userType));
     message.userRole !== undefined && (obj.userRole = userRoleToJSON(message.userRole));
     return obj;
@@ -1024,7 +1015,6 @@ export const User = {
     message.email = object.email ?? "";
     message.title = object.title ?? "";
     message.password = object.password ?? "";
-    message.identityProvider = object.identityProvider ?? "";
     message.userType = object.userType ?? 0;
     message.userRole = object.userRole ?? 0;
     return message;
