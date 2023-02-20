da6cba343c5f0cbe08f49f66bc24a989b84332a3
boojack <stevenlgtm@gmail.com>
2023-02-20 12:13:07 +0800

    feat: update related sheets's project id when transferring database (#4826)
    * feat: update sheet's project id when transferring database
    
    * chore: update
    
    Former-commit-id: c9df2a5df21c3ec7e258b44b107c8d7b0de68a74
    Former-commit-id: 3fb5a4bc81a6ea10060f1bf25155a434743d411b

--- backend/legacyapi/sheet.go
+++ backend/legacyapi/sheet.go
@@ -110,6 +110,7 @@ type SheetPatch struct {
 	UpdaterID int
 
 	// Related fields
+	ProjectID  *int `jsonapi:"attr,projectId"`
 	DatabaseID *int `jsonapi:"attr,databaseId"`
 
 	// Domain specific fields
