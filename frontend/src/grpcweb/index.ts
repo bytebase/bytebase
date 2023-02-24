aecc2a7f472ed88be8aabf3079e91533b26cd80c
boojack <stevenlgtm@gmail.com>
2023-02-24 11:41:21 +0800

    fix: grpcweb base url in local dev (#4867)
    Former-commit-id: 55a1e332d53d1cf0b570685342cc99a531eb3ee8

--- frontend/src/grpcweb/index.ts
+++ frontend/src/grpcweb/index.ts
@@ -1,6 +1,5 @@
 import { grpc } from "@improbable-eng/grpc-web";
 import { Channel, createChannel, createClientFactory } from "nice-grpc-web";
-import { isDev } from "@/utils";
 import { AuthServiceDefinition } from "@/types/proto/v1/auth_service";
 import { IdentityProviderServiceDefinition } from "@/types/proto/v1/idp_service";
 import { EnvironmentServiceDefinition } from "@/types/proto/v1/environment_service";
@@ -15,10 +14,9 @@ const getChannel = () => {
     return channelCache;
   }
 
-  // In dev mode, the grpc host is `http://localhost:8080`.
-  // In non-dev mode, as the frontend is embedded into server,
-  // the grpc host is equal to the frontend origin location.
-  const address = isDev() ? "http://localhost:8080" : window.location.origin;
+  const address = import.meta.env.BB_GRPC_LOCAL
+    ? "http://localhost:8080"
+    : window.location.origin;
   const channel = createChannel(
     address,
     grpc.CrossBrowserHttpTransport({
