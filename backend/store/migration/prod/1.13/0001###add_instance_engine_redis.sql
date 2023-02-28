34381c73a69936c7c81c7f055caa8838d8a61fec
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-02-28 23:10:57 +0800

    chore: support oracle (#4920)

--- backend/store/migration/prod/1.13/0001###add_instance_engine_redis.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE instance DROP CONSTRAINT instance_engine_check;
-
-ALTER TABLE instance ADD CONSTRAINT instance_engine_check CHECK (engine IN ('MYSQL', 'POSTGRES', 'TIDB', 'CLICKHOUSE', 'SNOWFLAKE', 'SQLITE', 'MONGODB', 'SPANNER', 'REDIS'));
