250df5492bf8122e0f5f65264915f8739a902213
rebelice <yangrebelice@gmail.com>
2023-03-01 09:21:34 +0800

    feat: check DROP operation for SDL task (#4914)
    * feat: check DROP operation for SDL task
    
    * chore: format dumped schema
    
    * chore: fix build
    
    * chore: make linter happy
    
    * chore: make linter happy
    
    * test: skip to test postgres SDL

---
+++ backend/runner/utils/sdl_utils.go
@@ -0,0 +1,55 @@
+// Package utils is the package for runner utils.
+package utils
+
+import (
+	"bytes"
+	"context"
+
+	"github.com/pkg/errors"
+
+	"github.com/bytebase/bytebase/backend/component/dbfactory"
+	"github.com/bytebase/bytebase/backend/plugin/db"
+	"github.com/bytebase/bytebase/backend/plugin/parser"
+	"github.com/bytebase/bytebase/backend/plugin/parser/differ"
+	"github.com/bytebase/bytebase/backend/plugin/parser/transform"
+	"github.com/bytebase/bytebase/backend/store"
+)
+
+// ComputeDatabaseSchemaDiff computes the diff between current database schema
+// and the given schema. It returns an empty string if there is no applicable
+// diff.
+func ComputeDatabaseSchemaDiff(ctx context.Context, instance *store.InstanceMessage, databaseName string, dbFactory *dbfactory.DBFactory, newSchema string) (string, error) {
+	driver, err := dbFactory.GetAdminDatabaseDriver(ctx, instance, databaseName)
+	if err != nil {
+		return "", errors.Wrap(err, "get admin driver")
+	}
+	defer func() {
+		_ = driver.Close(ctx)
+	}()
+
+	var schema bytes.Buffer
+	_, err = driver.Dump(ctx, databaseName, &schema, true /* schemaOnly */)
+	if err != nil {
+		return "", errors.Wrap(err, "dump old schema")
+	}
+
+	var engine parser.EngineType
+	switch instance.Engine {
+	case db.Postgres:
+		engine = parser.Postgres
+	case db.MySQL:
+		engine = parser.MySQL
+	default:
+		return "", errors.Errorf("unsupported database engine %q", instance.Engine)
+	}
+
+	sdlFormat, err := transform.SchemaTransform(engine, schema.String())
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to transform SDL format")
+	}
+	diff, err := differ.SchemaDiff(engine, sdlFormat, newSchema)
+	if err != nil {
+		return "", errors.Wrapf(err, "compute schema diff")
+	}
+	return diff, nil
+}
