96fc9a84b1b67feaf580b73e5260df3f79679d5e
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-03-01 18:38:20 +0800

    feat(frontend): support custom sql review rule description (#4922) (#4948)
    * feat: two-phases editing for sql-review policy
    
    * chore: wording
    
    * chore: reorder imports
    
    * feat: set custom description
    
    * feat: display rule detail dialog
    
    Former-commit-id: a8ec18349539cc58611708344495677763da588a

--- frontend/src/components/SQLReview/components/index.ts
+++ frontend/src/components/SQLReview/components/index.ts
@@ -1,7 +1,13 @@
 import SQLReviewTemplateSelector from "./SQLReviewTemplateSelector.vue";
 import SQLRuleTable from "./SQLRuleTable.vue";
 import SQLRuleFilter from "./SQLRuleFilter.vue";
+import SQLRuleEditDialog from "./SQLRuleEditDialog.vue";
 
-export { SQLReviewTemplateSelector, SQLRuleTable, SQLRuleFilter };
+export {
+  SQLReviewTemplateSelector,
+  SQLRuleTable,
+  SQLRuleFilter,
+  SQLRuleEditDialog,
+};
 export * from "./useSQLRuleFilter";
 export * from "./utils";
