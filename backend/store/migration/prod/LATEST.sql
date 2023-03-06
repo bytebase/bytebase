39383b3792fb4c723ba86af26d9ddddec49287eb
p0ny <p0uy@outlook.com>
2023-03-06 18:05:51 +0800

    fix: order

--- backend/store/migration/prod/LATEST.sql
+++ backend/store/migration/prod/LATEST.sql
@@ -318,59 +318,6 @@ UPDATE
     ON instance FOR EACH ROW
 EXECUTE FUNCTION trigger_update_updated_ts();
 
--- instance change history records the changes an instance and its databases.
-CREATE TABLE instance_change_history (
-    id BIGSERIAL PRIMARY KEY,
-    row_status row_status NOT NULL DEFAULT 'NORMAL',
-    creator_id INTEGER NOT NULL REFERENCES principal (id),
-    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-    updater_id INTEGER NOT NULL REFERENCES principal (id),
-    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-    instance_id INTEGER NOT NULL REFERENCES instance (id),
-    -- NULL means an instance-level change.
-    database_id INTEGER REFERENCES db (id),
-    -- issue_id is nullable because this field is backfilled and may not be present.
-    issue_id INTEGER REFERENCES issue (id),
-    -- Record the client version creating this migration history. For Bytebase, we use its binary release version. Different Bytebase release might
-    -- record different history info and this field helps to handle such situation properly. Moreover, it helps debugging.
-    release_version TEXT NOT NULL,
-    -- Used to detect out of order migration together with 'namespace' and 'version' column.
-    sequence BIGINT NOT NULL CONSTRAINT instance_change_history_sequence_check CHECK (sequence >= 0),
-    -- We call it source because maybe we could load history from other migration tool.
-    -- Currently allowed values are UI, VCS, LIBRARY.
-    source TEXT NOT NULL CONSTRAINT instance_change_history_source_check CHECK (source IN ('UI', 'VCS', 'LIBRARY')),
-    -- Currently allowed values are BASELINE, MIGRATE, MIGRATE_SDL, BRANCH, DATA.
-    type TEXT NOT NULL CONSTRAINT instance_change_history_type_check CHECK (type IN ('BASELINE', 'MIGRATE', 'MIGRATE_SDL', 'BRANCH', 'DATA')),
-    -- Currently allowed values are PENDING, DONE, FAILED.
-    -- PostgreSQL can't do cross database transaction, so we can't record DDL and migration_history into a single transaction.
-    -- Thus, we create a "PENDING" record before applying the DDL and update that record to "DONE" after applying the DDL.
-    status TEXT NOT NULL CONSTRAINT instance_change_history_status_check CHECK (status IN ('PENDING', 'DONE', 'FAILED')),
-    -- Record the migration version.
-    version TEXT NOT NULL,
-    description TEXT NOT NULL,
-    -- Record the migration statement
-    statement TEXT NOT NULL,
-    -- Record the schema after migration
-    schema TEXT NOT NULL,
-    -- Record the schema before migration. Though we could also fetch it from the previous migration history, it would complicate fetching logic.
-    -- Besides, by storing the schema_prev, we can perform consistency check to see if the migration history has any gaps.
-    schema_prev TEXT NOT NULL,
-    execution_duration_ns BIGINT NOT NULL,
-    payload JSONB NOT NULL DEFAULT '{}'
-);
-
-CREATE UNIQUE INDEX idx_unique_instance_change_history_instance_id_database_id_sequence ON instance_change_history (instance_id, database_id, sequence);
-
-CREATE UNIQUE INDEX idx_unique_instance_change_history_instance_id_database_id_version ON instance_change_history (instance_id, database_id, version);
-
-ALTER SEQUENCE instance_change_history_id_seq RESTART WITH 101;
-
-CREATE TRIGGER update_instance_change_history_updated_ts
-BEFORE
-UPDATE
-    ON instance_change_history FOR EACH ROW
-EXECUTE FUNCTION trigger_update_updated_ts();
-
 -- Instance user stores the users for a particular instance
 CREATE TABLE instance_user (
     id SERIAL PRIMARY KEY,
@@ -448,6 +395,59 @@ CREATE TRIGGER update_db_schema_updated_ts
 BEFORE
 UPDATE
     ON db_schema FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
+
+-- instance change history records the changes an instance and its databases.
+CREATE TABLE instance_change_history (
+    id BIGSERIAL PRIMARY KEY,
+    row_status row_status NOT NULL DEFAULT 'NORMAL',
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    updater_id INTEGER NOT NULL REFERENCES principal (id),
+    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    instance_id INTEGER NOT NULL REFERENCES instance (id),
+    -- NULL means an instance-level change.
+    database_id INTEGER REFERENCES db (id),
+    -- issue_id is nullable because this field is backfilled and may not be present.
+    issue_id INTEGER REFERENCES issue (id),
+    -- Record the client version creating this migration history. For Bytebase, we use its binary release version. Different Bytebase release might
+    -- record different history info and this field helps to handle such situation properly. Moreover, it helps debugging.
+    release_version TEXT NOT NULL,
+    -- Used to detect out of order migration together with 'namespace' and 'version' column.
+    sequence BIGINT NOT NULL CONSTRAINT instance_change_history_sequence_check CHECK (sequence >= 0),
+    -- We call it source because maybe we could load history from other migration tool.
+    -- Currently allowed values are UI, VCS, LIBRARY.
+    source TEXT NOT NULL CONSTRAINT instance_change_history_source_check CHECK (source IN ('UI', 'VCS', 'LIBRARY')),
+    -- Currently allowed values are BASELINE, MIGRATE, MIGRATE_SDL, BRANCH, DATA.
+    type TEXT NOT NULL CONSTRAINT instance_change_history_type_check CHECK (type IN ('BASELINE', 'MIGRATE', 'MIGRATE_SDL', 'BRANCH', 'DATA')),
+    -- Currently allowed values are PENDING, DONE, FAILED.
+    -- PostgreSQL can't do cross database transaction, so we can't record DDL and migration_history into a single transaction.
+    -- Thus, we create a "PENDING" record before applying the DDL and update that record to "DONE" after applying the DDL.
+    status TEXT NOT NULL CONSTRAINT instance_change_history_status_check CHECK (status IN ('PENDING', 'DONE', 'FAILED')),
+    -- Record the migration version.
+    version TEXT NOT NULL,
+    description TEXT NOT NULL,
+    -- Record the migration statement
+    statement TEXT NOT NULL,
+    -- Record the schema after migration
+    schema TEXT NOT NULL,
+    -- Record the schema before migration. Though we could also fetch it from the previous migration history, it would complicate fetching logic.
+    -- Besides, by storing the schema_prev, we can perform consistency check to see if the migration history has any gaps.
+    schema_prev TEXT NOT NULL,
+    execution_duration_ns BIGINT NOT NULL,
+    payload JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE UNIQUE INDEX idx_unique_instance_change_history_instance_id_database_id_sequence ON instance_change_history (instance_id, database_id, sequence);
+
+CREATE UNIQUE INDEX idx_unique_instance_change_history_instance_id_database_id_version ON instance_change_history (instance_id, database_id, version);
+
+ALTER SEQUENCE instance_change_history_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_instance_change_history_updated_ts
+BEFORE
+UPDATE
+    ON instance_change_history FOR EACH ROW
 EXECUTE FUNCTION trigger_update_updated_ts();
 
 -- data_source table stores the data source for a particular database
