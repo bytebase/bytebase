89af7602d1e3b2cd3e88a87b94a0353d8ca1bd3d
boojack <stevenlgtm@gmail.com>
2023-03-07 17:53:34 +0800

    feat: add skeleton for 2FA setup page (#5002)

--- frontend/src/components/ProfileDropdown.vue
+++ frontend/src/components/ProfileDropdown.vue
@@ -6,15 +6,15 @@
       class="flex text-sm text-white focus:outline-none focus:shadow-solid"
       aria-label="User menu"
       aria-haspopup="true"
-      @click.prevent="$refs.menu.toggle($event)"
-      @contextmenu.capture.prevent="$refs.menu.toggle($event)"
+      @click.prevent="menu.toggle()"
+      @contextmenu.capture.prevent="menu.toggle()"
     >
       <PrincipalAvatar :principal="currentUser" />
     </button>
     <BBContextMenu ref="menu" class="origin-top-left mt-2 w-48">
       <router-link
         class="px-4 py-3 menu-item"
-        :to="`/u/${currentUser.id}`"
+        to="/setting/profile"
         role="menuitem"
       >
         <p class="text-sm flex justify-between">
@@ -45,14 +45,14 @@
         }}</router-link>
         <div
           class="menu-item relative"
-          @mouseenter="$refs.languageMenu.toggle($event)"
-          @mouseleave="$refs.languageMenu.toggle($event)"
-          @click.capture.prevent="$refs.languageMenu.toggle($event)"
+          @mouseenter="languageMenu.toggle()"
+          @mouseleave="languageMenu.toggle()"
+          @click.capture.prevent="languageMenu.toggle()"
         >
           <span>{{ $t("common.language") }}</span>
           <BBContextMenu
             ref="languageMenu"
-            class="origin-left absolute left-0 top-0 -translate-x-48 transform"
+            class="origin-left absolute left-0 -top-1 -translate-x-48 transform"
           >
             <div
               class="menu-item px-3 py-1 hover:bg-gray-100"
@@ -94,7 +94,7 @@
       <div class="border-t border-gray-100"></div>
       <div v-if="allowToggleDebug" class="py-1 menu-item">
         <div class="flex flex-row items-center space-x-2 justify-between">
-          <span> Debug </span>
+          <span>Debug</span>
           <BBSwitch :value="isDebug" @toggle="switchDebug" />
         </div>
       </div>
@@ -108,8 +108,8 @@
   </div>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, ref } from "vue";
+<script lang="ts" setup>
+import { computed, ref } from "vue";
 import { useRouter } from "vue-router";
 import { storeToRefs } from "pinia";
 import { ServerInfo } from "@/types";
@@ -125,95 +125,76 @@ import {
 } from "@/store";
 import PrincipalAvatar from "./PrincipalAvatar.vue";
 
-export default defineComponent({
-  name: "ProfileDropdown",
-  components: { PrincipalAvatar },
-  setup() {
-    const actuatorStore = useActuatorStore();
-    const authStore = useAuthStore();
-    const debugStore = useDebugStore();
-    const uiStateStore = useUIStateStore();
-    const router = useRouter();
-    const { setLocale, locale } = useLanguage();
-    const languageMenu = ref();
-    const currentUser = useCurrentUser();
+const actuatorStore = useActuatorStore();
+const authStore = useAuthStore();
+const debugStore = useDebugStore();
+const uiStateStore = useUIStateStore();
+const router = useRouter();
+const { setLocale, locale } = useLanguage();
+const menu = ref();
+const languageMenu = ref();
+const currentUser = useCurrentUser();
 
-    // For now, debug mode is a global setting and will affect all users.
-    // So we only allow DBA and Owner to toggle it.
-    const allowToggleDebug = computed(() => {
-      return hasWorkspacePermission(
-        "bb.permission.workspace.debug",
-        currentUser.value.role
-      );
-    });
-
-    const showQuickstart = computed(() => !actuatorStore.isDemo);
-
-    const logout = () => {
-      authStore.logout().then(() => {
-        router.push({ name: "auth.signin" });
-      });
-    };
-
-    const resetQuickstart = () => {
-      const keys = [
-        "issue.visit",
-        "project.visit",
-        "environment.visit",
-        "instance.visit",
-        "database.visit",
-        "member.addOrInvite",
-        "hidden",
-        "guide.project",
-        "guide.environment",
-        "guide.instance",
-        "guide.database",
-        "kbar.open",
-      ];
-      keys.forEach((key) => {
-        uiStateStore.saveIntroStateByKey({
-          key,
-          newState: false,
-        });
-      });
-    };
-
-    const { isDebug } = storeToRefs(debugStore);
-
-    const switchDebug = () => {
-      debugStore.patchDebug({
-        isDebug: !isDebug.value,
-      });
-    };
-
-    const ping = () => {
-      actuatorStore.fetchServerInfo().then((info: ServerInfo) => {
-        pushNotification({
-          module: "bytebase",
-          style: "SUCCESS",
-          title: JSON.stringify(info, null, 4),
-        });
-      });
-    };
-
-    const toggleLocale = (lang: string) => {
-      setLocale(lang);
-      languageMenu.value.toggle();
-    };
-
-    return {
-      currentUser,
-      allowToggleDebug,
-      showQuickstart,
-      resetQuickstart,
-      logout,
-      isDebug,
-      switchDebug,
-      ping,
-      toggleLocale,
-      languageMenu,
-      locale,
-    };
-  },
+// For now, debug mode is a global setting and will affect all users.
+// So we only allow DBA and Owner to toggle it.
+const allowToggleDebug = computed(() => {
+  return hasWorkspacePermission(
+    "bb.permission.workspace.debug",
+    currentUser.value.role
+  );
 });
+
+const showQuickstart = computed(() => !actuatorStore.isDemo);
+
+const logout = () => {
+  authStore.logout().then(() => {
+    router.push({ name: "auth.signin" });
+  });
+};
+
+const resetQuickstart = () => {
+  const keys = [
+    "issue.visit",
+    "project.visit",
+    "environment.visit",
+    "instance.visit",
+    "database.visit",
+    "member.addOrInvite",
+    "hidden",
+    "guide.project",
+    "guide.environment",
+    "guide.instance",
+    "guide.database",
+    "kbar.open",
+  ];
+  keys.forEach((key) => {
+    uiStateStore.saveIntroStateByKey({
+      key,
+      newState: false,
+    });
+  });
+};
+
+const { isDebug } = storeToRefs(debugStore);
+
+const switchDebug = () => {
+  debugStore.patchDebug({
+    isDebug: !isDebug.value,
+  });
+};
+
+const ping = () => {
+  actuatorStore.fetchServerInfo().then((info: ServerInfo) => {
+    pushNotification({
+      module: "bytebase",
+      style: "SUCCESS",
+      title: JSON.stringify(info, null, 4),
+    });
+  });
+};
+
+const toggleLocale = (lang: string) => {
+  setLocale(lang);
+  languageMenu.value.toggle();
+};
 </script>
