f306b14e2d937e5b0dab616517960d3bd641b1bf
boojack <stevenlgtm@gmail.com>
2023-03-07 17:53:34 +0800

    feat: add skeleton for 2FA setup page (#5002)
    Former-commit-id: 89af7602d1e3b2cd3e88a87b94a0353d8ca1bd3d

---
+++ frontend/src/views/TwoFactorSetup.vue
@@ -0,0 +1,31 @@
+<template>
+  <p class="text-sm text-gray-500 mb-4">
+    {{ $t("two-factor.description") }}
+    <LearnMoreLink url="doc-link" />
+  </p>
+  <BBStepTab
+    :step-item-list="stepTabList"
+    :allow-next="true"
+    :finish-title="$t('common.confirm-and-add')"
+  >
+    <template #0>
+      <!-- placeholder -->
+    </template>
+    <template #1>
+      <!-- placeholder -->
+    </template>
+  </BBStepTab>
+</template>
+
+<script lang="ts" setup>
+import { useI18n } from "vue-i18n";
+import { BBStepTabItem } from "@/bbkit";
+import LearnMoreLink from "@/components/LearnMoreLink.vue";
+
+const { t } = useI18n();
+
+const stepTabList: BBStepTabItem[] = [
+  { title: t("two-factor.setup-steps.setup-auth-app") },
+  { title: t("two-factor.setup-steps.download-recovery-codes") },
+];
+</script>
