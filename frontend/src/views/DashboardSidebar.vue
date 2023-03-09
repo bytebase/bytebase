42f6ed58b9a9616669fa7f19fcf18dfc27b95e05
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-03-09 10:05:54 +0800

    chore(frontend): adjust the scrollable area of sidebar (#5019)
    * chore: adjust the scrollable part of sidebar
    
    * Update frontend/src/layouts/BodyLayout.vue
    
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    
    * Update frontend/src/layouts/BodyLayout.vue
    
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    
    ---------
    
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    Former-commit-id: 4e8783e401b74f90c9db20e5fab372fddfeca862
    Former-commit-id: 8e0e5b2db1e2329733ee17d597af422275eb80b7

--- frontend/src/views/DashboardSidebar.vue
+++ frontend/src/views/DashboardSidebar.vue
@@ -1,56 +1,60 @@
 <template>
   <!-- Navigation -->
-  <nav class="px-2">
-    <BytebaseLogo class="w-full px-1 mb-3" />
+  <nav class="overflow-y-hidden flex flex-col">
+    <BytebaseLogo class="w-full px-4 pb-2 shrink-0" />
 
-    <button
-      class="mb-2 w-full flex items-center justify-between rounded-md border border-control-border bg-white hover:bg-control-bg-hover pl-2 pr-1 py-0.5 outline-none"
-      @click="onClickSearchButton"
-    >
-      <span class="text-control-placeholder">{{ $t("common.search") }}</span>
-      <span class="flex items-center space-x-1">
-        <kbd
-          class="h-5 flex items-center justify-center bg-black bg-opacity-10 rounded text-sm px-1 text-control overflow-y-hidden"
-        >
-          <span v-if="isMac" class="text-xl px-0.5">⌘</span>
-          <span v-else class="tracking-tighter transform scale-x-90">Ctrl</span>
-          <span class="ml-1 mr-0.5">K</span>
-        </kbd>
-      </span>
-    </button>
-
-    <router-link to="/" class="outline-item group flex items-center">
-      <div
-        class="outline-item group flex items-center px-2 py-2"
-        data-label="bb-dashboard-sidebar-home-button"
+    <div class="flex-1 overflow-y-auto px-2 pt-1">
+      <button
+        class="mb-2 w-full flex items-center justify-between rounded-md border border-control-border bg-white hover:bg-control-bg-hover pl-2 pr-1 py-0.5 outline-none"
+        @click="onClickSearchButton"
       >
-        <heroicons-outline:home class="w-5 h-5 mr-2" />
-        {{ $t("common.home") }}
+        <span class="text-control-placeholder">{{ $t("common.search") }}</span>
+        <span class="flex items-center space-x-1">
+          <kbd
+            class="h-5 flex items-center justify-center bg-black bg-opacity-10 rounded text-sm px-1 text-control overflow-y-hidden"
+          >
+            <span v-if="isMac" class="text-xl px-0.5">⌘</span>
+            <span v-else class="tracking-tighter transform scale-x-90"
+              >Ctrl</span
+            >
+            <span class="ml-1 mr-0.5">K</span>
+          </kbd>
+        </span>
+      </button>
+
+      <router-link to="/" class="outline-item group flex items-center">
+        <div
+          class="outline-item group flex items-center px-2 py-2"
+          data-label="bb-dashboard-sidebar-home-button"
+        >
+          <heroicons-outline:home class="w-5 h-5 mr-2" />
+          {{ $t("common.home") }}
+        </div>
+      </router-link>
+      <a
+        href="/sql-editor"
+        target="_blank"
+        class="outline-item group flex items-center px-2 py-2"
+      >
+        <heroicons-solid:terminal class="w-5 h-5 mr-2" />
+        {{ $t("sql-editor.self") }}
+      </a>
+      <router-link
+        to="/anomaly-center"
+        class="outline-item group flex items-center px-2 py-2"
+      >
+        <heroicons-outline:shield-exclamation class="w-5 h-5 mr-2" />
+        {{ $t("anomaly-center") }}
+      </router-link>
+      <div>
+        <BookmarkListSidePanel />
       </div>
-    </router-link>
-    <a
-      href="/sql-editor"
-      target="_blank"
-      class="outline-item group flex items-center px-2 py-2"
-    >
-      <heroicons-solid:terminal class="w-5 h-5 mr-2" />
-      {{ $t("sql-editor.self") }}
-    </a>
-    <router-link
-      to="/anomaly-center"
-      class="outline-item group flex items-center px-2 py-2"
-    >
-      <heroicons-outline:shield-exclamation class="w-5 h-5 mr-2" />
-      {{ $t("anomaly-center") }}
-    </router-link>
-    <div>
-      <BookmarkListSidePanel />
-    </div>
-    <div class="mt-1">
-      <ProjectListSidePanel />
-    </div>
-    <div class="mt-1">
-      <DatabaseListSidePanel />
+      <div class="mt-1">
+        <ProjectListSidePanel />
+      </div>
+      <div class="mt-1">
+        <DatabaseListSidePanel />
+      </div>
     </div>
   </nav>
 </template>
