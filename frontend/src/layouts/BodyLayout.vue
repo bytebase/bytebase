9f34af94b2045d581e6f14f3a1b94f296aa7c2d7
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-03-09 10:03:59 +0800

    Update frontend/src/layouts/BodyLayout.vue
    Co-authored-by: boojack <stevenlgtm@gmail.com>

--- frontend/src/layouts/BodyLayout.vue
+++ frontend/src/layouts/BodyLayout.vue
@@ -93,7 +93,7 @@
         </div>
         <router-link
           to="/archive"
-          class="outline-item group flex items-center px-4 pt-2 pb-2"
+          class="outline-item group flex items-center px-4 py-2"
         >
           <heroicons-outline:archive class="w-5 h-5 mr-2" />
           {{ $t("common.archive") }}
