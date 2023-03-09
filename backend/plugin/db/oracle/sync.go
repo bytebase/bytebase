57de61885f0ad0f2d34a6e383d5b53b02aec4cc9
d-bytebase <d@bytebase.com>
2023-03-09 22:54:26 +0800

    chore: update

--- backend/plugin/db/oracle/sync.go
+++ backend/plugin/db/oracle/sync.go
@@ -10,11 +10,13 @@ import (
 // SyncInstance syncs the instance.
 func (*Driver) SyncInstance(_ context.Context) (*db.InstanceMetadata, error) {
 	// TODO(d): implement it.
-	return nil, nil
+	return &db.InstanceMetadata{}, nil
 }
 
 // SyncDBSchema syncs a single database schema.
 func (*Driver) SyncDBSchema(_ context.Context, _ string) (*storepb.DatabaseMetadata, error) {
 	// TODO(d): implement it.
-	return nil, nil
+	return &storepb.DatabaseMetadata{
+		Name: "orac",
+	}, nil
 }
