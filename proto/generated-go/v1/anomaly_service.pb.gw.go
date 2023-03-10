48498c2cf08348cc9648c59404f649c6093075a7
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-03-10 11:38:22 +0800

    chore: upgrade proto version (#5048)
    Former-commit-id: 7e95b2983f491c74be8a011b257efbd42253fdd7

--- proto/generated-go/v1/anomaly_service.pb.gw.go
+++ proto/generated-go/v1/anomaly_service.pb.gw.go
@@ -104,7 +104,7 @@ func RegisterAnomalyServiceHandlerServer(ctx context.Context, mux *runtime.Serve
 // RegisterAnomalyServiceHandlerFromEndpoint is same as RegisterAnomalyServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterAnomalyServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
 	if err != nil {
 		return err
 	}
