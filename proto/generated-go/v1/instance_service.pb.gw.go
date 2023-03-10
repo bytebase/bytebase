d174455d7a94c2e5e715701000d71b378c98b490
d-bytebase <d@bytebase.com>
2023-03-10 11:35:15 +0800

    chore: upgrade proto version

--- proto/generated-go/v1/instance_service.pb.gw.go
+++ proto/generated-go/v1/instance_service.pb.gw.go
@@ -900,7 +900,7 @@ func RegisterInstanceServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 // RegisterInstanceServiceHandlerFromEndpoint is same as RegisterInstanceServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterInstanceServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
 	if err != nil {
 		return err
 	}
--- proto/generated-go/v1/sql_service.pb.gw.go
+++ proto/generated-go/v1/sql_service.pb.gw.go
@@ -102,7 +102,7 @@ func RegisterSQLServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 // RegisterSQLServiceHandlerFromEndpoint is same as RegisterSQLServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterSQLServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
 	if err != nil {
 		return err
 	}
