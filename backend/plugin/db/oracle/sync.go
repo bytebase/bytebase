0c659d00469a92e245d7bdc3cb392c85d80d03db
d-bytebase <d@bytebase.com>
2023-03-12 20:48:07 +0800

    feat: sync oracle database tables

--- backend/plugin/db/oracle/sync.go
+++ backend/plugin/db/oracle/sync.go
@@ -2,8 +2,13 @@ package oracle
 
 import (
 	"context"
+	"database/sql"
+
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/bytebase/bytebase/backend/plugin/db"
+	"github.com/bytebase/bytebase/backend/plugin/db/util"
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 )
 
@@ -39,9 +44,146 @@ func (driver *Driver) SyncInstance(ctx context.Context) (*db.InstanceMetadata, e
 }
 
 // SyncDBSchema syncs a single database schema.
-func (*Driver) SyncDBSchema(_ context.Context, database string) (*storepb.DatabaseMetadata, error) {
+func (driver *Driver) SyncDBSchema(ctx context.Context, databaseName string) (*storepb.DatabaseMetadata, error) {
+	// TODO(d): filter out system tables.
+	txn, err := driver.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer txn.Rollback()
+
+	schemaNames, err := getSchemas(txn)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get schemas from database %q", databaseName)
+	}
+	tableMap, err := getTables(txn)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get tables from database %q", databaseName)
+	}
+
+	if err := txn.Commit(); err != nil {
+		return nil, err
+	}
+
+	databaseMetadata := &storepb.DatabaseMetadata{
+		Name: databaseName,
+	}
+	for _, schemaName := range schemaNames {
+		databaseMetadata.Schemas = append(databaseMetadata.Schemas, &storepb.SchemaMetadata{
+			Name:   schemaName,
+			Tables: tableMap[schemaName],
+		})
+	}
 	// TODO(d): implement it.
-	return &storepb.DatabaseMetadata{
-		Name: database,
-	}, nil
+	return databaseMetadata, nil
+}
+
+func getSchemas(txn *sql.Tx) ([]string, error) {
+	query := `SELECT username FROM all_users ORDER BY username`
+	rows, err := txn.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var result []string
+	for rows.Next() {
+		var schemaName string
+		if err := rows.Scan(&schemaName); err != nil {
+			return nil, err
+		}
+		result = append(result, schemaName)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// getTables gets all tables of a database.
+func getTables(txn *sql.Tx) (map[string][]*storepb.TableMetadata, error) {
+	columnMap, err := getTableColumns(txn)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get table columns")
+	}
+	// TODO(d): index and foreign keys.
+
+	tableMap := make(map[string][]*storepb.TableMetadata)
+	query := `
+		SELECT OWNER, TABLE_NAME, NUM_ROWS
+		FROM all_tables
+		ORDER BY OWNER, TABLE_NAME`
+	rows, err := txn.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		table := &storepb.TableMetadata{}
+		var schemaName string
+		if err := rows.Scan(&schemaName, &table.Name, &table.RowCount); err != nil {
+			return nil, err
+		}
+		key := db.TableKey{Schema: schemaName, Table: table.Name}
+		table.Columns = columnMap[key]
+		// table.Indexes = indexMap[key]
+		// table.ForeignKeys = foreignKeysMap[key]
+
+		tableMap[schemaName] = append(tableMap[schemaName], table)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return tableMap, nil
+}
+
+// getTableColumns gets the columns of a table.
+func getTableColumns(txn *sql.Tx) (map[db.TableKey][]*storepb.ColumnMetadata, error) {
+	columnsMap := make(map[db.TableKey][]*storepb.ColumnMetadata)
+
+	query := `
+		SELECT
+			OWNER,
+			TABLE_NAME,
+			COLUMN_NAME,
+			DATA_TYPE,
+			COLUMN_ID,
+			DATA_DEFAULT,
+			NULLABLE,
+			COLLATION
+		FROM sys.all_tab_columns
+		ORDER BY OWNER, TABLE_NAME, COLUMN_ID`
+	rows, err := txn.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		column := &storepb.ColumnMetadata{}
+		var schemaName, tableName, nullable string
+		var defaultStr, collation sql.NullString
+		if err := rows.Scan(&schemaName, &tableName, &column.Name, &column.Type, &column.Position, &defaultStr, &nullable, &collation); err != nil {
+			return nil, err
+		}
+		if defaultStr.Valid {
+			column.Default = &wrapperspb.StringValue{Value: defaultStr.String}
+		}
+		isNullBool, err := util.ConvertYesNo(nullable)
+		if err != nil {
+			return nil, err
+		}
+		column.Nullable = isNullBool
+		column.Collation = collation.String
+
+		key := db.TableKey{Schema: schemaName, Table: tableName}
+		columnsMap[key] = append(columnsMap[key], column)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return columnsMap, nil
 }
