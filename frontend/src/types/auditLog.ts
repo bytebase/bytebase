5309c21931b5fcf84421925f1b35385e5f04d158
h3n4l <oysterdays@gmail.com>
2023-03-16 19:25:54 +0800

    chore(audit-log): add update project member role type (#5132)
    Former-commit-id: 898ba4be799e9f08db14978bb391c450de7f1925
    Former-commit-id: 215f8855b7cfacba9f7404d1b15aaa8ead5209a8

--- frontend/src/types/auditLog.ts
+++ frontend/src/types/auditLog.ts
@@ -1,12 +1,17 @@
 export enum AuditActivityType {
+  // Workspace level.
+  //
   // Member related
-  MemberCreate = "bb.member.create",
-  MemberRoleUpdate = "bb.member.role.update",
-  MemberActivate = "bb.member.activate",
-  MemberDeactivate = "bb.member.deactivate",
+  WorkspaceMemberCreate = "bb.member.create",
+  WorkspaceMemberRoleUpdate = "bb.member.role.update",
+  WorkspaceMemberActivate = "bb.member.activate",
+  WorkspaceMemberDeactivate = "bb.member.deactivate",
 
+  // Project level.
+  //
   // Project related
   DatabaseTransfer = "bb.project.database.transfer",
+  ProjectMemberRoleUpdate = "bb.project.member.role.update",
 
   // SQL Editor related.
   SQLEditorQuery = "bb.sql-editor.query",
@@ -20,11 +25,18 @@ export enum AuditActivityLevel {
 
 // Mapping from an activity type to a human readable string.
 export const AuditActivityTypeI18nNameMap: Record<AuditActivityType, string> = {
-  [AuditActivityType.MemberCreate]: "audit-log.type.member-create",
-  [AuditActivityType.MemberRoleUpdate]: "audit-log.type.member-role-update",
-  [AuditActivityType.MemberActivate]: "audit-log.type.member-activate",
-  [AuditActivityType.MemberDeactivate]: "audit-log.type.member-deactivate",
-  [AuditActivityType.DatabaseTransfer]: "audit-log.type.database-transfer",
+  [AuditActivityType.WorkspaceMemberCreate]:
+    "audit-log.type.workspace.member-create",
+  [AuditActivityType.WorkspaceMemberRoleUpdate]:
+    "audit-log.type.workspace.member-role-update",
+  [AuditActivityType.WorkspaceMemberActivate]:
+    "audit-log.type.workspace.member-activate",
+  [AuditActivityType.WorkspaceMemberDeactivate]:
+    "audit-log.type.workspace.member-deactivate",
+  [AuditActivityType.DatabaseTransfer]:
+    "audit-log.type.project.database-transfer",
+  [AuditActivityType.ProjectMemberRoleUpdate]:
+    "audit-log.type.project.member-role-update",
   [AuditActivityType.SQLEditorQuery]: "audit-log.type.sql-editor-query",
 };
 
