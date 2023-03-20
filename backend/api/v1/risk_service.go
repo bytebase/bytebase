38914e5769321f9487d9196c5deda29f45c73a92
p0ny <p0uy@outlook.com>
2023-03-20 17:05:20 +0800

    feat: risk service skeleton

---
+++ backend/api/v1/risk_service.go
@@ -0,0 +1,34 @@
+package v1
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/bytebase/bytebase/backend/store"
+	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
+)
+
+// RiskService implements the risk service.
+type RiskService struct {
+	v1pb.UnimplementedRiskServiceServer
+	store *store.Store
+}
+
+// NewRiskService creates a new RiskService.
+func NewRiskService(store *store.Store) *RiskService {
+	return &RiskService{
+		store: store,
+	}
+}
+
+// ListRisks lists risks.
+func (s *RiskService) ListRisks(ctx context.Context, request *v1pb.ListRisksRequest) (*v1pb.ListRisksResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListRisks not implemented")
+}
+
+// UpdateRisk updates a risk.
+func (s *RiskService) UpdateRisk(context.Context, *v1pb.UpdateRiskRequest) (*v1pb.Risk, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateRisk not implemented")
+}
