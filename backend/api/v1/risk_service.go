7f096eece4b346e0fd43b52ca64cc2b41e709092
p0ny <p0uy@outlook.com>
2023-03-22 16:04:04 +0800

    risk service: implement list risks & update risk

--- backend/api/v1/risk_service.go
+++ backend/api/v1/risk_service.go
@@ -2,11 +2,13 @@ package v1
 
 import (
 	"context"
+	"fmt"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 
+	"github.com/bytebase/bytebase/backend/common"
 	"github.com/bytebase/bytebase/backend/store"
 	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
 )
@@ -24,9 +26,29 @@ func NewRiskService(store *store.Store) *RiskService {
 	}
 }
 
+func convertToRisk(risk *store.RiskMessage) *v1pb.Risk {
+	return &v1pb.Risk{
+		Name:       fmt.Sprintf("%s%s", riskPrefix, risk.ID),
+		Uid:        fmt.Sprintf("%s", risk.ID),
+		Source:     convertToSource(risk.Source),
+		Title:      risk.Name,
+		Level:      risk.Level,
+		Expression: risk.Expression,
+		Active:     risk.Active,
+	}
+}
+
 // ListRisks lists risks.
-func (*RiskService) ListRisks(context.Context, *v1pb.ListRisksRequest) (*v1pb.ListRisksResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ListRisks not implemented")
+func (s *RiskService) ListRisks(ctx context.Context, _ *v1pb.ListRisksRequest) (*v1pb.ListRisksResponse, error) {
+	risks, err := s.store.ListRisks(ctx)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	response := &v1pb.ListRisksResponse{}
+	for _, risk := range risks {
+		response.Risks = append(response.Risks, convertToRisk(risk))
+	}
+	return response, nil
 }
 
 // CreateRisk creates a risk.
@@ -35,8 +57,44 @@ func (*RiskService) CreateRisk(context.Context, *v1pb.CreateRiskRequest) (*v1pb.
 }
 
 // UpdateRisk updates a risk.
-func (*RiskService) UpdateRisk(context.Context, *v1pb.UpdateRiskRequest) (*v1pb.Risk, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateRisk not implemented")
+func (s *RiskService) UpdateRisk(ctx context.Context, request *v1pb.UpdateRiskRequest) (*v1pb.Risk, error) {
+	principalID := ctx.Value(common.PrincipalIDContextKey).(int)
+	if request.UpdateMask == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "update_mask must be set")
+	}
+	riskID, err := getRiskID(request.Risk.Name)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+	risk, err := s.store.GetRisk(ctx, riskID)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to get risk, error: %v", err)
+	}
+	if risk == nil {
+		return nil, status.Errorf(codes.NotFound, "risk %v not found", request.Risk.Name)
+	}
+	if risk.Deleted {
+		return nil, status.Errorf(codes.InvalidArgument, "risk %v has been deleted", request.Risk.Name)
+	}
+
+	patch := &store.UpdateRiskMessage{}
+	for _, path := range request.UpdateMask.Paths {
+		switch path {
+		case "risk.title":
+			patch.Name = &request.Risk.Title
+		case "risk.active":
+			patch.Active = &request.Risk.Active
+		case "risk.expression":
+			patch.Expression = request.Risk.Expression
+		}
+	}
+
+	risk, err = s.store.UpdateRisk(ctx, patch, riskID, principalID)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+
+	return convertToRisk(risk), nil
 }
 
 // DeleteRisk deletes a risk.
