40df02eddeff73231a1549481036fb5619f5d0d3
p0ny <xz@bytebase.com>
2023-03-23 11:21:06 +0800

    feat(proto): approval (#5101)
    * approval proto
    
    * chore: add comment
    
    * WIP approval service
    
    * fix enum name
    
    * approval service message
    
    * approval template service
    
    * list approvals
    
    * approval template service
    
    * approval template service: list approval templates
    
    * format
    
    * copy v1 approval to store approval proto
    
    * fix body name
    
    * buf generate
    
    * approval service: patch approval node status
    
    * buf format
    
    * buf generate
    
    * mvoe approval flow def to approval template service from approval service
    
    * remove approval step status
    
    * remove get approval
    
    * list approvals parent
    
    * approval message
    
    * approval history
    
    * patch approval node status
    
    * approve approval
    
    * buf format
    
    * buf generate
    
    * store/approval template
    
    * buf generate
    
    * buf generate
    
    * remove show deleted
    
    * buf generate
    
    * remove list approval templates request show deleted
    
    * buf generate
    
    * add output only
    
    * buf generate
    
    * store/approval
    
    * store/approval payload
    
    * buf generate
    
    * remove get create delete update approval template
    
    * set approval templates
    
    * add creator
    
    * buf generate
    
    * store/approval
    
    * remove approval service
    
    * buf generate
    
    * add approval template title & description fields
    
    * buf generate
    
    * setting proto wip
    
    * approval config setting
    
    * format
    
    * buf generate
    
    * comment
    
    * approval template goes to approval
    
    * buf generate
    
    * remove approval template service, use setting service instead
    
    * buf generate
    
    * approval template reposition
    
    * buf generate
    
    * rename principal_id to creator_id
    
    * store/issue issuePayload & issuePayloadApproval
    
    * buf gen
    
    * rename AND OR to ALL ANY
    
    * groupvalue & ANY IN GROUP
    
    * fix tag
    
    * inline approval node status
    
    * repeated approval template
    
    * buf gen
    
    * rename dba to workspace dba
    
    * rename approval history to approver
    
    * inline approver message def
    
    * rename node status to status
    
    * buf format & gen
    
    * remove node uid
    
    * buf gen
    
    * fix plural
    
    * buf gne
    
    Former-commit-id: 8ab2d7bf18489c22f223b7a4e744e2be64adcad1

--- frontend/src/types/proto/store/setting.ts
+++ frontend/src/types/proto/store/setting.ts
@@ -1,5 +1,7 @@
 /* eslint-disable */
 import * as _m0 from "protobufjs/minimal";
+import { ParsedExpr } from "../google/api/expr/v1alpha1/syntax";
+import { ApprovalTemplate } from "./approval";
 
 export const protobufPackage = "bytebase.store";
 
@@ -21,6 +23,15 @@ export interface AgentPluginSetting {
   url: string;
   /** The token for the agent. */
   token: string;
+}
+
+export interface ApprovalConfigSetting {
+  rules: ApprovalConfigSetting_Rule[];
+}
+
+export interface ApprovalConfigSetting_Rule {
+  expression?: ParsedExpr;
+  template?: ApprovalTemplate;
 }
 
 function createBaseWorkspaceProfileSetting(): WorkspaceProfileSetting {
@@ -132,6 +143,123 @@ export const AgentPluginSetting = {
     const message = createBaseAgentPluginSetting();
     message.url = object.url ?? "";
     message.token = object.token ?? "";
+    return message;
+  },
+};
+
+function createBaseApprovalConfigSetting(): ApprovalConfigSetting {
+  return { rules: [] };
+}
+
+export const ApprovalConfigSetting = {
+  encode(message: ApprovalConfigSetting, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    for (const v of message.rules) {
+      ApprovalConfigSetting_Rule.encode(v!, writer.uint32(10).fork()).ldelim();
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): ApprovalConfigSetting {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseApprovalConfigSetting();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.rules.push(ApprovalConfigSetting_Rule.decode(reader, reader.uint32()));
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): ApprovalConfigSetting {
+    return {
+      rules: Array.isArray(object?.rules) ? object.rules.map((e: any) => ApprovalConfigSetting_Rule.fromJSON(e)) : [],
+    };
+  },
+
+  toJSON(message: ApprovalConfigSetting): unknown {
+    const obj: any = {};
+    if (message.rules) {
+      obj.rules = message.rules.map((e) => e ? ApprovalConfigSetting_Rule.toJSON(e) : undefined);
+    } else {
+      obj.rules = [];
+    }
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<ApprovalConfigSetting>): ApprovalConfigSetting {
+    const message = createBaseApprovalConfigSetting();
+    message.rules = object.rules?.map((e) => ApprovalConfigSetting_Rule.fromPartial(e)) || [];
+    return message;
+  },
+};
+
+function createBaseApprovalConfigSetting_Rule(): ApprovalConfigSetting_Rule {
+  return { expression: undefined, template: undefined };
+}
+
+export const ApprovalConfigSetting_Rule = {
+  encode(message: ApprovalConfigSetting_Rule, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.expression !== undefined) {
+      ParsedExpr.encode(message.expression, writer.uint32(10).fork()).ldelim();
+    }
+    if (message.template !== undefined) {
+      ApprovalTemplate.encode(message.template, writer.uint32(18).fork()).ldelim();
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): ApprovalConfigSetting_Rule {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseApprovalConfigSetting_Rule();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.expression = ParsedExpr.decode(reader, reader.uint32());
+          break;
+        case 2:
+          message.template = ApprovalTemplate.decode(reader, reader.uint32());
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): ApprovalConfigSetting_Rule {
+    return {
+      expression: isSet(object.expression) ? ParsedExpr.fromJSON(object.expression) : undefined,
+      template: isSet(object.template) ? ApprovalTemplate.fromJSON(object.template) : undefined,
+    };
+  },
+
+  toJSON(message: ApprovalConfigSetting_Rule): unknown {
+    const obj: any = {};
+    message.expression !== undefined &&
+      (obj.expression = message.expression ? ParsedExpr.toJSON(message.expression) : undefined);
+    message.template !== undefined &&
+      (obj.template = message.template ? ApprovalTemplate.toJSON(message.template) : undefined);
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<ApprovalConfigSetting_Rule>): ApprovalConfigSetting_Rule {
+    const message = createBaseApprovalConfigSetting_Rule();
+    message.expression = (object.expression !== undefined && object.expression !== null)
+      ? ParsedExpr.fromPartial(object.expression)
+      : undefined;
+    message.template = (object.template !== undefined && object.template !== null)
+      ? ApprovalTemplate.fromPartial(object.template)
+      : undefined;
     return message;
   },
 };
