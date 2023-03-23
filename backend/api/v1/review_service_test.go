9607b0036258a1195c9260c026294b12af03f8fa
p0ny <xz@bytebase.com>
2023-03-23 15:21:01 +0800

    feat: impl review service GetReview (#5181)
    Former-commit-id: b24755c2e5924646890775d900f548f9a3878ee6
    Former-commit-id: 9e0158adb262ff6ce470bed0ee2c47ba89deebc7

---
+++ backend/api/v1/review_service_test.go
@@ -0,0 +1,38 @@
+package v1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
+)
+
+func TestConvertToApprovalNode(t *testing.T) {
+	tests := []struct {
+		node *storepb.ApprovalNode
+		want *v1pb.ApprovalNode
+	}{
+		{
+			node: &storepb.ApprovalNode{
+				Type: storepb.ApprovalNode_ANY_IN_GROUP,
+				Payload: &storepb.ApprovalNode_GroupValue_{
+					GroupValue: storepb.ApprovalNode_WORKSPACE_DBA,
+				},
+			},
+			want: &v1pb.ApprovalNode{
+				Type: v1pb.ApprovalNode_ANY_IN_GROUP,
+				Payload: &v1pb.ApprovalNode_GroupValue_{
+					GroupValue: v1pb.ApprovalNode_WORKSPACE_DBA,
+				},
+			},
+		},
+	}
+
+	a := require.New(t)
+	for _, test := range tests {
+		got := convertToApprovalNode(test.node)
+		a.Equal(test.want, got)
+	}
+}
