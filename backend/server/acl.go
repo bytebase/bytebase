2157f84cc932dccc596d9536519d786bfb9da2e7
h3n4l <oysterdays@gmail.com>
2023-03-27 00:06:05 +0800

    Update backend/server/acl.go
    Co-authored-by: Tianzhou (天舟) <t@bytebase.com>

--- backend/server/acl.go
+++ backend/server/acl.go
@@ -197,7 +197,7 @@ func enforceIssueRouteACL(path string, method string, queryParams url.Values, pr
 	}
 
 	if method == "GET" {
-		// For /issue route, users excluding BytebaseBot cannot list other users' issues.
+		// For /issue route, users other than BytebaseBot cannot list other users' issues.
 		if userStr := queryParams.Get("user"); userStr != "" {
 			if principalID == api.SystemBotID {
 				return nil
