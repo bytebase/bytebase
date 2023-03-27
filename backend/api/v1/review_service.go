885de9bc2e5c8f8293c1214a279f0c38ee792780
p0ny <xz@bytebase.com>
2023-03-27 17:44:25 +0800

    fix: typo (#5223)
    Former-commit-id: 7096115f918682afe0a651d13edd0c7a57a03c90

--- backend/api/v1/review_service.go
+++ backend/api/v1/review_service.go
@@ -68,7 +68,7 @@ func (s *ReviewService) ApproveReview(ctx context.Context, request *v1pb.Approve
 	if !payload.Approval.ApprovalFindingDone {
 		return nil, status.Errorf(codes.FailedPrecondition, "approval template finding is not done")
 	}
-	if len(payload.Approval.ApprovalTemplates) != 0 {
+	if len(payload.Approval.ApprovalTemplates) != 1 {
 		return nil, status.Errorf(codes.Internal, "expecting one approval template but got %v", len(payload.Approval.ApprovalTemplates))
 	}
 
