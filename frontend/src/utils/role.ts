833748fe273d6907048aff203c6744233fefb502
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-03-30 16:06:26 +0800

    chore(frontend): allow DBAs to manage risk center and custom approval (#5318)
    * chore: allow DBAs to manage risk center and custom approval
    
    * fix: rollout button

--- frontend/src/utils/role.ts
+++ frontend/src/utils/role.ts
@@ -49,7 +49,7 @@ export const WORKSPACE_PERMISSION_MATRIX: Map<
   ["bb.permission.workspace.manage-sso", [false, false, true]],
   ["bb.permission.workspace.manage-sensitive-data", [false, true, true]],
   ["bb.permission.workspace.manage-access-control", [false, true, true]],
-  ["bb.permission.workspace.manage-custom-approval", [false, false, true]],
+  ["bb.permission.workspace.manage-custom-approval", [false, true, true]],
   ["bb.permission.workspace.manage-subscription", [false, false, true]],
   ["bb.permission.workspace.admin-sql-editor", [false, true, true]],
   ["bb.permission.workspace.audit-log", [false, true, true]],
