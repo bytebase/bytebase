4143f85d37a5b370622cfc259f99619332022cae
h3n4l <oysterdays@gmail.com>
2023-03-30 17:12:29 +0800

    fix(rbac): forbid developer transfer the database to or under the project that he is not a member of (#5319) (#5327)
    * fix(rbac): forbid developer transfer the database to or under the unassigned(default) project
    
    * chore: disallow workspace dev to transfer a database to default project
    
    * Update frontend/src/views/DatabaseDetail.vue
    
    
    
    ---------
    
    Co-authored-by: Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    Former-commit-id: 5ac2ac90e3916e953673a663d9dd53ceeab13c11

--- backend/server/acl_database_test.go
+++ backend/server/acl_database_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 )
 
-func TestEnforceWorkspaceDeveloperProjectRouteACL2(t *testing.T) {
+func TestEnforceWorkspaceDeveloperDatabaseRouteACL(t *testing.T) {
 	type test struct {
 		desc        string
 		path        string
+		body        string
 		method      string
 		principalID int
 		errMsg      string
@@ -18,6 +19,7 @@ func TestEnforceWorkspaceDeveloperProjectRouteACL2(t *testing.T) {
 			desc:        "Retrieve the database which is unassigned to any project",
 			path:        "/database/301",
 			method:      "GET",
+			body:        "",
 			principalID: 201,
 			errMsg:      "user is not a member of project owns this database",
 		},
@@ -25,6 +27,7 @@ func TestEnforceWorkspaceDeveloperProjectRouteACL2(t *testing.T) {
 			desc:        "Retrieve the database which is assigned to a project, but the user is not a member of the project",
 			path:        "/database/302",
 			method:      "GET",
+			body:        "",
 			principalID: 201,
 			errMsg:      "user is not a member of project owns this database",
 		},
@@ -32,6 +35,31 @@ func TestEnforceWorkspaceDeveloperProjectRouteACL2(t *testing.T) {
 			desc:        "Retrieve the database which is assigned to a project, and the user is a member of the project",
 			path:        "/database/303",
 			method:      "GET",
+			body:        "",
+			principalID: 201,
+			errMsg:      "",
+		},
+		{
+			desc:        "Transfer the database to the project which the user is not a member of",
+			path:        "/database/303",
+			body:        `{"data":{"type":"databasePatch","attributes":{"projectId":101}}}`,
+			method:      "PATCH",
+			principalID: 201,
+			errMsg:      "user is not a member of project 101",
+		},
+		{
+			desc:        "Transfer out the database under the project which the user is not a member of",
+			path:        "/database/302",
+			body:        `{"data":{"type":"databasePatch","attributes":{"projectId":102}}}`,
+			method:      "PATCH",
+			principalID: 201,
+			errMsg:      "user is not a member of project owns the database 302",
+		},
+		{
+			desc:        "Transfer out the database under the project which the user is not a member of",
+			path:        "/database/304",
+			body:        `{"data":{"type":"databasePatch","attributes":{"projectId":103}}}`,
+			method:      "PATCH",
 			principalID: 201,
 			errMsg:      "",
 		},
@@ -39,7 +67,7 @@ func TestEnforceWorkspaceDeveloperProjectRouteACL2(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.desc, func(t *testing.T) {
-			err := enforceWorkspaceDeveloperDatabaseRouteACL(tc.path, tc.method, tc.principalID, isMemberOfAnyProjectOwnsDatabase)
+			err := enforceWorkspaceDeveloperDatabaseRouteACL(tc.path, tc.method, tc.body, tc.principalID, testWorkspaceDeveloperDatabaseRouteMockGetDatabaseProjectID, testWorkspaceDeveloperDatabaseRouteMockGetProjectMemberIDs)
 			if err != nil {
 				if tc.errMsg == "" {
 					t.Errorf("expect no error, got %s", err.Message)
