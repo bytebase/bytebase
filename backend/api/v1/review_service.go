915d93f2f8fc164d5e0433e3a2d9df5f1c527363
p0ny <p0uy@outlook.com>
2023-03-30 13:56:47 +0800

    convert

--- backend/api/v1/review_service.go
+++ backend/api/v1/review_service.go
@@ -259,6 +259,7 @@ func convertToReview(ctx context.Context, store *store.Store, issue *store.Issue
 	review := &v1pb.Review{}
 	if issuePayload.Approval != nil {
 		review.ApprovalFindingDone = issuePayload.Approval.ApprovalFindingDone
+		review.ApprovalFindingError = issuePayload.Approval.ApprovalFindingError
 		for _, template := range issuePayload.Approval.ApprovalTemplates {
 			review.ApprovalTemplates = append(review.ApprovalTemplates, convertToApprovalTemplate(template))
 		}
