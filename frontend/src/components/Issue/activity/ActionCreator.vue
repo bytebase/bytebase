1ed3a8084677dd4e8fdaa61017efdaa20cc0321c
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-04-03 16:00:52 +0800

    feat: activity part components

---
+++ frontend/src/components/Issue/activity/ActionCreator.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-link
+    v-if="activity.creator.id !== SYSTEM_BOT_ID"
+    :to="`/u/${activity.creator.id}`"
+    class="font-medium text-main whitespace-nowrap hover:underline"
+    exact-active-class=""
+    >{{ activity.creator.name }}</router-link
+  >
+</template>
+
+<script lang="ts" setup>
+import { Activity, SYSTEM_BOT_ID } from "@/types";
+
+defineProps<{
+  activity: Activity;
+}>();
+</script>
