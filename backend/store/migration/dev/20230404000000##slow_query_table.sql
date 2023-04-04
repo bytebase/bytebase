92a591a880ff2fb5aef12be8792f610a670d675d
rebelice <yangrebelice@gmail.com>
2023-04-04 17:18:36 +0800

    chore: update schema

--- backend/store/migration/dev/20230404000000##slow_query_table.sql
+++ backend/store/migration/dev/20230404000000##slow_query_table.sql
@@ -6,13 +6,17 @@ CREATE TABLE slow_query (
     created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
     updater_id INTEGER NOT NULL REFERENCES principal (id),
     updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    -- In MySQL, users can query without specifying a database. In this case, instance_id is used to identify the instance.
     instance_id INTEGER NOT NULL REFERENCES instance (id),
-    database_id INTEGER NOT NULL REFERENCES db (id),
-    log_date INTEGER NOT NULL,
-    sync_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    -- In MySQL, users can query without specifying a database. In this case, database_id is NULL.
+    database_id INTEGER NULL REFERENCES db (id),
+    -- It's hard to store all slow query logs, so the slow query is aggregated by day and database.
+    log_date_ts INTEGER NOT NULL,
+    -- It's hard to store all slow query logs, we sample the slow query log and store the part of them as details.
     slow_query_statistics JSONB NOT NULL DEFAULT '{}'
 );
 
+-- The slow query log is aggregated by day and database and we usually query the slow query log by day and database.
 CREATE UNIQUE INDEX idx_slow_query_database_id_log_date ON slow_query (database_id, log_date);
 
 ALTER SEQUENCE slow_query_id_seq RESTART WITH 101;
