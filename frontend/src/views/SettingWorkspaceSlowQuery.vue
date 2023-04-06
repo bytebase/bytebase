9b2e72e3bed258a96d53b3ffe5feea414ba89835
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-04-06 21:33:37 +0800

    chore: simplify

--- frontend/src/views/SettingWorkspaceSlowQuery.vue
+++ frontend/src/views/SettingWorkspaceSlowQuery.vue
@@ -6,28 +6,23 @@
       :description="$t('subscription.features.bb-feature-slow-query.desc')"
     />
 
-    <SlowQuerySettings @show-feature-modal="state.showFeatureModal = true" />
+    <SlowQuerySettings @show-feature-modal="showFeatureModal = true" />
   </div>
 
   <FeatureModal
-    v-if="state.showFeatureModal"
+    v-if="showFeatureModal"
     feature="bb.feature.slow-query"
-    @cancel="state.showFeatureModal = false"
+    @cancel="showFeatureModal = false"
   />
 </template>
 
 <script lang="ts" setup>
-import { reactive } from "vue";
+import { ref } from "vue";
 
 import { featureToRef } from "@/store";
 import { SlowQuerySettings } from "@/components/SlowQuery";
 
-interface LocalState {
-  showFeatureModal: boolean;
-}
+const showFeatureModal = ref(false);
 
-const state = reactive<LocalState>({
-  showFeatureModal: false,
-});
 const hasSlowQueryFeature = featureToRef("bb.feature.slow-query");
 </script>
