eeaf726b362d3518a9c57d6bd0082977169646c6
d-bytebase <d@bytebase.com>
2023-04-07 15:59:42 +0800

    chore: update
    Former-commit-id: 4b23f4687cb5bae5a81c4dca4c6f928a09709c7d

--- backend/runner/taskrun/schema_update_ghost_cutover_executor.go
+++ backend/runner/taskrun/schema_update_ghost_cutover_executor.go
@@ -126,8 +126,6 @@ func cutover(ctx context.Context, stores *store.Store, dbFactory *dbfactory.DBFa
 		}
 		defer driver.Close(ctx)
 
-		executor := driver.(util.MigrationExecutor)
-
 		var prevSchemaBuf bytes.Buffer
 		if _, err := driver.Dump(ctx, mi.Database, &prevSchemaBuf, true); err != nil {
 			return "", "", err
@@ -167,7 +165,7 @@ func cutover(ctx context.Context, stores *store.Store, dbFactory *dbfactory.DBFa
 		}
 
 		var afterSchemaBuf bytes.Buffer
-		if _, err := executor.Dump(ctx, mi.Database, &afterSchemaBuf, true /* schemaOnly */); err != nil {
+		if _, err := driver.Dump(ctx, mi.Database, &afterSchemaBuf, true /* schemaOnly */); err != nil {
 			return "", "", util.FormatError(err)
 		}
 
