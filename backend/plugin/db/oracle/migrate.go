ede6cd74ffd18a63cd3407312d1c99155f17d2ba
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-04-09 23:45:14 +0800

    chore: cleanup create database parameter (#5452)
    Former-commit-id: 1781652c5ab65b0c1d3c9a9c74640fa6a3ac0bad

--- backend/plugin/db/oracle/migrate.go
+++ backend/plugin/db/oracle/migrate.go
@@ -3,22 +3,15 @@ package oracle
 import (
 	"context"
 	"database/sql"
-	"errors"
-
-	"github.com/bytebase/bytebase/backend/plugin/db"
-	"github.com/bytebase/bytebase/backend/plugin/db/util"
 )
 
 // ExecuteMigrationWithBeforeCommitTxFunc executes the migration, `beforeCommitTxFunc` will be called before transaction commit and after executing `statement`.
 //
 // Callers can use `beforeCommitTx` to do some extra work before transaction commit, like get the transaction id.
 // Any error returned by `beforeCommitTx` will rollback the transaction, so it is the callers' responsibility to return nil if the error occurs in `beforeCommitTx` is not fatal.
-func (d *Driver) ExecuteMigrationWithBeforeCommitTxFunc(ctx context.Context, m *db.MigrationInfo, statement string, beforeCommitTxFunc func(tx *sql.Tx) error) (migrationHistoryID string, updatedSchema string, resErr error) {
-	if m.CreateDatabase {
-		return "", "", errors.New("creating databases is not supported")
-	}
-	if _, err := d.executeWithBeforeCommitTxFunc(ctx, statement, m.CreateDatabase, beforeCommitTxFunc); err != nil {
-		return "", "", util.FormatError(err)
+func (d *Driver) ExecuteMigrationWithBeforeCommitTxFunc(ctx context.Context, statement string, beforeCommitTxFunc func(tx *sql.Tx) error) (migrationHistoryID string, updatedSchema string, resErr error) {
+	if _, err := d.executeWithBeforeCommitTxFunc(ctx, statement, beforeCommitTxFunc); err != nil {
+		return "", "", err
 	}
 	return "", "", nil
 }
