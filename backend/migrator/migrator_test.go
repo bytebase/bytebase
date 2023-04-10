40cfd6d6b0b431cdbdf478c315b4f152e344026f
rebelice <yangrebelice@gmail.com>
2023-04-10 14:55:55 +0800

    feat: create slow_query table for prod (#5463)
    * feat: create slow_query table for prod
    
    * test: fix unit test
    
    Former-commit-id: 3da48e2dacfa69674745aef2249156b10a9efe3a
    Former-commit-id: 000000d291512873597429b0ef0658682bc0a291

--- backend/migrator/migrator_test.go
+++ backend/migrator/migrator_test.go
@@ -241,5 +241,5 @@ func TestMigrationCompatibility(t *testing.T) {
 func TestGetCutoffVersion(t *testing.T) {
 	releaseVersion, err := getProdCutoffVersion()
 	require.NoError(t, err)
-	require.Equal(t, semver.MustParse("1.15.5"), releaseVersion)
+	require.Equal(t, semver.MustParse("1.16.0"), releaseVersion)
 }
