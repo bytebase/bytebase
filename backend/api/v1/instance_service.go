e7fb33dfa3b7293d5331a8925ba5cee3cf56fe58
rebelice <yangrebelice@gmail.com>
2023-04-11 15:48:51 +0800

    feat: implement sync slow query API

--- backend/api/v1/instance_service.go
+++ backend/api/v1/instance_service.go
@@ -11,8 +11,10 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/bytebase/bytebase/backend/common"
+	"github.com/bytebase/bytebase/backend/component/state"
 	enterpriseAPI "github.com/bytebase/bytebase/backend/enterprise/api"
 	api "github.com/bytebase/bytebase/backend/legacyapi"
+	"github.com/bytebase/bytebase/backend/plugin/db"
 	"github.com/bytebase/bytebase/backend/store"
 	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
 )
@@ -23,14 +25,16 @@ type InstanceService struct {
 	store          *store.Store
 	licenseService enterpriseAPI.LicenseService
 	secret         string
+	stateCfg       *state.State
 }
 
 // NewInstanceService creates a new InstanceService.
-func NewInstanceService(store *store.Store, licenseService enterpriseAPI.LicenseService, secret string) *InstanceService {
+func NewInstanceService(store *store.Store, licenseService enterpriseAPI.LicenseService, secret string, stateCfg *state.State) *InstanceService {
 	return &InstanceService{
 		store:          store,
 		licenseService: licenseService,
 		secret:         secret,
+		stateCfg:       stateCfg,
 	}
 }
 
@@ -149,6 +153,31 @@ func (s *InstanceService) UpdateInstance(ctx context.Context, request *v1pb.Upda
 	// TODO(d): sync instance databases.
 
 	return convertToInstance(ins), nil
+}
+
+func (s *InstanceService) SyncSlowQueries(ctx context.Context, request *v1pb.SyncSlowQueriesRequest) (*emptypb.Empty, error) {
+	instance, err := s.getInstanceMessage(ctx, request.Instance)
+	if err != nil {
+		return nil, err
+	}
+	if instance.Deleted {
+		return nil, status.Errorf(codes.InvalidArgument, "instance %q has been deleted", request.Instance)
+	}
+
+	composedInstance, err := s.store.GetInstanceByID(ctx, instance.UID)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+	if composedInstance == nil {
+		return nil, status.Errorf(codes.NotFound, "instance %q not found", request.Instance)
+	}
+
+	if instance.Engine == db.MySQL {
+		// Sync slow queries for instance.
+		s.stateCfg.InstanceSlowQuerySyncChan <- composedInstance
+	}
+
+	return &emptypb.Empty{}, nil
 }
 
 // DeleteInstance deletes an instance.
