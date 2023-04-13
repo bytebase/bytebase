7630b4dca4bc2158b4b6d063e275b5d24e459364
ecmadao <wlec@outlook.com>
2023-04-13 14:14:46 +0800

    fix: refresh expired license (#5546) (#5557)
    Former-commit-id: 9f85c976b4cc923b34081c4f97cf6a7b2c127403

--- backend/server/server.go
+++ backend/server/server.go
@@ -669,7 +669,7 @@ func (s *Server) registerOpenAPIRoutes(e *echo.Echo, ce *casbin.Enforcer, prof c
 
 // initMetricReporter will initial the metric scheduler.
 func (s *Server) initMetricReporter() {
-	enabled := s.profile.Mode == common.ReleaseModeProd && s.profile.DemoName != "" && !s.profile.DisableMetric
+	enabled := s.profile.Mode == common.ReleaseModeProd && s.profile.DemoName == "" && !s.profile.DisableMetric
 	metricReporter := metricreport.NewReporter(s.store, s.licenseService, s.profile, enabled)
 	metricReporter.Register(metric.InstanceCountMetricName, metricCollector.NewInstanceCountCollector(s.store))
 	metricReporter.Register(metric.IssueCountMetricName, metricCollector.NewIssueCountCollector(s.store))
