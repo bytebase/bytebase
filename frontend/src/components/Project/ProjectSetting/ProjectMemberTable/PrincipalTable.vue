7b134ac21f72a5e6a5cf51dfc0e0296800036fdd
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-04-19 11:01:13 +0800

    feat(frontend): apply custom role(s) to project members (#5635)
    * chore: hide archived users by default
    
    * chore: utilities
    
    * chore: support loading state and custom placeholder content
    
    * chore: fetch role list globally
    
    * feat: utilities
    
    * chore: including inactive users by default
    
    * feat: implement project iam policy store
    
    * chore: expose modules
    
    * feat: implement project role selector component
    
    * chore: ready state
    
    * feat: implement new project member settings panel
    
    * chore: retire unused APIs and types
    
    Former-commit-id: 869b2a9085d62c10d106c0d48827761f71363b89
    Former-commit-id: 7b963810ad6b85d444e572b1be348e6d3a236b98

---
+++ frontend/src/components/Project/ProjectSetting/ProjectMemberTable/PrincipalTable.vue
@@ -0,0 +1,336 @@
+<template>
+  <BBGrid
+    :column-list="columnList"
+    :row-clickable="false"
+    :data-source="composedPrincipalList"
+    class="border"
+    row-key="email"
+  >
+    <template #item="{ item }: ComposedPrincipalRow">
+      <div class="bb-grid-cell gap-x-2">
+        <PrincipalAvatar :principal="item.principal" />
+        <div class="flex flex-col">
+          <div class="flex flex-row items-center space-x-2">
+            <router-link :to="`/u/${item.principal.id}`" class="normal-link">{{
+              item.principal.name
+            }}</router-link>
+            <span
+              v-if="currentUser.id == item.principal.id"
+              class="inline-flex items-center px-2 py-0.5 rounded-lg text-xs font-semibold bg-green-100 text-green-800"
+              >{{ $t("common.you") }}</span
+            >
+          </div>
+          <span class="textlabel">{{ item.principal.email }}</span>
+        </div>
+      </div>
+      <div v-if="hasRBACFeature" class="bb-grid-cell flex-wrap gap-x-2 gap-y-1">
+        <NTag
+          v-for="role in item.roleList"
+          :key="role"
+          :closable="allowRemoveRole(role)"
+          @close="removeRole(item, role)"
+        >
+          {{ roleNameText(extractRoleResourceName(role)) }}
+        </NTag>
+        <NPopselect
+          v-if="allowAddRole(item)"
+          :options="getRoleOptions(item)"
+          :scrollable="true"
+          trigger="click"
+          @update:value="(value) => addRole(item, value)"
+        >
+          <NButton quaternary size="tiny">
+            <heroicons-outline:plus />
+          </NButton>
+        </NPopselect>
+      </div>
+      <div class="bb-grid-cell">
+        <NTooltip v-if="allowAdmin" :disabled="allowRemovePrincipal(item)">
+          <template #trigger>
+            <NButton
+              tag="div"
+              text
+              :disabled="!allowRemovePrincipal(item)"
+              @click="removePrincipal(item)"
+            >
+              <heroicons-outline:trash class="w-4 h-4" />
+            </NButton>
+          </template>
+
+          <div>
+            {{ $t("project.settings.members.cannot-remove-last-owner") }}
+          </div>
+        </NTooltip>
+      </div>
+    </template>
+
+    <template #placeholder-content>
+      <div class="p-2">-</div>
+    </template>
+  </BBGrid>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import {
+  NButton,
+  NPopselect,
+  NTag,
+  NTooltip,
+  SelectOption,
+  useDialog,
+} from "naive-ui";
+import { cloneDeep, uniq } from "lodash-es";
+import { useI18n } from "vue-i18n";
+
+import { Principal, Project, ProjectRoleType, unknown } from "@/types";
+import { type BBGridColumn, type BBGridRow, BBGrid } from "@/bbkit";
+import { IamPolicy } from "@/types/proto/v1/project_service";
+import {
+  convertUserToPrincipal,
+  extractUserEmail,
+  featureToRef,
+  useCurrentUser,
+  useProjectIamPolicyStore,
+  useRoleStore,
+  useUserStore,
+} from "@/store";
+import {
+  extractRoleResourceName,
+  hasPermissionInProject,
+  hasWorkspacePermission,
+  roleNameText,
+  addRoleToProjectIamPolicy,
+  removeRoleFromProjectIamPolicy,
+  removeUserFromProjectIamPolicy,
+} from "@/utils";
+
+export type ComposedPrincipal = {
+  email: string;
+  principal: Principal;
+  roleList: ProjectRoleType[];
+};
+export type ComposedPrincipalRow = BBGridRow<ComposedPrincipal>;
+
+const props = defineProps<{
+  project: Project;
+  iamPolicy: IamPolicy;
+}>();
+
+const ROLE_OWNER = "roles/OWNER";
+const { t } = useI18n();
+const hasRBACFeature = featureToRef("bb.feature.rbac");
+const currentUser = useCurrentUser();
+const userStore = useUserStore();
+const roleStore = useRoleStore();
+const projectIamPolicyStore = useProjectIamPolicyStore();
+const dialog = useDialog();
+
+const projectResourceName = computed(
+  () => `projects/${props.project.resourceId}`
+);
+
+const columnList = computed(() => {
+  const ACCOUNT: BBGridColumn = {
+    title: t("settings.members.table.account"),
+    width: hasRBACFeature.value ? "minmax(auto, 18rem)" : "1fr",
+  };
+  const ROLE: BBGridColumn = {
+    title: t("settings.members.table.roles"),
+    width: "1fr",
+  };
+  const OPERATIONS: BBGridColumn = {
+    title: "",
+    width: "6rem",
+  };
+  if (hasRBACFeature.value) {
+    return [ACCOUNT, ROLE, OPERATIONS];
+  }
+  return [ACCOUNT, OPERATIONS];
+});
+
+const composedPrincipalList = computed(() => {
+  const distinctUserResourceNameList = uniq(
+    props.iamPolicy.bindings.flatMap((binding) => binding.members)
+  );
+
+  const userEmailList = distinctUserResourceNameList.map((user) =>
+    extractUserEmail(user)
+  );
+
+  const composedUserList = userEmailList.map((email) => {
+    const user = userStore.getUserByEmail(email);
+    const principal = user
+      ? convertUserToPrincipal(user)
+      : unknown("PRINCIPAL");
+    return { email, user, principal };
+  });
+
+  const usersByRole = props.iamPolicy.bindings.map((binding) => {
+    return {
+      role: binding.role,
+      users: new Set(binding.members),
+    };
+  });
+  const composedPrincipalList = composedUserList.map<ComposedPrincipal>(
+    ({ email, principal }) => {
+      const resourceName = `user:${email}`;
+      const roleList = usersByRole
+        .filter((binding) => binding.users.has(resourceName))
+        .map((binding) => binding.role);
+      return {
+        email,
+        principal,
+        roleList,
+      };
+    }
+  );
+
+  composedPrincipalList.sort((a, b) => {
+    const aIsOwner = a.roleList.includes(ROLE_OWNER);
+    const bIsOwner = b.roleList.includes(ROLE_OWNER);
+    if (aIsOwner && !bIsOwner) return -1;
+    if (!aIsOwner && bIsOwner) return 1;
+    return Number(a.principal.id) - Number(b.principal.id);
+  });
+
+  return composedPrincipalList;
+});
+
+const allowAdmin = computed(() => {
+  if (
+    hasWorkspacePermission(
+      "bb.permission.workspace.manage-project",
+      currentUser.value.role
+    )
+  ) {
+    return true;
+  }
+
+  if (
+    hasPermissionInProject(
+      props.project,
+      currentUser.value,
+      "bb.permission.project.manage-member"
+    )
+  ) {
+    return true;
+  }
+
+  return false;
+});
+
+// To prevent user accidentally removing roles and lock the project permanently, we take following measures:
+// 1. Disallow removing the last OWNER.
+// 2. Allow workspace roles who can manage project. This helps when the project OWNER is no longer available.
+const allowRemoveRole = (role: ProjectRoleType) => {
+  if (props.project.rowStatus == "ARCHIVED") {
+    return false;
+  }
+
+  if (role === ROLE_OWNER) {
+    const binding = props.iamPolicy.bindings.find(
+      (binding) => binding.role === ROLE_OWNER
+    );
+    if (!binding || binding.members.length === 1) {
+      return false;
+    }
+  }
+
+  return allowAdmin.value;
+};
+
+const removeRole = (item: ComposedPrincipal, role: string) => {
+  const title = t("project.settings.members.revoke-role-from-user", {
+    role: roleNameText(extractRoleResourceName(role)),
+    user: item.principal.name,
+  });
+  const d = dialog.error({
+    title,
+    content: t("common.cannot-undo-this-action"),
+    positiveText: t("common.revoke"),
+    negativeText: t("common.cancel"),
+    autoFocus: false,
+    onPositiveClick: async () => {
+      d.loading = true;
+      const user = `user:${item.email}`;
+      const policy = cloneDeep(props.iamPolicy);
+      removeRoleFromProjectIamPolicy(policy, user, role);
+      await projectIamPolicyStore.updateProjectIamPolicy(
+        projectResourceName.value,
+        policy
+      );
+    },
+  });
+};
+
+const allowAddRole = (item: ComposedPrincipal) => {
+  if (!allowAdmin.value) return false;
+  if (props.project.rowStatus == "ARCHIVED") {
+    return false;
+  }
+
+  return item.roleList.length < roleStore.roleList.length;
+};
+
+const getRoleOptions = (item: ComposedPrincipal) => {
+  return roleStore.roleList
+    .filter((role) => !item.roleList.includes(role.name))
+    .map<SelectOption>((role) => {
+      const name = extractRoleResourceName(role.name);
+      return {
+        label: roleNameText(name),
+        value: role.name,
+      };
+    });
+};
+
+const addRole = async (item: ComposedPrincipal, role: string) => {
+  const user = `user:${item.email}`;
+  const policy = cloneDeep(props.iamPolicy);
+  addRoleToProjectIamPolicy(policy, user, role);
+  await projectIamPolicyStore.updateProjectIamPolicy(
+    projectResourceName.value,
+    policy
+  );
+};
+
+const allowRemovePrincipal = (item: ComposedPrincipal) => {
+  if (props.project.rowStatus == "ARCHIVED") {
+    return false;
+  }
+
+  if (item.roleList.includes(ROLE_OWNER)) {
+    const binding = props.iamPolicy.bindings.find(
+      (binding) => binding.role === ROLE_OWNER
+    );
+    if (!binding || binding.members.length === 1) {
+      return false;
+    }
+  }
+
+  return allowAdmin.value;
+};
+
+const removePrincipal = (item: ComposedPrincipal) => {
+  const d = dialog.error({
+    title: t("project.settings.members.remove-user", {
+      user: item.principal.name,
+    }),
+    content: t("common.cannot-undo-this-action"),
+    positiveText: "Remove",
+    negativeText: "Cancel",
+    autoFocus: false,
+    onPositiveClick: async () => {
+      d.loading = true;
+      const user = `user:${item.email}`;
+      const policy = cloneDeep(props.iamPolicy);
+      removeUserFromProjectIamPolicy(policy, user);
+      await projectIamPolicyStore.updateProjectIamPolicy(
+        projectResourceName.value,
+        policy
+      );
+    },
+  });
+};
+</script>
---
+++ frontend/src/utils/projectIamPolicy.ts
@@ -0,0 +1,55 @@
+import { ProjectRoleType } from "@/types";
+import type { IamPolicy } from "@/types/proto/v1/project_service";
+
+const tidyUpPolicy = (policy: IamPolicy) => {
+  policy.bindings = policy.bindings.filter(
+    (binding) => binding.members.length > 0
+  );
+};
+
+export const removeRoleFromProjectIamPolicy = (
+  policy: IamPolicy,
+  user: string,
+  role: ProjectRoleType
+) => {
+  const binding = policy.bindings.find((binding) => binding.role === role);
+  if (binding) {
+    const index = binding.members.indexOf(user);
+    if (index >= 0) {
+      binding.members.splice(index, 1);
+
+      tidyUpPolicy(policy);
+    }
+  }
+};
+
+export const removeUserFromProjectIamPolicy = (
+  policy: IamPolicy,
+  user: string
+) => {
+  policy.bindings.forEach((binding) => {
+    const index = binding.members.indexOf(user);
+    if (index >= 0) {
+      binding.members.splice(index, 1);
+    }
+  });
+  tidyUpPolicy(policy);
+};
+
+export const addRoleToProjectIamPolicy = (
+  policy: IamPolicy,
+  user: string,
+  role: ProjectRoleType
+) => {
+  const binding = policy.bindings.find((binding) => binding.role === role);
+  if (binding) {
+    if (!binding.members.includes(user)) {
+      binding.members.push(user);
+    }
+  } else {
+    policy.bindings.push({
+      role,
+      members: [user],
+    });
+  }
+};
