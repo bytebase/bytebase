8f0db03e204f0fadf93e2806fa676cddb0a445db
p0ny <xz@bytebase.com>
2023-04-19 11:01:28 +0800

    feat: allow user having many roles in a project (#5634)
    * feat: allow user having many roles in a project
    
    * getIAMPolicyDiff: support multiple roles on one user
    
    * project id, principal id, role
    
    * remove unused
    
    * update TestGetIAMPolicyDiff
    
    * fix: comment

--- backend/migrator/migration/prod/LATEST.sql
+++ backend/migrator/migration/prod/LATEST.sql
@@ -277,7 +277,7 @@ CREATE TABLE project_member (
     payload JSONB NOT NULL DEFAULT '{}'
 );
 
-CREATE UNIQUE INDEX idx_project_member_unique_project_id_principal_id ON project_member(project_id, principal_id);
+CREATE UNIQUE INDEX idx_project_member_unique_project_id_principal_id_role ON project_member(project_id, principal_id, role);
 
 ALTER SEQUENCE project_member_id_seq RESTART WITH 101;
 
