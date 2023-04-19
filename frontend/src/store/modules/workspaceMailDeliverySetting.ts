e580a1cd4c5e6ad20a3d09273ee1b15d0e2081dc
h3n4l <oysterdays@gmail.com>
2023-04-19 11:51:38 +0800

    feat(frontend): the form of the mail delivery (#5636)
    Former-commit-id: bcc66c1715db0e2f2b4f90014b533f3b164270f8
    Former-commit-id: 9e4e716c7fe0a23d55dacc016f844ed7fa11d398

---
+++ frontend/src/store/modules/workspaceMailDeliverySetting.ts
@@ -0,0 +1,69 @@
+import { settingServiceClient } from "@/grpcweb";
+import {
+  SMTPMailDeliverySettingValue,
+  Setting,
+} from "@/types/proto/v1/setting_service";
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+const SETTING_NAME = "settings/bb.workspace.mail-delivery";
+
+export const useWorkspaceMailDeliverySettingStore = defineStore(
+  "workspaceMailDeliverySetting",
+  () => {
+    const mailDeliverySetting = ref<SMTPMailDeliverySettingValue>();
+
+    const setMailDeliverySetting = (setting: Setting) => {
+      const _mailDeliverySetting = setting.value!.smtpMailDeliverySettingValue;
+      if (_mailDeliverySetting) {
+        mailDeliverySetting.value = _mailDeliverySetting;
+      }
+    };
+
+    const fetchMailDeliverySetting = async () => {
+      try {
+        const setting = await settingServiceClient.getSetting({
+          name: SETTING_NAME,
+        });
+        setMailDeliverySetting(setting);
+      } catch (ex) {
+        console.error(ex);
+      }
+    };
+
+    const updateMailDeliverySetting = async (
+      value: SMTPMailDeliverySettingValue
+    ) => {
+      const setting = await settingServiceClient.setSetting({
+        setting: {
+          name: SETTING_NAME,
+          value: {
+            smtpMailDeliverySettingValue: value,
+          },
+        },
+      });
+      setMailDeliverySetting(setting);
+    };
+
+    const validateMailDeliverySetting = async (
+      value: SMTPMailDeliverySettingValue
+    ) => {
+      await settingServiceClient.setSetting({
+        setting: {
+          name: SETTING_NAME,
+          value: {
+            smtpMailDeliverySettingValue: value,
+          },
+        },
+        validateOnly: true,
+      });
+    };
+
+    return {
+      mailDeliverySetting,
+      fetchMailDeliverySetting,
+      updateMailDeliverySetting,
+      validateMailDeliverySetting,
+    };
+  }
+);
