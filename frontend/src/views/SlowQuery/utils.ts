b878fcc7d96af481e0d30dcfc7b8e707baed374a
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-04-21 16:42:24 +0800

    feat(frontend): support URL query params for slow query dashboard page (#5693)
    * feat: support url query
    
    * chore: use second level timestamp in query
    
    * chore: split fromTime and toTime
    
    Former-commit-id: b5cdca5c3b0d0074252b0084115be6f8f6c1f49f
    Former-commit-id: fac6674beec7dbcafa83eabe98571c2235f9101a

---
+++ frontend/src/views/SlowQuery/utils.ts
@@ -0,0 +1,90 @@
+import type { LocationQuery } from "vue-router";
+import { UNKNOWN_ID } from "@/types";
+import {
+  SlowQueryFilterParams,
+  defaultSlowQueryFilterParams,
+} from "@/components/SlowQuery";
+import {
+  useDatabaseStore,
+  useEnvironmentStore,
+  useInstanceStore,
+  useProjectStore,
+} from "@/store";
+
+export const wrapQueryFromFilterParams = (params: SlowQueryFilterParams) => {
+  const query: Record<string, any> = {};
+  if (params.project && params.project.id !== UNKNOWN_ID) {
+    query.project = params.project.id;
+  }
+  if (params.environment && params.environment.id !== UNKNOWN_ID) {
+    query.environment = params.environment.id;
+  }
+  if (params.instance && params.instance.id !== UNKNOWN_ID) {
+    query.instance = params.instance.id;
+  }
+  if (params.database && params.database.id !== UNKNOWN_ID) {
+    query.database = params.database.id;
+  }
+  if (params.fromTime) {
+    if (params.fromTime !== defaultSlowQueryFilterParams().fromTime) {
+      query.fromTime = Math.floor(params.fromTime / 1000);
+    }
+  } else {
+    query.fromTime = -1;
+  }
+  if (params.toTime) {
+    if (params.toTime !== defaultSlowQueryFilterParams().toTime) {
+      query.toTime = Math.floor(params.toTime / 1000);
+    }
+  } else {
+    query.toTime = -1;
+  }
+  return query;
+};
+
+export const extractSlowQueryLogFilterFromQuery = async (
+  query: LocationQuery
+) => {
+  const params: SlowQueryFilterParams = defaultSlowQueryFilterParams();
+  if (query.environment) {
+    const id = parseInt(query.environment as string, 10) ?? UNKNOWN_ID;
+    const environment = useEnvironmentStore().getEnvironmentById(id);
+    if (environment && environment.id !== UNKNOWN_ID) {
+      params.environment = environment;
+    }
+  }
+  if (query.project) {
+    const id = parseInt(query.project as string, 10) ?? UNKNOWN_ID;
+    const project = await useProjectStore().getOrFetchProjectById(id);
+    if (project && project.id !== UNKNOWN_ID) {
+      params.project = project;
+    }
+  }
+  if (query.instance) {
+    const id = parseInt(query.instance as string, 10) ?? UNKNOWN_ID;
+    const instance = await useInstanceStore().getOrFetchInstanceById(id);
+    if (instance && instance.id !== UNKNOWN_ID) {
+      params.instance = instance;
+    }
+  }
+  if (query.database) {
+    const id = parseInt(query.database as string, 10) ?? UNKNOWN_ID;
+    const database = await useDatabaseStore().getOrFetchDatabaseById(id);
+    if (database && database.id !== UNKNOWN_ID) {
+      params.database = database;
+    }
+  }
+  if (query.fromTime) {
+    const from = parseInt(query.fromTime as string, 10) * 1000 || undefined;
+    if (from) {
+      params.fromTime = from > 0 ? from : undefined;
+    }
+  }
+  if (query.toTime) {
+    const to = parseInt(query.toTime as string, 10) * 1000 || undefined;
+    if (to) {
+      params.toTime = to > 0 ? to : undefined;
+    }
+  }
+  return params;
+};
