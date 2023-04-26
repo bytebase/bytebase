75208ac2e1baba7bb80977aea63a383405d02c19
h3n4l <oysterdays@gmail.com>
2023-04-26 20:48:52 +0800

    feat(mybatis): implement a simple mybatis xml parser demo (#5814)
    * feat: implement a simple mybatis xml parser demo
    
    * chore: update
    
    * chore: update

---
+++ backend/plugin/parser/mybatis/ast/node.go
@@ -0,0 +1,60 @@
+// Package ast defines the abstract syntax tree of mybatis mapper xml.
+package ast
+
+import (
+	"io"
+)
+
+// Node is the interface implemented by all AST node types.
+type Node interface {
+	// RestoreSQL restores the node to the original SQL statement.
+	RestoreSQL(w io.Writer) error
+}
+
+// TextNode represents a text node.
+type TextNode struct {
+	Text string
+}
+
+// RestoreSQL implements Node interface.
+func (n *TextNode) RestoreSQL(w io.Writer) error {
+	_, err := w.Write([]byte(n.Text))
+	return err
+}
+
+// MapperNode represents a mapper node in mybatis mapper xml begin with <mapper>.
+type MapperNode struct {
+	Namespace  string
+	QueryNodes []*QueryNode
+}
+
+// RestoreSQL implements Node interface.
+func (n *MapperNode) RestoreSQL(w io.Writer) error {
+	for _, node := range n.QueryNodes {
+		if err := node.RestoreSQL(w); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// QueryNode represents a query node.
+type QueryNode struct {
+	// ID is the id of the query node.
+	ID string
+	// Children is the children of the query node.
+	Children []Node
+}
+
+// RestoreSQL implements Node interface.
+func (n *QueryNode) RestoreSQL(w io.Writer) error {
+	for _, node := range n.Children {
+		if err := node.RestoreSQL(w); err != nil {
+			return err
+		}
+	}
+	if _, err := w.Write([]byte(";\n")); err != nil {
+		return err
+	}
+	return nil
+}
