7fd7016992bb552a07d1862b42f234406e2c39b6
h3n4l <oysterdays@gmail.com>
2023-04-28 15:43:04 +0800

    feat(mybatis): support placeholder in mybatis xml (#5849)
    Former-commit-id: 6854af4675f3a4e6e0926d124f399e0fa8cb4901

---
+++ backend/plugin/parser/mybatis/ast/query_node.go
@@ -0,0 +1,68 @@
+// Package ast defines the abstract syntax tree of mybatis mapper xml.
+package ast
+
+import (
+	"encoding/xml"
+	"io"
+)
+
+var (
+	_ Node = (*TextNode)(nil)
+)
+
+// QueryNodeType is the type of the query node.
+type QueryNodeType uint
+
+const (
+	// QueryNodeTypeSelect represents a select query node.
+	QueryNodeTypeSelect QueryNodeType = iota
+	// QueryNodeTypeInsert represents a insert query node.
+	QueryNodeTypeInsert
+	// QueryNodeTypeUpdate represents a update query node.
+	QueryNodeTypeUpdate
+	// QueryNodeTypeDelete represents a delete query node.
+	QueryNodeTypeDelete
+)
+
+// QueryNode represents a query node.
+type QueryNode struct {
+	// ID is the id of the query node.
+	ID string
+	// Type is the type of the query node.
+	Type QueryNodeType
+	// Children is the children of the query node.
+	Children []Node
+}
+
+// RestoreSQL implements Node interface.
+func (n *QueryNode) RestoreSQL(w io.Writer) error {
+	for _, node := range n.Children {
+		if err := node.RestoreSQL(w); err != nil {
+			return err
+		}
+	}
+	if _, err := w.Write([]byte(";\n")); err != nil {
+		return err
+	}
+	return nil
+}
+
+// New creates a new query node.
+func (n *QueryNode) New(startEle *xml.StartElement) {
+	switch startEle.Name.Local {
+	case "select":
+		n.Type = QueryNodeTypeSelect
+	case "insert":
+		n.Type = QueryNodeTypeInsert
+	case "update":
+		n.Type = QueryNodeTypeUpdate
+	case "delete":
+		n.Type = QueryNodeTypeDelete
+	}
+
+	for _, attr := range startEle.Attr {
+		if attr.Name.Local == "id" {
+			n.ID = attr.Value
+		}
+	}
+}
