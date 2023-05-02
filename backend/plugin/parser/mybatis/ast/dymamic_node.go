7d61838c589c17357f313608755701d2374295f5
h3n4l <oysterdays@gmail.com>
2023-05-02 21:09:38 +0800

    feat(mybatis): support `where` `set` `trim` start element in trim dynamic sql (#5869)
    * feat: support where set trim dynamic sql
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: c1e99499ec883495f3f86a5a68ee42bf00fb90a3

--- backend/plugin/parser/mybatis/ast/dymamic_node.go
+++ backend/plugin/parser/mybatis/ast/dymamic_node.go
@@ -4,13 +4,19 @@ package ast
 import (
 	"encoding/xml"
 	"io"
+	"strings"
 )
 
 var (
 	_ Node = (*IfNode)(nil)
+
 	_ Node = (*ChooseNode)(nil)
 	_ Node = (*WhenNode)(nil)
 	_ Node = (*OtherwiseNode)(nil)
+
+	_ Node = (*WhereNode)(nil)
+	_ Node = (*SetNode)(nil)
+	_ Node = (*TrimNode)(nil)
 )
 
 // IfNode represents a if node in mybatis mapper xml likes <if test="condition">...</if>.
@@ -140,4 +146,144 @@ func (n *OtherwiseNode) RestoreSQL(w io.Writer) error {
 // AddChild adds a child to the otherwise node.
 func (n *OtherwiseNode) AddChild(child Node) {
 	n.Children = append(n.Children, child)
+}
+
+// TrimNode represents a trim node in mybatis mapper xml likes <trim prefix="prefix" suffix="suffix" prefixOverrides="prefixOverrides" suffixOverrides="suffixOverrides">...</trim>.
+type TrimNode struct {
+	Prefix               string
+	Suffix               string
+	PrefixOverridesParts []string
+	SuffixOverridesParts []string
+	Children             []Node
+}
+
+// NewTrimNode creates a new trim node.
+func NewTrimNode(startElement *xml.StartElement) *TrimNode {
+	var prefix, suffix, prefixOverrides, suffixOverrides string
+	for _, attr := range startElement.Attr {
+		switch attr.Name.Local {
+		case "prefix":
+			prefix = attr.Value
+		case "suffix":
+			suffix = attr.Value
+		case "prefixOverrides":
+			prefixOverrides = attr.Value
+		case "suffixOverrides":
+			suffixOverrides = attr.Value
+		}
+	}
+	return newTrimNodeWithAttrs(prefix, suffix, prefixOverrides, suffixOverrides)
+}
+
+// newTrimNodeWithAttrs creates a new trim node with given attributes.
+func newTrimNodeWithAttrs(prefix, suffix, prefixOverrides, suffixOverrides string) *TrimNode {
+	prefixOverridesParts := strings.Split(prefixOverrides, "|")
+	suffixOverridesParts := strings.Split(suffixOverrides, "|")
+	return &TrimNode{
+		Prefix:               prefix,
+		Suffix:               suffix,
+		PrefixOverridesParts: prefixOverridesParts,
+		SuffixOverridesParts: suffixOverridesParts,
+	}
+}
+
+// RestoreSQL implements Node interface.
+func (n *TrimNode) RestoreSQL(w io.Writer) error {
+	var stringsBuilder strings.Builder
+	for _, node := range n.Children {
+		if err := node.RestoreSQL(&stringsBuilder); err != nil {
+			return err
+		}
+	}
+	trimmed := strings.TrimSpace(stringsBuilder.String())
+	if len(trimmed) == 0 {
+		return nil
+	}
+	// Replace the prefix and suffix with empty string if matches the part in prefixOverridesParts and suffixOverridesParts.
+	for _, part := range n.PrefixOverridesParts {
+		if strings.HasPrefix(trimmed, part) {
+			trimmed = strings.TrimPrefix(trimmed, part)
+			break
+		}
+	}
+	for _, part := range n.SuffixOverridesParts {
+		if strings.HasSuffix(trimmed, part) {
+			trimmed = strings.TrimSuffix(trimmed, part)
+			break
+		}
+	}
+	if len(n.Prefix) > 0 {
+		if _, err := w.Write([]byte(" ")); err != nil {
+			return err
+		}
+		if _, err := w.Write([]byte(n.Prefix)); err != nil {
+			return err
+		}
+	}
+	if len(trimmed) > 0 {
+		if _, err := w.Write([]byte(" ")); err != nil {
+			return err
+		}
+		if _, err := w.Write([]byte(trimmed)); err != nil {
+			return err
+		}
+	}
+	if len(n.Suffix) > 0 {
+		if _, err := w.Write([]byte(" ")); err != nil {
+			return err
+		}
+		if _, err := w.Write([]byte(n.Suffix)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// AddChild adds a child to the trim node.
+func (n *TrimNode) AddChild(child Node) {
+	n.Children = append(n.Children, child)
+}
+
+// WhereNode represents a where node in mybatis mapper xml likes <where>...</where>.
+type WhereNode struct {
+	trimNode *TrimNode
+}
+
+// NewWhereNode creates a new where node.
+func NewWhereNode(_ *xml.StartElement) *WhereNode {
+	return &WhereNode{
+		trimNode: newTrimNodeWithAttrs("WHERE", "", "AND |OR ", ""),
+	}
+}
+
+// RestoreSQL implements Node interface.
+func (n *WhereNode) RestoreSQL(w io.Writer) error {
+	return n.trimNode.RestoreSQL(w)
+}
+
+// AddChild adds a child to the where node.
+func (n *WhereNode) AddChild(child Node) {
+	n.trimNode.AddChild(child)
+}
+
+// SetNode represents a set node in mybatis mapper xml likes <set>...</set>.
+type SetNode struct {
+	trimNode *TrimNode
+}
+
+// NewSetNode creates a new set node.
+func NewSetNode(_ *xml.StartElement) *SetNode {
+	return &SetNode{
+		trimNode: newTrimNodeWithAttrs("SET", "", "", ","),
+	}
+}
+
+// RestoreSQL implements Node interface.
+func (n *SetNode) RestoreSQL(w io.Writer) error {
+	return n.trimNode.RestoreSQL(w)
+}
+
+// AddChild adds a child to the set node.
+func (n *SetNode) AddChild(child Node) {
+	n.trimNode.AddChild(child)
 }
