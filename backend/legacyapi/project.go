c5935e4ce43437deff9d01b7ce1d027a4ba1a5bf
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-05-08 10:10:53 +0800

    chore: revert PR/5904 (#5910)

--- backend/legacyapi/project.go
+++ backend/legacyapi/project.go
@@ -74,6 +74,9 @@ type Project struct {
 	ResourceID string    `jsonapi:"attr,resourceId"`
 	RowStatus  RowStatus `jsonapi:"attr,rowStatus"`
 
+	// Related fields
+	ProjectMemberList []*ProjectMember `jsonapi:"relation,projectMember"`
+
 	// Domain specific fields
 	Name         string              `jsonapi:"attr,name"`
 	Key          string              `jsonapi:"attr,key"`
