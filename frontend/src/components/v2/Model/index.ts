e5682a60c5fd676d6db30b857e34b9795668182b
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-05-09 20:35:26 +0800

    chore: implement ProjectV1Table

--- frontend/src/components/v2/Model/index.ts
+++ frontend/src/components/v2/Model/index.ts
@@ -2,5 +2,12 @@ import DatabaseName from "./DatabaseName.vue";
 import InstanceName from "./InstanceName.vue";
 import EnvironmentName from "./EnvironmentName.vue";
 import ProjectName from "./ProjectName.vue";
+import ProjectV1Table from "./ProjectV1Table.vue";
 
-export { DatabaseName, InstanceName, EnvironmentName, ProjectName };
+export {
+  DatabaseName,
+  InstanceName,
+  EnvironmentName,
+  ProjectName,
+  ProjectV1Table,
+};
