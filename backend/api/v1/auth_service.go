8f71bc1988b70d0ed845cd900b8bf1fb3251ad3b
zlh <zlhgo@163.com>
2023-05-09 18:15:55 +0800

    typo (#5959)
    Former-commit-id: 5a37978a10f16750818c3d5173ed7df1edf7106c

--- backend/api/v1/auth_service.go
+++ backend/api/v1/auth_service.go
@@ -778,8 +778,8 @@ func (s *AuthService) challengeRecoveryCode(ctx context.Context, user *store.Use
 }
 
 func validateEmail(email string) error {
-	formatedEmail := strings.ToLower(email)
-	if email != formatedEmail {
+	formattedEmail := strings.ToLower(email)
+	if email != formattedEmail {
 		return errors.New("email should be lowercase")
 	}
 	if _, err := mail.ParseAddress(email); err != nil {
