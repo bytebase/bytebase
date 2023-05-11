4a65f4ab520898b2d160b42e83fa35966de9cc73
d-bytebase <d@bytebase.com>
2023-05-12 02:09:31 +0800

    chore: try self-hosted

--- backend/server/issue.go
+++ backend/server/issue.go
@@ -424,7 +424,7 @@ func (s *Server) createIssue(ctx context.Context, issueCreate *api.IssueCreate, 
 	if assignee == nil {
 		return nil, echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("assignee %d not found", issueCreate.AssigneeID))
 	}
-	// TODO(p0ny): remove issueCreate.Payload
+	// TODO(p0ny): remove issueCreate.Payload.
 	issueCreatePayload := &storepb.IssuePayload{
 		Approval: &storepb.IssuePayloadApproval{
 			ApprovalFindingDone: false,
