2eb79ca911afc642dd887eebd12246c1314a9ce4
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-05-12 14:34:47 +0800

    fix(sql-editor): restrict instance/database connection scope to provide different suggestion items (#6033)
    * fix: restrict instance/database connection scope to provide different suggestion items
    
    * fix: lint
    
    * fix: lint

--- frontend/src/plugins/sql-lsp/types/index.ts
+++ frontend/src/plugins/sql-lsp/types/index.ts
@@ -1,2 +1,13 @@
+import { Schema } from "./schema";
+import { SQLDialect } from "./sql";
+
 export * from "./schema";
 export * from "./sql";
+
+export type ConnectionScope = "instance" | "database";
+
+export type LanguageState = {
+  schema: Schema;
+  dialect: SQLDialect;
+  connectionScope: ConnectionScope;
+};
