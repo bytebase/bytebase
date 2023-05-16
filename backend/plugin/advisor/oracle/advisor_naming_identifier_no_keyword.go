3ec220cd850bbcf81afaee6d1e83b989bbeee7af
rebelice <yangrebelice@gmail.com>
2023-05-16 10:47:40 +0800

    feat: implement Oracle identifier is not keyword rule

---
+++ backend/plugin/advisor/oracle/advisor_naming_identifier_no_keyword.go
@@ -0,0 +1,84 @@
+// Package oracle is the advisor for oracle database.
+package oracle
+
+import (
+	"fmt"
+
+	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
+	parser "github.com/bytebase/plsql-parser"
+
+	"github.com/bytebase/bytebase/backend/plugin/advisor"
+	"github.com/bytebase/bytebase/backend/plugin/advisor/db"
+	sql "github.com/bytebase/bytebase/backend/plugin/parser/sql"
+)
+
+var (
+	_ advisor.Advisor = (*NamingIdentifierNoKeywordAdvisor)(nil)
+)
+
+func init() {
+	advisor.Register(db.Oracle, advisor.OracleIdentifierNamingNoKeyword, &NamingIdentifierNoKeywordAdvisor{})
+}
+
+// NamingIdentifierNoKeywordAdvisor is the advisor checking for identifier naming convention without keyword.
+type NamingIdentifierNoKeywordAdvisor struct {
+}
+
+// Check checks for identifier naming convention without keyword.
+func (*NamingIdentifierNoKeywordAdvisor) Check(ctx advisor.Context, statement string) ([]advisor.Advice, error) {
+	tree, errAdvice := parseStatement(statement)
+	if errAdvice != nil {
+		return errAdvice, nil
+	}
+
+	level, err := advisor.NewStatusBySQLReviewRuleLevel(ctx.Rule.Level)
+	if err != nil {
+		return nil, err
+	}
+
+	listener := &namingIdentifierNoKeywordListener{
+		level:         level,
+		title:         string(ctx.Rule.Type),
+		currentSchema: ctx.CurrentSchema,
+	}
+
+	antlr.ParseTreeWalkerDefault.Walk(listener, tree)
+
+	return listener.generateAdvice()
+}
+
+// namingIdentifierNoKeywordListener is the listener for identifier naming convention without keyword.
+type namingIdentifierNoKeywordListener struct {
+	*parser.BasePlSqlParserListener
+
+	level         advisor.Status
+	title         string
+	currentSchema string
+	adviceList    []advisor.Advice
+}
+
+func (l *namingIdentifierNoKeywordListener) generateAdvice() ([]advisor.Advice, error) {
+	if len(l.adviceList) == 0 {
+		l.adviceList = append(l.adviceList, advisor.Advice{
+			Status:  advisor.Success,
+			Code:    advisor.Ok,
+			Title:   "OK",
+			Content: "",
+		})
+	}
+	return l.adviceList, nil
+}
+
+// EnterId_expression is called when production id_expression is entered.
+func (l *namingIdentifierNoKeywordListener) EnterId_expression(ctx *parser.Id_expressionContext) {
+	identifier := normalizeIDExpression(ctx)
+	if sql.IsOracleKeyword(identifier) {
+		l.adviceList = append(l.adviceList, advisor.Advice{
+			Status:  l.level,
+			Code:    advisor.NameIsKeywordIdentifier,
+			Title:   l.title,
+			Content: fmt.Sprintf("Identifier %q is a keyword and should be avoided", identifier),
+			Line:    ctx.GetStart().GetLine(),
+		})
+	}
+}
