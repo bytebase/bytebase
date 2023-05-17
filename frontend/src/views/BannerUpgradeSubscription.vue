7736580a54796027baaba8855c726442ba05ab3c
steven <stevenlgtm@gmail.com>
2023-05-17 17:29:30 +0800

    chore: update

--- frontend/src/views/BannerUpgradeSubscription.vue
+++ frontend/src/views/BannerUpgradeSubscription.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="showBanner" class="bg-gray-200 overflow-clip">
-    <div class="w-full scroll-animation">
+    <div class="w-full h-10 scroll-animation">
       <div
         class="mx-auto py-1 px-3 w-full flex flex-row items-center justify-center flex-wrap"
       >
