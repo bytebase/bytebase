3781591dd48d362f5fd3bec57f3f04071244d637
p0ny <p0uy@outlook.com>
2023-05-22 15:31:41 +0800

    remove plancheckrun

--- backend/migrator/migration/dev/LATEST.sql
+++ backend/migrator/migration/dev/LATEST.sql
@@ -532,7 +532,7 @@ CREATE TABLE plan (
     project_id INTEGER NOT NULL REFERENCES project (id),
     name TEXT NOT NULL,
     description TEXT NOT NULL,
-    workflow JSONB NOT NULL DEFAULT '{}'
+    config JSONB NOT NULL DEFAULT '{}'
 );
 
 CREATE INDEX idx_plan_project_id ON plan(project_id);
@@ -544,30 +544,6 @@ BEFORE
 UPDATE
     ON plan FOR EACH ROW
 EXECUTE FUNCTION trigger_update_updated_ts();
-
-CREATE TABLE plan_check_run (
-    id BIGSERIAL PRIMARY KEY,
-    row_status row_status NOT NULL DEFAULT 'NORMAL',
-    creator_id INTEGER NOT NULL REFERENCES principal (id),
-    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-    updater_id INTEGER NOT NULL REFERENCES principal (id),
-    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-    plan_id BIGINT NOT NULL REFERENCES plan (id),
-    type TEXT NOT NULL,
-    status TEXT NOT NULL,
-    result JSONB NOT NULL DEFAULT '{}'
-);
-
-CREATE INDEX idx_plan_check_run_plan_id ON plan_check_run(plan_id);
-
-ALTER SEQUENCE plan_check_run_id_seq RESTART WITH 101;
-
-CREATE TRIGGER update_plan_check_run_updated_ts
-BEFORE
-UPDATE
-    ON plan_check_run FOR EACH ROW
-EXECUTE FUNCTION trigger_update_updated_ts();
-
 -- Plan related END
 -----------------------
 
