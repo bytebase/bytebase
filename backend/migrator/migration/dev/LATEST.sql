f25c64c82656fe88ff35d7a52330651159407842
p0ny <xz@bytebase.com>
2023-05-22 16:25:41 +0800

    feat: proto & schema def for plan (#6175)
    Former-commit-id: 20cc5ec56b6b21c0de3ebd266393dfbee6ed09c6

--- backend/migrator/migration/dev/LATEST.sql
+++ backend/migrator/migration/dev/LATEST.sql
@@ -677,6 +677,34 @@ EXECUTE FUNCTION trigger_update_updated_ts();
 
 -- Pipeline related END
 -----------------------
+-- Plan related BEGIN
+CREATE TABLE plan (
+    id BIGSERIAL PRIMARY KEY,
+    row_status row_status NOT NULL DEFAULT 'NORMAL',
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    updater_id INTEGER NOT NULL REFERENCES principal (id),
+    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    project_id INTEGER NOT NULL REFERENCES project (id),
+    pipeline_id INTEGER REFERENCES pipeline (id),
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    config JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE INDEX idx_plan_project_id ON plan(project_id);
+
+CREATE INDEX idx_plan_pipeline_id ON plan(pipeline_id);
+
+ALTER SEQUENCE plan_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_plan_updated_ts
+BEFORE
+UPDATE
+    ON plan FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
+-- Plan related END
+-----------------------
 -- issue
 CREATE TABLE issue (
     id SERIAL PRIMARY KEY,
@@ -686,6 +714,7 @@ CREATE TABLE issue (
     updater_id INTEGER NOT NULL REFERENCES principal (id),
     updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
     project_id INTEGER NOT NULL REFERENCES project (id),
+    plan_id BIGINT REFERENCES plan (id),
     pipeline_id INTEGER REFERENCES pipeline (id),
     name TEXT NOT NULL,
     status TEXT NOT NULL CHECK (status IN ('OPEN', 'DONE', 'CANCELED')),
@@ -698,6 +727,8 @@ CREATE TABLE issue (
 );
 
 CREATE INDEX idx_issue_project_id ON issue(project_id);
+
+CREATE INDEX idx_issue_plan_id ON issue(plan_id);
 
 CREATE INDEX idx_issue_pipeline_id ON issue(pipeline_id);
 
