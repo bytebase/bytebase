c5ff6ef17ebbb009278c2a2bef563be84e3fc757
p0ny <p0uy@outlook.com>
2023-05-22 16:11:38 +0800

    fix sql order

--- backend/migrator/migration/dev/LATEST.sql
+++ backend/migrator/migration/dev/LATEST.sql
@@ -519,38 +519,6 @@ UPDATE
     ON backup_setting FOR EACH ROW
 EXECUTE FUNCTION trigger_update_updated_ts();
 
-
------------------------
--- Plan related BEGIN
-CREATE TABLE plan (
-    id BIGSERIAL PRIMARY KEY,
-    row_status row_status NOT NULL DEFAULT 'NORMAL',
-    creator_id INTEGER NOT NULL REFERENCES principal (id),
-    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-    updater_id INTEGER NOT NULL REFERENCES principal (id),
-    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-    project_id INTEGER NOT NULL REFERENCES project (id),
-    pipeline_id INTEGER REFERENCES pipeline (id),
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    config JSONB NOT NULL DEFAULT '{}'
-);
-
-CREATE INDEX idx_plan_project_id ON plan(project_id);
-
-CREATE INDEX idx_plan_pipeline_id ON plan(pipeline_id);
-
-ALTER SEQUENCE plan_id_seq RESTART WITH 101;
-
-CREATE TRIGGER update_plan_updated_ts
-BEFORE
-UPDATE
-    ON plan FOR EACH ROW
-EXECUTE FUNCTION trigger_update_updated_ts();
--- Plan related END
------------------------
-
-
 -----------------------
 -- Pipeline related BEGIN
 -- pipeline table
@@ -708,6 +676,34 @@ UPDATE
 EXECUTE FUNCTION trigger_update_updated_ts();
 
 -- Pipeline related END
+-----------------------
+-- Plan related BEGIN
+CREATE TABLE plan (
+    id BIGSERIAL PRIMARY KEY,
+    row_status row_status NOT NULL DEFAULT 'NORMAL',
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    updater_id INTEGER NOT NULL REFERENCES principal (id),
+    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    project_id INTEGER NOT NULL REFERENCES project (id),
+    pipeline_id INTEGER REFERENCES pipeline (id),
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    config JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE INDEX idx_plan_project_id ON plan(project_id);
+
+CREATE INDEX idx_plan_pipeline_id ON plan(pipeline_id);
+
+ALTER SEQUENCE plan_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_plan_updated_ts
+BEFORE
+UPDATE
+    ON plan FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
+-- Plan related END
 -----------------------
 -- issue
 CREATE TABLE issue (
