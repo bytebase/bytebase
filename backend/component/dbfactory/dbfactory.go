8e0347801c049de278831a23a3d20bdc2ab2cd30
d-bytebase <d@bytebase.com>
2023-05-24 16:52:24 +0800

    chore: refactor db factory and backfill readonly data source host and port

--- backend/component/dbfactory/dbfactory.go
+++ backend/component/dbfactory/dbfactory.go
@@ -34,90 +34,11 @@ func New(mysqlBinDir, mongoBinDir, pgBinDir, dataDir, secret string) *DBFactory 
 // GetAdminDatabaseDriver gets the admin database driver using the instance's admin data source.
 // Upon successful return, caller must call driver.Close(). Otherwise, it will leak the database connection.
 func (d *DBFactory) GetAdminDatabaseDriver(ctx context.Context, instance *store.InstanceMessage, databaseName string) (db.Driver, error) {
-	adminDataSource := utils.DataSourceFromInstanceWithType(instance, api.Admin)
-	if adminDataSource == nil {
+	dataSource := utils.DataSourceFromInstanceWithType(instance, api.Admin)
+	if dataSource == nil {
 		return nil, common.Errorf(common.Internal, "admin data source not found for instance %q", instance.Title)
 	}
-
-	dbBinDir := ""
-	switch instance.Engine {
-	case db.MySQL, db.TiDB, db.MariaDB, db.OceanBase:
-		// TODO(d): use maria mysqlbinlog for MariaDB.
-		dbBinDir = d.mysqlBinDir
-	case db.Postgres:
-		dbBinDir = d.pgBinDir
-	case db.MongoDB:
-		dbBinDir = d.mongoBinDir
-	}
-
-	if databaseName == "" {
-		databaseName = adminDataSource.Database
-	}
-	password, err := common.Unobfuscate(adminDataSource.ObfuscatedPassword, d.secret)
-	if err != nil {
-		return nil, err
-	}
-	sslCA, err := common.Unobfuscate(adminDataSource.ObfuscatedSslCa, d.secret)
-	if err != nil {
-		return nil, err
-	}
-	sslCert, err := common.Unobfuscate(adminDataSource.ObfuscatedSslCert, d.secret)
-	if err != nil {
-		return nil, err
-	}
-	sslKey, err := common.Unobfuscate(adminDataSource.ObfuscatedSslKey, d.secret)
-	if err != nil {
-		return nil, err
-	}
-	sshPassword, err := common.Unobfuscate(adminDataSource.SSHObfuscatedPassword, d.secret)
-	if err != nil {
-		return nil, err
-	}
-	sshPrivateKey, err := common.Unobfuscate(adminDataSource.SSHObfuscatedPrivateKey, d.secret)
-	if err != nil {
-		return nil, err
-	}
-	sshConfig := db.SSHConfig{
-		Host:       adminDataSource.SSHHost,
-		Port:       adminDataSource.SSHPort,
-		User:       adminDataSource.SSHUser,
-		Password:   sshPassword,
-		PrivateKey: sshPrivateKey,
-	}
-	driver, err := getDatabaseDriver(
-		ctx,
-		instance.Engine,
-		db.DriverConfig{
-			DbBinDir:  dbBinDir,
-			BinlogDir: common.GetBinlogAbsDir(d.dataDir, instance.UID),
-		},
-		db.ConnectionConfig{
-			Username: adminDataSource.Username,
-			Password: password,
-			TLSConfig: db.TLSConfig{
-				SslCA:   sslCA,
-				SslCert: sslCert,
-				SslKey:  sslKey,
-			},
-			Host:                   adminDataSource.Host,
-			Port:                   adminDataSource.Port,
-			Database:               databaseName,
-			SRV:                    adminDataSource.SRV,
-			AuthenticationDatabase: adminDataSource.AuthenticationDatabase,
-			SID:                    adminDataSource.SID,
-			ServiceName:            adminDataSource.ServiceName,
-			SSHConfig:              sshConfig,
-		},
-		db.ConnectionContext{
-			EnvironmentID: instance.EnvironmentID,
-			InstanceID:    instance.ResourceID,
-		},
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	return driver, nil
+	return d.GetDataSourceDriver(ctx, instance.Engine, dataSource, databaseName, instance.ResourceID, instance.UID, false /* readOnly */)
 }
 
 // GetReadOnlyDatabaseDriver gets the read-only database driver using the instance's read-only data source.
@@ -134,28 +55,30 @@ func (d *DBFactory) GetReadOnlyDatabaseDriver(ctx context.Context, instance *sto
 		return nil, common.Errorf(common.Internal, "data source not found for instance %q", instance.Title)
 	}
 
-	host, port := dataSource.Host, dataSource.Port
-	if host == "" {
-		host = adminDataSource.Host
-	}
-	if port == "" {
-		port = adminDataSource.Port
-	}
+	return d.GetDataSourceDriver(ctx, instance.Engine, dataSource, databaseName, instance.ResourceID, instance.UID, true /* readOnly */)
+}
 
+// GetDataSourceDriver returns the database driver for a data source.
+func (d *DBFactory) GetDataSourceDriver(ctx context.Context, engine db.Type, dataSource *store.DataSourceMessage, databaseName, instanceID string, instanceUID int, readOnly bool) (db.Driver, error) {
 	dbBinDir := ""
-	switch instance.Engine {
+	switch engine {
 	case db.MySQL, db.TiDB, db.MariaDB, db.OceanBase:
 		// TODO(d): use maria mysqlbinlog for MariaDB.
 		dbBinDir = d.mysqlBinDir
 	case db.Postgres:
 		dbBinDir = d.pgBinDir
+	case db.MongoDB:
+		dbBinDir = d.mongoBinDir
 	}
 
+	if databaseName == "" {
+		databaseName = dataSource.Database
+	}
 	password, err := common.Unobfuscate(dataSource.ObfuscatedPassword, d.secret)
 	if err != nil {
 		return nil, err
 	}
-	sslCa, err := common.Unobfuscate(dataSource.ObfuscatedSslCa, d.secret)
+	sslCA, err := common.Unobfuscate(dataSource.ObfuscatedSslCa, d.secret)
 	if err != nil {
 		return nil, err
 	}
@@ -182,54 +105,38 @@ func (d *DBFactory) GetReadOnlyDatabaseDriver(ctx context.Context, instance *sto
 		Password:   sshPassword,
 		PrivateKey: sshPrivateKey,
 	}
-	driver, err := getDatabaseDriver(
+	driver, err := db.Open(
 		ctx,
-		instance.Engine,
+		engine,
 		db.DriverConfig{
 			DbBinDir:  dbBinDir,
-			BinlogDir: common.GetBinlogAbsDir(d.dataDir, instance.UID),
+			BinlogDir: common.GetBinlogAbsDir(d.dataDir, instanceUID),
 		},
 		db.ConnectionConfig{
 			Username: dataSource.Username,
 			Password: password,
-			Host:     host,
-			Port:     port,
-			Database: databaseName,
 			TLSConfig: db.TLSConfig{
-				SslCA:   sslCa,
+				SslCA:   sslCA,
 				SslCert: sslCert,
 				SslKey:  sslKey,
 			},
+			Host:                   dataSource.Host,
+			Port:                   dataSource.Port,
+			Database:               databaseName,
 			SRV:                    dataSource.SRV,
 			AuthenticationDatabase: dataSource.AuthenticationDatabase,
 			SID:                    dataSource.SID,
 			ServiceName:            dataSource.ServiceName,
 			SSHConfig:              sshConfig,
-			ReadOnly:               true,
+			ReadOnly:               readOnly,
 		},
 		db.ConnectionContext{
-			EnvironmentID: instance.EnvironmentID,
-			InstanceID:    instance.ResourceID,
+			InstanceID: instanceID,
 		},
 	)
 	if err != nil {
 		return nil, err
 	}
 
-	return driver, nil
-}
-
-// Retrieve db.Driver connection with standard parameters for all type data source.
-func getDatabaseDriver(ctx context.Context, engine db.Type, driverConfig db.DriverConfig, connectionConfig db.ConnectionConfig, connCtx db.ConnectionContext) (db.Driver, error) {
-	driver, err := db.Open(
-		ctx,
-		engine,
-		driverConfig,
-		connectionConfig,
-		connCtx,
-	)
-	if err != nil {
-		return nil, common.Wrapf(err, common.DbConnectionFailure, "failed to connect database at %s:%s with user %q", connectionConfig.Host, connectionConfig.Port, connectionConfig.Username)
-	}
 	return driver, nil
 }
