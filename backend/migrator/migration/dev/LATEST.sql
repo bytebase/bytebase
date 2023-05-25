a328e5fd7a09900954f5ec13e3a0b7c8cd6521cb
h3n4l <oysterdays@gmail.com>
2023-05-25 18:21:46 +0800

    feat: the database schema of database/schema group (#6252)
    * feat: the database schema of database/schema group
    
    * chore: update
    
    * chore: rename name to resource_id and add unique index
    
    * fix: fix test get cutoff version test
    
    * chore: remove description
    
    * chore: update
    
    * chore: update
    
    * chore: update

--- backend/migrator/migration/dev/LATEST.sql
+++ backend/migrator/migration/dev/LATEST.sql
@@ -1215,3 +1215,49 @@ BEFORE
 UPDATE
     ON slow_query FOR EACH ROW
 EXECUTE FUNCTION trigger_update_updated_ts();
+
+CREATE TABLE db_group (
+    id BIGSERIAL PRIMARY KEY,
+    row_status row_status NOT NULL DEFAULT 'NORMAL',
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    updater_id INTEGER NOT NULL REFERENCES principal (id),
+    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    project_resource_id TEXT NOT NULL REFERENCES project (resource_id),
+    resource_id TEXT NOT NULL,
+    placeholder TEXT NOT NULL DEFAULT '',
+    expression JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE UNIQUE INDEX idx_db_group_unique_resource_id ON db_group(resource_id);
+
+ALTER SEQUENCE db_group_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_db_group_updated_ts
+BEFORE
+UPDATE
+    ON db_group FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
+
+CREATE TABLE schema_group (
+    id BIGSERIAL PRIMARY KEY,
+    row_status row_status NOT NULL DEFAULT 'NORMAL',
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    updater_id INTEGER NOT NULL REFERENCES principal (id),
+    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    db_group_resource_id TEXT NOT NULL REFERENCES db_group (resource_id),
+    resource_id TEXT NOT NULL,
+    placeholder TEXT NOT NULL DEFAULT '',
+    expression JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE UNIQUE INDEX idx_schema_group_unique_resource_id ON schema_group(resource_id);
+
+ALTER SEQUENCE schema_group_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_schema_group_updated_ts
+BEFORE
+UPDATE
+    ON schema_group FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
