439a15c4e448c441c11a823ede9e26a0701ec9c7
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-05-25 10:50:35 +0800

    chore: remove unused

--- frontend/src/components/InstanceForm.vue
+++ frontend/src/components/InstanceForm.vue
@@ -1548,17 +1548,3 @@ const calcDataSourceUpdateMask = (
   return Array.from(updateMask);
 };
 </script>
-
-<style scoped>
-/*  Removed the ticker in the number field  */
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-/* Firefox */
-input[type="number"] {
-  -moz-appearance: textfield;
-}
-</style>
