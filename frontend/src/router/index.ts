a171f2b1125e248816f0be5d56045a1ce47798e9
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-05-31 15:25:06 +0800

    chore: lint

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1027,8 +1027,9 @@ const routes: Array<RouteRecordRaw> = [
                     if (slug.toLowerCase() == "new") {
                       return t("common.new");
                     }
-                    return useLegacyInstanceStore().getInstanceById(idFromSlug(slug))
-                      .name;
+                    return useInstanceV1Store().getInstanceByUID(
+                      String(idFromSlug(slug))
+                    ).title;
                   },
                 },
                 component: () => import("../views/InstanceDetail.vue"),
@@ -1662,13 +1663,16 @@ router.beforeEach((to, from, next) => {
     if (Number.isNaN(databaseId)) {
       // Connected to instance
       useConnectionTreeStore()
-        .fetchConnectionByInstanceId(instanceId)
+        .fetchConnectionByInstanceId(String(instanceId))
         .then(() => next())
         .catch(() => next());
     } else {
       // Connected to db
       useConnectionTreeStore()
-        .fetchConnectionByInstanceIdAndDatabaseId(instanceId, databaseId)
+        .fetchConnectionByInstanceIdAndDatabaseId(
+          String(instanceId),
+          String(databaseId)
+        )
         .then(() => next())
         .catch(() => next());
     }
