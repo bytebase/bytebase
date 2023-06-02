f2a8c4b5767130135a9dc3fa3523b1a7df29ba6f
p0ny <xz@bytebase.com>
2023-06-02 15:20:20 +0800

    fix: update vcs type sql (#6417)
    Former-commit-id: f05ef08ff0c442eec22d836a528bc6783ea9883a
    Former-commit-id: 562805c0fb891c090e6452c47e660e59e5fc9f0b

--- backend/migrator/migration/prod/2.2/0004##update_vcs_type_in_payload.sql
+++ backend/migrator/migration/prod/2.2/0004##update_vcs_type_in_payload.sql
@@ -1,7 +1,7 @@
-UPDATE task SET task.payload = jsonb_set(task.payload, '{pushEvent,vcsType}', '"GITLAB"') WHERE task.payload ? 'pushEvent' AND task.payload->'pushEvent'->>'vcsType' = 'GITLAB_SELF_HOST';
+UPDATE task SET payload = jsonb_set(payload, '{pushEvent,vcsType}', '"GITLAB"') WHERE payload ? 'pushEvent' AND payload->'pushEvent'->>'vcsType' = 'GITLAB_SELF_HOST';
 
-UPDATE task SET task.payload = jsonb_set(task.payload, '{pushEvent,vcsType}', '"GITHUB"') WHERE task.payload ? 'pushEvent' AND task.payload->'pushEvent'->>'vcsType' = 'GITHUB_COM';
+UPDATE task SET payload = jsonb_set(payload, '{pushEvent,vcsType}', '"GITHUB"') WHERE payload ? 'pushEvent' AND payload->'pushEvent'->>'vcsType' = 'GITHUB_COM';
 
-UPDATE instance_change_history SET instance_change_history.payload = jsonb_set(instance_change_history.payload, '{pushEvent,vcsType}', '"GITLAB"') WHERE task.payload ? 'pushEvent' AND task.payload->'pushEvent'->>'vcsType' = 'GITLAB_SELF_HOST';
+UPDATE instance_change_history SET payload = jsonb_set(payload, '{pushEvent,vcsType}', '"GITLAB"') WHERE payload ? 'pushEvent' AND payload->'pushEvent'->>'vcsType' = 'GITLAB_SELF_HOST';
 
-UPDATE instance_change_history SET instance_change_history.payload = jsonb_set(instance_change_history.payload, '{pushEvent,vcsType}', '"GITHUB"') WHERE task.payload ? 'pushEvent' AND task.payload->'pushEvent'->>'vcsType' = 'GITHUB_COM';
+UPDATE instance_change_history SET payload = jsonb_set(payload, '{pushEvent,vcsType}', '"GITHUB"') WHERE payload ? 'pushEvent' AND payload->'pushEvent'->>'vcsType' = 'GITHUB_COM';
