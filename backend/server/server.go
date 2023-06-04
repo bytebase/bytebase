b37f6f8abefe2d07a3f0fa3d3f965788ef496358
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-06-04 20:35:39 +0800

    chore: change risk expression to unparsed struct (#6433)
    * chore: backfill risk struct
    
    * chore: update
    
    Former-commit-id: 8d2313bb61e5918bd191bd143072c6f2e631e49c

--- backend/server/server.go
+++ backend/server/server.go
@@ -315,6 +315,9 @@ func NewServer(ctx context.Context, profile config.Profile) (*Server, error) {
 			return nil, err
 		}
 		s.SchemaVersion = metadataVersion
+		if err := storeInstance.BackfillRiskExpression(ctx); err != nil {
+			return nil, err
+		}
 	}
 
 	s.stateCfg = &state.State{
