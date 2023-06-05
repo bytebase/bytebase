f215994293b7fb40376e85be48e3d6ab17907d5d
rebelice <yangrebelice@gmail.com>
2023-06-05 15:21:52 +0800

    chore: upgrade plsql parser and ANTLR4 golang runtime (#6454)
    * chore: upgrade plsql parser and ANTLR4 golang runtime
    
    * chore: upgrade template

--- backend/plugin/advisor/oracle/advisor_naming_identifier_case.go
+++ backend/plugin/advisor/oracle/advisor_naming_identifier_case.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
+	"github.com/antlr4-go/antlr/v4"
 	parser "github.com/bytebase/plsql-parser"
 
 	"github.com/bytebase/bytebase/backend/plugin/advisor"
