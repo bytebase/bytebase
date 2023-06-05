b64e4301da7a52db3c1334b12af48ea837f4a97b
p0ny <p0uy@outlook.com>
2023-06-05 16:37:32 +0800

    store: phone, list and create

--- backend/store/principal.go
+++ backend/store/principal.go
@@ -101,6 +101,8 @@ type UserMessage struct {
 	Role          api.Role
 	MemberDeleted bool
 	MFAConfig     *storepb.MFAConfig
+	// Phone conforms E.164 format.
+	Phone string
 }
 
 // GetUser gets an user.
@@ -208,6 +210,7 @@ func (*Store) listUserImpl(ctx context.Context, tx *Tx, find *FindUserMessage) (
 		principal.type,
 		principal.password_hash,
 		principal.mfa_config,
+		principal.phone,
 		member.role,
 		member.row_status AS row_status
 	FROM principal
@@ -235,6 +238,7 @@ func (*Store) listUserImpl(ctx context.Context, tx *Tx, find *FindUserMessage) (
 			&userMessage.Type,
 			&userMessage.PasswordHash,
 			&mfaConfigBytes,
+			&userMessage.Phone,
 			&role,
 			&rowStatus,
 		); err != nil {
@@ -281,8 +285,8 @@ func (s *Store) CreateUser(ctx context.Context, create *UserMessage, creatorID i
 	}
 	defer tx.Rollback()
 
-	set := []string{"creator_id", "updater_id", "email", "name", "type", "password_hash"}
-	args := []any{creatorID, creatorID, create.Email, create.Name, create.Type, create.PasswordHash}
+	set := []string{"creator_id", "updater_id", "email", "name", "type", "password_hash", "phone"}
+	args := []any{creatorID, creatorID, create.Email, create.Name, create.Type, create.PasswordHash, create.Phone}
 	placeholder := []string{}
 	for index := range set {
 		placeholder = append(placeholder, fmt.Sprintf("$%d", index+1))
@@ -345,6 +349,7 @@ func (s *Store) CreateUser(ctx context.Context, create *UserMessage, creatorID i
 		Name:         create.Name,
 		Type:         create.Type,
 		PasswordHash: create.PasswordHash,
+		Phone:        create.Phone,
 		Role:         role,
 	}
 	s.userIDCache.Store(user.ID, user)
