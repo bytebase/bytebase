eb192543699e396c83a9bb65f97eb0f5b4c93dd0
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-06-06 10:27:52 +0800

    chore: migrate admin execute API in tests (#6471)
    * chore: deprecate more methods
    
    * chore: migrate admin API

--- backend/tests/environment.go
+++ backend/tests/environment.go
@@ -3,24 +3,19 @@ package tests
 import (
 	"context"
 	"fmt"
-	"strconv"
 
 	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
 )
 
 // getProjects gets the environments.
-func (ctl *controller) getEnvironment(ctx context.Context, id string) (*v1pb.Environment, int, error) {
+func (ctl *controller) getEnvironment(ctx context.Context, id string) (*v1pb.Environment, error) {
 	environment, err := ctl.environmentServiceClient.GetEnvironment(ctx,
 		&v1pb.GetEnvironmentRequest{
 			Name: fmt.Sprintf("environments/%s", id),
 		},
 	)
 	if err != nil {
-		return nil, 0, err
+		return nil, err
 	}
-	uid, err := strconv.Atoi(environment.Uid)
-	if err != nil {
-		return nil, 0, err
-	}
-	return environment, uid, nil
+	return environment, nil
 }
