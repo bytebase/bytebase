dd3e12c269f51ce632726d60847837d908be862e
steven <stevenlgtm@gmail.com>
2023-06-06 13:45:22 +0800

    chore: add back create table group button
    (cherry picked from commit 4cf5b4cc8f5a56e5811413999fd9577d5a030601)

--- frontend/src/components/DatabaseGroup/ProjectDatabaseGroupPanel.vue
+++ frontend/src/components/DatabaseGroup/ProjectDatabaseGroupPanel.vue
@@ -3,6 +3,12 @@
     <div class="w-full flex flex-row justify-between items-center">
       <span>{{ $t("database-group.self") }}</span>
       <div class="flex flex-row gap-x-2">
+        <NButton @click="handleCreateSchemaGroup">
+          <span class="mr-1">{{
+            $t("database-group.table-group.create")
+          }}</span>
+          <FeatureBadge feature="bb.feature.database-grouping" />
+        </NButton>
         <NButton @click="handleCreateDatabaseGroup">
           <span class="mr-1">{{ $t("database-group.create") }}</span>
           <FeatureBadge feature="bb.feature.database-grouping" />
@@ -77,6 +83,17 @@ const handleCreateDatabaseGroup = () => {
   }
 
   state.resourceType = "DATABASE_GROUP";
+  state.editingDatabaseGroup = undefined;
+  state.showDatabaseGroupPanel = true;
+};
+
+const handleCreateSchemaGroup = () => {
+  if (!hasFeature("bb.feature.database-grouping")) {
+    state.showFeatureModal = true;
+    return;
+  }
+
+  state.resourceType = "SCHEMA_GROUP";
   state.editingDatabaseGroup = undefined;
   state.showDatabaseGroupPanel = true;
 };
