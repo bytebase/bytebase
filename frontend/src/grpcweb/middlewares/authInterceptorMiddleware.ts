4ad6457b1bc1d530a10af54338217363c9f78b50
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-06-09 10:47:40 +0800

    chore(frontend): introduce grpc-web auth interceptor middleware (#6543)
    * chore: introduce grpc-web auth interceptor middleware
    
    * chore: re-order imports
    
    * chore: comments

---
+++ frontend/src/grpcweb/middlewares/authInterceptorMiddleware.ts
@@ -0,0 +1,54 @@
+import { ClientError, ServerError, Status } from "nice-grpc-common";
+import { ClientMiddleware } from "nice-grpc-web";
+
+import { useAuthStore } from "@/store";
+import { router } from "@/router";
+
+/**
+ * Way to define a grpc-web middleware
+ * ClientMiddleware<CallOptionsExt = {}, RequiredCallOptionsExt = {}>
+ * See
+ *   - https://github.com/deeplay-io/nice-grpc/blob/master/packages/nice-grpc-client-middleware-deadline/src/index.ts
+ *   - https://github.com/deeplay-io/nice-grpc/tree/master/packages/nice-grpc-web#middleware
+ *   as an example.
+ */
+export const authInterceptorMiddleware: ClientMiddleware = async function* (
+  call,
+  options
+) {
+  if (!call.responseStream) {
+    try {
+      const response = yield* call.next(call.request, options);
+      return response;
+    } catch (error) {
+      await handleError(error);
+    }
+  } else {
+    try {
+      for await (const response of call.next(call.request, options)) {
+        yield response;
+      }
+    } catch (error) {
+      await handleError(error);
+    }
+
+    return;
+  }
+};
+
+const handleError = async (error: unknown) => {
+  if (error instanceof ClientError || error instanceof ServerError) {
+    if (error.code === Status.UNAUTHENTICATED) {
+      // "Kick out" sign in status if access token expires.
+      try {
+        await useAuthStore().logout();
+      } finally {
+        router.push({ name: "auth.signin" });
+      }
+    } else if (error.code === Status.PERMISSION_DENIED) {
+      // Jump to 403 page
+      router.push({ name: "error.403" });
+    }
+  }
+  throw error;
+};
