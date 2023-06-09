ecd654fcf0d7473fd918a145acb505fe21363916
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-06-09 15:58:21 +0800

    chore: fix displayed environment id (#6558)

--- frontend/src/components/EnvironmentForm.vue
+++ frontend/src/components/EnvironmentForm.vue
@@ -24,7 +24,7 @@
             ref="resourceIdField"
             resource-type="environment"
             :readonly="!create"
-            :value="state.environment.name"
+            :value="extractEnvironmentResourceName(state.environment.name)"
             :resource-title="state.environment.title"
             :validate="validateResourceId"
           />
@@ -325,7 +325,11 @@ import { environmentNamePrefix } from "@/store/modules/v1/common";
 import { getErrorCode } from "@/utils/grpcweb";
 import { BBSwitch } from "@/bbkit";
 import { DrawerContent } from "@/components/v2";
-import { hasWorkspacePermissionV1, sqlReviewPolicySlug } from "@/utils";
+import {
+  extractEnvironmentResourceName,
+  hasWorkspacePermissionV1,
+  sqlReviewPolicySlug,
+} from "@/utils";
 import {
   pushNotification,
   useCurrentUserV1,
