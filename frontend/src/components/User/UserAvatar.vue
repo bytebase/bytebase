c291ac2b7ae508e95a6bef1392d004bce2d33e7e
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-06-12 18:33:54 +0800

    feat: implement issue review rejection

--- frontend/src/components/User/UserAvatar.vue
+++ frontend/src/components/User/UserAvatar.vue
@@ -1,12 +1,20 @@
 <template>
-  <BBAvatar :username="username" :size="size" />
+  <BBAvatar
+    :username="username"
+    :size="size"
+    :override-class="overrideClass"
+    :override-text-size="overrideTextSize"
+  />
 </template>
 
 <script lang="ts" setup>
 import { computed, PropType } from "vue";
+
+import { BBAvatar } from "@/bbkit";
 import { BBAvatarSizeType } from "@/bbkit/types";
 import { UNKNOWN_ID, unknownUser } from "@/types";
 import { User } from "@/types/proto/v1/auth_service";
+import { VueClass } from "@/utils";
 
 const props = defineProps({
   user: {
@@ -16,6 +24,14 @@ const props = defineProps({
   size: {
     type: String as PropType<BBAvatarSizeType>,
     default: "NORMAL",
+  },
+  overrideClass: {
+    type: [String, Object, Array] as PropType<VueClass>,
+    default: undefined,
+  },
+  overrideTextSize: {
+    type: String,
+    default: undefined,
   },
 });
 
