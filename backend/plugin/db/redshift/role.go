7ecdbf55509e6e5377341e60b4dec5540cfc0877
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-06-12 10:06:20 +0800

    fix: return response early (#6574)

--- backend/plugin/db/redshift/role.go
+++ backend/plugin/db/redshift/role.go
@@ -10,25 +10,25 @@ import (
 
 // CreateRole creates the role.
 func (*Driver) CreateRole(_ context.Context, _ *db.DatabaseRoleUpsertMessage) (*db.DatabaseRoleMessage, error) {
-	return nil, errors.Errorf("create role for MSSQL is not implemented yet")
+	return nil, errors.Errorf("create role for Redshift is not implemented yet")
 }
 
 // UpdateRole updates the role.
 func (*Driver) UpdateRole(_ context.Context, _ string, _ *db.DatabaseRoleUpsertMessage) (*db.DatabaseRoleMessage, error) {
-	return nil, errors.Errorf("update role for MSSQL is not implemented yet")
+	return nil, errors.Errorf("update role for Redshift is not implemented yet")
 }
 
 // FindRole finds the role by name.
 func (*Driver) FindRole(_ context.Context, _ string) (*db.DatabaseRoleMessage, error) {
-	return nil, errors.Errorf("find role for MSSQL is not implemented yet")
+	return nil, errors.Errorf("find role for Redshift is not implemented yet")
 }
 
 // ListRole lists the role.
 func (*Driver) ListRole(_ context.Context) ([]*db.DatabaseRoleMessage, error) {
-	return nil, errors.Errorf("list role for MSSQL is not implemented yet")
+	return nil, errors.Errorf("list role for Redshift is not implemented yet")
 }
 
 // DeleteRole deletes the role by name.
 func (*Driver) DeleteRole(_ context.Context, _ string) error {
-	return errors.Errorf("delete role for MSSQL is not implemented yet")
+	return errors.Errorf("delete role for Redshift is not implemented yet")
 }
