919f27ac1d8226419db987eb7a8468b78b47e35a
h3n4l <oysterdays@gmail.com>
2023-06-12 22:59:59 +0800

    chore: split into more action

--- backend/plugin/advisor/snowflake/snowflake_rules_test.go
+++ backend/plugin/advisor/snowflake/snowflake_rules_test.go
@@ -15,6 +15,6 @@ func TestSnowflakeRules(t *testing.T) {
 	}
 
 	for _, rule := range snowflakeRules {
-		advisor.RunSQLReviewRuleTest(t, rule, db.Snowflake, false /* record */)
+		advisor.RunSQLReviewRuleTest(t, rule, db.Snowflake, true /* record */)
 	}
 }
