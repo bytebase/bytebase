4f06570ae55a7c6aed50a208246e4cf9bb165b87
boojack <stevenlgtm@gmail.com>
2023-06-14 22:26:45 +0800

    chore: update request export entries (#6637)
    Former-commit-id: bcb5fd10e73499e4b41c4bfec1a8efdff30e7783

--- frontend/src/views/sql-editor/EditorCommon/ResultView/SingleResultViewV1.vue
+++ frontend/src/views/sql-editor/EditorCommon/ResultView/SingleResultViewV1.vue
@@ -58,7 +58,7 @@
         </NDropdown>
         <NButton
           v-if="showRequestExportButton"
-          @click="handleGotoRequestExportPage"
+          @click="state.showRequestExportPanel = true"
         >
           {{ $t("quick-action.request-export") }}
         </NButton>
@@ -93,6 +93,13 @@
   <template v-else-if="viewMode === 'ERROR'">
     <ErrorView :error="result.error" />
   </template>
+
+  <RequestExportPanel
+    v-if="state.showRequestExportPanel"
+    :database-id="currentTab.connection.databaseId"
+    :statement="currentTab.statement"
+    @close="state.showRequestExportPanel = false"
+  />
 </template>
 
 <script lang="ts" setup>
@@ -137,11 +144,12 @@ import {
   TabMode,
   UNKNOWN_ID,
 } from "@/types";
-import { useRouter } from "vue-router";
 import { useExportData } from "./useExportData";
+import RequestExportPanel from "@/components/Issue/panel/RequestExportPanel/index.vue";
 
 type LocalState = {
   search: string;
+  showRequestExportPanel: boolean;
 };
 type ViewMode = "RESULT" | "EMPTY" | "AFFECTED-ROWS" | "ERROR";
 
@@ -159,18 +167,19 @@ const props = defineProps<{
 
 const state = reactive<LocalState>({
   search: "",
+  showRequestExportPanel: false,
 });
 
 const { dark } = useSQLResultViewContext();
 
 const { t } = useI18n();
-const router = useRouter();
 const tabStore = useTabStore();
 const instanceStore = useInstanceV1Store();
 const databaseStore = useDatabaseV1Store();
 const currentUserV1 = useCurrentUserV1();
 const dataTable = ref<InstanceType<typeof DataTable>>();
 const { isExportingData, exportData } = useExportData();
+const currentTab = computed(() => tabStore.currentTab);
 
 const viewMode = computed((): ViewMode => {
   const { result } = props;
@@ -342,33 +351,6 @@ const showPagination = computed(() => data.value.length > PAGE_SIZES[0]);
 const handleChangePage = (page: number) => {
   table.setPageIndex(page - 1);
   dataTable.value?.scrollTo(0, 0);
-};
-
-const handleGotoRequestExportPage = () => {
-  const routeInfo = {
-    name: "workspace.issue.detail",
-    params: {
-      issueSlug: "new",
-    },
-    query: {
-      template: "bb.issue.grant.request",
-      role: "EXPORTER",
-      name: "New grant exporter request",
-    },
-  };
-
-  const currentTab = tabStore.currentTab;
-  if (String(currentTab.connection.databaseId) !== String(UNKNOWN_ID)) {
-    const database = databaseStore.getDatabaseByUID(
-      currentTab.connection.databaseId
-    );
-    (routeInfo.query as any).project = database.projectEntity.uid;
-    (routeInfo.query as any).databaseList = database.uid;
-    (routeInfo.query as any).sql =
-      currentTab.selectedStatement || currentTab.statement;
-  }
-
-  router.push(routeInfo);
 };
 
 const explainFromSQLResultSetV1 = (resultSet: SQLResultSetV1 | undefined) => {
