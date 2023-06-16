55efa642f89dc9adace1404ac18de98ad55d943b
rebelice <yangrebelice@gmail.com>
2023-06-16 18:11:34 +0800

    feat: extract Oracle Expression Part I

--- backend/plugin/db/util/mask_sensitive_data_plsql.go
+++ backend/plugin/db/util/mask_sensitive_data_plsql.go
@@ -134,11 +134,204 @@ func (extractor *sensitiveFieldExtractor) plsqlExtractQueryBlock(ctx plsql.IQuer
 		if selectedList.ASTERISK() != nil {
 			return fromFieldList, nil
 		}
+
+		selectListElements := selectedList.AllSelect_list_elements()
+		for _, element := range selectListElements {
+			if element.ASTERISK() != nil {
+				schemaName, tableName := normalizeTableViewName(extractor.currentDatabase, element.Tableview_name())
+				for _, field := range fromFieldList {
+					if schemaName == field.database && field.table == tableName {
+						result = append(result, field)
+					}
+				}
+			} else {
+				sensitive, err := extractor.plsqlIsSensitiveExpression(element.Expression())
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
 	}
 
-	// TODO(rebelice): handle other cases
-
 	return result, nil
+}
+
+func (extractor *sensitiveFieldExtractor) plsqlIsSensitiveExpression(ctx antlr.ParserRuleContext) (bool, error) {
+	if ctx == nil {
+		return false, nil
+	}
+
+	switch rule := ctx.(type) {
+	case plsql.IExpressionContext:
+		if rule.Logical_expression() != nil {
+			return extractor.plsqlIsSensitiveExpression(rule.Logical_expression())
+		}
+
+		return extractor.plsqlIsSensitiveExpression(rule.Cursor_expression())
+	case plsql.ICursor_expressionContext:
+		return extractor.plsqlIsSensitiveExpression(rule.Subquery())
+	case plsql.ISubqueryContext:
+		// For associated subquery, we should set the fromFieldList as the outerSchemaInfo.
+		// So that the subquery can access the outer schema.
+		// The reason for new extractor is that we still need the current fromFieldList, overriding it is not expected.
+		subqueryExtractor := &sensitiveFieldExtractor{
+			currentDatabase: extractor.currentDatabase,
+			schemaInfo:      extractor.schemaInfo,
+			outerSchemaInfo: append(extractor.outerSchemaInfo, extractor.fromFieldList...),
+		}
+		fieldList, err := subqueryExtractor.plsqlExtractSubquery(rule)
+		if err != nil {
+			return false, err
+		}
+		for _, field := range fieldList {
+			if field.sensitive {
+				return true, nil
+			}
+		}
+		return false, nil
+	case plsql.ILogical_expressionContext:
+		if rule.Unary_logical_expression() != nil {
+			return extractor.plsqlIsSensitiveExpression(rule.Unary_logical_expression())
+		}
+		var list []antlr.ParserRuleContext
+		for _, item := range rule.AllLogical_expression() {
+			list = append(list, item)
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.IUnary_logical_expressionContext:
+		return extractor.plsqlIsSensitiveExpression(rule.Multiset_expression())
+	case plsql.IMultiset_expressionContext:
+		var list []antlr.ParserRuleContext
+		if rule.Relational_expression() != nil {
+			list = append(list, rule.Relational_expression())
+		}
+		if rule.Concatenation() != nil {
+			list = append(list, rule.Concatenation())
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.IRelational_expressionContext:
+		var list []antlr.ParserRuleContext
+		for _, item := range rule.AllRelational_expression() {
+			list = append(list, item)
+		}
+		if rule.Compound_expression() != nil {
+			list = append(list, rule.Compound_expression())
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.ICompound_expressionContext:
+		var list []antlr.ParserRuleContext
+		for _, item := range rule.AllConcatenation() {
+			list = append(list, item)
+		}
+		if rule.In_elements() != nil {
+			list = append(list, rule.In_elements())
+		}
+		if rule.Between_elements() != nil {
+			list = append(list, rule.Between_elements())
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.IIn_elementsContext:
+		var list []antlr.ParserRuleContext
+		if rule.Subquery() != nil {
+			list = append(list, rule.Subquery())
+		}
+		for _, item := range rule.AllConcatenation() {
+			list = append(list, item)
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.IBetween_elementsContext:
+		var list []antlr.ParserRuleContext
+		for _, item := range rule.AllConcatenation() {
+			list = append(list, item)
+		}
+	case plsql.IConcatenationContext:
+		var list []antlr.ParserRuleContext
+		if rule.Model_expression() != nil {
+			list = append(list, rule.Model_expression())
+		}
+		if rule.Interval_expression() != nil {
+			list = append(list, rule.Interval_expression())
+		}
+		for _, item := range rule.AllConcatenation() {
+			list = append(list, item)
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.IModel_expressionContext:
+		var list []antlr.ParserRuleContext
+		if rule.Unary_expression() != nil {
+			list = append(list, rule.Unary_expression())
+		}
+		// TODO(rebelice): Add model_expression_element
+		if rule.Model_expression_element() != nil {
+			list = append(list, rule.Model_expression_element())
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.IInterval_expressionContext:
+		var list []antlr.ParserRuleContext
+		for _, item := range rule.AllConcatenation() {
+			list = append(list, item)
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.IUnary_expressionContext:
+		var list []antlr.ParserRuleContext
+		if rule.Unary_expression() != nil {
+			list = append(list, rule.Unary_expression())
+		}
+		if rule.Case_statement() != nil {
+			list = append(list, rule.Case_statement())
+		}
+		// TODO(rebelice): Add following rules
+		if rule.Quantified_expression() != nil {
+			list = append(list, rule.Quantified_expression())
+		}
+		if rule.Standard_function() != nil {
+			list = append(list, rule.Standard_function())
+		}
+		if rule.Atom() != nil {
+			list = append(list, rule.Atom())
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.ICase_statementContext:
+		var list []antlr.ParserRuleContext
+		if rule.Simple_case_statement() != nil {
+			list = append(list, rule.Simple_case_statement())
+		}
+		// TODO(rebelice): Add following rules
+		if rule.Searched_case_statement() != nil {
+			list = append(list, rule.Searched_case_statement())
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.ISimple_case_statementContext:
+		var list []antlr.ParserRuleContext
+		if rule.Expression() != nil {
+			list = append(list, rule.Expression())
+		}
+		for _, item := range rule.AllSimple_case_when_part() {
+			list = append(list, item)
+		}
+		// TODO(rebelice): Add following rules
+		if rule.Case_else_part() != nil {
+			list = append(list, rule.Case_else_part())
+		}
+		return extractor.plsqlExistSensitiveExpression(list)
+	case plsql.ISimple_case_when_partContext:
+		// TODO(rebelice): implement this
+	}
+
+	return false, nil
+}
+
+func (extractor *sensitiveFieldExtractor) plsqlExistSensitiveExpression(list []antlr.ParserRuleContext) (bool, error) {
+	for _, ctx := range list {
+		sensitive, err := extractor.plsqlIsSensitiveExpression(ctx)
+		if err != nil {
+			return false, err
+		}
+		if sensitive {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
 func (extractor *sensitiveFieldExtractor) plsqlExtractFromClause(ctx plsql.IFrom_clauseContext) ([]fieldInfo, error) {
