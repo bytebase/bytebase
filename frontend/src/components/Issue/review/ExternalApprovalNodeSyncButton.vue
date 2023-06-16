c0ff119a47b5641518226cd83936badf22c4bc5c
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-06-16 18:14:49 +0800

    feat(frontend): implement sync external approval node button (#6688)
    * feat: implement sync external approval node button
    
    * Update frontend/src/components/Issue/review/ExternalApprovalNodeSyncButton.vue
    
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    
    ---------
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    Former-commit-id: f9dd07583e8cf4908c4fa04dc943b01fa3997744
    Former-commit-id: b86d50837e2181506a9ff2ece4339b3f9d45a4ae

---
+++ frontend/src/components/Issue/review/ExternalApprovalNodeSyncButton.vue
@@ -0,0 +1,64 @@
+<template>
+  <NTooltip>
+    <template #trigger>
+      <NButton
+        size="tiny"
+        circle
+        class="!ml-1"
+        :loading="syncing"
+        :tertiary="synced"
+        :type="synced ? 'success' : 'default'"
+        @click="syncNow"
+      >
+        <template #icon>
+          <heroicons:arrow-path v-if="!synced" class="w-4 h-4" />
+          <heroicons:check v-else class="w-4 h-4" />
+        </template>
+      </NButton>
+    </template>
+
+    <div class="whitespace-nowrap">
+      {{ $t("common.sync-now") }}
+    </div>
+  </NTooltip>
+</template>
+
+<script setup lang="ts">
+import { ref, Ref } from "vue";
+import { NButton, NTooltip } from "naive-ui";
+import { useI18n } from "vue-i18n";
+
+import { Issue } from "@/types";
+import { useIssueLogic } from "../logic";
+import { pushNotification, useReviewStore } from "@/store";
+
+const { t } = useI18n();
+const syncing = ref(false);
+const synced = ref(false);
+const issueLogic = useIssueLogic();
+const issue = issueLogic.issue as Ref<Issue>;
+
+const syncNow = async () => {
+  if (syncing.value || synced.value) return;
+
+  syncing.value = true;
+  try {
+    await useReviewStore().fetchReviewByIssue(issue.value, true /* force */);
+
+    synced.value = true;
+    // Show 'synced' status for several seconds to avoid user clicking sync
+    // button too frequently.
+    setTimeout(() => {
+      synced.value = false;
+    }, 5000);
+
+    pushNotification({
+      module: "bytebase",
+      style: "SUCCESS",
+      title: t("common.success"),
+    });
+  } finally {
+    syncing.value = false;
+  }
+};
+</script>
