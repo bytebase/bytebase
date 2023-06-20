47fec39ca6b5ad32999ebd743ec24f149b2aea2e
d-bytebase <d@bytebase.com>
2023-06-20 17:31:32 +0800

    chore: deprecate mysql statement truncating
    Former-commit-id: b12e557eb7c298fe40f5a218fcbfb60b78cea543

--- backend/plugin/db/mysql/mysql.go
+++ backend/plugin/db/mysql/mysql.go
@@ -192,13 +192,6 @@ func (driver *Driver) getVersion(ctx context.Context) (string, error) {
 
 // Execute executes a SQL statement.
 func (driver *Driver) Execute(ctx context.Context, statement string, _ bool) (int64, error) {
-	log.Debug("start to split statement")
-	trunks, err := splitAndTransformDelimiter(statement)
-	if err != nil {
-		return 0, err
-	}
-	log.Debug("finished splitting statements", zap.Int("trunc", len(trunks)))
-
 	tx, err := driver.migrationConn.BeginTx(ctx, nil)
 	if err != nil {
 		return 0, errors.Wrapf(err, "failed to begin execute transaction")
@@ -206,20 +199,16 @@ func (driver *Driver) Execute(ctx context.Context, statement string, _ bool) (in
 	defer tx.Rollback()
 
 	var totalRowsAffected int64
-	for i, trunk := range trunks {
-		log.Debug("starting to run trunk", zap.Int("iteration", i))
-		sqlResult, err := tx.ExecContext(ctx, trunk)
-		if err != nil {
-			return 0, errors.Wrapf(err, "failed to execute context in a transaction")
-		}
-		log.Debug("finished run trunk", zap.Int("iteration", i))
-		rowsAffected, err := sqlResult.RowsAffected()
-		if err != nil {
-			// Since we cannot differentiate DDL and DML yet, we have to ignore the error.
-			log.Debug("rowsAffected returns error", zap.Error(err))
-		}
-		totalRowsAffected += rowsAffected
+	sqlResult, err := tx.ExecContext(ctx, statement)
+	if err != nil {
+		return 0, errors.Wrapf(err, "failed to execute context in a transaction")
 	}
+	rowsAffected, err := sqlResult.RowsAffected()
+	if err != nil {
+		// Since we cannot differentiate DDL and DML yet, we have to ignore the error.
+		log.Debug("rowsAffected returns error", zap.Error(err))
+	}
+	totalRowsAffected += rowsAffected
 
 	if err := tx.Commit(); err != nil {
 		return 0, errors.Wrapf(err, "failed to commit execute transaction")
