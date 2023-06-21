86390fd48aaae19bbdc54fd6f54fcb6729d41a8b
h3n4l <oysterdays@gmail.com>
2023-06-21 18:43:49 +0800

    chore: update

--- backend/tests/grouping_change_test.go
+++ backend/tests/grouping_change_test.go
@@ -2,6 +2,8 @@ package tests
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"strconv"
 	"strings"
@@ -10,7 +12,9 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/genproto/googleapis/type/expr"
 
+	api "github.com/bytebase/bytebase/backend/legacyapi"
 	"github.com/bytebase/bytebase/backend/plugin/db"
+	"github.com/bytebase/bytebase/backend/resources/mysql"
 	"github.com/bytebase/bytebase/backend/tests/fake"
 	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
 )
@@ -522,6 +526,244 @@ func TestCreateTableGroup(t *testing.T) {
 					for _, wantUnmatchedTableName := range wantTableNames.unmatched {
 						a.Contains(gotTableNames.unmatched, wantUnmatchedTableName)
 					}
+				}
+			}
+		})
+	}
+}
+
+func TestCreateGroupingChangeIssue(t *testing.T) {
+	type tableNames struct {
+		matched   []string
+		unmatched []string
+	}
+	type testCasePrepareInstance struct {
+		instanceTitle                  string
+		matchDatabasesNameTableName    map[string]tableNames
+		unmatchedDatabaseNameTableName map[string][]string
+		wantDatabaseTaskStatement      map[string][]string
+	}
+	type tableGroupMetaData struct {
+		tableGroupPlaceholder string
+		tableGroupExpr        string
+	}
+	testCases := []struct {
+		name                     string
+		databaseGroupPlaceholder string
+		databaseGroupExpr        string
+		statement                string
+		tableGroupsMetaData      []tableGroupMetaData
+		prepareInstances         []testCasePrepareInstance
+	}{
+		{
+			name:                     "simple statement",
+			databaseGroupPlaceholder: "employee",
+			databaseGroupExpr:        `(resource.database_name.startsWith("employee_"))`,
+			statement:                "ALTER TABLE salary ADD COLUMN num INT;",
+			tableGroupsMetaData: []tableGroupMetaData{
+				{
+					tableGroupPlaceholder: "salary",
+					tableGroupExpr:        `(resource.table_name.startsWith("salary_"))`,
+				},
+			},
+			prepareInstances: []testCasePrepareInstance{
+				{
+					instanceTitle: "TestCreateDatabaseGroups_AllMatched_OneInstance",
+					matchDatabasesNameTableName: map[string]tableNames{
+						"employee_01": {
+							matched: []string{"salary_01", "salary_02"},
+						},
+						"employee_02": {
+							matched: []string{"salary_03", "salary_04"},
+						},
+					},
+					unmatchedDatabaseNameTableName: map[string][]string{
+						"blog": {"comments"},
+					},
+					wantDatabaseTaskStatement: map[string][]string{
+						"employee_01": {
+							"ALTER TABLE salary_01 ADD COLUMN num INT\n;\n",
+							"ALTER TABLE salary_02 ADD COLUMN num INT\n;\n",
+						},
+						"employee_02": {
+							"ALTER TABLE salary_03 ADD COLUMN num INT\n;\n",
+							"ALTER TABLE salary_04 ADD COLUMN num INT\n;\n",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	a := require.New(t)
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			ctl := &controller{}
+			ctx := context.Background()
+			ctx, err := ctl.StartServerWithExternalPg(ctx, &config{
+				dataDir:            t.TempDir(),
+				vcsProviderCreator: fake.NewGitLab,
+			})
+			a.NoError(err)
+			defer func() {
+				_ = ctl.Close(ctx)
+			}()
+			err = ctl.setLicense()
+			a.NoError(err)
+
+			prodEnvironment, err := ctl.getEnvironment(ctx, "prod")
+			a.NoError(err)
+
+			project, err := ctl.createProject(ctx)
+			a.NoError(err)
+			projectUID, err := strconv.Atoi(project.Uid)
+			a.NoError(err)
+
+			instanceResourceID2InstanceTitle := make(map[string]string)
+			var stopInstances []func()
+
+			for _, prepareInstance := range tc.prepareInstances {
+				mysqlPort := getTestPort()
+				stopInstance := mysql.SetupTestInstance(t, mysqlPort, mysqlBinDir)
+				stopInstances = append(stopInstances, stopInstance)
+
+				instanceResourceID := generateRandomString("instance", 10)
+				instance, err := ctl.instanceServiceClient.CreateInstance(ctx, &v1pb.CreateInstanceRequest{
+					InstanceId: instanceResourceID,
+					Instance: &v1pb.Instance{
+						Title:       prepareInstance.instanceTitle,
+						Engine:      v1pb.Engine_MYSQL,
+						Environment: prodEnvironment.Name,
+						DataSources: []*v1pb.DataSource{{Type: v1pb.DataSourceType_ADMIN, Host: "127.0.0.1", Port: strconv.Itoa(mysqlPort), Username: "root", Password: ""}},
+					},
+				})
+				a.NoError(err)
+				instanceResourceID2InstanceTitle[instanceResourceID] = instance.Title
+
+				for preCreateDatabase := range prepareInstance.matchDatabasesNameTableName {
+					err = ctl.createDatabase(ctx, projectUID, instance, preCreateDatabase, "", nil /* labelMap */)
+					a.NoError(err)
+					dbDriver, err := sql.Open("mysql", fmt.Sprintf("root@tcp(127.0.0.1:%d)/%s", mysqlPort, preCreateDatabase))
+					a.NoError(err)
+
+					for _, preCreateTable := range prepareInstance.matchDatabasesNameTableName[preCreateDatabase].matched {
+						_, err = dbDriver.ExecContext(ctx, fmt.Sprintf(`CREATE TABLE %s (id INT);`, preCreateTable))
+						a.NoError(err)
+					}
+					for _, preCreateTable := range prepareInstance.matchDatabasesNameTableName[preCreateDatabase].unmatched {
+						_, err = dbDriver.ExecContext(ctx, fmt.Sprintf(`CREATE TABLE %s (id INT);`, preCreateTable))
+						a.NoError(err)
+					}
+					err = dbDriver.Close()
+					a.NoError(err)
+
+					_, err = ctl.databaseServiceClient.SyncDatabase(ctx, &v1pb.SyncDatabaseRequest{
+						Name: fmt.Sprintf("%s/databases/%s", instance.Name, preCreateDatabase),
+					})
+					a.NoError(err)
+				}
+				for preCreateDatabase, preCreateTables := range prepareInstance.unmatchedDatabaseNameTableName {
+					err = ctl.createDatabase(ctx, projectUID, instance, preCreateDatabase, "", nil /* labelMap */)
+					a.NoError(err)
+					dbDriver, err := sql.Open("mysql", fmt.Sprintf("root@tcp(127.0.0.1:%d)/%s", mysqlPort, preCreateDatabase))
+					a.NoError(err)
+
+					for _, preCreateTable := range preCreateTables {
+						_, err = dbDriver.ExecContext(ctx, fmt.Sprintf(`CREATE TABLE %s (id INT);`, preCreateTable))
+						a.NoError(err)
+					}
+					err = dbDriver.Close()
+					a.NoError(err)
+
+					_, err = ctl.databaseServiceClient.SyncDatabase(ctx, &v1pb.SyncDatabaseRequest{
+						Name: fmt.Sprintf("%s/databases/%s", instance.Name, preCreateDatabase),
+					})
+					a.NoError(err)
+				}
+			}
+
+			for _, stopInstance := range stopInstances {
+				defer stopInstance()
+			}
+
+			databaseGroup, err := ctl.projectServiceClient.CreateDatabaseGroup(ctx, &v1pb.CreateDatabaseGroupRequest{
+				Parent:          project.Name,
+				DatabaseGroupId: tc.databaseGroupPlaceholder,
+				DatabaseGroup: &v1pb.DatabaseGroup{
+					DatabasePlaceholder: tc.databaseGroupPlaceholder,
+					DatabaseExpr: &expr.Expr{
+						Expression: fmt.Sprintf(`(resource.environment_name == "environments/prod" && (%s))`, tc.databaseGroupExpr),
+					},
+				},
+			})
+			a.NoError(err)
+
+			databaseGroup, err = ctl.projectServiceClient.GetDatabaseGroup(ctx, &v1pb.GetDatabaseGroupRequest{
+				Name: databaseGroup.Name,
+				View: v1pb.DatabaseGroupView_DATABASE_GROUP_VIEW_FULL,
+			})
+			a.NoError(err)
+
+			for _, tableGroupMetaData := range tc.tableGroupsMetaData {
+				_, err := ctl.projectServiceClient.CreateSchemaGroup(ctx, &v1pb.CreateSchemaGroupRequest{
+					Parent:        databaseGroup.Name,
+					SchemaGroupId: tableGroupMetaData.tableGroupPlaceholder,
+					SchemaGroup: &v1pb.SchemaGroup{
+						TableExpr: &expr.Expr{
+							Expression: tableGroupMetaData.tableGroupExpr,
+						},
+						TablePlaceholder: tableGroupMetaData.tableGroupPlaceholder,
+					},
+				})
+				a.NoError(err)
+			}
+
+			createContext := &api.MigrationContext{
+				DetailList: []*api.MigrationDetail{
+					{
+						MigrationType:     db.Migrate,
+						DatabaseGroupName: databaseGroup.Name,
+						Statement:         tc.statement,
+						EarliestAllowedTs: 0,
+					},
+				},
+			}
+			createContextBytes, err := json.Marshal(createContext)
+			a.NoError(err)
+
+			issue, err := ctl.createIssue(api.IssueCreate{
+				ProjectID:     projectUID,
+				Name:          fmt.Sprintf("grouping change issue for test %s", t.Name()),
+				Type:          api.IssueDatabaseSchemaUpdate,
+				Description:   "",
+				AssigneeID:    api.SystemBotID,
+				CreateContext: string(createContextBytes),
+				ValidateOnly:  true,
+			})
+			a.NoError(err)
+			a.NotNil(issue)
+			a.Equal(1, len(issue.Pipeline.StageList))
+
+			gotInstanceDatabaseToTaskStatement := make(map[string]map[string][]string)
+			for _, task := range issue.Pipeline.StageList[0].TaskList {
+				a.Equal(api.TaskDatabaseSchemaUpdate, task.Type)
+				if _, ok := gotInstanceDatabaseToTaskStatement[task.Instance.Name]; !ok {
+					gotInstanceDatabaseToTaskStatement[task.Instance.Name] = make(map[string][]string)
+				}
+				gotInstanceDatabaseToTaskStatement[task.Instance.Name][task.Database.Name] = append(gotInstanceDatabaseToTaskStatement[task.Instance.Name][task.Database.Name], task.Statement)
+			}
+
+			for _, prepareInstance := range tc.prepareInstances {
+				a.Contains(gotInstanceDatabaseToTaskStatement, prepareInstance.instanceTitle)
+				gotInstanceDatabaseToTaskStatement := gotInstanceDatabaseToTaskStatement[prepareInstance.instanceTitle]
+				for wantDatabaseName, wantDatabaseStatements := range prepareInstance.wantDatabaseTaskStatement {
+					a.Contains(gotInstanceDatabaseToTaskStatement, wantDatabaseName)
+					gotDatabaseStatements := gotInstanceDatabaseToTaskStatement[wantDatabaseName]
+					a.Equal(len(wantDatabaseStatements), len(gotDatabaseStatements))
+					a.Equal(wantDatabaseStatements, gotDatabaseStatements)
 				}
 			}
 		})
