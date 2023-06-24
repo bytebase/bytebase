e3d0159730c029ff6bd96fb873edc94b5893d219
d-bytebase <d@bytebase.com>
2023-06-24 10:38:43 +0800

    chore: update

--- backend/store/instance_change_history.go
+++ backend/store/instance_change_history.go
@@ -49,7 +49,8 @@ type InstanceChangeHistoryMessage struct {
 	IssueProjectID string
 }
 
-const instanceChangeHistoryTruncateLength = 10240
+// instanceChangeHistoryTruncateLength is the maximum size of a sheet for displaying.
+const instanceChangeHistoryTruncateLength = 500 * 1024
 
 // FindInstanceChangeHistoryMessage is for listing a list of instance change history.
 type FindInstanceChangeHistoryMessage struct {
