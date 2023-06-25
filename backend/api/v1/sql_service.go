bf0af15381160ff6351fac82f4088f87eb05538f
rebelice <yangrebelice@gmail.com>
2023-06-25 12:03:56 +0800

    fix: deal with empty resource list for QUERY (#6808)
    * fix: deal with empty resource list for QUERY
    
    * chore: address comments

--- backend/api/v1/sql_service.go
+++ backend/api/v1/sql_service.go
@@ -1322,6 +1322,14 @@ func (s *SQLService) checkQueryRights(
 		if err != nil {
 			return err
 		}
+		if projectMessage == nil && databaseMessage == nil {
+			// If database not found, skip.
+			continue
+		}
+		if projectMessage == nil {
+			// Never happen
+			return status.Errorf(codes.Internal, "project not found for database: %s", databaseMessage.DatabaseName)
+		}
 		if project == nil {
 			project = projectMessage
 		}
@@ -1331,8 +1339,16 @@ func (s *SQLService) checkQueryRights(
 		databaseMessageMap[database] = databaseMessage
 	}
 
+	if len(databaseMessageMap) == 0 && project == nil {
+		project, _, err = s.getProjectAndDatabaseMessage(ctx, instance, databaseName)
+		if err != nil {
+			return err
+		}
+	}
+
 	if project == nil {
-		return status.Error(codes.NotFound, "project not found")
+		// Never happen
+		return status.Error(codes.Internal, "project not found")
 	}
 
 	projectPolicy, err := s.store.GetProjectPolicy(ctx, &store.GetProjectPolicyMessage{ProjectID: &project.ResourceID})
