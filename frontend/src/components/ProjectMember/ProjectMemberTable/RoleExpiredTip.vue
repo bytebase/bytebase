fe6608ebce34877e988e24834902b286b8f21811
boojack <stevenlgtm@gmail.com>
2023-06-25 11:07:06 +0800

    chore: show warning for expired roles (#6806)

---
+++ frontend/src/components/ProjectMember/ProjectMemberTable/RoleExpiredTip.vue
@@ -0,0 +1,12 @@
+<template>
+  <NTooltip>
+    <template #trigger>
+      <heroicons-outline:information-circle class="w-4 h-4 text-yellow-600" />
+    </template>
+    {{ $t("role.expired-tip") }}
+  </NTooltip>
+</template>
+
+<script lang="ts" setup>
+import { NTooltip } from "naive-ui";
+</script>
