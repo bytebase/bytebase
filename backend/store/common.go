04ac48948f2d82293ab6aacf3b43c7f450fd126c
boojack <stevenlgtm@gmail.com>
2023-06-26 17:16:40 +0800

    chore: add row status definition (#6844)
    * chore: add row status definition
    
    * chore: update
    
    Former-commit-id: 56f497fe287a850f75d2db39d059c9284b34f8db

---
+++ backend/store/common.go
@@ -0,0 +1,11 @@
+package store
+
+// RowStatus is the status for a row.
+type RowStatus string
+
+const (
+	// Normal is the status for a normal row.
+	Normal RowStatus = "NORMAL"
+	// Archived is the status for an archived row.
+	Archived RowStatus = "ARCHIVED"
+)
