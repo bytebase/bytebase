5427e1d7420c070ae7693ca9d8e7d72e591a4ab7
boojack <stevenlgtm@gmail.com>
2023-06-26 14:29:19 +0800

    chore: add `store/policy.proto` (#6839)
    * chore: add store policy proto
    
    * chore: update
    
    * chore: buf generate
    
    Former-commit-id: bc6efcc7bcdd10d4b5e0325326b11788eb1b26e2
    Former-commit-id: 9b307d6791bea002119b9391a121d2ebe83c4ac2

---
+++ frontend/src/types/proto/store/policy.ts
@@ -0,0 +1,194 @@
+/* eslint-disable */
+import * as _m0 from "protobufjs/minimal";
+import { Expr } from "../google/type/expr";
+
+export const protobufPackage = "bytebase.store";
+
+export interface IamPolicy {
+  /** Collection of binding. */
+  bindings: Binding[];
+}
+
+/** Reference: https://cloud.google.com/pubsub/docs/reference/rpc/google.iam.v1#binding */
+export interface Binding {
+  /**
+   * Role that is assigned to the list of members.
+   * Format: roles/{role}
+   */
+  role: string;
+  /**
+   * Specifies the principals requesting access for a Bytebase resource.
+   * `members` can have the following values:
+   *
+   * * `allUsers`: A special identifier that represents anyone.
+   * * `user:{emailid}`: An email address that represents a specific Bytebase account. For example, `alice@example.com`.
+   */
+  members: string[];
+  /**
+   * The condition that is associated with this binding.
+   * If the condition evaluates to true, then this binding applies to the current request.
+   * If the condition evaluates to false, then this binding does not apply to the current request. However, a different role binding might grant the same role to one or more of the principals in this binding.
+   */
+  condition?: Expr;
+}
+
+function createBaseIamPolicy(): IamPolicy {
+  return { bindings: [] };
+}
+
+export const IamPolicy = {
+  encode(message: IamPolicy, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    for (const v of message.bindings) {
+      Binding.encode(v!, writer.uint32(10).fork()).ldelim();
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): IamPolicy {
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseIamPolicy();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          if (tag !== 10) {
+            break;
+          }
+
+          message.bindings.push(Binding.decode(reader, reader.uint32()));
+          continue;
+      }
+      if ((tag & 7) === 4 || tag === 0) {
+        break;
+      }
+      reader.skipType(tag & 7);
+    }
+    return message;
+  },
+
+  fromJSON(object: any): IamPolicy {
+    return { bindings: Array.isArray(object?.bindings) ? object.bindings.map((e: any) => Binding.fromJSON(e)) : [] };
+  },
+
+  toJSON(message: IamPolicy): unknown {
+    const obj: any = {};
+    if (message.bindings) {
+      obj.bindings = message.bindings.map((e) => e ? Binding.toJSON(e) : undefined);
+    } else {
+      obj.bindings = [];
+    }
+    return obj;
+  },
+
+  create(base?: DeepPartial<IamPolicy>): IamPolicy {
+    return IamPolicy.fromPartial(base ?? {});
+  },
+
+  fromPartial(object: DeepPartial<IamPolicy>): IamPolicy {
+    const message = createBaseIamPolicy();
+    message.bindings = object.bindings?.map((e) => Binding.fromPartial(e)) || [];
+    return message;
+  },
+};
+
+function createBaseBinding(): Binding {
+  return { role: "", members: [], condition: undefined };
+}
+
+export const Binding = {
+  encode(message: Binding, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.role !== "") {
+      writer.uint32(10).string(message.role);
+    }
+    for (const v of message.members) {
+      writer.uint32(18).string(v!);
+    }
+    if (message.condition !== undefined) {
+      Expr.encode(message.condition, writer.uint32(26).fork()).ldelim();
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): Binding {
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseBinding();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          if (tag !== 10) {
+            break;
+          }
+
+          message.role = reader.string();
+          continue;
+        case 2:
+          if (tag !== 18) {
+            break;
+          }
+
+          message.members.push(reader.string());
+          continue;
+        case 3:
+          if (tag !== 26) {
+            break;
+          }
+
+          message.condition = Expr.decode(reader, reader.uint32());
+          continue;
+      }
+      if ((tag & 7) === 4 || tag === 0) {
+        break;
+      }
+      reader.skipType(tag & 7);
+    }
+    return message;
+  },
+
+  fromJSON(object: any): Binding {
+    return {
+      role: isSet(object.role) ? String(object.role) : "",
+      members: Array.isArray(object?.members) ? object.members.map((e: any) => String(e)) : [],
+      condition: isSet(object.condition) ? Expr.fromJSON(object.condition) : undefined,
+    };
+  },
+
+  toJSON(message: Binding): unknown {
+    const obj: any = {};
+    message.role !== undefined && (obj.role = message.role);
+    if (message.members) {
+      obj.members = message.members.map((e) => e);
+    } else {
+      obj.members = [];
+    }
+    message.condition !== undefined && (obj.condition = message.condition ? Expr.toJSON(message.condition) : undefined);
+    return obj;
+  },
+
+  create(base?: DeepPartial<Binding>): Binding {
+    return Binding.fromPartial(base ?? {});
+  },
+
+  fromPartial(object: DeepPartial<Binding>): Binding {
+    const message = createBaseBinding();
+    message.role = object.role ?? "";
+    message.members = object.members?.map((e) => e) || [];
+    message.condition = (object.condition !== undefined && object.condition !== null)
+      ? Expr.fromPartial(object.condition)
+      : undefined;
+    return message;
+  },
+};
+
+type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
+
+export type DeepPartial<T> = T extends Builtin ? T
+  : T extends Array<infer U> ? Array<DeepPartial<U>> : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
+  : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
+
+function isSet(value: any): boolean {
+  return value !== null && value !== undefined;
+}
