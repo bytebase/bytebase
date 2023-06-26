90c5ba06952f3240f4a314b4a3913bff1bb32e60
ecmadao <wlec@outlook.com>
2023-06-26 12:31:50 +0800

    chore: optimize inbox api

--- frontend/src/types/inbox.ts
+++
@@ -1,6 +0,0 @@
-import { LogEntity } from "@/types/proto/v1/logging_service";
-import { InboxMessage } from "@/types/proto/v1/inbox_service";
-
-export interface ComposedInbox extends InboxMessage {
-  activity: LogEntity;
-}
