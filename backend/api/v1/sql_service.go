86833f4f7d36bdbb57aff96c94c60616286b12d7
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-06-26 14:19:08 +0800

    chore: better error handling and response size check for admin execute (#6838)
    * chore: better error handling and response size check for admin execute
    
    * chore: update
    
    Former-commit-id: 994e0b93db6a51000271f471bb214c2a60c77596
    Former-commit-id: 4ddccca1896e99bc05ba723441e32e40515da32e

--- backend/api/v1/sql_service.go
+++ backend/api/v1/sql_service.go
@@ -142,16 +142,29 @@ func (s *SQLService) AdminExecute(server v1pb.SQLService_AdminExecuteServer) err
 		result, durationNs, queryErr := s.doAdminExecute(ctx, driver, conn, request)
 
 		if err := s.postAdminExecute(ctx, activity, durationNs, queryErr); err != nil {
-			return err
+			log.Error("failed to post admin execute activity", zap.Error(err))
 		}
 
+		response := &v1pb.AdminExecuteResponse{}
 		if queryErr != nil {
-			return status.Errorf(codes.Internal, "failed to execute statement: %v", queryErr)
+			response.Results = []*v1pb.QueryResult{
+				{
+					Error: err.Error(),
+				},
+			}
+		} else {
+			response.Results = result
 		}
 
-		if err := server.Send(&v1pb.AdminExecuteResponse{
-			Results: result,
-		}); err != nil {
+		if proto.Size(response) > maximumSQLResultSize {
+			response.Results = []*v1pb.QueryResult{
+				{
+					Error: fmt.Sprintf("Output of query exceeds max allowed output size of %dMB", maximumSQLResultSize/1024/1024),
+				},
+			}
+		}
+
+		if err := server.Send(response); err != nil {
 			return status.Errorf(codes.Internal, "failed to send response: %v", err)
 		}
 	}
