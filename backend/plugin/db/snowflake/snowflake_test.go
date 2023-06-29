06a49669d6deef905a77cbbc14d3cf978d35c0da
h3n4l <oysterdays@gmail.com>
2023-06-29 13:27:08 +0800

    fix(driver): percent encoding for mongodb and snowflake (#6866)
    * fix: percent encoding for mongodb and snowflake
    
    * Update backend/plugin/db/mongodb/mongodb.go
    
    * chore: using url package to build mongodb connection uri
    
    * chore: remove testhh
    
    * refactor: using driver provided function to build snowflake dsn
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: fce51022592d893cd4c5f76cfc64dac830c7bba4

---
+++ backend/plugin/db/snowflake/snowflake_test.go
@@ -0,0 +1,62 @@
+// Package snowflake is the plugin for Snowflake driver.
+package snowflake
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/bytebase/bytebase/backend/plugin/db"
+)
+
+func TestBuildSnowflakeDSN(t *testing.T) {
+	testCases := []struct {
+		input db.ConnectionConfig
+		want  string
+	}{
+		{
+			input: db.ConnectionConfig{
+				Host:     "nb47110.ap-southeast-1",
+				Port:     "443",
+				Username: "bytebase",
+				Password: "pwd",
+			},
+			want: "bytebase:pwd@nb47110.ap-southeast-1.snowflakecomputing.com:443?ocspFailOpen=true&region=ap-southeast-1&validateDefaultParameters=true",
+		},
+		{
+			input: db.ConnectionConfig{
+				Host:     "nb47110.ap-southeast-1",
+				Port:     "443",
+				Username: "bytebase",
+				Password: "pwd",
+				Database: "SAMPLE_DB",
+			},
+			want: "bytebase:pwd@nb47110.ap-southeast-1.snowflakecomputing.com:443?database=SAMPLE_DB&ocspFailOpen=true&region=ap-southeast-1&validateDefaultParameters=true",
+		},
+		{
+			input: db.ConnectionConfig{
+				Host:     "nb47110.ap-southeast-1@10.0.0.1",
+				Port:     "4182",
+				Username: "bytebase",
+				Password: "pwd",
+				Database: "",
+			},
+			want: "bytebase:pwd@10.0.0.1:443?account=nb47110&ocspFailOpen=true&region=ap-southeast-1&validateDefaultParameters=true",
+		},
+		{
+			input: db.ConnectionConfig{
+				Host:     "nb47110.ap-southeast-1@10.0.0.1",
+				Port:     "4182",
+				Username: "bytebase",
+				Password: "pwd",
+				Database: "SAMPLE_DB",
+			},
+			want: "bytebase:pwd@10.0.0.1:443?account=nb47110&database=SAMPLE_DB&ocspFailOpen=true&region=ap-southeast-1&validateDefaultParameters=true",
+		},
+	}
+	for _, testCase := range testCases {
+		got, _ /* redacted */, err := buildSnowflakeDSN(testCase.input)
+		require.NoError(t, err)
+		require.Equal(t, testCase.want, got)
+	}
+}
