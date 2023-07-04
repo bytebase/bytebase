d686947c4309c7c228583016a4fea8a6fd6000de
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-07-04 11:59:59 +0800

    chore: allow Mongo and Redis to query in SQL Editor (#6938)

--- backend/api/v1/sql_service.go
+++ backend/api/v1/sql_service.go
@@ -1349,6 +1349,8 @@ func (*SQLService) validateQueryRequest(instance *store.InstanceMessage, databas
 		if err := parser.PLSQLValidateForEditor(tree); err != nil {
 			return status.Errorf(codes.InvalidArgument, err.Error())
 		}
+	case db.MongoDB, db.Redis:
+		// Do nothing.
 	default:
 		// TODO(rebelice): support multiple statements here.
 		if !parser.ValidateSQLForEditor(convertToParserEngine(instance.Engine), statement) {
