64843aba44ad5f18e2a6cf6d662d3da8ae313ff0
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-07-04 11:34:30 +0800

    chore: remove deprecated sql admin API (#6937)

--- backend/server/server.go
+++ backend/server/server.go
@@ -515,7 +515,6 @@ func NewServer(ctx context.Context, profile config.Profile) (*Server, error) {
 	s.registerIssueSubscriberRoutes(apiGroup)
 	s.registerTaskRoutes(apiGroup)
 	s.registerStageRoutes(apiGroup)
-	s.registerSQLRoutes(apiGroup)
 
 	// Register healthz endpoint.
 	e.GET("/healthz", func(c echo.Context) error {
