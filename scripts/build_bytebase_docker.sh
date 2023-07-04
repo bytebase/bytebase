edd6f2ce76c501a00d24f14e919544132c31b0e7
Nash Tsai <nash.tsai@gmail.com>
2023-07-04 23:58:04 +0800

    chore: update Dockerfile build - setup buildx for multi-arch platforms; setup CN mirrors repository; setup build caches

--- scripts/build_bytebase_docker.sh
+++ scripts/build_bytebase_docker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # ===========================================================================
 # File: build_docker.sh
 # Description: usage: ./build_docker.sh
@@ -12,19 +12,47 @@ cd "$(dirname "$0")/../"
 
 echo "Start building Bytebase docker image ${VERSION}..."
 
-docker build -f ./scripts/Dockerfile \
+BUILDX_ENABLED=${BUILDX_ENABLED:-}
+if [ -z "${BUILDX_ENABLED}" ]; then
+    BUILDER_STATUS=$(docker buildx inspect 2>/dev/null | awk '/Status/ { print $2 }')
+    if [ "${BUILDER_STATUS}" == "running" ]; then
+		BUILDX_ENABLED=true
+	else
+		BUILDX_ENABLED=false
+	fi
+fi
+
+BUILD_PLATFORMS=${BUILD_PLATFORMS:-linux/amd64,linux/arm64}
+BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+if [ "${BUILDX_ENABLED}" == "true" ]; then
+    DOCKER_BUILD_CMD="buildx build --platform ${BUILD_PLATFORMS}"
+    DOCKER_PUSH_CMD_HELP="$ docker ${DOCKER_BUILD_CMD} -f ./scripts/Dockerfile \
     --build-arg VERSION=${VERSION} \
     --build-arg GO_VERSION="$(go version)" \
     --build-arg GIT_COMMIT="$(git rev-parse HEAD)"\
-    --build-arg BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"  \
+    --build-arg BUILD_TIME="${BUILD_TIME}" \
     --build-arg BUILD_USER="$(id -u -n)" \
-    -t bytebase/bytebase .
+    --tag bytebase/bytebase:${VERSION} \
+    --push ."
+else
+    DOCKER_BUILD_CMD="build"
+    DOCKER_PUSH_CMD_HELP="$$ docker tag bytebase/bytebase bytebase/bytebase:${VERSION}; docker push bytebase/bytebase:${VERSION}"
+fi
+
+DOCKER_BUILDKIT=1 docker ${DOCKER_BUILD_CMD} -f ./scripts/Dockerfile \
+    --build-arg VERSION=${VERSION} \
+    --build-arg GO_VERSION="$(go version)" \
+    --build-arg GIT_COMMIT="$(git rev-parse HEAD)"\
+    --build-arg BUILD_TIME="${BUILD_TIME}" \
+    --build-arg BUILD_USER="$(id -u -n)" \
+    --tag bytebase/bytebase .
 
 echo "${GREEN}Completed building Bytebase docker image ${VERSION}.${NC}"
 echo ""
 echo "Command to tag and push the image"
 echo ""
-echo "$ docker tag bytebase/bytebase bytebase/bytebase:${VERSION}; docker push bytebase/bytebase:${VERSION}"
+echo ${DOCKER_PUSH_CMD_HELP} 
 echo ""
 echo "Command to start Bytebase on port 8080"
 echo ""
