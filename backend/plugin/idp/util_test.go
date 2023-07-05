e62cca69d46306e224ce8e48fb5da41411c9f6e2
steven <stevenlgtm@gmail.com>
2023-07-05 17:41:06 +0800

    chore: update

--- backend/plugin/idp/util_test.go
+++ backend/plugin/idp/util_test.go
@@ -1,6 +1,11 @@
 package idp
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestGetValueWithKey(t *testing.T) {
 	tests := []struct {
@@ -28,6 +33,35 @@ func TestGetValueWithKey(t *testing.T) {
 
 	for _, test := range tests {
 		got := GetValueWithKey(test.data, test.key)
+		if test.want != got {
+			t.Errorf("GetValueWithKey got %v but want %v", got, test.want)
+		}
+	}
+}
+
+func TestGetValueWithKeyFromJSONString(t *testing.T) {
+	tests := []struct {
+		data string
+		key  string
+		want any
+	}{
+		{
+			data: `{"user": "test"}`,
+			key:  "user",
+			want: "test",
+		},
+		{
+			data: `{"user":"test","data":{"user":"data-user"}}`,
+			key:  "data.user",
+			want: "data-user",
+		},
+	}
+
+	for _, test := range tests {
+		var data map[string]interface{}
+		err := json.Unmarshal([]byte(test.data), &data)
+		require.NoError(t, err)
+		got := GetValueWithKey(data, test.key)
 		if test.want != got {
 			t.Errorf("GetValueWithKey got %v but want %v", got, test.want)
 		}
