cffed489d765dcc8af88ef1ae3caf092fd689602
d-bytebase <d@bytebase.com>
2023-07-11 00:19:49 +0800

    chore: restruct stage and pipeline

--- backend/store/rollout.go
+++ backend/store/rollout.go
@@ -1,18 +1,1 @@
 package store
-
-import api "github.com/bytebase/bytebase/backend/legacyapi"
-
-// Rollout is the API message for creating a pipeline.
-type Rollout struct {
-	Name      string
-	StageList []RolloutStage
-}
-
-// RolloutStage is the API message for a rollout stage.
-type RolloutStage struct {
-	Name             string
-	EnvironmentID    int
-	PipelineID       int
-	TaskList         []TaskMessage
-	TaskIndexDAGList []api.TaskIndexDAG
-}
