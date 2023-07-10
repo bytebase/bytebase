210e246ac6d67394c32c42d9a7821656707990e8
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-07-11 00:11:25 +0800

    chore: consolidate task message (#7018)
    Former-commit-id: 9c35944f8726437643fd56ed697152d9f27c05f7
    Former-commit-id: 8c8a4ee446ff5af19c924fbe3b941666395b7182

--- backend/store/rollout.go
+++ backend/store/rollout.go
@@ -13,31 +13,6 @@ type RolloutStage struct {
 	Name             string
 	EnvironmentID    int
 	PipelineID       int
-	TaskList         []RolloutTask
+	TaskList         []TaskMessage
 	TaskIndexDAGList []api.TaskIndexDAG
-}
-
-// RolloutTask is the API message for a rollout task.
-type RolloutTask struct {
-	// Standard fields
-	// Value is assigned from the jwt subject field passed by the client.
-	CreatorID int
-
-	// Related fields
-	PipelineID int
-	StageID    int
-	InstanceID int
-	// Tasks such as creating database may not have database.
-	DatabaseID *int
-
-	// Domain specific fields
-	Name   string
-	Status api.TaskStatus
-	Type   api.TaskType
-	// Payload is derived from fields below it
-	Payload           string
-	EarliestAllowedTs int64
-	DatabaseName      string
-	// Statement used by grouping batch change, Bytebase use it to render.
-	Statement string
 }
