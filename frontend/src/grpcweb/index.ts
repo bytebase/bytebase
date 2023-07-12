420a2c320e78defd3d44a3d03988bc5ae516e99b
Steven <stevenlgtm@gmail.com>
2023-07-12 11:02:13 +0800

    Revert "chore: add schema design pinia store"
    This reverts commit 0a240a696f19aa05730ec9a06a6253e7ade5798d.

--- frontend/src/grpcweb/index.ts
+++ frontend/src/grpcweb/index.ts
@@ -30,7 +30,6 @@ import { LoggingServiceDefinition } from "@/types/proto/v1/logging_service";
 import { BookmarkServiceDefinition } from "@/types/proto/v1/bookmark_service";
 import { InboxServiceDefinition } from "@/types/proto/v1/inbox_service";
 import { AnomalyServiceDefinition } from "@/types/proto/v1/anomaly_service";
-import { SchemaDesignServiceDefinition } from "@/types/proto/v1/schema_design_service";
 
 // Create each grpc service client.
 // Reference: https://github.com/deeplay-io/nice-grpc/blob/master/packages/nice-grpc-web/README.md
@@ -174,11 +173,6 @@ export const inboxServiceClient = clientFactory.create(
 
 export const anomalyServiceClient = clientFactory.create(
   AnomalyServiceDefinition,
-  channel
-);
-
-export const schemaDesignServiceClient = clientFactory.create(
-  SchemaDesignServiceDefinition,
   channel
 );
 
