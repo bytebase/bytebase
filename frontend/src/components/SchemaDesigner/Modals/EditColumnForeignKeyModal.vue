0a240a696f19aa05730ec9a06a6253e7ade5798d
Steven <stevenlgtm@gmail.com>
2023-07-12 11:02:07 +0800

    chore: add schema design pinia store

---
+++ frontend/src/components/SchemaDesigner/Modals/EditColumnForeignKeyModal.vue
@@ -0,0 +1,277 @@
+<template>
+  <BBModal
+    :title="$t('schema-editor.edit-foreign-key')"
+    class="shadow-inner outline outline-gray-200"
+    @close="dismissModal"
+  >
+    <div v-if="shouldShowSchemaSelector" class="w-72">
+      <p class="mb-2">{{ $t("schema-editor.select-reference-schema") }}</p>
+      <BBSelect
+        :selected-item="selectedSchema"
+        :item-list="schemaList"
+        :placeholder="$t('schema-editor.schema.select')"
+        :show-prefix-item="true"
+        @select-item="(schema: Schema) => state.referencedSchemaId = schema.id"
+      >
+        <template #menuItem="{ item }">
+          {{ item.name }}
+        </template>
+      </BBSelect>
+    </div>
+    <div class="w-72">
+      <p class="mt-4 mb-2">{{ $t("schema-editor.select-reference-table") }}</p>
+      <BBSelect
+        :selected-item="selectedTable"
+        :item-list="tableList"
+        :placeholder="$t('schema-editor.table.select')"
+        :show-prefix-item="true"
+        @select-item="(table: Table) => state.referencedTableId = table.id"
+      >
+        <template #menuItem="{ item }">
+          {{ item.name }}
+        </template>
+      </BBSelect>
+    </div>
+    <div class="w-72">
+      <p class="mt-4 mb-2">{{ $t("schema-editor.select-reference-column") }}</p>
+      <BBSelect
+        :selected-item="selectedColumn"
+        :item-list="columnList"
+        :placeholder="$t('schema-editor.column.select')"
+        :show-prefix-item="true"
+        @select-item="(column: Column) => state.referencedColumnId = column.id"
+      >
+        <template #menuItem="{ item }">
+          {{ item.name }}
+        </template>
+      </BBSelect>
+    </div>
+    <div
+      class="w-full flex items-center justify-between mt-6 space-x-2 pr-1 pb-1"
+    >
+      <div class="flex items-center justify-start">
+        <button
+          v-if="foreignKey !== undefined"
+          type="button"
+          class="btn-danger"
+          @click="handleRemoveFKButtonClick"
+        >
+          Remove
+        </button>
+      </div>
+      <div class="flex items-center justify-end space-x-2">
+        <button type="button" class="btn-normal" @click="dismissModal">
+          {{ $t("common.cancel") }}
+        </button>
+        <button class="btn-primary" @click="handleConfirmButtonClick">
+          {{ $t("common.save") }}
+        </button>
+      </div>
+    </div>
+  </BBModal>
+</template>
+
+<script lang="ts" setup>
+import { isUndefined } from "lodash-es";
+import { computed, onMounted, reactive, watch } from "vue";
+import { v1 as uuidv1 } from "uuid";
+import { UNKNOWN_ID } from "@/types";
+import { useSchemaEditorStore } from "@/store";
+import {
+  Column,
+  ForeignKey,
+  Schema,
+  Table,
+} from "@/types/schemaEditor/atomType";
+import { BBModal, BBSelect } from "@/bbkit";
+import { Engine } from "@/types/proto/v1/common";
+
+interface LocalState {
+  referencedSchemaId?: string;
+  referencedTableId?: string;
+  referencedColumnId?: string;
+}
+
+const props = defineProps({
+  databaseId: {
+    type: String,
+    default: String(UNKNOWN_ID),
+  },
+  schemaId: {
+    type: String,
+    default: "",
+  },
+  tableId: {
+    type: String,
+    default: "",
+  },
+  columnId: {
+    type: String,
+    default: "",
+  },
+});
+
+const emit = defineEmits<{
+  (event: "close"): void;
+}>();
+
+const editorStore = useSchemaEditorStore();
+const databaseSchema = computed(() => {
+  return editorStore.databaseSchemaById.get(props.databaseId)!;
+});
+const database = computed(() => databaseSchema.value.database);
+const databaseEngine = computed(() => database.value.instanceEntity.engine);
+const schemaList = computed(() => databaseSchema.value.schemaList);
+const state = reactive<LocalState>({
+  referencedSchemaId: props.schemaId,
+});
+
+const schema = computed(() => {
+  return editorStore.getSchema(props.databaseId, props.schemaId) as Schema;
+});
+
+const table = computed(() => {
+  return editorStore.getTable(props.databaseId, props.schemaId, props.tableId);
+});
+
+const propsColumn = computed(() => {
+  return table.value?.columnList.find((column) => column.id === props.columnId);
+});
+
+const foreignKeyList = computed(() => {
+  return schema.value?.foreignKeyList.filter(
+    (pk) => pk.tableId === props.tableId
+  );
+});
+
+const shouldShowSchemaSelector = computed(() => {
+  return databaseEngine.value === Engine.POSTGRES;
+});
+
+const selectedSchema = computed(() => {
+  return schemaList.value.find(
+    (schema) => schema.id === state.referencedSchemaId
+  );
+});
+
+const tableList = computed(() => {
+  return selectedSchema.value?.tableList || [];
+});
+
+const selectedTable = computed(() => {
+  return tableList.value.find(
+    (table: Table) => table.id === state.referencedTableId
+  );
+});
+
+const columnList = computed(() => {
+  if (!selectedTable.value) {
+    return [];
+  }
+
+  return selectedTable.value.columnList.filter(
+    (column) =>
+      column.id !== props.columnId &&
+      column.type.toUpperCase() === propsColumn.value?.type.toUpperCase() &&
+      selectedTable.value?.primaryKey.columnIdList.includes(column.id)
+  );
+});
+
+const selectedColumn = computed(() => {
+  return columnList.value.find(
+    (column) => column.id === state.referencedColumnId
+  );
+});
+
+const foreignKey = computed(() => {
+  for (const fk of foreignKeyList.value) {
+    const foundIndex = fk.columnIdList.findIndex(
+      (columnId) => columnId === props.columnId
+    );
+    if (foundIndex > -1) {
+      return fk;
+    }
+  }
+  return undefined;
+});
+
+onMounted(() => {
+  if (foreignKey.value) {
+    const foundIndex = foreignKey.value.columnIdList.findIndex(
+      (columnId) => columnId === props.columnId
+    );
+    if (foundIndex > -1) {
+      state.referencedSchemaId = foreignKey.value.referencedSchemaId;
+      state.referencedTableId = foreignKey.value.referencedTableId;
+      state.referencedColumnId =
+        foreignKey.value.referencedColumnIdList[foundIndex];
+    }
+  }
+});
+
+watch(
+  () => state.referencedTableId,
+  () => {
+    const found = columnList.value.find(
+      (column) => column.id === state.referencedColumnId
+    );
+    if (!found) {
+      state.referencedColumnId = undefined;
+    }
+  }
+);
+
+const handleRemoveFKButtonClick = async () => {
+  if (isUndefined(foreignKey.value)) {
+    return;
+  }
+
+  const index = foreignKey.value.columnIdList.findIndex(
+    (columnId) => columnId === propsColumn.value?.id
+  );
+  if (index > -1) {
+    foreignKey.value.referencedColumnIdList.splice(index, 1);
+    foreignKey.value.columnIdList.splice(index, 1);
+    dismissModal();
+  }
+};
+
+const handleConfirmButtonClick = async () => {
+  if (
+    isUndefined(state.referencedSchemaId) ||
+    isUndefined(state.referencedTableId) ||
+    isUndefined(state.referencedColumnId)
+  ) {
+    return;
+  }
+
+  const column = propsColumn.value as Column;
+  if (isUndefined(foreignKey.value)) {
+    const fk: ForeignKey = {
+      name: uuidv1(),
+      tableId: props.tableId,
+      columnIdList: [column.id],
+      referencedSchemaId: state.referencedSchemaId,
+      referencedTableId: state.referencedTableId,
+      referencedColumnIdList: [state.referencedColumnId],
+    };
+    schema.value.foreignKeyList.push(fk);
+  } else {
+    const index = foreignKey.value.columnIdList.findIndex(
+      (columnId) => columnId === column.id
+    );
+    if (index >= 0) {
+      foreignKey.value.referencedColumnIdList[index] = state.referencedColumnId;
+      foreignKey.value.columnIdList[index] = column.id;
+    } else {
+      foreignKey.value.referencedColumnIdList.push(state.referencedColumnId);
+      foreignKey.value.columnIdList.push(column.id);
+    }
+  }
+  dismissModal();
+};
+
+const dismissModal = () => {
+  emit("close");
+};
+</script>
---
+++ frontend/src/components/SchemaDesigner/TabsContainer.vue
@@ -0,0 +1,130 @@
+<template>
+  <div
+    ref="tabsContainerRef"
+    class="w-full max-w-full overflow-x-auto bg-gray-100 select-none shrink-0 rounded"
+  >
+    <div class="w-fit flex flex-nowrap">
+      <div
+        v-for="tab in tabList"
+        :key="tab.id"
+        class="relative px-1 pl-2 py-2 rounded w-40 flex flex-row justify-between items-center shrink-0 border border-transparent cursor-pointer"
+        :class="[
+          `tab-${tab.id}`,
+          tab.id === currentTab?.id && 'bg-white border-gray-200',
+        ]"
+        @click="handleSelectTab(tab)"
+      >
+        <div class="flex flex-row justify-start items-center mr-1">
+          <span class="mr-1">
+            <heroicons-outline:circle-stack
+              v-if="tab.type === SchemaEditorTabType.TabForDatabase"
+              class="rounded w-4 h-auto text-gray-400"
+            />
+            <heroicons-outline:table-cells
+              v-else-if="tab.type === SchemaEditorTabType.TabForTable"
+              class="rounded w-4 h-auto text-gray-400"
+            />
+          </span>
+          <NEllipsis
+            class="text-sm w-24"
+            :class="getTabComputedClassList(tab)"
+            >{{ getTabName(tab) }}</NEllipsis
+          >
+        </div>
+        <span class="tab-close-button shrink-0 flex">
+          <heroicons-outline:x
+            class="rounded w-4 h-auto text-gray-400 hover:text-gray-600"
+            @click.stop.prevent="handleCloseTab(tab)"
+          />
+        </span>
+        <div
+          v-if="tab.id === currentTab?.id"
+          class="absolute -bottom-px left-0 w-full h-[3px] bg-accent rounded"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NEllipsis } from "naive-ui";
+import { computed, nextTick, ref, watch } from "vue";
+import scrollIntoView from "scroll-into-view-if-needed";
+import { useSchemaEditorStore } from "@/store";
+import { TabContext, SchemaEditorTabType } from "@/types";
+import { isTableChanged } from "./utils/table";
+
+const editorStore = useSchemaEditorStore();
+const tabsContainerRef = ref();
+const tabList = computed(() => {
+  return Array.from(editorStore.tabState.tabMap.values());
+});
+const currentTab = computed(() => {
+  return editorStore.currentTab;
+});
+
+watch(
+  () => currentTab.value,
+  () => {
+    nextTick(() => {
+      const element = tabsContainerRef.value?.querySelector(
+        `.tab-${currentTab.value?.id}`
+      );
+      if (element) {
+        scrollIntoView(element, {
+          scrollMode: "if-needed",
+        });
+      }
+    });
+  }
+);
+
+const getTabComputedClassList = (tab: TabContext) => {
+  if (tab.type === SchemaEditorTabType.TabForTable) {
+    const table = editorStore.getTableWithTableTab(tab);
+    if (!table) {
+      return [];
+    }
+
+    if (table.status === "dropped") {
+      return ["text-red-700", "line-through"];
+    }
+    if (table.status === "created") {
+      return ["text-green-700"];
+    }
+
+    if (isTableChanged(tab.databaseId, tab.schemaId, tab.tableId)) {
+      return ["text-yellow-700"];
+    }
+  }
+
+  return [];
+};
+
+const getTabName = (tab: TabContext) => {
+  if (tab.type === SchemaEditorTabType.TabForDatabase) {
+    const database = editorStore.databaseList.find(
+      (database) => database.uid === tab.databaseId
+    );
+    return `${database?.databaseName || "unknown database"}`;
+  } else if (tab.type === SchemaEditorTabType.TabForTable) {
+    const table = editorStore.getTable(
+      tab.databaseId,
+      tab.schemaId,
+      tab.tableId
+    );
+    return `${table?.name || ""}`;
+  } else {
+    // Should never reach here.
+    return "unknown structure";
+  }
+};
+
+const handleSelectTab = (tab: TabContext) => {
+  editorStore.setCurrentTab(tab.id);
+};
+
+const handleCloseTab = (tab: TabContext) => {
+  editorStore.closeTab(tab.id);
+};
+</script>
