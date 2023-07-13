692aff04c5806d2553fe489929eedc5da32c8bad
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-07-13 09:53:40 +0800

    chore: ensure initialize sequence

--- frontend/src/components/IssueV1/logic/initialize/index.ts
+++ frontend/src/components/IssueV1/logic/initialize/index.ts
@@ -1,27 +1,57 @@
-import { computed, ref, Ref } from "vue";
-import { useRoute } from "vue-router";
+import { computed, ref, Ref, watch } from "vue";
+import { useRoute, useRouter, _RouteLocationBase } from "vue-router";
+
+import { ComposedIssue, EMPTY_ID, UNKNOWN_ID } from "@/types";
 import { experimentalFetchIssueByUID } from "@/store";
 import { idFromSlug } from "@/utils";
-import { computedAsync } from "@vueuse/core";
 import { createIssue } from "./create";
 
 export function useInitializeIssue(issueSlug: Ref<string>) {
   const isCreating = computed(() => issueSlug.value.toLowerCase() == "new");
+  const uid = computed(() => {
+    const slug = issueSlug.value;
+    if (slug.toLowerCase() === "new") return String(EMPTY_ID);
+    const uid = Number(idFromSlug(slug));
+    if (uid > 0) return String(uid);
+    return String(UNKNOWN_ID);
+  });
   const route = useRoute();
+  const router = useRouter();
   const isInitializing = ref(false);
 
-  const issue = computedAsync(
-    async () => {
-      console.log("call computed async", isCreating.value, issueSlug.value);
-      if (isCreating.value) {
-        return createIssue(route);
-      } else {
-        const uid = String(idFromSlug(issueSlug.value));
-        return await experimentalFetchIssueByUID(uid);
+  const issue = ref<ComposedIssue>();
+
+  const runner = async (uid: string, url: string) => {
+    router.resolve(url);
+    const issue =
+      uid === String(EMPTY_ID)
+        ? await createIssue(router.resolve(url))
+        : await experimentalFetchIssueByUID(uid);
+    return {
+      issue,
+      url,
+    };
+  };
+
+  watch(
+    uid,
+    (uid) => {
+      if (uid === String(UNKNOWN_ID)) {
+        router.push({ name: "error.404" });
+        return;
       }
+      const url = route.fullPath;
+      isInitializing.value = true;
+      runner(uid, url).then((result) => {
+        if (result.url !== route.fullPath) {
+          // the url changed, drop the outdated result
+          return;
+        }
+        issue.value = result.issue;
+        isInitializing.value = false;
+      });
     },
-    undefined,
-    isInitializing
+    { immediate: true }
   );
 
   return { isCreating, issue, isInitializing };
