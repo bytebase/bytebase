b5657e9412ba7cc24dda9b4c29a9679b01225811
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-07-13 09:24:00 +0800

    chore: layout

--- frontend/src/components/IssueV1/components/HeaderSection/HeaderSection.vue
+++ frontend/src/components/IssueV1/components/HeaderSection/HeaderSection.vue
@@ -1,31 +1,36 @@
 <template>
-  <div
-    class="flex flex-col gap-2 md:flex-row md:items-stretch md:justify-between px-4 py-2"
-  >
-    <div class="flex-1">
-      <div class="flex flex-col gap-y-1">
-        <div class="flex items-center gap-x-2">
-          <div v-if="!isCreating">
-            <IssueStatusIcon
-              :issue-status="issue.status"
-              :task-status="issueTaskStatus"
-            />
-          </div>
-
-          <Title />
+  <div class="flex flex-col gap-2 px-4 py-2">
+    <div
+      class="flex flex-col md:flex-row md:items-stretch md:justify-between gap-2"
+    >
+      <div class="flex-1 flex items-center gap-x-2">
+        <div v-if="!isCreating">
+          <IssueStatusIcon
+            :issue-status="issue.status"
+            :task-status="issueTaskStatus"
+          />
         </div>
 
+        <Title />
+      </div>
+
+      <Actions />
+    </div>
+    <div
+      class="flex flex-col md:flex-row md:items-stretch md:justify-between gap-2"
+    >
+      <div class="flex-1 flex flex-col gap-x-2">
         <Description />
 
         <VCSInfo />
 
         <RollbackFromTips />
       </div>
-    </div>
-    <div class="flex flex-col items-end gap-y-4">
-      <Actions />
-      <ReviewSection v-if="!isCreating" />
-      <Assignee />
+
+      <div class="flex flex-col items-end gap-y-2">
+        <ReviewSection v-if="!isCreating" />
+        <Assignee />
+      </div>
     </div>
   </div>
 </template>
