bddb7733ecf2310c1a5f9d32abd56be7983b5276
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-07-14 16:54:08 +0800

    chore: hide button when not needed

--- frontend/src/components/IssueV1/components/TaskListSection/TaskExtraActionsButton.vue
+++ frontend/src/components/IssueV1/components/TaskListSection/TaskExtraActionsButton.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="issue-debug">
+  <div v-if="show" class="issue-debug">
     <heroicons:ellipsis-vertical-solid class="w-4 h-4" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+
 import { Task } from "@/types/proto/v1/rollout_service";
+import { useIssueContext } from "../../logic";
 
 defineProps<{
   task: Task;
 }>();
+
+const { isCreating } = useIssueContext();
+
+const show = computed(() => {
+  if (isCreating.value) {
+    return false;
+  }
+
+  // TODO
+  return true;
+});
 </script>
