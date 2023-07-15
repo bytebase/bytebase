a2171552b70a790066139ec2241df4ddec7cb466
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-07-15 13:36:08 +0800

    chore: update server setting (#7060)
    * chore: update server setting
    
    * chore: update
    
    Former-commit-id: a574573a68c9e3f5e1906140a41a6b0bc3fea30b

--- backend/api/auth/header.go
+++ backend/api/auth/header.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"context"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
@@ -10,50 +11,54 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-// GatewayResponseModifier is the mux option for modifying response header.
-func GatewayResponseModifier(ctx context.Context, response http.ResponseWriter, _ proto.Message) error {
+// GatewayResponseModifier is the response modifier for grpc gateway.
+type GatewayResponseModifier struct {
+	ExternalURL string
+}
+
+// Modify is the mux option for modifying response header.
+func (m *GatewayResponseModifier) Modify(ctx context.Context, response http.ResponseWriter, _ proto.Message) error {
 	md, ok := runtime.ServerMetadataFromContext(ctx)
 	if !ok {
 		return errors.Errorf("failed to get ServerMetadata from context in the gateway response modifier")
 	}
-	processMetadata(md, GatewayMetadataAccessTokenKey, AccessTokenCookieName, true /* httpOnly */, response)
-	processMetadata(md, GatewayMetadataRefreshTokenKey, RefreshTokenCookieName, true /* httpOnly */, response)
-	processMetadata(md, GatewayMetadataUserIDKey, UserIDCookieName, false /* httpOnly */, response)
+	isHTTPS := strings.HasPrefix(m.ExternalURL, "https")
+	processMetadata(md, GatewayMetadataAccessTokenKey, AccessTokenCookieName, true /* httpOnly */, isHTTPS, response)
+	processMetadata(md, GatewayMetadataRefreshTokenKey, RefreshTokenCookieName, true /* httpOnly */, isHTTPS, response)
+	processMetadata(md, GatewayMetadataUserIDKey, UserIDCookieName, false /* httpOnly */, isHTTPS, response)
 	return nil
 }
 
-func processMetadata(md runtime.ServerMetadata, metadataKey, cookieName string, httpOnly bool, response http.ResponseWriter) {
+func processMetadata(md runtime.ServerMetadata, metadataKey, cookieName string, httpOnly, isHTTPS bool, response http.ResponseWriter) {
 	values := md.HeaderMD.Get(metadataKey)
 	if len(values) == 0 {
 		return
 	}
 	value := values[0]
 	if value == "" {
-		unsetCookie(response, cookieName)
+		// Unset cookie.
+		http.SetCookie(response, &http.Cookie{
+			Name:    cookieName,
+			Value:   "",
+			Expires: time.Unix(0, 0),
+			Path:    "/",
+		})
 	} else {
-		setCookie(response, cookieName, value, httpOnly)
+		// Set cookie.
+		sameSite := http.SameSiteStrictMode
+		if isHTTPS {
+			sameSite = http.SameSiteNoneMode
+		}
+		http.SetCookie(response, &http.Cookie{
+			Name:    cookieName,
+			Value:   value,
+			Expires: time.Now().Add(CookieExpDuration),
+			Path:    "/",
+			// Http-only helps mitigate the risk of client side script accessing the protected cookie.
+			HttpOnly: httpOnly,
+			// See https://github.com/bytebase/bytebase/issues/31.
+			Secure:   isHTTPS,
+			SameSite: sameSite,
+		})
 	}
-}
-
-func setCookie(response http.ResponseWriter, key, value string, httpOnly bool) {
-	http.SetCookie(response, &http.Cookie{
-		Name:    key,
-		Value:   value,
-		Expires: time.Now().Add(CookieExpDuration),
-		Path:    "/",
-		// Http-only helps mitigate the risk of client side script accessing the protected cookie.
-		HttpOnly: httpOnly,
-		// For now, we allow Bytebase to run on non-https host, see https://github.com/bytebase/bytebase/issues/31
-		// cookie.Secure = true
-		SameSite: http.SameSiteStrictMode,
-	})
-}
-
-func unsetCookie(response http.ResponseWriter, key string) {
-	http.SetCookie(response, &http.Cookie{
-		Name:    key,
-		Value:   "",
-		Expires: time.Unix(0, 0),
-		Path:    "/",
-	})
 }
