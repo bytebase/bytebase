8c2e07a0643a1056fbbb556caba4d96548e20776
p0ny <xz@bytebase.com>
2023-07-18 16:41:15 +0800

    fix(sql): disable trigger before updating task run (#7115)
    Former-commit-id: 3e71cb63aa9df0ec464db95d43dfe8453009cc0a

--- backend/migrator/migration/prod/2.5/0001##task_run_result_add_change_history.sql
+++ backend/migrator/migration/prod/2.5/0001##task_run_result_add_change_history.sql
@@ -1,3 +1,5 @@
+ALTER TABLE task_run DISABLE TRIGGER update_task_run_updated_ts;
+
 UPDATE task_run
 SET result = result || jsonb_build_object('changeHistory', concat('instances/', instance.resource_id, '/databases/', db.name, '/changeHistories/', instance_change_history.id))
 FROM instance_change_history, instance, db
@@ -6,3 +8,5 @@ WHERE
     AND (result->>'migrationId')::int = instance_change_history.id
     AND instance_change_history.instance_id = instance.id
     AND instance_change_history.database_id = db.id;
+
+ALTER TABLE task_run ENABLE TRIGGER update_task_run_updated_ts;
