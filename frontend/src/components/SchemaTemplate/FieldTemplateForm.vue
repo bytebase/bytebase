067d56eabe278a8626e498f18ba3f633d30ef934
ecmadao <wlec@outlook.com>
2023-07-18 14:43:33 +0800

    chore: support schema field template in frontend (#7104)
    * chore: update setting for schema template
    
    * chore: update
    
    * chore: add missing files
    
    * fix: lint
    
    * chore: update proto
    
    * fix: lint
    
    * chore: update todo comment
    
    * chore: update backend for schema template
    
    * chore: update
    
    * chore: update comment
    
    * chore: support schema field template
    
    * chore: add missing i18n
    
    * fix: eslint
    
    ---------
    
    Co-authored-by: d-bytebase <d@bytebase.com>
    Former-commit-id: 46c00a927b4a0019528d39d77b3a8bfbbc4b26dc

---
+++ frontend/src/components/SchemaTemplate/FieldTemplateForm.vue
@@ -0,0 +1,225 @@
+<template>
+  <DrawerContent :title="$t('schema-template.field-template')">
+    <div class="space-y-6 divide-y divide-block-border">
+      <div class="space-y-6">
+        <div class="w-full mb-6 space-y-1">
+          <label for="engine" class="textlabel">
+            {{ $t("database.engine") }}
+          </label>
+          <div class="grid grid-cols-4 gap-2">
+            <template v-for="engine in engineList" :key="engine">
+              <div
+                class="flex relative justify-start p-2 border rounded"
+                :class="[
+                  state.engine === engine && 'font-medium bg-control-bg-hover',
+                  allowEdit
+                    ? 'cursor-pointer hover:bg-control-bg-hover'
+                    : 'cursor-not-allowed',
+                ]"
+                @click.capture="changeEngine(engine)"
+              >
+                <div class="flex flex-row justify-start items-center">
+                  <input
+                    type="radio"
+                    class="btn mr-2"
+                    :checked="state.engine === engine"
+                  />
+                  <EngineIcon
+                    :engine="engine"
+                    custom-class="w-5 h-auto max-h-[20px] object-contain mr-1"
+                  />
+                  <p class="text-center text-sm">
+                    {{ engineNameV1(engine) }}
+                  </p>
+                </div>
+              </div>
+            </template>
+          </div>
+        </div>
+      </div>
+      <div class="space-y-6 pt-6">
+        <div class="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-4">
+          <!-- column name -->
+          <div class="sm:col-span-2 sm:col-start-1">
+            <label for="column-name" class="textlabel">
+              {{ $t("schema-template.form.column-name") }}
+              <span class="text-red-600 mr-2">*</span>
+            </label>
+            <input
+              v-model="state.column!.name"
+              required
+              name="column-name"
+              type="text"
+              placeholder="column name"
+              class="textfield mt-1 w-full"
+              :disabled="!allowEdit"
+            />
+          </div>
+
+          <!-- type -->
+          <div class="sm:col-span-2 sm:col-start-1">
+            <label for="column-type" class="textlabel">
+              {{ $t("schema-template.form.column-type") }}
+              <span class="text-red-600 mr-2">*</span>
+            </label>
+            <div
+              class="relative flex flex-row justify-between items-center mt-1"
+            >
+              <input
+                v-model="state.column!.type"
+                required
+                name="column-type"
+                type="text"
+                placeholder="column type"
+                class="textfield w-full"
+                :disabled="!allowEdit"
+              />
+              <NDropdown
+                trigger="click"
+                :options="dataTypeOptions"
+                :disabled="!allowEdit"
+                @select="(dataType: string) => (state.column!.type = dataType)"
+              >
+                <button class="absolute right-5">
+                  <heroicons-solid:chevron-up-down
+                    class="w-4 h-auto text-gray-400"
+                  />
+                </button>
+              </NDropdown>
+            </div>
+          </div>
+
+          <!-- default value -->
+          <div class="sm:col-span-2 sm:col-start-1">
+            <label for="default-value" class="textlabel">
+              {{ $t("schema-template.form.default-value") }}
+            </label>
+            <input
+              v-model="state.column!.default"
+              required
+              name="default-value"
+              type="text"
+              class="textfield mt-1 w-full"
+              :placeholder="getDefaultValue(state.column)"
+              :disabled="!allowEdit"
+            />
+          </div>
+
+          <!-- nullable -->
+          <div class="sm:col-span-2 ml-0 sm:ml-3 flex flex-col">
+            <label for="nullable" class="textlabel">
+              {{ $t("schema-template.form.nullable") }}
+            </label>
+            <BBSwitch
+              class="mt-4"
+              :text="false"
+              :value="state.column?.nullable"
+              :disabled="!allowEdit"
+              @toggle="(on: boolean) => state.column!.nullable = on"
+            />
+          </div>
+
+          <!-- comment -->
+          <div class="sm:col-span-4 sm:col-start-1">
+            <label for="comment" class="textlabel">
+              {{ $t("schema-template.form.comment") }}
+            </label>
+            <textarea
+              v-model="state.column!.comment"
+              rows="3"
+              class="textfield block w-full resize-none mt-1 text-sm text-control rounded-md whitespace-pre-wrap"
+              :disabled="!allowEdit"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <template #footer>
+      <div class="w-full flex justify-between items-center">
+        <div class="w-full flex justify-end items-center gap-x-3">
+          <NButton @click.prevent="$emit('dismiss')">
+            {{ $t("common.cancel") }}
+          </NButton>
+          <NButton
+            v-if="!readonly && allowEdit"
+            :disabled="sumbitDisabled"
+            type="primary"
+            @click.prevent="sumbit"
+          >
+            {{ create ? $t("common.create") : $t("common.update") }}
+          </NButton>
+        </div>
+      </div>
+    </template>
+  </DrawerContent>
+</template>
+
+<script lang="ts" setup>
+import { computed, reactive } from "vue";
+import { isEqual } from "lodash-es";
+import { DrawerContent } from "@/components/v2";
+import {
+  getDataTypeSuggestionList,
+  engineNameV1,
+  useWorkspacePermissionV1,
+} from "@/utils";
+import { Engine } from "@/types/proto/v1/common";
+import { useSchemaEditorStore } from "@/store";
+import { SchemaTemplateSetting_FieldTemplate } from "@/types/proto/v1/setting_service";
+import { engineList, getDefaultValue } from "./utils";
+
+const props = defineProps<{
+  create: boolean;
+  readonly?: boolean;
+  template: SchemaTemplateSetting_FieldTemplate;
+}>();
+
+const emit = defineEmits(["dismiss"]);
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface LocalState extends SchemaTemplateSetting_FieldTemplate {}
+
+const state = reactive<LocalState>({
+  id: props.template.id,
+  engine: props.template.engine,
+  column: Object.assign({}, props.template.column),
+});
+const store = useSchemaEditorStore();
+const allowEdit = computed(() => {
+  return (
+    useWorkspacePermissionV1("bb.permission.workspace.manage-general").value &&
+    !props.readonly
+  );
+});
+
+const dataTypeOptions = computed(() => {
+  return getDataTypeSuggestionList(state.engine).map((dataType) => {
+    return {
+      label: dataType,
+      key: dataType,
+    };
+  });
+});
+
+const changeEngine = (engine: Engine) => {
+  if (allowEdit.value) {
+    state.engine = engine;
+  }
+};
+
+const sumbitDisabled = computed(() => {
+  if (!state.column?.name || !state.column?.type) {
+    return true;
+  }
+  if (!props.create && isEqual(props.template, state)) {
+    return true;
+  }
+  return false;
+});
+
+const sumbit = async () => {
+  await store.upsertSchemaTemplate(state);
+  emit("dismiss");
+};
+</script>
--- frontend/src/components/v2/Model/Instance/InstanceV1EngineIcon.vue
+++ frontend/src/components/v2/Model/Instance/InstanceV1EngineIcon.vue
@@ -1,8 +1,8 @@
 <template>
-  <NTooltip v-if="icon" :disabled="!tooltip">
+  <NTooltip :disabled="!tooltip">
     <template #trigger>
       <div class="relative w-4" v-bind="$attrs">
-        <img class="h-4 w-auto mx-auto" :src="icon" />
+        <EngineIcon :engine="instance.engine" />
         <div
           v-if="showStatus"
           class="bg-green-400 border-surface-high rounded-full absolute border-2"
@@ -15,13 +15,11 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, PropType } from "vue";
+import { PropType } from "vue";
 import { NTooltip } from "naive-ui";
-
-import { Engine } from "@/types/proto/v1/common";
 import { Instance } from "@/types/proto/v1/instance_service";
 
-const props = defineProps({
+defineProps({
   instance: {
     required: true,
     type: Object as PropType<Instance>,
@@ -34,33 +32,5 @@ const props = defineProps({
     type: Boolean,
     default: true,
   },
-});
-
-const ICON_PATH_MAP = new Map([
-  [Engine.MYSQL, new URL("@/assets/db-mysql.png", import.meta.url).href],
-  [Engine.POSTGRES, new URL("@/assets/db-postgres.png", import.meta.url).href],
-  [Engine.TIDB, new URL("@/assets/db-tidb.png", import.meta.url).href],
-  [
-    Engine.SNOWFLAKE,
-    new URL("@/assets/db-snowflake.png", import.meta.url).href,
-  ],
-  [
-    Engine.CLICKHOUSE,
-    new URL("@/assets/db-clickhouse.png", import.meta.url).href,
-  ],
-  [Engine.MONGODB, new URL("@/assets/db-mongodb.png", import.meta.url).href],
-  [Engine.SPANNER, new URL("@/assets/db-spanner.png", import.meta.url).href],
-  [Engine.REDIS, new URL("@/assets/db-redis.png", import.meta.url).href],
-  [Engine.ORACLE, new URL("@/assets/db-oracle.svg", import.meta.url).href],
-  [Engine.MSSQL, new URL("@/assets/db-mssql.svg", import.meta.url).href],
-  [Engine.REDSHIFT, new URL("@/assets/db-redshift.svg", import.meta.url).href],
-  [Engine.MARIADB, new URL("@/assets/db-mariadb.png", import.meta.url).href],
-  [
-    Engine.OCEANBASE,
-    new URL("@/assets/db-oceanbase.png", import.meta.url).href,
-  ],
-]);
-const icon = computed(() => {
-  return ICON_PATH_MAP.get(props.instance.engine);
 });
 </script>
