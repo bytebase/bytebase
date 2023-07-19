3d776ef4142b38a88e6f9f91091ab070bd8f7a63
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-07-19 13:53:18 +0800

    chore: wording and default value (#7132)

--- frontend/src/components/InstanceForm/OracleSyncModeInput.vue
+++ frontend/src/components/InstanceForm/OracleSyncModeInput.vue
@@ -1,39 +1,44 @@
 <template>
-  <div class="sm:col-span-3 sm:col-start-1 flex flex-col gap-y-2">
+  <div class="sm:col-span-4 sm:col-start-1 flex flex-col gap-y-2">
     <label class="textlabel">
       {{ $t("instance.sync-mode.self") }}
     </label>
-    <div class="flex items-center gap-x-4">
-      <label class="radio">
+    <div class="flex items-center gap-x-6">
+      <label class="flex items-center gap-1.5">
         <input
           v-model="state.mode"
           tabindex="-1"
           type="radio"
           class="text-accent disabled:text-accent-disabled focus:ring-accent"
-          value="SYNC_SELF"
+          value="DATABASE"
           :disabled="!allowEdit"
         />
-        <span class="label">
-          {{ $t("instance.sync-mode.sync-self") }}
+        <span class="text-sm font-medium text-gray-700">
+          {{ $t("instance.sync-mode.database.self") }}
         </span>
       </label>
-      <label class="radio">
+      <label class="flex items-center gap-1.5">
         <input
           v-model="state.mode"
           tabindex="-1"
           type="radio"
           class="text-accent disabled:text-accent-disabled focus:ring-accent"
-          value="SYNC_ALL"
+          value="SCHEMA"
           :disabled="!allowEdit"
         />
-        <span class="label">
-          {{ $t("instance.sync-mode.sync-all") }}
+        <span class="text-sm font-medium text-gray-700">
+          {{ $t("instance.sync-mode.schema.self") }}
         </span>
       </label>
     </div>
 
-    <label v-if="state.mode === 'SYNC_ALL'" class="textinfolabel">
-      {{ $t("instance.sync-mode.description") }}
+    <label class="text-xs text-control-light ml-[calc(16px+0.375rem)]">
+      <template v-if="state.mode === 'DATABASE'">
+        {{ $t("instance.sync-mode.database.description") }}
+      </template>
+      <template v-if="state.mode === 'SCHEMA'">
+        {{ $t("instance.sync-mode.schema.description") }}
+      </template>
     </label>
   </div>
 </template>
@@ -41,7 +46,7 @@
 <script lang="ts" setup>
 import { reactive, watch } from "vue";
 
-type SyncMode = "SYNC_SELF" | "SYNC_ALL";
+type SyncMode = "SCHEMA" | "DATABASE";
 
 type LocalState = {
   mode: SyncMode;
@@ -57,8 +62,8 @@ const emit = defineEmits<{
 }>();
 
 const guessModeFromSchemaTenantMode = (schemaTenantMode: boolean): SyncMode => {
-  if (schemaTenantMode) return "SYNC_SELF";
-  return "SYNC_ALL";
+  if (schemaTenantMode) return "SCHEMA";
+  return "DATABASE";
 };
 
 const state = reactive<LocalState>({
@@ -66,7 +71,7 @@ const state = reactive<LocalState>({
 });
 
 const update = (mode: SyncMode) => {
-  emit("update:schemaTenantMode", mode === "SYNC_SELF" ? true : false);
+  emit("update:schemaTenantMode", mode === "SCHEMA" ? true : false);
 };
 
 watch(
