f6c4a78f81d93357f8091438c911d74f08499547
h3n4l <oysterdays@gmail.com>
2023-07-19 15:30:40 +0800

    feat(sql-review, mssql): support no keyword sql review rule of mssql (#7135)
    * feat(sql-review, mssql): support table naming no keyword sql review rule of mssql
    
    * feat(sql-review, mssql): support identifier naming no keyword sql review rule of mssql
    
    * chore: enable
    
    Former-commit-id: 12fbcb387ce912c845ed3dfa81c756d55e998712

--- backend/plugin/advisor/advisor.go
+++ backend/plugin/advisor/advisor.go
@@ -446,6 +446,12 @@ const (
 
 	// MSSQLNamingTableConvention is an advisor type for MSSQL table naming convention.
 	MSSQLNamingTableConvention Type = "bb.plugin.advisor.mssql.naming.table"
+
+	// MSSQLTableNamingNoKeyword is an advisor type for MSSQL table naming convention without keyword.
+	MSSQLTableNamingNoKeyword Type = "bb.plugin.advisor.mssql.naming.table-no-keyword"
+
+	// MSSQLIdentifierNamingNoKeyword is an advisor type for MSSQL identifier naming convention without keyword.
+	MSSQLIdentifierNamingNoKeyword Type = "bb.plugin.advisor.mssql.naming.identifier-no-keyword"
 )
 
 // Advice is the result of an advisor.
