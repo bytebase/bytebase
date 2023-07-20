f4b84f5a9bee772c505f56685304e8d27f139bd3
steven <stevenlgtm@gmail.com>
2023-07-20 17:19:04 +0800

    chore: update translate

--- frontend/src/components/SchemaDesigner/EmptyTips.vue
+++ frontend/src/components/SchemaDesigner/EmptyTips.vue
@@ -3,6 +3,6 @@
     class="w-full h-full flex flex-row justify-center items-center text-gray-600 font-medium"
   >
     <heroicons-solid:pencil-square class="w-4 h-auto mr-2" />
-    {{ $t("schema-editor.use-tips") }}
+    {{ $t("schema-designer.use-tips") }}
   </div>
 </template>
