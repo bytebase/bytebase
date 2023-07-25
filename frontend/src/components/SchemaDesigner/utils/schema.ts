0a98584c1819c8c7a4230ed5238c2647f5b84f92
steven <stevenlgtm@gmail.com>
2023-07-25 15:50:25 +0800

    chore: add computed class list to schema designer

--- frontend/src/components/SchemaDesigner/utils/schema.ts
+++ frontend/src/components/SchemaDesigner/utils/schema.ts
@@ -1,4 +1,11 @@
-// TODO(steven): Implement this function.
-export const isSchemaChanged = (): boolean => {
-  return false;
+import { isEqual } from "lodash-es";
+import { useSchemaDesignerContext } from "../common";
+
+export const isSchemaChanged = (schemaId: string): boolean => {
+  const { originalSchemas, editableSchemas } = useSchemaDesignerContext();
+  const originSchema = originalSchemas.value.find(
+    (schema) => schema.id === schemaId
+  );
+  const schema = editableSchemas.value.find((schema) => schema.id === schemaId);
+  return !isEqual(originSchema, schema);
 };
