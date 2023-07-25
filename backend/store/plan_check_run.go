bbee881bf92a87c0e03db7212e46e6e089e24293
p0ny <p0uy@outlook.com>
2023-07-25 11:35:58 +0800

    create plan check runs

--- backend/store/plan_check_run.go
+++ backend/store/plan_check_run.go
@@ -1,6 +1,14 @@
 package store
 
-import storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+import (
+	"context"
+	"strings"
+
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/encoding/protojson"
+
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
 
 // PlanCheckRunType is the type of a plan check run.
 type PlanCheckRunType string
@@ -52,4 +60,53 @@ type PlanCheckRunMessage struct {
 	Type   PlanCheckRunType
 	Config *storepb.PlanCheckRunConfig
 	Result *storepb.PlanCheckRunResult
+}
+
+func (s *Store) CreatePlanCheckRuns(ctx context.Context, creates ...*PlanCheckRunMessage) error {
+	var query strings.Builder
+	var values []any
+	if _, err := query.WriteString(`INSERT INTO plan_check_run (
+		creator_id,
+		updater_id,
+		plan_id,
+		status,
+		type,
+		config,
+		result
+	) VALUES
+	`); err != nil {
+		return err
+	}
+	for i, create := range creates {
+		config, err := protojson.Marshal(create.Config)
+		if err != nil {
+			return errors.Wrapf(err, "faield to marshal create config %v", create.Config)
+		}
+		result, err := protojson.Marshal(create.Result)
+		if err != nil {
+			return errors.Wrapf(err, "failed to marshal create result %v", create.Result)
+		}
+		values = append(values,
+			create.CreatorUID,
+			create.UpdaterUID,
+			create.PlanUID,
+			create.Status,
+			create.Type,
+			config,
+			result,
+		)
+		if i != 0 {
+			if _, err := query.WriteString(","); err != nil {
+				return err
+			}
+		}
+		count := 7
+		if _, err := query.WriteString(getPlaceholders(i*count+1, count)); err != nil {
+			return err
+		}
+	}
+	if _, err := s.db.db.ExecContext(ctx, query.String(), values...); err != nil {
+		return errors.Wrapf(err, "failed to execute insert")
+	}
+	return nil
 }
