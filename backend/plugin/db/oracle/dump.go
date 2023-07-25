cec53f637039861232eb3da04393a81d59944a2b
rebelice <yangrebelice@gmail.com>
2023-07-25 15:57:52 +0800

    chore: handle error in oracle server when dumping schemas

--- backend/plugin/db/oracle/dump.go
+++ backend/plugin/db/oracle/dump.go
@@ -3,14 +3,18 @@ package oracle
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"io"
-	"strings"
 
 	"go.uber.org/zap"
 
+	"github.com/pkg/errors"
+	go_ora "github.com/sijms/go-ora/v2"
+
 	"github.com/bytebase/bytebase/backend/common/log"
-	"github.com/bytebase/bytebase/backend/plugin/db/util"
+)
+
+const (
+	maxOutputSize = 500 * 1024 * 1024 // 500MB
 )
 
 // Dump dumps the database.
@@ -30,15 +34,15 @@ func (driver *Driver) Dump(ctx context.Context, out io.Writer, _ bool) (string, 
 		return "", nil
 	}
 
-	var quotedSchemas []string
+	var list []string
 	if driver.schemaTenantMode {
-		quotedSchemas = append(quotedSchemas, fmt.Sprintf("'%s'", driver.databaseName))
+		list = append(list, driver.databaseName)
 	} else {
 		for _, schema := range schemas {
-			quotedSchemas = append(quotedSchemas, fmt.Sprintf("'%s'", schema))
+			list = append(list, schema)
 		}
 	}
-	if err := dumpTxn(ctx, txn, quotedSchemas, out); err != nil {
+	if err := dumpTxn(ctx, txn, list, out); err != nil {
 		return "", err
 	}
 
@@ -51,61 +55,63 @@ func (driver *Driver) Dump(ctx context.Context, out io.Writer, _ bool) (string, 
 func dumpTxn(ctx context.Context, txn *sql.Tx, schemas []string, out io.Writer) error {
 	// Exclude nested tables, their DDL is part of their parent table.
 	// Exclude overflow segments, their DDL is part of their parent table.
-	query := fmt.Sprintf(`
-		WITH DISALLOW_OBJECTS AS (
-			SELECT OWNER, TABLE_NAME FROM DBA_NESTED_TABLES
-			UNION ALL
-			SELECT OWNER, TABLE_NAME FROM DBA_TABLES WHERE IOT_TYPE = 'IOT_OVERFLOW'
-		), NEED_OBJECTS AS (
-			SELECT
-				OWNER,
-				OBJECT_NAME,
-				decode(object_type,
-					'JOB',                'PROCOBJ',
-					'QUEUE',              'AQ_QUEUE',
-					object_type
-				) OBJECT_TYPE
-			FROM DBA_OBJECTS U
-			WHERE OWNER IN (%s) AND U.OBJECT_TYPE IN ('TABLE','INDEX','SEQUENCE','DIRECTORY','VIEW','FUNCTION','PROCEDURE','TRIGGER','SCHEDULE','JOB','QUEUE','WINDOW')
-			MINUS
-			SELECT OWNER, TABLE_NAME, 'TABLE' FROM DISALLOW_OBJECTS
-		)
-		SELECT
-			DBMS_METADATA.GET_DDL(U.OBJECT_TYPE, U.OBJECT_NAME, U.OWNER)
-		FROM NEED_OBJECTS U`,
-		strings.Join(schemas, ","))
+	query := `
+		DECLARE
+			TYPE type_user_name_list IS TABLE OF VARCHAR2(128) INDEX BY BINARY_INTEGER;
+			PROCEDURE fetch_ddl(
+				user_names type_user_name_list,
+				ddls OUT LONG
+			) IS
+			BEGIN
+				FOR user_name IN user_names.FIRST .. user_names.LAST LOOP
+					ddls := ddls || '/* Schema: ' || user_names(user_name) || ' */' || chr(10) || chr(10) ;
+					FOR object_meta IN (
+						WITH DISALLOW_OBJECTS AS (
+							SELECT OWNER, TABLE_NAME FROM DBA_NESTED_TABLES
+							UNION ALL
+							SELECT OWNER, TABLE_NAME FROM DBA_TABLES WHERE IOT_TYPE = 'IOT_OVERFLOW'
+						), NEED_OBJECTS AS (
+							SELECT
+								OWNER,
+								OBJECT_NAME,
+								decode(object_type,
+									'JOB',                'PROCOBJ',
+									'QUEUE',              'AQ_QUEUE',
+									object_type
+								) OBJECT_TYPE
+							FROM DBA_OBJECTS U
+							WHERE OWNER = user_names(user_name) AND U.OBJECT_TYPE IN ('TABLE','INDEX','SEQUENCE','DIRECTORY','VIEW','FUNCTION','PROCEDURE','TRIGGER','SCHEDULE','JOB','QUEUE','WINDOW')
+							MINUS
+							SELECT OWNER, TABLE_NAME, 'TABLE' FROM DISALLOW_OBJECTS
+						)
+						SELECT
+							U.OBJECT_TYPE, U.OBJECT_NAME, U.OWNER
+						FROM NEED_OBJECTS U
+					) LOOP
+						BEGIN
+							ddls := ddls || DBMS_METADATA.GET_DDL(object_meta.OBJECT_TYPE, object_meta.OBJECT_NAME, object_meta.OWNER) || ';' || chr(10) || chr(10);
+						EXCEPTION
+							WHEN OTHERS THEN
+							ddls := ddls || '/* Error: failed to get ddl for ' || object_meta.OBJECT_TYPE || ' ' || object_meta.OBJECT_NAME || ' in ' || object_meta.OWNER || ' */' || chr(10) || chr(10);
+						END;
+					END LOOP;
+				END LOOP;
+			END;
+		BEGIN
+			fetch_ddl(:1, :2);
+		END;
+	`
+
+	text := ""
 	log.Debug("start dumping Oracle schemas", zap.String("query", query))
-
-	rows, err := txn.QueryContext(ctx, query)
+	_, err := txn.ExecContext(ctx, query, schemas, go_ora.Out{Dest: &text, Size: maxOutputSize})
 	if err != nil {
-		log.Warn("query error", zap.Error(err))
-		return util.FormatErrorWithQuery(err, query)
+		return errors.Wrap(err, "failed to dump schemas")
 	}
-	defer rows.Close()
 
-	var ddls []string
-	for rows.Next() {
-		var databaseDDL string
-		if err := rows.Scan(&databaseDDL); err != nil {
-			log.Warn("ddl scan error", zap.Error(err))
-			return err
-		}
-		ddls = append(ddls, databaseDDL)
-	}
-	if err := rows.Err(); err != nil {
-		log.Warn("rows error", zap.Error(err))
+	if _, err := io.WriteString(out, text); err != nil {
+		log.Warn("write error", zap.Error(err))
 		return err
-	}
-
-	for _, ddl := range ddls {
-		if _, err := io.WriteString(out, ddl); err != nil {
-			log.Warn("write error", zap.Error(err))
-			return err
-		}
-		if _, err := io.WriteString(out, ";\n"); err != nil {
-			log.Warn("write newline error", zap.Error(err))
-			return err
-		}
 	}
 	return nil
 }
