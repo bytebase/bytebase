f59d3615b466b40c4b53251e0c6249f3ae2a077c
SIOOOO <gesiyuan01@gmail.com>
2023-07-26 11:21:25 +0800

    chore:remove personal comment

--- frontend/src/components/ColumnTable.vue
+++ frontend/src/components/ColumnTable.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: SIOOOO gesiyuan01@gmail.com
- * @Date: 2023-07-19 23:38:48
- * @LastEditors: SIOOOO gesiyuan01@gmail.com
- * @LastEditTime: 2023-07-20 01:51:13
- * @FilePath: /bytebase/frontend/src/components/ColumnTable.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <BBTable
     :column-list="columnNameList"
