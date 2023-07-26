96bc250b3b8165b1f9e6491697bb11c29164f39d
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-07-26 11:22:38 +0800

    chore: use case insensitive search (#7215)
    Former-commit-id: ca0f588d934f2ea153db589829301439902960ec

--- frontend/src/views/IssueDashboard.vue
+++ frontend/src/views/IssueDashboard.vue
@@ -200,7 +200,7 @@ const filter = (issue: Issue) => {
       return false;
     }
   }
-  const keyword = state.searchText.trim();
+  const keyword = state.searchText.trim().toLowerCase();
   if (keyword) {
     if (!issue.name.toLowerCase().includes(keyword)) {
       return false;
