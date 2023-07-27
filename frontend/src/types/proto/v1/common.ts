4c5ea2fd1def955f218472db72b915988bd7fa0a
boojack <stevenlgtm@gmail.com>
2023-07-27 11:41:06 +0800

    chore: buf generate (#7237)
    Former-commit-id: feb816500ae0a97f9fc7432a72bc9ee6e47f09af

--- frontend/src/types/proto/v1/common.ts
+++ frontend/src/types/proto/v1/common.ts
@@ -57,6 +57,7 @@ export enum Engine {
   REDSHIFT = 12,
   MARIADB = 13,
   OCEANBASE = 14,
+  DM = 15,
   UNRECOGNIZED = -1,
 }
 
@@ -107,6 +108,9 @@ export function engineFromJSON(object: any): Engine {
     case 14:
     case "OCEANBASE":
       return Engine.OCEANBASE;
+    case 15:
+    case "DM":
+      return Engine.DM;
     case -1:
     case "UNRECOGNIZED":
     default:
@@ -146,6 +150,8 @@ export function engineToJSON(object: Engine): string {
       return "MARIADB";
     case Engine.OCEANBASE:
       return "OCEANBASE";
+    case Engine.DM:
+      return "DM";
     case Engine.UNRECOGNIZED:
     default:
       return "UNRECOGNIZED";
