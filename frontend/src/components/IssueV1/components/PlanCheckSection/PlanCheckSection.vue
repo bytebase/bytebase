8551efc88635c360c8d4cf2400e2f3f229516b4a
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-07-27 17:53:22 +0800

    chore: print debug info

--- frontend/src/components/IssueV1/components/PlanCheckSection/PlanCheckSection.vue
+++ frontend/src/components/IssueV1/components/PlanCheckSection/PlanCheckSection.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="issue-debug">plan check section</div>
+  <div class="issue-debug">
+    <h3>plan check section</h3>
+    <div v-if="!isCreating">
+      <NButton type="primary" @click="runPlanChecks">Run plan checks</NButton>
+    </div>
+    <div>issue.planCheckRunList:</div>
+    <pre>{{ issue.planCheckRunList.map(PlanCheckRun.toJSON) }}</pre>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { NButton } from "naive-ui";
+import { useIssueContext } from "../../logic";
+import { rolloutServiceClient } from "@/grpcweb";
+import { PlanCheckRun } from "@/types/proto/v1/rollout_service";
+const { isCreating, issue } = useIssueContext();
+
+const runPlanChecks = async () => {
+  const plan = issue.value.planEntity;
+  if (!plan) return;
+
+  try {
+    const response = await rolloutServiceClient.runPlanChecks({
+      name: plan.name,
+    });
+    console.log("runPlanChecks response", response);
+  } catch (ex) {
+    debugger;
+  }
+};
+</script>
