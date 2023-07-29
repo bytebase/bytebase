0cecb1f83b70044e57c4d78443a727127d36f591
d-bytebase <d@bytebase.com>
2023-07-29 23:23:25 +0800

    chore: update

--- backend/api/v1/setting_service.go
+++ backend/api/v1/setting_service.go
@@ -169,7 +169,7 @@ func (s *SettingService) SetSetting(ctx context.Context, request *v1pb.SetSettin
 		if err != nil {
 			return nil, status.Errorf(codes.Internal, "failed to marshal setting for %s with error: %v", apiSettingName, err)
 		}
-		if payload.RefreshTokenDuration != nil && payload.RefreshTokenDuration.AsDuration() < time.Hour {
+		if payload.RefreshTokenDuration != nil && payload.RefreshTokenDuration.Seconds > 0 && payload.RefreshTokenDuration.AsDuration() < time.Hour {
 			return nil, status.Errorf(codes.InvalidArgument, "refresh token duration should be at least one hour")
 		}
 		storeSettingValue = string(bytes)
