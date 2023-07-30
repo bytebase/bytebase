c2d8acf1ba3896bb6d454d70b23900a98fca625b
d-bytebase <d@bytebase.com>
2023-07-30 23:18:02 +0800

    chore: update

--- backend/resources/mysql/resources_linux_arm64.go
+++ backend/resources/mysql/resources_linux_arm64.go
@@ -5,10 +5,10 @@ package mysql
 
 import "embed"
 
-//go:generate ./fetch_mysql.sh mysql-8.0.33-linux-glibc2.17-aarch64-minimal.tar.gz f5e01242975453ec6db02727848975c9
+//go:generate ./fetch_mysql.sh mysql-8.0.33-linux-glibc2.17-aarch64.tar.gz
 
 // To use this package in testing, download the MySQL binary first:
 // go generate -tags mysql ./...
 //
-//go:embed mysql-8.0.33-linux-glibc2.17-aarch64-minimal.tar.gz
+//go:embed mysql-8.0.33-linux-glibc2.17-aarch64.tar.gz
 var resources embed.FS
