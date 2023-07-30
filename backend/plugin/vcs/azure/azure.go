844f0ce3946a4d6517a245b7929d163f94340ea6
h3n4l <oysterdays@gmail.com>
2023-07-30 18:51:18 +0800

    fix: specify ref

--- backend/plugin/vcs/azure/azure.go
+++ backend/plugin/vcs/azure/azure.go
@@ -913,6 +913,8 @@ func (p *Provider) ReadFileMeta(ctx context.Context, oauthCtx common.OauthContex
 }
 
 // ReadFileContent reads the content of the given file in the repository.
+//
+// Docs: https://learn.microsoft.com/en-us/rest/api/azure/devops/git/items/get?view=azure-devops-rest-7.0&tabs=HTTP
 func (p *Provider) ReadFileContent(ctx context.Context, oauthCtx common.OauthContext, _ string, repositoryID string, filePath string, ref string) (string, error) {
 	parts := strings.Split(repositoryID, "/")
 	if len(parts) != 3 {
@@ -925,6 +927,7 @@ func (p *Provider) ReadFileContent(ctx context.Context, oauthCtx common.OauthCon
 	values.Set("resolveLfs", "true")
 	values.Set("includeContent", "true")
 	values.Set("path", filePath)
+	values.Set("versionDescriptor.version", ref)
 	url := fmt.Sprintf("https://dev.azure.com/%s/_apis/git/repositories/%s/items?%s", url.PathEscape(organizationName), url.PathEscape(repositoryID), values.Encode())
 
 	code, _, body, err := oauth.Get(ctx, p.client, url, &oauthCtx.AccessToken, tokenRefresher(
