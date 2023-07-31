780cd5454fff0e22f8e0086aa84c39038ba6c8ab
steven <stevenlgtm@gmail.com>
2023-07-31 17:07:57 +0800

    chore: update i18n for secure token

--- frontend/src/components/GeneralSetting/SignInFrequencySetting.vue
+++ frontend/src/components/GeneralSetting/SignInFrequencySetting.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="mb-7 mt-5 lg:mt-0">
-    <p class="font-medium">{{ "Sign-in frequency" }}</p>
-    <p class="text-sm text-gray-400">
-      {{ "Sign-in frequency is the period before a user must sign in again." }}
+    <p class="font-medium flex flex-row justify-start items-center">
+      <span class="mr-2">{{
+        $t("settings.general.workspace.sign-in-frequency.self")
+      }}</span>
+      <FeatureBadge feature="bb.feature.secure-token" />
     </p>
-    <div class="mt-4 w-full flex flex-row justify-start items-center">
+    <p class="text-sm text-gray-400 mt-1">
+      {{ $t("settings.general.workspace.sign-in-frequency.description") }}
+    </p>
+    <div class="mt-3 w-full flex flex-row justify-start items-center">
       <NInputNumber
         v-model:value="state.inputValue"
         class="w-20 mr-4"
@@ -14,8 +19,14 @@
         :precision="0"
       />
       <NRadioGroup v-model:value="state.timeFormat" :disabled="!allowEdit">
-        <NRadio :value="'HOURS'" :label="'Hour(s)'" />
-        <NRadio :value="'DAYS'" :label="'Day(s)'" />
+        <NRadio
+          :value="'HOURS'"
+          :label="$t('settings.general.workspace.sign-in-frequency.hours')"
+        />
+        <NRadio
+          :value="'DAYS'"
+          :label="$t('settings.general.workspace.sign-in-frequency.days')"
+        />
       </NRadioGroup>
     </div>
   </div>
@@ -67,22 +78,19 @@ const settingV1Store = useSettingV1Store();
 const currentUserV1 = useCurrentUserV1();
 const state = reactive<LocalState>(getInitialState());
 
-// TODO(steven): update feature name.
 const hasSecureTokenFeature = featureToRef("bb.feature.secure-token");
 
 const allowEdit = computed((): boolean => {
-  return hasWorkspacePermissionV1(
-    "bb.permission.workspace.manage-general",
-    currentUserV1.value.userRole
+  return (
+    hasSecureTokenFeature.value &&
+    hasWorkspacePermissionV1(
+      "bb.permission.workspace.manage-general",
+      currentUserV1.value.userRole
+    )
   );
 });
 
 const handleFrequencySettingChange = useDebounceFn(async () => {
-  if (!hasSecureTokenFeature.value) {
-    state.featureNameForModal = "bb.feature.secure-token";
-    return;
-  }
-
   const seconds =
     state.timeFormat === "HOURS"
       ? state.inputValue * 60 * 60
@@ -109,6 +117,11 @@ watch(
 watch(
   () => [state.inputValue, state.timeFormat],
   () => {
+    if (!hasSecureTokenFeature.value) {
+      state.featureNameForModal = "bb.feature.secure-token";
+      return;
+    }
+
     handleFrequencySettingChange();
   }
 );
