d586f7c6f8b9210065a3ac3a00ed784ded3d1e11
boojack <stevenlgtm@gmail.com>
2023-08-01 20:40:28 +0800

    feat: implement column types template (#7342)
    Former-commit-id: a89e0c11700a196e25b1f4cd895c56b7c3758c35
    Former-commit-id: 180bcee8d6f6e3c6a7522a520bc0e11a5256d064

--- frontend/src/views/SettingWorkspaceSchemaTemplate.vue
+++ frontend/src/views/SettingWorkspaceSchemaTemplate.vue
@@ -1,18 +1,53 @@
 <template>
-  <template v-if="state.selectedTab === 'FIELD_TEMPLATE'">
-    <FieldTemplates />
-  </template>
+  <NTabs v-model:value="state.selectedTab" type="line">
+    <NTabPane name="FIELD_TEMPLATE" tab="Field Template">
+      <FieldTemplates />
+    </NTabPane>
+    <NTabPane name="COLUMN_TYPE_RESTRICTION" tab="Column Type Restriction">
+      <ColumnTypes />
+    </NTabPane>
+  </NTabs>
 </template>
 
 <script lang="ts" setup>
-import { reactive } from "vue";
+import { NTabs, NTabPane } from "naive-ui";
+import { reactive, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import FieldTemplates from "@/views/SchemaTemplate/FieldTemplates.vue";
+import ColumnTypes from "@/views/SchemaTemplate/ColumnTypes.vue";
 
 interface LocalState {
-  selectedTab: "FIELD_TEMPLATE";
+  selectedTab: "FIELD_TEMPLATE" | "COLUMN_TYPE_RESTRICTION";
 }
 
+const route = useRoute();
+const router = useRouter();
 const state = reactive<LocalState>({
   selectedTab: "FIELD_TEMPLATE",
 });
+
+watch(
+  () => route.hash,
+  () => {
+    if (route.hash === "#column-type-restriction") {
+      state.selectedTab = "COLUMN_TYPE_RESTRICTION";
+    } else {
+      state.selectedTab = "FIELD_TEMPLATE";
+    }
+  },
+  {
+    immediate: true,
+  }
+);
+
+watch(
+  () => state.selectedTab,
+  () => {
+    if (state.selectedTab === "COLUMN_TYPE_RESTRICTION") {
+      router.push({ hash: "#column-type-restriction" });
+    } else if (state.selectedTab === "FIELD_TEMPLATE") {
+      router.push({ hash: "" });
+    }
+  }
+);
 </script>
