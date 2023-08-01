ca3a726f098b6182bfbee83c2d804a9072209f70
d-bytebase <d@bytebase.com>
2023-08-01 23:44:44 +0800

    chore: update

--- backend/resources/mysqlutil/resources_linux_arm64.go
+++ backend/resources/mysqlutil/resources_linux_arm64.go
@@ -2,10 +2,5 @@ package mysqlutil
 
 import "embed"
 
-////go:generate ./build_mysqlutil.sh ../mysql/mysql-8.0.33-linux-glibc2.17-aarch64.tar.gz linux arm64 mysqlutil-8.0.33
-
-// To use this package in testing, download the MySQL binary first:
-// go generate -tags mysqlutil ./...
-
 //go:embed mysqlutil-8.0.33-linux-arm64.tar.gz
 var resources embed.FS
