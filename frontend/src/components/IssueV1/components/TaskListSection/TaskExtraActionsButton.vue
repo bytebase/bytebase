3f69b631811fbb8d433326e3e1a05a6d89b2b3ed
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-08-01 12:20:24 +0800

    chore: task extra action button

--- frontend/src/components/IssueV1/components/TaskListSection/TaskExtraActionsButton.vue
+++ frontend/src/components/IssueV1/components/TaskListSection/TaskExtraActionsButton.vue
@@ -1,23 +1,46 @@
 <template>
-  <div v-if="show" class="issue-debug">
-    <heroicons:ellipsis-vertical-solid class="w-4 h-4" />
-  </div>
+  <NDropdown
+    v-if="options.length > 0"
+    trigger="click"
+    placement="bottom-end"
+    :options="options"
+    @select="handleSelect"
+  >
+    <NButton
+      quaternary
+      size="tiny"
+      style="--n-padding: 0 1px; --n-icon-size: 20px"
+    >
+      <template #icon>
+        <heroicons:ellipsis-vertical-solid class="w-5 h-5" />
+      </template>
+    </NButton>
+  </NDropdown>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
+import { DropdownOption, NDropdown, NButton } from "naive-ui";
+import { useI18n } from "vue-i18n";
 
 import { Task } from "@/types/proto/v1/rollout_service";
 import {
+  TaskRolloutAction,
   getApplicableTaskRolloutActionList,
   useIssueContext,
-} from "../../logic";
+} from "@/components/IssueV1/logic";
+
+type ExtraTaskRolloutActionDropdownOption = DropdownOption & {
+  action: TaskRolloutAction;
+  tasks: Task[];
+};
 
 const props = defineProps<{
   task: Task;
 }>();
 
-const { isCreating, activeTask, issue } = useIssueContext();
+const { t } = useI18n();
+const { events, isCreating, activeTask, issue } = useIssueContext();
 
 const actionList = computed(() => {
   return getApplicableTaskRolloutActionList(
@@ -27,14 +50,34 @@ const actionList = computed(() => {
   );
 });
 
-const show = computed(() => {
+const options = computed((): ExtraTaskRolloutActionDropdownOption[] => {
   if (isCreating.value) {
-    return false;
+    return [];
   }
   if (props.task.uid !== activeTask.value.uid) {
-    return false;
+    return [];
   }
-
-  return actionList.value.includes("SKIP");
+  const SKIP = actionList.value.includes("SKIP");
+  if (!SKIP) {
+    return [];
+  }
+  return [
+    {
+      key: "skip",
+      label: t("task.skip"),
+      action: "SKIP",
+      tasks: [props.task],
+    },
+  ];
 });
+
+const handleSelect = (key: string) => {
+  const option = options.value.find((opt) => opt.key === key);
+  if (!option) return;
+  const { action, tasks } = option;
+  events.emit("perform-task-rollout-action", {
+    action,
+    tasks,
+  });
+};
 </script>
