1f28381768e3b402733d52c1a5bae0e35605fa01
boojack <stevenlgtm@gmail.com>
2023-08-03 11:53:49 +0800

    chore: update null value display style in sql editor (#7375)
    * chore: update null value display style in sql editor
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 401db0c66bca3ed174887622e8d11209daeeaf43

--- frontend/src/utils/v1/sql.ts
+++ frontend/src/utils/v1/sql.ts
@@ -1,6 +1,11 @@
+import { NullValue } from "@/types/proto/google/protobuf/struct";
 import { RowValue } from "@/types/proto/v1/sql_service";
 
 export const extractSQLRowValue = (value: RowValue) => {
+  if (value.nullValue === NullValue.NULL_VALUE) {
+    return null;
+  }
+
   const plainObject = RowValue.toJSON(value) as Record<string, any>;
   const keys = Object.keys(plainObject);
   if (keys.length === 0) {
