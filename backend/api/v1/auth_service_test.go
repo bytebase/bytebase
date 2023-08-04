e64a960a7c8289772b993b37573d505c77802925
boojack <stevenlgtm@gmail.com>
2023-08-04 14:03:25 +0800

    chore: add phone to user create in SSO (#7404)
    * chore: add phone to user create in SSO
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: updat
    
    Former-commit-id: fef5f8da18a8cf8555882b40c4575a0796d41e37

--- backend/api/v1/auth_service_test.go
+++ backend/api/v1/auth_service_test.go
@@ -2,30 +2,6 @@ package v1
 
 import "testing"
 
-func TestValidatePhone(t *testing.T) {
-	tests := []struct {
-		phone string
-		want  bool
-	}{
-		{
-			phone: "1234567890",
-			want:  false,
-		},
-		{
-			phone: "+8615655556666",
-			want:  true,
-		},
-	}
-
-	for _, test := range tests {
-		got := validatePhone(test.phone)
-		isValid := got == nil
-		if isValid != test.want {
-			t.Errorf("validatePhone %s, err %v", test.phone, got)
-		}
-	}
-}
-
 func TestExtractDomain(t *testing.T) {
 	tests := []struct {
 		domain string
