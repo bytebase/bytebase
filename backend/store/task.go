c291a427f0c135fe6545117c88821df66f352833
p0ny <xz@bytebase.com>
2023-08-04 16:58:15 +0800

    feat: task status uses the latest task run status (#7412)

--- backend/store/task.go
+++ backend/store/task.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/lib/pq"
+	"github.com/jackc/pgtype"
 	"github.com/pkg/errors"
 
 	"github.com/bytebase/bytebase/backend/common"
@@ -45,6 +45,8 @@ type TaskMessage struct {
 	DatabaseName string
 	// Statement used by grouping batch change, Bytebase use it to render.
 	Statement string
+
+	LatestTaskRunStatus *api.TaskRunStatus
 }
 
 func (task *TaskMessage) toTask() *api.Task {
@@ -372,14 +374,21 @@ func (s *Store) ListTasks(ctx context.Context, find *api.TaskFind) ([]*TaskMessa
 			task.database_id,
 			task.name,
 			task.status,
+			latest_task_run.status,
 			task.type,
 			task.payload,
 			task.earliest_allowed_ts,
-			ARRAY_AGG (task_dag.from_task_id) blocked_by
+			(SELECT ARRAY_AGG (task_dag.from_task_id) FROM task_dag WHERE task_dag.to_task_id = task.id) blocked_by
 		FROM task
-		LEFT JOIN task_dag ON task.id = task_dag.to_task_id
+		LEFT JOIN LATERAL (
+			SELECT
+				task_run.status
+			FROM task_run
+			WHERE task_run.task_id = task.id
+			ORDER BY task_run.id DESC
+			LIMIT 1
+		) AS latest_task_run ON TRUE
 		WHERE %s
-		GROUP BY task.id
 		ORDER BY task.id ASC`, strings.Join(where, " AND ")),
 		args...,
 	)
@@ -391,7 +400,7 @@ func (s *Store) ListTasks(ctx context.Context, find *api.TaskFind) ([]*TaskMessa
 	var tasks []*TaskMessage
 	for rows.Next() {
 		task := &TaskMessage{}
-		var blockedBy []sql.NullInt32
+		var blockedBy pgtype.Int4Array
 		if err := rows.Scan(
 			&task.ID,
 			&task.CreatorID,
@@ -404,17 +413,16 @@ func (s *Store) ListTasks(ctx context.Context, find *api.TaskFind) ([]*TaskMessa
 			&task.DatabaseID,
 			&task.Name,
 			&task.Status,
+			&task.LatestTaskRunStatus,
 			&task.Type,
 			&task.Payload,
 			&task.EarliestAllowedTs,
-			pq.Array(&blockedBy),
+			&blockedBy,
 		); err != nil {
 			return nil, err
 		}
-		for _, v := range blockedBy {
-			if v.Valid {
-				task.BlockedBy = append(task.BlockedBy, int(v.Int32))
-			}
+		if err := blockedBy.AssignTo(&task.BlockedBy); err != nil {
+			return nil, err
 		}
 		tasks = append(tasks, task)
 	}
