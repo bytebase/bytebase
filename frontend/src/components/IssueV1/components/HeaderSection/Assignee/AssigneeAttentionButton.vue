725eab932adcacafd3a04c0cf67e3bf5f3d159d8
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-08-09 10:26:23 +0800

    feat: request assignee attention

--- frontend/src/components/IssueV1/components/HeaderSection/Assignee/AssigneeAttentionButton.vue
+++ frontend/src/components/IssueV1/components/HeaderSection/Assignee/AssigneeAttentionButton.vue
@@ -8,10 +8,13 @@
             ? 'cursor-default'
             : 'cursor-pointer hover:bg-control-bg-hover',
         ]"
+        :disabled="isUpdating"
         v-bind="$attrs"
         @click="notifyAssignee"
       >
+        <BBSpin v-if="isUpdating" class="w-4 h-4" />
         <heroicons-outline:bell-alert
+          v-else
           class="w-4 h-4"
           :class="[isAssigneeAttentionOn ? 'text-accent' : 'text-main']"
         />
@@ -30,28 +33,29 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { NTooltip } from "naive-ui";
 
 import { AppIMSetting_IMType } from "@/types/proto/v1/setting_service";
 import { Workflow } from "@/types/proto/v1/project_service";
-import { IssueStatus } from "@/types/proto/v1/issue_service";
+import { Issue, IssueStatus } from "@/types/proto/v1/issue_service";
+import { issueServiceClient } from "@/grpcweb";
 import { extractUserResourceName } from "@/utils";
 import {
   useSettingV1Store,
   useCurrentUserV1,
-  useIssueStore,
   useUserStore,
   pushNotification,
 } from "@/store";
-import { useIssueContext } from "../../../logic";
+import { useIssueContext } from "@/components/IssueV1/logic";
 
 const { t } = useI18n();
 const currentUserV1 = useCurrentUserV1();
 const settingV1Store = useSettingV1Store();
 const { isCreating, issue } = useIssueContext();
 const project = computed(() => issue.value.projectEntity);
+const isUpdating = ref(false);
 
 const showNotifyAssignee = computed(() => {
   if (isCreating.value) {
@@ -107,33 +111,39 @@ const imTypeName = computed((): string => {
   return t(`common.${type}`);
 });
 
-const notifyAssignee = () => {
+const notifyAssignee = async () => {
   if (!showNotifyAssignee.value) return;
   if (isAssigneeAttentionOn.value) return;
 
-  useIssueStore()
-    .patchIssue({
-      issueId: Number(issue.value.uid),
-      issuePatch: {
-        assigneeNeedAttention: true,
-      },
-    })
-    .then(() => {
-      const email = extractUserResourceName(issue.value.assignee);
-      const assignee = useUserStore().getUserByEmail(email);
-      const message = externalApprovalSetting.value.enabled
-        ? t("issue.assignee-attention.send-approval-request-successfully", {
-            im: imTypeName.value,
-          })
-        : t("issue.assignee-attention.send-attention-request-successfully", {
-            principal: assignee ? assignee.title : email,
-          });
-
-      pushNotification({
-        module: "bytebase",
-        style: "SUCCESS",
-        title: message,
-      });
+  try {
+    isUpdating.value = true;
+    const issuePatch = Issue.fromJSON({
+      ...issue.value,
+      assigneeAttention: true,
     });
+    const updated = await issueServiceClient.updateIssue({
+      issue: issuePatch,
+      updateMask: ["assignee_attention"],
+    });
+    Object.assign(issue.value, updated);
+
+    const email = extractUserResourceName(issue.value.assignee);
+    const assignee = useUserStore().getUserByEmail(email);
+    const message = externalApprovalSetting.value.enabled
+      ? t("issue.assignee-attention.send-approval-request-successfully", {
+          im: imTypeName.value,
+        })
+      : t("issue.assignee-attention.send-attention-request-successfully", {
+          principal: assignee ? assignee.title : email,
+        });
+
+    pushNotification({
+      module: "bytebase",
+      style: "SUCCESS",
+      title: message,
+    });
+  } finally {
+    isUpdating.value = false;
+  }
 };
 </script>
