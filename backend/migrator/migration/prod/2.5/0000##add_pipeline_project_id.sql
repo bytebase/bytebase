5185916aad71837abd1fd45b57e0b34cb3c98001
p0ny <p0uy@outlook.com>
2023-08-09 16:26:24 +0800

    fix: delete task and stage in pipelines

--- backend/migrator/migration/prod/2.5/0000##add_pipeline_project_id.sql
+++ backend/migrator/migration/prod/2.5/0000##add_pipeline_project_id.sql
@@ -10,6 +10,10 @@ SET project_id = issue.project_id
 FROM issue
 WHERE issue.pipeline_id = pipeline.id;
 
+DELETE FROM task WHERE task.pipeline_id IN (SELECT pipeline.id FROM pipeline WHERE pipeline.project_id IS NULL);
+
+DELETE FROM stage WHERE stage.pipeline_id IN (SELECT pipeline.id FROM pipeline WHERE pipeline.project_id IS NULL);
+
 DELETE FROM pipeline WHERE project_id IS NULL;
 
 ALTER TABLE pipeline ALTER COLUMN project_id SET NOT NULL;
