6083627475a34501f86c9d47b2587bfdb8c2ef55
p0ny <xz@bytebase.com>
2023-08-10 11:18:26 +0800

    chore: backport(2.5.0), delete pipelines with null project id (#7480)
    * fix: delete pipelines with null project id (#7472)
    
    * fix: delete task and stage in pipelines (#7474)
    
    * chore: delete pipeline task stage with NULL project_id if there are few (#7478)
    
    Former-commit-id: 912844645353668978b7d901b3689c1b3a1d59a8

--- backend/migrator/migration/prod/2.5/0000##add_pipeline_project_id.sql
+++ backend/migrator/migration/prod/2.5/0000##add_pipeline_project_id.sql
@@ -10,4 +10,15 @@ SET project_id = issue.project_id
 FROM issue
 WHERE issue.pipeline_id = pipeline.id;
 
+DO $$
+BEGIN
+    IF (SELECT COUNT(*) FROM pipeline WHERE project_id IS NULL) < 10 THEN
+        DELETE FROM task WHERE task.pipeline_id IN (SELECT pipeline.id FROM pipeline WHERE pipeline.project_id IS NULL);
+
+        DELETE FROM stage WHERE stage.pipeline_id IN (SELECT pipeline.id FROM pipeline WHERE pipeline.project_id IS NULL);
+
+        DELETE FROM pipeline WHERE project_id IS NULL;
+    END IF;
+END$$;
+
 ALTER TABLE pipeline ALTER COLUMN project_id SET NOT NULL;
