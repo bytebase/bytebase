a12475aa34b0b8c5c40e2d1c3fa17b8a1d8fff7e
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-08-10 18:16:33 +0800

    feat: modals to drawers (#7501)
    Former-commit-id: cb247c2d9bf51d82a128edce49166fdc98cc7a8f

--- frontend/src/components/IssueV1/components/Dialog/index.ts
+++
@@ -1,9 +0,0 @@
-import IssueReviewActionDialog from "./IssueReviewActionDialog";
-import IssueStatusActionDialog from "./IssueStatusActionDialog";
-import TaskRolloutActionDialog from "./TaskRolloutActionDialog";
-
-export {
-  IssueReviewActionDialog,
-  IssueStatusActionDialog,
-  TaskRolloutActionDialog,
-};
---
+++ frontend/src/components/IssueV1/components/Panel/index.ts
@@ -0,0 +1,9 @@
+import IssueReviewActionPanel from "./IssueReviewActionPanel.vue";
+import IssueStatusActionPanel from "./IssueStatusActionPanel.vue";
+import TaskRolloutActionPanel from "./TaskRolloutActionPanel.vue";
+
+export {
+  IssueStatusActionPanel,
+  IssueReviewActionPanel,
+  TaskRolloutActionPanel,
+};
