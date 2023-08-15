be40c19e51501765583284d5f549fde4d9987d4e
p0ny <p0uy@outlook.com>
2023-08-15 12:09:47 +0800

    impl sql review plan check

--- backend/server/server.go
+++ backend/server/server.go
@@ -512,6 +512,8 @@ func NewServer(ctx context.Context, profile config.Profile) (*Server, error) {
 			s.PlanCheckScheduler.Register(store.PlanCheckDatabaseConnect, databaseConnectExecutor)
 			statementTypeExecutor := plancheck.NewStatementTypeExecutor(storeInstance, s.dbFactory)
 			s.PlanCheckScheduler.Register(store.PlanCheckDatabaseStatementType, statementTypeExecutor)
+			statementAdviseExecutor := plancheck.NewStatementAdviseExecutor(storeInstance, s.dbFactory, s.licenseService)
+			s.PlanCheckScheduler.Register(store.PlanCheckDatabaseStatementAdvise, statementAdviseExecutor)
 		}
 
 		// Anomaly scanner
