3e36450d0820a8dbe5182dbcf1f1d853d09b750f
ecmadao <wlec@outlook.com>
2023-08-21 10:11:25 +0800

    chore: fix sql review layout in small screen (#7669)
    Former-commit-id: 4f406f50a2ddc7eb204cbbe78d3f65bf85ccd5f8
    Former-commit-id: 875cc16dd90e01587cfc8ac68711a37bc6b47b7c

--- frontend/src/components/SQLReview/components/SQLReviewCategorySummaryFilter.vue
+++ frontend/src/components/SQLReview/components/SQLReviewCategorySummaryFilter.vue
@@ -1,41 +1,50 @@
 <template>
-  <div class="flex items-center gap-x-5">
-    <label
-      v-for="stats in engineList"
-      :key="stats.engine"
-      class="flex items-center gap-x-1 text-sm text-gray-600"
-    >
-      <input
-        :id="engineToJSON(stats.engine)"
-        type="checkbox"
-        :value="stats.engine"
-        :checked="isCheckedEngine(stats.engine)"
-        class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
-        @input="e => emit('toggle-checked-engine', stats.engine, (e.target as HTMLInputElement).checked)"
-      />
-      <EngineIcon :engine="engineFromJSON(stats.engine)" custom-class="ml-1" />
-      <span
-        class="items-center text-xs px-1 py-0.5 rounded-full bg-gray-200 text-gray-800"
+  <div
+    class="flex flex-col items-start xl:flex-row xl:items-center gap-y-5 gap-x-5"
+  >
+    <div class="flex items-center gap-x-5">
+      <label
+        v-for="stats in engineList"
+        :key="stats.engine"
+        class="flex items-center gap-x-1 text-sm text-gray-600"
       >
-        {{ stats.count }}
-      </span>
-    </label>
-    <div class="h-[1.5rem] border-l border-control-border" />
-    <label
-      v-for="stats in errorLevelList"
-      :key="stats.level"
-      class="flex items-center gap-x-2 text-sm text-gray-600"
-    >
-      <input
-        :id="sQLReviewRuleLevelToJSON(stats.level)"
-        type="checkbox"
-        :value="stats.level"
-        :checked="isCheckedLevel(stats.level)"
-        class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
-        @input="e =>$emit('toggle-checked-level', stats.level, (e.target as HTMLInputElement).checked)"
-      />
-      <SQLRuleLevelBadge :level="stats.level" :suffix="`(${stats.count})`" />
-    </label>
+        <input
+          :id="engineToJSON(stats.engine)"
+          type="checkbox"
+          :value="stats.engine"
+          :checked="isCheckedEngine(stats.engine)"
+          class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+          @input="e => emit('toggle-checked-engine', stats.engine, (e.target as HTMLInputElement).checked)"
+        />
+        <EngineIcon
+          :engine="engineFromJSON(stats.engine)"
+          custom-class="ml-1"
+        />
+        <span
+          class="items-center text-xs px-1 py-0.5 rounded-full bg-gray-200 text-gray-800"
+        >
+          {{ stats.count }}
+        </span>
+      </label>
+    </div>
+    <div class="hidden xl:block h-[1.5rem] border-l border-control-border" />
+    <div class="flex items-center gap-x-5">
+      <label
+        v-for="stats in errorLevelList"
+        :key="stats.level"
+        class="flex items-center gap-x-2 text-sm text-gray-600"
+      >
+        <input
+          :id="sQLReviewRuleLevelToJSON(stats.level)"
+          type="checkbox"
+          :value="stats.level"
+          :checked="isCheckedLevel(stats.level)"
+          class="h-4 w-4 border-gray-300 rounded text-indigo-600 focus:ring-indigo-500"
+          @input="e =>$emit('toggle-checked-level', stats.level, (e.target as HTMLInputElement).checked)"
+        />
+        <SQLRuleLevelBadge :level="stats.level" :suffix="`(${stats.count})`" />
+      </label>
+    </div>
   </div>
 </template>
 
