d675418ebe1e59d876e21cde66cd1ccddb54d7d8
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-08-25 12:28:24 +0800

    fix: recover from check panic (#7763)
    * fix: recover from task panic
    
    * Update backend/runner/taskcheck/executor.go
    
    ---------
    
    Co-authored-by: h3n4l <oysterdays@gmail.com>

--- backend/runner/taskcheck/executor.go
+++ backend/runner/taskcheck/executor.go
@@ -3,9 +3,29 @@ package taskcheck
 import (
 	"context"
 
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+
+	"github.com/bytebase/bytebase/backend/common/log"
 	api "github.com/bytebase/bytebase/backend/legacyapi"
 	"github.com/bytebase/bytebase/backend/store"
 )
+
+func runExecutorOnce(ctx context.Context, exec Executor, taskCheckRun *store.TaskCheckRunMessage, task *store.TaskMessage) (result []api.TaskCheckResult, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			panicErr, ok := r.(error)
+			if !ok {
+				panicErr = errors.Errorf("%v", r)
+			}
+			log.Error("TaskChecker PANIC RECOVER", zap.Error(panicErr), zap.Stack("panic-stack"))
+			result = nil
+			err = errors.Errorf("encounter internal error when executing check")
+		}
+	}()
+
+	return exec.Run(ctx, taskCheckRun, task)
+}
 
 // Executor is the task check executor.
 type Executor interface {
