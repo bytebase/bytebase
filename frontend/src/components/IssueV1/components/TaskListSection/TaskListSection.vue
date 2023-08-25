e0fa9e6f6c1db9c99b3cdf60c43d255b15efd5fa
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-08-25 13:56:16 +0800

    chore: re-calculate list height

--- frontend/src/components/IssueV1/components/TaskListSection/TaskListSection.vue
+++ frontend/src/components/IssueV1/components/TaskListSection/TaskListSection.vue
@@ -2,10 +2,13 @@
   <div v-if="shouldShowTaskBar" class="relative">
     <div
       ref="taskBar"
-      class="task-list gap-2 py-2 px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 4xl:grid-cols-6 max-h-48 overflow-y-auto"
+      class="task-list gap-2 py-2 px-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 4xl:grid-cols-6 overflow-y-auto"
       :class="{
         'more-bottom': taskBarScrollState.bottom,
         'more-top': taskBarScrollState.top,
+      }"
+      :style="{
+        'max-height': `${MAX_LIST_HEIGHT}px`,
       }"
     >
       <TaskCard v-for="(task, i) in taskList" :key="i" :task="task" />
@@ -19,9 +22,12 @@ import { useVerticalScrollState } from "@/composables/useScrollState";
 import { useIssueContext } from "../../logic";
 import TaskCard from "./TaskCard.vue";
 
+// 3 * lines of cards + 2 * top and bottom padding + 2 * horizontal gaps + jitter
+const MAX_LIST_HEIGHT = 207;
+
 const { issue, selectedStage } = useIssueContext();
 const taskBar = ref<HTMLDivElement>();
-const taskBarScrollState = useVerticalScrollState(taskBar, 192);
+const taskBarScrollState = useVerticalScrollState(taskBar, MAX_LIST_HEIGHT);
 
 const rollout = computed(() => issue.value.rolloutEntity);
 const taskList = computed(() => selectedStage.value.tasks);
