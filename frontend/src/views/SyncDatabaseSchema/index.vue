49864f5d593fd7b8ed4da8f532d8bcca51295564
Tianzhou (天舟) <t@bytebase.com>
2023-08-25 10:43:58 +0800

    feat: re-purpose the scheme design feature to branching (#7754)

--- frontend/src/views/SyncDatabaseSchema/index.vue
+++ frontend/src/views/SyncDatabaseSchema/index.vue
@@ -25,10 +25,7 @@
               :value="'SCHEMA_HISTORY_VERSION'"
               :label="$t('database.sync-schema.schema-history-version')"
             />
-            <NRadio
-              :value="'SCHEMA_DESIGN'"
-              :label="$t('schema-designer.schema-design')"
-            />
+            <NRadio :value="'SCHEMA_DESIGN'" :label="$t('database.branch')" />
             <NRadio :value="'RAW_SQL'" :label="$t('schema-editor.raw-sql')" />
           </NRadioGroup>
         </div>
