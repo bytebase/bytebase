81341e8423ca8fb596cb92a0b342f9dcae02c476
p0ny <p0uy@outlook.com>
2023-08-28 17:42:58 +0800

    fix: migrate sheet payload usedByIssues

---
+++ backend/migrator/migration/prod/2.7/0002##sheet_payload_used_by_issues.sql
@@ -0,0 +1,7 @@
+ALTER TABLE sheet DISABLE TRIGGER update_sheet_updated_ts;
+
+UPDATE sheet
+SET payload = (payload || jsonb_build_object('usedByIssues', jsonb_build_array(jsonb_build_object('issueId', CAST(payload->>'issueId' AS INTEGER), 'issueName', payload->>'issueName')))) - 'type' - 'issueId' - 'issueName'
+WHERE payload ?& ARRAY['issueId','issueName'];
+
+ALTER TABLE sheet ENABLE TRIGGER update_sheet_updated_ts;
