8a99e6fe6b038fb3ba8a322976d67ca9354ffd89
rebelice <yangrebelice@gmail.com>
2023-08-28 16:20:06 +0800

    feat: impl diffColumn

--- backend/plugin/parser/sql/differ/plsql/differ.go
+++ backend/plugin/parser/sql/differ/plsql/differ.go
@@ -4,6 +4,7 @@ package plsql
 import (
 	"fmt"
 	"sort"
+	"strings"
 
 	"github.com/antlr4-go/antlr/v4"
 	"github.com/pkg/errors"
@@ -28,8 +29,10 @@ type SchemaDiffer struct {
 }
 
 type diffNode struct {
+	schemaName  string
 	dropTable   []string
 	createTable []string
+	addColumn   []string
 }
 
 // SchemaDiff implements the differ.SchemaDiffer interface.
@@ -43,7 +46,9 @@ func (*SchemaDiffer) SchemaDiff(oldStmt, newStmt string, _ bool) (string, error)
 		return "", errors.Wrapf(err, "failed to build schema info for new statement")
 	}
 
-	diff := &diffNode{}
+	diff := &diffNode{
+		schemaName: oldSchemaInfo.name,
+	}
 	var newTables []*tableInfo
 	for _, table := range newSchemaInfo.tableMap {
 		newTables = append(newTables, table)
@@ -58,7 +63,9 @@ func (*SchemaDiffer) SchemaDiff(oldStmt, newStmt string, _ bool) (string, error)
 			diff.createTable = append(diff.createTable, newTable.createTable.GetParser().GetTokenStream().GetTextFromRuleContext(newTable.createTable))
 			continue
 		}
-		diff.diffTable(oldTable, newTable)
+		if err := diff.diffTable(oldTable, newTable); err != nil {
+			return "", err
+		}
 		delete(oldSchemaInfo.tableMap, tableName)
 	}
 
@@ -76,22 +83,199 @@ func (*SchemaDiffer) SchemaDiff(oldStmt, newStmt string, _ bool) (string, error)
 	return "", nil
 }
 
-func (diff *diffNode) diffTable(oldTable, newTable *tableInfo) {
-	diff.diffColumn(oldTable, newTable)
-}
-
-func (diff *diffNode) diffColumn(oldTable, newTable *tableInfo) {
-	if newTable.createTable.Relational_table() == nil {
-		// TODO: support object_table and xmltype_table
-		return
+func (diff *diffNode) diffTable(oldTable, newTable *tableInfo) error {
+	if err := diff.diffColumn(oldTable, newTable); err != nil {
+		return err
 	}
 
+	return nil
+}
+
+func buildColumnMap(table *tableInfo) map[string]plsql.IColumn_definitionContext {
+	columnMap := make(map[string]plsql.IColumn_definitionContext)
+	if table.createTable.Relational_table() == nil {
+		return columnMap
+	}
+	for _, item := range table.createTable.Relational_table().AllRelational_property() {
+		if item.Column_definition() == nil {
+			continue
+		}
+		columnName := parser.PLSQLNormalizeIdentifierContext(item.Column_definition().Column_name().Identifier())
+		columnMap[columnName] = item.Column_definition()
+	}
+	return columnMap
+}
+
+func (diff *diffNode) diffColumn(oldTable, newTable *tableInfo) error {
+	if newTable.createTable.Relational_table() == nil {
+		// TODO: support object_table and xmltype_table
+		return nil
+	}
+
+	var addColumns []plsql.IColumn_definitionContext
+	var modifyColumns []plsql.IColumn_definitionContext
+	var dropColumns []string
+	oldColumnMap := buildColumnMap(oldTable)
 	for _, item := range newTable.createTable.Relational_table().AllRelational_property() {
 		if item.Column_definition() == nil {
 			continue
 		}
 
+		newColumnName := parser.PLSQLNormalizeIdentifierContext(item.Column_definition().Column_name().Identifier())
+		oldColumn, ok := oldColumnMap[newColumnName]
+		if !ok {
+			addColumns = append(addColumns, item.Column_definition())
+			continue
+		}
+
+		// Compare the column definition.
+		if !isColumnEqual(oldColumn, item.Column_definition()) {
+			modifyColumns = append(modifyColumns, item.Column_definition())
+		}
+		delete(oldColumnMap, newColumnName)
 	}
+	for _, column := range oldColumnMap {
+		dropColumns = append(dropColumns, parser.PLSQLNormalizeIdentifierContext(column.Column_name().Identifier()))
+	}
+
+	return diff.appendColumnDiff(oldTable.name, addColumns, modifyColumns, dropColumns)
+}
+
+func (diff *diffNode) appendColumnDiff(tableName string, addColumns []plsql.IColumn_definitionContext, modifyColumns []plsql.IColumn_definitionContext, dropColumns []string) error {
+	if len(addColumns) != 0 {
+		if err := diff.appendAddColumn(tableName, addColumns); err != nil {
+			return err
+		}
+	}
+	if len(modifyColumns) != 0 {
+		if err := diff.appendModifyColumn(tableName, modifyColumns); err != nil {
+			return err
+		}
+	}
+	if len(dropColumns) != 0 {
+		if err := diff.appendDropColumn(tableName, dropColumns); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (diff *diffNode) appendDropColumn(tableName string, dropColumns []string) error {
+	var buf strings.Builder
+
+	if _, err := buf.WriteString(`ALTER TABLE "`); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(diff.schemaName); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(`"."`); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(tableName); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(`" DROP (`); err != nil {
+		return err
+	}
+	for i, column := range dropColumns {
+		if i != 0 {
+			if _, err := buf.WriteString(`,`); err != nil {
+				return err
+			}
+		}
+		if _, err := buf.WriteString("\n	"); err != nil {
+			return err
+		}
+		if _, err := buf.WriteString(column); err != nil {
+			return err
+		}
+	}
+	if _, err := buf.WriteString("\n);"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (diff *diffNode) appendModifyColumn(tableName string, modifyColumns []plsql.IColumn_definitionContext) error {
+	var buf strings.Builder
+
+	if _, err := buf.WriteString(`ALTER TABLE "`); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(diff.schemaName); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(`"."`); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(tableName); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(`" MODIFY (`); err != nil {
+		return err
+	}
+	for i, column := range modifyColumns {
+		if i != 0 {
+			if _, err := buf.WriteString(`,`); err != nil {
+				return err
+			}
+		}
+		if _, err := buf.WriteString("\n	"); err != nil {
+			return err
+		}
+		if _, err := buf.WriteString(column.GetParser().GetTokenStream().GetTextFromRuleContext(column)); err != nil {
+			return err
+		}
+	}
+	if _, err := buf.WriteString("\n);"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (diff *diffNode) appendAddColumn(tableName string, addColumns []plsql.IColumn_definitionContext) error {
+	var buf strings.Builder
+
+	if _, err := buf.WriteString(`ALTER TABLE "`); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(diff.schemaName); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(`"."`); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(tableName); err != nil {
+		return err
+	}
+	if _, err := buf.WriteString(`" ADD (`); err != nil {
+		return err
+	}
+	for i, column := range addColumns {
+		if i != 0 {
+			if _, err := buf.WriteString(`,`); err != nil {
+				return err
+			}
+		}
+		if _, err := buf.WriteString("\n	"); err != nil {
+			return err
+		}
+		if _, err := buf.WriteString(column.GetParser().GetTokenStream().GetTextFromRuleContext(column)); err != nil {
+			return err
+		}
+	}
+	if _, err := buf.WriteString("\n);"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func isColumnEqual(oldColumn, newColumn plsql.IColumn_definitionContext) bool {
+	// TODO: compare column definition instead of text.
+	oldString := oldColumn.GetParser().GetTokenStream().GetTextFromRuleContext(oldColumn)
+	newString := newColumn.GetParser().GetTokenStream().GetTextFromRuleContext(newColumn)
+	return oldString == newString
 }
 
 func buildSchemaInfo(statement string) (*schemaInfo, error) {
