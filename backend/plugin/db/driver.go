6f5b6aa51875eb0731623f4713db1e46b4de011f
h3n4l <oysterdays@gmail.com>
2023-08-29 22:06:07 +0800

    feat(advanced-masking): evaluate the masking level of column (#7829)

--- backend/plugin/db/driver.go
+++ backend/plugin/db/driver.go
@@ -651,8 +651,10 @@ type TableSchema struct {
 
 // ColumnInfo is the column info using to extract sensitive fields.
 type ColumnInfo struct {
-	Name      string
-	Sensitive bool
+	Name string
+	// TODO(zp): retire Sensitive boolean flag.
+	Sensitive    bool
+	MaskingLevel storepb.MaskingLevel
 }
 
 // SensitiveField is the struct about SELECT fields.
