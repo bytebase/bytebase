83a7a4c496b7c92f3cbd7ec198fa68797c8f554a
d-bytebase <d@bytebase.com>
2023-08-30 14:25:26 +0800

    chore: backfill sheet vcs info

---
+++ backend/migrator/migration/prod/2.7/0003##sheet_pushevent.sql
@@ -0,0 +1,6 @@
+UPDATE
+  sheet
+SET
+  payload = json_build_object('vcsPayload', json_build_object('pushEvent', (task.payload->>'pushEvent')::JSONB))
+FROM task
+Where task.payload->>'pushEvent' IS NOT NULL AND task.payload->>'sheetId'::text = sheet.id::text ;
