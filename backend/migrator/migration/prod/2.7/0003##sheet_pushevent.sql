9b6af095e764b17967948910a2c7215d4bd7e87e
p0ny <xz@bytebase.com>
2023-08-30 14:31:53 +0800

    Update backend/migrator/migration/prod/2.7/0003##sheet_pushevent.sql

--- backend/migrator/migration/prod/2.7/0003##sheet_pushevent.sql
+++ backend/migrator/migration/prod/2.7/0003##sheet_pushevent.sql
@@ -1,6 +1,6 @@
 UPDATE
   sheet
 SET
-  payload = payload || json_build_object('vcsPayload', json_build_object('pushEvent', (task.payload->>'pushEvent')::JSONB))
+  payload = payload || jsonb_build_object('vcsPayload', jsonb_build_object('pushEvent', (task.payload->>'pushEvent')::JSONB))
 FROM task
 Where task.payload->>'pushEvent' IS NOT NULL AND task.payload->>'sheetId'::text = sheet.id::text ;
