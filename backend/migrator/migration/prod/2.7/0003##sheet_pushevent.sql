b0ca541e0532c7979d14b31b559e79d2d2fcf3e7
d-bytebase <d@bytebase.com>
2023-08-30 14:36:42 +0800

    chore: update

--- backend/migrator/migration/prod/2.7/0003##sheet_pushevent.sql
+++ backend/migrator/migration/prod/2.7/0003##sheet_pushevent.sql
@@ -1,6 +1,10 @@
+ALTER TABLE sheet DISABLE TRIGGER update_sheet_updated_ts;
+
 UPDATE
   sheet
 SET
   payload = sheet.payload || jsonb_build_object('vcsPayload', jsonb_build_object('pushEvent', (task.payload->>'pushEvent')::JSONB))
 FROM task
 Where task.payload->>'pushEvent' IS NOT NULL AND task.payload->>'sheetId'::text = sheet.id::text ;
+
+ALTER TABLE sheet ENABLE TRIGGER update_sheet_updated_ts;
