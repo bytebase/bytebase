b33e40f071c95cbc2b8d6af1fb2522716e266c74
zll600 <71714656+zll600@users.noreply.github.com>
2023-09-01 04:32:06 -0500

    feat: add announcement banner configuration (#7526)
    * feat(setting): add announcement banner configuration
    
    * chore(plan): fix typo in FeatureCustomAnnouncement comment
    
    * chore(Announcement): use Announcement instead of Custom Announcement
    
    * chore(Announcement): move feature announcement to Admin & Security
    
    * chore(proto): add UNSPECIFIED to AlertLevel
    
    * chore(proto): use ALERTLEVEL_CRITICAL instead of ALERTLEVEL_ERROR
    
    * chore(Announcement): fix typo in zh-CN.json
    
    * chore(proto): rename fields of AlertLevel in Announcement
    
    * chore(proto): update frontend
    
    Former-commit-id: 42674f8ae27a3f1bdef1e358ee6f096d45dbf569
    Former-commit-id: 01bfcb3947e6416d0a3d071508fe0e0ec91f1c29

---
+++ frontend/src/views/BannerAnnouncement.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-if="showBanner" :class="[`${bgColor}`]">
+    <div
+      class="mx-auto py-1 px-3 w-full flex flex-row items-center justify-center flex-wrap"
+    >
+      <div class="py-1 px-3 font-medium text-white flex flex-row items-center">
+        <p>{{ announcementText }}</p>
+      </div>
+      <div
+        v-if="announcementLink.length > 0"
+        class="item-center py-1 px-3 font-medium text-white truncate"
+      >
+        <a
+          :href="announcementLink"
+          target="_blank"
+          class="text-center underline"
+        >
+          <heroicons-solid:arrow-long-right class="mr-3 w-5 h-5" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useSettingV1Store } from "@/store";
+import { Announcement_AlertLevel } from "@/types/proto/v1/setting_service";
+import { urlfy } from "@/utils";
+
+const settingV1Store = useSettingV1Store();
+
+const showBanner = computed(() => {
+  return announcementText.value.length > 0;
+});
+
+const bgColor = computed(() => {
+  switch (settingV1Store.workspaceProfileSetting?.announcement?.level) {
+    case Announcement_AlertLevel.ALERT_LEVEL_INFO:
+      return "bg-info";
+    case Announcement_AlertLevel.ALERT_LEVEL_WARNING:
+      return "bg-warning";
+    case Announcement_AlertLevel.ALERT_LEVEL_CRITICAL:
+      return "bg-error";
+    default:
+      return "bg-info";
+  }
+});
+
+const announcementText = computed(() => {
+  return settingV1Store.workspaceProfileSetting?.announcement?.text ?? "";
+});
+
+const announcementLink = computed(() => {
+  const link = settingV1Store.workspaceProfileSetting?.announcement?.link ?? "";
+  if (link.length === 0) {
+    return link;
+  }
+
+  return urlfy(link);
+});
+</script>
