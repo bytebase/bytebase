83ecd9c05a6548f1907401bace59492ddae992cf
h3n4l <oysterdays@gmail.com>
2023-09-03 21:44:35 +0800

    feat(advanced-masking-policy): retire sensitive in tsql calculator (#7917)
    * feat: retire sensitive in tsql calculator
    
    * chore: update
    
    * chore: update

--- backend/plugin/db/util/mask_sensitive_data.go
+++ backend/plugin/db/util/mask_sensitive_data.go
@@ -15,12 +15,10 @@ var (
 )
 
 type fieldInfo struct {
-	name     string
-	table    string
-	schema   string
-	database string
-	// TODO(zp): retire sensitive boolean flag.
-	sensitive    bool
+	name         string
+	table        string
+	schema       string
+	database     string
 	maskingLevel storepb.MaskingLevel
 }
 
@@ -141,7 +139,18 @@ func extractSensitiveField(dbType db.Type, statement string, currentDatabase str
 			currentDatabase: currentDatabase,
 			schemaInfo:      schemaInfo,
 		}
-		return extractor.extractTSqlSensitiveFields(statement)
+		result, err := extractor.extractTSqlSensitiveFields(statement)
+		if err != nil {
+			return nil, err
+		}
+		// TODO(zp): remove it
+		// Backfill sensitive.
+		for i := range result {
+			if result[i].MaskingLevel == storepb.MaskingLevel_PARTIAL || result[i].MaskingLevel == storepb.MaskingLevel_FULL {
+				result[i].Sensitive = true
+			}
+		}
+		return result, nil
 	default:
 		return nil, nil
 	}
