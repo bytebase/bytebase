a19f2add2d5e941efbcd6688497d2b2e0fba9141
p0ny <xz@bytebase.com>
2023-09-06 16:16:59 +0800

    fix: demo task run data (#7998)
    Former-commit-id: bb36156809683fec2a64aeaa72ba56a4ce70bdc8
    Former-commit-id: 3025c4c78439502278ba67b07d25acd0d92214d5

--- backend/migrator/demo/default/10120##task_run.sql
+++ backend/migrator/demo/default/10120##task_run.sql
@@ -7,6 +7,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -20,6 +21,7 @@ VALUES
         1,
         1624873710,
         11002,
+        0,
         'Add initial schema 1624873710',
         'DONE',
         0,
@@ -35,6 +37,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -48,6 +51,7 @@ VALUES
         1,
         1624883710,
         11003,
+        0,
         'Add initial schema 1624883710',
         'DONE',
         0,
@@ -61,6 +65,7 @@ INSERT INTO
         creator_id,
         updater_id,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -72,6 +77,7 @@ VALUES
         101,
         101,
         11006,
+        0,
         'Update testdb_dev task run',
         'FAILED',
         103,
@@ -87,6 +93,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -100,6 +107,7 @@ VALUES
         1,
         1624865387,
         11008,
+        0,
         'Add initial schema 1624865387',
         'DONE',
         0,
@@ -115,6 +123,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -128,6 +137,7 @@ VALUES
         1,
         1624866790,
         11009,
+        0,
         'Add initial schema 1624866790',
         'DONE',
         0,
@@ -143,6 +153,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -156,6 +167,7 @@ VALUES
         1,
         1624868407,
         11010,
+        0,
         'Add initial schema 1624868407',
         'DONE',
         0,
@@ -171,6 +183,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -184,6 +197,7 @@ VALUES
         1,
         1624868680,
         11011,
+        0,
         'Add initial schema 1624868680',
         'DONE',
         0,
@@ -199,6 +213,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -212,13 +227,14 @@ VALUES
         1,
         1624869944,
         11012,
+        0,
         'Add created at column 1624869944',
         'FAILED',
         201,
         '{"detail":"database ''blog'' has already applied version 202106280100"}'
     );
 
--- Successful task run for task 10013 create table
+-- Successful task run for task 11013 create table
 INSERT INTO
     task_run (
         id,
@@ -227,6 +243,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -240,13 +257,14 @@ VALUES
         102,
         1624879944,
         11013,
+        0,
         'Update testdb_dev task run',
         'DONE',
         0,
         '{"detail":"Applied migration version 20210830011437.11013 to database \"testdb_dev\"","migrationId":1,"version":"20210830011437.11013"}'
     );
 
--- Successful task run for task 10014 create table
+-- Successful task run for task 11014 create table
 INSERT INTO
     task_run (
         id,
@@ -255,6 +273,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         result
@@ -267,12 +286,13 @@ VALUES
         102,
         1624879944,
         11014,
+        0,
         'Update testdb_integration task run',
         'DONE',
         '{"detail":"Applied migration version 20210830011437.11014 to database \"testdb_integration\"","migrationId":1,"version":"20210830011437.11014"}'
     );
 
--- Failed task run for task 10015 create table
+-- Failed task run for task 11015 create table
 INSERT INTO
     task_run (
         id,
@@ -281,6 +301,7 @@ INSERT INTO
         updater_id,
         updated_ts,
         task_id,
+        attempt,
         name,
         status,
         code,
@@ -294,6 +315,7 @@ VALUES
         102,
         1624879944,
         11015,
+        0,
         'Update testdb_staging task run',
         'FAILED',
         103,
