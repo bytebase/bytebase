28ba24b56a415893b6d349a30f92cc930ccca6b4
p0ny <p0uy@outlook.com>
2023-09-08 11:29:48 +0800

    revert utils change

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -301,15 +301,13 @@ func GetDatabaseMatrixFromDeploymentSchedule(schedule *api.DeploymentSchedule, d
 		for _, id := range matchedDatabaseList {
 			databaseList = append(databaseList, databaseMap[id])
 		}
-
-		if len(databaseList) == 0 {
-			continue
+		// sort databases in stage based on IDs.
+		if len(databaseList) > 0 {
+			sort.Slice(databaseList, func(i, j int) bool {
+				return databaseList[i].UID < databaseList[j].UID
+			})
 		}
 
-		// sort databases in stage based on IDs.
-		sort.Slice(databaseList, func(i, j int) bool {
-			return databaseList[i].UID < databaseList[j].UID
-		})
 		matrix = append(matrix, databaseList)
 	}
 
