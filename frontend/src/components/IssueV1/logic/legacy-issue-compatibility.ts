23c683ee2ba3e6941d960930bd4a4059381f4ca9
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-09-08 09:12:59 +0800

    feat: use legacy API to patch tasks' rollbackEnabled

--- frontend/src/components/IssueV1/logic/legacy-issue-compatibility.ts
+++ frontend/src/components/IssueV1/logic/legacy-issue-compatibility.ts
@@ -1,8 +1,31 @@
 import { useIssueStore, useSheetV1Store, useTaskStore } from "@/store";
-import { ComposedIssue, TaskPatch } from "@/types";
+import { ComposedIssue, Issue as LegacyIssue, TaskPatch } from "@/types";
 import { Task } from "@/types/proto/v1/rollout_service";
 import { extractSheetUID, setSheetStatement } from "@/utils";
 import { createEmptyLocalSheet } from "./sheet";
+
+export const batchPatchLegacyIssueTasks = async (
+  issue: ComposedIssue,
+  tasks: Task[],
+  patchGetter: (
+    legacyIssue: LegacyIssue,
+    task: Task
+  ) => TaskPatch | Promise<TaskPatch>
+) => {
+  const legacyIssue = await useIssueStore().getOrFetchIssueById(
+    Number(issue.uid)
+  );
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
+    const taskPatch = await patchGetter(legacyIssue, task);
+    await useTaskStore().patchTask({
+      issueId: legacyIssue.id,
+      pipelineId: legacyIssue.pipeline!.id,
+      taskId: Number(task.uid),
+      taskPatch,
+    });
+  }
+};
 
 export const patchLegacyIssueTasksStatement = async (
   issue: ComposedIssue,
@@ -19,19 +42,9 @@ export const patchLegacyIssueTasksStatement = async (
     sheetCreate
   );
 
-  const legacyIssue = await useIssueStore().getOrFetchIssueById(
-    Number(issue.uid)
-  );
-  for (let i = 0; i < tasks.length; i++) {
-    const task = tasks[i];
-    const taskPatch: TaskPatch = {
+  await batchPatchLegacyIssueTasks(issue, tasks, () => {
+    return {
       sheetId: Number(extractSheetUID(createdSheet.name)),
     };
-    await useTaskStore().patchTask({
-      issueId: legacyIssue.id,
-      pipelineId: legacyIssue.pipeline!.id,
-      taskId: Number(task.uid),
-      taskPatch,
-    });
-  }
+  });
 };
