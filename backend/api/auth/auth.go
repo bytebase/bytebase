565ef321e28a4c8a4895197b250d14889ccc8a03
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-09-08 11:18:40 +0800

    chore: deprecate refresh token (#8052)
    * chore: deprecate refresh token
    
    * chore: update

--- backend/api/auth/auth.go
+++ backend/api/auth/auth.go
@@ -30,47 +30,40 @@ const (
 	keyID = "v1"
 	// AccessTokenAudienceFmt is the format of the acccess token audience.
 	AccessTokenAudienceFmt = "bb.user.access.%s"
-	// RefreshTokenAudienceFmt is the format of the refresh token audience.
-	RefreshTokenAudienceFmt = "bb.user.refresh.%s"
 	// MFATempTokenAudienceFmt is the format of the MFA temp token audience.
 	MFATempTokenAudienceFmt = "bb.user.mfa-temp.%s"
 	apiTokenDuration        = 1 * time.Hour
-	accessTokenDuration     = 1 * time.Hour
-	// DefaultRefreshTokenDuration is the default refresh token expiration duration.
-	DefaultRefreshTokenDuration = 7 * 24 * time.Hour
+	// DefaultTokenDuration is the default token expiration duration.
+	DefaultTokenDuration = 7 * 24 * time.Hour
 
 	// AccessTokenCookieName is the cookie name of access token.
 	AccessTokenCookieName = "access-token"
-	// RefreshTokenCookieName is the cookie name of refresh token.
-	RefreshTokenCookieName = "refresh-token"
 	// UserIDCookieName is the cookie name of user ID.
 	UserIDCookieName = "user"
 
 	// GatewayMetadataAccessTokenKey is the gateway metadata key for access token.
 	GatewayMetadataAccessTokenKey = "bytebase-access-token"
-	// GatewayMetadataRefreshTokenKey is the gateway metadata key for refresh token.
-	GatewayMetadataRefreshTokenKey = "bytebase-refresh-token"
 	// GatewayMetadataUserIDKey is the gateway metadata key for user ID.
 	GatewayMetadataUserIDKey = "bytebase-user"
 )
 
 // APIAuthInterceptor is the auth interceptor for gRPC server.
 type APIAuthInterceptor struct {
-	store                *store.Store
-	secret               string
-	refreshTokenDuration time.Duration
-	licenseService       enterpriseAPI.LicenseService
-	mode                 common.ReleaseMode
+	store          *store.Store
+	secret         string
+	tokenDuration  time.Duration
+	licenseService enterpriseAPI.LicenseService
+	mode           common.ReleaseMode
 }
 
 // New returns a new API auth interceptor.
-func New(store *store.Store, secret string, refreshTokenDuration time.Duration, licenseService enterpriseAPI.LicenseService, mode common.ReleaseMode) *APIAuthInterceptor {
+func New(store *store.Store, secret string, tokenDuration time.Duration, licenseService enterpriseAPI.LicenseService, mode common.ReleaseMode) *APIAuthInterceptor {
 	return &APIAuthInterceptor{
-		store:                store,
-		secret:               secret,
-		refreshTokenDuration: refreshTokenDuration,
-		licenseService:       licenseService,
-		mode:                 mode,
+		store:          store,
+		secret:         secret,
+		tokenDuration:  tokenDuration,
+		licenseService: licenseService,
+		mode:           mode,
 	}
 }
 
@@ -80,12 +73,12 @@ func (in *APIAuthInterceptor) AuthenticationInterceptor(ctx context.Context, req
 	if !ok {
 		return nil, status.Errorf(codes.Unauthenticated, "failed to parse metadata from incoming context")
 	}
-	accessTokenStr, refreshTokenStr, err := getTokenFromMetadata(md)
+	accessTokenStr, err := getTokenFromMetadata(md)
 	if err != nil {
 		return nil, status.Errorf(codes.Unauthenticated, err.Error())
 	}
 
-	principalID, err := in.authenticate(ctx, accessTokenStr, refreshTokenStr)
+	principalID, err := in.authenticate(ctx, accessTokenStr)
 	if err != nil {
 		if IsAuthenticationAllowed(serverInfo.FullMethod) {
 			return handler(ctx, request)
@@ -105,12 +98,12 @@ func (in *APIAuthInterceptor) AuthenticationStreamInterceptor(request any, ss gr
 	if !ok {
 		return status.Errorf(codes.Unauthenticated, "failed to parse metadata from incoming context")
 	}
-	accessTokenStr, refreshTokenStr, err := getTokenFromMetadata(md)
+	accessTokenStr, err := getTokenFromMetadata(md)
 	if err != nil {
 		return status.Errorf(codes.Unauthenticated, err.Error())
 	}
 
-	principalID, err := in.authenticate(ctx, accessTokenStr, refreshTokenStr)
+	principalID, err := in.authenticate(ctx, accessTokenStr)
 	if err != nil {
 		if IsAuthenticationAllowed(serverInfo.FullMethod) {
 			return handler(request, ss)
@@ -133,13 +126,12 @@ func (s overrideStream) Context() context.Context {
 	return s.childCtx
 }
 
-func (in *APIAuthInterceptor) authenticate(ctx context.Context, accessTokenStr, refreshTokenStr string) (int, error) {
+func (in *APIAuthInterceptor) authenticate(ctx context.Context, accessTokenStr string) (int, error) {
 	if accessTokenStr == "" {
 		return 0, status.Errorf(codes.Unauthenticated, "access token not found")
 	}
 	claims := &claimsMessage{}
-	generateToken := false
-	accessToken, err := jwt.ParseWithClaims(accessTokenStr, claims, func(t *jwt.Token) (any, error) {
+	if _, err := jwt.ParseWithClaims(accessTokenStr, claims, func(t *jwt.Token) (any, error) {
 		if t.Method.Alg() != jwt.SigningMethodHS256.Name {
 			return nil, status.Errorf(codes.Unauthenticated, "unexpected access token signing method=%v, expect %v", t.Header["alg"], jwt.SigningMethodHS256)
 		}
@@ -149,19 +141,12 @@ func (in *APIAuthInterceptor) authenticate(ctx context.Context, accessTokenStr, 
 			}
 		}
 		return nil, status.Errorf(codes.Unauthenticated, "unexpected access token kid=%v", t.Header["kid"])
-	})
-	if err != nil {
+	}); err != nil {
 		var ve *jwt.ValidationError
 		if errors.As(err, &ve) && ve.Errors == jwt.ValidationErrorExpired {
-			// If expiration error is the only error, we will clear the err
-			// and generate new access token and refresh token
-			if refreshTokenStr == "" {
-				return 0, status.Errorf(codes.Unauthenticated, "access token is expired")
-			}
-			generateToken = true
-		} else {
-			return 0, status.Errorf(codes.Unauthenticated, "failed to parse claim")
+			return 0, status.Errorf(codes.Unauthenticated, "access token expired")
 		}
+		return 0, status.Errorf(codes.Unauthenticated, "failed to parse claim")
 	}
 	if !audienceContains(claims.Audience, fmt.Sprintf(AccessTokenAudienceFmt, in.mode)) {
 		return 0, status.Errorf(codes.Unauthenticated,
@@ -186,52 +171,6 @@ func (in *APIAuthInterceptor) authenticate(ctx context.Context, accessTokenStr, 
 		return 0, status.Errorf(codes.Unauthenticated, "user ID %q has been deactivated by administrators", principalID)
 	}
 
-	if generateToken {
-		generateTokenFunc := func() error {
-			// Parses token and checks if it's valid.
-			refreshTokenClaims := &claimsMessage{}
-			refreshToken, err := jwt.ParseWithClaims(refreshTokenStr, refreshTokenClaims, func(t *jwt.Token) (any, error) {
-				if t.Method.Alg() != jwt.SigningMethodHS256.Name {
-					return nil, status.Errorf(codes.Unauthenticated, "unexpected refresh token signing method=%v, expected %v", t.Header["alg"], jwt.SigningMethodHS256)
-				}
-
-				if kid, ok := t.Header["kid"].(string); ok {
-					if kid == "v1" {
-						return []byte(in.secret), nil
-					}
-				}
-				return nil, errs.Errorf("unexpected refresh token kid=%v", t.Header["kid"])
-			})
-			if err != nil {
-				if err == jwt.ErrSignatureInvalid {
-					return errs.Errorf("failed to generate access token: invalid refresh token signature")
-				}
-				return errs.Errorf("Server error to refresh expired token, user ID %d", principalID)
-			}
-
-			if !audienceContains(refreshTokenClaims.Audience, fmt.Sprintf(RefreshTokenAudienceFmt, in.mode)) {
-				return errs.Errorf("Invalid refresh token, audience mismatch, got %q, expected %q. you may send request to the wrong environment",
-					refreshTokenClaims.Audience,
-					fmt.Sprintf(RefreshTokenAudienceFmt, in.mode),
-				)
-			}
-
-			// If we have a valid refresh token, we will generate new access token and refresh token
-			if refreshToken != nil && refreshToken.Valid {
-				if err := generateTokensAndSetCookies(ctx, user.Name, user.ID, in.mode, in.secret, in.refreshTokenDuration); err != nil {
-					return errs.Wrapf(err, "failed to regenerate token")
-				}
-			}
-
-			return nil
-		}
-
-		// It may happen that we still have a valid access token, but we encounter issue when trying to generate new token
-		// In such case, we won't return the error.
-		if err := generateTokenFunc(); err != nil && !accessToken.Valid {
-			return 0, status.Errorf(codes.Unauthenticated, err.Error())
-		}
-	}
 	return principalID, nil
 }
 
@@ -262,17 +201,17 @@ func GetUserIDFromMFATempToken(token string, mode common.ReleaseMode, secret str
 	return userID, nil
 }
 
-func getTokenFromMetadata(md metadata.MD) (string, string, error) {
+func getTokenFromMetadata(md metadata.MD) (string, error) {
 	authorizationHeaders := md.Get("Authorization")
 	if len(md.Get("Authorization")) > 0 {
 		authHeaderParts := strings.Fields(authorizationHeaders[0])
 		if len(authHeaderParts) != 2 || strings.ToLower(authHeaderParts[0]) != "bearer" {
-			return "", "", errs.Errorf("authorization header format must be Bearer {token}")
+			return "", errs.Errorf("authorization header format must be Bearer {token}")
 		}
-		return authHeaderParts[1], "", nil
+		return authHeaderParts[1], nil
 	}
 	// check the HTTP cookie
-	var accessToken, refreshToken string
+	var accessToken string
 	for _, t := range append(md.Get("grpcgateway-cookie"), md.Get("cookie")...) {
 		header := http.Header{}
 		header.Add("Cookie", t)
@@ -280,14 +219,8 @@ func getTokenFromMetadata(md metadata.MD) (string, string, error) {
 		if v, _ := request.Cookie(AccessTokenCookieName); v != nil {
 			accessToken = v.Value
 		}
-		if v, _ := request.Cookie(RefreshTokenCookieName); v != nil {
-			refreshToken = v.Value
-		}
 	}
-	if accessToken != "" && refreshToken != "" {
-		return accessToken, refreshToken, nil
-	}
-	return "", "", nil
+	return accessToken, nil
 }
 
 func audienceContains(audience jwt.ClaimStrings, token string) bool {
@@ -304,28 +237,6 @@ type claimsMessage struct {
 	jwt.RegisteredClaims
 }
 
-// generateTokensAndSetCookies generates jwt token and saves it to the http-only cookie.
-func generateTokensAndSetCookies(ctx context.Context, userName string, userID int, mode common.ReleaseMode, secret string, refreshTokenDuration time.Duration) error {
-	accessToken, err := GenerateAccessToken(userName, userID, mode, secret)
-	if err != nil {
-		return errs.Wrap(err, "failed to generate access token")
-	}
-	// We generate here a new refresh token and saving it to the cookie.
-	refreshToken, err := GenerateRefreshToken(userName, userID, mode, secret, refreshTokenDuration)
-	if err != nil {
-		return errs.Wrap(err, "failed to generate refresh token")
-	}
-
-	if err := grpc.SetHeader(ctx, metadata.New(map[string]string{
-		GatewayMetadataAccessTokenKey:  accessToken,
-		GatewayMetadataRefreshTokenKey: refreshToken,
-		GatewayMetadataUserIDKey:       fmt.Sprintf("%d", userID),
-	})); err != nil {
-		return errs.Wrapf(err, "failed to set grpc header")
-	}
-	return nil
-}
-
 // GenerateAPIToken generates an API token.
 func GenerateAPIToken(userName string, userID int, mode common.ReleaseMode, secret string) (string, error) {
 	expirationTime := time.Now().Add(apiTokenDuration)
@@ -333,20 +244,14 @@ func GenerateAPIToken(userName string, userID int, mode common.ReleaseMode, secr
 }
 
 // GenerateAccessToken generates an access token for web.
-func GenerateAccessToken(userName string, userID int, mode common.ReleaseMode, secret string) (string, error) {
-	expirationTime := time.Now().Add(accessTokenDuration)
+func GenerateAccessToken(userName string, userID int, mode common.ReleaseMode, secret string, tokenDuration time.Duration) (string, error) {
+	expirationTime := time.Now().Add(tokenDuration)
 	return generateToken(userName, userID, fmt.Sprintf(AccessTokenAudienceFmt, mode), expirationTime, []byte(secret))
 }
 
-// GenerateRefreshToken generates a refresh token for web.
-func GenerateRefreshToken(userName string, userID int, mode common.ReleaseMode, secret string, refreshTokenDuration time.Duration) (string, error) {
-	expirationTime := time.Now().Add(refreshTokenDuration)
-	return generateToken(userName, userID, fmt.Sprintf(RefreshTokenAudienceFmt, mode), expirationTime, []byte(secret))
-}
-
 // GenerateMFATempToken generates a temporary token for MFA.
-func GenerateMFATempToken(userName string, userID int, mode common.ReleaseMode, secret string) (string, error) {
-	expirationTime := time.Now().Add(accessTokenDuration)
+func GenerateMFATempToken(userName string, userID int, mode common.ReleaseMode, secret string, tokenDuration time.Duration) (string, error) {
+	expirationTime := time.Now().Add(tokenDuration)
 	return generateToken(userName, userID, fmt.Sprintf(MFATempTokenAudienceFmt, mode), expirationTime, []byte(secret))
 }
 
--- backend/server/jwt.go
+++ backend/server/jwt.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
@@ -33,55 +32,6 @@ type Claims struct {
 
 func getPrincipalIDContextKey() string {
 	return principalIDContextKey
-}
-
-// generateTokensAndSetCookies generates jwt token and saves it to the http-only cookie.
-func generateTokensAndSetCookies(c echo.Context, user *store.UserMessage, mode common.ReleaseMode, secret string, refreshTokenDuration time.Duration) error {
-	accessToken, err := auth.GenerateAccessToken(user.Name, user.ID, mode, secret)
-	if err != nil {
-		return pkgerrors.Wrap(err, "failed to generate access token")
-	}
-
-	cookieExp := time.Now().Add(refreshTokenDuration - 1*time.Minute)
-	setTokenCookie(c, auth.AccessTokenCookieName, accessToken, cookieExp)
-	setUserCookie(c, user.ID, cookieExp)
-
-	// We generate here a new refresh token and saving it to the cookie.
-	refreshToken, err := auth.GenerateRefreshToken(user.Name, user.ID, mode, secret, refreshTokenDuration)
-	if err != nil {
-		return pkgerrors.Wrap(err, "failed to generate refresh token")
-	}
-	setTokenCookie(c, auth.RefreshTokenCookieName, refreshToken, cookieExp)
-
-	return nil
-}
-
-// Here we are creating a new cookie, which will store the valid JWT token.
-func setTokenCookie(c echo.Context, name, token string, expiration time.Time) {
-	cookie := new(http.Cookie)
-	cookie.Name = name
-	cookie.Value = token
-	cookie.Expires = expiration
-	cookie.Path = "/"
-	// Http-only helps mitigate the risk of client side script accessing the protected cookie.
-	cookie.HttpOnly = true
-	// For now, we allow Bytebase to run on non-https host, see https://github.com/bytebase/bytebase/issues/31
-	// cookie.Secure = true
-	cookie.SameSite = http.SameSiteStrictMode
-	c.SetCookie(cookie)
-}
-
-// Purpose of this cookie is to store the user's id.
-func setUserCookie(c echo.Context, userID int, expiration time.Time) {
-	cookie := new(http.Cookie)
-	cookie.Name = "user"
-	cookie.Value = strconv.Itoa(userID)
-	cookie.Expires = expiration
-	cookie.Path = "/"
-	// For now, we allow Bytebase to run on non-https host, see https://github.com/bytebase/bytebase/issues/31
-	// cookie.Secure = true
-	cookie.SameSite = http.SameSiteStrictMode
-	c.SetCookie(cookie)
 }
 
 func extractTokenFromHeader(c echo.Context) (string, error) {
@@ -119,7 +69,7 @@ func findAccessToken(c echo.Context) (string, error) {
 // JWTMiddleware validates the access token.
 // If the access token is about to expire or has expired and the request has a valid refresh token, it
 // will try to generate new access token and refresh token.
-func JWTMiddleware(pathPrefix string, principalStore *store.Store, next echo.HandlerFunc, mode common.ReleaseMode, secret string, refreshTokenDuration time.Duration) echo.HandlerFunc {
+func JWTMiddleware(pathPrefix string, principalStore *store.Store, next echo.HandlerFunc, mode common.ReleaseMode, secret string) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		path := strings.TrimPrefix(c.Request().URL.Path, pathPrefix)
 
@@ -146,7 +96,7 @@ func JWTMiddleware(pathPrefix string, principalStore *store.Store, next echo.Han
 		}
 
 		claims := &Claims{}
-		accessToken, err := jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (any, error) {
+		_, err = jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (any, error) {
 			if t.Method.Alg() != jwt.SigningMethodHS256.Name {
 				return nil, pkgerrors.Errorf("unexpected access token signing method=%v, expect %v", t.Header["alg"], jwt.SigningMethodHS256)
 			}
@@ -166,15 +116,13 @@ func JWTMiddleware(pathPrefix string, principalStore *store.Store, next echo.Han
 				))
 		}
 
-		generateToken := false
 		if err != nil {
 			var ve *jwt.ValidationError
 			if errors.As(err, &ve) {
 				// If expiration error is the only error, we will clear the err
 				// and generate new access token and refresh token
 				if ve.Errors == jwt.ValidationErrorExpired {
-					err = nil
-					generateToken = true
+					return echo.NewHTTPError(http.StatusUnauthorized, "access token expired")
 				}
 			}
 		}
@@ -194,60 +142,6 @@ func JWTMiddleware(pathPrefix string, principalStore *store.Store, next echo.Han
 			}
 			if user == nil {
 				return echo.NewHTTPError(http.StatusUnauthorized, fmt.Sprintf("Failed to find user ID: %d", principalID))
-			}
-
-			if generateToken {
-				generateTokenFunc := func() error {
-					rc, err := c.Cookie(auth.RefreshTokenCookieName)
-
-					if err != nil {
-						return echo.NewHTTPError(http.StatusUnauthorized, "Failed to generate access token. Missing refresh token.")
-					}
-
-					// Parses token and checks if it's valid.
-					refreshTokenClaims := &Claims{}
-					refreshToken, err := jwt.ParseWithClaims(rc.Value, refreshTokenClaims, func(t *jwt.Token) (any, error) {
-						if t.Method.Alg() != jwt.SigningMethodHS256.Name {
-							return nil, pkgerrors.Errorf("unexpected refresh token signing method=%v, expected %v", t.Header["alg"], jwt.SigningMethodHS256)
-						}
-
-						if kid, ok := t.Header["kid"].(string); ok {
-							if kid == "v1" {
-								return []byte(secret), nil
-							}
-						}
-						return nil, pkgerrors.Errorf("unexpected refresh token kid=%v", t.Header["kid"])
-					})
-					if err != nil {
-						if err == jwt.ErrSignatureInvalid {
-							return echo.NewHTTPError(http.StatusUnauthorized, "Failed to generate access token. Invalid refresh token signature.")
-						}
-						return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Server error to refresh expired token. User Id %d", principalID)).SetInternal(err)
-					}
-
-					if !audienceContains(refreshTokenClaims.Audience, fmt.Sprintf(auth.RefreshTokenAudienceFmt, mode)) {
-						return echo.NewHTTPError(http.StatusUnauthorized,
-							fmt.Sprintf("Invalid refresh token, audience mismatch, got %q, expected %q. you may send request to the wrong environment",
-								refreshTokenClaims.Audience,
-								fmt.Sprintf(auth.RefreshTokenAudienceFmt, mode),
-							))
-					}
-
-					// If we have a valid refresh token, we will generate new access token and refresh token
-					if refreshToken != nil && refreshToken.Valid {
-						if err := generateTokensAndSetCookies(c, user, mode, secret, refreshTokenDuration); err != nil {
-							return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Server error to refresh expired token. User Id %d", principalID)).SetInternal(err)
-						}
-					}
-
-					return nil
-				}
-
-				// It may happen that we still have a valid access token, but we encounter issue when trying to generate new token
-				// In such case, we won't return the error.
-				if err := generateTokenFunc(); err != nil && !accessToken.Valid {
-					return err
-				}
 			}
 
 			// Stores principalID into context.
