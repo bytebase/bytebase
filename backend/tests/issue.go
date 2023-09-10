250cfddfb551b6679a02ac0559dfda23ff1ac47b
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-09-10 11:50:47 +0800

    chore: refactor test (#8074)
    * chore: refactor test
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: cca427a390c9ca6793273eca756693169b4f65d3

--- backend/tests/issue.go
+++ backend/tests/issue.go
@@ -12,6 +12,20 @@ import (
 	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
 )
 
+func (ctl *controller) getLastOpenIssue(ctx context.Context, project *v1pb.Project) (*v1pb.Issue, error) {
+	resp, err := ctl.issueServiceClient.ListIssues(ctx, &v1pb.ListIssuesRequest{
+		Parent: project.Name,
+		Filter: "status = OPEN",
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(resp.Issues) == 0 {
+		return nil, errors.Errorf("open issue not found")
+	}
+	return resp.Issues[0], nil
+}
+
 func (ctl *controller) closeIssue(ctx context.Context, project *v1pb.Project, issueName string) error {
 	if _, err := ctl.issueServiceClient.BatchUpdateIssuesStatus(ctx, &v1pb.BatchUpdateIssuesStatusRequest{
 		Parent: project.Name,
