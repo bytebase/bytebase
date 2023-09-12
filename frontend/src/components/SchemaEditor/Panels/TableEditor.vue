64a1742f10682a07d7369f395bc6db689d8792a0
ecmadao <wlec@outlook.com>
2023-09-12 18:41:19 +0800

    fix: schema template issues (#8135)
    Former-commit-id: c79b32be969d5b5ac1a59c0d157e22ac7d1d58ca

--- frontend/src/components/SchemaEditor/Panels/TableEditor.vue
+++ frontend/src/components/SchemaEditor/Panels/TableEditor.vue
@@ -287,6 +287,7 @@
     <DrawerContent :title="$t('schema-template.field-template.self')">
       <div class="w-[calc(100vw-36rem)] min-w-[64rem] max-w-[calc(100vw-8rem)]">
         <FieldTemplates
+          :readonly="true"
           :engine="databaseEngine"
           @apply="handleApplyColumnTemplate"
         />
