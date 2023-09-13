838e5273d12bd5b491c04d73aa1b0a2fd4c01646
p0ny <p0uy@outlook.com>
2023-09-13 11:15:03 +0800

    refactor: UpdatePlan

--- backend/api/v1/rollout_service.go
+++ backend/api/v1/rollout_service.go
@@ -780,7 +780,8 @@ func (s *RolloutService) UpdatePlan(ctx context.Context, request *v1pb.UpdatePla
 		updatedByID[spec.Id] = spec
 	}
 
-	var doUpdateSheet bool
+	tasksMap := map[int]*store.TaskMessage{}
+	var taskPatchList []*api.TaskPatch
 	updaterID := ctx.Value(common.PrincipalIDContextKey).(int)
 	if oldPlan.PipelineUID != nil {
 		tasks, err := s.store.ListTasks(ctx, &api.TaskFind{PipelineID: oldPlan.PipelineUID})
@@ -793,6 +794,7 @@ func (s *RolloutService) UpdatePlan(ctx context.Context, request *v1pb.UpdatePla
 				ID:        task.ID,
 				UpdaterID: updaterID,
 			}
+			tasksMap[task.ID] = task
 
 			var taskSpecID struct {
 				SpecID string `json:"specId"`
@@ -871,7 +873,6 @@ func (s *RolloutService) UpdatePlan(ctx context.Context, request *v1pb.UpdatePla
 						return status.Errorf(codes.NotFound, "sheet %q not found", config.ChangeDatabaseConfig.Sheet)
 					}
 					doUpdate = true
-					doUpdateSheet = true
 					// TODO(p0ny): update schema version
 					taskPatch.SheetID = &sheet.UID
 				}
@@ -884,26 +885,39 @@ func (s *RolloutService) UpdatePlan(ctx context.Context, request *v1pb.UpdatePla
 				continue
 			}
 
-			if _, err := s.store.UpdateTaskV2(ctx, taskPatch); err != nil {
-				return nil, status.Errorf(codes.Internal, "failed to update task %q: %v", task.Name, err)
-			}
+			taskPatchList = append(taskPatchList, taskPatch)
+		}
+	}
 
-			taskPatched, err := s.store.GetTaskV2ByID(ctx, task.ID)
-			if err != nil {
-				return nil, status.Errorf(codes.Internal, "failed to get updated task %q: %v", task.Name, err)
-			}
+	var doUpdateSheet bool
+	for _, taskPatch := range taskPatchList {
+		if taskPatch.SheetID != nil {
+			doUpdateSheet = true
+			break
+		}
+	}
 
-			// enqueue or cancel after it's written to the database.
-			if taskPatch.RollbackEnabled != nil {
-				// Enqueue the rollback sql generation if the task done.
-				if *taskPatch.RollbackEnabled && taskPatched.LatestTaskRunStatus == api.TaskRunDone {
-					s.stateCfg.RollbackGenerate.Store(taskPatched.ID, taskPatched)
-				} else if !*taskPatch.RollbackEnabled {
-					// Cancel running rollback sql generation.
-					if v, ok := s.stateCfg.RollbackCancel.Load(taskPatched.ID); ok {
-						if cancel, ok := v.(context.CancelFunc); ok {
-							cancel()
-						}
+	for _, taskPatch := range taskPatchList {
+		task := tasksMap[taskPatch.ID]
+		if _, err := s.store.UpdateTaskV2(ctx, taskPatch); err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to update task %q: %v", task.Name, err)
+		}
+
+		taskPatched, err := s.store.GetTaskV2ByID(ctx, task.ID)
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to get updated task %q: %v", task.Name, err)
+		}
+
+		// enqueue or cancel after it's written to the database.
+		if taskPatch.RollbackEnabled != nil {
+			// Enqueue the rollback sql generation if the task done.
+			if *taskPatch.RollbackEnabled && taskPatched.LatestTaskRunStatus == api.TaskRunDone {
+				s.stateCfg.RollbackGenerate.Store(taskPatched.ID, taskPatched)
+			} else if !*taskPatch.RollbackEnabled {
+				// Cancel running rollback sql generation.
+				if v, ok := s.stateCfg.RollbackCancel.Load(taskPatched.ID); ok {
+					if cancel, ok := v.(context.CancelFunc); ok {
+						cancel()
 					}
 					// We don't erase the keys for RollbackCancel and RollbackGenerate here because they will eventually be erased by the rollback runner.
 				}
