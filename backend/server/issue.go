84424c2df54b615d81bc4b853adb8fae9328abcd
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-09-13 18:49:27 +0800

    chore: delete legacy list/patch issue backend API (#8169)
    * chore: delete legacy list/patch issue backend API
    
    * chore: update
    
    Former-commit-id: 0e572b95fdd7db9977d87b1971299f8c1b371bdf

--- backend/server/issue.go
+++ backend/server/issue.go
@@ -1,17 +1,12 @@
 package server
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"math"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/google/jsonapi"
 	"github.com/labstack/echo/v4"
-	"github.com/pkg/errors"
 
 	"github.com/bytebase/bytebase/backend/common"
 	api "github.com/bytebase/bytebase/backend/legacyapi"
@@ -20,121 +15,6 @@ import (
 )
 
 func (s *Server) registerIssueRoutes(g *echo.Group) {
-	g.GET("/issue", func(c echo.Context) error {
-		ctx := c.Request().Context()
-		issueFind := &store.FindIssueMessage{}
-
-		pageToken := c.QueryParams().Get("token")
-		// We use descending order by default for issues.
-		sinceID, err := unmarshalPageToken(pageToken, api.DESC)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "Malformed page token").SetInternal(err)
-		}
-		issueFind.SinceID = &sinceID
-
-		projectIDStr := c.QueryParams().Get("project")
-		if projectIDStr != "" {
-			projectID, err := strconv.Atoi(projectIDStr)
-			if err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("project query parameter is not a number: %s", projectIDStr)).SetInternal(err)
-			}
-			issueFind.ProjectUID = &projectID
-		}
-		if issueStatusListStr := c.QueryParam("status"); issueStatusListStr != "" {
-			statusList := []api.IssueStatus{}
-			for _, status := range strings.Split(issueStatusListStr, ",") {
-				statusList = append(statusList, api.IssueStatus(status))
-			}
-			issueFind.StatusList = statusList
-		}
-		if limitStr := c.QueryParam("limit"); limitStr != "" {
-			limit, err := strconv.Atoi(limitStr)
-			if err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("limit query parameter is not a number: %s", limitStr)).SetInternal(err)
-			}
-			issueFind.Limit = &limit
-		} else {
-			limit := api.DefaultPageSize
-			issueFind.Limit = &limit
-		}
-
-		if userIDStr := c.QueryParam("user"); userIDStr != "" {
-			userID, err := strconv.Atoi(userIDStr)
-			if err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("user query parameter is not a number: %s", userIDStr)).SetInternal(err)
-			}
-			issueFind.PrincipalID = &userID
-		}
-		if creatorIDStr := c.QueryParam("creator"); creatorIDStr != "" {
-			creatorID, err := strconv.Atoi(creatorIDStr)
-			if err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("creator query parameter is not a number: %s", creatorIDStr)).SetInternal(err)
-			}
-			issueFind.CreatorID = &creatorID
-		}
-		if assigneeIDStr := c.QueryParam("assignee"); assigneeIDStr != "" {
-			assigneeID, err := strconv.Atoi(assigneeIDStr)
-			if err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("assignee query parameter is not a number: %s", assigneeIDStr)).SetInternal(err)
-			}
-			issueFind.AssigneeID = &assigneeID
-		}
-		if subscriberIDStr := c.QueryParam("subscriber"); subscriberIDStr != "" {
-			subscriberID, err := strconv.Atoi(subscriberIDStr)
-			if err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("subscriber query parameter is not a number: %s", subscriberIDStr)).SetInternal(err)
-			}
-			issueFind.SubscriberID = &subscriberID
-		}
-
-		issueList, err := s.store.FindIssueStripped(ctx, issueFind)
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to fetch issue list").SetInternal(err)
-		}
-
-		for _, issue := range issueList {
-			if issue.Pipeline == nil {
-				continue
-			}
-			for _, stage := range issue.Pipeline.StageList {
-				for _, task := range stage.TaskList {
-					switch task.LatestTaskRunStatus {
-					case api.TaskRunNotStarted:
-						task.Status = api.TaskPendingApproval
-					case api.TaskRunPending:
-						task.Status = api.TaskPending
-					case api.TaskRunRunning:
-						task.Status = api.TaskRunning
-					case api.TaskRunDone:
-						task.Status = api.TaskDone
-					case api.TaskRunFailed:
-						task.Status = api.TaskFailed
-					case api.TaskRunCanceled:
-						task.Status = api.TaskCanceled
-					}
-				}
-			}
-		}
-
-		issueResponse := &api.IssueResponse{}
-		issueResponse.Issues = issueList
-
-		nextSinceID := sinceID
-		if len(issueList) > 0 {
-			// Decrement the ID as we use decreasing order by default.
-			nextSinceID = issueList[len(issueList)-1].ID - 1
-		}
-		if issueResponse.NextToken, err = marshalPageToken(nextSinceID); err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to marshal page token").SetInternal(err)
-		}
-
-		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
-		if err := jsonapi.MarshalPayload(c.Response().Writer, issueResponse); err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, "Failed to marshal issue list response").SetInternal(err)
-		}
-		return nil
-	})
-
 	g.GET("/issue/:issueID", func(c echo.Context) error {
 		ctx := c.Request().Context()
 		id, err := strconv.Atoi(c.Param("issueID"))
@@ -200,34 +80,4 @@ func (s *Server) registerIssueRoutes(g *echo.Group) {
 		}
 		return nil
 	})
-}
-
-func marshalPageToken(id int) (string, error) {
-	b, err := json.Marshal(id)
-	if err != nil {
-		return "", errors.Wrap(err, "failed to marshal page token")
-	}
-	return base64.StdEncoding.EncodeToString(b), nil
-}
-
-// unmarshalPageToken unmarshals the page token, and returns the last issue ID. If the page token nil empty, it returns MaxInt32.
-func unmarshalPageToken(pageToken string, sortOrder api.SortOrder) (int, error) {
-	if pageToken == "" {
-		if sortOrder == api.ASC {
-			return 0, nil
-		}
-		return math.MaxInt32, nil
-	}
-
-	bs, err := base64.StdEncoding.DecodeString(pageToken)
-	if err != nil {
-		return 0, errors.Wrap(err, "failed to decode page token")
-	}
-
-	var id int
-	if err := json.Unmarshal(bs, &id); err != nil {
-		return 0, errors.Wrap(err, "failed to unmarshal page token")
-	}
-
-	return id, nil
 }
