93baed4188a474669a96d9d6090301c7ce3df7a5
ecmadao <wlec@outlook.com>
2023-09-13 12:23:10 +0800

    chore: update

--- frontend/src/components/SensitiveData/SemanticTypesView.vue
+++ frontend/src/components/SensitiveData/SemanticTypesView.vue
@@ -4,30 +4,98 @@
       <NButton
         type="primary"
         :disabled="!hasPermission || !hasSensitiveDataFeature"
-        @click=""
+        @click="onAdd"
       >
         {{ $t("settings.sensitive-data.semantic-types.add-type") }}
       </NButton>
     </div>
     <div class="space-y-5 divide-y-2 pb-10 divide-gray-100">
-      <div
-        v-if="state.semanticTypeList.length === 0"
-        class="border-4 border-dashed border-gray-200 rounded-lg h-96 flex justify-center items-center"
+      <BBTable
+        ref="tableRef"
+        :column-list="tableHeaderList"
+        :data-source="state.semanticItemList"
+        :show-header="true"
+        :custom-header="false"
+        :left-bordered="true"
+        :right-bordered="true"
+        :top-bordered="true"
+        :bottom-bordered="true"
+        :compact-section="true"
+        :row-clickable="false"
       >
-        <div class="text-center flex flex-col justify-center items-center">
-          <img src="../../assets/illustration/no-data.webp" class="w-52" />
-        </div>
-      </div>
+        <template
+          #body="{ rowData, row }: { rowData: SemanticItem, row: number }"
+        >
+          <BBTableCell class="bb-grid-cell">
+            <NInput
+              :value="rowData.item.title"
+              class=""
+              type="text"
+              :placeholder="
+                $t('settings.sensitive-data.semantic-types.table.semantic-type')
+              "
+              @input="(val: string) => onInput(row, (data) => data.item.title = val)"
+              @blur="onBlur(rowData)"
+            />
+          </BBTableCell>
+          <BBTableCell class="bb-grid-cell">
+            <NInput
+              :value="rowData.item.description"
+              class=""
+              type="text"
+              :placeholder="
+                $t('settings.sensitive-data.semantic-types.table.description')
+              "
+              @input="(val: string) => onInput(row, (data) => data.item.description = val)"
+              @blur="onBlur(rowData)"
+            />
+          </BBTableCell>
+          <BBTableCell class="bb-grid-cell"> FULL MASKING </BBTableCell>
+          <BBTableCell class="bb-grid-cell"> HALF MASKING </BBTableCell>
+          <BBTableCell v-if="hasPermission" class="bb-grid-cell w-6">
+            <div class="flex items-center">
+              <NButton
+                v-if="rowData.mode === 'CREATE'"
+                type="primary"
+                @click.stop="onConfirm(row)"
+                :disabled="isConfirmDisabled(rowData) || !hasPermission"
+              >
+                {{ $t("common.confirm") }}
+              </NButton>
+              <NPopconfirm
+                v-if="rowData.mode === 'EDIT'"
+                @positive-click="onRemove(row)"
+              >
+                <template #trigger>
+                  <button
+                    class="w-5 h-5 p-0.5 hover:bg-gray-300 rounded cursor-pointer disabled:cursor-not-allowed disabled:hover:bg-white disabled:text-gray-400"
+                    @click.stop=""
+                    :disabled="!hasPermission"
+                  >
+                    <heroicons-outline:trash class="w-4 h-4" />
+                  </button>
+                </template>
+
+                <div class="whitespace-nowrap">
+                  {{
+                    $t("settings.sensitive-data.semantic-types.table.delete")
+                  }}
+                </div>
+              </NPopconfirm>
+            </div>
+          </BBTableCell>
+        </template>
+      </BBTable>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import { NPopconfirm } from "naive-ui";
+import { NPopconfirm, NButton, NInput } from "naive-ui";
 import type { SelectOption } from "naive-ui";
 import { v4 as uuidv4 } from "uuid";
 import { computed, reactive, nextTick, onMounted } from "vue";
 import { useI18n } from "vue-i18n";
-import { Factor } from "@/plugins/cel";
+import type { BBTableColumn } from "@/bbkit/types";
 import {
   featureToRef,
   pushNotification,
@@ -42,22 +110,24 @@ import {
   MaskingRulePolicy_MaskingRule,
 } from "@/types/proto/v1/org_policy_service";
 import { SemanticCategorySetting_SemanticCategory } from "@/types/proto/v1/setting_service";
-import { arraySwap, hasWorkspacePermissionV1 } from "@/utils";
-import {
-  getClassificationLevelOptions,
-  getEnvironmentIdOptions,
-  getInstanceIdOptions,
-  getProjectIdOptions,
-} from "./components/utils";
+import { hasWorkspacePermissionV1 } from "@/utils";
+
+type SemanticItemMode = "CREATE" | "EDIT";
+
+interface SemanticItem {
+  mode: SemanticItemMode;
+  dirty: boolean;
+  item: SemanticCategorySetting_SemanticCategory;
+}
 
 interface LocalState {
-  semanticTypeList: SemanticCategorySetting_SemanticCategory[];
+  semanticItemList: SemanticItem[];
   processing: boolean;
 }
 
 const { t } = useI18n();
 const state = reactive<LocalState>({
-  semanticTypeList: [],
+  semanticItemList: [],
   processing: false,
 });
 
@@ -71,4 +141,134 @@ const hasPermission = computed(() => {
   );
 });
 const hasSensitiveDataFeature = featureToRef("bb.feature.sensitive-data");
+
+const tableHeaderList = computed(() => {
+  const list: BBTableColumn[] = [
+    {
+      title: t("settings.sensitive-data.semantic-types.table.semantic-type"),
+    },
+    {
+      title: t("settings.sensitive-data.semantic-types.table.description"),
+    },
+    {
+      title: t(
+        "settings.sensitive-data.semantic-types.table.full-masking-algorithm"
+      ),
+    },
+    {
+      title: t(
+        "settings.sensitive-data.semantic-types.table.partial-masking-algorithm"
+      ),
+    },
+  ];
+  if (hasPermission) {
+    // operation.
+    list.push({
+      title: "",
+    });
+  }
+  return list;
+});
+
+const onAdd = () => {
+  state.semanticItemList.push({
+    mode: "CREATE",
+    dirty: false,
+    item: SemanticCategorySetting_SemanticCategory.fromJSON({
+      id: uuidv4(),
+    }),
+  });
+};
+
+const onRemove = async (index: number) => {
+  const item = state.semanticItemList[index];
+  if (!item) {
+    return;
+  }
+  state.semanticItemList.splice(index, 1);
+  if (item.mode === "CREATE") {
+    return;
+  }
+  // TODO: call api
+
+  pushNotification({
+    module: "bytebase",
+    style: "SUCCESS",
+    title: t("common.deleted"),
+  });
+};
+
+const onConfirm = async (index: number) => {
+  const item = state.semanticItemList[index];
+  if (!item || item.mode !== "CREATE") {
+    return;
+  }
+
+  state.semanticItemList[index] = {
+    ...item,
+    dirty: false,
+    mode: "EDIT",
+  };
+
+  // TODO: call api
+
+  pushNotification({
+    module: "bytebase",
+    style: "SUCCESS",
+    title: t("common.created"),
+  });
+};
+
+const onInput = (index: number, callback: (item: SemanticItem) => void) => {
+  const item = state.semanticItemList[index];
+  if (!item) {
+    return;
+  }
+  callback(item);
+};
+
+const onDropdownChange = async (
+  index: number,
+  callback: (item: SemanticItem) => void
+) => {
+  const item = state.semanticItemList[index];
+  if (!item) {
+    return;
+  }
+  callback(item);
+  if (item.mode === "CREATE") {
+    return;
+  }
+
+  // TODO: call api
+
+  pushNotification({
+    module: "bytebase",
+    style: "SUCCESS",
+    title: t("common.updated"),
+  });
+};
+
+const onBlur = async (item: SemanticItem) => {
+  if (!item.dirty) {
+    return;
+  }
+  if (item.mode === "CREATE") {
+    return;
+  }
+  // TODO: call api
+
+  pushNotification({
+    module: "bytebase",
+    style: "SUCCESS",
+    title: t("common.updated"),
+  });
+};
+
+const isConfirmDisabled = (data: SemanticItem): boolean => {
+  if (!data.item.title || !data.item.description) {
+    return true;
+  }
+  return false;
+};
 </script>
