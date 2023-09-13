2eb19610694ebd1d0e08527440650ca5bcc63ffa
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-09-13 22:15:52 +0800

    chore: deprecate legacy issue patch (#8174)
    Former-commit-id: ecc1e25d06372c5d6e0dbc97b7d6eb5d7f5236a3

--- backend/server/issue.go
+++ backend/server/issue.go
@@ -7,11 +7,6 @@ import (
 
 	"github.com/google/jsonapi"
 	"github.com/labstack/echo/v4"
-
-	"github.com/bytebase/bytebase/backend/common"
-	api "github.com/bytebase/bytebase/backend/legacyapi"
-	"github.com/bytebase/bytebase/backend/store"
-	"github.com/bytebase/bytebase/backend/utils"
 )
 
 func (s *Server) registerIssueRoutes(g *echo.Group) {
@@ -32,50 +27,6 @@ func (s *Server) registerIssueRoutes(g *echo.Group) {
 
 		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
 		if err := jsonapi.MarshalPayload(c.Response().Writer, issue); err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to marshal issue ID response: %v", id)).SetInternal(err)
-		}
-		return nil
-	})
-
-	g.PATCH("/issue/:issueID/status", func(c echo.Context) error {
-		ctx := c.Request().Context()
-		id, err := strconv.Atoi(c.Param("issueID"))
-		if err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, fmt.Sprintf("ID is not a number: %s", c.Param("issueID"))).SetInternal(err)
-		}
-
-		issueStatusPatch := &api.IssueStatusPatch{
-			ID:        id,
-			UpdaterID: c.Get(getPrincipalIDContextKey()).(int),
-		}
-		if err := jsonapi.UnmarshalPayload(c.Request().Body, issueStatusPatch); err != nil {
-			return echo.NewHTTPError(http.StatusBadRequest, "Malformed update issue status request").SetInternal(err)
-		}
-
-		issue, err := s.store.GetIssueV2(ctx, &store.FindIssueMessage{UID: &id})
-		if err != nil {
-			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to fetch issue ID: %v", id)).SetInternal(err)
-		}
-		if issue == nil {
-			return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("Issue ID not found: %d", id))
-		}
-
-		if err := utils.ChangeIssueStatus(ctx, s.store, s.activityManager, issue, issueStatusPatch.Status, issueStatusPatch.UpdaterID, issueStatusPatch.Comment); err != nil {
-			if common.ErrorCode(err) == common.NotFound {
-				return echo.NewHTTPError(http.StatusNotFound).SetInternal(err)
-			} else if common.ErrorCode(err) == common.Conflict {
-				return echo.NewHTTPError(http.StatusConflict).SetInternal(err)
-			}
-			return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
-		}
-
-		updatedComposedIssue, err := s.store.GetIssueByID(ctx, id)
-		if err != nil {
-			return err
-		}
-
-		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
-		if err := jsonapi.MarshalPayload(c.Response().Writer, updatedComposedIssue); err != nil {
 			return echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("Failed to marshal issue ID response: %v", id)).SetInternal(err)
 		}
 		return nil
