554013c9a8f6021082a8165f066bb4365365358c
steven <stevenlgtm@gmail.com>
2023-09-13 16:27:46 +0800

    chore: update

--- frontend/src/store/modules/schemaEditor.ts
+++ frontend/src/store/modules/schemaEditor.ts
@@ -10,8 +10,8 @@ import {
   DatabaseEdit,
   ResourceObject,
 } from "@/types";
-import { SchemaMetadata } from "@/types/proto/store/database";
 import { Engine } from "@/types/proto/v1/common";
+import { SchemaMetadata } from "@/types/proto/v1/database_service";
 import { DatabaseEditResult } from "@/types/schemaEditor";
 import {
   convertSchemaMetadataList,
