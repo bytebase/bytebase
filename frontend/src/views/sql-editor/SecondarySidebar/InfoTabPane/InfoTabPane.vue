dd472a3032d45601d1dde5922d7bc8f2bd61325f
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-09-20 09:51:06 +0800

    refactor: extract alter-schema events (#8343)
    Former-commit-id: c6b92227e06bc3e66833d8b218023cd7efb5fb23
    Former-commit-id: 9e25e1e6e61e504cd1c1e7aa8f44a0c2701d3118

--- frontend/src/views/sql-editor/SecondarySidebar/InfoTabPane/InfoTabPane.vue
+++ frontend/src/views/sql-editor/SecondarySidebar/InfoTabPane/InfoTabPane.vue
@@ -1,14 +1,7 @@
 <template>
-  <SchemaPanel @alter-schema="$emit('alter-schema', $event)" />
+  <SchemaPanel />
 </template>
 
 <script setup lang="ts">
 import SchemaPanel from "./SchemaPanel";
-
-defineEmits<{
-  (
-    event: "alter-schema",
-    params: { databaseId: string; schema: string; table: string }
-  ): void;
-}>();
 </script>
