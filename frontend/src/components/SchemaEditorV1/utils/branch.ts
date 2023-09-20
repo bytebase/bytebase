7798ded3d88f6b27c1b92398f900ebdb0449d069
boojack <stevenlgtm@gmail.com>
2023-09-20 14:37:52 +0800

    chore(branch): use parent branch baseline schema as origin (#8359)
    Former-commit-id: 64e1f1892ac7d3fa5d040531b4fe0f18919db117
    Former-commit-id: f47b9dfdbbfe31ca0c93ed951ef976edf671265e

--- frontend/src/components/SchemaEditorV1/utils/branch.ts
+++ frontend/src/components/SchemaEditorV1/utils/branch.ts
@@ -1,4 +1,8 @@
-import { SchemaDesign } from "@/types/proto/v1/schema_design_service";
+import { useSchemaDesignStore } from "@/store/modules/schemaDesign";
+import {
+  SchemaDesign,
+  SchemaDesign_Type,
+} from "@/types/proto/v1/schema_design_service";
 import {
   BranchSchema,
   convertSchemaMetadataList,
@@ -8,13 +12,25 @@ import { rebuildEditableSchemas } from "./metadataV1";
 export const convertBranchToBranchSchema = (
   branch: SchemaDesign
 ): BranchSchema => {
-  const originalSchemas = convertSchemaMetadataList(
-    branch.baselineSchemaMetadata?.schemas || []
-  );
+  let originalSchemas = [];
+  // For personal branches, we use its parent branch's schema as the original schema in editing state.
+  if (branch.type === SchemaDesign_Type.PERSONAL_DRAFT) {
+    const parentBranch = useSchemaDesignStore().getSchemaDesignByName(
+      branch.baselineSheetName
+    );
+    originalSchemas = convertSchemaMetadataList(
+      parentBranch?.baselineSchemaMetadata?.schemas || []
+    );
+  } else {
+    originalSchemas = convertSchemaMetadataList(
+      branch.baselineSchemaMetadata?.schemas || []
+    );
+  }
   const editableSchemas = rebuildEditableSchemas(
     originalSchemas,
     branch.schemaMetadata?.schemas || []
   );
+
   return {
     branch,
     schemaList: editableSchemas,
