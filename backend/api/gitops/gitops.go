4b5d6fd85999869a946962ff575dbbb7c11a3873
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-09-20 09:01:41 +0800

    chore: move GitOps webhook stuff around (#8340)
    Former-commit-id: ca6b0370cf1dc05d9f83680708140e9abf14e7f2

---
+++ backend/api/gitops/gitops.go
@@ -0,0 +1,43 @@
+// Package gitops is the package for GitOps APIs.
+package gitops
+
+import (
+	v1pb "github.com/bytebase/bytebase/backend/api/v1"
+	"github.com/bytebase/bytebase/backend/component/activity"
+	"github.com/bytebase/bytebase/backend/component/dbfactory"
+	"github.com/bytebase/bytebase/backend/component/state"
+	enterpriseAPI "github.com/bytebase/bytebase/backend/enterprise/api"
+	"github.com/bytebase/bytebase/backend/store"
+)
+
+// Service is the API endpoint for handling GitOps requests.
+type Service struct {
+	store           *store.Store
+	dbFactory       *dbfactory.DBFactory
+	activityManager *activity.Manager
+	stateCfg        *state.State
+	licenseService  enterpriseAPI.LicenseService
+	rolloutService  *v1pb.RolloutService
+	issueService    *v1pb.IssueService
+}
+
+// NewService creates a GitOps service.
+func NewService(
+	store *store.Store,
+	dbFactory *dbfactory.DBFactory,
+	activityManager *activity.Manager,
+	stateCfg *state.State,
+	licenseService enterpriseAPI.LicenseService,
+	rolloutService *v1pb.RolloutService,
+	issueService *v1pb.IssueService,
+) *Service {
+	return &Service{
+		store:           store,
+		dbFactory:       dbFactory,
+		activityManager: activityManager,
+		stateCfg:        stateCfg,
+		licenseService:  licenseService,
+		rolloutService:  rolloutService,
+		issueService:    issueService,
+	}
+}
