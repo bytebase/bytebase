e4ee645e71a6b2c5b9bf6921daea74ae74c38955
boojack <stevenlgtm@gmail.com>
2023-09-21 17:38:53 +0800

    chore: update create branch button position (#8382)
    * chore: update create branch button position
    
    * chore: update
    
    Former-commit-id: b26d292fdd67d63aaf07d26e862b90eb10bd1a12
    Former-commit-id: d658bdf68f7c888d830b3a32fa36c83a87a0017a

--- frontend/src/components/Branch/BranchDetailView.vue
+++ frontend/src/components/Branch/BranchDetailView.vue
@@ -46,7 +46,9 @@
 
     <div class="w-full flex flex-row justify-between items-center mt-1 gap-4">
       <div class="flex flex-row justify-start items-center opacity-80">
-        <span class="mr-4">{{ $t("schema-designer.baseline-version") }}:</span>
+        <span class="mr-4 shrink-0"
+          >{{ $t("schema-designer.baseline-version") }}:</span
+        >
         <DatabaseInfo
           class="flex-nowrap mr-4 shrink-0"
           :database="baselineDatabase"
