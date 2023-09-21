a0d830e56ee1b2f7c2600327e38af8215eb8dde2
rebelice <yangrebelice@gmail.com>
2023-09-21 15:54:18 +0800

    chore: use yaml.v3

--- backend/plugin/schema-engine/pg/structurer_test.go
+++ backend/plugin/schema-engine/pg/structurer_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 
 	// Import PostgreSQL parser.
 	_ "github.com/bytebase/bytebase/backend/plugin/parser/sql/engine/pg"
