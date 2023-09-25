237d507dab8804e86b3ae171329de26ba461331a
h3n4l <oysterdays@gmail.com>
2023-09-25 17:06:52 +0800

    chore: update

--- backend/api/v1/schema_design_transformer.go
+++ backend/api/v1/schema_design_transformer.go
@@ -752,6 +752,7 @@ func getMySQLDesignSchema(baselineSchema string, to *v1pb.DatabaseMetadata) (str
 
 	// The last statement of the result is SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 	// We should append a 0xa to the end of the result to avoid the extra newline diff.
+	// TODO(rebelice/zp): find a more elegant way to do this.
 	if err := listener.result.WriteByte('\n'); err != nil {
 		return "", err
 	}
