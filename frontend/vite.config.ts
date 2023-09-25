792d3fad66cbb7922836e5fe4bcf153e17aa1939
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-09-25 11:22:18 +0800

    chore: add max_old_space_size (#8427)
    Former-commit-id: c9aa24009d5afb99d3685e70c3d5151f39c2bb4c

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -48,7 +48,6 @@ export default defineConfig(() => {
           "explain-visualizer": resolve(__dirname, "explain-visualizer.html"),
         },
       },
-      chunkSizeWarningLimit: 1600,
     },
     server: {
       port: SERVER_PORT,
