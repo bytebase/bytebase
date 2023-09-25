1a0ded62eac7e3c5db3dd2044eb4dbc80fa64f0a
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-09-25 18:11:17 +0800

    chore: tips for SQL check button when no SQL review policy configured (#8446)
    Former-commit-id: 626cbbc243bb0a8a66fd619bf908cad4d56ab2f1
    Former-commit-id: 6dc538ac9f695b63f0f8b18c46c75d3cca8f0968

--- frontend/src/components/SQLCheck/SQLCheckButton.vue
+++ frontend/src/components/SQLCheck/SQLCheckButton.vue
@@ -2,11 +2,11 @@
   <div class="flex flex-row items-center gap-2">
     <slot name="result" :advices="advices" :is-running="isRunning" />
 
-    <NTooltip :disabled="!errors || errors.length === 0">
+    <NPopover :disabled="tooltipDisabled" to="body">
       <template #trigger>
         <NButton
           style="--n-padding: 0 14px 0 10px"
-          :disabled="disabled"
+          :disabled="buttonDisabled"
           :style="buttonStyle"
           tag="div"
           v-bind="buttonProps"
@@ -27,19 +27,37 @@
         </NButton>
       </template>
       <template #default>
-        <ErrorList :errors="errors ?? []" />
+        <template v-if="noReviewPolicyTips">
+          <i18n-t :keypath="noReviewPolicyTips" tag="div">
+            <template #environment>
+              <span>{{ database.effectiveEnvironmentEntity.title }}</span>
+            </template>
+            <template #link>
+              <router-link
+                v-if="hasManageSQLReviewPolicyPermission"
+                :to="`/setting/sql-review`"
+                class="ml-1 normal-link underline"
+              >
+                {{ $t("common.go-to-configure") }}
+              </router-link>
+            </template>
+          </i18n-t>
+        </template>
+        <ErrorList v-else :errors="errors ?? []" />
       </template>
-    </NTooltip>
+    </NPopover>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ButtonProps, NButton, NTooltip } from "naive-ui";
-import { computed, ref } from "vue";
-import { CSSProperties } from "vue";
+import { ButtonProps, NButton, NPopover } from "naive-ui";
+import { CSSProperties, computed, ref } from "vue";
 import { sqlServiceClient } from "@/grpcweb";
+import { usePolicyByParentAndType } from "@/store";
 import { ComposedDatabase } from "@/types";
+import { PolicyType } from "@/types/proto/v1/org_policy_service";
 import { Advice } from "@/types/proto/v1/sql_service";
+import { useWorkspacePermissionV1 } from "@/utils";
 import ErrorList from "../misc/ErrorList.vue";
 
 const props = defineProps<{
@@ -53,9 +71,35 @@ const props = defineProps<{
 const isRunning = ref(false);
 const advices = ref<Advice[]>();
 
-const disabled = computed(() => {
+const reviewPolicy = usePolicyByParentAndType(
+  computed(() => ({
+    parentPath: props.database.effectiveEnvironment,
+    policyType: PolicyType.SQL_REVIEW,
+  }))
+);
+
+const hasManageSQLReviewPolicyPermission = useWorkspacePermissionV1(
+  "bb.permission.workspace.manage-sql-review-policy"
+);
+
+const noReviewPolicyTips = computed(() => {
+  if (!reviewPolicy.value?.sqlReviewPolicy) {
+    if (hasManageSQLReviewPolicyPermission.value) {
+      return "issue.sql-check.no-configured-sql-review-policy.admin";
+    } else {
+      return "issue.sql-check.no-configured-sql-review-policy.developer";
+    }
+  }
+  return "";
+});
+
+const buttonDisabled = computed(() => {
+  if (noReviewPolicyTips.value) return true;
   if (!props.statement) return true;
   return props.errors && props.errors.length > 0;
+});
+const tooltipDisabled = computed(() => {
+  return !buttonDisabled.value;
 });
 
 const runChecks = async () => {
