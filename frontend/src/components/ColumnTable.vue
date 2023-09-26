2f109fb211eb7ebfa9aee82ff4759ab73eeff2a1
ecmadao <wlec@outlook.com>
2023-09-26 10:16:19 +0800

    chore: reset

--- frontend/src/components/ColumnTable.vue
+++ frontend/src/components/ColumnTable.vue
@@ -51,17 +51,6 @@
           </button>
         </div>
       </BBTableCell>
-      <BBTableCell v-if="showSensitiveColumn" class="bb-grid-cell">
-        <div class="flex items-center">
-          <button
-            v-if="allowAdmin"
-            class="w-5 h-5 p-0.5 hover:bg-gray-300 rounded cursor-pointer"
-            @click.prevent=""
-          >
-            <heroicons-outline:pencil class="w-4 h-4" />
-          </button>
-        </div>
-      </BBTableCell>
       <BBTableCell v-if="showClassificationColumn" class="bb-grid-cell">
         <div class="flex items-center">
           <ClassificationLevelBadge
@@ -251,16 +240,9 @@ const NORMAL_COLUMN_LIST = computed(() => {
     },
   ];
   if (showSensitiveColumn.value) {
-    columnList.splice(
-      1,
-      0,
-      {
-        title: t("settings.sensitive-data.masking-level.self"),
-      },
-      {
-        title: t("settings.sensitive-data.semantic-types.self"),
-      }
-    );
+    columnList.splice(1, 0, {
+      title: t("settings.sensitive-data.masking-level.self"),
+    });
   }
   if (showClassificationColumn.value) {
     columnList.splice(showSensitiveColumn.value ? 2 : 1, 0, {
@@ -291,16 +273,9 @@ const POSTGRES_COLUMN_LIST = computed(() => {
     },
   ];
   if (showSensitiveColumn.value) {
-    columnList.splice(
-      1,
-      0,
-      {
-        title: t("settings.sensitive-data.masking-level.self"),
-      },
-      {
-        title: t("settings.sensitive-data.semantic-types.self"),
-      }
-    );
+    columnList.splice(1, 0, {
+      title: t("settings.sensitive-data.masking-level.self"),
+    });
   }
   if (showClassificationColumn.value) {
     columnList.splice(showSensitiveColumn.value ? 2 : 1, 0, {
