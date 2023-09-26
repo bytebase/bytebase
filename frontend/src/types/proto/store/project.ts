61d1b0182a9a1c745cf116ab9bbb87d82977ea60
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-09-27 00:00:52 +0800

    chore: branch protection (#8479)
    Former-commit-id: 5083802b75ed6c9bef6132744cfad8a7bb12bc74

--- frontend/src/types/proto/store/project.ts
+++ frontend/src/types/proto/store/project.ts
@@ -13,7 +13,10 @@ export interface ProtectionRule {
   target: ProtectionRule_Target;
   /** The name of the branch/changelist or wildcard. */
   nameFilter: string;
-  /** The roles allowed to create branches or changelists. */
+  /**
+   * The roles allowed to create branches or changelists.
+   * Format: roles/OWNER.
+   */
   createAllowedRoles: string[];
 }
 
