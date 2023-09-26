3de09894fde03d31eb4e7eb0d94fd6c9897d98ba
zll600 <3400692417@qq.com>
2023-09-26 10:17:20 +0800

    feat: impl schema design for tidb

--- backend/api/v1/schema_design_transformer_test.go
+++ backend/api/v1/schema_design_transformer_test.go
@@ -96,6 +96,42 @@ func TestTransformSchemaString(t *testing.T) {
 	}
 }
 
+func TestTiDBTransformSchemaString(t *testing.T) {
+	const (
+		record = false
+	)
+	var (
+		filepath = "testdata/tidb/schema.yaml"
+	)
+
+	a := require.New(t)
+	yamlFile, err := os.Open(filepath)
+	a.NoError(err)
+
+	tests := []transformTest{}
+	byteValue, err := io.ReadAll(yamlFile)
+	a.NoError(yamlFile.Close())
+	a.NoError(err)
+	a.NoError(yaml.Unmarshal(byteValue, &tests))
+
+	for i, t := range tests {
+		result, err := transformSchemaStringToDatabaseMetadata(t.Engine, t.Schema)
+		a.NoError(err)
+		if record {
+			tests[i].Metadata = result
+		} else {
+			a.Equal(t.Metadata, result)
+		}
+	}
+
+	if record {
+		byteValue, err := yaml.Marshal(tests)
+		a.NoError(err)
+		err = os.WriteFile(filepath, byteValue, 0644)
+		a.NoError(err)
+	}
+}
+
 type designTest struct {
 	Engine   v1pb.Engine
 	Baseline string
@@ -139,6 +175,42 @@ func TestGetDesignSchema(t *testing.T) {
 	}
 }
 
+func TestTiDBGetDesignSchema(t *testing.T) {
+	const (
+		record = false
+	)
+	var (
+		filepath = "testdata/tidb/design.yaml"
+	)
+
+	a := require.New(t)
+	yamlFile, err := os.Open(filepath)
+	a.NoError(err)
+
+	tests := []designTest{}
+	byteValue, err := io.ReadAll(yamlFile)
+	a.NoError(yamlFile.Close())
+	a.NoError(err)
+	a.NoError(yaml.Unmarshal(byteValue, &tests))
+
+	for i, t := range tests {
+		result, err := getDesignSchema(t.Engine, t.Baseline, t.Target)
+		a.NoError(err)
+		if record {
+			tests[i].Result = result
+		} else {
+			a.Equal(t.Result, result)
+		}
+	}
+
+	if record {
+		byteValue, err := yaml.Marshal(tests)
+		a.NoError(err)
+		err = os.WriteFile(filepath, byteValue, 0644)
+		a.NoError(err)
+	}
+}
+
 type checkTest struct {
 	Engine   v1pb.Engine
 	Metadata *v1pb.DatabaseMetadata
@@ -151,6 +223,49 @@ func TestCheckDatabaseMetadata(t *testing.T) {
 	)
 	var (
 		filepath = "testdata/check.yaml"
+	)
+
+	a := require.New(t)
+	yamlFile, err := os.Open(filepath)
+	a.NoError(err)
+
+	tests := []checkTest{}
+	byteValue, err := io.ReadAll(yamlFile)
+	a.NoError(yamlFile.Close())
+	a.NoError(err)
+	a.NoError(yaml.Unmarshal(byteValue, &tests))
+
+	for i, t := range tests {
+		err := checkDatabaseMetadata(t.Engine, t.Metadata)
+		if record {
+			if err != nil {
+				tests[i].Err = err.Error()
+			} else {
+				tests[i].Err = ""
+			}
+		} else {
+			if t.Err == "" {
+				a.NoError(err)
+			} else {
+				a.Equal(t.Err, err.Error())
+			}
+		}
+	}
+
+	if record {
+		byteValue, err := yaml.Marshal(tests)
+		a.NoError(err)
+		err = os.WriteFile(filepath, byteValue, 0644)
+		a.NoError(err)
+	}
+}
+
+func TestTiDBCheckDatabaseMetadata(t *testing.T) {
+	const (
+		record = false
+	)
+	var (
+		filepath = "testdata/tidb/check.yaml"
 	)
 
 	a := require.New(t)
