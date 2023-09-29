de996f148c5f77db58d6ccae16834ba256ead541
d-bytebase <d@bytebase.com>
2023-09-29 16:38:03 +0800

    refactor: move validator
    Former-commit-id: 939b409c4c8940200f162931db5249e66170fb79
    Former-commit-id: 83f15dad769adc37a2f9d3941b25ac7714a3475c

--- backend/plugin/parser/sql/validate_sql_for_editor.go
+++ backend/plugin/parser/sql/validate_sql_for_editor.go
@@ -1,13 +1,9 @@
 package parser
 
 import (
-	"log/slog"
-	"regexp"
-	"strings"
-
-	"github.com/bytebase/bytebase/backend/common/log"
+	mysqlparser "github.com/bytebase/bytebase/backend/plugin/parser/mysql"
 	pgparser "github.com/bytebase/bytebase/backend/plugin/parser/pg"
-	"github.com/bytebase/bytebase/backend/plugin/parser/tokenizer"
+	standardparser "github.com/bytebase/bytebase/backend/plugin/parser/standard"
 )
 
 // ValidateSQLForEditor validates the SQL statement for editor.
@@ -21,69 +17,8 @@ func ValidateSQLForEditor(engine EngineType, statement string) bool {
 	case Postgres, Redshift, RisingWave:
 		return pgparser.ValidateSQLForEditor(statement)
 	case MySQL, TiDB, MariaDB, OceanBase:
-		return mysqlValidateSQLForEditor(statement)
+		return mysqlparser.ValidateSQLForEditor(statement)
 	default:
-		return standardValidateSQLForEditor(statement)
+		return standardparser.ValidateSQLForEditor(statement)
 	}
-}
-
-// standardValidateSQLForEditor validates the SQL statement for SQL editor.
-// We validate the statement by following steps:
-// 1. Remove all quoted text(quoted identifier, string literal) and comments from the statement.
-// 2. Use regexp to check if the statement is a normal SELECT statement and EXPLAIN statement.
-// 3. For CTE, use regexp to check if the statement has UPDATE, DELETE and INSERT statements.
-func standardValidateSQLForEditor(statement string) bool {
-	textWithoutQuotedAndComment, err := tokenizer.StandardRemoveQuotedTextAndComment(statement)
-	if err != nil {
-		slog.Debug("Failed to remove quoted text and comment", slog.String("statement", statement), log.BBError(err))
-		return false
-	}
-
-	return checkStatementWithoutQuotedTextAndComment(textWithoutQuotedAndComment)
-}
-
-// mysqlValidateSQLForEditor validates the SQL statement for SQL editor.
-// We validate the statement by following steps:
-// 1. Remove all quoted text(quoted identifier, string literal) and comments from the statement.
-// 2. Use regexp to check if the statement is a normal SELECT statement and EXPLAIN statement.
-// 3. For CTE, use regexp to check if the statement has UPDATE, DELETE and INSERT statements.
-func mysqlValidateSQLForEditor(statement string) bool {
-	textWithoutQuotedAndComment, err := tokenizer.MysqlRemoveQuotedTextAndComment(statement)
-	if err != nil {
-		slog.Debug("Failed to remove quoted text and comment", slog.String("statement", statement), log.BBError(err))
-		return false
-	}
-
-	return checkStatementWithoutQuotedTextAndComment(textWithoutQuotedAndComment)
-}
-
-func checkStatementWithoutQuotedTextAndComment(statement string) bool {
-	formattedStr := strings.ToUpper(strings.TrimSpace(statement))
-	if isSelect, _ := regexp.MatchString(`^SELECT\s+?`, formattedStr); isSelect {
-		return true
-	}
-
-	if isSelect, _ := regexp.MatchString(`^SELECT\*\s+?`, formattedStr); isSelect {
-		return true
-	}
-
-	if isExplain, _ := regexp.MatchString(`^EXPLAIN\s+?`, formattedStr); isExplain {
-		if isExplainAnalyze, _ := regexp.MatchString(`^EXPLAIN\s+ANALYZE\s+?`, formattedStr); isExplainAnalyze {
-			return false
-		}
-		return true
-	}
-
-	cteRegex := regexp.MustCompile(`^WITH\s+?`)
-	if matchResult := cteRegex.MatchString(formattedStr); matchResult {
-		dmlRegs := []string{`\bINSERT\b`, `\bUPDATE\b`, `\bDELETE\b`}
-		for _, reg := range dmlRegs {
-			if matchResult, _ := regexp.MatchString(reg, formattedStr); matchResult {
-				return false
-			}
-		}
-		return true
-	}
-
-	return false
 }
