e028686e91d9e254023f45bc678d3be5e8baf401
d-bytebase <d@bytebase.com>
2023-09-29 16:55:43 +0800

    refactor: move set line
    Former-commit-id: 3bb164ac87797d7b537679420048df43a3109836
    Former-commit-id: 9130b6c826320a214322a964f8224a71ac98a126

--- backend/plugin/parser/sql/engine/pg/convert_test.go
+++ backend/plugin/parser/sql/engine/pg/convert_test.go
@@ -3192,3 +3192,93 @@ func TestRenameSchema(t *testing.T) {
 
 	runTests(t, tests)
 }
+
+type setLineTestData struct {
+	statement          string
+	columnLineList     []int
+	constraintLineList []int
+}
+
+// TODO(d): fix this test with setLineForCreateTableStmt().
+func TestPGCreateTableSetLine(t *testing.T) {
+	tests := []setLineTestData{
+		{
+			statement: `
+			CREATE TABLE t(
+				a int, B int,
+				C int,
+				"D" int NOT NULL,
+				CONSTRAINT unique_a UNIQUE (a),
+				UNIQUE (b, c),
+				PRIMARY KEY (d),CHECK (a > 0),
+
+				-- it's a comment.
+				FOREIGN KEY (a, b, c) REFERENCES t1(a, b, c)
+				
+
+
+				
+			)
+			`,
+			columnLineList:     []int{3, 3, 4, 5},
+			constraintLineList: []int{6, 7, 8, 8, 11},
+		},
+		{
+			// test for Windows.
+			statement: "\r\n" +
+				"CREATE TABLE t(" + "\r\n" +
+				"a int, B int," + "\r\n" +
+				"C int," + "\r\n" +
+				`"D" int NOT NULL,` + "\r\n" +
+				"CONSTRAINT unique_a UNIQUE (a)," + "\r\n" +
+				"UNIQUE (b, c)," + "\r\n" +
+				"PRIMARY KEY (d),CHECK (a > 0)," + "\r\n" +
+				"\r\n" +
+				"FOREIGN KEY (a, b, c) REFERENCES t1(a, b, c)" + "\r\n" +
+				")",
+			columnLineList:     []int{3, 3, 4, 5},
+			constraintLineList: []int{6, 7, 8, 8, 10},
+		},
+		{
+			statement: `
+			CREATE TABLE t(
+				a int PRIMARY KEY,
+				b int CHECK(b>1), c int UNIQUE
+			)
+			`,
+			columnLineList:     []int{3, 4, 4},
+			constraintLineList: []int{},
+		},
+		{
+			statement: `-- complex example
+			CREATE TABLE t(
+				a int PRIMARY KEY,
+				name varchar(255) DEFAULT 'UNIQUE on (a, b, c)(',
+				UNIQUE(a, name),
+				UNIQUE(name)
+			)
+			`,
+			columnLineList:     []int{3, 4},
+			constraintLineList: []int{5, 6},
+		},
+	}
+
+	for _, test := range tests {
+		nodeList, err := parser.Parse(parser.Postgres, parser.ParseContext{}, test.statement)
+		require.NoError(t, err)
+		require.Len(t, nodeList, 1)
+		node, ok := nodeList[0].(*ast.CreateTableStmt)
+		require.True(t, ok)
+		require.Equal(t, len(test.columnLineList), len(node.ColumnList))
+		require.Equal(t, len(test.constraintLineList), len(node.ConstraintList))
+		for i, col := range node.ColumnList {
+			require.Equal(t, col.LastLine(), test.columnLineList[i], i)
+			for _, inlineCons := range col.ConstraintList {
+				require.Equal(t, test.columnLineList[i], inlineCons.LastLine())
+			}
+		}
+		for i, cons := range node.ConstraintList {
+			require.Equal(t, cons.LastLine(), test.constraintLineList[i], i)
+		}
+	}
+}
--- backend/plugin/parser/tidb/tidb.go
+++ backend/plugin/parser/tidb/tidb.go
@@ -1,8 +1,12 @@
 package tidb
 
 import (
+	"strings"
+
 	tidbparser "github.com/pingcap/tidb/parser"
 	"github.com/pingcap/tidb/parser/ast"
+
+	"github.com/bytebase/bytebase/backend/plugin/parser/tokenizer"
 )
 
 // ParseTiDB parses the given SQL statement and returns the AST.
@@ -15,4 +19,16 @@ func ParseTiDB(sql string, charset string, collation string) ([]ast.StmtNode, er
 
 	nodes, _, err := p.Parse(sql, charset, collation)
 	return nodes, err
+}
+
+// SetLineForMySQLCreateTableStmt sets the line for columns and table constraints in MySQL CREATE TABLE statments.
+// This is a temporary function. Because we do not convert tidb AST to our AST. So we have to implement this.
+// TODO(rebelice): remove it.
+func SetLineForMySQLCreateTableStmt(node *ast.CreateTableStmt) error {
+	// exclude CREATE TABLE ... AS and CREATE TABLE ... LIKE statement.
+	if len(node.Cols) == 0 {
+		return nil
+	}
+	firstLine := node.OriginTextPosition() - strings.Count(node.Text(), "\n")
+	return tokenizer.NewTokenizer(node.Text()).SetLineForMySQLCreateTableStmt(node, firstLine)
 }
