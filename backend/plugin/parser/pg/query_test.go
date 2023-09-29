6af83a7272b1b5d61740e4f22ff27db0f64c4070
d-bytebase <d@bytebase.com>
2023-09-29 16:31:39 +0800

    chore: move pg test
    Former-commit-id: 613489a3f6527bc19542908b556977f9e743175b
    Former-commit-id: a19eb0628143ce65de54a5b4be8dbe65bf347afd

---
+++ backend/plugin/parser/pg/query_test.go
@@ -0,0 +1,86 @@
+package pg
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type testData struct {
+	sql string
+	ans bool
+}
+
+func TestValidateSQLForEditor(t *testing.T) {
+	tests := []testData{
+		{
+			sql: `select* from t`,
+			ans: true,
+		},
+		{
+			sql: `explain select * from t;`,
+			ans: true,
+		},
+		{
+			sql: `explain    analyze select * from t`,
+			ans: false,
+		},
+		{
+			sql: `
+				With t as (
+					select * from t1
+				), tx as (
+					delete from t2
+				)
+				select * from t;
+				`,
+			ans: false,
+		},
+		{
+			sql: `
+				With t as (
+					select * from t1
+				), tx as (
+					select * from t1
+				)
+				update t set a = 1;
+				`,
+			ans: false,
+		},
+		{
+			sql: `
+				With t as (
+					select * from t1
+				), tx as (
+					select * from t1
+				)
+				insert into t values (1, 2, 3);
+				`,
+			ans: false,
+		},
+		{
+			sql: "select * from t where a = 'klasjdfkljsa$tag$; -- lkjdlkfajslkdfj'",
+			ans: true,
+		},
+		{
+			sql: `
+				With t as (
+					select * from t1 where a = 'insert'
+				), tx as (
+					select * from "delete"
+				) /* UPDATE */
+				select "update" from t;
+				`,
+			ans: true,
+		},
+		{
+			sql: `create table t (a int);`,
+			ans: false,
+		},
+	}
+
+	for _, test := range tests {
+		ans := ValidateSQLForEditor(test.sql)
+		require.Equal(t, test.ans, ans, test.sql)
+	}
+}
