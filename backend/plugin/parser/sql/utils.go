746e8113db4e01bcad026b54e0ec749d2962836c
d-bytebase <d@bytebase.com>
2023-09-29 22:02:20 +0800

    refactor: move validator

--- backend/plugin/parser/sql/utils.go
+++ backend/plugin/parser/sql/utils.go
@@ -169,3 +169,20 @@ func ExtractSensitiveField(dbType db.Type, statement string, currentDatabase str
 		return nil, nil
 	}
 }
+
+// ValidateSQLForEditor validates the SQL statement for editor.
+// We support the following SQLs:
+// 1. EXPLAIN statement, except EXPLAIN ANALYZE
+// 2. SELECT statement
+//
+// We also support CTE with SELECT statements, but not with DML statements.
+func ValidateSQLForEditor(engine EngineType, statement string) bool {
+	switch engine {
+	case Postgres, Redshift, RisingWave:
+		return pgparser.ValidateSQLForEditor(statement)
+	case MySQL, TiDB, MariaDB, OceanBase:
+		return mysqlparser.ValidateSQLForEditor(statement)
+	default:
+		return standardparser.ValidateSQLForEditor(statement)
+	}
+}
