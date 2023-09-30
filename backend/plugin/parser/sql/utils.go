1c890584437bd95d80c9969f62d862140d02822e
d-bytebase <d@bytebase.com>
2023-09-30 09:24:20 +0800

    refactor: unify get database list interface

--- backend/plugin/parser/sql/utils.go
+++ backend/plugin/parser/sql/utils.go
@@ -92,17 +92,17 @@ func SplitMultiSQLStream(engineType storepb.Engine, src io.Reader, f func(string
 }
 
 // ExtractDatabaseList extracts all databases from statement.
-func ExtractDatabaseList(engineType storepb.Engine, statement string, fallbackNormalizedDatabaseName string) ([]string, error) {
+func ExtractDatabaseList(engineType storepb.Engine, statement string, currentDatabase string) ([]string, error) {
 	switch engineType {
 	case storepb.Engine_MYSQL, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 		// TODO(d): use mysql parser.
-		return tidbparser.ExtractDatabaseList(statement)
+		return tidbparser.ExtractDatabaseList(statement, currentDatabase)
 	case storepb.Engine_TIDB:
-		return tidbparser.ExtractDatabaseList(statement)
+		return tidbparser.ExtractDatabaseList(statement, currentDatabase)
 	case storepb.Engine_SNOWFLAKE:
-		return snowparser.ExtractSnowSQLNormalizedDatabaseList(statement, fallbackNormalizedDatabaseName)
+		return snowparser.ExtractDatabaseList(statement, currentDatabase)
 	case storepb.Engine_MSSQL:
-		return tsqlparser.ExtractMSSQLNormalizedDatabaseList(statement, fallbackNormalizedDatabaseName)
+		return tsqlparser.ExtractDatabaseList(statement, currentDatabase)
 	default:
 		return nil, errors.Errorf("engine type is not supported: %s", engineType)
 	}
