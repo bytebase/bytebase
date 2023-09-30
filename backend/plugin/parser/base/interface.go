d268c6e1ddf644d9a1cee7b4b83483d0485e04d3
d-bytebase <d@bytebase.com>
2023-09-30 09:19:58 +0800

    refactor: move utils

--- backend/plugin/parser/base/interface.go
+++ backend/plugin/parser/base/interface.go
@@ -10,8 +10,8 @@ import (
 
 var (
 	mux             sync.Mutex
-	QueryValidators = make(map[storepb.Engine]ValidateSQLForEditorFunc)
-	FieldMaskers    = make(map[storepb.Engine]GetMaskedFieldsFunc)
+	queryValidators = make(map[storepb.Engine]ValidateSQLForEditorFunc)
+	fieldMaskers    = make(map[storepb.Engine]GetMaskedFieldsFunc)
 )
 
 type ValidateSQLForEditorFunc func(string) bool
@@ -21,17 +21,36 @@ type GetMaskedFieldsFunc func(string, string, *db.SensitiveSchemaInfo) ([]db.Sen
 func RegisterQueryValidator(engine storepb.Engine, f ValidateSQLForEditorFunc) {
 	mux.Lock()
 	defer mux.Unlock()
-	if _, dup := QueryValidators[engine]; dup {
+	if _, dup := queryValidators[engine]; dup {
 		panic(fmt.Sprintf("Register called twice %s", engine))
 	}
-	QueryValidators[engine] = f
+	queryValidators[engine] = f
 }
 
 func RegisterGetMaskedFieldsFunc(engine storepb.Engine, f GetMaskedFieldsFunc) {
 	mux.Lock()
 	defer mux.Unlock()
-	if _, dup := FieldMaskers[engine]; dup {
+	if _, dup := fieldMaskers[engine]; dup {
 		panic(fmt.Sprintf("Register called twice %s", engine))
 	}
-	FieldMaskers[engine] = f
+	fieldMaskers[engine] = f
+}
+
+func ExtractSensitiveField(engine storepb.Engine, statement string, currentDatabase string, schemaInfo *db.SensitiveSchemaInfo) ([]db.SensitiveField, error) {
+	if schemaInfo == nil {
+		return nil, nil
+	}
+
+	f := fieldMaskers[engine]
+	return f(statement, currentDatabase, schemaInfo)
+}
+
+// ValidateSQLForEditor validates the SQL statement for editor.
+// We support the following SQLs:
+// 1. EXPLAIN statement, except EXPLAIN ANALYZE
+// 2. SELECT statement
+// We also support CTE with SELECT statements, but not with DML statements.
+func ValidateSQLForEditor(engine storepb.Engine, statement string) bool {
+	f := queryValidators[engine]
+	return f(statement)
 }
