c082934e763a716f8c718c9e0427fba29ab2358e
d-bytebase <d@bytebase.com>
2023-09-30 09:30:52 +0800

    refactor: add register
    Former-commit-id: 2185726bcda82c17e2fb192effe1e0dc2fcc1e58
    Former-commit-id: 883b5f89bb519275520573262e8659ff46f84011

--- backend/plugin/parser/base/interface.go
+++ backend/plugin/parser/base/interface.go
@@ -9,14 +9,17 @@ import (
 )
 
 var (
-	mux             sync.Mutex
-	queryValidators = make(map[storepb.Engine]ValidateSQLForEditorFunc)
-	fieldMaskers    = make(map[storepb.Engine]GetMaskedFieldsFunc)
+	mux                    sync.Mutex
+	queryValidators        = make(map[storepb.Engine]ValidateSQLForEditorFunc)
+	fieldMaskers           = make(map[storepb.Engine]GetMaskedFieldsFunc)
+	changedResourcesGetter = make(map[storepb.Engine]ExtractChangedResourcesFunc)
+	resourcesGetter        = make(map[storepb.Engine]ExtractResourceListFunc)
 )
 
 type ValidateSQLForEditorFunc func(string) bool
-
 type GetMaskedFieldsFunc func(string, string, *db.SensitiveSchemaInfo) ([]db.SensitiveField, error)
+type ExtractChangedResourcesFunc func(string, string, string) ([]SchemaResource, error)
+type ExtractResourceListFunc func(string, string, string) ([]SchemaResource, error)
 
 func RegisterQueryValidator(engine storepb.Engine, f ValidateSQLForEditorFunc) {
 	mux.Lock()
@@ -34,6 +37,24 @@ func RegisterGetMaskedFieldsFunc(engine storepb.Engine, f GetMaskedFieldsFunc) {
 		panic(fmt.Sprintf("Register called twice %s", engine))
 	}
 	fieldMaskers[engine] = f
+}
+
+func RegisterExtractChangedResourcesFunc(engine storepb.Engine, f ExtractChangedResourcesFunc) {
+	mux.Lock()
+	defer mux.Unlock()
+	if _, dup := changedResourcesGetter[engine]; dup {
+		panic(fmt.Sprintf("Register called twice %s", engine))
+	}
+	changedResourcesGetter[engine] = f
+}
+
+func RegisterExtractResourceListFunc(engine storepb.Engine, f ExtractResourceListFunc) {
+	mux.Lock()
+	defer mux.Unlock()
+	if _, dup := resourcesGetter[engine]; dup {
+		panic(fmt.Sprintf("Register called twice %s", engine))
+	}
+	resourcesGetter[engine] = f
 }
 
 func ExtractSensitiveField(engine storepb.Engine, statement string, currentDatabase string, schemaInfo *db.SensitiveSchemaInfo) ([]db.SensitiveField, error) {
