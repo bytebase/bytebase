1dc36822507678e08faa9edacebc14c1426bcd1f
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-10-07 11:10:26 +0800

    feat: export selected changes

--- frontend/src/views/Changelist/ChangelistDetail/NavBar/ExportButton.vue
+++ frontend/src/views/Changelist/ChangelistDetail/NavBar/ExportButton.vue
@@ -3,7 +3,11 @@
     icon
     style="--n-padding: 0 10px"
     :errors="errors"
-    :disabled="!allowEdit"
+    :button-props="{
+      loading: isExporting,
+    }"
+    :disabled="!allowEdit || isExporting"
+    @click="handleExport"
   >
     <template #icon>
       <heroicons:arrow-down-tray />
@@ -12,13 +16,19 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import dayjs from "dayjs";
+import saveAs from "file-saver";
+import JSZip from "jszip";
+import { computed, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { ErrorTipsButton } from "@/components/v2";
+import { escapeFilename } from "@/utils";
 import { useChangelistDetailContext } from "../context";
+import { zipFileForChange } from "./export";
 
 const { t } = useI18n();
-const { allowEdit, selectedChanges } = useChangelistDetailContext();
+const { allowEdit, changelist, selectedChanges } = useChangelistDetailContext();
+const isExporting = ref(false);
 
 const errors = computed(() => {
   const errors: string[] = [];
@@ -27,4 +37,30 @@ const errors = computed(() => {
   }
   return errors;
 });
+
+const handleExport = async () => {
+  if (isExporting.value) {
+    return;
+  }
+
+  isExporting.value = true;
+  const zip = new JSZip();
+  const changes = selectedChanges.value;
+  for (let i = 0; i < changes.length; i++) {
+    await zipFileForChange(zip, changes[i], i);
+  }
+
+  try {
+    const content = await zip.generateAsync({ type: "blob" });
+    const basename = `${changelist.value.description}_${dayjs().format(
+      "YYYYMMDD"
+    )}`;
+    const fileName = `${escapeFilename(basename)}.zip`;
+    saveAs(content, fileName);
+  } catch (error) {
+    console.error(error);
+  }
+
+  isExporting.value = false;
+};
 </script>
