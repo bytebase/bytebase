a008a1448933a1c63b9b05ca400c2ada85012436
Tianzhou (天舟) <t@bytebase.com>
2023-10-09 00:00:35 +0800

    fix: custom style is not applied to FeatureBadge (#8582)
    Former-commit-id: 32c7564336aa1996f037d5edbffb89cd34578ca5
    Former-commit-id: f1e4bab77d75ca0a6d2a4c92a063b99250b5e1b0

--- frontend/src/views/SettingWorkspaceSSO.vue
+++ frontend/src/views/SettingWorkspaceSSO.vue
@@ -15,6 +15,7 @@
       <div>
         <button
           v-if="identityProviderList.length > 0"
+          type="button"
           class="btn-primary"
           @click="handleCreateSSO"
         >
@@ -33,13 +34,12 @@
         <div class="text-center flex flex-col justify-center items-center">
           <img src="@/assets/illustration/no-data.webp" class="w-52" />
           <div class="mt-6">
-            <button
-              type="button"
-              class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              @click="handleCreateSSO"
-            >
+            <button type="button" class="btn-primary" @click="handleCreateSSO">
               {{ $t("settings.sso.create") }}
-              <FeatureBadge :feature="'bb.feature.sso'" custom-class="ml-2" />
+              <FeatureBadge
+                :feature="'bb.feature.sso'"
+                custom-class="ml-2 !text-white"
+              />
             </button>
           </div>
         </div>
