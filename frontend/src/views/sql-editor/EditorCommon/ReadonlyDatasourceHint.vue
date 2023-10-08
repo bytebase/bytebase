7e1e305dc8c0f4bd64e9e02c717ef3a9ba1d8b38
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-10-08 18:53:34 +0800

    chore(sql-editor): adjust connection path bar layout and style (#8577)
    * chore: adjust connection path bar layout and style
    
    * chore: fix lint
    
    * chore: don't jump out in standalone mode
    
    * chore: update
    
    ---------
    
    Co-authored-by: d-bytebase <d@bytebase.com>
    Former-commit-id: 8ad711d28d7141a7aac1a826cfc1e0ecaaab4336

---
+++ frontend/src/views/sql-editor/EditorCommon/ReadonlyDatasourceHint.vue
@@ -0,0 +1,80 @@
+<template>
+  <NPopover v-if="showReadonlyDatasourceHint" trigger="hover">
+    <template #trigger>
+      <heroicons-outline:information-circle
+        class="h-5 w-5 flex-shrink-0 mr-2 text-info"
+      />
+    </template>
+    <p class="py-1">
+      <template v-if="allowManageInstance">
+        {{ $t("instance.no-read-only-data-source-warn-for-owner-dba") }}
+        <span
+          v-if="mode === 'BUNDLED'"
+          class="underline text-accent cursor-pointer hover:opacity-80"
+          @click="gotoInstanceDetailPage"
+        >
+          {{ $t("sql-editor.create-read-only-data-source") }}
+        </span>
+      </template>
+      <template v-else>
+        {{ $t("instance.no-read-only-data-source-warn-for-developer") }}
+      </template>
+    </p>
+  </NPopover>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+import { useCurrentUserV1, useSQLEditorStore, useTabStore } from "@/store";
+import { ComposedInstance, TabMode, UNKNOWN_ID } from "@/types";
+import { DataSourceType } from "@/types/proto/v1/instance_service";
+import { hasWorkspacePermissionV1, instanceV1Slug } from "@/utils";
+
+const props = defineProps<{
+  instance: ComposedInstance;
+}>();
+
+const router = useRouter();
+const { mode } = storeToRefs(useSQLEditorStore());
+const tabStore = useTabStore();
+const me = useCurrentUserV1();
+
+const isAdminMode = computed(() => {
+  return tabStore.currentTab.mode === TabMode.Admin;
+});
+
+const allowManageInstance = computed(() => {
+  return hasWorkspacePermissionV1(
+    "bb.permission.workspace.manage-instance",
+    me.value.userRole
+  );
+});
+
+const hasReadonlyDataSource = computed(() => {
+  return (
+    props.instance.dataSources.findIndex(
+      (ds) => ds.type === DataSourceType.READ_ONLY
+    ) !== -1
+  );
+});
+
+const showReadonlyDatasourceHint = computed(() => {
+  return (
+    !isAdminMode.value &&
+    props.instance.uid !== String(UNKNOWN_ID) &&
+    !hasReadonlyDataSource.value
+  );
+});
+
+const gotoInstanceDetailPage = () => {
+  const route = router.resolve({
+    name: "workspace.instance.detail",
+    params: {
+      instanceSlug: instanceV1Slug(props.instance),
+    },
+  });
+  window.open(route.href);
+};
+</script>
