a7922083bfb2857eb37b275c674fcb8e79adc7da
ecmadao <wlec@outlook.com>
2023-10-08 12:40:22 +0800

    chore: support select multiply databases

--- frontend/src/utils/issue/index.ts
+++ frontend/src/utils/issue/index.ts
@@ -1,7 +1,8 @@
+import dayjs from "dayjs";
 import { Issue, IssueType, StageId } from "@/types";
 
 export function stageName(issue: Issue, stageId: StageId): string {
-  for (const stage of issue.pipeline.stageList) {
+  for (const stage of issue.pipeline?.stageList ?? []) {
     if (stage.id == stageId) {
       return stage.name;
     }
@@ -25,3 +26,29 @@ export function isDatabaseRelatedIssueType(issueType: IssueType): boolean {
     "bb.issue.database.restore.pitr",
   ].includes(issueType);
 }
+
+export const generateIssueName = (
+  type: "bb.issue.database.schema.update" | "bb.issue.database.data.update",
+  databaseNameList: string[],
+  isOnlineMode: boolean
+) => {
+  // Create a user friendly default issue name
+  const issueNameParts: string[] = [];
+  if (databaseNameList.length === 1) {
+    issueNameParts.push(`[${databaseNameList[0]}]`);
+  } else {
+    issueNameParts.push(`[${databaseNameList.length} databases]`);
+  }
+  if (isOnlineMode) {
+    issueNameParts.push("Online schema change");
+  } else {
+    issueNameParts.push(
+      type === "bb.issue.database.schema.update" ? `Edit schema` : `Change data`
+    );
+  }
+  const datetime = dayjs().format("@MM-DD HH:mm");
+  const tz = "UTC" + dayjs().format("ZZ");
+  issueNameParts.push(`${datetime} ${tz}`);
+
+  return issueNameParts.join(" ");
+};
