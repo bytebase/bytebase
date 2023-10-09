39a0f1a44a2d35255a784f4a01db9965b0d29ad9
d-bytebase <d@bytebase.com>
2023-10-10 01:02:08 +0800

    chore: update

--- backend/migrator/migration/prod/2.9/0004##label_prefix.sql
+++ backend/migrator/migration/prod/2.9/0004##label_prefix.sql
@@ -1,11 +1,9 @@
 UPDATE db SET
     metadata = metadata || jsonb_build_object('labels', (metadata->'labels')::JSONB - 'bb.environment')
-WHERE metadata ? 'labels';
+WHERE metadata ? 'labels' AND (metadata->'labels')::JSONB ? 'bb.environment';
 
 UPDATE db SET
     metadata = metadata || jsonb_build_object('labels', REPLACE((metadata->'labels')::TEXT, 'bb.', '')::JSONB)
 WHERE metadata ? 'labels';
-
-select (metadata->'labels')::JSONB - 'tenant1' from db WHERE (metadata->'labels')::JSONB ? 'tenant11';
 
 UPDATE deployment_config SET config = REPLACE(config::TEXT, 'bb.', '')::JSONB;
