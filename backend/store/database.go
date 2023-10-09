7861cdff9f26121c7df5cad3dbf5c651fe95a760
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-10-09 02:48:06 -0500

    chore: don't return environment label (#8600)
    Former-commit-id: 32a0be65fa1beb25cb152ad9a11ea86e1dcbe40e
    Former-commit-id: 4091c46261681f77f6ce414e982674a63f49a03e

--- backend/store/database.go
+++ backend/store/database.go
@@ -34,18 +34,6 @@ type DatabaseMessage struct {
 	Metadata    *storepb.DatabaseMetadata
 }
 
-// GetEffectiveLabels gets the effective labels for a database.
-func (m *DatabaseMessage) GetEffectiveLabels() map[string]string {
-	ret := make(map[string]string)
-	for k, v := range m.Metadata.Labels {
-		ret[k] = v
-	}
-	// System default environment label.
-	// The value of bb.environment is resource ID of the environment.
-	ret[api.EnvironmentLabelKey] = m.EffectiveEnvironmentID
-	return ret
-}
-
 // UpdateDatabaseMessage is the mssage for updating a database.
 type UpdateDatabaseMessage struct {
 	InstanceID   string
