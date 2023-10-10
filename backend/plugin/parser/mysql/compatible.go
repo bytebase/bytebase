008d4caf91376e37b8a5bef7dc02b21d70691a5e
d-bytebase <d@bytebase.com>
2023-10-10 11:52:10 +0800

    chore: update

--- backend/plugin/parser/mysql/compatible.go
+++ backend/plugin/parser/mysql/compatible.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"regexp"
+	"strconv"
 
+	tidbparser "github.com/pingcap/tidb/parser"
+	"github.com/pingcap/tidb/parser/ast"
 	"github.com/pkg/errors"
 
-	tidbparser "github.com/bytebase/bytebase/backend/plugin/parser/tidb"
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
 )
 
 // extractTiDBUnsupportedStmts returns a list of unsupported statements in TiDB extracted from the `stmts`,
@@ -34,10 +37,53 @@ func extractTiDBUnsupportedStmts(stmts string) ([]string, string, error) {
 
 // isTiDBUnsupportStmt returns true if this statement is unsupported in TiDB.
 func isTiDBUnsupportStmt(stmt string) bool {
-	if _, err := tidbparser.ParseTiDB(stmt, "", ""); err != nil {
+	if _, err := ParseTiDB(stmt, "", ""); err != nil {
 		return true
 	}
 	return false
+}
+
+// ParseTiDB parses the given SQL statement and returns the AST.
+func ParseTiDB(sql string, charset string, collation string) ([]ast.StmtNode, error) {
+	p := tidbparser.New()
+
+	// To support MySQL8 window function syntax.
+	// See https://github.com/bytebase/bytebase/issues/175.
+	p.EnableWindowFunc(true)
+
+	nodes, _, err := p.Parse(sql, charset, collation)
+	if err != nil {
+		return nil, convertParserError(err)
+	}
+	return nodes, nil
+}
+
+var (
+	lineColumnRegex = regexp.MustCompile(`line (\d+) column (\d+)`)
+)
+
+func convertParserError(parserErr error) error {
+	// line 1 column 15 near "TO world;"
+	res := lineColumnRegex.FindAllStringSubmatch(parserErr.Error(), -1)
+	if len(res) != 1 {
+		return parserErr
+	}
+	if len(res[0]) != 3 {
+		return parserErr
+	}
+	line, err := strconv.Atoi(res[0][1])
+	if err != nil {
+		return parserErr
+	}
+	column, err := strconv.Atoi(res[0][2])
+	if err != nil {
+		return parserErr
+	}
+	return &base.SyntaxError{
+		Line:    line,
+		Column:  column,
+		Message: parserErr.Error(),
+	}
 }
 
 // IsTiDBUnsupportDDLStmt checks whether the `stmt` is unsupported DDL statement in TiDB, the following statements are unsupported:
