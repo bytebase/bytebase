c7d14afcd40802176759dc0d6f359221067f1cdb
Tianzhou (天舟) <t@bytebase.com>
2023-10-10 16:54:05 +0800

    chore: changelist ux tweak (#8634)

--- frontend/src/views/Changelist/ChangelistDetail/ChangeTable/Source.vue
+++ frontend/src/views/Changelist/ChangelistDetail/ChangeTable/Source.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
     <template v-if="type === 'CHANGE_HISTORY'">
-      {{ $t("common.change-history") }}
+      <div class="flex items-center">
+        <History :size="16" class="mr-1" />
+        {{ $t("common.change-history") }}
+      </div>
     </template>
     <template v-if="type === 'BRANCH'">
-      {{ $t("common.branch") }}
+      <div class="flex items-center">
+        <GitBranch :size="16" class="mr-1" />{{ $t("common.branch") }}
+      </div>
     </template>
     <template v-if="type === 'RAW_SQL'">
-      {{ $t("changelist.change-source.raw-sql") }}
+      <div class="flex items-center">
+        <File :size="16" class="mr-1" />
+        {{ $t("changelist.change-source.raw-sql") }}
+      </div>
     </template>
   </div>
 </template>
 
 <script setup lang="ts">
+import { File, GitBranch, History } from "lucide-vue-next";
 import { computed } from "vue";
 import { Changelist_Change as Change } from "@/types/proto/v1/changelist_service";
 import { getChangelistChangeSourceType } from "@/utils";
