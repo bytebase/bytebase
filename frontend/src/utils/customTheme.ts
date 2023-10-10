96e7a8f97ce48114e9b31a56d3eb2d63d990ea27
steven <stevenlgtm@gmail.com>
2023-10-10 13:28:53 +0800

    chore: update custom theme color

--- frontend/src/utils/customTheme.ts
+++ frontend/src/utils/customTheme.ts
@@ -4,8 +4,9 @@ import { useI18n } from "vue-i18n";
 export const applyCustomTheme = (theme: string) => {
   const rootElement = document.documentElement;
   if (theme === "lixiang") {
-    // TODO: update the color variables.
-    rootElement.style.setProperty("--color-accent", "#52ab7c");
+    rootElement.style.setProperty("--color-accent", "#00665f");
+    rootElement.style.setProperty("--color-accent-disabled", "#ecf3f3");
+    rootElement.style.setProperty("--color-accent-hover", "#00554f");
   }
 };
 
