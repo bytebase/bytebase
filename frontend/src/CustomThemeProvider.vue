b2e1136309f3d5e3b6965b8751f2449699ee3cb4
steven <stevenlgtm@gmail.com>
2023-10-10 13:35:44 +0800

    chore: update

--- frontend/src/CustomThemeProvider.vue
+++ frontend/src/CustomThemeProvider.vue
@@ -16,13 +16,16 @@ import { NConfigProvider } from "naive-ui";
 import { onMounted, ref } from "vue";
 import { themeOverrides, dateLang, generalLang } from "../naive-ui.config";
 import { provideAppRootContext } from "./AppRootContext";
+import { useLanguage } from "./composables/useLanguage";
 import { applyCustomTheme } from "./utils/customTheme";
 
 const { key } = provideAppRootContext();
+const { setLocale } = useLanguage();
 const initialized = ref(false);
 
 onMounted(() => {
   const searchParams = new URLSearchParams(window.location.search);
+  // Initial custom theme.
   let customTheme = searchParams.get("customTheme") || "";
   const cachedCustomTheme = useLocalStorage<string>("bb.custom-theme", "");
   if (!customTheme && cachedCustomTheme.value) {
@@ -31,6 +34,11 @@ onMounted(() => {
   if (customTheme) {
     cachedCustomTheme.value = customTheme;
     applyCustomTheme(customTheme);
+  }
+  // Initial custom language.
+  const lang = searchParams.get("lang") || "";
+  if (lang) {
+    setLocale(lang);
   }
   initialized.value = true;
 });
