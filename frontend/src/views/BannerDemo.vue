35352c1da8dfb08fe1e3343b7f625cbea3fd3bb7
Tianzhou Chen <t@bytebase.com>
2023-10-13 00:39:41 +0800

    fix: re-enable demo banner for demo.bytebase.com
    Demo name changed to “next” and was hidden before

--- frontend/src/views/BannerDemo.vue
+++ frontend/src/views/BannerDemo.vue
@@ -1,26 +1,26 @@
 <template>
   <div v-if="show" class="bg-accent">
-    <div class="mx-auto py-3 px-3">
+    <div class="mx-auto py-1 px-3">
       <div class="flex items-center justify-between flex-wrap">
         <div class="w-0 flex-1 flex items-center">
-          <p class="ml-3 font-medium text-white truncate">
+          <p class="flex font-medium text-white items-center truncate">
+            📆
             <a
               href="https://cal.com/adela-bytebase/30min"
               target="_blank"
-              class="flex underline"
+              class="flex underline ml-1"
             >
-              <heroicons-outline:calendar class="mr-1 w-6 h-6" />
               {{ $t("banner.request-demo") }}
             </a>
           </p>
         </div>
         <div
-          class="flex flex-row space-x-4 order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto"
+          class="flex flex-row space-x-4 order-3 my-2 flex-shrink-0 w-full sm:order-2 sm:py-1 sm:my-0 sm:w-auto"
         >
           <a
             href="https://bytebase.com/docs/get-started/install/deploy-with-docker?source=demo"
             target="_blank"
-            class="flex items-center justify-center pl-4 pr-2 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-accent bg-white hover:bg-indigo-50"
+            class="flex items-center justify-center pl-4 pr-2 py-1 border border-transparent rounded-sm text-base font-medium text-accent bg-white hover:bg-indigo-50"
           >
             {{ $t("banner.deploy") }}
             <heroicons-outline:arrow-down-tray class="ml-1 w-6 h-6" />
@@ -28,7 +28,7 @@
           <a
             href="https://hub.bytebase.com/workspace?source=demo"
             target="_blank"
-            class="flex items-center justify-center pl-4 pr-2 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-accent bg-white hover:bg-indigo-50"
+            class="flex items-center justify-center pl-4 pr-2 py-1 border border-transparent rounded-sm text-base font-medium text-accent bg-white hover:bg-indigo-50"
           >
             {{ $t("banner.cloud") }}
             <heroicons-outline:cloud class="ml-1 w-6 h-6" />
