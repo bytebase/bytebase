6620cb282ff73a0c2d51b669e0525a05f0ce5aa1
zll600 <3400692417@qq.com>
2023-10-12 16:53:40 +0800

    feat: use parser to set query limit for mysql

--- backend/plugin/db/mysql/query_test.go
+++ backend/plugin/db/mysql/query_test.go
@@ -36,3 +36,43 @@ func TestGetStatementWithResultLimitOfTiDB(t *testing.T) {
 		require.Equal(t, tc.want, got, tc.stmt)
 	}
 }
+
+func TestGetStatementWithResultLimitOfMySQL(t *testing.T) {
+	testCases := []struct {
+		stmt  string
+		count int
+		want  string
+	}{
+		{
+			stmt:  "SELECT * FROM t;",
+			count: 10,
+			want:  "SELECT * FROM t LIMIT 10;",
+		},
+		{
+			stmt:  "SELECT * FROM t LIMIT 5;",
+			count: 10,
+			want:  "SELECT * FROM t LIMIT 5;",
+		},
+		{
+			stmt:  "SELECT * FROM t2 JOIN t1 ON t2.c2 = t1.c2 where t2.c2 > 10;",
+			count: 10,
+			want:  "SELECT * FROM t2 JOIN t1 ON t2.c2 = t1.c2 where t2.c2 > 10 LIMIT 10;",
+		},
+		{
+			stmt:  "SELECT * FROM t1 ORDER BY c2;",
+			count: 10,
+			want:  "SELECT * FROM t1 ORDER BY c2 LIMIT 10;",
+		},
+		{
+			stmt:  "SELECT COUNT(col1) AS col2 FROM t GROUP BY col2 HAVING col2 = 2;",
+			count: 10,
+			want:  "SELECT COUNT(col1) AS col2 FROM t GROUP BY col2 HAVING col2 = 2 LIMIT 10;",
+		},
+	}
+
+	for _, tc := range testCases {
+		got, err := getStatementWithResultLimitForMySQL(tc.stmt, tc.count)
+		require.NoError(t, err, tc.stmt)
+		require.Equal(t, tc.want, got, tc.stmt)
+	}
+}
