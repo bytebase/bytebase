21b7244c801b7393c64a4f28cd5af1c2419d5a79
zll600 <71714656+zll600@users.noreply.github.com>
2023-10-13 12:15:57 +0800

    feat: use parser to set query limit for mysql (#8695)
    * feat: use parser to set query limit for mysql
    
    * Update backend/plugin/db/mysql/query.go
    
    Co-authored-by: h3n4l <oysterdays@gmail.com>
    
    * fix: only process outer most queryExpression and add more test cases
    
    * Update backend/plugin/db/mysql/query.go
    
    Co-authored-by: rebelice <yangrebelice@gmail.com>
    
    * fix: handle procedureAnalyse
    
    ---------
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    Co-authored-by: h3n4l <oysterdays@gmail.com>
    Co-authored-by: rebelice <yangrebelice@gmail.com>
    Former-commit-id: 4bde90e72a49dae18593947037915f54ee539586

--- backend/plugin/db/mysql/query.go
+++ backend/plugin/db/mysql/query.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/antlr4-go/antlr/v4"
 	"github.com/pkg/errors"
 
 	tidbast "github.com/pingcap/tidb/parser/ast"
 	"github.com/pingcap/tidb/parser/format"
 
+	mysql "github.com/bytebase/mysql-parser"
+
+	mysqlparser "github.com/bytebase/bytebase/backend/plugin/parser/mysql"
 	tidbparser "github.com/bytebase/bytebase/backend/plugin/parser/tidb"
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 )
@@ -19,7 +23,7 @@ func (driver *Driver) getStatementWithResultLimit(stmt string, limit int) (strin
 		return getStatementWithResultLimitForTiDB(stmt, limit)
 	default:
 		// MySQL 5.7 doesn't support WITH clause.
-		return fmt.Sprintf("SELECT * FROM (%s) result LIMIT %d;", stmt, limit), nil
+		return getStatementWithResultLimitForMySQL(stmt, limit)
 	}
 }
 
@@ -54,4 +58,57 @@ func getStatementWithResultLimitForTiDB(singleStatement string, limitCount int) 
 		}
 	}
 	return "", nil
+}
+
+// singleStatement must be a selectStatement for mysql.
+func getStatementWithResultLimitForMySQL(singleStatement string, limitCount int) (string, error) {
+	list, err := mysqlparser.ParseMySQL(singleStatement)
+	if err != nil {
+		return "", err
+	}
+
+	listener := &mysqlRewriter{
+		limitCount:     limitCount,
+		outerMostQuery: true,
+	}
+
+	for _, stmt := range list {
+		listener.rewriter = *antlr.NewTokenStreamRewriter(stmt.Tokens)
+		antlr.ParseTreeWalkerDefault.Walk(listener, stmt.Tree)
+		if listener.err != nil {
+			return "", errors.Wrapf(listener.err, "statement: %s", singleStatement)
+		}
+	}
+	return listener.rewriter.GetTextDefault(), nil
+}
+
+type mysqlRewriter struct {
+	*mysql.BaseMySQLParserListener
+
+	rewriter       antlr.TokenStreamRewriter
+	err            error
+	outerMostQuery bool
+	limitCount     int
+}
+
+func (r *mysqlRewriter) EnterQueryExpression(ctx *mysql.QueryExpressionContext) {
+	if !r.outerMostQuery {
+		return
+	}
+	r.outerMostQuery = false
+	if ctx.LimitClause() != nil {
+		// limit clause already exists.
+		return
+	}
+
+	if ctx.OrderClause() != nil {
+		r.rewriter.InsertAfterDefault(ctx.OrderClause().GetStop().GetTokenIndex(), fmt.Sprintf(" LIMIT %d", r.limitCount))
+	} else {
+		switch {
+		case ctx.QueryExpressionBody() != nil:
+			r.rewriter.InsertAfterDefault(ctx.QueryExpressionBody().GetStop().GetTokenIndex(), fmt.Sprintf(" LIMIT %d", r.limitCount))
+		case ctx.QueryExpressionParens() != nil:
+			r.rewriter.InsertAfterDefault(ctx.QueryExpressionParens().GetStop().GetTokenIndex(), fmt.Sprintf(" LIMIT %d", r.limitCount))
+		}
+	}
 }
