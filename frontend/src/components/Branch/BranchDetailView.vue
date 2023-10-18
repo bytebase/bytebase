58b1cd6717f46f93e40a2d7d77b55250f43ff380
tianzhou <t@bytebase.com>
2023-10-18 16:47:37 +0800

    chore: make branch basline text small

--- frontend/src/components/Branch/BranchDetailView.vue
+++ frontend/src/components/Branch/BranchDetailView.vue
@@ -47,7 +47,9 @@
 
     <NDivider />
 
-    <div class="w-full flex flex-row justify-between items-center mt-1 gap-4">
+    <div
+      class="w-full flex flex-row justify-between items-center text-sm mt-1 gap-4"
+    >
       <div class="flex flex-row justify-start items-center opacity-80">
         <span class="mr-4 shrink-0"
           >{{ $t("schema-designer.baseline-version") }}:</span
