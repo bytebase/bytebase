08cf190dce0ea3aa0e2b8e614b94817aa81d87db
boojack <stevenlgtm@gmail.com>
2023-10-20 16:45:33 +0800

    chore(branching): hide project selector under project view (#8819)
    * chore: hide project selector when creating branch from project
    
    * chore: update
    
    Former-commit-id: 661d7cd89f7419c5efe0c48c3825e483a285d2ed

--- frontend/src/components/Branch/BranchSelector.vue
+++ frontend/src/components/Branch/BranchSelector.vue
@@ -8,42 +8,58 @@
     :clearable="clearable"
     :filter="filterByName"
     class="bb-branch-select"
+    :render-label="renderLabel"
     @update:value="$emit('update:branch', $event)"
   />
 </template>
 
 <script lang="ts" setup>
-import { NSelect, SelectOption } from "naive-ui";
-import { computed } from "vue";
-import { useSchemaDesignList } from "@/store";
+import { NSelect, SelectOption, SelectRenderLabel } from "naive-ui";
+import { computed, h } from "vue";
+import {
+  useDatabaseV1Store,
+  useProjectV1Store,
+  useSchemaDesignList,
+} from "@/store";
+import {
+  getProjectAndSchemaDesignSheetId,
+  projectNamePrefix,
+} from "@/store/modules/v1/common";
+import { UNKNOWN_ID } from "@/types";
 import { SchemaDesign } from "@/types/proto/v1/schema_design_service";
+import { InstanceV1EngineIcon } from "../v2";
 
 interface BranchSelectOption extends SelectOption {
   value: string;
   branch: SchemaDesign;
 }
 
-const props = withDefaults(
-  defineProps<{
-    branch?: string | undefined;
-    filter?: (branch: SchemaDesign, index: number) => boolean;
-    clearable?: boolean;
-  }>(),
-  {
-    branch: undefined,
-    clearable: true,
-    filter: () => true,
-  }
-);
+const props = defineProps<{
+  project?: string;
+  branch?: string;
+  clearable?: boolean;
+  filter?: (branch: SchemaDesign, index: number) => boolean;
+}>();
 
 defineEmits<{
   (event: "update:branch", name: string | undefined): void;
 }>();
 
 const { schemaDesignList: branchList } = useSchemaDesignList();
+const projectStore = useProjectV1Store();
+const databaseStore = useDatabaseV1Store();
 
 const combinedBranchList = computed(() => {
   let list = branchList.value;
+  if (props.project) {
+    const project = projectStore.getProjectByUID(props.project);
+    if (project) {
+      list = list.filter((branch) => {
+        const [projectName] = getProjectAndSchemaDesignSheetId(branch.name);
+        return project.name === `${projectNamePrefix}${projectName}`;
+      });
+    }
+  }
   if (props.filter) {
     list = list.filter(props.filter);
   }
@@ -66,6 +82,32 @@ const filterByName = (pattern: string, option: SelectOption) => {
   return (
     branch.name.toLowerCase().includes(pattern) ||
     branch.title.toLowerCase().includes(pattern)
+  );
+};
+
+const renderLabel: SelectRenderLabel = (option) => {
+  const { branch } = option as BranchSelectOption;
+  if (!branch) {
+    return;
+  }
+
+  const children = [h("div", {}, [branch.title])];
+  const database = databaseStore.getDatabaseByName(branch.baselineDatabase);
+  if (database.uid !== String(UNKNOWN_ID)) {
+    // prefix engine icon
+    children.unshift(
+      h(InstanceV1EngineIcon, {
+        class: "mr-1",
+        instance: database.instanceEntity,
+      })
+    );
+  }
+  return h(
+    "div",
+    {
+      class: "w-full flex flex-row justify-start items-center truncate",
+    },
+    children
   );
 };
 </script>
