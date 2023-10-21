c1b8cba733739402d6fbb26af1b5e912cff6ec7f
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-10-21 10:49:57 +0800

    chore: handle tidb table comment (#8824)
    * chore: handle tidb table comment
    
    * chore: update
    
    Former-commit-id: 5ddcd2d30b0306bb112084978c45d990a9887379

--- backend/api/v1/schema_design_transformer_tidb.go
+++ backend/api/v1/schema_design_transformer_tidb.go
@@ -114,6 +114,11 @@ func (t *tidbTransformer) Enter(in tidbast.Node) (tidbast.Node, bool) {
 
 			table.columns[columnName] = columnState
 		}
+		for _, tableOption := range node.Options {
+			if tableOption.Tp == tidbast.TableOptionComment {
+				table.comment = tableComment(tableOption)
+			}
+		}
 
 		// primary and foreign key definition
 		for _, constraint := range node.Constraints {
@@ -218,6 +223,10 @@ func columnDefaultValue(column *tidbast.ColumnDef) (*string, error) {
 	return nil, nil
 }
 
+func tableComment(option *tidbast.TableOption) string {
+	return option.StrValue
+}
+
 func columnComment(column *tidbast.ColumnDef) (string, error) {
 	for _, option := range column.Options {
 		if option.Tp == tidbast.ColumnOptionComment {
@@ -240,6 +249,7 @@ func tidbRestoreNode(node tidbast.Node, flag tidbformat.RestoreFlags) (string, e
 	}
 	return buffer.String(), nil
 }
+
 func tidbRestoreNodeDefault(node tidbast.Node) (string, error) {
 	return tidbRestoreNode(node, tidbformat.DefaultRestoreFlags)
 }
@@ -741,12 +751,45 @@ func (g *tidbDesignSchemaGenerator) Leave(in tidbast.Node) (tidbast.Node, bool) 
 		}
 
 		// Table option.
+		hasTableComment := false
 		for _, option := range node.Options {
-			if optionStr, err := tidbRestoreTableOption(option); err == nil {
-				if _, err := g.result.WriteString(" " + optionStr); err != nil {
-					g.err = err
-					return in, true
+			if option.Tp == tidbast.TableOptionComment {
+				commentValue := tableComment(option)
+				if g.currentTable.comment == commentValue {
+					if commentStr, err := tidbRestoreNodeDefault(option.Value); err == nil {
+						if _, err := g.result.WriteString(" " + commentStr); err != nil {
+							g.err = err
+							return in, true
+						}
+					}
+				} else if g.currentTable.comment != "" {
+					if _, err := g.result.WriteString(" COMMENT"); err != nil {
+						g.err = err
+						return in, true
+					}
+					if _, err := g.result.WriteString(fmt.Sprintf(" '%s'", g.currentTable.comment)); err != nil {
+						g.err = err
+						return in, true
+					}
 				}
+				hasTableComment = true
+			} else {
+				if optionStr, err := tidbRestoreTableOption(option); err == nil {
+					if _, err := g.result.WriteString(" " + optionStr); err != nil {
+						g.err = err
+						return in, true
+					}
+				}
+			}
+		}
+		if !hasTableComment && g.currentTable.comment != "" {
+			if _, err := g.result.WriteString(" COMMENT"); err != nil {
+				g.err = err
+				return in, true
+			}
+			if _, err := g.result.WriteString(fmt.Sprintf(" '%s'", g.currentTable.comment)); err != nil {
+				g.err = err
+				return in, true
 			}
 		}
 
