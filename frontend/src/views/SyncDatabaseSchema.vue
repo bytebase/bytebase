9e23d042e81cead3e09d723871ee0d416d9c5448
ecmadao <wlec@outlook.com>
2023-10-25 11:53:23 +0800

    chore: add sync schema page in project (#8899)
    * chore: add sync schema page
    
    * chore: update
    
    * chore: update

---
+++ frontend/src/views/SyncDatabaseSchema.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="px-4">
+    <SyncDatabaseSchema />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SyncDatabaseSchema from "@/components/SyncDatabaseSchema/index.vue";
+</script>
