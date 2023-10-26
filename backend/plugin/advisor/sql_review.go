ac79d2b442770f9496420da6601425df3ecec881
zll600 <71714656+zll600@users.noreply.github.com>
2023-10-26 10:55:36 +0800

    chore: migrate column_naming rule (#8910)
    Former-commit-id: f89eec425a019b37f5ea50efed08061470ac8e98
    Former-commit-id: f13e589c31db76179c8d4e0b9ae7a9090a9b75c6

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1105,6 +1105,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleColumnNaming:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLNamingColumnConvention, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLNamingColumnConvention, nil
 		case storepb.Engine_POSTGRES:
