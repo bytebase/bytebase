2cceb0489f72d6d1d0273e5d5c412e714a2772db
d-bytebase <d@bytebase.com>
2023-10-26 16:01:58 +0800

    chore: update

--- backend/server/ultimate.go
+++ backend/server/ultimate.go
@@ -9,7 +9,6 @@ import (
 	_ "github.com/bytebase/bytebase/backend/plugin/db/mongodb"
 	_ "github.com/bytebase/bytebase/backend/plugin/db/mssql"
 
-	// _ "github.com/bytebase/bytebase/backend/plugin/db/obo"
 	_ "github.com/bytebase/bytebase/backend/plugin/db/oracle"
 	_ "github.com/bytebase/bytebase/backend/plugin/db/redis"
 	_ "github.com/bytebase/bytebase/backend/plugin/db/redshift"
