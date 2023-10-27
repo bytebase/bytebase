353065ebbe0c6a8d0782d7b8f70dc52ea7f532ad
zll600 <71714656+zll600@users.noreply.github.com>
2023-10-27 14:19:49 +0800

    chore: naming unique key (#8957)
    * chore: migrate naming_unique_key_convention
    
    * fix: add baseLine when rename index
    
    * fix: add a period
    
    Former-commit-id: 5c96de116034e1a568290583d461e5dba61a3c1a
    Former-commit-id: 15960d28d2a51e0d006dc810e45863c18651a2a1

--- backend/plugin/advisor/mysqlwip/advisor_naming_unique_key_convention.go
+++ backend/plugin/advisor/mysqlwip/advisor_naming_unique_key_convention.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/pingcap/tidb/parser/ast"
+	"github.com/antlr4-go/antlr/v4"
 	"github.com/pkg/errors"
+
+	mysql "github.com/bytebase/mysql-parser"
 
 	"github.com/bytebase/bytebase/backend/plugin/advisor"
 	"github.com/bytebase/bytebase/backend/plugin/advisor/catalog"
+	mysqlparser "github.com/bytebase/bytebase/backend/plugin/parser/mysql"
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 )
 
 var (
 	_ advisor.Advisor = (*NamingUKConventionAdvisor)(nil)
-	_ ast.Visitor     = (*namingUKConventionChecker)(nil)
 )
+
+func init() {
+	// only for mysqlwip test.
+	advisor.Register(storepb.Engine_ENGINE_UNSPECIFIED, advisor.MySQLNamingUKConvention, &NamingUKConventionAdvisor{})
+}
 
 // NamingUKConventionAdvisor is the advisor checking for unique key naming convention.
 type NamingUKConventionAdvisor struct {
@@ -22,9 +30,9 @@ type NamingUKConventionAdvisor struct {
 
 // Check checks for index naming convention.
 func (*NamingUKConventionAdvisor) Check(ctx advisor.Context, _ string) ([]advisor.Advice, error) {
-	root, ok := ctx.AST.([]ast.StmtNode)
+	root, ok := ctx.AST.([]*mysqlparser.ParseResult)
 	if !ok {
-		return nil, errors.Errorf("failed to convert to StmtNode")
+		return nil, errors.Errorf("failed to convert to mysql parse result")
 	}
 
 	level, err := advisor.NewStatusBySQLReviewRuleLevel(ctx.Rule.Level)
@@ -45,7 +53,8 @@ func (*NamingUKConventionAdvisor) Check(ctx advisor.Context, _ string) ([]adviso
 		catalog:      ctx.Catalog,
 	}
 	for _, stmtNode := range root {
-		(stmtNode).Accept(checker)
+		checker.baseLine = stmtNode.BaseLine
+		antlr.ParseTreeWalkerDefault.Walk(checker, stmtNode.Tree)
 	}
 
 	if len(checker.adviceList) == 0 {
@@ -60,6 +69,10 @@ func (*NamingUKConventionAdvisor) Check(ctx advisor.Context, _ string) ([]adviso
 }
 
 type namingUKConventionChecker struct {
+	*mysql.BaseMySQLParserListener
+
+	baseLine     int
+	text         string
 	adviceList   []advisor.Advice
 	level        advisor.Status
 	title        string
@@ -69,18 +82,148 @@ type namingUKConventionChecker struct {
 	catalog      *catalog.Finder
 }
 
-// Enter implements the ast.Visitor interface.
-func (checker *namingUKConventionChecker) Enter(in ast.Node) (ast.Node, bool) {
-	indexDataList := checker.getMetaDataList(in)
+func (checker *namingUKConventionChecker) EnterQuery(ctx *mysql.QueryContext) {
+	checker.text = ctx.GetParser().GetTokenStream().GetTextFromRuleContext(ctx)
+}
 
+func (checker *namingUKConventionChecker) EnterCreateTable(ctx *mysql.CreateTableContext) {
+	if ctx.TableName() == nil {
+		return
+	}
+	if ctx.TableElementList() == nil {
+		return
+	}
+
+	_, tableName := mysqlparser.NormalizeMySQLTableName(ctx.TableName())
+
+	var indexDataList []*indexMetaData
+	for _, tableElement := range ctx.TableElementList().AllTableElement() {
+		if tableElement == nil {
+			continue
+		}
+		if tableElement.TableConstraintDef() == nil {
+			continue
+		}
+		if metaData := checker.handleConstraintDef(tableName, tableElement.TableConstraintDef()); metaData != nil {
+			indexDataList = append(indexDataList, metaData)
+		}
+	}
+	checker.handleIndexList(indexDataList)
+}
+
+func (checker *namingUKConventionChecker) EnterAlterTable(ctx *mysql.AlterTableContext) {
+	if ctx.AlterTableActions() == nil {
+		return
+	}
+	if ctx.AlterTableActions().AlterCommandList() == nil {
+		return
+	}
+	if ctx.AlterTableActions().AlterCommandList().AlterList() == nil {
+		return
+	}
+	if ctx.TableRef() == nil {
+		return
+	}
+
+	_, tableName := mysqlparser.NormalizeMySQLTableRef(ctx.TableRef())
+	var indexDataList []*indexMetaData
+	for _, alterListItem := range ctx.AlterTableActions().AlterCommandList().AlterList().AllAlterListItem() {
+		if alterListItem == nil {
+			continue
+		}
+
+		switch {
+		// add index.
+		case alterListItem.ADD_SYMBOL() != nil && alterListItem.TableConstraintDef() != nil:
+			if metaData := checker.handleConstraintDef(tableName, alterListItem.TableConstraintDef()); metaData != nil {
+				indexDataList = append(indexDataList, metaData)
+			}
+		// rename index.
+		case alterListItem.RENAME_SYMBOL() != nil && alterListItem.KeyOrIndex() != nil && alterListItem.IndexRef() != nil && alterListItem.IndexName() != nil:
+			_, _, oldIndexName := mysqlparser.NormalizeIndexRef(alterListItem.IndexRef())
+			newIndexName := mysqlparser.NormalizeIndexName(alterListItem.IndexName())
+			indexStateMap := checker.catalog.Origin.Index(&catalog.TableIndexFind{
+				SchemaName: "",
+				TableName:  tableName,
+			})
+			if indexStateMap == nil {
+				continue
+			}
+			indexState, ok := (*indexStateMap)[oldIndexName]
+			if !ok {
+				continue
+			}
+			if !indexState.Unique() {
+				continue
+			}
+			columnList := indexState.ExpressionList()
+			metaData := map[string]string{
+				advisor.ColumnListTemplateToken: strings.Join(columnList, "_"),
+				advisor.TableNameTemplateToken:  tableName,
+			}
+			indexData := &indexMetaData{
+				indexName: newIndexName,
+				tableName: tableName,
+				metaData:  metaData,
+				line:      checker.baseLine + ctx.GetStart().GetLine(),
+			}
+			indexDataList = append(indexDataList, indexData)
+		}
+	}
+	checker.handleIndexList(indexDataList)
+}
+
+func (checker *namingUKConventionChecker) EnterCreateIndex(ctx *mysql.CreateIndexContext) {
+	// only focus on unique index.
+	if ctx.UNIQUE_SYMBOL() == nil {
+		return
+	}
+	if ctx.IndexName() == nil || ctx.CreateIndexTarget() == nil || ctx.CreateIndexTarget().TableRef() == nil {
+		return
+	}
+
+	indexName := mysqlparser.NormalizeIndexName(ctx.IndexName())
+	_, tableName := mysqlparser.NormalizeMySQLTableRef(ctx.CreateIndexTarget().TableRef())
+
+	if ctx.CreateIndexTarget().KeyListVariants() == nil {
+		return
+	}
+	columnList := mysqlparser.NormalizeKeyListVariants(ctx.CreateIndexTarget().KeyListVariants())
+	metaData := map[string]string{
+		advisor.ColumnListTemplateToken: strings.Join(columnList, "_"),
+		advisor.TableNameTemplateToken:  tableName,
+	}
+	indexDataList := []*indexMetaData{
+		{
+			indexName: indexName,
+			tableName: tableName,
+			metaData:  metaData,
+			line:      checker.baseLine + ctx.GetStart().GetLine(),
+		},
+	}
+	checker.handleIndexList(indexDataList)
+}
+
+func (checker *namingUKConventionChecker) handleIndexList(indexDataList []*indexMetaData) {
 	for _, indexData := range indexDataList {
+		// if indexName is not set explicitly. we throw an error.
+		if indexData.indexName == "" {
+			checker.adviceList = append(checker.adviceList, advisor.Advice{
+				Status:  checker.level,
+				Title:   checker.title,
+				Code:    advisor.NamingUKNameEmpty,
+				Content: fmt.Sprintf("Unique key in table `%s` should be set a name explicitly", indexData.tableName),
+				Line:    indexData.line,
+			})
+			continue
+		}
 		regex, err := getTemplateRegexp(checker.format, checker.templateList, indexData.metaData)
 		if err != nil {
 			checker.adviceList = append(checker.adviceList, advisor.Advice{
 				Status:  checker.level,
 				Code:    advisor.Internal,
 				Title:   "Internal error for unique key naming convention rule",
-				Content: fmt.Sprintf("%q meet internal error %q", in.Text(), err.Error()),
+				Content: fmt.Sprintf("%q meet internal error %q", checker.text, err.Error()),
 			})
 			continue
 		}
@@ -103,104 +246,28 @@ func (checker *namingUKConventionChecker) Enter(in ast.Node) (ast.Node, bool) {
 			})
 		}
 	}
-
-	return in, false
 }
 
-// Leave implements the ast.Visitor interface.
-func (*namingUKConventionChecker) Leave(in ast.Node) (ast.Node, bool) {
-	return in, true
-}
-
-// getMetaDataList returns the list of unique key with meta data.
-func (checker *namingUKConventionChecker) getMetaDataList(in ast.Node) []*indexMetaData {
-	var res []*indexMetaData
-
-	switch node := in.(type) {
-	case *ast.CreateTableStmt:
-		for _, constraint := range node.Constraints {
-			switch constraint.Tp {
-			case ast.ConstraintUniq, ast.ConstraintUniqKey, ast.ConstraintUniqIndex:
-				var columnList []string
-				for _, key := range constraint.Keys {
-					columnList = append(columnList, key.Column.Name.String())
-				}
-				metaData := map[string]string{
-					advisor.ColumnListTemplateToken: strings.Join(columnList, "_"),
-					advisor.TableNameTemplateToken:  node.Table.Name.String(),
-				}
-				res = append(res, &indexMetaData{
-					indexName: constraint.Name,
-					tableName: node.Table.Name.String(),
-					metaData:  metaData,
-					line:      constraint.OriginTextPosition(),
-				})
-			}
-		}
-	case *ast.AlterTableStmt:
-		for _, spec := range node.Specs {
-			switch spec.Tp {
-			case ast.AlterTableRenameIndex:
-				_, index := checker.catalog.Origin.FindIndex(&catalog.IndexFind{
-					TableName: node.Table.Name.String(),
-					IndexName: spec.FromKey.String(),
-				})
-				if index == nil {
-					continue
-				}
-				if !index.Unique() {
-					// Index naming convention should in advisor_naming_index_convention.go
-					continue
-				}
-				metaData := map[string]string{
-					advisor.ColumnListTemplateToken: strings.Join(index.ExpressionList(), "_"),
-					advisor.TableNameTemplateToken:  node.Table.Name.String(),
-				}
-				res = append(res, &indexMetaData{
-					indexName: spec.ToKey.String(),
-					tableName: node.Table.Name.String(),
-					metaData:  metaData,
-					line:      in.OriginTextPosition(),
-				})
-			case ast.AlterTableAddConstraint:
-				switch spec.Constraint.Tp {
-				case ast.ConstraintUniq, ast.ConstraintUniqKey, ast.ConstraintUniqIndex:
-					var columnList []string
-					for _, key := range spec.Constraint.Keys {
-						columnList = append(columnList, key.Column.Name.String())
-					}
-
-					metaData := map[string]string{
-						advisor.ColumnListTemplateToken: strings.Join(columnList, "_"),
-						advisor.TableNameTemplateToken:  node.Table.Name.String(),
-					}
-					res = append(res, &indexMetaData{
-						indexName: spec.Constraint.Name,
-						tableName: node.Table.Name.String(),
-						metaData:  metaData,
-						line:      in.OriginTextPosition(),
-					})
-				}
-			}
-		}
-	case *ast.CreateIndexStmt:
-		if node.KeyType == ast.IndexKeyTypeUnique {
-			var columnList []string
-			for _, spec := range node.IndexPartSpecifications {
-				columnList = append(columnList, spec.Column.Name.String())
-			}
-			metaData := map[string]string{
-				advisor.ColumnListTemplateToken: strings.Join(columnList, "_"),
-				advisor.TableNameTemplateToken:  node.Table.Name.String(),
-			}
-			res = append(res, &indexMetaData{
-				indexName: node.IndexName,
-				tableName: node.Table.Name.String(),
-				metaData:  metaData,
-				line:      in.OriginTextPosition(),
-			})
-		}
+func (checker *namingUKConventionChecker) handleConstraintDef(tableName string, ctx mysql.ITableConstraintDefContext) *indexMetaData {
+	// focus on unique index.
+	if ctx.UNIQUE_SYMBOL() == nil || ctx.KeyListVariants() == nil {
+		return nil
 	}
 
-	return res
+	indexName := ""
+	if ctx.IndexNameAndType() != nil && ctx.IndexNameAndType().IndexName() != nil {
+		indexName = mysqlparser.NormalizeIndexName(ctx.IndexNameAndType().IndexName())
+	}
+
+	columnList := mysqlparser.NormalizeKeyListVariants(ctx.KeyListVariants())
+	metaData := map[string]string{
+		advisor.ColumnListTemplateToken: strings.Join(columnList, "_"),
+		advisor.TableNameTemplateToken:  tableName,
+	}
+	return &indexMetaData{
+		indexName: indexName,
+		tableName: tableName,
+		metaData:  metaData,
+		line:      checker.baseLine + ctx.GetStart().GetLine(),
+	}
 }
