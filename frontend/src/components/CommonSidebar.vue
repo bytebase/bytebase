8987e28dc81d133889554a85dcb1ed6ed31fb88d
ecmadao <wlec@outlook.com>
2023-10-27 11:05:56 +0800

    chore: make sidebar smaller

--- frontend/src/components/CommonSidebar.vue
+++ frontend/src/components/CommonSidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="flex-1 flex flex-col overflow-y-hidden">
     <BytebaseLogo class="w-full px-4 shrink-0" />
-    <div class="space-y-1 flex-1 overflow-y-auto px-2 pb-4">
+    <div class="flex-1 overflow-y-auto px-2 pb-4">
       <button
         v-if="showGoBack"
         class="group shrink-0 flex items-center px-2 py-2 text-base leading-5 font-normal rounded-md text-gray-700 hover:opacity-80 focus:outline-none"
@@ -17,7 +17,7 @@
         <router-link
           v-if="type === 'route' && item.path"
           :to="item.path"
-          class="outline-item group w-full flex items-center pl-11 pr-2 py-1.5"
+          class="outline-item group w-full flex items-center px-2 py-1.5"
           :class="getItemClass(item.path)"
         >
           <component :is="item.icon" class="mr-2 w-5 h-5 text-gray-500" />
@@ -25,7 +25,7 @@
         </router-link>
         <div
           v-else
-          class="group flex items-center px-2 py-2 text-sm leading-5 font-medium rounded-md text-gray-700 outline-item"
+          class="group flex items-center px-2 py-1.5 text-sm leading-5 font-medium rounded-md text-gray-700 outline-item"
           :class="getItemClass(item.path)"
           @click="onClick(i)"
         >
@@ -38,12 +38,12 @@
         </div>
         <div
           v-if="item.children.length > 0 && state.expandedSidebar.has(i)"
-          class="space-y-1"
+          class=""
         >
           <template v-for="(child, j) in item.children" :key="`${i}-${j}`">
             <div
               v-if="type === 'div'"
-              class="group w-full flex items-center pl-11 pr-2 py-1.5 rounded-md"
+              class="group w-full flex items-center pl-11 pr-2 py-1.5 rounded-md outline-item"
               :class="getItemClass(child.path)"
               @click="$emit('select', child.path)"
             >
