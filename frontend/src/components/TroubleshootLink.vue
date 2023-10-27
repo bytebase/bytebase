bf02ed3f77f882714c1b00ba17fbd7c297e03156
Tianzhou (天舟) <t@bytebase.com>
2023-10-27 17:21:19 +0800

    chore: add troubleshoot link to project GitOps panel (#8974)
    Former-commit-id: 1d07a4ecd29d7afc7e5bc37dc606eaf79dce26a4
    Former-commit-id: bbbc533d9fdbd9b287690dd390233d0e5b81cc53

---
+++ frontend/src/components/TroubleshootLink.vue
@@ -0,0 +1,25 @@
+<template>
+  <a
+    class="inline-flex items-center"
+    :class="[color === 'normal' ? 'normal-link' : 'light-link']"
+    :href="url"
+    target="__BLANK"
+  >
+    🐞 {{ $t("common.troubleshoot") }}
+  </a>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from "vue";
+
+defineProps({
+  url: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String as PropType<"normal" | "light">,
+    default: "normal",
+  },
+});
+</script>
