3b4de7d0e00c0e16afee5bfadeeeca6f14e40860
zll600 <71714656+zll600@users.noreply.github.com>
2023-10-30 10:57:02 +0800

    chore: migrate naming_fk rule (#8973)

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1104,6 +1104,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleFKNaming:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLNamingFKConvention, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLNamingFKConvention, nil
 		case storepb.Engine_POSTGRES:
