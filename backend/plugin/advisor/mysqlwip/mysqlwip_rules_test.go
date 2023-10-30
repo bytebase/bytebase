4fb1898579b439cd98911060a42f3e9d242ac9d8
zll600 <3400692417@qq.com>
2023-10-30 17:04:54 +0800

    chore: update

--- backend/plugin/advisor/mysqlwip/mysqlwip_rules_test.go
+++ backend/plugin/advisor/mysqlwip/mysqlwip_rules_test.go
@@ -39,12 +39,3 @@ func TestMySQLWIPRules(t *testing.T) {
 		advisor.RunSQLReviewRuleTest(t, rule, storepb.Engine_ENGINE_UNSPECIFIED, false /* record */)
 	}
 }
-
-func TestRules(t *testing.T) {
-	mysqlwipRules := []advisor.SQLReviewRuleType{
-		advisor.SchemaRuleStatementDisallowOrderBy,
-	}
-	for _, rule := range mysqlwipRules {
-		advisor.RunSQLReviewRuleTest(t, rule, storepb.Engine_ENGINE_UNSPECIFIED, false /* record */)
-	}
-}
