916fe494c22748b6e697e2ae7d134647068de784
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-10-30 23:06:26 +0800

    chore: remove debug banner (#9027)
    Former-commit-id: 24039dcc0e83ad7a5177b88f17925addffcff767

--- frontend/src/components/BannersWrapper.vue
+++ frontend/src/components/BannersWrapper.vue
@@ -4,9 +4,6 @@
     <template v-if="shouldShowDemoBanner">
       <BannerDemo />
     </template>
-    <template v-if="shouldShowDebugBanner">
-      <BannerDebug />
-    </template>
     <template v-if="shouldShowSubscriptionBanner">
       <BannerSubscription />
     </template>
@@ -28,15 +25,10 @@
 <script lang="ts" setup>
 import { storeToRefs } from "pinia";
 import { computed } from "vue";
-import {
-  useActuatorV1Store,
-  useCurrentUserV1,
-  useSubscriptionV1Store,
-} from "@/store/modules";
+import { useActuatorV1Store, useSubscriptionV1Store } from "@/store/modules";
 import { PlanType } from "@/types/proto/v1/subscription_service";
-import { hasWorkspacePermissionV1, isDev } from "@/utils";
+import { isDev } from "@/utils";
 import BannerAnnouncement from "@/views/BannerAnnouncement.vue";
-import BannerDebug from "@/views/BannerDebug.vue";
 import BannerDemo from "@/views/BannerDemo.vue";
 import BannerExternalUrl from "@/views/BannerExternalUrl.vue";
 import BannerSubscription from "@/views/BannerSubscription.vue";
@@ -44,29 +36,15 @@ import BannerUpgradeSubscription from "@/views/BannerUpgradeSubscription.vue";
 import HideInStandaloneMode from "./misc/HideInStandaloneMode.vue";
 
 const actuatorStore = useActuatorV1Store();
-const currentUserV1 = useCurrentUserV1();
 const subscriptionStore = useSubscriptionV1Store();
 
-const { isDemo, isReadonly, isDebug, needConfigureExternalUrl } =
+const { isDemo, isReadonly, needConfigureExternalUrl } =
   storeToRefs(actuatorStore);
 const { isExpired, isTrialing, currentPlan, existTrialLicense } =
   storeToRefs(subscriptionStore);
 
 const shouldShowDemoBanner = computed(() => {
   return actuatorStore.serverInfo?.demoName != "";
-});
-
-// For now, debug mode is a global setting and will affect all users.
-// So we only allow DBA and Owner to toggle it and thus show a banner
-// reminding them to turn off
-const shouldShowDebugBanner = computed(() => {
-  return (
-    isDebug.value &&
-    hasWorkspacePermissionV1(
-      "bb.permission.workspace.debug",
-      currentUserV1.value.userRole
-    )
-  );
 });
 
 const shouldShowSubscriptionBanner = computed(() => {
