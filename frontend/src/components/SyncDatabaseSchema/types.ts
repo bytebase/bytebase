19cdf92bbf37eb379b9d7d17e0e470dd614d658e
steven <stevenlgtm@gmail.com>
2023-10-30 15:19:01 +0800

    chore: remove branch from sync schema

--- frontend/src/components/SyncDatabaseSchema/types.ts
+++ frontend/src/components/SyncDatabaseSchema/types.ts
@@ -1,10 +1,7 @@
 import { Engine } from "@/types/proto/v1/common";
 import { ChangeHistory } from "@/types/proto/v1/database_service";
 
-export type SourceSchemaType =
-  | "SCHEMA_HISTORY_VERSION"
-  | "SCHEMA_DESIGN"
-  | "RAW_SQL";
+export type SourceSchemaType = "SCHEMA_HISTORY_VERSION" | "RAW_SQL";
 
 export interface ChangeHistorySourceSchema {
   projectId?: string;
