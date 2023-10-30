9cc80a0e7d04fa57435c03e8411ce10594088482
zll600 <71714656+zll600@users.noreply.github.com>
2023-10-30 11:09:07 +0800

    chore: naming index (#8987)
    * chore: migrate naming_index rule
    
    * chore: update
    
    Former-commit-id: 80d3ca015f94128dd26e8bac3e6ce132ae38fd9d
    Former-commit-id: 4fb1cc5d232a282d6b3aef5421d652ab6e9079c0

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1083,6 +1083,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleIDXNaming:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLNamingIndexConvention, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLNamingIndexConvention, nil
 		case storepb.Engine_POSTGRES:
