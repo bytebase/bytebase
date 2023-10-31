59134213eac600e2fc26ae0a5ff264c0f029fab5
rebelice <yangrebelice@gmail.com>
2023-10-31 17:40:52 +0800

    feat(LSP): implement hello-world for completion

---
+++ backend/api/lsp/completion.go
@@ -0,0 +1,39 @@
+package lsp
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+	"github.com/sourcegraph/go-lsp"
+	"github.com/sourcegraph/jsonrpc2"
+)
+
+func (h *Handler) handleTextDocumentCompletion(ctx context.Context, conn *jsonrpc2.Conn, req *jsonrpc2.Request, params lsp.CompletionParams) (*lsp.CompletionList, error) {
+	if !IsURI(params.TextDocument.URI) {
+		return nil, &jsonrpc2.Error{
+			Code:    jsonrpc2.CodeInvalidParams,
+			Message: fmt.Sprintf("textDocument/completion not yet supported for out-of-workspace URI (%q)", params.TextDocument.URI),
+		}
+	}
+	content, err := h.readFile(ctx, params.TextDocument.URI)
+	if err != nil {
+		return nil, err
+	}
+	_, valid, why := offsetForPosition(content, params.Position)
+	if !valid {
+		return nil, errors.Errorf("invalid position %d:%d (%s)", params.Position.Line, params.Position.Character, why)
+	}
+
+	// TODO: implement
+	return &lsp.CompletionList{
+		IsIncomplete: false,
+		Items: []lsp.CompletionItem{
+			{
+				Label:  "hello",
+				Detail: "<world>",
+				Kind:   lsp.CIKKeyword,
+			},
+		},
+	}, nil
+}
