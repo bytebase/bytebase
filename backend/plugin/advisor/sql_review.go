70851af10459cb4311321daf32b64e03143e8cd9
zll600 <71714656+zll600@users.noreply.github.com>
2023-11-01 11:54:03 +0800

    chore: migrate table_require_pk (#9049)
    * chore: migrate table_require_pk
    
    Former-commit-id: 5faa7d7c80a6f37b4e2e6a24e38eefd8f3c14b07

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1289,6 +1289,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleTableRequirePK:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLTableRequirePK, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLTableRequirePK, nil
 		case storepb.Engine_POSTGRES:
