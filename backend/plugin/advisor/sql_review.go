4b47ac2e133ee5d47fc9cdc849f127bf81e6735e
zll600 <71714656+zll600@users.noreply.github.com>
2023-11-01 11:32:07 +0800

    chore: migrate table_no_fk (#9057)
    Former-commit-id: 3649e83f91e2454d6c4b172e29984db7eff6928e
    Former-commit-id: 0ab7d3feb864ed869ad75922a1f1a2311056c206

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1302,6 +1302,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleTableNoFK:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLTableNoFK, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLTableNoFK, nil
 		case storepb.Engine_POSTGRES:
