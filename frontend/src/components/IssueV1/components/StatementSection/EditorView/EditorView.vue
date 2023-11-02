6c9a99541df8c9eab802b9125e3317c03193c060
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-02 19:35:05 +0800

    chore: use Long type for Sheet.contentSize

--- frontend/src/components/IssueV1/components/StatementSection/EditorView/EditorView.vue
+++ frontend/src/components/IssueV1/components/StatementSection/EditorView/EditorView.vue
@@ -158,6 +158,7 @@
 
 <script setup lang="ts">
 import { cloneDeep } from "lodash-es";
+import Long from "long";
 import { NButton, NTooltip, useDialog } from "naive-ui";
 import { computed, h, reactive, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
@@ -303,7 +304,9 @@ const isTaskSheetOversize = computed(() => {
   if (state.isEditing) return false;
   if (!sheetReady.value) return false;
   if (!sheet.value) return false;
-  return getSheetStatement(sheet.value).length < sheet.value.contentSize;
+  return Long.fromNumber(getSheetStatement(sheet.value).length).lt(
+    sheet.value.contentSize
+  );
 });
 
 const denyEditTaskReasons = computed(() => {
