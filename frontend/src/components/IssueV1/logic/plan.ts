4223b600a97b46ea27b1f8eb94a53e16db8d1f9f
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-03 11:49:52 +0800

    chore: filter logic (#9126)
    Former-commit-id: 88a14c6219c8771870a22c42b3ba0944e9311619
    Former-commit-id: 9df79f5556d5004634986435c78bb1c24f2e8103

--- frontend/src/components/IssueV1/logic/plan.ts
+++ frontend/src/components/IssueV1/logic/plan.ts
@@ -1,5 +1,9 @@
 import { Plan_Spec } from "@/types/proto/v1/rollout_service";
 
+/**
+ *
+ * @returns empty string if no sheet found
+ */
 export const sheetNameForSpec = (spec: Plan_Spec): string => {
   return spec.changeDatabaseConfig?.sheet ?? "";
 };
