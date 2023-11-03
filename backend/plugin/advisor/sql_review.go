0feda169274ebffab31dd08fb3d5ec64af60605e
zll600 <71714656+zll600@users.noreply.github.com>
2023-11-03 17:08:06 +0800

    chore: migrate column_maximum_character_length (#9130)
    * chore: migrate column_maximum_character_length
    
    * chore: update
    
    Former-commit-id: 2f112659e3bb7c77c37856086a3b96bc026d095f

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1257,6 +1257,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleColumnMaximumCharacterLength:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLColumnMaximumCharacterLength, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLColumnMaximumCharacterLength, nil
 		case storepb.Engine_POSTGRES:
