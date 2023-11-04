486cf7666332b6bc0b73750c8622e19f0b911dad
Tianzhou (天舟) <t@bytebase.com>
2023-11-04 22:14:12 +0800

    chore: in release mode, replace demo dump owner “bbdev” with “bb” (#9148)
    * chore: in release mode, replace demo dump owner “bbdev” with “bb”
    
    * chore: fix lint
    
    Former-commit-id: f88b59b3c5851c9ae05fe177931f6cc8ea857795

--- backend/server/server.go
+++ backend/server/server.go
@@ -197,7 +197,7 @@ func NewServer(ctx context.Context, profile config.Profile) (*Server, error) {
 	if profile.Readonly {
 		slog.Info("Database is opened in readonly mode. Skip migration and demo data setup.")
 	} else {
-		if err := demo.LoadDemoDataIfNeeded(ctx, storeDB, s.pgBinDir, profile.DemoName); err != nil {
+		if err := demo.LoadDemoDataIfNeeded(ctx, storeDB, s.pgBinDir, profile.DemoName, profile.Mode); err != nil {
 			return nil, errors.Wrapf(err, "failed to load demo data")
 		}
 		if _, err := migrator.MigrateSchema(ctx, storeDB, s.pgBinDir, profile.Version, profile.Mode); err != nil {
