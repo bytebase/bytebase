d43b6a94cbb81916073ff189b7585d83904cb6dc
Tianzhou Chen <t@bytebase.com>
2023-11-04 18:44:36 +0800

    feat: add system label to system objects

---
+++ frontend/src/components/SystemLabel.vue
@@ -0,0 +1,15 @@
+<template>
+  <span
+    class="inline-flex items-center px-2 py-0.5 rounded-lg text-xs font-semibold bg-gray-100 text-gray-800"
+  >
+    {{ $t("common.system") }}
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SystemLabel",
+});
+</script>
