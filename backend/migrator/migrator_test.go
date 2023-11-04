2438fa9858985c765c65d3b34af4a7928bf72c76
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-04 20:05:42 +0800

    Update backend/migrator/migrator_test.go

--- backend/migrator/migrator_test.go
+++ backend/migrator/migrator_test.go
@@ -184,7 +184,7 @@ func TestMigrationCompatibility(t *testing.T) {
 	require.NoError(t, err)
 
 	db := store.NewDB(metadataConnConfig, pgBinDir, "", false, "", common.ReleaseModeDev)
-	err = db.Open(ctx, true /*createDB*/)
+	err = db.Open(ctx, true /* createDB */)
 	require.NoError(t, err)
 	storeInstance := store.New(db)
 
