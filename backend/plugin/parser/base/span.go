e7cc8ce79d079caf42324209bcc0a8c9b9b6f8cd
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-05 20:59:50 +0800

    chore: move database metadata to model (#9161)
    Former-commit-id: 2046aa6904e2a6c19bab4790a1952ae595bba4f6

--- backend/plugin/parser/base/span.go
+++ backend/plugin/parser/base/span.go
@@ -3,7 +3,7 @@ package base
 import (
 	"context"
 
-	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+	"github.com/bytebase/bytebase/backend/store/model"
 )
 
 // QuerySpan is the span for a query.
@@ -31,64 +31,4 @@ type ColumnResource struct {
 }
 
 // GetDatabaseMetadataFunc is the function to get database metadata.
-type GetDatabaseMetadataFunc func(context.Context, string) (*DatabaseMetadata, error)
-
-// DatabaseMetadata is the metadata for a database.
-type DatabaseMetadata struct {
-	internal map[string]*SchemaMetadata
-}
-
-// NewDatabaseMetadata creates a new database metadata.
-func NewDatabaseMetadata(metadata *storepb.DatabaseSchemaMetadata) *DatabaseMetadata {
-	databaseMetadata := &DatabaseMetadata{
-		internal: make(map[string]*SchemaMetadata),
-	}
-	for _, schema := range metadata.Schemas {
-		schemaMetadata := &SchemaMetadata{
-			internal: make(map[string]*TableMetadata),
-		}
-		for _, table := range schema.Tables {
-			tableMetadata := &TableMetadata{
-				internal: make(map[string]*storepb.ColumnMetadata),
-			}
-			for _, column := range table.Columns {
-				tableMetadata.internal[column.Name] = column
-				tableMetadata.columns = append(tableMetadata.columns, column)
-			}
-			schemaMetadata.internal[table.Name] = tableMetadata
-		}
-		databaseMetadata.internal[schema.Name] = schemaMetadata
-	}
-	return databaseMetadata
-}
-
-// GetSchema gets the schema by name.
-func (d *DatabaseMetadata) GetSchema(name string) *SchemaMetadata {
-	return d.internal[name]
-}
-
-// SchemaMetadata is the metadata for a schema.
-type SchemaMetadata struct {
-	internal map[string]*TableMetadata
-}
-
-// GetTable gets the schema by name.
-func (s *SchemaMetadata) GetTable(name string) *TableMetadata {
-	return s.internal[name]
-}
-
-// TableMetadata is the metadata for a table.
-type TableMetadata struct {
-	internal map[string]*storepb.ColumnMetadata
-	columns  []*storepb.ColumnMetadata
-}
-
-// GetColumn gets the column by name.
-func (t *TableMetadata) GetColumn(name string) *storepb.ColumnMetadata {
-	return t.internal[name]
-}
-
-// GetColumns gets the columns.
-func (t *TableMetadata) GetColumns() []*storepb.ColumnMetadata {
-	return t.columns
-}
+type GetDatabaseMetadataFunc func(context.Context, string) (*model.DatabaseMetadata, error)
