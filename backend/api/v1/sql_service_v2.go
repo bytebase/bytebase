ebd8afc729039795df859092fb2804b71fe68dab
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-05 21:46:32 +0800

    chore: query access control v2 (#9162)
    * chore: query access control v2
    
    * chore: update

--- backend/api/v1/sql_service_v2.go
+++ backend/api/v1/sql_service_v2.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/proto"
 
 	api "github.com/bytebase/bytebase/backend/legacyapi"
@@ -34,9 +37,15 @@ func (s *SQLService) QueryV2(ctx context.Context, request *v1pb.QueryRequest) (*
 	}
 
 	// Get query span.
-	_, err = base.GetQuerySpan(ctx, instance.Engine, statement, s.buildGetDatabaseMetadataFunc(instance))
+	span, err := base.GetQuerySpan(ctx, instance.Engine, statement, s.buildGetDatabaseMetadataFunc(instance))
 	if err != nil {
 		return nil, err
+	}
+
+	if s.licenseService.IsFeatureEnabled(api.FeatureAccessControl) == nil {
+		if err := s.accessCheck(ctx, instance, environment, user, request.Statement, span, request.Limit, false /* isAdmin */, false /* isExport */); err != nil {
+			return nil, err
+		}
 	}
 
 	// Run SQL review.
@@ -116,4 +125,70 @@ func (s *SQLService) buildGetDatabaseMetadataFunc(instance *store.InstanceMessag
 		}
 		return databaseMetadata.GetDatabaseMetadata(), nil
 	}
+}
+
+func (s *SQLService) accessCheck(
+	ctx context.Context,
+	instance *store.InstanceMessage,
+	environment *store.EnvironmentMessage,
+	user *store.UserMessage,
+	statement string,
+	span *base.QuerySpan,
+	limit int32,
+	isAdmin,
+	isExport bool) error {
+	// Check if the caller is admin for exporting with admin mode.
+	if isAdmin && (user.Role != api.Owner && user.Role != api.DBA) {
+		return status.Errorf(codes.PermissionDenied, "only workspace owner and DBA can export data using admin mode")
+	}
+
+	// Check if the environment is open for query privileges.
+	ok, err := s.checkWorkspaceIAMPolicy(ctx, environment, isExport)
+	if err != nil {
+		return err
+	}
+	if ok {
+		return nil
+	}
+
+	for column := range span.SourceColumns {
+		databaseResourceURL := fmt.Sprintf("instances/%s/databases/%s", instance.ResourceID, column.Database)
+		attributes := map[string]any{
+			"request.time":      time.Now(),
+			"resource.database": databaseResourceURL,
+			"resource.schema":   column.Schema,
+			"resource.table":    column.Table,
+			"request.statement": encodeToBase64String(statement),
+			"request.row_limit": limit,
+		}
+
+		project, database, err := s.getProjectAndDatabaseMessage(ctx, instance, column.Database)
+		if err != nil {
+			return err
+		}
+		if project == nil && database == nil {
+			// If database not found, skip.
+			// TODO(d): re-evaluate this case.
+			continue
+		}
+		if project == nil {
+			// Never happen
+			return status.Errorf(codes.Internal, "project not found for database: %s", column.Database)
+		}
+		// Allow query databases across different projects.
+		projectPolicy, err := s.store.GetProjectPolicy(ctx, &store.GetProjectPolicyMessage{ProjectID: &project.ResourceID})
+		if err != nil {
+			return err
+		}
+
+		ok, err := hasDatabaseAccessRights(user.ID, projectPolicy, attributes, isExport)
+		if err != nil {
+			return status.Errorf(codes.Internal, "failed to check access control for database: %q", column.Database)
+		}
+		if !ok {
+			return status.Errorf(codes.PermissionDenied, "permission denied to access resource: %q", column.String())
+		}
+	}
+
+	return nil
 }
