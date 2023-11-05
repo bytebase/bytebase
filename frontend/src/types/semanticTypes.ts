57e02f76bf00094b1a2e87dab13ef7b9b058f7f5
ecmadao <wlec@outlook.com>
2023-11-05 10:10:42 +0800

    chore: support the simplest semantic type template (#9154)
    * chore: support the simplest semantic type template
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: bcbfce6e3440939b2ed72fcf1e6e8052ea3ad910

---
+++ frontend/src/types/semanticTypes.ts
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from "uuid";
+import { useI18n } from "vue-i18n";
+import { SemanticTypeSetting_SemanticType } from "@/types/proto/v1/setting_service";
+import buildInSemanticTypes from "./semantic-types.yaml";
+
+interface BuildInSemantic {
+  id: string;
+  fullMaskAlgorithmId?: string;
+  partialMaskAlgorithmId?: string;
+}
+
+export const getSemanticTemplateList = () => {
+  const { t } = useI18n();
+  return (buildInSemanticTypes as BuildInSemantic[]).map((buildInSemantic) =>
+    SemanticTypeSetting_SemanticType.fromPartial({
+      id: uuidv4(),
+      title: t(
+        `settings.sensitive-data.semantic-types.template.${buildInSemantic.id}.title`
+      ),
+      description: t(
+        `settings.sensitive-data.semantic-types.template.${buildInSemantic.id}.description`
+      ),
+      fullMaskAlgorithmId: buildInSemantic.fullMaskAlgorithmId,
+      partialMaskAlgorithmId: buildInSemantic.partialMaskAlgorithmId,
+    })
+  );
+};
