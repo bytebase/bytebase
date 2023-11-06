9cb95a7482fd2712270eabbbfe46d9f29083f7c8
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-06 13:31:25 +0800

    chore: update gh-ost params UI (#9174)
    Former-commit-id: 507e111360534951de6f94bd50e7b0e3a2a432d2
    Former-commit-id: 4b7d37a0961749dda79589f1d155ad989e701744

--- frontend/src/components/IssueV1/components/StageSection/Actions/GhostSection/FlagsForm/constants.ts
+++ frontend/src/components/IssueV1/components/StageSection/Actions/GhostSection/FlagsForm/constants.ts
@@ -3,88 +3,22 @@ export type GhostParameterType = "bool" | "int" | "float" | "string";
 export type GhostParameter<T extends GhostParameterType = any> = {
   key: string;
   type: T;
+  defaults: string;
 };
 
-export type GhostParameterWithValue<T extends GhostParameterType = any> =
-  GhostParameter<T> & {
-    value: string;
-  };
-
 export const SupportedGhostParameters: GhostParameter[] = [
-  { key: "max-load", type: "string" },
-  { key: "chunk-size", type: "int" },
-  { key: "initially-drop-ghost-table", type: "bool" },
-  { key: "max-lag-millis", type: "int" },
-  { key: "allow-on-master", type: "bool" },
-  { key: "switch-to-rbr", type: "bool" },
-];
-
-export const DefaultGhostParameters: GhostParameterWithValue[] = [
-  {
-    key: "allow-on-master",
-    type: "bool",
-    value: "true",
-  },
-  {
-    key: "concurrent-rowcount",
-    type: "bool",
-    value: "true",
-  },
-  {
-    key: "hooks-status-interval",
-    type: "int",
-    value: "60",
-  },
-  {
-    key: "heartbeat-interval-millis",
-    type: "int",
-    value: "100",
-  },
-  {
-    key: "nice-ratio",
-    type: "float",
-    value: "0",
-  },
-  {
-    key: "chunk-size",
-    type: "int",
-    value: "1000",
-  },
-  {
-    key: "dml-batch-size",
-    type: "int",
-    value: "10",
-  },
-  {
-    key: "max-lag-millis",
-    type: "int",
-    value: "1500",
-  },
-  {
-    key: "default-retries",
-    type: "int",
-    value: "60",
-  },
-  {
-    key: "cut-over-lock-timeout-seconds",
-    type: "int",
-    value: "60",
-  },
-  {
-    key: "exponential-backoff-max-interval",
-    type: "int",
-    value: "64",
-  },
-  {
-    key: "throttle-http-interval-millis",
-    type: "int",
-    value: "100",
-  },
-  {
-    key: "throttle-http-timeout-millis",
-    type: "int",
-    value: "1000",
-  },
+  { key: "max-load", type: "string", defaults: "" },
+  { key: "chunk-size", type: "int", defaults: "1000" },
+  { key: "dml-batch-size", type: "int", defaults: "10" },
+  { key: "default-retries", type: "int", defaults: "60" },
+  { key: "cut-over-lock-timeout-seconds", type: "int", defaults: "60" },
+  { key: "exponential-backoff-max-interval", type: "int", defaults: "64" },
+  { key: "max-lag-millis", type: "int", defaults: "1500" },
+  { key: "allow-on-master", type: "bool", defaults: "true" },
+  { key: "switch-to-rbr", type: "bool", defaults: "false" },
+  { key: "assume-rbr", type: "bool", defaults: "false" },
+  { key: "heart-beat-interval-millis", type: "int", defaults: "100" },
+  { key: "nice-ratio", type: "float", defaults: "0" },
 ];
 
 export const isBoolParameter = (
@@ -103,4 +37,10 @@ export const isStringParameter = (
   param: GhostParameter
 ): param is GhostParameter<"string"> => {
   return param.type === "string";
+};
+
+export const isFloatParameter = (
+  param: GhostParameter
+): param is GhostParameter<"float"> => {
+  return param.type === "float";
 };
