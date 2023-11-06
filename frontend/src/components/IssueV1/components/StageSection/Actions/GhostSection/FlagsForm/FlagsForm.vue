7cbf304057be625f291e1189ae76a8a22fc7d543
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-06 13:31:25 +0800

    chore: update gh-ost params UI (#9174)
    Former-commit-id: 433973bd55d4925572ccbcf76dbffab350fbdbea

--- frontend/src/components/IssueV1/components/StageSection/Actions/GhostSection/FlagsForm/FlagsForm.vue
+++ frontend/src/components/IssueV1/components/StageSection/Actions/GhostSection/FlagsForm/FlagsForm.vue
@@ -13,23 +13,34 @@
         <NInput
           v-if="param.type === 'int'"
           size="small"
-          :value="getIntValue(param.key)"
+          :value="getIntValue(param)"
           :allow-input="onlyAllowNumber"
           :disabled="readonly"
-          @update:value="setIntValue(param.key, parseInt($event, 10))"
+          :placeholder="param.defaults"
+          @update:value="setIntValue(param, parseInt($event, 10))"
         />
-        <NCheckbox
-          v-if="param.type === 'bool'"
-          :checked="getBoolValue(param.key)"
+        <NInput
+          v-if="param.type === 'float'"
+          size="small"
+          :value="getFloatValue(param)"
           :disabled="readonly"
-          @update:checked="setBoolValue(param.key, $event)"
+          :placeholder="param.defaults"
+          @update:value="setFloatValue(param, $event)"
+        />
+        <BoolFlag
+          v-if="param.type === 'bool'"
+          :value="getBoolValue(param)"
+          :defaults="param.defaults === 'true'"
+          :readonly="readonly"
+          @update:value="setBoolValue(param, $event)"
         />
         <NInput
           v-if="param.type === 'string'"
           size="small"
-          :value="getStringValue(param.key)"
+          :value="getStringValue(param)"
           :disabled="readonly"
-          @update:value="setStringValue(param.key, $event)"
+          :placeholder="param.defaults"
+          @update:value="setStringValue(param, $event)"
         />
       </div>
     </div>
@@ -37,9 +48,10 @@
 </template>
 
 <script lang="ts" setup>
-import { NCheckbox, NInput } from "naive-ui";
+import { NInput } from "naive-ui";
 import { onlyAllowNumber } from "@/utils";
-import { SupportedGhostParameters } from "./constants";
+import BoolFlag from "./BoolFlag.vue";
+import { GhostParameter, SupportedGhostParameters } from "./constants";
 
 const props = defineProps<{
   flags: Record<string, string>;
@@ -49,29 +61,36 @@ const emit = defineEmits<{
   (event: "update:flags", flags: Record<string, string>): void;
 }>();
 
-const getBoolValue = (key: string) => {
-  return props.flags[key] === "true";
+const getBoolValue = (param: GhostParameter<"bool">) => {
+  const { key, defaults: value } = param;
+  if (props.flags[key] === "true") return true;
+  if (props.flags[key] === "false") return false;
+  return value === "true";
 };
-const getStringValue = (key: string) => {
-  return props.flags[key] ?? "";
+const getStringValue = (param: GhostParameter<"string">) => {
+  return props.flags[param.key] ?? "";
 };
-const getIntValue = (key: string) => {
-  const intVal = parseInt(props.flags[key], 10);
+const getIntValue = (param: GhostParameter<"int">) => {
+  const intVal = parseInt(props.flags[param.key], 10);
   if (Number.isNaN(intVal)) return undefined;
   return String(intVal);
 };
+const getFloatValue = (param: GhostParameter<"float">) => {
+  return props.flags[param.key];
+};
 
-const setBoolValue = (key: string, value: boolean) => {
+const setBoolValue = (param: GhostParameter, value: boolean) => {
+  const { key } = param;
   const updated = { ...props.flags };
-  if (value) {
-    updated[key] = "true";
-  } else {
+  updated[key] = value ? "true" : "false";
+  if (updated[key] === param.defaults) {
     delete updated[key];
   }
   emit("update:flags", updated);
 };
-const setStringValue = (key: string, value: string) => {
+const setStringValue = (param: GhostParameter<"string">, value: string) => {
   const updated = { ...props.flags };
+  const { key } = param;
   value = value.trim();
   if (value) {
     updated[key] = value;
@@ -80,11 +99,22 @@ const setStringValue = (key: string, value: string) => {
   }
   emit("update:flags", updated);
 };
-const setIntValue = (key: string, value: number) => {
+const setIntValue = (param: GhostParameter<"int">, value: number) => {
   const updated = { ...props.flags };
-
+  const { key } = param;
   if (!Number.isNaN(value)) {
     updated[key] = value.toString();
+  } else {
+    delete updated[key];
+  }
+  emit("update:flags", updated);
+};
+const setFloatValue = (param: GhostParameter<"float">, value: string) => {
+  const updated = { ...props.flags };
+  const { key } = param;
+  value = value.trim();
+  if (value !== "") {
+    updated[key] = value;
   } else {
     delete updated[key];
   }
