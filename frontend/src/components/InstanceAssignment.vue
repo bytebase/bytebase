864e71364f02c51b1e2187a7bb708e1ed9c4891a
ecmadao <wlec@outlook.com>
2023-11-06 10:57:47 +0800

    chore: update

--- frontend/src/components/InstanceAssignment.vue
+++ frontend/src/components/InstanceAssignment.vue
@@ -105,16 +105,9 @@
               />
             </BBTableCell>
             <BBTableCell class="bb-grid-cell">
-              <NTooltip>
-                <template #trigger>
-                  <div class="w-10 truncate">
-                    {{ hostPortOfInstanceV1(instance) }}
-                  </div>
-                </template>
-                <span class="text-sm">
-                  {{ hostPortOfInstanceV1(instance) }}
-                </span>
-              </NTooltip>
+              <EllipsisText class="w-10">
+                {{ hostPortOfInstanceV1(instance) }}
+              </EllipsisText>
             </BBTableCell>
           </template>
         </BBTable>
@@ -150,6 +143,7 @@ import { storeToRefs } from "pinia";
 import { reactive, computed, watchEffect, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import type { BBTableColumn, BBTableSectionDataSource } from "@/bbkit/types";
+import EllipsisText from "@/components/EllipsisText.vue";
 import { EnvironmentV1Name, InstanceV1EngineIcon } from "@/components/v2";
 import { Drawer, DrawerContent } from "@/components/v2";
 import {
