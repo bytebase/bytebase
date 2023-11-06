1bd1feb08e59b9badfffbde04ea879eead18b144
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-06 15:48:27 +0800

    chore: feature guard (#9180)
    Former-commit-id: 925f0af761ad4376c2578f44e3d8eeee0ca35f82

--- frontend/src/components/IssueV1/components/StageSection/Actions/GhostSection/GhostSection.vue
+++ frontend/src/components/IssueV1/components/StageSection/Actions/GhostSection/GhostSection.vue
@@ -3,39 +3,57 @@
     v-if="shouldShowGhostSection"
     class="flex items-center gap-x-3 min-h-[34px]"
   >
-    <NTooltip>
-      <template #trigger>
-        <div class="textlabel flex items-center">
+    <div class="textlabel flex items-center gap-x-1">
+      <NTooltip>
+        <template #trigger>
           {{ $t("task.online-migration.self") }}
-        </div>
-      </template>
-      <template #default>
-        <i18n-t
-          tag="p"
-          keypath="issue.migration-mode.online.description"
-          class="whitespace-pre-line max-w-[20rem]"
-        >
-          <template #link>
-            <LearnMoreLink
-              url="https://www.bytebase.com/docs/change-database/online-schema-migration-for-mysql"
-              color="light"
-            />
-          </template>
-        </i18n-t>
-      </template>
-    </NTooltip>
+        </template>
+        <template #default>
+          <i18n-t
+            tag="p"
+            keypath="issue.migration-mode.online.description"
+            class="whitespace-pre-line max-w-[20rem]"
+          >
+            <template #link>
+              <LearnMoreLink
+                url="https://www.bytebase.com/docs/change-database/online-schema-migration-for-mysql"
+                color="light"
+              />
+            </template>
+          </i18n-t>
+        </template>
+      </NTooltip>
+      <FeatureBadge feature="bb.feature.online-migration" />
+      <FeatureBadgeForInstanceLicense
+        feature="bb.feature.online-migration"
+        :show="hasOnlineMigrationFeature"
+        :instance="instance"
+      >
+        <LockIcon class="w-4 h-4 text-accent" />
+      </FeatureBadgeForInstanceLicense>
+    </div>
+
     <div class="w-[12rem] flex items-center gap-x-2">
       <GhostSwitch v-if="isCreating" />
       <GhostConfigButton v-if="viewType === 'ON'" />
     </div>
 
     <GhostFlagsPanel />
+
+    <FeatureModal
+      :open="showFeatureModal"
+      feature="bb.feature.online-migration"
+      @cancel="showFeatureModal = false"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
+import { LockIcon } from "lucide-vue-next";
 import { computed } from "vue";
-import { useIssueContext } from "@/components/IssueV1/logic";
+import FeatureBadgeForInstanceLicense from "@/components/FeatureGuard/FeatureBadgeForInstanceLicense.vue";
+import { databaseForTask, useIssueContext } from "@/components/IssueV1/logic";
+import { featureToRef } from "@/store";
 import { flattenTaskV1List } from "@/utils";
 import GhostConfigButton from "./GhostConfigButton.vue";
 import GhostFlagsPanel from "./GhostFlagsPanel.vue";
@@ -46,9 +64,9 @@ import {
   provideIssueGhostContext,
 } from "./common";
 
-const { isCreating, issue } = useIssueContext();
+const { isCreating, issue, selectedTask: task } = useIssueContext();
 
-const { viewType } = provideIssueGhostContext();
+const { viewType, showFeatureModal } = provideIssueGhostContext();
 
 const shouldShowGhostSection = computed(() => {
   // We need all tasks and specs to be gh-ost-able to enable gh-ost mode.
@@ -64,5 +82,11 @@ const shouldShowGhostSection = computed(() => {
   } else {
     return viewType.value === "ON";
   }
+});
+
+const hasOnlineMigrationFeature = featureToRef("bb.feature.online-migration");
+
+const instance = computed(() => {
+  return databaseForTask(issue.value, task.value).instanceEntity;
 });
 </script>
