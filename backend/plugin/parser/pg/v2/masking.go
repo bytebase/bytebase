cb8e4868a1c25c5130d6887430fca92fb97394c2
h3n4l <oysterdays@gmail.com>
2023-11-06 14:48:01 +0800

    feat: introduce querySpanExtractor

--- backend/plugin/parser/pg/v2/masking.go
+++ backend/plugin/parser/pg/v2/masking.go
@@ -3,19 +3,38 @@ package v2
 import (
 	"context"
 
+	pgquery "github.com/pganalyze/pg_query_go/v4"
+
 	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+	"github.com/bytebase/bytebase/backend/store/model"
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 )
 
 const (
 	// defaultSchemaName is the default schema name in PostgreSQL like DBMS.
 	defaultSchemaName = "public"
+	// unknownFieldName is the default field name for unknown field in PostgreSQL like DBMS.
+	unknownFieldName = "?column?"
 )
 
 func init() {
 	base.RegisterGetQuerySpan(storepb.Engine_POSTGRES, GetQuerySpan)
 	base.RegisterGetQuerySpan(storepb.Engine_REDSHIFT, GetQuerySpan)
 	base.RegisterGetQuerySpan(storepb.Engine_RISINGWAVE, GetQuerySpan)
+}
+
+// querySpanExtractor is the extractor to extract the query span from the given pgquery.RawStmt.
+type querySpanExtractor struct {
+	databaseMetadata *model.DatabaseMetadata
+	ast              *pgquery.RawStmt
+}
+
+// newQuerySpanExtractor creates a new query span extractor, the databaseMetadata and the ast are in the read guard.
+func newQuerySpanExtractor(databaseMetadata *model.DatabaseMetadata, ast *pgquery.RawStmt) *querySpanExtractor {
+	return &querySpanExtractor{
+		databaseMetadata: databaseMetadata,
+		ast:              ast,
+	}
 }
 
 // GetQuerySpan returns the query span for the given statement.
