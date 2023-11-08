acd99634137f2e4cf5433d42bce8ff7f46fea1dc
zll600 <71714656+zll600@users.noreply.github.com>
2023-11-08 17:40:26 +0800

    chore: migrate statement_affected_row_limit (#9035)

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1530,6 +1530,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleStatementAffectedRowLimit:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLStatementAffectedRowLimit, nil
 		case storepb.Engine_MYSQL, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLStatementAffectedRowLimit, nil
 		case storepb.Engine_POSTGRES:
