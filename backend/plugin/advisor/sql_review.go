2455f0dd665808d2122ff7e59f484ed6985a3e45
zll600 <3400692417@qq.com>
2023-11-08 16:03:49 +0800

    chore: migrate column_require_default

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1296,6 +1296,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleColumnRequireDefault:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLRequireColumnDefault, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLRequireColumnDefault, nil
 		case storepb.Engine_POSTGRES:
