6f2a65308902e1846aa2682a0ab1e63f869f0897
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-08 11:02:33 +0800

    feat(sql-editor): allow specify row count for exports from sql editor (#9208)
    * chore: allow to specify export row count
    
    * chore: add error tips to confirm button
    
    * chore: extract logic
    
    * Update frontend/src/components/IssueV1/components/StageSection/Actions/GhostSection/common.ts
    
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    
    ---------
    
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    Former-commit-id: 2a6cd1999050ab172e6b54007c385c209c9a9811

--- frontend/src/components/DataExportButton.vue
+++ frontend/src/components/DataExportButton.vue
@@ -2,13 +2,15 @@
   <NDropdown
     trigger="hover"
     :options="exportDropdownOptions"
-    @select="doExport"
+    :disabled="viewMode === 'DRAWER'"
+    @select="tryExportViaDropdown"
   >
     <NButton
       :quaternary="size === 'tiny'"
       :size="size"
       :loading="state.isRequesting"
       :disabled="state.isRequesting || disabled"
+      @click="handleClickExportButton"
     >
       <template #icon>
         <heroicons-outline:download class="h-5 w-5" />
@@ -18,45 +20,149 @@
       </span>
     </NButton>
   </NDropdown>
+
+  <Drawer v-if="viewMode === 'DRAWER'" v-model:show="state.showDrawer">
+    <DrawerContent
+      :title="$t('export-data.self')"
+      class="w-[30rem] max-w-[100vw] relative"
+    >
+      <template #default>
+        <NForm
+          ref="formRef"
+          :model="formData"
+          :rules="rules"
+          label-placement="left"
+        >
+          <NFormItem path="limit" :label="$t('export-data.export-rows')">
+            <NInputNumber
+              ref="limitInputRef"
+              v-model:value="formData.limit"
+              @keydown.enter.prevent
+            />
+          </NFormItem>
+          <NFormItem path="format" :label="$t('export-data.export-format')">
+            <NRadioGroup v-model:value="formData.format">
+              <NRadio
+                v-for="format in supportFormats"
+                :key="format"
+                :value="format"
+              >
+                {{ exportFormatToJSON(format) }}
+              </NRadio>
+            </NRadioGroup>
+          </NFormItem>
+        </NForm>
+      </template>
+      <template #footer>
+        <div class="flex flex-row items-center justify-end gap-x-3">
+          <NButton @click="state.showDrawer = false">
+            {{ $t("common.cancel") }}
+          </NButton>
+          <ErrorTipsButton
+            :button-props="{
+              type: 'primary',
+              loading: state.isRequesting,
+              disabled: formErrors.length > 0 || state.isRequesting,
+            }"
+            :errors="formErrors"
+            @click="tryExportViaForm"
+          >
+            {{ $t("common.confirm") }}
+          </ErrorTipsButton>
+        </div>
+      </template>
+    </DrawerContent>
+  </Drawer>
 </template>
 
 <script lang="ts" setup>
+import { asyncComputed } from "@vueuse/core";
 import dayjs from "dayjs";
-import { NButton, NDropdown } from "naive-ui";
+import {
+  FormInst,
+  FormRules,
+  InputNumberInst,
+  NButton,
+  NDropdown,
+  NForm,
+  NFormItem,
+  NInputNumber,
+  NRadio,
+  NRadioGroup,
+} from "naive-ui";
 import { BinaryLike } from "node:crypto";
-import { computed, reactive } from "vue";
+import { computed, reactive, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { pushNotification } from "@/store";
 import { ExportFormat, exportFormatToJSON } from "@/types/proto/v1/common";
+import { isNullOrUndefined } from "@/utils";
+import { ErrorTipsButton } from "./v2";
 
 interface LocalState {
   isRequesting: boolean;
+  showDrawer: boolean;
 }
-
+interface FormData {
+  limit: number;
+  format: ExportFormat;
+}
 const props = withDefaults(
   defineProps<{
     size?: "small" | "tiny" | "medium" | "large";
     disabled?: boolean;
     supportFormats: ExportFormat[];
+    allowSpecifyRowCount?: boolean;
   }>(),
   {
     size: "small",
     disabled: false,
+    allowSpecifyRowCount: false,
   }
 );
+const defaultFormData = (): FormData => ({
+  limit: 1000,
+  format: props.supportFormats[0],
+});
 
 const emit = defineEmits<{
   (
     event: "export",
     format: ExportFormat,
-    download: (content: BinaryLike | Blob, format: ExportFormat) => void
+    download: (content: BinaryLike | Blob, format: ExportFormat) => void,
+    limit: number | undefined // number if allowSpecifyRowCount, undefined otherwise
   ): Promise<void>;
 }>();
 
 const { t } = useI18n();
 const state = reactive<LocalState>({
   isRequesting: false,
+  showDrawer: false,
 });
+const formRef = ref<FormInst>();
+const limitInputRef = ref<InputNumberInst>();
+const formData = ref<FormData>(defaultFormData());
+
+const viewMode = computed(() => {
+  return props.allowSpecifyRowCount ? "DRAWER" : "DROPDOWN";
+});
+
+const rules: FormRules = {
+  limit: [
+    {
+      required: true,
+      validator: (rule, value: number) => {
+        if (isNullOrUndefined(value)) {
+          return new Error(t("export-data.error.export-rows-required"));
+        }
+        if (value <= 0) {
+          return new Error(t("export-data.error.export-rows-must-gt-zero"));
+        }
+        return true;
+      },
+      trigger: ["input", "blur"],
+    },
+  ],
+};
 
 const exportDropdownOptions = computed(() => {
   return props.supportFormats.map((format) => ({
@@ -67,7 +173,39 @@ const exportDropdownOptions = computed(() => {
   }));
 });
 
-const doExport = async (format: ExportFormat) => {
+const tryExportViaDropdown = async (format: ExportFormat) => {
+  doExport(format, undefined);
+};
+const tryExportViaForm = async (e: MouseEvent) => {
+  e.preventDefault();
+  formRef.value?.validate((errors) => {
+    if (errors) return;
+    doExport(formData.value.format, formData.value.limit);
+  });
+};
+const formErrors = asyncComputed(() => {
+  if (!formRef.value) return [];
+  try {
+    return new Promise<string[]>((resolve) => {
+      formRef.value!.validate((errors) => {
+        resolve(
+          errors?.flatMap((err) => err.map((e) => e.message ?? "")) ?? []
+        );
+      });
+    });
+  } catch {
+    return [];
+  }
+}, []);
+
+const handleClickExportButton = (e: MouseEvent) => {
+  e.preventDefault();
+  if (viewMode.value === "DROPDOWN") return;
+
+  state.showDrawer = true;
+};
+
+const doExport = async (format: ExportFormat, limit: number | undefined) => {
   if (state.isRequesting) {
     return;
   }
@@ -75,7 +213,7 @@ const doExport = async (format: ExportFormat) => {
   state.isRequesting = true;
 
   try {
-    await emit("export", format, doDownload);
+    await emit("export", format, doDownload, limit);
   } catch (error) {
     pushNotification({
       module: "bytebase",
@@ -115,4 +253,14 @@ const doDownload = (content: BinaryLike | Blob, format: ExportFormat) => {
   link.href = url;
   link.click();
 };
+
+watch(
+  () => state.showDrawer,
+  (show) => {
+    if (show) {
+      formData.value = defaultFormData();
+    }
+  },
+  { immediate: true }
+);
 </script>
