fcc7938e0cc890cd400ffad66f8a6924003c6cb6
zll600 <3400692417@qq.com>
2023-11-08 14:35:11 +0800

    chore: update

--- backend/plugin/db/mysql/sync.go
+++ backend/plugin/db/mysql/sync.go
@@ -268,7 +268,7 @@ func (driver *Driver) SyncDBSchema(ctx context.Context) (*storepb.DatabaseSchema
 			}
 		} else {
 			// TODO(zp): refactor column default value.
-			if strings.Contains(extra, "auto_increment") || strings.Contains(extra, "AUTO_INCREMENT") {
+			if strings.Contains(strings.ToLower(extra), "auto_increment") {
 				column.DefaultValue = &storepb.ColumnMetadata_DefaultExpression{DefaultExpression: "auto_increment"}
 			}
 		}
