6434a86aa9f6ee4c0c38979a5f16bf90a256bd22
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-09 10:18:58 +0800

    chore: deployment config UI (#9231)

--- frontend/src/components/DeploymentConfigTool/DeploymentStage.vue
+++ frontend/src/components/DeploymentConfigTool/DeploymentStage.vue
@@ -3,72 +3,69 @@
   <div class="deployment-stage flex w-full relative">
     <div
       v-if="allowEdit"
-      class="reorder flex flex-col items-center justify-start pr-2 py-4"
+      class="reorder flex flex-col items-center justify-start pr-2 pb-4 pt-5"
     >
-      <button
-        v-if="index > 0"
-        class="text-control hover:text-control-hover"
-        @click="$emit('prev')"
-      >
+      <NButton v-if="index > 0" text @click="$emit('prev')">
         <heroicons-solid:arrow-circle-up class="w-6 h-6" />
-      </button>
-      <button
-        v-if="index < max - 1"
-        class="text-control hover:text-control-hover"
-        @click="$emit('next')"
-      >
+      </NButton>
+      <NButton v-if="index < max - 1" text @click="$emit('next')">
         <heroicons-solid:arrow-circle-down class="w-6 h-6" />
-      </button>
+      </NButton>
     </div>
     <div
-      class="main flex-1 space-y-2 w-full"
-      :class="[layout === 'compact' ? 'py-2' : 'py-4']"
+      class="main flex-1 flex flex-col gap-y-2 w-full overflow-x-hidden"
+      :class="[layout === 'compact' ? 'my-2' : 'my-4']"
     >
       <h3 v-if="showHeader">
         <template v-if="allowEdit">
-          <input
-            v-model="deployment.title"
-            type="text"
+          <NInput
+            v-model:value="deployment.title"
             :placeholder="$t('deployment-config.name-placeholder')"
-            class="rounded-md border-control-border focus:ring-control focus:border-control disabled:bg-gray-50 h-8 py-0 text-sm"
+            style="width: 14rem"
           />
         </template>
         <template v-else>
           {{ deployment.title }}
         </template>
       </h3>
-      <div class="space-y-2 overflow-hidden">
+      <div class="flex flex-col gap-y-2 overflow-hidden">
         <div
-          v-for="(selector, j) in deployment.spec?.labelSelector
-            ?.matchExpressions"
+          v-for="(selector, j) in selectors"
           :key="j"
-          class="flex content-start"
+          class="flex content-start overflow-hidden"
         >
-          <SelectorItem
+          <SelectorItemV2
             :editable="allowEdit"
             :selector="selector"
+            :selectors="selectors"
+            :index="j"
             :database-list="databaseList"
             @remove="removeSelector(selector)"
           />
         </div>
       </div>
-      <button v-if="allowEdit" class="btn-normal btn-add" @click="addSelector">
+      <NButton v-if="allowEdit" class="self-start" @click="addSelector">
         {{ $t("deployment-config.add-selector") }}
-      </button>
+      </NButton>
     </div>
 
-    <span
+    <NButton
       v-if="allowEdit"
-      class="absolute right-2 top-2 p-1 text-control cursor-pointer hover:bg-gray-200 rounded-sm"
+      class="absolute right-0 top-0"
+      quaternary
+      style="--n-padding: 10px"
       @click="$emit('remove')"
     >
-      <heroicons:solid:x class="w-4 h-4" />
-    </span>
+      <XIcon class="w-4 h-4" />
+    </NButton>
   </div>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, PropType } from "vue";
+<script lang="ts" setup>
+import { head, without } from "lodash-es";
+import { XIcon } from "lucide-vue-next";
+import { NButton, NInput } from "naive-ui";
+import { computed, PropType } from "vue";
 import { ComposedDatabase } from "@/types";
 import {
   LabelSelectorRequirement,
@@ -76,81 +73,76 @@ import {
   ScheduleDeployment,
 } from "@/types/proto/v1/project_service";
 import { getAvailableDeploymentConfigMatchSelectorKeyList } from "@/utils";
-import SelectorItem from "./SelectorItem.vue";
+import SelectorItemV2 from "./SelectorItemV2.vue";
 
-export default defineComponent({
-  name: "DeploymentStage",
-  components: { SelectorItem },
-  props: {
-    deployment: {
-      type: Object as PropType<ScheduleDeployment>,
-      required: true,
-    },
-    index: {
-      type: Number,
-      default: -1,
-    },
-    max: {
-      type: Number,
-      default: -1,
-    },
-    allowEdit: {
-      type: Boolean,
-      default: false,
-    },
-    databaseList: {
-      type: Array as PropType<ComposedDatabase[]>,
-      default: () => [],
-    },
-    showHeader: {
-      type: Boolean,
-      default: true,
-    },
-    layout: {
-      type: String as PropType<"default" | "compact">,
-      default: "default",
-    },
+const props = defineProps({
+  deployment: {
+    type: Object as PropType<ScheduleDeployment>,
+    required: true,
   },
-  emits: ["remove", "prev", "next"],
-  setup(props) {
-    const keys = computed(() => {
-      return getAvailableDeploymentConfigMatchSelectorKeyList(
-        props.databaseList,
-        true /* withVirtualLabelKeys */,
-        true /* sort */
-      );
-    });
-
-    const removeSelector = (selector: LabelSelectorRequirement) => {
-      const array =
-        props.deployment.spec?.labelSelector?.matchExpressions ?? [];
-      const index = array.indexOf(selector);
-      if (index >= 0) {
-        array.splice(index, 1);
-      }
-    };
-
-    const addSelector = () => {
-      const array =
-        props.deployment.spec?.labelSelector?.matchExpressions ?? [];
-      const rule: LabelSelectorRequirement = {
-        key: keys.value[0] ?? "",
-        operator: OperatorType.OPERATOR_TYPE_IN,
-        values: [],
-      };
-      array.push(rule);
-    };
-
-    return {
-      removeSelector,
-      addSelector,
-    };
+  index: {
+    type: Number,
+    default: -1,
+  },
+  max: {
+    type: Number,
+    default: -1,
+  },
+  allowEdit: {
+    type: Boolean,
+    default: false,
+  },
+  databaseList: {
+    type: Array as PropType<ComposedDatabase[]>,
+    default: () => [],
+  },
+  showHeader: {
+    type: Boolean,
+    default: true,
+  },
+  layout: {
+    type: String as PropType<"default" | "compact">,
+    default: "default",
   },
 });
-</script>
 
-<style scoped lang="postcss">
-.btn-add {
-  @apply py-1.5 !important;
-}
-</style>
+defineEmits<{
+  (event: "remove"): void;
+  (event: "prev"): void;
+  (event: "next"): void;
+}>();
+
+const selectors = computed(() => {
+  return props.deployment.spec?.labelSelector?.matchExpressions ?? [];
+});
+
+const keys = computed(() => {
+  return getAvailableDeploymentConfigMatchSelectorKeyList(
+    props.databaseList,
+    true /* withVirtualLabelKeys */,
+    true /* sort */
+  );
+});
+
+const removeSelector = (selector: LabelSelectorRequirement) => {
+  const index = selectors.value.indexOf(selector);
+  if (index >= 0) {
+    selectors.value.splice(index, 1);
+  }
+};
+
+const addSelector = () => {
+  const usedKeys = selectors.value.map((s) => s.key);
+  const unusedKeys = without(keys.value, ...usedKeys);
+  const labelKeys = without(keys.value, "environment");
+  const suggestedNextKey =
+    head(unusedKeys) ?? head(labelKeys) ?? head(keys.value) ?? "";
+
+  const rule: LabelSelectorRequirement = {
+    key: suggestedNextKey,
+    operator: OperatorType.OPERATOR_TYPE_IN,
+    values: [],
+  };
+  selectors.value.push(rule);
+};
+</script>
