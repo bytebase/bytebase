1b70a1b5f64bbd148aee134b7eacdd379f92603c
zll600 <71714656+zll600@users.noreply.github.com>
2023-11-10 14:18:45 +0800

    chore: migrate schema_backward_compatibility (#9255)
    * chore: migrate schema_backward_compatibility
    
    * chore: update
    
    Former-commit-id: 1f1b74609a6f8df484a79dfdec75bb622a02cbd1
    Former-commit-id: 847a25619bf0927b0e9104909fc37252d49e9827

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1067,6 +1067,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleSchemaBackwardCompatibility:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLMigrationCompatibility, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLMigrationCompatibility, nil
 		case storepb.Engine_POSTGRES:
