71511cc0694e66a4165cfa21713fc30d1cdd3637
zll600 <71714656+zll600@users.noreply.github.com>
2023-11-10 10:58:04 +0800

    chore: migrate column_set_default_for_not_null (#9241)
    Former-commit-id: fb5409d92513a4ad2cf278c1da8562a53c0fe265

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1213,6 +1213,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleColumnSetDefaultForNotNull:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLColumnSetDefaultForNotNull, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLColumnSetDefaultForNotNull, nil
 		}
