c6102be64a48a5cf74bdd582ae9652cce2daed27
ecmadao <wlec@outlook.com>
2023-11-10 11:13:16 +0800

    fix: add missing localization (#9254)

--- frontend/src/components/Icon/EngineIcon.vue
+++ frontend/src/components/Icon/EngineIcon.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts" setup>
 import { computed } from "vue";
+import { EngineIconPath } from "@/components/InstanceForm/constants";
 import { Engine } from "@/types/proto/v1/common";
 
 const props = defineProps<{
@@ -11,40 +12,7 @@ const props = defineProps<{
   customClass?: string;
 }>();
 
-const ICON_PATH_MAP = new Map([
-  [Engine.MYSQL, new URL("@/assets/db-mysql.png", import.meta.url).href],
-  [Engine.POSTGRES, new URL("@/assets/db-postgres.png", import.meta.url).href],
-  [Engine.TIDB, new URL("@/assets/db-tidb.png", import.meta.url).href],
-  [
-    Engine.SNOWFLAKE,
-    new URL("@/assets/db-snowflake.png", import.meta.url).href,
-  ],
-  [
-    Engine.CLICKHOUSE,
-    new URL("@/assets/db-clickhouse.png", import.meta.url).href,
-  ],
-  [Engine.MONGODB, new URL("@/assets/db-mongodb.png", import.meta.url).href],
-  [Engine.SPANNER, new URL("@/assets/db-spanner.png", import.meta.url).href],
-  [Engine.REDIS, new URL("@/assets/db-redis.png", import.meta.url).href],
-  [Engine.ORACLE, new URL("@/assets/db-oracle.svg", import.meta.url).href],
-  [Engine.DM, new URL("@/assets/db-dm.png", import.meta.url).href],
-  [Engine.MSSQL, new URL("@/assets/db-mssql.svg", import.meta.url).href],
-  [Engine.REDSHIFT, new URL("@/assets/db-redshift.svg", import.meta.url).href],
-  [Engine.MARIADB, new URL("@/assets/db-mariadb.png", import.meta.url).href],
-  [
-    Engine.OCEANBASE,
-    new URL("@/assets/db-oceanbase-mysql.svg", import.meta.url).href,
-  ],
-  [
-    Engine.OCEANBASE_ORACLE,
-    new URL("@/assets/db-oceanbase-oracle.svg", import.meta.url).href,
-  ],
-  [
-    Engine.RISINGWAVE,
-    new URL("@/assets/db-risingwave.png", import.meta.url).href,
-  ],
-]);
 const icon = computed(() => {
-  return ICON_PATH_MAP.get(props.engine);
+  return EngineIconPath[props.engine];
 });
 </script>
