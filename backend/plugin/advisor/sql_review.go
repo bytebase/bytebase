9bf93e942ac63ad4ad13d059d58f4aba97deac49
zll600 <3400692417@qq.com>
2023-11-10 16:35:11 +0800

    chore: migrate column_current_time_count_limit

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -1309,6 +1309,9 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 		}
 	case SchemaRuleCurrentTimeColumnCountLimit:
 		switch engine {
+		// only for mysqlwip test.
+		case storepb.Engine_ENGINE_UNSPECIFIED:
+			return MySQLCurrentTimeColumnCountLimit, nil
 		case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MARIADB, storepb.Engine_OCEANBASE:
 			return MySQLCurrentTimeColumnCountLimit, nil
 		}
