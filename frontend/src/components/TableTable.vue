f98c8739f92b35e506afbf8e0cc671bbc1f478da
ecmadao <wlec@outlook.com>
2023-11-15 11:11:07 +0800

    chore: database config must changed with the schema (#9341)
    * chore: database config must changed with the schema
    
    * fix: eslint

--- frontend/src/components/TableTable.vue
+++ frontend/src/components/TableTable.vue
@@ -83,7 +83,7 @@ import { useSettingV1Store } from "@/store/modules";
 import { ComposedDatabase } from "@/types";
 import { Engine } from "@/types/proto/v1/common";
 import { TableMetadata } from "@/types/proto/v1/database_service";
-import { bytesToString, isGhostTable, isDev } from "@/utils";
+import { bytesToString, isGhostTable } from "@/utils";
 import TableDetailDrawer from "./TableDetailDrawer.vue";
 
 type LocalState = {
@@ -145,8 +145,7 @@ const hasSchemaProperty = computed(() => {
 const hasClassificationProperty = computed(() => {
   return (
     (engine.value === Engine.MYSQL || engine.value === Engine.POSTGRES) &&
-    classificationConfig.value &&
-    isDev()
+    classificationConfig.value
   );
 });
 
