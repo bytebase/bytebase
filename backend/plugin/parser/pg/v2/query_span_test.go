e0211c0b5ff1532b5ecf5cc3f27896944c0802de
h3n4l <oysterdays@gmail.com>
2023-11-15 11:53:42 +0800

    feat(masking): introduce query span for PostgreSQL (#9323)
    * chore: fix typo
    
    * feat: introduce pgv2 package to implement the GetQuerySpan
    
    * feat: introduce querySpanExtractor
    
    * feat: implement the framework of get query span
    
    * chore: update
    
    * chore: add server field in column resource
    
    * chore: wip
    
    * feat: introduce TableSource interface
    
    * chore: wip
    
    * chore: wip
    
    * chore: update
    
    * feat: happy path
    
    * chore: wip
    
    * fix: do the fail open strategy to find pseudo table with empty table name
    
    * chore: add more test cases
    
    * feat: default column name
    
    * chore: extend the interface signature
    
    * feat: migrate postgresql access table check
    
    * chore: remove the magic type assertion code
    
    * refactor: using interface signature instead of type assertion
    
    * chore: update
    
    * chore: update
    
    * chore: fix linter
    
    * chore: update
    
    * chore: using v3
    
    * chore: using v2
    
    * chore: tweak interface

---
+++ backend/plugin/parser/pg/v2/query_span_test.go
@@ -0,0 +1,79 @@
+package v2
+
+import (
+	"context"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/encoding/protojson"
+	"gopkg.in/yaml.v2"
+
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+	"github.com/bytebase/bytebase/backend/store/model"
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+func TestGetQuerySpan(t *testing.T) {
+	type testCase struct {
+		Description       string `yaml:"description,omitempty"`
+		Statement         string `yaml:"statement,omitempty"`
+		ConnectedDatabase string `yaml:"connectedDatabase,omitempty"`
+		// Metadata is the protojson encoded storepb.DatabaseSchemaMetadata,
+		// if it's empty, we will use the defaultDatabaseMetadata.
+		Metadata  string          `yaml:"metadata,omitempty"`
+		QuerySpan *base.QuerySpan `yaml:"querySpan,omitempty"`
+	}
+
+	const (
+		record       = false
+		testDataPath = "testdata/query_span.yaml"
+	)
+
+	a := require.New(t)
+	yamlFile, err := os.Open(testDataPath)
+	a.NoError(err)
+
+	var testCases []testCase
+	byteValue, err := io.ReadAll(yamlFile)
+	a.NoError(err)
+	a.NoError(yamlFile.Close())
+	a.NoError(yaml.Unmarshal(byteValue, &testCases))
+
+	for i, tc := range testCases {
+		metadata := &storepb.DatabaseSchemaMetadata{}
+		a.NoError(protojson.Unmarshal([]byte(tc.Metadata), metadata))
+		databaseMetadataGetter := buildMockDatabaseMetadataGetter([]*storepb.DatabaseSchemaMetadata{metadata})
+		result, err := GetQuerySpan(context.TODO(), tc.Statement, tc.ConnectedDatabase, databaseMetadataGetter)
+		a.NoError(err)
+		if record {
+			testCases[i].QuerySpan = result
+		} else {
+			a.Equal(tc.QuerySpan, result, "statement: %s", tc.Statement)
+		}
+	}
+
+	if record {
+		byteValue, err := yaml.Marshal(testCases)
+		a.NoError(err)
+		err = os.WriteFile(testDataPath, byteValue, 0644)
+		a.NoError(err)
+	}
+}
+
+func buildMockDatabaseMetadataGetter(databaseMetadata []*storepb.DatabaseSchemaMetadata) base.GetDatabaseMetadataFunc {
+	return func(_ context.Context, databaseName string) (*model.DatabaseMetadata, error) {
+		m := make(map[string]*model.DatabaseMetadata)
+		for _, metadata := range databaseMetadata {
+			m[metadata.Name] = model.NewDatabaseMetadata(metadata)
+		}
+
+		if databaseMetadata, ok := m[databaseName]; ok {
+			return databaseMetadata, nil
+		}
+
+		return nil, errors.Errorf("database %q not found", databaseName)
+	}
+}
