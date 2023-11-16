fde1fa54ce8380b7016e040791749bddb8c63204
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-16 16:36:52 +0800

    chore: better auto scroll to

--- frontend/src/components/IssueV1/components/IssueSearch/ScopeMenu.vue
+++ frontend/src/components/IssueV1/components/IssueSearch/ScopeMenu.vue
@@ -1,38 +1,100 @@
 <template>
-  <div class="flex flex-col">
-    <div
-      v-for="(option, index) in options"
-      :key="option.id"
-      class="flex gap-x-1 px-3 py-1 items-center cursor-pointer hover:bg-gray-100"
-      :class="index === menuIndex && 'bg-gray-100'"
-      @mousedown.prevent.stop="onScopeSelect(option.id)"
+  <div
+    v-if="options.length > 0"
+    v-show="show"
+    class="overflow-hidden"
+    :style="{
+      'max-height': `${maxListHeight}px`,
+    }"
+  >
+    <VirtualList
+      ref="virtualListRef"
+      :items="options"
+      :key-field="`id`"
+      :item-resizable="false"
+      :item-size="32"
     >
-      <div class="space-x-1 text-sm">
-        <span class="text-accent">{{ option.id }}:</span>
-        <span class="text-control-light">{{ option.description }}</span>
-      </div>
-    </div>
-    <div v-if="options.length === 0" class="px-3 py-1 text-control text-sm">
-      N/A
-    </div>
+      <template #default="{ item: option, index }: ListItem">
+        <div
+          class="h-[32px] flex gap-x-1 px-3 items-center cursor-pointer border-t text-sm"
+          :class="[
+            index === menuIndex && 'bg-gray-100',
+            index === 0 ? 'border-transparent' : 'border-block-border',
+          ]"
+          :data-index="index"
+          :data-id="option.id"
+          @mouseenter.prevent.stop="$emit('hover-item', index)"
+          @mousedown.prevent.stop="$emit('select-scope', option.id)"
+        >
+          <span class="text-accent">{{ option.id }}:</span>
+          <span class="text-control-light">{{ option.description }}</span>
+        </div>
+      </template>
+    </VirtualList>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useElementBounding, useWindowSize } from "@vueuse/core";
+import { computed, nextTick, ref, watch } from "vue";
+import { VirtualList } from "vueuc";
 import { SearchParams, SearchScopeId } from "@/utils";
 import { ScopeOption } from "./useSearchScopeOptions";
 
-defineProps<{
-  inputText: string;
+type ListItem = {
+  item: ScopeOption;
+  index: number;
+};
+
+const props = defineProps<{
+  show: boolean;
   params: SearchParams;
   options: ScopeOption[];
   menuIndex: number;
 }>();
-const emit = defineEmits<{
+defineEmits<{
   (event: "select-scope", id: SearchScopeId): void;
+  (event: "hover-item", index: number): void;
 }>();
 
-const onScopeSelect = (id: SearchScopeId) => {
-  emit("select-scope", id);
-};
+const containerRef = ref<HTMLElement>();
+const virtualListRef = ref<InstanceType<typeof VirtualList>>();
+const { top: containerTop } = useElementBounding(containerRef);
+const { height: windowHeight } = useWindowSize();
+
+const maxListHeight = computed(() => {
+  const MAX_HEIGHT = 480;
+  const PADDING_BOTTOM = 16;
+
+  return Math.min(
+    MAX_HEIGHT,
+    windowHeight.value - containerTop.value - PADDING_BOTTOM
+  );
+});
+
+const highlightedItem = computed((): ListItem | undefined => {
+  if (!props.show) return undefined;
+  const { options, menuIndex: index } = props;
+  const item = options[index];
+  if (item) {
+    return { item, index };
+  }
+  return undefined;
+});
+
+watch(
+  [highlightedItem, () => props.show],
+  ([item, show]) => {
+    if (!show) return;
+    if (!item) return;
+    nextTick(() => {
+      const virtualList = virtualListRef.value;
+      if (!virtualList) return;
+      virtualList.scrollTo({
+        index: item.index,
+      });
+    });
+  },
+  { immediate: true }
+);
 </script>
--- frontend/src/components/IssueV1/components/IssueSearch/ValueMenu.vue
+++ frontend/src/components/IssueV1/components/IssueSearch/ValueMenu.vue
@@ -1,26 +1,41 @@
 <template>
-  <div v-if="scopeOption" class="flex flex-col">
+  <div
+    v-if="scopeOption"
+    v-show="show"
+    ref="containerRef"
+    class="flex flex-col overflow-hidden"
+    :data-top="containerTop"
+  >
     <div
       v-if="scopeOption.title"
-      class="px-3 pt-2 pb-1 text-sm text-control font-semibold"
+      ref="titleRef"
+      class="px-3 py-2 text-sm text-control font-semibold"
+      :data-height="titleHeight"
     >
       {{ scopeOption.title }}
     </div>
-    <div v-if="valueOptions.length > 0" class="max-h-60 overflow-hidden">
+    <div
+      v-if="valueOptions.length > 0"
+      class="flex-1 overflow-hidden"
+      :style="{
+        'max-height': `${maxListHeight}px`,
+      }"
+    >
       <VirtualList
         ref="virtualListRef"
         :items="valueOptions"
         :key-field="`value`"
         :item-resizable="false"
-        :item-size="28"
+        :item-size="32"
       >
         <template #default="{ item: option, index }: ListItem">
           <div
-            class="h-[28px] flex gap-x-2 px-3 py-1 items-center cursor-pointer hover:bg-gray-100 border-t border-block-border"
+            class="h-[32px] flex gap-x-2 px-3 items-center cursor-pointer border-t border-block-border"
             :class="[index === menuIndex && 'bg-gray-100']"
             :data-index="index"
             :data-value="option.value"
-            @mousedown.prevent.stop="onValueSelect(option.value)"
+            @mouseenter.prevent.stop="$emit('hover-item', index)"
+            @mousedown.prevent.stop="$emit('select-value', option.value)"
           >
             <component :is="option.render" class="text-control text-sm" />
             <span class="text-control-light text-sm">{{ option.value }}</span>
@@ -35,8 +50,13 @@
 </template>
 
 <script setup lang="ts">
+import {
+  useElementBounding,
+  useElementSize,
+  useWindowSize,
+} from "@vueuse/core";
 import { NEmpty } from "naive-ui";
-import { computed, ref, watch } from "vue";
+import { computed, nextTick, ref, watch } from "vue";
 import { VirtualList } from "vueuc";
 import { SearchParams } from "@/utils";
 import { ScopeOption, ValueOption } from "./useSearchScopeOptions";
@@ -47,22 +67,38 @@ type ListItem = {
 };
 
 const props = defineProps<{
+  show: boolean;
   params: SearchParams;
   scopeOption?: ScopeOption;
   valueOptions: ValueOption[];
   menuIndex: number;
 }>();
-const emit = defineEmits<{
+defineEmits<{
   (event: "select-value", value: string): void;
+  (event: "hover-item", index: number): void;
 }>();
 
+const containerRef = ref<HTMLElement>();
+const titleRef = ref<HTMLElement>();
 const virtualListRef = ref<InstanceType<typeof VirtualList>>();
+const { height: titleHeight } = useElementSize(titleRef, undefined, {
+  box: "border-box",
+});
+const { top: containerTop } = useElementBounding(containerRef);
+const { height: windowHeight } = useWindowSize();
 
-const onValueSelect = (value: string) => {
-  emit("select-value", value);
-};
+const maxListHeight = computed(() => {
+  const MAX_HEIGHT = 240;
+  const PADDING_BOTTOM = 16;
+
+  return Math.min(
+    MAX_HEIGHT,
+    windowHeight.value - titleHeight.value - containerTop.value - PADDING_BOTTOM
+  );
+});
 
 const highlightedItem = computed((): ListItem | undefined => {
+  if (props.show) return undefined;
   const options = props.valueOptions;
   const index = props.menuIndex;
   const item = options[index];
@@ -73,13 +109,16 @@ const highlightedItem = computed((): ListItem | undefined => {
 });
 
 watch(
-  highlightedItem,
-  (item) => {
+  [highlightedItem, () => props.show],
+  ([item, show]) => {
+    if (!show) return;
     if (!item) return;
-    const virtualList = virtualListRef.value;
-    if (!virtualList) return;
-    virtualList.scrollTo({
-      index: item.index,
+    nextTick(() => {
+      const virtualList = virtualListRef.value;
+      if (!virtualList) return;
+      virtualList.scrollTo({
+        index: item.index,
+      });
     });
   },
   { immediate: true }
