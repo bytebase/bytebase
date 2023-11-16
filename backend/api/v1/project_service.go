9e8069dcfe867a3f4489a8c2605532f76b6a59a6
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-16 09:38:43 +0800

    chore: deprecate search project (#9366)

--- backend/api/v1/project_service.go
+++ backend/api/v1/project_service.go
@@ -282,35 +282,6 @@ func (s *ProjectService) UndeleteProject(ctx context.Context, request *v1pb.Unde
 	return convertToProject(project), nil
 }
 
-// SearchProjects searches all projects that the caller have permission to.
-func (s *ProjectService) SearchProjects(ctx context.Context, _ *v1pb.SearchProjectsRequest) (*v1pb.SearchProjectsResponse, error) {
-	principalID, ok := ctx.Value(common.PrincipalIDContextKey).(int)
-	if !ok {
-		return nil, status.Errorf(codes.Internal, "principal ID not found")
-	}
-	role, ok := ctx.Value(common.RoleContextKey).(api.Role)
-	if !ok {
-		return nil, status.Errorf(codes.Internal, "role not found")
-	}
-
-	projects, err := s.store.ListProjectV2(ctx, &store.FindProjectMessage{})
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, err.Error())
-	}
-	response := &v1pb.SearchProjectsResponse{}
-	for _, project := range projects {
-		policy, err := s.store.GetProjectPolicy(ctx, &store.GetProjectPolicyMessage{ProjectID: &project.ResourceID})
-		if err != nil {
-			return nil, status.Errorf(codes.Internal, err.Error())
-		}
-		if !isOwnerOrDBA(role) && !isProjectMember(principalID, policy) {
-			continue
-		}
-		response.Projects = append(response.Projects, convertToProject(project))
-	}
-	return response, nil
-}
-
 // GetIamPolicy returns the IAM policy for a project.
 func (s *ProjectService) GetIamPolicy(ctx context.Context, request *v1pb.GetIamPolicyRequest) (*v1pb.IamPolicy, error) {
 	projectID, err := common.GetProjectID(request.Project)
