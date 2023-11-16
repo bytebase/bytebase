269dbf3e6002439ce672b12d43e6a9bd78ce80e1
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-16 15:38:44 +0800

    chore: virtual list

--- frontend/src/components/IssueV1/components/IssueSearch/ValueMenu.vue
+++ frontend/src/components/IssueV1/components/IssueSearch/ValueMenu.vue
@@ -6,32 +6,47 @@
     >
       {{ scopeOption.title }}
     </div>
-    <div class="max-h-60 overflow-y-auto divide-block-border">
-      <div
-        v-for="(option, index) in valueOptions"
-        :key="option.value"
-        class="flex gap-x-2 px-3 py-1 items-center cursor-pointer hover:bg-gray-100"
-        :class="index === menuIndex && 'bg-gray-100'"
-        @mousedown.prevent.stop="onValueSelect(option.value)"
+    <div v-if="valueOptions.length > 0" class="max-h-60 overflow-hidden">
+      <VirtualList
+        ref="virtualListRef"
+        :items="valueOptions"
+        :key-field="`value`"
+        :item-resizable="false"
+        :item-size="28"
       >
-        <component :is="option.render" class="text-control text-sm" />
-        <span class="text-control-light text-sm">{{ option.value }}</span>
-      </div>
-      <div
-        v-if="valueOptions.length === 0"
-        class="px-3 py-1 text-control text-sm"
-      >
-        N/A
-      </div>
+        <template #default="{ item: option, index }: ListItem">
+          <div
+            class="h-[28px] flex gap-x-2 px-3 py-1 items-center cursor-pointer hover:bg-gray-100 border-t border-block-border"
+            :class="[index === menuIndex && 'bg-gray-100']"
+            :data-index="index"
+            :data-value="option.value"
+            @mousedown.prevent.stop="onValueSelect(option.value)"
+          >
+            <component :is="option.render" class="text-control text-sm" />
+            <span class="text-control-light text-sm">{{ option.value }}</span>
+          </div>
+        </template>
+      </VirtualList>
+    </div>
+    <div v-if="valueOptions.length === 0" class="pb-2">
+      <NEmpty />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { NEmpty } from "naive-ui";
+import { computed, ref, watch } from "vue";
+import { VirtualList } from "vueuc";
 import { SearchParams } from "@/utils";
 import { ScopeOption, ValueOption } from "./useSearchScopeOptions";
 
-defineProps<{
+type ListItem = {
+  item: ValueOption;
+  index: number;
+};
+
+const props = defineProps<{
   params: SearchParams;
   scopeOption?: ScopeOption;
   valueOptions: ValueOption[];
@@ -41,7 +56,32 @@ const emit = defineEmits<{
   (event: "select-value", value: string): void;
 }>();
 
+const virtualListRef = ref<InstanceType<typeof VirtualList>>();
+
 const onValueSelect = (value: string) => {
   emit("select-value", value);
 };
+
+const highlightedItem = computed((): ListItem | undefined => {
+  const options = props.valueOptions;
+  const index = props.menuIndex;
+  const item = options[index];
+  if (item) {
+    return { item, index };
+  }
+  return undefined;
+});
+
+watch(
+  highlightedItem,
+  (item) => {
+    if (!item) return;
+    const virtualList = virtualListRef.value;
+    if (!virtualList) return;
+    virtualList.scrollTo({
+      index: item.index,
+    });
+  },
+  { immediate: true }
+);
 </script>
