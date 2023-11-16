fbb7310e72a35b007e4c6aa7820e8307d92fe590
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-16 18:42:42 +0800

    feat(frontend): advanced search renew (#9382)
    * chore: fix advanced search box jitters
    
    * chore: reinvent AdvancedSearchBox
    
    * chore: scroll tag into view if needed
    
    * feat: backspace key to delete the last scope tag
    
    * chore: virtual list
    
    * chore: better backspace key events
    
    * chore: hover events
    
    * chore: better auto scroll to
    
    * chore: use match-sorter and improve style
    
    * chore: style
    
    * chore: remove debug
    
    * chore: minor updates
    
    * chore: placeholder
    
    * chore: hide menu on blur
    
    * chore: handle unsupported scopes
    
    * chore: don't show two overlays
    
    * chore: avoid but
    
    * chore: style
    
    Former-commit-id: 408739ab1b069790b30076eb6d0a6b86848644d4

---
+++ frontend/src/components/misc/SystemBotTag.vue
@@ -0,0 +1,7 @@
+<template>
+  <span
+    class="inline-flex items-center px-2 py-0.5 rounded-lg text-xs font-semibold bg-green-100 text-green-800"
+  >
+    {{ $t("settings.members.system-bot") }}
+  </span>
+</template>
