5f2677623a4ecfa513cf5d6b6d6049ae4a2f560f
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-18 01:23:53 +0800

    chore: show hover tooltip for project role description (#9412)
    * chore: show hover tooltip for role description
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: cb63ce3fd2b73cabac06f5c980063b592ba10e45
    Former-commit-id: b5efab8f60589dfcedd6abdec5b246a216042206

--- frontend/src/components/Role/Setting/components/RoleTableRow.vue
+++ frontend/src/components/Role/Setting/components/RoleTableRow.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bb-grid-cell whitespace-nowrap">
-    {{ title }}
+    {{ displayRoleTitle(role.name) }}
     <SystemLabel v-if="!isCustomRole(props.role.name)" class="ml-1" />
   </div>
 
   <div class="bb-grid-cell">
-    {{ description }}
+    {{ displayRoleDescription(role.name) }}
   </div>
   <div class="bb-grid-cell gap-x-1">
     <template v-if="isCustomRole(props.role.name)">
@@ -26,14 +26,16 @@
 
 <script lang="ts" setup>
 import { NButton } from "naive-ui";
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
 import SystemLabel from "@/components/SystemLabel.vue";
 import { SpinnerButton } from "@/components/v2";
 import { useRoleStore } from "@/store";
-import { PresetRoleType, isCustomRole } from "@/types";
+import { isCustomRole } from "@/types";
 import type { Role } from "@/types/proto/v1/role_service";
-import { useWorkspacePermissionV1 } from "@/utils";
+import {
+  displayRoleDescription,
+  displayRoleTitle,
+  useWorkspacePermissionV1,
+} from "@/utils";
 import { useCustomRoleSettingContext } from "../context";
 
 const props = defineProps<{
@@ -44,55 +46,8 @@ defineEmits<{
   (event: "edit", role: Role): void;
 }>();
 
-const { t } = useI18n();
 const { hasCustomRoleFeature, showFeatureModal } =
   useCustomRoleSettingContext();
-
-const description = computed(() => {
-  const { role } = props;
-  if (role.name === PresetRoleType.OWNER) {
-    return t("role.owner.description");
-  }
-  if (role.name === PresetRoleType.DEVELOPER) {
-    return t("role.developer.description");
-  }
-  if (role.name === PresetRoleType.EXPORTER) {
-    return t("role.exporter.description");
-  }
-  if (role.name === PresetRoleType.QUERIER) {
-    return t("role.querier.description");
-  }
-  if (role.name === PresetRoleType.RELEASER) {
-    return t("role.releaser.description");
-  }
-  if (role.name === PresetRoleType.VIEWER) {
-    return t("role.viewer.description");
-  }
-  return role.description;
-});
-
-const title = computed(() => {
-  const { role } = props;
-  if (role.name === PresetRoleType.OWNER) {
-    return t("common.role.owner");
-  }
-  if (role.name === PresetRoleType.DEVELOPER) {
-    return t("common.role.developer");
-  }
-  if (role.name === PresetRoleType.EXPORTER) {
-    return t("common.role.exporter");
-  }
-  if (role.name === PresetRoleType.QUERIER) {
-    return t("common.role.querier");
-  }
-  if (role.name === PresetRoleType.RELEASER) {
-    return t("common.role.releaser");
-  }
-  if (role.name === PresetRoleType.VIEWER) {
-    return t("common.role.viewer");
-  }
-  return role.title;
-});
 
 const allowAdmin = useWorkspacePermissionV1(
   "bb.permission.workspace.manage-general"
