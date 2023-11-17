82e27e1a527aa808b3d1e35ac0f3b908e414f531
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-17 10:20:27 +0800

    chore: remove some unused jsonapi code (#9389)
    * chore: remove some unused jsonapi code
    
    * chore: update
    
    * chore: update

--- frontend/src/types/jsonapi/index.ts
+++ frontend/src/types/jsonapi/index.ts
@@ -1,6 +1,2 @@
 // Based on https://github.com/rmarganti/json-api
-export * from "./request";
 export * from "./resourceObject";
-export * from "./response";
-export * from "./shared";
-export * from "./pagedResponse";
