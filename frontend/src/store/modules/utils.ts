b6f61c8902a3ea27571707b52e0ac88be1feb6e6
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-17 10:20:27 +0800

    chore: remove some unused jsonapi code (#9389)
    * chore: remove some unused jsonapi code
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: e32216a2ce2cd78875ce37ed63aec42cf25c9bbe
    Former-commit-id: 67a48b0b66ee2a552a6bf2f9f557faaeaa7e7e2d

--- frontend/src/store/modules/utils.ts
+++ frontend/src/store/modules/utils.ts
@@ -1,49 +1,7 @@
 import { ClientError } from "nice-grpc-web";
 import { t } from "@/plugins/i18n";
-import {
-  isPagedResponse,
-  PagedResponse,
-  ResourceIdentifier,
-  ResourceObject,
-  ResponseWithData,
-} from "@/types";
 import { extractGrpcErrorMessage } from "@/utils/grpcweb";
 import { pushNotification } from "./notification";
-
-type ConvertEntityFn<T> = (
-  data: ResourceObject,
-  includedList: ResourceObject[]
-) => T;
-
-type ConvertEntityFromIncludedListFn<T> = (
-  date:
-    | ResourceIdentifier<ResourceObject>
-    | ResourceIdentifier<ResourceObject>[]
-    | undefined,
-  includedList: ResourceObject[]
-) => T;
-
-// convert entity list from response data
-// works for normal array response data and paged response data
-export function convertEntityList<T, K extends string>(
-  responseData: ResponseWithData | PagedResponse<K>,
-  key: K,
-  convert: ConvertEntityFn<T>,
-  convertFromIncludedList: ConvertEntityFromIncludedListFn<T>
-) {
-  if (isPagedResponse(responseData, key)) {
-    const resourceIdentifierList =
-      responseData.data.relationships?.[key].data ?? [];
-    return resourceIdentifierList.map((item) => {
-      return convertFromIncludedList(item, responseData.included ?? []);
-    });
-  }
-
-  const resourceList = responseData.data as ResourceObject[];
-  return resourceList.map((obj: ResourceObject) => {
-    return convert(obj, responseData.included ?? []);
-  });
-}
 
 export const useGracefulRequest = async <T>(
   fn: () => Promise<T>
