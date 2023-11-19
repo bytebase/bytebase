527c70977fdca55cc62f2cb3baa79992a00710ac
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-19 21:01:11 +0800

    chore: update assignee options (#9419)
    Former-commit-id: 8dce622efb45d5eea103b8f1092c80444ffa4c7c

--- frontend/src/components/IssueV1/components/HeaderSection/Assignee/Assignee.vue
+++ frontend/src/components/IssueV1/components/HeaderSection/Assignee/Assignee.vue
@@ -181,13 +181,27 @@ const mapUserOptions = (users: User[]) => {
   };
 
   // Groups in order
+  // - current assignee
   // - approvers of current step (CI phase only)
   // - releasers of current stage (CD phase only)
   // - project owners
-  // - other project members
   // - other non-member assignee candidates
   //   - workspace owners
   //   - workspace DBAs
+  if (assigneeEmail.value) {
+    const assignee = userStore.getUserByEmail(assigneeEmail.value);
+    if (assignee) {
+      addGroup(
+        {
+          type: "group",
+          label: t("issue.assignee.current-assignee"),
+          key: "current-assignee",
+        },
+        [assignee]
+      );
+    }
+  }
+
   if (phase === "CI") {
     const steps = useWrappedReviewStepsV1(issue, reviewContext);
     const currentStep = steps.value?.find((step) => step.status === "CURRENT");
@@ -222,17 +236,6 @@ const mapUserOptions = (users: User[]) => {
       key: "project-owners",
     },
     projectOwners
-  );
-  const projectMembers = users.filter((user) =>
-    isMemberOfProjectV1(project.iamPolicy, user)
-  );
-  addGroup(
-    {
-      type: "group",
-      label: t("issue.assignee.project-members"),
-      key: "project-members",
-    },
-    projectMembers
   );
 
   // Add non-project members (workspace admins and DBAs)
