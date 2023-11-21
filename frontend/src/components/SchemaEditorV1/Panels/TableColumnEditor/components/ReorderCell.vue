d122e452bc402027b3dc5be209dd26d965a92296
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-21 16:05:21 +0800

    feat(schema-editor): allow reordering columns for newly created tables (#9460)
    * chore: lint
    
    * chore: reorder column
    
    * chore: for mysql and tidb only
    
    * chore: revert
    
    Former-commit-id: a464a824f6a77aee1fb2591ea04a0209764a49d9

---
+++ frontend/src/components/SchemaEditorV1/Panels/TableColumnEditor/components/ReorderCell.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-start items-center">
+    <MiniActionButton
+      :disabled="disabled || !allowMoveUp"
+      @click="$emit('reorder', -1)"
+    >
+      <ChevronUpIcon class="w-4 h-4" />
+    </MiniActionButton>
+    <MiniActionButton
+      :disabled="disabled || !allowMoveDown"
+      @click="$emit('reorder', 1)"
+    >
+      <ChevronDownIcon class="w-4 h-4" />
+    </MiniActionButton>
+  </div>
+</template>
+<script setup lang="ts">
+import { ChevronDownIcon, ChevronUpIcon } from "lucide-vue-next";
+import { MiniActionButton } from "@/components/v2";
+import { Column } from "@/types/v1/schemaEditor";
+
+defineProps<{
+  column: Column;
+  allowMoveUp?: boolean;
+  allowMoveDown?: boolean;
+  disabled?: boolean;
+}>();
+defineEmits<{
+  (event: "reorder", delta: -1 | 1): void;
+}>();
+</script>
