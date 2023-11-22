8fbaa30f230e4cd141c7cced5843c76c4a727840
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-22 21:59:19 +0800

    fix: sleepy mongo needs some rest (#9494)

--- backend/server/ultimate.go
+++ backend/server/ultimate.go
@@ -6,7 +6,6 @@ import (
 	// Drivers.
 	_ "github.com/bytebase/bytebase/backend/plugin/db/clickhouse"
 	_ "github.com/bytebase/bytebase/backend/plugin/db/dm"
-	_ "github.com/bytebase/bytebase/backend/plugin/db/mongodb"
 	_ "github.com/bytebase/bytebase/backend/plugin/db/mssql"
 	_ "github.com/bytebase/bytebase/backend/plugin/db/oracle"
 	_ "github.com/bytebase/bytebase/backend/plugin/db/redis"
