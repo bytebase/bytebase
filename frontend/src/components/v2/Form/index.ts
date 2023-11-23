67239214045cf7d18fb315b14afb5e12b5fdeb7d
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-23 12:55:43 +0800

    refactor: use naive-ui in environment page

--- frontend/src/components/v2/Form/index.ts
+++ frontend/src/components/v2/Form/index.ts
@@ -5,6 +5,7 @@ import ResourceIdField from "./ResourceIdField.vue";
 import SearchBox from "./SearchBox.vue";
 import SpinnerButton from "./SpinnerButton.vue";
 import SpinnerSwitch from "./SpinnerSwitch.vue";
+import Switch from "./Switch.vue";
 
 export * from "./types";
 export {
@@ -15,4 +16,5 @@ export {
   SpinnerButton,
   SearchBox,
   ResourceIdField,
+  Switch,
 };
