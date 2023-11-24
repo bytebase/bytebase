6bc312ae49e78d1dcbfee6e8a51d026504fff2ad
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-24 10:45:15 +0800

    chore(frontend): optimize base store initialization order (#9517)
    * chore: optimize basic store initialize order
    
    * chore: revert
    
    * chore: refactor
    
    Former-commit-id: 411bdc4d23910fbcc618de56e8f066253853a501
    Former-commit-id: eca6c01445f3cc7fe0c776926f0c016e412435d9

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -181,24 +181,25 @@ const initActuator = async () => {
 
   actuatorStore.fetchServerInfo();
 };
-const initSubscription = () => {
-  const subscriptionStore = useSubscriptionV1Store();
-  return subscriptionStore.fetchSubscription();
+const initSubscription = async () => {
+  await useSubscriptionV1Store().fetchSubscription();
 };
-const initFeatureMatrix = () => {
-  const subscriptionStore = useSubscriptionV1Store();
-  return subscriptionStore.fetchFeatureMatrix();
+const initFeatureMatrix = async () => {
+  await useSubscriptionV1Store().fetchFeatureMatrix();
 };
-const restoreUser = () => {
-  const authStore = useAuthStore();
-  return authStore.restoreUser();
+const restoreUser = async () => {
+  await useAuthStore().restoreUser();
 };
-Promise.all([
-  initActuator(),
-  initFeatureMatrix(),
-  initSubscription(),
-  restoreUser(),
-]).finally(() => {
+const initBasicModules = async () => {
+  await Promise.all([
+    initActuator(),
+    initFeatureMatrix(),
+    initSubscription(),
+    restoreUser(),
+  ]);
+};
+
+initBasicModules().finally(() => {
   // Install router after the necessary data fetching is complete.
   app.use(router).use(highlight).use(i18n).use(NaiveUI);
   app.mount("#app");
