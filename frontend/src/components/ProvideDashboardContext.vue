ab6c95ef907ce7fb428e449f4ad78bf5e95bdea7
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-24 10:45:15 +0800

    chore(frontend): optimize base store initialization order (#9517)
    * chore: optimize basic store initialize order
    
    * chore: revert
    
    * chore: refactor

--- frontend/src/components/ProvideDashboardContext.vue
+++ frontend/src/components/ProvideDashboardContext.vue
@@ -5,35 +5,21 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import {
-  useEnvironmentV1Store,
-  useRoleStore,
   useUIStateStore,
-  useUserStore,
-  useProjectV1Store,
-  usePolicyV1Store,
   useDatabaseV1Store,
   useDBGroupStore,
+  initCommonModelStores,
 } from "@/store";
-import { useInstanceV1Store } from "@/store/modules/v1/instance";
-import { useSettingV1Store } from "@/store/modules/v1/setting";
 
 export default defineComponent({
   name: "ProvideDashboardContext",
   async setup() {
-    await Promise.all([
-      useSettingV1Store().fetchSettingList(),
-      useRoleStore().fetchRoleList(),
-      useUserStore().fetchUserList(),
-      useEnvironmentV1Store().fetchEnvironments(),
-      useInstanceV1Store().fetchInstanceList(),
-      useProjectV1Store().fetchProjectList(true),
-    ]);
+    await initCommonModelStores();
     await Promise.all([
       useDatabaseV1Store().fetchDatabaseList({
         parent: "instances/-",
       }),
       useDBGroupStore().fetchAllDatabaseGroupList(),
-      usePolicyV1Store().getOrFetchPolicyByName("policies/WORKSPACE_IAM"),
       useUIStateStore().restoreState(),
     ]);
   },
