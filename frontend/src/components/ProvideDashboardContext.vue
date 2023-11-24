6cb69bbfc2b025b5bafc9fc855a874dc8e415424
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-24 10:25:03 +0800

    chore: optimize basic store initialize order

--- frontend/src/components/ProvideDashboardContext.vue
+++ frontend/src/components/ProvideDashboardContext.vue
@@ -5,29 +5,17 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import {
-  useEnvironmentV1Store,
-  useRoleStore,
   useUIStateStore,
-  useUserStore,
-  useProjectV1Store,
   usePolicyV1Store,
   useDatabaseV1Store,
   useDBGroupStore,
+  prepareBasicStores,
 } from "@/store";
-import { useInstanceV1Store } from "@/store/modules/v1/instance";
-import { useSettingV1Store } from "@/store/modules/v1/setting";
 
 export default defineComponent({
   name: "ProvideDashboardContext",
   async setup() {
-    await Promise.all([
-      useSettingV1Store().fetchSettingList(),
-      useRoleStore().fetchRoleList(),
-      useUserStore().fetchUserList(),
-      useEnvironmentV1Store().fetchEnvironments(),
-      useInstanceV1Store().fetchInstanceList(),
-      useProjectV1Store().fetchProjectList(true),
-    ]);
+    await prepareBasicStores();
     await Promise.all([
       useDatabaseV1Store().fetchDatabaseList({
         parent: "instances/-",
