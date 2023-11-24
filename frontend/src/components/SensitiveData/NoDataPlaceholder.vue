cf696fd960a0c0a0563c9b1467acbc51bb6fa2a1
ecmadao <wlec@outlook.com>
2023-11-24 21:45:23 +0800

    chore: ui update (#9532)
    * chore: refactor header and sidebar
    
    * chore: move search to sidebar
    
    * chore: multiple ui detail update
    
    * fix: eslint
    
    * chore: ui update
    
    * fix: eslint
    
    * chore: unify margin & padding between elements
    
    * chore: update
    
    * fix: eslint
    
    * chore: ui update

--- frontend/src/components/SensitiveData/NoDataPlaceholder.vue
+++ frontend/src/components/SensitiveData/NoDataPlaceholder.vue
@@ -1,12 +1,13 @@
 <template>
   <div
-    class="border-4 border-dashed border-gray-200 rounded-lg h-96 flex justify-center items-center"
+    class="border-4 border-dashed border-gray-200 rounded-lg h-96 flex flex-col justify-center items-center"
   >
     <NoData
       :img-attrs="{
-        class: '!w-auto max-h-[30vh]',
+        class: '!w-auto max-h-[20vh]',
       }"
     />
+    <slot />
   </div>
 </template>
 
