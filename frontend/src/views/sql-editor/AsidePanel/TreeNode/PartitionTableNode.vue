a0f52bc3724d19a8944598cd8d4e7dd9108ef767
boojack <stevenlgtm@gmail.com>
2023-11-28 16:16:00 +0800

    chore: show partition tables in sql editor (#9577)
    * chore: show partition tables in sql editor
    
    * chore: update

---
+++ frontend/src/views/sql-editor/AsidePanel/TreeNode/PartitionTableNode.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex items-center max-w-full overflow-hidden gap-x-1">
+    <TableIcon class="w-4 h-4" />
+    <HighlightLabelText
+      :text="target.partition.name"
+      :keyword="keyword"
+      class="flex-1 truncate"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { TableIcon } from "@/components/Icon";
+import {
+  SQLEditorTreeNode as TreeNode,
+  SQLEditorTreeFactor as Factor,
+} from "@/types";
+import HighlightLabelText from "./HighlightLabelText.vue";
+
+const props = defineProps<{
+  node: TreeNode;
+  factors: Factor[];
+  keyword: string;
+}>();
+
+const target = computed(
+  () => (props.node as TreeNode<"partition-table">).meta.target
+);
+</script>
