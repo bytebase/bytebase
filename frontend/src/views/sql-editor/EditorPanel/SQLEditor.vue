4983647aca47381c7447a528253e7d8ec0041812
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-11-28 13:55:02 +0800

    chore: update import

--- frontend/src/views/sql-editor/EditorPanel/SQLEditor.vue
+++ frontend/src/views/sql-editor/EditorPanel/SQLEditor.vue
@@ -45,13 +45,11 @@ import {
 import { formatEngineV1, useInstanceV1EditorLanguage } from "@/utils";
 import { useSQLEditorContext } from "../context";
 
-const [
-  { default: MonacoEditor },
-  { extensionNameOfLanguage, formatEditorContent },
-] = await Promise.all([
-  import("@/components/MonacoEditor/MonacoEditor.vue"),
-  import("@/components/MonacoEditor/utils"),
-]);
+const [{ MonacoEditor }, { extensionNameOfLanguage, formatEditorContent }] =
+  await Promise.all([
+    import("@/components/MonacoEditor"),
+    import("@/components/MonacoEditor/utils"),
+  ]);
 
 const emit = defineEmits<{
   (
