ace2e42510893c58f03a47eaa033ed3a8c56fae3
h3n4l <oysterdays@gmail.com>
2023-11-28 19:02:32 +0800

    feat: flatten the parition tables in build table metadata (#9583)
    * feat: flatten the parition tables in build table metadata
    
    * chore: add a simple unit test
    
    * chore: update
    
    Former-commit-id: 97feb863fc457e9ee99aefb76275183f06199a6f
    Former-commit-id: d798647551f8cd9374dc385b79c7620150c8a89c

---
+++ backend/store/model/database_test.go
@@ -0,0 +1,96 @@
+package model
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+func TestBuildTablesMetadata(t *testing.T) {
+	testCases := []struct {
+		input       *storepb.TableMetadata
+		wantNames   []string
+		wantColumns []*storepb.ColumnMetadata
+	}{
+		// No partitions.
+		{
+			input: &storepb.TableMetadata{
+				Name: "orders",
+				Columns: []*storepb.ColumnMetadata{
+					{
+						Name: "id",
+					},
+				},
+			},
+			wantNames: []string{"orders"},
+			wantColumns: []*storepb.ColumnMetadata{
+				{
+					Name: "id",
+				},
+			},
+		},
+		// Nested partitions.
+		{
+			input: &storepb.TableMetadata{
+				Name: "orders",
+				Columns: []*storepb.ColumnMetadata{
+					{
+						Name: "id",
+					},
+				},
+				Partitions: []*storepb.TablePartitionMetadata{
+					{
+						Name: "orders_0_100",
+						Subpartitions: []*storepb.TablePartitionMetadata{
+							{
+								Name: "orders_0_50",
+							},
+							{
+								Name: "orders_50_100",
+							},
+						},
+					},
+					{
+						Name: "orders_100_200",
+						Subpartitions: []*storepb.TablePartitionMetadata{
+							{
+								Name: "orders_100_150",
+							},
+							{
+								Name: "orders_150_200",
+							},
+						},
+					},
+				},
+			},
+			wantNames: []string{"orders", "orders_0_100", "orders_0_50", "orders_50_100", "orders_100_200", "orders_100_150", "orders_150_200"},
+			wantColumns: []*storepb.ColumnMetadata{
+				{
+					Name: "id",
+				},
+			},
+		},
+	}
+
+	a := require.New(t)
+	for _, tc := range testCases {
+		tables, names := buildTablesMetadata(tc.input)
+
+		// The length of the tables should be the same as the length of the names.
+		a.Equal(len(tables), len(names))
+
+		// The names should be the same as the expected names.
+		a.Equal(sort.StringSlice(names), sort.StringSlice(tc.wantNames))
+
+		// Each table should have the same columns as the input.
+		for _, table := range tables {
+			a.Equal(len(table.GetColumns()), len(tc.wantColumns))
+			for _, column := range tc.wantColumns {
+				a.NotNil(table.GetColumn(column.Name))
+			}
+		}
+	}
+}
