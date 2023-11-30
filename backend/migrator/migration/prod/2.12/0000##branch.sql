94d2082d8dccef395a3f54b5d4b99343cb0d466b
d-bytebase <d@bytebase.com>
2023-11-30 19:51:10 +0800

    chore: schema for branch

---
+++ backend/migrator/migration/prod/2.12/0000##branch.sql
@@ -0,0 +1,22 @@
+CREATE TABLE branch (
+  id SERIAL PRIMARY KEY,
+  row_status row_status NOT NULL DEFAULT 'NORMAL',
+  created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+  updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+  project_id INTEGER NOT NULL REFERENCES project (id),
+  name TEXT NOT NULL,
+  engine TEXT NOT NULL,
+  baseline JSONB NOT NULL,
+  head JSONB NOT NULL,
+  config JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE UNIQUE INDEX idx_branch_unique_resource_id ON idp(resource_id);
+
+ALTER SEQUENCE branch_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_branch_updated_ts
+BEFORE
+UPDATE
+    ON branch FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
