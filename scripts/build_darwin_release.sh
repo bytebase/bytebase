b26e17d83d4bdd3d5e0ee954f5ed9dd826f5d2ea
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-30 20:01:41 +0800

    chore: add a script to build tar for darwin release (#9626)
    * chore: add a script to build tar for darwin release
    
    * chore: update

---
+++ scripts/build_darwin_release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -eu
+
+./scripts/build_bytebase.sh
+./scripts/build_bb.sh
+
+tmp_dir=$(mktemp -d) || abort "cannot create temp directory"
+echo $tmp_dir
+cp ./bytebase-build/bytebase $tmp_dir
+cp ./bytebase-build/bb $tmp_dir
+cp ./LICENSE $tmp_dir
+cp ./LICENSE.enterprise $tmp_dir
+cp scripts/VERSION $tmp_dir
+
+tar -czvf ./bytebase-build/bytebase_darwin_arm64.tar.gz -C $tmp_dir .
