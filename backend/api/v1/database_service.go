e8c609125427a89604c5589a30d6f4b981e6ad0c
p0ny <p0uy@outlook.com>
2023-11-30 16:37:51 +0800

    list/search databases

--- backend/api/v1/database_service.go
+++ backend/api/v1/database_service.go
@@ -124,6 +124,10 @@ func (s *DatabaseService) GetDatabase(ctx context.Context, request *v1pb.GetData
 
 // ListDatabases lists all databases.
 func (s *DatabaseService) ListDatabases(ctx context.Context, request *v1pb.ListDatabasesRequest) (*v1pb.ListDatabasesResponse, error) {
+	user, ok := ctx.Value(common.UserContextKey).(*store.UserMessage)
+	if !ok {
+		return nil, status.Errorf(codes.Internal, "failed to retrieve user from context")
+	}
 	instanceID, err := common.GetInstanceID(request.Parent)
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, err.Error())
@@ -155,6 +159,15 @@ func (s *DatabaseService) ListDatabases(ctx context.Context, request *v1pb.ListD
 				continue
 			}
 			return nil, err
+		}
+		if s.profile.DevelopmentIAM {
+			ok, err := s.iamManager.CheckPermission(ctx, iam.PermissionDatabasesList, user, database.ProjectID)
+			if err != nil {
+				return nil, status.Errorf(codes.Internal, "failed to check permission, error: %v", err)
+			}
+			if !ok {
+				continue
+			}
 		}
 		response.Databases = append(response.Databases, convertToDatabase(database))
 	}
