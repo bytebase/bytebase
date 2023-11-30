763a58ef85477a8464b1a5dff641de7d545fdd75
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-11-30 20:49:45 +0800

    chore: schema for branch (#9627)
    * chore: schema for branch
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 63ee88e4439fdee72365863a625d5eb07061d8a8

--- backend/migrator/migration/dev/LATEST.sql
+++ backend/migrator/migration/dev/LATEST.sql
@@ -1249,3 +1249,26 @@ BEFORE
 UPDATE
     ON changelist FOR EACH ROW
 EXECUTE FUNCTION trigger_update_updated_ts();
+
+CREATE TABLE branch (
+  id SERIAL PRIMARY KEY,
+  row_status row_status NOT NULL DEFAULT 'NORMAL',
+  created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+  updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+  project_id INTEGER NOT NULL REFERENCES project (id),
+  name TEXT NOT NULL,
+  engine TEXT NOT NULL,
+  base JSONB NOT NULL,
+  head JSONB NOT NULL,
+  config JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE UNIQUE INDEX idx_branch_unique_project_id_name ON branch(project_id, name);
+
+ALTER SEQUENCE branch_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_branch_updated_ts
+BEFORE
+UPDATE
+    ON branch FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
