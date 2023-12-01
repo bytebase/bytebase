d44641d0de2ad0257ce593897f019ee3e6897234
zll600 <3400692417@qq.com>
2023-12-01 14:02:21 +0800

    chore: optimize where clause

--- backend/plugin/advisor/tidb/advisor_stmt_where_required.go
+++ backend/plugin/advisor/tidb/advisor_stmt_where_required.go
@@ -42,7 +42,8 @@ func (*WhereRequirementAdvisor) Check(ctx advisor.Context, _ string) ([]advisor.
 	for _, stmtNode := range root {
 		checker.text = stmtNode.Text()
 		checker.line = stmtNode.OriginTextPosition()
-		(stmtNode).Accept(checker)
+		// (stmtNode).Accept(checker)
+		checker.checkSelect(stmtNode)
 	}
 
 	if len(checker.adviceList) == 0 {
@@ -62,6 +63,8 @@ type whereRequirementChecker struct {
 	title      string
 	text       string
 	line       int
+
+	cteOuterSchema []string
 }
 
 // Enter implements the ast.Visitor interface.
@@ -100,4 +103,170 @@ func (v *whereRequirementChecker) Enter(in ast.Node) (ast.Node, bool) {
 // Leave implements the ast.Visitor interface.
 func (*whereRequirementChecker) Leave(in ast.Node) (ast.Node, bool) {
 	return in, true
+}
+
+func (v *whereRequirementChecker) checkSelect(in ast.Node) {
+	switch in.(type) {
+	case *ast.SelectStmt:
+	case *ast.SetOprStmt:
+	default:
+		// only focus on select statement or set statement.
+		return
+	}
+	v.checkNode(in)
+}
+
+func (v *whereRequirementChecker) checkNode(in ast.Node) bool {
+	switch node := in.(type) {
+	case *ast.SelectStmt:
+		return v.checkSelectStmt(node)
+	case *ast.Join:
+		return v.checkJoin(node)
+	case *ast.TableSource:
+		return v.checkTableSource(node)
+	case *ast.SetOprStmt:
+		return v.checkSetOpr(node)
+	}
+	return true
+}
+
+func (v *whereRequirementChecker) checkSetOpr(node *ast.SetOprStmt) bool {
+	if node.With != nil {
+		cteOuterLength := len(v.cteOuterSchema)
+		defer func() {
+			v.cteOuterSchema = v.cteOuterSchema[:cteOuterLength]
+		}()
+		for _, cte := range node.With.CTEs {
+			v.checkCTE(cte)
+			v.cteOuterSchema = append(v.cteOuterSchema, cte.Name.O)
+		}
+	}
+
+	for _, selectStmt := range node.SelectList.Selects {
+		v.checkNode(selectStmt)
+	}
+	return false
+}
+
+func (v *whereRequirementChecker) checkRecursiveCTE(node *ast.CommonTableExpression) bool {
+	switch x := node.Query.Query.(type) {
+	case *ast.SetOprStmt:
+		if x.With != nil {
+			for _, cte := range x.With.CTEs {
+				v.checkCTE(cte)
+			}
+		}
+	default:
+		v.checkNonRecursiveCTE(node)
+	}
+	return false
+}
+
+func (v *whereRequirementChecker) checkNonRecursiveCTE(node *ast.CommonTableExpression) bool {
+	return v.checkNode(node.Query.Query)
+}
+
+func (v *whereRequirementChecker) checkCTE(node *ast.CommonTableExpression) bool {
+	if node.IsRecursive {
+		return v.checkRecursiveCTE(node)
+	}
+	return v.checkNonRecursiveCTE(node)
+}
+
+func (v *whereRequirementChecker) checkSelectStmt(node *ast.SelectStmt) bool {
+	if node.With != nil {
+		cteOuterLength := len(v.cteOuterSchema)
+		defer func() {
+			v.cteOuterSchema = v.cteOuterSchema[:cteOuterLength]
+		}()
+		for _, cte := range node.With.CTEs {
+			v.checkCTE(cte)
+			v.cteOuterSchema = append(v.cteOuterSchema, cte.Name.O)
+		}
+	}
+
+	isPhysical := false
+	if node.From != nil {
+		isPhysical = v.checkNode(node.From.TableRefs)
+	}
+
+	if isPhysical && node.Where == nil {
+		v.adviceList = append(v.adviceList, advisor.Advice{
+			Status:  v.level,
+			Code:    advisor.StatementNoWhere,
+			Title:   v.title,
+			Content: fmt.Sprintf("\"%s\" requires WHERE clause", v.text),
+			Line:    v.line,
+		})
+	}
+
+	if node.Where != nil {
+		v.checkExprNode(node.Where)
+	}
+
+	if node.Fields != nil {
+		for _, field := range node.Fields.Fields {
+			if field.WildCard != nil {
+				continue
+			}
+
+			v.checkExprNode(field.Expr)
+		}
+	}
+
+	return false
+}
+
+func (v *whereRequirementChecker) checkTableSource(in *ast.TableSource) bool {
+	switch node := in.Source.(type) {
+	case *ast.TableName:
+		// search whether this table name is a cte name.
+		for i := len(v.cteOuterSchema) - 1; i >= 0; i-- {
+			if node.Name.O == v.cteOuterSchema[i] {
+				return false
+			}
+		}
+		return true
+	default:
+		v.checkNode(in.Source)
+		return false
+	}
+}
+
+func (v *whereRequirementChecker) checkJoin(node *ast.Join) bool {
+	if node.Right == nil {
+		return v.checkNode(node.Left)
+	}
+
+	// check left
+	v.checkNode(node.Left)
+	v.checkNode(node.Right)
+	return false
+}
+
+func (v *whereRequirementChecker) checkExprNode(in ast.ExprNode) bool {
+	if in == nil {
+		return false
+	}
+
+	switch node := in.(type) {
+	case *ast.BinaryOperationExpr:
+		v.checkExprNode(node.L)
+		v.checkExprNode(node.R)
+		return false
+	case *ast.AggregateFuncExpr:
+		v.checkExprNodeList(node.Args)
+		return false
+	case *ast.SubqueryExpr:
+		v.checkNode(node.Query)
+		return false
+	}
+	return false
+}
+
+func (v *whereRequirementChecker) checkExprNodeList(nodeList []ast.ExprNode) bool {
+	for _, node := range nodeList {
+		v.checkExprNode(node)
+	}
+	return false
 }
