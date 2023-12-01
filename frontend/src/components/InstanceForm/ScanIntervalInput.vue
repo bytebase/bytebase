8bb512d8aa661e0e9248b4b89929f8b264c2b41f
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-12-01 16:32:43 +0800

    chore(frontend): re-invent instance scan interval input (#9652)
    * chore: scan interval input improvements
    
    * chore: fix
    
    * chore: details
    
    * chore: style
    
    * Update frontend/src/components/InstanceForm/ScanIntervalInput.vue
    
    ---------
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    Former-commit-id: dde14676df2dbc6b3751a7ea6c207b540d9e12b1
    Former-commit-id: 5acecf1f14eb0cd245e993636e57af6c8042f798

--- frontend/src/components/InstanceForm/ScanIntervalInput.vue
+++ frontend/src/components/InstanceForm/ScanIntervalInput.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="flex items-center gap-x-6">
       <NRadio
-        :checked="mode === 'DEFAULT'"
+        :checked="state.mode === 'DEFAULT'"
         :disabled="!allowEdit || !hasSecretFeature"
         value="DEFAULT"
         @click="handleModeChange('DEFAULT')"
@@ -18,7 +18,7 @@
 
       <div class="flex items-center">
         <NRadio
-          :checked="mode === 'CUSTOM'"
+          :checked="state.mode === 'CUSTOM'"
           :disabled="!allowEdit || !hasSecretFeature"
           value="CUSTOM"
           class="!items-center"
@@ -27,15 +27,23 @@
           <div class="flex items-center gap-x-1.5">
             <span>{{ $t("common.custom") }}</span>
             <NInputNumber
-              v-model:value="minutes"
+              :value="state.minutes"
               :show-button="false"
-              :min="30"
-              placeholder=""
+              :placeholder="`>= ${MIN_MINUTES}`"
               size="small"
               style="width: 4rem"
-              :disabled="mode !== 'CUSTOM'"
+              :status="state.isValid ? undefined : 'error'"
+              :disabled="state.mode !== 'CUSTOM'"
+              @update:value="handleMinuteChange"
             />
-            <span>{{ $t("common.minutes") }}</span>
+            <span v-if="!state.isValid" class="text-error">
+              {{
+                $t("instance.scan-interval.min-value", {
+                  value: MIN_MINUTES,
+                })
+              }}
+            </span>
+            <span v-else>{{ $t("common.minutes") }}</span>
           </div>
         </NRadio>
         <FeatureBadge
@@ -50,12 +58,20 @@
 
 <script setup lang="ts">
 import { NInputNumber, NRadio } from "naive-ui";
-import { computed, reactive } from "vue";
+import { computed, reactive, watch } from "vue";
 import { useSubscriptionV1Store } from "@/store";
 import { Duration } from "@/types/proto/google/protobuf/duration";
 import { useInstanceFormContext } from "./context";
 
 type Mode = "DEFAULT" | "CUSTOM";
+type LocalState = {
+  mode: Mode;
+  lastValidDuration: Duration | undefined;
+  isValid: boolean;
+  minutes: number | undefined;
+};
+
+const MIN_MINUTES = 30;
 
 const props = defineProps<{
   scanInterval?: Duration | undefined;
@@ -76,51 +92,76 @@ const hasSecretFeature = computed(() => {
   );
 });
 
-const mode = computed(() => {
-  const duration = props.scanInterval;
-  if (!duration) return "DEFAULT";
-  if (duration.seconds.toNumber() === 0) return "DEFAULT";
-  return "CUSTOM";
-});
+const extractStateFromDuration = (
+  duration: Duration | undefined
+): { mode: Mode; minutes: number | undefined } => {
+  if (!duration || duration.seconds.toNumber() === 0) {
+    return {
+      mode: "DEFAULT",
+      minutes: undefined,
+    };
+  }
+  return {
+    mode: "CUSTOM",
+    minutes: Math.floor(duration.seconds.toNumber() / 60),
+  };
+};
 
-const state = reactive({
-  minutes: undefined as number | undefined, // Local input state
-});
-
-const minutes = computed({
-  get() {
-    const duration = props.scanInterval;
-    if (!duration) return undefined;
-    if (duration.seconds.toNumber() === 0) return undefined;
-    return Math.floor(duration.seconds.toNumber() / 60);
-  },
-  set(value) {
-    if (!value) {
-      emit(
-        "update:scan-interval",
-        Duration.fromPartial({
-          seconds: 0,
-        })
-      );
-      return;
-    }
-
-    state.minutes = value;
-    const duration = Duration.fromPartial({
-      seconds: value * 60,
-    });
-    emit("update:scan-interval", duration);
-  },
+const state = reactive<LocalState>({
+  ...extractStateFromDuration(props.scanInterval),
+  lastValidDuration: props.scanInterval,
+  isValid: true,
 });
 
 const handleModeChange = (targetMode: Mode) => {
-  if (targetMode === mode.value) {
+  if (targetMode === state.mode) {
     return;
   }
+  state.mode = targetMode;
   if (targetMode === "DEFAULT") {
-    minutes.value = undefined;
+    emit(
+      "update:scan-interval",
+      Duration.fromPartial({
+        seconds: 0,
+      })
+    );
   } else {
-    minutes.value = state.minutes ?? 1440;
+    emit(
+      "update:scan-interval",
+      Duration.fromPartial({
+        seconds: 24 * 60 * 60,
+      })
+    );
   }
 };
+
+const handleMinuteChange = (minute: number | undefined) => {
+  state.minutes = minute;
+  if (!minute || minute < MIN_MINUTES) {
+    state.isValid = false;
+    return;
+  }
+  state.isValid = true;
+  emit(
+    "update:scan-interval",
+    Duration.fromPartial({
+      seconds: minute * 60,
+    })
+  );
+};
+
+watch(
+  () => props.scanInterval,
+  (duration) => {
+    Object.assign(state, extractStateFromDuration(duration));
+    state.lastValidDuration = duration;
+    state.isValid = true;
+  }
+);
+
+defineExpose({
+  validate: () => {
+    return state.isValid;
+  },
+});
 </script>
