8cc5bf46bd9fd983a710fee964da9c898b409098
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-12-06 18:11:06 -0800

    chore: sheet/branch same name in existing data (#9734)

--- backend/migrator/migration/prod/2.12/0001##branch_data.sql
+++ backend/migrator/migration/prod/2.12/0001##branch_data.sql
@@ -4,8 +4,11 @@ SELECT
 	sheet.created_ts,
 	sheet.updater_id,
 	sheet.updated_ts,
-	sheet.project_id,	
-	sheet.name,
+	sheet.project_id,
+	CASE WHEN (SELECT COUNT(1) > 1 AS c FROM sheet as ns WHERE sheet.project_id = ns.project_id AND sheet.name = ns.name)
+	THEN sheet.name || '-' || sheet.id
+	ELSE sheet.name
+	END,
 	sheet.payload->'schemaDesign'->>'engine',
 	(SELECT jsonb_build_object('schema', replace(encode(ps.statement::bytea, 'base64'), E'\n', '')) FROM sheet AS ps WHERE ps.id = (sheet.payload->'schemaDesign'->>'baselineSheetId')::int LIMIT 1),
 	jsonb_build_object('schema', replace(encode(sheet.statement::bytea, 'base64'), E'\n', '')),
