0dfee9d5bc23efa9297a81b789b89796ea26cb37
boojack <stevenlgtm@gmail.com>
2023-12-12 11:07:51 +0800

    chore: hide issue buttons in standalone mode (#9795)
    * chore: hide issue buttons in standalone mode
    
    * chore: update
    
    Former-commit-id: a074165e97fa82e3392072a2f96355e6307dc87a
    Former-commit-id: e3f6791755a124c0eadb6f160763d0f8df8f72b4

--- frontend/src/components/v2/Model/EnvironmentV1Name.vue
+++ frontend/src/components/v2/Model/EnvironmentV1Name.vue
@@ -3,7 +3,7 @@
     :is="link ? 'router-link' : tag"
     v-bind="bindings"
     class="inline-flex items-center gap-x-1"
-    :class="link && !plain && 'normal-link'"
+    :class="[link && !plain && 'normal-link', link && 'hover:underline']"
   >
     <span class="line-clamp-1" :class="textClass">
       {{ environmentV1Name(environment) }}
