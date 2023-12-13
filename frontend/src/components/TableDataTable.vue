8476635406efb4359eda15c638d5d5ea635395b0
boojack <stevenlgtm@gmail.com>
2023-12-13 15:23:46 +0800

    fix: table comment display (#9813)
    Former-commit-id: 36e92b9da6e81493d4bba1c6ee8a65926cf1d144

--- frontend/src/components/TableDataTable.vue
+++ frontend/src/components/TableDataTable.vue
@@ -24,7 +24,6 @@ import { DataTableColumn, NDataTable } from "naive-ui";
 import { computed, PropType, reactive, onMounted, h } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRoute } from "vue-router";
-import EllipsisText from "@/components/EllipsisText.vue";
 import ClassificationLevelBadge from "@/components/SchemaTemplate/ClassificationLevelBadge.vue";
 import { useSettingV1Store } from "@/store/modules";
 import { ComposedDatabase } from "@/types";
@@ -176,8 +175,11 @@ const columns = computed(() => {
     {
       key: "comment",
       title: t("database.comment"),
+      ellipsis: {
+        tooltip: true,
+      },
       render: (row) => {
-        return h(EllipsisText, { text: row.userComment });
+        return row.userComment;
       },
     },
   ];
