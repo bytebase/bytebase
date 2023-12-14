3528aa99c1b7e4871ce9a94a513a985b1013f6d2
d-bytebase <d@bytebase.com>
2023-12-13 21:01:10 -0800

    chore: branch reconcile state index

--- backend/migrator/migration/prod/2.12/0004##branch_reconcile.sql
+++ backend/migrator/migration/prod/2.12/0004##branch_reconcile.sql
@@ -1,6 +1,7 @@
 ALTER TABLE branch ADD COLUMN base_schema TEXT NOT NULL DEFAULT '';
 ALTER TABLE branch ADD COLUMN head_schema TEXT NOT NULL DEFAULT '';
 ALTER TABLE branch ADD COLUMN reconcile_state TEXT NOT NULL DEFAULT '';
+CREATE INDEX idx_branch_reconcile_state ON branch(reconcile_state);
 
 UPDATE branch SET base_schema = convert_from(decode(base->>'schema', 'base64'), 'utf8');
 UPDATE branch SET head_schema = convert_from(decode(head->>'schema', 'base64'), 'utf8');
