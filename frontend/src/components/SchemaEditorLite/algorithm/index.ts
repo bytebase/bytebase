3d50b252a2cc93ee72f6fcc595feec5a2b1a1bec
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2023-12-14 15:42:18 +0800

    chore: cleanup

--- frontend/src/components/SchemaEditorLite/algorithm/index.ts
+++ frontend/src/components/SchemaEditorLite/algorithm/index.ts
@@ -75,6 +75,7 @@ const cleanupUnusedConfigs = (metadata: DatabaseMetadata) => {
     tableConfig: TableConfig
   ) => {
     const columnMap = keyBy(table.columns, (column) => column.name);
+    // Remove unused column configs
     tableConfig.columnConfigs = tableConfig.columnConfigs.filter((cc) =>
       columnMap.has(cc.name)
     );
@@ -84,21 +85,33 @@ const cleanupUnusedConfigs = (metadata: DatabaseMetadata) => {
     schemaConfig: SchemaConfig
   ) => {
     const tableMap = keyBy(schema.tables, (table) => table.name);
+    // Remove unused table configs
     schemaConfig.tableConfigs = schemaConfig.tableConfigs.filter((tc) =>
       tableMap.has(tc.name)
     );
+    // Recursively cleanup column configs
     schemaConfig.tableConfigs.forEach((tc) => {
       cleanupColumnConfigs(tableMap.get(tc.name)!, tc);
     });
+    // Cleanup empty table configs
+    schemaConfig.tableConfigs = schemaConfig.tableConfigs.filter(
+      (tc) => tc.columnConfigs.length > 0
+    );
   };
   const cleanupSchemaConfigs = (metadata: DatabaseMetadata) => {
     const schemaMap = keyBy(metadata.schemas, (schema) => schema.name);
+    // Remove unused schema configs
     metadata.schemaConfigs = metadata.schemaConfigs.filter((sc) =>
       schemaMap.has(sc.name)
     );
+    // Recursively cleanup table configs
     metadata.schemaConfigs.forEach((sc) => {
       cleanupTableConfigs(schemaMap.get(sc.name)!, sc);
     });
+    // Cleanup empty schema configs
+    metadata.schemaConfigs = metadata.schemaConfigs.filter(
+      (sc) => sc.tableConfigs.length > 0
+    );
   };
 
   cleanupSchemaConfigs(metadata);
