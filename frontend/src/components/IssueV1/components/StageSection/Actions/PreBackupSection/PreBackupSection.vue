497456d998eca784c2080efa5c7e9cc3a44e07e8
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-12-15 09:05:12 -0800

    chore: prior backup UI (#9877)
    * chore: prior backup UI
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 9b3c3cf7defd54cc578fa05add89375c0867d096

---
+++ frontend/src/components/IssueV1/components/StageSection/Actions/PreBackupSection/PreBackupSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    v-if="showPreBackupSection"
+    class="flex items-center gap-x-3 min-h-[34px]"
+  >
+    <NTooltip>
+      <template #trigger>
+        <div class="textlabel flex items-center">Prior Backup</div>
+      </template>
+    </NTooltip>
+    <div class="w-[12rem] flex items-center gap-x-2">
+      <PreBackupSwitch />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PreBackupSwitch from "./PreBackupSwitch.vue";
+import { usePreBackupContext } from "./common";
+
+const { showPreBackupSection } = usePreBackupContext();
+</script>
