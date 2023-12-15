7419fb4f77af666b6dd0b9133cb390dfef207e4b
d-bytebase <d@bytebase.com>
2023-12-15 09:20:38 -0800

    chore: use create table like to be compatible with tidb

--- backend/runner/taskrun/data_update_executor_test.go
+++ backend/runner/taskrun/data_update_executor_test.go
@@ -8,5 +8,5 @@ import (
 
 func TestUpdateToSelect(t *testing.T) {
 	got := updateToSelect("UPDATE hello SET column1 = value1, column2 = value2 WHERE id > 3;", "todozp", "113")
-	assert.Equal(t, "CREATE TABLE todozp.hello113 AS SELECT * FROM hello WHERE id > 3;", got)
+	assert.Equal(t, "CREATE TABLE todozp.hello113 LIKE hello; INSERT INTO todozp.hello113 SELECT * FROM hello WHERE id > 3;", got)
 }
