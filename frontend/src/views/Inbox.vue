97ca6aa0dbd49c8dbb3c30980a23b10ac7d45604
boojack <stevenlgtm@gmail.com>
2023-12-15 11:38:14 +0800

    chore: update inbox page (#9857)
    Former-commit-id: b18efbdecd30ce8650c508e95e418346ee4ef5c5
    Former-commit-id: f2c790c0f63eb3ed7911f8b84c821e29a83668b9

--- frontend/src/views/Inbox.vue
+++ frontend/src/views/Inbox.vue
@@ -1,6 +1,5 @@
 <template>
-  <!-- This example requires Tailwind CSS v2.0+ -->
-  <div class="space-y-4">
+  <div class="space-y-4 py-4">
     <div class="mx-6 space-y-2">
       <div
         class="flex items-center justify-between pb-2 border-b border-block-border"
@@ -8,12 +7,10 @@
         <div class="text-lg leading-6 font-medium text-main">
           {{ $t("common.unread") }}
         </div>
-        <button type="button" class="btn-normal" @click.prevent="markAllAsRead">
-          <heroicons-outline:check
-            class="-ml-1 mr-2 h-5 w-5 text-control-light"
-          />
-          <span>{{ $t("inbox.mark-all-as-read") }}</span>
-        </button>
+        <NButton @click="markAllAsRead">
+          <Check class="h-auto w-4 mr-1" />
+          {{ $t("inbox.mark-all-as-read") }}
+        </NButton>
       </div>
       <InboxList :inbox-list="state.unreadList" />
     </div>
@@ -33,7 +30,9 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import { Check } from "lucide-vue-next";
+import { NButton } from "naive-ui";
 import { reactive, watchEffect } from "vue";
 import { useRouter } from "vue-router";
 import { useCurrentUser, useInboxV1Store } from "@/store";
@@ -54,80 +53,68 @@ interface LocalState {
   readCreatedAfterTs: number;
 }
 
-export default {
-  name: "Inbox",
-  components: { InboxList },
-  setup() {
-    const inboxV1Store = useInboxV1Store();
-    const router = useRouter();
+const inboxV1Store = useInboxV1Store();
+const router = useRouter();
 
-    const state = reactive<LocalState>({
-      readList: [],
-      unreadList: [],
-      readCreatedAfterTs:
-        parseInt(router.currentRoute.value.query.created as string) ||
-        Math.ceil(Date.now() / 1000) - READ_INBOX_DURATION_STEP,
-    });
+const state = reactive<LocalState>({
+  readList: [],
+  unreadList: [],
+  readCreatedAfterTs:
+    parseInt(router.currentRoute.value.query.created as string) ||
+    Math.ceil(Date.now() / 1000) - READ_INBOX_DURATION_STEP,
+});
 
-    const currentUser = useCurrentUser();
+const currentUser = useCurrentUser();
 
-    const prepareInboxList = () => {
-      // It will also be called when user logout
-      if (currentUser.value.id != UNKNOWN_ID) {
-        inboxV1Store.fetchInboxList(state.readCreatedAfterTs).then((list) => {
-          state.readList = [];
-          state.unreadList = [];
-
-          for (const item of list) {
-            if (!item.activity) {
-              continue;
-            }
-            if (item.status == InboxMessage_Status.STATUS_READ) {
-              state.readList.push(item);
-            } else if (item.status == InboxMessage_Status.STATUS_UNREAD) {
-              state.unreadList.push(item);
-            }
-          }
-        });
-      }
-    };
-
-    watchEffect(prepareInboxList);
-
-    const markAllAsRead = async () => {
-      const list = await Promise.all(
-        state.unreadList.map(async (item) => {
-          item.status = InboxMessage_Status.STATUS_READ;
-          // TODO: use batch instead.
-          await inboxV1Store.patchInbox(item);
-          return item;
-        })
-      );
-      state.readList.push(...list);
+const prepareInboxList = () => {
+  // It will also be called when user logout
+  if (currentUser.value.id != UNKNOWN_ID) {
+    inboxV1Store.fetchInboxList(state.readCreatedAfterTs).then((list) => {
+      state.readList = [];
       state.unreadList = [];
-      await inboxV1Store.fetchInboxSummary();
-    };
 
-    const viewOlder = () => {
-      state.readCreatedAfterTs -= READ_INBOX_DURATION_STEP;
-      router
-        .replace({
-          name: "workspace.inbox",
-          query: {
-            ...router.currentRoute.value.query,
-            created: state.readCreatedAfterTs,
-          },
-        })
-        .then(() => {
-          prepareInboxList();
-        });
-    };
+      for (const item of list) {
+        if (!item.activity) {
+          continue;
+        }
+        if (item.status == InboxMessage_Status.STATUS_READ) {
+          state.readList.push(item);
+        } else if (item.status == InboxMessage_Status.STATUS_UNREAD) {
+          state.unreadList.push(item);
+        }
+      }
+    });
+  }
+};
 
-    return {
-      state,
-      markAllAsRead,
-      viewOlder,
-    };
-  },
+watchEffect(prepareInboxList);
+
+const markAllAsRead = async () => {
+  const list = await Promise.all(
+    state.unreadList.map(async (item) => {
+      item.status = InboxMessage_Status.STATUS_READ;
+      // TODO: use batch instead.
+      await inboxV1Store.patchInbox(item);
+      return item;
+    })
+  );
+  state.readList.push(...list);
+  state.unreadList = [];
+  await inboxV1Store.fetchInboxSummary();
+};
+
+const viewOlder = () => {
+  state.readCreatedAfterTs -= READ_INBOX_DURATION_STEP;
+  router
+    .replace({
+      name: "workspace.inbox",
+      query: {
+        ...router.currentRoute.value.query,
+        created: state.readCreatedAfterTs,
+      },
+    })
+    .then(() => {
+      prepareInboxList();
+    });
 };
 </script>
