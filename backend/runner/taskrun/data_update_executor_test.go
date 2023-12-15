3e56badf8e7818256804d7a5b88e00580fc596a3
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-12-15 06:31:33 -0800

    chore: backup updated data (#9875)
    * chore: backup updated data
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 4e841ab8926c6d6a9c77b93faf1ac2072d285ce0
    Former-commit-id: 71366d10656719dfd99d1a6ff263bf76bd0bf8f7

---
+++ backend/runner/taskrun/data_update_executor_test.go
@@ -0,0 +1,12 @@
+package taskrun
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateToSelect(t *testing.T) {
+	got := updateToSelect("UPDATE hello SET column1 = value1, column2 = value2 WHERE id > 3;", "todozp", "113")
+	assert.Equal(t, "CREATE TABLE todozp.hello113 AS SELECT * FROM hello WHERE id > 3;", got)
+}
