753edc6319309673fbcda23b0a135206a2d42d23
boojack <stevenlgtm@gmail.com>
2023-12-15 17:41:36 +0800

    chore: update fetch database filter (#9868)
    * chore: update fetch database filter
    
    * chore: update
    
    Former-commit-id: 1bdc76a682755643ea0512f506af45fc9162d825
    Former-commit-id: 0c40288e9f5aa13514f098eafa39a76dbe6f63c5

--- frontend/src/components/ProvideDashboardContext.vue
+++ frontend/src/components/ProvideDashboardContext.vue
@@ -1,9 +1,10 @@
 <template>
-  <slot />
+  <slot v-if="!isLoading" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 import {
   useEnvironmentV1Store,
   useInstanceV1Store,
@@ -16,26 +17,43 @@ import {
   useDatabaseV1Store,
   useDBGroupStore,
 } from "@/store";
+import { projectNamePrefix } from "@/store/modules/v1/common";
 
-export default defineComponent({
-  name: "ProvideDashboardContext",
-  async setup() {
-    await Promise.all([
-      useUserStore().fetchUserList(),
-      useSettingV1Store().fetchSettingList(),
-      useRoleStore().fetchRoleList(),
-      useEnvironmentV1Store().fetchEnvironments(),
-      useInstanceV1Store().fetchInstanceList(),
-      useProjectV1Store().fetchProjectList(true),
-      usePolicyV1Store().getOrFetchPolicyByName("policies/WORKSPACE_IAM"),
-    ]);
-    await Promise.all([
-      useDatabaseV1Store().fetchDatabaseList({
-        parent: "instances/-",
-      }),
-      useDBGroupStore().fetchAllDatabaseGroupList(),
-      useUIStateStore().restoreState(),
-    ]);
-  },
+const route = useRoute();
+const isLoading = ref<boolean>(true);
+
+const databaseStore = useDatabaseV1Store();
+
+const prepareDatabases = async () => {
+  let filter = "";
+  // If `projectId` is provided in the route, filter the database list by the project.
+  if (route.params.projectId) {
+    filter = `project == "${projectNamePrefix}${route.params.projectId}"`;
+  }
+
+  await databaseStore.fetchDatabaseList({
+    parent: "instances/-",
+    filter: filter,
+  });
+};
+
+onMounted(async () => {
+  await Promise.all([
+    useUserStore().fetchUserList(),
+    useSettingV1Store().fetchSettingList(),
+    useRoleStore().fetchRoleList(),
+    useEnvironmentV1Store().fetchEnvironments(),
+    useInstanceV1Store().fetchInstanceList(),
+    useProjectV1Store().fetchProjectList(true),
+    usePolicyV1Store().getOrFetchPolicyByName("policies/WORKSPACE_IAM"),
+  ]);
+
+  await Promise.all([
+    prepareDatabases(),
+    useDBGroupStore().fetchAllDatabaseGroupList(),
+    useUIStateStore().restoreState(),
+  ]);
+
+  isLoading.value = false;
 });
 </script>
