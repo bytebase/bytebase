5dd54a83c98814bf16f390c8f120e72ee3af8c96
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-12-16 06:18:07 -0800

    chore: preview DDL in branch rollout view (#9928)
    Former-commit-id: a28ad30f077b0aa7c69d5e05c773edd96f49d977

--- frontend/src/components/Branch/BranchRolloutView/BranchRolloutView.vue
+++ frontend/src/components/Branch/BranchRolloutView/BranchRolloutView.vue
@@ -39,6 +39,29 @@
         </NButton>
       </div>
     </div>
+    <div class="flex items-center flex-start">
+      <button
+        class="-mb-px px-3 leading-9 rounded-t-md flex items-center text-sm text-gray-500 border border-b-0 border-transparent cursor-pointer select-none outline-none"
+        :class="
+          state.selectedTab === 'schema-editor' &&
+          'bg-white !border-gray-300 text-gray-800'
+        "
+        @click="handleChangeTab('schema-editor')"
+      >
+        {{ $t("schema-editor.self") }}
+      </button>
+      <button
+        class="-mb-px px-3 leading-9 rounded-t-md text-sm text-gray-500 border border-b-0 border-transparent cursor-pointer select-none outline-none"
+        :class="
+          state.selectedTab === 'raw-sql-preview' &&
+          'bg-white !border-gray-300 text-gray-800'
+        "
+        @click="handleChangeTab('raw-sql-preview')"
+      >
+        {{ $t("schema-designer.raw-sql-preview") }}
+      </button>
+    </div>
+
     <div class="flex-1 overflow-hidden relative">
       <SchemaEditorLite
         v-if="database"
@@ -69,6 +92,22 @@
       </template>
     </div>
 
+    <div
+      v-if="state.selectedTab === 'raw-sql-preview'"
+      class="w-full h-full pt-2 overflow-y-auto relative"
+    >
+      <MaskSpinner v-if="rawSQLPreviewState.isFetching">
+        <div class="text-sm">Generating diff DDL</div>
+      </MaskSpinner>
+      <MonacoEditor
+        class="w-full h-full border rounded-lg overflow-auto"
+        data-label="bb-schema-editor-sql-editor"
+        :content="rawSQLPreviewState.value"
+        :readonly="true"
+        :auto-focus="false"
+      />
+    </div>
+
     <MaskSpinner
       v-if="virtualBranchReady && isGeneratingDDL"
       class="!bg-white/75"
@@ -91,7 +130,7 @@
 import dayjs from "dayjs";
 import { cloneDeep } from "lodash-es";
 import { NButton } from "naive-ui";
-import { computed, ref, toRef, watch } from "vue";
+import { computed, reactive, ref, toRef, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRouter } from "vue-router";
 import SchemaEditorLite, {
@@ -119,12 +158,26 @@ import {
 import { extractSheetUID, setSheetStatement } from "@/utils";
 import { useVirtualBranch } from "./useVirtualBranch";
 
+type TabType = "schema-editor" | "raw-sql-preview";
+
+interface LocalState {
+  selectedTab: TabType;
+}
+
 const props = defineProps<{
   project: ComposedProject;
   branch: Branch;
 }>();
 
 const { t } = useI18n();
+const state = reactive<LocalState>({
+  selectedTab: "schema-editor",
+});
+const rawSQLPreviewState = reactive({
+  value: "",
+  isFetching: false,
+});
+
 const router = useRouter();
 const schemaEditorRef = ref<InstanceType<typeof SchemaEditorLite>>();
 const environment = ref<Environment>();
@@ -171,6 +224,59 @@ const allowPreviewIssue = computed(() => {
   if (!virtualBranchReady.value) return false;
   return selectedRolloutObjects.value.length > 0;
 });
+
+const handleChangeTab = async (tab: TabType) => {
+  state.selectedTab = tab;
+  if (tab === "raw-sql-preview") {
+    await fetchRawSQLPreview();
+  }
+};
+
+const fetchRawSQLPreview = async () => {
+  if (rawSQLPreviewState.isFetching) {
+    return;
+  }
+  rawSQLPreviewState.isFetching = true;
+
+  const cleanup = (errors: string[], fatal: boolean) => {
+    if (errors.length > 0) {
+      pushNotification({
+        module: "bytebase",
+        style: fatal ? "CRITICAL" : "WARN",
+        title: t("common.error"),
+        description: errors.join("\n"),
+      });
+    }
+    rawSQLPreviewState.isFetching = false;
+  };
+  const source = cloneDeep(virtualBranch.value.baselineSchemaMetadata);
+  const target = cloneDeep(virtualBranch.value.schemaMetadata);
+  const db = database.value;
+  const editor = schemaEditorRef.value;
+  if (!source) return;
+  if (!target) return;
+  if (!db) return;
+  if (!editor) return;
+  editor.applySelectedMetadataEdit(
+    db,
+    source,
+    target,
+    selectedRolloutObjects.value
+  );
+
+  const { statement, errors, fatal } = await generateDiffDDL(
+    db,
+    source,
+    target,
+    /* !allowEmptyDiffDDLWithConfigChange */ false
+  );
+  if (errors.length > 0) {
+    return cleanup(errors, fatal);
+  }
+
+  rawSQLPreviewState.value = statement;
+  rawSQLPreviewState.isFetching = false;
+};
 
 const handlePreviewIssue = async () => {
   const cleanup = (errors: string[], fatal: boolean) => {
