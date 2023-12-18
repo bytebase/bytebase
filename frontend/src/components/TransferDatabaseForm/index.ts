eedeb2b41b8174c186ba69da656827996344f9a5
ecmadao <wlec@outlook.com>
2023-12-18 16:38:19 +0800

    chore: ui update & fix (#9946)
    * chore: disallow preview issue if database not changed
    
    * chore: ui update
    
    * chore: update shadow
    
    * chore: refactor the alter schema drawer
    
    * chore: ui update & fix
    
    * chore: update

--- frontend/src/components/TransferDatabaseForm/index.ts
+++ frontend/src/components/TransferDatabaseForm/index.ts
@@ -1,11 +1,6 @@
 import MultipleDatabaseSelector from "./MultipleDatabaseSelector.vue";
-import TransferSingleDatabase from "./TransferSingleDatabase.vue";
 import TransferSourceSelector from "./TransferSourceSelector.vue";
 
 export * from "./utils";
 
-export {
-  MultipleDatabaseSelector,
-  TransferSourceSelector,
-  TransferSingleDatabase,
-};
+export { MultipleDatabaseSelector, TransferSourceSelector };
