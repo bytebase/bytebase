b13f446b041dd518985faf6aba6d6f5f8b3ccbba
zll600 <3400692417@qq.com>
2023-12-19 18:45:22 +0800

    chore: arm64 building

--- scripts/build_bytebase_docker.sh
+++ scripts/build_bytebase_docker.sh
@@ -18,6 +18,8 @@ docker build -f ./scripts/Dockerfile \
     --build-arg GIT_COMMIT="$(git rev-parse HEAD)"\
     --build-arg BUILD_TIME="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"  \
     --build-arg BUILD_USER="$(id -u -n)" \
+    --progress=plain \
+    --network=host \
     -t bytebase/bytebase .
 
 echo "${GREEN}Completed building Bytebase docker image ${VERSION}.${NC}"
