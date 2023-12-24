11c64bd284b36a84013da36db7b7402bb22a83eb
zll600 <3400692417@qq.com>
2023-12-24 15:02:59 +0800

    fix: stopInstance for ghost test

--- backend/tests/ghost_test.go
+++ backend/tests/ghost_test.go
@@ -354,6 +354,7 @@ func TestGhostTenant(t *testing.T) {
 func getMySQLInstanceForGhostTest(t *testing.T) (int, func(), error) {
 	mysqlPort := getTestPort()
 	stopInstance := mysql.SetupTestInstance(t, mysqlPort, mysqlBinDir)
+	defer stopInstance()
 
 	mysqlDB, err := connectTestMySQL(mysqlPort, "")
 	if err != nil {
