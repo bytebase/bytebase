fc18de63f4f358f04852c590d439d68b2ff289cf
boojack <stevenlgtm@gmail.com>
2023-12-26 16:03:14 +0800

    chore(sql-editor): use NEllipsis in hover panel (#10058)
    Former-commit-id: 90b1750ae82f0a53a047bb31081ee9c8973eb68c

--- frontend/src/views/sql-editor/SecondarySidebar/InfoTabPane/SchemaPanel/HoverPanel/InfoItem.vue
+++ frontend/src/views/sql-editor/SecondarySidebar/InfoTabPane/SchemaPanel/HoverPanel/InfoItem.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="contents">
-    <div class="text-gray-500 font-medium">
-      <slot name="title">{{ title }}</slot>
-    </div>
-    <div class="text-main text-right">
-      <slot name="default"></slot>
-    </div>
+  <div class="w-full grid grid-cols-2 gap-x-2">
+    <NEllipsis class="max-w-1/3">
+      <span class="text-gray-500 font-medium whitespace-nowrap">
+        <slot name="title">{{ title }}</slot>
+      </span>
+    </NEllipsis>
+    <NEllipsis class="max-w-2/3 text-right">
+      <span class="whitespace-nowrap">
+        <slot name="default"></slot>
+      </span>
+    </NEllipsis>
   </div>
 </template>
 
 <script setup lang="ts">
+import { NEllipsis } from "naive-ui";
+
 defineProps<{
   title?: string;
 }>();
