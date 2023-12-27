967853e83d9d90ba43e0bbb7ef3dc2b199916ed9
boojack <stevenlgtm@gmail.com>
2023-12-27 16:23:52 +0800

    chore(sql-editor): add search history in database tree (#10077)
    * chore(sql-editor): add search history in database tree
    
    * chore: update
    
    Former-commit-id: 4dc51751c0029851a3e46c382b6c62fe156dd4bc

--- frontend/src/views/sql-editor/AsidePanel/AsidePanel.vue
+++ frontend/src/views/sql-editor/AsidePanel/AsidePanel.vue
@@ -2,15 +2,12 @@
   <div class="aside-panel h-full flex flex-col overflow-hidden">
     <GroupingBar />
     <div class="flex-1 overflow-hidden">
-      <DatabaseTree v-model:search-pattern="searchPattern" />
+      <DatabaseTree />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
 import DatabaseTree from "./DatabaseTree.vue";
 import GroupingBar from "./GroupingBar";
-
-const searchPattern = ref("");
 </script>
