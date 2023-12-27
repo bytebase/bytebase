045f99499654f0bbe474e87d92d6c86e339bcf32
ecmadao <wlec@outlook.com>
2023-12-27 20:37:54 +0800

    chore: migrate text/textarea/password type input to BBTextField (#10088)
    * fix: filter removed project
    
    * chore: refactor sheet list
    
    * chore: update draft group
    
    * chore: i18n
    
    * chore: migrate input to BBTextField
    
    Former-commit-id: 4bfa5947cd1075d02efc1e36de346113b41851a2
    Former-commit-id: 9f16c6a3ddb218f2f2becbe6739e83a55558105f

--- frontend/src/components/SQLReview/components/RuleConfigComponents/StringComponent.vue
+++ frontend/src/components/SQLReview/components/RuleConfigComponents/StringComponent.vue
@@ -1,14 +1,9 @@
 <template>
-  <input
+  <BBTextField
     :value="value"
-    type="text"
     :disabled="disabled"
-    :class="[
-      'shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full border-gray-300 rounded-md',
-      disabled && 'cursor-not-allowed',
-    ]"
-    :placeholder="config.payload.default as string"
-    @change="$emit('update:value', ($event.target as HTMLInputElement).value)"
+    :placeholder="`${config.payload.default}`"
+    @update:value="$emit('update:value', $event)"
   />
 </template>
 
