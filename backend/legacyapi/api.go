1c1ce8d2066d7baa1df56b5335f57ba013037174
h3n4l <oysterdays@gmail.com>
2023-12-29 17:58:17 +0800

    chore: remove some dead code (#10122)
    Former-commit-id: 3590d0ab2a47d15e0be37f9aeb707fc6eeee64cf
    Former-commit-id: 343141caf9054f6048bd379a2424ec454a0ae801

--- backend/legacyapi/api.go
+++ backend/legacyapi/api.go
@@ -1,9 +1,6 @@
 // Package api provides the definition of all APIs.
 package api
 
-// UnknownID is the ID for unknowns.
-const UnknownID = -1
-
 // RowStatus is the status for a row.
 type RowStatus string
 
@@ -22,9 +19,4 @@ const (
 	ASC SortOrder = "ASC"
 	// DESC is the sort order to return in descending order.
 	DESC SortOrder = "DESC"
-)
-
-const (
-	// DefaultPageSize is the default number of items in a page.
-	DefaultPageSize = 1000
 )
