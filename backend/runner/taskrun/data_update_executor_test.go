533343a7928e3f54215a07b4c70f77649746913b
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2023-12-29 00:24:24 -0800

    chore: post prior backup activity (#10118)
    * chore: post prior backup activity
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 54a59da9ba4c2faf67e6de707537775589dfae4e
    Former-commit-id: 29511be67223b709f3e5b9bbaafa89f3ab0cc51d

--- backend/runner/taskrun/data_update_executor_test.go
+++ backend/runner/taskrun/data_update_executor_test.go
@@ -7,6 +7,6 @@ import (
 )
 
 func TestUpdateToSelect(t *testing.T) {
-	got := updateToSelect("UPDATE hello SET column1 = value1, column2 = value2 WHERE id > 3;", "todozp", "113")
+	got, _ := updateToSelect("UPDATE hello SET column1 = value1, column2 = value2 WHERE id > 3;", "todozp", "113")
 	assert.Equal(t, "CREATE TABLE `todozp`.`hello_113` LIKE hello; INSERT INTO `todozp`.`hello_113` SELECT * FROM hello WHERE id > 3;", got)
 }
