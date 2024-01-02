20aa16060fcfd1aecc4e3b2bc4f58c3d9c1d5cba
rebelice <yangrebelice@gmail.com>
2024-01-02 18:01:31 +0800

    feat: implement rollback UPDATE for MySQL

---
+++ backend/plugin/parser/mysql/rollback_test.go
@@ -0,0 +1,51 @@
+package mysql
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+)
+
+type rollbackCase struct {
+	Input  string
+	Result []string
+}
+
+func TestRollback(t *testing.T) {
+	tests := []rollbackCase{}
+
+	const (
+		record = false
+	)
+	var (
+		filepath = "test-data/test_rollback.yaml"
+	)
+
+	a := require.New(t)
+	yamlFile, err := os.Open(filepath)
+	a.NoError(err)
+
+	byteValue, err := io.ReadAll(yamlFile)
+	a.NoError(yamlFile.Close())
+	a.NoError(err)
+	a.NoError(yaml.Unmarshal(byteValue, &tests))
+
+	for i, t := range tests {
+		result, err := TransformDMLToSelect(t.Input, "db", "backupDB", "_rollback")
+		a.NoError(err)
+		if record {
+			tests[i].Result = result
+		} else {
+			a.Equal(t.Result, result, t.Input)
+		}
+	}
+	if record {
+		byteValue, err := yaml.Marshal(tests)
+		a.NoError(err)
+		err = os.WriteFile(filepath, byteValue, 0644)
+		a.NoError(err)
+	}
+}
