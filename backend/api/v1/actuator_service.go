5b501adafc8091b0281b614588fb765fe06c1041
p0ny <p0uy@outlook.com>
2024-01-08 16:07:46 +0800

    impl return unlicensed features

--- backend/api/v1/actuator_service.go
+++ backend/api/v1/actuator_service.go
@@ -10,8 +10,12 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	"github.com/pkg/errors"
+
+	"github.com/bytebase/bytebase/backend/api/auth"
 	"github.com/bytebase/bytebase/backend/common/log"
 	"github.com/bytebase/bytebase/backend/component/config"
+	enterprise "github.com/bytebase/bytebase/backend/enterprise/api"
 	api "github.com/bytebase/bytebase/backend/legacyapi"
 	"github.com/bytebase/bytebase/backend/store"
 	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
@@ -23,14 +27,16 @@ type ActuatorService struct {
 	store           *store.Store
 	profile         *config.Profile
 	errorRecordRing *api.ErrorRecordRing
+	licenseService  enterprise.LicenseService
 }
 
 // NewActuatorService creates a new ActuatorService.
-func NewActuatorService(store *store.Store, profile *config.Profile, errorRecordRing *api.ErrorRecordRing) *ActuatorService {
+func NewActuatorService(store *store.Store, profile *config.Profile, errorRecordRing *api.ErrorRecordRing, licenseService enterprise.LicenseService) *ActuatorService {
 	return &ActuatorService{
 		store:           store,
 		profile:         profile,
 		errorRecordRing: errorRecordRing,
+		licenseService:  licenseService,
 	}
 }
 
@@ -97,24 +103,125 @@ func (s *ActuatorService) getServerInfo(ctx context.Context) (*v1pb.ActuatorInfo
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 
+	usedFeatures, err := s.getUsedFeatures(ctx)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to get used features, error: %v", err)
+	}
+	unlicensedFeatures := s.getUnlicensedFeatures(usedFeatures)
+	var unlicensedFeaturesString []string
+	for _, f := range unlicensedFeatures {
+		unlicensedFeaturesString = append(unlicensedFeaturesString, f.String())
+	}
+
 	serverInfo := v1pb.ActuatorInfo{
-		Version:          s.profile.Version,
-		GitCommit:        s.profile.GitCommit,
-		Readonly:         s.profile.Readonly,
-		Saas:             s.profile.SaaS,
-		DemoName:         s.profile.DemoName,
-		NeedAdminSetup:   count == 0,
-		ExternalUrl:      setting.ExternalUrl,
-		DisallowSignup:   setting.DisallowSignup,
-		Require_2Fa:      setting.Require_2Fa,
-		LastActiveTime:   timestamppb.New(time.Unix(s.profile.LastActiveTs, 0)),
-		WorkspaceId:      workspaceID,
-		GitopsWebhookUrl: setting.GitopsWebhookUrl,
-		Debug:            slog.Default().Enabled(ctx, slog.LevelDebug),
-		Lsp:              s.profile.Lsp,
-		PreUpdateBackup:  s.profile.PreUpdateBackup,
-		IamGuard:         s.profile.DevelopmentIAM,
+		Version:            s.profile.Version,
+		GitCommit:          s.profile.GitCommit,
+		Readonly:           s.profile.Readonly,
+		Saas:               s.profile.SaaS,
+		DemoName:           s.profile.DemoName,
+		NeedAdminSetup:     count == 0,
+		ExternalUrl:        setting.ExternalUrl,
+		DisallowSignup:     setting.DisallowSignup,
+		Require_2Fa:        setting.Require_2Fa,
+		LastActiveTime:     timestamppb.New(time.Unix(s.profile.LastActiveTs, 0)),
+		WorkspaceId:        workspaceID,
+		GitopsWebhookUrl:   setting.GitopsWebhookUrl,
+		Debug:              slog.Default().Enabled(ctx, slog.LevelDebug),
+		Lsp:                s.profile.Lsp,
+		PreUpdateBackup:    s.profile.PreUpdateBackup,
+		IamGuard:           s.profile.DevelopmentIAM,
+		UnlicensedFeatures: unlicensedFeaturesString,
 	}
 
 	return &serverInfo, nil
+}
+
+func (s *ActuatorService) getUsedFeatures(ctx context.Context) ([]api.FeatureType, error) {
+	var features []api.FeatureType
+
+	// idp
+	idps, err := s.store.ListIdentityProviders(ctx, &store.FindIdentityProviderMessage{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to list identity providers")
+	}
+	if len(idps) > 0 {
+		features = append(features, api.FeatureSSO)
+	}
+
+	// setting
+	settingName := api.SettingBrandingLogo
+	brandingLogo, err := s.store.GetSettingV2(ctx, &store.FindSettingMessage{Name: &settingName})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get branding logo setting")
+	}
+	if brandingLogo != nil && brandingLogo.Value != "" {
+		features = append(features, api.FeatureBranding)
+	}
+
+	settingName = api.SettingWatermark
+	watermark, err := s.store.GetSettingV2(ctx, &store.FindSettingMessage{Name: &settingName})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get watermark setting")
+	}
+	if watermark != nil && watermark.Value == "1" {
+		features = append(features, api.FeatureWatermark)
+	}
+
+	settingName = api.SettingPluginOpenAIKey
+	openAIKey, err := s.store.GetSettingV2(ctx, &store.FindSettingMessage{Name: &settingName})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get openai key setting")
+	}
+	if openAIKey != nil && openAIKey.Value != "" {
+		features = append(features, api.FeaturePluginOpenAI)
+	}
+
+	setting, err := s.store.GetWorkspaceGeneralSetting(ctx)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get workspace general setting")
+	}
+	if setting.DisallowSignup && !s.profile.SaaS {
+		features = append(features, api.FeatureDisallowSignup)
+	}
+	if setting.Require_2Fa {
+		features = append(features, api.Feature2FA)
+	}
+	if setting.GetTokenDuration().GetSeconds() > 0 && float64(setting.GetTokenDuration().GetSeconds()) != auth.DefaultTokenDuration.Seconds() {
+		features = append(features, api.FeatureSecureToken)
+	}
+	if setting.GetAnnouncement().GetText() != "" {
+		features = append(features, api.FeatureAnnouncement)
+	}
+
+	// environment tier
+	environments, err := s.store.ListEnvironmentV2(ctx, &store.FindEnvironmentMessage{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to list environments")
+	}
+	for _, env := range environments {
+		if env.Protected {
+			features = append(features, api.FeatureEnvironmentTierPolicy)
+			break
+		}
+	}
+
+	// database group
+	databaseGroups, err := s.store.ListDatabaseGroups(ctx, &store.FindDatabaseGroupMessage{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to list database groups")
+	}
+	if len(databaseGroups) > 0 {
+		features = append(features, api.FeatureDatabaseGrouping)
+	}
+	return features, nil
+}
+
+func (s *ActuatorService) getUnlicensedFeatures(features []api.FeatureType) []api.FeatureType {
+	var unlicensedFeatures []api.FeatureType
+	for _, feature := range features {
+		if err := s.licenseService.IsFeatureEnabled(feature); err != nil {
+			unlicensedFeatures = append(unlicensedFeatures, feature)
+		}
+	}
+	return unlicensedFeatures
 }
