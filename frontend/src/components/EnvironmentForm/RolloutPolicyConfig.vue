eac621784b33ac81f42b07b7c9a69ed340da7001
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-01-08 11:55:32 +0800

    feat: support custom project roles

--- frontend/src/components/EnvironmentForm/RolloutPolicyConfig.vue
+++ frontend/src/components/EnvironmentForm/RolloutPolicyConfig.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex flex-col gap-y-2">
+  <div class="flex flex-col items-start gap-y-2">
     <div class="flex flex-col gap-y-2">
       <NRadio
-        :checked="rolloutPolicy.automatic"
+        :checked="isAutomaticRolloutChecked"
         :disabled="disabled"
         style="--n-label-padding: 0 0 0 1rem"
         @update:checked="selectAutomaticRollout"
@@ -103,6 +103,33 @@
             {{ $t("policy.rollout.manual-by-issue-creator") }}
           </div>
         </NCheckbox>
+        <div class="flex flex-col gap-y-1">
+          <NCheckbox
+            :checked="customProjectRoles.checked"
+            :disabled="disabled"
+            style="--n-label-padding: 0 0 0 1rem"
+            @update:checked="toggleCustomProjectRoles($event)"
+          >
+            <div class="textlabel">
+              {{ $t("policy.rollout.manual-by-custom-project-roles") }}
+            </div>
+          </NCheckbox>
+          <div v-if="customProjectRoles.checked" class="pl-8">
+            <ProjectRoleSelect
+              :roles="customProjectRoles.roles"
+              :multiple="true"
+              :filter="filterProjectRole"
+              :status="
+                customProjectRoles.roles.length === 0 ? 'error' : undefined
+              "
+              :tooltip-props="{
+                placement: 'right',
+              }"
+              style="width: 24rem"
+              @update:roles="handleUpdateCustomProjectRoles"
+            />
+          </div>
+        </div>
       </div>
     </div>
     <div class="flex flex-col gap-y-2">
@@ -128,6 +155,7 @@ import { ref, watch } from "vue";
 import { computed } from "vue";
 import { PresetRoleType, VirtualRoleType } from "@/types";
 import { Policy, RolloutPolicy } from "@/types/proto/v1/org_policy_service";
+import { Role } from "@/types/proto/v1/role_service";
 import FeatureBadge from "../FeatureGuard/FeatureBadge.vue";
 
 const props = defineProps<{
@@ -139,7 +167,14 @@ const emit = defineEmits<{
 }>();
 
 const rolloutPolicy = ref(cloneDeep(props.policy.rolloutPolicy!));
+const PresetProjectRoles = [
+  PresetRoleType.PROJECT_OWNER,
+  PresetRoleType.PROJECT_RELEASER,
+];
 
+const isAutomaticRolloutChecked = computed(() => {
+  return rolloutPolicy.value.automatic && !customProjectRoles.value.checked;
+});
 const isDBAChecked = computed(() => {
   return rolloutPolicy.value.workspaceRoles.includes(
     VirtualRoleType.WORKSPACE_DBA
@@ -163,10 +198,31 @@ const isProjectReleaserChecked = computed(() => {
 const isIssueCreatorChecked = computed(() => {
   return rolloutPolicy.value.issueRoles.includes(VirtualRoleType.CREATOR);
 });
+
+const extractCustomProjectRolesFromRolloutPolicy = (rp: RolloutPolicy) => {
+  const customRoles = rp.projectRoles.filter((role) => {
+    return !PresetProjectRoles.includes(role);
+  });
+  return {
+    checked: customRoles.length > 0,
+    roles: customRoles,
+  };
+};
+const customProjectRoles = ref(
+  extractCustomProjectRolesFromRolloutPolicy(rolloutPolicy.value)
+);
+
 const isIssueLastApproverChecked = computed(() => {
-  return rolloutPolicy.value.issueRoles.includes(VirtualRoleType.LAST_APPROVER);
+  return (
+    rolloutPolicy.value.issueRoles.includes(VirtualRoleType.LAST_APPROVER) &&
+    !customProjectRoles.value.checked
+  );
 });
 const isManualRolloutByDedicatedRolesChecked = computed(() => {
+  if (customProjectRoles.value.checked) {
+    return true;
+  }
+
   if (rolloutPolicy.value.automatic) {
     return false;
   }
@@ -256,11 +312,60 @@ const toggleDedicatedRoles = (
   pull(patch.issueRoles, VirtualRoleType.LAST_APPROVER);
   update(patch);
 };
+const toggleCustomProjectRoles = (checked: boolean) => {
+  if (checked) {
+    customProjectRoles.value = {
+      checked: true,
+      roles: [],
+    };
+  } else {
+    const rp = rolloutPolicy.value;
+    // Remove custom project roles
+    // remaining preset project roles
+    const set = new Set(
+      rp.projectRoles.filter((role) => PresetProjectRoles.includes(role))
+    );
+    const patch = RolloutPolicy.fromPartial({
+      ...rp,
+      automatic: false,
+      projectRoles: Array.from(set),
+    });
+    pull(patch.issueRoles, VirtualRoleType.LAST_APPROVER);
+    customProjectRoles.value = {
+      checked: false,
+      roles: [],
+    };
+    update(patch);
+  }
+};
+const handleUpdateCustomProjectRoles = (roles: string[]) => {
+  const rp = rolloutPolicy.value;
+  const presetProjectRoleSet = new Set(
+    rp.projectRoles.filter((role) => PresetProjectRoles.includes(role))
+  );
+  const customProjectRoleSet = new Set(roles);
+  const patch = RolloutPolicy.fromPartial({
+    ...rp,
+    automatic: false,
+    projectRoles: [
+      ...Array.from(presetProjectRoleSet),
+      ...Array.from(customProjectRoleSet),
+    ],
+  });
+  pull(patch.issueRoles, VirtualRoleType.LAST_APPROVER);
+  customProjectRoles.value.roles = roles;
+  update(patch);
+};
+
+const filterProjectRole = (role: Role) => {
+  return !PresetProjectRoles.includes(role.name);
+};
 
 watch(
   () => props.policy.rolloutPolicy!,
   (p) => {
     rolloutPolicy.value = cloneDeep(p);
+    customProjectRoles.value = extractCustomProjectRolesFromRolloutPolicy(p);
   },
   { immediate: true, deep: true }
 );
