8007c212030ed578407a082ccc2a4c158213883f
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-01-08 00:19:33 -0800

    chore: split files (#10234)
    Former-commit-id: a0692a87be98168050f6cc7cba2460e717154c41
    Former-commit-id: cea55626baca01014f2cd8b630dec066f83a7c61

---
+++ backend/plugin/schema/mysql/get_design_schema_test.go
@@ -0,0 +1,57 @@
+package mysql
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/encoding/protojson"
+	"gopkg.in/yaml.v3"
+
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+func TestGetDesignSchema(t *testing.T) {
+	type designTest struct {
+		Baseline string
+		Target   string
+		Result   string
+	}
+
+	const (
+		record = false
+	)
+	var (
+		filepath = "testdata/get_design_schema.yaml"
+	)
+
+	a := require.New(t)
+	yamlFile, err := os.Open(filepath)
+	a.NoError(err)
+
+	tests := []designTest{}
+	byteValue, err := io.ReadAll(yamlFile)
+	a.NoError(yamlFile.Close())
+	a.NoError(err)
+	a.NoError(yaml.Unmarshal(byteValue, &tests))
+
+	for i, t := range tests {
+		targetMeta := &storepb.DatabaseSchemaMetadata{}
+		a.NoError(protojson.Unmarshal([]byte(t.Target), targetMeta))
+		result, err := GetDesignSchema(t.Baseline, targetMeta)
+		a.NoError(err)
+		if record {
+			tests[i].Result = result
+		} else {
+			a.Equal(t.Result, result)
+		}
+	}
+
+	if record {
+		byteValue, err := yaml.Marshal(tests)
+		a.NoError(err)
+		err = os.WriteFile(filepath, byteValue, 0644)
+		a.NoError(err)
+	}
+}
