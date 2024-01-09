76ae463e66da0f1aea7f0dea3d9463fe85cb2552
boojack <stevenlgtm@gmail.com>
2024-01-09 21:16:04 +0800

    chore: split dashboard routes (#10251)
    * chore: split dashboard routes
    
    * chore: stash
    
    * chore: update
    
    Former-commit-id: 20d23da890b851583c0c7e72f0e2024b85868466

---
+++ frontend/src/router/dashboard/index.ts
@@ -0,0 +1,36 @@
+import { RouteRecordRaw } from "vue-router";
+import BodyLayout from "@/layouts/BodyLayout.vue";
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
+import databaseRoutes from "./database";
+import environmentRoutes from "./environment";
+import instanceRoutes from "./instance";
+import issueRoutes from "./issue";
+import projectRoutes from "./project";
+import projectV1Routes from "./projectV1";
+import workspaceRoutes from "./workspace";
+import workspaceSettingRoutes from "./workspaceSetting";
+
+const dashboardRoutes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    component: DashboardLayout,
+    children: [
+      {
+        path: "",
+        components: { body: BodyLayout },
+        children: [
+          ...workspaceRoutes,
+          ...workspaceSettingRoutes,
+          ...environmentRoutes,
+          ...instanceRoutes,
+          ...projectRoutes,
+          ...projectV1Routes,
+          ...issueRoutes,
+          ...databaseRoutes,
+        ],
+      },
+    ],
+  },
+];
+
+export default dashboardRoutes;
