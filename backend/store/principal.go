88537b9ea54cc80eabbabb92fb51013a80f45c69
ecmadao <wlec@outlook.com>
2024-01-10 12:13:27 +0800

    fix: skip empty role string

--- backend/store/principal.go
+++ backend/store/principal.go
@@ -216,6 +216,9 @@ func (*Store) listUserImpl(ctx context.Context, tx *Tx, find *FindUserMessage) (
 			return nil, errors.Wrapf(err, "failed to scan roles")
 		}
 		for _, r := range rolesString {
+			if r == "" {
+				continue
+			}
 			userMessage.Roles = append(userMessage.Roles, api.Role(r))
 		}
 		if userMessage.ID == api.SystemBotID {
