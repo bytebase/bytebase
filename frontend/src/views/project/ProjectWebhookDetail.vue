334d9a2eafa618404e0dc30ec008298059855a8c
ecmadao <wlec@outlook.com>
2024-01-11 18:36:43 +0800

    fix: test

--- frontend/src/views/project/ProjectWebhookDetail.vue
+++ frontend/src/views/project/ProjectWebhookDetail.vue
@@ -4,25 +4,26 @@
       <div class="flex flex-row space-x-2 items-center">
         <!-- This awkward code is author couldn't figure out proper way to use dynamic src under vite
                    https://github.com/vitejs/vite/issues/1265 -->
+        <!-- TODO(ed): extract the code -->
         <template v-if="projectWebhook.type === Webhook_Type.TYPE_SLACK">
-          <img class="h-6 w-6" src="../assets/slack-logo.png" alt="" />
+          <img class="h-6 w-6" src="../../assets/slack-logo.png" alt="" />
         </template>
         <template v-else-if="projectWebhook.type === Webhook_Type.TYPE_DISCORD">
-          <img class="h-6 w-6" src="../assets/discord-logo.svg" />
+          <img class="h-6 w-6" src="../../assets/discord-logo.svg" />
         </template>
         <template v-else-if="projectWebhook.type === Webhook_Type.TYPE_TEAMS">
-          <img class="h-6 w-6" src="../assets/teams-logo.svg" />
+          <img class="h-6 w-6" src="../../assets/teams-logo.svg" />
         </template>
         <template
           v-else-if="projectWebhook.type === Webhook_Type.TYPE_DINGTALK"
         >
-          <img class="h-6 w-6" src="../assets/dingtalk-logo.png" />
+          <img class="h-6 w-6" src="../../assets/dingtalk-logo.png" />
         </template>
         <template v-else-if="projectWebhook.type === Webhook_Type.TYPE_FEISHU">
-          <img class="h-6 w-6" src="../assets/feishu-logo.webp" />
+          <img class="h-6 w-6" src="../../assets/feishu-logo.webp" />
         </template>
         <template v-else-if="projectWebhook.type === Webhook_Type.TYPE_WECOM">
-          <img class="h-6 w-6" src="../assets/wecom-logo.png" />
+          <img class="h-6 w-6" src="../../assets/wecom-logo.png" />
         </template>
         <template v-else-if="projectWebhook.type === Webhook_Type.TYPE_CUSTOM">
           <heroicons-outline:puzzle class="w-6 h-6" />
