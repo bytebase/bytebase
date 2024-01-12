302c57f471acdb037b89e521fb0156fb16e7d041
h3n4l <oysterdays@gmail.com>
2024-01-12 16:25:12 +0800

    chore: update

--- backend/plugin/schema/mysql/utils.go
+++ backend/plugin/schema/mysql/utils.go
@@ -17,7 +17,17 @@ const (
 var (
 	// https://dev.mysql.com/doc/refman/8.0/en/data-type-defaults.html
 	// expressionDefaultOnlyTypes is a list of types that only accept expression as default
-	// value.
+	// value. While we restore the following types, we should not restore the default null.
+	// +-------+--------------------------------------------------------------------+
+	// | Table | Create Table                                                       |
+	// +-------+--------------------------------------------------------------------+
+	// | u     | CREATE TABLE `u` (                                                 |
+	// |       |   `b` blob,                                                        |
+	// |       |   `t` text,                                                        |
+	// |       |   `g` geometry DEFAULT NULL,                                       |
+	// |       |   `j` json DEFAULT NULL                                            |
+	// |       | ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci |
+	// +-------+--------------------------------------------------------------------+
 	expressionDefaultOnlyTypes = map[string]bool{
 		// BLOB & TEXT
 		// https://dev.mysql.com/doc/refman/8.0/en/blob.html
@@ -30,6 +40,7 @@ var (
 		"MEDIUMTEXT": true,
 		"LONGTEXT":   true,
 
+		// In practice, the following types restore the default null by mysqldump.
 		// // GEOMETRY
 		// "GEOMETRY": true,
 		// // JSON
