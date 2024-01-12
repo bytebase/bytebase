ec3704b48c9c310359b62e9c07f34a7f23dbb2eb
rebelice <yangrebelice@gmail.com>
2024-01-12 16:19:34 +0800

    feat: sync materialized views and support them in getQuerySpan (#10302)
    Former-commit-id: 7cba0cc79eed1dcda31970328055ac0bd6795264

--- frontend/src/types/proto/store/database.ts
+++ frontend/src/types/proto/store/database.ts
@@ -56,6 +56,8 @@ export interface SchemaMetadata {
   streams: StreamMetadata[];
   /** The routines is the list of routines in a schema, currently, only used for Snowflake. */
   tasks: TaskMetadata[];
+  /** The materialized_views is the list of materialized views in a schema. */
+  materializedViews: MaterializedViewMetadata[];
 }
 
 export interface TaskMetadata {
@@ -839,7 +841,16 @@ export const DatabaseSchemaMetadata = {
 };
 
 function createBaseSchemaMetadata(): SchemaMetadata {
-  return { name: "", tables: [], externalTables: [], views: [], functions: [], streams: [], tasks: [] };
+  return {
+    name: "",
+    tables: [],
+    externalTables: [],
+    views: [],
+    functions: [],
+    streams: [],
+    tasks: [],
+    materializedViews: [],
+  };
 }
 
 export const SchemaMetadata = {
@@ -864,6 +875,9 @@ export const SchemaMetadata = {
     }
     for (const v of message.tasks) {
       TaskMetadata.encode(v!, writer.uint32(58).fork()).ldelim();
+    }
+    for (const v of message.materializedViews) {
+      MaterializedViewMetadata.encode(v!, writer.uint32(66).fork()).ldelim();
     }
     return writer;
   },
@@ -924,6 +938,13 @@ export const SchemaMetadata = {
 
           message.tasks.push(TaskMetadata.decode(reader, reader.uint32()));
           continue;
+        case 8:
+          if (tag !== 66) {
+            break;
+          }
+
+          message.materializedViews.push(MaterializedViewMetadata.decode(reader, reader.uint32()));
+          continue;
       }
       if ((tag & 7) === 4 || tag === 0) {
         break;
@@ -948,6 +969,9 @@ export const SchemaMetadata = {
         ? object.streams.map((e: any) => StreamMetadata.fromJSON(e))
         : [],
       tasks: globalThis.Array.isArray(object?.tasks) ? object.tasks.map((e: any) => TaskMetadata.fromJSON(e)) : [],
+      materializedViews: globalThis.Array.isArray(object?.materializedViews)
+        ? object.materializedViews.map((e: any) => MaterializedViewMetadata.fromJSON(e))
+        : [],
     };
   },
 
@@ -974,6 +998,9 @@ export const SchemaMetadata = {
     if (message.tasks?.length) {
       obj.tasks = message.tasks.map((e) => TaskMetadata.toJSON(e));
     }
+    if (message.materializedViews?.length) {
+      obj.materializedViews = message.materializedViews.map((e) => MaterializedViewMetadata.toJSON(e));
+    }
     return obj;
   },
 
@@ -989,6 +1016,7 @@ export const SchemaMetadata = {
     message.functions = object.functions?.map((e) => FunctionMetadata.fromPartial(e)) || [];
     message.streams = object.streams?.map((e) => StreamMetadata.fromPartial(e)) || [];
     message.tasks = object.tasks?.map((e) => TaskMetadata.fromPartial(e)) || [];
+    message.materializedViews = object.materializedViews?.map((e) => MaterializedViewMetadata.fromPartial(e)) || [];
     return message;
   },
 };
