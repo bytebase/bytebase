81caa5e35dbdff20d058a533e468117f422ded48
ecmadao <wlec@outlook.com>
2024-01-15 16:27:44 +0800

    chore: refactor component (#10333)
    * chore: migrate quick action with iam permissions
    
    * fix: sidebar default expand
    
    * fix: eslint
    
    * chore: update
    
    * fix: invalid role for new users
    
    * chore: refactor component
    
    * fix: eslint
    
    Former-commit-id: dd279ddfb258d224bbb13ac1d56b4f28d4dbc952

---
+++ frontend/src/components/misc/NoDataPlaceholder.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="border-4 border-dashed border-gray-200 rounded-lg h-96 flex flex-col justify-center items-center"
+  >
+    <div class="text-center flex flex-col justify-center items-center">
+      <img
+        src="@/assets/illustration/no-data.webp"
+        class="!w-auto max-h-[20vh]"
+        v-bind="imgAttrs"
+      />
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { HTMLAttributes } from "vue";
+
+defineProps<{
+  imgAttrs?: HTMLAttributes;
+}>();
+</script>
