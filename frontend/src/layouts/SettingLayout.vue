3420b61ce219d02f65b6974b0c10c43fc8e2e2cf
boojack <stevenlgtm@gmail.com>
2024-01-15 18:11:23 +0800

    fix: setting permission check (#10340)
    Former-commit-id: 460737a4c1c804500ce1fafef13a7a0f3f3a5ac2
    Former-commit-id: 5d0a6bad987bd072c3c9fa7039f49d02447e8a77

--- frontend/src/layouts/SettingLayout.vue
+++ frontend/src/layouts/SettingLayout.vue
@@ -4,8 +4,26 @@
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: "SettingLayout",
-};
+<script lang="ts" setup>
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { useCurrentUserV1 } from "@/store";
+import { hasWorkspacePermissionV2 } from "@/utils";
+
+const router = useRouter();
+const currentUser = useCurrentUserV1();
+
+onMounted(() => {
+  const hasPermission = hasWorkspacePermissionV2(
+    currentUser.value,
+    "bb.settings.set"
+  );
+
+  if (!hasPermission) {
+    router.push({
+      name: "error.403",
+      replace: false,
+    });
+  }
+});
 </script>
