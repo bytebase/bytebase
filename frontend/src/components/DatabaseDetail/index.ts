238a0f174799ffc9aa848ab77a07aa0bd8918a05
ecmadao <wlec@outlook.com>
2024-01-16 13:38:25 +0800

    chore: migrate database permissions (#10353)
    * chore: migrate quick action with iam permissions
    
    * fix: sidebar default expand
    
    * fix: eslint
    
    * chore: update
    
    * fix: invalid role for new users
    
    * chore: refactor component
    
    * fix: eslint
    
    * chore: update project select
    
    * chore: permission check via route
    
    * chore: refactor
    
    * chore: update branch permission
    
    * chore: update
    
    * chore: update changelist permissions
    
    * chore: update
    
    * chore: migrate database permissions
    
    Former-commit-id: 89665c7f5b65d747639e2939e6a3784d42ea42bd
    Former-commit-id: a1963ccb438e1e849d3923edcc502d8d70334dc3

--- frontend/src/components/DatabaseDetail/index.ts
+++ frontend/src/components/DatabaseDetail/index.ts
@@ -1,5 +1,11 @@
 import PITRRestoreButton from "./PITRRestoreButton.vue";
 import SQLEditorButtonV1 from "./SQLEditorButtonV1.vue";
+import SchemaDiagramButton from "./SchemaDiagramButton.vue";
 import { DatabaseSettingsPanel } from "./Settings";
 
-export { PITRRestoreButton, SQLEditorButtonV1, DatabaseSettingsPanel };
+export {
+  PITRRestoreButton,
+  SQLEditorButtonV1,
+  DatabaseSettingsPanel,
+  SchemaDiagramButton,
+};
