acf1176ba55cae1c3bc839139a685d2bc5d3168b
steven <stevenlgtm@gmail.com>
2024-01-16 11:48:25 +0800

    chore: update

--- frontend/src/views/DashboardSidebar.vue
+++ frontend/src/views/DashboardSidebar.vue
@@ -160,7 +160,7 @@ const navigationKbarActions = computed(() => {
       shortcut: ["g", "e"],
       section: t("kbar.navigation"),
       keywords: "navigation",
-      perform: () => router.push({ name: "workspace.environment" }),
+      perform: () => router.push({ name: "workspace.environment.dashboar" }),
     })
   );
   if (shouldShowSyncSchemaEntry.value) {
