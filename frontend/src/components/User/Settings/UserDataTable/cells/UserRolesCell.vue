2d4eea71e6345fddafe65fe9e0af56107bc1c689
boojack <stevenlgtm@gmail.com>
2024-01-22 10:53:32 +0800

    chore: sort member roles (#10432)

--- frontend/src/components/User/Settings/UserDataTable/cells/UserRolesCell.vue
+++ frontend/src/components/User/Settings/UserDataTable/cells/UserRolesCell.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-row items-center flex-wrap gap-2">
-    <NTag v-for="role in user.roles" :key="role">
+    <NTag v-for="role in sortRoles(user.roles)" :key="role">
       {{ displayRoleTitle(role) }}
     </NTag>
   </div>
@@ -9,7 +9,7 @@
 <script lang="ts" setup>
 import { NTag } from "naive-ui";
 import { User } from "@/types/proto/v1/auth_service";
-import { displayRoleTitle } from "@/utils";
+import { displayRoleTitle, sortRoles } from "@/utils";
 
 defineProps<{
   user: User;
