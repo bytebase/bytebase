5e73bf339b152959ceb0cdf3528cd68628d1a0e5
boojack <stevenlgtm@gmail.com>
2024-01-23 17:34:23 +0800

    chore: fix role type checks (#10471)
    * chore: fix role type checks
    
    * chore: update
    
    Former-commit-id: b655773ef9ac8a9b78b94caad445129dccd29b9b

--- frontend/src/utils/iam/role.ts
+++ frontend/src/utils/iam/role.ts
@@ -7,19 +7,15 @@ import {
 } from "@/types";
 
 export const isWorkspaceLevelRole = (role: string) => {
-  const roleStore = useRoleStore();
-  return (
-    WorkspaceLevelRoles.includes(role) &&
-    roleStore
-      .getRoleByName(role)
-      ?.permissions.every((permission) =>
-        WORKSPACE_PERMISSIONS.includes(permission as WorkspacePermission)
-      )
-  );
+  return useRoleStore()
+    .getRoleByName(role)
+    ?.permissions.every((permission) =>
+      WORKSPACE_PERMISSIONS.includes(permission as WorkspacePermission)
+    );
 };
 
 export const isProjectLevelRole = (role: string) => {
-  return ProjectLevelRoles.includes(role) || !isWorkspaceLevelRole(role);
+  return !isWorkspaceLevelRole(role);
 };
 
 export const isCustomRole = (role: string) => {
