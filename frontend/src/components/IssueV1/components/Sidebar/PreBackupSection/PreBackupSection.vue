ff26a1b4a6c9eb8deb5fe1f17731d6b5a3f20592
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-01-23 15:30:08 +0800

    chore: update tooltips

--- frontend/src/components/IssueV1/components/Sidebar/PreBackupSection/PreBackupSection.vue
+++ frontend/src/components/IssueV1/components/Sidebar/PreBackupSection/PreBackupSection.vue
@@ -3,10 +3,19 @@
     v-if="showPreBackupSection"
     class="flex items-center justify-between gap-x-2 min-h-[34px] whitespace-nowrap"
   >
-    <div class="textlabel flex items-center">
-      {{ $t("task.prior-backup") }}
-      <BBBetaBadge class="scale-75" />
-    </div>
+    <NTooltip>
+      <template #trigger>
+        <div class="textlabel flex items-center">
+          {{ $t("task.prior-backup") }}
+          <BBBetaBadge class="scale-75" />
+        </div>
+      </template>
+      <template #default>
+        <div class="max-w-[20rem]">
+          {{ $t("task.prior-backup-tips") }}
+        </div>
+      </template>
+    </NTooltip>
     <div class="flex items-center gap-x-2">
       <PreBackupSwitch />
     </div>
@@ -14,6 +23,7 @@
 </template>
 
 <script lang="ts" setup>
+import { NTooltip } from "naive-ui";
 import { BBBetaBadge } from "@/bbkit";
 import PreBackupSwitch from "./PreBackupSwitch.vue";
 import { usePreBackupContext } from "./common";
