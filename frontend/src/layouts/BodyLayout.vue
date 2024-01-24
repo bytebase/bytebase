e8b5cae20cae6374825bdc5784556ee9b22deedd
boojack <stevenlgtm@gmail.com>
2024-01-24 16:58:17 +0800

    chore: fix padding style (#10503)

--- frontend/src/layouts/BodyLayout.vue
+++ frontend/src/layouts/BodyLayout.vue
@@ -194,14 +194,17 @@
               >
                 <QuickActionPanel
                   :quick-action-list="quickActionList"
-                  class="flex-1 pt-4"
+                  class="flex-1 pt-4 pb-2"
                 />
               </div>
             </div>
           </div>
         </HideInStandaloneMode>
         <!-- This area may scroll -->
-        <div id="bb-layout-main" class="md:min-w-0 flex-1 overflow-y-auto py-4">
+        <div
+          id="bb-layout-main"
+          class="md:min-w-0 flex-1 overflow-y-auto pt-2 pb-4"
+        >
           <!-- Start main area-->
           <router-view name="content" />
           <!-- End main area -->
