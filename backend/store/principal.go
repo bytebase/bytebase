bca0e172422ef799bcdf70dd7a44eafa762df555
p0ny <xz@bytebase.com>
2024-01-24 10:23:03 +0800

    chore: grant projectOwner to the first user (#10482)
    Former-commit-id: 943db4177e9506bf0161be97071f25a263ef4aa9
    Former-commit-id: 9e3709aa4967b75b0666292b44e995c29bb10777

--- backend/store/principal.go
+++ backend/store/principal.go
@@ -289,6 +289,9 @@ func (s *Store) CreateUser(ctx context.Context, create *UserMessage, creatorID i
 	// Grant the member Owner role if there is no existing member.
 	if firstMember {
 		roles = []api.Role{api.WorkspaceAdmin}
+		if s.profile.DevelopmentIAM {
+			roles = append(roles, api.ProjectOwner)
+		}
 	}
 	roles = uniq(roles)
 
