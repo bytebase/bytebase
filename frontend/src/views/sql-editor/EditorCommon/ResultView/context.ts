d708951768156f1ed4a430e75ceb37981f20c840
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-01-25 15:28:12 +0800

    chore: use table model (#10514)
    Former-commit-id: be5f813a2d4420da5fd6b7ebcca9c350fdd6908e
    Former-commit-id: 021525abada63bdbfff774094fa8c3ae59d3b50d

--- frontend/src/views/sql-editor/EditorCommon/ResultView/context.ts
+++ frontend/src/views/sql-editor/EditorCommon/ResultView/context.ts
@@ -1,3 +1,4 @@
+import { type Table } from "@tanstack/vue-table";
 import { type Ref, type InjectionKey, provide, inject } from "vue";
 
 export type SQLResultViewContext = {
@@ -9,6 +10,7 @@ export type SQLResultViewContext = {
     set: number; // The index of selected result set.
     row: number; // The row index of selected record.
     col: number; // The column index of selected cell.
+    table: Table<string[]> | undefined;
   }>;
 };
 
