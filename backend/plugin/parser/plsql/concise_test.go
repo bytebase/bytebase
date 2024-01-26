aa06b3a12d6adda1845c00174f5022433933e7aa
rebelice <yangrebelice@gmail.com>
2024-01-26 17:03:35 +0800

    feat: implement concise schema for Oracle (#10522)
    * feat: implement concise schema for Oracle
    
    * chore: fix linter error
    
    * chore: add tests and fix issues
    
    * chore: readable index name
    
    Former-commit-id: aadbc5e406b7a777685a4781dfa0c3e83f7e4c6b

---
+++ backend/plugin/parser/plsql/concise_test.go
@@ -0,0 +1,51 @@
+package plsql
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+)
+
+type conciseTestCase struct {
+	Input string
+	Want  string
+}
+
+func TestConciseSchema(t *testing.T) {
+	tests := []conciseTestCase{}
+	const (
+		record = true
+	)
+	var (
+		filepath = "test-data/test_concise_schema.yaml"
+	)
+
+	a := require.New(t)
+	yamlFile, err := os.Open(filepath)
+	a.NoError(err)
+
+	byteValue, err := io.ReadAll(yamlFile)
+	a.NoError(yamlFile.Close())
+	a.NoError(err)
+	a.NoError(yaml.Unmarshal(byteValue, &tests))
+
+	for i, t := range tests {
+		result, err := GetConciseSchema(t.Input)
+		a.NoError(err)
+		if record {
+			tests[i].Want = result
+		} else {
+			a.Equal(t.Want, result, t.Input)
+		}
+	}
+
+	if record {
+		byteValue, err := yaml.Marshal(tests)
+		a.NoError(err)
+		err = os.WriteFile(filepath, byteValue, 0644)
+		a.NoError(err)
+	}
+}
