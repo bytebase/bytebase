492d5d0362fcc89a2ca215aee8b62b3c05f85837
p0ny <xz@bytebase.com>
2024-02-01 14:49:17 +0800

    chore: set backoff initial interval to 5s (#10629)
    Former-commit-id: 27915109e2b17f09764647a0a856ae66bdae7a8d

--- backend/common/retry.go
+++ backend/common/retry.go
@@ -9,13 +9,16 @@ import (
 
 const (
 	// Just choose an empirical timeout value. The default 15 minutes seems too long.
-	timeout = 5 * time.Minute
+	timeout         = 5 * time.Minute
+	initialInterval = 5 * time.Second
 )
 
 // Retry uses exponential backoff with timeout.
 func Retry(ctx context.Context, fn func() error) error {
 	b := backoff.NewExponentialBackOff()
 	b.MaxElapsedTime = timeout
+	b.InitialInterval = initialInterval
+	b.Reset()
 	bWithContext := backoff.WithContext(b, ctx)
 	return backoff.Retry(fn, bWithContext)
 }
