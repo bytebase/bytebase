8f13fe3ab684717a80c7cb5954dca0c24d45e2e1
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-02-01 09:46:37 +0800

    feat: show candidate roles when action is not allowed

--- frontend/src/components/IssueV1/components/HeaderSection/Actions/review/ReviewActionButton.vue
+++ frontend/src/components/IssueV1/components/HeaderSection/Actions/review/ReviewActionButton.vue
@@ -21,7 +21,6 @@
 import { NButton, NTooltip } from "naive-ui";
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
-import { ErrorList } from "@/components/IssueV1/components/common";
 import {
   IssueReviewAction,
   issueReviewActionDisplayName,
@@ -29,7 +28,14 @@ import {
   allowUserToApplyReviewAction,
   useIssueContext,
 } from "@/components/IssueV1/logic";
+import ErrorList, { ErrorItem } from "@/components/misc/ErrorList.vue";
 import { useCurrentUserV1 } from "@/store";
+import { PresetRoleType } from "@/types";
+import {
+  ApprovalNode_GroupValue,
+  ApprovalNode_Type,
+} from "@/types/proto/v1/issue_service";
+import { displayRoleTitle } from "@/utils";
 
 const props = defineProps<{
   action: IssueReviewAction;
@@ -44,7 +50,7 @@ const { issue, reviewContext } = useIssueContext();
 const currentUser = useCurrentUserV1();
 
 const errors = computed(() => {
-  const errors: string[] = [];
+  const errors: ErrorItem[] = [];
 
   if (
     !allowUserToApplyReviewAction(
@@ -55,6 +61,47 @@ const errors = computed(() => {
     )
   ) {
     errors.push(t("issue.error.you-are-not-allowed-to-perform-this-action"));
+    const flow = reviewContext.flow.value;
+    const index = flow.currentStepIndex;
+    const steps = flow.template.flow?.steps ?? [];
+    const step = steps[index];
+    if (step) {
+      for (let i = 0; i < step.nodes.length; i++) {
+        const node = step.nodes[i];
+        const {
+          type,
+          groupValue = ApprovalNode_GroupValue.UNRECOGNIZED,
+          role,
+        } = node;
+        if (type !== ApprovalNode_Type.ANY_IN_GROUP) continue;
+        if (groupValue === ApprovalNode_GroupValue.WORKSPACE_OWNER) {
+          errors.push({
+            error: displayRoleTitle(PresetRoleType.WORKSPACE_ADMIN),
+            indent: 1,
+          });
+        } else if (groupValue === ApprovalNode_GroupValue.WORKSPACE_DBA) {
+          errors.push({
+            error: displayRoleTitle(PresetRoleType.WORKSPACE_DBA),
+            indent: 1,
+          });
+        } else if (groupValue === ApprovalNode_GroupValue.PROJECT_OWNER) {
+          errors.push({
+            error: displayRoleTitle(PresetRoleType.PROJECT_OWNER),
+            indent: 1,
+          });
+        } else if (groupValue === ApprovalNode_GroupValue.PROJECT_MEMBER) {
+          errors.push({
+            error: t("common.project-member"),
+            indent: 1,
+          });
+        } else if (role) {
+          errors.push({
+            error: displayRoleTitle(role),
+            indent: 1,
+          });
+        }
+      }
+    }
   }
   return errors;
 });
