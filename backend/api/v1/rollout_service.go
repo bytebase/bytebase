cd251ba4a932fee0658849950a386b1bb2b73203
p0ny <xz@bytebase.com>
2024-02-05 12:51:46 +0800

    chore: deprecate Task.Status (#10702)
    Former-commit-id: 23b726a2b21f9b3e31d424fdd74633d13133fb4b

--- backend/api/v1/rollout_service.go
+++ backend/api/v1/rollout_service.go
@@ -502,8 +502,7 @@ func (s *RolloutService) BatchRunTasks(ctx context.Context, request *v1pb.BatchR
 		return nil, status.Errorf(codes.Internal, "failed to find the stage to run")
 	}
 
-	pendingApprovalStatus := []api.TaskStatus{api.TaskPendingApproval}
-	stageToRunTasks, err := s.store.ListTasks(ctx, &api.TaskFind{PipelineID: &rolloutID, StageID: &stageToRun.ID, StatusList: &pendingApprovalStatus})
+	stageToRunTasks, err := s.store.ListTasks(ctx, &api.TaskFind{PipelineID: &rolloutID, StageID: &stageToRun.ID})
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to list tasks, error: %v", err)
 	}
