8bb8af00af97b9f20d5bfb7fbca2b133ab2dd520
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-02-05 16:00:07 +0800

    chore(frontend): branch minor fixes (#10706)
    * chore: default coloring on to child branches
    
    * fix: columns disable coloring
    
    * chore: lint
    
    Former-commit-id: 8167739008e15219f261fe7a27a5741c66bb8af4
    Former-commit-id: e32ab96591f406778828fbaed7ac705abcd53078

--- frontend/src/components/SchemaEditorLite/Panels/TableColumnEditor/TableColumnEditor.vue
+++ frontend/src/components/SchemaEditorLite/Panels/TableColumnEditor/TableColumnEditor.vue
@@ -21,6 +21,7 @@
       :bordered="true"
       :bottom-bordered="true"
       class="schema-editor-table-column-editor"
+      :class="[disableDiffColoring && 'disable-diff-coloring']"
     />
   </div>
 
@@ -177,12 +178,13 @@ const state = reactive<LocalState>({
 
 const {
   resourceType,
+  disableDiffColoring,
+  selectionEnabled,
   markEditStatus,
   getColumnStatus,
   getColumnConfig,
   upsertColumnConfig,
   useConsumePendingScrollToColumn,
-  selectionEnabled,
   getAllColumnsSelectionState,
   updateAllColumnsSelection,
 } = useSchemaEditorContext();
@@ -779,15 +781,15 @@ useConsumePendingScrollToColumn(
 .schema-editor-table-column-editor :deep(.n-data-table-td.text-cell) {
   @apply pr-1 py-0;
 }
-.schema-editor-table-column-editor
+.schema-editor-table-column-editor:not(.disable-diff-coloring)
   :deep(.n-data-table-tr.created .n-data-table-td) {
   @apply text-green-700 !bg-green-50;
 }
-.schema-editor-table-column-editor
+.schema-editor-table-column-editor:not(.disable-diff-coloring)
   :deep(.n-data-table-tr.dropped .n-data-table-td) {
   @apply text-red-700 cursor-not-allowed !bg-red-50 opacity-70;
 }
-.schema-editor-table-column-editor
+.schema-editor-table-column-editor:not(.disable-diff-coloring)
   :deep(.n-data-table-tr.updated .n-data-table-td) {
   @apply text-yellow-700 !bg-yellow-50;
 }
