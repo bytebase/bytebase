08acbe87a7458e90befb70fe3202f5cffb1987b6
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-02-06 08:04:36 -0800

    chore: do not use separate txn to get version (#10720)
    * chore: do not use separate txn to get version
    
    * chore: update
    
    Former-commit-id: 8fb13d0c7b91dca88675a6ec30c2ecec6013642a

--- backend/plugin/db/oracle/oracle_test.go
+++ backend/plugin/db/oracle/oracle_test.go
@@ -14,21 +14,21 @@ func TestParseVersion(t *testing.T) {
 	}
 	tests := []testData{
 		{
-			Banner: "Oracle Database 12c Enterprise Edition Release 12.1.0.2.0 - 64bit Production",
+			Banner: "12.1.0.2.0",
 			First:  12,
 			Second: 1,
 		},
 		{
-			Banner: "Oracle Database 12.1.0.",
+			Banner: "12.1.0.",
 			First:  12,
 			Second: 1,
 		},
 	}
 
 	for _, test := range tests {
-		first, second, err := parseVersion(test.Banner)
+		v, err := parseVersion(test.Banner)
 		require.NoError(t, err)
-		require.Equal(t, test.First, first)
-		require.Equal(t, test.Second, second)
+		require.Equal(t, test.First, v.first)
+		require.Equal(t, test.Second, v.second)
 	}
 }
