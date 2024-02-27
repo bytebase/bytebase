e05550930ea99f6c8b41d93d831ee239d1852cfb
p0ny <p0uy@outlook.com>
2024-02-27 14:54:37 +0800

    chore: remove dead code

--- backend/component/state/state.go
+++ backend/component/state/state.go
@@ -45,14 +45,8 @@ type State struct {
 
 	// RunningBackupDatabases is the set of databases running backups.
 	RunningBackupDatabases sync.Map // map[databaseID]bool
-	// RunningTaskChecks is the set of running task checks.
-	RunningTaskChecks sync.Map // map[taskCheckID]bool
-	// RunningTasks is the set of running tasks.
-	RunningTasks sync.Map // map[taskID]bool
 	// RunningPlanChecks is the set of running plan checks.
 	RunningPlanChecks sync.Map
-	// RunningTasksCancel is the cancel's of running tasks.
-	RunningTasksCancel sync.Map // map[taskID]context.CancelFunc
 	// InstanceOutstandingConnections is the maximum number of connections per instance.
 	InstanceOutstandingConnections map[int]int
 
