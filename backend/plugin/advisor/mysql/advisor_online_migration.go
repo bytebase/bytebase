2b87b7f8acafbcef8ac9f61fd7b3e2942a47e79e
p0ny <p0uy@outlook.com>
2024-03-04 16:40:10 +0800

    update comment

--- backend/plugin/advisor/mysql/advisor_online_migration.go
+++ backend/plugin/advisor/mysql/advisor_online_migration.go
@@ -76,7 +76,7 @@ func (*OnlineMigrationAdvisor) Check(ctx advisor.Context, _ string) ([]advisor.A
 				Status:  checker.level,
 				Code:    advisor.AdviseOnlineMigration,
 				Title:   checker.title,
-				Content: fmt.Sprintf("Estimated table row count of %q is %d, consider using gh-ost to migrate", fmt.Sprintf("%s.%s", resource.Schema, resource.Table), tableRows),
+				Content: fmt.Sprintf("Estimated table row count of %q is %d, greater than the set value %d. Consider using gh-ost to migrate", fmt.Sprintf("%s.%s", resource.Schema, resource.Table), tableRows, minRows),
 			})
 		}
 	}
