21da092d560434932ab8a17e9b8c63d01ae8e386
h3n4l <oysterdays@gmail.com>
2024-03-04 14:44:11 +0800

    fix: update time on helm index yaml (#10944)
    Signed-off-by: h3n4l <oysterdays@gmail.com>

--- scripts/package_helm.sh
+++ scripts/package_helm.sh
@@ -6,8 +6,13 @@ set -e
 # Get current absolute path
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Goto ../helm-charts directory and package the helm chart.
-cd $DIR/../helm-charts && helm package bytebase && mv bytebase-*.tgz $DIR/../docs/
+cd $DIR/../helm-charts
 
-# Goto ../docs directory and update the index.yaml
-cd $DIR/../docs && helm repo index .
+mkdir -p new_charts
+helm package -u -d new_charts bytebase
+helm repo index --merge $DIR/../docs/index.yaml new_charts
+
+# Update charts repo.
+mv new_charts/*.tgz $DIR/../docs
+mv new_charts/index.yaml $DIR/../docs/index.yaml
+rm -rf new_charts
