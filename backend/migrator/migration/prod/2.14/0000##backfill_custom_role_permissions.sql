e1e8723d90f820c89b3b4144b347b6f3da17c991
p0ny <p0uy@outlook.com>
2024-03-05 11:05:22 +0800

    chore: backfill perms

---
+++ backend/migrator/migration/prod/2.14/0000##backfill_custom_role_permissions.sql
@@ -0,0 +1,2 @@
+UPDATE role
+SET permissions = '{"permissions": ["bb.changeHistories.get", "bb.changeHistories.list", "bb.databases.get", "bb.databases.getSchema", "bb.databases.list", "bb.issueComments.create", "bb.issues.get", "bb.issues.list", "bb.planCheckRuns.list", "bb.planCheckRuns.run", "bb.plans.get", "bb.plans.list", "bb.projects.get", "bb.projects.getIamPolicy", "bb.rollouts.get", "bb.taskRuns.list"]}'::JSONB;
