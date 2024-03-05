588fbcac25f38b304b67bbff627c7e833e4937f7
steven <stevenlgtm@gmail.com>
2024-03-05 14:07:36 +0800

    chore: update

--- frontend/src/utils/permission.ts
+++ frontend/src/utils/permission.ts
@@ -1,5 +1,7 @@
+const PERMISSION_PREFIX = "bb.";
+
 // displayPermissionTitle return the formatted permission title.
 // e.g., bb.databases.get -> databases.get
 export const displayPermissionTitle = (permission: string): string => {
-  return permission.slice(3);
+  return permission.slice(PERMISSION_PREFIX.length);
 };
