fa60262fd77c8bcb462131d204a818162a4edf38
steven <stevenlgtm@gmail.com>
2024-03-06 15:49:10 +0800

    chore: update

--- backend/plugin/advisor/mysql/mysql_rules_test.go
+++ backend/plugin/advisor/mysql/mysql_rules_test.go
@@ -53,8 +53,6 @@ func TestMySQLRules(t *testing.T) {
 		advisor.SchemaRuleStatementAffectedRowLimit,
 		// advisor.SchemaRuleStatementDMLDryRun dry run the dml.
 		advisor.SchemaRuleStatementDMLDryRun,
-		// advisor.SchemaRuleStatementDisallowUsingFilesort disallow the using filesort.
-		advisor.SchemaRuleStatementDisallowUsingFilesort,
 
 		// Database related rules.
 		// advisor.SchemaRuleDropEmptyDatabase enforce the MySQL support check if the database is empty before users drop it.
