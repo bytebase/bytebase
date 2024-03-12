3351951417dacaa44750199df540ed6af040d79a
d-bytebase <d@bytebase.com>
2024-03-12 07:08:50 -0700

    chore: add dummy advisor for mix ddl/dml

---
+++ backend/plugin/advisor/tidb/advisor_statement_disallow_mix_dml.go
@@ -0,0 +1,72 @@
+package tidb
+
+import (
+	"github.com/pingcap/tidb/pkg/parser/ast"
+	"github.com/pkg/errors"
+
+	"github.com/bytebase/bytebase/backend/plugin/advisor"
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+var (
+	_ advisor.Advisor = (*NoSelectAllAdvisor)(nil)
+	_ ast.Visitor     = (*noSelectAllChecker)(nil)
+)
+
+func init() {
+	advisor.Register(storepb.Engine_TIDB, advisor.MySQLStatementDisallowMixDML, &StatementDisallowmixDMLAdvisor{})
+}
+
+// StatementDisallowmixDDLDMLAdvisor is the advisor checking for no "select *".
+type StatementDisallowmixDMLAdvisor struct {
+}
+
+// Check checks for no "select *".
+func (*StatementDisallowmixDMLAdvisor) Check(ctx advisor.Context, _ string) ([]advisor.Advice, error) {
+	root, ok := ctx.AST.([]ast.StmtNode)
+	if !ok {
+		return nil, errors.Errorf("failed to convert to StmtNode")
+	}
+
+	level, err := advisor.NewStatusBySQLReviewRuleLevel(ctx.Rule.Level)
+	if err != nil {
+		return nil, err
+	}
+	checker := &statementDisallowMixDMLChecker{
+		level: level,
+		title: string(ctx.Rule.Type),
+	}
+	for _, stmtNode := range root {
+		checker.text = stmtNode.Text()
+		checker.line = stmtNode.OriginTextPosition()
+		(stmtNode).Accept(checker)
+	}
+
+	if len(checker.adviceList) == 0 {
+		checker.adviceList = append(checker.adviceList, advisor.Advice{
+			Status:  advisor.Success,
+			Code:    advisor.Ok,
+			Title:   "OK",
+			Content: "",
+		})
+	}
+	return checker.adviceList, nil
+}
+
+type statementDisallowMixDMLChecker struct {
+	adviceList []advisor.Advice
+	level      advisor.Status
+	title      string
+	text       string
+	line       int
+}
+
+// Enter implements the ast.Visitor interface.
+func (*statementDisallowMixDMLChecker) Enter(in ast.Node) (ast.Node, bool) {
+	return in, false
+}
+
+// Leave implements the ast.Visitor interface.
+func (*statementDisallowMixDMLChecker) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
+}
