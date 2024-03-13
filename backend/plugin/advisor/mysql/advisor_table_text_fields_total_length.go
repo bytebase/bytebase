0af7dfb1aea6e19b37c4da30354d2c15d9302f61
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-03-13 00:11:08 -0700

    chore: implement table text fields total length rule (#11082)
    * chore: fix
    
    * chore: implement table text fields total length rule
    
    Former-commit-id: ea2874979a72038ad5a4e4747e5529cba18ea950
    Former-commit-id: 3245c1e20ee388586bd27a6b7ec8519733bebb59

--- backend/plugin/advisor/mysql/advisor_table_text_fields_total_length.go
+++ backend/plugin/advisor/mysql/advisor_table_text_fields_total_length.go
@@ -1,12 +1,18 @@
 package mysql
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
 	"github.com/antlr4-go/antlr/v4"
 	"github.com/pkg/errors"
 
 	mysql "github.com/bytebase/mysql-parser"
 
 	"github.com/bytebase/bytebase/backend/plugin/advisor"
+	"github.com/bytebase/bytebase/backend/plugin/advisor/catalog"
 	mysqlparser "github.com/bytebase/bytebase/backend/plugin/parser/mysql"
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 )
@@ -32,9 +38,14 @@ func (*TableMaximumVarcharLengthAdvisor) Check(ctx advisor.Context, _ string) ([
 	if err != nil {
 		return nil, err
 	}
+	payload, err := advisor.UnmarshalNumberTypeRulePayload(ctx.Rule.Payload)
+	if err != nil {
+		return nil, err
+	}
 	checker := &tableFieldsMaximumVarcharLengthChecker{
-		level: level,
-		title: string(ctx.Rule.Type),
+		level:   level,
+		title:   string(ctx.Rule.Type),
+		maximum: payload.Number,
 	}
 
 	for _, stmt := range stmtList {
@@ -53,17 +64,104 @@ func (*TableMaximumVarcharLengthAdvisor) Check(ctx advisor.Context, _ string) ([
 	return checker.adviceList, nil
 }
 
-// TODO(sql-review): implement me please.
 type tableFieldsMaximumVarcharLengthChecker struct {
 	*mysql.BaseMySQLParserListener
 
-	baseLine   int
+	catalog    *catalog.Finder
 	adviceList []advisor.Advice
 	level      advisor.Status
 	title      string
-	text       string
+	baseLine   int
+	maximum    int
 }
 
-func (checker *tableFieldsMaximumVarcharLengthChecker) EnterQuery(ctx *mysql.QueryContext) {
-	checker.text = ctx.GetParser().GetTokenStream().GetTextFromRuleContext(ctx)
+func (checker *tableFieldsMaximumVarcharLengthChecker) EnterCreateTable(ctx *mysql.CreateTableContext) {
+	if ctx.TableElementList() == nil || ctx.TableName() == nil {
+		return
+	}
+
+	_, tableName := mysqlparser.NormalizeMySQLTableName(ctx.TableName())
+	if tableName == "" {
+		return
+	}
+	tableInfo := checker.catalog.Final.FindTable(&catalog.TableFind{TableName: tableName})
+	if tableInfo == nil {
+		return
+	}
+	total := getTotalTextLength(tableInfo)
+	if total > int64(checker.maximum) {
+		checker.adviceList = append(checker.adviceList, advisor.Advice{
+			Status:  checker.level,
+			Code:    advisor.IndexCountExceedsLimit,
+			Title:   checker.title,
+			Content: fmt.Sprintf("Table %q total text column length (%d) exceeds the limit (%d).", tableName, total, checker.maximum),
+			Line:    checker.baseLine + ctx.GetStart().GetLine(),
+		})
+	}
+}
+
+func (checker *tableFieldsMaximumVarcharLengthChecker) EnterAlterTable(ctx *mysql.AlterTableContext) {
+	if ctx.AlterTableActions() == nil {
+		return
+	}
+	if ctx.AlterTableActions().AlterCommandList() == nil {
+		return
+	}
+	if ctx.AlterTableActions().AlterCommandList().AlterList() == nil {
+		return
+	}
+
+	_, tableName := mysqlparser.NormalizeMySQLTableRef(ctx.TableRef())
+	if tableName == "" {
+		return
+	}
+	tableInfo := checker.catalog.Final.FindTable(&catalog.TableFind{TableName: tableName})
+	if tableInfo == nil {
+		return
+	}
+	total := getTotalTextLength(tableInfo)
+	if total > int64(checker.maximum) {
+		checker.adviceList = append(checker.adviceList, advisor.Advice{
+			Status:  checker.level,
+			Code:    advisor.TotalTextLengthExceedsLimit,
+			Title:   checker.title,
+			Content: fmt.Sprintf("Table %q total text column length (%d) exceeds the limit (%d).", tableName, total, checker.maximum),
+			Line:    checker.baseLine + ctx.GetStart().GetLine(),
+		})
+	}
+}
+
+func getTotalTextLength(tableInfo *catalog.TableState) int64 {
+	var total int64
+	columns := tableInfo.ListColumns()
+	for _, column := range columns {
+		total += getTextLength(column.Type())
+	}
+	return total
+}
+
+func getTextLength(s string) int64 {
+	s = strings.ToLower(s)
+	switch s {
+	case "char", "binary":
+		return 1
+	case "tinyblob", "tinytext":
+		return 255
+	case "blob", "text":
+		return 65_535
+	case "mediumblob", "mediumtext":
+		return 16_777_215
+	case "longblob", "longtext":
+		return 4_294_967_295
+	default:
+		re := regexp.MustCompile(`[a-z]+\((\d+)\)`)
+		match := re.FindStringSubmatch(s)
+		if len(match) >= 2 {
+			n, err := strconv.ParseInt(match[1], 10, 64)
+			if err == nil {
+				return int64(n)
+			}
+		}
+	}
+	return 0
 }
---
+++ backend/plugin/advisor/mysql/advisor_table_text_fields_total_length_test.go
@@ -0,0 +1,35 @@
+package mysql
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetTextLength(t *testing.T) {
+	tests := []struct {
+		s    string
+		want int64
+	}{
+		{
+			s:    "CHAR",
+			want: 1,
+		},
+		{
+			s:    "longtext",
+			want: 4_294_967_295,
+		},
+		{
+			s:    "int",
+			want: 0,
+		},
+		{
+			s:    "char(123)",
+			want: 123,
+		},
+	}
+	for _, tc := range tests {
+		got := getTextLength(tc.s)
+		require.Equal(t, tc.want, got, tc.s)
+	}
+}
