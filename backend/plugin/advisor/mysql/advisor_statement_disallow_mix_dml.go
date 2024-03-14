3ba726e91ddb9f5ea10d95531ad2d92e7d5651c8
p0ny <xz@bytebase.com>
2024-03-14 17:42:04 +0800

    feat(sql-review-rule): impl mysql disallow mix dml (#11093)
    Former-commit-id: bbd22bfb3cfbe96799f6aa46d486ec47a6f259a6
    Former-commit-id: d21b605eb92898821bde2f08ff8ae7448830112e

--- backend/plugin/advisor/mysql/advisor_statement_disallow_mix_dml.go
+++ backend/plugin/advisor/mysql/advisor_statement_disallow_mix_dml.go
@@ -1,6 +1,9 @@
 package mysql
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/antlr4-go/antlr/v4"
 	"github.com/pkg/errors"
 
@@ -32,14 +35,33 @@ func (*StatementDisallowMixDMLAdvisor) Check(ctx advisor.Context, _ string) ([]a
 	if err != nil {
 		return nil, err
 	}
+
 	checker := &statementDisallowMixDMLChecker{
-		level: level,
-		title: string(ctx.Rule.Type),
+		level:             level,
+		title:             string(ctx.Rule.Type),
+		dmlStatementCount: make(map[table]map[string]int),
 	}
 
 	for _, stmt := range stmtList {
 		checker.baseLine = stmt.BaseLine
 		antlr.ParseTreeWalkerDefault.Walk(checker, stmt.Tree)
+	}
+
+	for table, dmlCount := range checker.dmlStatementCount {
+		if len(dmlCount) > 1 {
+			content := "Found"
+			for t, count := range dmlCount {
+				content += fmt.Sprintf(" %d %s,", count, t)
+			}
+			content = strings.TrimSuffix(content, ",")
+			content += fmt.Sprintf(" on table `%s`.`%s`, disallow mixing different types of DML statements", table.database, table.table)
+			checker.adviceList = append(checker.adviceList, advisor.Advice{
+				Status:  checker.level,
+				Code:    advisor.StatementDisallowMixDML,
+				Title:   checker.title,
+				Content: content,
+			})
+		}
 	}
 
 	if len(checker.adviceList) == 0 {
@@ -53,7 +75,6 @@ func (*StatementDisallowMixDMLAdvisor) Check(ctx advisor.Context, _ string) ([]a
 	return checker.adviceList, nil
 }
 
-// TODO(sql-review): implement me please.
 type statementDisallowMixDMLChecker struct {
 	*mysql.BaseMySQLParserListener
 
@@ -61,9 +82,183 @@ type statementDisallowMixDMLChecker struct {
 	adviceList []advisor.Advice
 	level      advisor.Status
 	title      string
-	text       string
+
+	dmlStatementCount map[table]map[string]int
 }
 
-func (checker *statementDisallowMixDMLChecker) EnterQuery(ctx *mysql.QueryContext) {
-	checker.text = ctx.GetParser().GetTokenStream().GetTextFromRuleContext(ctx)
+type table struct {
+	database string
+	table    string
+}
+
+func (c *statementDisallowMixDMLChecker) EnterInsertStatement(ctx *mysql.InsertStatementContext) {
+	if ctx.TableRef() == nil {
+		return
+	}
+	databaseName, tableName := mysqlparser.NormalizeMySQLTableRef(ctx.TableRef())
+	table := table{
+		database: databaseName,
+		table:    tableName,
+	}
+	if _, ok := c.dmlStatementCount[table]; !ok {
+		c.dmlStatementCount[table] = make(map[string]int)
+	}
+	c.dmlStatementCount[table]["INSERT"]++
+}
+
+func (c *statementDisallowMixDMLChecker) EnterUpdateStatement(ctx *mysql.UpdateStatementContext) {
+	var allTables []table
+	for _, tableRefCtx := range ctx.TableReferenceList().AllTableReference() {
+		tables, err := extractTableReference(tableRefCtx)
+		if err != nil {
+			c.adviceList = append(c.adviceList, advisor.Advice{
+				Status:  c.level,
+				Code:    advisor.Internal,
+				Title:   c.title,
+				Content: fmt.Sprintf("Failed to extract table reference: %v", err),
+				Line:    tableRefCtx.GetStart().GetLine() + c.baseLine,
+			})
+			continue
+		}
+		allTables = append(allTables, tables...)
+	}
+	for _, table := range allTables {
+		if _, ok := c.dmlStatementCount[table]; !ok {
+			c.dmlStatementCount[table] = make(map[string]int)
+		}
+		c.dmlStatementCount[table]["UPDATE"]++
+	}
+}
+
+func (c *statementDisallowMixDMLChecker) EnterDeleteStatement(ctx *mysql.DeleteStatementContext) {
+	var allTables []table
+	if ctx.TableRef() != nil {
+		tables, err := extractTableRef(ctx.TableRef())
+		if err != nil {
+			c.adviceList = append(c.adviceList, advisor.Advice{
+				Status:  c.level,
+				Code:    advisor.Internal,
+				Title:   c.title,
+				Content: fmt.Sprintf("Failed to extract table reference: %v", err),
+				Line:    ctx.GetStart().GetLine() + c.baseLine,
+			})
+		} else {
+			allTables = append(allTables, tables...)
+		}
+	}
+	if ctx.TableReferenceList() != nil {
+		tables, err := extractTableReferenceList(ctx.TableReferenceList())
+		if err != nil {
+			c.adviceList = append(c.adviceList, advisor.Advice{
+				Status:  c.level,
+				Code:    advisor.Internal,
+				Title:   c.title,
+				Content: fmt.Sprintf("Failed to extract table reference: %v", err),
+				Line:    ctx.GetStart().GetLine() + c.baseLine,
+			})
+		} else {
+			allTables = append(allTables, tables...)
+		}
+	}
+	for _, table := range allTables {
+		if _, ok := c.dmlStatementCount[table]; !ok {
+			c.dmlStatementCount[table] = make(map[string]int)
+		}
+		c.dmlStatementCount[table]["DELETE"]++
+	}
+}
+
+func extractTableReference(ctx mysql.ITableReferenceContext) ([]table, error) {
+	if ctx.TableFactor() == nil {
+		return nil, nil
+	}
+	res, err := extractTableFactor(ctx.TableFactor())
+	if err != nil {
+		return nil, err
+	}
+	for _, joinedTableCtx := range ctx.AllJoinedTable() {
+		tables, err := extractJoinedTable(joinedTableCtx)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, tables...)
+	}
+
+	return res, nil
+}
+
+func extractTableRef(ctx mysql.ITableRefContext) ([]table, error) {
+	if ctx == nil {
+		return nil, nil
+	}
+	databaseName, tableName := mysqlparser.NormalizeMySQLTableRef(ctx)
+	return []table{
+		{
+			database: databaseName,
+			table:    tableName,
+		},
+	}, nil
+}
+
+func extractTableReferenceList(ctx mysql.ITableReferenceListContext) ([]table, error) {
+	var res []table
+	for _, tableRefCtx := range ctx.AllTableReference() {
+		tables, err := extractTableReference(tableRefCtx)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, tables...)
+	}
+	return res, nil
+}
+
+func extractTableReferenceListParens(ctx mysql.ITableReferenceListParensContext) ([]table, error) {
+	if ctx.TableReferenceList() != nil {
+		return extractTableReferenceList(ctx.TableReferenceList())
+	}
+	if ctx.TableReferenceListParens() != nil {
+		return extractTableReferenceListParens(ctx.TableReferenceListParens())
+	}
+	return nil, nil
+}
+
+func extractTableFactor(ctx mysql.ITableFactorContext) ([]table, error) {
+	switch {
+	case ctx.SingleTable() != nil:
+		return extractSingleTable(ctx.SingleTable())
+	case ctx.SingleTableParens() != nil:
+		return extractSingleTableParens(ctx.SingleTableParens())
+	case ctx.DerivedTable() != nil:
+		return nil, nil
+	case ctx.TableReferenceListParens() != nil:
+		return extractTableReferenceListParens(ctx.TableReferenceListParens())
+	case ctx.TableFunction() != nil:
+		return nil, nil
+	default:
+		return nil, nil
+	}
+}
+
+func extractSingleTable(ctx mysql.ISingleTableContext) ([]table, error) {
+	return extractTableRef(ctx.TableRef())
+}
+
+func extractSingleTableParens(ctx mysql.ISingleTableParensContext) ([]table, error) {
+	if ctx.SingleTable() != nil {
+		return extractSingleTable(ctx.SingleTable())
+	}
+	if ctx.SingleTableParens() != nil {
+		return extractSingleTableParens(ctx.SingleTableParens())
+	}
+	return nil, nil
+}
+
+func extractJoinedTable(ctx mysql.IJoinedTableContext) ([]table, error) {
+	if ctx.TableFactor() != nil {
+		return extractTableFactor(ctx.TableFactor())
+	}
+	if ctx.TableReference() != nil {
+		return extractTableReference(ctx.TableReference())
+	}
+	return nil, nil
 }
