fec13189e56ede0ef4dc21080e2b849933c0e276
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-03-18 10:21:40 +0800

    chore: implement new URL routes

--- frontend/src/router/sqlEditor.ts
+++ frontend/src/router/sqlEditor.ts
@@ -2,6 +2,10 @@ import { RouteRecordRaw } from "vue-router";
 import SQLEditorLayout from "@/layouts/SQLEditorLayout.vue";
 
 export const SQL_EDITOR_HOME_MODULE = "sql-editor.home";
+export const SQL_EDITOR_PROJECT_MODULE = "sql-editor.project";
+export const SQL_EDITOR_INSTANCE_MODULE = "sql-editor.instance";
+export const SQL_EDITOR_DATABASE_MODULE = "sql-editor.database";
+export const SQL_EDITOR_WORKSHEET_MODULE = "sql-editor.worksheet";
 export const SQL_EDITOR_DETAIL_MODULE_LEGACY = "sql-editor.legacy-detail";
 export const SQL_EDITOR_SHARE_MODULE_LEGACY = "sql-editor.legacy-share";
 
@@ -14,6 +18,30 @@ const sqlEditorRoutes: RouteRecordRaw[] = [
       {
         path: "",
         name: SQL_EDITOR_HOME_MODULE,
+        meta: { title: () => "Bytebase SQL Editor" },
+        component: () => import("../views/sql-editor/SQLEditorPage.vue"),
+      },
+      {
+        path: "projects/:project",
+        name: SQL_EDITOR_PROJECT_MODULE,
+        meta: { title: () => "Bytebase SQL Editor" },
+        component: () => import("../views/sql-editor/SQLEditorPage.vue"),
+      },
+      {
+        path: "projects/:project/instances/:instance/databases/:database",
+        name: SQL_EDITOR_DATABASE_MODULE,
+        meta: { title: () => "Bytebase SQL Editor" },
+        component: () => import("../views/sql-editor/SQLEditorPage.vue"),
+      },
+      {
+        path: "projects/:project/instances/:instance",
+        name: SQL_EDITOR_INSTANCE_MODULE,
+        meta: { title: () => "Bytebase SQL Editor" },
+        component: () => import("../views/sql-editor/SQLEditorPage.vue"),
+      },
+      {
+        path: "projects/:project/sheets/:sheet",
+        name: SQL_EDITOR_WORKSHEET_MODULE,
         meta: { title: () => "Bytebase SQL Editor" },
         component: () => import("../views/sql-editor/SQLEditorPage.vue"),
       },
