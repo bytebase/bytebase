a5f055f4592e449891881fffd6e43c6af8b950ae
h3n4l <oysterdays@gmail.com>
2024-03-19 16:51:02 +0800

    refactor: split large test file (#11163)
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    Former-commit-id: 669f99c3db4bdb5551b7cfedce81d9d23ed9744d
    Former-commit-id: b998241e06a6d3a1739380773aea6bf66287cda6

--- backend/plugin/schema/mysql/parse_to_metadata_test.go
+++ backend/plugin/schema/mysql/parse_to_metadata_test.go
@@ -14,46 +14,58 @@ import (
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 )
 
-func TestParseToMetadata(t *testing.T) {
-	type transformTest struct {
-		Schema   string
-		Metadata string
-	}
-	const (
-		record = false
-	)
-	var (
-		filepath = "testdata/parse_to_metadata.yaml"
-	)
+type transformTest struct {
+	Schema   string
+	Metadata string
+}
 
+func TestParseToMetadata_Partition(t *testing.T) {
+	filepaths := []string{
+		"testdata/parse-to-metadata/partition.yaml",
+	}
+
+	runParseToMetadataTest(t, filepaths, false)
+}
+
+func TestParseToMetadata_Standard(t *testing.T) {
+	filepaths := []string{
+		"testdata/parse-to-metadata/standard.yaml",
+	}
+
+	runParseToMetadataTest(t, filepaths, false)
+}
+
+func runParseToMetadataTest(t *testing.T, filepaths []string, record bool) {
 	a := require.New(t)
-	yamlFile, err := os.Open(filepath)
-	a.NoError(err)
-
-	tests := []transformTest{}
-	byteValue, err := io.ReadAll(yamlFile)
-	a.NoError(yamlFile.Close())
-	a.NoError(err)
-	a.NoError(yaml.Unmarshal(byteValue, &tests))
-
-	for i, t := range tests {
-		result, err := ParseToMetadata("", t.Schema)
+	for _, fp := range filepaths {
+		yamlFile, err := os.Open(fp)
 		a.NoError(err)
-		if record {
-			tests[i].Metadata = protojson.MarshalOptions{Multiline: true, Indent: "  "}.Format(result)
-		} else {
-			want := &storepb.DatabaseSchemaMetadata{}
-			err = protojson.Unmarshal([]byte(t.Metadata), want)
+
+		tests := []transformTest{}
+		byteValue, err := io.ReadAll(yamlFile)
+		a.NoError(yamlFile.Close())
+		a.NoError(err)
+		a.NoError(yaml.Unmarshal(byteValue, &tests))
+
+		for i, t := range tests {
+			result, err := ParseToMetadata("", t.Schema)
 			a.NoError(err)
-			diff := cmp.Diff(want, result, protocmp.Transform())
-			a.Equal("", diff)
+			if record {
+				tests[i].Metadata = protojson.MarshalOptions{Multiline: true, Indent: "  "}.Format(result)
+			} else {
+				want := &storepb.DatabaseSchemaMetadata{}
+				err = protojson.Unmarshal([]byte(t.Metadata), want)
+				a.NoError(err)
+				diff := cmp.Diff(want, result, protocmp.Transform())
+				a.Equal("", diff)
+			}
 		}
-	}
 
-	if record {
-		byteValue, err := yaml.Marshal(tests)
-		a.NoError(err)
-		err = os.WriteFile(filepath, byteValue, 0644)
-		a.NoError(err)
+		if record {
+			byteValue, err := yaml.Marshal(tests)
+			a.NoError(err)
+			err = os.WriteFile(fp, byteValue, 0644)
+			a.NoError(err)
+		}
 	}
 }
