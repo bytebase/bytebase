769c9d1ec526995207c097ab744e466f8050b2fc
Azusain <azusaings@gmail.com>
2024-03-20 11:30:27 +0800

    feat(hive): role management (#11169)
    * feat(hive): hive connection
    
    * feat(hive): hive connection
    
    * feat(hive): hive connection
    
    * feat(hive): hive connection
    
    * feat(hive): hive connection
    
    * feat(hive): hive connection
    
    * feat(hive): hive connection
    
    * feat(hive): support query operation
    
    * feat(hive): support running statements
    
    * feat(hive): support running statements
    
    * feat(hive): sync instance
    
    * feat(hive): sync instance
    
    * refactor(hive): use base.SplitMultiSQL
    
    * feat(hive): role management
    
    * chore(hive): fix formatting
    
    * chore(hive): lint
    
    * chore(hive): lint
    
    Former-commit-id: 0687de70571a08bae8a1ec5473bef6017b77b320

--- backend/plugin/db/hive/role.go
+++ backend/plugin/db/hive/role.go
@@ -2,34 +2,80 @@ package hive
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/pkg/errors"
 
 	"github.com/bytebase/bytebase/backend/plugin/db"
 )
 
-// Role
-// CreateRole creates the role.
-func (*Driver) CreateRole(_ context.Context, _ *db.DatabaseRoleUpsertMessage) (*db.DatabaseRoleMessage, error) {
-	return nil, errors.Errorf("Not implemeted")
+func (d *Driver) CreateRole(ctx context.Context, roleUpsertMessage *db.DatabaseRoleUpsertMessage) (*db.DatabaseRoleMessage, error) {
+	_, err := d.Execute(ctx, fmt.Sprintf("CREATE ROLE %s", roleUpsertMessage.Name), db.ExecuteOptions{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create role: %s", roleUpsertMessage.Name)
+	}
+	return &db.DatabaseRoleMessage{
+		Name: roleUpsertMessage.Name,
+	}, nil
 }
 
-// UpdateRole updates the role.
 func (*Driver) UpdateRole(_ context.Context, _ string, _ *db.DatabaseRoleUpsertMessage) (*db.DatabaseRoleMessage, error) {
-	return nil, errors.Errorf("Not implemeted")
+	return nil, errors.Errorf("UpdateRole() is not applicable to Hive")
 }
 
-// FindRole finds the role by name.
 func (*Driver) FindRole(_ context.Context, _ string) (*db.DatabaseRoleMessage, error) {
-	return nil, errors.Errorf("Not implemeted")
+	return nil, errors.Errorf("FindRole() is not applicable to Hive")
 }
 
-// ListRole lists the role.
-func (*Driver) ListRole(_ context.Context) ([]*db.DatabaseRoleMessage, error) {
-	return nil, errors.Errorf("Not implemeted")
+func (d *Driver) ListRole(ctx context.Context) ([]*db.DatabaseRoleMessage, error) {
+	roleResults, err := d.QueryConn(ctx, nil, "SHOW ROLES", nil)
+	var roleMessages []*db.DatabaseRoleMessage
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get roles")
+	}
+
+	for _, row := range roleResults[0].Rows {
+		roleMessages = append(roleMessages, &db.DatabaseRoleMessage{
+			Name: row.Values[0].GetStringValue(),
+		})
+	}
+
+	return roleMessages, nil
 }
 
-// DeleteRole deletes the role by name.
-func (*Driver) DeleteRole(_ context.Context, _ string) error {
-	return errors.Errorf("Not implemeted")
+func (d *Driver) DeleteRole(ctx context.Context, roleName string) error {
+	_, err := d.Execute(ctx, fmt.Sprintf("DROP ROLE %s", roleName), db.ExecuteOptions{})
+	if err != nil {
+		return errors.Wrapf(err, "failed to drop role %s", roleName)
+	}
+	return nil
+}
+
+// some DMLs need admin role.
+func (d *Driver) SetRole(ctx context.Context, roleName string) error {
+	_, err := d.Execute(ctx, fmt.Sprintf("SET ROLE %s", roleName), db.ExecuteOptions{})
+	if err != nil {
+		return errors.Wrapf(err, "failed to set role to %s", roleName)
+	}
+	return nil
+}
+
+func (d *Driver) GetRoleGrant(ctx context.Context, roleName string) (string, error) {
+	grantResults, err := d.QueryConn(ctx, nil, fmt.Sprintf("SHOW GRANT ROLE %s", roleName), nil)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to get grant from %s", roleName)
+	}
+	var grantStrings []string
+	// get grant-info rows from a certain role and combine them into a string.
+	for _, row := range grantResults[0].Rows {
+		databaseName := row.Values[0].GetStringValue()
+		tableName := row.Values[1].GetStringValue()
+		privilege := row.Values[6].GetStringValue()
+		// TODO(tommy): the format of this string should be carefully considered.
+		grantStrings = append(grantStrings, fmt.Sprintf("%s/%s: %s", databaseName, tableName, privilege))
+	}
+	grantString := strings.Join(grantStrings, "\n")
+
+	return grantString, nil
 }
