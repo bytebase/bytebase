5593f6a176a6fd7f480959643fda04bd1973e7aa
d-bytebase <d@bytebase.com>
2024-03-19 22:14:23 -0700

    chore: no discriminated trial

--- backend/enterprise/service/license.go
+++ backend/enterprise/service/license.go
@@ -136,10 +136,6 @@ func (s *LicenseService) GetPlanLimitValue(ctx context.Context, name enterprise.
 	subscription := s.LoadSubscription(ctx)
 
 	limit := v[subscription.Plan]
-	if subscription.Trialing {
-		limit = v[api.FREE]
-	}
-
 	if limit == -1 {
 		return math.MaxInt64
 	}
