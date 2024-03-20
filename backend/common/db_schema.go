81be69a8e927137aae8d9348f608a5738e9e77f6
ecmadao <wlec@outlook.com>
2024-03-20 11:13:01 +0800

    fix: handle legacy data in specific workspace (#11170)
    * fix: handle legacy data in specific workspace
    
    * fix: test
    
    Former-commit-id: 0dfbfcaf1907fb09d7691b64bf660411ef8360c0
    Former-commit-id: 1a86ba6a751751d10f2d1e4eba147a70acecbd36

--- backend/common/db_schema.go
+++ backend/common/db_schema.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"fmt"
+	"os"
 	"regexp"
 	"strings"
 )
@@ -9,8 +10,9 @@ import (
 // classificationIDPattern is the pattern for classification, it should joined id (numbers) by "-", like 1-2, 1-2-3.
 const classificationIDPattern = "^[0-9]+(-([0-9])+){0,}"
 
-// GetClassificationAndUserComment parses classification and user comment from the given comment.
-func GetClassificationAndUserComment(comment string) (string, string) {
+var numberReg = regexp.MustCompile("^[0-9]+$")
+
+func getClassificationAndUserCommentFromLegacy(comment string) (string, string) {
 	classificationIDReg := regexp.MustCompile(classificationIDPattern)
 	classification := classificationIDReg.FindString(comment)
 	if classification == comment {
@@ -26,6 +28,28 @@ func GetClassificationAndUserComment(comment string) (string, string) {
 	classification = strings.TrimSuffix(classification, "-")
 	classification = strings.TrimSuffix(classification, " ")
 	return classification, userComment
+}
+
+// GetClassificationAndUserComment parses classification and user comment from the given comment.
+func GetClassificationAndUserComment(comment string) (string, string) {
+	workspaceID := os.Getenv("BYTEBASE_WORKSPACE_ID")
+	if workspaceID == "a4fea42c-c097-47c3-b661-4a6fcea1cf6d" {
+		// handle legacy data in specific workspace
+		return getClassificationAndUserCommentFromLegacy(comment)
+	}
+
+	sections := strings.Split(comment, "-")
+	classification := []string{}
+	userComment := ""
+	for i, section := range sections {
+		if numberReg.MatchString(section) {
+			classification = append(classification, section)
+		} else {
+			userComment = strings.Join(sections[i:], "-")
+			break
+		}
+	}
+	return strings.Join(classification, "-"), userComment
 }
 
 // GetCommentFromClassificationAndUserComment returns the comment from the given classification and user comment.
