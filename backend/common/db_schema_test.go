05f54da4c9b2002cf7c81f247435b37737c60fb5
ecmadao <wlec@outlook.com>
2024-03-20 12:01:45 +0800

    chore: revert changes for classification (#11172)
    * chore: revert changes
    
    * chore: update test
    
    Former-commit-id: b4f69a6f7fe32810404381205aa9732d10d51dc6

--- backend/common/db_schema_test.go
+++ backend/common/db_schema_test.go
@@ -2,7 +2,6 @@ package common
 
 import (
 	"fmt"
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,101 +12,76 @@ func TestClassification(t *testing.T) {
 		rawComment     string
 		classification string
 		userComment    string
-		rebuildComment string
-		workspaceID    string
 	}{
 		{
 			rawComment:     "1abc",
 			classification: "",
 			userComment:    "1abc",
-			rebuildComment: "1abc",
 		},
 		{
 			rawComment:     "abc",
 			classification: "",
 			userComment:    "abc",
-			rebuildComment: "abc",
 		},
 		{
 			rawComment:     "-abc",
 			classification: "",
 			userComment:    "-abc",
-			rebuildComment: "-abc",
 		},
 		{
 			rawComment:     "0-abc",
 			classification: "0",
 			userComment:    "abc",
-			rebuildComment: "0-abc",
 		},
 		{
 			rawComment:     "1-2-abc",
 			classification: "1-2",
 			userComment:    "abc",
-			rebuildComment: "1-2-abc",
-		},
-		{
-			rawComment:     "1-2 abc",
-			classification: "1-2",
-			userComment:    "abc",
-			rebuildComment: "1-2-abc",
-			workspaceID:    "a4fea42c-c097-47c3-b661-4a6fcea1cf6d",
-		},
-		{
-			rawComment:     "1 abc",
-			classification: "1",
-			userComment:    "abc",
-			rebuildComment: "1-abc",
-			workspaceID:    "a4fea42c-c097-47c3-b661-4a6fcea1cf6d",
 		},
 		{
 			rawComment:     "1",
 			classification: "1",
 			userComment:    "",
-			rebuildComment: "1",
 		},
 		{
 			rawComment:     "1- 2",
 			classification: "1",
 			userComment:    " 2",
-			rebuildComment: "1- 2",
-		},
-		{
-			rawComment:     "1 2",
-			classification: "1",
-			userComment:    "2",
-			rebuildComment: "1-2",
-			workspaceID:    "a4fea42c-c097-47c3-b661-4a6fcea1cf6d",
 		},
 		{
 			rawComment:     "1-2",
 			classification: "1-2",
 			userComment:    "",
-			rebuildComment: "1-2",
 		},
 		{
 			rawComment:     "1-2a",
 			classification: "1",
 			userComment:    "2a",
-			rebuildComment: "1-2a",
 		},
 		{
 			rawComment:     "1:a",
 			classification: "",
 			userComment:    "1:a",
-			rebuildComment: "1:a",
+		},
+		{
+			rawComment:     "1-2 abc",
+			classification: "1",
+			userComment:    "2 abc",
+		},
+		{
+			rawComment:     "1 2",
+			classification: "",
+			userComment:    "1 2",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("test classification for comment: %v", test.rawComment), func(t *testing.T) {
-			err := os.Setenv("BYTEBASE_WORKSPACE_ID", test.workspaceID)
-			assert.NoError(t, err)
 			classification, userComment := GetClassificationAndUserComment(test.rawComment)
 			assert.Equal(t, test.classification, classification)
 			assert.Equal(t, test.userComment, userComment)
 			rebuildComment := GetCommentFromClassificationAndUserComment(classification, userComment)
-			assert.Equal(t, test.rebuildComment, rebuildComment)
+			assert.Equal(t, test.rawComment, rebuildComment)
 		})
 	}
 }
