9b1c233b42bb9ca49c66458f68263bfa179271a2
boojack <stevenlgtm@gmail.com>
2024-03-20 16:18:34 +0800

    chore: update view project members by role (#11180)
    * chore: update view members by role
    
    * chore: update
    
    * chore: update
    
    * chore: update

---
+++ frontend/src/components/ProjectMember/ProjectMemberDataTableByRole/cells/UserNameCell.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="flex flex-row items-center space-x-2">
+    <UserAvatar :user="user" />
+
+    <div class="flex flex-row items-center">
+      <div class="flex flex-col">
+        <div class="flex flex-row items-center space-x-2">
+          <router-link
+            :to="`/users/${user.email}`"
+            class="normal-link truncate max-w-[10em]"
+          >
+            {{ user.title }}
+          </router-link>
+          <YouTag v-if="currentUserV1.name === user.name" />
+          <SystemBotTag v-if="user.name === SYSTEM_BOT_USER_NAME" />
+          <ServiceAccountTag
+            v-if="user.userType === UserType.SERVICE_ACCOUNT"
+          />
+        </div>
+        <span v-if="user.name !== SYSTEM_BOT_USER_NAME" class="textlabel">
+          {{ user.email }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useCurrentUserV1 } from "@/store";
+import { SYSTEM_BOT_USER_NAME } from "@/types";
+import { User, UserType } from "@/types/proto/v1/auth_service";
+import { ProjectMember } from "../../types";
+
+const props = defineProps<{
+  projectMember: ProjectMember;
+}>();
+
+defineEmits<{
+  (event: "reset-service-key", user: User): void;
+}>();
+
+const currentUserV1 = useCurrentUserV1();
+
+const user = computed(() => props.projectMember.user);
+</script>
