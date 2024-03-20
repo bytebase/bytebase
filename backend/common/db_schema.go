ff451994b5d44ca548192ed2230604ffe4ab84fb
ecmadao <wlec@outlook.com>
2024-03-20 11:53:20 +0800

    chore: revert changes

--- backend/common/db_schema.go
+++ backend/common/db_schema.go
@@ -1,43 +1,14 @@
 package common
 
 import (
-	"fmt"
-	"os"
 	"regexp"
 	"strings"
 )
 
-// classificationIDPattern is the pattern for classification, it should joined id (numbers) by "-", like 1-2, 1-2-3.
-const classificationIDPattern = "^[0-9]+(-([0-9])+){0,}"
-
 var numberReg = regexp.MustCompile("^[0-9]+$")
-
-func getClassificationAndUserCommentFromLegacy(comment string) (string, string) {
-	classificationIDReg := regexp.MustCompile(classificationIDPattern)
-	classification := classificationIDReg.FindString(comment)
-	if classification == comment {
-		// the extract classification id matches full comment, for example, raw comment is "1-2-3"
-		return classification, ""
-	}
-
-	// we will handle "{classification id}-{comment}" and "{classification id} {comment}"
-	classificationIDReg = regexp.MustCompile(fmt.Sprintf(`%s[-|\s]{1}`, classificationIDPattern))
-	classification = classificationIDReg.FindString(comment)
-
-	userComment := strings.TrimPrefix(comment, classification)
-	classification = strings.TrimSuffix(classification, "-")
-	classification = strings.TrimSuffix(classification, " ")
-	return classification, userComment
-}
 
 // GetClassificationAndUserComment parses classification and user comment from the given comment.
 func GetClassificationAndUserComment(comment string) (string, string) {
-	workspaceID := os.Getenv("BYTEBASE_WORKSPACE_ID")
-	if workspaceID == "a4fea42c-c097-47c3-b661-4a6fcea1cf6d" {
-		// handle legacy data in specific workspace
-		return getClassificationAndUserCommentFromLegacy(comment)
-	}
-
 	sections := strings.Split(comment, "-")
 	classification := []string{}
 	userComment := ""
