2fe651259cb4933573bd9608990585cfa4a674c0
d-bytebase <d@bytebase.com>
2024-03-23 00:19:27 -0700

    chore: add idx_activity_resource_container

--- backend/migrator/migration/prod/2.15/0001##activity_container.sql
+++ backend/migrator/migration/prod/2.15/0001##activity_container.sql
@@ -1,2 +1,4 @@
 ALTER TABLE activity ADD COLUMN resource_container TEXT;
+CREATE INDEX idx_activity_resource_container ON activity(resource_container);
+
 DELETE FROM activity WHERE type = 'bb.database.recovery.pitr.done';
