2acef575aa17377af1e8a202aa65d66e1a1bbd66
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-03-23 00:59:13 -0700

    chore: resource_container for issue and pipeline activity (#11215)

--- backend/migrator/migration/prod/2.15/0001##activity_container.sql
+++ backend/migrator/migration/prod/2.15/0001##activity_container.sql
@@ -3,3 +3,5 @@ CREATE INDEX idx_activity_resource_container ON activity(resource_container);
 
 DELETE FROM activity WHERE type = 'bb.database.recovery.pitr.done';
 UPDATE activity SET resource_container = 'projects/' || coalesce((SELECT resource_id FROM project WHERE project.id = activity.container_id), 'default') WHERE type LIKE '%project%';
+UPDATE activity SET resource_container = 'projects/' || coalesce((SELECT project.resource_id FROM project JOIN issue ON project.id = issue.project_id WHERE issue.id = activity.container_id), 'default') WHERE type LIKE '%issue%';
+UPDATE activity SET resource_container = 'projects/' || coalesce((SELECT project.resource_id FROM project JOIN pipeline ON project.id = pipeline.project_id WHERE pipeline.id = activity.container_id), 'default') WHERE type LIKE '%pipeline%';
