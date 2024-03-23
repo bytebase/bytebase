c638d29bef996085b5463e8dcb4f3da04a873ef2
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-03-23 00:46:41 -0700

    chore: resource_container for project activity (#11214)
    Former-commit-id: 33a3ec5b6b8c6ce42aacd6063b489b43051fc54b
    Former-commit-id: f80e89ecd00f6f5b35c92d3e54ab3dfe3412e77e

--- backend/migrator/migration/prod/2.15/0001##activity_container.sql
+++ backend/migrator/migration/prod/2.15/0001##activity_container.sql
@@ -2,3 +2,4 @@ ALTER TABLE activity ADD COLUMN resource_container TEXT;
 CREATE INDEX idx_activity_resource_container ON activity(resource_container);
 
 DELETE FROM activity WHERE type = 'bb.database.recovery.pitr.done';
+UPDATE activity SET resource_container = 'projects/' || coalesce((SELECT resource_id FROM project WHERE project.id = activity.container_id), 'default') WHERE type LIKE '%project%';
