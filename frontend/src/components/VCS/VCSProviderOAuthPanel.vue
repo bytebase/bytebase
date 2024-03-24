602b559bbbaf76a42d7613988fdba5fb2a53fdcf
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-03-24 06:37:50 -0700

    chore: remove callback table (#11226)
    * chore: remove callback table
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 14cd46e2c1d93dacc9bc72378caabea3fa049ad4
    Former-commit-id: ed8f7974c25a46dc1bef3a6c1b5f706a7f383fe5

--- frontend/src/components/VCS/VCSProviderOAuthPanel.vue
+++ frontend/src/components/VCS/VCSProviderOAuthPanel.vue
@@ -62,65 +62,12 @@
               "gitops.setting.add-git-provider.oauth-info.gitlab-self-host-visit-admin-page"
             )
           }}
-          <a
-            :href="createOAuthApplicationUrl"
-            target="_blank"
-            class="normal-link"
-            >{{
-              $t("gitops.setting.add-git-provider.oauth-info.direct-link")
-            }}</a
-          >
         </li>
         <li>
           3.
           {{
             $t("gitops.setting.add-git-provider.oauth-info.create-oauth-app")
           }}
-          <div class="m-4 flex justify-center">
-            <dl
-              class="divide-y divide-block-border border border-block-border shadow rounded-lg"
-            >
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Name
-                </dt>
-                <dd class="text-sm text-main">Bytebase</dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2 items-center">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Redirect URI
-                </dt>
-                <dd class="text-sm text-main items-center flex">
-                  {{ redirectUrl() }}
-                  <button
-                    tabindex="-1"
-                    class="ml-1 text-sm font-medium text-control-light hover:bg-gray-100"
-                    @click.prevent="copyRedirectURI"
-                  >
-                    <heroicons-outline:clipboard class="w-6 h-6" />
-                  </button>
-                </dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Trusted
-                </dt>
-                <dd class="text-sm text-main">Yes</dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Confidential
-                </dt>
-                <dd class="text-sm text-main">Yes</dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Scopes
-                </dt>
-                <dd class="text-sm text-main">api</dd>
-              </div>
-            </dl>
-          </div>
         </li>
         <li>
           4.
@@ -145,59 +92,12 @@
               "gitops.setting.add-git-provider.oauth-info.gitlab-com-visit-application-page"
             )
           }}
-          <a
-            :href="createOAuthApplicationUrl"
-            target="_blank"
-            class="normal-link"
-            >{{
-              $t("gitops.setting.add-git-provider.oauth-info.direct-link")
-            }}</a
-          >
         </li>
         <li>
           3.
           {{
             $t("gitops.setting.add-git-provider.oauth-info.create-oauth-app")
           }}
-          <div class="m-4 flex justify-center">
-            <dl
-              class="divide-y divide-block-border border border-block-border shadow rounded-lg"
-            >
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Name
-                </dt>
-                <dd class="text-sm text-main">Bytebase</dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2 items-center">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Redirect URI
-                </dt>
-                <dd class="text-sm text-main items-center flex">
-                  {{ redirectUrl() }}
-                  <button
-                    tabindex="-1"
-                    class="ml-1 text-sm font-medium text-control-light hover:bg-gray-100"
-                    @click.prevent="copyRedirectURI"
-                  >
-                    <heroicons-outline:clipboard class="w-6 h-6" />
-                  </button>
-                </dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Confidential
-                </dt>
-                <dd class="text-sm text-main">Yes</dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Scopes
-                </dt>
-                <dd class="text-sm text-main">api</dd>
-              </div>
-            </dl>
-          </div>
         </li>
         <li>
           4.
@@ -231,48 +131,6 @@
           {{
             $t("gitops.setting.add-git-provider.oauth-info.create-oauth-app")
           }}
-          <div class="m-4 flex justify-center">
-            <dl
-              class="divide-y divide-block-border border border-block-border shadow rounded-lg"
-            >
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Application name
-                </dt>
-                <dd class="text-sm text-main">Bytebase</dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2 items-center">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Homepage URL
-                </dt>
-                <dd class="text-sm text-main items-center flex">
-                  https://bytebase.com
-                  <button
-                    tabindex="-1"
-                    class="ml-1 text-sm font-medium text-control-light hover:bg-gray-100"
-                    @click.prevent="copyHomepageURL"
-                  >
-                    <heroicons-outline:clipboard class="w-6 h-6" />
-                  </button>
-                </dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2 items-center">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Authorization callback URL
-                </dt>
-                <dd class="text-sm text-main items-center flex">
-                  {{ redirectUrl() }}
-                  <button
-                    tabindex="-1"
-                    class="ml-1 text-sm font-medium text-control-light hover:bg-gray-100"
-                    @click.prevent="copyRedirectURI"
-                  >
-                    <heroicons-outline:clipboard class="w-6 h-6" />
-                  </button>
-                </dd>
-              </div>
-            </dl>
-          </div>
         </li>
         <li>
           4.
@@ -306,48 +164,6 @@
           {{
             $t("gitops.setting.add-git-provider.oauth-info.create-oauth-app")
           }}
-          <div class="m-4 flex justify-center">
-            <dl
-              class="divide-y divide-block-border border border-block-border shadow rounded-lg"
-            >
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Application name
-                </dt>
-                <dd class="text-sm text-main">Bytebase</dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2 items-center">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Homepage URL
-                </dt>
-                <dd class="text-sm text-main items-center flex">
-                  https://bytebase.com
-                  <button
-                    tabindex="-1"
-                    class="ml-1 text-sm font-medium text-control-light hover:bg-gray-100"
-                    @click.prevent="copyHomepageURL"
-                  >
-                    <heroicons-outline:clipboard class="w-6 h-6" />
-                  </button>
-                </dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2 items-center">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Authorization callback URL
-                </dt>
-                <dd class="text-sm text-main items-center flex">
-                  {{ redirectUrl() }}
-                  <button
-                    tabindex="-1"
-                    class="ml-1 text-sm font-medium text-control-light hover:bg-gray-100"
-                    @click.prevent="copyRedirectURI"
-                  >
-                    <heroicons-outline:clipboard class="w-6 h-6" />
-                  </button>
-                </dd>
-              </div>
-            </dl>
-          </div>
         </li>
         <li>
           4.
@@ -380,43 +196,6 @@
           {{
             $t("gitops.setting.add-git-provider.oauth-info.create-oauth-app")
           }}
-          <div class="m-4 flex justify-center">
-            <dl
-              class="divide-y divide-block-border border border-block-border shadow rounded-lg"
-            >
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Name
-                </dt>
-                <dd class="text-sm text-main">Bytebase</dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2 items-center">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Callback URL
-                </dt>
-                <dd class="text-sm text-main items-center flex">
-                  {{ redirectUrl() }}
-                  <button
-                    tabindex="-1"
-                    class="ml-1 text-sm font-medium text-control-light hover:bg-gray-100"
-                    @click.prevent="copyRedirectURI"
-                  >
-                    <heroicons-outline:clipboard class="w-6 h-6" />
-                  </button>
-                </dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Permissions
-                </dt>
-                <dd class="text-sm text-main">
-                  Account > Read, Webhooks > Read and write, Repositories >
-                  Write and Admin, Pipelines > Edit variables, Pull requests >
-                  Write
-                </dd>
-              </div>
-            </dl>
-          </div>
         </li>
         <li>
           4.
@@ -441,71 +220,12 @@
               "gitops.setting.add-git-provider.oauth-info.azure-devops-visit-application-page"
             )
           }}
-          <a
-            :href="createOAuthApplicationUrl"
-            target="_blank"
-            class="normal-link"
-          >
-            {{ $t("gitops.setting.add-git-provider.oauth-info.direct-link") }}
-          </a>
         </li>
         <li>
           3.
           {{
             $t("gitops.setting.add-git-provider.oauth-info.create-oauth-app")
           }}
-          <div class="m-4 flex justify-center">
-            <dl
-              class="divide-y divide-block-border border border-block-border shadow rounded-lg"
-            >
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Name
-                </dt>
-                <dd class="text-sm text-main">Bytebase</dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2 items-center">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Homepage URL
-                </dt>
-                <dd class="text-sm text-main items-center flex">
-                  https://bytebase.com
-                  <button
-                    tabindex="-1"
-                    class="ml-1 text-sm font-medium text-control-light hover:bg-gray-100"
-                    @click.prevent="copyHomepageURL"
-                  >
-                    <heroicons-outline:clipboard class="w-6 h-6" />
-                  </button>
-                </dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2 items-center">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Redirect URL
-                </dt>
-                <dd class="text-sm text-main items-center flex">
-                  {{ redirectUrl() }}
-                  <button
-                    tabindex="-1"
-                    class="ml-1 text-sm font-medium text-control-light hover:bg-gray-100"
-                    @click.prevent="copyRedirectURI"
-                  >
-                    <heroicons-outline:clipboard class="w-6 h-6" />
-                  </button>
-                </dd>
-              </div>
-              <div class="grid grid-cols-2 gap-4 px-4 py-2">
-                <dt class="text-sm font-medium text-control-light text-right">
-                  Scopes
-                </dt>
-                <!-- TODO: decide necessary scopes -->
-                <dd class="text-sm text-main">
-                  Code (full), Identity (read), Project and team (read), Build
-                  (read and execute)
-                </dd>
-              </div>
-            </dl>
-          </div>
         </li>
         <li>
           4.
@@ -532,50 +252,11 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
-import { pushNotification } from "@/store";
-import { VCSConfig, redirectUrl } from "@/types";
-import { toClipboard } from "@/utils";
+import { VCSConfig } from "@/types";
 
 const props = defineProps<{
   config: VCSConfig;
 }>();
-
-const { t } = useI18n();
-
-const createOAuthApplicationUrl = computed((): string => {
-  if (props.config.uiType == "GITLAB_SELF_HOST") {
-    return `${props.config.instanceUrl}/admin/applications/new`;
-  } else if (props.config.uiType == "GITLAB_COM") {
-    return `https://gitlab.com/-/profile/applications`;
-  } else if (props.config.uiType == "GITHUB_COM") {
-    return `https://github.com/settings/applications/new`;
-  } else if (props.config.uiType == "AZURE_DEVOPS") {
-    return `https://app.vsaex.visualstudio.com/app/register`;
-  }
-  return "";
-});
-
-const copyHomepageURL = () => {
-  toClipboard("https://bytebase.com").then(() => {
-    pushNotification({
-      module: "bytebase",
-      style: "INFO",
-      title: t("gitops.setting.add-git-provider.oauth-info.copy-homepage-url"),
-    });
-  });
-};
-
-const copyRedirectURI = () => {
-  toClipboard(redirectUrl()).then(() => {
-    pushNotification({
-      module: "bytebase",
-      style: "INFO",
-      title: t("gitops.setting.add-git-provider.oauth-info.copy-redirect-uri"),
-    });
-  });
-};
 
 const changeAccessToken = (value: string) => {
   // eslint-disable-next-line vue/no-mutating-props
