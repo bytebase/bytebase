b36055ed98725dcd578dee536d529cad624c8645
d-bytebase <d@bytebase.com>
2024-03-24 21:43:26 -0700

    chore: resource id

--- backend/migrator/migration/prod/LATEST.sql
+++ backend/migrator/migration/prod/LATEST.sql
@@ -854,11 +854,14 @@ CREATE TABLE vcs (
     created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
     updater_id INTEGER NOT NULL REFERENCES principal (id),
     updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    resource_id TEXT NOT NULL,
     name TEXT NOT NULL,
     type TEXT NOT NULL CHECK (type IN ('GITLAB', 'GITHUB', 'BITBUCKET', 'AZURE_DEVOPS')),
     instance_url TEXT NOT NULL CHECK ((instance_url LIKE 'http://%' OR instance_url LIKE 'https://%') AND instance_url = rtrim(instance_url, '/')),
     access_token TEXT NOT NULL DEFAULT ''
 );
+
+CREATE UNIQUE INDEX idx_vcs_unique_resource_id ON vcs(resource_id);
 
 ALTER SEQUENCE vcs_id_seq RESTART WITH 101;
 
@@ -880,6 +883,7 @@ CREATE TABLE repository (
     updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
     vcs_id INTEGER NOT NULL REFERENCES vcs (id),
     project_id INTEGER NOT NULL REFERENCES project (id),
+    resource_id TEXT NOT NULL,
     -- Name from the corresponding VCS provider.
     -- For GitLab, this is the project name. e.g. project 1
     name TEXT NOT NULL,
@@ -913,7 +917,7 @@ CREATE TABLE repository (
     webhook_secret_token TEXT NOT NULL
 );
 
-CREATE UNIQUE INDEX idx_repository_unique_project_id ON repository(project_id);
+CREATE UNIQUE INDEX idx_repository_unique_project_id_resource_id ON repository(project_id, resource_id);
 
 ALTER SEQUENCE repository_id_seq RESTART WITH 101;
 
