54a78069b5a602f93415ce505e4922f4b7e470f2
steven <stevenlgtm@gmail.com>
2024-03-25 16:24:30 +0800

    chore: update

--- backend/migrator/migration/prod/LATEST.sql
+++ backend/migrator/migration/prod/LATEST.sql
@@ -1234,9 +1234,7 @@ EXECUTE FUNCTION trigger_update_updated_ts();
 
 CREATE TABLE export_archive (
   id SERIAL PRIMARY KEY,
-  row_status row_status NOT NULL DEFAULT 'NORMAL',
   created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-  updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
   bytes BYTEA,
   payload JSONB NOT NULL DEFAULT '{}'
 );
