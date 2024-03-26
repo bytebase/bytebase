8b72a1c478186bfb2dd622df959f9da4f6d6b2b3
d-bytebase <d@bytebase.com>
2024-03-26 10:11:49 -0700

    chore: query history dml

--- backend/migrator/migration/prod/2.15/0012##query_history.sql
+++ backend/migrator/migration/prod/2.15/0012##query_history.sql
@@ -13,3 +13,13 @@ CREATE TABLE query_history (
 CREATE INDEX idx_query_history_creator_id_created_ts_project_id ON query_history(creator_id, created_ts, project_id DESC);
 
 ALTER SEQUENCE query_history_id_seq RESTART WITH 101;
+
+INSERT INTO query_history(creator_id, created_ts, project_id, database, statement, type)
+SELECT
+	creator_id,
+	created_ts,
+	REPLACE(resource_container, 'projects/', ''),
+	(SELECT 'instances/' || instance.resource_id || '/databases/' || db.name FROM instance JOIN db ON instance.id = db.instance_id WHERE (db.id)::text = (activity.payload->'databaseId')::text),
+	TRIM('"' FROM (activity.payload->'statement')::text),
+	'QUERY'
+FROM activity WHERE type = 'bb.sql.query';
