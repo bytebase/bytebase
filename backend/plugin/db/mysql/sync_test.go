4e2e2152f1f182301d93c75ff5f6c6469b7c3a38
h3n4l <oysterdays@gmail.com>
2024-03-26 11:22:35 +0800

    fix: reconcile view def (#11330)
    * fix: reconcile view def
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    ---------
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    Former-commit-id: dcb008e4ec43005a2faa0b82d85401c3c4c4ca4b
    Former-commit-id: 46e5d9529c96143847cfb7b5b02c22660a661941

--- backend/plugin/db/mysql/sync_test.go
+++ backend/plugin/db/mysql/sync_test.go
@@ -431,3 +431,25 @@ func TestSetColumnMetadataDefault(t *testing.T) {
 		a.Equal(tc.want, column, tc.name)
 	}
 }
+
+func TestGetViewDefFromCreateView(t *testing.T) {
+	testCases := []struct {
+		stmt string
+		def  string
+	}{
+		{
+			stmt: "CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY INVOKER VIEW `actor_info` AS select `a`.`actor_id` AS `actor_id`,`a`.`first_name` AS `first_name`,`a`.`last_name` AS `last_name`,group_concat(distinct concat(`c`.`name`,': ',(select group_concat(`f`.`title` order by `f`.`title` ASC separator ', ') from ((`film` `f` join `film_category` `fc` on((`f`.`film_id` = `fc`.`film_id`))) join `film_actor` `fa` on((`f`.`film_id` = `fa`.`film_id`))) where ((`fc`.`category_id` = `c`.`category_id`) and (`fa`.`actor_id` = `a`.`actor_id`)))) order by `c`.`name` ASC separator '; ') AS `film_info` from (((`actor` `a` left join `film_actor` `fa` on((`a`.`actor_id` = `fa`.`actor_id`))) left join `film_category` `fc` on((`fa`.`film_id` = `fc`.`film_id`))) left join `category` `c` on((`fc`.`category_id` = `c`.`category_id`))) group by `a`.`actor_id`,`a`.`first_name`,`a`.`last_name`",
+			def:  "select `a`.`actor_id` AS `actor_id`,`a`.`first_name` AS `first_name`,`a`.`last_name` AS `last_name`,group_concat(distinct concat(`c`.`name`,': ',(select group_concat(`f`.`title` order by `f`.`title` ASC separator ', ') from ((`film` `f` join `film_category` `fc` on((`f`.`film_id` = `fc`.`film_id`))) join `film_actor` `fa` on((`f`.`film_id` = `fa`.`film_id`))) where ((`fc`.`category_id` = `c`.`category_id`) and (`fa`.`actor_id` = `a`.`actor_id`)))) order by `c`.`name` ASC separator '; ') AS `film_info` from (((`actor` `a` left join `film_actor` `fa` on((`a`.`actor_id` = `fa`.`actor_id`))) left join `film_category` `fc` on((`fa`.`film_id` = `fc`.`film_id`))) left join `category` `c` on((`fc`.`category_id` = `c`.`category_id`))) group by `a`.`actor_id`,`a`.`first_name`,`a`.`last_name`",
+		},
+		{
+			stmt: "CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY INVOKER VIEW `actor_info` (`id`) AS select idid from t",
+			def:  "select idid from t",
+		},
+	}
+
+	for _, tc := range testCases {
+		got, err := getViewDefFromCreateView(tc.stmt)
+		require.NoError(t, err)
+		require.Equal(t, tc.def, got)
+	}
+}
