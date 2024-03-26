3917c325ebf9f7c14e95541b3ffe7889d674c59c
ecmadao <wlec@outlook.com>
2024-03-26 18:10:28 +0800

    chore: update

--- backend/migrator/migration/prod/2.15/0012##query_history.sql
+++ backend/migrator/migration/prod/2.15/0012##query_history.sql
@@ -8,7 +8,18 @@ CREATE TABLE query_history (
     project_id INTEGER NOT NULL REFERENCES project (id),
     database_id INTEGER NULL REFERENCES db (id),
     statement TEXT NOT NULL,
-    database TEXT NOT NULL,
     type TEXT NOT NULL, -- the history type, support QUERY and EXPORT.
     payload JSONB NOT NULL DEFAULT '{}' -- saved for details, like error, duration, etc.
 );
+
+CREATE INDEX idx_query_history_creator_id_project_id ON query_history(creator_id, project_id);
+
+CREATE INDEX idx_query_history_created_ts ON query_history(created_ts);
+
+ALTER SEQUENCE query_history_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_query_history_updated_ts
+BEFORE
+UPDATE
+    ON query_history FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
--- backend/migrator/migration/prod/LATEST.sql
+++ backend/migrator/migration/prod/LATEST.sql
@@ -799,6 +799,33 @@ UPDATE
     ON activity FOR EACH ROW
 EXECUTE FUNCTION trigger_update_updated_ts();
 
+CREATE TABLE query_history (
+    id BIGSERIAL PRIMARY KEY,
+    row_status row_status NOT NULL DEFAULT 'NORMAL',
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    updater_id INTEGER NOT NULL REFERENCES principal (id),
+    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    project_id INTEGER NOT NULL REFERENCES project (id),
+    database_id INTEGER NULL REFERENCES db (id),
+    statement TEXT NOT NULL,
+    database TEXT NOT NULL,
+    type TEXT NOT NULL, -- the history type, support QUERY and EXPORT.
+    payload JSONB NOT NULL DEFAULT '{}' -- saved for details, like error, duration, etc.
+);
+
+CREATE INDEX idx_query_history_creator_id_project_id ON query_history(creator_id, project_id);
+
+CREATE INDEX idx_query_history_created_ts ON query_history(created_ts);
+
+ALTER SEQUENCE query_history_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_query_history_updated_ts
+BEFORE
+UPDATE
+    ON query_history FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
+
 -- inbox table stores the inbox entry for the corresponding activity.
 -- Unlike other tables, it doesn't have row_status/creator_id/created_ts/updater_id/updated_ts.
 -- We design in this way because:
