86778ee69fb00cdbd0213375bb63a7e923fceb44
p0ny <xz@bytebase.com>
2024-03-29 12:12:44 +0800

    feat: add issue_comment table (#11397)

--- backend/migrator/migration/prod/LATEST.sql
+++ backend/migrator/migration/prod/LATEST.sql
@@ -799,6 +799,27 @@ UPDATE
     ON activity FOR EACH ROW
 EXECUTE FUNCTION trigger_update_updated_ts();
 
+CREATE TABLE issue_comment (
+    id BIGSERIAL PRIMARY KEY,
+    row_status row_status NOT NULL DEFAULT 'NORMAL',
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    updater_id INTEGER NOT NULL REFERENCES principal (id),
+    updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    issue_id INTEGER NOT NULL REFERENCES issue (id),
+    payload JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE INDEX idx_issue_comment_issue_id ON issue_comment(issue_id);
+
+ALTER SEQUENCE issue_comment_id_seq RESTART WITH 101;
+
+CREATE TRIGGER update_issue_comment_updated_ts
+BEFORE
+UPDATE
+    ON issue_comment FOR EACH ROW
+EXECUTE FUNCTION trigger_update_updated_ts();
+
 CREATE TABLE query_history (
     id BIGSERIAL PRIMARY KEY,
     row_status row_status NOT NULL DEFAULT 'NORMAL',
