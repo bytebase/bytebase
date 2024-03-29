0d2c6144272bdcd8bbfe19c8710751a6e7483c33
steven <stevenlgtm@gmail.com>
2024-03-29 15:53:20 +0800

    chore: update

--- frontend/src/components/IssueV1/components/GrantRequest/GrantRequestExporterForm.vue
+++ frontend/src/components/IssueV1/components/GrantRequest/GrantRequestExporterForm.vue
@@ -9,30 +9,13 @@
       <div
         class="w-full flex flex-row justify-start items-start flex-wrap gap-2 gap-x-4"
       >
-        <span
-          v-if="
-            exportMethod !== 'SQL' && selectedDatabaseResources.length === 0
-          "
-          >{{ $t("issue.grant-request.all-databases") }}</span
-        >
+        <span v-if="selectedDatabaseResources.length === 0">{{
+          $t("issue.grant-request.all-databases")
+        }}</span>
         <DatabaseResourceTable
           v-else
           class="w-full"
           :database-resource-list="selectedDatabaseResources"
-        />
-      </div>
-    </div>
-    <div
-      v-if="exportMethod === 'SQL'"
-      class="w-full flex flex-col justify-start items-start"
-    >
-      <span class="flex items-center textlabel mb-2">SQL</span>
-      <div class="w-full border rounded min-h-[6rem] relative">
-        <MonacoEditor
-          class="w-full h-[300px]"
-          :content="state.statement"
-          :auto-focus="false"
-          :readonly="true"
         />
       </div>
     </div>
@@ -57,8 +40,7 @@
 
 <script lang="ts" setup>
 import dayjs from "dayjs";
-import { computed, onMounted, reactive, ref } from "vue";
-import { MonacoEditor } from "@/components/MonacoEditor";
+import { onMounted, reactive, ref } from "vue";
 import type { DatabaseResource } from "@/types";
 import { PresetRoleType } from "@/types";
 import { convertFromCELString } from "@/utils/issue/cel";
@@ -67,21 +49,15 @@ import DatabaseResourceTable from "./DatabaseResourceTable.vue";
 
 interface LocalState {
   maxRowCount: number;
-  statement: string;
   expiredAt: string;
 }
 
 const { issue } = useIssueContext();
 const state = reactive<LocalState>({
   maxRowCount: 1000,
-  statement: "",
   expiredAt: "",
 });
 const selectedDatabaseResources = ref<DatabaseResource[]>([]);
-
-const exportMethod = computed(() => {
-  return state.statement === "" ? "DATABASE" : "SQL";
-});
 
 onMounted(async () => {
   const grantRequest = issue.value.grantRequest!;
@@ -104,9 +80,6 @@ onMounted(async () => {
     );
   } else {
     state.expiredAt = "-";
-  }
-  if (conditionExpression.statement !== undefined) {
-    state.statement = conditionExpression.statement;
   }
   if (conditionExpression.rowLimit !== undefined) {
     state.maxRowCount = conditionExpression.rowLimit;
