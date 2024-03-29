7ed7a13a45eb523bb348749658a2e9480ac1e6d2
h3n4l <oysterdays@gmail.com>
2024-03-29 17:01:16 +0800

    feat: support Query Validator for MSSQL (#11405)
    * fix: query item like  in TSQL join
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    ---------
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    Former-commit-id: 14518d584d1caee69fe372470444d68449224fa6
    Former-commit-id: a17a177dabea7dfc34f0112dbccf743fb76767d5

--- backend/plugin/parser/tsql/query.go
+++ backend/plugin/parser/tsql/query.go
@@ -13,6 +13,97 @@ import (
 
 func init() {
 	base.RegisterExtractResourceListFunc(storepb.Engine_MSSQL, ExtractResourceList)
+	base.RegisterQueryValidator(storepb.Engine_MSSQL, ValidateSQLForEditor)
+}
+
+func ValidateSQLForEditor(statement string) (bool, error) {
+	parseResult, err := ParseTSQL(statement)
+	if err != nil {
+		return false, err
+	}
+	if parseResult == nil {
+		return false, nil
+	}
+
+	l := &queryValidateListener{
+		valid: true,
+	}
+	antlr.ParseTreeWalkerDefault.Walk(l, parseResult.Tree)
+	return l.valid, nil
+}
+
+type queryValidateListener struct {
+	*parser.BaseTSqlParserListener
+
+	valid bool
+}
+
+func (q *queryValidateListener) EnterBatch(ctx *parser.BatchContext) {
+	if !q.valid {
+		return
+	}
+	if ctx.Batch_level_statement() != nil {
+		q.valid = false
+		return
+	}
+}
+
+func (q *queryValidateListener) EnterSql_clauses(ctx *parser.Sql_clausesContext) {
+	if !q.valid {
+		return
+	}
+	if ctx.Dml_clause() == nil {
+		q.valid = false
+		return
+	}
+}
+
+func (q *queryValidateListener) EnterDml_clause(ctx *parser.Dml_clauseContext) {
+	if !q.valid {
+		return
+	}
+	_, ok := ctx.GetParent().(*parser.Sql_clausesContext)
+	if !ok {
+		return
+	}
+	if ctx.Select_statement_standalone() == nil {
+		q.valid = false
+		return
+	}
+}
+
+func (q *queryValidateListener) EnterSelect_statement_standalone(ctx *parser.Select_statement_standaloneContext) {
+	if !q.valid {
+		return
+	}
+	_, ok := ctx.GetParent().(*parser.Dml_clauseContext)
+	if !ok {
+		return
+	}
+	if ctx.Select_statement() == nil {
+		q.valid = false
+		return
+	}
+}
+
+func (q *queryValidateListener) EnterQuery_specification(ctx *parser.Query_specificationContext) {
+	if !q.valid {
+		return
+	}
+	if ctx.INTO() != nil {
+		// For Into clause, we only select into temporary table, likes "SELECT ... INTO #temp FROM ...".
+		isValid := false
+		// NOTE: normal mode is not in single session mode, so temporary table is meaningless.
+		// if tableName := ctx.Table_name(); tableName != nil {
+		// 	if allID := tableName.AllId_(); len(allID) == 1 {
+		// 		if id := allID[0].TEMP_ID(); id != nil {
+		// 			isValid = true
+		// 		}
+		// 	}
+		// }
+		q.valid = isValid
+		return
+	}
 }
 
 // ExtractResourceList extracts the list of resources from the SELECT statement, and normalizes the object names with the NON-EMPTY currentNormalizedDatabase and currentNormalizedSchema.
