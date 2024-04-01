e9146452cfb10cd7e29315cbd7b51502df8ca282
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-04-01 18:33:51 +0800

    chore: lint

--- frontend/src/views/sql-editor/AsidePanel/GutterBar/TabItem.vue
+++ frontend/src/views/sql-editor/AsidePanel/GutterBar/TabItem.vue
@@ -2,8 +2,7 @@
   <NTooltip placement="right" :delay="300" :disabled="disabled">
     <template #trigger>
       <div class="px-2 py-3 select-none" :class="classes" v-bind="$attrs">
-        <LinkIcon v-if="tab === 'CONNECTION'" class="w-4 h-4" />
-        <FileCodeIcon v-else-if="tab === 'WORKSHEET'" class="w-4 h-4" />
+        <FileCodeIcon v-if="tab === 'WORKSHEET'" class="w-4 h-4" />
         <DatabaseIcon v-else-if="tab === 'SCHEMA'" class="w-4 h-4" />
         <HistoryIcon v-else-if="tab === 'HISTORY'" class="w-4 h-4" />
         <span v-else></span>
