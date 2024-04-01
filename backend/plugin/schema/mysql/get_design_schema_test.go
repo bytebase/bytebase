1677d6db684bac262b52a86649276b443d754878
h3n4l <oysterdays@gmail.com>
2024-04-01 23:47:45 +0800

    chore: update
    Signed-off-by: h3n4l <oysterdays@gmail.com>

--- backend/plugin/schema/mysql/get_design_schema_test.go
+++ backend/plugin/schema/mysql/get_design_schema_test.go
@@ -22,7 +22,7 @@ func TestGetDesignSchema_Debug(t *testing.T) {
 	TestGetDesignSchema_Partition(t)
 	TestGetDesignSchema_View(t)
 	TestGetDesignSchema_Function(t)
-	TestGetDesignSchema_Procedure(t)
+	// TestGetDesignSchema_Procedure(t)
 }
 
 func TestGetDesignSchema_Basic(t *testing.T) {
@@ -53,10 +53,10 @@ func TestGetDesignSchema_View(t *testing.T) {
 
 func TestGetDesignSchema_Function(t *testing.T) {
 	testGetDesignSchema(t, []string{
-		// "testdata/get-design-schema/function/create-new-function.yaml",
+		"testdata/get-design-schema/function/create-new-function.yaml",
 		"testdata/get-design-schema/function/delete-function.yaml",
 		// "testdata/get-design-schema/function/modify-function.yaml",
-	}, true)
+	}, false)
 }
 
 func TestGetDesignSchema_Procedure(t *testing.T) {
