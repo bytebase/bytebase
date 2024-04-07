b90df757c15310663a20ff6f37cd562b462bc4bf
boojack <stevenlgtm@gmail.com>
2024-04-07 14:48:27 +0800

    chore: hide branding images in auth pages (#11546)
    * chore: hide branding images in auth pages
    
    * chore: update
    
    Former-commit-id: 43acd4f7ad6fd389329a972b8151e8cf4eadb506

--- frontend/src/layouts/SplashLayout.vue
+++ frontend/src/layouts/SplashLayout.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="min-h-screen overflow-hidden flex">
-    <div class="hidden bg-white lg:block relative w-0 flex-1">
+    <div
+      v-if="showBrandingImage"
+      class="hidden bg-white lg:block relative w-0 flex-1"
+    >
       <img
         v-if="route === AUTH_SIGNUP_MODULE"
         class="absolute inset-0 h-full w-full object-cover"
@@ -15,7 +18,7 @@
       />
     </div>
     <div
-      class="relative flex-1 flex flex-col justify-center py-12 pb-24 px-4 sm:px-6 lg:flex-none lg:px-20 lg:w-1/2 xl:px-24"
+      class="relative mx-auto flex-1 flex flex-col justify-center py-12 pb-24 px-4 sm:px-6 lg:flex-none lg:px-20 lg:w-1/2 xl:px-24"
     >
       <router-view />
     </div>
@@ -26,10 +29,20 @@
 import { computed } from "vue";
 import { useRouter } from "vue-router";
 import { AUTH_SIGNUP_MODULE } from "@/router/auth";
+import { useSubscriptionV1Store } from "@/store";
+import { PlanType } from "@/types/proto/v1/subscription_service";
 
 const router = useRouter();
+const subscriptionStore = useSubscriptionV1Store();
 
 const route = computed(() => {
   return router.currentRoute.value.name;
+});
+
+const showBrandingImage = computed(() => {
+  return (
+    subscriptionStore.currentPlan !== PlanType.ENTERPRISE ||
+    subscriptionStore.isTrialing
+  );
 });
 </script>
