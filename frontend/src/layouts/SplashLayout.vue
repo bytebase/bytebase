74cfd1cc12834d095769e48f2a4c91d3148b2ac7
steven <stevenlgtm@gmail.com>
2024-04-07 14:27:13 +0800

    chore: remove branding image in auth pages

--- frontend/src/layouts/SplashLayout.vue
+++ frontend/src/layouts/SplashLayout.vue
@@ -1,35 +1,9 @@
 <template>
   <div class="min-h-screen overflow-hidden flex">
-    <div class="hidden bg-white lg:block relative w-0 flex-1">
-      <img
-        v-if="route === AUTH_SIGNUP_MODULE"
-        class="absolute inset-0 h-full w-full object-cover"
-        src="@/assets/illustration/signup.webp"
-        alt=""
-      />
-      <img
-        v-else
-        class="absolute inset-0 h-full w-full object-cover"
-        src="@/assets/illustration/signin.webp"
-        alt=""
-      />
-    </div>
     <div
-      class="relative flex-1 flex flex-col justify-center py-12 pb-24 px-4 sm:px-6 lg:flex-none lg:px-20 lg:w-1/2 xl:px-24"
+      class="relative mx-auto flex-1 flex flex-col justify-center py-12 pb-24 px-4 sm:px-6 lg:px-20 xl:px-24"
     >
       <router-view />
     </div>
   </div>
 </template>
-
-<script lang="ts" setup>
-import { computed } from "vue";
-import { useRouter } from "vue-router";
-import { AUTH_SIGNUP_MODULE } from "@/router/auth";
-
-const router = useRouter();
-
-const route = computed(() => {
-  return router.currentRoute.value.name;
-});
-</script>
