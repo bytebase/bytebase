90b2536a70ad224fe4de934ec77e124bda750cfb
ecmadao <wlec@outlook.com>
2024-04-08 19:12:23 +0800

    fix: vcs info sidebar

--- frontend/src/components/IssueV1/components/Sidebar/VCSInfo.vue
+++ frontend/src/components/IssueV1/components/Sidebar/VCSInfo.vue
@@ -1,32 +1,23 @@
 <template>
   <div
-    v-if="issue.planEntity?.vcsSource && vcsConnector"
+    v-if="issue.planEntity?.vcsSource?.pullRequestUrl"
     class="text-sm text-control-light flex space-x-1 items-center"
   >
     <VCSIcon :type="issue.planEntity?.vcsSource.vcsType" />
-    <a
-      :href="issue.planEntity?.vcsSource.pullRequestUrl"
-      target="_blank"
-      class="normal-link"
-    >
-      {{ `${vcsConnector.branch}@${vcsConnector.fullPath}` }}
-    </a>
-  </div>
-  <div
-    v-if="
-      issue.planEntity?.vcsSource?.pullRequestUrl !== undefined &&
-      vcsProvider === undefined
-    "
-    class="text-sm text-control-light flex space-x-1 items-center"
-  >
-    <VCSIcon :type="issue.planEntity?.vcsSource.vcsType" />
-    <a
-      :href="issue.planEntity?.vcsSource.pullRequestUrl"
-      target="_blank"
-      class="normal-link"
-    >
-      {{ issue.planEntity?.vcsSource.pullRequestUrl }}
-    </a>
+    <EllipsisText>
+      <a
+        :href="issue.planEntity?.vcsSource.pullRequestUrl"
+        target="_blank"
+        class="normal-link"
+      >
+        <span v-if="vcsConnector">
+          {{ `${vcsConnector.branch}@${vcsConnector.fullPath}` }}
+        </span>
+        <span v-else>
+          {{ issue.planEntity?.vcsSource?.pullRequestUrl }}
+        </span>
+      </a>
+    </EllipsisText>
   </div>
 </template>
 
