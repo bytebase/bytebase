5c06b9661898d8042cd98252b404a1024f2a43ba
d-bytebase <d@bytebase.com>
2024-04-09 02:41:42 -0700

    chore: deprecate legacy issue route

--- frontend/src/router/dashboard/issue.ts
+++ frontend/src/router/dashboard/issue.ts
@@ -1,5 +1,4 @@
 import type { RouteRecordRaw } from "vue-router";
-import ProjectSidebarV1 from "@/components/Project/ProjectSidebarV1.vue";
 import { t } from "@/plugins/i18n";
 import DashboardSidebar from "@/views/DashboardSidebar.vue";
 
@@ -16,19 +15,6 @@ const issueRoutes: RouteRecordRaw[] = [
     components: {
       content: () => import("@/views/IssueDashboard.vue"),
       leftSidebar: DashboardSidebar,
-    },
-    props: { content: true, leftSidebar: true },
-  },
-  // legacy issue detail route have to be kept for a long time.
-  {
-    path: "issue/:issueSlug",
-    name: ISSUE_ROUTE_DETAIL,
-    meta: {
-      overrideTitle: true,
-    },
-    components: {
-      content: () => import("@/views/IssueDetailV1.vue"),
-      leftSidebar: ProjectSidebarV1,
     },
     props: { content: true, leftSidebar: true },
   },
