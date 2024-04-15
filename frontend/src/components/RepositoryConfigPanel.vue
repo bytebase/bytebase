3a5c9bda5ec987a3d41f17c8b145299d9371e54f
ecmadao <wlec@outlook.com>
2024-04-15 16:13:45 +0800

    chore: optimize vcs base directory (#11642)
    * chore: allow change instance environment
    
    * fix: eslint
    
    * fix: select instance role
    
    * chore: optimize vcs base directory

--- frontend/src/components/RepositoryConfigPanel.vue
+++ frontend/src/components/RepositoryConfigPanel.vue
@@ -10,18 +10,11 @@
 </template>
 
 <script lang="ts" setup>
-import type { PropType } from "vue";
 import type { Project } from "@/types/proto/v1/project_service";
 import type { ProjectRepositoryConfig } from "../types";
 
-defineProps({
-  config: {
-    required: true,
-    type: Object as PropType<ProjectRepositoryConfig>,
-  },
-  project: {
-    required: true,
-    type: Object as PropType<Project>,
-  },
-});
+defineProps<{
+  config: ProjectRepositoryConfig;
+  project: Project;
+}>();
 </script>
