64c769745575c7e28d62ba643b8ea93cbb781fe2
p0ny <p0uy@outlook.com>
2024-04-18 17:35:08 +0800

    indexes

--- backend/migrator/migration/prod/LATEST.sql
+++ backend/migrator/migration/prod/LATEST.sql
@@ -648,6 +648,12 @@ CREATE TABLE audit_log (
 
 CREATE INDEX idx_audit_log_created_ts ON audit_log(created_ts);
 
+CREATE INDEX idx_audit_log_payload_method ON audit_log(payload->>'method');
+
+CREATE INDEX idx_audit_log_payload_resource ON audit_log(payload->>'resource');
+
+CREATE INDEX idx_audit_log_payload_user ON audit_log(payload->>'user');
+
 ALTER SEQUENCE audit_log_id_seq RESTART WITH 101;
 
 CREATE TABLE issue_comment (
