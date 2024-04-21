bac4e6da1eb5c6730b577b3424da7dbe3082d7ab
d-bytebase <d@bytebase.com>
2024-04-21 09:07:50 -0700

    chore: update

--- backend/migrator/migration/prod/2.16/0003##schema_tenant_mode.sql
+++ backend/migrator/migration/prod/2.16/0003##schema_tenant_mode.sql
@@ -1,3 +1,3 @@
 UPDATE instance
-SET options = (options || jsonb_build_object('schemaTenantMode', (options->>'schema_tenant_mode')::boolean)) - 'schema_tenant_mode'
-WHERE options ? 'schema_tenant_mode';
+SET options = options - 'schemaTenantMode'
+WHERE options ? 'schemaTenantMode';
