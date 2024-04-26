a10020156436301ebe1df51f90f604b7dfd07a31
boojack <stevenlgtm@gmail.com>
2024-04-26 15:44:02 +0800

    chore: update user store (#11825)

--- frontend/src/store/modules/user.ts
+++ frontend/src/store/modules/user.ts
@@ -1,135 +1,141 @@
 import { isEqual, isUndefined, orderBy } from "lodash-es";
 import { defineStore } from "pinia";
+import { computed, ref } from "vue";
 import { authServiceClient } from "@/grpcweb";
 import type { PrincipalType } from "@/types";
 import { PresetRoleType } from "@/types";
-import { ALL_USERS_USER_ID, ALL_USERS_USER_EMAIL, allUsersUser } from "@/types";
+import { ALL_USERS_USER_EMAIL, allUsersUser } from "@/types";
 import type { UpdateUserRequest, User } from "@/types/proto/v1/auth_service";
 import { UserType } from "@/types/proto/v1/auth_service";
 import { State } from "@/types/proto/v1/common";
 import { userNamePrefix, getUserEmailFromIdentifier } from "./v1/common";
 
-interface UserState {
-  userMapByName: Map<string, User>;
-}
+export const useUserStore = defineStore("user", () => {
+  const userMapByName = ref<Map<string, User>>(new Map());
 
-export const useUserStore = defineStore("user", {
-  state: (): UserState => ({
-    userMapByName: new Map(),
-  }),
-  getters: {
-    userList(state) {
-      return Array.from(state.userMapByName.values());
-    },
-    activeUserList(state) {
-      const list = Array.from(state.userMapByName.values()).filter(
-        (user) => user.state === State.ACTIVE
-      );
-      return orderBy(
-        list,
-        [
-          (user) =>
-            user.userType === UserType.SYSTEM_BOT
-              ? 0
-              : user.userType === UserType.SERVICE_ACCOUNT
-                ? 1
-                : 2,
-          (user) => user.roles.includes(PresetRoleType.WORKSPACE_ADMIN),
-          (user) => user.roles.includes(PresetRoleType.WORKSPACE_DBA),
-        ],
-        ["asc", "desc", "desc"]
-      );
-    },
-  },
-  actions: {
-    async fetchUserList() {
-      const { users } = await authServiceClient.listUsers({
-        showDeleted: true,
-      });
-      for (const user of users) {
-        this.userMapByName.set(user.name, user);
+  const userList = computed(() => {
+    return orderBy(
+      Array.from(userMapByName.value.values()),
+      [
+        (user) =>
+          user.userType === UserType.SYSTEM_BOT
+            ? 0
+            : user.userType === UserType.SERVICE_ACCOUNT
+              ? 1
+              : 2,
+        (user) => user.roles.includes(PresetRoleType.WORKSPACE_ADMIN),
+        (user) => user.roles.includes(PresetRoleType.WORKSPACE_DBA),
+      ],
+      ["asc", "desc", "desc"]
+    );
+  });
+  // The active user list and exclude allUsers.
+  const activeUserList = computed(() => {
+    return userList.value.filter(
+      (user) =>
+        user.state === State.ACTIVE && user.email !== ALL_USERS_USER_EMAIL
+    );
+  });
+
+  const fetchUserList = async () => {
+    const { users } = await authServiceClient.listUsers({
+      showDeleted: true,
+    });
+    for (const user of users) {
+      userMapByName.value.set(user.name, user);
+    }
+    return users;
+  };
+  const fetchUser = async (name: string, silent = false) => {
+    const user = await authServiceClient.getUser(
+      {
+        name,
+      },
+      {
+        silent,
       }
-      return users;
-    },
-    async fetchUser(name: string, silent = false) {
-      const user = await authServiceClient.getUser(
-        {
-          name,
-        },
-        {
-          silent,
-        }
-      );
-      this.userMapByName.set(user.name, user);
-      return user;
-    },
-    async createUser(user: User) {
-      const createdUser = await authServiceClient.createUser({
-        user,
-      });
-      this.userMapByName.set(createdUser.name, createdUser);
-      return createdUser;
-    },
-    async updateUser(updateUserRequest: UpdateUserRequest) {
-      const name = updateUserRequest.user?.name || "";
-      const originData = await this.getOrFetchUserByName(name);
-      if (!originData) {
-        throw new Error(`user with name ${name} not found`);
-      }
-      const user = await authServiceClient.updateUser(updateUserRequest);
-      this.userMapByName.set(user.name, user);
-      return user;
-    },
-    async getOrFetchUserByName(name: string, silent = false) {
-      const cachedData = this.userMapByName.get(name);
-      if (cachedData) {
-        return cachedData;
-      }
-      const user = await this.fetchUser(name, silent);
-      this.userMapByName.set(user.name, user);
-      return user;
-    },
-    getUserByName(name: string) {
-      return this.userMapByName.get(name);
-    },
-    async getOrFetchUserById(uid: string, silent = false) {
-      return await this.getOrFetchUserByName(
-        getUserNameWithUserId(uid),
-        silent
-      );
-    },
-    getUserById(uid: string) {
-      if (uid === ALL_USERS_USER_ID) {
-        return allUsersUser();
-      }
-      return this.userMapByName.get(getUserNameWithUserId(uid));
-    },
-    getUserByIdentifier(identifier: string) {
-      return this.getUserByEmail(getUserEmailFromIdentifier(identifier));
-    },
-    getUserByEmail(email: string) {
-      if (email === ALL_USERS_USER_EMAIL) {
-        return allUsersUser();
-      }
-      return [...this.userMapByName.values()].find(
-        (user) => user.email === email
-      );
-    },
-    async archiveUser(user: User) {
-      await authServiceClient.deleteUser({
-        name: user.name,
-      });
-      user.state = State.DELETED;
-      return user;
-    },
-    async restoreUser(user: User) {
-      const restoredUser = await authServiceClient.undeleteUser({
-        name: user.name,
-      });
-      this.userMapByName.set(restoredUser.name, restoredUser);
-      return restoredUser;
-    },
-  },
+    );
+    userMapByName.value.set(user.name, user);
+    return user;
+  };
+  const createUser = async (user: User) => {
+    const createdUser = await authServiceClient.createUser({
+      user,
+    });
+    userMapByName.value.set(createdUser.name, createdUser);
+    return createdUser;
+  };
+  const updateUser = async (updateUserRequest: UpdateUserRequest) => {
+    const name = updateUserRequest.user?.name || "";
+    const originData = await getOrFetchUserByName(name);
+    if (!originData) {
+      throw new Error(`user with name ${name} not found`);
+    }
+    const user = await authServiceClient.updateUser(updateUserRequest);
+    userMapByName.value.set(user.name, user);
+    return user;
+  };
+  const getOrFetchUserByName = async (name: string, silent = false) => {
+    const cachedData = userMapByName.value.get(name);
+    if (cachedData) {
+      return cachedData;
+    }
+    const user = await fetchUser(name, silent);
+    userMapByName.value.set(user.name, user);
+    return user;
+  };
+  const getUserByName = (name: string) => {
+    return userMapByName.value.get(name);
+  };
+  const getOrFetchUserById = async (uid: string, silent = false) => {
+    return await getOrFetchUserByName(getUserNameWithUserId(uid), silent);
+  };
+  const getUserById = (uid: string) => {
+    return getUserByName(getUserNameWithUserId(uid));
+  };
+  const getUserByIdentifier = (identifier: string) => {
+    return getUserByEmail(getUserEmailFromIdentifier(identifier));
+  };
+  const getUserByEmail = (email: string) => {
+    if (email === ALL_USERS_USER_EMAIL) {
+      return allUsersUser();
+    }
+    return [...userMapByName.value.values()].find(
+      (user) => user.email === email
+    );
+  };
+  const archiveUser = async (user: User) => {
+    await authServiceClient.deleteUser({
+      name: user.name,
+    });
+    user.state = State.DELETED;
+    return user;
+  };
+  const restoreUser = async (user: User) => {
+    const restoredUser = await authServiceClient.undeleteUser({
+      name: user.name,
+    });
+    userMapByName.value.set(restoredUser.name, restoredUser);
+    return restoredUser;
+  };
+
+  return {
+    userMapByName,
+    userList,
+    activeUserList,
+    fetchUserList,
+    fetchUser,
+    createUser,
+    updateUser,
+    getOrFetchUserByName,
+    getUserByName,
+    getOrFetchUserById,
+    getUserById,
+    getUserByIdentifier,
+    getUserByEmail,
+    archiveUser,
+    restoreUser,
+  };
 });
 
 export const extractUserEmail = (emailResource: string) => {
