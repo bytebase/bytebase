6f3c1a6bbbf5384582afe91c643d628532bfabc3
h3n4l <oysterdays@gmail.com>
2024-04-26 10:46:57 +0800

    chore: add limit.table-size in mergeIndividualConfigAsRule
    Signed-off-by: h3n4l <oysterdays@gmail.com>

--- frontend/src/types/sqlReview.ts
+++ frontend/src/types/sqlReview.ts
@@ -778,6 +778,7 @@ const mergeIndividualConfigAsRule = (
     case "system.comment.length":
     case "advice.online-migration":
     case "table.text-fields-total-length":
+    case "table.limit-size":
     case "statement.where.maximum-logical-operator-count":
     case "statement.maximum-limit-value":
     case "statement.maximum-join-table-count":
