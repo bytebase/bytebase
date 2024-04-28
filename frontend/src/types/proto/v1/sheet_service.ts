28689c458b9c53d58a835505ef42208b552ad76e
p0ny <xz@bytebase.com>
2024-04-28 16:58:13 +0800

    feat: sheet engine (#11833)
    Former-commit-id: f596cdf615c618d68a3f7054021446b2a1faf4b0

--- frontend/src/types/proto/v1/sheet_service.ts
+++ frontend/src/types/proto/v1/sheet_service.ts
@@ -3,6 +3,7 @@ import Long from "long";
 import _m0 from "protobufjs/minimal";
 import { FieldMask } from "../google/protobuf/field_mask";
 import { Timestamp } from "../google/protobuf/timestamp";
+import { Engine, engineFromJSON, engineToJSON } from "./common";
 import { DatabaseConfig } from "./database_service";
 
 export const protobufPackage = "bytebase.v1";
@@ -84,7 +85,11 @@ export interface Sheet {
   content: Uint8Array;
   /** content_size is the full size of the content, may not match the size of the `content` field. */
   contentSize: Long;
-  payload: SheetPayload | undefined;
+  payload:
+    | SheetPayload
+    | undefined;
+  /** The SQL dialect. */
+  engine: Engine;
 }
 
 export interface SheetPayload {
@@ -364,6 +369,7 @@ function createBaseSheet(): Sheet {
     content: new Uint8Array(0),
     contentSize: Long.ZERO,
     payload: undefined,
+    engine: 0,
   };
 }
 
@@ -395,6 +401,9 @@ export const Sheet = {
     }
     if (message.payload !== undefined) {
       SheetPayload.encode(message.payload, writer.uint32(106).fork()).ldelim();
+    }
+    if (message.engine !== 0) {
+      writer.uint32(112).int32(message.engine);
     }
     return writer;
   },
@@ -469,6 +478,13 @@ export const Sheet = {
 
           message.payload = SheetPayload.decode(reader, reader.uint32());
           continue;
+        case 14:
+          if (tag !== 112) {
+            break;
+          }
+
+          message.engine = reader.int32() as any;
+          continue;
       }
       if ((tag & 7) === 4 || tag === 0) {
         break;
@@ -489,6 +505,7 @@ export const Sheet = {
       content: isSet(object.content) ? bytesFromBase64(object.content) : new Uint8Array(0),
       contentSize: isSet(object.contentSize) ? Long.fromValue(object.contentSize) : Long.ZERO,
       payload: isSet(object.payload) ? SheetPayload.fromJSON(object.payload) : undefined,
+      engine: isSet(object.engine) ? engineFromJSON(object.engine) : 0,
     };
   },
 
@@ -521,6 +538,9 @@ export const Sheet = {
     if (message.payload !== undefined) {
       obj.payload = SheetPayload.toJSON(message.payload);
     }
+    if (message.engine !== 0) {
+      obj.engine = engineToJSON(message.engine);
+    }
     return obj;
   },
 
@@ -542,6 +562,7 @@ export const Sheet = {
     message.payload = (object.payload !== undefined && object.payload !== null)
       ? SheetPayload.fromPartial(object.payload)
       : undefined;
+    message.engine = object.engine ?? 0;
     return message;
   },
 };
