a5fca8b76eceb4f4ecdf2e26a3d83b93def0c68f
Azusain <azusaings@gmail.com>
2024-04-30 15:50:29 +0800

    feat(sql-review): new rule for checking fully qualified object name (#11860)
    * feat(sql-review): new rule for checking fully qualified object name
    
    * test: yaml test files for fully qualified object name
    
    * fix: filter out pseudo table
    
    Former-commit-id: cdbd837ead988c71b6eb0cc3e9af8711382b0c13

--- backend/plugin/advisor/pg/pg_rules_test.go
+++ backend/plugin/advisor/pg/pg_rules_test.go
@@ -15,6 +15,7 @@ func TestPostgreSQLRules(t *testing.T) {
 		advisor.SchemaRuleCommentLength,
 		advisor.SchemaRuleIndexKeyNumberLimit,
 		advisor.SchemaRuleIndexNoDuplicateColumn,
+		advisor.SchemaRuleFullyQualifiedObjectName,
 		advisor.SchemaRuleColumnNaming,
 		advisor.SchemaRuleFKNaming,
 		advisor.SchemaRuleIDXNaming,
@@ -56,6 +57,38 @@ func TestPostgreSQLRules(t *testing.T) {
 	}
 
 	for _, rule := range pgRules {
-		advisor.RunSQLReviewRuleTest(t, rule, storepb.Engine_POSTGRES, false /* record */)
+		var dbSchemaMetadata *storepb.DatabaseSchemaMetadata
+		if _, needMockData := advisorNeedMockData[rule]; needMockData {
+			dbSchemaMetadata = getMockDBSChemaData()
+		}
+		advisor.RunSQLReviewRuleTest(t, rule, storepb.Engine_POSTGRES, dbSchemaMetadata, false /* record */)
+	}
+}
+
+// Add SQL review type here if you need metadata for test.
+var advisorNeedMockData = map[advisor.SQLReviewRuleType]bool{
+	advisor.SchemaRuleFullyQualifiedObjectName: true,
+}
+
+func getMockDBSChemaData() *storepb.DatabaseSchemaMetadata {
+	return &storepb.DatabaseSchemaMetadata{
+		Name: "TEST_DB",
+		Schemas: []*storepb.SchemaMetadata{
+			{
+				Name: "public",
+				Tables: []*storepb.TableMetadata{
+					{Name: "pokes"},
+					{Name: "pokesv1"},
+					{Name: "pokesv2"},
+					{Name: "pokesv3"},
+				},
+				ExternalTables: []*storepb.ExternalTableMetadata{
+					{Name: "pokesv4"},
+					{Name: "pokesv5"},
+					{Name: "pokesv6"},
+					{Name: "pokesv7"},
+				},
+			},
+		},
 	}
 }
