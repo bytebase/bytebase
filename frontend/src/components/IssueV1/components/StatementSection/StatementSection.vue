594a43d971cc902a696683bc333c05462e993745
h3n4l <oysterdays@gmail.com>
2024-05-07 13:10:32 +0800

    feat: click plan check detail line anchor element will navigate to line (#11914)
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    ---------
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    Co-authored-by: d-bytebase <d@bytebase.com>

--- frontend/src/components/IssueV1/components/StatementSection/StatementSection.vue
+++ frontend/src/components/IssueV1/components/StatementSection/StatementSection.vue
@@ -1,12 +1,15 @@
 <template>
   <div v-if="viewMode !== 'NONE'" class="px-4 py-2 flex flex-col gap-y-2">
-    <EditorView v-if="viewMode === 'EDITOR'" />
+    <EditorView v-if="viewMode === 'EDITOR'" ref="editorViewRef" />
     <SDLView v-if="viewMode === 'SDL'" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
+import { ref } from "vue";
+import { nextTick } from "vue";
+import { useRouter } from "vue-router";
 import { TaskTypeListWithStatement } from "@/types";
 import { Task_Type } from "@/types/proto/v1/rollout_service";
 import { useIssueContext } from "../../logic";
@@ -14,6 +17,9 @@ import EditorView from "./EditorView";
 import SDLView from "./SDLView";
 
 const { isCreating, selectedTask } = useIssueContext();
+
+const editorViewRef = ref<InstanceType<typeof EditorView>>();
+const router = useRouter();
 
 type ViewMode = "NONE" | "EDITOR" | "SDL";
 
@@ -32,4 +38,21 @@ const viewMode = computed((): ViewMode => {
 
   return "NONE";
 });
+
+router.afterEach((to) => {
+  if (to.hash) {
+    scrollToLineByHash(to.hash);
+  }
+});
+
+const scrollToLineByHash = (hash: string) => {
+  const match = hash.match(/^#L(\d+)$/);
+  if (!match) return;
+  const lineNumber = parseInt(match[1], 10);
+  nextTick(() => {
+    editorViewRef.value?.editor?.monacoEditor?.editor?.codeEditor?.revealLineNearTop(
+      lineNumber
+    );
+  });
+};
 </script>
