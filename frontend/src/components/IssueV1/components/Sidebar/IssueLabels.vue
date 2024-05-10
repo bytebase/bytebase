855204092b040aaa450f9a4d115a56d880f0dcea
ecmadao <wlec@outlook.com>
2024-05-10 13:43:09 +0800

    chore: optimize issue labels (#11950)

--- frontend/src/components/IssueV1/components/Sidebar/IssueLabels.vue
+++ frontend/src/components/IssueV1/components/Sidebar/IssueLabels.vue
@@ -3,36 +3,25 @@
     <div class="flex items-center gap-x-1 textlabel">
       <span>{{ $t("common.labels") }}</span>
     </div>
-    <NSelect
-      :multiple="true"
-      :options="options"
+    <IssueLabelSelector
       :disabled="!hasEditPermission"
-      :consistent-menu-width="true"
-      max-tag-count="responsive"
-      :render-label="renderLabel"
-      :render-tag="renderTag"
-      :value="issueLabels"
-      @update:value="onLablesUpdate"
+      :selected="issue.labels"
+      :labels="issue.projectEntity.issueLabels"
+      :size="'medium'"
+      @update:selected="onLablesUpdate"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { NCheckbox, NSelect, NTag } from "naive-ui";
-import type { SelectOption } from "naive-ui";
-import type { SelectBaseOption } from "naive-ui/lib/select/src/interface";
-import { computed, h } from "vue";
+import { computed } from "vue";
 import { useI18n } from "vue-i18n";
+import IssueLabelSelector from "@/components/IssueV1/components/IssueLabelSelector.vue";
 import { useIssueContext } from "@/components/IssueV1/logic";
 import { issueServiceClient } from "@/grpcweb";
 import { pushNotification, useCurrentUserV1 } from "@/store";
 import { Issue } from "@/types/proto/v1/issue_service";
 import { hasProjectPermissionV2 } from "@/utils";
-
-type IsseuLabelOption = SelectOption & {
-  value: string;
-  color: string;
-};
 
 const { isCreating, issue } = useIssueContext();
 const { t } = useI18n();
@@ -45,23 +34,6 @@ const hasEditPermission = computed(() =>
     isCreating.value ? "bb.issues.create" : "bb.issues.update"
   )
 );
-
-const issueLabels = computed(() => {
-  const pool = new Set(
-    issue.value.projectEntity.issueLabels.map((label) => label.value)
-  );
-  return issue.value.labels.filter((label) => pool.has(label));
-});
-
-const options = computed(() => {
-  return issue.value.projectEntity.issueLabels.map<IsseuLabelOption>(
-    (label) => ({
-      label: label.value,
-      value: label.value,
-      color: label.color,
-    })
-  );
-});
 
 const onLablesUpdate = async (labels: string[]) => {
   if (isCreating.value) {
@@ -82,43 +54,5 @@ const onLablesUpdate = async (labels: string[]) => {
       title: t("common.updated"),
     });
   }
-};
-
-const renderLabel = (option: IsseuLabelOption, selected: boolean) => {
-  const { color, value } = option as IsseuLabelOption;
-  return h("div", { class: "flex items-center gap-x-2" }, [
-    h(NCheckbox, { checked: selected, size: "small" }),
-    h("div", {
-      class: "w-4 h-4 rounded cursor-pointer relative",
-      style: `background-color: ${color};`,
-      onClick: () => {},
-    }),
-    value,
-  ]);
-};
-
-const renderTag = (props: {
-  option: SelectBaseOption;
-  handleClose: () => void;
-}) => {
-  const { color, value } = props.option as IsseuLabelOption;
-
-  return h(
-    NTag,
-    {
-      closable: true,
-      onClose: props.handleClose,
-    },
-    {
-      default: () =>
-        h("div", { class: "flex items-center gap-x-2" }, [
-          h("div", {
-            class: "w-4 h-4 rounded",
-            style: `background-color: ${color};`,
-          }),
-          value,
-        ]),
-    }
-  );
 };
 </script>
