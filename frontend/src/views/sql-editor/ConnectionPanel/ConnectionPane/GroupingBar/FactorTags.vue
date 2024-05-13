875eb886f99140c235a88a31114ba5ecd22c4e9b
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-05-13 23:52:33 +0800

    feat(sql-editor): Improve the UX of project group by experience (#11967)
    * feat: refactor GroupingBar
    
    * feat: new UI
    
    * chore: refactor node keys
    
    * chore: drag
    
    * chore: minor update
    
    * chore: i18n
    
    * chore: lint and remove unused

--- frontend/src/views/sql-editor/ConnectionPanel/ConnectionPane/GroupingBar/FactorTags.vue
+++ frontend/src/views/sql-editor/ConnectionPanel/ConnectionPane/GroupingBar/FactorTags.vue
@@ -1,26 +1,51 @@
 <template>
-  <div class="flex flex-row items-start flex-wrap gap-x-1.5 gap-y-1.5">
-    <FactorTag
-      v-for="(factor, i) in factorList"
-      :key="i"
-      :factor="factor"
-      :allow-disable="filteredFactorList.length > 1"
-      @toggle-disabled="toggleDisabled(factor, i)"
-      @remove="remove(factor, i)"
-    />
-  </div>
+  <Draggable
+    v-model="factorList"
+    item-key="factor"
+    animation="300"
+    class="flex flex-col items-start gap-y-1.5 hide-scrollbar"
+    ghost-class="opacity-50"
+    draggable=".factor"
+    handle=".handle"
+    @change="handleDrag"
+  >
+    <template
+      #item="{ element: sf, index }: { element: StatefulFactor; index: number }"
+    >
+      <FactorTag
+        :key="index"
+        :factor="sf"
+        class="factor"
+        style="--n-padding: 0 8px 0 2px; --n-icon-margin: 6px 2px 6px 0"
+        @toggle-disabled="toggleDisabled(sf, index)"
+        @remove="remove(sf, index)"
+      >
+        <template #icon>
+          <GripVerticalIcon
+            class="handle w-4 h-4 opacity-50 cursor-ns-resize"
+          />
+        </template>
+      </FactorTag>
+    </template>
+    <template #footer>
+      <AddFactorButton />
+    </template>
+  </Draggable>
 </template>
 
 <script lang="ts" setup>
+import { GripVerticalIcon } from "lucide-vue-next";
 import { storeToRefs } from "pinia";
+import Draggable from "vuedraggable";
 import { useSQLEditorTreeStore } from "@/store";
 import type { StatefulSQLEditorTreeFactor as StatefulFactor } from "@/types";
 import { useSQLEditorContext } from "@/views/sql-editor/context";
+import AddFactorButton from "./AddFactorButton.vue";
 import FactorTag from "./FactorTag.vue";
 
 const treeStore = useSQLEditorTreeStore();
 const { events } = useSQLEditorContext();
-const { factorList, filteredFactorList } = storeToRefs(treeStore);
+const { factorList } = storeToRefs(treeStore);
 
 const toggleDisabled = (factor: StatefulFactor, index: number) => {
   factor.disabled = !factor.disabled;
@@ -30,12 +55,11 @@ const toggleDisabled = (factor: StatefulFactor, index: number) => {
 
 const remove = (factor: StatefulFactor, index: number) => {
   factorList.value.splice(index, 1);
-  if (factorList.value.length === 0) {
-    factorList.value.push({
-      factor: "project",
-      disabled: false,
-    });
-  }
+  treeStore.buildTree();
+  events.emit("tree-ready");
+};
+
+const handleDrag = () => {
   treeStore.buildTree();
   events.emit("tree-ready");
 };
