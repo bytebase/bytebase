49787ed60a5f4b76dfe4cf0f802794869024a88c
p0ny <xz@bytebase.com>
2024-05-17 11:06:45 +0800

    fix: task run sheet id sql (#12016)
    Former-commit-id: b800bad298823013a82f3e8c849685ac649cb2b2
    Former-commit-id: 1d66a727e2c10622d3a45543d3a86f1502956949

--- backend/migrator/migration/prod/2.16/0005##task_run_sheet_id.sql
+++ backend/migrator/migration/prod/2.16/0005##task_run_sheet_id.sql
@@ -2,10 +2,16 @@ ALTER TABLE task_run ADD COLUMN sheet_id INTEGER;
 
 ALTER TABLE task_run ADD CONSTRAINT task_run_sheet_id_fkey FOREIGN KEY (sheet_id) REFERENCES sheet (id);
 
-UPDATE task_run
-SET sheet_id = (
+WITH t AS (
     SELECT
-        (task.payload->>'sheetId')::INTEGER
-    FROM task
-    WHERE task.id = task_run.task_id
+        task_run.id AS task_run_id,
+        sheet.id AS sheet_id
+    FROM task_run
+    LEFT JOIN task ON task.id = task_run.task_id
+    LEFT JOIN sheet ON task.payload->>'sheetId' = (sheet.id)::TEXT
+    WHERE sheet.id IS NOT NULL
 )
+UPDATE task_run
+SET sheet_id = t.sheet_id
+FROM t
+WHERE task_run.id = t.task_run_id;
