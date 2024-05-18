8dd3b082f94873d45dde04682b63cdb4a43473dc
ecmadao <wlec@outlook.com>
2024-05-18 10:01:26 +0800

    chore: add example for upload json file (#12035)
    * chore: support upload algorithm json file
    
    * chore: add example for upload json file
    
    Former-commit-id: 1b33af66c67b66d44dac66247b6ca88379010fd7

--- frontend/src/components/SensitiveData/ClassificationView.vue
+++ frontend/src/components/SensitiveData/ClassificationView.vue
@@ -18,12 +18,12 @@
       />
     </div>
     <div class="textinfolabel">
-      {{ $t("settings.sensitive-data.classification.label") }}
+      {{ $t("settings.sensitive-data.classification.upload-label") }}
       <span
         class="normal-link cursor-pointer hover:underline"
         @click="state.showExampleModal = true"
       >
-        {{ $t("settings.sensitive-data.classification.view-example") }}
+        {{ $t("settings.sensitive-data.view-example") }}
       </span>
     </div>
     <div
@@ -52,8 +52,9 @@
     </div>
   </div>
 
-  <ClassificationExampleModal
+  <DataExampleModal
     v-if="state.showExampleModal"
+    :example="JSON.stringify(example, null, 2)"
     @dismiss="state.showExampleModal = false"
   />
 
@@ -229,5 +230,63 @@ const onFileSelect = (file: File) => {
     return;
   };
   fr.readAsText(file);
+};
+
+const example: UploadClassificationConfig = {
+  title: "Classification Example",
+  levels: [
+    {
+      id: "1",
+      title: "Level 1",
+      description: "",
+    },
+    {
+      id: "2",
+      title: "Level 2",
+      description: "",
+    },
+  ],
+  classifications: [
+    {
+      id: "1",
+      title: "Basic",
+      description: "",
+    },
+    {
+      id: "1-1",
+      title: "Basic",
+      description: "",
+    },
+    {
+      id: "1-2",
+      title: "Assert",
+      description: "",
+    },
+    {
+      id: "1-3",
+      title: "Contact",
+      description: "",
+    },
+    {
+      id: "1-4",
+      title: "Health",
+      description: "",
+    },
+    {
+      id: "2",
+      title: "Relationship",
+      description: "",
+    },
+    {
+      id: "2-1",
+      title: "Social",
+      description: "",
+    },
+    {
+      id: "2-2",
+      title: "Business",
+      description: "",
+    },
+  ],
 };
 </script>
