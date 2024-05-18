9b55afe209038e4de3014ee4b4b276c32f653656
ecmadao <wlec@outlook.com>
2024-05-18 10:01:26 +0800

    chore: add example for upload json file (#12035)
    * chore: support upload algorithm json file
    
    * chore: add example for upload json file
    
    Former-commit-id: 3f1833e060deaf1e2125b383c7be5c8e04213bf4
    Former-commit-id: 216f6ee50600589bea90fd9ac23f1d76847836ef

--- frontend/src/components/SensitiveData/components/DataExampleModal.vue
+++ frontend/src/components/SensitiveData/components/DataExampleModal.vue
@@ -3,7 +3,7 @@
     :close-on-esc="true"
     :mask-closable="true"
     :trap-focus="false"
-    :title="$t('settings.sensitive-data.classification.json-format-example')"
+    :title="$t('settings.sensitive-data.json-format-example')"
     class="w-[48rem] max-w-full"
     @close="$emit('dismiss')"
   >
@@ -16,7 +16,7 @@
         <heroicons-outline:clipboard-document class="w-4 h-4" />
       </div>
       <NConfigProvider :hljs="hljs">
-        <NCode language="json" :code="JSON.stringify(example, null, 2)" />
+        <NCode language="json" :code="example" />
       </NConfigProvider>
     </div>
   </BBModal>
@@ -29,68 +29,13 @@ import { NCode, NConfigProvider } from "naive-ui";
 import { useI18n } from "vue-i18n";
 import { pushNotification } from "@/store";
 
+const props = defineProps<{
+  example: string;
+}>();
+
 defineEmits<{
   (event: "dismiss"): void;
 }>();
-
-// TODO: localization.
-const example = {
-  title: "Classification Example",
-  levels: [
-    {
-      id: "1",
-      title: "Level 1",
-      description: "",
-    },
-    {
-      id: "2",
-      title: "Level 2",
-      description: "",
-    },
-  ],
-  classifications: [
-    {
-      id: "1",
-      title: "Basic",
-      description: "",
-    },
-    {
-      id: "1-1",
-      title: "Basic",
-      description: "",
-    },
-    {
-      id: "1-2",
-      title: "Assert",
-      description: "",
-    },
-    {
-      id: "1-3",
-      title: "Contact",
-      description: "",
-    },
-    {
-      id: "1-4",
-      title: "Health",
-      description: "",
-    },
-    {
-      id: "2",
-      title: "Relationship",
-      description: "",
-    },
-    {
-      id: "2-1",
-      title: "Social",
-      description: "",
-    },
-    {
-      id: "2-2",
-      title: "Business",
-      description: "",
-    },
-  ],
-};
 
 const { copy: copyTextToClipboard, isSupported } = useClipboard({
   legacy: true,
@@ -98,7 +43,7 @@ const { copy: copyTextToClipboard, isSupported } = useClipboard({
 const { t } = useI18n();
 
 const handleCopy = () => {
-  copyTextToClipboard(JSON.stringify(example, null, 2)).then(() => {
+  copyTextToClipboard(props.example).then(() => {
     pushNotification({
       module: "bytebase",
       style: "SUCCESS",
