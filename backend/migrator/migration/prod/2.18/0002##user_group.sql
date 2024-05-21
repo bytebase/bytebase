a59483a085e5332b27b66211352a0a550261ad44
ecmadao <wlec@outlook.com>
2024-05-21 11:40:28 +0800

    chore: update

--- backend/migrator/migration/prod/2.18/0002##user_group.sql
+++ backend/migrator/migration/prod/2.18/0002##user_group.sql
@@ -1,19 +1,12 @@
 CREATE TABLE user_group (
-    id SERIAL PRIMARY KEY,
-    row_status row_status NOT NULL DEFAULT 'NORMAL',
     creator_id INTEGER NOT NULL REFERENCES principal (id),
     created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
     updater_id INTEGER NOT NULL REFERENCES principal (id),
     updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
     name TEXT NOT NULL,
-    resource_id TEXT NOT NULL,
-    email TEXT NOT NULL,
+    email TEXT PRIMARY KEY,
     description TEXT NOT NULL DEFAULT '',
     payload JSONB NOT NULL DEFAULT '{}'
 );
-
-CREATE UNIQUE INDEX idx_user_group_unique_resource_id ON user_group(resource_id);
-
-CREATE UNIQUE INDEX idx_user_group_unique_email ON user_group(email);
 
 ALTER SEQUENCE user_group_id_seq RESTART WITH 101;
