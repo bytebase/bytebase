cb775f10b9bea5a0c401b61ff980d110c714d757
ecmadao <wlec@outlook.com>
2024-05-21 11:44:41 +0800

    chore: update

--- backend/migrator/migration/prod/2.18/0002##user_group.sql
+++ backend/migrator/migration/prod/2.18/0002##user_group.sql
@@ -1,12 +1,10 @@
 CREATE TABLE user_group (
+    email TEXT PRIMARY KEY,
     creator_id INTEGER NOT NULL REFERENCES principal (id),
     created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
     updater_id INTEGER NOT NULL REFERENCES principal (id),
     updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
     name TEXT NOT NULL,
-    email TEXT PRIMARY KEY,
     description TEXT NOT NULL DEFAULT '',
     payload JSONB NOT NULL DEFAULT '{}'
 );
-
-ALTER SEQUENCE user_group_id_seq RESTART WITH 101;
