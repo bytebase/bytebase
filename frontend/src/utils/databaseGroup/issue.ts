87efb09fdadf84ce1be1e577b68597a783d50325
boojack <stevenlgtm@gmail.com>
2024-05-22 15:22:18 +0800

    chore: add sql review only checkbox (#12074)

--- frontend/src/utils/databaseGroup/issue.ts
+++ frontend/src/utils/databaseGroup/issue.ts
@@ -1,12 +1,16 @@
 import dayjs from "dayjs";
-import { PROJECT_V1_ROUTE_ISSUE_DETAIL } from "@/router/dashboard/projectV1";
+import {
+  PROJECT_V1_ROUTE_ISSUE_DETAIL,
+  PROJECT_V1_ROUTE_PLAN_DETAIL,
+} from "@/router/dashboard/projectV1";
 import type { ComposedDatabaseGroup } from "@/types";
 import { extractProjectResourceName } from "../v1";
 
 export const generateDatabaseGroupIssueRoute = (
   type: "bb.issue.database.schema.update" | "bb.issue.database.data.update",
   databaseGroup: ComposedDatabaseGroup,
-  sql = ""
+  sql = "",
+  planOnly = false
 ) => {
   const issueNameParts: string[] = [];
   issueNameParts.push(`[${databaseGroup.databasePlaceholder}]`);
@@ -27,10 +31,13 @@ export const generateDatabaseGroupIssueRoute = (
   };
 
   return {
-    name: PROJECT_V1_ROUTE_ISSUE_DETAIL,
+    name: planOnly
+      ? PROJECT_V1_ROUTE_PLAN_DETAIL
+      : PROJECT_V1_ROUTE_ISSUE_DETAIL,
     params: {
       projectId: extractProjectResourceName(databaseGroup.projectEntity.name),
       issueSlug: "create",
+      planSlug: "create",
     },
     query,
   };
