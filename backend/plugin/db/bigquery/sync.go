0d3a4472765b681a03e3e15d87a9afe996262495
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-05-23 17:13:34 +0800

    chore: bq sync table meta (#12097)

--- backend/plugin/db/bigquery/sync.go
+++ backend/plugin/db/bigquery/sync.go
@@ -2,12 +2,16 @@ package bigquery
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"cloud.google.com/go/bigquery"
 	"github.com/pkg/errors"
 	"google.golang.org/api/iterator"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 
 	"github.com/bytebase/bytebase/backend/plugin/db"
+	"github.com/bytebase/bytebase/backend/plugin/db/util"
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 )
 
@@ -32,10 +36,67 @@ func (d *Driver) SyncInstance(ctx context.Context) (*db.InstanceMetadata, error)
 	}, nil
 }
 
+type columnRow struct {
+	TableName       string              `bigquery:"table_name"`
+	ColumnName      string              `bigquery:"column_name"`
+	OrdinalPosition int32               `bigquery:"ordinal_position"`
+	IsNullable      string              `bigquery:"is_nullable"`
+	DataType        string              `bigquery:"data_type"`
+	CollationName   bigquery.NullString `bigquery:"collation_name"`
+	ColumnDefault   bigquery.NullString `bigquery:"column_default"`
+}
+
 // SyncDBSchema syncs a single database schema.
 func (d *Driver) SyncDBSchema(ctx context.Context) (*storepb.DatabaseSchemaMetadata, error) {
 	schemaMetadata := &storepb.SchemaMetadata{}
 
+	columnMap := make(map[db.TableKey][]*storepb.ColumnMetadata)
+	q := d.client.Query(fmt.Sprintf(`
+		SELECT
+			table_name,
+			column_name,
+			ordinal_position,
+			is_nullable,
+			data_type,
+			collation_name,
+			column_default
+		FROM %s.INFORMATION_SCHEMA.COLUMNS ORDER BY table_name, ordinal_position;`, d.databaseName))
+	it, err := q.Read(ctx)
+	if err != nil {
+		return nil, err
+	}
+	for {
+		var row columnRow
+		err := it.Next(&row)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		nullableBool, err := util.ConvertYesNo(row.IsNullable)
+		if err != nil {
+			return nil, err
+		}
+
+		column := &storepb.ColumnMetadata{
+			Name:     row.ColumnName,
+			Position: row.OrdinalPosition,
+			Nullable: nullableBool,
+			Type:     row.DataType,
+		}
+		if row.CollationName.Valid {
+			column.Collation = row.CollationName.String()
+		}
+		if row.ColumnDefault.Valid {
+			column.DefaultValue = &storepb.ColumnMetadata_Default{Default: &wrapperspb.StringValue{Value: row.ColumnDefault.String()}}
+		} else {
+			column.DefaultValue = &storepb.ColumnMetadata_DefaultNull{DefaultNull: true}
+		}
+
+		key := db.TableKey{Schema: "", Table: row.TableName}
+		columnMap[key] = append(columnMap[key], column)
+	}
 	ts := d.client.Dataset(d.databaseName).Tables(ctx)
 	for {
 		t, err := ts.Next()
@@ -45,7 +106,20 @@ func (d *Driver) SyncDBSchema(ctx context.Context) (*storepb.DatabaseSchemaMetad
 		if err != nil {
 			return nil, err
 		}
-		schemaMetadata.Tables = append(schemaMetadata.Tables, &storepb.TableMetadata{Name: t.TableID})
+
+		key := db.TableKey{Schema: "", Table: t.TableID}
+		columns := columnMap[key]
+		tmd, err := t.Metadata(ctx)
+		if err != nil {
+			return nil, err
+		}
+		schemaMetadata.Tables = append(schemaMetadata.Tables, &storepb.TableMetadata{
+			Name:     t.TableID,
+			Columns:  columns,
+			RowCount: int64(tmd.NumRows),
+			Comment:  tmd.Description,
+			DataSize: tmd.NumBytes,
+		})
 	}
 	return &storepb.DatabaseSchemaMetadata{
 		Name:    d.databaseName,
