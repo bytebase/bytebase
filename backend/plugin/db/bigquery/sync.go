58f95625eea46974ad9e5a4b1fca8f08f0e47b1b
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-05-23 14:16:54 +0800

    chore: bq sync tables (#12094)
    Former-commit-id: 131114bb882242ee2b0bdcb744b0b591b9aecb82

--- backend/plugin/db/bigquery/sync.go
+++ backend/plugin/db/bigquery/sync.go
@@ -5,19 +5,52 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	"google.golang.org/api/iterator"
 
 	"github.com/bytebase/bytebase/backend/plugin/db"
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 )
 
 // SyncInstance syncs the instance.
-func (*Driver) SyncInstance(_ context.Context) (*db.InstanceMetadata, error) {
-	return nil, nil
+func (d *Driver) SyncInstance(ctx context.Context) (*db.InstanceMetadata, error) {
+	var databases []*storepb.DatabaseSchemaMetadata
+
+	it := d.client.Datasets(ctx)
+	for {
+		dataset, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		databases = append(databases, &storepb.DatabaseSchemaMetadata{Name: dataset.DatasetID})
+	}
+
+	return &db.InstanceMetadata{
+		Databases: databases,
+	}, nil
 }
 
 // SyncDBSchema syncs a single database schema.
-func (*Driver) SyncDBSchema(_ context.Context) (*storepb.DatabaseSchemaMetadata, error) {
-	return nil, nil
+func (d *Driver) SyncDBSchema(ctx context.Context) (*storepb.DatabaseSchemaMetadata, error) {
+	schemaMetadata := &storepb.SchemaMetadata{}
+
+	ts := d.client.Dataset(d.databaseName).Tables(ctx)
+	for {
+		t, err := ts.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		schemaMetadata.Tables = append(schemaMetadata.Tables, &storepb.TableMetadata{Name: t.TableID})
+	}
+	return &storepb.DatabaseSchemaMetadata{
+		Name:    d.databaseName,
+		Schemas: []*storepb.SchemaMetadata{schemaMetadata},
+	}, nil
 }
 
 // SyncSlowQuery syncs the slow query.
