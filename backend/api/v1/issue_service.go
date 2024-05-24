3a5b457b658c4b94af2077ba9c923f5ff75c8946
p0ny <p0uy@outlook.com>
2024-05-24 16:36:56 +0800

    create issue comment return self

--- backend/api/v1/issue_service.go
+++ backend/api/v1/issue_service.go
@@ -16,7 +16,6 @@ import (
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
-	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/bytebase/bytebase/backend/common"
 	"github.com/bytebase/bytebase/backend/common/log"
@@ -1702,28 +1701,22 @@ func (s *IssueService) CreateIssueComment(ctx context.Context, request *v1pb.Cre
 	}
 	activityCreate.Payload = string(bytes)
 
-	activity, err := s.webhookManager.CreateActivity(ctx, activityCreate, &webhook.Metadata{Issue: issue})
-	if err != nil {
+	if _, err := s.webhookManager.CreateActivity(ctx, activityCreate, &webhook.Metadata{Issue: issue}); err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to create issue comment: %v", err.Error())
 	}
 
 	// TODO(p0ny): dual-write issue_comment and activity for now. Remove activity in the future.
-	if err := s.store.CreateIssueComment(ctx, &store.IssueCommentMessage{
+	ic, err := s.store.CreateIssueComment(ctx, &store.IssueCommentMessage{
 		IssueUID: issue.UID,
 		Payload: &storepb.IssueCommentPayload{
 			Comment: request.IssueComment.Comment,
 		},
-	}, principalID); err != nil {
+	}, principalID)
+	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to create issue comment: %v", err)
 	}
 
-	return &v1pb.IssueComment{
-		Uid:        fmt.Sprintf("%d", activity.UID),
-		Comment:    activity.Comment,
-		Payload:    activity.Payload,
-		CreateTime: timestamppb.New(time.Unix(activity.CreatedTs, 0)),
-		UpdateTime: timestamppb.New(time.Unix(activity.UpdatedTs, 0)),
-	}, nil
+	return convertToIssueComment(request.Parent, ic), nil
 }
 
 // UpdateIssueComment updates the issue comment.
