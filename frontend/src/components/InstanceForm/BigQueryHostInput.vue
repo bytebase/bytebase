e3d501899419cb54b300a761c83b42559c600e9c
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-05-24 14:58:34 +0800

    chore: bq host ui (#12112)
    * chore: bq ui
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: f7aecd1b2f91989d2ccd2c260700b7377ed0af09

---
+++ frontend/src/components/InstanceForm/BigQueryHostInput.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="grid grid-cols-2 gap-x-2 gap-y-1">
+    <div>
+      <label class="textlabel">
+        {{ $t("instance.project-id") }}
+        <span style="color: red">*</span>
+      </label>
+      <NInput
+        v-model:value="state.projectId"
+        required
+        placeholder="projectId"
+        class="mt-1 w-full"
+        :status="state.dirty && !isValidProjectId ? 'error' : undefined"
+        :disabled="!allowEdit"
+      />
+    </div>
+
+    <p class="col-span-2 textinfolabel">
+      {{ $t("instance.find-gcp-project-id-and-instance-id") }}
+      <a
+        href="https://www.bytebase.com/docs/get-started/instance/#specify-google-cloud-project-id-and-spanner-instance-id?source=console"
+        target="_blank"
+        class="normal-link inline-flex items-center"
+      >
+        {{ $t("common.detailed-guide") }}
+        <heroicons-outline:external-link class="w-4 h-4 ml-1" />
+      </a>
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NInput } from "naive-ui";
+import { computed, reactive, watch } from "vue";
+
+type LocalState = {
+  projectId: string;
+  dirty: boolean;
+};
+
+const props = defineProps<{
+  host: string;
+  allowEdit: boolean;
+}>();
+
+const emit = defineEmits<{
+  (name: "update:host", host: string): void;
+}>();
+
+const RE_PROJECT_ID = /^(?:[a-z]|[-.:]|[0-9])+$/;
+
+const state = reactive<LocalState>({
+  projectId: "",
+  dirty: false,
+});
+
+const isValidProjectId = computed(() => {
+  return RE_PROJECT_ID.test(state.projectId);
+});
+
+const update = () => {
+  if (!isValidProjectId.value) {
+    emit("update:host", "");
+    return;
+  }
+  const host = `${state.projectId}`;
+  emit("update:host", host);
+};
+
+watch([() => state.projectId], () => {
+  state.dirty = true;
+  update();
+});
+
+watch(
+  () => props.host,
+  (host) => {
+    if (!host) return;
+    state.projectId = host;
+  },
+  { immediate: true }
+);
+</script>
