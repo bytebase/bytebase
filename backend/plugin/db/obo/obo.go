7229ffdf8d72592453a369767c5cfa3870878704
d-bytebase <d@bytebase.com>
2024-05-27 01:07:35 -0700

    chore: oracle explain

--- backend/plugin/db/obo/obo.go
+++ backend/plugin/db/obo/obo.go
@@ -3,8 +3,10 @@ package obo
 
 import (
 	"context"
+	"crypto/rand"
 	"database/sql"
 	"fmt"
+	"math/big"
 	"net/url"
 	"strings"
 	"time"
@@ -190,9 +192,30 @@ func (driver *Driver) QueryConn(ctx context.Context, conn *sql.Conn, statement s
 
 func (*Driver) querySingleSQL(ctx context.Context, conn *sql.Conn, singleSQL base.SingleSQL, queryContext *db.QueryContext) (*v1pb.QueryResult, error) {
 	statement := strings.TrimRight(singleSQL.Text, " \n\t;")
+
 	if queryContext.Explain {
-		statement = fmt.Sprintf("EXPLAIN %s", statement)
-	} else if queryContext.Limit > 0 {
+		startTime := time.Now()
+		randNum, err := rand.Int(rand.Reader, big.NewInt(999))
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to generate random statement ID")
+		}
+		randomID := fmt.Sprintf("%d%d", startTime.UnixMilli(), randNum.Int64())
+
+		statement = fmt.Sprintf("EXPLAIN PLAN SET STATEMENT_ID = '%s' FOR %s", randomID, statement)
+		if _, err := conn.ExecContext(ctx, statement); err != nil {
+			return nil, err
+		}
+		explainQuery := fmt.Sprintf(`SELECT LPAD(' ', LEVEL-1) || OPERATION || ' (' || OPTIONS || ')' "Operation", OBJECT_NAME "Object", OPTIMIZER "Optimizer", COST "Cost", CARDINALITY "Cardinality", BYTES "Bytes", PARTITION_START "Partition Start", PARTITION_ID "Partition ID", ACCESS_PREDICATES "Access Predicates",FILTER_PREDICATES "Filter Predicates" FROM PLAN_TABLE START WITH ID = 0 AND statement_id = '%s' CONNECT BY PRIOR ID=PARENT_ID AND statement_id = '%s' ORDER BY id`, randomID, randomID)
+		result, err := util.Query(ctx, storepb.Engine_ORACLE, conn, explainQuery, queryContext)
+		if err != nil {
+			return nil, err
+		}
+		result.Latency = durationpb.New(time.Since(startTime))
+		result.Statement = statement
+		return result, nil
+	}
+
+	if queryContext.Limit > 0 {
 		statement = fmt.Sprintf("SELECT * FROM (%s) WHERE ROWNUM <= %d", statement, queryContext.Limit)
 	}
 
