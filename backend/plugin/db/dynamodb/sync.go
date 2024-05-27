d72a23e436f7fe4147d2678ea8293aaf0fb76aa5
h3n4l <oysterdays@gmail.com>
2024-05-27 16:06:27 +0800

    feat: sync dynamodb
    Signed-off-by: h3n4l <oysterdays@gmail.com>

--- backend/plugin/db/dynamodb/sync.go
+++ backend/plugin/db/dynamodb/sync.go
@@ -2,8 +2,11 @@ package dynamodb
 
 import (
 	"context"
+	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/pkg/errors"
 
@@ -39,8 +42,100 @@ func (d *Driver) SyncInstance(ctx context.Context) (*db.InstanceMetadata, error)
 }
 
 // SyncDBSchema syncs a single database schema.
-func (*Driver) SyncDBSchema(_ context.Context) (*storepb.DatabaseSchemaMetadata, error) {
-	panic("implement me")
+func (d *Driver) SyncDBSchema(ctx context.Context) (*storepb.DatabaseSchemaMetadata, error) {
+	schemaMetadata := &storepb.SchemaMetadata{}
+
+	tableNames, err := d.listAllTables(ctx)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to list all tables")
+	}
+
+	// DynamoDB do not support batch describe table, the operation that we call describe table one by one may be
+	// very slow because the multi round trip to AWS.
+	// We may need to optimize this in the future.
+	for _, tableName := range tableNames {
+		tableMetadata, err := d.syncTable(ctx, tableName)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to describe table: %s", tableName)
+		}
+		if tableMetadata != nil {
+			schemaMetadata.Tables = append(schemaMetadata.Tables, tableMetadata)
+		}
+	}
+
+	return &storepb.DatabaseSchemaMetadata{
+		Name: formatDatabaseName(d.config.Database, d.awsConfig.Region),
+		Schemas: []*storepb.SchemaMetadata{
+			schemaMetadata,
+		},
+	}, nil
+}
+
+func (d *Driver) syncTable(ctx context.Context, tableName string) (*storepb.TableMetadata, error) {
+	tableMetadata := &storepb.TableMetadata{
+		Name: tableName,
+	}
+	out, err := d.client.DescribeTable(ctx, &dynamodb.DescribeTableInput{
+		TableName: &tableName,
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to describe table: %s", tableName)
+	}
+	if out.Table == nil {
+		return nil, errors.Errorf("table not found: %s", tableName)
+	}
+	if out.Table.TableStatus != types.TableStatusActive {
+		return nil, nil
+	}
+	if out.Table.KeySchema != nil {
+		var hashKeyAttributes, rangeKeyAttributes []string
+		for _, keySchema := range out.Table.KeySchema {
+			if keySchema.KeyType == types.KeyTypeHash {
+				hashKeyAttributes = append(hashKeyAttributes, *keySchema.AttributeName)
+			} else if keySchema.KeyType == types.KeyTypeRange {
+				rangeKeyAttributes = append(rangeKeyAttributes, *keySchema.AttributeName)
+			}
+		}
+		tableMetadata.Indexes = append(tableMetadata.Indexes, &storepb.IndexMetadata{
+			Name:        strings.Join(hashKeyAttributes, ","),
+			Expressions: append([]string{}, hashKeyAttributes...),
+			Type:        "HASH",
+		})
+		tableMetadata.Indexes = append(tableMetadata.Indexes, &storepb.IndexMetadata{
+			Name:        strings.Join(rangeKeyAttributes, ","),
+			Expressions: append([]string{}, rangeKeyAttributes...),
+			Type:        "RANGE",
+		})
+	}
+	if out.Table.TableName != nil {
+		tableMetadata.Name = *out.Table.TableName
+	}
+	if out.Table.ItemCount != nil {
+		tableMetadata.RowCount = *out.Table.ItemCount
+	}
+
+	return tableMetadata, nil
+}
+
+func (d *Driver) listAllTables(ctx context.Context) ([]string, error) {
+	var result []string
+	var limit int32 = 100
+	var exclusiveStartTableName *string
+	for {
+		out, err := d.client.ListTables(ctx, &dynamodb.ListTablesInput{
+			Limit:                   &limit,
+			ExclusiveStartTableName: exclusiveStartTableName,
+		})
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to list tables")
+		}
+		result = append(result, out.TableNames...)
+		if out.LastEvaluatedTableName == nil {
+			break
+		}
+		exclusiveStartTableName = out.LastEvaluatedTableName
+	}
+	return result, nil
 }
 
 // SyncSlowQuery syncs the slow query.
