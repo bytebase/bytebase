009e362055aec46db3e9c02244a8370ab40a5146
boojack <stevenlgtm@gmail.com>
2024-05-30 12:28:01 +0800

    chore: update role expiration validator checks (#12185)
    * chore: update role expiration validator checks
    
    * chore: update
    
    Former-commit-id: 3c8e497f992d8f20d159628498b33029af6244b5
    Former-commit-id: d8ab488b265ad42a8b861aa59cabac5c3560f1f4

--- backend/api/v1/project_service.go
+++ backend/api/v1/project_service.go
@@ -2076,8 +2076,9 @@ func (*ProjectService) validateBindings(bindings []*v1pb.Binding, roles []*v1pb.
 			return err
 		}
 
+		// Only validate when maximumRoleExpiration is set and the role is ProjectQuerier or ProjectExporter.
 		rolesToValidate := []string{fmt.Sprintf("roles/%s", api.ProjectQuerier), fmt.Sprintf("roles/%s", api.ProjectExporter)}
-		if binding.Condition != nil && binding.Condition.Expression != "" && slices.Contains(rolesToValidate, binding.Role) {
+		if maximumRoleExpiration != nil && binding.Condition != nil && binding.Condition.Expression != "" && slices.Contains(rolesToValidate, binding.Role) {
 			if err := validateIAMPolicyExpression(binding.Condition.Expression, maximumRoleExpiration); err != nil {
 				return err
 			}
