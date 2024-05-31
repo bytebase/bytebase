4cdebaf6c0bc73445073b32e20708b613353f373
boojack <stevenlgtm@gmail.com>
2024-05-31 10:34:30 +0800

    chore: remove unused components (#12200)
    * chore: remove unused components
    
    * chore: update
    
    Former-commit-id: ea6314b0786aeaf65f1e104075dcc8adf474a991

--- frontend/src/components/IssueV1/components/ReviewIssueSpecSection/ReviewOptionSection/types.ts
+++
@@ -1,5 +0,0 @@
-import type { Plan_ChangeDatabaseConfig_Type } from "@/types/proto/v1/plan_service";
-
-export type StatementType =
-  | Plan_ChangeDatabaseConfig_Type.MIGRATE
-  | Plan_ChangeDatabaseConfig_Type.DATA;
