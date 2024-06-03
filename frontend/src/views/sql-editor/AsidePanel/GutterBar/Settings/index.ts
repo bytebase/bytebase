5ed1fa24d8c5304c4d1a47266e168fd1882c2b25
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-06-03 18:05:01 +0800

    chore: remove unused

--- frontend/src/views/sql-editor/AsidePanel/GutterBar/Settings/index.ts
+++ frontend/src/views/sql-editor/AsidePanel/GutterBar/Settings/index.ts
@@ -1,5 +1,3 @@
-import SettingsButton from "./SettingsButton.vue";
 import SettingsPanel from "./SettingsPanel.vue";
 
-export * from "./context";
-export { SettingsButton, SettingsPanel };
+export { SettingsPanel };
