8fd89d9067296eb78fb8eb55b79ead80ebb5895f
h3n4l <oysterdays@gmail.com>
2024-06-03 13:25:45 +0800

    feat: enable partiql syntax check
    Signed-off-by: h3n4l <oysterdays@gmail.com>

--- backend/plugin/parser/partiql/split_test.go
+++ backend/plugin/parser/partiql/split_test.go
@@ -9,10 +9,11 @@ import (
 )
 
 func TestSplitSQL(t *testing.T) {
-	statement := `
-SELECT * FROM my_table;
-SELECT * FROM my_table;
-foobar();`
+	statement := `DELETE FROM Music WHERE Artist = 'Pink Floyd' AND SongTitle = 'Money';
+	INSERT INTO Music VALUE {'AlbumTitle': 'The Dark Side of the Moon', 'Artist': 'Pink Floyd', 'Awards': 300, 'SongTitle': 'Money'};
+	INSERT INTO Music VALUE {'AlbumTitle': 'The Dark Side of the Moon', 'Artist': 'Pink Floyd', 'Awards': 300, 'SongTitle': 'Money02'};
+	INSERT INTO Music VALUE {'AlbumTitle': 'The Dark Side of the Moon', 'Artist': 'Pink Floyd', 'Awards': 300, 'SongTitle': 'Money03'};
+	DELETE FROM Music WHERE Artist = 'Pink Floyd' AND SongTitle = 'Money02'`
 	want := []base.SingleSQL{
 		{
 			Text:                 "\nSELECT * FROM my_table;",
