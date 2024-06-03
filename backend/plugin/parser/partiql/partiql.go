a1864fb5d3ec8aed04f2f886039dc4263998b736
h3n4l <oysterdays@gmail.com>
2024-06-03 13:31:21 +0800

    feat: enable partiql syntax check (#12245)
    Signed-off-by: h3n4l <oysterdays@gmail.com>

---
+++ backend/plugin/parser/partiql/partiql.go
@@ -0,0 +1,53 @@
+package partiql
+
+import (
+	"strings"
+
+	"github.com/antlr4-go/antlr/v4"
+
+	parser "github.com/bytebase/partiql-parser"
+
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+)
+
+type ParseResult struct {
+	Tree   antlr.Tree
+	Tokens *antlr.CommonTokenStream
+}
+
+// ParsePartiQL parses the given PartiQL statement by using antlr4. Returns the AST and token stream if no error.
+func ParsePartiQL(statement string) (*ParseResult, error) {
+	statement = strings.TrimRight(statement, " \t\n\r\f;") + "\n;"
+	inputStream := antlr.NewInputStream(statement)
+	lexer := parser.NewPartiQLLexer(inputStream)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	p := parser.NewPartiQLParserParser(stream)
+
+	// Remove default error listener and add our own error listener.
+	lexer.RemoveErrorListeners()
+	lexerErrorListener := &base.ParseErrorListener{}
+	lexer.AddErrorListener(lexerErrorListener)
+
+	p.RemoveErrorListeners()
+	parserErrorListener := &base.ParseErrorListener{}
+	p.AddErrorListener(parserErrorListener)
+
+	p.BuildParseTrees = true
+
+	tree := p.Script()
+
+	if lexerErrorListener.Err != nil {
+		return nil, lexerErrorListener.Err
+	}
+
+	if parserErrorListener.Err != nil {
+		return nil, parserErrorListener.Err
+	}
+
+	result := &ParseResult{
+		Tree:   tree,
+		Tokens: stream,
+	}
+
+	return result, nil
+}
