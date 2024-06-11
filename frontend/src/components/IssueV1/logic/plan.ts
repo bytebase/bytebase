dc6ca0ca19365c3deb9d3e4c38c1b713b1376009
steven <stevenlgtm@gmail.com>
2024-06-11 14:52:18 +0800

    chore: use dbGroup.matchedDatabases instead of fetching them

--- frontend/src/components/IssueV1/logic/plan.ts
+++ frontend/src/components/IssueV1/logic/plan.ts
@@ -110,13 +110,8 @@ export const databaseEngineForSpec = async (
   if (extractDatabaseGroupName(target)) {
     const dbGroupStore = useDBGroupStore();
     const dbGroup = await dbGroupStore.getOrFetchDBGroupByName(target);
-    const { matchedDatabaseList } =
-      await dbGroupStore.fetchDatabaseGroupMatchList({
-        projectName: project.name,
-        expr: dbGroup.simpleExpr,
-      });
     // Might be flaky: use the first database in the db group
-    const dbName = head(matchedDatabaseList)?.name;
+    const dbName = head(dbGroup.matchedDatabases)?.name;
     if (dbName) {
       const db = await useDatabaseV1Store().getOrFetchDatabaseByName(
         dbName,
