f8fcf5bacc1e2ccb853b3100fb6317fb8bedcf81
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-06-11 18:43:01 +0800

    feat: scaffold

---
+++ frontend/src/views/sql-editor/Setting/Project/Detail.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="flex-1 relative overflow-auto"
+    style="width: calc(100vw - 8rem); max-width: 50rem"
+  >
+    <DatabaseV1Table
+      v-if="ready"
+      mode="PROJECT_SHORT"
+      :database-list="databaseList"
+      :custom-click="true"
+      :show-selection="false"
+      :show-sql-editor-button="false"
+      :row-clickable="false"
+    />
+    <MaskSpinner v-if="!ready || refreshing" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import MaskSpinner from "@/components/misc/MaskSpinner.vue";
+import DatabaseV1Table from "@/components/v2/Model/DatabaseV1Table";
+import { databaseServiceClient } from "@/grpcweb";
+import { DEFAULT_DATABASE_PAGE_SIZE, batchComposeDatabase } from "@/store";
+import type { ComposedDatabase, ComposedProject } from "@/types";
+
+const props = defineProps<{
+  project: ComposedProject;
+}>();
+
+const ready = ref(false);
+const refreshing = ref(false);
+const databaseList = ref<ComposedDatabase[]>([]);
+
+const fetchDatabaseList = async (force: boolean) => {
+  refreshing.value = true;
+  if (force) {
+    ready.value = false;
+  }
+  const response = await databaseServiceClient.listDatabases({
+    parent: "instances/-",
+    filter: `project == "${props.project.name}"`,
+    pageSize: DEFAULT_DATABASE_PAGE_SIZE,
+  });
+
+  const list = await batchComposeDatabase(response.databases);
+
+  databaseList.value = list;
+
+  refreshing.value = false;
+  ready.value = true;
+};
+
+watch(
+  () => props.project.name,
+  () => fetchDatabaseList(/* force */ true),
+  { immediate: true }
+);
+</script>
--- frontend/src/views/sql-editor/Setting/Project/Project.vue
+++ frontend/src/views/sql-editor/Setting/Project/Project.vue
@@ -1,3 +1,111 @@
 <template>
-  <h1>Project</h1>
+  <div class="w-full flex flex-col gap-4 py-4 px-2 overflow-y-auto">
+    <div class="flex flex-col items-start gap-2 sm:flex-row sm:justify-between sm:items-center">
+      <div class="flex justify-start items-center">
+        <NButton @click="handleClickNewProject">
+          <template #icon>
+            <PlusIcon class="h-4 w-4" />
+          </template>
+          <NEllipsis>
+            {{ $t("quick-action.new-project") }}
+          </NEllipsis>
+        </NButton>
+      </div>
+
+      <div class="flex justify-end items-center">
+        <SearchBox
+          v-model:value="state.keyword"
+          class="!max-w-full md:!max-w-[18rem]"
+          :placeholder="$t('common.filter-by-name')"
+          :autofocus="true"
+        />
+      </div>
+    </div>
+
+    <ProjectV1Table
+      :project-list="filteredProjectList"
+      :on-click="showProjectDetail"
+    />
+
+    <Drawer
+      :show="state.detail.show && !!state.detail.project"
+      :close-on-esc="true"
+      :mask-closable="true"
+      @update:show="hideDrawer"
+    >
+      <DrawerContent
+        :title="detailTitle"
+        body-content-class="flex flex-col gap-2 overflow-hidden"
+      >
+        <Detail v-if="state.detail.project" :project="state.detail.project" />
+      </DrawerContent>
+    </Drawer>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { PlusIcon } from "lucide-vue-next";
+import { NButton, NEllipsis } from "naive-ui";
+import { computed, reactive } from "vue";
+import { useI18n } from "vue-i18n";
+import { useRoute, useRouter } from "vue-router";
+import {
+  Drawer,
+  DrawerContent,
+  ProjectV1Table,
+  SearchBox,
+} from "@/components/v2";
+import { useProjectV1List } from "@/store";
+import { DEFAULT_PROJECT_ID, type ComposedProject } from "@/types";
+import { filterProjectV1ListByKeyword } from "@/utils";
+import Detail from "./Detail.vue";
+
+interface LocalState {
+  keyword: string;
+  detail: {
+    show: boolean;
+    project: ComposedProject | undefined;
+  };
+}
+
+const route = useRoute();
+const router = useRouter();
+const { t } = useI18n();
+
+const state = reactive<LocalState>({
+  keyword: "",
+  detail: {
+    show: false,
+    project: undefined,
+  },
+});
+const { projectList, ready } = useProjectV1List(
+  /* showDeleted */ false,
+  /* forceUpdate */ true
+);
+
+const filteredProjectList = computed(() => {
+  return filterProjectV1ListByKeyword(projectList.value, state.keyword);
+});
+
+const detailTitle = computed(() => {
+  return state.detail.project
+    ? `${t("common.project")} - ${state.detail.project.title}`
+    : t("quick-action.new-project");
+});
+
+const handleClickNewProject = () => {
+  state.detail.show = true;
+  state.detail.project = undefined;
+};
+
+const showProjectDetail = (project: ComposedProject) => {
+  state.detail.show = true;
+  state.detail.project = project;
+};
+
+const hideDrawer = () => {
+  state.detail.show = false;
+  state.detail.project = undefined;
+};
+</script>
