9ff8dbf3c5e142a06ad8f562ae3e6e248a6c1e9b
d-bytebase <d@bytebase.com>
2024-06-11 19:45:08 -0700

    chore: update

--- backend/plugin/db/obo/sync.go
+++ backend/plugin/db/obo/sync.go
@@ -91,7 +91,7 @@ func (driver *Driver) SyncDBSchema(ctx context.Context) (*storepb.DatabaseSchema
 		Name: driver.databaseName,
 	}
 	databaseMetadata.Schemas = append(databaseMetadata.Schemas, &storepb.SchemaMetadata{
-		Name:   driver.databaseName,
+		Name:   "",
 		Tables: tableMap[driver.databaseName],
 		Views:  viewMap[driver.databaseName],
 	})
