e9c73ba753982d0ebddf9ee067e34cb81db39c1a
h3n4l <oysterdays@gmail.com>
2024-06-17 16:23:53 +0800

    refactor: remove dead code (#12399)
    Signed-off-by: h3n4l <oysterdays@gmail.com>

--- backend/plugin/db/oracle/oracle.go
+++ backend/plugin/db/oracle/oracle.go
@@ -279,20 +279,6 @@ func (driver *Driver) querySingleSQL(ctx context.Context, conn *sql.Conn, single
 		statement = stmt
 	}
 
-	if queryContext != nil && queryContext.SensitiveSchemaInfo != nil {
-		for _, database := range queryContext.SensitiveSchemaInfo.DatabaseList {
-			if len(database.SchemaList) == 0 {
-				continue
-			}
-			if len(database.SchemaList) > 1 {
-				return nil, errors.Errorf("Oracle schema info should only have one schema per database, but got %d, %v", len(database.SchemaList), database.SchemaList)
-			}
-			if database.SchemaList[0].Name != database.Name {
-				return nil, errors.Errorf("Oracle schema info should have the same database name and schema name, but got %s and %s", database.Name, database.SchemaList[0].Name)
-			}
-		}
-	}
-
 	startTime := time.Now()
 	result, err := util.Query(ctx, storepb.Engine_ORACLE, conn, statement, queryContext)
 	if err != nil {
