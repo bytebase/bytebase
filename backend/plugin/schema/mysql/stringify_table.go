27081b863ea405bdcb625054ec3f4f57a974957b
rebelice <yangrebelice@gmail.com>
2024-06-17 17:46:47 +0800

    feat: finish

--- backend/plugin/schema/mysql/stringify_table.go
+++ backend/plugin/schema/mysql/stringify_table.go
@@ -13,8 +13,13 @@ const (
 	mysqlTypeTinyBob    = "tinyblob"
 	mysqlTypeMediumBlob = "mediumblob"
 	mysqlTypeLongBlob   = "longblob"
-	mysqlTypeJson       = "json"
+	mysqlTypeJSON       = "json"
 	mysqlTypeGeometry   = "geometry"
+
+	mysqlIndexFullText = "FULLTEXT"
+	mysqlIndexSpatial  = "SPATIAL"
+
+	mysqlNoAction = "NO ACTION"
 )
 
 func init() {
@@ -52,7 +57,96 @@ func StringifyTable(metadata *storepb.TableMetadata) (string, error) {
 		}
 	}
 
+	for _, fk := range metadata.ForeignKeys {
+		if err := printForeignKeyClause(&buf, fk); err != nil {
+			return "", err
+		}
+	}
+
+	for _, check := range metadata.CheckConstraints {
+		if err := printCheckClause(&buf, check); err != nil {
+			return "", err
+		}
+	}
+
+	if _, err := fmt.Fprintf(&buf, "\n) ENGINE=%s", metadata.Engine); err != nil {
+		return "", err
+	}
+
+	if metadata.Collation != "" {
+		if _, err := fmt.Fprintf(&buf, " COLLATE=%s", metadata.Collation); err != nil {
+			return "", err
+		}
+	}
+
+	if metadata.Comment != "" {
+		if _, err := fmt.Fprintf(&buf, " COMMENT='%s'", metadata.Comment); err != nil {
+			return "", err
+		}
+	}
+
+	if _, err := fmt.Fprintf(&buf, ";\n"); err != nil {
+		return "", err
+	}
+
 	return buf.String(), nil
+}
+
+func printCheckClause(buf *strings.Builder, check *storepb.CheckConstraintMetadata) error {
+	if _, err := fmt.Fprintf(buf, ",\n  CONSTRAINT `%s` CHECK %s", check.Name, check.Expression); err != nil {
+		return err
+	}
+	return nil
+}
+
+func printForeignKeyClause(buf *strings.Builder, fk *storepb.ForeignKeyMetadata) error {
+	if _, err := fmt.Fprintf(buf, ",\n  CONSTRAINT `%s` FOREIGN KEY (", fk.Name); err != nil {
+		return err
+	}
+
+	for i, column := range fk.Columns {
+		if i != 0 {
+			if _, err := fmt.Fprintf(buf, ", "); err != nil {
+				return err
+			}
+		}
+		if _, err := fmt.Fprintf(buf, "`%s`", column); err != nil {
+			return err
+		}
+	}
+
+	if _, err := fmt.Fprintf(buf, ") REFERENCES `%s`.`%s` (", fk.ReferencedSchema, fk.ReferencedTable); err != nil {
+		return err
+	}
+
+	for i, column := range fk.ReferencedColumns {
+		if i != 0 {
+			if _, err := fmt.Fprintf(buf, ", "); err != nil {
+				return err
+			}
+		}
+		if _, err := fmt.Fprintf(buf, "`%s`", column); err != nil {
+			return err
+		}
+	}
+
+	if _, err := fmt.Fprintf(buf, ")"); err != nil {
+		return err
+	}
+
+	if fk.OnDelete != "" && !strings.EqualFold(fk.OnDelete, mysqlNoAction) {
+		if _, err := fmt.Fprintf(buf, " ON DELETE %s", fk.OnDelete); err != nil {
+			return err
+		}
+	}
+
+	if fk.OnUpdate != "" && !strings.EqualFold(fk.OnUpdate, mysqlNoAction) {
+		if _, err := fmt.Fprintf(buf, " ON UPDATE %s", fk.OnUpdate); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func printIndexClause(buf *strings.Builder, index *storepb.IndexMetadata) error {
@@ -61,25 +155,74 @@ func printIndexClause(buf *strings.Builder, index *storepb.IndexMetadata) error 
 	}
 
 	if _, err := fmt.Fprintf(buf, ",\n  "); err != nil {
-		return nil
+		return err
 	}
+
+	if index.Unique {
+		if _, err := fmt.Fprintf(buf, "UNIQUE "); err != nil {
+			return err
+		}
+	} else if strings.EqualFold(index.Type, mysqlIndexFullText) {
+		if _, err := fmt.Fprintf(buf, "FULLTEXT "); err != nil {
+			return err
+		}
+	} else if strings.EqualFold(index.Type, mysqlIndexSpatial) {
+		if _, err := fmt.Fprintf(buf, "SPATIAL "); err != nil {
+			return err
+		}
+	}
+
+	if _, err := fmt.Fprintf(buf, "KEY `%s` (", index.Name); err != nil {
+		return err
+	}
+
+	for i, expr := range index.Expressions {
+		if i != 0 {
+			if _, err := fmt.Fprintf(buf, ", "); err != nil {
+				return err
+			}
+		}
+		if _, err := fmt.Fprintf(buf, "%s", expr); err != nil {
+			return err
+		}
+
+		if len(index.Descending) > i && index.Descending[i] {
+			if _, err := fmt.Fprintf(buf, " DESC"); err != nil {
+				return err
+			}
+		}
+	}
+
+	if _, err := fmt.Fprintf(buf, ")"); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func printPrimaryKeyClause(buf *strings.Builder, indexes []*storepb.IndexMetadata) error {
 	for _, index := range indexes {
 		if index.Primary {
 			if _, err := fmt.Fprintf(buf, ",\n  PRIMARY KEY ("); err != nil {
-				return nil
+				return err
 			}
 			for i, column := range index.Expressions {
 				if i != 0 {
 					if _, err := fmt.Fprintf(buf, ", "); err != nil {
-						return nil
+						return err
 					}
 				}
 				if _, err := fmt.Fprintf(buf, "`%s`", column); err != nil {
-					return nil
+					return err
 				}
+				if len(index.Descending) > i && index.Descending[i] {
+					if _, err := fmt.Fprintf(buf, " DESC"); err != nil {
+						return err
+					}
+				}
+			}
+			if _, err := fmt.Fprintf(buf, ")"); err != nil {
+				return err
 			}
 			return nil
 		}
@@ -203,7 +346,7 @@ func printDefaultClause(buf *strings.Builder, column *storepb.ColumnMetadata) er
 
 func typeSupportsDefaultValue(tp string) bool {
 	switch strings.ToLower(tp) {
-	case mysqlTypeBlob, mysqlTypeTinyBob, mysqlTypeMediumBlob, mysqlTypeLongBlob, mysqlTypeJson, mysqlTypeGeometry:
+	case mysqlTypeBlob, mysqlTypeTinyBob, mysqlTypeMediumBlob, mysqlTypeLongBlob, mysqlTypeJSON, mysqlTypeGeometry:
 		return false
 	default:
 		return true
