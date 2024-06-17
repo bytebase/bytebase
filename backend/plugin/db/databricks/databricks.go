ae144f195569f1548444bddd16a379763bf0b785
Azusain <azusaings@gmail.com>
2024-06-17 02:10:23 -0700

    feat(databricks): query (#12401)
    Former-commit-id: 48dc996431540071797b09b7bf11f52dd495b39a
    Former-commit-id: 8db372fc4366f61518b2b095e010e66284e53020

--- backend/plugin/db/databricks/databricks.go
+++ backend/plugin/db/databricks/databricks.go
@@ -3,15 +3,19 @@ package databricks
 import (
 	"context"
 	"database/sql"
+	"strconv"
+	"time"
 
 	"github.com/databricks/databricks-sdk-go"
 	"github.com/databricks/databricks-sdk-go/service/workspace"
 	"github.com/pkg/errors"
+	"google.golang.org/protobuf/types/known/durationpb"
 
 	// Databricks SQL.
 	dbsql "github.com/databricks/databricks-sdk-go/service/sql"
 
 	"github.com/bytebase/bytebase/backend/plugin/db"
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
 
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
@@ -85,16 +89,56 @@ func (*Driver) GetType() storepb.Engine {
 	return storepb.Engine_DATABRICKS
 }
 
-func (*Driver) QueryConn(_ context.Context, _ *sql.Conn, _ string, _ *db.QueryContext) ([]*v1pb.QueryResult, error) {
-	return nil, nil
+func (d *Driver) QueryConn(ctx context.Context, _ *sql.Conn, stmt string, _ *db.QueryContext) ([]*v1pb.QueryResult, error) {
+	return d.RunStatement(ctx, nil, stmt)
 }
 
-func (*Driver) RunStatement(_ context.Context, _ *sql.Conn, _ string) ([]*v1pb.QueryResult, error) {
-	return nil, nil
+func (d *Driver) RunStatement(ctx context.Context, _ *sql.Conn, statementStr string) ([]*v1pb.QueryResult, error) {
+	var results []*v1pb.QueryResult
+	stmts, err := base.SplitMultiSQL(storepb.Engine_DATABRICKS, statementStr)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, stmt := range stmts {
+		result := &v1pb.QueryResult{}
+		startTime := time.Now()
+		dataArr, colInfo, err := d.execSingleSQLSync(ctx, stmt.Text)
+		if err != nil {
+			return nil, err
+		}
+		if dataArr == nil || colInfo == nil {
+			break
+		}
+
+		colNames, colTypeNames := toStrColInfo(colInfo)
+		result.ColumnNames = colNames
+		result.ColumnTypeNames = colTypeNames
+
+		// process rows.
+		for _, rowData := range dataArr {
+			queryRow := &v1pb.QueryRow{}
+			// process a single row.
+			for idx, rowVal := range rowData {
+				v1pbRowVal, err := toV1pbRowVal(colInfo[idx].TypeName, rowVal)
+				if err != nil {
+					return nil, err
+				}
+				queryRow.Values = append(queryRow.Values, v1pbRowVal)
+			}
+			result.Rows = append(result.Rows, queryRow)
+		}
+		result.Latency = durationpb.New(time.Since(startTime))
+		results = append(results, result)
+	}
+
+	return results, nil
 }
 
-func (*Driver) Execute(_ context.Context, _ string, _ db.ExecuteOptions) (int64, error) {
-	return 0, nil
+func (d *Driver) Execute(ctx context.Context, stmt string, _ db.ExecuteOptions) (int64, error) {
+	_, err := d.RunStatement(ctx, nil, stmt)
+	// TODO(tommy): no approaches for fetching affected rows.
+	return 0, err
 }
 
 func (*Driver) CheckSlowQueryLogEnabled(_ context.Context) error {
@@ -102,16 +146,90 @@ func (*Driver) CheckSlowQueryLogEnabled(_ context.Context) error {
 }
 
 // Execute SQL statement synchronously and return row data or error.
-func (d *Driver) execSQLSync(ctx context.Context, statement string) ([][]string, error) {
+func (d *Driver) execSingleSQLSync(ctx context.Context, statement string) ([][]string, []dbsql.ColumnInfo, error) {
 	resp, err := d.Client.StatementExecution.ExecuteAndWait(ctx, dbsql.ExecuteStatementRequest{
 		Statement:   statement,
 		WarehouseId: d.WarehouseID,
 	})
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	if resp.Result == nil {
-		return nil, errors.New("no response")
+		return nil, nil, errors.New("no response")
 	}
-	return resp.Result.DataArray, nil
+
+	if len(resp.Result.DataArray) != 0 {
+		if resp.Manifest == nil || resp.Manifest.Schema == nil || len(resp.Manifest.Schema.Columns) == 0 {
+			return nil, nil, errors.New("missing column info")
+		}
+		return resp.Result.DataArray, resp.Manifest.Schema.Columns, nil
+	}
+	return nil, nil, nil
+}
+
+// return a column type name array and a column name array.
+func toStrColInfo(colInfo []dbsql.ColumnInfo) ([]string, []string) {
+	colNames := []string{}
+	colTypeNames := []string{}
+	for _, col := range colInfo {
+		colNames = append(colNames, col.Name)
+		colTypeNames = append(colTypeNames, string(col.TypeName))
+	}
+	return colNames, colTypeNames
+}
+
+func toV1pbRowVal(colType dbsql.ColumnInfoTypeName, val string) (*v1pb.RowValue, error) {
+	rowVal := v1pb.RowValue{}
+	switch colType {
+	case dbsql.ColumnInfoTypeNameBoolean:
+		boolVal, err := strconv.ParseBool(val)
+		if err != nil {
+			return nil, err
+		}
+		rowVal.Kind = &v1pb.RowValue_BoolValue{BoolValue: boolVal}
+
+	case dbsql.ColumnInfoTypeNameBinary:
+		rowVal.Kind = &v1pb.RowValue_BytesValue{BytesValue: []byte(val)}
+
+	case dbsql.ColumnInfoTypeNameShort:
+		shortVal, err := strconv.ParseInt(val, 10, 16)
+		if err != nil {
+			return nil, err
+		}
+		rowVal.Kind = &v1pb.RowValue_Int32Value{Int32Value: int32(shortVal)}
+
+	case dbsql.ColumnInfoTypeNameInt:
+		i32Val, err := strconv.ParseInt(val, 10, 32)
+		if err != nil {
+			return nil, err
+		}
+		rowVal.Kind = &v1pb.RowValue_Int32Value{Int32Value: int32(i32Val)}
+
+	case dbsql.ColumnInfoTypeNameLong:
+		i64Val, err := strconv.ParseInt(val, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		rowVal.Kind = &v1pb.RowValue_Int64Value{Int64Value: i64Val}
+
+	case dbsql.ColumnInfoTypeNameFloat:
+		floatVal, err := strconv.ParseFloat(val, 32)
+		if err != nil {
+			return nil, err
+		}
+		rowVal.Kind = &v1pb.RowValue_FloatValue{FloatValue: float32(floatVal)}
+
+	case dbsql.ColumnInfoTypeNameDouble:
+		doubleVal, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			return nil, err
+		}
+		rowVal.Kind = &v1pb.RowValue_DoubleValue{DoubleValue: doubleVal}
+
+	default:
+		// convert all remaining types to string.
+		rowVal.Kind = &v1pb.RowValue_StringValue{StringValue: val}
+	}
+
+	return &rowVal, nil
 }
