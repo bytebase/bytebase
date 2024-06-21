a6eb35469dd6e77f03116e8a2529a09cf9643180
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-06-21 17:00:03 +0800

    chore: texts

--- frontend/src/views/sql-editor/EditorCommon/QueryModeSelect.vue
+++ frontend/src/views/sql-editor/EditorCommon/QueryModeSelect.vue
@@ -8,25 +8,24 @@
     <div class="flex flex-col gap-2">
       <p class="font-semibold">{{ $t("common.mode") }}</p>
 
-      <NRadioGroup v-model:value="tab.mode" class="gap-2 !flex flex-col">
-        <NRadio value="STANDARD" class="flex flex-row">
-          <div class="flex flex-col gap-1 text-sm">
-            <p class="font-normal">
-              {{ $t("sql-editor.tab-mode.standard") }}
-            </p>
-            <p class="text-control-light text-xs">
-              Allow DDL and DML statements.
-            </p>
-          </div>
-        </NRadio>
+      <NRadioGroup v-model:value="tab.mode" class="!flex flex-col gap-2">
         <NRadio value="READONLY" class="flex flex-row">
           <div class="flex flex-col gap-1 text-sm">
             <p class="font-normal">
               {{ $t("sql-editor.tab-mode.readonly") }}
             </p>
             <p class="text-control-light text-xs">
-              Allow <code>SELECT</code>, <code>SHOW</code> and
-              <code>SET</code> statements.
+              {{ $t("sql-editor.tab-mode.description.readonly") }}
+            </p>
+          </div>
+        </NRadio>
+        <NRadio value="STANDARD" class="flex flex-row">
+          <div class="flex flex-col gap-1 text-sm">
+            <p class="font-normal">
+              {{ $t("sql-editor.tab-mode.standard") }}
+            </p>
+            <p class="text-control-light text-xs">
+              {{ $t("sql-editor.tab-mode.description.standard") }}
             </p>
           </div>
         </NRadio>
