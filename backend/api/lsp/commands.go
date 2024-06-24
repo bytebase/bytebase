7f28e3038deb6d4f1845a0ce8c70d1a5f8216b07
rebelice <yangrebelice@gmail.com>
2024-06-24 11:21:04 +0800

    chore: add scene config for completion (#12484)
    * chore: add scene config for completion
    
    * chore: address comments
    
    Former-commit-id: 88b5085322c7f17945df4cc432b4e68b33ce0de3

--- backend/api/lsp/commands.go
+++ backend/api/lsp/commands.go
@@ -23,4 +23,11 @@ type SetMetadataCommandArguments struct {
 	// For PostgreSQL, it's required.
 	// For other database engines, it's optional.
 	DatabaseName string `json:"databaseName,omitempty"`
+	// The scene is the scene for completion.
+	// Available scenes: "query", "all".
+	// If not provided, it defaults to "all".
+	// If the scene is "query", it will only return completion items for query statements,
+	// 	in other words, SELECT statements only.
+	// If the scene is "all", it will return completion items for all statements.
+	Scene string `json:"scene,omitempty"`
 }
