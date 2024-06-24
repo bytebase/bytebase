f491d5e49125310051b4c59230950fca0006f23b
rebelice <yangrebelice@gmail.com>
2024-06-24 11:08:48 +0800

    chore: address comments

--- backend/api/lsp/commands.go
+++ backend/api/lsp/commands.go
@@ -25,5 +25,9 @@ type SetMetadataCommandArguments struct {
 	DatabaseName string `json:"databaseName,omitempty"`
 	// The scene is the scene for completion.
 	// Available scenes: "query", "all".
+	// If not provided, it defaults to "all".
+	// If the scene is "query", it will only return completion items for query statements,
+	// 	in other words, SELECT statements only.
+	// If the scene is "all", it will return completion items for all statements.
 	Scene string `json:"scene,omitempty"`
 }
