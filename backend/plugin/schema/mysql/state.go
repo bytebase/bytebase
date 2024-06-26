78d8dd66873045f57d85a3da723ce47d00c219ab
h3n4l <oysterdays@gmail.com>
2024-06-26 17:49:22 +0800

    feat: write canonical data type in our standard format (#12529)
    * feat: standardrize use-input schema in branch rebase
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * fix: considering mysql synonymous type in tryMerge
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: fix linter
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * feat: write canonical data type in our standard format
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    ---------
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    Former-commit-id: ec78156532a98544d8d048a265f71bfa40ded2d4

--- backend/plugin/schema/mysql/state.go
+++ backend/plugin/schema/mysql/state.go
@@ -16,6 +16,8 @@ import (
 
 	mysql "github.com/bytebase/mysql-parser"
 
+	mysqldb "github.com/bytebase/bytebase/backend/plugin/db/mysql"
+
 	mysqlparser "github.com/bytebase/bytebase/backend/plugin/parser/mysql"
 
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
@@ -203,6 +205,9 @@ func (t *tableState) toString(buf io.StringWriter) error {
 	sort.Slice(indexes, func(i, j int) bool {
 		if indexes[i].primary {
 			return true
+		}
+		if indexes[j].primary {
+			return false
 		}
 		return indexes[i].name < indexes[j].name
 	})
@@ -1314,7 +1319,8 @@ type columnState struct {
 }
 
 func (c *columnState) toString(buf io.StringWriter) error {
-	if _, err := buf.WriteString(fmt.Sprintf("`%s` %s", c.name, c.tp)); err != nil {
+	columnCanonicalType := mysqldb.GetColumnTypeCanonicalSynonym(strings.ToLower(c.tp))
+	if _, err := buf.WriteString(fmt.Sprintf("`%s` %s", c.name, columnCanonicalType)); err != nil {
 		return err
 	}
 	if !c.nullable {
