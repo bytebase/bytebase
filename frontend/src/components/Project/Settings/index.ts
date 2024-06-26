e7ca6ca9e15b78eb6b0b19c8c87174a765e98f77
boojack <stevenlgtm@gmail.com>
2024-06-26 11:30:59 +0800

    feat: implement project issue related settings (#12513)
    Former-commit-id: 7b42dfd03cd1ca849cddebf70093570ded4da8b7

---
+++ frontend/src/components/Project/Settings/index.ts
@@ -0,0 +1,11 @@
+import ProjectBranchProtectionRulesSettingPanel from "./ProjectBranchProtectionRulesSettingPanel.vue";
+import ProjectGeneralSettingPanel from "./ProjectGeneralSettingPanel.vue";
+import ProjectIssueRelatedSettingPanel from "./ProjectIssueRelatedSettingPanel.vue";
+import ProjectSecuritySettingPanel from "./ProjectSecuritySettingPanel.vue";
+
+export {
+  ProjectBranchProtectionRulesSettingPanel,
+  ProjectGeneralSettingPanel,
+  ProjectSecuritySettingPanel,
+  ProjectIssueRelatedSettingPanel,
+};
