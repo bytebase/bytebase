a9bc50c0cd60bd2d1b86e4d1b87b39700906ce7d
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-06-28 17:37:19 +0800

    feat(sql-editor): add "Add a new instance" to SQL Editor welcome page (#12551)
    * fix: use proxy value
    
    * chore: format
    
    * chore: implementation details
    
    * chore: add a new instance button
    
    Former-commit-id: 45b05b99e7f132120f1ddcd0b5f234dc21145769

--- frontend/src/views/sql-editor/EditorPanel/Welcome.vue
+++ frontend/src/views/sql-editor/EditorPanel/Welcome.vue
@@ -3,16 +3,35 @@
     class="w-full h-auto flex-grow flex flex-col items-center justify-center"
   >
     <HideInStandaloneMode>
-      <BytebaseLogo component="span" class="mb-4 -mt-[20%]" />
+      <BytebaseLogo component="span" class="mb-4 -mt-[10vh]" />
     </HideInStandaloneMode>
-    <div class="flex flex-col items-start gap-y-1 w-max">
-      <NButton type="primary" class="!w-full !justify-start" @click="changeConnection">
+    <div class="flex flex-col items-start gap-y-2 w-max">
+      <NButton
+        type="primary"
+        class="!w-full !justify-start"
+        @click="changeConnection"
+      >
         <template #icon>
           <LinkIcon class="w-4 h-4" />
         </template>
         {{ $t("sql-editor.connect-to-a-database") }}
       </NButton>
-      <NButton quaternary class="!w-full !justify-start" @click="createNewWorksheet">
+      <NButton
+        v-if="showCreateInstanceButton"
+        type="default"
+        class="!w-full !justify-start"
+        @click="gotoInstanceCreatePage"
+      >
+        <template #icon>
+          <LayersIcon class="w-4 h-4" />
+        </template>
+        {{ $t("sql-editor.add-a-new-instance") }}
+      </NButton>
+      <NButton
+        type="default"
+        class="!w-full !justify-start"
+        @click="createNewWorksheet"
+      >
         <template #icon>
           <SquarePenIcon class="w-4 h-4" />
         </template>
@@ -23,13 +42,27 @@
 </template>
 
 <script setup lang="ts">
-import { LinkIcon, SquarePenIcon } from "lucide-vue-next";
+import { LayersIcon, LinkIcon, SquarePenIcon } from "lucide-vue-next";
 import { NButton } from "naive-ui";
+import { computed } from "vue";
+import { useRouter } from "vue-router";
 import BytebaseLogo from "@/components/BytebaseLogo.vue";
+import { SQL_EDITOR_SETTING_INSTANCE_MODULE } from "@/router/sqlEditor";
 import { useSQLEditorTabStore } from "@/store";
+import { useSidebarItems as useSettingItems } from "../Setting/Sidebar";
 import { useSQLEditorContext } from "../context";
 
 const { showConnectionPanel } = useSQLEditorContext();
+const { itemList: settingItemList } = useSettingItems();
+const router = useRouter();
+
+const showCreateInstanceButton = computed(() => {
+  return (
+    settingItemList.value.findIndex(
+      (item) => item.name === SQL_EDITOR_SETTING_INSTANCE_MODULE
+    ) >= 0
+  );
+});
 
 const changeConnection = () => {
   showConnectionPanel.value = true;
@@ -37,5 +70,19 @@ const changeConnection = () => {
 
 const createNewWorksheet = () => {
   useSQLEditorTabStore().addTab();
+};
+
+const gotoInstanceCreatePage = () => {
+  if (
+    settingItemList.value.findIndex(
+      (item) => item.name === SQL_EDITOR_SETTING_INSTANCE_MODULE
+    ) < 0
+  ) {
+    return;
+  }
+  router.push({
+    name: SQL_EDITOR_SETTING_INSTANCE_MODULE,
+    hash: `#add`,
+  });
 };
 </script>
