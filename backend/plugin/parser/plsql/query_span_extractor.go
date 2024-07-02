96ed51332d0dd1353e5c7e0727e51c9edc45d5d9
rebelice <yangrebelice@gmail.com>
2024-07-02 15:33:31 +0800

    chore: consider db link name with dot (#12643)
    Former-commit-id: 581cafc3240989c0153ca98b08dd55bb8c63d2d3

--- backend/plugin/parser/plsql/query_span_extractor.go
+++ backend/plugin/parser/plsql/query_span_extractor.go
@@ -37,12 +37,7 @@ func newQuerySpanExtractor(connectionDatabase string, gCtx base.GetQuerySpanCont
 	}
 }
 
-func (q *querySpanExtractor) getLinkedDatabaseMetadata(name []string, schema string) (string, *model.DatabaseMetadata, error) {
-	if len(name) == 0 {
-		return "", nil, errors.New("empty linked database name")
-	}
-	// TODO: The last element may not be the name.
-	linkName := name[len(name)-1]
+func (q *querySpanExtractor) getLinkedDatabaseMetadata(linkName string, schema string) (string, *model.DatabaseMetadata, error) {
 	if meta, ok := q.linkedMetaCache[linkName]; ok {
 		return linkName, meta, nil
 	}
@@ -1244,12 +1239,12 @@ func (q *querySpanExtractor) plsqlFindTableSchema(dbLink []string, schemaName, t
 		}, nil
 	}
 	if len(dbLink) > 0 {
-		databaseName, linkedMeta, err := q.getLinkedDatabaseMetadata(dbLink, schemaName)
+		linkName := strings.Join(dbLink, ".")
+		databaseName, linkedMeta, err := q.getLinkedDatabaseMetadata(linkName, schemaName)
 		if err != nil {
 			return nil, errors.Wrapf(err, "failed to get linked database metadata for: %s", dbLink)
 		}
 		if linkedMeta == nil {
-			linkName := strings.Join(dbLink, ".")
 			return nil, &parsererror.ResourceNotFoundError{
 				DatabaseLink: &linkName,
 			}
