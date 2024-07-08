e7265411d70152b56ec21feab3b067e53c4e4ce9
p0ny <xz@bytebase.com>
2024-07-08 17:28:46 +0800

    feat: impl more task run log (#12683)
    Former-commit-id: d86311abe807a0c9ac1910174228d85de36c4c2d
    Former-commit-id: 6f62e26bfdfed5408f4ac947781870e0abe9b4c7

--- backend/runner/taskrun/data_update_executor.go
+++ backend/runner/taskrun/data_update_executor.go
@@ -82,13 +82,29 @@ func (exec *DataUpdateExecutor) RunOnce(ctx context.Context, driverCtx context.C
 	version := model.Version{Version: payload.SchemaVersion}
 	terminated, result, err := runMigration(ctx, driverCtx, exec.store, exec.dbFactory, exec.stateCfg, exec.profile, task, taskRunUID, db.Data, statement, version, &payload.SheetID)
 	// sync database schema anyways
+	exec.store.CreateTaskRunLogS(ctx, taskRunUID, time.Now(), &storepb.TaskRunLog{
+		Type:              storepb.TaskRunLog_DATABASE_SYNC_START,
+		DatabaseSyncStart: &storepb.TaskRunLog_DatabaseSyncStart{},
+	})
 	if err := exec.schemaSyncer.SyncDatabaseSchema(ctx, database, true /* force */); err != nil {
+		exec.store.CreateTaskRunLogS(ctx, taskRunUID, time.Now(), &storepb.TaskRunLog{
+			Type: storepb.TaskRunLog_DATABASE_SYNC_END,
+			DatabaseSyncEnd: &storepb.TaskRunLog_DatabaseSyncEnd{
+				Error: err.Error(),
+			},
+		})
 		slog.Error("failed to sync database schema",
 			slog.String("instanceName", instance.ResourceID),
 			slog.String("databaseName", database.DatabaseName),
 			log.BBError(err),
 		)
 	}
+	exec.store.CreateTaskRunLogS(ctx, taskRunUID, time.Now(), &storepb.TaskRunLog{
+		Type: storepb.TaskRunLog_DATABASE_SYNC_END,
+		DatabaseSyncEnd: &storepb.TaskRunLog_DatabaseSyncEnd{
+			Error: "",
+		},
+	})
 
 	return terminated, result, err
 }
