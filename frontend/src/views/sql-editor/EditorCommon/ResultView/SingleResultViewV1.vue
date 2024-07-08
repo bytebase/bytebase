d83b57aad2ccf4b4230c616bc3d74b21e2f37ece
ecmadao <wlec@outlook.com>
2024-07-08 11:50:04 +0800

    chore: show both error msg and result rows (#12665)

--- frontend/src/views/sql-editor/EditorCommon/ResultView/SingleResultViewV1.vue
+++ frontend/src/views/sql-editor/EditorCommon/ResultView/SingleResultViewV1.vue
@@ -1,5 +1,8 @@
 <template>
   <template v-if="viewMode === 'RESULT'">
+    <BBAttention v-if="result.error" class="w-full mb-2" :type="'error'">
+      <ErrorView :error="result.error" />
+    </BBAttention>
     <div
       class="w-full shrink-0 flex flex-row justify-between items-center mb-2 overflow-x-auto"
     >
@@ -262,7 +265,7 @@ const { instance: connectedInstance } = useConnectionOfCurrentSQLEditorTab();
 
 const viewMode = computed((): ViewMode => {
   const { result } = props;
-  if (result.error) {
+  if (result.error && data.value.length === 0) {
     return "ERROR";
   }
   const columnNames = result.columnNames;
