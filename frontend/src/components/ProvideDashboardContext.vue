3fe166da3a03da59825d9e66b8cce984f70a7a46
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-07-09 16:54:07 +0800

    chore: optimize project switching

--- frontend/src/components/ProvideDashboardContext.vue
+++ frontend/src/components/ProvideDashboardContext.vue
@@ -1,10 +1,18 @@
 <template>
-  <slot v-if="!isLoading" />
+  <slot v-if="!isInitializing" />
   <MaskSpinner v-else class="!bg-white" />
+
+  <div
+    v-if="!isInitializing && isSwitchingProject"
+    class="fixed inset-0 z-[1000000] bg-white/50 flex flex-col items-center justify-center"
+  >
+    <NSpin />
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, watch } from "vue";
+import { NSpin } from "naive-ui";
+import { ref, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import {
   useEnvironmentV1Store,
@@ -24,7 +32,8 @@ import MaskSpinner from "./misc/MaskSpinner.vue";
 
 const route = useRoute();
 const router = useRouter();
-const isLoading = ref<boolean>(true);
+const isInitializing = ref<boolean>(true);
+const isSwitchingProject = ref(false);
 
 const policyStore = usePolicyV1Store();
 const databaseStore = useDatabaseV1Store();
@@ -47,6 +56,7 @@ const fetchDatabases = async (project: string) => {
     filter: filters.join(" && "),
   });
 };
+
 const fetchInstancesAndDatabases = async (project: string) => {
   await fetchInstances(project);
   await fetchDatabases(project);
@@ -74,14 +84,25 @@ onMounted(async () => {
   await Promise.all([useUIStateStore().restoreState()]);
   await fetchInstancesAndDatabases(route.params.projectId as string);
 
-  watch(
-    () => route.params.projectId,
-    (project) => {
-      fetchInstancesAndDatabases(project as string);
-    },
-    { immediate: false }
-  );
+  isInitializing.value = false;
 
-  isLoading.value = false;
+  router.beforeEach((to, from, next) => {
+    const fromProject = from.params.projectId as string;
+    const toProject = to.params.projectId as string;
+    if (fromProject !== toProject) {
+      console.debug(
+        `[ProvideDashboardContext] project switched ${fromProject} -> ${toProject}`
+      );
+      isSwitchingProject.value = true;
+      fetchInstancesAndDatabases(toProject).finally(() => {
+        isSwitchingProject.value = false;
+
+        next();
+      });
+      return;
+    }
+
+    next();
+  });
 });
 </script>
