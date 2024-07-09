d5db65e66773fc1fae8c3d7935d2ca9af6682342
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-07-09 12:46:11 +0800

    chore(frontend): task run log extended (#12694)
    * chore: task run log extended
    
    * chore: time col
    
    * chore: details
    
    * chore: use point in time if duration is not recorded

--- frontend/src/components/IssueV1/components/TaskRunSection/TaskRunLogTable/common.ts
+++ frontend/src/components/IssueV1/components/TaskRunSection/TaskRunLogTable/common.ts
@@ -1,7 +1,12 @@
-import type {
-  TaskRunLogEntry_CommandExecute,
-  TaskRunLogEntry_SchemaDump,
+import { t } from "@/plugins/i18n";
+import {
   TaskRunLogEntry_Type,
+  taskRunLogEntry_TypeToJSON,
+  type TaskRunLogEntry_CommandExecute,
+  type TaskRunLogEntry_SchemaDump,
+  type TaskRunLogEntry_TaskRunStatusUpdate,
+  type TaskRunLogEntry_TransactionControl,
+  type TaskRunLogEntry_DatabaseSync,
 } from "@/types/proto/v1/rollout_service";
 
 export type FlattenLogEntry = {
@@ -16,4 +21,30 @@ export type FlattenLogEntry = {
     commandIndex: number;
     affectedRows?: number;
   };
+  taskRunStatusUpdate?: TaskRunLogEntry_TaskRunStatusUpdate;
+  transactionControl?: TaskRunLogEntry_TransactionControl;
+  databaseSync?: TaskRunLogEntry_DatabaseSync;
+};
+
+export const displayTaskRunLogEntryType = (type: TaskRunLogEntry_Type) => {
+  if (type === TaskRunLogEntry_Type.COMMAND_EXECUTE) {
+    return t("issue.task-run.task-run-log.entry-type.command-execute");
+  }
+  if (type === TaskRunLogEntry_Type.SCHEMA_DUMP) {
+    return t("issue.task-run.task-run-log.entry-type.schema-dump");
+  }
+  if (type === TaskRunLogEntry_Type.TASK_RUN_STATUS_UPDATE) {
+    return t("issue.task-run.task-run-log.entry-type.task-run-status-update");
+  }
+  if (type === TaskRunLogEntry_Type.TRANSACTION_CONTROL) {
+    return t("issue.task-run.task-run-log.entry-type.transaction-control");
+  }
+  if (type === TaskRunLogEntry_Type.DATABASE_SYNC) {
+    return t("issue.task-run.task-run-log.entry-type.database-sync");
+  }
+
+  console.warn(
+    `[displayTaskRunLogEntryType] should never reach this line: type=${taskRunLogEntry_TypeToJSON(type)}`
+  );
+  return "";
 };
