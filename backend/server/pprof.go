87acfd91faaf4a32c5557cb5ba97b2d9d089240e
p0ny <xz@bytebase.com>
2024-07-10 19:18:36 +0800

    chore: runtime pprof (#12740)
    Former-commit-id: 419f5d84aad7e71f032782ce070be3944d359bd9
    Former-commit-id: b2f87501bb201d748f88501bb207b85df83f3fdb

---
+++ backend/server/pprof.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"net/http"
+	"net/http/pprof"
+	"sync/atomic"
+
+	"github.com/labstack/echo/v4"
+)
+
+func registerPprof(e *echo.Echo, runtimeDebug *atomic.Bool) {
+	router := e.Group("/debug/pprof")
+	handler := func(h http.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if !runtimeDebug.Load() {
+				return echo.NewHTTPError(echo.ErrNotFound.Code, "")
+			}
+			h.ServeHTTP(c.Response().Writer, c.Request())
+			return nil
+		}
+	}
+	router.GET("/", handler(pprof.Index))
+	router.GET("/allocs", handler(pprof.Handler("allocs").ServeHTTP))
+	router.GET("/block", handler(pprof.Handler("block").ServeHTTP))
+	router.GET("/cmdline", handler(pprof.Cmdline))
+	router.GET("/goroutine", handler(pprof.Handler("goroutine").ServeHTTP))
+	router.GET("/heap", handler(pprof.Handler("heap").ServeHTTP))
+	router.GET("/mutex", handler(pprof.Handler("mutex").ServeHTTP))
+	router.GET("/profile", handler(pprof.Profile))
+	router.POST("/symbol", handler(pprof.Symbol))
+	router.GET("/symbol", handler(pprof.Symbol))
+	router.GET("/threadcreate", handler(pprof.Handler("threadcreate").ServeHTTP))
+	router.GET("/trace", handler(pprof.Trace))
+}
--- backend/server/routers.go
+++ backend/server/routers.go
@@ -8,7 +8,6 @@ import (
 
 	grpcruntime "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/improbable-eng/grpc-web/go/grpcweb"
-	"github.com/labstack/echo-contrib/pprof"
 	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -70,9 +69,9 @@ func configureEchoRouters(e *echo.Echo, grpcServer *grpc.Server, lspServer *lsp.
 			return context.JSON(http.StatusTooManyRequests, nil)
 		},
 	}))
-	if profile.Debug {
-		pprof.Register(e)
-	}
+
+	registerPprof(e, &profile.RuntimeDebug)
+
 	p := prometheus.NewPrometheus("api", nil)
 	p.Use(e)
 
