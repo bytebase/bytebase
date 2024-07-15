53adcfeb09b1645b65d1b21945306d52572f406d
boojack <stevenlgtm@gmail.com>
2024-07-15 11:54:18 +0800

    chore: add `BatchCancelPlanCheckRuns` endpoint definition (#12799)
    * chore: add `BatchCancelPlanCheckRuns` endpoint definition
    
    * chore: buf generate

--- proto/generated-go/v1/plan_service.pb.gw.go
+++ proto/generated-go/v1/plan_service.pb.gw.go
@@ -513,6 +513,66 @@ func local_request_PlanService_RunPlanChecks_0(ctx context.Context, marshaler ru
 
 }
 
+func request_PlanService_BatchCancelPlanCheckRuns_0(ctx context.Context, marshaler runtime.Marshaler, client PlanServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq BatchCancelPlanCheckRunsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["parent"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "parent")
+	}
+
+	protoReq.Parent, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "parent", err)
+	}
+
+	msg, err := client.BatchCancelPlanCheckRuns(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_PlanService_BatchCancelPlanCheckRuns_0(ctx context.Context, marshaler runtime.Marshaler, server PlanServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq BatchCancelPlanCheckRunsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["parent"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "parent")
+	}
+
+	protoReq.Parent, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "parent", err)
+	}
+
+	msg, err := server.BatchCancelPlanCheckRuns(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterPlanServiceHandlerServer registers the http handlers for service PlanService to "mux".
 // UnaryRPC     :call PlanServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -691,6 +751,31 @@ func RegisterPlanServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 		}
 
 		forward_PlanService_RunPlanChecks_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_PlanService_BatchCancelPlanCheckRuns_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/bytebase.v1.PlanService/BatchCancelPlanCheckRuns", runtime.WithHTTPPathPattern("/v1/{parent=projects/*/plans/*}/planCheckRuns:batchCancel"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_PlanService_BatchCancelPlanCheckRuns_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PlanService_BatchCancelPlanCheckRuns_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -889,6 +974,28 @@ func RegisterPlanServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux
 
 	})
 
+	mux.Handle("POST", pattern_PlanService_BatchCancelPlanCheckRuns_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/bytebase.v1.PlanService/BatchCancelPlanCheckRuns", runtime.WithHTTPPathPattern("/v1/{parent=projects/*/plans/*}/planCheckRuns:batchCancel"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_PlanService_BatchCancelPlanCheckRuns_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PlanService_BatchCancelPlanCheckRuns_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -906,6 +1013,8 @@ var (
 	pattern_PlanService_ListPlanCheckRuns_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 2, 2, 1, 0, 4, 4, 5, 3, 2, 4}, []string{"v1", "projects", "plans", "parent", "planCheckRuns"}, ""))
 
 	pattern_PlanService_RunPlanChecks_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 2, 2, 1, 0, 4, 4, 5, 3}, []string{"v1", "projects", "plans", "name"}, "runPlanChecks"))
+
+	pattern_PlanService_BatchCancelPlanCheckRuns_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 2, 2, 1, 0, 4, 4, 5, 3, 2, 4}, []string{"v1", "projects", "plans", "parent", "planCheckRuns"}, "batchCancel"))
 )
 
 var (
@@ -922,4 +1031,6 @@ var (
 	forward_PlanService_ListPlanCheckRuns_0 = runtime.ForwardResponseMessage
 
 	forward_PlanService_RunPlanChecks_0 = runtime.ForwardResponseMessage
+
+	forward_PlanService_BatchCancelPlanCheckRuns_0 = runtime.ForwardResponseMessage
 )
