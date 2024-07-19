56c0dd7e1e0dc21c67f7ccd900fca98a2f8617c4
ecmadao <wlec@outlook.com>
2024-07-19 14:01:59 +0800

    chore: optimize fetch workspace iam policy (#12873)
    * chore: not store the workspace member role
    
    * chore: update
    
    * chore: migrate workspace role to iam policy
    
    * Update backend/migrator/migration/prod/2.22/0000##workspace_iam_policy.sql
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: support iam in policy api
    
    * chore: update
    
    * chore: workspace service
    
    * fix: proto lint
    
    * fix: lint
    
    * refactor: deprecate roles in user store message
    
    * fix: buf format
    
    * refactor: remove roles in user api
    
    * chore: update
    
    * refactor: etag for iam policy
    
    * fix: lint
    
    * fix: lint
    
    * fix: fetch workspace iam policy
    
    * chore: optimize fetch workspace iam policy
    
    * fix: eslint
    
    * chore: update
    
    ---------
    
    Co-authored-by: Danny Xu <98006139+d-bytebase@users.noreply.github.com>

--- frontend/src/store/modules/auth.ts
+++ frontend/src/store/modules/auth.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
-import { isEqual } from "lodash-es";
 import { defineStore } from "pinia";
 import { computed } from "vue";
 import { authServiceClient } from "@/grpcweb";
 import type { SignupInfo, ActivateInfo } from "@/types";
 import { unknownUser } from "@/types";
-import type { ComposedUser } from "@/types";
 import type {
   LoginRequest,
   LoginResponse,
@@ -13,101 +11,103 @@ import type {
 import { UserType } from "@/types/proto/v1/auth_service";
 import { getIntCookie } from "@/utils";
 import { useUserStore } from ".";
-import { useWorkspaceV1Store } from "./v1/workspace";
 
-interface AuthState {
-  currentUser: ComposedUser;
-}
+export const useAuthStore = defineStore("auth_v1", () => {
+  const userStore = useUserStore();
 
-export const useAuthStore = defineStore("auth_v1", {
-  state: (): AuthState => ({
-    currentUser: unknownUser(),
-  }),
-  actions: {
-    isLoggedIn: () => {
-      return getIntCookie("user") != undefined;
-    },
-    async login(request: Partial<LoginRequest>) {
-      const {
-        data: { mfaTempToken },
-      } = await axios.post<LoginResponse>("/v1/auth/login", request);
-      if (mfaTempToken) {
-        return mfaTempToken;
-      }
+  const currentUser = computed(() => {
+    const userId = getIntCookie("user");
+    if (userId) {
+      return userStore.getUserById(`${userId}`) ?? unknownUser();
+    }
+    return unknownUser();
+  });
 
-      const userId = getIntCookie("user");
-      if (userId) {
-        await useWorkspaceV1Store().fetchIamPolicy();
-        const loggedInUser = await useUserStore().getOrFetchUserById(
-          String(userId)
-        );
-        this.currentUser = loggedInUser;
-      }
-    },
-    async signup(signupInfo: SignupInfo) {
-      await authServiceClient.createUser({
-        user: {
-          email: signupInfo.email,
-          title: signupInfo.name,
-          password: signupInfo.password,
-          userType: UserType.USER,
-        },
-      });
-      await this.login({
+  const isLoggedIn = () => {
+    return getIntCookie("user") != undefined;
+  };
+
+  const login = async (request: Partial<LoginRequest>) => {
+    const {
+      data: { mfaTempToken },
+    } = await axios.post<LoginResponse>("/v1/auth/login", request);
+    if (mfaTempToken) {
+      return mfaTempToken;
+    }
+
+    const userId = getIntCookie("user");
+    if (userId) {
+      await useUserStore().getOrFetchUserById(String(userId));
+    }
+  };
+
+  const signup = async (signupInfo: SignupInfo) => {
+    await authServiceClient.createUser({
+      user: {
         email: signupInfo.email,
+        title: signupInfo.name,
         password: signupInfo.password,
-        web: true,
-      });
-    },
-    async logout() {
-      const unknown = unknownUser();
-      try {
-        await axios.post("/v1/auth/logout");
-      } finally {
-        this.currentUser = unknown;
-      }
-      return unknown;
-    },
-    async activate(activateInfo: ActivateInfo) {
-      const activatedUser = (
-        await axios.post("/api/auth/activate", {
-          data: { type: "activateInfo", attributes: activateInfo },
-        })
-      ).data.data;
+        userType: UserType.USER,
+      },
+    });
+    await login({
+      email: signupInfo.email,
+      password: signupInfo.password,
+      web: true,
+    });
+  };
 
-      // Refresh the corresponding user.
-      await useWorkspaceV1Store().fetchIamPolicy();
-      const user = await useUserStore().getOrFetchUserById(
-        String(activatedUser.id)
+  const logout = async () => {
+    try {
+      await axios.post("/v1/auth/logout");
+    } catch {
+      // nothing
+    }
+  };
+
+  const activate = async (activateInfo: ActivateInfo) => {
+    const activatedUser = (
+      await axios.post("/api/auth/activate", {
+        data: { type: "activateInfo", attributes: activateInfo },
+      })
+    ).data.data;
+
+    // Refresh the corresponding user.
+    const user = await useUserStore().getOrFetchUserById(
+      String(activatedUser.id)
+    );
+    return user;
+  };
+
+  const restoreUser = async () => {
+    const userId = getIntCookie("user");
+    if (userId) {
+      await useUserStore().getOrFetchUserById(
+        String(userId),
+        true // silent
       );
-      this.currentUser = user;
-      return user;
-    },
-    async restoreUser() {
-      const userId = getIntCookie("user");
-      if (userId) {
-        await useWorkspaceV1Store().fetchIamPolicy();
-        const loggedInUser = await useUserStore().getOrFetchUserById(
-          String(userId),
-          true // silent
-        );
-        this.currentUser = loggedInUser;
-        return loggedInUser;
-      }
-      return unknownUser();
-    },
-    async refreshUserIfNeeded(name: string) {
-      if (name === this.currentUser.name) {
-        const refreshedUser = await useUserStore().fetchUser(
-          name,
-          true // silent
-        );
-        if (!isEqual(refreshedUser, this.currentUser)) {
-          this.currentUser = refreshedUser;
-        }
-      }
-    },
-  },
+    }
+  };
+
+  const refreshUserIfNeeded = async (name: string) => {
+    if (name === currentUser.value.name) {
+      await useUserStore().fetchUser(
+        name,
+        true // silent
+      );
+    }
+  };
+
+  return {
+    currentUser,
+    isLoggedIn,
+    login,
+    signup,
+    logout,
+    activate,
+    restoreUser,
+    refreshUserIfNeeded,
+  };
 });
 
 export const useCurrentUserV1 = () => {
