5c9092730edaad4033080507c2a40f181d740e1b
d-bytebase <d@bytebase.com>
2024-07-19 06:31:15 -0700

    refactor: move diff metadata

--- backend/api/v1/acl_iam.go
+++ backend/api/v1/acl_iam.go
@@ -112,9 +112,6 @@ func isSkippedMethod(fullMethod string) bool {
 		v1pb.BranchService_MergeBranch_FullMethodName,
 		v1pb.BranchService_RebaseBranch_FullMethodName:
 		return true
-	// no need to check.
-	case v1pb.BranchService_DiffMetadata_FullMethodName:
-		return true
 	// handled in the method because we need to consider changelist.Creator.
 	case
 		v1pb.ChangelistService_UpdateChangelist_FullMethodName,
