03ba5c52b30090d5cdaa6143eb13fa096be378ef
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-07-23 09:26:37 +0800

    chore: refactor some hide when embedded cases

--- frontend/src/components/IssueV1/components/StatementSection/SDLView/SDLView.vue
+++ frontend/src/components/IssueV1/components/StatementSection/SDLView/SDLView.vue
@@ -31,13 +31,12 @@
                 </template>
                 <div class="whitespace-nowrap">
                   <span>{{ $t("issue.sdl.left-schema-may-change") }}</span>
-                  <HideInStandaloneMode>
-                    <LearnMoreLink
-                      url="https://www.bytebase.com/docs/change-database/state-based-migration/#caveats?source=console"
-                      color="light"
-                      class="ml-1"
-                    />
-                  </HideInStandaloneMode>
+                  <LearnMoreLink
+                    url="https://www.bytebase.com/docs/change-database/state-based-migration/#caveats?source=console"
+                    color="light"
+                    class="ml-1"
+                    hide-when-embedded
+                  />
                 </div>
               </NTooltip>
             </div>
