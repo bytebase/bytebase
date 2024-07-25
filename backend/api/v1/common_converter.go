3e64fdd8f519fdc9d08c3eb13d8f876e01db828a
steven <stevenlgtm@gmail.com>
2024-07-25 11:32:32 +0800

    chore: update

--- backend/api/v1/common_converter.go
+++ backend/api/v1/common_converter.go
@@ -6,6 +6,10 @@ import (
 )
 
 func convertToPosition(position *storepb.Position) *v1pb.Position {
+	if position == nil {
+		return nil
+	}
+
 	return &v1pb.Position{
 		Line:   position.Line,
 		Column: position.Column,
