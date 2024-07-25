0adc1ae94890c7330904510a5bcb74ce57112a42
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-07-25 10:29:41 +0800

    refactor: query context (#12957)
    * refactor: query context
    
    * chore: update
    
    * chore: update
    
    * chore: update

--- backend/plugin/db/mssql/mssql.go
+++ backend/plugin/db/mssql/mssql.go
@@ -236,11 +236,6 @@ func execute(ctx context.Context, tx *sql.Tx, statement string) (int64, error) {
 
 // QueryConn queries a SQL statement in a given connection.
 func (driver *Driver) QueryConn(ctx context.Context, conn *sql.Conn, statement string, queryContext *db.QueryContext) ([]*v1pb.QueryResult, error) {
-	// MSSQL does not support transaction isolation level for read-only queries.
-	if queryContext != nil {
-		queryContext.ReadOnly = false
-	}
-
 	singleSQLs, err := tsqlparser.SplitSQL(statement)
 	if err != nil {
 		return nil, err
@@ -279,7 +274,7 @@ func (*Driver) querySingleSQL(ctx context.Context, conn *sql.Conn, singleSQL bas
 	}
 
 	startTime := time.Now()
-	result, err := util.Query(ctx, storepb.Engine_MSSQL, conn, statement, queryContext)
+	result, err := util.Query(ctx, storepb.Engine_MSSQL, conn, statement)
 	if err != nil {
 		return nil, err
 	}
