bd6de15ac2684b4516f43d0d316e190afdedb649
d-bytebase <d@bytebase.com>
2024-07-25 18:49:07 -0700

    chore: iam auth

--- backend/api/v1/acl.go
+++ backend/api/v1/acl.go
@@ -206,6 +206,29 @@ func (in *ACLInterceptor) doIAMPermissionCheck(ctx context.Context, fullMethod s
 	if authContext.AuthMethod == common.AuthMethodCustom {
 		return true, nil, nil
 	}
+	if authContext.AuthMethod == common.AuthMethodIAM {
+		// Handle GetProject() error status.
+		for _, resource := range authContext.Resources {
+			slog.Debug("IAM auth method", slog.String("method", fullMethod), slog.String("permission", authContext.Permission), slog.String("project", resource.ProjectID), slog.Bool("workspace", resource.Workspace))
+			if resource.Workspace {
+				ok, err := in.iamManager.CheckPermission(ctx, authContext.Permission, user)
+				if err != nil {
+					return false, nil, err
+				}
+				if !ok {
+					return false, nil, nil
+				}
+			} else {
+				ok, err := in.iamManager.CheckPermission(ctx, authContext.Permission, user, resource.ProjectID)
+				if err != nil {
+					return false, []string{resource.ProjectID}, err
+				}
+				if !ok {
+					return false, []string{resource.ProjectID}, nil
+				}
+			}
+		}
+	}
 
 	p := authContext.Permission
 	switch fullMethod {
@@ -270,9 +293,14 @@ func (in *ACLInterceptor) populateRawResources(ctx context.Context, authContext 
 		switch {
 		case strings.HasPrefix(resource.Name, "projects/"):
 			match := projectRegex.FindString(resource.Name)
-			if match != "" {
-				resource.Project = match
+			if match == "" {
+				return errors.Errorf("invalid project resource %q", resource.Name)
 			}
+			projectID, err := common.GetProjectID(resource.Name)
+			if err != nil {
+				return err
+			}
+			resource.ProjectID = projectID
 		case strings.HasPrefix(resource.Name, "instances/") && strings.Contains(resource.Name, "/databases/"):
 			match := databaseRegex.FindString(resource.Name)
 			if match != "" {
@@ -280,7 +308,7 @@ func (in *ACLInterceptor) populateRawResources(ctx context.Context, authContext 
 				if err != nil {
 					return errors.Wrapf(err, "failed to get database %q", match)
 				}
-				resource.Project = database.ProjectID
+				resource.ProjectID = database.ProjectID
 			}
 		}
 		authContext.Resources = append(authContext.Resources, resource)
