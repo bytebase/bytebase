34688009caf1cbd350b9748b0175ed47aa446617
h3n4l <oysterdays@gmail.com>
2024-07-30 11:45:36 +0800

    feat: prepend bytebase app name for mysql statement

--- backend/plugin/db/util/driverutil.go
+++ backend/plugin/db/util/driverutil.go
@@ -4,6 +4,7 @@ package util
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log/slog"
 	"strings"
 	"time"
@@ -65,39 +66,43 @@ func RunStatement(ctx context.Context, engineType storepb.Engine, conn *sql.Conn
 	var results []*v1pb.QueryResult
 	for _, singleSQL := range singleSQLs {
 		startTime := time.Now()
-		if mysqlparser.IsMySQLAffectedRowsStatement(singleSQL.Text) {
-			sqlResult, err := conn.ExecContext(ctx, singleSQL.Text)
-			if err != nil {
-				return nil, err
-			}
-			affectedRows, err := sqlResult.RowsAffected()
-			if err != nil {
-				slog.Info("rowsAffected returns error", log.BBError(err))
-			}
+		runningStatement := singleSQL.Text
+		if engineType == storepb.Engine_MYSQL {
+			runningStatement := MySQLPrependBytebaseAppComment(singleSQL.Text)
+			if mysqlparser.IsMySQLAffectedRowsStatement(singleSQL.Text) {
+				sqlResult, err := conn.ExecContext(ctx, runningStatement)
+				if err != nil {
+					return nil, err
+				}
+				affectedRows, err := sqlResult.RowsAffected()
+				if err != nil {
+					slog.Info("rowsAffected returns error", log.BBError(err))
+				}
 
-			field := []string{"Affected Rows"}
-			types := []string{"INT"}
-			rows := []*v1pb.QueryRow{
-				{
-					Values: []*v1pb.RowValue{
-						{
-							Kind: &v1pb.RowValue_Int64Value{
-								Int64Value: affectedRows,
+				field := []string{"Affected Rows"}
+				types := []string{"INT"}
+				rows := []*v1pb.QueryRow{
+					{
+						Values: []*v1pb.RowValue{
+							{
+								Kind: &v1pb.RowValue_Int64Value{
+									Int64Value: affectedRows,
+								},
 							},
 						},
 					},
-				},
+				}
+				results = append(results, &v1pb.QueryResult{
+					ColumnNames:     field,
+					ColumnTypeNames: types,
+					Rows:            rows,
+					Latency:         durationpb.New(time.Since(startTime)),
+					Statement:       singleSQL.Text,
+				})
+				continue
 			}
-			results = append(results, &v1pb.QueryResult{
-				ColumnNames:     field,
-				ColumnTypeNames: types,
-				Rows:            rows,
-				Latency:         durationpb.New(time.Since(startTime)),
-				Statement:       singleSQL.Text,
-			})
-			continue
 		}
-		results = append(results, adminQuery(ctx, engineType, conn, singleSQL.Text))
+		results = append(results, adminQuery(ctx, engineType, conn, runningStatement))
 	}
 
 	return results, nil
@@ -300,4 +305,8 @@ func mssqlMakeScanDestByTypeName(tn string) (dest any, wantByte bool) {
 // TrimStatement trims the unused characters from the statement for making getStatementWithResultLimit() happy.
 func TrimStatement(statement string) string {
 	return strings.TrimLeft(strings.TrimRight(statement, " \n\t;"), " \n\t")
+}
+
+func MySQLPrependBytebaseAppComment(statement string) string {
+	return fmt.Sprintf("/* ApplicationName=Bytebase */ %s", statement)
 }
