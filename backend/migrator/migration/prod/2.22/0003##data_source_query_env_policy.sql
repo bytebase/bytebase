47573ad0b37bf6c2c756de7099c111767d00ebb4
steven <stevenlgtm@gmail.com>
2024-07-31 17:50:19 +0800

    chore: update

--- backend/migrator/migration/prod/2.22/0003##data_source_query_env_policy.sql
+++ backend/migrator/migration/prod/2.22/0003##data_source_query_env_policy.sql
@@ -1,10 +1,7 @@
-INSERT INTO policy (row_status, creator_id, created_ts, updater_id, updated_ts, type, payload, resource_type, resource_id, inherit_from_parent)
+INSERT INTO policy (creator_id, updater_id, type, payload, resource_type, resource_id, inherit_from_parent)
 SELECT 
-  'NORMAL', 
   101, 
-  extract(epoch from now()), 
   101, 
-  extract(epoch from now()), 
   'bb.policy.data-source-query', 
   '{"adminDataSourceRestriction": "FALLBACK"}', 
   'ENVIRONMENT', 
