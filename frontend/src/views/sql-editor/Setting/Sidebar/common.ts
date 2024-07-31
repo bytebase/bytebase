5da32ee38288099453ee368adcf20c4b99c43b79
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-07-31 19:44:57 +0800

    chore: hide projects and environments setting

--- frontend/src/views/sql-editor/Setting/Sidebar/common.ts
+++ frontend/src/views/sql-editor/Setting/Sidebar/common.ts
@@ -1,18 +1,11 @@
-import {
-  BuildingIcon,
-  GalleryHorizontalEndIcon,
-  LayersIcon,
-  SquareStackIcon,
-} from "lucide-vue-next";
+import { BuildingIcon, LayersIcon } from "lucide-vue-next";
 import { computed, h } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRoute, type RouteRecordRaw } from "vue-router";
 import type { SidebarItem } from "@/components/CommonSidebar.vue";
 import sqlEditorRoutes, {
   SQL_EDITOR_SETTING_GENERAL_MODULE,
-  SQL_EDITOR_SETTING_ENVIRONMENT_MODULE,
   SQL_EDITOR_SETTING_INSTANCE_MODULE,
-  SQL_EDITOR_SETTING_PROJECT_MODULE,
 } from "@/router/sqlEditor";
 import { useCurrentUserV1, useAppFeature } from "@/store";
 import type { WorkspacePermission } from "@/types";
@@ -111,18 +104,6 @@ export const useSidebarItems = () => {
         title: t("common.instances"),
         icon: () => h(LayersIcon),
         name: SQL_EDITOR_SETTING_INSTANCE_MODULE,
-        type: "route",
-      },
-      {
-        title: t("common.projects"),
-        icon: () => h(GalleryHorizontalEndIcon),
-        name: SQL_EDITOR_SETTING_PROJECT_MODULE,
-        type: "route",
-      },
-      {
-        title: t("common.environments"),
-        icon: () => h(SquareStackIcon),
-        name: SQL_EDITOR_SETTING_ENVIRONMENT_MODULE,
         type: "route",
       },
     ];
