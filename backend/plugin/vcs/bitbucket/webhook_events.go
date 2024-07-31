61590ae2261c85c4a21792ebd6174b3a795b702e
ecmadao <wlec@outlook.com>
2024-07-31 15:32:32 +0800

    chore: gitops sql review support azure (#13050)
    * chore: prepare for sql review ci
    
    * chore: use enum
    
    * chore: support upsert comment
    
    * chore: comment for github pr
    
    * chore: optimize sql review comment content
    
    * chore: add file url
    
    * chore: update
    
    * chore: support azure
    
    Former-commit-id: e1cd778a846c503ce6c9431fe2980b1cf018ce44
    Former-commit-id: ffe6c402a917711debde748f5a943d999d18bb3a

--- backend/plugin/vcs/bitbucket/webhook_events.go
+++ backend/plugin/vcs/bitbucket/webhook_events.go
@@ -1,6 +1,18 @@
 package bitbucket
 
 // Header X-Event-Key: pullrequest:created, pullrequest:updated, pullrequest:fulfilled.
+//
+// https://support.atlassian.com/bitbucket-cloud/docs/event-payloads
+type PullRequestEventType string
+
+const (
+	// A pull request is created in a Git repository.
+	PullRequestEventCreated PullRequestEventType = "pullrequest:created"
+	// A pull request is updated.
+	PullRequestEventUpdated PullRequestEventType = "pullrequest:updated"
+	// A pull request is merged.
+	PullRequestEventFulfilled PullRequestEventType = "pullrequest:fulfilled"
+)
 
 // PullRequestPushEvent is the json message for pull request push event.
 type PullRequestPushEvent struct {
