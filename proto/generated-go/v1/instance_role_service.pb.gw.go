407ccc27d83e83d83955649261b1af7cd3b7db86
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-08-01 11:05:11 +0800

    chore: issue comment iam (#13077)
    * chore: explain 10 times
    
    * chore: update
    
    * chore: issue comment IAM
    
    Former-commit-id: fad6532b8d5cf8a0397b9eac551f5becc3789fc9
    Former-commit-id: 344b13ee6a0e41533a6608af3213b62fda4d2e8c

--- proto/generated-go/v1/instance_role_service.pb.gw.go
+++ proto/generated-go/v1/instance_role_service.pb.gw.go
@@ -157,6 +157,7 @@ func local_request_InstanceRoleService_ListInstanceRoles_0(ctx context.Context, 
 // UnaryRPC     :call InstanceRoleServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterInstanceRoleServiceHandlerFromEndpoint instead.
+// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterInstanceRoleServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server InstanceRoleServiceServer) error {
 
 	mux.Handle("GET", pattern_InstanceRoleService_GetInstanceRole_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -247,7 +248,7 @@ func RegisterInstanceRoleServiceHandler(ctx context.Context, mux *runtime.ServeM
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "InstanceRoleServiceClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "InstanceRoleServiceClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "InstanceRoleServiceClient" to call the correct interceptors.
+// "InstanceRoleServiceClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterInstanceRoleServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client InstanceRoleServiceClient) error {
 
 	mux.Handle("GET", pattern_InstanceRoleService_GetInstanceRole_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
--- proto/generated-go/v1/risk_service.pb.gw.go
+++ proto/generated-go/v1/risk_service.pb.gw.go
@@ -249,6 +249,7 @@ func local_request_RiskService_DeleteRisk_0(ctx context.Context, marshaler runti
 // UnaryRPC     :call RiskServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterRiskServiceHandlerFromEndpoint instead.
+// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterRiskServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server RiskServiceServer) error {
 
 	mux.Handle("GET", pattern_RiskService_ListRisks_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -389,7 +390,7 @@ func RegisterRiskServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "RiskServiceClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "RiskServiceClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "RiskServiceClient" to call the correct interceptors.
+// "RiskServiceClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterRiskServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client RiskServiceClient) error {
 
 	mux.Handle("GET", pattern_RiskService_ListRisks_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
