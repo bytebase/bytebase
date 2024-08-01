2fc2c0ec8bc1053a491c72385a7bdd576f558bef
boojack <stevenlgtm@gmail.com>
2024-08-01 12:22:48 +0800

    chore: add permission checks for issue subscribe section (#13082)
    * chore: add permission checks for issue subscribe section
    
    * chore: update

--- frontend/src/components/IssueV1/components/IssueCommentSection/Subscribers/Subscribers.vue
+++ frontend/src/components/IssueV1/components/IssueCommentSection/Subscribers/Subscribers.vue
@@ -1,11 +1,39 @@
 <template>
   <div class="flex items-center gap-x-2">
-    <SubscribeButton />
-    <SubscriberList />
+    <SubscribeButton v-if="allowSubscribe" />
+    <SubscriberList :readonly="!allowSubscribe" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
+import { useIssueContext } from "@/components/IssueV1/logic";
+import { useCurrentUserV1 } from "@/store";
+import { extractUserResourceName, hasProjectPermissionV2 } from "@/utils";
 import SubscribeButton from "./SubscribeButton.vue";
 import SubscriberList from "./SubscriberList.vue";
+
+const { issue } = useIssueContext();
+const currentUser = useCurrentUserV1();
+
+const allowSubscribe = computed(() => {
+  if (
+    extractUserResourceName(issue.value.creator) === currentUser.value.email
+  ) {
+    // Allowed if current user is the creator.
+    return true;
+  }
+
+  if (
+    hasProjectPermissionV2(
+      issue.value.projectEntity,
+      currentUser.value,
+      "bb.issues.update"
+    )
+  ) {
+    return true;
+  }
+
+  return false;
+});
 </script>
