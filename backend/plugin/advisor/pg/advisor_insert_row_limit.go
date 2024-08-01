e5a676cdc6d2fc2f61eede23f061ad2577a23480
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-08-01 19:27:53 +0800

    chore: explain burnout (#13107)
    Former-commit-id: f4de619d97151acd56e6753bd444d21031268a3e

--- backend/plugin/advisor/pg/advisor_insert_row_limit.go
+++ backend/plugin/advisor/pg/advisor_insert_row_limit.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/pkg/errors"
 
+	"github.com/bytebase/bytebase/backend/common"
 	"github.com/bytebase/bytebase/backend/plugin/advisor"
 	"github.com/bytebase/bytebase/backend/plugin/parser/sql/ast"
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
@@ -55,6 +56,9 @@ func (*InsertRowLimitAdvisor) Check(ctx advisor.Context, _ string) ([]*storepb.A
 			checker.text = stmt.Text()
 			checker.line = stmt.LastLine()
 			ast.Walk(checker, stmt)
+			if checker.explainCount >= common.MaximumLintExplainSize {
+				break
+			}
 		}
 	}
 
@@ -62,14 +66,15 @@ func (*InsertRowLimitAdvisor) Check(ctx advisor.Context, _ string) ([]*storepb.A
 }
 
 type insertRowLimitChecker struct {
-	adviceList []*storepb.Advice
-	level      storepb.Advice_Status
-	title      string
-	text       string
-	line       int
-	maxRow     int
-	driver     *sql.DB
-	ctx        context.Context
+	adviceList   []*storepb.Advice
+	level        storepb.Advice_Status
+	title        string
+	text         string
+	line         int
+	maxRow       int
+	driver       *sql.DB
+	ctx          context.Context
+	explainCount int
 }
 
 // Visit implements the ast.Visitor interface.
@@ -87,6 +92,7 @@ func (checker *insertRowLimitChecker) Visit(node ast.Node) ast.Visitor {
 			}
 		} else if checker.driver != nil {
 			// For INSERT INTO ... SELECT statements, use EXPLAIN.
+			checker.explainCount++
 			res, err := advisor.Query(checker.ctx, checker.driver, storepb.Engine_POSTGRES, fmt.Sprintf("EXPLAIN %s", node.Text()))
 			if err != nil {
 				checker.adviceList = append(checker.adviceList, &storepb.Advice{
