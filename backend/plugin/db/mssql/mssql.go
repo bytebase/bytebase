bb492119e6cf922c6e490c35cec2a8410a552a80
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-08-02 13:32:30 +0800

    chore: one mssql query (#13122)

--- backend/plugin/db/mssql/mssql.go
+++ backend/plugin/db/mssql/mssql.go
@@ -235,7 +235,7 @@ func execute(ctx context.Context, tx *sql.Tx, statement string) (int64, error) {
 }
 
 // QueryConn queries a SQL statement in a given connection.
-func (driver *Driver) QueryConn(ctx context.Context, conn *sql.Conn, statement string, queryContext *db.QueryContext) ([]*v1pb.QueryResult, error) {
+func (*Driver) QueryConn(ctx context.Context, conn *sql.Conn, statement string, queryContext *db.QueryContext) ([]*v1pb.QueryResult, error) {
 	singleSQLs, err := tsqlparser.SplitSQL(statement)
 	if err != nil {
 		return nil, err
@@ -247,57 +247,70 @@ func (driver *Driver) QueryConn(ctx context.Context, conn *sql.Conn, statement s
 
 	var results []*v1pb.QueryResult
 	for _, singleSQL := range singleSQLs {
-		result, err := driver.querySingleSQL(ctx, conn, singleSQL, queryContext)
-		if err != nil {
-			results = append(results, &v1pb.QueryResult{
-				Error: err.Error(),
-			})
-		} else {
-			results = append(results, result)
+		statement := singleSQL.Text
+		if queryContext != nil && queryContext.Explain {
+			statement = fmt.Sprintf("EXPLAIN %s", statement)
+		} else if queryContext != nil && queryContext.Limit > 0 {
+			stmt, err := getMSSQLStatementWithResultLimit(statement, queryContext.Limit)
+			if err != nil {
+				slog.Error("fail to add limit clause", "statement", statement, log.BBError(err))
+				stmt = fmt.Sprintf("WITH result AS (%s) SELECT TOP %d * FROM result;", util.TrimStatement(stmt), queryContext.Limit)
+			}
+			statement = stmt
 		}
+
+		_, allQuery, err := base.ValidateSQLForEditor(storepb.Engine_MSSQL, statement)
+		if err != nil {
+			return nil, err
+		}
+		startTime := time.Now()
+		var queryResult *v1pb.QueryResult
+		if allQuery {
+			r, err := func() (*v1pb.QueryResult, error) {
+				rows, err := conn.QueryContext(ctx, statement)
+				if err != nil {
+					return nil, util.FormatErrorWithQuery(err, statement)
+				}
+				defer rows.Close()
+				r, err := util.RowsToQueryResult(storepb.Engine_MSSQL, rows)
+				if err != nil {
+					return nil, err
+				}
+				if err := rows.Err(); err != nil {
+					return nil, err
+				}
+				return r, nil
+			}()
+			if err != nil {
+				queryResult = &v1pb.QueryResult{
+					Error: err.Error(),
+				}
+			} else {
+				queryResult = r
+			}
+		} else {
+			sqlResult, err := conn.ExecContext(ctx, statement)
+			if err != nil {
+				return nil, err
+			}
+			affectedRows, err := sqlResult.RowsAffected()
+			if err != nil {
+				slog.Info("rowsAffected returns error", log.BBError(err))
+			}
+
+			queryResult = util.BuildAffectedRowsResult(affectedRows)
+		}
+		queryResult.Statement = singleSQL.Text
+		queryResult.Latency = durationpb.New(time.Since(startTime))
+		results = append(results, queryResult)
 	}
 
 	return results, nil
 }
 
-func (*Driver) querySingleSQL(ctx context.Context, conn *sql.Conn, singleSQL base.SingleSQL, queryContext *db.QueryContext) (*v1pb.QueryResult, error) {
-	statement := singleSQL.Text
-	if queryContext != nil && queryContext.Explain {
-		statement = fmt.Sprintf("EXPLAIN %s", statement)
-	} else if queryContext != nil && queryContext.Limit > 0 {
-		stmt, err := getMSSQLStatementWithResultLimit(statement, queryContext.Limit)
-		if err != nil {
-			slog.Error("fail to add limit clause", "statement", statement, log.BBError(err))
-			stmt = fmt.Sprintf("WITH result AS (%s) SELECT TOP %d * FROM result;", util.TrimStatement(stmt), queryContext.Limit)
-		}
-		statement = stmt
-	}
-
-	startTime := time.Now()
-	rows, err := conn.QueryContext(ctx, statement)
-	if err != nil {
-		return nil, util.FormatErrorWithQuery(err, statement)
-	}
-	defer rows.Close()
-
-	result, err := util.RowsToQueryResult(storepb.Engine_MSSQL, rows)
-	if err != nil {
-		// nolint
-		return &v1pb.QueryResult{
-			Error: err.Error(),
-		}, nil
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	result.Latency = durationpb.New(time.Since(startTime))
-	result.Statement = statement
-	return result, nil
-}
-
 // RunStatement runs a SQL statement.
-func (*Driver) RunStatement(ctx context.Context, conn *sql.Conn, statement string) ([]*v1pb.QueryResult, error) {
-	return util.RunStatement(ctx, storepb.Engine_MSSQL, conn, statement)
+func (driver *Driver) RunStatement(ctx context.Context, conn *sql.Conn, statement string) ([]*v1pb.QueryResult, error) {
+	return driver.QueryConn(ctx, conn, statement, nil)
 }
 
 func NewBatch(statement string) *tsqlbatch.Batch {
