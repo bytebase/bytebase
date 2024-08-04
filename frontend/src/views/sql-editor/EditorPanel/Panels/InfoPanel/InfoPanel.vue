b1d43272d10c9f037868c6148ccff0cacfeab4d0
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-08-04 12:29:37 +0800

    chore: info panel

--- frontend/src/views/sql-editor/EditorPanel/Panels/InfoPanel/InfoPanel.vue
+++ frontend/src/views/sql-editor/EditorPanel/Panels/InfoPanel/InfoPanel.vue
@@ -1,1 +1,12 @@
-<template>InfoPanel</template>
+<template>
+  <div class="p-2">
+    <DatabaseOverviewInfo :database="database" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DatabaseOverviewInfo from "@/components/Database/DatabaseOverviewInfo.vue";
+import { useConnectionOfCurrentSQLEditorTab } from "@/store";
+
+const { database } = useConnectionOfCurrentSQLEditorTab();
+</script>
