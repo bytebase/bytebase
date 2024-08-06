379305bc7170344d8a33b012e3c6948312351f57
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-08-06 17:50:49 +0800

    chore: hide environment tag when not needed (#13211)
    Former-commit-id: 3322b7371f6979cefe9b37d0ffa7916f83d50d05

--- frontend/src/views/sql-editor/ConnectionPanel/ConnectionPane/TreeNode/DatabaseNode.vue
+++ frontend/src/views/sql-editor/ConnectionPanel/ConnectionPane/TreeNode/DatabaseNode.vue
@@ -6,10 +6,7 @@
     />
 
     <EnvironmentV1Name
-      v-if="
-        !hasEnvironmentContext ||
-        database.effectiveEnvironment !== database.instanceResource.environment
-      "
+      v-if="showEnvironment"
       :environment="database.effectiveEnvironmentEntity"
       :link="false"
       class="text-control-light"
@@ -59,6 +56,9 @@ const me = useCurrentUserV1();
 const disallowRequestQuery = useAppFeature(
   "bb.feature.sql-editor.disallow-request-query"
 );
+const hideEnvironments = useAppFeature(
+  "bb.feature.sql-editor.hide-environments"
+);
 
 const database = computed(
   () => (props.node as TreeNode<"database">).meta.target
@@ -74,5 +74,24 @@ const hasInstanceContext = computed(() => {
 
 const hasEnvironmentContext = computed(() => {
   return props.factors.includes("environment");
+});
+
+const showEnvironment = computed(() => {
+  // Don't show environment tag anyway if disabled via appFeature
+  if (hideEnvironments.value) {
+    return false;
+  }
+  // If we don't have "environment" factor in the custom tree structure
+  // we should indicate the database's environment
+  if (!hasEnvironmentContext.value) {
+    return true;
+  }
+  // If we have "environment" factor in the custom tree structure
+  // only show the environment tag when a database's effectiveEnvironment is
+  // not equal to it's physical instance's environment
+  return (
+    database.value.effectiveEnvironment !==
+    database.value.instanceResource.environment
+  );
 });
 </script>
