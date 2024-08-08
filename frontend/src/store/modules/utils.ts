fddfd582009a53cb7b111665575a923ddb63f6eb
ecmadao <wlec@outlook.com>
2024-08-08 23:33:16 +0800

    chore: custom roles can be deleted only when no policy references them (#13248)
    * chore: custom roles can be deleted only when no policy references them
    
    * chore: update
    
    * chore: use sql
    
    Former-commit-id: 98581a0a4005fec56517eded6c3b3fff960cd593
    Former-commit-id: 2c4607ccff896b2df118fba9afae9eb9fdfe3fa5

--- frontend/src/store/modules/utils.ts
+++ frontend/src/store/modules/utils.ts
@@ -1,24 +1,13 @@
-import { ClientError } from "nice-grpc-web";
-import { t } from "@/plugins/i18n";
-import { extractGrpcErrorMessage } from "@/utils/grpcweb";
-import { pushNotification } from "./notification";
-
 export const useGracefulRequest = async <T>(
-  fn: () => Promise<T>
-): Promise<T> => {
+  fn: () => Promise<T>,
+  errorHandler: (err: unknown) => void = (err) => {
+    throw err;
+  }
+): Promise<T | void> => {
   try {
     const result = await fn();
     return result;
   } catch (err) {
-    const description = extractGrpcErrorMessage(err);
-    if (err instanceof ClientError) {
-      pushNotification({
-        module: "bytebase",
-        style: "CRITICAL",
-        title: t("common.error"),
-        description,
-      });
-    }
-    throw err;
+    return errorHandler(err);
   }
 };
