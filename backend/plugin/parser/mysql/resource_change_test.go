9e019cf8d7ca9ce7c7ed58db7e326f421d7619fc
d-bytebase <d@bytebase.com>
2024-08-08 21:36:36 -0700

    chore: record dmls

---
+++ backend/plugin/parser/mysql/resource_change_test.go
@@ -0,0 +1,38 @@
+package mysql
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+)
+
+func TestExtractMySQLChangedResources(t *testing.T) {
+	statement := `CREATE TABLE t1 (c1 INT);
+	DROP TABLE t1;
+	ALTER TABLE t1 ADD COLUMN c1 INT;
+	RENAME TABLE t1 TO t2;
+	INSERT INTO t1 (c1) VALUES (1);
+	`
+	want := &base.ChangeSummary{
+		Resources: []base.SchemaResource{
+			{
+				Database: "db",
+				Table:    "t1",
+			},
+			{
+				Database: "db",
+				Table:    "t2",
+			},
+		},
+		DMLs: []string{
+			"INSERT INTO t1 (c1) VALUES (1);",
+		},
+	}
+
+	asts, _ := ParseMySQL(statement)
+	got, err := extractChangedResources("db", "", asts)
+	require.NoError(t, err)
+	require.Equal(t, want, got)
+}
