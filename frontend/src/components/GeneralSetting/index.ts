972295f78296b960559f211d5906d46d64e7c569
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-08-09 18:50:04 +0800

    chore: also add max sql result size setting to SQLEditorSettingPage (#13267)
    Former-commit-id: e8aad789d85e88c31366e9785965dc66c265c2c8

--- frontend/src/components/GeneralSetting/index.ts
+++ frontend/src/components/GeneralSetting/index.ts
@@ -2,12 +2,14 @@ import AIAugmentationSetting from "./AIAugmentationSetting.vue";
 import AnnouncementSetting from "./AnnouncementSetting.vue";
 import BrandingSetting from "./BrandingSetting.vue";
 import DatabaseChangeModeSetting from "./DatabaseChangeModeSetting.vue";
+import MaximumSQLResultSizeSetting from "./MaximumSQLResultSizeSetting.vue";
 import NetworkSetting from "./NetworkSetting.vue";
 import SecuritySetting from "./SecuritySetting.vue";
 
 export {
   BrandingSetting,
   SecuritySetting,
+  MaximumSQLResultSizeSetting,
   NetworkSetting,
   AIAugmentationSetting,
   AnnouncementSetting,
--- frontend/src/views/sql-editor/Setting/General/General.vue
+++ frontend/src/views/sql-editor/Setting/General/General.vue
@@ -1,9 +1,25 @@
 <template>
   <div
-    class="w-full flex flex-col gap-4 py-4 px-4 divide-y divide-block-border overflow-y-auto"
+    class="w-full flex flex-col py-4 px-4 divide-y divide-block-border overflow-y-auto"
   >
-    <DatabaseChangeModeSetting :allow-edit="allowEdit" />
-    <AIAugmentationSetting :allow-edit="allowEdit" />
+    <DatabaseChangeModeSetting :allow-edit="allowEdit" class="pb-0" />
+
+    <div class="pt-6 pb-0 lg:flex">
+      <div class="text-left lg:w-1/4">
+        <div class="flex items-center space-x-2">
+          <h1 class="text-2xl font-bold">
+            {{ $t("settings.general.workspace.security") }}
+          </h1>
+        </div>
+      </div>
+      <div class="flex-1 lg:px-4 gap">
+        <MaximumSQLResultSizeSetting
+          :allow-edit="allowEdit"
+          :show-update-button="true"
+        />
+      </div>
+    </div>
+    <AIAugmentationSetting :allow-edit="allowEdit" class="pb-0" />
   </div>
 </template>
 
@@ -12,6 +28,7 @@ import { computed } from "vue";
 import {
   AIAugmentationSetting,
   DatabaseChangeModeSetting,
+  MaximumSQLResultSizeSetting,
 } from "@/components/GeneralSetting";
 import { useCurrentUserV1 } from "@/store";
 import { hasWorkspacePermissionV2 } from "@/utils";
