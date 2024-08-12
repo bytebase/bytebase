51b97f2c423ff23654fb250d9f64ed75ee35fc4c
rebelice <yangrebelice@gmail.com>
2024-08-12 18:36:35 +0800

    chore(restore): support MySQL generated column (#13314)
    Former-commit-id: 0d70bc943173cafe357b2359c885022f4cd2b011
    Former-commit-id: f33564889cade0c7f0faf0c44655ee76ba4f036c

--- backend/plugin/parser/base/interface.go
+++ backend/plugin/parser/base/interface.go
@@ -39,7 +39,7 @@ type GetQuerySpanFunc func(ctx context.Context, gCtx GetQuerySpanContext, statem
 // TransformDMLToSelectFunc is the interface of transforming DML statements to SELECT statements.
 type TransformDMLToSelectFunc func(ctx context.Context, tCtx TransformContext, statement string, sourceDatabase string, targetDatabase string, tablePrefix string) ([]BackupStatement, error)
 
-type GenerateRestoreSQLFunc func(statement string, backupDatabase string, backupTable string, originalDatabase string, originalTable string) (string, error)
+type GenerateRestoreSQLFunc func(ctx context.Context, rCtx RestoreContext, statement string, backupDatabase string, backupTable string, originalDatabase string, originalTable string) (string, error)
 
 func RegisterQueryValidator(engine storepb.Engine, f ValidateSQLForEditorFunc) {
 	mux.Lock()
@@ -223,12 +223,12 @@ func RegisterGenerateRestoreSQL(engine storepb.Engine, f GenerateRestoreSQLFunc)
 	generateRestoreSQL[engine] = f
 }
 
-func GenerateRestoreSQL(engine storepb.Engine, statement string, backupDatabase string, backupTable string, originalDatabase string, originalTable string) (string, error) {
+func GenerateRestoreSQL(ctx context.Context, engine storepb.Engine, rCtx RestoreContext, statement string, backupDatabase string, backupTable string, originalDatabase string, originalTable string) (string, error) {
 	f, ok := generateRestoreSQL[engine]
 	if !ok {
 		return "", errors.Errorf("engine %s is not supported", engine)
 	}
-	return f(statement, backupDatabase, backupTable, originalDatabase, originalTable)
+	return f(ctx, rCtx, statement, backupDatabase, backupTable, originalDatabase, originalTable)
 }
 
 type ChangeSummary struct {
