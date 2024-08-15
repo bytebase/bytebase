4d0b619e62ddfcbfbf766285ac8ca411ee47efd8
Azusain <azusaings@gmail.com>
2024-08-15 11:35:49 +0800

    chore(databricks): remove password authentication from backend and api (#13375)
    Former-commit-id: 434d3913f8e6eb1a5bfb055c9742897f9090fd5a
    Former-commit-id: 653c7161f8b7d33961495bcf6deb061f64690bac

--- backend/component/dbfactory/dbfactory.go
+++ backend/component/dbfactory/dbfactory.go
@@ -233,7 +233,6 @@ func (d *DBFactory) GetDataSourceDriver(ctx context.Context, instance *store.Ins
 			ReplicaSet:               dataSource.ReplicaSet,
 			DirectConnection:         dataSource.DirectConnection,
 			Region:                   dataSource.Region,
-			AccountID:                dataSource.AccountID,
 			WarehouseID:              dataSource.WarehouseID,
 			RedisType:                dataSource.RedisType,
 			MasterName:               dataSource.MasterName,
