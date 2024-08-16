c4e66e6caf39189f0f47c76f313ee20b468025c2
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-08-16 15:33:04 +0800

    chore: style

--- frontend/src/views/sql-editor/EditorPanel/Panels/InfoPanel/InfoPanel.vue
+++ frontend/src/views/sql-editor/EditorPanel/Panels/InfoPanel/InfoPanel.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="px-2 py-2 gap-4 h-full overflow-hidden flex flex-col">
-    <div class="grid gap-2 items-center text-sm" style="grid-template-columns: max-content max-content">
+    <div
+      class="grid gap-2 items-center text-sm"
+      style="grid-template-columns: max-content max-content"
+    >
       <div class="flex items-center gap-x-1">
         <DatabaseIcon class="w-4 h-4" />
         <span>{{ $t("common.database") }}:</span>
@@ -25,7 +28,7 @@
 
       <div class="flex flex-col gap-2">
         <h2 class="text-lg">{{ $t("db.tables") }}</h2>
-        <div class="max-h-[20rem] overflow-x-auto overflow-y-hidden">
+        <div class="max-h-[16rem] overflow-x-auto overflow-y-hidden">
           <TablesTable
             v-if="metadata.schema"
             :db="database"
@@ -46,7 +49,7 @@
 
       <div class="flex flex-col gap-2">
         <h2 class="text-lg">{{ $t("db.views") }}</h2>
-        <div class="max-h-[20rem] overflow-x-auto overflow-y-hidden">
+        <div class="max-h-[16rem] overflow-x-auto overflow-y-hidden">
           <ViewsTable
             v-if="metadata.schema"
             :db="database"
@@ -67,7 +70,7 @@
 
       <div class="flex flex-col gap-2">
         <h2 class="text-lg">{{ $t("db.functions") }}</h2>
-        <div class="max-h-[20rem] overflow-x-auto overflow-y-hidden">
+        <div class="max-h-[16rem] overflow-x-auto overflow-y-hidden">
           <FunctionsTable
             v-if="metadata.schema"
             :db="database"
@@ -88,7 +91,7 @@
 
       <div class="flex flex-col gap-2">
         <h2 class="text-lg">{{ $t("db.procedures") }}</h2>
-        <div class="max-h-[20rem] overflow-x-auto overflow-y-hidden">
+        <div class="max-h-[16rem] overflow-x-auto overflow-y-hidden">
           <ProceduresTable
             v-if="metadata.schema"
             :db="database"
@@ -112,7 +115,7 @@
         class="flex flex-col gap-2"
       >
         <h2 class="text-lg">{{ $t("db.external-tables") }}</h2>
-        <div class="max-h-[20rem] overflow-x-auto overflow-y-hidden">
+        <div class="max-h-[16rem] overflow-x-auto overflow-y-hidden">
           <ExternalTablesTable
             v-if="metadata.schema"
             :db="database"
@@ -152,7 +155,6 @@ import FunctionsTable from "../FunctionsPanel/FunctionsTable.vue";
 import ProceduresTable from "../ProceduresPanel/ProceduresTable.vue";
 import TablesTable from "../TablesPanel/TablesTable.vue";
 import ViewsTable from "../ViewsPanel/ViewsTable.vue";
-import { SchemaSelectToolbar } from "../common";
 
 const { instance, database } = useConnectionOfCurrentSQLEditorTab();
 const { viewState, updateViewState, selectedSchemaName } =
