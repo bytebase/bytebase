0222b0046abd65b2ca40015e38ac10816f192cea
h3n4l <oysterdays@gmail.com>
2024-08-18 20:33:49 +0800

    fix: split multi add column spec into different statement in TiDB (#13416)
    * fix: char(1) canonical
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    ---------
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    Former-commit-id: d5d60635a951e6e7ac971e2d43e6ba22f91f80f9

--- backend/plugin/parser/tidb/differ_column_test.go
+++ backend/plugin/parser/tidb/differ_column_test.go
@@ -10,12 +10,12 @@ func TestColumnExist(t *testing.T) {
 		{
 			old:  `CREATE TABLE book(id INT, PRIMARY KEY(id));`,
 			new:  `CREATE TABLE book(id INT, price INT, PRIMARY KEY(id));`,
-			want: "ALTER TABLE `book` ADD COLUMN `price` INT AFTER `id`;\n\n",
+			want: "ALTER TABLE `book` ADD COLUMN `price` INT AFTER `id`;\n",
 		},
 		{
 			old:  `CREATE TABLE book(id INT, PRIMARY KEY(id))`,
 			new:  `CREATE TABLE book(id INT, price INT, code VARCHAR(50), PRIMARY KEY(id));`,
-			want: "ALTER TABLE `book` ADD COLUMN `price` INT AFTER `id`, ADD COLUMN `code` VARCHAR(50) AFTER `price`;\n\n",
+			want: "ALTER TABLE `book` ADD COLUMN `price` INT AFTER `id`;\nALTER TABLE `book` ADD COLUMN `code` VARCHAR(50) AFTER `price`;\n",
 		},
 		{
 			old: ``,
@@ -50,13 +50,13 @@ func TestColumnType(t *testing.T) {
 		{
 			old:  `CREATE TABLE book(id INT);`,
 			new:  `CREATE TABLE book(id VARCHAR(50));`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `id` VARCHAR(50);\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `id` VARCHAR(50);\n",
 		},
 
 		{
 			old:  `CREATE TABLE book(id INT, isbn VARCHAR(50));`,
 			new:  `CREATE TABLE book(id VARCHAR(50), isbn VARCHAR(100));`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `id` VARCHAR(50), MODIFY COLUMN `isbn` VARCHAR(100);\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `id` VARCHAR(50);\nALTER TABLE `book` MODIFY COLUMN `isbn` VARCHAR(100);\n",
 		},
 		{
 			old:  `CREATE TABLE book(id INT, isbn VARCHAR(50));`,
@@ -72,28 +72,28 @@ func TestColumnOption(t *testing.T) {
 		{
 			old:  `CREATE TABLE book(id INT COLUMN_FORMAT FIXED, PRIMARY KEY(id));`,
 			new:  `CREATE TABLE book(id INT COLUMN_FORMAT DYNAMIC, PRIMARY KEY(id));`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT COLUMN_FORMAT DYNAMIC;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT COLUMN_FORMAT DYNAMIC;\n",
 		},
 		// NULL option not match.
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50));`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50);\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50);\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) NOT NULL DEFAULT 'Harry Potter');`,
 			new:  `CREATE TABLE book(name VARCHAR(50) NULL DEFAULT 'Harry Potter');`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) NULL DEFAULT 'Harry Potter';\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) NULL DEFAULT 'Harry Potter';\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) NOT NULL DEFAULT 'Harry Potter');`,
 			new:  `CREATE TABLE book(name VARCHAR(50) DEFAULT 'Harry Potter');`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) DEFAULT 'Harry Potter';\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) DEFAULT 'Harry Potter';\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) NULL);`,
@@ -124,22 +124,22 @@ func TestColumnComment(t *testing.T) {
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) COMMENT 'Author Name' NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) COMMENT 'Book Name' NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COMMENT 'Book Name' NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COMMENT 'Book Name' NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) COMMENT 'Author Name' NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) COMMENT 'AUTHOR NAME' NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COMMENT 'AUTHOR NAME' NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COMMENT 'AUTHOR NAME' NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) COMMENT 'Book Name' NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COMMENT 'Book Name' NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COMMENT 'Book Name' NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) COMMENT 'Book Name' NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) NOT NULL);`,
@@ -160,17 +160,17 @@ func TestColumnDefaultValue(t *testing.T) {
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) DEFAULT 'Harry Potter' NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) DEFAULT 'Harry Potter' NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) DEFAULT 'Harry Potter' NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) DEFAULT 'Harry Potter' NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) DEFAULT 'Holmes' NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) DEFAULT 'Harry Potter' NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) DEFAULT 'Harry Potter' NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) DEFAULT 'Harry Potter' NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) DEFAULT 'Holmes' NOT NULL);`,
@@ -180,17 +180,17 @@ func TestColumnDefaultValue(t *testing.T) {
 		{
 			old:  `CREATE TABLE book(id INT DEFAULT 0 NOT NULL);`,
 			new:  `CREATE TABLE book(id INT NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(id INT NOT NULL);`,
 			new:  `CREATE TABLE book(id INT DEFAULT 0 NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT DEFAULT 0 NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT DEFAULT 0 NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(id INT DEFAULT 0 NOT NULL);`,
 			new:  `CREATE TABLE book(id INT DEFAULT 1 NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT DEFAULT 1 NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT DEFAULT 1 NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(id INT DEFAULT 0 NOT NULL);`,
@@ -203,7 +203,7 @@ func TestColumnDefaultValue(t *testing.T) {
 				"`last_update` timestamp not null default current_timestamp);",
 			new: "CREATE TABLE action(action_id smallint(5) unsigned NOT NULL AUTO_INCREMENT," +
 				"`last_update` timestamp not null default current_timestamp(1));",
-			want: "ALTER TABLE `action` MODIFY COLUMN `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(1);\n\n",
+			want: "ALTER TABLE `action` MODIFY COLUMN `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(1);\n",
 		},
 	}
 	testDiffWithoutDisableForeignKeyCheck(t, tests)
@@ -214,12 +214,12 @@ func TestColumnCollate(t *testing.T) {
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) COLLATE utf8mb4_bin DEFAULT 'Harry Potter' NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) COLLATE utf8mb4_polish_ci DEFAULT 'Harry Potter' NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COLLATE utf8mb4_polish_ci DEFAULT 'Harry Potter' NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COLLATE utf8mb4_polish_ci DEFAULT 'Harry Potter' NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) DEFAULT 'Harry Potter' NOT NULL);`,
 			new:  `CREATE TABLE book(name VARCHAR(50) COLLATE utf8mb4_polish_ci DEFAULT 'Harry Potter' NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COLLATE utf8mb4_polish_ci DEFAULT 'Harry Potter' NOT NULL;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) COLLATE utf8mb4_polish_ci DEFAULT 'Harry Potter' NOT NULL;\n",
 		},
 		{
 			old:  `CREATE TABLE book(name VARCHAR(50) DEFAULT 'Holmes' NOT NULL);`,
@@ -246,51 +246,51 @@ func TestColumnOrder(t *testing.T) {
 		{
 			old:  `CREATE TABLE book(id INT PRIMARY KEY);`,
 			new:  `CREATE TABLE book(id INT PRIMARY KEY, name VARCHAR(50) NOT NULL, author VARCHAR(50) NOT NULL);`,
-			want: "ALTER TABLE `book` ADD COLUMN `name` VARCHAR(50) NOT NULL AFTER `id`, ADD COLUMN `author` VARCHAR(50) NOT NULL AFTER `name`;\n\n",
+			want: "ALTER TABLE `book` ADD COLUMN `name` VARCHAR(50) NOT NULL AFTER `id`;\nALTER TABLE `book` ADD COLUMN `author` VARCHAR(50) NOT NULL AFTER `name`;\n",
 		},
 		// Add the column at the beginning of the table.
 		{
 			old:  `CREATE TABLE book(author VARCHAR(50) NOT NULL);`,
 			new:  `CREATE TABLE book(id INT PRIMARY KEY, name VARCHAR(50) NOT NULL, author VARCHAR(50) NOT NULL);`,
-			want: "ALTER TABLE `book` ADD COLUMN `id` INT PRIMARY KEY FIRST, ADD COLUMN `name` VARCHAR(50) NOT NULL AFTER `id`;\n\n",
+			want: "ALTER TABLE `book` ADD COLUMN `id` INT PRIMARY KEY FIRST;\nALTER TABLE `book` ADD COLUMN `name` VARCHAR(50) NOT NULL AFTER `id`;\n",
 		},
 		// Add the column in the middle of the table.
 		{
 			old:  `CREATE TABLE book(id INT PRIMARY KEY, author VARCHAR(50) NOT NULL);`,
 			new:  `CREATE TABLE book(id INT PRIMARY KEY, name VARCHAR(50) NOT NULL, author VARCHAR(50) NOT NULL);`,
-			want: "ALTER TABLE `book` ADD COLUMN `name` VARCHAR(50) NOT NULL AFTER `id`;\n\n",
+			want: "ALTER TABLE `book` ADD COLUMN `name` VARCHAR(50) NOT NULL AFTER `id`;\n",
 		},
 		// Modify the existing column order.
 		{
 			old:  `CREATE TABLE book(author VARCHAR(50) NOT NULL, id INT PRIMARY KEY, name VARCHAR(50) NOT NULL);`,
 			new:  `CREATE TABLE book(id INT PRIMARY KEY, name VARCHAR(50) NOT NULL, author VARCHAR(50) NOT NULL);`,
-			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT PRIMARY KEY FIRST, MODIFY COLUMN `name` VARCHAR(50) NOT NULL AFTER `id`;\n\n",
+			want: "ALTER TABLE `book` MODIFY COLUMN `id` INT PRIMARY KEY FIRST;\nALTER TABLE `book` MODIFY COLUMN `name` VARCHAR(50) NOT NULL AFTER `id`;\n",
 		},
 		// Complicated case.
 		{
 			old: `CREATE TABLE book(c1 INT, c2 INT, c3 INT, c4 INT, c5 INT);`,
 			new: `CREATE TABLE book(c6 INT, c2 INT, c3 INT, c7 INT, c8 INT, c4 INT, c5 INT);`,
-			want: "ALTER TABLE `book` ADD COLUMN `c6` INT FIRST, " + // c6, c1, c2, c3, c4, c5
-				"ADD COLUMN `c7` INT AFTER `c3`, " + // c6, c1, c2, c3, c7, c4, c5
-				"ADD COLUMN `c8` INT AFTER `c7`;\n\n" + // c6, c1, c2, c3, c7, c8, c4, c5
+			want: "ALTER TABLE `book` ADD COLUMN `c6` INT FIRST;\n" + // c6, c1, c2, c3, c4, c5
+				"ALTER TABLE `book` ADD COLUMN `c7` INT AFTER `c3`;\n" + // c6, c1, c2, c3, c7, c4, c5
+				"ALTER TABLE `book` ADD COLUMN `c8` INT AFTER `c7`;\n" + // c6, c1, c2, c3, c7, c8, c4, c5
 				"ALTER TABLE `book` DROP COLUMN `c1`;\n\n", // c6, c2, c3, c7, c8, c4, c5
 
 		},
 		{
 			old: `CREATE TABLE book(c1 INT, c2 INT, c3 INT, c4 INT, c8 INT);`,
 			new: `CREATE TABLE book(c9 INT, c8 VARCHAR(10), c4 INT, c2 VARCHAR(10), c9 INT);`,
-			want: "ALTER TABLE `book` ADD COLUMN `c9` INT FIRST, " +
-				"MODIFY COLUMN `c8` VARCHAR(10) AFTER `c9`, " +
-				"MODIFY COLUMN `c4` INT AFTER `c8`, " +
-				"MODIFY COLUMN `c2` VARCHAR(10), " +
-				"ADD COLUMN `c9` INT AFTER `c2`;\n\n" +
+			want: "ALTER TABLE `book` ADD COLUMN `c9` INT FIRST;\n" +
+				"ALTER TABLE `book` MODIFY COLUMN `c8` VARCHAR(10) AFTER `c9`;\n" +
+				"ALTER TABLE `book` MODIFY COLUMN `c4` INT AFTER `c8`;\n" +
+				"ALTER TABLE `book` MODIFY COLUMN `c2` VARCHAR(10);\n" +
+				"ALTER TABLE `book` ADD COLUMN `c9` INT AFTER `c2`;\n" +
 				"ALTER TABLE `book` DROP COLUMN `c1`;\n\n" +
 				"ALTER TABLE `book` DROP COLUMN `c3`;\n\n",
 		},
 		{
 			old: `CREATE TABLE t(a int);`,
 			new: `CREATE TABLE t(b int);`,
-			want: "ALTER TABLE `t` ADD COLUMN `b` INT FIRST;\n\n" +
+			want: "ALTER TABLE `t` ADD COLUMN `b` INT FIRST;\n" +
 				"ALTER TABLE `t` DROP COLUMN `a`;\n\n",
 		},
 	}
