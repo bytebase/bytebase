9059283e94d199bbafdaf6f9ac7d63364c9bdbe1
ecmadao <wlec@outlook.com>
2024-08-19 12:44:30 +0800

    chore: optimize

--- frontend/src/router/useRecentVisit.ts
+++ frontend/src/router/useRecentVisit.ts
@@ -20,9 +20,8 @@ export function useRecentVisit() {
     return recentVisit.value[0];
   });
 
-  const record = (path: string) => {
-    const list = recentVisit.value;
-    const index = list.findIndex((item) => {
+  const remove = (path: string) => {
+    const index = recentVisit.value.findIndex((item) => {
       // We treat the two URLs "the same" when their urls'
       //   `path` are the same (means ignoring querystring and hash).
       //   e.g. "/db?environment=5003" & "/db?environment=5005"
@@ -31,23 +30,26 @@ export function useRecentVisit() {
       return getPath(item) === getPath(path);
     });
     if (index >= 0) {
-      // current page exists in the history already
-      // pull it out before next step
-      list.splice(index, 1);
+      recentVisit.value.splice(index, 1);
     }
-    // then prepend the latest item to the queue
-    list.unshift(path);
+  };
+
+  const record = (path: string) => {
+    // current page exists in the history already
+    // pull it out before next step
+    remove(path);
 
     // ensure the queue's length
     // should be no more than (MAX_HISTORY + 1)
     // because current page will always be the first one in the list
     // but it will be not shown in kbar
-    while (list.length > MAX_HISTORY + 1) {
-      list.pop();
+    while (recentVisit.value.length > MAX_HISTORY + 1) {
+      recentVisit.value.pop();
     }
   };
 
   return {
+    remove,
     record,
     lastVisit,
   };
