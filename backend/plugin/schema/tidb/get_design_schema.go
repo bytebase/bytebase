0eb9f3d51c54644f4f08b8eea06d8c3aa0533d8c
h3n4l <oysterdays@gmail.com>
2024-08-20 15:58:03 +0800

    feat: tidb get design schema support view (#13457)
    Signed-off-by: h3n4l <oysterdays@gmail.com>

--- backend/plugin/schema/tidb/get_design_schema.go
+++ backend/plugin/schema/tidb/get_design_schema.go
@@ -21,6 +21,9 @@ func GetDesignSchema(_ string, to *storepb.DatabaseSchemaMetadata) (string, erro
 	if err := writeTables(&sb, to, toState); err != nil {
 		return "", err
 	}
+	if err := writeViews(&sb, to, toState); err != nil {
+		return "", err
+	}
 
 	s := sb.String()
 	// Make goyamlv3 happy.
@@ -57,6 +60,40 @@ func writeTables(w io.StringWriter, to *storepb.DatabaseSchemaMetadata, state *d
 		}
 	}
 	return nil
+}
+
+func writeViews(w io.StringWriter, to *storepb.DatabaseSchemaMetadata, state *databaseState) error {
+	// Follow the order of the input schemas.
+	for _, schema := range to.Schemas {
+		schemaState, ok := state.schemas[schema.Name]
+		if !ok {
+			continue
+		}
+		// Follow the order of the input views.
+		for _, view := range schema.Views {
+			view, ok := schemaState.views[view.Name]
+			if !ok {
+				continue
+			}
+			if _, err := w.WriteString(getViewAnnouncement(view.name)); err != nil {
+				return err
+			}
+
+			buf := &strings.Builder{}
+			if err := view.toString(buf); err != nil {
+				return err
+			}
+			if _, err := w.WriteString(buf.String()); err != nil {
+				return err
+			}
+			delete(schemaState.views, view.name)
+		}
+	}
+	return nil
+}
+
+func getViewAnnouncement(name string) string {
+	return fmt.Sprintf("\nDROP VIEW IF EXISTS `%s`;\n--\n-- View structure for `%s`\n--\n", name, name)
 }
 
 func getTableAnnouncement(name string) string {
