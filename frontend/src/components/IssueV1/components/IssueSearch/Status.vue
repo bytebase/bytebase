fb92176f7496fe4b2bfab53d7303b971b214fec8
steven <stevenlgtm@gmail.com>
2024-08-21 17:40:15 +0800

    chore: add all tab to issue status

--- frontend/src/components/IssueV1/components/IssueSearch/Status.vue
+++ frontend/src/components/IssueV1/components/IssueSearch/Status.vue
@@ -31,6 +31,10 @@ const { t } = useI18n();
 const tabItemList = computed(() => {
   return [
     {
+      value: "",
+      label: t("common.all"),
+    },
+    {
       value: "OPEN",
       label: t("issue.table.open"),
     },
@@ -49,7 +53,7 @@ const tab = computed(() => {
 });
 
 const updateStatus = (value: SemanticIssueStatus) => {
-  if (!["OPEN", "CLOSED"].includes(value)) return;
+  if (!["", "OPEN", "CLOSED"].includes(value)) return;
 
   const updated = upsertScope(props.params, {
     id: "status",
