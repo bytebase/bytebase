e22488562ba4601234a6436fe78507ef4b81d6a9
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-08-22 09:37:57 +0800

    chore: max height limitation (#13501)
    Former-commit-id: 6cb48f0d11c8194e8123c819dd46ca227df24b5b

--- frontend/src/views/sql-editor/EditorPanel/common.ts
+++ frontend/src/views/sql-editor/EditorPanel/common.ts
@@ -1,13 +1,13 @@
-import { useElementSize } from "@vueuse/core";
+import { pausableWatch, useElementSize } from "@vueuse/core";
 import { first } from "lodash-es";
 import type { DataTableInst, SelectOption } from "naive-ui";
-import { computed, ref, watch } from "vue";
+import { computed, ref, unref, watch, type MaybeRef } from "vue";
 import {
   useConnectionOfCurrentSQLEditorTab,
   useDBSchemaV1Store,
 } from "@/store";
 import { DatabaseMetadataView } from "@/types/proto/v1/database_service";
-import { hasSchemaProperty } from "@/utils";
+import { hasSchemaProperty, nextAnimationFrame } from "@/utils";
 import { useEditorPanelContext } from "./context";
 
 export const useSelectSchema = () => {
@@ -42,7 +42,22 @@ export const useSelectSchema = () => {
   return { selectedSchemaName, showSchemaSelect, options };
 };
 
-export const useAutoHeightDataTable = () => {
+export type AutoHeightDataTableOptions = {
+  maxHeight: number | null;
+};
+const defaultAutoHeightDataTableOptions = (): AutoHeightDataTableOptions => ({
+  maxHeight: null,
+});
+
+export const useAutoHeightDataTable = (
+  data?: MaybeRef<unknown[]>,
+  options?: MaybeRef<Partial<AutoHeightDataTableOptions>>
+) => {
+  const opts = computed(() => ({
+    ...defaultAutoHeightDataTableOptions(),
+    ...unref(options),
+  }));
+  const GAP_HEIGHT = 2;
   const dataTableRef = ref<DataTableInst>();
   const containerElRef = ref<HTMLElement>();
   const tableHeaderElRef = computed(
@@ -53,9 +68,30 @@ export const useAutoHeightDataTable = () => {
   );
   const { height: containerHeight } = useElementSize(containerElRef);
   const { height: tableHeaderHeight } = useElementSize(tableHeaderElRef);
-  const tableBodyHeight = computed(() => {
-    return containerHeight.value - tableHeaderHeight.value - 2;
-  });
+  const tableBodyHeight = ref(0);
+
+  const { pause: pauseWatch, resume: resumeWatch } = pausableWatch(
+    [() => unref(data), containerHeight, tableHeaderHeight, opts],
+    async () => {
+      if (containerHeight.value === 0 || tableHeaderHeight.value === 0) return;
+      if (opts.value.maxHeight === null) {
+        // The table height is always limited by the container height
+        // and need not to be adjusted automatically
+        tableBodyHeight.value =
+          containerHeight.value - tableHeaderHeight.value - GAP_HEIGHT;
+        return;
+      }
+
+      pauseWatch();
+      const experimentalMaxBodyHeight =
+        opts.value.maxHeight - tableHeaderHeight.value - GAP_HEIGHT;
+      tableBodyHeight.value = experimentalMaxBodyHeight;
+      await nextAnimationFrame();
+      resumeWatch();
+    },
+    { immediate: true }
+  );
+
   // Use this to avoid unnecessary initial rendering
   const layoutReady = computed(() => tableHeaderHeight.value > 0);
 
