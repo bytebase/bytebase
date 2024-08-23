e4d3c47550040f36760e52ce708ce8752698bf88
d-bytebase <d@bytebase.com>
2024-08-23 00:41:52 -0700

    chore: update

--- backend/plugin/db/mysql/query_test.go
+++ backend/plugin/db/mysql/query_test.go
@@ -18,9 +18,14 @@ func TestGetStatementWithResultLimitOfMySQL(t *testing.T) {
 			want:  "SELECT * FROM t LIMIT 10;",
 		},
 		{
+			stmt:  "SELECT * FROM t LIMIT 10;",
+			count: 5,
+			want:  "SELECT * FROM t LIMIT 5;",
+		},
+		{
 			stmt:  "SELECT * FROM t LIMIT 5;",
 			count: 10,
-			want:  "SELECT * FROM t LIMIT 10;",
+			want:  "SELECT * FROM t LIMIT 5;",
 		},
 		{
 			stmt:  "SELECT * FROM t LIMIT 123;",
