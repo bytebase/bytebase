7ac36ac5cc0078c9fbb010946d942156826a944b
Azusain <azusaings@gmail.com>
2024-08-26 17:05:17 +0800

    fix(es): add support for \r on Windows and refactor code (#13549)
    * fix(es): add support for \r on Windows and refactor code
    
    * chore: go mod tidy
    
    * chore: update
    
    * chore: go mod tidy
    
    * chore: golangci-lint
    
    * chore: golangci-lint
    
    * fix: typo
    
    * chore: naming
    
    * test: add leading CRLF characters
    
    * hasmore
    
    * handle eof
    
    * test
    
    * transfer
    
    * rename
    
    * return
    
    * update
    
    * err
    
    ---------
    
    Co-authored-by: p0ny <p0uy@outlook.com>
    Former-commit-id: 1aae379a6fb3d7530335ad6eadee2bd6bc2db07e

--- backend/plugin/db/elasticsearch/spliter_test.go
+++ backend/plugin/db/elasticsearch/spliter_test.go
@@ -2,14 +2,20 @@ package elasticsearch
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestSpliter(t *testing.T) {
-	test := struct {
+	tests := []struct {
+		name     string
 		input    string
-		expected []Statement
+		expected []*statement
 	}{
-		input: `POST /_bulk
+		// Test 1: basic test.
+		{
+			name: "test 1",
+			input: `POST /_bulk
 { "index" : { "_index" : "books" } }
 {"name": "Revelation Space", "author": "天舟", "page_count": 585}
 { "index" : { "_index" : "books" } }
@@ -21,41 +27,88 @@ GET books/_search
 	}
 }
 GET _cat/indices`,
-		expected: []Statement{{
-			method: "POST",
-			route:  []byte("/_bulk"),
-			queryString: []byte(`{ "index" : { "_index" : "books" } }
+			expected: []*statement{
+				{
+					method: []byte("POST"),
+					route:  []byte("/_bulk"),
+					queryBody: []byte(`{ "index" : { "_index" : "books" } }
 {"name": "Revelation Space", "author": "天舟", "page_count": 585}
 { "index" : { "_index" : "books" } }
 {"name": "1984", "author": "天舟", "page_count": 328}
 `),
-		}, {
-			method: "GET",
-			route:  []byte(`books/_search`),
-			queryString: []byte(`{
+				},
+				{
+					method: []byte("GET"),
+					route:  []byte(`/books/_search`),
+					queryBody: []byte(`{
 	"query": {
 		"match_all": {}
 	}
 }
 `),
-		}, {
-			method:      "GET",
-			route:       []byte(`_cat/indices`),
-			queryString: []byte{},
-		}},
+				},
+				{
+					method: []byte("GET"),
+					route:  []byte(`/_cat/indices`),
+				}},
+		},
+		// Test 2: with CR characters.
+		{
+			name:  "test 2",
+			input: "\nGET /_cat/indices\r\n\r\nget /_cat/indices\r\nget /_cat/indices\r\n\r\n",
+			expected: []*statement{
+				{
+					method: []byte("GET"),
+					route:  []byte("/_cat/indices"),
+				},
+				{
+					method: []byte("GET"),
+					route:  []byte("/_cat/indices"),
+				},
+				{
+					method: []byte("GET"),
+					route:  []byte("/_cat/indices"),
+				},
+			},
+		},
+		// Test 3: with emojis.
+		{
+			name:  "test 3",
+			input: "\r\n\rPUT /my_index\r\n\r\nPOST /my_index/_doc\r\n{\r\n\t\"emoji\":\"😈😈😈😈\"}\r\nGET /",
+			expected: []*statement{
+				{
+					method: []byte("PUT"),
+					route:  []byte("/my_index"),
+				},
+				{
+					method:    []byte("POST"),
+					route:     []byte("/my_index/_doc"),
+					queryBody: []byte("{\r\n\t\"emoji\":\"😈😈😈😈\"}\n"),
+				},
+				{
+					method: []byte("GET"),
+					route:  []byte("/"),
+				},
+			},
+		},
+		// Test 4: bulk APIs.
+		{
+			name:  "test 4",
+			input: "\r\nPOST /_bulk\r\n{ \"index\" : { \"_index\" : \"test\", \"_id\" : \"1\" } }\r\n{ \"field1\" : \"value1\" }\r\n\r\n",
+			expected: []*statement{
+				{
+					method:    []byte("POST"),
+					route:     []byte("/_bulk"),
+					queryBody: []byte("{ \"index\" : { \"_index\" : \"test\", \"_id\" : \"1\" } }\n{ \"field1\" : \"value1\" }\n\n"),
+				},
+			},
+		},
 	}
 
-	stats, err := SplitElasticsearchStatements(test.input)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	for index, statement := range stats {
-		expectedStatement := test.expected[index]
-		if expectedStatement.method != statement.method ||
-			string(expectedStatement.route) != string(statement.route) ||
-			string(statement.queryString) != string(expectedStatement.queryString) {
-			t.Fail()
-		}
+	a := require.New(t)
+	for _, test := range tests {
+		stmts, err := splitElasticsearchStatements(test.input)
+		a.NoError(err, test.name)
+		a.Equal(test.expected, stmts, test.name)
 	}
 }
