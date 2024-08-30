a335a341348638c5a976b7c5be860396f6348b56
ecmadao <wlec@outlook.com>
2024-08-30 11:45:44 +0800

    chore: support sync group (#13596)
    * feat: add webhook for scim user sync
    
    * fix: lint
    
    * fix: lint
    
    * chore: only sync in workspace
    
    * chore: support sync group
    
    * chore: update

--- backend/api/directory-sync/aad.go
+++ backend/api/directory-sync/aad.go
@@ -6,6 +6,10 @@ type AADUserEmail struct {
 	Primary bool   `json:"primary"`
 }
 
+type AADResourceMeta struct {
+	ResourceType string `json:"resourceType"`
+}
+
 // AAD user schema
 // Docs: https://learn.microsoft.com/en-us/entra/identity/app-provisioning/use-scim-to-provision-users-and-groups#design-your-user-and-group-schema
 type AADUser struct {
@@ -13,10 +17,20 @@ type AADUser struct {
 	Schemas    []string `json:"schemas"`
 	ExternalID string   `json:"externalId"`
 	// Map to userPrincipalName in AAD.
-	UserName    string          `json:"userName"`
-	Active      bool            `json:"active"`
-	DisplayName string          `json:"displayName"`
-	Emails      []*AADUserEmail `json:"emails"`
+	UserName    string           `json:"userName"`
+	Active      bool             `json:"active"`
+	DisplayName string           `json:"displayName"`
+	Emails      []*AADUserEmail  `json:"emails"`
+	Meta        *AADResourceMeta `json:"meta"`
+}
+
+type AADGroup struct {
+	ID          string           `json:"id"`
+	Schemas     []string         `json:"schemas"`
+	ExternalID  string           `json:"externalId"`
+	DisplayName string           `json:"displayName"`
+	Members     []string         `json:"members"`
+	Meta        *AADResourceMeta `json:"meta"`
 }
 
 // Docs: https://learn.microsoft.com/en-us/entra/identity/app-provisioning/use-scim-to-provision-users-and-groups#get-user-by-query
@@ -26,14 +40,24 @@ type ListUsersResponse struct {
 	Resources    []*AADUser `json:"Resources"`
 }
 
-type PatchUserOperation struct {
+type ListGroupsResponse struct {
+	Schemas      []string    `json:"schemas"`
+	TotalResults int         `json:"totalResults"`
+	Resources    []*AADGroup `json:"Resources"`
+}
+
+type PatchMember struct {
+	Value string `json:"value"`
+}
+
+type PatchOperation struct {
 	OP    string `json:"op"`
 	Path  string `json:"path"`
 	Value any    `json:"value"`
 }
 
 // Docs: https://learn.microsoft.com/en-us/entra/identity/app-provisioning/use-scim-to-provision-users-and-groups#update-user-multi-valued-properties
-type PatchUserRequest struct {
-	Schemas    []string              `json:"schemas"`
-	Operations []*PatchUserOperation `json:"Operations"`
+type PatchRequest struct {
+	Schemas    []string          `json:"schemas"`
+	Operations []*PatchOperation `json:"Operations"`
 }
