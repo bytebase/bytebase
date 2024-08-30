e243c6fd230aa301ad55e49bb555607553edcb09
boojack <24653555+boojack@users.noreply.github.com>
2024-08-30 16:03:11 +0800

    chore: update database transfer (#13600)
    * chore: use database v1 table instead of bbgrid
    
    * chore: update
    
    * chore: update
    
    * chore: update

--- frontend/src/components/TransferDatabaseForm/index.ts
+++ frontend/src/components/TransferDatabaseForm/index.ts
@@ -1,6 +1,5 @@
-import MultipleDatabaseSelector from "./MultipleDatabaseSelector.vue";
 import TransferDatabaseForm from "./TransferDatabaseForm.vue";
 
 export * from "./utils";
 
-export { MultipleDatabaseSelector, TransferDatabaseForm };
+export { TransferDatabaseForm };
