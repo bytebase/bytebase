8d71decf4584cd88425e88857e302d0b7f53c28d
h3n4l <oysterdays@gmail.com>
2024-09-02 12:01:24 +0800

    fix: correct proto cmp (#13626)
    * fix: correct proto cmp
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * fix: correct proto cmp
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    ---------
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>

--- backend/api/v1/branch_service.go
+++ backend/api/v1/branch_service.go
@@ -1608,7 +1608,7 @@ func updateDatabaseConfigLastModifierForMerge(baseMetadata *storepb.DatabaseSche
 			if baseTableConfig == nil {
 				baseTableConfig = initTableConfig(baseTable.GetProto(), "", "", now)
 			}
-			if diff := cmp.Diff(baseTable.GetProto(), baseTable, protocmp.Transform()); diff != "" {
+			if diff := cmp.Diff(headTable, baseTable.GetProto(), protocmp.Transform()); diff != "" {
 				baseTableConfig.SourceBranch = headTableConfig.SourceBranch
 				baseTableConfig.Updater = headTableConfig.Updater
 				baseTableConfig.UpdateTime = now
