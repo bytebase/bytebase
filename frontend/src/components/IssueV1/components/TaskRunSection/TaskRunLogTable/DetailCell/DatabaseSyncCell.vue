f3fc6b6fadc334374a66001276c386d581a7fccf
steven <stevenlgtm@gmail.com>
2024-09-02 12:07:39 +0800

    chore: use `NEllipsis` in task run log detail cell

--- frontend/src/components/IssueV1/components/TaskRunSection/TaskRunLogTable/DetailCell/DatabaseSyncCell.vue
+++ frontend/src/components/IssueV1/components/TaskRunSection/TaskRunLogTable/DetailCell/DatabaseSyncCell.vue
@@ -1,19 +1,15 @@
 <template>
-  <div class="text-sm">
-    <div v-if="error" class="text-error">{{ error }}</div>
-    <div v-else class="text-control-placeholder">-</div>
-  </div>
+  <span v-if="error" class="text-error">{{ error }}</span>
+  <span v-else class="text-control-placeholder">-</span>
 </template>
 
 <script setup lang="ts">
 import { computed } from "vue";
 import { TaskRunLogEntry_Type } from "@/types/proto/v1/rollout_service";
-import type { Sheet } from "@/types/proto/v1/sheet_service";
 import type { FlattenLogEntry } from "../common";
 
 const props = defineProps<{
   entry: FlattenLogEntry;
-  sheet?: Sheet;
 }>();
 
 const error = computed(() => {
