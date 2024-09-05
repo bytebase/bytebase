183aac9c3635b03c49d24b1b6c3a4644982a1618
ecmadao <wlec@outlook.com>
2024-09-05 08:30:53 +0800

    fix: worksheet permission check (#13674)
    * fix: worksheet permission check
    
    * fix: worksheet permission check
    
    Former-commit-id: ad61bd50cf9c9a6d1ba69ca0979070185332b480

--- frontend/src/utils/v1/worksheet.ts
+++ frontend/src/utils/v1/worksheet.ts
@@ -1,9 +1,9 @@
 import { useAuthStore, useProjectV1Store } from "@/store";
 import { getUserEmailFromIdentifier } from "@/store/modules/v1/common";
-import { PresetRoleType } from "@/types";
+import { UNKNOWN_PROJECT_NAME } from "@/types";
 import type { Worksheet } from "@/types/proto/v1/worksheet_service";
 import { Worksheet_Visibility } from "@/types/proto/v1/worksheet_service";
-import { hasWorkspaceLevelRole, hasProjectPermissionV2 } from "@/utils";
+import { hasProjectPermissionV2, hasWorkspacePermissionV2 } from "@/utils";
 
 export const extractWorksheetUID = (name: string) => {
   const pattern = /(?:^|\/)worksheets\/([^/]+)(?:$|\/)/;
@@ -25,10 +25,7 @@ export const isWorksheetReadableV1 = (sheet: Worksheet) => {
     return true;
   }
 
-  if (
-    hasWorkspaceLevelRole(PresetRoleType.WORKSPACE_ADMIN) ||
-    hasWorkspaceLevelRole(PresetRoleType.WORKSPACE_DBA)
-  ) {
+  if (hasWorkspacePermissionV2("bb.worksheets.manage")) {
     return true;
   }
 
@@ -38,6 +35,9 @@ export const isWorksheetReadableV1 = (sheet: Worksheet) => {
     case Worksheet_Visibility.VISIBILITY_PROJECT_READ:
     case Worksheet_Visibility.VISIBILITY_PROJECT_WRITE: {
       const projectV1 = useProjectV1Store().getProjectByName(sheet.project);
+      if (projectV1.name === UNKNOWN_PROJECT_NAME) {
+        return false;
+      }
       return hasProjectPermissionV2(projectV1, "bb.worksheets.get");
     }
   }
@@ -57,18 +57,20 @@ export const isWorksheetWritableV1 = (sheet: Worksheet) => {
     // Always writable to the creator
     return true;
   }
-  if (
-    hasWorkspaceLevelRole(PresetRoleType.WORKSPACE_ADMIN) ||
-    hasWorkspaceLevelRole(PresetRoleType.WORKSPACE_DBA)
-  ) {
+
+  if (hasWorkspacePermissionV2("bb.worksheets.manage")) {
     return true;
   }
 
   const projectV1 = useProjectV1Store().getProjectByName(sheet.project);
+  if (projectV1.name === UNKNOWN_PROJECT_NAME) {
+    return false;
+  }
   switch (sheet.visibility) {
     case Worksheet_Visibility.VISIBILITY_PRIVATE:
       return false;
     case Worksheet_Visibility.VISIBILITY_PROJECT_WRITE:
+      return hasProjectPermissionV2(projectV1, "bb.projects.get");
     case Worksheet_Visibility.VISIBILITY_PROJECT_READ:
       return hasProjectPermissionV2(projectV1, "bb.worksheets.manage");
   }
