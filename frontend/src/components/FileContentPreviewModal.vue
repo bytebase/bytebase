59867daf494ba398570000aeb3b0f46fae47fae5
boojack <stevenlgtm@gmail.com>
2024-09-06 15:11:37 +0800

    chore: allow to select encoding in changelist (#13691)
    Former-commit-id: dcc35f13c6c3bb3b63cdd487c11ded1295eb358b

--- frontend/src/components/FileContentPreviewModal.vue
+++ frontend/src/components/FileContentPreviewModal.vue
@@ -43,53 +43,7 @@ import { computed, reactive, ref, watch } from "vue";
 import { BBModal } from "@/bbkit";
 import { MonacoEditor } from "@/components/MonacoEditor";
 import { pushNotification } from "@/store";
-import { readFileAsArrayBuffer } from "@/utils";
-
-// Reference: https://developer.mozilla.org/en-US/docs/Web/API/Encoding_API/Encodings
-const ENCODINGS = [
-  "utf-8",
-  "ibm866",
-  "iso-8859-2",
-  "iso-8859-3",
-  "iso-8859-4",
-  "iso-8859-5",
-  "iso-8859-6",
-  "iso-8859-7",
-  "iso-8859-8",
-  "iso-8859-8i",
-  "iso-8859-10",
-  "iso-8859-13",
-  "iso-8859-14",
-  "iso-8859-15",
-  "iso-8859-16",
-  "koi8-r",
-  "koi8-u",
-  "macintosh",
-  "windows-874",
-  "windows-1250",
-  "windows-1251",
-  "windows-1252",
-  "windows-1253",
-  "windows-1254",
-  "windows-1255",
-  "windows-1256",
-  "windows-1257",
-  "windows-1258",
-  "x-mac-cyrillic",
-  "gbk",
-  "gb18030",
-  "hz-gb-2312",
-  "big5",
-  "euc-jp",
-  "iso-2022-jp",
-  "shift-jis",
-  "euc-kr",
-  "iso-2022-kr",
-  "utf-16be",
-  "utf-16le",
-];
-
-type Encoding = (typeof ENCODINGS)[number];
+import { ENCODINGS, readFileAsArrayBuffer, type Encoding } from "@/utils";
 
 interface LocalState {
   encoding: Encoding;
