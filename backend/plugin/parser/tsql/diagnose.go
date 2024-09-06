fee124afbbabdf0e9ede2676a2bc9a1142f3ae3e
h3n4l <oysterdays@gmail.com>
2024-09-06 15:24:30 +0800

    feat: diagnose for sql server
    Signed-off-by: h3n4l <oysterdays@gmail.com>

---
+++ backend/plugin/parser/tsql/diagnose.go
@@ -0,0 +1,57 @@
+package tsql
+
+import (
+	"context"
+
+	"github.com/antlr4-go/antlr/v4"
+	parser "github.com/bytebase/tsql-parser"
+
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+	"github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+func init() {
+	base.RegisterDiagnoseFunc(store.Engine_MSSQL, Diagnose)
+}
+
+func Diagnose(_ context.Context, _ base.DiagnoseContext, statement string) ([]base.Diagnostic, error) {
+	diagnostics := make([]base.Diagnostic, 0)
+	syntaxError := parseTSQLStatement(statement)
+	if syntaxError != nil {
+		diagnostics = append(diagnostics, base.ConvertSyntaxErrorToDiagnostic(syntaxError))
+	}
+
+	return diagnostics, nil
+}
+
+func parseTSQLStatement(statement string) *base.SyntaxError {
+	inputStream := antlr.NewInputStream(statement)
+	lexer := parser.NewTSqlLexer(inputStream)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	p := parser.NewTSqlParser(stream)
+	lexerErrorListener := &base.ParseErrorListener{
+		BaseLine: 0,
+	}
+	lexer.RemoveErrorListeners()
+	lexer.AddErrorListener(lexerErrorListener)
+
+	parserErrorListener := &base.ParseErrorListener{
+		BaseLine: 0,
+	}
+	p.RemoveErrorListeners()
+	p.AddErrorListener(parserErrorListener)
+
+	p.BuildParseTrees = false
+
+	_ = p.Tsql_file()
+
+	if lexerErrorListener.Err != nil {
+		return lexerErrorListener.Err
+	}
+
+	if parserErrorListener.Err != nil {
+		return parserErrorListener.Err
+	}
+	return nil
+}
