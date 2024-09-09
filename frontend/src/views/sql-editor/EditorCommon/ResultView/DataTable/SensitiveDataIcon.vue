9be8f47b2033254529a240133a1fa4f1435d18ee
ecmadao <wlec@outlook.com>
2024-09-09 14:39:22 +0800

    chore: restructure workspace sidebar (#13706)
    Former-commit-id: 3d3cb56906fdb570dc5acfdbbf13f543d9c5c017
    Former-commit-id: 53ed0fded00930efd0a25786f20cd113a945032e

--- frontend/src/views/sql-editor/EditorCommon/ResultView/DataTable/SensitiveDataIcon.vue
+++ frontend/src/views/sql-editor/EditorCommon/ResultView/DataTable/SensitiveDataIcon.vue
@@ -19,7 +19,7 @@
 import { NTooltip } from "naive-ui";
 import { computed } from "vue";
 import { useRouter } from "vue-router";
-import { WORKSPACE_ROUTE_SENSITIVE_DATA } from "@/router/dashboard/workspaceRoutes";
+import { WORKSPACE_ROUTE_DATA_ACCESS_CONTROL } from "@/router/dashboard/workspaceRoutes";
 import { hasWorkspacePermissionV2 } from "@/utils";
 
 const router = useRouter();
@@ -32,7 +32,7 @@ const handleClick = () => {
   if (!clickable.value) {
     return;
   }
-  const url = router.resolve({ name: WORKSPACE_ROUTE_SENSITIVE_DATA });
+  const url = router.resolve({ name: WORKSPACE_ROUTE_DATA_ACCESS_CONTROL });
   window.open(url.href, "_BLANK");
 };
 </script>
