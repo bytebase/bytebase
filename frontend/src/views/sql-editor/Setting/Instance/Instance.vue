71a4747b7996fd620bcb1c641745763f6f122183
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-09-09 16:53:47 +0800

    chore: instance detail link

--- frontend/src/views/sql-editor/Setting/Instance/Instance.vue
+++ frontend/src/views/sql-editor/Setting/Instance/Instance.vue
@@ -41,9 +41,30 @@
         @dismiss="state.detail.show = false"
       >
         <DrawerContent
-          :title="detailTitle"
+          class="instance-detail-drawer"
           body-content-class="flex flex-col gap-2 overflow-hidden"
         >
+          <template #header>
+            <span>{{ detailTitle }}</span>
+            <NTooltip v-if="state.detail.instance" placement="bottom">
+              <template #trigger>
+                <NButton
+                  quaternary
+                  size="tiny"
+                  style="--n-padding: 0 4px"
+                  @click="toDetailPage(state.detail.instance)"
+                >
+                  <template #icon>
+                    <ExternalLinkIcon class="w-4 h-4" />
+                  </template>
+                </NButton>
+              </template>
+              <template #default>
+                {{ $t("common.detail") }}
+              </template>
+            </NTooltip>
+          </template>
+
           <InstanceFormBody
             :hide-archive-restore="true"
             class="flex-1 overflow-auto"
@@ -60,8 +81,8 @@
 </template>
 
 <script lang="ts" setup>
-import { PlusIcon } from "lucide-vue-next";
-import { NButton, NEllipsis } from "naive-ui";
+import { ExternalLinkIcon, PlusIcon } from "lucide-vue-next";
+import { NButton, NEllipsis, NTooltip } from "naive-ui";
 import { computed, onMounted, reactive, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRoute, useRouter } from "vue-router";
@@ -74,6 +95,7 @@ import {
   Buttons as InstanceFormButtons,
 } from "@/components/InstanceForm";
 import { Drawer, DrawerContent, InstanceV1Table } from "@/components/v2";
+import { INSTANCE_ROUTE_DETAIL } from "@/router/dashboard/instance";
 import {
   useSubscriptionV1Store,
   useEnvironmentV1List,
@@ -89,6 +111,7 @@ import {
   sortInstanceV1ListByEnvironmentV1,
   extractEnvironmentResourceName,
   wrapRefAsPromise,
+  extractInstanceResourceName,
 } from "@/utils";
 
 interface LocalState {
@@ -223,4 +246,21 @@ const showInstanceDetail = (instance: Instance) => {
   state.detail.show = true;
   state.detail.instance = instance;
 };
+
+const toDetailPage = (instance: Instance) => {
+  const instanceId = extractInstanceResourceName(instance.name);
+  const route = router.resolve({
+    name: INSTANCE_ROUTE_DETAIL,
+    params: {
+      instanceId,
+    },
+  });
+  window.open(route.fullPath, "_blank");
+};
 </script>
+
+<style scoped lang="postcss">
+.instance-detail-drawer :deep(.n-drawer-header__main) {
+  @apply flex-1 flex items-center justify-between;
+}
+</style>
