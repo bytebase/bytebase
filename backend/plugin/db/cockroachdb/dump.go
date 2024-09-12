d458208ace978d10f061f4be849452dceda8ff7b
h3n4l <oysterdays@gmail.com>
2024-09-12 16:44:32 +0800

    feat(cockroachdb): tiny schema dump for cockroachdb (#13745)
    * feat: tiny schema dump for cockroachdb
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    ---------
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    Former-commit-id: 95306ee15b4f1b404be5820a5d8f0eb18e275545
    Former-commit-id: bab660158dce727f1cc7f7c9b019314098469bfd

--- backend/plugin/db/cockroachdb/dump.go
+++ backend/plugin/db/cockroachdb/dump.go
@@ -3,9 +3,39 @@ package cockroachdb
 import (
 	"context"
 	"io"
+	"strings"
 )
 
 // Dump dumps the database.
-func (*Driver) Dump(context.Context, io.Writer) (string, error) {
-	return "", nil
+func (driver *Driver) Dump(ctx context.Context, w io.Writer) (string, error) {
+	conn, err := driver.db.Conn(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	sb := &strings.Builder{}
+	rows, err := conn.QueryContext(ctx, "SELECT create_statement FROM [SHOW CREATE ALL TABLES];")
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var payload string
+		if err := rows.Scan(&payload); err != nil {
+			return "", err
+		}
+		_, _ = sb.WriteString(payload)
+		_, _ = sb.WriteString("\n\n")
+	}
+
+	if err := rows.Err(); err != nil {
+		return "", err
+	}
+
+	if _, err := w.Write([]byte(sb.String())); err != nil {
+		return "", err
+	}
+
+	return sb.String(), nil
 }
