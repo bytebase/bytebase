1bd42c0d21f7806167ccfa41cad55fc2ed1acef5
h3n4l <oysterdays@gmail.com>
2024-09-12 15:42:14 +0800

    feat: tiny schema dump for cockroachdb
    Signed-off-by: h3n4l <oysterdays@gmail.com>

--- backend/plugin/db/cockroachdb/dump.go
+++ backend/plugin/db/cockroachdb/dump.go
@@ -3,9 +3,37 @@ package cockroachdb
 import (
 	"context"
 	"io"
+	"strings"
 )
 
 // Dump dumps the database.
-func (*Driver) Dump(context.Context, io.Writer) (string, error) {
-	return "", nil
+func (driver *Driver) Dump(ctx context.Context, w io.Writer) (string, error) {
+	conn, err := driver.db.Conn(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer conn.Close()
+
+	sb := &strings.Builder{}
+	rows, err := conn.QueryContext(ctx, "SELECT create_statement FROM [SHOW CREATE ALL TABLES];")
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var payload string
+		if err := rows.Scan(&payload); err != nil {
+			return "", err
+		}
+		sb.WriteString(payload)
+		sb.WriteString("\n\n")
+	}
+
+	if err := rows.Err(); err != nil {
+		return "", err
+	}
+
+	w.Write([]byte(sb.String()))
+
+	return sb.String(), nil
 }
