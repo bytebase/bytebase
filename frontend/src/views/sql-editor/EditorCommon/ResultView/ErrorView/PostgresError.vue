e8c62b93a67f25986ef837e9695e33eac4f4f63e
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-09-13 17:08:33 +0800

    feat: display postgresError

---
+++ frontend/src/views/sql-editor/EditorCommon/ResultView/ErrorView/PostgresError.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-for="(error, i) in postgresErrors" :key="i" class="text-sm grid gap-1 pl-8" style="grid-template-columns: auto 1fr;">
+    <template v-if="error.detail">
+      <div>DETAIL:</div>
+      <div>{{error.detail}}</div>
+    </template>
+    <template v-if="error.hint">
+      <div>HINT:</div>
+      <div>{{error.hint}}</div>
+    </template>
+    <template v-if="error.where">
+      <div>WHERE:</div>
+      <div>{{error.where}}</div>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import type { SQLResultSetV1 } from "@/types";
+import type { QueryResult_PostgresError } from "@/types/proto/v1/sql_service";
+
+const props = defineProps<{
+  resultSet: SQLResultSetV1;
+}>();
+
+const postgresErrors = computed(() => {
+  const errors: QueryResult_PostgresError[] = [];
+  props.resultSet.results.forEach((result) => {
+    if (result.postgresError) {
+      errors.push(result.postgresError);
+    }
+  });
+  return errors;
+});
+</script>
