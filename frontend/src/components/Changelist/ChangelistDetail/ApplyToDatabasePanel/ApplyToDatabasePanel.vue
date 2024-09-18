0e04679c375f53c72f0c7d2310beb164f10833b1
steven <stevenlgtm@gmail.com>
2024-09-18 15:00:35 +0800

    feat: apply changelist to database group

--- frontend/src/components/Changelist/ChangelistDetail/ApplyToDatabasePanel/ApplyToDatabasePanel.vue
+++ frontend/src/components/Changelist/ChangelistDetail/ApplyToDatabasePanel/ApplyToDatabasePanel.vue
@@ -13,22 +13,43 @@
                 <NRadio :value="'DML'" :label="t('issue.title.change-data')" />
               </NRadioGroup>
             </div>
-            <DatabaseV1Table
-              mode="PROJECT"
-              :database-list="schemaDatabaseList"
-              :show-selection="true"
-              :selected-database-names="state.selectedDatabaseNameList"
-              @update:selected-databases="
-                state.selectedDatabaseNameList = Array.from($event)
-              "
-            />
-            <SchemalessDatabaseTable
-              v-if="
-                state.changeType === 'DDL' && schemalessDatabaseList.length > 0
-              "
-              mode="PROJECT"
-              :database-list="schemalessDatabaseList"
-            />
+
+            <NTabs v-model:value="state.changeSource" type="card">
+              <NTabPane name="DATABASE" :tab="$t('common.databases')">
+                <DatabaseV1Table
+                  mode="PROJECT"
+                  :database-list="schemaDatabaseList"
+                  :show-selection="true"
+                  :selected-database-names="state.selectedDatabaseNameList"
+                  @update:selected-databases="
+                    state.selectedDatabaseNameList = Array.from($event)
+                  "
+                />
+                <SchemalessDatabaseTable
+                  v-if="
+                    state.changeType === 'DDL' &&
+                    schemalessDatabaseList.length > 0
+                  "
+                  mode="PROJECT"
+                  :database-list="schemalessDatabaseList"
+                />
+              </NTabPane>
+              <NTabPane name="GROUP" :tab="$t('common.database-groups')">
+                <DatabaseGroupDataTable
+                  :database-group-list="dbGroupList"
+                  :show-selection="true"
+                  :single-selection="true"
+                  :selected-database-group-names="
+                    state.selectedDatabaseGroup
+                      ? [state.selectedDatabaseGroup]
+                      : []
+                  "
+                  @update:selected-database-groups="
+                    state.selectedDatabaseGroup = head(Array.from($event))
+                  "
+                />
+              </NTabPane>
+            </NTabs>
           </div>
           <div
             v-if="!ready || state.isGenerating"
@@ -44,7 +65,10 @@
         <div class="flex-1 flex items-center justify-between">
           <div>
             <div
-              v-if="flattenSelectedDatabaseNameList.length > 0"
+              v-if="
+                state.changeSource === 'DATABASE' &&
+                flattenSelectedDatabaseNameList.length > 0
+              "
               class="textinfolabel"
             >
               {{
@@ -78,22 +102,29 @@
 </template>
 
 <script lang="ts" setup>
+import { head } from "lodash-es";
 import { NButton } from "naive-ui";
-import { NRadioGroup, NRadio } from "naive-ui";
+import { NRadioGroup, NRadio, NTabs, NTabPane } from "naive-ui";
 import { zindexable as vZindexable } from "vdirs";
 import { computed, reactive, watch } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRouter } from "vue-router";
 import { BBSpin } from "@/bbkit";
 import SchemalessDatabaseTable from "@/components/AlterSchemaPrepForm/SchemalessDatabaseTable.vue";
+import DatabaseGroupDataTable from "@/components/DatabaseGroup/DatabaseGroupDataTable.vue";
 import { Drawer, DrawerContent, ErrorTipsButton } from "@/components/v2";
 import DatabaseV1Table from "@/components/v2/Model/DatabaseV1Table/DatabaseV1Table.vue";
 import { PROJECT_V1_ROUTE_ISSUE_DETAIL } from "@/router/dashboard/projectV1";
-import { useDatabaseV1Store } from "@/store";
+import {
+  useDatabaseV1Store,
+  useDBGroupListByProject,
+  useDBGroupStore,
+} from "@/store";
 import { useDatabaseV1List } from "@/store/modules/v1/databaseList";
 import type { ComposedDatabase } from "@/types";
 import { DEFAULT_PROJECT_NAME } from "@/types";
 import { State } from "@/types/proto/v1/common";
+import { DatabaseGroup } from "@/types/proto/v1/database_group_service";
 import {
   extractProjectResourceName,
   generateIssueTitle,
@@ -105,12 +136,15 @@ import { useChangelistDetailContext } from "../context";
 type LocalState = {
   selectedDatabaseNameList: string[];
   changeType: "DDL" | "DML";
+  changeSource: "DATABASE" | "GROUP";
   isGenerating: boolean;
+  selectedDatabaseGroup?: string;
 };
 
 const { t } = useI18n();
 const router = useRouter();
 const databaseStore = useDatabaseV1Store();
+const dbGroupStore = useDBGroupStore();
 const {
   changelist,
   project,
@@ -120,10 +154,12 @@ const {
 const state = reactive<LocalState>({
   selectedDatabaseNameList: [],
   changeType: "DDL",
+  changeSource: "DATABASE",
   isGenerating: false,
 });
 
 const { ready } = useDatabaseV1List(project.value.name);
+const { dbGroupList } = useDBGroupListByProject(project.value.name);
 
 const databaseList = computed(() => {
   let list: ComposedDatabase[] = [];
@@ -157,8 +193,11 @@ const flattenSelectedDatabaseNameList = computed(() => {
 
 const nextButtonErrors = computed(() => {
   const errors: string[] = [];
-
-  if (flattenSelectedDatabaseNameList.value.length === 0) {
+  if (
+    (state.changeSource === "DATABASE" &&
+      flattenSelectedDatabaseNameList.value.length === 0) ||
+    (state.changeSource === "GROUP" && !state.selectedDatabaseGroup)
+  ) {
     errors.push(t("changelist.error.select-at-least-one-database"));
   }
   return errors;
@@ -170,7 +209,9 @@ const handleClickNext = async () => {
     const databaseList = flattenSelectedDatabaseNameList.value.map((name) =>
       databaseStore.getDatabaseByName(name)
     );
-
+    const databaseGroup = DatabaseGroup.fromPartial({
+      ...dbGroupStore.getDBGroupByName(state.selectedDatabaseGroup || ""),
+    });
     const query: Record<string, any> = {
       template:
         state.changeType === "DDL"
@@ -178,12 +219,18 @@ const handleClickNext = async () => {
           : "bb.issue.database.data.update",
       name: generateIssueTitle(
         "bb.issue.database.schema.update",
-        databaseList.map((db) => db.databaseName)
+        state.changeSource === "DATABASE"
+          ? databaseList.map((db) => db.databaseName)
+          : [databaseGroup?.databasePlaceholder]
       ),
       changelist: changelist.value.name,
-      databaseList: databaseList.map((db) => db.name).join(","),
       description: `Apply changelist [${changelist.value.description}]`,
     };
+    if (state.changeSource === "DATABASE") {
+      query.databaseList = databaseList.map((db) => db.name).join(",");
+    } else {
+      query.databaseGroupName = state.selectedDatabaseGroup;
+    }
 
     router.push({
       name: PROJECT_V1_ROUTE_ISSUE_DETAIL,
--- frontend/src/components/DatabaseGroup/ProjectDatabaseGroupPanel.vue
+++ frontend/src/components/DatabaseGroup/ProjectDatabaseGroupPanel.vue
@@ -3,13 +3,10 @@
     <FeatureAttention feature="bb.feature.database-grouping" />
 
     <DatabaseGroupDataTable
-      :database-group-list="databaseGroupList"
+      :database-group-list="dbGroupList"
       :custom-click="true"
-      :show-selection="false"
-      :show-project="false"
-      :loading="isLoading"
+      :loading="!ready"
       @row-click="handleDatabaseGroupClick"
-      @edit="handleEditDatabaseGroup"
     />
   </div>
 
@@ -28,12 +25,11 @@
 </template>
 
 <script lang="ts" setup>
-import { cloneDeep } from "lodash-es";
-import { computed, onMounted, reactive, ref } from "vue";
+import { reactive } from "vue";
 import { useRouter } from "vue-router";
 import DatabaseGroupDataTable from "@/components/DatabaseGroup/DatabaseGroupDataTable.vue";
 import { PROJECT_V1_ROUTE_DATABASE_GROUP_DETAIL } from "@/router/dashboard/projectV1";
-import { useDBGroupStore, hasFeature } from "@/store";
+import { useDBGroupListByProject } from "@/store";
 import type { ComposedDatabaseGroup, ComposedProject } from "@/types";
 import { FeatureAttention, FeatureModal } from "../FeatureGuard";
 import DatabaseGroupPanel from "./DatabaseGroupPanel.vue";
@@ -49,25 +45,11 @@ const props = defineProps<{
 }>();
 
 const router = useRouter();
-const dbGroupStore = useDBGroupStore();
 const state = reactive<LocalState>({
   showFeatureModal: false,
   showDatabaseGroupPanel: false,
 });
-const isLoading = ref(true);
-
-const hasDatabaseGroupFeature = computed(() => {
-  return hasFeature("bb.feature.database-grouping");
-});
-
-const databaseGroupList = computed(() => {
-  return dbGroupStore.getDBGroupListByProjectName(props.project.name);
-});
-
-onMounted(async () => {
-  await dbGroupStore.getOrFetchDBGroupListByProjectName(props.project.name);
-  isLoading.value = false;
-});
+const { dbGroupList, ready } = useDBGroupListByProject(props.project.name);
 
 const handleDatabaseGroupClick = (
   event: MouseEvent,
@@ -84,14 +66,5 @@ const handleDatabaseGroupClick = (
   } else {
     router.push(url);
   }
-};
-
-const handleEditDatabaseGroup = (databaseGroup: ComposedDatabaseGroup) => {
-  if (!hasDatabaseGroupFeature.value) {
-    state.showFeatureModal = true;
-    return;
-  }
-  state.editingDatabaseGroup = cloneDeep(databaseGroup);
-  state.showDatabaseGroupPanel = true;
 };
 </script>
