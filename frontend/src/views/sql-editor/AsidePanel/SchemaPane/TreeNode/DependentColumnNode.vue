37ca003e8ee93e299c8de0d23a483543e09b0bfb
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-09-24 10:26:21 +0800

    feat: click dependentColumn node BYT-6387

--- frontend/src/views/sql-editor/AsidePanel/SchemaPane/TreeNode/DependentColumnNode.vue
+++ frontend/src/views/sql-editor/AsidePanel/SchemaPane/TreeNode/DependentColumnNode.vue
@@ -3,26 +3,12 @@
     <template #icon>
       <ColumnIcon class="w-4 h-4" />
     </template>
-    <template #suffix>
-      <div
-        class="flex items-center justify-end gap-1 overflow-hidden whitespace-nowrap shrink-0"
-      >
-        <NButton size="tiny" text @click.stop="goRef">
-          <template #icon>
-            <FileSymlinkIcon class="w-4 h-4" />
-          </template>
-        </NButton>
-      </div>
-    </template>
   </CommonNode>
 </template>
 
 <script setup lang="ts">
-import { FileSymlinkIcon } from "lucide-vue-next";
-import { NButton } from "naive-ui";
 import { computed } from "vue";
 import { ColumnIcon } from "@/components/Icon";
-import { useEditorPanelContext } from "@/views/sql-editor/EditorPanel";
 import type { TreeNode } from "../common";
 import CommonNode from "./CommonNode.vue";
 
@@ -30,7 +16,6 @@ const props = defineProps<{
   node: TreeNode;
   keyword: string;
 }>();
-const { updateViewState } = useEditorPanelContext();
 
 const target = computed(
   () => (props.node as TreeNode<"dependent-column">).meta.target
@@ -42,16 +27,4 @@ const text = computed(() => {
   if (schema) parts.unshift(schema);
   return parts.join(".");
 });
-
-const goRef = () => {
-  const { schema, table, column } = target.value.dependentColumn;
-  updateViewState({
-    view: "TABLES",
-    schema,
-    detail: {
-      table,
-      column,
-    },
-  });
-};
 </script>
