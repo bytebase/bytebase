15b020a08b886bc1a0638b061a9622dac22b7594
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-09-27 11:00:20 +0800

    feat(sql-editor): mass query result data rendering in SQL Editor (#13880)
    * chore: bi-directional virtual scroll
    
    * chore: double click
    
    * chore: style
    
    * feat: guess column width by characters
    
    * chore: const use the same logic
    
    Former-commit-id: e880e0b49b7f0a89ca707feaf5f146cf2357c716
    Former-commit-id: 89afc24696d095b505e6241ece35b285d64e7aa9

--- frontend/src/views/sql-editor/EditorCommon/ResultView/DataTableLite/useTableResize.ts
+++ frontend/src/views/sql-editor/EditorCommon/ResultView/DataTableLite/useTableResize.ts
@@ -1,13 +1,18 @@
+import type { Header, Table } from "@tanstack/vue-table";
 import { useEventListener, usePointer } from "@vueuse/core";
-import { reject, sumBy } from "lodash-es";
-import type { Ref } from "vue";
+import { reject } from "lodash-es";
+import stringWidth from "string-width";
+import type { MaybeRef, Ref } from "vue";
 import { computed, onBeforeUnmount, reactive, unref, watch } from "vue";
-import { minmax } from "@/utils";
+import type { QueryRow, RowValue } from "@/types/proto/v1/sql_service";
+import { extractSQLRowValue, minmax, wrapRefAsPromise } from "@/utils";
 
-const MAX_AUTO_ADJUST_SCAN_ROWS = 20;
-const MAX_AUTO_ADJUST_COLUMNS = 20;
+const MAX_AUTO_ADJUST_ROW_COUNT = 20;
+const MAX_AUTO_ADJUST_COL_COUNT = 50;
 
 export type TableResizeOptions = {
+  table: MaybeRef<Table<QueryRow>>;
+  containerWidth: Ref<number>;
   scrollerRef: Ref<HTMLElement | undefined>;
   queryTableHeaderElement: () => HTMLElement | undefined;
   queryTableBodyElement: () => HTMLElement | undefined;
@@ -41,90 +46,156 @@ const useTableResize = (options: TableResizeOptions) => {
   });
   const pointer = usePointer();
   const scroller = options.scrollerRef;
-
-  const containerWidth = computed(() => {
-    return scroller.value?.scrollWidth || 0;
-  });
+  const layoutReady = wrapRefAsPromise(
+    computed(() => options.containerWidth.value > 0),
+    true
+  );
 
   const normalizeWidth = (width: number) => {
     if (state.columns.length === 1) {
       // When there is only one column, display it with full width.
       // minus 1px to avoid unexpected horizontal scrollbar.
-      return containerWidth.value - 1;
+      return options.containerWidth.value - 1;
     }
     return minmax(width, options.minWidth, options.maxWidth);
   };
 
+  const maxAutoAdjustColCount = computed(() => {
+    return Math.min(
+      options.columnCount.value,
+      Math.ceil(options.containerWidth.value / options.minWidth),
+      MAX_AUTO_ADJUST_COL_COUNT
+    );
+  });
+  const maxAutoAdjustRowCount = computed(() => {
+    return Math.min(
+      unref(options.table).getRowCount(),
+      MAX_AUTO_ADJUST_ROW_COUNT
+    );
+  });
+
   const reset = () => {
-    state.drag = undefined;
+    layoutReady.then(() => {
+      // reset state
+      state.drag = undefined;
+      state.autoAdjusting.clear();
+      state.columns = new Array(options.columnCount.value);
 
-    state.autoAdjusting.clear();
+      // set initial column widths
+      const indexList: number[] = [];
+      for (let index = 0; index < options.columnCount.value; index++) {
+        if (index < maxAutoAdjustColCount.value) {
+          // for columns that will be rendered in the first screen
+          // set their widths to minWidth for further render-based auto-width
+          state.columns[index] = {
+            width: options.minWidth,
+          };
+          indexList.push(index);
+        } else {
+          // for columns that won't be rendered in the first screen
+          // guess their widths by characters
+          state.columns[index] = {
+            width: guessColumnWidth(index),
+          };
+        }
+      }
 
-    const columnCount = unref(options.columnCount);
-    state.columns = new Array(columnCount);
-
-    const indexList: number[] = [];
-    for (let i = 0; i < columnCount; i++) {
-      indexList.push(i);
-      state.columns[i] = {
-        width: i < MAX_AUTO_ADJUST_COLUMNS ? 0 : options.defaultWidth, // For auto adjust below
-      };
-    }
-
-    // Calculate a friendly width for every column when the first render happened.
-    const scanner = async (n = 0) => {
-      // Wait for several frames for the initial render of the table
-      requestAnimationFrame(() => {
-        const headerTable = options.queryTableHeaderElement();
-        const bodyTable = options.queryTableBodyElement();
-        if (headerTable && bodyTable) {
-          autoAdjustColumnWidth(
-            indexList.slice(0, MAX_AUTO_ADJUST_COLUMNS)
-          ).catch(() => {
-            for (let i = 0; i < state.columns.length; i++) {
-              state.columns[i].width = options.defaultWidth;
+      // Calculate a friendly width for every column when the first render happened.
+      const scanner = async (n = 0) => {
+        // Wait for several frames for the initial render of the table
+        requestAnimationFrame(() => {
+          const headerTable = options.queryTableHeaderElement();
+          const bodyTable = options.queryTableBodyElement();
+          const trList = [
+            ...Array.from(headerTable?.querySelectorAll("tr") || []),
+            ...Array.from(
+              bodyTable?.querySelectorAll("tr.n-data-table-tr") || []
+            ),
+          ] as HTMLElement[];
+          if (!headerTable || !bodyTable || trList.length === 0) {
+            return scanner(n + 1);
+          }
+          autoAdjustColumnWidth(indexList).catch(() => {
+            // auto adjust failed, fallback to guessing column width.
+            for (let index = 0; index < state.columns.length; index++) {
+              state.columns[index].width = guessColumnWidth(index);
             }
           });
-          return;
-        }
-        scanner(n + 1);
-      });
-    };
-    scanner();
+        });
+      };
+      scanner();
+    });
   };
 
   // Automatically estimate the width of a column.
   // Like double-clicking a cell border of Excel.
   // Able to estimate multiple columns in a time.
-  const autoAdjustColumnWidth = (indexList: number[]): Promise<number[]> => {
+  const autoAdjustColumnWidth = (
+    indexList: number[]
+  ): Promise<Map<number, number>> => {
     return new Promise((resolve) => {
+      if (indexList.length === 0) {
+        return resolve(new Map());
+      }
+
       const headerTable = options.queryTableHeaderElement();
       const bodyTable = options.queryTableBodyElement();
       const trList = [
         ...Array.from(headerTable?.querySelectorAll("tr") || []),
-        ...Array.from(bodyTable?.querySelectorAll("tr") || []),
+        ...Array.from(
+          bodyTable?.querySelectorAll("tr.n-data-table-tr") || []
+        ).slice(0, maxAutoAdjustRowCount.value), // Do not adjust too many rows
       ] as HTMLElement[];
+      console.debug(
+        "[autoAdjustColumnWidth]",
+        indexList.join("|"),
+        `(${trList.length} rows)`
+      );
       if (!headerTable || !bodyTable || trList.length === 0) {
         return reject(undefined);
       }
 
-      const numScanRows = Math.min(trList.length, MAX_AUTO_ADJUST_SCAN_ROWS);
-      const cellsMapByColumn = new Map<number, HTMLElement[]>();
+      const cellsMapByColumn = new Map<number, Array<HTMLElement>>();
       indexList.forEach((index) => {
-        const cells: HTMLElement[] = [];
-        for (let row = 0; row < numScanRows; row++) {
+        const cells: Array<HTMLElement> = [];
+        for (let row = 0; row < trList.length; row++) {
           const tr = trList[row];
-          const cell = tr.children[index] as HTMLElement;
-          if (cell && ["th", "td"].includes(cell.tagName.toLowerCase())) {
+          const children = Array.from(tr.children).filter((child) => {
+            return isNDataTableCell(child);
+          });
+          if (children.length === 0) {
+            return;
+          }
+          // When the table is scrolled horizontally, some left columns will not
+          // be actually rendered.
+          // So we need to find out the col index of the first rendered column
+          // to get the column with offset.
+          const first = children[0];
+          const offset = parseInt(
+            first.getAttribute("data-col-key") || "0",
+            10
+          );
+          const cell = children[index - offset];
+          if (cell) {
             cells.push(cell);
           }
         }
-        cellsMapByColumn.set(index, cells);
-        return cells;
+        if (cells.length > 0) {
+          cellsMapByColumn.set(index, cells);
+        }
       });
+      console.debug(
+        "[autoAdjustColumnWidth] found cells",
+        Array.from(cellsMapByColumn.entries())
+          .map(([index, cells]) => `${index}(${cells.length})`)
+          .join("|")
+      );
 
       // Update the cells' and table's style to estimate the width.
-      indexList.forEach((colIndex) => state.autoAdjusting.add(colIndex));
+      for (const index of cellsMapByColumn.keys()) {
+        state.autoAdjusting.add(index);
+      }
+
       for (const [_index, cells] of cellsMapByColumn.entries()) {
         cells.forEach((cell) => {
           cell.style.whiteSpace = "nowrap";
@@ -139,42 +210,57 @@ const useTableResize = (options: TableResizeOptions) => {
       headerTable.style.width = "auto";
       bodyTable.style.width = "auto";
 
-      // Wait for the next render frame.
-      requestAnimationFrame(() => {
-        // Read the rendered widths.
-        const widthList = indexList.map((index) => {
-          const cells = cellsMapByColumn.get(index) ?? [];
-          const stretchedWidths = cells.map((cell) => getElementWidth(cell));
-          const finalWidth = normalizeWidth(Math.max(...stretchedWidths));
-
-          const column = state.columns[index];
-          if (column) {
-            // Sometimes the `columns` is out-of-sync with the `indexList`
-            // so we need to detect and suppress errors here.
-            // Only occurs in dev hot reload mode.
-            column.width = finalWidth;
-          }
-
-          return finalWidth;
-        });
-
-        // Reset the cells' and table's style.
-        for (const [_index, cells] of cellsMapByColumn.entries()) {
-          cells.forEach((cell) => {
-            cell.style.whiteSpace = "";
-            cell.style.overflow = "";
-            cell.style.width = "";
-            cell.style.maxWidth = "";
-            cell.style.minWidth = "";
-          });
+      // Read the rendered widths.
+      const widthMapByColumn = new Map<number, number>();
+      indexList.forEach((index) => {
+        const cells = cellsMapByColumn.get(index);
+        if (!cells) {
+          return;
         }
-        headerTable.style.width = headerTableWidthBackup;
-        bodyTable.style.width = bodyTableWidthBackup;
-        state.autoAdjusting.clear();
 
-        resolve(widthList);
-        // Style bindings will be automatically updated next frame.
+        const stretchedWidths = cells.map((cell) => getElementWidth(cell));
+        const finalWidth = normalizeWidth(Math.max(...stretchedWidths));
+        console.debug(`col#${index}`, stretchedWidths.join("|"), finalWidth);
+
+        widthMapByColumn.set(index, finalWidth);
       });
+
+      console.debug(
+        Array.from(widthMapByColumn.entries())
+          .map(([index, width]) => `${index}:${width}`)
+          .join("|")
+      );
+
+      indexList.forEach((index) => {
+        const width = widthMapByColumn.get(index) ?? guessColumnWidth(index);
+
+        const column = state.columns[index];
+        if (!column) {
+          // Sometimes the `columns` is out-of-sync with the `indexList`
+          // so we need to detect and suppress errors here.
+          // Only occurs in dev hot reload mode.
+          return;
+        }
+        column.width = width;
+      });
+
+      // Reset the cells' and table's style.
+      for (const [_index, cells] of cellsMapByColumn.entries()) {
+        cells.forEach((cell) => {
+          if (!cell) return;
+          cell.style.whiteSpace = "";
+          cell.style.overflow = "";
+          cell.style.width = "";
+          cell.style.maxWidth = "";
+          cell.style.minWidth = "";
+        });
+      }
+      headerTable.style.width = headerTableWidthBackup;
+      bodyTable.style.width = bodyTableWidthBackup;
+      state.autoAdjusting.clear();
+
+      resolve(widthMapByColumn);
+      // Style bindings will be automatically updated next frame.
     });
   };
 
@@ -212,28 +298,66 @@ const useTableResize = (options: TableResizeOptions) => {
     }
   });
 
-  const getCellProps = (index: number) => {
-    const column = state.columns[index];
-    if (!column) return {};
-    return {
-      style: {
-        width: `${column.width}px`,
-      },
-      class: "truncate",
-      "data-index": index,
-    };
+  const getColumnWidth = (index: number) => {
+    return state.columns[index]?.width ?? options.defaultWidth;
   };
 
-  const getTableProps = () => {
-    const width =
-      state.autoAdjusting.size > 0
-        ? "auto"
-        : `calc(min(100%, ${sumBy(state.columns, (col) => col.width) + 2}px))`;
-    return {
-      style: {
-        width,
-      },
+  const totalWidth = computed(() => {
+    let totalWidth = 0;
+    for (let index = 0; index < options.columnCount.value; index++) {
+      const columnWidth = state.autoAdjusting.has(index)
+        ? options.maxWidth
+        : getColumnWidth(index);
+      totalWidth += columnWidth;
+    }
+    return totalWidth;
+  });
+
+  const getTableRenderWidth = () => {
+    return Math.min(options.containerWidth.value, totalWidth.value + 2);
+  };
+
+  const getTableScrollWidth = () => {
+    return totalWidth.value;
+  };
+
+  const guessColumnWidth = (
+    index: number,
+    opts = {
+      em: 8,
+      padding: 8,
+    }
+  ) => {
+    const table = unref(options.table);
+    const maxAutoAdjustRowCount = Math.min(
+      table.getRowCount(),
+      MAX_AUTO_ADJUST_ROW_COUNT
+    );
+
+    const charsWidth = (content: string) => {
+      return stringWidth(content) * opts.em + opts.padding * 2;
     };
+
+    const widths: number[] = [];
+
+    const header = table.getFlatHeaders()[index] as Header<QueryRow, RowValue>;
+    if (header) {
+      const content = String(header.column.columnDef.header);
+      const width = charsWidth(content) + 4 + 16; // plus sort icon and gap
+      widths.push(width);
+    }
+
+    for (let r = 0; r < maxAutoAdjustRowCount; r++) {
+      const row = table.getRowModel().rows[r];
+      const cell = row?.getVisibleCells()[index];
+      if (!cell) continue;
+      const value = cell.getValue() as RowValue;
+      const content = String(extractSQLRowValue(value).plain);
+      widths.push(charsWidth(content));
+    }
+
+    const guessed = Math.max(...widths);
+    return normalizeWidth(guessed);
   };
 
   onBeforeUnmount(() => {
@@ -241,10 +365,12 @@ const useTableResize = (options: TableResizeOptions) => {
   });
 
   return {
-    reset,
     state,
-    getCellProps,
-    getTableProps,
+    layoutReady,
+    reset,
+    getColumnWidth,
+    getTableRenderWidth,
+    getTableScrollWidth,
     startResizing,
     autoAdjustColumnWidth,
   };
@@ -258,7 +384,7 @@ const scrollMaxX = (elem: HTMLElement | undefined) => {
   elem.scrollTo(max, elem.scrollTop);
 };
 
-const getElementWidth = (elem: HTMLElement) => {
+const getElementWidth = (elem: HTMLElement | undefined) => {
   if (!elem) return 0;
   const rect = elem.getBoundingClientRect();
   return rect.width;
@@ -270,13 +396,20 @@ const toggleDragStyle = (
 ) => {
   if (on) {
     // Prevent text selection while dragging.
-    element.value?.classList.add("select-none");
+    document.body.classList.add("select-none");
     // Set the cursor style.
     document.body.classList.add("cursor-col-resize");
   } else {
     requestAnimationFrame(() => {
-      element.value?.classList.remove("select-none");
+      document.body.classList.remove("select-none");
       document.body.classList.remove("cursor-col-resize");
     });
   }
+};
+
+const isNDataTableCell = (elem: Element): elem is HTMLElement => {
+  return (
+    elem.classList.contains("n-data-table-th") ||
+    elem.classList.contains("n-data-table-td")
+  );
 };
