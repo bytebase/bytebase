82b27065219d4c6d61464be62eeb505915ffe641
p0ny <p0uy@outlook.com>
2024-09-27 11:04:02 +0800

    revision table

---
+++ backend/migrator/migration/dev/2024092400001##revision.sql
@@ -0,0 +1,8 @@
+CREATE TABLE revision (
+    id BIGSERIAL PRIMARY KEY,
+    instance_id INTEGER NOT NULL REFERENCES instance (id),
+    database_id INTEGER NOT NULL REFERENCES db (id),
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
+    payload JSONB NOT NULL DEFAULT '{}'
+);
