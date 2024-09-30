1b431905209a4e7e501e0fb2ea6238c8b993712a
ecmadao <wlec@outlook.com>
2024-09-30 09:53:57 +0800

    chore: issue and permission (#13903)
    * chore: issue and permission
    
    * fix: eslint
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 1e9549236dafce1e059b2f3db95576620e4ed58a

---
+++ frontend/src/views/project/ProjectLandingPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div />
+</template>
+
+<script lang="ts" setup>
+import { watchEffect, computed } from "vue";
+import { useRouter } from "vue-router";
+import {
+  PROJECT_V1_ROUTE_ISSUES,
+  PROJECT_V1_ROUTE_DATABASES,
+} from "@/router/dashboard/projectV1";
+import { useProjectV1Store } from "@/store";
+import { projectNamePrefix } from "@/store/modules/v1/common";
+import { hasProjectPermissionV2 } from "@/utils";
+
+const props = defineProps<{
+  projectId: string;
+}>();
+
+const projectName = computed(() => `${projectNamePrefix}${props.projectId}`);
+const projectStore = useProjectV1Store();
+const router = useRouter();
+
+watchEffect(() => {
+  const project = projectStore.getProjectByName(projectName.value);
+  if (hasProjectPermissionV2(project, "bb.issues.list")) {
+    router.replace({
+      name: PROJECT_V1_ROUTE_ISSUES,
+    });
+  } else {
+    router.replace({
+      name: PROJECT_V1_ROUTE_DATABASES,
+    });
+  }
+});
+</script>
