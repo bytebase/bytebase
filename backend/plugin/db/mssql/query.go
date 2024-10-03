dc8332f08f9f7fdae2fa7a8dfcb70b08cef0b953
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2024-10-02 18:10:46 -0700

    chore: mssql time (#13993)
    Former-commit-id: 73ad18c775032c1220f02a11df10f3ab9290cf66
    Former-commit-id: c01dc1f9717ca4d6026c57748128b2c19382760e

--- backend/plugin/db/mssql/query.go
+++ backend/plugin/db/mssql/query.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/antlr4-go/antlr/v4"
 	"github.com/pkg/errors"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	tsql "github.com/bytebase/tsql-parser"
 	mssqldb "github.com/microsoft/go-mssqldb"
@@ -46,8 +47,10 @@ func makeValueByTypeName(typeName string, _ *sql.ColumnType) any {
 	// // Source values of type [time.Time] may be scanned into values of type
 	// *time.Time, *interface{}, *string, or *[]byte. When converting to
 	// the latter two, [time.RFC3339Nano] is used.
-	case "SMALLDATETIME", "DATETIME", "DATETIME2", "DATE", "TIME", "DATETIMEOFFSET":
+	case "SMALLDATETIME", "DATETIME", "DATETIME2", "DATE", "TIME":
 		return new(sql.NullString)
+	case "DATETIMEOFFSET":
+		return new(sql.NullTime)
 	case "IMAGE":
 		return new([]byte)
 	case "BINARY":
@@ -58,10 +61,19 @@ func makeValueByTypeName(typeName string, _ *sql.ColumnType) any {
 	return new(sql.NullString)
 }
 
-func convertValue(_ string, value any) *v1pb.RowValue {
+func convertValue(typeName string, value any) *v1pb.RowValue {
 	switch raw := value.(type) {
 	case *sql.NullString:
 		if raw.Valid {
+			if typeName == "DATETIME" || typeName == "DATETIME2" || typeName == "SMALLDATETIME" {
+				// Convert "2024-09-24T03:23:28.921281Z" to "2024-09-24 03:23:28.921281"
+				timestampWithoutTimezone := strings.ReplaceAll(strings.ReplaceAll(raw.String, "T", " "), "Z", "")
+				return &v1pb.RowValue{
+					Kind: &v1pb.RowValue_StringValue{
+						StringValue: timestampWithoutTimezone,
+					},
+				}
+			}
 			return &v1pb.RowValue{
 				Kind: &v1pb.RowValue_StringValue{
 					StringValue: raw.String,
@@ -110,6 +122,12 @@ func convertValue(_ string, value any) *v1pb.RowValue {
 				Kind: &v1pb.RowValue_StringValue{
 					StringValue: raw.UUID.String(),
 				},
+			}
+		}
+	case *sql.NullTime:
+		if raw.Valid {
+			return &v1pb.RowValue{
+				Kind: &v1pb.RowValue_TimestampValue{TimestampValue: timestamppb.New(raw.Time)},
 			}
 		}
 	}
