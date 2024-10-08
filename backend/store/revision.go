1ad2045c99ed33e3c2e7ee67414445e21018eb04
p0ny <xz@bytebase.com>
2024-10-08 12:30:12 +0900

    feat: revision (#13902)
    Former-commit-id: 1c60c19e58fbfa5cc8b8ae20f3c882edd36d3634
    Former-commit-id: 98810fe4f6553b9a9c162552824991db11910324

---
+++ backend/store/revision.go
@@ -0,0 +1,150 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/encoding/protojson"
+
+	"github.com/bytebase/bytebase/backend/common"
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+type RevisionMessage struct {
+	InstanceUID int
+	DatabaseUID int
+
+	Payload *storepb.RevisionPayload
+
+	// output only
+	UID         int64
+	CreatorUID  int
+	CreatedTime time.Time
+}
+
+type FindRevisionMessage struct {
+	InstanceUID int
+	DatabaseUID int
+
+	Limit  *int
+	Offset *int
+}
+
+func (s *Store) ListRevisions(ctx context.Context, find *FindRevisionMessage) ([]*RevisionMessage, error) {
+	query := `
+		SELECT
+			id,
+			instance_id,
+			database_id,
+			creator_id,
+			created_ts,
+			payload
+		FROM revision
+		WHERE instance_id = $1
+		AND database_id = $2
+	`
+
+	if v := find.Limit; v != nil {
+		query += fmt.Sprintf(" LIMIT %d", *v)
+	}
+	if v := find.Offset; v != nil {
+		query += fmt.Sprintf(" OFFSET %d", *v)
+	}
+
+	tx, err := s.db.BeginTx(ctx, &sql.TxOptions{ReadOnly: true})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to begin tx")
+	}
+	defer tx.Rollback()
+
+	rows, err := tx.QueryContext(ctx, query, find.InstanceUID, find.DatabaseUID)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to query context")
+	}
+	defer rows.Close()
+
+	var revisions []*RevisionMessage
+	for rows.Next() {
+		r := RevisionMessage{
+			Payload: &storepb.RevisionPayload{},
+		}
+		var p []byte
+		if err := rows.Scan(
+			&r.UID,
+			&r.InstanceUID,
+			&r.DatabaseUID,
+			&r.CreatorUID,
+			&r.CreatedTime,
+			&p,
+		); err != nil {
+			return nil, errors.Wrapf(err, "failed to scan")
+		}
+
+		if err := common.ProtojsonUnmarshaler.Unmarshal(p, r.Payload); err != nil {
+			return nil, errors.Wrapf(err, "failed to unmarshal")
+		}
+
+		revisions = append(revisions, &r)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, errors.Wrapf(err, "rows err")
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, errors.Wrapf(err, "failed to commit tx")
+	}
+
+	return revisions, nil
+}
+
+func (s *Store) CreateRevision(ctx context.Context, revision *RevisionMessage, creatorUID int) (*RevisionMessage, error) {
+	query := `
+		INSERT INTO revision (
+			instance_id,
+			database_id,
+			creator_id,
+			payload
+		) VALUES (
+		 	$1,
+			$2,
+			$3,
+			$4
+		)
+		RETURNING id, created_ts
+	`
+
+	p, err := protojson.Marshal(revision.Payload)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to marshal revision payload")
+	}
+
+	tx, err := s.db.BeginTx(ctx, &sql.TxOptions{ReadOnly: true})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to begin tx")
+	}
+	defer tx.Rollback()
+
+	var id int64
+	var createdTime time.Time
+	if err := tx.QueryRowContext(ctx, query,
+		revision.InstanceUID,
+		revision.DatabaseUID,
+		creatorUID,
+		p,
+	).Scan(&id, &createdTime); err != nil {
+		return nil, errors.Wrapf(err, "failed to query and scan")
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, errors.Wrapf(err, "failed to commit tx")
+	}
+
+	revision.UID = id
+	revision.CreatedTime = createdTime
+
+	return revision, nil
+}
