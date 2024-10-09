5a22ef67db5644c5d1c5b43355710b4612c7058f
steven <stevenlgtm@gmail.com>
2024-10-09 10:59:59 +0800

    feat: add max execution time rule for mysql&mariadb

--- backend/plugin/advisor/sql_review.go
+++ backend/plugin/advisor/sql_review.go
@@ -131,6 +131,8 @@ const (
 	SchemaRuleStatementDisallowOfflineDDL = "statement.disallow-offline-ddl"
 	// SchemaRuleStatementDisallowCrossDBQueries disallow cross database queries.
 	SchemaRuleStatementDisallowCrossDBQueries = "statement.disallow-cross-db-queries"
+	// SchemaRuleStatementMaxExecutionTime enforce the maximum execution time.
+	SchemaRuleStatementMaxExecutionTime = "statement.max-execution-time"
 	// SchemaRuleTableRequirePK require the table to have a primary key.
 	SchemaRuleTableRequirePK SQLReviewRuleType = "table.require-pk"
 	// SchemaRuleTableNoFK require the table disallow the foreign key.
@@ -1479,6 +1481,10 @@ func getAdvisorTypeByRule(ruleType SQLReviewRuleType, engine storepb.Engine) (Ty
 	case SchemaRuleStatementDisallowCrossDBQueries:
 		if engine == storepb.Engine_MSSQL {
 			return MSSQLStatementDisallowCrossDBQueries, nil
+		}
+	case SchemaRuleStatementMaxExecutionTime:
+		if engine == storepb.Engine_MYSQL || engine == storepb.Engine_MARIADB {
+			return MySQLStatementMaxExecutionTime, nil
 		}
 	case SchemaRuleCommentLength:
 		if engine == storepb.Engine_POSTGRES {
