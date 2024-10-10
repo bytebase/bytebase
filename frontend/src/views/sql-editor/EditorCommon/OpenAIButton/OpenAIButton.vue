ddc086d431273b9c5b3dc0e2da581a988b07a631
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-10-10 17:23:32 +0800

    chore: tooltips when not configured

--- frontend/src/views/sql-editor/EditorCommon/OpenAIButton/OpenAIButton.vue
+++ frontend/src/views/sql-editor/EditorCommon/OpenAIButton/OpenAIButton.vue
@@ -1,30 +1,72 @@
 <template>
-  <NPopselect
-    v-if="showButton"
-    placement="bottom-end"
-    :options="options"
-    @update:value="handleSelect"
-  >
-    <Button v-bind="$attrs" @click="handleClickButton" />
-    <template #header>
-      <span class="font-semibold">{{ $t("plugin.ai.ai-assistant") }}</span>
+  <template v-if="showButton">
+    <template v-if="enabled">
+      <NPopselect
+        v-if="options.length > 0"
+        placement="bottom-end"
+        :options="options"
+        @update:value="handleSelect"
+      >
+        <Button v-bind="$attrs" @click="handleClickButton" />
+        <template #header>
+          <span class="font-semibold">{{ $t("plugin.ai.ai-assistant") }}</span>
+        </template>
+      </NPopselect>
+      <Button v-else v-bind="$attrs" @click="handleClickButton" />
     </template>
-  </NPopselect>
+    <NPopover v-if="!enabled" placement="bottom-end">
+      <template #trigger>
+        <Button v-bind="$attrs" :disabled="true" />
+      </template>
+      <template #default>
+        <div class="flex flex-col">
+          <div class="pb-1 border-b">
+            <span class="font-semibold">
+              {{ $t("plugin.ai.ai-assistant") }}
+            </span>
+          </div>
+          <div class="pt-2 max-w-[20rem] flex flex-col text-gray-500">
+            <p>
+              {{ $t("plugin.ai.not-configured.self") }}
+              <NButton
+                v-if="allowConfigure"
+                size="small"
+                text
+                type="primary"
+                class="inline-block"
+                @click="goConfigure"
+              >
+                {{ $t("plugin.ai.not-configured.go-to-configure") }}
+              </NButton>
+              <template v-else>
+                {{ $t("plugin.ai.not-configured.contact-admin-to-configure") }}
+              </template>
+            </p>
+          </div>
+        </div>
+      </template>
+    </NPopover>
+  </template>
 </template>
 
 <script setup lang="ts">
-import { NPopselect, type SelectOption } from "naive-ui";
+import { NPopselect, NPopover, type SelectOption, NButton } from "naive-ui";
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
+import { useRouter } from "vue-router";
 import type { ChatAction } from "@/plugins/ai";
 import { useAIContext } from "@/plugins/ai/logic";
 import * as promptUtils from "@/plugins/ai/logic/prompt";
+import { SETTING_ROUTE_WORKSPACE_GENERAL } from "@/router/dashboard/workspaceSetting";
+import { SQL_EDITOR_SETTING_GENERAL_MODULE } from "@/router/sqlEditor";
 import {
+  useAppFeature,
   useConnectionOfCurrentSQLEditorTab,
   useSettingV1Store,
   useSQLEditorTabStore,
 } from "@/store";
-import { nextAnimationFrame } from "@/utils";
+import { DatabaseChangeMode } from "@/types/proto/v1/setting_service";
+import { hasWorkspacePermissionV2, nextAnimationFrame } from "@/utils";
 import { useSQLEditorContext } from "../../context";
 import Button from "./Button.vue";
 
@@ -38,8 +80,11 @@ const props = defineProps<{
 }>();
 
 const { t } = useI18n();
+const router = useRouter();
 const tabStore = useSQLEditorTabStore();
 const settingV1Store = useSettingV1Store();
+const databaseChangeMode = useAppFeature("bb.feature.database-change-mode");
+
 const openAIKeySetting = computed(() =>
   settingV1Store.getSettingByName("bb.plugin.openai.key")
 );
@@ -73,12 +118,15 @@ const options = computed(() => {
 });
 
 const showButton = computed(() => {
-  return (
-    openAIKey.value &&
-    !tabStore.isDisconnected &&
-    tabStore.currentTab?.mode === "WORKSHEET" &&
-    options.value.length > 0
-  );
+  return !tabStore.isDisconnected && tabStore.currentTab?.mode === "WORKSHEET";
+});
+
+const enabled = computed(() => {
+  return openAIKey.value;
+});
+
+const allowConfigure = computed((): boolean => {
+  return hasWorkspacePermissionV2("bb.settings.set");
 });
 
 const handleSelect = async (action: ChatAction) => {
@@ -112,5 +160,15 @@ const handleSelect = async (action: ChatAction) => {
 
 const handleClickButton = () => {
   showAIPanel.value = true;
+};
+
+const goConfigure = () => {
+  router.push({
+    name:
+      databaseChangeMode.value === DatabaseChangeMode.EDITOR
+        ? SQL_EDITOR_SETTING_GENERAL_MODULE
+        : SETTING_ROUTE_WORKSPACE_GENERAL,
+    hash: "#ai-assistant",
+  });
 };
 </script>
