1746185f2b3b6c0f85ff1c41a3c079b7054d0e0c
p0ny <xz@bytebase.com>
2024-10-11 12:51:34 +0900

    chore: update (#14044)
    Former-commit-id: ab0b83bf5fc1a989113e9eed44970d84765c20c0

--- backend/migrator/migration/dev/20240924000001##revision.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS revision (
-    id BIGSERIAL PRIMARY KEY,
-    instance_id INTEGER NOT NULL REFERENCES instance (id),
-    database_id INTEGER NOT NULL REFERENCES db (id),
-    creator_id INTEGER NOT NULL REFERENCES principal (id),
-    created_ts TIMESTAMPTZ NOT NULL DEFAULT now(),
-    payload JSONB NOT NULL DEFAULT '{}'
-);
