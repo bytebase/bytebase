32897c3b289a3bceeb9589980cfb170c64ece3b2
p0ny <p0uy@outlook.com>
2024-10-14 10:57:19 +0800

    remove instance_id

--- backend/migrator/migration/dev/20241014000000##release.sql
+++ backend/migrator/migration/dev/20241014000000##release.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS revision (
     id BIGSERIAL PRIMARY KEY,
-    instance_id INTEGER NOT NULL REFERENCES instance (id),
     database_id INTEGER NOT NULL REFERENCES db (id),
     creator_id INTEGER NOT NULL REFERENCES principal (id),
     created_ts TIMESTAMPTZ NOT NULL DEFAULT now(),
