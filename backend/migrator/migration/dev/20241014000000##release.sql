dc95ad6627c32f840ec8871f8824b394f1e4bf94
p0ny <p0uy@outlook.com>
2024-10-14 10:41:49 +0800

    chore: dev release revision sql

---
+++ backend/migrator/migration/dev/20241014000000##release.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS revision (
+    id BIGSERIAL PRIMARY KEY,
+    instance_id INTEGER NOT NULL REFERENCES instance (id),
+    database_id INTEGER NOT NULL REFERENCES db (id),
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts TIMESTAMPTZ NOT NULL DEFAULT now(),
+    payload JSONB NOT NULL DEFAULT '{}'
+);
+
+CREATE TABLE IF NOT EXISTS release (
+    id BIGSERIAL PRIMARY KEY,
+    project_id INTEGER NOT NULL REFERENCES project (id),
+    creator_id INTEGER NOT NULL REFERENCES principal (id),
+    created_ts TIMESTAMPTZ NOT NULL DEFAULT now(),
+    payload JSONB NOT NULL DEFAULT '{}'
+);
