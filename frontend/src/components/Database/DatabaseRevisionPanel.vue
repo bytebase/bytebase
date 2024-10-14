6f53cad9cf0a2712dbf87b0593d1727ccdeab00f
boojack <stevenlgtm@gmail.com>
2024-10-14 16:59:43 +0800

    feat: add revisions tab to database detail page (#14083)
    * feat: add revisions tab to database detail page
    
    * chore: update
    
    Former-commit-id: 1202a3f26a79f543ead69c33eb72f5eb907f68bd

---
+++ frontend/src/components/Database/DatabaseRevisionPanel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col space-y-4" v-bind="$attrs">
+    <PagedRevisionTable
+      :database="database"
+      session-key="bb.paged-revision-table"
+    >
+      <template #table="{ list, loading }">
+        <RevisionDataTable
+          :key="`revision-table.${database.name}`"
+          :loading="loading"
+          :revisions="list"
+          :custom-click="true"
+          :show-selection="true"
+        />
+      </template>
+    </PagedRevisionTable>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PagedRevisionTable, RevisionDataTable } from "@/components/Revision";
+import type { ComposedDatabase } from "@/types";
+
+defineProps<{
+  database: ComposedDatabase;
+}>();
+</script>
