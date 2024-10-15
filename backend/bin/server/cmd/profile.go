6f6396d570e2be944396d005cc4e168eb180767b
p0ny <p0uy@outlook.com>
2024-10-15 15:34:19 +0800

    fix lint

--- backend/bin/server/cmd/profile.go
+++ backend/bin/server/cmd/profile.go
@@ -17,22 +17,22 @@ func getBaseProfile(dataDir string) *config.Profile {
 	}
 
 	return &config.Profile{
-		ExternalURL:        flags.externalURL,
-		Port:               flags.port,     // Using flags.port as our gRPC server port.
-		DatastorePort:      flags.port + 2, // Using flags.port + 2 as our datastore port.
-		SampleDatabasePort: sampleDatabasePort,
-		Readonly:           flags.readonly,
-		SaaS:               flags.saas,
-		Debug:              flags.debug,
-		DataDir:            dataDir,
-		ResourceDir:        common.GetResourceDir(dataDir),
-		DemoName:           flags.demoName,
-		Version:            version,
-		GitCommit:          gitcommit,
-		PgURL:              flags.pgURL,
-		DeployID:           uuid.NewString()[:8],
-		LastActiveTs:       time.Now().Unix(),
-		Lsp:                flags.lsp,
+		ExternalURL:          flags.externalURL,
+		Port:                 flags.port,     // Using flags.port as our gRPC server port.
+		DatastorePort:        flags.port + 2, // Using flags.port + 2 as our datastore port.
+		SampleDatabasePort:   sampleDatabasePort,
+		Readonly:             flags.readonly,
+		SaaS:                 flags.saas,
+		Debug:                flags.debug,
+		DataDir:              dataDir,
+		ResourceDir:          common.GetResourceDir(dataDir),
+		DemoName:             flags.demoName,
+		Version:              version,
+		GitCommit:            gitcommit,
+		PgURL:                flags.pgURL,
+		DeployID:             uuid.NewString()[:8],
+		LastActiveTs:         time.Now().Unix(),
+		Lsp:                  flags.lsp,
 		DevelopmentVersioned: flags.developmentVersioned,
 	}
 }
