c11cd516d3d65c36b8e2463d779a8e7c1eee8cab
h3n4l <oysterdays@gmail.com>
2024-10-17 17:23:07 +0800

    feat(query-span): bigquery query span access tables part happy path (#14122)
    * chore: bump google sql parser
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * feat: bigquery dignose
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * feat: query span access tables part happy path
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: update
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    ---------
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>

---
+++ backend/plugin/parser/bigquery/query_span.go
@@ -0,0 +1,29 @@
+package bigquery
+
+import (
+	"context"
+
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+func init() {
+	base.RegisterGetQuerySpan(storepb.Engine_BIGQUERY, GetQuerySpan)
+}
+
+// GetQuerySpan returns the query span for the given statement.
+func GetQuerySpan(
+	ctx context.Context,
+	gCtx base.GetQuerySpanContext,
+	statement, database, _ string,
+	// getDatabaseMetadata base.GetDatabaseMetadataFunc,
+	// listDatabaseFunc base.ListDatabaseNamesFunc,
+	ignoreCaseSensitive bool,
+) (*base.QuerySpan, error) {
+	q := newQuerySpanExtractor(database, gCtx, ignoreCaseSensitive)
+	querySpan, err := q.getQuerySpan(ctx, statement)
+	if err != nil {
+		return nil, err
+	}
+	return querySpan, nil
+}
---
+++ backend/plugin/parser/bigquery/query_span_test.go
@@ -0,0 +1,96 @@
+package bigquery
+
+import (
+	"context"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+
+	"github.com/bytebase/bytebase/backend/common"
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+	"github.com/bytebase/bytebase/backend/store/model"
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+func TestGetQuerySpan(t *testing.T) {
+	type testCase struct {
+		Description        string `yaml:"description,omitempty"`
+		Statement          string `yaml:"statement,omitempty"`
+		DefaultDatabase    string `yaml:"defaultDatabase,omitempty"`
+		IgnoreCaseSensitve bool   `yaml:"ignoreCaseSensitive,omitempty"`
+		// Metadata is the protojson encoded storepb.DatabaseSchemaMetadata,
+		// if it's empty, we will use the defaultDatabaseMetadata.
+		Metadata  string              `yaml:"metadata,omitempty"`
+		QuerySpan *base.YamlQuerySpan `yaml:"querySpan,omitempty"`
+	}
+
+	var (
+		record        = false
+		testDataPaths = []string{
+			"test-data/query-span/standard.yaml",
+		}
+	)
+
+	a := require.New(t)
+	for _, testDataPath := range testDataPaths {
+		testDataPath := testDataPath
+
+		yamlFile, err := os.Open(testDataPath)
+		a.NoError(err)
+
+		var testCases []testCase
+		byteValue, err := io.ReadAll(yamlFile)
+		a.NoError(err)
+		a.NoError(yamlFile.Close())
+		a.NoError(yaml.Unmarshal(byteValue, &testCases))
+
+		for i, tc := range testCases {
+			metadata := &storepb.DatabaseSchemaMetadata{}
+			a.NoErrorf(common.ProtojsonUnmarshaler.Unmarshal([]byte(tc.Metadata), metadata), "cases %d", i+1)
+			databaseMetadataGetter, databaseNameLister := buildMockDatabaseMetadataGetter([]*storepb.DatabaseSchemaMetadata{metadata})
+			result, err := GetQuerySpan(context.TODO(), base.GetQuerySpanContext{
+				GetDatabaseMetadataFunc: databaseMetadataGetter,
+				ListDatabaseNamesFunc:   databaseNameLister,
+			}, tc.Statement, tc.DefaultDatabase, "dbo", tc.IgnoreCaseSensitve)
+			a.NoErrorf(err, "statement: %s", tc.Statement)
+			resultYaml := result.ToYaml()
+			if record {
+				testCases[i].QuerySpan = resultYaml
+			} else {
+				a.Equalf(tc.QuerySpan, resultYaml, "statement: %s", tc.Statement)
+			}
+		}
+
+		if record {
+			byteValue, err := yaml.Marshal(testCases)
+			a.NoError(err)
+			err = os.WriteFile(testDataPath, byteValue, 0644)
+			a.NoError(err)
+		}
+	}
+}
+
+func buildMockDatabaseMetadataGetter(databaseMetadata []*storepb.DatabaseSchemaMetadata) (base.GetDatabaseMetadataFunc, base.ListDatabaseNamesFunc) {
+	return func(_ context.Context, _, databaseName string) (string, *model.DatabaseMetadata, error) {
+			m := make(map[string]*model.DatabaseMetadata)
+			for _, metadata := range databaseMetadata {
+				m[metadata.Name] = model.NewDatabaseMetadata(metadata)
+			}
+
+			if databaseMetadata, ok := m[databaseName]; ok {
+				return "", databaseMetadata, nil
+			}
+
+			return "", nil, errors.Errorf("database %q not found", databaseName)
+		}, func(_ context.Context, _ string) ([]string, error) {
+			var names []string
+			for _, metadata := range databaseMetadata {
+				names = append(names, metadata.Name)
+			}
+			return names, nil
+		}
+}
