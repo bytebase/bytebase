abad498cac81c6e6e16315a90970d3073f52cb4c
boojack <stevenlgtm@gmail.com>
2024-10-17 17:23:25 +0800

    feat(sql-review): table comment convention for oracle (#14121)
    * feat(sql-review): table comment convention for oracel
    
    * chore: update
    
    Former-commit-id: 054e028dba4e1f879e650a4669c026ffaa77aef4

--- backend/plugin/advisor/oracle/oracle_rules_test.go
+++ backend/plugin/advisor/oracle/oracle_rules_test.go
@@ -30,6 +30,7 @@ func TestOracleRules(t *testing.T) {
 		advisor.SchemaRuleIdentifierNoKeyword,
 		advisor.SchemaRuleIdentifierCase,
 		advisor.SchemaRuleStatementDisallowMixDDLDML,
+		advisor.SchemaRuleTableCommentConvention,
 		advisor.SchemaRuleStatementDisallowMixInDDL,
 		advisor.SchemaRuleStatementDisallowMixInDML,
 	}
