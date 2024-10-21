464c736925fa598430fe2cfbf25b8e2e4f5b40e7
p0ny <p0uy@outlook.com>
2024-10-21 10:48:54 +0800

    update sql, keep `comment`

--- backend/migrator/migration/prod/3.0/0000##migrate_ddl_dml_mix_rule.sql
+++ backend/migrator/migration/prod/3.0/0000##migrate_ddl_dml_mix_rule.sql
@@ -1,16 +1,16 @@
 WITH t1 AS (
-	SELECT review_config.id, jsonb_build_object('type', e.type, 'level', e.level, 'engine', e.engine, 'payload', e.payload) AS rule FROM review_config
+	SELECT review_config.id, jsonb_strip_nulls(jsonb_build_object('type', e.type, 'level', e.level, 'engine', e.engine, 'payload', e.payload, 'comment', e.comment)) AS rule FROM review_config
 	LEFT JOIN LATERAL (
-		SELECT x.type, x.level, x.engine, x.payload
-		FROM jsonb_to_recordset(review_config.payload->'sqlReviewRules') AS x(type TEXT, level TEXT, engine TEXT, payload TEXT)
+		SELECT x.type, x.level, x.engine, x.payload, x.comment
+		FROM jsonb_to_recordset(review_config.payload->'sqlReviewRules') AS x(type TEXT, level TEXT, engine TEXT, payload TEXT, comment TEXT)
 		WHERE type != 'statement.disallow-mix-ddl-dml'
 		UNION ALL 
-		SELECT 'statement.disallow-mix-in-ddl', x.level, x.engine, x.payload
-		FROM jsonb_to_recordset(review_config.payload->'sqlReviewRules') AS x(type TEXT, level TEXT, engine TEXT, payload TEXT)
+		SELECT 'statement.disallow-mix-in-ddl', x.level, x.engine, x.payload, x.comment
+		FROM jsonb_to_recordset(review_config.payload->'sqlReviewRules') AS x(type TEXT, level TEXT, engine TEXT, payload TEXT, comment TEXT)
 		WHERE type = 'statement.disallow-mix-ddl-dml'
 		UNION ALL 
-		SELECT 'statement.disallow-mix-in-dml', x.level, x.engine, x.payload
-		FROM jsonb_to_recordset(review_config.payload->'sqlReviewRules') AS x(type TEXT, level TEXT, engine TEXT, payload TEXT)
+		SELECT 'statement.disallow-mix-in-dml', x.level, x.engine, x.payload, x.comment
+		FROM jsonb_to_recordset(review_config.payload->'sqlReviewRules') AS x(type TEXT, level TEXT, engine TEXT, payload TEXT, comment TEXT)
 		WHERE type = 'statement.disallow-mix-ddl-dml'
 	) AS e ON TRUE
 ), t2 AS (
