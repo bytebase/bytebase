43cc95e396b11e666071bee64d42a502bf4dcc07
steven <stevenlgtm@gmail.com>
2024-10-25 17:54:53 +0800

    chore: implement archive/unarchive release

--- frontend/src/components/Project/ProjectReleasesPanel.vue
+++ frontend/src/components/Project/ProjectReleasesPanel.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="flex flex-col gap-y-2">
-    <div class="relative min-h-[20rem]">
-      <PagedReleaseTable
-        :key="project.name"
-        :session-key="`project-${project.name}-releases`"
-        :project="project.name"
-        :page-size="50"
-      >
-        <template #table="{ releaseList, loading }">
-          <ReleaseDataTable
-            :bordered="true"
-            :loading="loading"
-            :release-list="releaseList"
-          />
-        </template>
-      </PagedReleaseTable>
-    </div>
+    <PagedReleaseTable
+      :key="project.name"
+      :session-key="`project-${project.name}-releases`"
+      :project="project.name"
+      :page-size="50"
+      :show-deleted="state.showArchived"
+    >
+      <template #table="{ releaseList, loading }">
+        <ReleaseDataTable
+          :bordered="true"
+          :loading="loading"
+          :release-list="releaseList"
+        />
+      </template>
+    </PagedReleaseTable>
+    <p>
+      <NCheckbox v-model:checked="state.showArchived">
+        <span class="textinfolabel">{{ $t("release.show-archived") }}</span>
+      </NCheckbox>
+    </p>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { NCheckbox } from "naive-ui";
+import { reactive } from "vue";
 import type { ComposedProject } from "@/types";
 import PagedReleaseTable from "../Release/PagedReleaseTable.vue";
 import ReleaseDataTable from "../Release/ReleaseDataTable.vue";
@@ -27,4 +33,12 @@ import ReleaseDataTable from "../Release/ReleaseDataTable.vue";
 defineProps<{
   project: ComposedProject;
 }>();
+
+interface LocalState {
+  showArchived: boolean;
+}
+
+const state = reactive<LocalState>({
+  showArchived: false,
+});
 </script>
