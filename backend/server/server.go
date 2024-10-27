65d1f2a8869de050f30efe8ef03873c1c874ea55
d-bytebase <d@bytebase.com>
2024-10-27 11:26:55 -0700

    chore: is docker

--- backend/server/server.go
+++ backend/server/server.go
@@ -121,6 +121,7 @@ func NewServer(ctx context.Context, profile *config.Profile) (*Server, error) {
 		profile:   profile,
 		startedTs: time.Now().Unix(),
 	}
+	fmt.Printf("Barny1: %v\n", profile.IsDocker)
 
 	// Display config
 	slog.Info("-----Config BEGIN-----")
