8363c75092950815a17365601f23030f1fa6ea1b
boojack <stevenlgtm@gmail.com>
2024-10-28 12:25:20 +0800

    chore: add tag for archived releases (#14212)
    Former-commit-id: 872ddcdbb8b30e1d524e15cce30c1fa32dad894f

--- frontend/src/components/IssueV1/components/Sidebar/ReleaseInfo.vue
+++ frontend/src/components/IssueV1/components/Sidebar/ReleaseInfo.vue
@@ -1,24 +1,32 @@
 <template>
   <div
     v-if="isValidReleaseName(release.name) && ready"
-    class="text-sm text-control-light flex space-x-1 items-center"
+    class="w-full text-sm text-control-light flex space-x-1 items-center"
   >
-    <PackageIcon class="w-4 h-auto" />
-    <EllipsisText>
-      <a :href="`/${release.name}`" target="_blank" class="normal-link">
-        <span>{{ release.title }}</span>
-      </a>
-    </EllipsisText>
+    <PackageIcon class="w-5 h-auto shrink-0" />
+    <a :href="`/${release.name}`" target="_blank" class="normal-link truncate">
+      {{ release.title }}
+    </a>
+    <NTag
+      v-if="release.state === State.DELETED"
+      class="shrink-0"
+      type="warning"
+      size="small"
+      round
+    >
+      {{ $t("common.archived") }}
+    </NTag>
   </div>
 </template>
 
 <script setup lang="ts">
 import { PackageIcon } from "lucide-vue-next";
+import { NTag } from "naive-ui";
 import { computed } from "vue";
-import EllipsisText from "@/components/EllipsisText.vue";
 import { useIssueContext } from "@/components/IssueV1";
 import { useReleaseByName } from "@/store";
 import { isValidReleaseName } from "@/types";
+import { State } from "@/types/proto/v1/common";
 
 const { issue } = useIssueContext();
 
