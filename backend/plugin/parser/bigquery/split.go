7638868552bad1d86c11f9077e998ba38b5485b9
h3n4l <oysterdays@gmail.com>
2024-10-31 15:01:18 +0800

    feat: bigquery spliter
    Signed-off-by: h3n4l <oysterdays@gmail.com>

---
+++ backend/plugin/parser/bigquery/split.go
@@ -0,0 +1,54 @@
+package bigquery
+
+import (
+	"strings"
+
+	"github.com/antlr4-go/antlr/v4"
+	parser "github.com/bytebase/google-sql-parser"
+
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+func init() {
+	base.RegisterSplitterFunc(storepb.Engine_BIGQUERY, SplitSQL)
+}
+
+func SplitSQL(statement string) ([]base.SingleSQL, error) {
+	lexer := parser.NewGoogleSQLLexer(antlr.NewInputStream(statement))
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	stream.Fill()
+
+	tokens := stream.GetAllTokens()
+	var buf []antlr.Token
+	var sqls []base.SingleSQL
+	for i, token := range tokens {
+		if i < len(tokens)-1 {
+			buf = append(buf, token)
+		}
+		if (token.GetTokenType() == parser.GoogleSQLLexerSEMI_SYMBOL || i == len(tokens)-1) && len(buf) > 0 {
+			bufStr := new(strings.Builder)
+			empty := true
+
+			for _, b := range buf {
+				bufStr.WriteString(b.GetText())
+				if b.GetChannel() != antlr.TokenHiddenChannel {
+					empty = false
+				}
+			}
+			line, col := base.FirstDefaultChannelTokenPosition(buf)
+			sqls = append(sqls, base.SingleSQL{
+				Text:                 bufStr.String(),
+				BaseLine:             buf[0].GetLine() - 1,
+				LastLine:             buf[len(buf)-1].GetLine() - 1,
+				LastColumn:           buf[len(buf)-1].GetColumn(),
+				FirstStatementLine:   line,
+				FirstStatementColumn: col,
+				Empty:                empty,
+			})
+			buf = nil
+			continue
+		}
+	}
+	return sqls, nil
+}
