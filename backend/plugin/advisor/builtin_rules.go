4ec9a650c42a737de76cdfd674b15aab414916a3
rebelice <yangrebelice@gmail.com>
2024-11-01 12:20:50 +0800

    chore: change object owner check rule (#14275)
    Former-commit-id: 448411c7cb338399404b6313b507ddcb7e2aa2a8

--- backend/plugin/advisor/builtin_rules.go
+++ backend/plugin/advisor/builtin_rules.go
@@ -4,33 +4,15 @@ import storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 
 const (
 	BuiltinRulePriorBackupCheck SQLReviewRuleType = "builtin.prior-backup-check"
-	BuiltinRuleObjectOwnerCheck SQLReviewRuleType = "builtin.object-owner-check"
 )
 
 func GetBuiltinRules(engine storepb.Engine) []*storepb.SQLReviewRule {
 	switch engine {
-	case storepb.Engine_MYSQL, storepb.Engine_TIDB, storepb.Engine_MSSQL, storepb.Engine_ORACLE:
+	case storepb.Engine_MYSQL, storepb.Engine_POSTGRES, storepb.Engine_TIDB, storepb.Engine_MSSQL, storepb.Engine_ORACLE:
 		return []*storepb.SQLReviewRule{
 			{
 				Type:    string(BuiltinRulePriorBackupCheck),
 				Level:   storepb.SQLReviewRuleLevel_ERROR,
-				Payload: "",
-				Engine:  engine,
-				Comment: "",
-			},
-		}
-	case storepb.Engine_POSTGRES:
-		return []*storepb.SQLReviewRule{
-			{
-				Type:    string(BuiltinRulePriorBackupCheck),
-				Level:   storepb.SQLReviewRuleLevel_ERROR,
-				Payload: "",
-				Engine:  engine,
-				Comment: "",
-			},
-			{
-				Type:    string(BuiltinRuleObjectOwnerCheck),
-				Level:   storepb.SQLReviewRuleLevel_WARNING,
 				Payload: "",
 				Engine:  engine,
 				Comment: "",
