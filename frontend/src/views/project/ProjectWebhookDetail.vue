43ffe95457a217194a9497557171c23285fb47d8
ecmadao <wlec@outlook.com>
2024-11-01 17:03:37 +0800

    refactor: ui layout

--- frontend/src/views/project/ProjectWebhookDetail.vue
+++ frontend/src/views/project/ProjectWebhookDetail.vue
@@ -1,26 +1,14 @@
 <template>
-  <div class="space-y-4 divide-y divide-block-border">
-    <div class="flex justify-between">
-      <div class="flex flex-row space-x-2 items-center">
-        <WebhookTypeIcon :type="projectWebhook.type" class="h-6 w-6" />
-        <h3 class="text-xl leading-6 font-medium text-main">
-          {{ projectWebhook.title }}
-        </h3>
-      </div>
-    </div>
-    <ProjectWebhookForm
-      class="pt-4"
-      :allow-edit="allowEdit"
-      :create="false"
-      :project="project"
-      :webhook="projectWebhook"
-    />
-  </div>
+  <ProjectWebhookForm
+    :allow-edit="allowEdit"
+    :create="false"
+    :project="project"
+    :webhook="projectWebhook"
+  />
 </template>
 
 <script lang="ts" setup>
 import { computed } from "vue";
-import WebhookTypeIcon from "@/components/Project/WebhookTypeIcon.vue";
 import ProjectWebhookForm from "@/components/ProjectWebhookForm.vue";
 import { useProjectByName, useProjectWebhookV1Store } from "@/store";
 import { projectNamePrefix } from "@/store/modules/v1/common";
