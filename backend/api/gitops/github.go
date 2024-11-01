44bbfb36a1e85d8371b5061184db10bc471a2507
p0ny <xz@bytebase.com>
2024-11-01 15:28:34 +0800

    feat: create release via github (#14279)
    Former-commit-id: 8cf20c8bbf232a8d3dadebca69c9e77ee87aa1df
    Former-commit-id: ec58063ec5ddeb870087f59dea978297a4aa9c34

--- backend/api/gitops/github.go
+++ backend/api/gitops/github.go
@@ -7,13 +7,15 @@ import (
 
 	"github.com/pkg/errors"
 
+	"github.com/bytebase/bytebase/backend/common"
+	"github.com/bytebase/bytebase/backend/component/config"
 	"github.com/bytebase/bytebase/backend/plugin/vcs"
 	"github.com/bytebase/bytebase/backend/plugin/vcs/github"
 	"github.com/bytebase/bytebase/backend/store"
 	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
 )
 
-func getGitHubPullRequestInfo(ctx context.Context, vcsProvider *store.VCSProviderMessage, vcsConnector *store.VCSConnectorMessage, body []byte) (*pullRequestInfo, error) {
+func getGitHubPullRequestInfo(ctx context.Context, vcsProvider *store.VCSProviderMessage, vcsConnector *store.VCSConnectorMessage, body []byte, profile *config.Profile) (*pullRequestInfo, error) {
 	var pushEvent github.PullRequestPushEvent
 	if err := json.Unmarshal(body, &pushEvent); err != nil {
 		return nil, errors.Errorf("failed to unmarshal push event, error %v", err)
@@ -27,7 +29,11 @@ func getGitHubPullRequestInfo(ctx context.Context, vcsProvider *store.VCSProvide
 		if !pushEvent.PullRequest.Merged {
 			return nil, errors.Errorf("skip pull request close action, pull request is not merged")
 		}
-		actionType = webhookActionCreateIssue
+		if common.IsDev() && profile.DevelopmentVersioned {
+			actionType = webhookActionCreateRelease
+		} else {
+			actionType = webhookActionCreateIssue
+		}
 	default:
 		return nil, errors.Errorf(`skip webhook event action "%s"`, pushEvent.Action)
 	}
@@ -57,5 +63,35 @@ func getGitHubPullRequestInfo(ctx context.Context, vcsProvider *store.VCSProvide
 		}
 		file.content = convertFileContentToUTF8String(content)
 	}
+
+	if actionType == webhookActionCreateRelease {
+		prInfo.getAllFiles = func(ctx context.Context) ([]*fileChange, error) {
+			p, ok := vcs.Get(storepb.VCSType_GITHUB, vcs.ProviderConfig{InstanceURL: vcsProvider.InstanceURL, AuthToken: vcsProvider.AccessToken}).(*github.Provider)
+			if !ok {
+				return nil, errors.Errorf("expect github.Provider, got %T", p)
+			}
+
+			mergeCommitSha, err := p.GetPullRequestMergedCommit(ctx, vcsConnector.Payload.ExternalId, fmt.Sprintf("%d", pushEvent.Number))
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to get pull request %d", pushEvent.Number)
+			}
+
+			dirFiles, err := p.GetDirectoryFiles(ctx, vcsConnector.Payload.ExternalId, mergeCommitSha, vcsConnector.Payload.BaseDirectory)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to get directory files")
+			}
+			var allFiles []*fileChange
+			for _, f := range dirFiles {
+				f.Content = convertFileContentToUTF8String(f.Content)
+				converted, err := convertVcsFile(f)
+				if err != nil {
+					return nil, errors.Wrapf(err, "failed to convert vcs file")
+				}
+				allFiles = append(allFiles, converted)
+			}
+			return allFiles, nil
+		}
+	}
+
 	return prInfo, nil
 }
