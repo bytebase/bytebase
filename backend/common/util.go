2aae4cbcab0a36aa6b6b5038d62107010d716470
rebelice <yangrebelice@gmail.com>
2024-11-05 15:17:32 +0800

    chore: increase the size limit to 2M (#14331)
    Former-commit-id: bed7493543db95c4f871e15b7d5d1c91c8629ae0

--- backend/common/util.go
+++ backend/common/util.go
@@ -21,10 +21,10 @@ import (
 )
 
 const (
-	// MaxSheetSize is the maximum size (1M) of a sheet for displaying.
-	MaxSheetSize = 1024 * 1024
+	// MaxSheetSize is the maximum size (2M) of a sheet for displaying.
+	MaxSheetSize = 2 * 1024 * 1024
 	// MaxSheetCheckSize is the maximum size of a sheet for checking changes.
-	MaxSheetCheckSize = 1024 * 1024
+	MaxSheetCheckSize = 2 * 1024 * 1024
 	// The maximum number of bytes for sql results in response body.
 	// 100 MB.
 	DefaultMaximumSQLResultSize = 100 * 1024 * 1024
