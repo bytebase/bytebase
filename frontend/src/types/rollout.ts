1d17a2dfa46531f3d3da06d2e03c203139d82b16
steven <stevenlgtm@gmail.com>
2024-11-08 17:29:39 +0800

    feat: implement part of standalone rollout pages

--- frontend/src/types/rollout.ts
+++ frontend/src/types/rollout.ts
@@ -1,61 +1,63 @@
-import { getProjectNameReleaseId } from "@/store/modules/v1/common";
+import { getProjectNameRolloutId } from "@/store/modules/v1/common";
 import { EMPTY_ID, UNKNOWN_ID } from "./const";
 import type { User } from "./proto/v1/auth_service";
-import { Release } from "./proto/v1/release_service";
+import { Rollout } from "./proto/v1/rollout_service";
 import { emptyUser, unknownUser } from "./v1";
 import {
+  EMPTY_PROJECT_NAME,
   emptyProject,
   UNKNOWN_PROJECT_NAME,
   unknownProject,
   type ComposedProject,
 } from "./v1/project";
 
-export interface ComposedRelease extends Release {
+export interface ComposedRollout extends Rollout {
   // Format: projects/{project}
   project: string;
   projectEntity: ComposedProject;
   creatorEntity: User;
 }
 
-export const UNKNOWN_RELEASE_NAME = `${UNKNOWN_PROJECT_NAME}/releases/${UNKNOWN_ID}`;
+export const EMPTY_ROLLOUT_NAME = `${EMPTY_PROJECT_NAME}/rollouts/${EMPTY_ID}`;
+export const UNKNOWN_ROLLOUT_NAME = `${UNKNOWN_PROJECT_NAME}/rollouts/${UNKNOWN_ID}`;
 
-export const emptyRelease = (): ComposedRelease => {
+export const emptyRollout = (): ComposedRollout => {
   const projectEntity = emptyProject();
-  const release = Release.fromJSON({
-    name: `${projectEntity.name}/releases/${EMPTY_ID}`,
+  const rollout = Rollout.fromJSON({
+    name: `${projectEntity.name}/rollouts/${EMPTY_ID}`,
     uid: String(EMPTY_ID),
     project: projectEntity.name,
   });
   return {
-    ...release,
+    ...rollout,
     project: projectEntity.name,
     projectEntity,
     creatorEntity: emptyUser(),
   };
 };
 
-export const unknownRelease = (): ComposedRelease => {
+export const unknownRollout = (): ComposedRollout => {
   const projectEntity = unknownProject();
-  const release = Release.fromJSON({
-    name: `${projectEntity.name}/releases/${UNKNOWN_ID}`,
+  const rollout = Rollout.fromJSON({
+    name: `${projectEntity.name}/rollouts/${UNKNOWN_ID}`,
     uid: String(UNKNOWN_ID),
     project: projectEntity.name,
   });
   return {
-    ...release,
+    ...rollout,
     project: projectEntity.name,
     projectEntity,
     creatorEntity: unknownUser(),
   };
 };
 
-export const isValidReleaseName = (name: any): name is string => {
+export const isValidRolloutName = (name: any): name is string => {
   if (typeof name !== "string") return false;
-  const [projectName, releaseName] = getProjectNameReleaseId(name);
+  const [projectName, rolloutName] = getProjectNameRolloutId(name);
   return Boolean(
     projectName &&
       projectName !== String(UNKNOWN_ID) &&
-      releaseName &&
-      releaseName !== String(UNKNOWN_ID)
+      rolloutName &&
+      rolloutName !== String(UNKNOWN_ID)
   );
 };
