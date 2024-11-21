481441da05b0508e9d02bc4086dedf130a26c40e
boojack <stevenlgtm@gmail.com>
2024-11-21 15:41:12 +0800

    chore: remove unused files in plan (#14469)
    Former-commit-id: 78c49a21c145beee66b298f477312acfad0257ca
    Former-commit-id: 699db0e898f5849afc61c687ace7d1af5cc3c6c4

--- frontend/src/components/Plan/components/common/index.ts
+++ frontend/src/components/Plan/components/common/index.ts
@@ -1,5 +1,3 @@
-import DropdownItemWithErrorList from "./DropdownItemWithErrorList.vue";
 import ErrorList from "./ErrorList.vue";
-import TaskSpinner from "./TaskSpinner.vue";
 
-export { ErrorList, DropdownItemWithErrorList, TaskSpinner };
+export { ErrorList };
