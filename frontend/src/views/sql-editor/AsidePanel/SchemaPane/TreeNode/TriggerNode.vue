fbb614ede324a8749e857ee8a57ba8391da9bb42
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-11-25 16:11:15 +0800

    chore(frontend): display sequence and trigger metadata (#14498)
    * chore: display sequences on database detail view
    
    * chore: sequence node in tree
    
    * chore: display sequences in SQL Editor
    
    * chore: style
    
    * chore: trigger i18n
    
    * feat: display triggers on database detail page
    
    * chore: ellipsis
    
    * chore: trigger data table style
    
    * feat: triggers in SQL Editor
    
    Former-commit-id: 6af7d6ac9238dc94823ac713e02eee29975a3ee9
    Former-commit-id: 8ea94c9e69b5fef40824795a024cbc351cf50afe

---
+++ frontend/src/views/sql-editor/AsidePanel/SchemaPane/TreeNode/TriggerNode.vue
@@ -0,0 +1,26 @@
+<template>
+  <CommonNode
+    :text="target.trigger.name"
+    :keyword="keyword"
+    :highlight="true"
+    :indent="0"
+  >
+    <template #icon>
+      <TriggerIcon class="w-4 h-4 text-gray-500" />
+    </template>
+  </CommonNode>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { TriggerIcon } from "@/components/Icon";
+import type { TreeNode } from "../common";
+import CommonNode from "./CommonNode.vue";
+
+const props = defineProps<{
+  node: TreeNode;
+  keyword: string;
+}>();
+
+const target = computed(() => (props.node as TreeNode<"trigger">).meta.target);
+</script>
--- frontend/src/views/sql-editor/EditorPanel/Panels/InfoPanel/InfoPanel.vue
+++ frontend/src/views/sql-editor/EditorPanel/Panels/InfoPanel/InfoPanel.vue
@@ -118,6 +118,70 @@
       </div>
 
       <div
+        v-if="instanceV1SupportsSequence(database.instanceResource)"
+        class="flex flex-col gap-2"
+      >
+        <div class="flex items-center justify-between">
+          <h2 class="text-lg">{{ $t("db.sequences") }}</h2>
+          <SearchBox v-model:value="state.keywords.sequences" />
+        </div>
+        <div class="max-h-[16rem] overflow-x-auto overflow-y-hidden">
+          <SequencesTable
+            v-if="metadata.schema"
+            :db="database"
+            :database="metadata.database"
+            :schema="metadata.schema"
+            :sequences="metadata.schema.sequences"
+            :keyword="state.keywords.sequences"
+            :max-height="230"
+            @click="
+              updateViewState({
+                view: 'SEQUENCES',
+                detail: {
+                  sequence: keyWithPosition(
+                    $event.sequence.name,
+                    $event.position
+                  ),
+                },
+              })
+            "
+          />
+        </div>
+      </div>
+
+      <div
+        v-if="instanceV1SupportsTrigger(database.instanceResource)"
+        class="flex flex-col gap-2"
+      >
+        <div class="flex items-center justify-between">
+          <h2 class="text-lg">{{ $t("db.triggers") }}</h2>
+          <SearchBox v-model:value="state.keywords.triggers" />
+        </div>
+        <div class="max-h-[16rem] overflow-x-auto overflow-y-hidden">
+          <TriggersTable
+            v-if="metadata.schema"
+            :db="database"
+            :database="metadata.database"
+            :schema="metadata.schema"
+            :triggers="metadata.schema.triggers"
+            :keyword="state.keywords.triggers"
+            :max-height="230"
+            @click="
+              updateViewState({
+                view: 'TRIGGERS',
+                detail: {
+                  trigger: keyWithPosition(
+                    $event.trigger.name,
+                    $event.position
+                  ),
+                },
+              })
+            "
+          />
+        </div>
+      </div>
+
+      <div
         v-if="instanceV1SupportsExternalTable(database.instanceResource)"
         class="flex flex-col gap-2"
       >
@@ -193,6 +257,8 @@ import { DatabaseMetadataView } from "@/types/proto/v1/database_service";
 import {
   instanceV1SupportsExternalTable,
   instanceV1SupportsPackage,
+  instanceV1SupportsSequence,
+  instanceV1SupportsTrigger,
 } from "@/utils";
 import { keyWithPosition } from "@/views/sql-editor/EditorCommon";
 import DatabaseChooser from "@/views/sql-editor/EditorCommon/DatabaseChooser.vue";
@@ -201,7 +267,9 @@ import ExternalTablesTable from "../ExternalTablesPanel/ExternalTablesTable.vue"
 import FunctionsTable from "../FunctionsPanel/FunctionsTable.vue";
 import PackagesTable from "../PackagesPanel/PackagesTable.vue";
 import ProceduresTable from "../ProceduresPanel/ProceduresTable.vue";
+import SequencesTable from "../SequencesPanel/SequencesTable.vue";
 import TablesTable from "../TablesPanel/TablesTable.vue";
+import TriggersTable from "../TriggersPanel/TriggersTable.vue";
 import ViewsTable from "../ViewsPanel/ViewsTable.vue";
 import { SchemaSelectToolbar } from "../common";
 
@@ -211,6 +279,8 @@ const state = reactive({
     views: "",
     functions: "",
     procedures: "",
+    sequences: "",
+    triggers: "",
     externalTables: "",
     packages: "",
   },
