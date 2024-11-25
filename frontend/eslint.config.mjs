6eaa1d78e2f1a6bd983269de00bbf674f4d29be7
steven <stevenlgtm@gmail.com>
2024-11-25 16:09:40 +0800

    chore: update

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -4,9 +4,6 @@ import pluginVue from "eslint-plugin-vue";
 export default [
   ...pluginVue.configs["flat/essential"],
   ...vueTsEslintConfig({
-    // Optional: extend additional configurations from `typescript-eslint`.
-    // Supports all the configurations in
-    // https://typescript-eslint.io/users/configs#recommended-configurations
     extends: ["recommended"],
     supportedScriptLangs: {
       ts: true,
