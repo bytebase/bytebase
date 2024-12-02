d54f8871a11feef1573a157266ede4a51da17340
h3n4l <oysterdays@gmail.com>
2024-12-02 11:07:52 +0800

    feat(spanner): spanner masking (#14574)
    * chore: wip
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: SPANNER_SYS is system resource
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * feat: spanner masking
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    * chore: enable spanner masking
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    
    ---------
    
    Signed-off-by: h3n4l <oysterdays@gmail.com>
    Former-commit-id: 2a1f845245d8a2449aadacb5dba89a48346c838f
    Former-commit-id: c833e8c5638a8c55fb470c2de8570b90f5a159fc

---
+++ backend/plugin/parser/spanner/spanner.go
@@ -0,0 +1,54 @@
+package spanner
+
+import (
+	"strings"
+
+	"github.com/antlr4-go/antlr/v4"
+
+	parser "github.com/bytebase/google-sql-parser"
+
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+	"github.com/bytebase/bytebase/backend/utils"
+)
+
+type ParseResult struct {
+	Tree   antlr.Tree
+	Tokens *antlr.CommonTokenStream
+}
+
+// ParseSpannerGoogleSQL parses the given SQL statement by using antlr4. Returns the AST and token stream if no error.
+func ParseSpannerGoogleSQL(statement string) (*ParseResult, error) {
+	statement = strings.TrimRightFunc(statement, utils.IsSpaceOrSemicolon) + "\n;"
+	inputStream := antlr.NewInputStream(statement)
+	lexer := parser.NewGoogleSQLLexer(inputStream)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	p := parser.NewGoogleSQLParser(stream)
+
+	// Remove default error listener and add our own error listener.
+	lexer.RemoveErrorListeners()
+	lexerErrorListener := &base.ParseErrorListener{}
+	lexer.AddErrorListener(lexerErrorListener)
+
+	p.RemoveErrorListeners()
+	parserErrorListener := &base.ParseErrorListener{}
+	p.AddErrorListener(parserErrorListener)
+
+	p.BuildParseTrees = true
+
+	tree := p.Root()
+
+	if lexerErrorListener.Err != nil {
+		return nil, lexerErrorListener.Err
+	}
+
+	if parserErrorListener.Err != nil {
+		return nil, parserErrorListener.Err
+	}
+
+	result := &ParseResult{
+		Tree:   tree,
+		Tokens: stream,
+	}
+
+	return result, nil
+}
