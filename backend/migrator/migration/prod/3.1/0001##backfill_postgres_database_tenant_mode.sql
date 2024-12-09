ca94db5822eb899cc4df9a44fa4dafe70d4e1186
p0ny <p0uy@outlook.com>
2024-12-09 15:59:57 +0800

    chore: update pg tenant mode backfill

--- backend/migrator/migration/prod/3.1/0001##backfill_postgres_database_tenant_mode.sql
+++ backend/migrator/migration/prod/3.1/0001##backfill_postgres_database_tenant_mode.sql
@@ -1,7 +1,2 @@
 UPDATE project
 SET setting = setting || '{"postgresDatabaseTenantMode": true}'
-FROM db_group
-WHERE EXISTS (
-	SELECT 1 FROM db_group
-	WHERE db_group.project_id = project.id AND db_group.payload->>'multitenancy' = 'true'
-)
