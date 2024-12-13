914b0f469b97ae01b1371c34495d0952b459f371
Liu Ji <2749742+LiuJi-Jim@users.noreply.github.com>
2024-12-13 13:49:47 +0800

    chore: improve type definitions (#14647)

--- frontend/src/components/IssueV1/components/StatementSection/EditorView/EditorView.vue
+++ frontend/src/components/IssueV1/components/StatementSection/EditorView/EditorView.vue
@@ -203,7 +203,7 @@
 
 <script setup lang="ts">
 import { useElementSize } from "@vueuse/core";
-import { cloneDeep, head, uniq } from "lodash-es";
+import { cloneDeep, head, isEmpty, uniq } from "lodash-es";
 import { ExpandIcon } from "lucide-vue-next";
 import { NButton, NTooltip, useDialog } from "naive-ui";
 import { computed, h, reactive, ref, toRef, watch } from "vue";
