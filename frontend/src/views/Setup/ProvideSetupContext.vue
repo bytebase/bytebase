994291e5995427c2ccf43a6a738d211dbabf5cb9
ecmadao <wlec@outlook.com>
2024-12-17 15:13:32 +0800

    chore: popup login modal if token expired (#14669)
    * chore: popup login modal if token expired
    
    * fix: eslint
    
    Former-commit-id: 1c75663cad0ab3f4a4c5a01d524a93b0808c7dc6
    Former-commit-id: 5151a2981eeff3811f9796ebac351e2d351ec5c8

--- frontend/src/views/Setup/ProvideSetupContext.vue
+++ frontend/src/views/Setup/ProvideSetupContext.vue
@@ -10,12 +10,7 @@ import {
   type RouteLocationNormalizedLoadedGeneric,
 } from "vue-router";
 import MaskSpinner from "@/components/misc/MaskSpinner.vue";
-import {
-  usePolicyV1Store,
-  useRoleStore,
-  useSettingV1Store,
-  useAuthStore,
-} from "@/store";
+import { usePolicyV1Store, useRoleStore, useSettingV1Store } from "@/store";
 import { PolicyResourceType } from "@/types/proto/v1/org_policy_service";
 import { hasWorkspacePermissionV2 } from "@/utils";
 
@@ -50,7 +45,10 @@ onMounted(async () => {
     (route) => {
       if (!checkPermissions(route)) {
         ready.value = false;
-        return useAuthStore().logout();
+        router.push({
+          name: "error.403",
+        });
+        return;
       }
       ready.value = true;
     },
