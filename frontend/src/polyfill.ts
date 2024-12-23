63aa8bfe8b6e763068904b79dec5201b922e4542
steven <stevenlgtm@gmail.com>
2024-12-23 16:06:24 +0800

    chore: update

--- frontend/src/polyfill.ts
+++ frontend/src/polyfill.ts
@@ -1,7 +1,19 @@
 (() => {
   if (!String.prototype.replaceAll) {
-    String.prototype.replaceAll = function (search: any, replacement: any) {
-      return this.replace(new RegExp(search, "g"), replacement);
+    String.prototype.replaceAll = function (
+      search: string | RegExp,
+      replacement: any
+    ) {
+      if (search instanceof RegExp) {
+        if (!search.flags.includes("g")) {
+          throw new TypeError("replaceAll must be called with a global RegExp");
+        }
+        return this.replace(search, replacement);
+      } else {
+        const escapedSearch = search.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+        const regex = new RegExp(escapedSearch, "g");
+        return this.replace(regex, replacement);
+      }
     };
   }
 })();
