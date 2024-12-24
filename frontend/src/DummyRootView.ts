4159edbb7d350a4cf1bbaf4ad6ed11a9fb02af09
boojack <stevenlgtm@gmail.com>
2024-12-24 15:49:25 +0800

    chore: remove workspace-level role checks (#14766)
    * chore: remove almost workspace-level role checks
    
    * chore: remove workspace-level role checks
    
    * chore: update
    
    * chore: update

--- frontend/src/DummyRootView.ts
+++ frontend/src/DummyRootView.ts
@@ -1,32 +1,18 @@
-import { defineComponent, watch, computed } from "vue";
+import { defineComponent, watch } from "vue";
 import { useRouter } from "vue-router";
-import { getProjectName } from "@/store/modules/v1/common";
-import { PresetRoleType } from "@/types";
-import { PROJECT_V1_ROUTE_DETAIL } from "./router/dashboard/projectV1";
 import { WORKSPACE_ROUTE_LANDING } from "./router/dashboard/workspaceRoutes";
 import { SQL_EDITOR_HOME_MODULE } from "./router/sqlEditor";
 import { useRecentVisit } from "./router/useRecentVisit";
-import { useAppFeature, useProjectV1List } from "./store";
-import { hasWorkspaceLevelRole } from "./utils";
+import { useAppFeature } from "./store";
 
 export default defineComponent({
   name: "DummyRootView",
   setup() {
     const { lastVisit } = useRecentVisit();
-
     const router = useRouter();
     const defaultWorkspaceView = useAppFeature(
       "bb.feature.default-workspace-view"
     );
-
-    const hasWorkspaceRole = computed(() => {
-      return (
-        hasWorkspaceLevelRole(PresetRoleType.WORKSPACE_ADMIN) ||
-        hasWorkspaceLevelRole(PresetRoleType.WORKSPACE_DBA)
-      );
-    });
-
-    const { projectList } = useProjectV1List();
 
     watch(
       defaultWorkspaceView,
@@ -39,18 +25,10 @@ export default defineComponent({
         }
 
         const fallback = () => {
-          if (hasWorkspaceRole.value || projectList.value.length !== 1) {
-            router.replace({
-              name: WORKSPACE_ROUTE_LANDING,
-            });
-          } else {
-            router.replace({
-              name: PROJECT_V1_ROUTE_DETAIL,
-              params: {
-                projectId: getProjectName(projectList.value[0].name),
-              },
-            });
-          }
+          // If no last visit, always redirect to landing page.
+          router.replace({
+            name: WORKSPACE_ROUTE_LANDING,
+          });
         };
 
         // Redirect to
