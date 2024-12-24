81740aa73bf81cb03d208e0110b8f670f784bc00
steven <stevenlgtm@gmail.com>
2024-12-24 15:45:46 +0800

    chore: update

--- frontend/src/DummyRootView.ts
+++ frontend/src/DummyRootView.ts
@@ -1,22 +1,18 @@
 import { defineComponent, watch } from "vue";
 import { useRouter } from "vue-router";
-import { getProjectName } from "@/store/modules/v1/common";
-import { PROJECT_V1_ROUTE_DETAIL } from "./router/dashboard/projectV1";
 import { WORKSPACE_ROUTE_LANDING } from "./router/dashboard/workspaceRoutes";
 import { SQL_EDITOR_HOME_MODULE } from "./router/sqlEditor";
 import { useRecentVisit } from "./router/useRecentVisit";
-import { useAppFeature, useProjectV1List } from "./store";
+import { useAppFeature } from "./store";
 
 export default defineComponent({
   name: "DummyRootView",
   setup() {
     const { lastVisit } = useRecentVisit();
-
     const router = useRouter();
     const defaultWorkspaceView = useAppFeature(
       "bb.feature.default-workspace-view"
     );
-    const { projectList } = useProjectV1List();
 
     watch(
       defaultWorkspaceView,
@@ -29,20 +25,10 @@ export default defineComponent({
         }
 
         const fallback = () => {
-          // Redirect to the first project if there are more than one project
-          if (projectList.value.length > 1) {
-            router.replace({
-              name: PROJECT_V1_ROUTE_DETAIL,
-              params: {
-                projectId: getProjectName(projectList.value[0].name),
-              },
-            });
-          } else {
-            // Otherwise, redirect to the landing page.
-            router.replace({
-              name: WORKSPACE_ROUTE_LANDING,
-            });
-          }
+          // If no last visit, always redirect to landing page.
+          router.replace({
+            name: WORKSPACE_ROUTE_LANDING,
+          });
         };
 
         // Redirect to
