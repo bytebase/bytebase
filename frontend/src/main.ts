52f5b2d520544eccbbd188ed236a34536d4347f9
boojack <stevenlgtm@gmail.com>
2024-12-24 17:28:29 +0800

    chore: use vite legacy plugin (#14770)
    * chore: use vite legacy plugin
    
    * chore: update

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,9 @@
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import axios from "axios";
+import "core-js/stable";
 import Long from "long";
 import protobufjs from "protobufjs";
+import "regenerator-runtime/runtime";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/css/github-markdown-style.css";
