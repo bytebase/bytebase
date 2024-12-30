af4974c81e069403ecf4630f07eca3ee7cd40d6c
ecmadao <wlec@outlook.com>
2024-12-30 15:30:45 +0800

    chore: support select sync databases during instance creation (#14808)
    * feat(proto): config sync databases in the instance options
    
    * chore: sync selected databases
    
    * fix: test
    
    * feat: allow sync specific databases
    
    * chore: support select sync databases during instance creation
    
    * fix: eslint

--- proto/generated-go/v1/instance_service.pb.gw.go
+++ proto/generated-go/v1/instance_service.pb.gw.go
@@ -353,6 +353,48 @@ func local_request_InstanceService_SyncInstance_0(ctx context.Context, marshaler
 	return msg, metadata, err
 }
 
+func request_InstanceService_ListInstanceDatabase_0(ctx context.Context, marshaler runtime.Marshaler, client InstanceServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq ListInstanceDatabaseRequest
+		metadata runtime.ServerMetadata
+		err      error
+	)
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	val, ok := pathParams["name"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "name")
+	}
+	protoReq.Name, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "name", err)
+	}
+	msg, err := client.ListInstanceDatabase(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+}
+
+func local_request_InstanceService_ListInstanceDatabase_0(ctx context.Context, marshaler runtime.Marshaler, server InstanceServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var (
+		protoReq ListInstanceDatabaseRequest
+		metadata runtime.ServerMetadata
+		err      error
+	)
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	val, ok := pathParams["name"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "name")
+	}
+	protoReq.Name, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "name", err)
+	}
+	msg, err := server.ListInstanceDatabase(ctx, &protoReq)
+	return msg, metadata, err
+}
+
 func request_InstanceService_BatchSyncInstances_0(ctx context.Context, marshaler runtime.Marshaler, client InstanceServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
 		protoReq BatchSyncInstancesRequest
@@ -733,6 +775,26 @@ func RegisterInstanceServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 		}
 		forward_InstanceService_SyncInstance_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
+	mux.Handle(http.MethodPost, pattern_InstanceService_ListInstanceDatabase_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/bytebase.v1.InstanceService/ListInstanceDatabase", runtime.WithHTTPPathPattern("/v1/{name=instances/*}:databases"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_InstanceService_ListInstanceDatabase_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_InstanceService_ListInstanceDatabase_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
 	mux.Handle(http.MethodPost, pattern_InstanceService_BatchSyncInstances_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1012,6 +1074,23 @@ func RegisterInstanceServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 		}
 		forward_InstanceService_SyncInstance_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
+	mux.Handle(http.MethodPost, pattern_InstanceService_ListInstanceDatabase_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/bytebase.v1.InstanceService/ListInstanceDatabase", runtime.WithHTTPPathPattern("/v1/{name=instances/*}:databases"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_InstanceService_ListInstanceDatabase_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		forward_InstanceService_ListInstanceDatabase_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+	})
 	mux.Handle(http.MethodPost, pattern_InstanceService_BatchSyncInstances_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -1118,33 +1197,35 @@ func RegisterInstanceServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 }
 
 var (
-	pattern_InstanceService_GetInstance_0        = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, ""))
-	pattern_InstanceService_ListInstances_0      = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "instances"}, ""))
-	pattern_InstanceService_CreateInstance_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "instances"}, ""))
-	pattern_InstanceService_UpdateInstance_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "instance.name"}, ""))
-	pattern_InstanceService_DeleteInstance_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, ""))
-	pattern_InstanceService_UndeleteInstance_0   = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "undelete"))
-	pattern_InstanceService_SyncInstance_0       = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "sync"))
-	pattern_InstanceService_BatchSyncInstances_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "instances"}, "batchSync"))
-	pattern_InstanceService_AddDataSource_0      = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "addDataSource"))
-	pattern_InstanceService_RemoveDataSource_0   = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "removeDataSource"))
-	pattern_InstanceService_UpdateDataSource_0   = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "updateDataSource"))
-	pattern_InstanceService_SyncSlowQueries_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "parent"}, "syncSlowQueries"))
-	pattern_InstanceService_SyncSlowQueries_1    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "projects", "parent"}, "syncSlowQueries"))
+	pattern_InstanceService_GetInstance_0          = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, ""))
+	pattern_InstanceService_ListInstances_0        = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "instances"}, ""))
+	pattern_InstanceService_CreateInstance_0       = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "instances"}, ""))
+	pattern_InstanceService_UpdateInstance_0       = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "instance.name"}, ""))
+	pattern_InstanceService_DeleteInstance_0       = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, ""))
+	pattern_InstanceService_UndeleteInstance_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "undelete"))
+	pattern_InstanceService_SyncInstance_0         = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "sync"))
+	pattern_InstanceService_ListInstanceDatabase_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "databases"))
+	pattern_InstanceService_BatchSyncInstances_0   = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "instances"}, "batchSync"))
+	pattern_InstanceService_AddDataSource_0        = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "addDataSource"))
+	pattern_InstanceService_RemoveDataSource_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "removeDataSource"))
+	pattern_InstanceService_UpdateDataSource_0     = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "name"}, "updateDataSource"))
+	pattern_InstanceService_SyncSlowQueries_0      = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "instances", "parent"}, "syncSlowQueries"))
+	pattern_InstanceService_SyncSlowQueries_1      = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 2, 5, 2}, []string{"v1", "projects", "parent"}, "syncSlowQueries"))
 )
 
 var (
-	forward_InstanceService_GetInstance_0        = runtime.ForwardResponseMessage
-	forward_InstanceService_ListInstances_0      = runtime.ForwardResponseMessage
-	forward_InstanceService_CreateInstance_0     = runtime.ForwardResponseMessage
-	forward_InstanceService_UpdateInstance_0     = runtime.ForwardResponseMessage
-	forward_InstanceService_DeleteInstance_0     = runtime.ForwardResponseMessage
-	forward_InstanceService_UndeleteInstance_0   = runtime.ForwardResponseMessage
-	forward_InstanceService_SyncInstance_0       = runtime.ForwardResponseMessage
-	forward_InstanceService_BatchSyncInstances_0 = runtime.ForwardResponseMessage
-	forward_InstanceService_AddDataSource_0      = runtime.ForwardResponseMessage
-	forward_InstanceService_RemoveDataSource_0   = runtime.ForwardResponseMessage
-	forward_InstanceService_UpdateDataSource_0   = runtime.ForwardResponseMessage
-	forward_InstanceService_SyncSlowQueries_0    = runtime.ForwardResponseMessage
-	forward_InstanceService_SyncSlowQueries_1    = runtime.ForwardResponseMessage
+	forward_InstanceService_GetInstance_0          = runtime.ForwardResponseMessage
+	forward_InstanceService_ListInstances_0        = runtime.ForwardResponseMessage
+	forward_InstanceService_CreateInstance_0       = runtime.ForwardResponseMessage
+	forward_InstanceService_UpdateInstance_0       = runtime.ForwardResponseMessage
+	forward_InstanceService_DeleteInstance_0       = runtime.ForwardResponseMessage
+	forward_InstanceService_UndeleteInstance_0     = runtime.ForwardResponseMessage
+	forward_InstanceService_SyncInstance_0         = runtime.ForwardResponseMessage
+	forward_InstanceService_ListInstanceDatabase_0 = runtime.ForwardResponseMessage
+	forward_InstanceService_BatchSyncInstances_0   = runtime.ForwardResponseMessage
+	forward_InstanceService_AddDataSource_0        = runtime.ForwardResponseMessage
+	forward_InstanceService_RemoveDataSource_0     = runtime.ForwardResponseMessage
+	forward_InstanceService_UpdateDataSource_0     = runtime.ForwardResponseMessage
+	forward_InstanceService_SyncSlowQueries_0      = runtime.ForwardResponseMessage
+	forward_InstanceService_SyncSlowQueries_1      = runtime.ForwardResponseMessage
 )
