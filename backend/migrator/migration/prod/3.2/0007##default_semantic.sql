9a36d94c57beae2484b3f0e896cbb33ec0e92d36
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-01-02 14:22:33 +0800

    chore: backfill default and default partial semantic type (#14857)

---
+++ backend/migrator/migration/prod/3.2/0007##default_semantic.sql
@@ -0,0 +1,3 @@
+UPDATE setting
+SET value = jsonb_build_object('types', '[{"id":"default","title":"Default","description":"Default type with full masking","algorithm":{"id":"default","title":"Default","fullMask":{"substitution":"******"}}},{"id":"default-partial","title":"Default Partial","description":"Default partial type with partial masking","algorithm":{"id":"default-partial","title":"Default Partial"}}]'::jsonb || (value::jsonb->'types'))
+WHERE name = 'bb.workspace.semantic-types';
