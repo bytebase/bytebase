781dc612374ffcfdb195dc245acb70357e23780b
rebelice <yangrebelice@gmail.com>
2025-01-03 11:19:33 +0800

    feat: get PostgreSQL DUMP from metadata (#14593)
    * chore: WIP
    
    * chore: wip
    
    * feat: implement view, materialized view, partitions and subpartitions
    
    * chore: fix sequence dependency
    
    * feat: dump primary key
    
    * feat: dump unique key
    
    * feat: support indexes
    
    * feat: support fks
    
    * feat: construct enum
    
    * feat: construct triggers
    
    * feat: construct comments
    
    * feat: construct indexes for materialized view
    
    * chore: refine index
    
    * feat: enable pg new dump and fix tests
    
    * chore: remove useless code
    
    Former-commit-id: e78b7bfd1891dd8fe8adcbe6a2529b958969c126
    Former-commit-id: 6194ec42042ccdfc879292f7f56520f979050a29

---
+++ backend/plugin/db/pg/topo_sort.go
@@ -0,0 +1,78 @@
+package pg
+
+import (
+	"sort"
+
+	"github.com/pkg/errors"
+)
+
+func NewGraph() *Graph {
+	return &Graph{
+		NodeMap: make(map[string]*Node),
+	}
+}
+
+type Graph struct {
+	NodeMap  map[string]*Node
+	EdgeList []*Edge
+}
+
+type Node struct {
+	ID string
+}
+
+type Edge struct {
+	Start string
+	End   string
+}
+
+func (g *Graph) AddNode(id string) {
+	g.NodeMap[id] = &Node{ID: id}
+}
+
+func (g *Graph) AddEdge(start, end string) {
+	if _, ok := g.NodeMap[start]; !ok {
+		g.AddNode(start)
+	}
+	if _, ok := g.NodeMap[end]; !ok {
+		g.AddNode(end)
+	}
+	g.EdgeList = append(g.EdgeList, &Edge{Start: start, End: end})
+}
+
+func (g *Graph) GetTopoSort() ([]string, error) {
+	var result []string
+	inDegree := make(map[string]int)
+	outEdge := make(map[string][]*Edge)
+
+	for _, edge := range g.EdgeList {
+		inDegree[edge.End]++
+		outEdge[edge.Start] = append(outEdge[edge.Start], edge)
+	}
+
+	var queue []string
+	for id := range g.NodeMap {
+		if inDegree[id] == 0 {
+			queue = append(queue, id)
+		}
+	}
+	sort.Strings(queue)
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		for _, edge := range outEdge[node] {
+			inDegree[edge.End]--
+			if inDegree[edge.End] == 0 {
+				queue = append(queue, edge.End)
+			}
+		}
+		result = append(result, node)
+	}
+
+	if len(result) != len(g.NodeMap) {
+		return nil, errors.Errorf("graph has cycle")
+	}
+
+	return result, nil
+}
