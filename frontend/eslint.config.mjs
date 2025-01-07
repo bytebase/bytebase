f381acf1f455b92363892e87aa4a879b999ceb3a
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-01-07 17:47:21 +0800

    chore: unused i18n warning (#14916)
    * chore: unused i18n warning
    
    * chore: update
    
    * Revert "chore: update"
    
    This reverts commit 10df54c3636d4f1e068f9fe27ca97dbfdff6fdf6 [formerly 48547b64c2859cfb4addbccf4af114884ba628f5].
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 07ecf8d6327d5c4ebf04b9a514e25ce8cebd85bf

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,5 +1,6 @@
 import vueTsEslintConfig from "@vue/eslint-config-typescript";
 import pluginVue from "eslint-plugin-vue";
+import vueI18n from '@intlify/eslint-plugin-vue-i18n'
 
 export default [
   ...pluginVue.configs["flat/essential"],
@@ -11,6 +12,7 @@ export default [
     },
     rootDir: import.meta.dirname,
   }),
+  ...vueI18n.configs['flat/recommended'],
   {
     ignores: ["**/dist/**", "**/node_modules/**", "**/proto/**"],
   },
@@ -24,6 +26,17 @@ export default [
         "error",
         { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
       ],
+      "@intlify/vue-i18n/no-unused-keys": [
+        "warn",
+        {
+          "src": "./src",
+          "extensions": [".js", ".vue", ".ts", ".tsx"],
+          "ignores": [],
+          "enableFix": false
+        }
+      ],
+      "@intlify/vue-i18n/no-missing-keys": "off",
+      "@intlify/vue-i18n/no-raw-text": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "vue/no-mutating-props": "error",
       "vue/no-unused-components": "error",
@@ -44,6 +57,12 @@ export default [
         },
       ],
       "vue/multi-word-component-names": "off",
+    },
+    settings: {
+      'vue-i18n': {
+        localeDir: './src/locales/*.json',
+        messageSyntaxVersion: '^9.0.0'
+      }
     },
   },
 ];
