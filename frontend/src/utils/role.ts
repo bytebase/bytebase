1382dfbccc20486e08b6837a6dde404e4d23cb7b
Vincent Huang <vhuan025@ucr.edu>
2025-01-08 19:47:09 -0800

    dynamic to static translation fix role.ts

--- frontend/src/utils/role.ts
+++ frontend/src/utils/role.ts
@@ -2,17 +2,47 @@ import { t } from "@/plugins/i18n";
 import { useRoleStore } from "@/store";
 import { PRESET_ROLES } from "@/types";
 
-const roleTranslations = {
-  "workspace-admin": "role.workspace-admin",
-  "workspace-dba": "role.workspace-dba",
-  "workspace-member": "role.workspace-member",
-  "project-owner": "role.project-owner",
-  "project-developer": "role.project-developer",
-  "project-releaser": "role.project-releaser",
-  "project-querier": "role.project-querier",
-  "sql-editor-user": "role.sql-editor-user",
-  "project-exporter": "role.project-exporter",
-  "project-viewer": "role.project-viewer"
+const roleTranslationsKey = {
+  "workspace-admin": {
+    self: "role.workspace-admin.self",
+    description: "role.workspace-admin.description"
+  },
+  "workspace-dba": {
+    self: "role.workspace-dba.self",
+    description: "role.workspace-dba.description"
+  },
+  "workspace-member": {
+    self: "role.workspace-member.self",
+    description: "role.workspace-member.description"
+  },
+  "project-owner": {
+    self: "role.project-owner.self",
+    description: "role.project-owner.description"
+  },
+  "project-developer": {
+    self: "role.project-developer.self",
+    description: "role.project-developer.description"
+  },
+  "project-releaser": {
+    self: "role.project-releaser.self",
+    description: "role.project-releaser.description"
+  },
+  "project-querier": {
+    self: "role.project-querier.self",
+    description: "role.project-querier.description"
+  },
+  "sql-editor-user": {
+    self: "role.sql-editor-user.self",
+    description: "role.sql-editor-user.description"
+  },
+  "project-exporter": {
+    self: "role.project-exporter.self",
+    description: "role.project-exporter.description"
+  },
+  "project-viewer": {
+    self: "role.project-viewer.self",
+    description: "role.project-viewer.description"
+  }
 };
 
 export const extractRoleResourceName = (resourceId: string): string => {
@@ -23,7 +53,7 @@ export const extractRoleResourceName = (resourceId: string): string => {
 
 export const displayRoleTitle = (role: string): string => {
   if (PRESET_ROLES.includes(role)) {
-    return t(roleTranslations[extractRoleResourceName(role) as keyof typeof roleTranslations] + ".self");
+    return t(roleTranslationsKey[extractRoleResourceName(role) as keyof typeof roleTranslationsKey].self);
   }
   // Use role.title if possible
   const item = useRoleStore().roleList.find((r) => r.name === role);
@@ -33,7 +63,7 @@ export const displayRoleTitle = (role: string): string => {
 
 export const displayRoleDescription = (role: string): string => {
   if (PRESET_ROLES.includes(role)) {
-    return t(roleTranslations[extractRoleResourceName(role) as keyof typeof roleTranslations] + ".description");
+    return t(roleTranslationsKey[extractRoleResourceName(role) as keyof typeof roleTranslationsKey].description);
   }
   // Use role.description if possible
   const item = useRoleStore().roleList.find((r) => r.name === role);
