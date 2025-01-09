09a6288ee42c7ad64a20e1c4659bcbc176dec87a
Vincent Huang <40749774+vsai12@users.noreply.github.com>
2025-01-08 23:40:11 -0800

    chore: Dynamic to Static for "role" and "status" translation (#14945)
    * dynamic to static translation initial fix
    
    * dynamic to static translation fix role.ts
    
    * change to switch-case
    
    * change to camel case; remove project-querier
    
    * Update error case
    
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    
    * update role default case
    
    * small fix
    
    * small fix
    
    ---------
    
    Co-authored-by: Vincent Huang <vhuan025@ucr.edu>
    Co-authored-by: boojack <stevenlgtm@gmail.com>
    Former-commit-id: 271ad1c4c696f25640ecc6b55f01b6d962ddc583

--- frontend/src/utils/role.ts
+++ frontend/src/utils/role.ts
@@ -1,7 +1,6 @@
-import { kebabCase } from "lodash-es";
 import { t } from "@/plugins/i18n";
 import { useRoleStore } from "@/store";
-import { PRESET_ROLES } from "@/types";
+import { PresetRoleType } from "@/types";
 
 export const extractRoleResourceName = (resourceId: string): string => {
   const pattern = /(?:^|\/)roles\/([^/]+)(?:$|\/)/;
@@ -10,8 +9,25 @@ export const extractRoleResourceName = (resourceId: string): string => {
 };
 
 export const displayRoleTitle = (role: string): string => {
-  if (PRESET_ROLES.includes(role)) {
-    return t(`role.${kebabCase(extractRoleResourceName(role))}.self`);
+  switch (role) {
+    case PresetRoleType.WORKSPACE_ADMIN:
+      return t("role.workspace-admin.self");
+    case PresetRoleType.WORKSPACE_DBA:
+      return t("role.workspace-dba.self");
+    case PresetRoleType.WORKSPACE_MEMBER:
+      return t("role.workspace-member.self");
+    case PresetRoleType.PROJECT_OWNER:
+      return t("role.project-owner.self");
+    case PresetRoleType.PROJECT_DEVELOPER:
+      return t("role.project-developer.self");
+    case PresetRoleType.PROJECT_RELEASER:
+      return t("role.project-releaser.self");
+    case PresetRoleType.SQL_EDITOR_USER:
+      return t("role.sql-editor-user.self");
+    case PresetRoleType.PROJECT_EXPORTER:
+      return t("role.project-exporter.self");
+    case PresetRoleType.PROJECT_VIEWER:
+      return t("role.project-viewer.self");
   }
   // Use role.title if possible
   const item = useRoleStore().roleList.find((r) => r.name === role);
@@ -20,8 +36,25 @@ export const displayRoleTitle = (role: string): string => {
 };
 
 export const displayRoleDescription = (role: string): string => {
-  if (PRESET_ROLES.includes(role)) {
-    return t(`role.${kebabCase(extractRoleResourceName(role))}.description`);
+  switch (role) {
+    case PresetRoleType.WORKSPACE_ADMIN:
+      return t("role.workspace-admin.description");
+    case PresetRoleType.WORKSPACE_DBA:
+      return t("role.workspace-dba.description");
+    case PresetRoleType.WORKSPACE_MEMBER:
+      return t("role.workspace-member.description");
+    case PresetRoleType.PROJECT_OWNER:
+      return t("role.project-owner.description");
+    case PresetRoleType.PROJECT_DEVELOPER:
+      return t("role.project-developer.description");
+    case PresetRoleType.PROJECT_RELEASER:
+      return t("role.project-releaser.description");
+    case PresetRoleType.SQL_EDITOR_USER:
+      return t("role.sql-editor-user.description");
+    case PresetRoleType.PROJECT_EXPORTER:
+      return t("role.project-exporter.description");
+    case PresetRoleType.PROJECT_VIEWER:
+      return t("role.project-viewer.description");
   }
   // Use role.description if possible
   const item = useRoleStore().roleList.find((r) => r.name === role);
