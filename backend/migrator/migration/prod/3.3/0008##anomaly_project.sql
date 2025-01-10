ccd03a7cbd94aa5a35d1a8bc3f41dacbdeee73cc
d-bytebase <d@bytebase.com>
2025-01-09 19:11:00 -0800

    chore: update

--- backend/migrator/migration/prod/3.3/0008##anomaly_project.sql
+++ backend/migrator/migration/prod/3.3/0008##anomaly_project.sql
@@ -1,1 +1,7 @@
 ALTER TABLE anomaly ADD COLUMN project TEXT;
+UPDATE anomaly
+SET project = project.resource_id
+FROM db
+    JOIN project ON db.project_id = project.id
+WHERE
+    db.id = anomaly.database_id;
