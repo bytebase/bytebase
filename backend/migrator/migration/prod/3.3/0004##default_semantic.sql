14172018ebdf95ac267f278c6b771d9b665bd56f
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-01-13 11:59:31 +0800

    fix: semantic non-exist case (#14978)
    Former-commit-id: e1ba9e27f309665ae84ec3e616029a71ac20a07d
    Former-commit-id: 48a8d0bf037154901b2b6cbdf12f9cead00e109a

--- backend/migrator/migration/prod/3.3/0004##default_semantic.sql
+++ backend/migrator/migration/prod/3.3/0004##default_semantic.sql
@@ -1,3 +1,7 @@
+INSERT INTO setting (creator_id, updater_id, name, value)
+VALUES (1, 1, 'bb.workspace.semantic-types', '{}')
+ON CONFLICT (name) DO NOTHING;
+
 UPDATE setting
 SET value = jsonb_build_object('types', '[{"id":"bb.default","title":"Default","description":"Default type with full masking"},{"id":"bb.default-partial","title":"Default Partial","description":"Default partial type with partial masking"}]'::jsonb || coalesce(value::jsonb->'types', '[]'::jsonb))
 WHERE name = 'bb.workspace.semantic-types';
