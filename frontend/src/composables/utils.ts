5d11b159fdfafc351f992d8dee69c8a658ab5f22
ecmadao <wlec@outlook.com>
2025-01-20 14:55:38 +0800

    fix: eslint

--- frontend/src/composables/utils.ts
+++ frontend/src/composables/utils.ts
@@ -11,9 +11,9 @@ type NoSQLRowData = {
 };
 
 function base64ToArrayBuffer(base64: string) {
-  var binaryString = atob(base64);
-  var bytes = new Uint8Array(binaryString.length);
-  for (var i = 0; i < binaryString.length; i++) {
+  const binaryString = atob(base64);
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
     bytes[i] = binaryString.charCodeAt(i);
   }
   return bytes.buffer;
