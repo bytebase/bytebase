86fb51381059b7163791ec381d9e96e9be050993
steven <stevenlgtm@gmail.com>
2025-02-05 13:47:42 +0800

    chore: fix comment typo

--- frontend/src/types/proto/v1/rollout_service.ts
+++ frontend/src/types/proto/v1/rollout_service.ts
@@ -6563,7 +6563,7 @@ export const RolloutServiceDefinition = {
       },
     },
     /**
-     * BatchSkipTasks cancels the specified task runs in batch.
+     * BatchCancelTaskRuns cancels the specified task runs.
      * The access is the same as BatchRunTasks().
      */
     batchCancelTaskRuns: {
