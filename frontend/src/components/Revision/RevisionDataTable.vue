973d69c0fd263a468a60e472aab519fd365c1956
steven <stevenlgtm@gmail.com>
2025-02-07 14:13:11 +0800

    chore: align table cells for changelog and revision

--- frontend/src/components/Revision/RevisionDataTable.vue
+++ frontend/src/components/Revision/RevisionDataTable.vue
@@ -77,23 +77,9 @@ const columnList = computed(() => {
       hide: !props.showSelection,
     },
     {
-      key: "applied-at",
-      title: t("database.revision.applied-at"),
-      width: 128,
-      render: (revision) => (
-        <HumanizeDate date={getDateForPbTimestamp(revision.createTime)} />
-      ),
-    },
-    {
-      key: "version",
-      title: t("common.version"),
-      width: 128,
-      render: (revision) => revision.version,
-    },
-    {
       key: "issue",
       title: t("common.issue"),
-      width: "5rem",
+      width: 96,
       render: (revision) => {
         const uid = extractIssueUID(revision.issue);
         if (!uid) return null;
@@ -120,17 +106,36 @@ const columnList = computed(() => {
       },
     },
     {
+      key: "version",
+      title: t("common.version"),
+      width: 128,
+      render: (revision) => revision.version,
+    },
+    {
       key: "statement",
       title: t("common.statement"),
-      minWidth: "12rem",
+      resizable: true,
+      minWidth: "13rem",
+      ellipsis: true,
       render: (revision) => {
         return <p class="truncate whitespace-nowrap">{revision.statement}</p>;
       },
     },
     {
+      key: "created-at",
+      title: t("common.created-at"),
+      width: 128,
+      resizable: true,
+      render: (revision) => (
+        <HumanizeDate date={getDateForPbTimestamp(revision.createTime)} />
+      ),
+    },
+    {
       key: "creator",
       title: t("common.creator"),
       width: 128,
+      resizable: true,
+      ellipsis: true,
       render: (revision) => {
         const creator = creatorOfRevision(revision);
         if (!creator) return null;
