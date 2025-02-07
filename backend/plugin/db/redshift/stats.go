19ae9b5915f888fd471f3d5c0c21179b33e37798
boojack <stevenlgtm@gmail.com>
2025-02-07 11:59:15 +0800

    feat: support statement report for redshift (#15124)
    Former-commit-id: 86e09006696692f09499cc0f087e18edeeb48a6c

---
+++ backend/plugin/db/redshift/stats.go
@@ -0,0 +1,57 @@
+package redshift
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"github.com/pkg/errors"
+)
+
+var rowsRegexp = regexp.MustCompile("rows=([0-9]+)")
+
+func (driver *Driver) CountAffectedRows(ctx context.Context, statement string) (int64, error) {
+	explainSQL := fmt.Sprintf("EXPLAIN %s", statement)
+	rows, err := driver.db.QueryContext(ctx, explainSQL)
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+
+	// # EXPLAIN INSERT INTO t SELECT * FROM t;
+	// QUERY PLAN
+	// -------------------------------------------------------------
+	// XN Seq Scan on t  (cost=0.00..0.11 rows=11 width=49)
+	//
+	// # EXPLAIN SELECT * FROM t;
+	// QUERY PLAN
+	// ------------------------------------------------------
+	// XN Seq Scan on t  (cost=0.00..0.11 rows=11 width=49)
+	var rowCount int64
+	for rows.Next() {
+		var planRow sql.NullString
+		if err := rows.Scan(&planRow); err != nil {
+			return 0, err
+		}
+
+		if !planRow.Valid {
+			continue
+		}
+
+		matches := rowsRegexp.FindStringSubmatch(planRow.String)
+		if len(matches) != 2 {
+			continue
+		}
+		v, err := strconv.ParseInt(matches[1], 10, 64)
+		if err != nil {
+			return 0, errors.Errorf("failed to get integer from %q", matches[1])
+		}
+		rowCount = v
+	}
+	if err := rows.Err(); err != nil {
+		return 0, err
+	}
+	return rowCount, nil
+}
