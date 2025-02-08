2312b4cf0457be39e252db14389e07e0d9c88fa7
d-bytebase <d@bytebase.com>
2025-02-08 01:26:54 -0800

    chore: update

--- backend/migrator/migrator_test.go
+++ backend/migrator/migrator_test.go
@@ -19,7 +19,7 @@ import (
 )
 
 func TestGetMinorMigrationVersions(t *testing.T) {
-	names := []string{latestDataFile, latestSchemaFile, "1.0", "1.1", "1.2", "1.3", "1.4"}
+	names := []string{latestSchemaFile, "1.0", "1.1", "1.2", "1.3", "1.4"}
 
 	tests := []struct {
 		names          []string
@@ -60,11 +60,11 @@ func TestGetMinorVersions(t *testing.T) {
 		want  []semver.Version
 	}{
 		{
-			names: []string{fmt.Sprintf("migration/release/%s", latestDataFile), fmt.Sprintf("migration/dev/%s", latestSchemaFile), "migration/release/1.1", "migration/release/1.0"},
+			names: []string{fmt.Sprintf("migration/dev/%s", latestSchemaFile), "migration/release/1.1", "migration/release/1.0"},
 			want:  []semver.Version{semver.MustParse("1.0.0"), semver.MustParse("1.1.0")},
 		},
 		{
-			names: []string{fmt.Sprintf("migration/release/%s", latestDataFile), fmt.Sprintf("migration/dev/%s", latestSchemaFile)},
+			names: []string{fmt.Sprintf("migration/dev/%s", latestSchemaFile)},
 			want:  nil,
 		},
 	}
