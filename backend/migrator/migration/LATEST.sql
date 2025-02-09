0c3db94ac3d49b669b84b70b8bc60dfb00b33336
d-bytebase <d@bytebase.com>
2025-02-09 07:41:38 -0800

    chore: update

--- backend/migrator/migration/LATEST.sql
+++ backend/migrator/migration/LATEST.sql
@@ -675,21 +675,8 @@ CREATE TABLE export_archive (
   payload JSONB NOT NULL DEFAULT '{}'
 );
 
-CREATE TABLE sql_lint_config (
-  id TEXT NOT NULL PRIMARY KEY,
-  creator_id INTEGER NOT NULL REFERENCES principal (id),
-  created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-  updater_id INTEGER NOT NULL REFERENCES principal (id),
-  updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-  config JSONB NOT NULL DEFAULT '{}'
-);
-
 CREATE TABLE user_group (
   email TEXT PRIMARY KEY,
-  creator_id INTEGER NOT NULL REFERENCES principal (id),
-  created_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
-  updater_id INTEGER NOT NULL REFERENCES principal (id),
-  updated_ts BIGINT NOT NULL DEFAULT extract(epoch from now()),
   name TEXT NOT NULL,
   description TEXT NOT NULL DEFAULT '',
   payload JSONB NOT NULL DEFAULT '{}'
