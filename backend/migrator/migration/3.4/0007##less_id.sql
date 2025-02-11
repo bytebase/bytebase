3ccccd394e49872faf5da3bd63e61b488089f2f0
d-bytebase <d@bytebase.com>
2025-02-11 09:49:11 -0800

    chore: update

--- backend/migrator/migration/3.4/0007##less_id.sql
+++ backend/migrator/migration/3.4/0007##less_id.sql
@@ -1,9 +1,3 @@
-ALTER TABLE stage ADD COLUMN environment TEXT;
-UPDATE stage SET environment = environment.resource_id FROM environment WHERE environment.id = stage.environment_id;
-ALTER TABLE stage DROP COLUMN environment_id;
-ALTER TABLE stage ALTER COLUMN environment SET NOT NULL;
-ALTER TABLE stage ADD constraint stage_environment_fkey FOREIGN KEY (environment) references environment(resource_id);
-
 ALTER TABLE project_webhook ADD COLUMN project TEXT;
 UPDATE project_webhook SET project = project.resource_id FROM project WHERE project.id = project_webhook.project_id;
 ALTER TABLE project_webhook DROP COLUMN project_id;
@@ -60,6 +54,12 @@ UPDATE pipeline SET project = project.resource_id FROM project WHERE project.id 
 ALTER TABLE pipeline DROP COLUMN project_id;
 ALTER TABLE pipeline ALTER COLUMN project SET NOT NULL;
 ALTER TABLE pipeline ADD constraint pipeline_project_fkey FOREIGN KEY (project) references project(resource_id);
+
+ALTER TABLE stage ADD COLUMN environment TEXT;
+UPDATE stage SET environment = environment.resource_id FROM environment WHERE environment.id = stage.environment_id;
+ALTER TABLE stage DROP COLUMN environment_id;
+ALTER TABLE stage ALTER COLUMN environment SET NOT NULL;
+ALTER TABLE stage ADD constraint stage_environment_fkey FOREIGN KEY (environment) references environment(resource_id);
 
 ALTER TABLE task ADD COLUMN instance TEXT;
 UPDATE task SET instance = instance.resource_id FROM instance WHERE instance.id = task.instance_id;
