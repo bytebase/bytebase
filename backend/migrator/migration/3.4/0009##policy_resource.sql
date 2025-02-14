880892ed2d932103932a8b38a7dc0b9744201c7a
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-02-14 21:01:40 +0800

    chore: less database group uid (#15224)
    * chore: less database group uid
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 42ab95f95f279363be05fc37fc01c5b8b8131781

--- backend/migrator/migration/3.4/0009##policy_resource.sql
+++ backend/migrator/migration/3.4/0009##policy_resource.sql
@@ -2,7 +2,7 @@ DROP INDEX IF EXISTS idx_policy_unique_resource_type_resource_id_type;
 ALTER TABLE policy ADD COLUMN IF NOT EXISTS resource TEXT;
 -- DML.
 UPDATE policy SET resource = '' WHERE resource_type = 'WORKSPACE';
-UPDATE policy SET resource = 'projectss/' || project.resource_id FROM project WHERE resource_type = 'PROJECT' AND project.id = policy.resource_id;
+UPDATE policy SET resource = 'projects/' || project.resource_id FROM project WHERE resource_type = 'PROJECT' AND project.id = policy.resource_id;
 UPDATE policy SET resource = 'environments/' || environment.resource_id FROM environment WHERE resource_type = 'ENVIRONMENT' AND environment.id = policy.resource_id;
 UPDATE policy SET resource = 'instances/' || instance.resource_id FROM instance WHERE resource_type = 'INSTANCE' AND instance.id = policy.resource_id;
 UPDATE policy SET resource = 'instances/' || db.instance || '/databases/' || db.name FROM db WHERE resource_type = 'DATABASE' AND db.id = policy.resource_id;
