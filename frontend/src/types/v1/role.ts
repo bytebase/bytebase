e6b592b46e85b4deefe418b8b6da7734226ad6bc
steven <stevenlgtm@gmail.com>
2025-02-19 17:57:45 +0800

    chore: update frontend

--- frontend/src/types/v1/role.ts
+++ frontend/src/types/v1/role.ts
@@ -2,10 +2,8 @@ import { roleNamePrefix } from "@/store/modules/v1/common";
 import { PresetRoleType } from "../iam";
 
 export const VirtualRoleType = {
-  WORKSPACE_ADMIN: PresetRoleType.WORKSPACE_ADMIN,
-  WORKSPACE_DBA: PresetRoleType.WORKSPACE_DBA,
-  LAST_APPROVER: `${roleNamePrefix}LAST_APPROVER`,
   CREATOR: `${roleNamePrefix}CREATOR`,
+  LAST_APPROVER: `${roleNamePrefix}LAST_APPROVER`,
 };
 
 export const IssueReleaserRoleType = {
