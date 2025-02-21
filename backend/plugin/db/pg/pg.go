834545f8b8f20e485e9aaa962b72b25cca8ea8ba
d-bytebase <d@bytebase.com>
2025-02-20 23:20:11 -0800

    refactor: connection string

--- backend/plugin/db/pg/pg.go
+++ backend/plugin/db/pg/pg.go
@@ -83,6 +83,10 @@ func (driver *Driver) Open(ctx context.Context, _ storepb.Engine, config db.Conn
 	if err != nil {
 		return nil, err
 	}
+	pgxConnConfig.RuntimeParams["application_name"] = "bytebase"
+	if config.ReadOnly {
+		pgxConnConfig.RuntimeParams["default_transaction_read_only"] = "true"
+	}
 
 	if config.SSHConfig.Host != "" {
 		sshClient, err := util.GetSSHClient(config.SSHConfig)
@@ -169,10 +173,6 @@ func getPGConnectionConfig(config db.ConnectionConfig) (*pgx.ConnConfig, error) 
 	if cfg != nil {
 		connConfig.TLSConfig = cfg
 	}
-	if config.ReadOnly {
-		connConfig.RuntimeParams["default_transaction_read_only"] = "true"
-		connConfig.RuntimeParams["application_name"] = "bytebase"
-	}
 
 	return connConfig, nil
 }
@@ -202,7 +202,7 @@ func getRDSConnectionConfig(ctx context.Context, conf db.ConnectionConfig) (*pgx
 		return nil, err
 	}
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s application_name=bytebase",
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s",
 		conf.Host, conf.Port, conf.Username, password, conf.Database,
 	)
 	return pgx.ParseConfig(dsn)
@@ -218,7 +218,7 @@ func getCloudSQLConnectionConfig(ctx context.Context, conf db.ConnectionConfig) 
 		return nil, err
 	}
 
-	dsn := fmt.Sprintf("user=%s database=%s application_name=bytebase", conf.Username, conf.Database)
+	dsn := fmt.Sprintf("user=%s database=%s", conf.Username, conf.Database)
 	config, err := pgx.ParseConfig(dsn)
 	if err != nil {
 		return nil, err
