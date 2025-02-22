08a54056d0f76e4bf79bbb4c5d5954ca44ad48a6
d-bytebase <d@bytebase.com>
2025-02-22 06:42:47 -0800

    chore: combine db icons

--- frontend/src/components/InstanceForm/constants.ts
+++ frontend/src/components/InstanceForm/constants.ts
@@ -65,46 +65,46 @@ export const EngineList = computed(() => {
 });
 
 export const EngineIconPath: Record<string, string> = {
-  [Engine.MYSQL]: new URL("@/assets/db-mysql.png", import.meta.url).href,
-  [Engine.POSTGRES]: new URL("@/assets/db-postgres.png", import.meta.url).href,
-  [Engine.TIDB]: new URL("@/assets/db-tidb.png", import.meta.url).href,
-  [Engine.SNOWFLAKE]: new URL("@/assets/db-snowflake.png", import.meta.url)
+  [Engine.MYSQL]: new URL("@/assets/db/mysql.png", import.meta.url).href,
+  [Engine.POSTGRES]: new URL("@/assets/db/postgres.png", import.meta.url).href,
+  [Engine.TIDB]: new URL("@/assets/db/tidb.png", import.meta.url).href,
+  [Engine.SNOWFLAKE]: new URL("@/assets/db/snowflake.png", import.meta.url)
     .href,
-  [Engine.CLICKHOUSE]: new URL("@/assets/db-clickhouse.png", import.meta.url)
+  [Engine.CLICKHOUSE]: new URL("@/assets/db/clickhouse.png", import.meta.url)
     .href,
-  [Engine.MONGODB]: new URL("@/assets/db-mongodb.png", import.meta.url).href,
-  [Engine.SPANNER]: new URL("@/assets/db-spanner.png", import.meta.url).href,
-  [Engine.REDIS]: new URL("@/assets/db-redis.png", import.meta.url).href,
-  [Engine.ORACLE]: new URL("@/assets/db-oracle.svg", import.meta.url).href,
-  [Engine.DM]: new URL("@/assets/db-dm.png", import.meta.url).href,
-  [Engine.MSSQL]: new URL("@/assets/db-mssql.svg", import.meta.url).href,
-  [Engine.REDSHIFT]: new URL("@/assets/db-redshift.svg", import.meta.url).href,
-  [Engine.MARIADB]: new URL("@/assets/db-mariadb.png", import.meta.url).href,
+  [Engine.MONGODB]: new URL("@/assets/db/mongodb.png", import.meta.url).href,
+  [Engine.SPANNER]: new URL("@/assets/db/spanner.png", import.meta.url).href,
+  [Engine.REDIS]: new URL("@/assets/db/redis.png", import.meta.url).href,
+  [Engine.ORACLE]: new URL("@/assets/db/oracle.svg", import.meta.url).href,
+  [Engine.DM]: new URL("@/assets/db/dm.png", import.meta.url).href,
+  [Engine.MSSQL]: new URL("@/assets/db/mssql.svg", import.meta.url).href,
+  [Engine.REDSHIFT]: new URL("@/assets/db/redshift.svg", import.meta.url).href,
+  [Engine.MARIADB]: new URL("@/assets/db/mariadb.png", import.meta.url).href,
   [Engine.OCEANBASE]: new URL(
-    "@/assets/db-oceanbase-mysql.svg",
+    "@/assets/db/oceanbase-mysql.svg",
     import.meta.url
   ).href,
   [Engine.OCEANBASE_ORACLE]: new URL(
-    "@/assets/db-oceanbase-oracle.svg",
+    "@/assets/db/oceanbase-oracle.svg",
     import.meta.url
   ).href,
-  [Engine.RISINGWAVE]: new URL("@/assets/db-risingwave.png", import.meta.url)
+  [Engine.RISINGWAVE]: new URL("@/assets/db/risingwave.png", import.meta.url)
     .href,
-  [Engine.STARROCKS]: new URL("@/assets/db-starrocks.png", import.meta.url)
+  [Engine.STARROCKS]: new URL("@/assets/db/starrocks.png", import.meta.url)
     .href,
-  [Engine.DORIS]: new URL("@/assets/db-doris.png", import.meta.url).href,
-  [Engine.HIVE]: new URL("@/assets/db-hive.svg", import.meta.url).href,
+  [Engine.DORIS]: new URL("@/assets/db/doris.png", import.meta.url).href,
+  [Engine.HIVE]: new URL("@/assets/db/hive.svg", import.meta.url).href,
   [Engine.ELASTICSEARCH]: new URL(
-    "@/assets/db-elasticsearch.svg",
+    "@/assets/db/elasticsearch.svg",
     import.meta.url
   ).href,
-  [Engine.BIGQUERY]: new URL("@/assets/bigquery.svg", import.meta.url).href,
-  [Engine.DYNAMODB]: new URL("@/assets/db-dynamodb.svg", import.meta.url).href,
-  [Engine.DATABRICKS]: new URL("@/assets/db-databricks.svg", import.meta.url)
+  [Engine.BIGQUERY]: new URL("@/assets/db/bigquery.svg", import.meta.url).href,
+  [Engine.DYNAMODB]: new URL("@/assets/db/dynamodb.svg", import.meta.url).href,
+  [Engine.DATABRICKS]: new URL("@/assets/db/databricks.svg", import.meta.url)
     .href,
-  [Engine.COCKROACHDB]: new URL("@/assets/db-cockroachdb.png", import.meta.url)
+  [Engine.COCKROACHDB]: new URL("@/assets/db/cockroachdb.png", import.meta.url)
     .href,
-  [Engine.COSMOSDB]: new URL("@/assets/db-cosmosdb.svg", import.meta.url).href,
+  [Engine.COSMOSDB]: new URL("@/assets/db/cosmosdb.svg", import.meta.url).href,
 };
 
 export const MongoDBConnectionStringSchemaList = [
