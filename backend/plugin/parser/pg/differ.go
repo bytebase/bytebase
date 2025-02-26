650993d39ba4139cf5bfe4bf98bcb8b64c3b6718
boojack <stevenlgtm@gmail.com>
2025-02-26 14:19:59 +0800

    chore: schema diff for cockroach (#15330)
    * chore: schema diff for cockroach
    
    * chore: update comments
    
    * chore: update
    
    * chore: update
    
    * chore: fix linter
    
    * chore: update

--- backend/plugin/parser/pg/differ.go
+++ backend/plugin/parser/pg/differ.go
@@ -23,7 +23,6 @@ import (
 
 func init() {
 	base.RegisterSchemaDiffFunc(storepb.Engine_POSTGRES, SchemaDiff)
-	base.RegisterSchemaDiffFunc(storepb.Engine_COCKROACHDB, SchemaDiff)
 }
 
 // diffNode defines different modification types as the safe change order.
