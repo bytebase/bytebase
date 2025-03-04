feaacae7580398ce04b5542f7ed61dd8f0c59df3
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-03-04 13:06:49 +0800

    chore: combine sync and metadata (#15397)

--- backend/migrator/migration/3.5/0003##db_sync.sql
+++ backend/migrator/migration/3.5/0003##db_sync.sql
@@ -1,3 +1,2 @@
-ALTER TABLE db ADD COLUMN sync jsonb NOT NULL DEFAULT '{}';
 ALTER TABLE db ADD COLUMN deleted boolean NOT NULL DEFAULT FALSE;
 UPDATE db SET deleted = TRUE WHERE sync_status = 'NOT_FOUND';
