b70839c05324d7be2128d87a72a9e95b4a684a0c
h3n4l <oysterdays@gmail.com>
2025-03-06 11:52:37 +0800

    Update backend/plugin/db/mssql/mssql.go
    Co-authored-by: p0ny <p0uy@outlook.com>

--- backend/plugin/db/mssql/mssql.go
+++ backend/plugin/db/mssql/mssql.go
@@ -67,9 +67,6 @@ func (driver *Driver) Open(_ context.Context, _ storepb.Engine, config db.Connec
 
 	var err error
 	if config.TLSConfig.UseSSL && config.TLSConfig.SslCA != "" {
-		// We should not TrustServerCertificate in production environment, otherwise, TLS is susceptible
-		// to man-in-the middle attacks. TrustServerCertificate makes driver accepts any certificate presented by the server
-		// and any host name in that certificate.
 		// Due to Golang runtime limitation, x509 package will throw the error of 'certificate relies on legacy Common Name field, use SANs instead.
 		// Driver reads the certificate from file instead of regarding it as certificate content.
 		// https://github.com/microsoft/go-mssqldb/blob/main/msdsn/conn_str.go#L159
