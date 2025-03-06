4413fef4d5b6c63c84237b4954da5fdc818f1571
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-03-06 11:24:46 +0800

    chore: no deployment config UI (#15414)
    * chore: no deployment config UI
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: cc92cc505f395253857c9357998ce43bec65ee92

--- frontend/src/components/DeploymentConfigTool/index.ts
+++
@@ -1,7 +0,0 @@
-import DeploymentConfigTool from "./DeploymentConfigTool.vue";
-import DeploymentMatrix from "./DeploymentMatrix.vue";
-import DeploymentStage from "./DeploymentStage.vue";
-
-export default DeploymentConfigTool;
-
-export { DeploymentConfigTool, DeploymentStage, DeploymentMatrix };
