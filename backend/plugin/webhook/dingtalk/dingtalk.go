537449d19f1e55b45891b1c197c501f87f90a1f6
ecmadao <wlec@outlook.com>
2025-03-11 20:23:34 +0800

    fix: lint

--- backend/plugin/webhook/dingtalk/dingtalk.go
+++ backend/plugin/webhook/dingtalk/dingtalk.go
@@ -45,14 +45,14 @@ type Message struct {
 }
 
 func init() {
-	webhook.Register("bb.plugin.webhook.dingtalk", &DingTalkReceiver{})
+	webhook.Register("bb.plugin.webhook.dingtalk", &dingTalkReceiver{})
 }
 
-// DingTalkReceiver is the receiver for DingTalk.
-type DingTalkReceiver struct {
+// dingTalkReceiver is the receiver for DingTalk.
+type dingTalkReceiver struct {
 }
 
-func (*DingTalkReceiver) Post(context webhook.Context) error {
+func (*dingTalkReceiver) Post(context webhook.Context) error {
 	if context.DirectMessage && len(context.MentionEndUsers) > 0 {
 		sendDirectMessage(context)
 		return nil
@@ -60,7 +60,7 @@ func (*DingTalkReceiver) Post(context webhook.Context) error {
 	return sendMessage(context)
 }
 
-// returns true if the message is sent successfully
+// sendDirectMessage returns true if the message is sent successfully.
 func sendDirectMessage(webhookCtx webhook.Context) {
 	dingtalk := webhookCtx.IMSetting.GetDingtalk()
 	if dingtalk == nil {
