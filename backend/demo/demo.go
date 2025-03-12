e6e7e62021cf79599b91715bf4b1b8550c24e722
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-03-12 11:57:49 +0800

    chore: refactor demo (#15500)
    Former-commit-id: 6a8cb33b652a8d5567f5990855910d6620fa1493
    Former-commit-id: c05acc56eb4398996bea3143b042479e2ef0c2e1

--- backend/demo/demo.go
+++ backend/demo/demo.go
@@ -7,68 +7,46 @@ import (
 	"fmt"
 	"io/fs"
 	"log/slog"
-
-	"github.com/pkg/errors"
-
-	"github.com/bytebase/bytebase/backend/store"
 )
 
 //go:embed data
 var demoFS embed.FS
 
 // LoadDemoDataIfNeeded loads the demo data if specified.
-func LoadDemoDataIfNeeded(ctx context.Context, stores *store.Store, demoName string) error {
-	if demoName == "" {
+func LoadDemoDataIfNeeded(ctx context.Context, db *sql.DB, demo string) error {
+	if demo == "" {
 		slog.Debug("Skip setting up demo data. Demo not specified.")
 		return nil
 	}
-	slog.Info(fmt.Sprintf("Setting up demo %q...", demoName))
+	slog.Info(fmt.Sprintf("Setting up demo %q...", demo))
 
-	db := stores.GetDB()
-	var exists bool
+	var ok bool
 	if err := db.QueryRowContext(ctx,
 		`SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'environment')`,
-	).Scan(&exists); err != nil {
+	).Scan(&ok); err != nil {
 		return err
 	}
-	if exists {
+	if ok {
 		slog.Info("Skip setting up demo data. Data already exists.")
 		return nil
 	}
 
-	names, err := fs.Glob(demoFS, fmt.Sprintf("data/%s/*.sql", demoName))
+	buf, err := fs.ReadFile(demoFS, "data/dump.sql")
 	if err != nil {
 		return err
 	}
-
-	// Loop over all data files and execute them in order.
-	for _, name := range names {
-		if err := applyDataFile(name, db); err != nil {
-			return errors.Wrapf(err, "Failed to load file: %q", name)
-		}
+	txn, err := db.Begin()
+	if err != nil {
+		return err
 	}
+	defer txn.Rollback()
+	if _, err := txn.Exec(string(buf)); err != nil {
+		return err
+	}
+	if err := txn.Commit(); err != nil {
+		return err
+	}
+
 	slog.Info("Completed demo data setup.")
 	return nil
-}
-
-// applyDataFile runs a single demo data file within a transaction.
-func applyDataFile(name string, db *sql.DB) error {
-	slog.Info(fmt.Sprintf("Applying data file %s...", name))
-	tx, err := db.Begin()
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	// Read and execute migration file.
-	var buf []byte
-	if buf, err = fs.ReadFile(demoFS, name); err != nil {
-		return err
-	}
-	stmt := string(buf)
-	if _, err := tx.Exec(stmt); err != nil {
-		return err
-	}
-
-	return tx.Commit()
 }
