8f808b780ff4c1a6bfd98166cba33a35e0815a8c
d-bytebase <d@bytebase.com>
2025-03-11 20:54:20 -0700

    chore: refactor demo

--- backend/server/server.go
+++ backend/server/server.go
@@ -166,7 +166,7 @@ func NewServer(ctx context.Context, profile *config.Profile) (*Server, error) {
 	if profile.Readonly {
 		slog.Info("Database is opened in readonly mode. Skip migration and demo data setup.")
 	} else {
-		if err := demo.LoadDemoDataIfNeeded(ctx, stores, profile.DemoName); err != nil {
+		if err := demo.LoadDemoDataIfNeeded(ctx, stores.GetDB(), profile.DemoName); err != nil {
 			return nil, errors.Wrapf(err, "failed to load demo data")
 		}
 		if err := migrator.MigrateSchema(ctx, stores.GetDB()); err != nil {
