d5567218b7c86c85c835a3f94e0bfb0c2c2c0218
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-03-12 18:20:20 -0700

    chore: remove unmaintained stuff (#15518)
    * chore: remove unmaintained stuff
    
    * chore: remove unmaintained stuff
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: c79f120b390d77d2d4a60e67ba9787ba75f5bdbf
    Former-commit-id: 5ffa56dfa2a4b6768c3fe34e9efe41c9b855294d

--- backend/resources/postgres/meta_instance.go
+++ backend/resources/postgres/meta_instance.go
@@ -5,23 +5,15 @@ import (
 	"database/sql"
 	"fmt"
 	"log/slog"
-	"os"
-	"os/exec"
 	"path"
-	"path/filepath"
-	"time"
-
-	"github.com/pkg/errors"
 
 	"github.com/bytebase/bytebase/backend/common"
 )
 
 // StartMetadataInstance starts the metadata instance.
-func StartMetadataInstance(ctx context.Context, dataDir, resourceDir, pgBinDir, pgUser, demoName string, port int, mode common.ReleaseMode) (func(), error) {
+func StartMetadataInstance(ctx context.Context, dataDir, pgBinDir, pgUser, demoName string, port int, mode common.ReleaseMode) (func(), error) {
 	pgDataDir := getPostgresDataDir(dataDir, demoName)
-	if err := upgradePostgres(dataDir, resourceDir, pgBinDir, pgDataDir, pgUser, port); err != nil {
-		return nil, err
-	}
+
 	slog.Info("-----Embedded Postgres BEGIN-----")
 	slog.Info(fmt.Sprintf("Start embedded Postgres datastorePort=%d pgDataDir=%s", port, pgDataDir))
 	if err := initDB(pgBinDir, pgDataDir, pgUser); err != nil {
@@ -64,92 +56,4 @@ func getPostgresDataDir(dataDir string, demoName string) string {
 		return path.Join(dataDir, "pgdata-demo", demoName)
 	}
 	return path.Join(dataDir, "pgdata")
-}
-
-func upgradePostgres(dataDir, resourceDir, pgBinDir, pgDataDir, pgUser string, port int) error {
-	version, err := getVersion(pgDataDir)
-	if err != nil {
-		return err
-	}
-	if version == "" {
-		return nil
-	}
-	if version == currentVersion {
-		return nil
-	}
-
-	previousBinDir, err := getPreviousVersionBinDir(resourceDir, version)
-	if err != nil {
-		return err
-	}
-
-	// Dump metadata SQL.
-	slog.Info("started metadata SQL dump")
-	if err := start(port, previousBinDir, pgDataDir, true /* serverLog */); err != nil {
-		return err
-	}
-	dumpFilePath := path.Join(dataDir, "meta.sql")
-	cmd := exec.Command(filepath.Join(previousBinDir, "pg_dump"), "-U", pgUser, "-h", common.GetPostgresSocketDir(), "-p", fmt.Sprintf("%d", port), pgUser)
-	dumpFile, err := os.Create(dumpFilePath)
-	if err != nil {
-		return err
-	}
-	defer dumpFile.Close()
-	cmd.Stdout = dumpFile
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-	if err := cmd.Wait(); err != nil {
-		return err
-	}
-	if err := stop(previousBinDir, pgDataDir); err != nil {
-		return err
-	}
-	slog.Info("finished metadata SQL dump")
-	if err := os.Rename(pgDataDir, path.Join(dataDir, fmt.Sprintf("pgdata-%d", time.Now().Unix()))); err != nil {
-		return err
-	}
-	slog.Info("renamed old pgdata directory")
-
-	if err := initDB(pgBinDir, pgDataDir, pgUser); err != nil {
-		return err
-	}
-	if err := start(port, pgBinDir, pgDataDir, true /* serverLog */); err != nil {
-		return err
-	}
-	createDatabaseSQL := fmt.Sprintf("CREATE DATABASE %s;", pgUser)
-	cmd = exec.Command(filepath.Join(pgBinDir, "psql"), "-U", pgUser, "-h", common.GetPostgresSocketDir(), "-p", fmt.Sprintf("%d", port), "postgres", "-c", createDatabaseSQL)
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-	cmd = exec.Command(filepath.Join(pgBinDir, "psql"), "-U", pgUser, "-h", common.GetPostgresSocketDir(), "-p", fmt.Sprintf("%d", port), pgUser, "-f", dumpFilePath)
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-	if err := stop(pgBinDir, pgDataDir); err != nil {
-		return err
-	}
-
-	if err := os.Remove(dumpFilePath); err != nil {
-		return err
-	}
-	slog.Info("deleted metadata SQL dump")
-
-	slog.Info("finished pg version upgrade")
-	return nil
-}
-
-func getPreviousVersionBinDir(resourceDir, version string) (string, error) {
-	prefix, err := getTarName()
-	if err != nil {
-		return "", err
-	}
-
-	switch version {
-	case "14":
-		return path.Join(resourceDir, prefix, "bin"), nil
-	case "16":
-		return path.Join(resourceDir, fmt.Sprintf("%s%s", prefix, version), "bin"), nil
-	}
-	return "", errors.Errorf("invalid postgres version %q", version)
 }
