34d74225119032acc00a2e86742512568fc2691c
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-03-13 19:09:36 -0700

    chore: cleanup flags (#15543)
    * chore: cleanup flags
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: f0d4780d9e4497b8d1c64dfeeb259451684cd300
    Former-commit-id: 781f69f14e06219542071ce143b109a23c31ec8f

--- backend/bin/server/cmd/profile.go
+++ backend/bin/server/cmd/profile.go
@@ -18,24 +18,22 @@ func getBaseProfile(dataDir string) *config.Profile {
 	}
 
 	return &config.Profile{
-		ExternalURL:          flags.externalURL,
-		Port:                 flags.port,     // Using flags.port as our gRPC server port.
-		DatastorePort:        flags.port + 2, // Using flags.port + 2 as our datastore port.
-		SampleDatabasePort:   sampleDatabasePort,
-		Readonly:             flags.readonly,
-		SaaS:                 flags.saas,
-		EnableJSONLogging:    flags.enableJSONLogging,
-		Debug:                flags.debug,
-		IsDocker:             isDocker(),
-		DataDir:              dataDir,
-		ResourceDir:          common.GetResourceDir(dataDir),
-		DemoName:             flags.demoName,
-		Version:              version,
-		GitCommit:            gitcommit,
-		PgURL:                flags.pgURL,
-		DeployID:             uuid.NewString()[:8],
-		LastActiveTs:         time.Now().Unix(),
-		DevelopmentVersioned: flags.developmentVersioned,
+		ExternalURL:        flags.externalURL,
+		Port:               flags.port,     // Using flags.port as our gRPC server port.
+		DatastorePort:      flags.port + 2, // Using flags.port + 2 as our datastore port.
+		SampleDatabasePort: sampleDatabasePort,
+		SaaS:               flags.saas,
+		EnableJSONLogging:  flags.enableJSONLogging,
+		Debug:              flags.debug,
+		IsDocker:           isDocker(),
+		DataDir:            dataDir,
+		ResourceDir:        common.GetResourceDir(dataDir),
+		Demo:               flags.demo,
+		Version:            version,
+		GitCommit:          gitcommit,
+		PgURL:              flags.pgURL,
+		DeployID:           uuid.NewString()[:8],
+		LastActiveTs:       time.Now().Unix(),
 	}
 }
 
--- backend/bin/server/cmd/profile_release.go
+++ backend/bin/server/cmd/profile_release.go
@@ -3,8 +3,6 @@
 package cmd
 
 import (
-	"time"
-
 	"github.com/bytebase/bytebase/backend/common"
 	"github.com/bytebase/bytebase/backend/component/config"
 )
@@ -13,11 +11,9 @@ func activeProfile(dataDir string) *config.Profile {
 	p := getBaseProfile(dataDir)
 	p.RuntimeDebug.Store(p.Debug)
 	p.Mode = common.ReleaseModeProd
-	p.PgUser = "bb"
-	p.BackupRunnerInterval = 10 * time.Minute
-	p.AppRunnerInterval = 30 * time.Second
 	// Enable metric if it's not explicitly disabled and it's not running in demo mode.
-	p.EnableMetric = !flags.disableMetric && p.DemoName == ""
-	p.MetricConnectionKey = "so9lLwj5zLjH09sxNabsyVNYSsAHn68F"
+	f !flags.disableMetric && !p.Demo {
+		p.MetricConnectionKey = "so9lLwj5zLjH09sxNabsyVNYSsAHn68F"
+	}
 	return p
 }
