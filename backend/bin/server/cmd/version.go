e3012912f57175fcf8286c891091c9ae3006ad59
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-03-14 00:20:10 -0700

    chore: remove cgo as it's already the default (#15548)
    * chore: remove cgo as it's already the default
    
    * chore: update

--- backend/bin/server/cmd/version.go
+++ backend/bin/server/cmd/version.go
@@ -8,10 +8,7 @@ import (
 
 // These should be set via go build -ldflags -X 'xxxx'.
 var version = "development"
-var goversion = "unknown"
 var gitcommit = "unknown"
-var buildtime = "unknown"
-var builduser = "unknown"
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
@@ -22,9 +19,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version of Bytebase",
 	Run: func(_ *cobra.Command, _ []string) {
 		fmt.Printf("Bytebase version: %s\n", version)
-		fmt.Printf("Golang version: %s\n", goversion)
 		fmt.Printf("Git commit hash: %s\n", gitcommit)
-		fmt.Printf("Built on: %s\n", buildtime)
-		fmt.Printf("Built by: %s\n", builduser)
 	},
 }
