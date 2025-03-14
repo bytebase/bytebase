4e3267edc72e87636ac64b8be16ee4d926105e45
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-03-14 02:17:21 -0700

    chore: clean up build args (#15550)
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: c497c92ec3868d802d9ceaf3d1bc538da6fb9373
    Former-commit-id: 4705905ced1fae52007ae000c6c8e00382d48334

--- scripts/build_init.sh
+++ scripts/build_init.sh
@@ -13,6 +13,7 @@ NC='\033[0m' # No Color
 
 # Invoke from project root
 VERSION=$(cat ./scripts/VERSION)
+GIT_COMMIT=$(git rev-parse HEAD)
 
 # Version function used for version string comparison
 version() { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
@@ -27,3 +28,22 @@ mkdir_output() {
     fi
     echo "$OUTPUT_DIR"
 }
+
+# Go and node version checks.
+TARGET_GO_VERSION="1.24.0"
+GO_VERSION=`go version | { read _ _ v _; echo ${v#go}; }`
+if [ "$(version ${GO_VERSION})" -lt "$(version $TARGET_GO_VERSION)" ];
+then
+   echo "${RED}Precheck failed.${NC} Require go version >= $TARGET_GO_VERSION. Current version ${GO_VERSION}."; exit 1;
+fi
+
+NODE_VERSION=`node -v | { read v; echo ${v#v}; }`
+if [ "$(version ${NODE_VERSION})" -lt "$(version 22.13.0)" ];
+then
+   echo "${RED}Precheck failed.${NC} Require node.js version >= 22.13.0. Current version ${NODE_VERSION}."; exit 1;
+fi
+
+if ! command -v npm > /dev/null
+then
+   echo "${RED}Precheck failed.${NC} npm is not installed."; exit 1;
+fi
