64fe7bf71844cd8fa56c6c08d1ab4dd377ac2303
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-03-24 01:53:40 -0700

    chore: migrate ai setting (#15611)

---
+++ backend/migrator/migration/3.5/0016##ai.sql
@@ -0,0 +1,12 @@
+INSERT INTO setting (name, value) VALUES ('bb.ai',
+    json_build_object(
+        'provider', 'OPEN_AI',
+        'apiKey', (SELECT COALESCE(value, '') FROM setting WHERE name = 'bb.plugin.openai.key'),
+        'endpoint', (
+            SELECT TRIM(TRAILING '/' FROM (CASE WHEN COALESCE(value, '') = '' THEN 'https://api.openai.com/' ELSE COALESCE(value, '') END)) || '/v1/chat/completions'
+            FROM setting WHERE name = 'bb.plugin.openai.endpoint'),
+        'model', 'gpt-3.5-turbo'
+    )
+);
+DELETE FROM setting WHERE name = 'bb.ai' AND (value::jsonb)->>'apiKey' = '';
+DELETE FROM setting WHERE name LIKE 'bb.plugin.openai%';
