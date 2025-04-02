282de77d4a3f4d532b1ca9cc2b38d14fb5718eae
boojack <stevenlgtm@gmail.com>
2025-04-02 10:55:25 +0800

    chore: remove empty database (#15707)
    (cherry picked from commit f5e0b06e745aea108a3c9bfe26dedb0d2fc1217e)

--- frontend/src/types/v1/database.ts
+++ frontend/src/types/v1/database.ts
@@ -1,17 +1,13 @@
-import { extractDatabaseResourceName } from "@/utils";
+import { extractDatabaseResourceName, isNullOrUndefined } from "@/utils";
 import { EMPTY_ID, UNKNOWN_ID } from "../const";
 import { State } from "../proto/v1/common";
 import { Database } from "../proto/v1/database_service";
 import type { Environment } from "../proto/v1/environment_service";
 import type { InstanceResource } from "../proto/v1/instance_service";
-import { emptyEnvironment, unknownEnvironment } from "./environment";
-import {
-  emptyInstanceResource,
-  unknownInstance,
-  unknownInstanceResource,
-} from "./instance";
+import { unknownEnvironment } from "./environment";
+import { unknownInstance, unknownInstanceResource } from "./instance";
 import type { ComposedProject } from "./project";
-import { emptyProject, unknownProject } from "./project";
+import { unknownProject } from "./project";
 
 export interface ComposedDatabase extends Database {
   /** related project entity */
@@ -27,27 +23,6 @@ export interface ComposedDatabase extends Database {
 }
 
 export const UNKNOWN_DATABASE_NAME = `${unknownInstance().name}/databases/${UNKNOWN_ID}`;
-
-export const emptyDatabase = (): ComposedDatabase => {
-  const projectEntity = emptyProject();
-  const instanceResource = emptyInstanceResource();
-  const effectiveEnvironmentEntity = emptyEnvironment();
-  const database = Database.fromJSON({
-    name: `${instanceResource.name}/databases/${EMPTY_ID}`,
-    uid: String(EMPTY_ID),
-    state: State.ACTIVE,
-    project: projectEntity.name,
-    effectiveEnvironment: effectiveEnvironmentEntity.name,
-  });
-  return {
-    ...database,
-    databaseName: "",
-    instance: instanceResource.name,
-    instanceResource,
-    projectEntity,
-    effectiveEnvironmentEntity,
-  };
-};
 
 export const unknownDatabase = (): ComposedDatabase => {
   const projectEntity = unknownProject();
@@ -73,12 +48,11 @@ export const unknownDatabase = (): ComposedDatabase => {
 export const isValidDatabaseName = (name: any): name is string => {
   if (typeof name !== "string") return false;
   const { instanceName, databaseName } = extractDatabaseResourceName(name);
-  return Boolean(
-    instanceName &&
-      instanceName !== String(UNKNOWN_ID) &&
-      instanceName !== String(EMPTY_ID) &&
-      databaseName &&
-      databaseName !== String(UNKNOWN_ID) &&
-      databaseName !== String(EMPTY_ID)
+  return (
+    !isNullOrUndefined(instanceName) &&
+    !isNullOrUndefined(databaseName) &&
+    instanceName !== String(UNKNOWN_ID) &&
+    instanceName !== String(EMPTY_ID) &&
+    databaseName !== String(UNKNOWN_ID)
   );
 };
