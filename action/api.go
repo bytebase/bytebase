7d32839093b753789d2dbafbdaf9806f426cf577
d-bytebase <d@bytebase.com>
2025-04-02 04:04:08 -0700

    chore: update action

---
+++ action/api.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"context"
+)
+
+func login(_ context.Context) (string, error) {
+	return "", nil
+}
--- action/main.go
+++ action/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
 )
 
+func run() error {
+	ctx := context.Background()
+	if _, err := login(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	platform := getJobPlatform()
-	fmt.Printf("Hello, World - %s!", platform.String())
+	fmt.Printf("Hello, World - %s!\n", platform.String())
+	if err := run(); err != nil {
+		panic(err)
+	}
 }
