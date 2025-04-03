843dbadf8205d04211aef026d6d896110068ec52
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-04-02 19:35:11 -0700

    chore: action login (#15720)
    * chore: update action
    
    * chore: update
    
    * chore: main
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 85ffb8b1b5e4b4b435a72199c0ef8d9bfcc66e0a

---
+++ action/file.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
+)
+
+func getReleaseFiles(pattern string) ([]*v1pb.Release_File, error) {
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	var files []*v1pb.Release_File
+	for _, m := range matches {
+		content, err := os.ReadFile(m)
+		if err != nil {
+			return nil, err
+		}
+		base := filepath.Base(m)
+		t := v1pb.Release_File_DDL
+		switch {
+		case strings.Contains(base, "dml"):
+			t = v1pb.Release_File_DML
+		case strings.Contains(base, "ghost"):
+			t = v1pb.Release_File_DDL_GHOST
+		}
+		version := ""
+		fields := strings.Fields(base)
+		if len(fields) > 0 {
+			version = fields[0]
+		}
+		files = append(files, &v1pb.Release_File{
+			Path:       m,
+			Version:    version,
+			ChangeType: t,
+			Statement:  string(content),
+		})
+	}
+
+	return files, nil
+}
--- action/main.go
+++ action/main.go
@@ -2,9 +2,54 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/pkg/errors"
+
+	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
 )
+
+func run() error {
+	url, serviceAccount, serviceAccountSecret := os.Getenv("BYTEBASE_URL"), os.Getenv("BYTEBASE_SERVICE_ACCOUNT"), os.Getenv("BYTEBASE_SERVICE_ACCOUNT_SECRET")
+	if url == "" {
+		return errors.Errorf("environment BYTEBASE_URL is not set")
+	}
+	if serviceAccount == "" {
+		return errors.Errorf("environment BYTEBASE_SERVICE_ACCOUNT is not set")
+	}
+	if serviceAccountSecret == "" {
+		return errors.Errorf("environment BYTEBASE_SERVICE_ACCOUNT_SECRET is not set")
+	}
+	project, targets, filePattern := os.Getenv("BYTEBASE_PROJECT"), os.Getenv("BYTEBASE_TARGETS"), os.Getenv("FILE_PATTERN")
+	if project == "" {
+		return errors.Errorf("environment BYTEBASE_PROJECT is not set")
+	}
+	if targets == "" {
+		return errors.Errorf("environment BYTEBASE_TARGETS is not set")
+	}
+	if filePattern == "" {
+		return errors.Errorf("environment FILE_PATTERN is not set")
+	}
+
+	client, err := NewClient(url, serviceAccount, serviceAccountSecret)
+	if err != nil {
+		return err
+	}
+
+	releaseFiles, err := getReleaseFiles(filePattern)
+	if err != nil {
+		return err
+	}
+	if _, err := client.checkRelease(project, &v1pb.Release{Files: releaseFiles}); err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	platform := getJobPlatform()
-	fmt.Printf("Hello, World - %s!", platform.String())
+	fmt.Printf("Hello, World - %s!\n", platform.String())
+	if err := run(); err != nil {
+		panic(err)
+	}
 }
