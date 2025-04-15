4d402722c465875b9aa4ba1eadda5139e15bb4b6
p0ny <p0uy@outlook.com>
2025-04-15 16:55:19 +0800

    Update backend/plugin/schema/oracle/get_database_definition.go

--- backend/plugin/schema/oracle/get_database_definition.go
+++ backend/plugin/schema/oracle/get_database_definition.go
@@ -201,7 +201,7 @@ func writeIndex(buf *strings.Builder, table string, index *storepb.IndexMetadata
 			if _, err := buf.WriteString(`"`); err != nil {
 				return err
 			}
-			if len(index.Descending) > i && index.Descending[i] {
+			if i < len(index.Descending) && index.Descending[i] {
 				if _, err := buf.WriteString(` DESC`); err != nil {
 					return err
 				}
