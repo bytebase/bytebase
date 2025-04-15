648b347ccb9936aaa4301b6f3c3b52d529728d35
p0ny <xz@bytebase.com>
2025-04-15 11:11:17 +0800

    fix: sql (#15820)

--- backend/migrator/migrator_test.go
+++ backend/migrator/migrator_test.go
@@ -15,6 +15,21 @@ func TestLatestVersion(t *testing.T) {
 	require.Equal(t, semver.MustParse("3.6.1"), *files[len(files)-1].version)
 }
 
+func TestVersionUnique(t *testing.T) {
+	files, err := getSortedVersionedFiles()
+	require.NoError(t, err)
+	versions := make(map[string]struct{})
+	for _, file := range files {
+		if file.version == nil {
+			continue
+		}
+		if _, ok := versions[file.version.String()]; ok {
+			require.Fail(t, "duplicate version %s", file.version.String())
+		}
+		versions[file.version.String()] = struct{}{}
+	}
+}
+
 func TestGetVersionFromPath(t *testing.T) {
 	v, err := getVersionFromPath("migration/3.5/0000##vcs.sql")
 	require.NoError(t, err)
