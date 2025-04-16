ae2abddf38693deb967534c51a9f7650ab85d5c7
boojack <stevenlgtm@gmail.com>
2025-04-16 13:34:49 +0800

    chore: update auth context (#15853)
    * chore: update auth context
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: 5c64c8ab8f960b5418bfaa4427851c8d20f6091b
    Former-commit-id: 81d8f09e0b9706f1c9d0e14b6cf8f72b52217297

--- frontend/src/views/auth/SigninModal.vue
+++ frontend/src/views/auth/SigninModal.vue
@@ -1,12 +1,12 @@
 <template>
   <BBModal
-    :show="authStore.showLoginModal"
+    :show="shouldShow"
     :trap-focus="true"
     :show-close="false"
     :mask-closable="false"
     :header-class="'!hidden'"
   >
-    <div style="width: 30vw; height: 70vh" class="flex items-center">
+    <div class="flex items-center w-auto md:min-w-96 max-w-full h-auto md:py-4">
       <div class="flex flex-col justify-center items-center flex-1 space-y-2">
         <Signin :allow-signup="false">
           <template #footer>
@@ -22,9 +22,19 @@
 
 <script lang="tsx" setup>
 import { NButton } from "naive-ui";
-import { watch } from "vue";
+import { computed } from "vue";
 import { useRoute } from "vue-router";
 import { BBModal } from "@/bbkit";
+import {
+  AUTH_MFA_MODULE,
+  AUTH_OAUTH_CALLBACK_MODULE,
+  AUTH_OIDC_CALLBACK_MODULE,
+  AUTH_PASSWORD_FORGOT_MODULE,
+  AUTH_PASSWORD_RESET_MODULE,
+  AUTH_SIGNIN_ADMIN_MODULE,
+  AUTH_SIGNIN_MODULE,
+  AUTH_SIGNUP_MODULE,
+} from "@/router/auth";
 import { useAuthStore } from "@/store";
 import Signin from "@/views/auth/Signin.vue";
 
@@ -32,17 +42,28 @@ const route = useRoute();
 const authStore = useAuthStore();
 
 const logout = () => {
-  authStore.showLoginModal = false;
+  authStore.unauthenticatedOccurred = false;
   authStore.logout();
 };
 
-// Auto-close the modal when the route changed.
-watch(
-  () => route.name,
-  () => {
-    if (authStore.showLoginModal) {
-      authStore.showLoginModal = false;
-    }
+const shouldShow = computed(() => {
+  // Do not show the modal when the user is in auth related pages.
+  if (
+    route.name &&
+    [
+      AUTH_SIGNIN_MODULE,
+      AUTH_SIGNIN_ADMIN_MODULE,
+      AUTH_SIGNUP_MODULE,
+      AUTH_MFA_MODULE,
+      AUTH_PASSWORD_RESET_MODULE,
+      AUTH_PASSWORD_FORGOT_MODULE,
+      AUTH_OAUTH_CALLBACK_MODULE,
+      AUTH_OIDC_CALLBACK_MODULE,
+    ].includes(route.name.toString())
+  ) {
+    return false;
   }
-);
+
+  return Boolean(authStore.unauthenticatedOccurred);
+});
 </script>
