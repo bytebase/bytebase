33016cb97b7ec256852922c64b80449dd3b35563
boojack <stevenlgtm@gmail.com>
2025-04-16 15:10:09 +0800

    chore: add auth check timer (#15858)
    * chore: add auth check timer
    
    * chore: update
    
    Former-commit-id: ad80b089f9f3be8c192bb7300201a723d476bb6d
    Former-commit-id: 364a75becb28f8119fcc63d0274105925be9161d

--- frontend/src/AuthContext.vue
+++ frontend/src/AuthContext.vue
@@ -4,27 +4,55 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, watch } from "vue";
+import { onMounted, onUnmounted, ref, watch } from "vue";
 import { useRouter } from "vue-router";
+import { isAuthRelatedRoute } from "@/utils/auth";
 import SigninModal from "@/views/auth/SigninModal.vue";
 import { t } from "./plugins/i18n";
 import { AUTH_PASSWORD_RESET_MODULE } from "./router/auth";
 import { WORKSPACE_ROOT_MODULE } from "./router/dashboard/workspaceRoutes";
 import { useAuthStore, pushNotification, useWorkspaceV1Store } from "./store";
 
+// Check authorization every 60 seconds.
+const CHECK_AUTHORIZATION_INTERVAL = 60 * 1000;
+
 const router = useRouter();
 const authStore = useAuthStore();
 const workspaceStore = useWorkspaceV1Store();
 
+const authCheckIntervalId = ref<NodeJS.Timeout>();
+
 onMounted(() => {
-  if (!authStore.isLoggedIn) {
-    return;
-  }
-  if (authStore.requireResetPassword) {
-    router.replace({
-      name: AUTH_PASSWORD_RESET_MODULE,
-    });
-    return;
+  // Periodically checks if the user's session is still valid.
+  // Skips check if user is not logged in or on an auth-related route.
+  authCheckIntervalId.value = setInterval(async () => {
+    if (!authStore.isLoggedIn || authStore.unauthenticatedOccurred) {
+      return;
+    }
+    if (
+      router.currentRoute.value.name &&
+      isAuthRelatedRoute(router.currentRoute.value.name.toString())
+    ) {
+      return;
+    }
+
+    const user = await authStore.fetchCurrentUser();
+    if (!user) {
+      authStore.unauthenticatedOccurred = true;
+      pushNotification({
+        module: "bytebase",
+        style: "WARN",
+        title: t("auth.token-expired-title"),
+        description: t("auth.token-expired-description"),
+      });
+    }
+  }, CHECK_AUTHORIZATION_INTERVAL);
+});
+
+onUnmounted(() => {
+  // Clean up the interval when component is unmounted.
+  if (authCheckIntervalId.value) {
+    clearInterval(authCheckIntervalId.value);
   }
 });
 
@@ -55,8 +83,16 @@ watch(
 watch(
   () => authStore.isLoggedIn,
   async () => {
-    if (authStore.isLoggedIn) {
-      await workspaceStore.fetchIamPolicy();
+    if (!authStore.isLoggedIn) {
+      return;
+    }
+
+    await workspaceStore.fetchIamPolicy();
+    if (authStore.requireResetPassword) {
+      router.replace({
+        name: AUTH_PASSWORD_RESET_MODULE,
+      });
+      return;
     }
   },
   {
