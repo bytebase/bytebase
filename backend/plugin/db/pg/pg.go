8a62bc5fd368dc61740ed5f962cf0cf6c4542eb5
rebelice <yangrebelice@gmail.com>
2025-04-21 17:44:48 +0800

    fix: execute panic

--- backend/plugin/db/pg/pg.go
+++ backend/plugin/db/pg/pg.go
@@ -364,7 +364,8 @@ func (d *Driver) Execute(ctx context.Context, statement string, opts db.ExecuteO
 	if err == nil {
 		return affectedRows, nil
 	}
-	if !errors.As(err, &LockTimeoutError{}) {
+	var lockErr *LockTimeoutError
+	if !errors.As(err, &lockErr) {
 		return affectedRows, err
 	}
 
@@ -382,7 +383,7 @@ func (d *Driver) Execute(ctx context.Context, statement string, opts db.ExecuteO
 		if err == nil {
 			break
 		}
-		if !errors.As(err, &LockTimeoutError{}) {
+		if !errors.As(err, &lockErr) {
 			break
 		}
 	}
