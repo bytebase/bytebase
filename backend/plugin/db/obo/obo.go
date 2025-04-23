7b4b148e58334567ff72e6b364b782201cf1b64a
h3n4l <oysterdays@gmail.com>
2025-04-23 11:09:34 +0800

    refactor: use common position (#15938)
    Former-commit-id: 6db31d97c220d725d9d8aac55a72fe1b0d93277f

--- backend/plugin/db/obo/obo.go
+++ backend/plugin/db/obo/obo.go
@@ -110,11 +110,11 @@ func (d *Driver) Execute(ctx context.Context, statement string, opts db.ExecuteO
 		if err != nil {
 			return 0, &db.ErrorWithPosition{
 				Err: errors.Wrapf(err, "failed to execute context in a transaction"),
-				Start: &storepb.TaskRunResult_Position{
+				Start: &storepb.Position{
 					Line:   int32(singleSQL.FirstStatementLine),
 					Column: int32(singleSQL.FirstStatementColumn),
 				},
-				End: &storepb.TaskRunResult_Position{
+				End: &storepb.Position{
 					Line:   int32(singleSQL.LastLine),
 					Column: int32(singleSQL.LastColumn),
 				},
--- backend/plugin/db/pg/pg.go
+++ backend/plugin/db/pg/pg.go
@@ -494,11 +494,11 @@ func (d *Driver) tryExecute(
 
 					return &db.ErrorWithPosition{
 						Err: errors.Wrapf(err, "failed to execute context in a transaction"),
-						Start: &storepb.TaskRunResult_Position{
+						Start: &storepb.Position{
 							Line:   int32(command.FirstStatementLine),
 							Column: int32(command.FirstStatementColumn),
 						},
-						End: &storepb.TaskRunResult_Position{
+						End: &storepb.Position{
 							Line:   int32(command.LastLine),
 							Column: int32(command.LastColumn),
 						},
