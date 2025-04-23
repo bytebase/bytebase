7cfdfc9679b7480a516af4218a64f85d957c1742
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-04-22 20:22:41 -0700

    fix: sample plan check run key (#15939)

--- backend/api/v1/plan_service_plan_check.go
+++ backend/api/v1/plan_service_plan_check.go
@@ -56,7 +56,7 @@ func getPlanCheckRunsFromPlanSpecs(ctx context.Context, s *store.Store, plan *st
 		var updatedRuns []*store.PlanCheckRunMessage
 		countMap := make(map[string]int32)
 		for _, run := range planCheckRuns {
-			key := fmt.Sprintf("%s/%s/%s/%d", run.Type, run.Config.GetInstanceId(), run.Config.GetDatabaseName(), run.Config.GetSheetUid())
+			key := fmt.Sprintf("%s/%d", run.Type, run.Config.GetSheetUid())
 			if countMap[key] >= project.Setting.GetCiSamplingSize() {
 				continue
 			}
