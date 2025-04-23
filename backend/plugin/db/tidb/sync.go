3b376031852a820d03628d58f902f4ee0d8ab94d
d-bytebase <d@bytebase.com>
2025-04-23 15:02:57 -0700

    chore: fix tidb table charset

--- backend/plugin/db/tidb/sync.go
+++ backend/plugin/db/tidb/sync.go
@@ -436,6 +436,7 @@ func (d *Driver) SyncDBSchema(ctx context.Context) (*storepb.DatabaseSchemaMetad
 				DataFree:      dataFree,
 				CreateOptions: createOptions,
 				Comment:       comment,
+				Charset:       convertCollationToCharset(collation),
 				Partitions:    partitionTables[key],
 			}
 			if tableCollation.Valid {
@@ -485,6 +486,15 @@ func (d *Driver) SyncDBSchema(ctx context.Context) (*storepb.DatabaseSchemaMetad
 	}
 
 	return databaseMetadata, err
+}
+
+func convertCollationToCharset(collation string) string {
+	// See mappings from "SHOW CHARACTER SET;".
+	tokens := strings.Split(collation, "_")
+	if len(tokens) == 0 {
+		return collation
+	}
+	return tokens[0]
 }
 
 func setColumnMetadataDefault(column *storepb.ColumnMetadata, defaultStr sql.NullString, nullableBool bool, extra string) {
