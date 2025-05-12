b2f6c955e7f3faddeb85074919638b2a913d58a2
ecmadao <wlec@outlook.com>
2025-05-12 16:59:50 +0800

    chore: action bar for database info (#16084)

---
+++ frontend/src/views/sql-editor/AsidePanel/ActionBar/ActionBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="h-full flex flex-col items-stretch justify-between overflow-hidden text-sm p-1 shrink-0"
+  >
+    <div class="flex flex-col gap-y-1">
+      <TabItem
+        v-for="action in availableActions"
+        :key="action.view"
+        :action="action"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCurrentTabViewStateContext } from "../../EditorPanel/context/viewState.tsx";
+import TabItem from "./TabItem.vue";
+
+const { availableActions } = useCurrentTabViewStateContext();
+</script>
