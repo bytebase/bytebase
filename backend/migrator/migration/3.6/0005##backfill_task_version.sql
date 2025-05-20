91c72a135f5f7f7bbf055c8f58f5d968cfcf8e34
Danny <d@bytebase.com>
2025-05-20 00:49:40 -0700

    chore: update

--- backend/migrator/migration/3.6/0005##backfill_task_version.sql
+++ backend/migrator/migration/3.6/0005##backfill_task_version.sql
@@ -2,5 +2,5 @@ UPDATE task
 SET payload = payload - 'schema_version'
 WHERE 
     payload ? 'schema_version'
-    AND (payload->>'schema_version' ~ '.*[^0-9].*') -- contains non-digit;
+    AND (payload->>'schema_version' ~ '[^0-9]') -- contains non-digit
 ;
