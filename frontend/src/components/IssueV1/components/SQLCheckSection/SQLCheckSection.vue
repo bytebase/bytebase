3775850ae399e6390e6c5be836ec1d45f2697143
steven <stevenlgtm@gmail.com>
2025-05-21 14:36:15 +0800

    chore: risk level calculate func

--- frontend/src/components/IssueV1/components/SQLCheckSection/SQLCheckSection.vue
+++ frontend/src/components/IssueV1/components/SQLCheckSection/SQLCheckSection.vue
@@ -18,11 +18,17 @@
               <span class="opacity-80"
                 >{{ $t("task.check-type.affected-rows.self") }}:
               </span>
-              <span>{{ checkResult.affectedRows }}</span>
+              <span class="font-medium">{{ checkResult.affectedRows }}</span>
             </NTag>
           </template>
           {{ $t("task.check-type.affected-rows.description") }}
         </NTooltip>
+        <NTag round v-if="shouldShowRiskLevelWarning">
+          <span class="opacity-80">{{ $t("issue.risk-level.self") }}: </span>
+          <span class="font-medium">{{
+            $t(`issue.risk-level.${checkResult.riskLevel.toLowerCase()}`)
+          }}</span>
+        </NTag>
       </div>
 
       <SQLCheckButton
@@ -41,6 +47,7 @@ import { computed } from "vue";
 import { useIssueContext } from "@/components/IssueV1/logic";
 import { databaseForTask } from "@/components/Rollout/RolloutDetail";
 import { TaskTypeListWithStatement } from "@/types";
+import { CheckReleaseResponse_RiskLevel } from "@/types/proto/v1/release_service";
 import { Task_Type } from "@/types/proto/v1/rollout_service";
 import SQLCheckBadge from "./SQLCheckBadge.vue";
 import SQLCheckButton from "./SQLCheckButton.vue";
@@ -74,5 +81,13 @@ const checkResult = computed(() => {
     selectedTask.value
   );
   return resultMap.value[database.name] || undefined;
+});
+
+const shouldShowRiskLevelWarning = computed(() => {
+  return [
+    CheckReleaseResponse_RiskLevel.LOW,
+    CheckReleaseResponse_RiskLevel.MODERATE,
+    CheckReleaseResponse_RiskLevel.HIGH,
+  ].includes(checkResult.value.riskLevel);
 });
 </script>
