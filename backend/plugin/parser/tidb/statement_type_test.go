e48ee4238f2e52bdef40b9380ed7a57bbaf09c22
h3n4l <oysterdays@gmail.com>
2025-06-05 11:25:15 +0800

    refactor: move extractResourceList to query span to reduce parse times (#16409)
    * refactor: move extractResourceList to query span to reduce parse
    
    * refactor: move extractResourceList to query span to reduce parse times
    
    * chore: update
    
    * chore: fix linter
    
    * chore: reduce log
    
    * chore: logloglog
    
    Former-commit-id: 6d125f7034143e05f42dee3db281a37c00482133
    Former-commit-id: f532473fdccb44f65e378a20317adf4cd049ec5c

--- backend/plugin/parser/tidb/statement_type_test.go
+++ backend/plugin/parser/tidb/statement_type_test.go
@@ -35,7 +35,6 @@ func TestGetStatementType(t *testing.T) {
 	a.NoError(yaml.Unmarshal(byteValue, &tests))
 
 	for i, test := range tests {
-		t.Log(test.Statement)
 		asts, err := ParseTiDB(test.Statement, "", "")
 		a.NoError(err)
 
