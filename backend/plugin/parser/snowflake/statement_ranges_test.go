4298d764d92ede0e93eb021fd15e85f5e2657a5a
h3n4l <oysterdays@gmail.com>
2025-06-05 17:24:46 +0800

    feat: statement range for snowflake (#16416)
    * chore: update
    
    * chore: update
    
    * golangci-lint run --fix
    
    ---------
    
    Co-authored-by: p0ny <p0uy@outlook.com>
    Former-commit-id: a57c684da704d394f698caebd50b21319f900c6b

---
+++ backend/plugin/parser/snowflake/statement_ranges_test.go
@@ -0,0 +1,63 @@
+package snowflake
+
+import (
+	"context"
+	"io"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+
+	"github.com/bytebase/bytebase/backend/plugin/parser/base"
+)
+
+func TestGetStatementRange(t *testing.T) {
+	type testCase struct {
+		Statement string       `yaml:"statement,omitempty"`
+		Expected  []base.Range `yaml:"ranges,omitempty"`
+	}
+
+	const (
+		record      = false
+		testDataDir = "test-data/statement-ranges"
+	)
+	a := require.New(t)
+
+	// Recursively find all YAML files in the testDataDir
+	entries, err := os.ReadDir(testDataDir)
+	a.NoError(err)
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		filepath := path.Join(testDataDir, entry.Name())
+		yamlFile, err := os.Open(filepath)
+		a.NoError(err)
+		var testCases []testCase
+		byteValue, err := io.ReadAll(yamlFile)
+		a.NoError(err)
+		a.NoError(yamlFile.Close())
+		a.NoError(yaml.Unmarshal(byteValue, &testCases))
+		for i, tc := range testCases {
+			if tc.Statement == "" {
+				continue
+			}
+			ranges, err := GetStatementRanges(context.TODO(), base.StatementRangeContext{}, tc.Statement)
+			a.NoError(err)
+			if record {
+				testCases[i].Expected = ranges
+			} else {
+				a.Equal(tc.Expected, ranges, "statement: %s", tc.Statement)
+			}
+		}
+
+		if record {
+			yamlData, err := yaml.Marshal(testCases)
+			a.NoError(err)
+			err = os.WriteFile(filepath, yamlData, 0644)
+			a.NoError(err)
+		}
+	}
+}
