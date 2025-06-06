f3dce2cac67ea2d2a52918bca95a9269dd288c7e
p0ny <xz@bytebase.com>
2025-06-06 14:32:00 +0800

    feat(action): print version and check server version (#16431)
    * feat(action): print action version
    
    * chore: check version
    
    * fix: lint
    
    Former-commit-id: 0bdd66a0d8c0eb2ce146c58a135c393594722bde

--- action/main.go
+++ action/main.go
@@ -200,6 +200,9 @@ func runCheck(*cobra.Command, []string) error {
 		return err
 	}
 
+	// Check version compatibility
+	checkVersionCompatibility(client, args.Version)
+
 	releaseFiles, err := getReleaseFiles(Config.FilePattern)
 	if err != nil {
 		return err
@@ -267,6 +270,9 @@ func runRollout(command *cobra.Command, _ []string) error {
 	if err != nil {
 		return errors.Wrapf(err, "failed to create client")
 	}
+
+	// Check version compatibility
+	checkVersionCompatibility(client, args.Version)
 
 	var plan *v1pb.Plan
 	if Config.Plan != "" {
@@ -573,8 +579,38 @@ func waitForRollout(ctx context.Context, client *Client, rolloutPreview *v1pb.Ro
 	return nil
 }
 
+func checkVersionCompatibility(client *Client, cliVersion string) {
+	if cliVersion == "unknown" {
+		slog.Warn("CLI version unknown, unable to check compatibility")
+		return
+	}
+
+	actuatorInfo, err := client.getActuatorInfo()
+	if err != nil {
+		slog.Warn("Unable to get server version for compatibility check", "error", err)
+		return
+	}
+
+	serverVersion := actuatorInfo.Version
+	if serverVersion == "" {
+		slog.Warn("Server version is empty, unable to check compatibility")
+		return
+	}
+
+	if cliVersion == "latest" {
+		slog.Warn("Using 'latest' CLI version. It is recommended to use a specific version like bytebase-action:" + serverVersion + " to match your Bytebase server version " + serverVersion)
+		return
+	}
+
+	if cliVersion != serverVersion {
+		slog.Warn("CLI version mismatch", "cliVersion", cliVersion, "serverVersion", serverVersion, "recommendation", "use bytebase-action:"+serverVersion+" to match your Bytebase server")
+	} else {
+		slog.Info("CLI version matches server version", "version", cliVersion)
+	}
+}
+
 func main() {
-	slog.Info("bytebase-action built at commit " + args.Gitcommit)
+	slog.Info("bytebase-action version " + args.Version + " built at commit " + args.Gitcommit)
 	ctx, cancel := context.WithCancel(context.Background())
 	c := make(chan os.Signal, 1)
 	// Trigger graceful shutdown on SIGINT or SIGTERM.
