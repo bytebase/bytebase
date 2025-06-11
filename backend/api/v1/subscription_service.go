3cb58333531bfae681cb6541de54c1f59711b734
Danny Xu <98006139+d-bytebase@users.noreply.github.com>
2025-06-11 22:40:25 +0800

    chore: cleanup license code (#16494)
    * chore: cleanup
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    Former-commit-id: a47468bd4dbd45673411d9821361e1cda61d7d71
    Former-commit-id: d610904876073a1d33ebd5c271ab238bc8d1ab0c

--- backend/api/v1/subscription_service.go
+++ backend/api/v1/subscription_service.go
@@ -2,11 +2,9 @@ package v1
 
 import (
 	"context"
-	"time"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/bytebase/bytebase/backend/common"
 	"github.com/bytebase/bytebase/backend/component/config"
@@ -41,43 +39,17 @@ func NewSubscriptionService(
 
 // GetSubscription gets the subscription.
 func (s *SubscriptionService) GetSubscription(ctx context.Context, _ *v1pb.GetSubscriptionRequest) (*v1pb.Subscription, error) {
-	return s.loadSubscription(ctx)
+	return s.licenseService.LoadSubscription(ctx), nil
 }
 
 // UpdateSubscription updates the subscription license.
 func (s *SubscriptionService) UpdateSubscription(ctx context.Context, request *v1pb.UpdateSubscriptionRequest) (*v1pb.Subscription, error) {
-	principalID, ok := ctx.Value(common.PrincipalIDContextKey).(int)
-	if !ok {
-		return nil, status.Errorf(codes.Internal, "principal ID not found")
-	}
-	if err := s.licenseService.StoreLicense(ctx, &enterprise.SubscriptionPatch{
-		UpdaterID: principalID,
-		License:   request.Patch.License,
-	}); err != nil {
+	if err := s.licenseService.StoreLicense(ctx, request.Patch.License); err != nil {
 		if common.ErrorCode(err) == common.Invalid {
 			return nil, status.Error(codes.InvalidArgument, err.Error())
 		}
 		return nil, status.Errorf(codes.Internal, "failed to store license: %v", err.Error())
 	}
 
-	return s.loadSubscription(ctx)
-}
-
-func (s *SubscriptionService) loadSubscription(ctx context.Context) (*v1pb.Subscription, error) {
-	sub := s.licenseService.LoadSubscription(ctx)
-
-	subscription := &v1pb.Subscription{
-		SeatCount:     int32(sub.Seat),
-		InstanceCount: int32(sub.InstanceCount),
-		Plan:          sub.Plan,
-		Trialing:      sub.Trialing,
-		OrgId:         sub.OrgID,
-		OrgName:       sub.OrgName,
-	}
-	if sub.Plan != v1pb.PlanType_FREE {
-		subscription.ExpiresTime = timestamppb.New(time.Unix(sub.ExpiresTS, 0))
-		subscription.StartedTime = timestamppb.New(time.Unix(sub.StartedTS, 0))
-	}
-
-	return subscription, nil
+	return s.licenseService.LoadSubscription(ctx), nil
 }
