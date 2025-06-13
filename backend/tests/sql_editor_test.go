ad76538921e0fdd1de2dda97325e7d4a7f1ec6de
p0ny <xz@bytebase.com>
2025-06-13 11:40:35 +0800

    feat: migrate tests to using connect rpc clients (#16516)
    * feat: migrate tests to using connect rpc clients
    
    * simplify auth token capture
    
    * fix: golangci-lint
    
    * chore: update test
    
    * chore: update

--- backend/tests/sql_editor_test.go
+++ backend/tests/sql_editor_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"connectrpc.com/connect"
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/testing/protocmp"
@@ -142,7 +143,7 @@ func TestAdminQueryAffectedRows(t *testing.T) {
 	}()
 	a.NoError(err)
 
-	mysqlInstance, err := ctl.instanceServiceClient.CreateInstance(ctx, &v1pb.CreateInstanceRequest{
+	mysqlInstanceResp, err := ctl.instanceServiceClient.CreateInstance(ctx, connect.NewRequest(&v1pb.CreateInstanceRequest{
 		InstanceId: generateRandomString("instance", 10),
 		Instance: &v1pb.Instance{
 			Title:       "mysqlInstance",
@@ -151,10 +152,11 @@ func TestAdminQueryAffectedRows(t *testing.T) {
 			Activation:  true,
 			DataSources: []*v1pb.DataSource{{Type: v1pb.DataSourceType_ADMIN, Host: mysqlContainer.host, Port: mysqlContainer.port, Username: "root", Password: "root-password", Id: "admin"}},
 		},
-	})
+	}))
 	a.NoError(err)
+	mysqlInstance := mysqlInstanceResp.Msg
 
-	pgInstance, err := ctl.instanceServiceClient.CreateInstance(ctx, &v1pb.CreateInstanceRequest{
+	pgInstanceResp, err := ctl.instanceServiceClient.CreateInstance(ctx, connect.NewRequest(&v1pb.CreateInstanceRequest{
 		InstanceId: generateRandomString("instance", 10),
 		Instance: &v1pb.Instance{
 			Title:       "pgInstance",
@@ -163,8 +165,9 @@ func TestAdminQueryAffectedRows(t *testing.T) {
 			Activation:  true,
 			DataSources: []*v1pb.DataSource{{Type: v1pb.DataSourceType_ADMIN, Host: pgContainer.host, Port: pgContainer.port, Username: "postgres", Password: "root-password", Id: "admin"}},
 		},
-	})
+	}))
 	a.NoError(err)
+	pgInstance := pgInstanceResp.Msg
 
 	for _, tt := range tests {
 		var instance *v1pb.Instance
@@ -181,19 +184,21 @@ func TestAdminQueryAffectedRows(t *testing.T) {
 		err = ctl.createDatabaseV2(ctx, ctl.project, instance, nil /* environment */, tt.databaseName, databaseOwner)
 		a.NoError(err)
 
-		database, err := ctl.databaseServiceClient.GetDatabase(ctx, &v1pb.GetDatabaseRequest{
+		databaseResp, err := ctl.databaseServiceClient.GetDatabase(ctx, connect.NewRequest(&v1pb.GetDatabaseRequest{
 			Name: fmt.Sprintf("%s/databases/%s", instance.Name, tt.databaseName),
-		})
+		}))
 		a.NoError(err)
+		database := databaseResp.Msg
 
-		sheet, err := ctl.sheetServiceClient.CreateSheet(ctx, &v1pb.CreateSheetRequest{
+		sheetResp, err := ctl.sheetServiceClient.CreateSheet(ctx, connect.NewRequest(&v1pb.CreateSheetRequest{
 			Parent: ctl.project.Name,
 			Sheet: &v1pb.Sheet{
 				Title:   "prepareStatements",
 				Content: []byte(tt.prepareStatements),
 			},
-		})
+		}))
 		a.NoError(err)
+		sheet := sheetResp.Msg
 
 		err = ctl.changeDatabase(ctx, ctl.project, database, sheet, v1pb.Plan_ChangeDatabaseConfig_MIGRATE)
 		a.NoError(err)
