1023373b96db042d897b490ab90340158c460bb1
p0ny <p0uy@outlook.com>
2025-06-16 13:17:26 +0800

    feat: register grpc gateway

--- backend/server/grpc_routes.go
+++ backend/server/grpc_routes.go
@@ -2,9 +2,11 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	connectcors "connectrpc.com/cors"
 	grpcruntime "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
@@ -21,6 +23,7 @@ import (
 	"github.com/bytebase/bytebase/backend/runner/metricreport"
 	"github.com/bytebase/bytebase/backend/runner/schemasync"
 	"github.com/bytebase/bytebase/backend/store"
+	v1pb "github.com/bytebase/bytebase/proto/generated-go/v1"
 	"github.com/bytebase/bytebase/proto/generated-go/v1/v1connect"
 )
 
@@ -39,8 +42,8 @@ func withCORS(h http.Handler) http.Handler {
 }
 
 func configureGrpcRouters(
-	_ context.Context,
-	_ *grpcruntime.ServeMux,
+	ctx context.Context,
+	mux *grpcruntime.ServeMux,
 	_ *grpc.Server,
 	stores *store.Store,
 	sheetManager *sheet.Manager,
@@ -185,88 +188,106 @@ func configureGrpcRouters(
 	reviewConfigPath, reviewConfigHandler := v1connect.NewReviewConfigServiceHandler(reviewConfigService)
 	connectHandlers[reviewConfigPath] = withCORS(reviewConfigHandler)
 
-	// Register services.
-	// v1pb.RegisterSubscriptionServiceServer(grpcServer, apiv1.NewSubscriptionService(
-	// 	stores,
-	// 	profile,
-	// 	metricReporter,
-	// 	licenseService))
-	// InstanceService is now handled by Connect RPC
-	// ProjectService is now handled by Connect RPC
-	// DatabaseService is now handled by Connect RPC
-	// v1pb.RegisterDatabaseCatalogServiceServer(grpcServer, apiv1.NewDatabaseCatalogService(stores, licenseService))
-	// v1pb.RegisterInstanceRoleServiceServer(grpcServer, apiv1.NewInstanceRoleService(stores, dbFactory))
-	// v1pb.RegisterOrgPolicyServiceServer(grpcServer, apiv1.NewOrgPolicyService(stores, licenseService))
-	// v1pb.RegisterIdentityProviderServiceServer(grpcServer, apiv1.NewIdentityProviderService(stores, licenseService))
-	// SettingService is now handled by Connect RPC
-	// SQLService is now handled by Connect RPC
-	// releaseService := apiv1.NewReleaseService(stores, sheetManager, schemaSyncer, dbFactory)
-	// v1pb.RegisterReleaseServiceServer(grpcServer, releaseService)
-	// PlanService is now handled by Connect RPC
-	// IssueService is now handled by Connect RPC
-	// RolloutService is now handled by Connect RPC
-	// RoleService is now handled by Connect RPC
-	// SheetService is now handled by Connect RPC
-	// DatabaseGroupService is now handled by Connect RPC
-	// v1pb.RegisterChangelistServiceServer(grpcServer, apiv1.NewChangelistService(stores, profile, iamManager))
-	// GroupService is now handled by Connect RPC
-	// v1pb.RegisterReviewConfigServiceServer(grpcServer, apiv1.NewReviewConfigService(stores, licenseService))
+	// REST gateway proxy.
+	grpcEndpoint := fmt.Sprintf(":%d", profile.Port)
+	grpcConn, err := grpc.NewClient(
+		grpcEndpoint,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultCallOptions(
+			grpc.MaxCallRecvMsgSize(100*1024*1024), // Set MaxCallRecvMsgSize to 100M so that users can receive up to 100M via REST calls.
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
 
-	// All services have been migrated to Connect RPC
-
-	// Sort by service name, align with api.bytebase.com.
-	// ActuatorService is now handled by Connect RPC
-	// UserService is now handled by Connect RPC
-	// AuditLogService is now handled by Connect RPC
-	// AuthService is now handled by Connect RPC
-	// CelService is now handled by Connect RPC
-	// ChangelistService is now handled by Connect RPC
-	// if err := v1pb.RegisterChangelistServiceHandler(ctx, mux, grpcConn); err != nil {
-	// 	return nil, err
-	// }
-	// DatabaseGroupService is now handled by Connect RPC
-	// DatabaseService is now handled by Connect RPC
-	// RevisionService is now handled by Connect RPC
-	// DatabaseCatalogService is now handled by Connect RPC
-	// if err := v1pb.RegisterDatabaseCatalogServiceHandler(ctx, mux, grpcConn); err != nil {
-	// 	return nil, err
-	// }
-	// GroupService is now handled by Connect RPC
-	// IdentityProviderService is now handled by Connect RPC
-	// if err := v1pb.RegisterIdentityProviderServiceHandler(ctx, mux, grpcConn); err != nil {
-	// 	return nil, err
-	// }
-	// InstanceRoleService is now handled by Connect RPC
-	// if err := v1pb.RegisterInstanceRoleServiceHandler(ctx, mux, grpcConn); err != nil {
-	// 	return nil, err
-	// }
-	// InstanceService is now handled by Connect RPC
-	// IssueService is now handled by Connect RPC
-	// OrgPolicyService is now handled by Connect RPC
-	// if err := v1pb.RegisterOrgPolicyServiceHandler(ctx, mux, grpcConn); err != nil {
-	// 	return nil, err
-	// }
-	// PlanService is now handled by Connect RPC
-	// ProjectService is now handled by Connect RPC
-	// ReviewConfigService is now handled by Connect RPC
-	// if err := v1pb.RegisterReviewConfigServiceHandler(ctx, mux, grpcConn); err != nil {
-	// 	return nil, err
-	// }
-	// RiskService is now handled by Connect RPC
-	// RoleService is now handled by Connect RPC
-	// RolloutService is now handled by Connect RPC
-	// SQLService is now handled by Connect RPC
-	// SettingService is now handled by Connect RPC
-	// SheetService is now handled by Connect RPC
-	// SubscriptionService is now handled by Connect RPC
-	// if err := v1pb.RegisterSubscriptionServiceHandler(ctx, mux, grpcConn); err != nil {
-	// 	return nil, err
-	// }
-	// WorksheetService is now handled by Connect RPC
-	// WorkspaceService is now handled by Connect RPC
-	// ReleaseService is now handled by Connect RPC
-	// if err := v1pb.RegisterReleaseServiceHandler(ctx, mux, grpcConn); err != nil {
-	// 	return nil, err
-	// }
+	// Register all services in alphabetical order
+	if err := v1pb.RegisterActuatorServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterAuditLogServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterAuthServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterCelServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterChangelistServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterDatabaseCatalogServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterDatabaseGroupServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterDatabaseServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterGroupServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterIdentityProviderServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterInstanceRoleServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterInstanceServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterIssueServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterOrgPolicyServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterPlanServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterProjectServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterReleaseServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterReviewConfigServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterRevisionServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterRiskServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterRoleServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterRolloutServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterSettingServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterSheetServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterSQLServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterSubscriptionServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterUserServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterWorksheetServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
+	if err := v1pb.RegisterWorkspaceServiceHandler(ctx, mux, grpcConn); err != nil {
+		return nil, err
+	}
 	return connectHandlers, nil
 }
