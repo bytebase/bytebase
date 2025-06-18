d852ef7af2b56cf5d2976f9a74199fa55a9d33c4
p0ny <xz@bytebase.com>
2025-06-18 12:48:10 +0800

    feat: set-cookie on connect handlers. grpc-gateway propagates set-cookie header (#16591)
    * feat: set-cookie on connect handlers. grpc-gateway propagates set-cookie header
    
    * chore: update
    
    ---------
    
    Co-authored-by: Danny <d@bytebase.com>
    Former-commit-id: 477711ff26ffc41fc544cbd489b1dab966cfe96e

--- backend/api/auth/header.go
+++ backend/api/auth/header.go
@@ -22,48 +22,39 @@ type GatewayResponseModifier struct {
 }
 
 // Modify is the mux option for modifying response header.
-func (m *GatewayResponseModifier) Modify(ctx context.Context, response http.ResponseWriter, _ proto.Message) error {
+func (*GatewayResponseModifier) Modify(ctx context.Context, response http.ResponseWriter, _ proto.Message) error {
 	md, ok := runtime.ServerMetadataFromContext(ctx)
 	if !ok {
 		return errors.Errorf("failed to get ServerMetadata from context in the gateway response modifier")
 	}
 
-	isHTTPS := false
-	for _, v := range md.HeaderMD.Get(GatewayMetadataRequestOriginKey) {
-		if strings.HasPrefix(v, "https") {
-			isHTTPS = true
+	if vs := md.HeaderMD.Get("Set-Cookie"); len(vs) > 0 {
+		for _, v := range vs {
+			response.Header().Add("Set-Cookie", v)
 		}
 	}
-	m.processMetadata(ctx, md, GatewayMetadataAccessTokenKey, AccessTokenCookieName, true /* httpOnly */, isHTTPS, response)
 	return nil
 }
 
-func (m *GatewayResponseModifier) processMetadata(ctx context.Context, md runtime.ServerMetadata, metadataKey, cookieName string, httpOnly, isHTTPS bool, response http.ResponseWriter) {
-	values := md.HeaderMD.Get(metadataKey)
-	if len(values) == 0 {
-		return
-	}
-	value := values[0]
-	if value == "" {
-		// Unset cookie.
-		http.SetCookie(response, &http.Cookie{
-			Name:    cookieName,
+// token="" => unset
+func GetTokenCookie(ctx context.Context, stores *store.Store, licenseService *enterprise.LicenseService, origin, token string) *http.Cookie {
+	if token == "" {
+		return &http.Cookie{
+			Name:    AccessTokenCookieName,
 			Value:   "",
 			Expires: time.Unix(0, 0),
 			Path:    "/",
-		})
-		return
+		}
 	}
-
-	// Set cookie.
+	isHTTPS := strings.HasPrefix(origin, "https")
 	sameSite := http.SameSiteStrictMode
 	if isHTTPS {
 		sameSite = http.SameSiteNoneMode
 	}
-	tokenDuration := GetTokenDuration(ctx, m.Store, m.LicenseService)
-	http.SetCookie(response, &http.Cookie{
-		Name:  cookieName,
-		Value: value,
+	tokenDuration := GetTokenDuration(ctx, stores, licenseService)
+	return &http.Cookie{
+		Name:  AccessTokenCookieName,
+		Value: token,
 		// CookieExpDuration expires slightly earlier than the jwt expiration. Client would be logged out if the user
 		// cookie expires, thus the client would always logout first before attempting to make a request with the expired jwt.
 		// Suppose we have a valid refresh token, we will refresh the token in 2 cases:
@@ -72,11 +63,11 @@ func (m *GatewayResponseModifier) processMetadata(ctx context.Context, md runtim
 		Expires: time.Now().Add(tokenDuration - 1*time.Second),
 		Path:    "/",
 		// Http-only helps mitigate the risk of client side script accessing the protected cookie.
-		HttpOnly: httpOnly,
+		HttpOnly: true,
 		// See https://github.com/bytebase/bytebase/issues/31.
 		Secure:   isHTTPS,
 		SameSite: sameSite,
-	})
+	}
 }
 
 func GetTokenDuration(ctx context.Context, store *store.Store, licenseService *enterprise.LicenseService) time.Duration {
