5ad6467b7eb246bcd96f6eba0042e18e46fd6821
p0ny <p0uy@outlook.com>
2025-06-19 11:51:38 +0800

    migrate methods with audit

--- frontend/src/grpcweb/methods.ts
+++ frontend/src/grpcweb/methods.ts
@@ -1,74 +1,77 @@
-import { ActuatorServiceDefinition } from "@/types/proto/v1/actuator_service";
-import { AuditLogServiceDefinition } from "@/types/proto/v1/audit_log_service";
-import { AuthServiceDefinition } from "@/types/proto/v1/auth_service";
-import { CelServiceDefinition } from "@/types/proto/v1/cel_service";
-import { ChangelistServiceDefinition } from "@/types/proto/v1/changelist_service";
-import { DatabaseGroupServiceDefinition } from "@/types/proto/v1/database_group_service";
-import { DatabaseServiceDefinition } from "@/types/proto/v1/database_service";
-import { GroupServiceDefinition } from "@/types/proto/v1/group_service";
-import { IdentityProviderServiceDefinition } from "@/types/proto/v1/idp_service";
-import { InstanceServiceDefinition } from "@/types/proto/v1/instance_service";
-import { IssueServiceDefinition } from "@/types/proto/v1/issue_service";
-import { OrgPolicyServiceDefinition } from "@/types/proto/v1/org_policy_service";
-import { PlanServiceDefinition } from "@/types/proto/v1/plan_service";
-import { ProjectServiceDefinition } from "@/types/proto/v1/project_service";
-import { ReleaseServiceDefinition } from "@/types/proto/v1/release_service";
-import { ReviewConfigServiceDefinition } from "@/types/proto/v1/review_config_service";
-import { RiskServiceDefinition } from "@/types/proto/v1/risk_service";
-import { RoleServiceDefinition } from "@/types/proto/v1/role_service";
-import { RolloutServiceDefinition } from "@/types/proto/v1/rollout_service";
-import { SettingServiceDefinition } from "@/types/proto/v1/setting_service";
-import { SheetServiceDefinition } from "@/types/proto/v1/sheet_service";
-import { SQLServiceDefinition } from "@/types/proto/v1/sql_service";
-import { SubscriptionServiceDefinition } from "@/types/proto/v1/subscription_service";
-import { UserServiceDefinition } from "@/types/proto/v1/user_service";
-import { WorksheetServiceDefinition } from "@/types/proto/v1/worksheet_service";
-import { WorkspaceServiceDefinition } from "@/types/proto/v1/workspace_service";
-
-// The code of audit field in generated method options.
-// A workaround code for the outdated ts-proto version in buf community plugins.
-// This hack can be removed once the ts-proto version is upgraded.
-// TODO(steven): remove me later.
-const AUDIT_TAG_CODE = "800024";
+import { getOption } from '@bufbuild/protobuf';
+import { ActuatorService } from "@/types/proto-es/v1/actuator_service_pb";
+import { audit } from "@/types/proto-es/v1/annotation_pb";
+import { AuditLogService } from "@/types/proto-es/v1/audit_log_service_pb";
+import { AuthService } from "@/types/proto-es/v1/auth_service_pb";
+import { CelService } from "@/types/proto-es/v1/cel_service_pb";
+import { ChangelistService } from "@/types/proto-es/v1/changelist_service_pb";
+import { DatabaseCatalogService } from "@/types/proto-es/v1/database_catalog_service_pb";
+import { DatabaseGroupService } from "@/types/proto-es/v1/database_group_service_pb";
+import { DatabaseService } from "@/types/proto-es/v1/database_service_pb";
+import { GroupService } from "@/types/proto-es/v1/group_service_pb";
+import { IdentityProviderService } from "@/types/proto-es/v1/idp_service_pb";
+import { InstanceRoleService } from "@/types/proto-es/v1/instance_role_service_pb";
+import { InstanceService } from "@/types/proto-es/v1/instance_service_pb";
+import { IssueService } from "@/types/proto-es/v1/issue_service_pb";
+import { OrgPolicyService } from "@/types/proto-es/v1/org_policy_service_pb";
+import { PlanService } from "@/types/proto-es/v1/plan_service_pb";
+import { ProjectService } from "@/types/proto-es/v1/project_service_pb";
+import { ReleaseService } from "@/types/proto-es/v1/release_service_pb";
+import { RevisionService } from "@/types/proto-es/v1/revision_service_pb";
+import { ReviewConfigService } from "@/types/proto-es/v1/review_config_service_pb";
+import { RiskService } from "@/types/proto-es/v1/risk_service_pb";
+import { RoleService } from "@/types/proto-es/v1/role_service_pb";
+import { RolloutService } from "@/types/proto-es/v1/rollout_service_pb";
+import { SettingService } from "@/types/proto-es/v1/setting_service_pb";
+import { SheetService } from "@/types/proto-es/v1/sheet_service_pb";
+import { SQLService } from "@/types/proto-es/v1/sql_service_pb";
+import { SubscriptionService } from "@/types/proto-es/v1/subscription_service_pb";
+import { UserService } from "@/types/proto-es/v1/user_service_pb";
+import { WorksheetService } from "@/types/proto-es/v1/worksheet_service_pb";
+import { WorkspaceService } from "@/types/proto-es/v1/workspace_service_pb";
 
 // The methods that have audit field in their options.
 // Format: /bytebase.v1.ServiceName/MethodName
 export const ALL_METHODS_WITH_AUDIT = [
-  ActuatorServiceDefinition,
-  AuditLogServiceDefinition,
-  AuthServiceDefinition,
-  CelServiceDefinition,
-  ChangelistServiceDefinition,
-  DatabaseGroupServiceDefinition,
-  DatabaseServiceDefinition,
-  GroupServiceDefinition,
-  IdentityProviderServiceDefinition,
-  InstanceServiceDefinition,
-  IssueServiceDefinition,
-  OrgPolicyServiceDefinition,
-  PlanServiceDefinition,
-  ProjectServiceDefinition,
-  ReleaseServiceDefinition,
-  ReviewConfigServiceDefinition,
-  RiskServiceDefinition,
-  RoleServiceDefinition,
-  RolloutServiceDefinition,
-  SettingServiceDefinition,
-  SheetServiceDefinition,
-  SQLServiceDefinition,
-  SubscriptionServiceDefinition,
-  UserServiceDefinition,
-  WorksheetServiceDefinition,
-  WorkspaceServiceDefinition,
+  ActuatorService,
+  AuditLogService,
+  AuthService,
+  CelService,
+  ChangelistService,
+  DatabaseCatalogService,
+  DatabaseGroupService,
+  DatabaseService,
+  GroupService,
+  IdentityProviderService,
+  InstanceRoleService,
+  InstanceService,
+  IssueService,
+  OrgPolicyService,
+  PlanService,
+  ProjectService,
+  ReleaseService,
+  RevisionService,
+  ReviewConfigService,
+  RiskService,
+  RoleService,
+  RolloutService,
+  SettingService,
+  SheetService,
+  SQLService,
+  SubscriptionService,
+  UserService,
+  WorksheetService,
+  WorkspaceService,
 ]
-  .map((serviceDefinition) => {
+  .map((service) => {
     const methods: string[] = [];
-    for (const method of Object.values(serviceDefinition.methods)) {
-      const fullName = serviceDefinition.fullName;
-      if (method.options._unknownFields?.[AUDIT_TAG_CODE]?.length > 0) {
-        methods.push(`/${fullName}/${method.name}`);
+    for (const method of service.methods) {
+      const auditOption = getOption(method, audit);
+      if (auditOption) {
+        methods.push(`/${service.typeName}/${method.name}`);
       }
     }
     return methods;
   })
-  .flat();
+  .flat()
+  .sort();
