9b43cdcca493b4f103df74df2d06a1b0553979fa
Vincent Huang <40749774+vsai12@users.noreply.github.com>
2025-06-19 22:48:28 -0700

    chore: add aws-iam (#16612)
    * chore: add aws-iam
    
    * chore: update

---
+++ backend/plugin/db/elasticsearch/elasticsearch_test.go
@@ -0,0 +1,58 @@
+package elasticsearch
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/bytebase/bytebase/backend/plugin/db"
+	storepb "github.com/bytebase/bytebase/proto/generated-go/store"
+)
+
+func TestOpenWithAWSAuth(t *testing.T) {
+	tests := []struct {
+		name    string
+		config  db.ConnectionConfig
+		wantErr string
+	}{
+		{
+			name: "missing region",
+			config: db.ConnectionConfig{
+				DataSource: &storepb.DataSource{
+					Host:               "search-test.us-east-1.es.amazonaws.com",
+					Port:               "443",
+					AuthenticationType: storepb.DataSource_AWS_RDS_IAM,
+				},
+			},
+			wantErr: "region is required for AWS IAM authentication",
+		},
+		{
+			name: "basic auth still works",
+			config: db.ConnectionConfig{
+				DataSource: &storepb.DataSource{
+					Host:               "localhost",
+					Port:               "9200",
+					Username:           "elastic",
+					AuthenticationType: storepb.DataSource_PASSWORD,
+				},
+				Password: "password123",
+			},
+			// This will fail to connect but should not error during Open
+			wantErr: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			driver := &Driver{}
+			_, err := driver.Open(context.Background(), storepb.Engine_ELASTICSEARCH, tt.config)
+			if tt.wantErr != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tt.wantErr)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
