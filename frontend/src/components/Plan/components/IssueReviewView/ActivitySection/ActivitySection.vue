c695f65366efce1d11b3d34447a6b133a048f5bc
steven <stevenlgtm@gmail.com>
2025-06-23 11:53:37 +0800

    chore: issue activities in review section

---
+++ frontend/src/components/Plan/components/IssueReviewView/ActivitySection/ActivitySection.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="px-4 py-4 space-y-6">
+    <div class="flex items-center justify-between">
+      <h3 class="text-base font-medium">
+        {{ $t("common.activity") }}
+      </h3>
+    </div>
+    <IssueCommentList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import IssueCommentList from "@/components/IssueV1/components/IssueCommentSection/IssueCommentList.vue";
+</script>
