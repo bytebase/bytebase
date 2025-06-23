a302ce54cd25121585f355cf1a6215787a5e8041
ecmadao <wlec@outlook.com>
2025-06-23 11:29:15 +0800

    chore: optimize advanced search (#16630)
    * chore: auto-fetch next page
    
    * chore: optimize advanced search

--- frontend/src/components/SyncDatabaseSchemaV1/TargetDatabasesSelectPanel.vue
+++ frontend/src/components/SyncDatabaseSchemaV1/TargetDatabasesSelectPanel.vue
@@ -17,6 +17,7 @@
         :autofocus="false"
         :placeholder="$t('database.filter-database')"
         :scope-options="scopeOptions"
+        :override-route-query="false"
       />
 
       <PagedDatabaseTable
