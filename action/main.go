68eee7f01133ef72642b14d592b08cba0aa54924
p0ny <xz@bytebase.com>
2025-07-08 12:01:21 +0800

    refactor: action pkgs (#16804)
    Former-commit-id: 1efffe7a784bbc62ef686373cec07a1b1aafa78b
    Former-commit-id: 3e55e26a11433ec21495db07f2f4bb65b73f8957

--- action/main.go
+++ action/main.go
@@ -9,39 +9,10 @@ import (
 	"syscall"
 
 	"github.com/bytebase/bytebase/action/args"
+	"github.com/bytebase/bytebase/action/command"
 	"github.com/bytebase/bytebase/action/world"
 	"github.com/bytebase/bytebase/backend/common/log"
 )
-
-func checkVersionCompatibility(client *Client, cliVersion string) {
-	if cliVersion == "unknown" {
-		slog.Warn("CLI version unknown, unable to check compatibility")
-		return
-	}
-
-	actuatorInfo, err := client.getActuatorInfo()
-	if err != nil {
-		slog.Warn("Unable to get server version for compatibility check", "error", err)
-		return
-	}
-
-	serverVersion := actuatorInfo.Version
-	if serverVersion == "" {
-		slog.Warn("Server version is empty, unable to check compatibility")
-		return
-	}
-
-	if cliVersion == "latest" {
-		slog.Warn("Using 'latest' CLI version. It is recommended to use a specific version like bytebase-action:" + serverVersion + " to match your Bytebase server version " + serverVersion)
-		return
-	}
-
-	if cliVersion != serverVersion {
-		slog.Warn("CLI version mismatch", "cliVersion", cliVersion, "serverVersion", serverVersion, "recommendation", "use bytebase-action:"+serverVersion+" to match your Bytebase server")
-	} else {
-		slog.Info("CLI version matches server version", "version", cliVersion)
-	}
-}
 
 func main() {
 	slog.Info("bytebase-action version " + args.Version + " built at commit " + args.Gitcommit)
@@ -58,7 +29,7 @@ func main() {
 	}()
 
 	w := world.NewWorld()
-	cmd := NewRootCommand(w)
+	cmd := command.NewRootCommand(w)
 
 	if err := cmd.ExecuteContext(ctx); err != nil {
 		slog.Error("failed to execute command", log.BBError(err))
