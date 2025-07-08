f536562ea3b500ad57ec166a8b5f1a3a122a83dd
h3n4l <oysterdays@gmail.com>
2025-07-08 15:27:06 +0800

    fix: test

--- backend/plugin/schema/oracle/generate_migration.go
+++ backend/plugin/schema/oracle/generate_migration.go
@@ -1043,17 +1043,11 @@ func writeAddColumn(out *strings.Builder, schema, table string, column *storepb.
 	_, _ = out.WriteString(`" `)
 	_, _ = out.WriteString(column.Type)
 
-	if column.DefaultExpression != "" {
+	defaultExpr := getDefaultExpression(column)
+	if defaultExpr != "" {
 		_, _ = out.WriteString(` DEFAULT `)
-		_, _ = out.WriteString(column.DefaultExpression)
-	} else if column.Default != "" {
-		_, _ = out.WriteString(` DEFAULT '`)
-		_, _ = out.WriteString(column.Default)
-		_, _ = out.WriteString(`'`)
-	} else if column.DefaultNull {
-		_, _ = out.WriteString(` DEFAULT NULL`)
+		_, _ = out.WriteString(defaultExpr)
 	}
-
 	if !column.Nullable {
 		_, _ = out.WriteString(` NOT NULL`)
 	}
