7ea4fa0dbd1a47e97936c79c8264ab09de2c371a
steven <stevenlgtm@gmail.com>
2025-07-23 17:26:55 +0800

    chore: update plan check results

--- frontend/src/components/Plan/logic/plan-check.ts
+++ frontend/src/components/Plan/logic/plan-check.ts
@@ -1,8 +1,12 @@
+import { flatten } from "lodash-es";
+import { useDBGroupStore } from "@/store";
+import { isValidDatabaseGroupName, isValidDatabaseName } from "@/types";
+import { DatabaseGroupView } from "@/types/proto-es/v1/database_group_service_pb";
 import type {
   Plan_Spec,
   PlanCheckRun,
 } from "@/types/proto-es/v1/plan_service_pb";
-import { sheetNameForSpec, targetsForSpec } from "./plan";
+import { sheetNameForSpec } from "./plan";
 
 export const planSpecHasPlanChecks = (spec: Plan_Spec) => {
   if (spec.config?.case === "changeDatabaseConfig") {
@@ -11,11 +15,38 @@ export const planSpecHasPlanChecks = (spec: Plan_Spec) => {
   return false;
 };
 
+// flattenTargetsOfSpec flattens the targets of a plan spec, which can be a mix of database names and database group names.
+export const flattenTargetsOfSpec = (spec: Plan_Spec): string[] => {
+  let rawTargets: string[] = [];
+  if (spec.config.case === "changeDatabaseConfig") {
+    rawTargets = spec.config.value.targets || [];
+  }
+  if (spec.config.case === "exportDataConfig") {
+    rawTargets = spec.config.value.targets || [];
+  }
+  const targets = flatten(
+    rawTargets.map((target) => {
+      if (isValidDatabaseName(target)) {
+        return target;
+      }
+      if (isValidDatabaseGroupName(target)) {
+        const dbGroup = useDBGroupStore().getDBGroupByName(
+          target,
+          DatabaseGroupView.FULL
+        );
+        return dbGroup?.matchedDatabases.map((db) => db.name);
+      }
+      return target;
+    })
+  ).filter(Boolean) as string[];
+  return targets;
+};
+
 export const planCheckRunListForSpec = (
   planCheckRunList: PlanCheckRun[],
   spec: Plan_Spec
 ) => {
-  const targets = targetsForSpec(spec);
+  const targets = flattenTargetsOfSpec(spec);
   const sheet = sheetNameForSpec(spec);
   return planCheckRunList.filter((check) => {
     if (!targets.includes(check.target)) {
