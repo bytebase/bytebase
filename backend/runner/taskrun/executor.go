65632a07f2db9e543e5498b578e22cc62d82cd03
p0ny <xz@bytebase.com>
2025-08-05 16:13:42 +0800

    chore: update (#17076)
    Former-commit-id: e8291af9052bf6665c66cdbaf732d852e9831021

--- backend/runner/taskrun/executor.go
+++ backend/runner/taskrun/executor.go
@@ -341,7 +341,7 @@ func postMigration(ctx context.Context, stores *store.Store, mc *migrateContext,
 	if _, err := stores.UpdateDatabase(ctx, &store.UpdateDatabaseMessage{
 		InstanceID:   database.InstanceID,
 		DatabaseName: database.DatabaseName,
-		Metadata: []func(*storepb.DatabaseMetadata){func(md *storepb.DatabaseMetadata) {
+		MetadataUpdates: []func(*storepb.DatabaseMetadata){func(md *storepb.DatabaseMetadata) {
 			md.Drifted = false
 		}},
 	}); err != nil {
@@ -521,7 +521,7 @@ func endMigration(ctx context.Context, storeInstance *store.Store, mc *migrateCo
 				if _, err := storeInstance.UpdateDatabase(ctx, &store.UpdateDatabaseMessage{
 					InstanceID:   mc.database.InstanceID,
 					DatabaseName: mc.database.DatabaseName,
-					Metadata: []func(*storepb.DatabaseMetadata){func(md *storepb.DatabaseMetadata) {
+					MetadataUpdates: []func(*storepb.DatabaseMetadata){func(md *storepb.DatabaseMetadata) {
 						md.Version = mc.version
 					}},
 				}); err != nil {
