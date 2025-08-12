3f355a513fbd0a37a043205e3693c0f0c8e16a1f
Danny <d@bytebase.com>
2025-08-12 01:19:40 -0700

    fix: format profile.go to fix golangci-lint issue

--- backend/bin/server/cmd/profile.go
+++ backend/bin/server/cmd/profile.go
@@ -11,19 +11,19 @@ import (
 
 func getBaseProfile(dataDir string) *config.Profile {
 	config := &config.Profile{
-		ExternalURL:   flags.externalURL,
-		Port:          flags.port,     // Using flags.port as our gRPC server port.
-		DatastorePort: flags.port + 2, // Using flags.port + 2 as our datastore port.
-		HA:            flags.ha,
-		SaaS:          flags.saas,
-		EnableJSONLogging:  flags.enableJSONLogging,
-		IsDocker:           isDocker(),
-		DataDir:            dataDir,
-		Demo:               flags.demo,
-		Version:            version,
-		GitCommit:          gitcommit,
-		PgURL:              os.Getenv("PG_URL"),
-		DeployID:           uuid.NewString()[:8],
+		ExternalURL:       flags.externalURL,
+		Port:              flags.port,     // Using flags.port as our gRPC server port.
+		DatastorePort:     flags.port + 2, // Using flags.port + 2 as our datastore port.
+		HA:                flags.ha,
+		SaaS:              flags.saas,
+		EnableJSONLogging: flags.enableJSONLogging,
+		IsDocker:          isDocker(),
+		DataDir:           dataDir,
+		Demo:              flags.demo,
+		Version:           version,
+		GitCommit:         gitcommit,
+		PgURL:             os.Getenv("PG_URL"),
+		DeployID:          uuid.NewString()[:8],
 	}
 
 	config.LastActiveTS.Store(time.Now().Unix())
