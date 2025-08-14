59bdf47eb9a6f5ebe2dafc20cd59267318e5a701
ecmadao <wlec@outlook.com>
2025-08-14 14:08:36 +0800

    chore: use environment name component (#17171)
    * chore: handle null environment
    
    * fix: lint
    
    * chore: use environment name component

--- frontend/src/components/IssueV1/components/IssueCommentSection/IssueCommentView/StageName.vue
+++ frontend/src/components/IssueV1/components/IssueCommentSection/IssueCommentView/StageName.vue
@@ -1,21 +1,20 @@
 <template>
   <span class="font-medium text-main">
-    {{ environment.title }}
+    <EnvironmentV1Name
+      :link="false"
+      :environment="environmentStore.getEnvironmentByName(stage.environment)"
+    />
   </span>
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
+import { EnvironmentV1Name } from "@/components/v2";
 import { useEnvironmentV1Store } from "@/store";
 import type { Stage } from "@/types/proto-es/v1/rollout_service_pb";
 
-const props = defineProps<{
+defineProps<{
   stage: Stage;
 }>();
 
 const environmentStore = useEnvironmentV1Store();
-
-const environment = computed(() =>
-  environmentStore.getEnvironmentByName(props.stage.environment)
-);
 </script>
