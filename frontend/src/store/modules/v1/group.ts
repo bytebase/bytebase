b9e8bac3326eb4eb88b5da4f8668ff8f0199aa6d
ecmadao <wlec@outlook.com>
2025-08-15 14:15:24 +0800

    chore: update

--- frontend/src/store/modules/v1/group.ts
+++ frontend/src/store/modules/v1/group.ts
@@ -73,12 +73,6 @@ export const useGroupStore = defineStore("group", () => {
     groups: Group[];
     nextPageToken: string;
   }> => {
-    if (!hasWorkspacePermissionV2("bb.groups.list")) {
-      return {
-        groups: [],
-        nextPageToken: "",
-      };
-    }
     const request = create(ListGroupsRequestSchema, {
       pageSize: params.pageSize,
       pageToken: params.pageToken,
@@ -97,9 +91,6 @@ export const useGroupStore = defineStore("group", () => {
   };
 
   const batchFetchGroups = async (groupNameList: string[]) => {
-    if (!hasWorkspacePermissionV2("bb.groups.get")) {
-      return;
-    }
     if (groupNameList.length === 0) {
       return [];
     }
