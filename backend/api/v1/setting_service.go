cf449dd03156631ffbac133cd2f8d5ed9fe45ee8
ecmadao <wlec@outlook.com>
2025-08-15 10:14:20 +0800

    chore: remove dev guard

--- backend/api/v1/setting_service.go
+++ backend/api/v1/setting_service.go
@@ -507,36 +507,18 @@ func (s *SettingService) UpdateSetting(ctx context.Context, request *connect.Req
 		for _, env := range oldEnvironmentSetting.Environments {
 			if !newEnvIDMap[env.Id] {
 				// deleted
-				if s.profile.Mode == common.ReleaseModeDev {
-					emptyStr := ""
-					if _, err := s.store.UpdateInstanceV2(ctx, &store.UpdateInstanceMessage{
-						EnvironmentID:       &emptyStr,
-						FindByEnvironmentID: &env.Id,
-					}); err != nil {
-						return nil, connect.NewError(connect.CodeInternal, errors.Wrapf(err, "failed to unset environment %v for instances", env.Id))
-					}
-					if _, err := s.store.BatchUpdateDatabases(ctx, nil, &store.BatchUpdateDatabases{
-						EnvironmentID:       &emptyStr,
-						FindByEnvironmentID: &env.Id,
-					}); err != nil {
-						return nil, connect.NewError(connect.CodeInternal, errors.Wrapf(err, "failed to unset environment %v for databases", env.Id))
-					}
-				} else {
-					// check if instances are using the environments
-					count, err := s.store.CountInstance(ctx, &store.CountInstanceMessage{EnvironmentID: &env.Id})
-					if err != nil {
-						return nil, connect.NewError(connect.CodeInternal, err)
-					}
-					if count > 0 {
-						return nil, connect.NewError(connect.CodeFailedPrecondition, errors.Errorf("all instances in the environment %v should be deleted first", env.Id))
-					}
-					uses, err := s.store.CheckDatabaseUseEnvironment(ctx, env.Id)
-					if err != nil {
-						return nil, connect.NewError(connect.CodeInternal, err)
-					}
-					if uses {
-						return nil, connect.NewError(connect.CodeFailedPrecondition, errors.Errorf("all databases in the environment %v should be deleted first", env.Id))
-					}
+				emptyStr := ""
+				if _, err := s.store.UpdateInstanceV2(ctx, &store.UpdateInstanceMessage{
+					EnvironmentID:       &emptyStr,
+					FindByEnvironmentID: &env.Id,
+				}); err != nil {
+					return nil, connect.NewError(connect.CodeInternal, errors.Wrapf(err, "failed to unset environment %v for instances", env.Id))
+				}
+				if _, err := s.store.BatchUpdateDatabases(ctx, nil, &store.BatchUpdateDatabases{
+					EnvironmentID:       &emptyStr,
+					FindByEnvironmentID: &env.Id,
+				}); err != nil {
+					return nil, connect.NewError(connect.CodeInternal, errors.Wrapf(err, "failed to unset environment %v for databases", env.Id))
 				}
 			}
 		}
