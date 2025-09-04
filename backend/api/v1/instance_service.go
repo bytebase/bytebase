7e115d0899c6810bb296f3463008efbb9b096662
ecmadao <wlec@outlook.com>
2025-09-04 14:13:46 +0800

    chore: update

--- backend/api/v1/instance_service.go
+++ backend/api/v1/instance_service.go
@@ -857,7 +857,7 @@ func (s *InstanceService) UpdateDataSource(ctx context.Context, req *connect.Req
 			dataSource.MasterPassword = req.Msg.DataSource.MasterPassword
 		case "extra_connection_parameters":
 			dataSource.ExtraConnectionParameters = req.Msg.DataSource.ExtraConnectionParameters
-		case "iam_extension":
+		case "azure_credential", "aws_credential", "gcp_credential":
 			switch req.Msg.DataSource.AuthenticationType {
 			case v1pb.DataSource_AZURE_IAM:
 				if azureCredential := req.Msg.DataSource.GetAzureCredential(); azureCredential != nil {
