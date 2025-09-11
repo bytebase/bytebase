6351730a23e3878e36534c6400acc8110b710f67
ecmadao <wlec@outlook.com>
2025-09-11 14:43:27 +0800

    fix: lint

--- frontend/src/components/ProfileDropdown.vue
+++ frontend/src/components/ProfileDropdown.vue
@@ -312,7 +312,12 @@ const options = computed((): DropdownOption[] => [
           : SQL_EDITOR_HOME_MODULE,
       });
       return (
-        <a class="menu-item" href={link.fullPath} target="_blank" rel="noopener noreferrer">
+        <a
+          class="menu-item"
+          href={link.fullPath}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {isSQLEditorRoute(router)
             ? t(
                 "settings.general.workspace.database-change-mode.go-to-workspace"
