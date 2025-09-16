f9bccb5096b473bbe75ef5fee535ce4266aa6a50
ecmadao <wlec@outlook.com>
2025-09-16 15:02:22 +0800

    chore: show matched database (#17425)
    * chore: optimize database group
    
    * chore: update
    
    * Update frontend/src/store/modules/dbGroup.ts
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * chore: update
    
    * fix: lint
    
    * chore: calculate the unmatched databases
    
    * chore: update
    
    * chore: update
    
    * fix: test
    
    * chore: show matched database
    
    * chore: update
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- frontend/src/views/sql-editor/ConnectionPanel/ConnectionPane/DatabaseGroupTag.vue
+++ frontend/src/views/sql-editor/ConnectionPanel/ConnectionPane/DatabaseGroupTag.vue
@@ -13,6 +13,7 @@
 import { NTag, NTooltip } from "naive-ui";
 import { computed } from "vue";
 import { useDatabaseGroupByName } from "@/store";
+import { DatabaseGroupView } from "@/types/proto-es/v1/database_group_service_pb";
 
 const props = defineProps<{
   databaseGroupName: string;
@@ -23,6 +24,7 @@ defineEmits<{
 }>();
 
 const { databaseGroup } = useDatabaseGroupByName(
-  computed(() => props.databaseGroupName)
+  computed(() => props.databaseGroupName),
+  DatabaseGroupView.FULL
 );
 </script>
