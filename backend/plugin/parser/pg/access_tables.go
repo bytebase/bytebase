020b2fccf5a0634c0e98287c27d7407f6c47e0ea
h3n4l <oysterdays@gmail.com>
2025-09-17 16:12:18 +0800

    refactor: remove unused

--- backend/plugin/parser/pg/access_tables.go
+++ backend/plugin/parser/pg/access_tables.go
@@ -66,3 +66,39 @@ func (l *querySpanExtractor) EnterQualified_name(qn *postgresql.Qualified_nameCo
 
 	l.accessTables = append(l.accessTables, resource)
 }
+
+// isMixedQuery checks whether the query accesses the user table and system table at the same time.
+func isMixedQuery(m base.SourceColumnSet) (bool, bool) {
+	hasSystem, hasUser := false, false
+	for table := range m {
+		if isSystemResource(table) {
+			hasSystem = true
+		} else {
+			hasUser = true
+		}
+	}
+
+	if hasSystem && hasUser {
+		return false, true
+	}
+
+	return !hasUser && hasSystem, false
+}
+
+func isSystemResource(resource base.ColumnResource) bool {
+	// User can access the system table/view by name directly without database/schema name.
+	// For example: `SELECT * FROM pg_database`, which will access the system table `pg_database`.
+	// Additionally, user can create a table/view with the same name with system table/view and access them
+	// by specify the schema name, for example:
+	// `CREATE TABLE pg_database(id INT); SELECT * FROM public.pg_database;` which will access the user table `pg_database`.
+	if IsSystemSchema(resource.Schema) {
+		return true
+	}
+	if resource.Schema == "" && IsSystemView(resource.Table) {
+		return true
+	}
+	if resource.Schema == "" && IsSystemTable(resource.Table) {
+		return true
+	}
+	return false
+}
