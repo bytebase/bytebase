3df069582bb5f0ee43feda8dec348ecb5815ef10
p0ny <p0uy@outlook.com>
2025-10-20 17:26:32 +0800

    chore: update csp headers
    allow it to have update notifications

--- backend/server/echo_routes.go
+++ backend/server/echo_routes.go
@@ -110,7 +110,7 @@ func securityHeadersMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 		"script-src 'self' " + strings.Join(scriptHashes, " ") + " 'wasm-unsafe-eval'; " +
 		"style-src 'self' 'unsafe-inline'; " +
 		"img-src 'self' data: blob:; " +
-		"connect-src 'self' data: ws: wss:; " +
+		"connect-src 'self' data: ws: wss: https://api.github.com; " +
 		"font-src 'self'; " +
 		"object-src 'none'; " +
 		"base-uri 'self'; " +
