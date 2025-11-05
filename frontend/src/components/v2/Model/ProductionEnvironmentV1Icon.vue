02b4bcc84a0efc1cf8918031bfb9ab0a5137ea64
p0ny <xz@bytebase.com>
2025-11-05 15:38:50 +0800

    fix: [intlify] Not found parent scope. use the global scope (#17992)
    * fix: [intlify] Not found parent scope. use the global scope
    
    * format
    
    * format

--- frontend/src/components/v2/Model/ProductionEnvironmentV1Icon.vue
+++ frontend/src/components/v2/Model/ProductionEnvironmentV1Icon.vue
@@ -8,16 +8,19 @@
       />
     </template>
 
-    <span>{{ $t("environment.production-environment") }}</span>
+    <span>{{ t("environment.production-environment") }}</span>
   </NTooltip>
 </template>
 
 <script lang="ts" setup>
 import { NTooltip } from "naive-ui";
 import { computed } from "vue";
+import { useI18n } from "vue-i18n";
 import { featureToRef } from "@/store";
 import { PlanFeature } from "@/types/proto-es/v1/subscription_service_pb";
 import type { Environment } from "@/types/v1/environment";
+
+const { t } = useI18n();
 
 const props = withDefaults(
   defineProps<{
