603fc7eb7af2500474237797d863a82ddaec21a5
ecmadao <wlec@outlook.com>
2025-11-27 10:59:18 +0800

    fix: lint

--- frontend/src/components/InstanceForm/constants.ts
+++ frontend/src/components/InstanceForm/constants.ts
@@ -64,8 +64,7 @@ export const EngineList = computed(() => {
 
 export const EngineIconPath: Record<string, string> = {
   [Engine.MYSQL]: new URL("@/assets/db/mysql.png", import.meta.url).href,
-  [Engine.POSTGRES]: new URL("@/assets/db/postgres.png", import.meta.url)
-    .href,
+  [Engine.POSTGRES]: new URL("@/assets/db/postgres.png", import.meta.url).href,
   [Engine.TIDB]: new URL("@/assets/db/tidb.png", import.meta.url).href,
   [Engine.SNOWFLAKE]: new URL("@/assets/db/snowflake.png", import.meta.url)
     .href,
@@ -76,8 +75,7 @@ export const EngineIconPath: Record<string, string> = {
   [Engine.REDIS]: new URL("@/assets/db/redis.png", import.meta.url).href,
   [Engine.ORACLE]: new URL("@/assets/db/oracle.svg", import.meta.url).href,
   [Engine.MSSQL]: new URL("@/assets/db/mssql.svg", import.meta.url).href,
-  [Engine.REDSHIFT]: new URL("@/assets/db/redshift.svg", import.meta.url)
-    .href,
+  [Engine.REDSHIFT]: new URL("@/assets/db/redshift.svg", import.meta.url).href,
   [Engine.MARIADB]: new URL("@/assets/db/mariadb.png", import.meta.url).href,
   [Engine.OCEANBASE]: new URL(
     "@/assets/db/oceanbase-mysql.svg",
@@ -91,18 +89,13 @@ export const EngineIconPath: Record<string, string> = {
     "@/assets/db/elasticsearch.svg",
     import.meta.url
   ).href,
-  [Engine.BIGQUERY]: new URL("@/assets/db/bigquery.svg", import.meta.url)
-    .href,
-  [Engine.DYNAMODB]: new URL("@/assets/db/dynamodb.svg", import.meta.url)
-    .href,
+  [Engine.BIGQUERY]: new URL("@/assets/db/bigquery.svg", import.meta.url).href,
+  [Engine.DYNAMODB]: new URL("@/assets/db/dynamodb.svg", import.meta.url).href,
   [Engine.DATABRICKS]: new URL("@/assets/db/databricks.svg", import.meta.url)
     .href,
-  [Engine.COCKROACHDB]: new URL(
-    "@/assets/db/cockroachdb.png",
-    import.meta.url
-  ).href,
-  [Engine.COSMOSDB]: new URL("@/assets/db/cosmosdb.svg", import.meta.url)
+  [Engine.COCKROACHDB]: new URL("@/assets/db/cockroachdb.png", import.meta.url)
     .href,
+  [Engine.COSMOSDB]: new URL("@/assets/db/cosmosdb.svg", import.meta.url).href,
   [Engine.CASSANDRA]: new URL("@/assets/db/cassandra.svg", import.meta.url)
     .href,
   [Engine.TRINO]: new URL("@/assets/db/trino.svg", import.meta.url).href,
