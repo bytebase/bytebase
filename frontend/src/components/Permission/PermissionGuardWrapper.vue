e9a9c8b242982967122de6004dce628d038b1e44
ecmadao <wlec@outlook.com>
2026-01-07 17:54:18 +0800

    fix: use div wrap (#18803)

--- frontend/src/components/Permission/PermissionGuardWrapper.vue
+++ frontend/src/components/Permission/PermissionGuardWrapper.vue
@@ -1,17 +1,19 @@
 <template>
-  <NTooltip :disabled="missedPermissions.length === 0">
-    <template #trigger>
-      <slot :disabled="missedPermissions.length > 0" />
-    </template>
-    <div class="flex flex-col gap-1">
-      {{ project ? $t("common.missing-required-permission-for-resource", { resource: project.name }) : $t("common.missing-required-permission") }}
-      <ul class="list-disc pl-4">
-        <li v-for="permission in missedPermissions" :key="permission">
-          {{ permission }}
-        </li>
-      </ul>
-    </div>
-  </NTooltip>
+  <div>
+    <NTooltip :disabled="missedPermissions.length === 0">
+      <template #trigger>
+        <slot :disabled="missedPermissions.length > 0" />
+      </template>
+      <div class="flex flex-col gap-1">
+        {{ project ? $t("common.missing-required-permission-for-resource", { resource: project.name }) : $t("common.missing-required-permission") }}
+        <ul class="list-disc pl-4">
+          <li v-for="permission in missedPermissions" :key="permission">
+            {{ permission }}
+          </li>
+        </ul>
+      </div>
+    </NTooltip>
+  </div>
 </template>
 
 <script lang="tsx" setup>
