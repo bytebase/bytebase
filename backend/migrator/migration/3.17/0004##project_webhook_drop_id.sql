88a87f24f9f47bdff8d043ba468335d6b038a72d
ecmadao <wlec@outlook.com>
2026-03-13 20:36:25 +0800

    chore: update

--- backend/migrator/migration/3.17/0004##project_webhook_drop_id.sql
+++ backend/migrator/migration/3.17/0004##project_webhook_drop_id.sql
@@ -5,6 +5,7 @@ ALTER TABLE project_webhook DROP CONSTRAINT IF EXISTS project_webhook_pkey;
 DROP INDEX IF EXISTS idx_project_webhook_unique_resource_id;
 
 ALTER TABLE project_webhook DROP COLUMN IF EXISTS id;
-ALTER TABLE project_webhook ADD PRIMARY KEY (resource_id);
+
+ALTER TABLE project_webhook ADD CONSTRAINT project_webhook_pkey PRIMARY KEY (resource_id) NOT VALID;
 
 DROP SEQUENCE IF EXISTS project_webhook_id_seq;
