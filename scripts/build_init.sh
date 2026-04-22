c34d0069080df7c6c63068c07316e14748e6457c
p0ny <xz@bytebase.com>
2026-04-22 11:32:16 +0800

    chore: align Go precheck version (#20079)

--- scripts/build_init.sh
+++ scripts/build_init.sh
@@ -30,7 +30,7 @@ mkdir_output() {
 }
 
 # Go and node version checks.
-TARGET_GO_VERSION="1.24.4"
+TARGET_GO_VERSION="1.26.0"
 GO_VERSION=`go version | { read _ _ v _; echo ${v#go}; }`
 if [ "$(version ${GO_VERSION})" -lt "$(version $TARGET_GO_VERSION)" ];
 then
