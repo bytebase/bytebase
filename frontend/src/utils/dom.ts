0c707e9b5c77f49c51eebee15e1468200c30aa73
ecmadao <wlec@outlook.com>
2026-06-04 23:52:27 +0800

    refactor: remove vue dependency

--- frontend/src/utils/dom.ts
+++ frontend/src/utils/dom.ts
@@ -1,5 +1,3 @@
-import { onBeforeUnmount, type Ref, unref, watch } from "vue";
-
 export const isDescendantOf = (
   node: Element | null | undefined,
   selector: string
@@ -42,47 +40,4 @@ export const getScrollParent = (
 
 export const nextAnimationFrame = () => {
   return new Promise<number>((resolve) => requestAnimationFrame(resolve));
-};
-
-export const usePreventBackAndForward = (
-  elemRef: Ref<HTMLElement | null | undefined> | HTMLElement = document.body
-) => {
-  const preventBackForward = (elem: HTMLElement) => {
-    function handleWheel(e: WheelEvent) {
-      const maxX = elem.scrollWidth - elem.clientWidth;
-      const scrollTarget = elem.scrollLeft + e.deltaX;
-      if (scrollTarget < 0 || scrollTarget > maxX) {
-        e.preventDefault();
-      }
-    }
-
-    elem.addEventListener("wheel", handleWheel, {
-      passive: false,
-    });
-
-    return () => {
-      elem.removeEventListener("wheel", handleWheel);
-    };
-  };
-
-  let unregister: () => void;
-  watch(
-    () => unref(elemRef),
-    (elem) => {
-      if (!elem) return;
-      if (unregister) {
-        unregister();
-      }
-      unregister = preventBackForward(elem);
-    },
-    {
-      immediate: true,
-    }
-  );
-
-  onBeforeUnmount(() => {
-    if (unregister) {
-      unregister();
-    }
-  });
 };
