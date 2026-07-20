92f6b4fc8a301c750054d472cdba2d1260cc730a
ecmadao <wlec@outlook.com>
2026-07-20 16:31:13 +0800

    chore: update

--- backend/migrator/migrator_test.go
+++ backend/migrator/migrator_test.go
@@ -17,8 +17,8 @@ import (
 func TestLatestVersion(t *testing.T) {
 	files, err := getSortedVersionedFiles()
 	require.NoError(t, err)
-	require.Equal(t, semver.MustParse("3.21.1"), *files[len(files)-1].version)
-	require.Equal(t, "migration/3.21/0001##backfill_ui_plan_draft_issues.sql", files[len(files)-1].path)
+	require.Equal(t, semver.MustParse("3.21.2"), *files[len(files)-1].version)
+	require.Equal(t, "migration/3.21/0002##migrate_instance_sync_databases.sql", files[len(files)-1].path)
 }
 
 func TestVersionUnique(t *testing.T) {
@@ -36,7 +36,7 @@ func TestVersionUnique(t *testing.T) {
 	}
 }
 
-func TestMigration3_20_5_MigrateInstanceSyncDatabases(t *testing.T) {
+func TestMigration3_21_2_MigrateInstanceSyncDatabases(t *testing.T) {
 	ctx := context.Background()
 	container := testcontainer.GetTestPgContainer(ctx, t)
 	t.Cleanup(func() { container.Close(ctx) })
@@ -70,7 +70,7 @@ func TestMigration3_20_5_MigrateInstanceSyncDatabases(t *testing.T) {
 	_, err := db.ExecContext(ctx, setup)
 	require.NoError(t, err)
 
-	statement, err := migrationFS.ReadFile("migration/3.20/0005##migrate_instance_sync_databases.sql")
+	statement, err := migrationFS.ReadFile("migration/3.21/0002##migrate_instance_sync_databases.sql")
 	require.NoError(t, err)
 
 	_, err = db.ExecContext(ctx, string(statement))
